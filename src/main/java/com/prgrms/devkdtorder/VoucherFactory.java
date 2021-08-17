c934270414c06919b57a8116ff1c9f601dad61a2
maenguin <maenguin924@gmail.com>
2021-08-17 21:50:29 +0900

    feat : voucherFactory에 uuid 없는 생성자 추가

--- src/main/java/com/prgrms/devkdtorder/VoucherFactory.java
+++ src/main/java/com/prgrms/devkdtorder/VoucherFactory.java
@@ -5,7 +5,12 @@ import java.util.UUID;
 public class VoucherFactory {
 
     public static Voucher create(VoucherType type, UUID voucherId, long value) {
-        if (type == VoucherType.FiXEDAMOUNT) return new FixedAmountVoucher(voucherId, value);
+        if (type == VoucherType.FIXEDAMOUNT) return new FixedAmountVoucher(voucherId, value);
         return new PercentDiscountVoucher(voucherId,value);
+    }
+
+    public static Voucher create(VoucherType type, long value) {
+        if (type == VoucherType.FIXEDAMOUNT) return new FixedAmountVoucher(UUID.randomUUID(), value);
+        return new PercentDiscountVoucher(UUID.randomUUID(),value);
     }
 }
