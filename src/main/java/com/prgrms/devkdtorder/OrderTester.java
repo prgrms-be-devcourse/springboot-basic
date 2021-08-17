dc49e5a10cb8371de076e336e9daf6bf263db827
maenguin <maenguin924@gmail.com>
2021-08-17 21:59:23 +0900

    feat : OrderTester에 커맨드라인앱 실행 코드 추가

--- src/main/java/com/prgrms/devkdtorder/OrderTester.java
+++ src/main/java/com/prgrms/devkdtorder/OrderTester.java
@@ -1,5 +1,6 @@
 package com.prgrms.devkdtorder;
 
+import com.prgrms.devkdtorder.cla.CommandLineApplication;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.util.Assert;
 
@@ -11,14 +12,17 @@ public class OrderTester {
 
         AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfiguration.class);
 
-        var customerId = UUID.randomUUID();
-        var orderItems = new ArrayList<OrderItem>() {{
-            add(new OrderItem(UUID.randomUUID(), 100L, 1));
-        }};
+//        var customerId = UUID.randomUUID();
+//        var orderItems = new ArrayList<OrderItem>() {{
+//            add(new OrderItem(UUID.randomUUID(), 100L, 1));
+//        }};
+//
+//        OrderService orderService = applicationContext.getBean(OrderService.class);
+//        Order order = orderService.createOrder(customerId, orderItems);
+//
+//        Assert.isTrue(order.totalAmount() == 90L, "totalAmount "+order.totalAmount());
 
-        OrderService orderService = applicationContext.getBean(OrderService.class);
-        Order order = orderService.createOrder(customerId, orderItems);
-
-        Assert.isTrue(order.totalAmount() == 90L, "totalAmount "+order.totalAmount());
+        CommandLineApplication bean = applicationContext.getBean(CommandLineApplication.class);
+        bean.run();
     }
 }
