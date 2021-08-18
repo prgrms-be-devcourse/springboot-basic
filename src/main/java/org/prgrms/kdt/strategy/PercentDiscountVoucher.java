698c7eb8d2f0e0ad8f13e41a9d810b4178a7f271
suhyunsim <cnc1106@naver.com>
2021-08-18 18:26:28 +0900

    feat: PercentDiscountVoucer, FixedAmountVoucher 구현
    - Voucher 인터페이스를 implement해서 퍼센트 할인인 PercentDiscountVoucher를 구현한다
    - Voucher 인터페이스를 implement해서 가격 할인인 FixedAmountVoucher를 구현한다

---
+++ src/main/java/org/prgrms/kdt/strategy/PercentDiscountVoucher.java
@@ -0,0 +1,18 @@
+package org.prgrms.kdt.strategy;
+
+import java.util.UUID;
+
+public class PercentDiscountVoucher implements Voucher {
+    private final UUID voucherId;
+    private final long percent;
+
+    public PercentDiscountVoucher(UUID voucherId, long percent) {
+        this.voucherId = voucherId;
+        this.percent = percent;
+    }
+
+    @Override
+    public long discount(long beforeDiscount) {
+        return beforeDiscount * (percent / 100);
+    }
+}
