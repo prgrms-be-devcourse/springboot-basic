4a4acfda6709dabbf018111fe843d0ef4083a9d0
0923kdh <0923kdh@gmail.com>
2021-08-18 16:24:12 +0900

    style : 불필요 코드 제거

--- kdtSpringOrder/src/main/java/org/prgrms/kdt/config/AppConfiguration.java
+++ kdtSpringOrder/src/main/java/org/prgrms/kdt/config/AppConfiguration.java
@@ -12,9 +12,6 @@ import org.prgrms.kdt.service.VoucherService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.Optional;
-import java.util.UUID;
-
 //VoucherService, OrderService, OrderRepository, VoucherRepository 의 생성에 대한 책임을 가지게 된다.
 //IoC 컨테이너 역할을 하기때문에 객체들의 의존관계 설정, 객체 생성, 파괴를 담당한다.
 @Configuration
