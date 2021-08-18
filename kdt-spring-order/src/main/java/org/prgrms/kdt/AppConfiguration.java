61ca3570e6104b024e66a06572cc1ba7f4867e98
suyeon <soo5717@sookmyung.ac.kr>
2021-08-19 05:39:46 +0900

    feat(W3D2): ApplicationContext와 Bean
    - AppConfiguration로 OrderContext 이름 변경
    - AppConfiguration에 @Configuration, @Bean 추가
    - ApplicationContext 사용하도록 OrderTester 변경

--- kdt-spring-order/src/main/java/org/prgrms/kdt/AppConfiguration.java
+++ kdt-spring-order/src/main/java/org/prgrms/kdt/AppConfiguration.java
@@ -1,5 +1,9 @@
 package org.prgrms.kdt;
 
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 import java.util.Optional;
 import java.util.UUID;
 
@@ -7,7 +11,9 @@ import java.util.UUID;
 * 레포지토리 생성에 대한 책임을 가지고 있음.
 * 각 서비스들이 어떤 레포지토리를 사용하는지 관계에 대한 책임도 가지고 있음.
 * */
-public class OrderContext {
+@Configuration
+public class AppConfiguration {
+    @Bean
     public VoucherRepository voucherRepository() {
         return new VoucherRepository() {
             @Override
@@ -17,6 +23,7 @@ public class OrderContext {
         };
     }
 
+    @Bean
     public OrderRepository orderRepository() {
         return new OrderRepository() {
             @Override
@@ -26,10 +33,12 @@ public class OrderContext {
         };
     }
 
+    @Bean
     public VoucherService voucherService() {
         return new VoucherService(voucherRepository());
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderService(voucherService(), orderRepository());
     }
--- kdt-spring-order/src/main/java/org/prgrms/kdt/OrderTester.java
+++ kdt-spring-order/src/main/java/org/prgrms/kdt/OrderTester.java
@@ -1,5 +1,6 @@
 package org.prgrms.kdt;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.util.Assert;
 
 import java.text.MessageFormat;
@@ -8,9 +9,10 @@ import java.util.UUID;
 
 public class OrderTester {
     public static void main(String[] args) {
+        var applicationContext = new AnnotationConfigApplicationContext(AppConfiguration.class); // ApplicationContext
+
         var customerId = UUID.randomUUID();
-        var orderContext = new OrderContext();
-        var orderService = orderContext.orderService();
+        var orderService= applicationContext.getBean(OrderService.class); // Bean
         var order = orderService.createOder(customerId, new ArrayList<>() {{
             add(new OrderItem(UUID.randomUUID(), 100L, 1));
         }});
