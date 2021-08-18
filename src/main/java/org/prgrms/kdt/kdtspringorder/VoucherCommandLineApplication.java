a018b225bf5f912bc13597abb531bc22f99a284e
devrunner21 <devrunner21@gmail.com>
2021-08-18 19:22:06 +0900

    [FEAT] VoucherCommandLineApplication의 메인 클래스를 정의하라

---
+++ src/main/java/org/prgrms/kdt/kdtspringorder/VoucherCommandLineApplication.java
@@ -0,0 +1,12 @@
+package org.prgrms.kdt.kdtspringorder;
+
+/**
+ * Voucher Command Line 어플리케이션을 실행한다.
+ */
+public class VoucherCommandLineApplication {
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/main/java/org/prgrms/kdt/kdtspringorder/voucher/application/VoucherCommandLine.java
@@ -0,0 +1,12 @@
+package org.prgrms.kdt.kdtspringorder.voucher.application;
+
+/**
+ * VoucherCommandLine의 메인 로직을 담당합니다.
+ */
+public class VoucherCommandLine {
+
+    public void start() {
+        
+    }
+
+}
