e8365f7d36294ddae48e29260c4259aff9b3714f
HwiNyeon <z2b23173us@gmail.com>
2021-08-18 15:23:36 +0900

    VoucherRepository의 기본 기능 추가

--- src/main/java/org/programmers/kdt/voucher/VoucherRepository.java
+++ src/main/java/org/programmers/kdt/voucher/VoucherRepository.java
@@ -1,10 +1,11 @@
 package org.programmers.kdt.voucher;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface VoucherRepository {
     Optional<Voucher> findById(UUID voucherId);
-    void addVoucher(Voucher voucher);
-    void listAllVouchers();
+    void save(Voucher voucher);
+    List<Voucher> findAll();
 }
