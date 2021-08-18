3083e773d3873f33bc1d26e1015fcdfe9a7c567b
DongSubKim <dskim9218@gmail.com>
2021-08-18 20:02:18 +0900

    delete unnecessary order files and add file voucher repo

---
+++ orderApp/src/main/java/org/prgrms/orderapp/Utils.java
@@ -0,0 +1,2 @@
+package org.prgrms.orderapp;public class utils {
+}
--- orderApp/src/main/java/org/prgrms/orderapp/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package org.prgrms.orderapp.repository;
-
-import org.prgrms.orderapp.Order;
-
-public interface OrderRepository {
-    void save(Order order);
-}
