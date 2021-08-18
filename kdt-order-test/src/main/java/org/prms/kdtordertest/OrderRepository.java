b67c934b99eef630b43f2a639455442bb5abdfeb
sds1vrk <sdsvrk@gmail.com>
2021-08-19 03:38:56 +0900

    rm

--- kdt-order-test/src/main/java/org/prms/kdtordertest/OrderRepository.java
+++
@@ -1,6 +0,0 @@
-package org.prms.kdtordertest;
-
-public interface OrderRepository {
-
-    public void insert(Order order);
-}
--- test/src/main/java/tesat.java
+++
@@ -1,5 +0,0 @@
-public class tesat {
-    public static void main(String[] args) {
-        System.out.println("testasdf");
-    }
-}
