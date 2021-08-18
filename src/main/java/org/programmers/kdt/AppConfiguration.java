24a06258d8274d6d3bc5a8cfc77dbe54ae4356a4
HwiNyeon <z2b23173us@gmail.com>
2021-08-18 16:47:33 +0900

    ComponentScan Annotation 추가 및 Bean 자동 등록 성공 확인

--- src/main/java/org/programmers/kdt/AppConfiguration.java
+++ src/main/java/org/programmers/kdt/AppConfiguration.java
@@ -1,22 +1,14 @@
 package org.programmers.kdt;
 
-import org.programmers.kdt.io.Console;
 import org.programmers.kdt.order.Order;
 import org.programmers.kdt.order.OrderRepository;
-import org.programmers.kdt.order.OrderSerivce;
-import org.programmers.kdt.voucher.MemoryVoucherRepository;
-import org.programmers.kdt.voucher.VoucherRepository;
-import org.programmers.kdt.voucher.VoucherService;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan
 public class AppConfiguration {
-
-    @Bean
-    public VoucherRepository voucherRepository() {
-        return new MemoryVoucherRepository();
-    }
 
     @Bean
     public OrderRepository orderRepository() {
@@ -26,20 +18,5 @@ public class AppConfiguration {
 
             }
         };
-    }
-
-    @Bean
-    public VoucherService voucherService(VoucherRepository voucherRepository) {
-        return new VoucherService(voucherRepository);
-    }
-
-    @Bean
-    public OrderSerivce orderSerivce(VoucherService voucherService, OrderRepository orderRepository) {
-        return new OrderSerivce(voucherService, orderRepository);
-    }
-
-    @Bean
-    public Console console() {
-        return new Console();
     }
 }
