d0b8df62738b1d153de25be22b8643a599df7c4b
HwiNyeon <z2b23173us@gmail.com>
2021-08-18 15:21:21 +0900

    Input 값 검증 등에 필요한 유틸함수 저장용 클래스 생성

--- src/main/java/org/programmers/kdt/utils/MyUtils.java
+++ src/main/java/org/programmers/kdt/utils/MyUtils.java
@@ -3,10 +3,18 @@ package org.programmers.kdt.utils;
 public class MyUtils {
     public static boolean isDigits(String str) {
         try {
-            Integer.parseInt(str);
+            Long.parseLong(str);
             return true;
         } catch (Exception e) {
             return false;
         }
+    }
+
+    public static boolean isBetweenZeroAndHundred(String str) {
+        int percentage = Integer.parseInt(str);
+        if (percentage < 0 || percentage > 100) {
+            return false;
+        }
+        return true;
     }
 }
