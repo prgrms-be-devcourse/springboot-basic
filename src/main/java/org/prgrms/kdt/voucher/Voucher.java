4713c4ad7558c6e9ad632275f22685e961c9ce36
Jihun-Hwang <goharrm@ajou.ac.kr>
2021-08-19 02:46:37 +0900

    Voucher 생성에 대한 Factory method 추가

--- src/main/java/org/prgrms/kdt/voucher/Voucher.java
+++ src/main/java/org/prgrms/kdt/voucher/Voucher.java
@@ -6,4 +6,24 @@ public interface Voucher {
     UUID getVoucherId();
 
     long discount(long beforeDiscount);
+
+    /**
+     * voucherType에 맞는 Voucher 객체를 생성하는 팩토리 메소드.
+     */
+    static Voucher voucherFactory(VoucherType voucherType, long size){
+        Voucher newVoucher = null;
+
+        switch(voucherType){
+            case FIXED_AMOUNT:
+                newVoucher = new FixedAmountVoucher(UUID.randomUUID(), size);
+                break;
+            case PERSENT_DISCOUNT:
+                newVoucher = new PercentDiscountVoucher(UUID.randomUUID(), size);
+                break;
+            default:
+                return null;
+        }
+
+        return newVoucher;
+    }
 }
