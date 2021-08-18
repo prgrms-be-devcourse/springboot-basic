b50562469239dacfb13d4bda5e75df3878197916
HwiNyeon <z2b23173us@gmail.com>
2021-08-18 15:27:17 +0900

    PercentDiscountVoucher의 할인율 범위 조절 추가
    - 퍼센트 단위의 할인율은 [0, 100]%의 범위에서 가능
    - 이 **범위 검증을 어느 위치에서 해 주어야** 하는지 확신이 서지 않음.
        - 방법 1. [0, 100]범위의 값만 들어온다고 가정하고, 생성자에서 검증하지 않음. 바우처 생성을 담당하는 쪽에서 검증하기
        - 방법 2. 이 코드처럼 PercentDiscountVoucher에서 자체적으로 범위검증하기

--- src/main/java/org/programmers/kdt/voucher/PercentDiscountVoucher.java
+++ src/main/java/org/programmers/kdt/voucher/PercentDiscountVoucher.java
@@ -3,10 +3,23 @@ package org.programmers.kdt.voucher;
 import java.util.UUID;
 
 public class PercentDiscountVoucher implements Voucher {
+    private final int MAXIMUM_DISCOUNT_PERCENTAGE = 100;
+    private final int MINIMUM_DISCOUNT_PERCENTAGE = 0;
     private final UUID voucherId;
     private final long percent;
 
     public PercentDiscountVoucher(UUID voucherId, long percent) {
+        if (percent < 0) {
+            System.out.println("!! WARNING !!\nInvalid discount percentage : "
+                            + percent
+                            + "%\nAutomatically changed to " + MINIMUM_DISCOUNT_PERCENTAGE + "%");
+            percent = 0;
+        } else if ( percent > 100) {
+            System.out.println("!! WARNING !!\nInvalid discount percentage : "
+                    + percent
+                    + "%\nAutomatically changed to " + MAXIMUM_DISCOUNT_PERCENTAGE + "%");
+            percent = 100;
+        }
         this.voucherId = voucherId;
         this.percent = percent;
     }
