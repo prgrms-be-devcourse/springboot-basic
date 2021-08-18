e2a1a7b07bd8464752770613e04b19cf30804e33
awesomeo184 <63030569+awesomeo184@users.noreply.github.com>
2021-08-19 02:02:02 +0900

    feat: CommandLineApplication 추가

---
+++ src/main/java/com/example/kdtspringmission/CommandLineApplication.java
@@ -0,0 +1,42 @@
+package com.example.kdtspringmission;
+
+import java.util.Scanner;
+
+public class CommandLineApplication {
+
+    private Scanner scanner = new Scanner(System.in);
+    private VoucherRepository voucherRepository = new MemoryVoucherRepository();
+
+    public void run() {
+        while (true) {
+
+            System.out.println("=== Voucher Program ===\n"
+                + "Type 'exit' to exit the program\n"
+                + "Type 'create' to create voucher\n"
+                + "Type 'list' to list vouchers");
+
+            String userInput = scanner.nextLine();
+
+            if (userInput.equals("exit")) {
+                break;
+            }
+
+            if (userInput.equals("create")) {
+                System.out.println("Which one? (1.FixedAmountVoucher, 2.RateAmountVoucher)");
+                String voucher = scanner.nextLine();
+                if (voucher.equals("1")) {
+                    voucherRepository.insert(VoucherFactory.create("FixedAmountVoucher"));
+                } else if (voucher.equals("2")) {
+                    voucherRepository.insert(VoucherFactory.create("RateAmountVoucher"));
+                } else {
+                    throw new IllegalArgumentException();
+                }
+            }
+
+            if (userInput.equals("list")) {
+                System.out.println(voucherRepository.findAll());
+            }
+        }
+    }
+
+}
