ac9cd9e900c67bd6a0c0781835e7c0e2dd008e8e
jummi <98qkrwjdal@naver.com>
2021-08-18 19:04:17 +0900

    Initial commit

---
+++ kdt-spring-order/src/test/java/com/prgms/kdtspringorder/KdtSpringOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prgms.kdtspringorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KdtSpringOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
