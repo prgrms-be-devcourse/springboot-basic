ae0dbd45befb3ab81d7256364bee333ba1c9a398
eunseo2 <20180972@sungshin.ac.kr>
2021-08-18 17:27:01 +0900

    [feat] MemoryRepository

--- src/main/java/org/prgms/MemoryVoucherRepository.java
+++ src/main/java/org/prgms/MemoryVoucherRepository.java
@@ -1,23 +1,28 @@
 package org.prgms;
 
-import java.util.*;
+import org.springframework.stereotype.Repository;
 
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Repository
 public class MemoryVoucherRepository implements VoucherRepository{
 
-    private List<Voucher> vouchers = new ArrayList<>();
+    private final Map<UUID,Voucher> storage = new ConcurrentHashMap<>(); // thread safety ConcurrentHashMap 사용
 
     @Override
     public Optional<Voucher> findById(UUID voucherId) {
-        return Optional.empty();
+        return Optional.ofNullable(storage.get(voucherId)); // null일 경우 empty
     }
 
     @Override
-    public void save(Voucher voucher) {
-        this.vouchers.add(voucher);
+    public Voucher save(Voucher voucher) {
+        storage.put(voucher.getVoucherId(),voucher);
+        return voucher;
     }
 
     @Override
     public List<Voucher> findAll() {
-        return vouchers;
+        return new ArrayList<>(storage.values());
     }
 }
