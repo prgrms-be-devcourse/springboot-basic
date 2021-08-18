810e4ed59dd7f36af04ed9128cef4fd33a384b56
HwiNyeon <z2b23173us@gmail.com>
2021-08-18 18:47:07 +0900

    [강의] MemoryOrderRepository 추가

---
+++ src/main/java/org/programmers/kdt/order/MemoryOrderRepository.java
@@ -0,0 +1,18 @@
+package org.programmers.kdt.order;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Repository
+public class MemoryOrderRepository implements OrderRepository {
+    private final Map<UUID, Order> repository = new ConcurrentHashMap<>();
+
+    @Override
+    public Order save(Order order) {
+        repository.put(order.getOrderId(), order);
+        return order;
+    }
+}
