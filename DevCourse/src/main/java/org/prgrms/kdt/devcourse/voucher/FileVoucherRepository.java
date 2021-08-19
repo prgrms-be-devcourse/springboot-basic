2e06c119e32f120b48bb4659618ac89c94c5a073
kwhyo <rkgmlgygml@naver.com>
2021-08-20 00:29:59 +0900

    FileRepository의 insert 기능 구현

--- DevCourse/src/main/java/org/prgrms/kdt/devcourse/voucher/FileVoucherRepository.java
+++ DevCourse/src/main/java/org/prgrms/kdt/devcourse/voucher/FileVoucherRepository.java
@@ -1,10 +1,15 @@
 package org.prgrms.kdt.devcourse.voucher;
 
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Repository;
+
 import java.io.*;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
+@Primary
 public class FileVoucherRepository implements VoucherRepository{
     private final String filePath = System.getProperty("user.dir")+"/voucher.csv";
     private File csvFile = new File(filePath);
@@ -16,7 +21,18 @@ public class FileVoucherRepository implements VoucherRepository{
 
     @Override
     public Voucher insert(Voucher voucher) {
+        try {
+            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(csvFile,true));
 
+            String newData = voucher.getVoucherId()+","+voucher.getVoucherAmount()+","+voucher.getVoucherType();
+            bufferedWriter.write(newData);
+            bufferedWriter.newLine();
+
+            bufferedWriter.flush();
+            bufferedWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return voucher;
     }
 
