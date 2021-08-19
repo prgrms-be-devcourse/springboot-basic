09b57191fa43f3c84feec19d8afa5fa48ba814e6
BeautterLife <pgh9801@gmail.com>
2021-08-19 22:24:18 +0900

    feat/Order 상태를 표현하는 Enum 생성

---
+++ src/main/java/org/prgrms/kdt/VO/OrderStatus.java
@@ -0,0 +1,13 @@
+package org.prgrms.kdt.VO;
+
+// Value Object
+public enum OrderStatus {
+    ACCEPTED,
+    PAYMENT_REQUIRED,
+    PAYMENT_CONFIRMED,
+    PAYMENT_REJECTED,
+    READY_FOR_DELIVERY,
+    SHIPPED,
+    SETTLED,
+    CANCELED
+}
