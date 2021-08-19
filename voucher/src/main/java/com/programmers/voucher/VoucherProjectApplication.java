c6c7488656df24f5829a36718f2f4bcddb22e9cb
Kwonkyu <park2348190@naver.com>
2021-08-20 00:59:51 +0900

    project init

---
+++ voucher/src/main/java/com/programmers/voucher/VoucherProjectApplication.java
@@ -0,0 +1,15 @@
+package com.programmers.voucher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+@SpringBootApplication
+public class VoucherProjectApplication {
+
+	public static void main(String[] args) {
+//		SpringApplication.run(VoucherProjectApplication.class, args);
+	}
+
+}
