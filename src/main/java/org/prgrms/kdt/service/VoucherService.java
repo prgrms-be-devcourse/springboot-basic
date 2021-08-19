77a669770046fcf7801323053d43ac0874fb8cf2
suhyunsim <cnc1106@naver.com>
2021-08-20 01:51:38 +0900

    feat: Voucher 생성을 위한 메서드 추가

--- src/main/java/org/prgrms/kdt/service/VoucherService.java
+++ src/main/java/org/prgrms/kdt/service/VoucherService.java
@@ -1,18 +1,23 @@
 package org.prgrms.kdt.service;
 
+import org.prgrms.kdt.domain.VoucherMachine;
 import org.prgrms.kdt.domain.VoucherRepository;
+import org.prgrms.kdt.domain.VoucherType;
 import org.prgrms.kdt.strategy.Voucher;
 
 import java.text.MessageFormat;
+import java.util.List;
 import java.util.UUID;
 
 import static org.prgrms.kdt.exception.Message.CANNOT_FIND_VOUCHER;
 
 public class VoucherService {
     private final VoucherRepository voucherRepository;
+    private final VoucherMachine voucherMachine;
 
-    public VoucherService(VoucherRepository voucherRepository) {
+    public VoucherService(VoucherRepository voucherRepository, VoucherMachine voucherMachine) {
         this.voucherRepository = voucherRepository;
+        this.voucherMachine = voucherMachine;
     }
 
     public Voucher getVoucher(UUID voucherId) {
@@ -23,6 +28,10 @@ public class VoucherService {
 
     public void useVoucher(Voucher voucher) {
         //TODO
+    }
+
+    public Voucher create(String voucherType) {
+        return voucherMachine.createVoucher(VoucherType.valueOf(voucherType));
     }
 
 }
