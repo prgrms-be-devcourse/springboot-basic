f6f740c094a32df3f104ad076ea82048afed9f08
suhyunsim <cnc1106@naver.com>
2021-08-20 01:47:50 +0900

    feat: AppConfig에 VoucherMachine 빈으로 등록

--- src/main/java/org/prgrms/kdt/common/AppConfiguration.java
+++ src/main/java/org/prgrms/kdt/common/AppConfiguration.java
@@ -1,14 +1,14 @@
 package org.prgrms.kdt.common;
 
-import org.prgrms.kdt.domain.Order;
-import org.prgrms.kdt.domain.OrderRepository;
-import org.prgrms.kdt.domain.VoucherRepository;
+import org.prgrms.kdt.domain.*;
 import org.prgrms.kdt.service.OrderService;
 import org.prgrms.kdt.service.VoucherService;
 import org.prgrms.kdt.strategy.Voucher;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -21,6 +21,11 @@ public class AppConfiguration {
             @Override
             public Optional<Voucher> findById(UUID voucherId) {
                 return Optional.empty();
+            }
+
+            @Override
+            public List<Voucher> findAll() {
+                return new ArrayList<>();
             }
         };
     }
@@ -36,8 +41,13 @@ public class AppConfiguration {
     }
 
     @Bean
-    public VoucherService voucherService(VoucherRepository voucherRepository) {
-        return new VoucherService(voucherRepository);
+    public VoucherMachine voucherMachine() {
+        return new VoucherMachine();
+    }
+
+    @Bean
+    public VoucherService voucherService(VoucherRepository voucherRepository, VoucherMachine voucherMachine) {
+        return new VoucherService(voucherRepository, voucherMachine);
     }
 
     @Bean
