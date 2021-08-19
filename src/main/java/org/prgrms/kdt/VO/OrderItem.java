6554fa5d298ae776cd0278ba0dc684470314ebea
BeautterLife <pgh9801@gmail.com>
2021-08-19 22:23:40 +0900

    feat/상품 주문 내역 정보를 가지는 Value Object

---
+++ src/main/java/org/prgrms/kdt/VO/OrderItem.java
@@ -0,0 +1,29 @@
+package org.prgrms.kdt.VO;
+
+import java.util.UUID;
+
+// Value Object
+public class OrderItem {
+    public final UUID productId;
+    public final long productPrice;
+    public final long quantity;
+
+    public OrderItem(UUID productId, long productPrice, long quantity) {
+        this.productId = productId;
+        this.productPrice = productPrice;
+        this.quantity = quantity;
+    }
+
+    public UUID getProductId() {
+        return productId;
+    }
+
+    public long getProductPrice() {
+        return productPrice;
+    }
+
+    public long getQuantity() {
+        return quantity;
+    }
+
+}
