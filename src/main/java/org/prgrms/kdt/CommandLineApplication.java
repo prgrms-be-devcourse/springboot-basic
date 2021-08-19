84aea0062543926b7f907dac4b4e11f862c27733
BeautterLife <pgh9801@gmail.com>
2021-08-19 22:26:23 +0900

    feat/Voucher 관리 프로그램의 Entry 프로그램

---
+++ src/main/java/org/prgrms/kdt/CommandLineApplication.java
@@ -0,0 +1,8 @@
+package org.prgrms.kdt;
+
+public class CommandLineApplication {
+    public static void main(String[] args) {
+        VoucherTester voucherTester = new VoucherTester();
+        voucherTester.run();
+    }
+}
