331d96136c47b825f6b981b3742d7ebcaad30305
suhyunsim <cnc1106@naver.com>
2021-08-20 03:26:44 +0900

    refactor: VoucherRepository 구현체로 Vouchers 활용

--- src/main/java/org/prgrms/kdt/common/AppConfiguration.java
+++ src/main/java/org/prgrms/kdt/common/AppConfiguration.java
@@ -3,31 +3,15 @@ package org.prgrms.kdt.common;
 import org.prgrms.kdt.domain.*;
 import org.prgrms.kdt.service.OrderService;
 import org.prgrms.kdt.service.VoucherService;
-import org.prgrms.kdt.strategy.Voucher;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
 
 @Configuration
 public class AppConfiguration {
 
     @Bean
     public VoucherRepository voucherRepository() {
-        return new VoucherRepository() {
-            @Override
-            public Optional<Voucher> findById(UUID voucherId) {
-                return Optional.empty();
-            }
-
-            @Override
-            public List<Voucher> findAll() {
-                return new ArrayList<>();
-            }
-        };
+        return new Vouchers();
     }
 
     @Bean
