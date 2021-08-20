527a8a983640965265574ae68b20ff40b5e7d1e8
yhh1056 <yhh1056@naver.com>
2021-08-20 11:54:53 +0900

    Refac: 오타 수정 및 네이밍 변경

--- src/main/java/org/prgrms/kdt/MissionApp.java
+++ src/main/java/org/prgrms/kdt/MissionApp.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
  * Created by yhh1056
  * Date: 2021/08/18 Time: 12:53 오전
  */
-public class Mission1W2D {
+public class MissionApp {
 
     public static void main(String[] args) {
         VoucherService voucherService = getVoucherService(getApplicationContext());
--- src/main/java/org/prgrms/kdt/file/FileRepository.java
+++ src/main/java/org/prgrms/kdt/file/FileRepository.java
@@ -4,7 +4,6 @@ package org.prgrms.kdt.file;
  * Created by yhh1056
  * Date: 2021/08/19 Time: 10:53 오전
  *
- * T : 반환 할 타입
  * E : 저장 할 객체
  */
 public interface FileRepository<E> {
