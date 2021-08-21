650b0b4c3605fb983cd593a1d46f993d033ca9d5
Chanui Jeon <jcu011@naver.com>
2021-08-21 20:25:47 +0900

    Update VoucherRepositoryImpl.java
    @Component

--- jcu/src/main/java/W3D2/jcu/voucher/VoucherRepositoryImpl.java
+++ jcu/src/main/java/W3D2/jcu/voucher/VoucherRepositoryImpl.java
@@ -4,10 +4,10 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
+import org.springframework.stereotype.Component;
 
+@Component
 public class VoucherRepositoryImpl implements VoucherRepository{
-
-    // 3일차 강의내용을 그대로 사용해서 HashMap을 사용했습니다.
     private final Map<UUID, Voucher> storage = new ConcurrentHashMap<>();
 
     @Override
@@ -20,8 +20,7 @@ public class VoucherRepositoryImpl implements VoucherRepository{
         storage.put(voucher.getVoucherId(), voucher);
         return voucher;
     }
-    
-    // 추가된 메소드, 인터페이스에도 프로토타입 추가
+
     @Override
     public Map<UUID, Voucher> findAll(){
         return storage;
