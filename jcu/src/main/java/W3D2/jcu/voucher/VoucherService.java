e7788f30ba3e645061819cfaa56be26352840c83
Chanui Jeon <jcu011@naver.com>
2021-08-21 20:24:10 +0900

    Update VoucherService.java
    @RequiredArgsConstructor

--- jcu/src/main/java/W3D2/jcu/voucher/VoucherService.java
+++ jcu/src/main/java/W3D2/jcu/voucher/VoucherService.java
@@ -3,30 +3,24 @@ package W3D2.jcu.voucher;
 import java.text.MessageFormat;
 import java.util.Map;
 import java.util.UUID;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class VoucherService {
-
     private final VoucherRepository voucherRepository;
 
-    public VoucherService(VoucherRepository voucherRepository) {
-        this.voucherRepository = voucherRepository;
-    }
-    
-    // Voucher추가
     public void insertVoucher(Voucher voucher){
         voucherRepository.insert(voucher);
     }
-    
-    // 수업내용인데 사용하지는 않았습니다.
+
     public Voucher getVoucher(UUID voucherId) {
         return voucherRepository
             .findById(voucherId)
             .orElseThrow(() -> new RuntimeException(MessageFormat.format("Can not find a voucher for {0}", voucherId)));
     }
-    
-    // UUID를 그대로 받아오면 안되기 때문에 Voucher정보만 toString을 이용하여 StringBuilder로 반환
+
     public StringBuilder showVouchers(){
         Map<UUID, Voucher> findVouchers = voucherRepository.findAll();
         StringBuilder sb = new StringBuilder();
@@ -36,5 +30,5 @@ public class VoucherService {
         }
         return sb;
     }
-
+    
 }
