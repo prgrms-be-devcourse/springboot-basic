1a5e3a602df0bc9e5fcb336f7bc1f6bfc7504a79
suyeon <soo5717@sookmyung.ac.kr>
2021-08-22 08:46:20 +0900

    feat(W3D2): CommandType getListOfEnum 메서드 구현
    - 명령어 목록 출력을 위해 enum 목록을 문자열로 반환

--- kdt-spring-order/src/main/java/org/prgrms/kdt/application/voucher/command/CommandType.java
+++ kdt-spring-order/src/main/java/org/prgrms/kdt/application/voucher/command/CommandType.java
@@ -16,8 +16,20 @@ public enum CommandType {
             Collections.unmodifiableMap(Stream.of(values())
                     .collect(Collectors.toMap(Enum::ordinal, Function.identity())));
 
+    private static final String LIST_OF_ENUM =
+            ORDINAL_TO_ENUM.entrySet()
+                    .stream()
+                    .filter(entry -> entry.getValue() != ERROR)
+                    .map(entry -> entry.getKey() + ": " + entry.getValue())
+                    .collect(Collectors.joining("\n"));
+
+
     public static CommandType fromOrdinal(int ordinal) {
         CommandType commandType = ORDINAL_TO_ENUM.get(ordinal);
         return commandType != null ? commandType : ERROR;
+    }
+
+    public static String getListOfEnum() {
+        return LIST_OF_ENUM;
     }
 }
