d8d2ef239f07c66ead4963546f0ac7a3d5c28551
0923kdh <0923kdh@gmail.com>
2021-08-23 01:44:23 +0900

    feat : SpringBoot Application 으로 전환

--- kdtSpringOrder/src/main/java/org/prgrms/kdt/KdtApplication.java
+++ kdtSpringOrder/src/main/java/org/prgrms/kdt/KdtApplication.java
@@ -1,13 +1,90 @@
 package org.prgrms.kdt;
 
+import org.prgrms.kdt.controller.InputController;
+import org.prgrms.kdt.controller.OutputController;
+import org.prgrms.kdt.dto.VoucherSaveRequestDto;
+import org.prgrms.kdt.enums.CommandType;
+import org.prgrms.kdt.enums.VoucherType;
+import org.prgrms.kdt.helper.MessageHelper;
+import org.prgrms.kdt.service.CustomerService;
+import org.prgrms.kdt.service.VoucherService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class KdtApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(KdtApplication.class, args);
+        var springApplication = new SpringApplication(KdtApplication.class);
+		springApplication.setAdditionalProfiles("prod");
+		ConfigurableApplicationContext applicationContext = springApplication.run(args);
+
+        voucherProcess(applicationContext);
 	}
+
+    public static void voucherProcess(ConfigurableApplicationContext applicationContext) {
+
+        InputController inputController = applicationContext.getBean(InputController.class);
+        OutputController outputController = applicationContext.getBean(OutputController.class);
+        MessageHelper messageHelper = applicationContext.getBean(MessageHelper.class);
+        VoucherService voucherService = applicationContext.getBean(VoucherService.class);
+        CustomerService customerService = applicationContext.getBean(CustomerService.class);
+
+        while(true){
+            messageHelper.showVoucherProgramGuideMessage();
+            CommandType commandType = inputController.getCommandType();
+            switch (commandType) {
+                case EXIT -> {
+                    runExit(messageHelper);
+                }
+                case CREATE -> {
+                    runCreate(inputController, messageHelper, voucherService);
+                }
+                case LIST -> {
+                    runList(outputController, voucherService);
+                }
+                case BLACKLIST -> {
+                    runBadCustomerList(outputController, customerService);
+                }
+                case REPLAY -> {
+                    runReplay(messageHelper);
+                }
+                default -> {
+                    runRetry(messageHelper);
+                }
+            }
+        }
+    }
+
+    public static void runExit(MessageHelper messageHelper) {
+        messageHelper.showExitMessage();
+        System.exit(0);
+    }
+
+    public static void runCreate(InputController inputController, MessageHelper messageHelper, VoucherService voucherService) {
+        messageHelper.showVoucherSelectionMessage();
+        VoucherType type = inputController.getVoucherType();
+        messageHelper.showEnterVoucherDiscount();
+        long discount = inputController.getDiscount();
+        voucherService.createVoucher(new VoucherSaveRequestDto(type, discount));
+        messageHelper.showVoucherRegistrationSuccessMessage();
+    }
+
+    public static void runList(OutputController outputController, VoucherService voucherService) {
+        outputController.showVoucherList(voucherService.getAllVouchers());
+    }
+
+    public static void runBadCustomerList(OutputController outputController, CustomerService customerService) {
+        outputController.showBadCustomerList(customerService.getAllVouchers());
+    }
+
+    public static void runReplay(MessageHelper messageHelper) {
+        messageHelper.showVoucherProgramGuideMessage();
+    }
+
+    public static void runRetry(MessageHelper messageHelper) {
+        messageHelper.showRetryMessage();
+    }
 
 }
