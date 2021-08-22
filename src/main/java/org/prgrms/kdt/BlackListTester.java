27ef1feb710310a17b47a00da4c6ef95a5de7a2e
Sangmin Lee <sangminlee7648@gmail.com>
2021-08-23 03:21:02 +0900

    Feat: 추가 블랙리스트 명단 기능

---
+++ src/main/java/org/prgrms/kdt/BlackListTester.java
@@ -0,0 +1,18 @@
+package org.prgrms.kdt;
+
+import org.prgrms.kdt.configuration.AppConfiguration;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Arrays;
+
+public class BlackListTester {
+    public static void main(String[] args) throws IOException {
+        var applicationContext = new AnnotationConfigApplicationContext(AppConfiguration.class);
+        var blacklistResource = applicationContext.getResource("file:src/main/resources/customer_blacklist.csv");
+        var blacklistFile = blacklistResource.getFile();
+        var blacklist = Files.readAllLines(blacklistFile.toPath());
+        System.out.println(blacklist.stream().reduce("", (a, b) -> a + "\n" + b));
+    }
+}
