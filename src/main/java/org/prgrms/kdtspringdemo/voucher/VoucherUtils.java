c5a855fbdfc2592afeaef35405f73892369a9beb
UJHa <ddkk94@naver.com>
2021-08-22 09:26:08 +0900

    feat:create 커맨드 실패 시의 에러 안내 메세지 추가

--- src/main/java/org/prgrms/kdtspringdemo/voucher/VoucherUtils.java
+++ src/main/java/org/prgrms/kdtspringdemo/voucher/VoucherUtils.java
@@ -4,6 +4,7 @@ import org.prgrms.kdtspringdemo.AppConfiguration;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.util.Assert;
 
+import java.text.MessageFormat;
 import java.util.UUID;
 
 public class VoucherUtils {
@@ -15,16 +16,23 @@ public class VoucherUtils {
 
     public void createVoucher(String[] splitList) {
         Assert.isTrue(splitList.length == 2, "This command did not receive the required arguments.");
-        String voucherName = splitList[0];
-        if (voucherName.equals("F")) {
-            Voucher voucher = voucherService.createFixedAmountVoucher(UUID.randomUUID(), Long.parseLong(splitList[1]));
-            System.out.println("This is create : " + voucher);
-        } else if (voucherName.equals("P")) {
-            Voucher voucher = voucherService.createPercentDiscountVoucher(UUID.randomUUID(), Long.parseLong(splitList[1]));
-            System.out.println("This is create : " + voucher);
-        } else {
-            System.out.println("None Voucher!!! : " + voucherName);
+        try {
+            String voucherName = splitList[0];
+            Long data = Long.parseLong(splitList[1]);
+            if (voucherName.equals("F")) {
+                Voucher voucher = voucherService.createFixedAmountVoucher(UUID.randomUUID(), data);
+                System.out.println("This is create : " + voucher);
+            } else if (voucherName.equals("P")) {
+                Voucher voucher = voucherService.createPercentDiscountVoucher(UUID.randomUUID(), data);
+                System.out.println("This is create : " + voucher);
+            } else {
+                System.out.println("None Voucher!!! : " + voucherName);
+            }
+        } catch (Exception e) {
+            System.out.println("[ERROR]Invalid create command");
+            System.out.println(MessageFormat.format("Your input : {0}, {1}", splitList[0], splitList[1]));
         }
+
     }
 
     public void printAll() {
