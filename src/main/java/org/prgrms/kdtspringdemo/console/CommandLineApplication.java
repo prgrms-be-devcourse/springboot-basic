de31b207ff275434eee4ece45eb495191cc44190
UJHa <ddkk94@naver.com>
2021-08-22 09:11:25 +0900

    fix:사용자 입력 에러 값 처리, create 커맨드에 대한 안내 메세지 추가

--- src/main/java/org/prgrms/kdtspringdemo/console/CommandLineApplication.java
+++ src/main/java/org/prgrms/kdtspringdemo/console/CommandLineApplication.java
@@ -2,6 +2,7 @@ package org.prgrms.kdtspringdemo.console;
 
 import org.prgrms.kdtspringdemo.voucher.VoucherUtils;
 
+import java.text.MessageFormat;
 import java.util.Scanner;
 
 public class CommandLineApplication {
@@ -13,20 +14,36 @@ public class CommandLineApplication {
                 Type exit to exit the program.
                 Type create to create a new voucher.
                 Type list to list all vouchers.""";
+        String createInfoMessage = """
+                input voucher info
+                <vouhcerType> <data>
+                voucherType
+                \tP : FixedAmountVoucher
+                \tF : PercentDiscountVoucher
+                data is long value
+                ex) P 10""";
         System.out.println(startMessage);
         System.out.println(helpMessage);
 
         Scanner scanner = new Scanner(System.in);
         String command = "";
 
-        while (!command.equals("exit")) {
+        while (true) {
             String commandLine = scanner.nextLine();
             String[] splitList = commandLine.split(" ");
             command = splitList[0];
             if (command.equals("create")) {
-                voucherUtils.createVoucher(splitList);
+                System.out.println(createInfoMessage);
+                String[] createCommand = scanner.nextLine().split(" ");
+                voucherUtils.createVoucher(createCommand);
             } else if (command.equals("list")) {
                 voucherUtils.printAll();
+            } else if (command.equals("exit")) {
+                break;
+            } else {
+                System.out.println(MessageFormat.format("[ERROR]Invalid command type \nYour input : {0}", command));
+                System.out.println(helpMessage);
+                System.out.println("=================");
             }
         }
     }
--- src/main/java/org/prgrms/kdtspringdemo/voucher/VoucherUtils.java
+++ src/main/java/org/prgrms/kdtspringdemo/voucher/VoucherUtils.java
@@ -7,20 +7,20 @@ import org.springframework.util.Assert;
 import java.util.UUID;
 
 public class VoucherUtils {
-    private VoucherService voucherService;
+    private final VoucherService voucherService;
     public VoucherUtils() {
         var application = new AnnotationConfigApplicationContext(AppConfiguration.class);
         voucherService = application.getBean(VoucherService.class);
     }
 
     public void createVoucher(String[] splitList) {
-        Assert.isTrue(splitList.length == 3, "This command did not receive the required arguments.");
-        String voucherName = splitList[1];
+        Assert.isTrue(splitList.length == 2, "This command did not receive the required arguments.");
+        String voucherName = splitList[0];
         if (voucherName.equals("F")) {
-            Voucher voucher = voucherService.createFixedAmountVoucher(UUID.randomUUID(), Long.parseLong(splitList[2]));
+            Voucher voucher = voucherService.createFixedAmountVoucher(UUID.randomUUID(), Long.parseLong(splitList[1]));
             System.out.println("This is create : " + voucher);
         } else if (voucherName.equals("P")) {
-            Voucher voucher = voucherService.createPercentDiscountVoucher(UUID.randomUUID(), Long.parseLong(splitList[2]));
+            Voucher voucher = voucherService.createPercentDiscountVoucher(UUID.randomUUID(), Long.parseLong(splitList[1]));
             System.out.println("This is create : " + voucher);
         } else {
             System.out.println("None Voucher!!! : " + voucherName);
