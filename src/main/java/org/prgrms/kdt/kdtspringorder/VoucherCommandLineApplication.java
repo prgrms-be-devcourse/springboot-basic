3e0446c30bd7b42f782ab2725addbb285d8f0948
devrunner21 <devrunner21@gmail.com>
2021-08-23 18:12:07 +0900

    [FIX] 필요 없는 메인 클래스를 지우고 @SpringBootApplication를 필요한 메인 클래스로 옮겨라.

--- src/main/java/org/prgrms/kdt/kdtspringorder/VoucherCommandLineApplication.java
+++ src/main/java/org/prgrms/kdt/kdtspringorder/VoucherCommandLineApplication.java
@@ -2,11 +2,13 @@ package org.prgrms.kdt.kdtspringorder;
 
 import org.prgrms.kdt.kdtspringorder.common.config.AppConfiguration;
 import org.prgrms.kdt.kdtspringorder.voucher.application.VoucherCommandLine;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 /**
  * Voucher Command Line 어플리케이션을 실행한다.
  */
+@SpringBootApplication
 public class VoucherCommandLineApplication {
 
     public static void main(String[] args) {
