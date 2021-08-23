3cd26bfb28ccb2ef6001b05d8fcfc57a8ebb56e4
한재원 <hanjo8813@gmail.com>
2021-08-23 14:26:21 +0900

    feat : logback.xml 등록, 로깅 기능 추가

--- kdt-spring-order/src/main/java/org/prgrms/kdt/KdtApplication.java
+++ kdt-spring-order/src/main/java/org/prgrms/kdt/KdtApplication.java
@@ -2,8 +2,11 @@ package org.prgrms.kdt;
 
 import org.prgrms.kdt.customer.controller.CustomerController;
 import org.prgrms.kdt.customer.service.CustomerService;
+import org.prgrms.kdt.order.OrderTester;
 import org.prgrms.kdt.voucher.controller.VoucherController;
 import org.prgrms.kdt.voucher.service.VoucherService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -15,7 +18,7 @@ public class KdtApplication {
 	public static void main(String[] args) {
 		// app context 등록
 		var springApplication = new SpringApplication(KdtApplication.class);
-		springApplication.setAdditionalProfiles("dev");
+		springApplication.setAdditionalProfiles("prod");
 		var applicationContext = springApplication.run(args);
 
 		// customer -> black list 불러오기 (컨트롤러 생성자에서 출력하므로 생성만함)
--- kdt-spring-order/src/main/java/org/prgrms/kdt/io/Input.java
+++ kdt-spring-order/src/main/java/org/prgrms/kdt/io/Input.java
@@ -1,5 +1,9 @@
 package org.prgrms.kdt.io;
 
+import org.prgrms.kdt.voucher.domain.VoucherType;
+
 public interface Input {
-    String inputCommand(String prompt);
+    String inputCommand();
+    String inputVoucherType();
+    Long inputVoucherValue();
 }
