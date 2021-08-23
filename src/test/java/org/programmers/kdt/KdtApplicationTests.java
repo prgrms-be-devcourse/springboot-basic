f9eb34cdea765eadb6a514497f7a0f6c1c893137
HwiNyeon <z2b23173us@gmail.com>
2021-08-24 00:37:06 +0900

    [fix] - @SpringBootTest 테스트파일에서 ApplicationContext를 읽어오지 못하는 문제 해결

--- src/test/java/org/programmers/kdt/KdtApplicationTests.java
+++ src/test/java/org/programmers/kdt/KdtApplicationTests.java
@@ -1,9 +1,13 @@
 package org.programmers.kdt;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
+@SpringBootTest(classes = {SpringBootApplication.class})
+@RunWith(SpringRunner.class)
 class KdtApplicationTests {
 
 	@Test
