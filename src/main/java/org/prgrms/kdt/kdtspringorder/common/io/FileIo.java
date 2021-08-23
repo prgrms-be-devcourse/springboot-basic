fda1a3497f4ffcdf0b0b4c4206e66e3bdb7cb2f0
devrunner21 <devrunner21@gmail.com>
2021-08-23 21:30:50 +0900

    [FIX] FileIO 인터페이스를 수정하라. ( 제네릭 사용 )

--- src/main/java/org/prgrms/kdt/kdtspringorder/common/io/FileIo.java
+++ src/main/java/org/prgrms/kdt/kdtspringorder/common/io/FileIo.java
@@ -7,14 +7,14 @@ public interface FileIo<E> {
 
     /**
      * 파일 쓰기
-     * @param oList 파일에 쓸 객체 리스트
+     * @param oList 파일에 쓸 데이터 리스트
      * @throws IOException
      */
-    public void write(List<Object> oList);
+    public void write(List<E> oList);
 
     /**
      * 파일 읽기
-     * @return 읽은 파일의 객체들을 리스트 형태로 반환합니다.
+     * @return 읽은 파일의 데이터들을 리스트 형태로 반환합니다.
      * @throws IOException
      */
     public List<E> readAllLines();
