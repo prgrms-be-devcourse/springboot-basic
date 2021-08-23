534ebe0259bc8423bd7ca833bb489fba7ef94d9b
suebeen <1003jamie@naver.com>
2021-08-24 03:13:27 +0900

    feat : yaml 파일 생성, property 등록 할 수 있도록 구현

--- src/main/java/com/prgrm/kdt/AppConfiguration.java
+++ src/main/java/com/prgrm/kdt/AppConfiguration.java
@@ -1,11 +1,16 @@
 package com.prgrm.kdt;
 
+import com.prgrm.kdt.config.YamlPropertiesFactory;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-@ComponentScan(basePackages = {"com.prgrm.kdt.order", "com.prgrm.kdt.voucher"})
-//@ComponentScan(basePackageClasses = {Order.class, Voucher.class})
+@ComponentScan(
+        basePackages = {"com.prgrm.kdt.order", "com.prgrm.kdt.voucher", "com.prgrm.kdt.customer", "com.prgrm.kdt.config"}
+)
+
+@PropertySource(value = "application.yaml", factory = YamlPropertiesFactory.class)
 public class AppConfiguration {
 
 }
