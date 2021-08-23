202a246f80f07b6ceb2b0fbfce3158d7297944b5
seung-h <dau4723@gmail.com>
2021-08-23 16:47:25 +0900

    FEAT: 스프링부트 W4D1 강의

---
+++ kdt-spring-order/src/main/java/org/prgrms/kdt/repository/voucher/JdbcVoucherRepository.java
@@ -0,0 +1,31 @@
+package org.prgrms.kdt.repository.voucher;
+
+import org.prgrms.kdt.domain.voucher.Voucher;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Repository;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Repository
+@Profile({"staging", "production"})
+public class JdbcVoucherRepository implements VoucherRepository {
+
+    private final Map<UUID, Voucher> storage = new ConcurrentHashMap<>();
+
+    @Override
+    public Optional<Voucher> findById(UUID voucherId) {
+        return Optional.ofNullable(storage.get(voucherId));
+    }
+
+    @Override
+    public List<Voucher> findAll() {
+        return new ArrayList<>(storage.values());
+    }
+
+    @Override
+    public Voucher insert(Voucher voucher) {
+        storage.put(voucher.getVoucherId(), voucher);
+        return voucher;
+    }
+}
