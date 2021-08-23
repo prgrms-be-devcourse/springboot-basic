46d668be074c13679736cc490e22e75ddbedda24
HwiNyeon <z2b23173us@gmail.com>
2021-08-23 17:00:21 +0900

    [Feature] - 4일차 과제 - (2) - yaml 파일을 통해 설정 읽을 수 있도록 수정
    << TODO >>
    * YAML 파일을 통해 어떤 설정들을 만들 수 있을지 생각해보기

--- src/main/java/org/programmers/kdt/AppConfiguration.java
+++ src/main/java/org/programmers/kdt/AppConfiguration.java
@@ -1,10 +1,14 @@
 package org.programmers.kdt;
 
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @ComponentScan
+@PropertySource(value = "application.yaml", factory = YamlPropertiesFactory.class)
+@EnableConfigurationProperties
 public class AppConfiguration {
 
 }
