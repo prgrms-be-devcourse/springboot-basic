61477dd2c2aec5b8f0d9522f197de624a099d7f3
dlfdyd96 <1yongs_@naver.com>
2021-08-24 14:55:07 +0900

    ADD: W3D5 - 1. Logger ~ 2. Logback

--- src/main/java/org/prgrms/kdt/order/property/OrderProperties.java
+++ src/main/java/org/prgrms/kdt/order/property/OrderProperties.java
@@ -1,5 +1,7 @@
 package org.prgrms.kdt.order.property;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -20,6 +22,9 @@ import java.util.List;
 @Configuration
 @ConfigurationProperties(prefix = "kdt") // spring boot에서 나온 것. 그래서 Appconfigt에서 @EnableConfiguration 달아줘야함
 public class OrderProperties implements InitializingBean {
+
+    private final static Logger logger = LoggerFactory.getLogger(OrderProperties.class);
+
     private String version;
     private int minimumOrderAmount;
     private List<String> supportVendors;
@@ -27,9 +32,9 @@ public class OrderProperties implements InitializingBean {
 
     @Override
     public void afterPropertiesSet() throws Exception {
-        System.out.println(MessageFormat.format("[OrderProperties] version -> {0}", version ));
-        System.out.println(MessageFormat.format("[OrderProperties] minimumOrderAmount -> {0}", minimumOrderAmount ));
-        System.out.println(MessageFormat.format("[OrderProperties] supportVendors -> {0}", supportVendors ));
+        logger.debug("[OrderProperties] version -> {}", version );
+        logger.debug("[OrderProperties] minimumOrderAmount -> {}", minimumOrderAmount );
+        logger.debug("[OrderProperties] supportVendors -> {}", supportVendors );
     }
 
     public String getVersion() {
