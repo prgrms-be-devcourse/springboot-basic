3fc5cb2f25317ff9fd2ba7ebb7df363a4cd7c7ab
Jihun-Hwang <goharrm@ajou.ac.kr>
2021-08-24 16:56:33 +0900

    feat: 컴포넌트 스캔 대상 추가

--- src/main/java/org/prgrms/kdt/AppConfiguration.java
+++ src/main/java/org/prgrms/kdt/AppConfiguration.java
@@ -4,6 +4,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackages = "org.prgrms.kdt.voucher")
+@ComponentScan(basePackages = {"org.prgrms.kdt.voucher", "org.prgrms.kdt.customer"})
 public class AppConfiguration {
 }
