b6e75f911eadea745da9f55704f3553229065942
eunseo2 <20180972@sungshin.ac.kr>
2021-08-25 17:07:49 +0900

    [feat] 바우처 메모리 레포지토리 profile 추가(dev에서만 동작)

--- src/main/java/org/prgms/voucher/MemoryVoucherRepository.java
+++ src/main/java/org/prgms/voucher/MemoryVoucherRepository.java
@@ -1,11 +1,13 @@
 package org.prgms.voucher;
 
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Repository;
 
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Repository
+@Profile("dev")
 public class MemoryVoucherRepository implements VoucherRepository{
 
     private final Map<UUID,Voucher> storage = new ConcurrentHashMap<>(); // thread safety ConcurrentHashMap 사용
--- src/test/java/org/prgms/OrderTester.java
+++ src/test/java/org/prgms/OrderTester.java
@@ -3,6 +3,7 @@ package org.prgms;
 import org.prgms.order.OrderItem;
 import org.prgms.order.OrderService;
 import org.prgms.voucher.FixedAmountVoucher;
+import org.prgms.voucher.MemoryVoucherRepository;
 import org.prgms.voucher.VoucherRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,26 +20,33 @@ public class OrderTester {
     private static final Logger log = LoggerFactory.getLogger(OrderTester.class);
 
     public static void main(String[] args) throws IOException {
+
+        // 프로파일 적용 후  refresh를 해야함. 안그러면 이미 디폴트 프로파일로 읽어져서 원하는 결과를 얻을 수 없음
+        var applicationContext = new AnnotationConfigApplicationContext();
+        applicationContext.getEnvironment().setActiveProfiles("dev");;
+        applicationContext.register(AppConfiguration.class);
+        applicationContext.refresh();
+        
+//        var version = environment.getProperty("kdt.version");
+//        var minimumOrderAmount = environment.getProperty("kdt.minimum-order-amount", Integer.class);
+//
+//        log.info("version -> {}", version);
+//        log.info("minimumOrderAmount -> {}", minimumOrderAmount);
+
         var customerId = UUID.randomUUID();
-
-        var applicationContext = new AnnotationConfigApplicationContext(AppConfiguration.class);
-
-        var environment = applicationContext.getEnvironment();
-        var version = environment.getProperty("kdt.version");
-        var minimumOrderAmount = environment.getProperty("kdt.minimum-order-amount", Integer.class);
-
-        log.info("version -> {}", version);
-        log.info("minimumOrderAmount -> {}", minimumOrderAmount);
-
         var voucherRepository = applicationContext.getBean(VoucherRepository.class);
-
         var voucher = voucherRepository.save(new FixedAmountVoucher(UUID.randomUUID(), 10L));
 
-        var orderService = applicationContext.getBean(OrderService.class);
+        System.out.println(MessageFormat.format("is MemoryRepo -> {0}", voucherRepository instanceof MemoryVoucherRepository));
+        System.out.println(MessageFormat.format("is MemoryRepo -> {0}", voucherRepository.getClass().getCanonicalName()));
 
+
+        var orderService = applicationContext.getBean(OrderService.class);
         var order = orderService.createOrder(customerId, new ArrayList<OrderItem>() {{
             add(new OrderItem(UUID.randomUUID(), 100L, 1));
         }}, voucher.getVoucherId());
+
+
         Assert.isTrue(order.totalAmount() == 90L,
                 MessageFormat.format("totalAmount({0}) is not 90L", order.totalAmount()));
     }
