127061447bad9617f625524b1ec96fc69f4234a3
eunseo2 <20180972@sungshin.ac.kr>
2021-08-25 17:03:48 +0900

    [feat] 스프링부트 Profile

--- src/main/java/org/prgms/KdtApplication.java
+++ src/main/java/org/prgms/KdtApplication.java
@@ -1,13 +1,25 @@
 package org.prgms;
 
+import org.prgms.black.BlackProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+import java.text.MessageFormat;
 
 @SpringBootApplication
+@ComponentScan(basePackageClasses = {BlackProperties.class})
 public class KdtApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(KdtApplication.class, args);
-    }
+        var springApplication = new SpringApplication(KdtApplication.class);
+        springApplication.setAdditionalProfiles("dev");
+        var context = springApplication.run();
 
+        var BlackProperties = context.getBean(BlackProperties.class);
+        System.out.println(MessageFormat.format("getVersion -> {0}", BlackProperties.getVersion()));
+        System.out.println(MessageFormat.format("getTitile -> {0}", BlackProperties.getTitle()));
+
+    }
 }
+
