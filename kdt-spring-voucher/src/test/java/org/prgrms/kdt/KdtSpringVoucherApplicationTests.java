4c637cff055336dad77a16ef137bf47f40d0cece
dbwlgna98@naver.com <dbwlgna98@naver.com>
2021-08-26 03:51:33 +0900

    실행가능한 jar파일 생성

--- kdt-spring-voucher/src/test/java/org/prgrms/kdt/KdtSpringVoucherApplicationTests.java
+++ kdt-spring-voucher/src/test/java/org/prgrms/kdt/KdtSpringVoucherApplicationTests.java
@@ -3,11 +3,6 @@ package org.prgrms.kdt;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class KdtSpringVoucherApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
