8e608a9f8ae2c23d1a29971341b17a7072eee4de
eunseo2 <20180972@sungshin.ac.kr>
2021-08-25 16:10:52 +0900

    [feat] Yaml properties

--- src/main/java/org/prgms/AppConfiguration.java
+++ src/main/java/org/prgms/AppConfiguration.java
@@ -1,18 +1,22 @@
 package org.prgms;
 
 
+import org.prgms.black.BlackProperties;
 import org.prgms.io.Io;
 import org.prgms.order.Order;
 import org.prgms.voucher.Voucher;
+import org.prgms.black.YamlPropertiesFactory;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 
 @Configuration
-@ComponentScan(basePackageClasses = {Order.class, Voucher.class, Io.class}) //order, voucher, Io가 속한 패키지 기준
-@PropertySource("application.properties")
+@ComponentScan(basePackageClasses = {Order.class, Voucher.class, Io.class, BlackProperties.class})
+@PropertySource(value = "application.yaml", factory = YamlPropertiesFactory.class)
+@EnableConfigurationProperties
 
 public class AppConfiguration {
-    
+
 }
