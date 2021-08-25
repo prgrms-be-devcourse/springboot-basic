fa232b663c49e313ab24dd7d5d5bde38027c120a
jungeu1509 <jungeu1509@gmail.com>
2021-08-25 19:54:38 +0900

    [feedback refactory - 1] fix console input output

---
+++ src/main/java/programmers/org/kdt/engine/io/ConsoleTerminal.java
@@ -0,0 +1,24 @@
+package programmers.org.kdt.engine.io;
+
+import java.util.Scanner;
+
+public class ConsoleTerminal extends ConsoleIO {
+    private final Scanner scanner;
+
+    public ConsoleTerminal() {
+        scanner = new Scanner(System.in);
+    }
+
+    //input
+    @Override
+    public String input(String command) {
+        System.out.println(command);
+        return scanner.nextLine();
+    }
+
+    //output
+    @Override
+    public void print(String message) {
+        System.out.println(message);
+    }
+}
--- src/main/java/programmers/org/kdt/engine/io/Output.java
+++ src/main/java/programmers/org/kdt/engine/io/Output.java
@@ -1,14 +1,5 @@
 package programmers.org.kdt.engine.io;
 
-import java.util.Map.Entry;
-import java.util.Set;
-import java.util.UUID;
-import programmers.org.kdt.engine.voucher.Voucher;
-
 public interface Output {
-    void help();
-
-    void list(Set<Entry<UUID, Voucher>> entrySet);
-
-	void error();
+    void print(String message);
 }
