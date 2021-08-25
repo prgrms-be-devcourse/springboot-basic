1a716f4e8cfff8eb971351773ca86ffaad65464f
HwiNyeon <z2b23173us@gmail.com>
2021-08-25 21:06:46 +0900

    Initial Commit
    Project Setup

---
+++ src/main/java/org/programmers/kdt/io/Input.java
@@ -0,0 +1,2 @@
+package org.programmers.kdt.io;public interface Input {
+}
---
+++ src/test/java/org/programmers/kdt/KdtApplicationTests.java
@@ -0,0 +1,13 @@
+package org.programmers.kdt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KdtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
