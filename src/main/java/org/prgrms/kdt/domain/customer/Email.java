d35b02c2b68d81afe38e99fa98f62ce603b8164f
suhyunsim <cnc1106@naver.com>
2021-08-26 12:14:51 +0900

    feat: Customer 관련 원시값 Name, Email 포장
    - Name과 Email을 포장하고 각각 정규식으로 검증 로직 추가

---
+++ src/main/java/org/prgrms/kdt/domain/customer/Email.java
@@ -0,0 +1,25 @@
+package org.prgrms.kdt.domain.customer;
+
+import org.prgrms.kdt.exception.Message;
+import org.prgrms.kdt.exception.ValidationException;
+
+import java.util.regex.Pattern;
+
+public class Email {
+
+    private static final String PATTERN = "/^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;";
+
+    private final String email;
+
+    public Email(String email) {
+        validate(email);
+        this.email = email;
+    }
+
+    public void validate(String email) {
+        if (!Pattern.matches(PATTERN, email)) {
+            throw new ValidationException(Message.WRONG_FORMAT_MESSAGE);
+        }
+    }
+
+}
---
+++ src/main/java/org/prgrms/kdt/domain/customer/Name.java
@@ -0,0 +1,23 @@
+package org.prgrms.kdt.domain.customer;
+
+import org.prgrms.kdt.exception.Message;
+import org.prgrms.kdt.exception.ValidationException;
+
+import java.util.regex.Pattern;
+
+public class Name {
+    private static final String PATTERN = "^[가-힣]{2,4}$";
+
+    private final String name;
+
+    public Name(String name) {
+        validate(name);
+        this.name = name;
+    }
+
+    public void validate(String name) {
+        if (!Pattern.matches(PATTERN, name)) {
+            throw new ValidationException(Message.WRONG_FORMAT_MESSAGE);
+        }
+    }
+}
