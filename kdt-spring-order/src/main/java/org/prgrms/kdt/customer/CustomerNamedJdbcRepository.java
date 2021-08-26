2a150b3e5bbaf557755240d52189510ce4ffbde3
seung-h <dau4723@gmail.com>
2021-08-26 17:44:09 +0900

    FEAT: 스프링 부트 W4D4 강의

---
+++ kdt-spring-order/src/main/java/org/prgrms/kdt/customer/CustomerNamedJdbcRepository.java
@@ -0,0 +1,123 @@
+package org.prgrms.kdt.customer;
+
+import org.prgrms.kdt.JdbcCustomerRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import javax.sql.DataSource;
+import java.nio.ByteBuffer;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.*;
+
+@Repository
+public class CustomerNamedJdbcRepository implements CustomerRepository {
+
+    private static final Logger logger = LoggerFactory.getLogger(JdbcCustomerRepository.class);
+
+    private final NamedParameterJdbcTemplate jdbcTemplate;
+
+    private static RowMapper<Customer> customerRowMapper = (resultSet, i) -> {
+        String customerName = resultSet.getString("name");
+        UUID customerId = toUUID(resultSet.getBytes("customer_id"));
+        String email = resultSet.getString("email");
+        LocalDateTime lastLoginAt = resultSet.getTimestamp("last_login_at") != null ?
+                resultSet.getTimestamp("last_login_at").toLocalDateTime() : null;
+        LocalDateTime createdAt = resultSet.getTimestamp("created_at").toLocalDateTime();
+        return new Customer(customerId, customerName, email, lastLoginAt, createdAt);
+    };
+
+    public CustomerNamedJdbcRepository(NamedParameterJdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public Customer insert(Customer customer) {
+        int insert = jdbcTemplate.update("INSERT INTO customers (customer_id, name, email, created_at) VALUES (UUID_TO_BIN(:customerId), :name, :email, :createdAt)", toParamMap(customer));
+
+        if (insert != 1)
+            throw new RuntimeException("Nothing was inserted");
+
+        return customer;
+    }
+
+    @Override
+    public Customer update(Customer customer) {
+        int update = jdbcTemplate.update("UPDATE customers SET name = :name, email = :email, last_login_at = :lastLoginAt WHERE customer_id = UUID_TO_BIN(:customerId)", toParamMap(customer));
+        if (update != 1)
+            throw new RuntimeException("Nothing was updated");
+
+        return customer;
+    }
+
+    @Override
+    public List<Customer> findAll() {
+        return jdbcTemplate.query("SELECT * FROM customers", customerRowMapper);
+    }
+
+    @Override
+    public Optional<Customer> findById(UUID customerId) {
+        try {
+            return Optional.ofNullable(jdbcTemplate.queryForObject("SELECT * FROM customers WHERE customer_id = UUID_TO_BIN(:customerId)"
+                    , Collections.singletonMap("customerId", customerId.toString().getBytes())
+                    , customerRowMapper));
+        } catch (EmptyResultDataAccessException e) {
+            logger.error("Got empty result", e);
+            return Optional.empty();
+        }
+    }
+
+    @Override
+    public Optional<Customer> findByName(String name) {
+        try {
+            return Optional.ofNullable(jdbcTemplate.queryForObject("SELECT * FROM customers WHERE name = :name"
+                    , Collections.singletonMap("name", name)
+                    , customerRowMapper));
+        } catch (EmptyResultDataAccessException e) {
+            logger.error("Got empty result", e);
+            return Optional.empty();
+        }
+    }
+
+    @Override
+    public Optional<Customer> findByEmail(String email) {
+        try {
+            return Optional.ofNullable(jdbcTemplate.queryForObject("SELECT * FROM customers WHERE email = :email"
+                    , Collections.singletonMap("email", email)
+                    , customerRowMapper));
+        } catch (EmptyResultDataAccessException e) {
+            logger.error("Got empty result", e);
+            return Optional.empty();
+        }
+    }
+
+    @Override
+    public int count() {
+        return jdbcTemplate.queryForObject("SELECT COUNT(*) FROM customers", Collections.emptyMap(), Integer.class);
+    }
+
+    @Override
+    public void deleteAll() {
+        jdbcTemplate.update("DELETE FROM customers", Collections.emptyMap());
+    }
+
+    static UUID toUUID(byte[] bytes) {
+        ByteBuffer byteBuffer = ByteBuffer.wrap(bytes);
+        return new UUID(byteBuffer.getLong(), byteBuffer.getLong());
+    }
+
+    private Map<String, Object> toParamMap(Customer customer) {
+        return new HashMap<>(){{
+            put("name", customer.getName());
+            put("email", customer.getEmail());
+            put("lastLoginAt", customer.getLastLoginAt() != null ? Timestamp.valueOf(customer.getLastLoginAt()) : null);
+            put("customerId", customer.getCustomerId().toString().getBytes());
+            put("createdAt", Timestamp.valueOf(customer.getCreatedAt()));
+        }};
+    }
+}
---
+++ kdt-spring-order/src/test/java/org/prgrms/kdt/customer/CustomerNamedJdbcTemplateRepositoryTest.java
@@ -0,0 +1,157 @@
+package org.prgrms.kdt.customer;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+import javax.sql.DataSource;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringJUnitConfig
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class CustomerNamedJdbcTemplateRepositoryTest {
+
+    @Configuration
+    @ComponentScan(
+            basePackages = "org.prgrms.kdt.customer"
+    )
+    static class Config {
+
+        @Bean
+        public DataSource dataSource() {
+            HikariDataSource dataSource = DataSourceBuilder.create()
+                    .url("jdbc:mysql://localhost/order_mgmt")
+                    .username("root")
+                    .password("admin")
+                    .type(HikariDataSource.class)
+                    .build();
+            dataSource.setMaximumPoolSize(1000);
+            dataSource.setMinimumIdle(100);
+            return dataSource;
+        }
+
+        @Bean
+        public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+            return new JdbcTemplate(dataSource);
+        }
+        @Bean
+        public NamedParameterJdbcTemplate namedParameterJdbcTemplate() {
+            return new NamedParameterJdbcTemplate(dataSource());
+        }
+    }
+
+    @Autowired
+    CustomerNamedJdbcRepository customerRepository;
+
+    @Autowired
+    DataSource dataSource;
+
+    Customer newCustomer;
+
+    @BeforeAll
+    void setup() {
+        customerRepository.deleteAll();
+        newCustomer = new Customer(UUID.randomUUID(), "test-user", "test-user125553@gmail.com", LocalDateTime.now());
+    }
+
+    @Test
+    @Order(1)
+    public void testHikariConnectionPool() {
+        assertThat(dataSource.getClass().getName()).isEqualTo("com.zaxxer.hikari.HikariDataSource");
+    }
+
+    @Test
+    @Order(2)
+    @DisplayName("고객을 추가할 수 있다")
+    public void insert() {
+
+        // given
+        customerRepository.insert(newCustomer);
+
+        // when
+        Optional<Customer> retrievedCustomer = customerRepository.findById(newCustomer.getCustomerId());
+
+        // then
+        assertThat(retrievedCustomer.isEmpty()).isFalse();
+        assertThat(retrievedCustomer.get())
+                .extracting(Customer::getCustomerId, Customer::getName, Customer::getEmail)
+                .containsExactly(newCustomer.getCustomerId(), newCustomer.getName(), newCustomer.getEmail());
+    }
+
+    @Test
+    @DisplayName("전체 고객을 조회할 수 있다")
+    public void findAll() throws InterruptedException {
+        // given
+        List<Customer> customers = customerRepository.findAll();
+        // when
+
+        // then
+        assertThat(customers.isEmpty()).isFalse();
+
+    }
+
+    @Test
+    @DisplayName("이름으로 고객을 조회할 수 있다")
+    public void findByName() {
+        // given
+        Optional<Customer> customer = customerRepository.findByName(newCustomer.getName());
+
+        // when
+
+        // then
+        assertThat(customer.isEmpty()).isFalse();
+    }
+
+    @Test
+    @DisplayName("이메일로 고객을 조회할 수 있다")
+    public void findByEmail() {
+        // given
+        Optional<Customer> customer = customerRepository.findByEmail(newCustomer.getEmail());
+        // when
+
+        // then
+        assertThat(customer.isEmpty()).isFalse();
+    }
+
+    @Test
+    @DisplayName("고객을 수정할 수 있다.")
+    public void updateCustomer() {
+        // given
+        newCustomer.changeName("updated-user");
+        // when
+        customerRepository.update(newCustomer);
+
+        // then
+        List<Customer> all = customerRepository.findAll();
+
+        assertThat(all.isEmpty()).isFalse();
+        assertThat(all.size()).isEqualTo(1);
+        assertThat(all.get(0))
+                .extracting(Customer::getCustomerId, Customer::getName, Customer::getEmail)
+                .containsExactly(newCustomer.getCustomerId(), newCustomer.getName(), newCustomer.getEmail());
+
+
+        Optional<Customer> retrievedCustomer = customerRepository.findById(newCustomer.getCustomerId());
+        assertThat(retrievedCustomer.isEmpty()).isFalse();
+        assertThat(retrievedCustomer.get())
+                .extracting(Customer::getCustomerId, Customer::getName, Customer::getEmail)
+                .containsExactly(newCustomer.getCustomerId(), newCustomer.getName(), newCustomer.getEmail());
+
+    }
+
+
+}
