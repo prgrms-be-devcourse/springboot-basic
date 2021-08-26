f5fbee06bbee3b849846ec59e80b51e66dfa04da
yekyeong <kwonliz6.6@gmail.com>
2021-08-26 18:21:20 +0900

    feat : logback 설정추가
    error가 파일로 기록될 수 있게 함

--- project_order/src/main/java/org/programmers/ProjectOrderApplication.java
+++ project_order/src/main/java/org/programmers/ProjectOrderApplication.java
@@ -1,13 +1,24 @@
 package org.programmers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(
+        basePackages = {"org.programmers.order", "org.programmers.voucher", "org.programmers.customer"}
+)
 public class ProjectOrderApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ProjectOrderApplication.class, args);
-    }
+    private static final Logger logger = LoggerFactory.getLogger(ProjectOrderApplication.class);
 
+    public static void main(String[] args) {
+
+        SpringApplication.run(ProjectOrderApplication.class, args);
+
+        logger.error("logger name -> {}", logger.getName());
+
+    }
 }
