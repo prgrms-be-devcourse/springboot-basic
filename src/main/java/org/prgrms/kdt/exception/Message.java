a6073e07f704520104d3fc37bfb484690059fe56
suhyunsim <cnc1106@naver.com>
2021-08-26 11:51:15 +0900

    feat: 이전 과제에 구현한 exception 관련 내용 추가

---
+++ src/main/java/org/prgrms/kdt/exception/Message.java
@@ -0,0 +1,7 @@
+package org.prgrms.kdt.exception;
+
+public class Message {
+    public static final String CANNOT_FIND_VOUCHER = "Cannot find a voucher for ";
+    public static final String WRONG_COMMAND_MESSAGE = "Wrong input. Type exit, create or list";
+    public static final String WRONG_TYPE_MESSAGE = "Wrong select. Choose type number 1 or 2";
+}
