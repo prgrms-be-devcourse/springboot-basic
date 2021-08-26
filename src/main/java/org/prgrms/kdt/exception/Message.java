530221a72e46e29235ad2d0791ee21d416234570
suhyunsim <cnc1106@naver.com>
2021-08-26 12:16:46 +0900

    feat: Custom 예외구현 및 메세지 상수화가

--- src/main/java/org/prgrms/kdt/exception/Message.java
+++ src/main/java/org/prgrms/kdt/exception/Message.java
@@ -4,4 +4,5 @@ public class Message {
     public static final String CANNOT_FIND_VOUCHER = "Cannot find a voucher for ";
     public static final String WRONG_COMMAND_MESSAGE = "Wrong input. Type exit, create or list";
     public static final String WRONG_TYPE_MESSAGE = "Wrong select. Choose type number 1 or 2";
+    public static final String WRONG_FORMAT_MESSAGE = "Wrong format";
 }
