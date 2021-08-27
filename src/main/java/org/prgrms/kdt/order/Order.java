39dcd7a6d79a4cece701618621c747e1ed74daa6
BeautterLife <pgh9801@gmail.com>
2021-08-27 21:53:34 +0900

    refactor/Entity->order 폴더 이동

--- src/main/java/org/prgrms/kdt/order/Order.java
+++ src/main/java/org/prgrms/kdt/order/Order.java
@@ -1,24 +1,18 @@
-package org.prgrms.kdt.Entity;
-//entity class
-//비즈니스 로직 - discount, amount, totalMoney 등
+package org.prgrms.kdt.order;
 
-import org.prgrms.kdt.VO.OrderItem;
-import org.prgrms.kdt.VO.OrderStatus;
-import org.prgrms.kdt.VO.Voucher;
+import org.prgrms.kdt.voucher.Voucher;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public class Order {
-    private final UUID orderId; //identifier
+    private final UUID orderId;
     private final UUID customerId;
     private final List<OrderItem> orderItems;
-    // private long discountAmount; Class로 정의하여 Order이 discountAmout 객체를 사용하게 하여, 의존성부여
     private Optional<Voucher> voucher;
-    private OrderStatus orderStatus = OrderStatus.ACCEPTED; //초기 상태
+    private OrderStatus orderStatus = OrderStatus.ACCEPTED;
 
-    // voucher 없이 order
     public Order(UUID orderId, UUID customerId, List<OrderItem> orderItems) {
         this.orderId = orderId;
         this.customerId = customerId;
