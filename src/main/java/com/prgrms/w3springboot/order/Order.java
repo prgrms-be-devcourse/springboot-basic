3c616edcecd88a45609942652bd6cfb06025e19d
samkimuel <samkim2626@gmail.com>
2021-08-30 16:13:28 +0900

    refactor: Order class 사용하지 않는 Setter 제거, 필드에 final 키워드 추가, orderStatus를 생성자에서 초기화하도록 수정, 매직 넘버를 상수로 선언

--- src/main/java/com/prgrms/w3springboot/order/Order.java
+++ src/main/java/com/prgrms/w3springboot/order/Order.java
@@ -7,17 +7,20 @@ import java.util.Optional;
 import java.util.UUID;
 
 public class Order {
+    private static final long INITIAL_VALUE = 0L;
+
     private final UUID orderId;
     private final UUID customerId;
     private final List<OrderItem> orderItems;
-    private Optional<Voucher> voucher;
-    private OrderStatus orderStatus = OrderStatus.ACCEPTED;
+    private final Optional<Voucher> voucher;
+    private final OrderStatus orderStatus;
 
     public Order(UUID orderId, UUID customerId, List<OrderItem> orderItems) {
         this.orderId = orderId;
         this.customerId = customerId;
         this.orderItems = orderItems;
         this.voucher = Optional.empty();
+        this.orderStatus = OrderStatus.ACCEPTED;
     }
 
     public Order(UUID orderId, UUID customerId, List<OrderItem> orderItems, Voucher voucher) {
@@ -25,17 +28,14 @@ public class Order {
         this.customerId = customerId;
         this.orderItems = orderItems;
         this.voucher = Optional.of(voucher);
+        this.orderStatus = OrderStatus.ACCEPTED;
     }
 
     public long totalAmount() {
         var beforeDiscount = orderItems.stream()
                 .map(v -> v.getProductPrice() * v.getQuantity())
-                .reduce(0L, Long::sum);
+                .reduce(INITIAL_VALUE, Long::sum);
         return voucher.map(value -> value.discount(beforeDiscount)).orElse(beforeDiscount);
-    }
-
-    public void setOrderStatus(OrderStatus orderStatus) {
-        this.orderStatus = orderStatus;
     }
 
     public UUID getOrderId() {
