33a059bfd36605e5ac024f5c0d766a32f133216c
kwhyo <rkgmlgygml@naver.com>
2021-08-31 02:04:57 +0900

    config 수정 - 데이터소스, JdbcTemplate 빈생성

--- DevCourse/src/main/java/org/prgrms/kdt/devcourse/AppConfiguration.java
+++ DevCourse/src/main/java/org/prgrms/kdt/devcourse/AppConfiguration.java
@@ -1,13 +1,30 @@
 package org.prgrms.kdt.devcourse;
 
+import com.zaxxer.hikari.HikariDataSource;
 import org.prgrms.kdt.devcourse.config.YamlPropertiesFactory;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.*;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+
+import javax.sql.DataSource;
 
 @Configuration
 @ComponentScan
 @PropertySource("classpath:application.yaml")
 public class AppConfiguration {
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource dataSource(){
+        return DataSourceBuilder.create()
+                .build();
+    }
+    @Bean
+    public NamedParameterJdbcTemplate namedParameterJdbcTemplate(DataSource dataSource){
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+        return new NamedParameterJdbcTemplate(jdbcTemplate);
+    }
+
 
 }
