9db0474b59412b38df03f2ec60472d59a8eab35a
yhh1056 <yhh1056@naver.com>
2021-08-30 17:51:51 +0900

    Build: project init
    mysql 커넥팅 정보와 디펜던시를 추가하였습니다.

---
+++ src/main/java/org/prgms/kdt/KdtApplication.java
@@ -0,0 +1,13 @@
+package org.prgms.kdt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KdtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KdtApplication.class, args);
+    }
+
+}
