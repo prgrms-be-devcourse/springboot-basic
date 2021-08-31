b8f0ffc524505662c0adf42caa0ed33f0b9006d3
devrunner21 <devrunner21@gmail.com>
2021-08-31 22:55:57 +0900

    [FEAT] Customers와 Vouchers 테이블 스키마 파일을 커밋하라.

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,20 @@
+CREATE TABLE customers (
+    customer_id BINARY(16) PRIMARY KEY,
+    name varchar(20) NOT NULL,
+    email varchar(50) NOT NULL,
+    last_login_at datetime DEFAULT NULL,
+    created_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+    CONSTRAINT unq_user_email UNIQUE (email)
+);
+
+
+CREATE TABLE vouchers (
+    voucher_id	BINARY(16) NOT NULL PRIMARY KEY,
+    customer_id	BINARY(16) NOT NULL,
+    voucher_type char(2) NOT NULL,
+    discount_percent bigint	NULL DEFAULT 0,
+    discount_amount	bigint NULL	DEFAULT 0,
+    use_yn char(2) NOT NULL	DEFAULT 'N',
+    created_at datetime	NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+    used_at datetime NULL
+);
