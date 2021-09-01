f574a9b84a3c1bc79c46f7b6e68ca144472e8e6b
suhyunsim <cnc1106@naver.com>
2021-09-02 04:22:46 +0900

    feat: 예외처리 메세지 추가

--- src/main/java/org/prgrms/kdt/exception/Message.java
+++ src/main/java/org/prgrms/kdt/exception/Message.java
@@ -5,4 +5,7 @@ public class Message {
     public static final String WRONG_COMMAND_MESSAGE = "Wrong input. Type exit, create or list";
     public static final String WRONG_TYPE_MESSAGE = "Wrong select. Choose type number 1 or 2";
     public static final String WRONG_FORMAT_MESSAGE = "Wrong format";
+    public static final String NEGATIVE_AMOUNT_MESSAGE = "Amount should be positive";
+    public static final String ZERO_AMOUNT_MESSAGE = "Amount should not be zero";
+    public static final String MAXIMUM_AMOUNT_MESSAGE = "Amount should be less than 10000";
 }
