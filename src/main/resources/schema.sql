a5068cd1fe9d530e01faf8e4e54fe1bcdd3de31b
suhyunsim <cnc1106@naver.com>
2021-09-02 07:56:03 +0900

    feat: vouchers 테이블 스키마 작성

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,4 +1,4 @@
-DROP TABLE IF EXISTS customers;
+DROP TABLE IF EXISTS customers, vouchers;
 
 CREATE TABLE customers
 (
@@ -14,4 +14,14 @@ CREATE TABLE customers
                   datetime
                               NOT NULL DEFAULT CURRENT_TIMESTAMP(),
     CONSTRAINT unq_user_email UNIQUE (email)
+);
+
+CREATE TABLE vouchers
+(
+    voucher_id BINARY(16) PRIMARY KEY,
+    fixed BOOLEAN NOT NULL DEFAULT 0,
+    percent BOOLEAN NOT NULL DEFAULT 0,
+    amount LONG NOT NULL,
+    created_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+    customer_id BINARY(16) REFERENCES customers (customer_id) ON UPDATE CASCADE
 );
