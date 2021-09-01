8c789450461d212b67d2ae8884e8575c2631c807
suhyunsim <cnc1106@naver.com>
2021-09-02 04:23:55 +0900

    test: OrderService 테스트 코드 추가

---
+++ src/test/java/org/prgrms/kdt/service/OrderServiceTest.java
@@ -0,0 +1,72 @@
+package org.prgrms.kdt.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.prgrms.kdt.domain.order.Order;
+import org.prgrms.kdt.domain.order.OrderItem;
+import org.prgrms.kdt.domain.order.OrderRepository;
+import org.prgrms.kdt.domain.order.OrderStatus;
+import org.prgrms.kdt.domain.voucher.FixedAmountVoucher;
+import org.prgrms.kdt.domain.voucher.Vouchers;
+
+import java.util.List;
+import java.util.UUID;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.*;
+
+class OrderServiceTest {
+
+    @Test
+    @DisplayName("오더 생성 (stub)")
+    void createOrder() {
+        //given
+        var voucherRepository = new Vouchers();
+        var fixedAmountVoucher = new FixedAmountVoucher(UUID.randomUUID(), 100);
+        voucherRepository.insert(fixedAmountVoucher);
+        var sut = new OrderService(new VoucherService(voucherRepository), new OrderRepositoryStub());
+
+        //when
+        var order = sut.createOrder(UUID.randomUUID(), List.of(new OrderItem(UUID.randomUUID(), 200, 1)), fixedAmountVoucher.getVoucherId());
+
+        //then
+        assertThat(order.totalAmount(), is(100L));
+        assertThat(order.getVoucher().isEmpty(), is(false));
+        assertThat(order.getVoucher().get().getVoucherId(), is(fixedAmountVoucher.getVoucherId()));
+        assertThat(order.getOrderStatus(), is(OrderStatus.ACCEPTED));
+    }
+
+    @Test
+    @DisplayName("오더 생성 (mock)")
+    void createOrderByMock() {
+        //given
+        var voucherServiceMock = mock(VoucherService.class);
+        var orderRepositoryMock = mock(OrderRepository.class);
+        var fixedAmountVoucher = new FixedAmountVoucher(UUID.randomUUID(), 100);
+        when(voucherServiceMock.getVoucher(fixedAmountVoucher.getVoucherId())).thenReturn(fixedAmountVoucher);
+        var sut = new OrderService(voucherServiceMock, orderRepositoryMock);
+
+        //when
+        var order = sut.createOrder(
+                UUID.randomUUID(),
+                List.of(new OrderItem(UUID.randomUUID(), 200, 1)),
+                fixedAmountVoucher.getVoucherId());
+
+        //then
+        assertThat(order.totalAmount(), is(100L));
+        assertThat(order.getVoucher().isEmpty(), is(false));
+        var inOrder = inOrder(voucherServiceMock, orderRepositoryMock);
+        inOrder.verify(voucherServiceMock).getVoucher(fixedAmountVoucher.getVoucherId());
+        inOrder.verify(orderRepositoryMock).insert(order);
+        inOrder.verify(voucherServiceMock).useVoucher(fixedAmountVoucher);
+
+    }
+
+    class OrderRepositoryStub implements OrderRepository {
+        @Override
+        public void insert(Order order) {
+        }
+    }
+
+}
