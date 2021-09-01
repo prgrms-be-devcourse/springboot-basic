06eba2f17221ea55e54d0f92f16a505ffa0a612d
maenguin <maenguin924@gmail.com>
2021-09-02 00:47:47 +0900

    refactor : Utils 클래스 생성 후 toUUID 메소드 이동

---
+++ src/main/java/com/prgrms/devkdtorder/util/Utils.java
@@ -0,0 +1,11 @@
+package com.prgrms.devkdtorder.util;
+
+import java.nio.ByteBuffer;
+import java.util.UUID;
+
+public class Utils {
+    public static UUID toUUID(byte[] bytes) {
+        ByteBuffer byteBuffer = ByteBuffer.wrap(bytes);
+        return new UUID(byteBuffer.getLong(), byteBuffer.getLong());
+    }
+}
