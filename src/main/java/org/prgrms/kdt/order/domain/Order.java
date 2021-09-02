1583e8ef70b14fd43f4785e29dbe3673014685cb
dlfdyd96 <1yongs_@naver.com>
2021-09-02 15:00:45 +0900

    FIX: 의미가 불분명한 매직넘버 처리

--- src/main/java/org/prgrms/kdt/order/domain/Order.java
+++ src/main/java/org/prgrms/kdt/order/domain/Order.java
@@ -9,20 +9,21 @@ import java.util.Optional;
 import java.util.UUID;
 
 public class Order {
+    private static final Long INIT_BEFORE_DISCOUNT = 0L;
+
     private final UUID orderId;
     private final UUID customerId;
     private final List<OrderItem> orderItems;
     private Optional<Voucher> voucher;
     private OrderStatus orderStatus = OrderStatus.ACCEPTED;
 
-    public Order(UUID orderId, UUID customerId, List<OrderItem> orderItems, Voucher voucher) {
+    public Order(UUID orderId, UUID customerId, List<OrderItem> orderItems, Voucher voucher) { // 다음 강의에서 사
         this.orderId = orderId;
         this.customerId = customerId;
         this.orderItems = orderItems;
         this.voucher = Optional.of(voucher);
     }
 
-    // voucher가 없을 때
     public Order(UUID orderId, UUID customerId, List<OrderItem> orderItems) {
         this.orderId = orderId;
         this.customerId = customerId;
@@ -32,18 +33,17 @@ public class Order {
 
     public long totalAmount() {
         var beforeDiscount = orderItems.stream()
-                .map(v -> v.getProductPrice() * v.getQuantity())
-                .reduce(0L, Long::sum);
-        // voucher가 없을 때
+                .map(voucher -> voucher.getProductPrice() * voucher.getQuantity())
+                .reduce(INIT_BEFORE_DISCOUNT, Long::sum);
         return voucher.map(value -> value.discount(beforeDiscount)).orElse(beforeDiscount);
     }
 
     public void setOrderStatus(OrderStatus orderStatus) {
         this.orderStatus = orderStatus;
-    }
+    } // 다음 강의에서 사용
 
     public void applyVoucher(Voucher voucher) {
         this.voucher = Optional.of(voucher);
-    }
+    } // 다음 강의에서 사용
 
 }
