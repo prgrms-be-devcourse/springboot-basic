2382d21fb5b42eb1c7a4dc0bcfcdfe644814cff2
devrunner21 <devrunner21@gmail.com>
2021-09-02 17:35:31 +0900

    [FEAT] VoucherRepository 테스트 코드를 작성하라.

--- src/test/java/org/prgrms/kdt/kdtspringorder/voucher/repository/VoucherRepositoryTest.java
+++ src/test/java/org/prgrms/kdt/kdtspringorder/voucher/repository/VoucherRepositoryTest.java
@@ -1,13 +1,15 @@
 package org.prgrms.kdt.kdtspringorder.voucher.repository;
 
+import com.wix.mysql.EmbeddedMysql;
+import com.wix.mysql.config.Charset;
+import com.wix.mysql.config.MysqldConfig;
 import org.junit.jupiter.api.*;
 import org.prgrms.kdt.kdtspringorder.voucher.domain.FixedAmountVoucher;
 import org.prgrms.kdt.kdtspringorder.voucher.domain.Voucher;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -17,16 +19,39 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 import static org.hamcrest.Matchers.hasSize;
 
-@Profile("dev")
-@SpringJUnitConfig
+import static com.wix.mysql.EmbeddedMysql.anEmbeddedMysql;
+import static com.wix.mysql.ScriptResolver.classPathScript;
+import static com.wix.mysql.distribution.Version.v8_0_11;
+
+@SpringBootTest
+@Transactional
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @DisplayName("VoucherRepository 단위 테스트")
 class VoucherRepositoryTest {
 
-    private MemoryVoucherRepository voucherRepository;
+    @Autowired
+    private VoucherRepository voucherRepository;
 
-    @BeforeEach
+    EmbeddedMysql embeddedMysql;
+
+    @BeforeAll
     void setUp(){
-        voucherRepository = new MemoryVoucherRepository();
+
+        MysqldConfig config = MysqldConfig.aMysqldConfig(v8_0_11)
+            .withCharset(Charset.UTF8)
+            .withPort(2215)
+//            .withUser("test", "1234")
+            .withTimeZone("Asia/Seoul")
+            .build();
+
+        embeddedMysql = anEmbeddedMysql(config)
+            .addSchema("test-order-mgmt", classPathScript("schema.sql"))
+            .start();
+    }
+
+    @AfterAll
+    void cleanup() {
+        embeddedMysql.stop();
     }
 
     @Nested
@@ -34,6 +59,21 @@ class VoucherRepositoryTest {
     class Describe_findAll {
 
         @Nested
+        @DisplayName("만약 목록이 비어있으면")
+        class Context_empty{
+
+            @Test
+            @DisplayName("빈 바우처 목록을 반환합니다.")
+            void It_return_empty_list() {
+                final List<Voucher> vouchers = voucherRepository.findAll();
+                assertThat(vouchers, notNullValue());
+                assertThat(vouchers, hasSize(0));
+            }
+
+        }
+
+        @Nested
+        @Rollback
         @DisplayName("만약 목록이 비어있지 않으면")
         class Context_not_empty{
 
@@ -58,36 +98,30 @@ class VoucherRepositoryTest {
 
         }
 
-//        @Nested
-//        @DisplayName("만약 목록이 비어있으면")
-//        class Context_empty{
-//
-//            @Test
-//            @DisplayName("빈 바우처 목록을 반환합니다.")
-//            void It_return_empty_list() {
-//                final List<Voucher> vouchers = voucherRepository.findAll();
-//                assertThat(vouchers, notNullValue());
-//                assertThat(vouchers, hasSize(0));
-//            }
-//
-//        }
-
     }
 
     @Nested
     @DisplayName("findById 메서드는")
     class Describe_getVoucher {
 
+        private Voucher newVoucher;
+        private UUID newVoucherId = UUID.randomUUID();
+        private long newVoucherDiscount = 100;
+
+        @BeforeEach
+        void setUp() {
+            newVoucher = new FixedAmountVoucher(newVoucherId, newVoucherDiscount);
+            voucherRepository.insert(newVoucher);
+        }
+
         @Nested
         @DisplayName("VoucherId로 바우처를 조회한다면")
         class Context_voucher_id{
 
-            private UUID invalidVoucherId = UUID.randomUUID();
-
             @Test
             @DisplayName("Optional<Voucher> 객체를 반환합니다.")
             void it_return_voucher_optional() {
-                final Optional<Voucher> foundVoucherOptional = voucherRepository.findById(invalidVoucherId);
+                final Optional<Voucher> foundVoucherOptional = voucherRepository.findById(newVoucherId);
                 assertThat(foundVoucherOptional, not(nullValue()));
                 assertThat(foundVoucherOptional, instanceOf(Optional.class));
             }
