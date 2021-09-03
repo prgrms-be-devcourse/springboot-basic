de1c51f194551461544f9491a54ad2c935deb0f5
yekyeong <kwonliz6.6@gmail.com>
2021-09-03 16:42:35 +0900

    refactor : customer의 name 수정 가능 및 name의 관한 validation 추가

--- project_order/src/main/java/org/programmers/customer/Customer.java
+++ project_order/src/main/java/org/programmers/customer/Customer.java
@@ -6,13 +6,15 @@ import java.util.UUID;
 public class Customer {
 
     private final UUID customerId;
-    private final String name;
+    private String name;
     private final Gender gender;
     private final String email;
     private final LocalDateTime createdAt;
     private LocalDateTime lastLoginAt;
 
     public Customer(UUID customerId, String name, Gender gender, String email, LocalDateTime createdAt) {
+        validateName(name);
+
         this.customerId = customerId;
         this.name = name;
         this.gender = gender;
@@ -21,12 +23,26 @@ public class Customer {
     }
 
     public Customer(UUID customerId, String name, Gender gender, String email, LocalDateTime createdAt, LocalDateTime lastLoginAt) {
+        validateName(name);
+
         this.customerId = customerId;
         this.name = name;
         this.gender = gender;
         this.email = email;
         this.createdAt = createdAt;
         this.lastLoginAt = lastLoginAt;
+    }
+
+    private void validateName(String name) {
+        if (name.isBlank()) {
+            new RuntimeException("Name should not be blank");
+        }
+    }
+
+    private void changeName(String name){
+        validateName(name);
+
+        this.name = name;
     }
 
     public void login() {
