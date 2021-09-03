cbcada50d6856dae654a05f3039a1d4708ab1bc3
yhh1056 <yhh1056@naver.com>
2021-09-03 16:51:07 +0900

    Refactor: 테스트 코드 방식 변경

--- src/test/java/org/prgrms/kdt/customer/CustomerJdbcRepositoryTest.java
+++ src/test/java/org/prgrms/kdt/customer/CustomerJdbcRepositoryTest.java
@@ -9,6 +9,7 @@ import static org.hamcrest.core.Is.is;
 import java.time.LocalDateTime;
 import java.util.UUID;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
@@ -24,58 +25,62 @@ class CustomerJdbcRepositoryTest extends BaseRepositoryTest {
     @Autowired
     CustomerJdbcRepository customerJdbcRepository;
 
-    Customer newCustomer;
-
-    @BeforeAll
-    void beforeAll() {
-        newCustomer = new Customer(UUID.randomUUID(), "test-user", "test-user@email.com", LocalDateTime.now());
+    @BeforeEach
+    void beforeEach() {
+        customerJdbcRepository.deleteAll();
     }
 
     @Test
-    @Order(1)
     void testHikariConnectionPool() {
         assertThat(dataSource.getClass().getName(), is("com.zaxxer.hikari.HikariDataSource"));
     }
 
     @Test
-    @Order(2)
     @DisplayName("고객을 추가할 수 있다.")
     void testInsert() {
-        customerJdbcRepository.insert(newCustomer);
+        UUID customerId = UUID.randomUUID();
+        var customer = givenCustomer(customerId);
+        customerJdbcRepository.insert(customer);
 
-        var unknown = customerJdbcRepository.findById(newCustomer.getCustomerId());
+        var unknown = customerJdbcRepository.findById(customer.getCustomerId());
 
         assertThat(unknown.isEmpty(), is(false));
-        assertThat(unknown.get(), samePropertyValuesAs(newCustomer));
+        assertThat(unknown.get(), samePropertyValuesAs(customer));
     }
 
     @Test
-    @Order(3)
     @DisplayName("전체 고객을 조회할 수 있다.")
-    void testFindAll() throws InterruptedException {
+    void testFindAll() {
+        customerJdbcRepository.insert(givenCustomer("tester1"));
+        customerJdbcRepository.insert(givenCustomer("tester2"));
+        customerJdbcRepository.insert(givenCustomer("tester3"));
+
         var customers = customerJdbcRepository.findAll();
         assertThat(customers.isEmpty(), is(false));
-
-//        Thread.sleep(5000);
+        assertThat(customers, hasSize(3));
     }
 
     @Test
-    @Order(4)
     @DisplayName("이름으로 고객을 조회할 수 있다.")
     void testFindByName() {
-        var customer = customerJdbcRepository.findByName(newCustomer.getName());
-        assertThat(customer.isEmpty(), is(false));
+        var customer = givenCustomer(UUID.randomUUID());
+        customerJdbcRepository.insert(customer);
+
+        var findCustomer = customerJdbcRepository.findByName(customer.getName());
+        assertThat(findCustomer.isEmpty(), is(false));
 
         var unknown = customerJdbcRepository.findByName("unknown-user");
         assertThat(unknown.isEmpty(), is(true));
     }
 
     @Test
-    @Order(5)
     @DisplayName("이메일로 고객을 조회할 수 있다.")
     void testFindByEmail() {
-        var customer = customerJdbcRepository.findByEmail(newCustomer.getEmail());
-        assertThat(customer.isEmpty(), is(false));
+        var customer = givenCustomer(UUID.randomUUID());
+        customerJdbcRepository.insert(customer);
+
+        var findCustomer = customerJdbcRepository.findByEmail(customer.getEmail());
+        assertThat(findCustomer.isEmpty(), is(false));
 
         var unknown = customerJdbcRepository.findByEmail("unknown-user@email.com");
         assertThat(unknown.isEmpty(), is(true));
@@ -83,21 +88,31 @@ class CustomerJdbcRepositoryTest extends BaseRepositoryTest {
 
 
     @Test
-    @Order(6)
     @DisplayName("고객을 수정할 수 있다.")
     void testUpdate() {
-        newCustomer.changeName("updated-user");
-        customerJdbcRepository.update(newCustomer);
+        var customer = givenCustomer(UUID.randomUUID());
+        customerJdbcRepository.insert(customer);
+
+        customer.changeName("updated-user");
+        customerJdbcRepository.update(customer);
 
         var all = customerJdbcRepository.findAll();
 
         assertThat(all, hasSize(1));
-        assertThat(all, everyItem(samePropertyValuesAs(newCustomer)));
+        assertThat(all, everyItem(samePropertyValuesAs(customer)));
 
-        var retrievedCustomer = customerJdbcRepository.findById(newCustomer.getCustomerId());
+        var findCustomer = customerJdbcRepository.findById(customer.getCustomerId());
 
-        assertThat(retrievedCustomer.isEmpty(), is(false));
-        assertThat(retrievedCustomer.get(), samePropertyValuesAs(newCustomer));
+        assertThat(findCustomer.isEmpty(), is(false));
+        assertThat(findCustomer.get(), samePropertyValuesAs(customer));
     }
 
+
+    private Customer givenCustomer(UUID customerId) {
+        return new Customer(customerId, "tester", "tester@email.comn", LocalDateTime.now());
+    }
+
+    private Customer givenCustomer(String name) {
+        return new Customer(UUID.randomUUID(), name, name + "@email.com", LocalDateTime.now());
+    }
 }
