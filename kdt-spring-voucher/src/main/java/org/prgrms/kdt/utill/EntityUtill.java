dd87767c40944ec8a97b7834fdc3f4c9ebf492b0
dbwlgna98@naver.com <dbwlgna98@naver.com>
2021-09-05 03:45:47 +0900

    Feat: JdbcRepo 공통 기능 EntityUtill 클래스에 추가

---
+++ kdt-spring-voucher/src/main/java/org/prgrms/kdt/utill/EntityUtill.java
@@ -0,0 +1,86 @@
+package org.prgrms.kdt.utill;
+
+import org.prgrms.kdt.domain.CustomerEntity;
+import org.prgrms.kdt.domain.CustomerVoucherEntity;
+import org.prgrms.kdt.domain.VoucherEntity;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
+
+import java.nio.ByteBuffer;
+import java.sql.Timestamp;
+import java.util.HashMap;
+import java.util.UUID;
+
+import static org.prgrms.kdt.jdbcRepository.CustomerJdbcRepository.toUUID;
+
+@Component
+public class EntityUtill {
+
+    public static UUID toUUID(byte[] bytes) {
+        var byteBuffer = ByteBuffer.wrap(bytes);
+        return new UUID(byteBuffer.getLong(), byteBuffer.getLong());
+    }
+
+    private HashMap<String, Object> toParamMap(CustomerEntity customer) {
+        return new HashMap<>() {
+            {
+                put("customerId", customer.getCustomerId().toString().getBytes());
+                put("name", customer.getName());
+                put("email", customer.getEmail());
+                put("createdAt", Timestamp.valueOf(customer.getCreatedAt()));
+                put("lastLoginAt", customer.getLastLoginAt() != null ? Timestamp.valueOf(customer.getLastLoginAt()) : null);
+            }
+        };
+    }
+
+    public static RowMapper<CustomerEntity> customerEntityRowMapper = (resultSet, i) -> {
+        var customerId = toUUID(resultSet.getBytes("customer_id"));
+        var customerName = resultSet.getString("name");
+        var email = resultSet.getString("email");
+        var lastLoginAt = resultSet.getTimestamp("last_login_at") != null ? resultSet.getTimestamp("last_login_at").toLocalDateTime() : null;
+        var createdAt = resultSet.getTimestamp("created_at").toLocalDateTime();
+        return new CustomerEntity(customerId, customerName, email, lastLoginAt, createdAt);
+    };
+
+    private HashMap<String, Object> toParamMap(CustomerVoucherEntity customerVoucherEntity) {
+        return new HashMap<>() {
+            {
+                put("customerVoucherId", customerVoucherEntity.getCustomerVoucherId().toString().getBytes());
+                put("customerId", customerVoucherEntity.getCustomerId().toString().getBytes());
+                put("voucherId", customerVoucherEntity.getVoucherId().toString().getBytes());
+                put("createdAt", Timestamp.valueOf(customerVoucherEntity.getCreatedAt()));
+            }
+        };
+    }
+
+    private static RowMapper<CustomerVoucherEntity> customerVoucherEntityRowMapper = (resultSet, i) -> {
+        var customerVoucherId = toUUID(resultSet.getBytes("customer_voucher_id"));
+        var customerId = toUUID(resultSet.getBytes("customer_id"));
+        var voucherId = toUUID(resultSet.getBytes("voucher_id"));
+        var createdAt = resultSet.getTimestamp("created_at").toLocalDateTime();
+        return new CustomerVoucherEntity(customerVoucherId, customerId, voucherId, createdAt);
+    };
+
+    private HashMap<String, Object> toParamMap(VoucherEntity voucherEntity) {
+        return new HashMap<>() {
+            {
+                put("voucherId", voucherEntity.getVoucherId().toString().getBytes());
+                put("voucherType", voucherEntity.getVoucherType());
+                put("discount", voucherEntity.getDiscount());
+                put("createdAt", Timestamp.valueOf(voucherEntity.getCreatedAt()));
+            }
+        };
+    }
+
+    public static RowMapper<VoucherEntity> voucherEntityRowMapper = (resultSet, i) -> {
+        var voucherId = toUUID(resultSet.getBytes("voucher_id"));
+        var voucherType = resultSet.getString("voucher_type");
+        var discount = resultSet.getLong("discount");
+        var createdAt = resultSet.getTimestamp("created_at").toLocalDateTime();
+        return new VoucherEntity(voucherId, voucherType, discount, createdAt);
+    };
+
+
+
+
+}
