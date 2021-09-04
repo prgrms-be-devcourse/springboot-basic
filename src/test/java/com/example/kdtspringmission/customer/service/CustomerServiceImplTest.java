0479bf53cf7bf340009f805bff86203655365cb1
awesomeo184 <63030569+awesomeo184@users.noreply.github.com>
2021-09-04 18:27:04 +0900

    feat: CustomerService 테스트 코드 추가

--- src/test/java/com/example/kdtspringmission/customer/service/CustomerServiceImplTest.java
+++ src/test/java/com/example/kdtspringmission/customer/service/CustomerServiceImplTest.java
@@ -6,9 +6,13 @@ import static org.mockito.Mockito.*;
 
 import com.example.kdtspringmission.customer.domain.Customer;
 import com.example.kdtspringmission.customer.repository.CustomerRepository;
+import com.example.kdtspringmission.voucher.domain.FixedAmountVoucher;
+import com.example.kdtspringmission.voucher.domain.Voucher;
+import com.example.kdtspringmission.voucher.service.VoucherService;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -32,8 +36,9 @@ public class CustomerServiceImplTest {
                 LocalDateTime.now()));
         }};
         CustomerRepository customerRepository = mock(CustomerRepository.class);
+        VoucherService voucherService = mock(VoucherService.class);
         when(customerRepository.findAll()).thenReturn(customers);
-        CustomerService customerService = new CustomerService(customerRepository);
+        CustomerService customerService = new CustomerService(customerRepository, voucherService);
 
         // when
         List<Customer> allCustomers = customerService.allCustomers();
@@ -41,6 +46,31 @@ public class CustomerServiceImplTest {
         // then
         assertThat(allCustomers).hasSameElementsAs(customers);
         verify(customerRepository).findAll();
+    }
+
+    @Test
+    @DisplayName("고객에게 바우처를 할당한다")
+    void testAssignVoucher() {
+        //given
+        Customer customer = new Customer(UUID.randomUUID(), "tester", "tester@test.com",
+            LocalDateTime.now());
+
+        Voucher voucher = new FixedAmountVoucher(UUID.randomUUID(), 150L);
+
+        CustomerRepository customerRepository = mock(CustomerRepository.class);
+        VoucherService voucherService = mock(VoucherService.class);
+
+        when(customerRepository.findByName(customer.getName())).thenReturn(Optional.of(customer));
+        when(voucherService.getVoucher(voucher.getId())).thenReturn(voucher);
+        CustomerService customerService = new CustomerService(customerRepository, voucherService);
+
+        //when
+        customerService.assignVoucher(customer.getName(), voucher.getId());
+
+        //then
+        assertThat(voucher.getOwnerId()).isEqualTo(customer.getCustomerId());
+        verify(customerRepository).findByName(customer.getName());
+        verify(voucherService).getVoucher(voucher.getId());
     }
 
 }
