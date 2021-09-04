f0eed6dd458418596b7e20759c56ff8ddae9c1fb
eunseo2 <20180972@sungshin.ac.kr>
2021-09-04 23:00:53 +0900

    [mod] vouchers 테이블 정의

--- src/main/java/org/prgms/voucher/vouchers.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE vouchers
-(
-    voucher_id BINARY(16) PRIMARY KEY,
-    voucher_type varchar(20) NOT NULL,
-    amount INT,
-    percent INT,
-    created_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP(),
-);
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -7,3 +7,13 @@ CREATE TABLE customers
     created_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP(),
     CONSTRAINT unq_user_email UNIQUE (email)
 );
+
+
+CREATE TABLE vouchers
+(
+    voucher_id BINARY(16) PRIMARY KEY,
+    voucher_type varchar(20) NOT NULL,
+    amount INT,
+    percent INT,
+    created_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP()
+);
