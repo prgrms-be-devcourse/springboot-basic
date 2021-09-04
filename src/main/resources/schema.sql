c0fff63d4b506f7c46067526b38ef584b7e87529
maenguin <maenguin924@gmail.com>
2021-09-04 18:06:54 +0900

    feat: customer_voucher 스키마 변경

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -20,17 +20,18 @@ CREATE TABLE vouchers
 
 CREATE TABLE customer_voucher
 (
-  customer_voucher_id BINARY(16) PRIMARY KEY,
-  voucher_id          BINARY(16)  NOT NULL,
-  customer_id         BINARY(16)  NOT NULL,
-  use                 BIT(1)      NOT NULL DEFAULT 0,
-  used_at             DATETIME(6),
-  created_at          DATETIME(6) NOT NULL DEFAULT NOW(6),
-  CONSTRAINT fk_voucher_id
-    FOREIGN KEY (voucher_id)
-      REFERENCES vouchers (voucher_id),
-  CONSTRAINT fk_customer_id
-    FOREIGN KEY (customer_id)
-      REFERENCES customers (customer_id)
+    customer_voucher_id BINARY(16) PRIMARY KEY,
+    voucher_id          BINARY(16)  NOT NULL,
+    customer_id         BINARY(16)  NOT NULL,
+    used                BIT(1)      NOT NULL DEFAULT 0,
+    used_at             DATETIME(6),
+    created_at          DATETIME(6) NOT NULL DEFAULT NOW(6),
+    expired_at          DATETIME(6),
+    CONSTRAINT fk_voucher_id
+        FOREIGN KEY (voucher_id)
+            REFERENCES vouchers (voucher_id),
+    CONSTRAINT fk_customer_id
+        FOREIGN KEY (customer_id)
+            REFERENCES customers (customer_id)
 );
 
