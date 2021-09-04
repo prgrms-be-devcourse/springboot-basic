b3fdf7765785facf085e9f7f4d124d20fce82bd8
Kwonkyu <park2348190@naver.com>
2021-09-05 00:00:49 +0900

    Implemented test codes for jdbc voucher repository. - MySQL 8.0 / JDBC를 사용하는 Voucher Repository에 대한 CRUD 테스트 코드 작성. - 테스트 독립성을 위해 매 테스트마다 vouchers, customers DB 초기화.

---
+++ voucher/src/test/java/com/programmers/voucher/repository/voucher/JdbcVoucherRepositoryTest.java
@@ -0,0 +1,108 @@
+package com.programmers.voucher.repository.voucher;
+
+import com.programmers.voucher.entity.customer.Customer;
+import com.programmers.voucher.entity.voucher.DiscountPolicy;
+import com.programmers.voucher.entity.voucher.Voucher;
+import com.programmers.voucher.repository.customer.CustomerRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class JdbcVoucherRepositoryTest {
+
+    @Autowired
+    VoucherRepository jdbcVoucherRepository;
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @BeforeEach
+    void init() {
+        jdbcTemplate.execute("DELETE FROM customers");
+        jdbcTemplate.execute("DELETE FROM vouchers");
+    }
+
+    @Test
+    @DisplayName("Voucher Creation Test")
+    void createVoucher() {
+        final Customer customer = customerRepository.save(new Customer("username", "alias"));
+
+        String voucherName = "voucher1";
+        int price = 10000;
+        DiscountPolicy discountPolicy = new DiscountPolicy(2500, DiscountPolicy.Type.FIXED);
+
+        final Voucher voucher = jdbcVoucherRepository.save(new Voucher(voucherName, discountPolicy, customer.getId()));
+        assertEquals(voucherName, voucher.getName());
+        assertNotNull(voucher.getCreatedAt());
+        assertEquals(discountPolicy.getType(), voucher.getDiscountPolicy().getType());
+        assertEquals(discountPolicy.getAmount(), voucher.getDiscountPolicy().getAmount());
+
+        assertEquals(price - discountPolicy.getAmount(), discountPolicy.discount(price));
+    }
+
+    @Test
+    @DisplayName("Voucher Read Test")
+    void readVoucher() {
+        final Customer customer = customerRepository.save(new Customer("username", "alias"));
+
+        String voucherName = "voucherReadme";
+        final DiscountPolicy discountPolicy = new DiscountPolicy(2500, DiscountPolicy.Type.FIXED);
+        final Voucher voucher = jdbcVoucherRepository.save(new Voucher(voucherName, discountPolicy, customer.getId()));
+
+        final Optional<Voucher> byId = jdbcVoucherRepository.findById(voucher.getId());
+        assertTrue(byId.isPresent());
+        assertEquals(voucherName, byId.get().getName());
+
+        final List<Voucher> allByCustomer = jdbcVoucherRepository.findAllByCustomer(customer.getId());
+        assertEquals(1, allByCustomer.size());
+        assertEquals(byId.get(), allByCustomer.get(0));
+    }
+
+    @Test
+    @DisplayName("Voucher Update Test")
+    void updateVoucher() {
+        final Customer customer = customerRepository.save(new Customer("username", "alias"));
+
+        String voucherName = "voucherUpdateMe";
+        final DiscountPolicy discountPolicy = new DiscountPolicy(2500, DiscountPolicy.Type.FIXED);
+        final Voucher voucher = jdbcVoucherRepository.save(new Voucher(voucherName, discountPolicy, customer.getId()));
+
+        discountPolicy.setAmount(5000);
+        jdbcVoucherRepository.update(voucher);
+
+        final Optional<Voucher> updatedAmount = jdbcVoucherRepository.findById(voucher.getId());
+        assertTrue(updatedAmount.isPresent());
+        assertEquals(5000, updatedAmount.get().getDiscountPolicy().getAmount());
+
+        discountPolicy.setType(DiscountPolicy.Type.PERCENTAGE);
+        jdbcVoucherRepository.update(voucher);
+
+        final Optional<Voucher> updatedType = jdbcVoucherRepository.findById(voucher.getId());
+        assertTrue(updatedType.isPresent());
+        assertEquals(DiscountPolicy.Type.PERCENTAGE, updatedType.get().getDiscountPolicy().getType());
+    }
+
+    @Test
+    @DisplayName("Voucher Deletion Test")
+    void deleteVoucher() {
+        final Customer customer = customerRepository.save(new Customer("username", "alias"));
+        final Voucher voucher = jdbcVoucherRepository.save(new Voucher("voucher", new DiscountPolicy(2500, DiscountPolicy.Type.FIXED), customer.getId()));
+        jdbcVoucherRepository.deleteById(voucher.getId());
+        assertTrue(jdbcVoucherRepository.findById(voucher.getId()).isEmpty());
+    }
+    
+}
