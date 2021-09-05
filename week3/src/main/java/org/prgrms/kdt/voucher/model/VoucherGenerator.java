97cfb07ba076ffdef5dd620d1087ed4de05ac71e
minhee <queen.minhee@gmail.com>
2021-09-05 15:28:27 +0900

    [피드백5] Bean 등록 필요 없는 클래스 - 정적 호출하려 했는데 컨테이너에 올라갈 만한 객체가 아니므로 제외하는 것으로 변경해주기!!

--- week3/src/main/java/org/prgrms/kdt/voucher/model/VoucherGenerator.java
+++ week3/src/main/java/org/prgrms/kdt/voucher/model/VoucherGenerator.java
@@ -4,7 +4,6 @@ import org.springframework.stereotype.Component;
 
 import java.util.UUID;
 
-@Component
 public class VoucherGenerator {
 
     public static Voucher createVoucher(UUID voucherId, long amount, String voucherType){
