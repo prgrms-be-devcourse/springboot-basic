9aaa8fe09ea5e49cc4bc87cc3c704960daa42c7a
Kwonkyu <park2348190@naver.com>
2021-09-06 17:16:46 +0900

    Implemented customer create page. - 회원을 생성할 수 있는 페이지(뷰) 및 컨트롤러 구현.

--- voucher/src/main/java/com/programmers/voucher/controller/CustomerController.java
+++ voucher/src/main/java/com/programmers/voucher/controller/CustomerController.java
@@ -1,13 +1,16 @@
 package com.programmers.voucher.controller;
 
+import com.programmers.voucher.entity.customer.Customer;
 import com.programmers.voucher.service.customer.CustomerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/customer")
@@ -19,6 +22,26 @@ public class CustomerController {
         this.basicCustomerService = basicCustomerService;
     }
 
+    @GetMapping("/create")
+    public String requestCreateCustomer() {
+        return "customer/create.html";
+    }
+
+    @PostMapping("/create")
+    public String submitCreateCustomer(@RequestParam(name="username", defaultValue = "") String username,
+                                       @RequestParam(name="alias", defaultValue = "") String alias,
+                                       Model model) {
+        if(username.isBlank() || alias.isBlank()) {
+            model.addAttribute("username", username);
+            model.addAttribute("alias", alias);
+            model.addAttribute("error", "Required field should not be empty.");
+            return "customer/create.html";
+        }
+
+        Customer customer = basicCustomerService.create(username, alias);
+        return "redirect:/customer/read?id=" + customer.getId();
+    }
+
     @GetMapping("/read")
     public String getCustomer(@RequestParam(name = "id", required = false) Long id,
                               Model model) {
@@ -27,12 +50,15 @@ public class CustomerController {
         if (id == null || id < 1) return "customer/read.html";
 
         basicCustomerService.findById(id).ifPresentOrElse(
-                customer -> {
-                    model.addAttribute("customer", customer);
-                },
+                customer -> model.addAttribute("customer", customer),
                 () -> model.addAttribute("error", "No customer found."));
 
         return "customer/read.html";
     }
 
+    @GetMapping("/list")
+    public String getCustomers(Model model) {
+        model.addAttribute("customers", basicCustomerService.listAll());
+
+    }
 }
