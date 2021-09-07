01f56170743b6233f3bf41637b9e7db20de599e2
Younghyun Kim <kyh970107@ajou.ac.kr>
2021-09-07 20:56:08 +0900

    refactor : SpringBoot를 통해서 실행 될 수 있도록 변경

--- src/main/java/org/prgrms/kdtspringorder/KdtSpringOrderApplication.java
+++ src/main/java/org/prgrms/kdtspringorder/KdtSpringOrderApplication.java
@@ -2,12 +2,15 @@ package org.prgrms.kdtspringorder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class KdtSpringOrderApplication {
 
-  public static void main(String[] args) {
-    SpringApplication.run(KdtSpringOrderApplication.class, args);
-  }
+    public static void main(String[] args) {
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(KdtSpringOrderApplication.class, args);
+        App app = applicationContext.getBean(App.class);
+        app.run();
+    }
 
 }
