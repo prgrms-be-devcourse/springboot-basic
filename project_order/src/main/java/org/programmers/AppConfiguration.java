e148c10618436d45743bbe8fc24e8b796a80d433
yekyeong <kwonliz6.6@gmail.com>
2021-09-07 18:38:07 +0900

    feat : datasource 설정 추가

--- project_order/src/main/java/org/programmers/AppConfiguration.java
+++ project_order/src/main/java/org/programmers/AppConfiguration.java
@@ -1,11 +1,34 @@
 package org.programmers;
 
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.*;
+
+import javax.sql.DataSource;
 
 @Configuration
 @ComponentScan
-@PropertySource(value = "application.yaml", factory = YamlPropertiesFactory.class )
+@PropertySource(value = "application.yaml", factory = YamlPropertiesFactory.class)
 public class AppConfiguration {
+
+    @Value("${spring.datasource.url}")
+    private String url;
+
+    @Value("${spring.datasource.username}")
+    private String username;
+
+    @Value("${spring.datasource.password}")
+    private String password;
+
+    @Bean
+    public DataSource dataSource() {
+        return DataSourceBuilder.create()
+                .url(url)
+                .username(username)
+                .password(password)
+                .type(HikariDataSource.class)
+                .build();
+    }
+
 }
