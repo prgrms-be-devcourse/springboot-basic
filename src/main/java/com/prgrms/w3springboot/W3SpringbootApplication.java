5552f8eb62968e5f9c827c183ca3850f9761bcde
samkimuel <samkim2626@gmail.com>
2021-09-07 21:46:50 +0900

    chore: SpringBoot 애플리케이션으로 변경, profile 수정

--- src/main/java/com/prgrms/w3springboot/W3SpringbootApplication.java
+++ src/main/java/com/prgrms/w3springboot/W3SpringbootApplication.java
@@ -1,5 +1,6 @@
 package com.prgrms.w3springboot;
 
+import com.prgrms.w3springboot.io.CommandLine;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,8 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class W3SpringbootApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(W3SpringbootApplication.class, args);
-        System.out.println("hello!");
+        var applicationContext = SpringApplication.run(W3SpringbootApplication.class, args);
+        applicationContext.getBean(CommandLine.class).run();
     }
 
 }
