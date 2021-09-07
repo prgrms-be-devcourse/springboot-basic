aaca47ad23a1577a47fb2d5940da1e90452f13fa
Sangmin Lee <sangminlee7648@gmail.com>
2021-09-07 15:31:55 +0900

    Fix: 삭제 인터페이스에 작성된 @Repository

--- src/main/java/org/prgrms/kdt/engine/customer/repository/CustomerRepository.java
+++ src/main/java/org/prgrms/kdt/engine/customer/repository/CustomerRepository.java
@@ -1,13 +1,11 @@
 package org.prgrms.kdt.engine.customer.repository;
 
 import org.prgrms.kdt.engine.customer.domain.Customer;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-@Repository
 public interface CustomerRepository {
     int count();
     Customer insert(Customer customer);
