99056214254b22c68d6b84a018f5f6fb3964198c
maenguin <maenguin924@gmail.com>
2021-09-09 17:29:26 +0900

    feat : BlackCustomers에 Iterable 구현 및 메서드 선언 위치 조정

--- src/main/java/com/prgrms/devkdtorder/customer/domain/BlackCustomers.java
+++ src/main/java/com/prgrms/devkdtorder/customer/domain/BlackCustomers.java
@@ -1,18 +1,43 @@
 package com.prgrms.devkdtorder.customer.domain;
 
-import org.springframework.util.Assert;
-
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Objects;
 import java.util.function.Consumer;
 
-public class BlackCustomers {
+public class BlackCustomers implements Iterable<Customer> {
 
     private final List<Customer> customers;
+
+    public static BlackCustomers valueOf(List<Customer> customers) {
+        return new BlackCustomers(customers);
+    }
+
+    public static BlackCustomers empty() {
+        return new BlackCustomers(new ArrayList<>());
+    }
 
     private BlackCustomers(List<Customer> customers) {
         Objects.requireNonNull(customers);
         validateBlack(customers);
         this.customers = customers;
+    }
+
+    public int size() {
+        return customers.size();
+    }
+
+    @Override
+    public void forEach(Consumer<? super Customer> action) {
+        for (Customer customer : customers) {
+            action.accept(customer);
+        }
+    }
+
+    @Override
+    public Iterator<Customer> iterator() {
+        return customers.iterator();
     }
 
     private void validateBlack(List<Customer> customers) {
@@ -21,23 +46,4 @@ public class BlackCustomers {
             throw new IllegalArgumentException("고객 리스트에 블랙되지 않은 고객이 존재합니다.");
         }
     }
-
-    public void forEach(Consumer<Customer> action) {
-        for (Customer customer : customers) {
-            action.accept(customer);
-        }
-    }
-
-    public int size() {
-        return customers.size();
-    }
-
-    public static BlackCustomers empty(){
-        return new BlackCustomers(new ArrayList<>());
-    }
-
-    public static BlackCustomers valueOf(List<Customer> customers){
-        return new BlackCustomers(customers);
-    }
-
 }
