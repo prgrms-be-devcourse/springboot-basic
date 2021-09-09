b5ebdab1ff6e89b3faacd19ab13a5f252de91efb
devrunner21 <devrunner21@gmail.com>
2021-09-09 22:48:35 +0900

    [FEAT] 바우처 지갑 기능을 위해 추가된 메소드의 단위테스트를 작성하라.

--- src/test/java/org/prgrms/kdt/kdtspringorder/voucher/repository/VoucherRepositoryTest.java
+++ src/test/java/org/prgrms/kdt/kdtspringorder/voucher/repository/VoucherRepositoryTest.java
@@ -4,6 +4,7 @@ import com.wix.mysql.EmbeddedMysql;
 import com.wix.mysql.config.Charset;
 import com.wix.mysql.config.MysqldConfig;
 import org.junit.jupiter.api.*;
+import org.prgrms.kdt.kdtspringorder.common.exception.VoucherNotFoundException;
 import org.prgrms.kdt.kdtspringorder.voucher.domain.FixedAmountVoucher;
 import org.prgrms.kdt.kdtspringorder.voucher.domain.Voucher;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +16,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 import static org.hamcrest.Matchers.hasSize;
@@ -102,7 +104,7 @@ class VoucherRepositoryTest {
 
     @Nested
     @DisplayName("findById 메서드는")
-    class Describe_getVoucher {
+    class Describe_findById {
 
         private Voucher newVoucher;
         private UUID newVoucherId = UUID.randomUUID();
@@ -131,6 +133,36 @@ class VoucherRepositoryTest {
     }
 
     @Nested
+    @DisplayName("findByCustomerId 메서드는")
+    class Describe_findByCustomerId {
+
+        private Voucher newVoucher;
+        private UUID newVoucherId = UUID.randomUUID();
+        private UUID newCustomerId = UUID.randomUUID();
+        private long newVoucherDiscount = 100;
+
+        @BeforeEach
+        void setUp() {
+            newVoucher = new FixedAmountVoucher(newVoucherId, newCustomerId, newVoucherDiscount);
+            repository.insert(newVoucher);
+        }
+
+        @Nested
+        @DisplayName("CustomerId로 바우처를 조회한다면")
+        class Context_voucher_id{
+            @Test
+            @DisplayName("고객에게 할당된 바우처 목록을 반환합니다.")
+            void it_return_allocated_voucher_list() {
+                final List<Voucher> allocatedVoucherList = repository.findByCustomerId(newCustomerId);
+                assertThat(allocatedVoucherList, not(nullValue()));
+                assertThat(allocatedVoucherList, hasSize(1));
+            }
+
+        }
+
+    }
+
+    @Nested
     @DisplayName("insert 메서드는")
     class Describe_insert {
 
@@ -152,6 +184,181 @@ class VoucherRepositoryTest {
             void it_return_voucher_optional() {
                 final UUID createdVoucherId = repository.insert(newVoucher);
                 assertThat(createdVoucherId, equalTo(newVoucherId));
+            }
+
+        }
+
+    }
+
+    @Nested
+    @DisplayName("delete 메소드는")
+    class Describe_delete {
+
+        @Nested
+        @DisplayName("만약 바우처 목록에 없는 바우처를 삭제하는 경우")
+        class Context_invalid_voucher_id{
+
+            private UUID invalidVoucherId;
+
+            @BeforeEach
+            void setUp() {
+                invalidVoucherId = UUID.randomUUID();
+            }
+
+            @Test
+            @DisplayName("VoucherNotFound Exception을 던집니다.")
+            void it_return_throw_voucher_not_found_exception() {
+
+                assertThatThrownBy( () -> repository.delete(invalidVoucherId))
+                    .isInstanceOf(VoucherNotFoundException.class);
+
+            }
+
+        }
+
+        @Nested
+        @DisplayName("만약 바우처 목록에 있는 바우처를 삭제하는 경우")
+        class Context_valid_voucher_id{
+
+            private Voucher newVoucher;
+            private UUID newVoucherId = UUID.randomUUID();
+            private long newVoucherDiscount = 100;
+
+            @BeforeEach
+            void setUp() {
+                newVoucher = new FixedAmountVoucher(newVoucherId, newVoucherDiscount);
+                repository.insert(newVoucher);
+            }
+
+            @Test
+            @DisplayName("삭제한 컬럼 수를 반환합니다.")
+            void it_return_deleted_column_count() {
+
+                final int deletedColumnCount = repository.delete(newVoucherId);
+                assertThat(deletedColumnCount, equalTo(1));
+                final Optional<Voucher> foundVoucherOptional = repository.findById(newVoucherId);
+                assertThat(foundVoucherOptional.isPresent(), is(false));
+
+            }
+
+        }
+
+    }
+
+    @Nested
+    @DisplayName("updateDiscount 메소드는")
+    class Describe_updateDiscount {
+
+        @Nested
+        @DisplayName("만약 바우처 목록에 없는 바우처의 할인금액 or 할인률을 수정하는 경우")
+        class Context_invalid_id{
+
+            private UUID invalidVoucherId;
+            private long changeVoucherDiscount = 1000;
+
+            @BeforeEach
+            void setUp() {
+                invalidVoucherId = UUID.randomUUID();
+            }
+
+            @Test
+            @DisplayName("VoucherNotFound Exception을 던집니다.")
+            void it_return_throw_voucher_not_found_exception() {
+
+                assertThatThrownBy( () -> repository.updateDiscount(invalidVoucherId, changeVoucherDiscount))
+                    .isInstanceOf(VoucherNotFoundException.class);
+
+            }
+
+        }
+
+        @Nested
+        @DisplayName("만약 바우처 목록에 있는 바우처의 할인금액 or 할인률을 수정하는 경우")
+        class Context_valid_voucher_id{
+
+            private Voucher newVoucher;
+            private UUID newVoucherId = UUID.randomUUID();
+            private long newVoucherDiscount = 100;
+            private long changeVoucherDiscount = 1000;
+
+            @BeforeEach
+            void setUp() {
+                newVoucher = new FixedAmountVoucher(newVoucherId, newVoucherDiscount);
+                repository.insert(newVoucher);
+            }
+
+            @Test
+            @DisplayName("할인금액 or 할인률을 수정한 후, 수정한 바우처의 UUID를 반환합니다.")
+            void it_return_updated_voucher_id() {
+
+                final UUID updatedVoucherId = repository.updateDiscount(newVoucher.getVoucherId(), changeVoucherDiscount);
+                assertThat(updatedVoucherId, equalTo(newVoucher.getVoucherId()));
+
+                final Optional<Voucher> foundVoucherOptional = repository.findById(updatedVoucherId);
+                assertThat(foundVoucherOptional.isPresent(), is(true));
+
+                final Voucher updatedVoucher = foundVoucherOptional.get();
+                assertThat(updatedVoucher.getAmount(), equalTo(changeVoucherDiscount));
+
+            }
+
+        }
+
+    }
+
+    @Nested
+    @DisplayName("updateCustomerId 메소드는")
+    class Describe_updateCustomerId {
+
+        @Nested
+        @DisplayName("만약 바우처 목록에 없는 바우처의 고객ID를 수정하는 경우")
+        class Context_invalid_id{
+
+            private UUID invalidVoucherId;
+
+            @BeforeEach
+            void setUp() {
+                invalidVoucherId = UUID.randomUUID();
+            }
+
+            @Test
+            @DisplayName("VoucherNotFound Exception을 던집니다.")
+            void it_return_throw_voucher_not_found_exception() {
+
+                assertThatThrownBy( () -> repository.delete(invalidVoucherId))
+                    .isInstanceOf(VoucherNotFoundException.class);
+
+            }
+
+        }
+
+        @Nested
+        @DisplayName("만약 바우처 목록에 있는 바우처의 고객ID를 수정하는 경우")
+        class Context_valid_voucher_id{
+
+            private Voucher newVoucher;
+            private UUID newVoucherId = UUID.randomUUID();
+            private UUID newCustomerId = UUID.randomUUID();
+            private UUID changeCustomerId = UUID.randomUUID();
+            private long newVoucherDiscount = 100;
+
+            @BeforeEach
+            void setUp() {
+                newVoucher = new FixedAmountVoucher(newVoucherId, newCustomerId, newVoucherDiscount);
+                repository.insert(newVoucher);
+            }
+
+            @Test
+            @DisplayName("바우처가 할당된 고객 ID를 수정한 후, 수정한 바우처의 UUID를 반환합니다.")
+            void it_return_updated_voucher_id() {
+
+                final UUID updatedVoucherId = repository.updateCustomerId(newVoucher.getVoucherId(), changeCustomerId);
+                final Optional<Voucher> foundVoucherOptional = repository.findById(updatedVoucherId);
+                assertThat(foundVoucherOptional.isPresent(), is(true));
+
+                final Voucher updatedVoucher = foundVoucherOptional.get();
+                assertThat(updatedVoucher.getCustomerId(), equalTo(changeCustomerId));
+
             }
 
         }
