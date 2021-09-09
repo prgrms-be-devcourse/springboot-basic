92e58a2b49a927819427387feb8a85bf3ebbe954
김부희 <kimbuhui@naver.com>
2021-09-09 16:50:08 +0900

    refactor : yaml resource null 방지 수정

--- dev-spring-w3/src/main/java/org/prgrms/dev/config/YmlPropertiesFactory.java
+++ dev-spring-w3/src/main/java/org/prgrms/dev/config/YmlPropertiesFactory.java
@@ -5,19 +5,34 @@ import org.springframework.core.env.PropertiesPropertySource;
 import org.springframework.core.env.PropertySource;
 import org.springframework.core.io.support.EncodedResource;
 import org.springframework.core.io.support.PropertySourceFactory;
+import org.springframework.lang.Nullable;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 
 public class YmlPropertiesFactory implements PropertySourceFactory {
 
     @Override
-    public PropertySource<?> createPropertySource(String s, EncodedResource encodedResource) throws IOException {
-        YamlPropertiesFactoryBean ymlPropertiesFactoryBean = new YamlPropertiesFactoryBean();
-        ymlPropertiesFactoryBean.setResources(encodedResource.getResource());
-
-        Properties properties = ymlPropertiesFactoryBean.getObject();
-        return new PropertiesPropertySource(encodedResource.getResource().getFilename(), properties);
+    public PropertySource<?> createPropertySource(@Nullable String name, EncodedResource encodedResource) throws IOException {
+        Properties properties = loadYamlIntoProperties(encodedResource);
+        String sourceName = name != null ? name : encodedResource.getResource().getFilename();
+        return new PropertiesPropertySource(sourceName, properties);
     }
 
+    private Properties loadYamlIntoProperties(EncodedResource resource) throws FileNotFoundException {
+        try {
+            YamlPropertiesFactoryBean factory = new YamlPropertiesFactoryBean();
+            factory.setResources(resource.getResource());
+            factory.afterPropertiesSet();
+            return factory.getObject();
+        } catch (IllegalStateException e) {
+            // for ignoreResourceNotFound
+            Throwable cause = e.getCause();
+            if (cause instanceof FileNotFoundException)
+                throw (FileNotFoundException) e.getCause();
+            throw e;
+        }
+    }
 }
+
