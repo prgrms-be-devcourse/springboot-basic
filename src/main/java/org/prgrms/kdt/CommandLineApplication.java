fc7a399bf72677ab7a36ed1535fa750a90b76d31
suhyunsim <cnc1106@naver.com>
2021-09-11 05:44:47 +0900

    feat: CommandLineApplication에 전체 로직 추가

---
+++ src/main/java/org/prgrms/kdt/CommandLineApplication.java
@@ -0,0 +1,81 @@
+package org.prgrms.kdt;
+
+import lombok.extern.slf4j.Slf4j;
+import org.prgrms.kdt.common.AppConfiguration;
+import org.prgrms.kdt.domain.customer.Customer;
+import org.prgrms.kdt.domain.voucher.Voucher;
+import org.prgrms.kdt.io.ConsoleInput;
+import org.prgrms.kdt.io.ConsoleOutput;
+import org.prgrms.kdt.io.Input;
+import org.prgrms.kdt.io.Output;
+import org.prgrms.kdt.service.CustomerService;
+import org.prgrms.kdt.service.VoucherService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.List;
+
+import static org.prgrms.kdt.exception.Message.WRONG_COMMAND_MESSAGE;
+
+@Slf4j
+public class CommandLineApplication {
+
+    private static final Input inputView = new ConsoleInput();
+    private static final Output outputView = new ConsoleOutput();
+
+    public static void main(String[] args) {
+        log.info("[*]");
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfiguration.class);
+        var voucherService = applicationContext.getBean(VoucherService.class);
+        log.info("[*] voucher service: {}", voucherService);
+        var customerService = applicationContext.getBean(CustomerService.class);
+        while (true) {
+            inputView.inputHelp();
+            String command = inputView.inputCommand();
+            inputView.newLine();
+            if (command.equalsIgnoreCase("exit")) {
+                System.out.println("Bye");
+                break;
+            } else if (command.equalsIgnoreCase("create")) {
+                processCreateCommand(voucherService);
+            } else if (command.equalsIgnoreCase("list")) {
+                processListCommand(voucherService);
+            } else if (command.equalsIgnoreCase("allocate")) {
+                processAllocateCommand(voucherService);
+            } else if (command.equalsIgnoreCase("customers")) {
+                processCustomersCommand(customerService);
+            } else {
+                System.out.println(WRONG_COMMAND_MESSAGE);
+            }
+            inputView.newLine();
+        }
+        applicationContext.close();
+    }
+
+    private static void processCustomersCommand(CustomerService customerService) {
+        List<Customer> customers = customerService.list();
+        for (Customer customer : customers) {
+            outputView.printCustomers(customer);
+        }
+    }
+
+    private static void processAllocateCommand(VoucherService voucherService) {
+        Long voucherId = inputView.inputVoucherId();
+        Long customerId = inputView.inputCustomerId();
+        Voucher voucher = voucherService.allocate(voucherId, customerId);
+        outputView.printVoucher(voucher);
+    }
+
+    private static void processCreateCommand(VoucherService voucherService) {
+        String voucherType = inputView.inputVoucherType();
+        Voucher voucher = voucherService.create(voucherType);
+        outputView.printVoucher(voucher);
+    }
+
+    private static void processListCommand(VoucherService voucherService) {
+        List<Voucher> vouchers = voucherService.list();
+        for (Voucher voucher : vouchers) {
+            outputView.printVoucher(voucher);
+        }
+    }
+
+}
