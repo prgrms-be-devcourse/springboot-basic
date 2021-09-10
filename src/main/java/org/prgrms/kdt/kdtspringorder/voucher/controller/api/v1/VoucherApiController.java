8f9a864a2f3e8b870e77e84e260b939f0acc99ed
devrunner21 <devrunner21@gmail.com>
2021-09-10 17:13:58 +0900

    [FEAT] 바우처 목록 조회 API를 구현하라.

--- src/main/java/org/prgrms/kdt/kdtspringorder/voucher/controller/api/v1/VoucherApiController.java
+++ src/main/java/org/prgrms/kdt/kdtspringorder/voucher/controller/api/v1/VoucherApiController.java
@@ -1,10 +1,14 @@
 package org.prgrms.kdt.kdtspringorder.voucher.controller.api.v1;
 
 import lombok.RequiredArgsConstructor;
+import org.prgrms.kdt.kdtspringorder.common.dto.ApiResponse;
+import org.prgrms.kdt.kdtspringorder.voucher.dto.VoucherDto;
 import org.prgrms.kdt.kdtspringorder.voucher.repository.VoucherJdbcRepository;
 import org.prgrms.kdt.kdtspringorder.voucher.service.VoucherService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,5 +19,20 @@ public class VoucherApiController {
     private static final Logger logger = LoggerFactory.getLogger(VoucherJdbcRepository.class);
 
     private final VoucherService voucherService;
+
+    @GetMapping("/vouchers")
+    public ResponseEntity<ApiResponse> list() {
+
+        logger.debug("Access list()");
+
+        final ApiResponse apiResponse = ApiResponse.builder()
+            .payload(this.voucherService.getVouchers())
+            .status(HttpStatus.OK)
+            .success(true)
+            .build();
+
+        return ResponseEntity.ok(apiResponse);
+
+    }
 
 }
