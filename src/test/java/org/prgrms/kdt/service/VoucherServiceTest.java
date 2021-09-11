43c60e9e026c4f2bbee4c18946dac62227ca8912
suhyunsim <cnc1106@naver.com>
2021-09-11 17:14:45 +0900

    test: VoucherServiceTest에 id 타입 변경사항 반영 후 stub에 오버라이드할  메서드 추가

--- src/test/java/org/prgrms/kdt/service/VoucherServiceTest.java
+++ src/test/java/org/prgrms/kdt/service/VoucherServiceTest.java
@@ -1,14 +1,16 @@
 package org.prgrms.kdt.service;
 
+import org.apache.commons.math3.random.RandomDataGenerator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.prgrms.kdt.domain.customer.Customer;
 import org.prgrms.kdt.domain.voucher.FixedAmountVoucher;
 import org.prgrms.kdt.domain.voucher.Voucher;
 import org.prgrms.kdt.domain.voucher.VoucherRepository;
+import org.prgrms.kdt.domain.voucher.VoucherType;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.UUID;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
@@ -19,7 +21,7 @@ class VoucherServiceTest {
     @DisplayName("바우처 생성 (stub)")
     void createVoucherByStub() {
         var voucherRepository = new VoucherRepositoryStub();
-        var fixedAmountVoucher = new FixedAmountVoucher(UUID.randomUUID(), 100L);
+        var fixedAmountVoucher = new FixedAmountVoucher(new RandomDataGenerator().nextLong(0, 10000), VoucherType.FIXED_AMOUNT, 100L);
         voucherRepository.insert(fixedAmountVoucher);
 
         assertThat(fixedAmountVoucher.getAmount(), is(100L));
@@ -28,8 +30,13 @@ class VoucherServiceTest {
 
     class VoucherRepositoryStub implements VoucherRepository {
         @Override
-        public Optional<Voucher> findById(UUID voucherId) {
+        public Optional<Voucher> findById(Long voucherId) {
             return Optional.empty();
+        }
+
+        @Override
+        public List<Voucher> findByType(VoucherType type) {
+            return null;
         }
 
         @Override
@@ -39,6 +46,11 @@ class VoucherServiceTest {
 
         @Override
         public Voucher insert(Voucher voucher) {
+            return null;
+        }
+
+        @Override
+        public Voucher update(Voucher voucher, Customer customer) {
             return null;
         }
 
