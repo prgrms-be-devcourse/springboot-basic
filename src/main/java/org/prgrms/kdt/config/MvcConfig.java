4b8c0efee525af58d1d32d8c969dcf2f2c4351b1
suhyunsim <cnc1106@naver.com>
2021-09-12 19:23:49 +0900

    feat: MvcConfig 설정 관련 파일 추가

--- src/main/java/org/prgrms/kdt/config/MvcConfig.java
+++ src/main/java/org/prgrms/kdt/config/MvcConfig.java
@@ -1,2 +1,39 @@
-package org.prgrms.kdt.config;public class MvcConfig {
+package org.prgrms.kdt.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.xml.MarshallingHttpMessageConverter;
+import org.springframework.oxm.xstream.XStreamMarshaller;
+import org.springframework.web.servlet.config.annotation.*;
+
+import java.util.List;
+
+@Configuration
+@EnableWebMvc
+public class MvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        registry.jsp("/WEB-INF/view/", ".jsp");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("*");
+    }
+
+    @Override
+    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        var messageConverter = new MarshallingHttpMessageConverter();
+        var xStreamMarshaller = new XStreamMarshaller();
+        messageConverter.setMarshaller(xStreamMarshaller);
+        messageConverter.setUnmarshaller(xStreamMarshaller);
+        converters.add(0, messageConverter);
+    }
 }
