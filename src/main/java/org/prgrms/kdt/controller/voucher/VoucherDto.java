99f04b2d4d2b15df55cfac5be29fe4211fbcf0cb
suhyunsim <cnc1106@naver.com>
2021-09-12 19:52:38 +0900

    feat: Voucher API를 위해 request와 dto 구현

--- src/main/java/org/prgrms/kdt/controller/voucher/VoucherDto.java
+++ src/main/java/org/prgrms/kdt/controller/voucher/VoucherDto.java
@@ -1,2 +1,37 @@
-package org.prgrms.kdt.controller.voucher;public class VoucherDto {
+package org.prgrms.kdt.controller.voucher;
+
+import org.prgrms.kdt.domain.voucher.Voucher;
+import org.prgrms.kdt.domain.voucher.VoucherType;
+
+import static org.springframework.beans.BeanUtils.copyProperties;
+
+public class VoucherDto {
+    private Long voucherId;
+    private VoucherType voucherType;
+    private long amount;
+    //TODO: Customer vs customerId
+    private Long customerId;
+
+
+    public VoucherDto(Voucher source) {
+        copyProperties(source, this);
+        this.voucherType = source.getType();
+        this.customerId = source.getCustomerId();
+    }
+
+    public Long getVoucherId() {
+        return voucherId;
+    }
+
+    public long getAmount() {
+        return amount;
+    }
+
+    public Long getCustomerId() {
+        return customerId;
+    }
+
+    public VoucherType getVoucherType() {
+        return voucherType;
+    }
 }
--- src/main/java/org/prgrms/kdt/controller/voucher/VoucherRequest.java
+++ src/main/java/org/prgrms/kdt/controller/voucher/VoucherRequest.java
@@ -1,2 +1,25 @@
-package org.prgrms.kdt.controller.voucher;public class VoucherRequest {
+package org.prgrms.kdt.controller.voucher;
+
+public class VoucherRequest {
+
+    private Long voucherId;
+    private String voucherType;
+    private long amount;
+    private Long customerId;
+
+    public Long getVoucherId() {
+        return voucherId;
+    }
+
+    public String getVoucherType() {
+        return voucherType;
+    }
+
+    public long getAmount() {
+        return amount;
+    }
+
+    public Long getCustomerId() {
+        return customerId;
+    }
 }
