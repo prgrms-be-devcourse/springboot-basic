9c2760b729d04527a195d5444f4c61748951cf00
Young-BLUE <youngblue.coding@gmail.com>
2021-09-12 11:49:38 +0900

    [1기-C] 3주차 W3D2~W3D5 Test 수정
    [1기-C] 3주차 W3D2~W3D5 Test 수정

--- src/test/java/org/prgrms/kdt/KdtApplicationTests.java
+++ src/test/java/org/prgrms/kdt/KdtApplicationTests.java
@@ -1,13 +1,34 @@
 package org.prgrms.kdt;
 
+import org.hamcrest.core.IsInstanceOf;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.prgrms.kdt.assignments.CreateVoucher;
+import org.prgrms.kdt.assignments.VoucherData;
+import org.prgrms.kdt.voucher.FixedAmountVoucher;
+import org.prgrms.kdt.voucher.VoucherService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.Matchers.*;
 
 @SpringBootTest
+@ComponentScan( basePackages = {"org.kdt.prgrms.assignments", "org.kdt.prgrms.voucher", "org.kdt.prgrms.configuration"})
 class KdtApplicationTests {
 
+	@Autowired
+	CreateVoucher createVoucher;
+
+	@Autowired
+	VoucherService voucherService;
+
 	@Test
-	void contextLoads() {
+	@DisplayName("생성하고자 하는 바우처의 타입이 같아야한다")
+	void creaVoucherTest() {
+		var voucherData = new VoucherData(1,20);
+		var voucher = createVoucher.create(voucherData);
+		assertThat(voucher.getClass(), is(FixedAmountVoucher.class));
 	}
 
 }
