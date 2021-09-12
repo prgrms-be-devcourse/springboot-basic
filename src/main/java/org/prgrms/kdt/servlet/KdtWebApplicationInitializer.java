1912736e7b610411029b60ddb71dfaa673e23d51
suhyunsim <cnc1106@naver.com>
2021-09-12 19:25:22 +0900

    feat: Servlet 관련 파일 추가

--- src/main/java/org/prgrms/kdt/servlet/KdtWebApplicationInitializer.java
+++ src/main/java/org/prgrms/kdt/servlet/KdtWebApplicationInitializer.java
@@ -1,2 +1,55 @@
-package org.prgrms.kdt.servlet;public class KdtWebApplicationInitializer {
+package org.prgrms.kdt.servlet;
+
+import com.zaxxer.hikari.HikariDataSource;
+import lombok.extern.slf4j.Slf4j;
+import org.prgrms.kdt.controller.customer.CustomerRestController;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.*;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.sql.DataSource;
+
+@Slf4j
+public class KdtWebApplicationInitializer implements WebApplicationInitializer {
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        log.info("[*] Start");
+        var rootApplicationContext = new AnnotationConfigWebApplicationContext();
+        rootApplicationContext.register(RootConfig.class);
+        var loaderListener = new ContextLoaderListener(rootApplicationContext);
+        servletContext.addListener(loaderListener);
+
+        var applicationContext = new AnnotationConfigWebApplicationContext();
+        applicationContext.register(ServletContext.class);
+        var dispatcherServlet = new DispatcherServlet(applicationContext);
+        var servletRegistration = servletContext.addServlet("test", dispatcherServlet);
+        servletRegistration.addMapping("/");
+        servletRegistration.setLoadOnStartup(1);
+    }
+
+    @Configuration
+    @ComponentScan(basePackages = "org.prgrms.kdt",
+    excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = CustomerRestController.class)
+    )
+    @EnableTransactionManagement
+    static class RootConfig {
+        @Bean
+        public DataSource dataSource() {
+            var dataSource = DataSourceBuilder.create()
+                    .url("jdbc:mysql://localhost:3306/order_mgmt?characterEncoding=utf8&serverTimezone=UTC")
+                    .username("kdt")
+                    .password("kdt")
+                    .type(HikariDataSource.class)
+                    .build();
+            dataSource.setMaximumPoolSize(1000);
+            dataSource.setMinimumIdle(100);
+            return dataSource;
+        }
+    }
 }
--- src/main/java/org/prgrms/kdt/servlet/TestServlet.java
+++ src/main/java/org/prgrms/kdt/servlet/TestServlet.java
@@ -1,2 +1,28 @@
-package org.prgrms.kdt.servlet;public class TestServlet {
+package org.prgrms.kdt.servlet;
+
+import lombok.extern.slf4j.Slf4j;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+public class TestServlet extends HttpServlet {
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        log.info("Init Servlet");
+    }
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        var requestURI = request.getRequestURI();
+        log.info("Got Request from: {}", requestURI);
+
+        var writer = response.getWriter();
+        writer.println("Hello Servlet");
+    }
 }
