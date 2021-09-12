20372f5c95fcdcd3465658b8d50c318bba0f7c07
devrunner21 <devrunner21@gmail.com>
2021-09-12 16:53:40 +0900

    [FIX] DTO에서 final을 빼라.

--- src/main/java/org/prgrms/kdt/kdtspringorder/voucher/dto/VoucherDto.java
+++ src/main/java/org/prgrms/kdt/kdtspringorder/voucher/dto/VoucherDto.java
@@ -2,9 +2,7 @@ package org.prgrms.kdt.kdtspringorder.voucher.dto;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.Builder;
-import lombok.Data;
-import lombok.RequiredArgsConstructor;
+import lombok.*;
 import org.prgrms.kdt.kdtspringorder.common.enums.VoucherType;
 
 import javax.validation.constraints.Min;
@@ -16,28 +14,30 @@ public class VoucherDto {
 
     @Data
     @Builder
-    @RequiredArgsConstructor
+    @NoArgsConstructor
+    @AllArgsConstructor
     public static class Create {
 
         @NotNull(message = "바우처 타입은 필수 입력값 입니다.")
-        private final VoucherType voucherType;
+        private VoucherType voucherType;
 
         @Min(value = 0, message = "적어도 0원 이상을 입력해주세요.")
         @NotNull(message = "할인률 또는 할인가격은 필수 입력값 입니다.")
-        private final long amount;
+        private long amount;
 
     }
 
     @Data
     @Builder
-    @RequiredArgsConstructor
+    @NoArgsConstructor
+    @AllArgsConstructor
     public static class Allocate {
 
         @NotNull(message = "바우처 ID는 필수 입력값 입니다.")
-        private final UUID voucherId;
+        private UUID voucherId;
 
         @NotNull(message = "고객 ID는 필수 입력값 입니다.")
-        private final UUID customerId;
+        private UUID customerId;
 
     }
 
