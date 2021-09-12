e3c99303b6b23f829b8443630c6198127b4181fe
samkimuel <samkim2626@gmail.com>
2021-09-13 03:47:01 +0900

    chore: basePackage 수정

--- src/main/java/com/prgrms/w3springboot/configuration/AppConfig.java
+++ src/main/java/com/prgrms/w3springboot/configuration/AppConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @ComponentScan(
-        basePackages = {"com.prgrms.w3springboot.configuration", "com.prgrms.w3springboot.io", "com.prgrms.w3springboot.voucher", "com.prgrms.w3springboot.order", "com.prgrms.w3springboot.customer"}
+        basePackages = {"com.prgrms.w3springboot"}
 )
 @PropertySource(value = "application.yml", factory = YamlPropertiesFactory.class)
 @EnableConfigurationProperties
