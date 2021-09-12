b3b59272fc68fb12f4866bfc01a989fdaba02382
samkimuel <samkim2626@gmail.com>
2021-09-13 03:25:53 +0900

    style: 필드 네이밍 수정

--- src/main/java/com/prgrms/w3springboot/order/Order.java
+++ src/main/java/com/prgrms/w3springboot/order/Order.java
@@ -33,7 +33,7 @@ public class Order {
 
     public long totalAmount() {
         var beforeDiscount = orderItems.stream()
-                .map(voucher -> voucher.getProductPrice() * voucher.getQuantity())
+                .map(orderItem -> orderItem.getProductPrice() * orderItem.getQuantity())
                 .reduce(INITIAL_VALUE, Long::sum);
         return voucher.map(value -> value.discount(beforeDiscount)).orElse(beforeDiscount);
     }
