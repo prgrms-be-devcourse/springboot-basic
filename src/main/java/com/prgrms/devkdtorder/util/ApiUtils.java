f81b56fd5d54909f7d250d6b89a9cfa94e5c8242
maenguin <maenguin924@gmail.com>
2021-09-12 20:28:36 +0900

    feat : API Reponse 생성을 위한 ApiUitls 추가

---
+++ src/main/java/com/prgrms/devkdtorder/util/ApiUtils.java
@@ -0,0 +1,51 @@
+package com.prgrms.devkdtorder.util;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+public class ApiUtils {
+
+    public static <T> ApiResponse<T> success(T response) {
+        return new ApiResponse<>(true, response, null);
+    }
+
+    public static ApiResponse<?> error(Throwable throwable, HttpStatus status) {
+        return new ApiResponse<>(false, null, new ApiError(throwable, status));
+    }
+
+    public static ApiResponse<?> error(String message, HttpStatus status) {
+        return new ApiResponse<>(false, null, new ApiError(message, status));
+    }
+
+    @Getter
+    @RequiredArgsConstructor
+    public static class ApiResponse<T> {
+
+        private final boolean success;
+        private final T data;
+        private final ApiError error;
+
+    }
+
+    @Getter
+    public static class ApiError {
+
+        private final String message;
+        private final int status;
+
+        ApiError(Throwable throwable, HttpStatus status) {
+            this(throwable.getMessage(), status);
+        }
+
+        ApiError(String message, HttpStatus status) {
+            this.message = message;
+            this.status = status.value();
+        }
+    }
+}
+
+
+
+
+
