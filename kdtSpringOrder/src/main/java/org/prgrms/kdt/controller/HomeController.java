d65cb4f9877d8fe85c2b9881cd89c68cd4d1806d
0923kdh <0923kdh@gmail.com>
2021-09-13 23:18:17 +0900

    feat : home 화면 오류 수정

--- kdtSpringOrder/src/main/java/org/prgrms/kdt/controller/HomeController.java
+++ kdtSpringOrder/src/main/java/org/prgrms/kdt/controller/HomeController.java
@@ -1,11 +1,14 @@
 package org.prgrms.kdt.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 public class HomeController {
 
-    @RequestMapping("/")
+    @GetMapping("/")
     public String home() {
-        return "home";
+        return "views/home";
     }
 }
