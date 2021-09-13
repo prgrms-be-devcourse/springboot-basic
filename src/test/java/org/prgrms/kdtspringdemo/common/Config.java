9d7d8c55706b43a57f439287e1472b4e2d866c55
UJHa <ddkk94@naver.com>
2021-09-14 00:38:42 +0900

    fix:불필요한 file,memory Repository 제거

--- src/test/java/org/prgrms/kdtspringdemo/common/Config.java
+++ src/test/java/org/prgrms/kdtspringdemo/common/Config.java
@@ -2,11 +2,10 @@ package org.prgrms.kdtspringdemo.common;
 
 import com.zaxxer.hikari.HikariDataSource;
 import org.prgrms.kdtspringdemo.console.VoucherOperator;
-import org.prgrms.kdtspringdemo.voucher.repository.FileVoucherRepository;
+import org.prgrms.kdtspringdemo.voucher.repository.JdbcVoucherRepository;
 import org.prgrms.kdtspringdemo.voucher.service.VoucherService;
 import org.prgrms.kdtspringdemo.wallet.WalletService;
 import org.prgrms.kdtspringdemo.wallet.repository.JdbcWalletRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -22,19 +21,6 @@ import javax.sql.DataSource;
 })
 public class Config {
     @Bean
-    public FileVoucherRepository fileVoucherRepository() {
-        return new FileVoucherRepository();
-    }
-
-    @Autowired
-    FileVoucherRepository fileVoucherRepository;
-
-    @Bean
-    public VoucherService voucherService() {
-        return new VoucherService(fileVoucherRepository);
-    }
-
-    @Bean
     public DataSource dataSource() {
 //          docker에 올린 DB로 시작하기(mysql v8.0.11)
         var dataSource = DataSourceBuilder.create()
@@ -49,6 +35,16 @@ public class Config {
     @Bean
     NamedParameterJdbcTemplate namedParameterJdbcTemplate(DataSource dataSource) {
         return new NamedParameterJdbcTemplate(dataSource);
+    }
+
+    @Bean
+    public JdbcVoucherRepository jdbcVoucherRepository(NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
+        return new JdbcVoucherRepository(namedParameterJdbcTemplate);
+    }
+
+    @Bean
+    public VoucherService voucherService(JdbcVoucherRepository jdbcVoucherRepository) {
+        return new VoucherService(jdbcVoucherRepository);
     }
 
     @Bean
