76b0b76e489d7618caf1e4c0089250f74e5fb7d0
dlfdyd96 <1yongs_@naver.com>
2021-09-15 00:01:41 +0900

    ADD: w3d5 mssion - SpringBoot 애플리케이션으로 변경합니다. (SpringApplication 를 사용해주세요)

--- src/main/java/org/prgrms/kdt/CommandAppTest.java
+++ src/main/java/org/prgrms/kdt/CommandAppTest.java
@@ -1,13 +1,19 @@
 package org.prgrms.kdt;
 
 import org.prgrms.kdt.command.CommandLineApplication;
+import org.prgrms.kdt.customer.domain.Customer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
+import java.util.Optional;
+
+@SpringBootApplication
 public class CommandAppTest {
     public static void main(String[] args) {
-
-        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfiguration.class);
+        var applicationContext = SpringApplication.run(CommandAppTest.class, args);
 
         applicationContext.getBean(CommandLineApplication.class).run();
     }
