8fb4db5c9fa6645c44c7cabca4677f15de203185
samkimuel <samkim2626@gmail.com>
2021-09-14 17:04:36 +0900

    test: Order 관련 테스트 메서드 수정

--- src/test/java/com/prgrms/w3springboot/order/OrderTest.java
+++ src/test/java/com/prgrms/w3springboot/order/OrderTest.java
@@ -2,8 +2,6 @@ package com.prgrms.w3springboot.order;
 
 import com.prgrms.w3springboot.configuration.AppConfig;
 import com.prgrms.w3springboot.order.service.OrderService;
-import com.prgrms.w3springboot.voucher.FixedAmountVoucher;
-import com.prgrms.w3springboot.voucher.PercentAmountVoucher;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -15,34 +13,8 @@ import java.util.UUID;
 
 class OrderTest {
 
+    @DisplayName("의존성 주입으로 주문 생성 테스트")
     @Test
-    @DisplayName("고정 할인 바우처 생성 테스트")
-    void testCreateFixedVoucher() {
-        var customerId = UUID.randomUUID();
-        var orderItems = List.of(
-                new OrderItem(UUID.randomUUID(), 100L, 1)
-        );
-        var fixedAmountVoucher = new FixedAmountVoucher(UUID.randomUUID(), 10L);
-        var order = new Order(UUID.randomUUID(), customerId, orderItems, fixedAmountVoucher);
-
-        Assert.isTrue(order.totalAmount() == 90L, MessageFormat.format("totalAmount is not {0}", order.totalAmount()));
-    }
-
-    @Test
-    @DisplayName("퍼센트 할인 바우처 생성 테스트")
-    void testCreatePercentVoucher() {
-        var customerId = UUID.randomUUID();
-        var orderItems = List.of(
-                new OrderItem(UUID.randomUUID(), 100L, 1)
-        );
-        var percentAmountVoucher = new PercentAmountVoucher(UUID.randomUUID(), 200L);
-        var order = new Order(UUID.randomUUID(), customerId, orderItems, percentAmountVoucher);
-
-        Assert.isTrue(order.totalAmount() == 200L, MessageFormat.format("totalAmount is not {0}", order.totalAmount()));
-    }
-
-    @Test
-    @DisplayName("의존성 빈으로 주문 생성 테스트")
     void testCreateOrderByDI() {
         var applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
         var orderService = applicationContext.getBean(OrderService.class);
