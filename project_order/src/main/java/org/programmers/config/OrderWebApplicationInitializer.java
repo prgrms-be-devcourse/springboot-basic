2b1e411348e2af7d6d1b308e902642817af24072
yekyeong <kwonliz6.6@gmail.com>
2021-09-15 02:30:57 +0900

    feat : WebApplicationInitializer 구현 클래스 생성

---
+++ project_order/src/main/java/org/programmers/config/OrderWebApplicationInitializer.java
@@ -0,0 +1,113 @@
+package org.programmers.config;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeansException;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.resource.EncodedResourceResolver;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+
+import javax.servlet.ServletContext;
+import javax.sql.DataSource;
+
+public class OrderWebApplicationInitializer implements WebApplicationInitializer {
+
+    private static final Logger logger = LoggerFactory.getLogger(OrderWebApplicationInitializer.class);
+
+    @Configuration
+    @EnableWebMvc
+    @ComponentScan(basePackages = "org.programmers.voucher")
+    @EnableTransactionManagement
+    static class AppConfig implements WebMvcConfigurer, ApplicationContextAware {
+
+        ApplicationContext applicationContext;
+
+        @Override
+        public void configureViewResolvers(ViewResolverRegistry registry) {
+            var springResourceTemplateResolver = new SpringResourceTemplateResolver();
+            springResourceTemplateResolver.setApplicationContext(applicationContext);
+            springResourceTemplateResolver.setPrefix("/WEB-INF/");
+            springResourceTemplateResolver.setSuffix(".html");
+            var springTemplateEngine = new SpringTemplateEngine();
+            springTemplateEngine.setTemplateResolver(springResourceTemplateResolver);
+
+            var thymeleafViewResolver = new ThymeleafViewResolver();
+            thymeleafViewResolver.setTemplateEngine(springTemplateEngine);
+            thymeleafViewResolver.setViewNames(new String[]{"views/*"});
+            registry.viewResolver(thymeleafViewResolver);
+        }
+
+        @Override
+        public void addResourceHandlers(ResourceHandlerRegistry registry) {
+            registry.addResourceHandler("/resources/**")
+                    .addResourceLocations("/resources/")
+                    .setCachePeriod(60)
+                    .resourceChain(true)
+                    .addResolver(new EncodedResourceResolver());
+        }
+
+        @Bean
+        public DataSource dataSource() {
+            var dataSource = DataSourceBuilder.create()
+                    .url("jdbc:mysql://localhost/order_mgmt")
+                    .username("root")
+                    .password("root1234!")
+                    .type(HikariDataSource.class)
+                    .build();
+            dataSource.setMaximumPoolSize(1000);
+            dataSource.setMinimumIdle(100);
+            return dataSource;
+        }
+
+        @Bean
+        public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+            return new JdbcTemplate(dataSource);
+        }
+
+        @Bean
+        public NamedParameterJdbcTemplate namedParameterJdbcTemplate(JdbcTemplate jdbcTemplate) {
+            return new NamedParameterJdbcTemplate(jdbcTemplate);
+        }
+
+        @Bean
+        public PlatformTransactionManager platformTransactionManager(DataSource dataSource) {
+            return new DataSourceTransactionManager(dataSource);
+        }
+
+        @Override
+        public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+            this.applicationContext = applicationContext;
+        }
+
+    }
+
+    @Override
+    public void onStartup(ServletContext servletContext) {
+        logger.info("Staring Server...");
+        var applicationContext = new AnnotationConfigWebApplicationContext();
+        applicationContext.register(AppConfig.class);
+
+        var dispatcherServlet = new DispatcherServlet(applicationContext);
+        var servletRegistration = servletContext.addServlet("test", dispatcherServlet);
+        servletRegistration.addMapping("/");
+        servletRegistration.setLoadOnStartup(1);
+    }
+
+}
