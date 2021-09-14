cc0376e80948b3c6ea3790cb226a6c7912b348ef
Sangmin Lee <sangminlee7648@gmail.com>
2021-09-14 15:16:09 +0900

    Feat: 추가 바우처 API 전체 조회 기능

---
+++ src/main/java/org/prgrms/kdt/engine/voucher/controller/VoucherRestController.java
@@ -0,0 +1,25 @@
+package org.prgrms.kdt.engine.voucher.controller;
+
+import org.prgrms.kdt.engine.voucher.domain.Voucher;
+import org.prgrms.kdt.engine.voucher.service.VoucherService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+import java.util.UUID;
+
+@RestController
+public class VoucherRestController {
+    private final VoucherService voucherService;
+
+    public VoucherRestController(VoucherService voucherService) {
+        this.voucherService = voucherService;
+    }
+
+    @GetMapping("/api/v1/vouchers")
+    public Map<UUID, Voucher> listVouchers() {
+        var allVouchers = voucherService.listVoucher();
+        if (allVouchers.isEmpty())
+            return Map.of();
+        return allVouchers.get();
+    }
+}
