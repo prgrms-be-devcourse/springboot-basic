ab05a46260739062717474f1353aecfdbb91fc17
minhee <queen.minhee@gmail.com>
2021-09-15 02:26:46 +0900

    [누락되었던 파일 업로드]

---
+++ week3/src/main/java/org/prgrms/kdt/voucher/service/VoucherJdbcService.java
@@ -0,0 +1,54 @@
+package org.prgrms.kdt.voucher.service;
+
+import org.prgrms.kdt.voucher.model.FixedAmountVoucher;
+import org.prgrms.kdt.voucher.model.PercentDiscountVoucher;
+import org.prgrms.kdt.voucher.model.Voucher;
+import org.prgrms.kdt.voucher.model.VoucherType;
+import org.prgrms.kdt.voucher.repository.VoucherRepository;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+@Primary
+public class VoucherJdbcService implements VoucherService{
+
+    private final VoucherRepository voucherRepository;
+
+    public VoucherJdbcService(VoucherRepository voucherRepository) {
+        this.voucherRepository = voucherRepository;
+    }
+
+    @Override
+    public void create(Voucher voucher) {
+        voucherRepository.insert(voucher);
+    }
+
+    @Override
+    public List<Voucher> list() {
+        return voucherRepository.findAll();
+    }
+
+    @Override
+    public void create(String voucherTypeStr, Long amount) {
+        var voucherType = VoucherType.valueOf(voucherTypeStr);
+        switch (voucherType){
+            case PERCENT_DISCOUNT_VOUCHER -> voucherRepository.insert(new PercentDiscountVoucher(UUID.randomUUID(), amount, voucherType, LocalDateTime.now()));
+            case FIXED_AMOUNT_VOUCHER -> voucherRepository.insert(new FixedAmountVoucher(UUID.randomUUID(), amount, voucherType, LocalDateTime.now()));
+        };
+    }
+
+    @Override
+    public Optional<Voucher> getVoucher(UUID voucherId) {
+        return voucherRepository.findById(voucherId);
+    }
+
+    @Override
+    public void deleteById(UUID voucherId) {
+        voucherRepository.deleteByVoucherId(voucherId);
+    }
+}
