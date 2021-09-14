65cb0b031de28c9d3183b2ff350a3a571e4ae70f
BeautterLife <pgh9801@gmail.com>
2021-09-15 00:40:06 +0900

    fix/삭제 메서드 추가, 바우처 종류별 반환 메서드로 변경

--- src/main/java/org/prgrms/kdt/voucher/VoucherRepository.java
+++ src/main/java/org/prgrms/kdt/voucher/VoucherRepository.java
@@ -1,10 +1,13 @@
 package org.prgrms.kdt.voucher;
 
+import org.prgrms.kdt.VoucherType;
+
 import java.util.*;
 
 public interface VoucherRepository {
     Optional<Voucher> findById(UUID voucherId);
     Voucher insert(Voucher voucher);
-    int numVouchers();
-    List<?> getList();
+    int count();
+    List<Voucher> getListByType(VoucherType voucherType);
+    void deleteAll();
 }
