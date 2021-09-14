653837bd69b6874879cb786a8741136981d27aab
BeautterLife <pgh9801@gmail.com>
2021-09-15 00:35:39 +0900

    feat/VoucherTester 입력 가이드라인 분리

---
+++ src/main/java/org/prgrms/kdt/util/VoucherTesterGuideComment.java
@@ -0,0 +1,15 @@
+package org.prgrms.kdt.util;
+
+public class VoucherTesterGuideComment {
+    public static final String commandGuide = "사용 가능한 명령은 4가지 입니다. 한번에 하나의 명령만 가능합니다. 대소문자는 구분하지 않습니다.\n바우처 생성하기 : create\nvoucher 리스트 보기 : list\nblackcustomer 리스트 보기 : black\n종료하기 : exit 를 입력하세요.";
+    public static final String commandInputErrorMsg = "명령어 입력이 잘못되었습니다. 다시 입력해 주세요.";
+    public static final String voucherGuide = "fixed형 voucher를 생성하려면 f를, percent형 voucher를 생성하려면 p를 입력하세요.";
+    public static final String voucherInfoGuide = "fixed amount 또는 percent discount를 입력하세요.";
+    public static final String voucherCreateSuccess = "바우처 생성 성공";
+    public static final String voucherCreateFail = "바우처 생성 실패";
+    public static final String voucherMatcherGuide = "전체 바우처 목록 조회 : all\nfixedAmountVoucher 목록 조회 : fixed\npercentDiscountVoucher 목록 조회 : percent\n이전 메뉴 : exit";
+    public static final String inputErrorMsg = "입력 형식이 잘못되었습니다. 다시 입력해 주세요.";
+    public static final String voucherNotMatchErrorMsg = "바우처 타입 입력이 잘못되었습니다. 다시 입력해 주세요.";
+    public static final String exitMsg = "바우처 관리 프로그램을 종료합니다.";
+    public static final String endLine = "======================================================================";
+}
