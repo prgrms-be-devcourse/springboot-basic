b4640e75d2b28548570f87b36ac4787984f74564
BeautterLife <pgh9801@gmail.com>
2021-09-15 22:07:56 +0900

    fix/고객 model 관리 메소드 추가

--- src/main/java/org/prgrms/kdt/customer/CustomerRepository.java
+++ src/main/java/org/prgrms/kdt/customer/CustomerRepository.java
@@ -1,7 +1,23 @@
 package org.prgrms.kdt.customer;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 public interface CustomerRepository {
-    List<String> getList();
+    Customer insert(Customer customer);
+
+    Customer update(Customer customer);
+
+    int count();
+
+    List<Customer> getList();
+
+    Optional<Customer> findById(UUID customerId);
+
+    Optional<Customer> findByName(String name);
+
+    Optional<Customer> findByEmail(String email);
+
+    void deleteAll();
 }
