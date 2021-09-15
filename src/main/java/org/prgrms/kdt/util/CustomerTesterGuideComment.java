d609797b2900de759da9da19eabf0bbd1b46c3e7
BeautterLife <pgh9801@gmail.com>
2021-09-15 21:59:03 +0900

    fix/고객관리 프로그램 입력 가이드라인 분리

---
+++ src/main/java/org/prgrms/kdt/util/CustomerTesterGuideComment.java
@@ -0,0 +1,8 @@
+package org.prgrms.kdt.util;
+
+public class CustomerTesterGuideComment {
+    public static final String exitMsg = "고객 관리 프로그램을 종료합니다.";
+    public static final String endLine = "======================================================================";
+    public static final String inputErrorMsg = "입력 형식이 잘못되었습니다. 다시 입력해 주세요.";
+    public static final String commandGuide = "사용 가능한 명령은 3가지 입니다. 한번에 하나의 명령만 가능합니다. 대소문자는 구분하지 않습니다.\n고객 정보 생성하기 : create\n고객 리스트 보기 : list\n종료하기 : exit 를 입력하세요.";
+}
