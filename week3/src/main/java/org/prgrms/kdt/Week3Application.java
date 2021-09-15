fec8d86247f25ea55017b4bcbd4704ab97e6e14c
minhee <queen.minhee@gmail.com>
2021-09-15 12:19:17 +0900

    CLI -> Web 으로 사용 변경

--- week3/src/main/java/org/prgrms/kdt/Week3Application.java
+++ week3/src/main/java/org/prgrms/kdt/Week3Application.java
@@ -1,9 +1,5 @@
 package org.prgrms.kdt;
 
-import org.prgrms.kdt.io.CommandLineInterface;
-import org.prgrms.kdt.voucher.VoucherProgram;
-import org.prgrms.kdt.blacklist.BlackListrService;
-import org.prgrms.kdt.voucher.service.MemoryVoucherService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -19,14 +15,6 @@ import java.io.IOException;
 public class Week3Application {
 
     public static void main(String[] args) throws IOException {
-        var applicationContext = SpringApplication.run(Week3Application.class, args);
-
-        new VoucherProgram(
-                applicationContext.getBean(BlackListrService.class),
-                applicationContext.getBean(MemoryVoucherService.class),
-                applicationContext.getBean(CommandLineInterface.class)
-        ).runProgram();
-
-        applicationContext.close();
+       SpringApplication.run(Week3Application.class, args);
     }
 }
