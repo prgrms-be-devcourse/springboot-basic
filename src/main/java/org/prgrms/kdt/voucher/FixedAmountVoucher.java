37b30969b506f5769203e8a24a9cba6e4bc5aa87
BeautterLife <pgh9801@gmail.com>
2021-09-15 21:34:28 +0900

    fix/바우처 model 필드 추가

--- src/main/java/org/prgrms/kdt/voucher/FixedAmountVoucher.java
+++ src/main/java/org/prgrms/kdt/voucher/FixedAmountVoucher.java
@@ -1,29 +1,43 @@
 package org.prgrms.kdt.voucher;
 
+import org.prgrms.kdt.VoucherType;
+
 import java.io.Serializable;
+import java.time.LocalDateTime;
 import java.util.UUID;
 
-public class FixedAmountVoucher implements Voucher, Serializable {
-    private final long amount;
+public class FixedAmountVoucher  implements Voucher,Serializable {
+    private final long discountAmount;
     private final UUID voucherId;
-
-    public FixedAmountVoucher(UUID voucherId, long amount){
-        this.amount = amount;
+    private final LocalDateTime createdAt;
+    public FixedAmountVoucher(UUID voucherId, long amount, LocalDateTime createdAt){
+        this.discountAmount = amount;
         this.voucherId = voucherId;
+        this.createdAt = createdAt;
     }
 
     @Override
     public long discount(Long beforeDiscount) {
-        return beforeDiscount-amount;
+        return beforeDiscount- discountAmount;
     }
 
     @Override
-    public UUID getID() {
+    public UUID getId() {
         return voucherId;
     }
 
     @Override
     public String toString(){
-        return amount+" fixed amount voucher";
+        return discountAmount +" fixed amount voucher";
+    }
+
+    @Override
+    public VoucherType getType() {
+        return VoucherType.FIXED;
+    }
+
+    @Override
+    public long getDiscount() {
+        return discountAmount;
     }
 }
--- src/main/java/org/prgrms/kdt/voucher/PercentDiscountVoucher.java
+++ src/main/java/org/prgrms/kdt/voucher/PercentDiscountVoucher.java
@@ -1,29 +1,46 @@
 package org.prgrms.kdt.voucher;
 
+import org.prgrms.kdt.VoucherType;
+
 import java.io.Serializable;
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 public class PercentDiscountVoucher implements Voucher, Serializable {
-    private final long percent;
+    private final long percentDiscount;
     private final UUID voucherId;
+    private final LocalDateTime createdAt;
 
-    public PercentDiscountVoucher(UUID voucherId, long percent){
-        this.percent = percent;
+    public PercentDiscountVoucher(UUID voucherId, long percent, LocalDateTime createdAt){
+        this.percentDiscount = percent;
         this.voucherId = voucherId;
+        this.createdAt = createdAt;
     }
 
     @Override
     public long discount(Long beforeDiscount) {
-        return beforeDiscount*(percent/100);
+        return beforeDiscount*(percentDiscount /100);
     }
 
     @Override
-    public UUID getID() {
+    public UUID getId() {
         return voucherId;
     }
 
     @Override
     public String toString(){
-        return percent + " percent discount voucher";
+        return percentDiscount + " percent discount voucher";
     }
+
+    @Override
+    public VoucherType getType() {
+        return VoucherType.PERCENT;
+    }
+
+    @Override
+    public long getDiscount() {
+        return percentDiscount;
+    }
+
+
 }
