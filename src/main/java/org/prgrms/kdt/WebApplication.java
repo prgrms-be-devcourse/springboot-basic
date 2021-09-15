97a9130d459ee27416ca3d73c5617cf36fed5584
Sangmin Lee <sangminlee7648@gmail.com>
2021-09-15 23:50:02 +0900

    Feat: 추가 바우처 관리자 조회 페이지

--- src/main/java/org/prgrms/kdt/WebApplication.java
+++ src/main/java/org/prgrms/kdt/WebApplication.java
@@ -1,14 +1,11 @@
-package org.prgrms.kdt.application;
+package org.prgrms.kdt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FilterType;
 
 @SpringBootApplication
-@ComponentScan(
-    basePackages = {"org.prgrms.kdt.engine"}
-)
+@ComponentScan(basePackages = {"org.prgrms.kdt.engine"})
 public class WebApplication {
     public static void main(String[] args) {
         SpringApplication.run(WebApplication.class, args);
---
+++ src/main/java/org/prgrms/kdt/engine/voucher/controller/VoucherThymeleafController.java
@@ -0,0 +1,27 @@
+package org.prgrms.kdt.engine.voucher.controller;
+
+import org.prgrms.kdt.engine.voucher.domain.Voucher;
+import org.prgrms.kdt.engine.voucher.service.VoucherService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Map;
+import java.util.Optional;
+import java.util.UUID;
+
+@Controller
+public class VoucherThymeleafController {
+    private final VoucherService voucherService;
+
+    public VoucherThymeleafController(VoucherService voucherService) {
+        this.voucherService = voucherService;
+    }
+
+    @GetMapping("/vouchers")
+    public String vouchersPage(Model model) {
+        Optional<Map<UUID, Voucher>> vouchers = voucherService.listVoucher();
+        model.addAttribute("vouchers", vouchers.get());
+        return "voucher-list";
+    }
+}
