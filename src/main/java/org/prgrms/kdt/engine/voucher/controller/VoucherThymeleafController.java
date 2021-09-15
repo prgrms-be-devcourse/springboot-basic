7c152bac126ba3498dd9929778670930d26141af
Sangmin Lee <sangminlee7648@gmail.com>
2021-09-16 00:35:14 +0900

    Feat: 추가 바우처 관리자 바우처 삭제

--- src/main/java/org/prgrms/kdt/engine/voucher/controller/VoucherThymeleafController.java
+++ src/main/java/org/prgrms/kdt/engine/voucher/controller/VoucherThymeleafController.java
@@ -4,7 +4,10 @@ import org.prgrms.kdt.engine.voucher.domain.Voucher;
 import org.prgrms.kdt.engine.voucher.service.VoucherService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.Map;
 import java.util.Optional;
@@ -23,5 +26,11 @@ public class VoucherThymeleafController {
         Optional<Map<UUID, Voucher>> vouchers = voucherService.listVoucher();
         model.addAttribute("vouchers", vouchers.get());
         return "voucher-list";
+    }
+
+    @DeleteMapping("/vouchers/{voucherId}")
+    public String deleteVoucher(@PathVariable("voucherId") UUID voucherId) {
+        voucherService.deleteVoucher(voucherId);
+        return "redirect:/vouchers";
     }
 }
