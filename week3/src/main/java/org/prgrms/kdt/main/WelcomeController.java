357b96339d34d2ce6863a5a135a0035eeec91829
minhee <queen.minhee@gmail.com>
2021-09-16 19:18:27 +0900

    처음 프로젝트 실행시 진입 컨트롤러

---
+++ week3/src/main/java/org/prgrms/kdt/main/WelcomeController.java
@@ -0,0 +1,12 @@
+package org.prgrms.kdt.main;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WelcomeController {
+    @GetMapping("/")
+    public String welcome(){
+        return "redirect:/customers";
+    }
+}
