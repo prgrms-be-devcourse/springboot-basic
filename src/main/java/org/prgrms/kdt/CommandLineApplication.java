68d5c0a9e9c661c3d500f2e89558f7732885537b
BeautterLife <pgh9801@gmail.com>
2021-09-16 13:16:55 +0900

    delete/레거시 파일 삭제

--- src/main/java/org/prgrms/kdt/CommandLineApplication.java
+++
@@ -1,8 +0,0 @@
-package org.prgrms.kdt;
-
-public class CommandLineApplication {
-    public static void main(String[] args) {
-        VoucherTester voucherTester = new VoucherTester();
-        voucherTester.run();
-    }
-}
