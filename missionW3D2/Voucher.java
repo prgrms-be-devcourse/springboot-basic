934a71c1ebb1f4d16014433c08bd511bd7324cab
healtheefart <63666375+healtheefart@users.noreply.github.com>
2021-09-16 20:47:21 +0900

    Delete missionW3D2 directory

--- missionW3D2/Voucher.java
+++
@@ -1,9 +0,0 @@
-package com.prgms.missionW3D2;
-
-import java.util.UUID;
-
-public interface Voucher {
-    UUID getVoucherId();
-    long discount(long beforeDiscount);
-    String getVoucherInfo();
-}
--- missionW3D2/VoucherType.java
+++
@@ -1,6 +0,0 @@
-package com.prgms.missionW3D2;
-
-public enum VoucherType {
-    FixedAmountVoucher,
-    PercentDiscountVoucher
-}
