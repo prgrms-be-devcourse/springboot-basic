67956a2dcd723fd28649ae91f5992ba2fb7c200d
Spencer <spencer@grepp.co>
2021-09-17 02:58:41 +0900

    Revert "[W3D4] 고객 블랙 리스트 명단을 작성"
    This reverts commit 5ad80e495b569cc1f241e68b97678d666a481422.

--- kdt-spring-order_copy/src/main/java/org/prgrms/kdtspringorder/Voucher.java
+++
@@ -1,8 +0,0 @@
-package org.prgrms.kdtspringorder;
-
-import java.util.UUID;
-import java.util.Optional;
-public interface Voucher {
-    UUID getVoucherId();
-    long discount(long beforeDiscount);
-}
