ca9e1ec7b34536e1b8e984d63b9ee1732fe9e7d4
venzym <venzym@naver.com>
2021-09-17 23:35:42 +0900

    Fix: amount 입력값 예외 처리 추가

--- src/main/java/org/prgrms/kdtspringhomework/voucher/domain/PercentDiscountVoucher.java
+++ src/main/java/org/prgrms/kdtspringhomework/voucher/domain/PercentDiscountVoucher.java
@@ -3,12 +3,28 @@ package org.prgrms.kdtspringhomework.voucher.domain;
 import java.util.UUID;
 
 public class PercentDiscountVoucher implements Voucher {
+    private static final long ZERO_VOUCHER_AMOUNT = 0;
+    private static final long MAX_VOUCHER_AMOUNT = 10000;
+
     private final UUID voucherId;
     private final long percent;
 
     public PercentDiscountVoucher(UUID voucherId, long percent) {
+        validationPercent(percent);
         this.voucherId = voucherId;
         this.percent = percent;
+    }
+
+    private void validationPercent(long percent) {
+        if (percent < ZERO_VOUCHER_AMOUNT) {
+            throw new IllegalArgumentException("Percent should be positive");
+        }
+        if (percent == ZERO_VOUCHER_AMOUNT) {
+            throw new IllegalArgumentException("Percent should not be zero");
+        }
+        if (percent > MAX_VOUCHER_AMOUNT) {
+            throw new IllegalArgumentException(String.format("Percent should be less than %d", MAX_VOUCHER_AMOUNT));
+        }
     }
 
     @Override
