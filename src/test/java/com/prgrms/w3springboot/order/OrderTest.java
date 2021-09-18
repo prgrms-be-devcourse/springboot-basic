8344c933656e8339dd06631b5b95c412b14ca9cb
samkimuel <samkim2626@gmail.com>
2021-09-18 15:26:05 +0900

    fix: assertj 를 사용하여 테스트로 진행하는 것으로 수정
    - org.springframework.util.Assert는 테스트를 위한 역할의 클래스가 아님

--- src/test/java/com/prgrms/w3springboot/order/OrderTest.java
+++ src/test/java/com/prgrms/w3springboot/order/OrderTest.java
@@ -1,56 +1,57 @@
 package com.prgrms.w3springboot.order;
 
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 import com.prgrms.w3springboot.configuration.AppConfig;
 import com.prgrms.w3springboot.order.service.OrderService;
 import com.prgrms.w3springboot.voucher.FixedAmountVoucher;
 import com.prgrms.w3springboot.voucher.PercentAmountVoucher;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.util.Assert;
-
-import java.text.MessageFormat;
-import java.util.List;
-import java.util.UUID;
 
 class OrderTest {
 
-    @Test
-    @DisplayName("고정 할인 바우처 생성 테스트")
-    void testCreateFixedVoucher() {
-        var customerId = UUID.randomUUID();
-        var orderItems = List.of(
-                new OrderItem(UUID.randomUUID(), 100L, 1)
-        );
-        var fixedAmountVoucher = new FixedAmountVoucher(UUID.randomUUID(), 10L);
-        var order = new Order(UUID.randomUUID(), customerId, orderItems, fixedAmountVoucher);
+	@Test
+	@DisplayName("고정 할인 바우처 생성 테스트")
+	void testCreateFixedVoucher() {
+		var customerId = UUID.randomUUID();
+		var orderItems = List.of(
+			new OrderItem(UUID.randomUUID(), 100L, 1)
+		);
+		var fixedAmountVoucher = new FixedAmountVoucher(UUID.randomUUID(), 10L);
+		var order = new Order(UUID.randomUUID(), customerId, orderItems, fixedAmountVoucher);
 
-        Assert.isTrue(order.totalAmount() == 90L, MessageFormat.format("totalAmount is not {0}", order.totalAmount()));
-    }
+		assertThat(order.totalAmount()).isEqualTo(90L);
+	}
 
-    @Test
-    @DisplayName("퍼센트 할인 바우처 생성 테스트")
-    void testCreatePercentVoucher() {
-        var customerId = UUID.randomUUID();
-        var orderItems = List.of(
-                new OrderItem(UUID.randomUUID(), 100L, 1)
-        );
-        var percentAmountVoucher = new PercentAmountVoucher(UUID.randomUUID(), 200L);
-        var order = new Order(UUID.randomUUID(), customerId, orderItems, percentAmountVoucher);
+	@Test
+	@DisplayName("퍼센트 할인 바우처 생성 테스트")
+	void testCreatePercentVoucher() {
+		var customerId = UUID.randomUUID();
+		var orderItems = List.of(
+			new OrderItem(UUID.randomUUID(), 100L, 1)
+		);
+		var percentAmountVoucher = new PercentAmountVoucher(UUID.randomUUID(), 200L);
+		var order = new Order(UUID.randomUUID(), customerId, orderItems, percentAmountVoucher);
 
-        Assert.isTrue(order.totalAmount() == 200L, MessageFormat.format("totalAmount is not {0}", order.totalAmount()));
-    }
+		assertThat(order.totalAmount()).isEqualTo(200L);
+	}
 
-    @Test
-    @DisplayName("의존성 빈으로 주문 생성 테스트")
-    void testCreateOrderByDI() {
-        var applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
-        var orderService = applicationContext.getBean(OrderService.class);
-        var order = orderService.createOrder(UUID.randomUUID(), List.of(
-                new OrderItem(UUID.randomUUID(), 100L, 1))
-        );
+	@Test
+	@DisplayName("의존성 빈으로 주문 생성 테스트")
+	void testCreateOrderByDI() {
+		var applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+		var orderService = applicationContext.getBean(OrderService.class);
+		var order = orderService.createOrder(UUID.randomUUID(), List.of(
+			new OrderItem(UUID.randomUUID(), 100L, 1))
+		);
 
-        Assert.isTrue(order.totalAmount() == 100L, MessageFormat.format("totalAmount is not {0}", order.totalAmount()));
-    }
+		assertThat(order.totalAmount()).isEqualTo(100L);
+	}
 
 }
