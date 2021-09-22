888f3ee85a6a0b50b16f35521c39f93db1943c30
Chaeonida <46310555+Chaeonida@users.noreply.github.com>
2021-09-22 16:35:46 +0900

    [리뷰반영] enum으로 분기문 줄임

---
+++ kdt-spring-order_copy/src/main/java/org/prgrms/kdtspringorder/Command.java
@@ -0,0 +1,67 @@
+package org.prgrms.kdtspringorder;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Locale;
+import java.util.Map;
+import java.util.UUID;
+import java.util.function.Consumer;
+
+public enum Command {
+    CREATE("create", voucherService -> {
+        System.out.println("선택하세요 :");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("1. FixedAmountVoucher, 2. PercentDiscountVoucher");
+        String select = null;
+        try {
+            select = br.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if (select.equals("1")) {
+            Voucher voucher = voucherService.createVoucher(VoucherType.FixedAmountVoucher);
+            voucherService.addVoucher(voucher);
+            System.out.println("생성이 완료 되었습니다.");
+        } else if (select.equals("2")) {
+            Voucher voucher = voucherService.createVoucher(VoucherType.PercentDiscountVoucher);
+            voucherService.addVoucher(voucher);
+            System.out.println("생성이 완료 되었습니다.");
+        } else {
+            System.out.println("다시입력하세요");
+                }
+    }),
+
+    LIST("list", voucherService -> { Map<UUID, Voucher> list = voucherService.getAllVouchers();
+        int i =1;
+        for(Map.Entry<UUID,Voucher> entry : list.entrySet()){
+            System.out.println("["+i+"]" + entry.getValue());
+            i++;
+        }}),
+    EXIT("exit", aa -> {
+        System.out.println("종료");
+        System.exit(0);} );
+
+    private String value;
+
+    private Consumer<VoucherService> consumer;
+
+    public String value(){
+        return value;
+    }
+
+    Command(String value, Consumer<VoucherService> consumer){
+        this.value = value;
+        this.consumer = consumer;
+    }
+
+    public static Command from(String value){
+        return Command.valueOf(value.toUpperCase());
+    }
+
+    public void accept(VoucherService voucherService) {
+        this.consumer.accept(voucherService);
+    }
+
+}
--- kdt-spring-order_copy/src/main/java/org/prgrms/kdtspringorder/CommandLineApplication.java
+++ kdt-spring-order_copy/src/main/java/org/prgrms/kdtspringorder/CommandLineApplication.java
@@ -5,64 +5,34 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-import java.util.UUID;
+import java.util.*;
 
 public class CommandLineApplication {
+    static void tutorial() {
+        System.out.println("\n=== Voucher Program ===\n"
+                + "Type exit to exit the program.\n"
+                + "Type create to create a new voucher\n"
+                + "Type list to list all vouchers.");
+    }
+
     public static void main(String[] args) throws IOException {
-        StringBuilder sb = new StringBuilder();
-        sb.append("=== Voucher Program ===" + "\n");
-        sb.append("Type exit to exit the program." + "\n");
-        sb.append("Type create to create a new voucher." + "\n");
-        sb.append("Type list to list all voucher.");
 
         var applicationContext = new AnnotationConfigApplicationContext();
         applicationContext.register(AppConfiguration.class);
         var environment = applicationContext.getEnvironment();
         environment.setActiveProfiles("local");
         applicationContext.refresh();
-
-
         var voucherService = applicationContext.getBean(VoucherService.class);
-        System.out.println(sb);
-        while (true) {
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            String Com = br.readLine();
-            if (Com.equals("exit")) {
-                System.out.println("Program Exit");
-                break;
-            } else if (Com.equals("create")) {
-                System.out.println("선택하세요 :");
-                System.out.println("1. FixedAmountVoucher, 2. PercentDiscountVoucher");
-                String select = br.readLine();
-                if (select.equals("1")) {
-                    Voucher voucher = voucherService.createVoucher(VoucherType.FixedAmountVoucher);
-                    voucherService.addVoucher(voucher);
-                    System.out.println("생성이 완료 되었습니다.");
-                } else if (select.equals("2")) {
-                    Voucher voucher = voucherService.createVoucher(VoucherType.PercentDiscountVoucher);
-                    voucherService.addVoucher(voucher);
-                    System.out.println("생성이 완료 되었습니다.");
-                } else {
-                    System.out.println("다시입력하세요");
-                }
-            } else if (Com.equals("list")) {
-                Map<UUID, Voucher> list = voucherService.getAllVouchers();
-                int i =1;
-                for(Map.Entry<UUID,Voucher> entry :
-                        list.entrySet()){
-                    System.out.println("["+i+"]" + entry.getValue());
-                    i++;
-                }
-//                    for (int i = 0; i < list.size(); i++) {
-//                        Voucher voucher = list.get(i);
-//                        System.out.println(i +1 + "번 바우처 : " + voucher);//                   }
-                } else {
-                    System.out.println("exit, list, create 중에서 입력히세요");
-                }
-            }
 
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            tutorial();
+            System.out.printf(">> ");
+            String command = sc.nextLine(); // create or list
+
+            Command commandEnum = Command.from(command);
+            commandEnum.accept(voucherService);
         }
     }
+}
+
