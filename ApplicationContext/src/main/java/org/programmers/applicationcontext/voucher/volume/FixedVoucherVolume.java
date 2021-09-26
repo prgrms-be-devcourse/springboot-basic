aea31d51f8cdc61041a9ea88e25c603c0545e02a
wooko5 <wooko5@naver.com>
2021-09-26 18:51:37 +0900

    var 제거해서 구체적인 클래스 이름 사용

--- ApplicationContext/src/main/java/org/programmers/applicationcontext/voucher/volume/FixedVoucherVolume.java
+++ ApplicationContext/src/main/java/org/programmers/applicationcontext/voucher/volume/FixedVoucherVolume.java
@@ -2,7 +2,6 @@ package org.programmers.applicationcontext.voucher.volume;
 
 import org.programmers.applicationcontext.voucher.Voucher;
 import org.programmers.applicationcontext.voucher.VoucherService;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.util.List;
@@ -13,7 +12,7 @@ public class FixedVoucherVolume implements VoucherVolume{
     @Override
     public boolean change(List<Voucher> voucherList, BufferedReader br, UUID voucherId, VoucherService voucherService) throws IOException {
         long amount = Long.parseLong(br.readLine());
-        var fixedAmountVoucher = voucherService.createFixedAmountVoucher(voucherId, amount);
+        FixedAmountVoucher fixedAmountVoucher = voucherService.createFixedAmountVoucher(voucherId, amount);
         voucherList.add(fixedAmountVoucher);
         return false;
     }
