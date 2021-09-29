eb5aa69fd9664cc493a012f543db5eb200f3acda
samkimuel <samkim2626@gmail.com>
2021-09-30 04:37:20 +0900

    test: Wallet 도메인 관련 기능 테스트 코드 작성

--- src/test/java/com/prgrms/w3springboot/wallet/repository/NamedJdbcWalletRepositoryTest.java
+++ src/test/java/com/prgrms/w3springboot/wallet/repository/NamedJdbcWalletRepositoryTest.java
@@ -7,6 +7,7 @@ import static com.wix.mysql.config.MysqldConfig.*;
 import static com.wix.mysql.distribution.Version.*;
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.List;
 import java.util.UUID;
 
 import javax.sql.DataSource;
@@ -24,6 +25,8 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
+import com.prgrms.w3springboot.customer.Customer;
+import com.prgrms.w3springboot.voucher.Voucher;
 import com.prgrms.w3springboot.wallet.Wallet;
 import com.wix.mysql.EmbeddedMysql;
 import com.wix.mysql.config.MysqldConfig;
@@ -32,8 +35,6 @@ import com.zaxxer.hikari.HikariDataSource;
 @SpringJUnitConfig
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class NamedJdbcWalletRepositoryTest {
-
-	private static final int SUCCESS = 1;
 	private static final UUID NEW_WALLET_ID = UUID.randomUUID();
 	private static final UUID EXISTING_CUSTOMER_ID = UUID.fromString("75810d66-19bb-11ec-9621-0242ac130002");
 	private static final UUID EXISTING_VOUCHER_ID = UUID.fromString("9cbb3d0a-158a-11ec-82a8-0242ac130003");
@@ -65,10 +66,52 @@ class NamedJdbcWalletRepositoryTest {
 	@DisplayName("특정 고객에게 바우처를 할당한다.")
 	@Test
 	void testInsertWallet() {
+		// given
 		Wallet wallet = new Wallet(NEW_WALLET_ID, EXISTING_CUSTOMER_ID, EXISTING_VOUCHER_ID);
 
-		int result = walletRepository.insert(wallet);
-		assertThat(result).isEqualTo(SUCCESS);
+		// when
+		Wallet inserted = walletRepository.insert(wallet);
+
+		// then
+		assertThat(inserted.getWalletId()).isEqualTo(NEW_WALLET_ID);
+	}
+
+	@DisplayName("고객ID로 바우처 목록을 조회한다.")
+	@Test
+	void testFindVouchersByCustomerId() {
+		// when
+		List<Voucher> vouchers = walletRepository.findVouchersByCustomerId(EXISTING_CUSTOMER_ID);
+
+		// then
+		assertThat(vouchers).hasSize(1);
+	}
+
+	@DisplayName("고객에게 할당된 바우처를 삭제한다.")
+	@Test
+	void testDeleteVoucherFromWallet() {
+		// given
+		UUID walletId = UUID.randomUUID();
+		Wallet wallet = new Wallet(walletId, EXISTING_CUSTOMER_ID, EXISTING_VOUCHER_ID);
+		walletRepository.insert(wallet);
+		List<Voucher> beforeVouchers = walletRepository.findVouchersByCustomerId(EXISTING_CUSTOMER_ID);
+		assertThat(beforeVouchers).hasSize(2);
+
+		// when
+		walletRepository.delete(walletId);
+		List<Voucher> afterVouchers = walletRepository.findVouchersByCustomerId(EXISTING_CUSTOMER_ID);
+
+		// then
+		assertThat(afterVouchers).hasSize(1);
+	}
+
+	@DisplayName("특정 바우처를 보유한 고객을 조회한다.")
+	@Test
+	void testFindCustomersByVoucherId() {
+		// when
+		List<Customer> customers = walletRepository.findCustomersByVoucherId(EXISTING_VOUCHER_ID);
+
+		// then
+		assertThat(customers).hasSize(1);
 	}
 
 	@Configuration
