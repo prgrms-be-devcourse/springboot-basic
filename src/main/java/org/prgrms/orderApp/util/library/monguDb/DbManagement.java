912beb1a6433347c02b196635e7c840f1b3e3dbd
yun <locodingve@gmail.com>
2021-10-07 17:47:51 +0900

    Chore: update - 오타

--- src/main/java/org/prgrms/orderApp/util/library/monguDb/DbManagement.java
+++ src/main/java/org/prgrms/orderApp/util/library/monguDb/DbManagement.java
@@ -1,11 +1,10 @@
 package org.prgrms.orderApp.util.library.monguDb;
 
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 
-@Service
+@Component
 public class DbManagement {
     private DbConnection dbConnection;
-
 
     public DbConnection getConnection(){
         if (dbConnection == null){
