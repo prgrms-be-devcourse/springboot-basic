51f103a2baf1343d9cc472431499a4f94106f6d9
yun <locodingve@gmail.com>
2021-10-07 17:45:38 +0900

    feat: add - 이메일 형식 유효성 검사 기능

--- src/main/java/org/prgrms/orderApp/util/library/Validate.java
+++ src/main/java/org/prgrms/orderApp/util/library/Validate.java
@@ -1,9 +1,11 @@
 package org.prgrms.orderApp.util.library;
 
 
+import java.util.regex.Pattern;
+
 public class Validate {
 
-    public static void checkBlankForString(String valueName, String valueToCheck){
-        if (valueToCheck.isBlank()) throw new RuntimeException("%s should not be blank".formatted(valueName));
+    public static boolean checkAddress(String address) {
+        return Pattern.matches("\\b[\\w\\.-]+@[\\w\\.-]+\\.\\w{2,4}\\b", address);
     }
 }
