8f90a5838d98dbf8ba382220207ae1d133fbda8d
yun <locodingve@gmail.com>
2021-10-08 23:20:52 +0900

    Build: 추가 - Springboot App Config 파일

--- src/main/java/org/prgrms/orderApp/config/AppConfigration.java
+++ src/main/java/org/prgrms/orderApp/config/AppConfigration.java
@@ -1,5 +1,7 @@
 package org.prgrms.orderApp.config;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
