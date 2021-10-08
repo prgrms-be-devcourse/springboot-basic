d97d0cf527f953a06f4a5688b06dcdf6d09b2469
yun <locodingve@gmail.com>
2021-10-08 23:26:12 +0900

    Feat: 추가 - voucher service 구현 코드

--- src/main/java/org/prgrms/orderApp/voucher/VoucherService.java
+++ src/main/java/org/prgrms/orderApp/voucher/VoucherService.java
@@ -1,10 +1,14 @@
 package org.prgrms.orderApp.voucher;
 
-import org.json.simple.parser.ParseException;
-import org.springframework.stereotype.Service;
 
-import java.io.IOException;
+import org.prgrms.orderApp.error.CustomRuntimeException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
 import java.util.*;
+
+import static org.prgrms.orderApp.error.RestApiErrorCode.*;
 
 @Service
 public class VoucherService {
@@ -14,24 +18,80 @@ public class VoucherService {
     public VoucherService(VoucherRepository voucherRepository){
         this.voucherRepository = voucherRepository;
     }
-    private UUID voucherId ;
 
-    public Voucher getVoucher(UUID voucherId) {
-        return voucherRepository
-                .findById(voucherId)
-                .orElseThrow(() -> new RuntimeException("Can not find a voucher for " + voucherId));
+    @Transactional
+    public VoucherDto saveVoucher(VoucherDto voucherDto) {
+        var insertVoucher = new Voucher(
+                voucherDto.getName(),
+                voucherDto.getAmount(),
+                voucherDto.getVoucherType(),
+                voucherDto.getExpireAt()
+        );
+
+        if (insertVoucher.checkPercentVoucherOver100()) throw new CustomRuntimeException(PERCENT_VOUCHER_OVER_100);
+        if (voucherRepository.insert(insertVoucher) != 1) throw new CustomRuntimeException(FAIL_SAVE_VOUCHER);
+
+
+
+        return voucherRepository.findById(insertVoucher.getVoucherId())
+                .map(v -> new VoucherDto(
+                        v.getVoucherId(),
+                        v.getAmount(),
+                        v.getAvailable(),
+                        v.getVoucherType(),
+                        v.getName(),
+                        v.getExpireAt(),
+                        v.getCreatedAt(),
+                        v.getCustomerCount()
+                )).orElseThrow( () -> new CustomRuntimeException(FAIL_SAVE_VOUCHER) );
     }
 
-    public Optional<Voucher> saveVoucher(Voucher voucher) throws IOException {
-        return voucherRepository.insert(voucher) >0 ? Optional.of(voucher):Optional.empty();
+    public List<VoucherDto> getAllVouchers() {
+        return voucherRepository.findAll()
+                .stream()
+                .sorted(
+                        Comparator.comparing(Voucher::getCreatedAt))
+                .map(v -> new VoucherDto(
+                        v.getVoucherId(),
+                        v.getAmount(),
+                        v.getAvailable(),
+                        v.getVoucherType(),
+                        v.getName(),
+                        v.getExpireAt(),
+                        v.getCreatedAt(),
+                        v.getCustomerCount())
+                ).toList();
     }
 
-    public List<Voucher> getAllVouchers() throws IOException, ParseException {
-        return voucherRepository.findAll();
+
+    public VoucherDto getVoucherById(UUID voucherId){
+        return voucherRepository.findById(voucherId)
+                .map(v -> new VoucherDto(
+                        v.getVoucherId(),
+                        v.getAmount(),
+                        v.getAvailable(),
+                        v.getVoucherType(),
+                        v.getName(),
+                        v.getExpireAt(),
+                        v.getCreatedAt(),
+                        v.getCustomerCount()
+                ))
+                .orElseThrow(() -> new CustomRuntimeException(NOT_FOUNT_VOUCHER_ID));
     }
 
-    public void useVoucher(Voucher voucher) {
-
+    public UUID deleteById(UUID voucherId){
+        if (voucherRepository.deleteById(voucherId) == 0) throw new CustomRuntimeException(FAIL_DELETE_BY_ID);
+        return voucherId;
     }
+
+    public List<Voucher> getAllVoucherByType(String dataType){
+        return voucherRepository.findAllByType(dataType);
+    }
+
+    public List<Voucher> getAllVoucherByCreateDate(LocalDateTime fromDate, LocalDateTime toDate){
+        return voucherRepository.findAllByCreatedDate(fromDate, toDate);
+    }
+
 
 }
+
