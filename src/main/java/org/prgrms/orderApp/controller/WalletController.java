3cd826a7b03f230e99ed71036160df7f13715562
yun <locodingve@gmail.com>
2021-10-08 23:42:40 +0900

    Chore: 추가 - 상세 구현 코드 없음. 구조 잡기 위해 코드 생성

---
+++ src/main/java/org/prgrms/orderApp/controller/WalletController.java
@@ -0,0 +1,15 @@
+package org.prgrms.orderApp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/wallet")
+public class WalletController {
+
+
+
+}
+
