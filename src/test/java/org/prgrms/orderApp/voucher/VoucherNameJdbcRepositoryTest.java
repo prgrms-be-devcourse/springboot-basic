c98e89124ae36c32bcd9f894270e0082c5db0ca0
yun <locodingve@gmail.com>
2021-10-08 23:43:21 +0900

    Test: 추가 - voucher 레포지토리 테스트 코드

--- src/test/java/org/prgrms/orderApp/voucher/VoucherNameJdbcRepositoryTest.java
+++ src/test/java/org/prgrms/orderApp/voucher/VoucherNameJdbcRepositoryTest.java
@@ -2,6 +2,7 @@ package org.prgrms.orderApp.voucher;
 
 import com.zaxxer.hikari.HikariDataSource;
 import org.json.simple.parser.ParseException;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
@@ -13,6 +14,7 @@ import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.test.context.ActiveProfiles;
@@ -24,6 +26,7 @@ import org.springframework.transaction.support.TransactionTemplate;
 import javax.sql.DataSource;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -31,7 +34,6 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
 @SpringJUnitConfig
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class VoucherNameJdbcRepositoryTest {
 
     private static Logger logger = LoggerFactory.getLogger(VoucherNameJdbcRepositoryTest.class);
@@ -51,20 +53,24 @@ class VoucherNameJdbcRepositoryTest {
                     .password("root1234!")
                     .type(HikariDataSource.class)
                     .build();
-//            dataSource.setMaximumPoolSize(100);
-//            dataSource.setMinimumIdle(13);
             return dataSource;
 
         }
         @Bean
-        public NamedParameterJdbcTemplate namedParameterJdbcTemplate(DataSource dataSource){
-            return new NamedParameterJdbcTemplate(dataSource);
+        public JdbcTemplate jdbcTemplate(DataSource dataSource){
+            return new JdbcTemplate(dataSource);
+        }
+
+        @Bean
+        public NamedParameterJdbcTemplate namedParameterJdbcTemplate(JdbcTemplate jdbcTemplate){
+            return new NamedParameterJdbcTemplate(jdbcTemplate);
         }
 
         @Bean
         public PlatformTransactionManager platformTransactionManager(DataSource dataSource){
             return new DataSourceTransactionManager(dataSource);
         }
+
         @Bean
         public TransactionTemplate transactionTemplate(PlatformTransactionManager platformTransactionManager){
             return new TransactionTemplate(platformTransactionManager);
@@ -79,26 +85,99 @@ class VoucherNameJdbcRepositoryTest {
     @Autowired
     DataSource dataSource;
 
-    @Test
-    @DisplayName("FixedAmountData를 저장할 수 있다. ")
-    void insertFixedAmountData() throws IOException {
-        var insertResult = voucherNameJdbcRepository.insert(new FixedAmountVoucher(UUID.randomUUID(), 20));
-        assertThat(insertResult, is(1));
+
+    @AfterEach
+    void deleteAll(){
+        voucherNameJdbcRepository.findAll()
+                .stream().forEach(v -> voucherNameJdbcRepository.deleteById(v.getVoucherId()));
+
     }
 
     @Test
-    @DisplayName("PercentAmountData를 저장할 수 있다. ")
-    void insertPercentAmountData() throws IOException {
-        var insertResult = voucherNameJdbcRepository.insert(new PercentDiscountVoucher(UUID.randomUUID(), 50));
+    @DisplayName("바우처 정보를 저장할 수 있다.")
+    void insertTest() throws IOException {
+        //Given
+        var voucher = new Voucher("VOUCHER-1234", 20, "Fixed", LocalDateTime.now().toLocalDate());
+
+        //When
+        var insertResult = voucherNameJdbcRepository.insert(voucher);
+
+        //Then
         assertThat(insertResult, is(1));
     }
 
+
+
     @Test
-    @DisplayName("모든 고객 정보를 얻을 수 있다. ")
-    void findAll() throws IOException, ParseException {
+    @DisplayName("모든 바우처 정보를 추출할 수 있다.")
+    void findAllTest() throws IOException, ParseException {
+        //Given
+        voucherNameJdbcRepository.insert(new Voucher("VOUCHER-1234", 20, "Fixed", LocalDateTime.now().toLocalDate()));
+        voucherNameJdbcRepository.insert(new Voucher("VOUCHER-1223", 60, "Percent", LocalDateTime.now().toLocalDate()));
+        voucherNameJdbcRepository.insert(new Voucher("VOUCHER-2443", 2000, "Fixed", LocalDateTime.now().toLocalDate()));
+
+        //When
         var findData = voucherNameJdbcRepository.findAll();
+
+        //Then
         assertThat(findData.isEmpty(), is(false));
-        findData.forEach(v->System.out.println(v));
+    }
+
+    @Test
+    @DisplayName("바우처 아이디를 이용하여 바우처 정보를 추출할 수 있다.")
+    void findByIdTest(){
+        //Given
+        var voucher1 = new Voucher("VOUCHER-1234", 20, "Fixed", LocalDateTime.now().toLocalDate());
+        var voucher2 = new Voucher("VOUCHER-1223", 60, "Percent", LocalDateTime.now().toLocalDate());
+        voucherNameJdbcRepository.insert(voucher1);
+        voucherNameJdbcRepository.insert(voucher2);
+
+        //When
+        var dataSelected1 = voucherNameJdbcRepository.findById(voucher1.getVoucherId()).get();
+        var dataSelected2 = voucherNameJdbcRepository.findById(voucher2.getVoucherId()).get();
+
+        //Then
+        assertThat(dataSelected1.getName(), is(voucher1.getName()));
+        assertThat(dataSelected1.getVoucherType(), is(voucher1.getVoucherType()));
+        assertThat(dataSelected2.getName(), is(voucher2.getName()));
+        assertThat(dataSelected2.getVoucherType(), is(voucher2.getVoucherType()));
 
     }
+
+    @Test
+    @DisplayName("바우처 타입을 이용하여, 바우처 정보를 추출할 수 있다.")
+    void findAllByType(){
+        //Given
+        var voucher1 = new Voucher("VOUCHER-1234", 20, "Fixed", LocalDateTime.now().toLocalDate());
+        var voucher2 = new Voucher("VOUCHER-1223", 60, "percent", LocalDateTime.now().toLocalDate());
+        voucherNameJdbcRepository.insert(voucher1);
+        voucherNameJdbcRepository.insert(voucher2);
+
+        //When
+        var fixedVouchersSelected = voucherNameJdbcRepository.findAllByType("Fixed");
+        var percentVoucherSelected = voucherNameJdbcRepository.findAllByType("percent");
+
+        //Then
+        assertThat(fixedVouchersSelected.stream().allMatch(v -> v.getVoucherType().equals("Fixed")), is(true) );
+        assertThat(percentVoucherSelected.stream().allMatch(v -> v.getVoucherType().equals("percent")), is(true) );
+    }
+
+    @Test
+    @DisplayName("바우처 정보 생성 날짜를 설정하여, 바우처 정보를 추출할 수 있다.")
+    void findAllByCreateDate(){
+        //Given
+        var voucher1 = new Voucher("VOUCHER-1234", 20, "Fixed", LocalDateTime.now().toLocalDate());
+        var voucher2 = new Voucher("VOUCHER-1223", 60, "percent", LocalDateTime.now().toLocalDate());
+        voucherNameJdbcRepository.insert(voucher1);
+        voucherNameJdbcRepository.insert(voucher2);
+
+        //When
+        var voucherByCreatedDate1 = voucherNameJdbcRepository.findAllByCreatedDate(LocalDateTime.now().minusDays(1), LocalDateTime.now());
+        var voucherByCreatedDate2 = voucherNameJdbcRepository.findAllByCreatedDate(LocalDateTime.now().minusDays(2), LocalDateTime.now().minusDays(1));
+
+        //Then
+        assertThat(voucherByCreatedDate1.size(), greaterThan(0));
+        assertThat(voucherByCreatedDate2.size(), is(0));
+    }
+
 }
