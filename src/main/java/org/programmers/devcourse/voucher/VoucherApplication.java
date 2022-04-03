d4d619e5b9bc1f045e8442dfb34b3ba3bd1317a2
Minsung Kim <epicblue@hanmail.net>
2022-04-04 00:01:17 +0900

    chore : spring 프로젝트 초기화

---
+++ src/main/java/org/programmers/devcourse/voucher/VoucherApplication.java
@@ -0,0 +1,15 @@
+package org.programmers.devcourse.voucher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoucherApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(VoucherApplication.class, args);
+    System.out.println("Hello World");
+  }
+
+
+}
---
+++ src/test/java/org/programmers/devcourse/voucher/VoucherApplicationTests.java
@@ -0,0 +1,13 @@
+package org.programmers.devcourse.voucher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoucherApplicationTests {
+	
+	@Test
+	void contextLoads() {
+	}
+
+}
