e2059bb87baceef424ac1cd2eb807d2da2dc5a0d
hong yuseok <jericoh@naver.com>
2022-04-05 01:05:16 +0900

    프로젝트 초기 설정

---
+++ src/main/java/org/prgms/voucheradmin/VoucherAdminApplication.java
@@ -0,0 +1,13 @@
+package org.prgms.voucheradmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoucherAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoucherAdminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/prgms/voucheradmin/VoucherAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package org.prgms.voucheradmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoucherAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
