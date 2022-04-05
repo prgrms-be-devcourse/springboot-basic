bc8a9844f855c959370e962d3b92d691ed3a8a2e
suy2on <tnwjdrms@naver.com>
2022-04-05 17:10:49 +0900

    # feat : 주문상태 열거형으로 구현

---
+++ src/main/java/org/prgrms/spring_week1/models/OrderStatus.java
@@ -0,0 +1,12 @@
+package org.prgrms.spring_week1.models;
+
+public enum OrderStatus {
+    ACCEPTED,
+    PAYMENT_REQUIRED,
+    PAYMENT_CONFIRMED,
+    PAYMENT_REJECTED,
+    READY_FOR_DELIVERY,
+    SHIPPED,
+    SETTLED,
+    CANCELLED
+}
