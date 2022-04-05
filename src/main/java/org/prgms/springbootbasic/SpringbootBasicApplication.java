cff191c3eb2df6afd1740dfe07f17aca1331d30c
nnagman <ckdgh930314@naver.com>
2022-04-05 16:52:16 +0900

    feat : SpringBoot CLI로 프로젝트 생성 및 README.md 수정

---
+++ src/main/java/org/prgms/springbootbasic/SpringbootBasicApplication.java
@@ -0,0 +1,12 @@
+package org.prgms.springbootbasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBasicApplication.class, args);
+	}
+}
