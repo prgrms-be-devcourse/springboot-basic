e9a1d6acff82b222868fe21e398f25c0709666cc
suy2on <tnwjdrms@naver.com>
2022-04-05 16:13:50 +0900

    # Feat : init

---
+++ src/main/java/org/prgrms/spring_week1/SpringWeek1Application.java
@@ -0,0 +1,13 @@
+package org.prgrms.spring_week1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWeek1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWeek1Application.class, args);
+	}
+
+}
