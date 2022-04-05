d84623b2ad38ded8d92c4bd5808bf1dc47da5491
suy2on <tnwjdrms@naver.com>
2022-04-05 17:11:35 +0900

    # feat : 주문과 주문상품 클래스 구현

---
+++ src/main/java/org/prgrms/spring_week1/models/Order.java
@@ -0,0 +1,48 @@
+package org.prgrms.spring_week1.models;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public class Order {
+    UUID orderId;
+    UUID customerId;
+    private List<OrderItem> orderItemList;
+    private Optional<Voucher> voucher;
+    private OrderStatus orderStatus = OrderStatus.ACCEPTED;
+
+    // 바우처 없는 경우
+    public Order(UUID orderId, UUID customerId, List<OrderItem> orderItemList, Optional<Voucher> voucher, OrderStatus orderStatus) {
+        this.orderId = orderId;
+        this.customerId = customerId;
+        this.orderItemList = orderItemList;
+        this.voucher = voucher;
+        this.orderStatus = orderStatus;
+    }
+
+    // 바우처 있는경우
+    public Order(UUID orderId, UUID customerId, List<OrderItem> orderItemList, OrderStatus orderStatus) {
+        this.orderId = orderId;
+        this.customerId = customerId;
+        this.orderItemList = orderItemList;
+        this.orderStatus = orderStatus;
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        this.orderStatus = orderStatus;
+    }
+
+    public long totalPrice(){
+        Long beforeDiscount = orderItemList
+                                    .stream()
+                                    .map( i -> i.getProductPrice() * i.getQuantity())
+                                    .reduce(0L, Long::sum);
+
+        if(voucher.isPresent()){
+            return voucher.get().discount(beforeDiscount);
+        } else {
+            return beforeDiscount;
+        }
+
+    }
+}
