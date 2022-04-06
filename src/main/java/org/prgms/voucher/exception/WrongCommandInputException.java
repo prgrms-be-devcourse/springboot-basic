670f5fd691ed8d3b2882d8534f2ac80cda91aa35
yuminhwan <ymh7532@icloud.com>
2022-04-07 00:58:32 +0900

    feat: 커스텀 Exception 추가

--- src/main/java/org/prgms/voucher/exception/WrongCommandInputException.java
+++
@@ -1,7 +0,0 @@
-package org.prgms.voucher.exception;
-
-public class WrongCommandInputException extends Exception {
-    public WrongCommandInputException() {
-        super("[ERROR] 올바른 커맨드가 아닙니다.");
-    }
-}
---
+++ src/main/java/org/prgms/voucher/exception/WrongInputVoucherCommandException.java
@@ -0,0 +1,7 @@
+package org.prgms.voucher.exception;
+
+public class WrongInputVoucherCommandException extends Exception {
+    public WrongInputVoucherCommandException() {
+        super("[ERROR] 올바른 바우처 커맨드가 아닙니다.");
+    }
+}
