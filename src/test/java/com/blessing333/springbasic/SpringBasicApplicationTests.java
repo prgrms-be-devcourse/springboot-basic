67678f5b6b44c9cfb59381198db0f981ba4f7328
blessing333 <>
2022-04-06 16:04:40 +0900

    init

---
+++ src/test/java/com/blessing333/springbasic/SpringBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blessing333.springbasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBasicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
