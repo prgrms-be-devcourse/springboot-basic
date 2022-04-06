552437e8b2089ecab6c71a3c6bd2eb4a5a890ec9
yuminhwan <ymh7532@icloud.com>
2022-04-07 00:09:08 +0900

    style: @DisplayName 어노테이션 추가

--- src/test/java/org/prgms/voucher/entity/VoucherTypeTest.java
+++ src/test/java/org/prgms/voucher/entity/VoucherTypeTest.java
@@ -2,10 +2,12 @@ package org.prgms.voucher.entity;
 
 import static org.assertj.core.api.Assertions.*;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 class VoucherTypeTest {
+    @DisplayName("해당하는 VoucherType을 반환한다.")
     @ParameterizedTest
     @CsvSource(value = {"1,FIXED_AMOUNT", "2,PERCENT_DISCOUNT", "asd,EMPTY", ",EMPTY"})
     void of_VoucherCommand_ReturnVoucherType(String command, VoucherType voucherType) {
