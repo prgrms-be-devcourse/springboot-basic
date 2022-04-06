f1868e4ffb2e7caeee7cba184061e652188ab8fa
suy2on <tnwjdrms@naver.com>
2022-04-06 18:59:58 +0900

    # feat : 컴포넌트스캔으로 변형

--- src/main/java/org/prgrms/spring_week1/AppConfig.java
+++ src/main/java/org/prgrms/spring_week1/AppConfig.java
@@ -7,28 +7,30 @@ import org.prgrms.spring_week1.repositories.VoucherRepository;
 import org.prgrms.spring_week1.services.OrderService;
 import org.prgrms.spring_week1.services.VoucherService;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan
 public class AppConfig {
 
-    @Bean
-    public VoucherRepository voucherRepository(){
-        return new VoucherMemoryRepository();
-    }
-
-    @Bean
-    public OrderRepository oerderRepository(){
-        return new OrderMemoryRepository();
-    }
-
-    @Bean
-    public VoucherService voucherService(VoucherRepository voucherRepository){
-        return new VoucherService(voucherRepository);
-    }
-
-    @Bean
-    public OrderService orderService(VoucherService voucherService, OrderRepository orderRepository){
-        return new OrderService(orderRepository, voucherService);
-    }
+//    @Bean
+//    public VoucherRepository voucherRepository(){
+//        return new VoucherMemoryRepository();
+//    }
+//
+//    @Bean
+//    public OrderRepository oerderRepository(){
+//        return new OrderMemoryRepository();
+//    }
+//
+//    @Bean
+//    public VoucherService voucherService(VoucherRepository voucherRepository){
+//        return new VoucherService(voucherRepository);
+//    }
+//
+//    @Bean
+//    public OrderService orderService(VoucherService voucherService, OrderRepository orderRepository){
+//        return new OrderService(orderRepository, voucherService);
+//    }
 }
