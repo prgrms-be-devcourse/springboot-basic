aabf162b181efa024feb24772115ea272821397d
HyunJi Park <rioreo2022@gmail.com>
2022-04-06 20:23:30 +0900

    Initial commit

---
+++ src/test/java/org/prgrms/vouchermanager/VoucherManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.prgrms.vouchermanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoucherManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
