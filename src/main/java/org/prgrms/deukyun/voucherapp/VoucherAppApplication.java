24b48afbf6dad8fce094086ffa2bfc59d993a28a
ndy2 <emrdbs12@gmail.com>
2022-04-06 21:32:38 +0900

    initial commit by ndy2

---
+++ src/main/java/org/prgrms/deukyun/voucherapp/VoucherAppApplication.java
@@ -0,0 +1,13 @@
+package org.prgrms.deukyun.voucherapp;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoucherAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoucherAppApplication.class, args);
+	}
+}
---
+++ src/test/java/org/prgrms/deukyun/voucherapp/VoucherAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.prgrms.deukyun.voucherapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoucherAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
