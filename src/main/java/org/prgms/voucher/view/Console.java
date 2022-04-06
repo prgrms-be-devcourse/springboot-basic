30229190d4bed9ab2c88e6a90f00e2de0843d276
yuminhwan <ymh7532@icloud.com>
2022-04-07 01:15:45 +0900

    style: 메시지 알맞게 출력하도록 변경

--- src/main/java/org/prgms/voucher/view/Console.java
+++ src/main/java/org/prgms/voucher/view/Console.java
@@ -9,24 +9,26 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Console implements InputView, OutputView {
-    private static final String REQUEST_INPUT_COMMAND = "=== Voucher Program ===\nType **exit** to exit the program.\nType **create** to create a new voucher.\nType **list** to list all vouchers.";
-    private static final String REQUEST_INPUT_VOUCHER_TYPE = "Select a voucher type\n 1:FixedAmountVoucher 2:PercentDiscountVoucher";
-    private static final String REQUEST_INPUT_DISCOUNT_AMOUNT = "Input voucher discount amount : ";
-    private static final String REQUEST_INPUT_DISCOUNT_PERCENTAGE = "Input voucher discount percentage : ";
+    private static final String PROMPT = "> ";
+    private static final String REQUEST_INPUT_COMMAND = "=== Voucher Program ===\nType \"exit\" to exit the program.\nType \"create\" to create a new voucher.\nType \"list\" to list all vouchers.";
+    private static final String REQUEST_INPUT_VOUCHER_TYPE = "\nSelect a voucher type\nType \"1\" to create a new FixedAmountVoucher\nType \"2\" to create a new PercentDiscountVoucher";
+    private static final String REQUEST_INPUT_DISCOUNT_AMOUNT = "\nInput voucher discount amount : ";
+    private static final String REQUEST_INPUT_DISCOUNT_PERCENTAGE = "\nInput voucher discount percentage : ";
     private static final String EMPTY_VOUCHERS = "Empty Vouchers";
     private static final String ERROR_INPUT_NUMBER_TYPE = "[ERROR] 정수만 입력가능합니다.";
-
     private final Scanner scanner = new Scanner(System.in);
 
     @Override
     public String inputMenu() {
         System.out.println(REQUEST_INPUT_COMMAND);
+        System.out.print(PROMPT);
         return scanner.nextLine().trim().toLowerCase();
     }
 
     @Override
     public String inputVoucherCommand() {
         System.out.println(REQUEST_INPUT_VOUCHER_TYPE);
+        System.out.print(PROMPT);
         return scanner.nextLine().trim().toLowerCase();
     }
 
@@ -38,7 +40,7 @@ public class Console implements InputView, OutputView {
             message = REQUEST_INPUT_DISCOUNT_AMOUNT;
         }
 
-        System.out.println(message);
+        System.out.print(message);
         return convertToLong(scanner.nextLine().trim());
     }
 
@@ -52,22 +54,25 @@ public class Console implements InputView, OutputView {
 
     @Override
     public void printVoucher(Voucher voucher) {
-        System.out.println(voucher);
+        System.out.printf("%s\n%n", voucher);
     }
 
     @Override
     public void printAllVoucher(List<Voucher> vouchers) {
         if (vouchers.isEmpty()) {
-            System.out.println(EMPTY_VOUCHERS);
+            System.out.printf("\n%s%n\n", EMPTY_VOUCHERS);
+            return;
         }
 
+        System.out.println();
         for (Voucher voucher : vouchers) {
             System.out.println(voucher);
         }
+        System.out.println();
     }
 
     @Override
     public void printError(String message) {
-        System.out.println(message);
+        System.out.println("\n" + message + "\n");
     }
 }
