676357352d0a3cc5138d3b2f88dcdec40c6eefbd
yuminhwan <ymh7532@icloud.com>
2022-04-07 16:02:46 +0900

    feat: Voucher 파일로 저장하는 기능 구현

--- src/main/java/org/prgms/voucher/entity/Voucher.java
+++ src/main/java/org/prgms/voucher/entity/Voucher.java
@@ -1,8 +1,9 @@
 package org.prgms.voucher.entity;
 
+import java.io.Serializable;
 import java.util.UUID;
 
-public interface Voucher {
+public interface Voucher extends Serializable {
     long discount(long beforeDiscount);
 
     UUID getVoucherId();
