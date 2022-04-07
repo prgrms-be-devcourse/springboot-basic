903db07891a6cd49b1ea8d0ebfb0dcca715b9435
kimziou77 <kimziou77@naver.com>
2022-04-08 08:29:52 +0900

    Init project
    environment : java11 / gradle
    dependency : lombok
    docs : .gitignore

---
+++ voucher-project/src/main/java/com/example/voucherproject/VoucherProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.voucherproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoucherProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VoucherProjectApplication.class, args);
+    }
+
+}
