f39716cf35e2ed6ffbc9e4eaf89d3bff6d2d3a7b
pjh612 <62292492+pjh612@users.noreply.github.com>
2022-04-07 16:55:05 +0900

    feat : blacklistRepository 추가
    BlacklistRepository.java
    - findAll() : 블랙리스트 전체조회
    
    customer_blacklist.csv
    - db 파일

---
+++ voucher-management/src/main/java/com/voucher/vouchermanagement/repository/blacklist/BlacklistRepository.java
@@ -0,0 +1,51 @@
+package com.voucher.vouchermanagement.repository.blacklist;
+
+import com.voucher.vouchermanagement.model.user.User;
+import com.voucher.vouchermanagement.repository.io.FileInput;
+import com.voucher.vouchermanagement.repository.io.FileOutput;
+import com.voucher.vouchermanagement.repository.utils.CsvDeserializer;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class BlacklistRepository {
+
+  private final File blacklistDb;
+  private final FileInput input;
+  private final FileOutput output;
+  private final CsvDeserializer<User> csvDeserializer;
+  private static final Logger logger = LoggerFactory.getLogger(BlacklistRepository.class);
+
+  public BlacklistRepository(@Qualifier("blacklistDb") File blacklistDb, FileInput input, FileOutput output,
+      @Qualifier("blacklistDeserializer") CsvDeserializer<User> csvDeserializer) {
+    this.blacklistDb = blacklistDb;
+    this.input = input;
+    this.output = output;
+    this.csvDeserializer = csvDeserializer;
+  }
+
+  public List<User> findAll() throws IOException {
+    logger.info("findAll()");
+
+    List<User> foundBlacklist = input
+        .readAllLine(blacklistDb)
+        .stream().map(csvDeserializer::deserialize)
+        .collect(Collectors.toList());
+
+    logger.info("found blacklist data");
+    foundBlacklist
+        .stream()
+        .forEach(user -> logger.info(user.toString()));
+
+    return input
+        .readAllLine(blacklistDb)
+        .stream().map(csvDeserializer::deserialize)
+        .collect(Collectors.toList());
+  }
+}
