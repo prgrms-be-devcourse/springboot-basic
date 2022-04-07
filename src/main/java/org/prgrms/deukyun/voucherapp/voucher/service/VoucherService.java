cc54036cf151ce22ee583bc6cd92ede90d0d49b7
ndy2 <emrdbs12@gmail.com>
2022-04-08 01:19:07 +0900

    주석 추가

--- src/main/java/org/prgrms/deukyun/voucherapp/voucher/service/VoucherService.java
+++ src/main/java/org/prgrms/deukyun/voucherapp/voucher/service/VoucherService.java
@@ -19,7 +19,7 @@ public class VoucherService {
         this.voucherRepository = voucherRepository;
     }
 
-    /**
+    /** 바우처 리포지토리에서 주어진 아이디의 바우처를 찾아 리턴
      * @param voucherId 바우처 아이디
      * @return 바우처
      * @throws RuntimeException 바우처 리포지토리에서 주어진 아이디의 바우처를 찾지 못한 경우 런 타임 예외
