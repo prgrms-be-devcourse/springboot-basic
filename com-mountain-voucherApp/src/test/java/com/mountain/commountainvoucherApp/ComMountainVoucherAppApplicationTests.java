4a66c5ac6b6d30bbcb5c53794ec3684b2c245fdb
taesan <pawer02023@gmail.com>
2022-04-08 22:57:43 +0900

    feat: CLI를 통한 프로젝트 생성
    - Spring-Boot CLI를 통한 프로젝트 생성

---
+++ com-mountain-voucherApp/src/test/java/com/mountain/commountainvoucherApp/ComMountainVoucherAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mountain.commountainvoucherApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComMountainVoucherAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
