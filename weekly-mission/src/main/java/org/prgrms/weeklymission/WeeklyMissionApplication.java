625fb9429c2f038a4640e9a677f54cf43ae672d2
Seonghyeon Kim <goshk1995@gmail.com>
2022-04-08 19:41:08 +0900

    initial commit

---
+++ weekly-mission/src/main/java/org/prgrms/weeklymission/WeeklyMissionApplication.java
@@ -0,0 +1,13 @@
+package org.prgrms.weeklymission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeeklyMissionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeeklyMissionApplication.class, args);
+	}
+
+}
---
+++ weekly-mission/src/test/java/org/prgrms/weeklymission/WeeklyMissionApplicationTests.java
@@ -0,0 +1,13 @@
+package org.prgrms.weeklymission;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeeklyMissionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
