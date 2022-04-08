93a039f71dc6ebd0f51f823e573dcbcd2382a4c7
beomsic <qjatjr29@khu.ac.kr>
2022-04-08 17:48:51 +0900

    configuration 파일

---
+++ src/main/java/com/prgrms/voucher_manager/configuration/AppConfiguration.java
@@ -0,0 +1,10 @@
+package com.prgrms.voucher_manager.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = {"com.prgrms.voucher_manager"})
+public class AppConfiguration {
+
+}
