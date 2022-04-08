cc711b61bfba8266e39d9b1f4666a6bb2088e2d6
mountain <pawer02023@gmail.com>
2022-04-08 23:36:52 +0900

    fix/1 프로젝트 이름 변경 (#4)
    * fix: 프로젝트 이름 변경
    
    프로젝트 이름 변경

---
+++ voucherApp/src/test/java/com/mountain/voucherApp/VoucherAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mountain.voucherApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoucherAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
