e16d82195edb0a076c51db7af61cdc5abf3d9157
Hyunggeun447 <bopi447@naver.com>
2022-04-09 02:12:31 +0900

    VoucherProjectApplication

--- src/main/java/org/voucherProject/voucherProject/VoucherProjectApplication.java
+++ src/main/java/org/voucherProject/voucherProject/VoucherProjectApplication.java
@@ -3,18 +3,16 @@ package org.voucherProject.voucherProject;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class VoucherProjectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(VoucherProjectApplication.class, args);
-	}
-
-	@Bean
-	public CommandLineRunner run(VoucherEnrollSystem voucherEnrollSystem) {
-		return args -> voucherEnrollSystem.run();
+		ApplicationContext ac = SpringApplication.run(VoucherProjectApplication.class, args);
+		ac.getBean(VoucherEnrollSystem.class).run();
 	}
 
 }
