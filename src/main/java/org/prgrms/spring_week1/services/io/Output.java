86bf17d6f006be43e8fe6424c4936ac8abdf34c6
suy2on <tnwjdrms@naver.com>
2022-04-08 18:35:43 +0900

    # feat : 콘솔 입출력 추가

--- src/main/java/org/prgrms/spring_week1/services/io/Output.java
+++ src/main/java/org/prgrms/spring_week1/services/io/Output.java
@@ -2,5 +2,7 @@ package org.prgrms.spring_week1.services.io;
 
 public interface Output {
     void output(String str);
-    void menu();
+    void mainMenu();
+    void voucherCreate();
+    void wrongInput();
 }
