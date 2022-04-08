629ad0797afbb2937490ae98a31b03be908d28fa
beomsic <qjatjr29@khu.ac.kr>
2022-04-08 17:51:19 +0900

    voucher repository가 비었을 경우 예외

---
+++ src/main/java/com/prgrms/voucher_manager/exception/EmptyVoucherException.java
@@ -0,0 +1,7 @@
+package com.prgrms.voucher_manager.exception;
+
+public class EmptyVoucherException extends RuntimeException{
+    public EmptyVoucherException(String message) {
+        super(message);
+    }
+}
