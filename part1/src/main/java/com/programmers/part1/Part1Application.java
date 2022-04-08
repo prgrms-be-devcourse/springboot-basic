e82d44bbfaa84ee8516cfe6ab820220f23e75cdb
jki503 <jki503@naver.com>
2022-04-09 03:18:26 +0900

    Init : 프로젝트 생성 Chore : Lombok 추가 Docs : customer_blacklist.csv 추가

---
+++ part1/src/main/java/com/programmers/part1/Part1Application.java
@@ -0,0 +1,12 @@
+package com.programmers.part1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Part1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Part1Application.class, args);
+    }
+}
