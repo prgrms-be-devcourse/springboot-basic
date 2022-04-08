a3f7476f46e0137d83b275fe29d5043fa71f08a7
suy2on <tnwjdrms@naver.com>
2022-04-08 17:11:53 +0900

    # feat : 콘솔 입출력 추가

---
+++ src/main/java/org/prgrms/spring_week1/services/io/Input.java
@@ -0,0 +1,5 @@
+package org.prgrms.spring_week1.services.io;
+
+public interface Input {
+    void input();
+}
---
+++ src/main/java/org/prgrms/spring_week1/services/io/Output.java
@@ -0,0 +1,6 @@
+package org.prgrms.spring_week1.services.io;
+
+public interface Output {
+    void output(String str);
+    void menu();
+}
