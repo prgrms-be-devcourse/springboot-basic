259d157e9b58f07a0da28a21da19b8cd3698082a
Yanjoo <hanjub3@gmail.com>
2022-04-10 02:08:32 +0900

    Voucher 인터페이스 생성

---
+++ src/main/java/me/programmers/springboot/basic/springbootbasic/voucher/model/Voucher.java
@@ -0,0 +1,9 @@
+package me.programmers.springboot.basic.springbootbasic.voucher.model;
+
+import java.util.UUID;
+
+public interface Voucher {
+
+    UUID getVoucherId();
+    long discount(long beforeDiscount);
+}
