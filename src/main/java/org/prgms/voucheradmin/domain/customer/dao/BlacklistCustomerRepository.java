b616e125af6df2bbfb8b74ca2da8f302deccd16e
hong yuseok <jericoh@naver.com>
2022-04-10 03:16:14 +0900

    feat: jar 파일 생성

--- src/main/java/org/prgms/voucheradmin/domain/customer/dao/BlacklistCustomerRepository.java
+++ src/main/java/org/prgms/voucheradmin/domain/customer/dao/BlacklistCustomerRepository.java
@@ -1,6 +1,10 @@
 package org.prgms.voucheradmin.domain.customer.dao;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.List;
@@ -10,6 +14,9 @@ import org.prgms.voucheradmin.global.properties.VoucherAdminProperties;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.FileCopyUtils;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
 
 @Repository
 public class BlacklistCustomerRepository implements CustomerRepository {
@@ -24,11 +31,12 @@ public class BlacklistCustomerRepository implements CustomerRepository {
     @Override
     public List<Customer> getAll() throws IOException {
         Resource resource = resourceLoader.getResource(voucherAdminProperties.getBlacklistFilePath());
-        List<String> records = Files.readAllLines(resource.getFile().toPath());
+        Reader reader = new InputStreamReader(resource.getInputStream(), UTF_8);
+        String[] records = FileCopyUtils.copyToString(reader).split("\n");
 
         List<Customer> customers = new ArrayList<>();
-        for(int i = 1; i < records.size(); i++) {
-            String[] columns = records.get(i).split(",");
+        for(int i = 0; i < records.length; i++) {
+            String[] columns = records[i].split(",");
             customers.add(new Customer(Long.parseLong(columns[0]), columns[1]));
         }
 
