8b4001fbe035849e39fa103e0e488d6762646c1c
yuminhwan <ymh7532@icloud.com>
2022-04-10 03:09:32 +0900

    fix: UserRepository 주입방식 수정

--- src/main/java/org/prgms/voucherProgram/service/UserService.java
+++ src/main/java/org/prgms/voucherProgram/service/UserService.java
@@ -3,19 +3,18 @@ package org.prgms.voucherProgram.service;
 import java.util.List;
 
 import org.prgms.voucherProgram.entity.user.User;
-import org.prgms.voucherProgram.repository.user.FileUserRepository;
 import org.prgms.voucherProgram.repository.user.UserRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
-    private final UserRepository fileUserRepository;
+    private final UserRepository userRepository;
 
-    public UserService(FileUserRepository fileUserRepository) {
-        this.fileUserRepository = fileUserRepository;
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
     }
 
     public List<User> findBlackList() {
-        return fileUserRepository.findAll();
+        return userRepository.findAll();
     }
 }
--- src/test/java/org/prgms/voucherProgram/service/UserServiceTest.java
+++ src/test/java/org/prgms/voucherProgram/service/UserServiceTest.java
@@ -12,19 +12,19 @@ import org.prgms.voucherProgram.entity.user.User;
 import org.prgms.voucherProgram.repository.user.UserRepository;
 
 class UserServiceTest {
-    UserRepository userRepository = new UserRepository() {
+    UserService userService = new UserService(new UserRepository() {
         @Override
         public List<User> findAll() {
             return Arrays.asList(new User(UUID.randomUUID(), "jin"),
                 new User(UUID.randomUUID(), "hwan"),
                 new User(UUID.randomUUID(), "pobi"));
         }
-    };
+    });
 
     @DisplayName("모든 유저를 반환한다.")
     @Test
     void findAll_ReturnAllVoucher() {
-        assertThat(userRepository.findAll()).hasSize(3)
+        assertThat(userService.findBlackList()).hasSize(3)
             .extracting("name").contains("hwan", "jin", "pobi");
     }
 }
