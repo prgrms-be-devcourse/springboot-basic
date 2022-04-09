4d3fddf1316d0b23b8a16c60b00435ce9a2ec480
unnokid <kimki0609@gmail.com>
2022-04-10 00:14:05 +0900

    Change type of applicationContext to ApplicationContext

--- src/main/java/org/programmer/kdtspringboot/KdtSpringBootApplication.java
+++ src/main/java/org/programmer/kdtspringboot/KdtSpringBootApplication.java
@@ -2,15 +2,14 @@ package org.programmer.kdtspringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class KdtSpringBootApplication {
 
     public static void main(String[] args) {
-        ConfigurableApplicationContext applicationContext = SpringApplication.run(KdtSpringBootApplication.class, args);
+        ApplicationContext applicationContext = SpringApplication.run(KdtSpringBootApplication.class, args);
         applicationContext.getBean(VoucherSystem.class).run();
-        applicationContext.close();
     }
 
 }
