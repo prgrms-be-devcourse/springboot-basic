ecc5a70a62938420dafca104044d8e7bab3e214f
wisehero <godjw96@gmail.com>
2022-04-09 23:16:53 +0900

    # 실행 파일 작성

--- src/main/java/org/programmers/kdtspring/KdtSpringApplication.java
+++ src/main/java/org/programmers/kdtspring/KdtSpringApplication.java
@@ -2,12 +2,14 @@ package org.programmers.kdtspring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class KdtSpringApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(KdtSpringApplication.class, args);
-	}
+    public static void main(String[] args) {
+        ApplicationContext ac = SpringApplication.run(VoucherManagement.class, args);
+        ac.getBean(VoucherManagement.class).run();
+    }
 
 }
