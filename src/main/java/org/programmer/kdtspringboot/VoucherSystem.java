7d8f085f17987b283ff97de914ca9d2da5ecf342
unnokid <kimki0609@gmail.com>
2022-04-10 00:14:46 +0900

    Update VoucherSystem
    Add `isNumber()` and Logger

--- src/main/java/org/programmer/kdtspringboot/VoucherSystem.java
+++ src/main/java/org/programmer/kdtspringboot/VoucherSystem.java
@@ -2,6 +2,8 @@ package org.programmer.kdtspringboot;
 
 import org.programmer.kdtspringboot.voucher.Voucher;
 import org.programmer.kdtspringboot.voucher.VoucherService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
@@ -12,6 +14,7 @@ public class VoucherSystem {
 
     private final Console console;
     private final VoucherService voucherService;
+    private static final Logger logger = LoggerFactory.getLogger(VoucherSystem.class);
 
     public VoucherSystem(Console console, VoucherService voucherService) {
         this.console = console;
@@ -22,18 +25,22 @@ public class VoucherSystem {
 
         while (true) {
             console.menu();
-            String inputString = console.input("선택 :").toLowerCase();
+            String inputString = console.input("선택: ").toLowerCase();
             switch (inputString) {
                 case "exit":
+                    logger.info("exit 입력");
                     console.exit();
                     return;
                 case "list":
+                    logger.info("list 입력");
                     showVoucherList();
                     break;
                 case "create":
+                    logger.info("create 입력");
                     createVoucher();
                     break;
                 default:
+                    logger.warn("잘못 입력");
                     console.print("제대로 입력해주세요");
             }
         }
@@ -50,15 +57,37 @@ public class VoucherSystem {
 
     private void createVoucher() {
         console.choice();
-        String inputString = console.input("선택 :").toLowerCase();
-        int value = Integer.parseInt(console.input("할인값 :"));
+
+        String inputString = console.input("선택: ").toLowerCase();
+        logger.info("Voucher 종류 선택: " + inputString);
+        String discountString = console.input("할인값: ");
+        if (!isNumber(discountString)) {
+            console.print("할인값을 잘못 입력하셨습니다");
+            logger.warn("할인값 잘못 입력");
+            return;
+        }
+        int discount = Integer.parseInt(discountString);
+        logger.info("할인값 입력: " + discountString);
+
+        Voucher voucher;
         if (inputString.equals("amount")) {
-            voucherService.createFixedAmountVoucher(UUID.randomUUID(), value);
+            voucher = voucherService.createFixedAmountVoucher(UUID.randomUUID(), discount);
         } else if (inputString.equals("percent")) {
-            voucherService.createPercentDiscountVoucher(UUID.randomUUID(), value);
+            voucher = voucherService.createPercentDiscountVoucher(UUID.randomUUID(), discount);
         } else {
             console.print("잘못입력하셨습니다. 처음부터 다시 해주세요");
+            return;
         }
+        logger.info("voucher 생성(" + voucher.getVoucherId() + "," + voucher.getValue() + ")");
+    }
+
+    private boolean isNumber(String str) {
+        for (int i = 0; i < str.length(); i++) {
+            if (!Character.isDigit(str.charAt(i))) {
+                return false;
+            }
+        }
+        return true;
     }
 
 }
