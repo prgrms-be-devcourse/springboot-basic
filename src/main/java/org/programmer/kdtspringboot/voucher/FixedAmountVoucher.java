28a220bd04001586ae5cf40fa326defaee20f8a7
unnokid <kimki0609@gmail.com>
2022-04-09 21:49:44 +0900

    Add FixedAmountVoucher

---
+++ src/main/java/org/programmer/kdtspringboot/voucher/FixedAmountVoucher.java
@@ -0,0 +1,29 @@
+package org.programmer.kdtspringboot.voucher;
+
+import java.util.UUID;
+
+public class FixedAmountVoucher implements Voucher{
+
+    private final UUID voucherId;
+    private final long amount;
+
+    public FixedAmountVoucher(UUID voucherId, long amount) {
+        this.voucherId = voucherId;
+        this.amount = amount;
+    }
+
+    @Override
+    public UUID getVoucherId() {
+        return voucherId;
+    }
+
+    @Override
+    public long getValue() {
+        return amount;
+    }
+
+    @Override
+    public long discount(long beforeDiscount) {
+        return beforeDiscount - amount;
+    }
+}
