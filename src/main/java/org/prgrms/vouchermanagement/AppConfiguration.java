df7400337fbc7c31475fc5cba7ddf4a44c2c9133
ecvheo1 <ecvheo@naver.com>
2022-04-09 21:46:41 +0900

    feat : YAML property 생성, 설정

---
+++ src/main/java/org/prgrms/vouchermanagement/AppConfiguration.java
@@ -0,0 +1,10 @@
+package org.prgrms.vouchermanagement;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource(value = "classpath:application.yaml")
+public class AppConfiguration {
+
+}
