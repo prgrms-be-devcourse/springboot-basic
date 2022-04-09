eed961653af76dd5dff6c1ac4674444c0ab6e1d4
ㅎㅈ <hgkimmme9@gmail.com>
2022-04-09 23:56:56 +0900

    프로젝트 생성

---
+++ src/main/java/org/prgrms/voucherprgrms/VoucherPrgrmsApplication.java
@@ -0,0 +1,13 @@
+package org.prgrms.voucherprgrms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoucherPrgrmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoucherPrgrmsApplication.class, args);
+	}
+
+}
