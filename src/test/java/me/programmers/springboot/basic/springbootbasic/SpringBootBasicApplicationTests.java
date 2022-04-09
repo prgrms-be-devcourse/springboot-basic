b139dca3f102a2423ec37af2fa44571b6ab3429b
Yanjoo <hanjub3@gmail.com>
2022-04-09 09:49:36 +0900

    initial commit

---
+++ src/test/java/me/programmers/springboot/basic/springbootbasic/SpringBootBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package me.programmers.springboot.basic.springbootbasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
