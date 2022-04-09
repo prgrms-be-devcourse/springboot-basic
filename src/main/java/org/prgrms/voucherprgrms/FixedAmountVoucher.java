ad61e6929848aad3f09702ba96613003aec0d18d
ㅎㅈ <hgkimmme9@gmail.com>
2022-04-10 00:15:45 +0900

    docs : voucher interface

---
+++ src/main/java/org/prgrms/voucherprgrms/FixedAmountVoucher.java
@@ -0,0 +1,19 @@
+package org.prgrms.voucherprgrms;
+
+import java.util.UUID;
+
+public class FixedAmountVoucher implements Voucher {
+
+    private final UUID voucherId;
+    private final long amount;
+
+    public FixedAmountVoucher(UUID voucherId, long amount) {
+        this.voucherId = voucherId;
+        this.amount = amount;
+    }
+
+    @Override
+    public UUID getVoucherId() {
+        return voucherId;
+    }
+}
---
+++ src/main/java/org/prgrms/voucherprgrms/Voucher.java
@@ -0,0 +1,7 @@
+package org.prgrms.voucherprgrms;
+
+import java.util.UUID;
+
+public interface Voucher {
+    UUID getVoucherId();
+}
