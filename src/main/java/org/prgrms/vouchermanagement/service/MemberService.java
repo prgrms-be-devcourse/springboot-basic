460e83e9a8233fc66a070916d74550ae374ca28e
ecvheo1 <ecvheo@naver.com>
2022-04-09 21:49:09 +0900

    feat : 블랙 리스트 조회 구조 구현

---
+++ src/main/java/org/prgrms/vouchermanagement/service/MemberService.java
@@ -0,0 +1,4 @@
+package org.prgrms.vouchermanagement.service;
+
+public interface MemberService {
+}
---
+++ src/main/java/org/prgrms/vouchermanagement/service/MemberServiceImpl.java
@@ -0,0 +1,8 @@
+package org.prgrms.vouchermanagement.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MemberServiceImpl implements MemberService{
+
+}
