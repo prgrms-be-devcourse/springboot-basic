45e18defec6f1e9a048ae7f42948b2d1e1a2e41f
unnokid <kimki0609@gmail.com>
2022-04-09 21:26:24 +0900

    Add VoucherSystem

--- src/main/java/org/programmer/kdtspringboot/KdtSpringBootApplication.java
+++ src/main/java/org/programmer/kdtspringboot/KdtSpringBootApplication.java
@@ -2,12 +2,14 @@ package org.programmer.kdtspringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class KdtSpringBootApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(KdtSpringBootApplication.class, args);
+        ApplicationContext applicationContext = SpringApplication.run(KdtSpringBootApplication.class, args);
+        applicationContext.getBean(VoucherSystem.class).run();
     }
 
 }
---
+++ src/main/java/org/programmer/kdtspringboot/VoucherSystem.java
@@ -0,0 +1,12 @@
+package org.programmer.kdtspringboot;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class VoucherSystem implements Runnable{
+
+    @Override
+    public void run() {
+        
+    }
+}
