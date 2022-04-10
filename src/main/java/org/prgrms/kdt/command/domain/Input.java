a863500c739fb4ff7618a31f318a7ce8d2eb16e6
ordilov <ordilov@gmail.com>
2022-04-10 10:40:01 +0900

    feat: 콘솔을 추가하라

---
+++ src/main/java/org/prgrms/kdt/command/domain/Input.java
@@ -0,0 +1,10 @@
+package org.prgrms.kdt.command.domain;
+
+public interface Input {
+
+  String read();
+
+  Integer readInt();
+
+  Long readLong();
+}
---
+++ src/main/java/org/prgrms/kdt/command/domain/Output.java
@@ -0,0 +1,8 @@
+package org.prgrms.kdt.command.domain;
+
+public interface Output {
+
+  void printLine(String message);
+
+  void printMenu();
+}
