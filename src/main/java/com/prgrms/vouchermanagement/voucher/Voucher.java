dc7bf40aebac1db2ccf3682307d77c230272296a
jeonghaemin <jhm0828@gmail.com>
2022-04-11 02:04:00 +0900

    예외 처리 로직 추가
    - FixedAmountVoucher를 생성할 때 amount에 0보다 작은 값이 입력된 경우 예외 처리

--- src/main/java/com/prgrms/vouchermanagement/voucher/Voucher.java
+++ src/main/java/com/prgrms/vouchermanagement/voucher/Voucher.java
@@ -4,6 +4,8 @@ import com.prgrms.vouchermanagement.VoucherType;
 
 import java.util.UUID;
 
+import static com.prgrms.vouchermanagement.VoucherType.*;
+
 public interface Voucher {
     long discount(long beforeDiscountPrice);
 
@@ -14,13 +16,13 @@ public interface Voucher {
      * @throws IllegalArgumentException : Voucher 타입이 PercentDiscount 인 경우 0~100% 의 범위를 벗어나는 수가 입력되면 예외를 던진다.
      */
     static Voucher createVoucher(VoucherType type, long amount) throws IllegalArgumentException {
+        checkAmountRightRange(type, amount);
 
-        if (type == VoucherType.FIXED_DISCOUNT) {
+        if (type == FIXED_DISCOUNT) {
             return new FixedAmountVoucher(amount);
         }
 
-        if (type == VoucherType.PERCENT_DISCOUNT) {
-            checkAmountRightRange(amount);
+        if (type == PERCENT_DISCOUNT) {
             return new PercentDiscountVoucher(amount);
         }
 
@@ -28,10 +30,15 @@ public interface Voucher {
     }
 
     /**
-     * Voucher 타입이 PercentDiscount 인 경우 0~100 범위의 수가 입력되었는지 확인한다.
+     * amount 값이 올바른 범위인지 확인한다.
+     * - FixedDiscountVoucher : 0보다 작은 값이 입력되면 예외
+     * - PercentDiscountVoucher : 0~100 범위를 벗어나는 값이 입력되면 예외
      */
-    private static void checkAmountRightRange(long amount) throws IllegalArgumentException {
-        if (amount < 0 || amount > 100)
+    private static void checkAmountRightRange(VoucherType type, long amount) throws IllegalArgumentException {
+        if (amount < 0) throw new IllegalArgumentException("0보다 작은 값은 입력할 수 없습니다.");
+
+        if (type == PERCENT_DISCOUNT && amount > 100) {
             throw new IllegalArgumentException("Percent discount 는 0 ~ 100% 범위의 값을 입력해주세요.");
+        }
     }
 }
