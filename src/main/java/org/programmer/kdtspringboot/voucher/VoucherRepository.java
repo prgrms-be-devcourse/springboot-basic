37684b8aaec5d08c254cd514ba7852a02314d77b
unnokid <kimki0609@gmail.com>
2022-04-10 20:28:04 +0900

    Change `insert()` and `findAll` return type

--- src/main/java/org/programmer/kdtspringboot/voucher/VoucherRepository.java
+++ src/main/java/org/programmer/kdtspringboot/voucher/VoucherRepository.java
@@ -1,11 +1,10 @@
 package org.programmer.kdtspringboot.voucher;
 
+import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.UUID;
 
 public interface VoucherRepository {
-    Optional<Voucher> findById(UUID voucherId);
-    Voucher insert(Voucher voucher);
-    Map<UUID, Voucher> findAll();
+    void insert(Voucher voucher);
+    List<Voucher> findAll();
 }
