f15295560f12f85e9fd69928f1ebfdd52801566e
San Kim <waterfog9580@gmail.com>
2022-04-10 15:32:47 +0900

    Controller 클래스 추가

--- src/main/java/com/waterfogsw/voucher/VoucherController.java
+++ src/main/java/com/waterfogsw/voucher/VoucherController.java
@@ -1,4 +1,11 @@
 package com.waterfogsw.voucher;
 
-public class VoucherController {
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+
+public class VoucherController implements ApplicationRunner {
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+
+    }
 }
