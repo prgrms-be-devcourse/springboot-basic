75f63402b8193b783877757b9dfb66a0ed75362e
Gosh95 <goshk1995@gmail.com>
2022-04-11 02:49:28 +0900

    프로파일 dev 세팅 후 파일 레포 정상 동작

--- weekly-mission/src/main/java/org/prgrms/weeklymission/WeeklyMissionApplication.java
+++ weekly-mission/src/main/java/org/prgrms/weeklymission/WeeklyMissionApplication.java
@@ -1,13 +1,16 @@
 package org.prgrms.weeklymission;
 
+import org.prgrms.weeklymission.application.Application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WeeklyMissionApplication {
-
 	public static void main(String[] args) {
-		SpringApplication.run(WeeklyMissionApplication.class, args);
+		var springApplication = new SpringApplication(WeeklyMissionApplication.class);
+		springApplication.setAdditionalProfiles("dev");
+		var applicationContext = springApplication.run(args);
+		var app = applicationContext.getBean(Application.class);
+		app.run();
 	}
-
 }
