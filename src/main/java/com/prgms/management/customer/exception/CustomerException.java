64290fd463b822d950c785621d55f49fc9f74a6c
moosongsong <songe08@gmail.com>
2022-04-11 04:17:57 +0900

    feat: 고객 정보 조회 시, 발생 가능한 예외 추가

---
+++ src/main/java/com/prgms/management/customer/exception/CustomerException.java
@@ -0,0 +1,9 @@
+package com.prgms.management.customer.exception;
+
+public class CustomerException extends Exception{
+    private static final long serialVersionUID = 7544775631058799907L;
+
+    protected CustomerException(String message) {
+        super(message);
+    }
+}
