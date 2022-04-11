e9bb673aa56a14e291b04883f2394895965eee2b
Yongsu Kang <dksktm15@naver.com>
2022-04-11 16:13:21 +0900

    BlackList Service 기능 구현

---
+++ kdt-spring-voucher/src/main/java/org/prgms/kdtspringvoucher/blackList/service/BlackListService.java
@@ -0,0 +1,30 @@
+package org.prgms.kdtspringvoucher.blackList.service;
+
+import org.prgms.kdtspringvoucher.blackList.domain.BlackList;
+import org.prgms.kdtspringvoucher.blackList.repository.BlackListRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BlackListService {
+    private static final Logger logger = LoggerFactory.getLogger(BlackListService.class);
+    private final BlackListRepository blackListRepository;
+
+    public BlackListService(BlackListRepository blackListRepository) {
+        this.blackListRepository = blackListRepository;
+    }
+
+    public void showAllBlackList() {
+        List<BlackList> blackList = blackListRepository.findAll();
+        if (blackList.isEmpty()) {
+            logger.error("Have not any vouchers in list => {}", blackList);
+            System.out.println("No blackList.....\n");
+        } else {
+            logger.info("Succeed show all blackList in repository");
+            blackList.forEach(System.out::println);
+        }
+    }
+}
