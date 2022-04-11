37e8f9e09e634e4005be1fa95925ba5eb755e6e3
HunkiKim <gnsrl76@naver.com>
2022-04-12 00:14:58 +0900

    feat : add voucher and menu enum
    입력을 위한 바우처,메뉴 ENUM 추가

---
+++ src/main/java/org/prgrms/kdt/shop/enums/MenuStatus.java
@@ -0,0 +1,18 @@
+package org.prgrms.kdt.shop.enums;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+public enum MenuStatus {
+    LIST("list"), EXIT("exit"), CREATE("create");
+
+    private final String inputMenu;
+
+    MenuStatus(String inputMenu) {
+        this.inputMenu = inputMenu;
+    }
+
+    public static MenuStatus find(String inputMenu) {
+        return Arrays.stream(values()).filter(menuStatus -> menuStatus.inputMenu.equals(inputMenu)).findAny().orElseThrow(( ) -> new IllegalArgumentException("\n[ERROR] 제대로 된 문자를 입력하세요."));
+    }
+}
---
+++ src/main/java/org/prgrms/kdt/shop/enums/VoucherStatus.java
@@ -0,0 +1,20 @@
+package org.prgrms.kdt.shop.enums;
+
+import org.prgrms.kdt.shop.domain.Voucher;
+import org.prgrms.kdt.shop.service.VoucherService;
+
+import java.util.Arrays;
+
+public enum VoucherStatus {
+    FIXED_AMOUNT("1"), PERCENT_DISCOUNT("2");
+
+    private String inputVoucher;
+
+    VoucherStatus(String inputVoucher) {
+        this.inputVoucher = inputVoucher;
+    }
+
+    public static VoucherStatus find(String inputVoucher) {
+        return Arrays.stream(values()).filter(voucherStatus -> voucherStatus.inputVoucher.equals(inputVoucher)).findAny().orElseThrow(( ) -> new IllegalArgumentException("\n[ERROR] 제대로 된 문자를 입력하세요."));
+    }
+}
