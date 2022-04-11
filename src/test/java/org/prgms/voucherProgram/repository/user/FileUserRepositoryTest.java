96ca2a3b11676ae1035a756934795596ae2a62cf
yuminhwan <ymh7532@icloud.com>
2022-04-12 01:19:50 +0900

    refactor: 테스트 수정 및 테스트 케이스 추가

--- src/test/java/org/prgms/voucherProgram/repository/user/FileUserRepositoryTest.java
+++ src/test/java/org/prgms/voucherProgram/repository/user/FileUserRepositoryTest.java
@@ -1,19 +1,25 @@
 package org.prgms.voucherProgram.repository.user;
 
-import org.assertj.core.api.Assertions;
+import static org.assertj.core.api.Assertions.*;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.core.io.ClassPathResource;
 
 class FileUserRepositoryTest {
 
-    FileUserRepository fileUserRepository = new FileUserRepository(
-        new ClassPathResource("file/customer_blacklist.csv"));
-
-    @DisplayName("파일에 저장되어 있는 모든 User를 List형으로 반환한다.")
+    @DisplayName("파일에 저장되어 있는 모든 blackUser를 List형으로 반환한다.")
     @Test
     void findBlackUsers_ReturnBlackUsers() {
-        Assertions.assertThat(fileUserRepository.findBlackUsers()).hasSize(3)
+        FileUserRepository fileUserRepository = new FileUserRepository("file/customer_blacklist.csv");
+        assertThat(fileUserRepository.findBlackUsers()).hasSize(3)
             .extracting("name").contains("hwan", "jin", "pobi");
+    }
+
+    @DisplayName("잘못된 파일일 경우 예외를 발생한다.")
+    @Test
+    void findBlackUsers_WrongFile_ThrowException() {
+        assertThatThrownBy(() -> new FileUserRepository("file/customer_black.csv"))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("[ERROR] 올바른 유저 파일이 아닙니다.");
     }
 }
