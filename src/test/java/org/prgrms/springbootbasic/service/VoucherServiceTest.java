473dcfd7cdd9313d89fde2099fe29eda31eb279c
hyuk0309 <bahngeh@naver.com>
2022-04-11 18:34:02 +0900

    test : VoucherServiceTest test 추가

--- src/test/java/org/prgrms/springbootbasic/service/VoucherServiceTest.java
+++ src/test/java/org/prgrms/springbootbasic/service/VoucherServiceTest.java
@@ -1,23 +1,23 @@
 package org.prgrms.springbootbasic.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.prgrms.springbootbasic.entity.Voucher;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.prgrms.springbootbasic.repository.MemoryVoucherRepository;
 
 class VoucherServiceTest {
 
-    VoucherService voucherService;
-    MemoryVoucherRepository memoryVoucherRepository;
+    MemoryVoucherRepository memoryVoucherRepository = new MemoryVoucherRepository();
+    VoucherService voucherService = new VoucherService(memoryVoucherRepository);
 
     @BeforeEach
     void init() {
-        memoryVoucherRepository = new MemoryVoucherRepository();
-        voucherService = new VoucherService(memoryVoucherRepository);
+        memoryVoucherRepository.removeAll();
     }
 
     @DisplayName("FixedAmountVoucher 만들기 테스트")
@@ -30,8 +30,19 @@ class VoucherServiceTest {
         voucherService.createFixedAmountVoucher(amount);
 
         //then
-        assertThat(memoryVoucherRepository.getVoucherTotalNumber())
+        assertThat(memoryVoucherRepository.findAll().size())
             .isEqualTo(1);
+    }
+
+    @DisplayName("FixedAmountVoucher 만들기 실패")
+    @ParameterizedTest
+    @ValueSource(longs = {0, -1, 100000})
+    void createFixedAmountVoucherFail(long amount) {
+        //given
+        //when
+        //then
+        assertThatThrownBy(() -> voucherService.createFixedAmountVoucher(amount))
+            .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("PercentAmountVoucher 만들기 테스트")
@@ -44,8 +55,19 @@ class VoucherServiceTest {
         voucherService.createPercentAmountVoucher(percent);
 
         //then
-        assertThat(memoryVoucherRepository.getVoucherTotalNumber())
+        assertThat(memoryVoucherRepository.findAll().size())
             .isEqualTo(1);
+    }
+
+    @DisplayName("PercentAmountVoucher 만들기 실패")
+    @ParameterizedTest
+    @ValueSource(ints = {0, 101})
+    void createPercentAmountVoucherFail(int percent) {
+        //given
+        //when
+        //then
+        assertThatThrownBy(() -> voucherService.createPercentAmountVoucher(percent))
+            .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("Voucher 전체 조회 테스트")
@@ -56,10 +78,8 @@ class VoucherServiceTest {
         voucherService.createPercentAmountVoucher(10);
 
         //when
-        List<Voucher> vouchers = voucherService.findAll();
-
         //then
-        assertThat(vouchers.size())
+        assertThat(voucherService.findAll().size())
             .isEqualTo(2);
     }
 }
