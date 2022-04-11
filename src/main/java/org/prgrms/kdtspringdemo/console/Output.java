ee8ccd84ebb6fe79878e473a54f30d7cba76754b
dldydgns530@gmail.com <dldydgns530@gmail.com>
2022-04-12 01:17:35 +0900

    사용자 대상 String 출력전용 클래스 구현

---
+++ src/main/java/org/prgrms/kdtspringdemo/console/Output.java
@@ -0,0 +1,30 @@
+package org.prgrms.kdtspringdemo.console;
+
+public class Output {
+
+    public String initMessage() {
+        String message = "=== Voucher Program ===\n" +
+                "Type exit to exit the program.\n" +
+                "Type create to create a new voucher.\n" +
+                "Type list to list all vouchers.\n" +
+                "write one of three word : (exit, create, list)";
+
+        return message;
+    }
+
+    public String createListMessage() {
+        String message = "=== Create Voucher ===\n" +
+                "FixedAmountVoucher = 1\n" +
+                "PercentDiscountVoucher = 2\n" +
+                "Write number or voucher name : ";
+
+        return message;
+    }
+
+    public String showAllMessage() {
+        String message = "=== Show all Voucher ===\n" +
+                "These are all vouchers list";
+
+        return message;
+    }
+}
