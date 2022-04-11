74f5146fe961dd14ca71cf98a773d0bcf69f2ddd
HunkiKim <gnsrl76@naver.com>
2022-04-12 00:03:25 +0900

    feat : add insert and findByAll(list) to voucherservice
    VoucherService에 리스트 전체 조회와 레포지토리에 추가하는 insert기능 설정

--- src/main/java/org/prgrms/kdt/shop/service/VoucherService.java
+++ src/main/java/org/prgrms/kdt/shop/service/VoucherService.java
@@ -2,12 +2,19 @@ package org.prgrms.kdt.shop.service;
 
 import org.prgrms.kdt.shop.domain.Voucher;
 import org.prgrms.kdt.shop.repository.VoucherRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+@Service
 public class VoucherService {
     private final VoucherRepository voucherRepository;
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     public VoucherService(VoucherRepository voucherRepository) {
         this.voucherRepository = voucherRepository;
@@ -15,5 +22,20 @@ public class VoucherService {
 
     public Optional<Voucher> getVoucher(UUID voucherId) {
         return Optional.ofNullable(voucherRepository.findById(voucherId).orElseThrow(( ) -> new RuntimeException("Can not find a voucher for%s".formatted(voucherId))));
+    }
+
+    public Voucher insert(Voucher voucher) {
+        voucherRepository.insert(voucher);
+        logger.info("바우처 생성 -> {}", voucher);
+        return voucher;
+    }
+
+    public void findByAll( ) {
+        List<Voucher> voucherList = voucherRepository.findByAll();
+        if (voucherList.isEmpty()) {
+            System.out.println("바우처가 없습니다.");
+        } else {
+            voucherList.forEach(s -> System.out.println("Voucher Id : " + s.getVoucherId() + "  Discount Info : " + s.getAmount()));
+        }
     }
 }
