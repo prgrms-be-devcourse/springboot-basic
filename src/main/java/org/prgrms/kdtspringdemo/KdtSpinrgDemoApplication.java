cabd637506fecabb357c2642ee91b43486a7e7cf
hayoung <hyng0115@gmail.com>
2022-04-11 09:25:11 +0900

    프로젝트 생성

---
+++ src/main/java/org/prgrms/kdtspringdemo/KdtSpinrgDemoApplication.java
@@ -0,0 +1,13 @@
+package org.prgrms.kdtspringdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KdtSpinrgDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KdtSpinrgDemoApplication.class, args);
+	}
+
+}
