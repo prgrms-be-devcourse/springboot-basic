d5c25c8dc60b418bb7df33714bae99d3a577f02f
hyuk0309 <bahngeh@naver.com>
2022-04-11 16:38:13 +0900

    test : MemoryVoucherRepositoryTest test 추가

--- src/test/java/org/prgrms/springbootbasic/repository/MemoryVoucherRepositoryTest.java
+++ src/test/java/org/prgrms/springbootbasic/repository/MemoryVoucherRepositoryTest.java
@@ -20,17 +20,52 @@ class MemoryVoucherRepositoryTest {
         memoryVoucherRepository.removeAll();
     }
 
-    @DisplayName("store 테스트")
+    @DisplayName("FixedAmountVoucher 저장 테스트")
+    @Test
+    void saveFixedAmountVoucher() {
+        //given
+        Voucher voucher1 = new FixedAmountVoucher(UUID.randomUUID(), 10L);
+        Voucher voucher2 = new FixedAmountVoucher(UUID.randomUUID(), 10L);
+
+        //when
+        memoryVoucherRepository.save(voucher1);
+        memoryVoucherRepository.save(voucher2);
+
+        //then
+        assertThat(memoryVoucherRepository.findAll())
+            .containsExactlyInAnyOrder(voucher1, voucher2);
+    }
+
+    @DisplayName("PercentAmountVoucher 저장 테스트")
+    @Test
+    void savePercentAmountVoucher() {
+        //given
+        Voucher voucher1 = new PercentDiscountVoucher(UUID.randomUUID(), 10);
+        Voucher voucher2 = new PercentDiscountVoucher(UUID.randomUUID(), 20);
+
+        //when
+        memoryVoucherRepository.save(voucher1);
+        memoryVoucherRepository.save(voucher2);
+
+        //then
+        assertThat(memoryVoucherRepository.findAll())
+            .containsExactlyInAnyOrder(voucher1, voucher2);
+    }
+
+    @DisplayName("FixedAmountVoucher, PercentAmountVoucher 저장 테스트")
     @Test
     void store() {
         //given
-        Voucher voucher = new FixedAmountVoucher(UUID.randomUUID(), 10L);
+        Voucher fixedAmountVoucher = new FixedAmountVoucher(UUID.randomUUID(), 10L);
+        Voucher percentDiscountVoucher = new PercentDiscountVoucher(UUID.randomUUID(), 20);
 
         //when
-        memoryVoucherRepository.save(voucher);
+        memoryVoucherRepository.save(fixedAmountVoucher);
+        memoryVoucherRepository.save(percentDiscountVoucher);
 
         //then
-        assertThat(memoryVoucherRepository.getVoucherTotalNumber()).isEqualTo(1);
+        assertThat(memoryVoucherRepository.findAll())
+            .containsExactlyInAnyOrder(fixedAmountVoucher, percentDiscountVoucher);
     }
 
     @DisplayName("전체 조회 테스트")
