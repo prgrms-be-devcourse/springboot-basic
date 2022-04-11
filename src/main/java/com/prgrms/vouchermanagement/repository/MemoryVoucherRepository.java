ea87fb708ee9bd03b623a147f508e462f305af57
jeonghaemin <jhm0828@gmail.com>
2022-04-11 23:21:58 +0900

    MemoryVoucherRepository 프로파일 설정 및 로그 추가

--- src/main/java/com/prgrms/vouchermanagement/repository/MemoryVoucherRepository.java
+++ src/main/java/com/prgrms/vouchermanagement/repository/MemoryVoucherRepository.java
@@ -1,22 +1,31 @@
 package com.prgrms.vouchermanagement.repository;
 
 import com.prgrms.vouchermanagement.voucher.Voucher;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
 @Repository
+@Profile("memory")
 public class MemoryVoucherRepository implements VoucherRepository {
+
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
 
     private final Map<UUID, Voucher> store = new HashMap<>();
 
     @Override
     public void save(Voucher voucher) {
         store.put(voucher.getId(), voucher);
+        log.info("voucher is saved  to memory - {}", voucher);
     }
 
     @Override
     public List<Voucher> findAll() {
-        return new ArrayList<>(store.values());
+        ArrayList<Voucher> vouchers = new ArrayList<>(store.values());
+        log.info("find all vouchers from memory. size={}", vouchers.size());
+        return vouchers;
     }
 }
