ef0e9d70f703376527af74a0c3af6ef4977e2f87
julie0005 <julie0005@ajou.ac.kr>
2022-04-11 22:49:23 +0900

    init : 프로젝트 구조 초기화

---
+++ src/main/java/org/prgrms/voucherapp/VoucherApplication.java
@@ -0,0 +1,13 @@
+package org.prgrms.voucherapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoucherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VoucherApplication.class, args);
+    }
+
+}
