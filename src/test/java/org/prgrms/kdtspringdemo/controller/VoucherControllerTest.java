bcf89079c30c1de86b88612b63495810ad2ac750
hayoung <hyng0115@gmail.com>
2022-04-11 16:25:18 +0900

    테스트 디렉토리 구조 변경

---
+++ src/test/java/org/prgrms/kdtspringdemo/controller/VoucherControllerTest.java
@@ -0,0 +1,31 @@
+package org.prgrms.kdtspringdemo.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class VoucherControllerTest {
+    @Test
+    @DisplayName("바우처 저장을 테스트합니다.")
+    public void save(){
+
+    }
+
+    @Test
+    @DisplayName("PercentDiscountVoucher의 할인율이 100을 넘는다면 예외가 발생합니다.")
+    public void saveWithInvalidValue1(){
+
+    }
+
+    @Test
+    @DisplayName("PercentDiscountVoucher의 할인 금액이 마이너스라면 예외가 발생합니다.")
+    public void saveWithInvalidValue2(){
+
+    }
+
+    @Test
+    @DisplayName("바우처 전체 조회를 테스트합니다.")
+    public void findAll(){
+
+    }
+}
+
