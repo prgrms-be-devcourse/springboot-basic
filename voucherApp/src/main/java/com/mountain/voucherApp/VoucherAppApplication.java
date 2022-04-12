caf2645c335f3e46bdf315b7e92885c2b8d5e6a4
taesan <pawer02023@gmail.com>
2022-04-12 23:02:46 +0900

    fix: 피드백1 - properties 불변으로 변경
    properties 불변으로 변경

--- voucherApp/src/main/java/com/mountain/voucherApp/VoucherAppApplication.java
+++ voucherApp/src/main/java/com/mountain/voucherApp/VoucherAppApplication.java
@@ -3,9 +3,11 @@ package com.mountain.voucherApp;
 import com.mountain.voucherApp.engine.ExecuteEngine;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class VoucherAppApplication {
 
 	public static void main(String[] args) {
--- voucherApp/src/main/java/com/mountain/voucherApp/properties/FileRepositoryProperties.java
+++ voucherApp/src/main/java/com/mountain/voucherApp/properties/FileRepositoryProperties.java
@@ -1,28 +1,24 @@
 package com.mountain.voucherApp.properties;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.context.properties.ConstructorBinding;
 
-@Configuration
 @ConfigurationProperties(prefix = "file-repository")
+@ConstructorBinding
 public class FileRepositoryProperties {
 
-    private String dir;
-    private String fileName;
+    private final String dir;
+    private final String fileName;
+
+    public FileRepositoryProperties(String dir, String fileName) {
+        this.dir = dir;
+        this.fileName = fileName;
+    }
 
     public String getDir() {
         return dir;
     }
-
-    public void setDir(String dir) {
-        this.dir = dir;
-    }
-
     public String getFileName() {
         return fileName;
-    }
-
-    public void setFileName(String fileName) {
-        this.fileName = fileName;
     }
 }
