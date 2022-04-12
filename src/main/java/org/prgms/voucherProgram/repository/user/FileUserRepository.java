4538552770bd201464571a15d4b5cc95b90f13bf
yuminhwan <ymh7532@icloud.com>
2022-04-12 10:50:40 +0900

    refactor: 캐싱 방식 변경

--- src/main/java/org/prgms/voucherProgram/repository/user/FileUserRepository.java
+++ src/main/java/org/prgms/voucherProgram/repository/user/FileUserRepository.java
@@ -5,7 +5,9 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
 
 import org.prgms.voucherProgram.entity.user.User;
 import org.slf4j.Logger;
@@ -20,10 +22,11 @@ public class FileUserRepository implements UserRepository {
     private static final String ERROR_WRONG_FILE = "[ERROR] 올바른 유저 파일이 아닙니다.";
     private static final int USER_ID_INDEX = 0;
     private static final int USER_NAME_INDEX = 1;
-    private static final List<User> blackUsers = new ArrayList<>();
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
     private final String userFilePath;
+    private final Map<UUID, User> storage = new ConcurrentHashMap<>();
 
     public FileUserRepository(@Value("${file.path.blacklist}") String userFilePath) {
         this.userFilePath = userFilePath;
@@ -33,24 +36,24 @@ public class FileUserRepository implements UserRepository {
     private void readBlackUsers() {
         try (BufferedReader bufferedReader = new BufferedReader(
             new InputStreamReader(new ClassPathResource(userFilePath).getInputStream()))) {
-            addBlackUsers(blackUsers, bufferedReader);
+            addBlackUsers(storage, bufferedReader);
         } catch (IOException e) {
             logger.error("Fail to find a blacklist file");
             throw new IllegalArgumentException(ERROR_WRONG_FILE);
         }
     }
 
-    private void addBlackUsers(List<User> users, BufferedReader bufferedReader) throws IOException {
+    private void addBlackUsers(Map<UUID, User> storage, BufferedReader bufferedReader) throws IOException {
         String line;
         while ((line = bufferedReader.readLine()) != null) {
             String[] splitLine = line.split(DELIMITER);
             UUID userId = UUID.fromString(splitLine[USER_ID_INDEX].trim());
             String userName = splitLine[USER_NAME_INDEX].trim();
-            users.add(new User(userId, userName));
+            storage.put(userId, new User(userId, userName));
         }
     }
 
     public List<User> findBlackUsers() {
-        return blackUsers;
+        return new ArrayList<>(storage.values());
     }
 }
