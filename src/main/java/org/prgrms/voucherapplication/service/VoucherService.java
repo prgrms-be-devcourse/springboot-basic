81eb06d276aa223f18fa355702e1be38fe557565
HyeonseoJUNG <windymay1920@gmail.com>
2022-04-13 08:37:15 +0900

    Feat: service 구현

---
+++ src/main/java/org/prgrms/voucherapplication/service/VoucherService.java
@@ -0,0 +1,50 @@
+package org.prgrms.voucherapplication.service;
+
+import org.prgrms.voucherapplication.entity.FixedAmountVoucher;
+import org.prgrms.voucherapplication.entity.PercentDiscountVoucher;
+import org.prgrms.voucherapplication.entity.Voucher;
+import org.prgrms.voucherapplication.exception.NoSuchVoucherException;
+import org.prgrms.voucherapplication.repository.VoucherRepository;
+import org.prgrms.voucherapplication.view.io.VoucherType;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class VoucherService {
+    private VoucherRepository voucherRepository;
+
+    public VoucherService(VoucherRepository voucherRepository) {
+        this.voucherRepository = voucherRepository;
+    }
+
+    public void saveVoucher(Voucher voucher) {
+        voucherRepository.insert(voucher);
+    }
+
+    public Voucher getVoucher(UUID voucherId) {
+        return voucherRepository.findById(voucherId).orElseThrow(() -> new NoSuchVoucherException(voucherId));
+    }
+
+    public List<Voucher> getAllVoucher() {
+        return voucherRepository.findAll();
+    }
+
+    public Voucher createVoucher(VoucherType voucherType, long discountValue) {
+        UUID voucherId = UUID.randomUUID();
+        if (voucherType.equals(VoucherType.FixedAmount)) {
+            return createFixedAmountVoucher(voucherId, discountValue);
+        } else {
+            return createPercentDiscountVoucher(voucherId, discountValue);
+        }
+    }
+
+    private FixedAmountVoucher createFixedAmountVoucher(UUID voucherId, long amount) {
+        return new FixedAmountVoucher(voucherId, amount);
+    }
+
+    private PercentDiscountVoucher createPercentDiscountVoucher(UUID voucherId, long percent) {
+        return new PercentDiscountVoucher(voucherId, percent);
+    }
+}
