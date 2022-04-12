12ec366a68c91012dbd379c63064b49260c9349f
Juhan <juhan211@naver.com>
2022-04-13 02:29:41 +0900

    테스트케이스 추가:  discount 적용과 만료일자 이후,만료일자 이전

--- src/test/java/com/manager/voucher/domain/FixedDiscountVoucherTest.java
+++ src/test/java/com/manager/voucher/domain/FixedDiscountVoucherTest.java
@@ -3,6 +3,8 @@ package com.manager.voucher.domain;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDateTime;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class FixedDiscountVoucherTest {
@@ -11,19 +13,33 @@ class FixedDiscountVoucherTest {
 
     @Test
     public void 확인FixedDiscountVoucher(){
-        voucher = new FixedDiscountVoucher(52000);
+        voucher = new FixedDiscountVoucher(52000, LocalDateTime.now().plusMonths(1));
         assertEquals(voucher.getClass().getSimpleName(),"FixedDiscountVoucher");
     }
 
     @Test
     public void toString함수확인(){
-        voucher = new FixedDiscountVoucher(5000);
+        voucher = new FixedDiscountVoucher(5000, LocalDateTime.now().plusMonths(1));
         assertEquals(voucher.toString(), "fixed Amount: 5000");
     }
 
     @Test
-    public void 바우처만료일지남(){
-        voucher = new FixedDiscountVoucher(15000);
+    public void 바우처만료일안지남(){
+        voucher = new FixedDiscountVoucher(15000, LocalDateTime.now().plusMonths(1));
         assertEquals(false,voucher.isExpired());
+    }
+
+    @Test
+    public void 바우처만료일지남(){
+        voucher = new FixedDiscountVoucher(12000,LocalDateTime.now().minusMonths(1));
+        assertEquals(true,voucher.isExpired());
+    }
+
+    @Test
+    public void 할인적용Amount(){
+        Item item = new Item(10000);
+        voucher = new FixedDiscountVoucher(400,LocalDateTime.now().plusMonths(1));
+        voucher.applyDiscountTo(item);
+        assertEquals(9600,item.getPrice());
     }
 }
--- src/test/java/com/manager/voucher/domain/PercentDiscountVoucherTest.java
+++ src/test/java/com/manager/voucher/domain/PercentDiscountVoucherTest.java
@@ -2,6 +2,8 @@ package com.manager.voucher.domain;
 
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDateTime;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PercentDiscountVoucherTest {
@@ -10,19 +12,34 @@ class PercentDiscountVoucherTest {
 
     @Test
     public void 확인FixedDiscountVoucher(){
-        voucher = new PercentDiscountVoucher(20);
+        voucher = new PercentDiscountVoucher(20, LocalDateTime.now().plusMonths(1));
         assertEquals(voucher.getClass().getSimpleName(),"PercentDiscountVoucher");
     }
 
     @Test
     public void toString함수확인(){
-        voucher = new PercentDiscountVoucher(50);
+        voucher = new PercentDiscountVoucher(50,LocalDateTime.now().plusMonths(1));
         assertEquals(voucher.toString(), "percent discount: 50");
     }
 
     @Test
     public void 바우처만료일지남(){
-        voucher = new PercentDiscountVoucher(40);
+        voucher = new PercentDiscountVoucher(40,LocalDateTime.now().minusMonths(1));
+        assertEquals(true,voucher.isExpired());
+    }
+
+    @Test
+    public void 바우처만료일안지남(){
+        voucher = new PercentDiscountVoucher(40,LocalDateTime.now().plusMonths(1));
         assertEquals(false,voucher.isExpired());
+    }
+    //바우처 사용에대한 내용
+
+    @Test
+    public void 할인적용Percent(){
+        Item item = new Item(100000);
+        voucher = new PercentDiscountVoucher(40,LocalDateTime.now().plusMonths(1));
+        voucher.applyDiscountTo(item);
+        assertEquals(60000,item.getPrice());
     }
 }
