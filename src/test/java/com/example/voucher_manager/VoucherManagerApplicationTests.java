77554a9f8bbb4940a05a20176fe0221a084f2ba0
Yoonoh <xkakak142@naver.com>
2022-04-13 02:54:27 +0900

    refactor: SpringBootTest Annotation 제거

--- src/test/java/com/example/voucher_manager/VoucherManagerApplicationTests.java
+++ src/test/java/com/example/voucher_manager/VoucherManagerApplicationTests.java
@@ -3,11 +3,6 @@ package com.example.voucher_manager;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class VoucherManagerApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
