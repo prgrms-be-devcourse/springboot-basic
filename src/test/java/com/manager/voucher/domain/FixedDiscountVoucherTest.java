2c3d36a07b81f7bb83f663a4a3fafd8afbf8b0c0
Juhan <juhan211@naver.com>
2022-04-12 19:54:18 +0900

    바우처 생성 관리: 테스트 케이스 수정 추가

--- src/test/java/com/manager/voucher/domain/FixedDiscountVoucherTest.java
+++ src/test/java/com/manager/voucher/domain/FixedDiscountVoucherTest.java
@@ -11,16 +11,19 @@ class FixedDiscountVoucherTest {
 
     @Test
     public void 확인FixedDiscountVoucher(){
+        voucher = new FixedDiscountVoucher(52000);
         assertEquals(voucher.getClass().getSimpleName(),"FixedDiscountVoucher");
     }
 
     @Test
     public void toString함수확인(){
+        voucher = new FixedDiscountVoucher(5000);
         assertEquals(voucher.toString(), "fixed Amount: 5000");
     }
 
     @Test
     public void 바우처만료일지남(){
-        assertEquals(true,voucher.isExpired());
+        voucher = new FixedDiscountVoucher(15000);
+        assertEquals(false,voucher.isExpired());
     }
 }
--- src/test/java/com/manager/voucher/domain/PercentDiscountVoucherTest.java
+++ src/test/java/com/manager/voucher/domain/PercentDiscountVoucherTest.java
@@ -10,16 +10,19 @@ class PercentDiscountVoucherTest {
 
     @Test
     public void 확인FixedDiscountVoucher(){
-        assertEquals(voucher.getClass().getSimpleName(),"FixedDiscountVoucher");
+        voucher = new PercentDiscountVoucher(20);
+        assertEquals(voucher.getClass().getSimpleName(),"PercentDiscountVoucher");
     }
 
     @Test
     public void toString함수확인(){
+        voucher = new PercentDiscountVoucher(50);
         assertEquals(voucher.toString(), "percent discount: 50");
     }
 
     @Test
     public void 바우처만료일지남(){
-        assertEquals(true,voucher.isExpired());
+        voucher = new PercentDiscountVoucher(40);
+        assertEquals(false,voucher.isExpired());
     }
 }
