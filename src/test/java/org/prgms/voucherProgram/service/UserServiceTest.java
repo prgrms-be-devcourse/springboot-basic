5ed7953ef89ec39edbdc83d408879209f4091beb
yuminhwan <ymh7532@icloud.com>
2022-04-12 10:37:16 +0900

    refactor: Mockito 적용

--- src/test/java/org/prgms/voucherProgram/service/UserServiceTest.java
+++ src/test/java/org/prgms/voucherProgram/service/UserServiceTest.java
@@ -1,23 +1,41 @@
 package org.prgms.voucherProgram.service;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.mockito.BDDMockito.*;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.UUID;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.prgms.voucherProgram.entity.user.User;
+import org.prgms.voucherProgram.repository.user.UserRepository;
 
+@ExtendWith(MockitoExtension.class)
 class UserServiceTest {
-    UserService userService = new UserService(() -> Arrays.asList(new User(UUID.randomUUID(), "jin"),
-        new User(UUID.randomUUID(), "hwan"),
-        new User(UUID.randomUUID(), "pobi")));
 
-    @DisplayName("모든 유저를 반환한다.")
+    @Mock
+    private UserRepository userRepository;
+
+    @InjectMocks
+    private UserService userService;
+
+    @DisplayName("모든 블랙리스트를 반환한다.")
     @Test
     void findBlackList_ReturnBlackUsers() {
+        List<User> mockBlackUsers = Arrays.asList(new User(UUID.randomUUID(), "jin"),
+            new User(UUID.randomUUID(), "hwan"),
+            new User(UUID.randomUUID(), "pobi"));
+
+        given(userRepository.findBlackUsers()).willReturn(mockBlackUsers);
+
         assertThat(userService.findBlackList()).hasSize(3)
             .extracting("name").contains("hwan", "jin", "pobi");
+        then(userRepository).should(times(1)).findBlackUsers();
     }
 }
--- src/test/java/org/prgms/voucherProgram/service/VoucherServiceTest.java
+++ src/test/java/org/prgms/voucherProgram/service/VoucherServiceTest.java
@@ -1,6 +1,7 @@
 package org.prgms.voucherProgram.service;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.mockito.BDDMockito.*;
 
 import java.util.Arrays;
 import java.util.List;
@@ -8,6 +9,10 @@ import java.util.UUID;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.prgms.voucherProgram.entity.voucher.FixedAmountVoucher;
 import org.prgms.voucherProgram.entity.voucher.PercentDiscountVoucher;
 import org.prgms.voucherProgram.entity.voucher.Voucher;
@@ -16,24 +21,14 @@ import org.prgms.voucherProgram.exception.WrongDiscountAmountException;
 import org.prgms.voucherProgram.exception.WrongDiscountPercentException;
 import org.prgms.voucherProgram.repository.voucher.VoucherRepository;
 
+@ExtendWith(MockitoExtension.class)
 class VoucherServiceTest {
 
-    VoucherService voucherService = new VoucherService(new VoucherRepository() {
-        @Override
-        public Voucher save(Voucher voucher) {
-            return voucher;
-        }
+    @Mock
+    VoucherRepository voucherRepository;
 
-        @Override
-        public List<Voucher> findAll() {
-            try {
-                return Arrays.asList(new FixedAmountVoucher(UUID.randomUUID(), 10L),
-                    new PercentDiscountVoucher(UUID.randomUUID(), 20L));
-            } catch (Exception ignored) {
-            }
-            return null;
-        }
-    });
+    @InjectMocks
+    VoucherService voucherService;
 
     @DisplayName("FixedAmountVoucerType을 주면 FixedAmountVoucher를 반환한다.")
     @Test
@@ -41,10 +36,15 @@ class VoucherServiceTest {
         WrongDiscountPercentException,
         WrongDiscountAmountException {
 
+        Voucher mockVoucher = new FixedAmountVoucher(UUID.randomUUID(), 10L);
+        given(voucherRepository.save(any(FixedAmountVoucher.class))).willReturn(mockVoucher);
+
         Voucher voucher = voucherService.create(VoucherType.FIXED_AMOUNT, 10L);
 
         assertThat(voucher).isInstanceOf(FixedAmountVoucher.class);
-        assertThat(voucher.discount(100L)).isEqualTo(90L);
+        assertThat(voucher).extracting("discountAmount")
+            .isEqualTo(10L);
+        then(voucherRepository).should(times(1)).save(any(Voucher.class));
     }
 
     @DisplayName("PercentDiscountVoucerType을 주면 PercentDiscountVoucher를 반환한다.")
@@ -52,17 +52,28 @@ class VoucherServiceTest {
     void create_PercentDiscountType_ReturnPercentDiscountVoucher() throws
         WrongDiscountPercentException,
         WrongDiscountAmountException {
+        Voucher mockVoucher = new PercentDiscountVoucher(UUID.randomUUID(), 10L);
+        given(voucherRepository.save(any(PercentDiscountVoucher.class))).willReturn(mockVoucher);
+
         Voucher voucher = voucherService.create(VoucherType.PERCENT_DISCOUNT, 10L);
 
         assertThat(voucher).isInstanceOf(PercentDiscountVoucher.class);
-        assertThat(voucher.discount(1000L)).isEqualTo(900L);
+        assertThat(voucher).extracting("discountPercent")
+            .isEqualTo(10L);
+        then(voucherRepository).should(times(1)).save(any(Voucher.class));
     }
 
     @DisplayName("모든 바우처를 반환한다.")
     @Test
-    void findAllVoucher_ReturnAllVoucher() {
+    void findAllVoucher_ReturnAllVoucher() throws WrongDiscountAmountException, WrongDiscountPercentException {
+        List<Voucher> mockVouchers = Arrays.asList(new FixedAmountVoucher(UUID.randomUUID(), 10L),
+            new PercentDiscountVoucher(UUID.randomUUID(), 20L));
+        given(voucherRepository.findAll()).willReturn(mockVouchers);
+
         List<Voucher> vouchers = voucherService.findAllVoucher();
 
-        assertThat(vouchers).hasSize(2);
+        assertThat(vouchers).hasSize(2)
+            .isEqualTo(mockVouchers);
+        then(voucherRepository).should(times(1)).findAll();
     }
 }
