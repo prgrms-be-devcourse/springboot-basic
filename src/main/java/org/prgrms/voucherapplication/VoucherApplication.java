acaff9cc80b81369da62350e7a8b9dac50adf4e7
HyeonseoJUNG <windymay1920@gmail.com>
2022-04-13 08:40:03 +0900

    Feat: VoucherApplication 수정

---
+++ src/main/java/org/prgrms/voucherapplication/VoucherApplication.java
@@ -0,0 +1,16 @@
+package org.prgrms.voucherapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoucherApplication {
+
+	public static void main(String[] args) {
+		var configurableApplicationContext = SpringApplication.run(VoucherApplication.class, args);
+
+		VoucherManagement management = configurableApplicationContext.getBean(VoucherManagement.class);
+		management.run();
+	}
+
+}
