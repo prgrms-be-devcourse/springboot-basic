c4d01c1b747fdfebf6060e2524fb2a89687a302b
yuminhwan <ymh7532@icloud.com>
2022-04-12 13:44:15 +0900

    refactor: @Value 적용 및 파일 못찾을 시 예외 발생하도록 수정

--- src/main/java/org/prgms/voucherProgram/repository/voucher/FileVoucherRepository.java
+++ src/main/java/org/prgms/voucherProgram/repository/voucher/FileVoucherRepository.java
@@ -2,7 +2,6 @@ package org.prgms.voucherProgram.repository.voucher;
 
 import java.io.EOFException;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
@@ -13,20 +12,26 @@ import java.util.List;
 import org.prgms.voucherProgram.entity.voucher.Voucher;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Repository;
 
 @Repository
 @Profile("local")
 public class FileVoucherRepository implements VoucherRepository {
-    public static final String FILE_NAME = "./voucherData.txt";
     private static final String ERROR_WRONG_FILE = "[ERROR] 올바른 voucher 파일이 아닙니다.";
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    private final String voucherFilePath;
+
+    public FileVoucherRepository(@Value("${file.path.voucher}") String voucherFilePath) {
+        this.voucherFilePath = voucherFilePath;
+    }
 
     @Override
     public Voucher save(Voucher voucher) {
-        try (ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream(FILE_NAME, true))) {
+        try (ObjectOutputStream objectOutputStream = new ObjectOutputStream(
+            new FileOutputStream(voucherFilePath, true))) {
             objectOutputStream.writeObject(voucher);
         } catch (IOException e) {
             logger.error("Fail to find a voucher file");
@@ -38,13 +43,13 @@ public class FileVoucherRepository implements VoucherRepository {
     @Override
     public List<Voucher> findAll() {
         List<Voucher> vouchers = new ArrayList<>();
-        try (FileInputStream fileInputStream = new FileInputStream(FILE_NAME)) {
+        try (FileInputStream fileInputStream = new FileInputStream(voucherFilePath)) {
             while (true) {
                 ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
                 Voucher voucher = (Voucher)objectInputStream.readObject();
                 vouchers.add(voucher);
             }
-        } catch (EOFException | FileNotFoundException e) {
+        } catch (EOFException e) {
             return vouchers;
         } catch (IOException | ClassNotFoundException e) {
             logger.error("Fail to find a voucher file");
