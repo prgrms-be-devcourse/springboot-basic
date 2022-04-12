801e9ad0f3006441f4b6961bd88fc0d8b015fa82
ecvheo1 <ecvheo@naver.com>
2022-04-12 20:35:23 +0900

    :tada: init project

---
+++ src/main/java/kdt/vouchermanagement/VoucherManagementApplication.java
@@ -0,0 +1,13 @@
+package kdt.vouchermanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoucherManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoucherManagementApplication.class, args);
+	}
+
+}
