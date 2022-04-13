0f2d3896f4a1d305361911127655c666679f2aad
HyeonseoJUNG <windymay1920@gmail.com>
2022-04-13 10:39:57 +0900

    Feat: Blacklist customer entity 추가

---
+++ src/main/java/org/prgrms/voucherapplication/entity/BlackListCustomer.java
@@ -0,0 +1,14 @@
+package org.prgrms.voucherapplication.entity;
+
+public record BlackListCustomer(long id, String name) implements Customer{
+    @Override
+    public long getCustomerId() {
+        return id;
+    }
+
+    @Override
+    public String getCustomerName() {
+        return name;
+    }
+
+}
---
+++ src/main/java/org/prgrms/voucherapplication/entity/Customer.java
@@ -0,0 +1,11 @@
+package org.prgrms.voucherapplication.entity;
+
+import java.util.UUID;
+
+public interface Customer {
+    long getCustomerId();
+
+    String getCustomerName();
+
+    String toString();
+}
