107385f3c39159fcd4537ec5181a6ab6442b491e
jeonghaemin <jhm0828@gmail.com>
2022-04-14 22:36:28 +0900

    BlackListRepository 파일 입출력 코드 개선
    - 빈 초기화 시점에 한번만 파일을 불러오는 방식으로 변경

--- src/main/java/com/prgrms/vouchermanagement/customer/BlackListRepository.java
+++ src/main/java/com/prgrms/vouchermanagement/customer/BlackListRepository.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 
+import javax.annotation.PostConstruct;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -14,23 +15,24 @@ import java.util.List;
 public class BlackListRepository {
     private static final String BLACK_LIST_FILE_NAME = "customer_black_list.csv";
     private final Logger log = LoggerFactory.getLogger(this.getClass());
+    private final List<Customer> blackList = new ArrayList<>();
 
-    /**
-     * customer_black_list.csv 파일을 읽어서 Member 타입의 List 로 변환하여 반환한다.
-     */
-    public List<Customer> findAll() {
-        List<Customer> blackList = new ArrayList<>();
-
+    @PostConstruct
+    private void init() {
+        // customer_black_list.csv 파일을 읽어서 Member 타입의 List 로 변환한다.
         try (BufferedReader br = new BufferedReader(new FileReader(BLACK_LIST_FILE_NAME))) {
             String name = null;
             while ((name = br.readLine()) != null) {
                 blackList.add(new Customer(name));
             }
-            log.info("find all blackList. size={}", blackList.size());
+            log.info("init blackList. size={}", blackList.size());
         } catch (IOException e) {
             log.error("not found {}", BLACK_LIST_FILE_NAME, e);
         }
+    }
 
+    public List<Customer> findAll() {
+        log.info("find all blackList. size={}", blackList.size());
         return blackList;
     }
 }
