42108d2156137eb52780cca3b4c54750de653c04
dbslzld15 <dbslzld15@naver.com>
2022-04-14 13:46:47 +0900

    feat: 바우처, 커맨드 enum타입 테스트 코드 추가

---
+++ src/test/java/org/prgrms/kdt/domain/voucher/types/VoucherTypeTest.java
@@ -0,0 +1,35 @@
+package org.prgrms.kdt.domain.voucher.types;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class VoucherTypeTest {
+
+    @ParameterizedTest
+    @ValueSource(strings = {"fixedAmount", "percentDiscount"})
+    @DisplayName("유효한 바우처종류를 입력할 경우 이에 해당하는 바우처타입을 반환한다.")
+    public void findVoucherType(String type){
+        //given
+        //when
+        VoucherType voucherType = VoucherType.findVoucherType(type);
+        //then
+        assertThat(VoucherType.values()).contains(voucherType);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", " ", "voucher"})
+    @DisplayName("지원하지 않는 바우처 타입을 입력시 예외를 발생시킨다.")
+    public void exception_findVoucherType(String type){
+        //given
+        //when
+        //then
+        assertThatThrownBy(() -> VoucherType.findVoucherType(type))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("지원하지 않는 바우처타입 입니다.");
+    }
+
+}
