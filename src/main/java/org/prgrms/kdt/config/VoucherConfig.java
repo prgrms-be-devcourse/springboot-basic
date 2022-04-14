99a4e25cd25dce8111dac27d17fe7dd43f7eff81
HyoungUkJJang <guddnr1451@gmail.com>
2022-04-14 22:09:54 +0900

    feat : 바우처 관리 설정 클래스 작성하기

---
+++ src/main/java/org/prgrms/kdt/config/VoucherConfig.java
@@ -0,0 +1,15 @@
+package org.prgrms.kdt.config;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@ComponentScan(basePackages = {"org.prgrms.kdt.voucher",
+    "org.prgrms.kdt.controller"})
+@EnableConfigurationProperties
+@PropertySource(value = "application.properties")
+public class VoucherConfig {
+
+}
