31706574cf710170820c9bcea052583d344a2184
hyuk0309 <bahngeh@naver.com>
2022-04-14 19:05:53 +0900

    test : VoucherServiceTest 수정
    mocking을 이용한 단위 테스트로 수정

--- src/test/java/org/prgrms/springbootbasic/service/VoucherServiceTest.java
+++ src/test/java/org/prgrms/springbootbasic/service/VoucherServiceTest.java
@@ -2,36 +2,46 @@ package org.prgrms.springbootbasic.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+import java.util.List;
+import java.util.UUID;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-import org.prgrms.springbootbasic.repository.voucher.MemoryVoucherRepository;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.prgrms.springbootbasic.entity.FixedAmountVoucher;
+import org.prgrms.springbootbasic.entity.PercentDiscountVoucher;
+import org.prgrms.springbootbasic.entity.Voucher;
+import org.prgrms.springbootbasic.repository.voucher.VoucherRepository;
 
+@ExtendWith(MockitoExtension.class)
 class VoucherServiceTest {
 
-    MemoryVoucherRepository memoryVoucherRepository = new MemoryVoucherRepository();
-    VoucherService voucherService = new VoucherService(memoryVoucherRepository);
+    @Mock
+    VoucherRepository voucherRepository;
+    VoucherService voucherService;
 
     @BeforeEach
     void init() {
-        memoryVoucherRepository.removeAll();
+        voucherService = new VoucherService(voucherRepository);
     }
 
     @DisplayName("FixedAmountVoucher 만들기 테스트")
     @Test
     void createFixedAmountVoucher() {
         //given
-        int amount = 10;
-
         //when
-        voucherService.createFixedAmountVoucher(amount);
+        voucherService.createFixedAmountVoucher(10);
 
         //then
-        assertThat(memoryVoucherRepository.findAll().size())
-            .isEqualTo(1);
+        verify(voucherRepository).save(any(FixedAmountVoucher.class));
     }
 
     @DisplayName("FixedAmountVoucher 만들기 실패")
@@ -49,14 +59,11 @@ class VoucherServiceTest {
     @Test
     void createPercentAmountVoucher() {
         //given
-        int percent = 10;
-
         //when
-        voucherService.createPercentAmountVoucher(percent);
+        voucherService.createPercentAmountVoucher(20);
 
         //then
-        assertThat(memoryVoucherRepository.findAll().size())
-            .isEqualTo(1);
+        verify(voucherRepository).save(any(PercentDiscountVoucher.class));
     }
 
     @DisplayName("PercentAmountVoucher 만들기 실패")
@@ -74,12 +81,17 @@ class VoucherServiceTest {
     @Test
     void findAll() {
         //given
-        voucherService.createFixedAmountVoucher(10);
-        voucherService.createPercentAmountVoucher(10);
+        var fixedAmountVoucher = new FixedAmountVoucher(UUID.randomUUID(), 1000);
+        var percentDiscountVoucher = new PercentDiscountVoucher(UUID.randomUUID(), 20);
+        List<Voucher> vouchers = List.of(fixedAmountVoucher, percentDiscountVoucher);
+
+        when(voucherRepository.findAll()).thenReturn(vouchers);
 
         //when
+        var voucherList = voucherService.findAll();
+
         //then
-        assertThat(voucherService.findAll().size())
-            .isEqualTo(2);
+        assertThat(voucherList)
+            .containsExactlyInAnyOrder(fixedAmountVoucher, percentDiscountVoucher);
     }
 }
