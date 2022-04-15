de39e083cd9dd59fa159f213a88ee43cfb61c058
midas <midas117@knou.ac.kr>
2022-04-15 21:32:40 +0900

    feat: Model Class 생성 - Voucher(← FixedAmountVoucher, PercentDiscountVoucher)

---
+++ src/main/java/org/prgrms/kdt/model/voucher/FixedAmountVoucher.java
@@ -0,0 +1,34 @@
+package org.prgrms.kdt.model.voucher;
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+import java.util.UUID;
+
+public class FixedAmountVoucher implements Voucher {
+    private final UUID voucherId;
+    private final long amount;
+
+    public FixedAmountVoucher(UUID voucherId, long amount) {
+        this.voucherId = voucherId;
+        this.amount = amount;
+    }
+
+    @Override
+    public UUID getVoucherId() {
+        return voucherId;
+    }
+
+    @Override
+    public long discount(long beforeDiscount) {
+        return beforeDiscount - amount;
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this, ToStringStyle.SHORT_PREFIX_STYLE)
+            .append("voucherId", voucherId)
+            .append("amount", amount)
+            .toString();
+    }
+}
---
+++ src/main/java/org/prgrms/kdt/model/voucher/PercentDiscountVoucher.java
@@ -0,0 +1,34 @@
+package org.prgrms.kdt.model.voucher;
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+import java.util.UUID;
+
+public class PercentDiscountVoucher implements Voucher {
+    private final UUID voucherId;
+    private final long percent;
+
+    public PercentDiscountVoucher(UUID voucherId, long percent) {
+        this.voucherId = voucherId;
+        this.percent = percent;
+    }
+
+    @Override
+    public UUID getVoucherId() {
+        return voucherId;
+    }
+
+    @Override
+    public long discount(long beforeDiscount) {
+        return beforeDiscount * (percent / 100);
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this, ToStringStyle.SHORT_PREFIX_STYLE)
+            .append("voucherId", voucherId)
+            .append("percent", percent)
+            .toString();
+    }
+}
