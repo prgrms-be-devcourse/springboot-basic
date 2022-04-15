5c8334fb101154f5430a3a2fddc417e6d232eb37
pjh612 <62292492+pjh612@users.noreply.github.com>
2022-04-15 16:31:43 +0900

    refactor : db는 빈으로 관리 되지 않도록 변경

--- voucher-management/src/main/java/com/voucher/vouchermanagement/repository/blacklist/BlacklistRepository.java
+++ voucher-management/src/main/java/com/voucher/vouchermanagement/repository/blacklist/BlacklistRepository.java
@@ -4,24 +4,25 @@ import com.voucher.vouchermanagement.model.customer.Customer;
 import com.voucher.vouchermanagement.repository.io.FileInput;
 import com.voucher.vouchermanagement.repository.io.FileOutput;
 import com.voucher.vouchermanagement.repository.utils.CsvDeserializer;
-import java.io.File;
 import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class BlacklistRepository {
 
-  private final File blacklistDb;
+  private final String blacklistDbName = "customer_blacklist.csv";
+  private final Resource blackListDb = new ClassPathResource("db/" + blacklistDbName);
   private final FileInput input;
   private final FileOutput output;
   private final CsvDeserializer<Customer> csvDeserializer;
 
-  public BlacklistRepository(@Qualifier("blacklistDb") File blacklistDb, FileInput input, FileOutput output,
+  public BlacklistRepository(FileInput input, FileOutput output,
       @Qualifier("blacklistDeserializer") CsvDeserializer<Customer> csvDeserializer) {
-    this.blacklistDb = blacklistDb;
     this.input = input;
     this.output = output;
     this.csvDeserializer = csvDeserializer;
@@ -29,7 +30,7 @@ public class BlacklistRepository {
 
   public List<Customer> findAll() throws IOException {
     return input
-        .readAllLine(blacklistDb)
+        .readAllLine(blackListDb.getFile())
         .stream().map(csvDeserializer::deserialize)
         .collect(Collectors.toList());
   }
--- voucher-management/src/main/java/com/voucher/vouchermanagement/repository/voucher/VoucherFileRepository.java
+++ voucher-management/src/main/java/com/voucher/vouchermanagement/repository/voucher/VoucherFileRepository.java
@@ -4,7 +4,6 @@ import com.voucher.vouchermanagement.model.voucher.Voucher;
 import com.voucher.vouchermanagement.repository.io.FileInput;
 import com.voucher.vouchermanagement.repository.io.FileOutput;
 import com.voucher.vouchermanagement.repository.utils.CsvDeserializer;
-import java.io.File;
 import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -12,22 +11,23 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Repository;
 
 @Repository
 @Profile("prod")
 public class VoucherFileRepository implements VoucherRepository {
 
-  private final File voucherDb;
+  private final String voucherDbName = "voucher_db.csv";
+  private final Resource voucherDb = new ClassPathResource("db/" + voucherDbName);
   private final FileOutput output;
   private final FileInput input;
   private final CsvDeserializer<Voucher> voucherDeserializer;
   private static final Logger logger = LoggerFactory.getLogger(VoucherFileRepository.class);
 
-  public VoucherFileRepository(
-      @Qualifier("voucherDb") File voucherDb, FileOutput output, FileInput input,
+  public VoucherFileRepository(FileOutput output, FileInput input,
       @Qualifier("voucherDeserializer") CsvDeserializer<Voucher> voucherDeserializer) {
-    this.voucherDb = voucherDb;
     this.output = output;
     this.input = input;
     this.voucherDeserializer = voucherDeserializer;
@@ -40,12 +40,12 @@ public class VoucherFileRepository implements VoucherRepository {
         voucher.getValue().toString(),
         voucher.getCreatedAt().toString());
 
-    output.writeln(voucherDb, voucherCsvData);
+    output.writeln(voucherDb.getFile(), voucherCsvData);
   }
 
   @Override
   public List<Voucher> findAll() throws IOException {
-    List<String> foundDataStrings = input.readAllLine(voucherDb);
+    List<String> foundDataStrings = input.readAllLine(voucherDb.getFile());
 
     return foundDataStrings
         .stream()
