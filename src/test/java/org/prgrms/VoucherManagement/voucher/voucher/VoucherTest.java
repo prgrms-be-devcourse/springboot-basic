0767bb0deb03292fd3d464e2420a6204ea695767
sdardew <sdardew@gmail.com>
2022-04-16 02:38:16 +0900

    chore: LocalDateTime 타입의 Voucher 생성일 추가

--- src/test/java/org/prgrms/VoucherManagement/voucher/voucher/VoucherTest.java
+++ src/test/java/org/prgrms/VoucherManagement/voucher/voucher/VoucherTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 import static org.hamcrest.MatcherAssert.*;
@@ -18,12 +19,12 @@ public class VoucherTest {
   @Test
   @DisplayName("Voucher는 Voucher 클래스를 상속 받아야 한다")
   void testVoucherInheritance() {
-    FixedAmountVoucher fixedAmountVoucher = new FixedAmountVoucher(UUID.randomUUID(), 10L);
+    FixedAmountVoucher fixedAmountVoucher = new FixedAmountVoucher(UUID.randomUUID(), 10L, LocalDateTime.now());
     assertThat(
       (fixedAmountVoucher instanceof Voucher),
       is(true));
 
-    PercentDiscountVoucher percentDiscountVoucher = new PercentDiscountVoucher(UUID.randomUUID(), 10L);
+    PercentDiscountVoucher percentDiscountVoucher = new PercentDiscountVoucher(UUID.randomUUID(), 10L, LocalDateTime.now());
     assertThat((percentDiscountVoucher instanceof Voucher),
       is(true));
   }
@@ -46,7 +47,7 @@ public class VoucherTest {
   @DisplayName("FixedAmountVoucher 클래스의 discount() 메서드 테스트")
   void testFixedAmountVoucherDiscount() {
     FixedAmountVoucher fixedAmountVoucher;
-    fixedAmountVoucher = new FixedAmountVoucher(UUID.randomUUID(), 100L);
+    fixedAmountVoucher = new FixedAmountVoucher(UUID.randomUUID(), 100L, LocalDateTime.now());
     assertThat(fixedAmountVoucher.discount(1000L), is(900L));
     assertThat(fixedAmountVoucher.discount(100L), is(0L));
     assertThat(fixedAmountVoucher.discount(10L), is(0L));
@@ -56,7 +57,7 @@ public class VoucherTest {
   @DisplayName("PercentDiscountVoucher 클래스의 discount() 메서드 테스트")
   void testPercentDiscountVoucherDiscount() {
     PercentDiscountVoucher percentDiscountVoucher;
-    percentDiscountVoucher = new PercentDiscountVoucher(UUID.randomUUID(), 10L);
+    percentDiscountVoucher = new PercentDiscountVoucher(UUID.randomUUID(), 10L, LocalDateTime.now());
     assertThat(percentDiscountVoucher.discount(1000L), is(900L));
     assertThat(percentDiscountVoucher.discount(100L), is(90L));
     assertThat(percentDiscountVoucher.discount(10L), is(9L));
@@ -65,19 +66,19 @@ public class VoucherTest {
   @Test
   @DisplayName("VoucherFactory로 Voucher를 생성할 수 있어야 한다")
   void testVoucherFactory() {
-    Voucher voucher = VoucherFactory.createVoucher(1, 1000);
+    Voucher voucher = VoucherFactory.createVoucher(1, 1000, LocalDateTime.now());
     assertThat(voucher instanceof FixedAmountVoucher, is(true));
 
-    voucher = VoucherFactory.createVoucher(2, 10);
+    voucher = VoucherFactory.createVoucher(2, 10, LocalDateTime.now());
     assertThat(voucher instanceof PercentDiscountVoucher, is(true));
   }
 
   @Test
   @DisplayName("reduction의 범위에 벗어나는 Voucher을 생성할 수는 없다")
   void testVoucherFactoryException() {
-    assertThrows(RuntimeException.class, () -> VoucherFactory.createVoucher(1, 10_000_000));
-    assertThrows(RuntimeException.class, () -> VoucherFactory.createVoucher(1, 0));
-    assertThrows(RuntimeException.class, () -> VoucherFactory.createVoucher(2, 101));
-    assertThrows(RuntimeException.class, () -> VoucherFactory.createVoucher(2, 0));
+    assertThrows(RuntimeException.class, () -> VoucherFactory.createVoucher(1, 10_000_000, LocalDateTime.now()));
+    assertThrows(RuntimeException.class, () -> VoucherFactory.createVoucher(1, 0, LocalDateTime.now()));
+    assertThrows(RuntimeException.class, () -> VoucherFactory.createVoucher(2, 101, LocalDateTime.now()));
+    assertThrows(RuntimeException.class, () -> VoucherFactory.createVoucher(2, 0, LocalDateTime.now()));
   }
 }
