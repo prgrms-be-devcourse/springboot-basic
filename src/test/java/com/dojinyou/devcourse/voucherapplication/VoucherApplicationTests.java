07c7876e91c04fc90c4a1e66cfdc2771562b54e7
dojinyou <djyou128@gmail.com>
2022-04-15 22:37:48 +0900

    :tada: init: 프로젝트 초기 셋팅

---
+++ src/test/java/com/dojinyou/devcourse/voucherapplication/VoucherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dojinyou.devcourse.voucherapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoucherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
