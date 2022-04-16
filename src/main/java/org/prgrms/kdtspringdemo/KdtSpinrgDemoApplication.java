ff3b13671cfa1d8a07558f7f8894ef59aa0d22db
hayoung <60775067+gkdud583@users.noreply.github.com>
2022-04-16 15:15:05 +0900

    Update KdtSpinrgDemoApplication.java

--- src/main/java/org/prgrms/kdtspringdemo/KdtSpinrgDemoApplication.java
+++ src/main/java/org/prgrms/kdtspringdemo/KdtSpinrgDemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class KdtSpinrgDemoApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(KdtSpinrgDemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(KdtSpinrgDemoApplication.class, args);
+    }
 }
