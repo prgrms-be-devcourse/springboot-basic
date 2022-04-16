de5c66350df0ebd96cf583fcdf298b72c27636c4
hayoung <60775067+gkdud583@users.noreply.github.com>
2022-04-16 15:18:02 +0900

    Update VoucherFileRepositoryTest.java

--- src/test/java/org/prgrms/kdtspringdemo/domain/voucher/repository/VoucherFileRepositoryTest.java
+++ src/test/java/org/prgrms/kdtspringdemo/domain/voucher/repository/VoucherFileRepositoryTest.java
@@ -7,22 +7,22 @@ import org.junit.jupiter.api.Test;
 public class VoucherFileRepositoryTest {
 
     @Test
-	@DisplayName("save()호출 시 FileUtils.writeFile()을 호출합니다.")
-	public void save() {
-	}
+    @DisplayName("save()호출 시 FileUtils.writeFile()을 호출합니다.")
+    public void save() {
+    }
 
-	@Test
-	@DisplayName("save()호출 시 FileUtils.writeFile()에 예외가 발생하면 예외가 전달됩니다.")
-	public void saveWithFileUtilsException() {
-	}
+    @Test
+    @DisplayName("save()호출 시 FileUtils.writeFile()에 예외가 발생하면 예외가 전달됩니다.")
+    public void saveWithFileUtilsException() {
+    }
 
-	@Test
-	@DisplayName("findAll()호출 시 FileUtils.getFileString()을 호출합니다.")
-	public void findAll() {
-	}
+    @Test
+    @DisplayName("findAll()호출 시 FileUtils.getFileString()을 호출합니다.")
+    public void findAll() {
+    }
 
-	@Test
-	@DisplayName("findAll()호출 시 FileUtils.getFileString()에 예외가 발생하면 예외가 전달됩니다.")
-	public void findAllWithFileUtilsException() {
-	}
+    @Test
+    @DisplayName("findAll()호출 시 FileUtils.getFileString()에 예외가 발생하면 예외가 전달됩니다.")
+    public void findAllWithFileUtilsException() {
+    }
 }
