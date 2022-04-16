fc063a5a52d319f8af18a2a8a45850403ba49cb9
hayoung <60775067+gkdud583@users.noreply.github.com>
2022-04-16 15:19:17 +0900

    Update FixedAmountVoucherTest.java

--- src/test/java/org/prgrms/kdtspringdemo/domain/voucher/FixedAmountVoucherTest.java
+++ src/test/java/org/prgrms/kdtspringdemo/domain/voucher/FixedAmountVoucherTest.java
@@ -1,17 +1,21 @@
 package org.prgrms.kdtspringdemo.domain.voucher;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 public class FixedAmountVoucherTest {
 
+    @Test
     @DisplayName("기존 금액에서 FixedAmountVoucher 적용된 가격을 반환합니다.")
     public void discount() {
     }
 
+    @Test
     @DisplayName("FixedAmountVoucher 할인 금액이 마이너스 일 경우 예외가 발생합니다.")
     public void minusDiscountFixAmount() {
     }
 
+    @Test
     @DisplayName("FixedAmountVoucher 할인 금액이 기존 금액보다 클 경우 0을 반환합니다.")
     public void discountWithGreaterDiscountAmountThanBeforeDiscount() {
     }
