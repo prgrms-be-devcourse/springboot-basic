ffb3f6686338e6b0edeb2098d583fcda3fd39c10
ecvheo1 <ecvheo@naver.com>
2022-04-16 21:50:22 +0900

    :white_check_mark: test: 콘솔 입력 테스트 코드 import class

--- src/test/java/kdt/vouchermanagement/ConsoleDispatcherTest.java
+++ src/test/java/kdt/vouchermanagement/ConsoleDispatcherTest.java
@@ -1,5 +1,7 @@
 package kdt.vouchermanagement;
 
+import kdt.vouchermanagement.exception.InvalidMenuException;
+import kdt.vouchermanagement.io.Input;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
