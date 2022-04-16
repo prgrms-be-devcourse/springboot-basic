67cb26d26b88bd89170166d3081251a23b15ba5e
hayoung <60775067+gkdud583@users.noreply.github.com>
2022-04-16 15:20:34 +0900

    Update PercentDiscountVoucherTest.java

--- src/test/java/org/prgrms/kdtspringdemo/domain/voucher/PercentDiscountVoucherTest.java
+++ src/test/java/org/prgrms/kdtspringdemo/domain/voucher/PercentDiscountVoucherTest.java
@@ -5,10 +5,12 @@ import org.junit.jupiter.api.Test;
 
 public class PercentDiscountVoucherTest {
 
+    @Test
     @DisplayName("기존 금액에서 PercentDiscountVoucher 적용된 가격을 반환합니다.")
     public void discount() {
     }
 
+    @Test
     @DisplayName("PercentDiscountVoucher 할인율이 마이너스 일 경우 예외가 발생합니다.")
     public void minusDiscountPercent() {
     }
