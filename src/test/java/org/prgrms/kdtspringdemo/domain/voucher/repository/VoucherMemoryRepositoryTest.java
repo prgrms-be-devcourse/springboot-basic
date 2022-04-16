e6007f0c8b58ebef273c1e0aa42fed777f56298d
hayoung <60775067+gkdud583@users.noreply.github.com>
2022-04-16 15:21:19 +0900

    Update VoucherMemoryRepositoryTest.java

--- src/test/java/org/prgrms/kdtspringdemo/domain/voucher/repository/VoucherMemoryRepositoryTest.java
+++ src/test/java/org/prgrms/kdtspringdemo/domain/voucher/repository/VoucherMemoryRepositoryTest.java
@@ -5,14 +5,13 @@ import org.junit.jupiter.api.Test;
 
 public class VoucherMemoryRepositoryTest {
 
-	@Test
-	@DisplayName("바우처 한건이 저장됩니다.")
-	public void save() {
-	}
+    @Test
+    @DisplayName("바우처 한건이 저장됩니다.")
+    public void save() {
+    }
 
-
-	@Test
-	@DisplayName("바우처가 전체 조회됩니다.")
-	public void findAll() {
-	}
+    @Test
+    @DisplayName("바우처가 전체 조회됩니다.")
+    public void findAll() {
+    }
 }
