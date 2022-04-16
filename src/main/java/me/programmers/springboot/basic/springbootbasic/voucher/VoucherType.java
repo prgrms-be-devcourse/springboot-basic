d267af098dbcc34632f71bd2a96ae909f686ece2
Yanjoo <hanjub3@gmail.com>
2022-04-17 03:32:36 +0900

    Voucher 타입 비교 오류 해결

--- src/main/java/me/programmers/springboot/basic/springbootbasic/voucher/VoucherType.java
+++ src/main/java/me/programmers/springboot/basic/springbootbasic/voucher/VoucherType.java
@@ -5,9 +5,9 @@ import java.util.Arrays;
 public enum VoucherType {
     FIXED, PERCENT;
 
-    public static VoucherType getVoucherStatus(String voucherType) {
+    public static VoucherType getVoucherStatus(String voucherTypeString) {
         return Arrays.stream(VoucherType.values())
-                .filter(e -> e.name().equals(voucherType))
+                .filter(e -> e.name().equals(voucherTypeString.toUpperCase()))
                 .findFirst()
                 .orElseThrow(() -> new IllegalArgumentException("잘못된 바우처 타입 입력"));
     }
