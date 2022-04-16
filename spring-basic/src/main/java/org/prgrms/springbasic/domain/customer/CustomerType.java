6bdc8212b63c1b7155640a8e60f07e0b7b23ecd5
Gosh95 <goshk1995@gmail.com>
2022-04-17 02:38:00 +0900

    refactor: 불필요한 value 제거

--- spring-basic/src/main/java/org/prgrms/springbasic/domain/customer/CustomerType.java
+++ spring-basic/src/main/java/org/prgrms/springbasic/domain/customer/CustomerType.java
@@ -2,15 +2,5 @@ package org.prgrms.springbasic.domain.customer;
 
 public enum CustomerType {
 
-    BLACK("black"), NORMAL("normal");
-
-    private final String value;
-
-    CustomerType(String value) {
-        this.value = value;
-    }
-
-    public String getValue() {
-        return value;
-    }
+    BLACK, NORMAL
 }
--- spring-basic/src/main/java/org/prgrms/springbasic/domain/voucher/VoucherType.java
+++ spring-basic/src/main/java/org/prgrms/springbasic/domain/voucher/VoucherType.java
@@ -2,15 +2,5 @@ package org.prgrms.springbasic.domain.voucher;
 
 public enum VoucherType {
 
-    FIXED("fixed"), PERCENT("percent");
-
-    private final String value;
-
-    VoucherType(String value) {
-        this.value = value;
-    }
-
-    public String getValue() {
-        return value;
-    };
+    FIXED, PERCENT
 }
