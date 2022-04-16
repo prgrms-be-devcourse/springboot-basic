ec2cdd089fb9db56de5165701dc4d72aa6398585
hymn-fly <rlfrmsdh1@gmail.com>
2022-04-16 22:07:21 +0900

    feature. domain Validator 추가

--- src/main/java/org/prgms/customer/Customer.java
+++ src/main/java/org/prgms/customer/Customer.java
@@ -1,6 +1,21 @@
 package org.prgms.customer;
 
+import org.prgms.validator.Validators;
+import org.prgms.voucher.Voucher;
+
+import java.util.List;
 import java.util.UUID;
 
-public record Customer(UUID customerId, String name, String email) {
+public record Customer(UUID customerId, String name, String email, List<Voucher> vouchers) {
+    public Customer {
+        Validators.notNullAndEmptyCheck(customerId, name, email, vouchers);
+    }
+
+    public void addVoucher(Voucher voucher) {
+        vouchers.add(voucher);
+    }
+
+    public List<Voucher> getVouchers() {
+        return this.vouchers;
+    }
 }
