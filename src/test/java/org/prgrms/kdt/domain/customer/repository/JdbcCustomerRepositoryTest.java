52cbe23ee7c00bd3dd1182318b86a6ffe52f501b
dbslzld15 <dbslzld15@naver.com>
2022-04-17 20:52:22 +0900

    feat: 고객 CRUD 테스트 코드 추가 구현

--- src/test/java/org/prgrms/kdt/domain/customer/repository/JdbcCustomerRepositoryTest.java
+++ src/test/java/org/prgrms/kdt/domain/customer/repository/JdbcCustomerRepositoryTest.java
@@ -5,6 +5,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.prgrms.kdt.domain.customer.model.Customer;
 import org.prgrms.kdt.domain.customer.model.CustomerType;
+import org.prgrms.kdt.domain.voucher.model.Voucher;
+import org.prgrms.kdt.domain.voucher.model.VoucherType;
+import org.prgrms.kdt.domain.voucher.repository.JdbcVoucherRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
@@ -16,6 +19,7 @@ import javax.sql.DataSource;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -41,21 +45,25 @@ class JdbcCustomerRepositoryTest {
     }
 
     @Autowired
-    JdbcCustomerRepository repository;
+    JdbcCustomerRepository customerRepository;
+
+    @Autowired
+    JdbcVoucherRepository voucherRepository;
 
     @BeforeEach
     void setUp() {
-        repository.deleteAll();
+        voucherRepository.deleteAll();
+        customerRepository.deleteAll();
     }
 
     @Test
     public void save() throws Exception {
         //given
-        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime now = LocalDateTime.now().withNano(0);
         UUID customerId = UUID.randomUUID();
         Customer customer = new Customer(customerId,"park" , "dbslzld15@naver.com", CustomerType.BLACK_LIST, now, now);
         //when
-        UUID savedId = repository.save(customer);
+        UUID savedId = customerRepository.save(customer);
         //then
         assertThat(savedId).isEqualTo(customerId);
     }
@@ -63,26 +71,84 @@ class JdbcCustomerRepositoryTest {
     @Test
     public void findAll() throws Exception {
         //given
-        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime now = LocalDateTime.now().withNano(0);
         UUID customerId = UUID.randomUUID();
         Customer customer = new Customer(customerId,"park" , "dbslzld15@naver.com", CustomerType.BLACK_LIST, now, now);
-        repository.save(customer);
+        customerRepository.save(customer);
         //when
-        List<Customer> customers = repository.findAll();
+        List<Customer> customers = customerRepository.findAll();
         //then
         assertThat(customers.size()).isEqualTo(1);
+        assertThat(customers.get(0)).usingRecursiveComparison().isEqualTo(customer);
     }
 
     @Test
-    public void update() throws Exception {
+    public void findById() throws Exception {
+        //given
+        LocalDateTime now = LocalDateTime.now().withNano(0);
+        UUID customerId = UUID.randomUUID();
+        Customer customer = new Customer(customerId,"park" , "dbslzld15@naver.com", CustomerType.BLACK_LIST, now, now);
+        customerRepository.save(customer);
+        //when
+        Optional<Customer> findCustomer = customerRepository.findById(customerId);
+        //then
+        assertThat(findCustomer.get()).usingRecursiveComparison().isEqualTo(customer);
+    }
+
+    @Test
+    public void findByVoucherId() throws Exception {
+        //given
+        LocalDateTime now = LocalDateTime.now().withNano(0);
+        UUID customerId = UUID.randomUUID();
+        Customer customer = new Customer(customerId, "park", "d@naver.com", now, now);
+        customerRepository.save(customer);
+        UUID voucherId = UUID.randomUUID();
+        Voucher voucher = new Voucher(voucherId, VoucherType.PERCENT_DISCOUNT, 10L, customerId, now, now);
+        voucherRepository.save(voucher);
+        //when
+        Optional<Customer> findCustomer = customerRepository.findByVoucherId(voucherId);
+        //then
+        assertThat(findCustomer.get()).usingRecursiveComparison().isEqualTo(customer);
+    }
+
+    @Test
+    public void updateById() throws Exception {
         //given
         LocalDateTime now = LocalDateTime.now();
         UUID customerId = UUID.randomUUID();
         Customer customer = new Customer(customerId,"park" , "dbslzld15@naver.com", CustomerType.BLACK_LIST, now, now);
-        repository.save(customer);
+        customerRepository.save(customer);
         //when
-        int updateCnt = repository.updateById(new Customer(customerId, "kim", "a@naver.com", CustomerType.NORMAL, LocalDateTime.now(), LocalDateTime.now()));
+        int updateCnt = customerRepository.updateById(new Customer(customerId, "kim", "a@naver.com", CustomerType.NORMAL, LocalDateTime.now(), LocalDateTime.now()));
         //then
         assertThat(updateCnt).isEqualTo(1);
+    }
+
+    @Test
+    public void deleteById() throws Exception {
+        //given
+        LocalDateTime now = LocalDateTime.now();
+        UUID customerId = UUID.randomUUID();
+        Customer customer = new Customer(customerId,"park" , "dbslzld15@naver.com", CustomerType.BLACK_LIST, now, now);
+        customerRepository.save(customer);
+        //when
+        customerRepository.deleteById(customerId);
+        List<Customer> customers = customerRepository.findAll();
+        //then
+        assertThat(customers).isEmpty();
+    }
+
+    @Test
+    public void deleteAll() throws Exception {
+        //given
+        LocalDateTime now = LocalDateTime.now();
+        UUID customerId = UUID.randomUUID();
+        Customer customer = new Customer(customerId,"park" , "dbslzld15@naver.com", CustomerType.BLACK_LIST, now, now);
+        customerRepository.save(customer);
+        //when
+        customerRepository.deleteAll();
+        //then
+        List<Customer> customers = customerRepository.findAll();
+        assertThat(customers).isEmpty();
     }
 }
