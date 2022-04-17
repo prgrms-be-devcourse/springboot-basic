56bd5f9a912d0b6f2e6b1e8c72fdffaf9d6cb801
Yanjoo <hanjub3@gmail.com>
2022-04-18 04:51:09 +0900

    enum을 활용한 바우처 생성

--- src/main/java/me/programmers/springboot/basic/springbootbasic/command/CreateCommand.java
+++ src/main/java/me/programmers/springboot/basic/springbootbasic/command/CreateCommand.java
@@ -3,13 +3,10 @@ package me.programmers.springboot.basic.springbootbasic.command;
 import me.programmers.springboot.basic.springbootbasic.io.Console;
 import me.programmers.springboot.basic.springbootbasic.io.ConsoleInput;
 import me.programmers.springboot.basic.springbootbasic.voucher.VoucherType;
-import me.programmers.springboot.basic.springbootbasic.voucher.model.FixedAmountVoucher;
-import me.programmers.springboot.basic.springbootbasic.voucher.model.PercentAmountVoucher;
+import me.programmers.springboot.basic.springbootbasic.voucher.model.Voucher;
 import me.programmers.springboot.basic.springbootbasic.voucher.service.VoucherService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.UUID;
 
 public class CreateCommand implements CommandStrategy {
 
@@ -23,23 +20,13 @@ public class CreateCommand implements CommandStrategy {
 
     @Override
     public void operateCommand() {
-        String cmd = console.inputCommand("Type fixed or percent: ");
-
+        String voucherTypeCommand = console.inputCommand("Type fixed or percent: ");
         try {
-            VoucherType voucherStatus = VoucherType.getVoucherStatus(cmd);
-            switch (voucherStatus) {
-                case FIXED:
-                    voucherService.save(new FixedAmountVoucher(UUID.randomUUID(), 100));
-                    break;
-                case PERCENT:
-                    voucherService.save(new PercentAmountVoucher(UUID.randomUUID(), 50));
-                    break;
-                default:
-                    logger.error("잘못된 바우처 타입 입력: {}", cmd);
-                    break;
-            }
+            VoucherType voucherType = VoucherType.getVoucherStatus(voucherTypeCommand);
+            Voucher voucher = VoucherType.getSpecificVoucher(voucherType);
+            voucherService.save(voucher);
         } catch (IllegalArgumentException e) {
-            logger.error(e.getMessage());
+            logger.error("잘못된 바우처 타입 입력 {}", voucherTypeCommand);
         }
     }
 }
--- src/main/java/me/programmers/springboot/basic/springbootbasic/voucher/VoucherType.java
+++ src/main/java/me/programmers/springboot/basic/springbootbasic/voucher/VoucherType.java
@@ -1,6 +1,11 @@
 package me.programmers.springboot.basic.springbootbasic.voucher;
 
+import me.programmers.springboot.basic.springbootbasic.voucher.model.FixedAmountVoucher;
+import me.programmers.springboot.basic.springbootbasic.voucher.model.PercentAmountVoucher;
+import me.programmers.springboot.basic.springbootbasic.voucher.model.Voucher;
+
 import java.util.Arrays;
+import java.util.UUID;
 
 public enum VoucherType {
     FIXED, PERCENT;
@@ -10,5 +15,21 @@ public enum VoucherType {
                 .filter(voucherType -> voucherType.name().equals(inputVoucherType.toUpperCase()))
                 .findFirst()
                 .orElseThrow(() -> new IllegalArgumentException("잘못된 바우처 타입 입력 " + inputVoucherType));
+    }
+
+    public static Voucher getSpecificVoucher(VoucherType voucherType) {
+        Voucher voucher = null;
+
+        switch (voucherType) {
+            case FIXED:
+                voucher = new FixedAmountVoucher(UUID.randomUUID(), 100);
+                break;
+            case PERCENT:
+                voucher = new PercentAmountVoucher(UUID.randomUUID(), 50);
+                break;
+            default:
+                break;
+        }
+        return voucher;
     }
 }
