7bfefecf4b0a2c8d6d31f0b2b4af3b62c9f4aa44
ecvheo1 <ecvheo@naver.com>
2022-04-17 23:18:21 +0900

    :white_check_mark: test: create FixedAmountVoucher 기능 테스트 코드 구현, 수정

--- src/test/java/kdt/vouchermanagement/VoucherMemoryRepositoryTest.java
+++ src/test/java/kdt/vouchermanagement/VoucherMemoryRepositoryTest.java
@@ -1,17 +1,22 @@
 package kdt.vouchermanagement;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import kdt.vouchermanagement.domain.voucher.domain.FixedAmountVoucher;
+import kdt.vouchermanagement.domain.voucher.domain.Voucher;
+import kdt.vouchermanagement.domain.voucher.domain.VoucherType;
+import org.junit.jupiter.api.*;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class VoucherMemoryRepositoryTest {
 
     VoucherMemoryRepository voucherMemoryRepository = new VoucherMemoryRepository();
 
     @Test
+    @Order(1)
     @DisplayName("바우처 저장_성공")
     void saveVoucher() {
         //given
@@ -26,5 +31,24 @@ public class VoucherMemoryRepositoryTest {
         //then
         assertThat(savedVoucher.getVoucherId()).isEqualTo(voucherId);
         assertThat(savedVoucher.getVoucherType()).isEqualTo(VoucherType.from(voucherTypeNum));
+    }
+
+    @Test
+    @Order(2)
+    @DisplayName("VoucherType과 DiscountValue로 바우처 조회_성공")
+    void findVoucherByVoucherTypeAndDiscountValue() {
+        //given
+        UUID voucherId = UUID.randomUUID();
+        String voucherTypeNum = "1";
+        String discountValue = "10000";
+        voucherMemoryRepository.save(new FixedAmountVoucher(voucherId, VoucherType.from(voucherTypeNum), discountValue));
+
+        //when
+        Optional<Voucher> foundVoucher = voucherMemoryRepository.findByVoucherTypeAndDiscountValue(VoucherType.from(voucherTypeNum), discountValue);
+
+        //then
+        assertThat(foundVoucher.isPresent()).isEqualTo(true);
+        assertThat(foundVoucher.get().getVoucherId()).isEqualTo(voucherId);
+        assertThat(foundVoucher.get().getVoucherType()).isEqualTo(VoucherType.from(voucherTypeNum));
     }
 }
--- src/test/java/kdt/vouchermanagement/VoucherServiceTest.java
+++ src/test/java/kdt/vouchermanagement/VoucherServiceTest.java
@@ -1,5 +1,8 @@
 package kdt.vouchermanagement;
 
+import kdt.vouchermanagement.domain.voucher.domain.FixedAmountVoucher;
+import kdt.vouchermanagement.domain.voucher.domain.Voucher;
+import kdt.vouchermanagement.domain.voucher.domain.VoucherType;
 import kdt.vouchermanagement.domain.voucher.repository.VoucherRepository;
 import kdt.vouchermanagement.domain.voucher.service.VoucherService;
 import org.junit.jupiter.api.BeforeEach;
@@ -13,7 +16,8 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.Optional;
 import java.util.UUID;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
@@ -40,6 +44,17 @@ public class VoucherServiceTest {
 
         //when, then
         assertThrows(InvalidVoucherTypeException.class, () -> voucherService.createVoucher(voucherTypeNum, discountValue));
+    }
+
+    @Test
+    @DisplayName("생성할 VoucherType과 DiscountValue가 주어질 때 유효한 VoucherType이면 _성공")
+    void validVoucherType() {
+        //given
+        String voucherTypeNum = "1";
+        String discountValue = "10";
+
+        //when, then
+        assertDoesNotThrow(() -> voucherService.createVoucher(voucherTypeNum, discountValue));
     }
 
     @Test
@@ -76,50 +91,6 @@ public class VoucherServiceTest {
     }
 
     @Test
-    @DisplayName("PercentDiscountVoucher 타입의 할인값 입력시 문자열을 입력하면_실패")
-    void stringDiscountValueWhenPercentDiscountVoucher() {
-        //given
-        String voucherTypeNum = "2";
-        String discountValue = "blahblah";
-
-        //when, then
-        assertThrows(InvalidDiscountValueException.class, () -> voucherService.createVoucher(voucherTypeNum, discountValue));
-    }
-
-    @Test
-    @DisplayName("PercentDiscountVoucher 타입의 할인값 입력시 음수를 입력하면_실패")
-    void negativeDiscountValueWhenPercentDiscountVoucher() {
-        //given
-        String voucherTypeNum = "2";
-        String discountValue = "-1";
-
-        //when, then
-        assertThrows(InvalidDiscountValueException.class, () -> voucherService.createVoucher(voucherTypeNum, discountValue));
-    }
-
-    @Test
-    @DisplayName("PercentDiscountVoucher 타입의 할인값 입력시 0을 입력하면_실패")
-    void zeroDiscountValueWhenPercentDiscountVoucher() {
-        //given
-        String voucherTypeNum = "2";
-        String discountValue = "0";
-
-        //when, then
-        assertThrows(InvalidDiscountValueException.class, () -> voucherService.createVoucher(voucherTypeNum, discountValue));
-    }
-
-    @Test
-    @DisplayName("PercentDiscountVoucher 타입의 할인값 입력시 100보다 큰 수를 입력하면_실패")
-    void over100DiscountValueWhenPercentDiscountVoucher() {
-        //given
-        String voucherTypeNum = "2";
-        String discountValue = "500";
-
-        //when, then
-        assertThrows(InvalidDiscountValueException.class, () -> voucherService.createVoucher(voucherTypeNum, discountValue));
-    }
-
-    @Test
     @DisplayName("생성할 VoucherType과 DiscountValue가 주어질 때 해당 값을 가진 바우처가 이미 존재하면_실패")
     void isDuplicateVoucher() {
         //given
@@ -143,25 +114,6 @@ public class VoucherServiceTest {
 
         doReturn(voucher).when(voucherRepository).save(any());
         doReturn(Optional.empty()).when(voucherRepository).findByVoucherTypeAndDiscountValue(VoucherType.from(voucherTypeNum), discountValue);
-
-        //when
-        Voucher createdVoucher = voucherService.createVoucher(voucherTypeNum, discountValue);
-
-        //then
-        assertThat(createdVoucher.getVoucherId()).isNotNull();
-        assertThat(createdVoucher.getVoucherType()).isEqualTo(VoucherType.from(voucherTypeNum));
-        verify(voucherRepository, times(1)).save(any());
-    }
-
-    @Test
-    @DisplayName("PercentAmountVoucher 생성_성공")
-    void createPercentAmountVoucher() {
-        //given
-        String voucherTypeNum = "2";
-        String discountValue = "10";
-        Voucher voucher = new PercentDiscountVoucher(UUID.randomUUID(), VoucherType.from(voucherTypeNum), discountValue);
-
-        doReturn(voucher).when(voucherRepository).save(any());
 
         //when
         Voucher createdVoucher = voucherService.createVoucher(voucherTypeNum, discountValue);
