9533d1e9ab7b09a8af977f5364bd542f46bee173
HYEBPARK <hyeqls3742@gmail.com>
2022-04-17 11:07:00 +0900

    feat : 고객 생성, 블랙리스트 조회 기능 추가 이전 push에 문제가 있어서 파일이 삭제된 것 같음

---
+++ src/main/java/org/programmers/kdt/weekly/customer/CustomerService.java
@@ -0,0 +1,33 @@
+package org.programmers.kdt.weekly.customer;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+import org.programmers.kdt.weekly.customer.repository.CustomerRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerService {
+
+    private static CustomerRepository customerRepository;
+
+    public CustomerService(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    public Customer createCustomer(CustomerType customerType, String name) {
+        Customer customer;
+        try {
+            customer = customerType.create(UUID.randomUUID(), name);
+            customerRepository.insert(customer);
+        } catch (IllegalArgumentException e) {
+            throw e;
+        }
+        return customer;
+    }
+
+    public Optional<List<Customer>> blackList() {
+        return Optional.ofNullable(customerRepository.findAll());
+    }
+
+}
