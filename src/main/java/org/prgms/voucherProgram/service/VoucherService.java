2290f385557fb286b0c699ff30f1a817c0f1113d
yuminhwan <ymh7532@icloud.com>
2022-04-18 00:30:38 +0900

    feat: 바우처 수정 서비스 구현

--- src/main/java/org/prgms/voucherProgram/service/VoucherService.java
+++ src/main/java/org/prgms/voucherProgram/service/VoucherService.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import org.prgms.voucherProgram.domain.voucher.Voucher;
 import org.prgms.voucherProgram.dto.VoucherDto;
+import org.prgms.voucherProgram.exception.VoucherIsNotExistsException;
 import org.prgms.voucherProgram.repository.voucher.VoucherRepository;
 import org.springframework.stereotype.Service;
 
@@ -23,6 +24,17 @@ public class VoucherService {
         return VoucherDto.from(voucherRepository.save(voucher));
     }
 
+    public VoucherDto update(VoucherDto voucherDto) {
+        Voucher voucher = voucherDto.toEntity();
+
+        voucherRepository.findById(voucherDto.getVoucherId())
+            .orElseThrow(() -> {
+                throw new VoucherIsNotExistsException();
+            });
+
+        return VoucherDto.from(voucherRepository.update(voucher));
+    }
+    
     public List<VoucherDto> findAllVoucher() {
         return voucherRepository.findAll().stream()
             .map(VoucherDto::from)
--- src/test/java/org/prgms/voucherProgram/service/VoucherServiceTest.java
+++ src/test/java/org/prgms/voucherProgram/service/VoucherServiceTest.java
@@ -4,11 +4,16 @@ import static org.assertj.core.api.Assertions.*;
 import static org.mockito.BDDMockito.*;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -16,6 +21,7 @@ import org.prgms.voucherProgram.domain.voucher.FixedAmountVoucher;
 import org.prgms.voucherProgram.domain.voucher.PercentDiscountVoucher;
 import org.prgms.voucherProgram.domain.voucher.Voucher;
 import org.prgms.voucherProgram.dto.VoucherDto;
+import org.prgms.voucherProgram.exception.VoucherIsNotExistsException;
 import org.prgms.voucherProgram.repository.voucher.VoucherRepository;
 
 @ExtendWith(MockitoExtension.class)
@@ -27,11 +33,17 @@ class VoucherServiceTest {
     @InjectMocks
     VoucherService voucherService;
 
-    @DisplayName("FixedAmountVoucerType을 주면 FixedAmountVoucher를 반환한다.")
-    @Test
-    void create_FixedAmountVoucherType_ReturnFixedAmountVoucher() {
+    private static Stream<Arguments> provideVoucher() {
+        return Stream.of(
+            Arguments.of(new VoucherDto(UUID.randomUUID(), 1, 10L)),
+            Arguments.of(new VoucherDto(UUID.randomUUID(), 2, 30L))
+        );
+    }
 
-        VoucherDto voucherDto = new VoucherDto(UUID.randomUUID(), UUID.randomUUID(), 1, 100L);
+    @DisplayName("바우처를 저장한다.")
+    @ParameterizedTest
+    @MethodSource("provideVoucher")
+    void should_ReturnVoucher_When_createVoucher(VoucherDto voucherDto) {
         given(voucherRepository.save(any(Voucher.class))).willReturn(voucherDto.toEntity());
 
         VoucherDto newVoucher = voucherService.create(voucherDto);
@@ -41,17 +53,39 @@ class VoucherServiceTest {
         then(voucherRepository).should(times(1)).save(any(Voucher.class));
     }
 
-    @DisplayName("PercentDiscountVoucerType을 주면 PercentDiscountVoucher를 반환한다.")
-    @Test
-    void create_PercentDiscountType_ReturnPercentDiscountVoucher() {
-        VoucherDto voucherDto = new VoucherDto(UUID.randomUUID(), UUID.randomUUID(), 2, 100L);
-        given(voucherRepository.save(any(Voucher.class))).willReturn(voucherDto.toEntity());
+    @DisplayName("수정할 바우처가 존재한다면 바우처를 수정한다.")
+    @ParameterizedTest
+    @MethodSource("provideVoucher")
+    void sholud_ReturnUpdateVoucher_When_VoucherIsExists(VoucherDto voucherDto) {
+        // given
+        given(voucherRepository.findById(voucherDto.getVoucherId())).willReturn(Optional.of(voucherDto.toEntity()));
+        voucherDto.setType(2);
+        voucherDto.setDiscountValue(50);
+        given(voucherRepository.update(any(Voucher.class))).willReturn(voucherDto.toEntity());
 
-        VoucherDto newVoucher = voucherService.create(voucherDto);
+        // when
+        VoucherDto updateVoucher = voucherService.update(voucherDto);
 
-        assertThat(newVoucher).usingRecursiveComparison()
+        // then
+        assertThat(updateVoucher).usingRecursiveComparison()
             .isEqualTo(voucherDto);
-        then(voucherRepository).should(times(1)).save(any(Voucher.class));
+        then(voucherRepository).should(times(1)).findById(any(UUID.class));
+        then(voucherRepository).should(times(1)).update(any(Voucher.class));
+    }
+
+    @DisplayName("수정할 바우처가 없다면 예외를 발생한다.")
+    @ParameterizedTest
+    @MethodSource("provideVoucher")
+    void should_ThrowException_When_VoucherIsNotExists(VoucherDto voucherDto) {
+        // given
+        given(voucherRepository.findById(voucherDto.getVoucherId())).willReturn(Optional.empty());
+        // when
+        // then
+        assertThatThrownBy(() -> voucherService.update(voucherDto))
+            .isInstanceOf(VoucherIsNotExistsException.class)
+            .hasMessage("[ERROR] 해당 아이디로 저장된 바우처가 없습니다.");
+        then(voucherRepository).should(times(1)).findById(voucherDto.getVoucherId());
+        then(voucherRepository).should(times(0)).update(any(Voucher.class));
     }
 
     @DisplayName("모든 바우처를 반환한다.")
