2d4f4575e451e06bce8f6fd3ae2a8960e5d14ce7
Yanjoo <hanjub3@gmail.com>
2022-04-18 04:08:47 +0900

    csv 파일을 못 찾는 경우 예외 처리

--- src/main/java/me/programmers/springboot/basic/springbootbasic/ApplicationController.java
+++ src/main/java/me/programmers/springboot/basic/springbootbasic/ApplicationController.java
@@ -1,5 +1,7 @@
 package me.programmers.springboot.basic.springbootbasic;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.core.io.Resource;
@@ -9,6 +11,8 @@ import java.nio.file.Files;
 import java.util.List;
 
 public class ApplicationController {
+    private static final Logger logger = LoggerFactory.getLogger(ApplicationController.class);
+    private static final String csvFile = "file:customer_blacklist.csv";
 
     private final ConfigurableApplicationContext context;
 
@@ -24,11 +28,11 @@ public class ApplicationController {
 
     public void showBlackList() {
         try {
-            Resource resource = context.getResource("file:customer_blacklist.csv");
+            Resource resource = context.getResource(csvFile);
             List<String> blackList = Files.readAllLines(resource.getFile().toPath());
             System.out.println("customer blacklist " + blackList.stream().reduce("", (a, b) -> a + "\n" + b));
         } catch (IOException e) {
-
+            logger.error("해당 파일을 찾을 수 없습니다. " + csvFile);
         }
 
     }
