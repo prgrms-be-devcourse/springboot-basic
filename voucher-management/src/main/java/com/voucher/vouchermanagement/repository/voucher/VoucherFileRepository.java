57c5013020c8bee92bae1bd6f2f4a8552545a53c
pjh612 <62292492+pjh612@users.noreply.github.com>
2022-04-17 23:34:51 +0900

    refactor : VoucherFileRepository 리팩토링
    FileIO와 Deserializer는 삭제되고 FileRepository 내부의 private 함수로 변경

--- voucher-management/src/main/java/com/voucher/vouchermanagement/repository/voucher/VoucherFileRepository.java
+++ voucher-management/src/main/java/com/voucher/vouchermanagement/repository/voucher/VoucherFileRepository.java
@@ -1,68 +1,122 @@
 package com.voucher.vouchermanagement.repository.voucher;
 
 import com.voucher.vouchermanagement.model.voucher.Voucher;
-import com.voucher.vouchermanagement.repository.io.FileInput;
-import com.voucher.vouchermanagement.repository.io.FileOutput;
-import com.voucher.vouchermanagement.repository.utils.CsvDeserializer;
+import com.voucher.vouchermanagement.model.voucher.VoucherType;
+import com.voucher.vouchermanagement.service.CreateVoucherDto;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Repository;
 
-import java.io.IOException;
-import java.util.Collections;
+import java.io.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.StringTokenizer;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 @Repository
 @Profile("prod")
 public class VoucherFileRepository implements VoucherRepository {
 
-    private final String voucherDbName = "voucher_db.csv";
-    private final Resource voucherDb = new ClassPathResource("db/" + voucherDbName);
-    private final FileOutput output;
-    private final FileInput input;
-    private final CsvDeserializer<Voucher> voucherDeserializer;
+    @Value("${db.path}")
+    private String dbDirectory;
+    @Value("${db.voucher.name}")
+    private String voucherDbName;
+    private final ResourceLoader resourceLoader;
     private static final Logger logger = LoggerFactory.getLogger(VoucherFileRepository.class);
 
-    public VoucherFileRepository(FileOutput output, FileInput input,
-                                 @Qualifier("voucherDeserializer") CsvDeserializer<Voucher> voucherDeserializer) {
-        this.output = output;
-        this.input = input;
-        this.voucherDeserializer = voucherDeserializer;
+    public VoucherFileRepository(ResourceLoader resourceLoader) {
+        this.resourceLoader = resourceLoader;
     }
 
     @Override
     public void insert(Voucher voucher) {
+        BufferedWriter voucherWriter = getBufferedWriter(this.dbDirectory, this.voucherDbName);
         try {
-            String voucherCsvData = String.join(",", voucher.getClass().getSimpleName(),
-                    voucher.getVoucherId().toString(),
-                    voucher.getValue().toString(),
-                    voucher.getCreatedAt().toString());
-
-            output.writeln(voucherDb.getFile(), voucherCsvData);
+            voucherWriter.write(csvSerialize(voucher));
+            voucherWriter.newLine();
+            voucherWriter.close();
         } catch (IOException e) {
-            logger.error(e.getMessage());
+            logAndPrintException(e);
         }
     }
 
     @Override
     public List<Voucher> findAll() {
-        try {
-            List<String> foundDataStrings = input.readAllLine(voucherDb.getFile());
+        BufferedReader voucherReader = getBufferedReader(this.dbDirectory, this.voucherDbName);
 
-            return foundDataStrings
-                    .stream()
-                    .map(voucherDeserializer::deserialize)
-                    .collect(Collectors.toList());
-        } catch (IOException e) {
-            logger.error(e.getMessage());
-        }
-
-        return Collections.emptyList();
+        return readAllFromReader(voucherReader)
+                .stream()
+                .map(this::csvDeserialize)
+                .collect(Collectors.toList());
     }
 
+    private String csvSerialize(Voucher voucher) {
+        return String.join(",", voucher.getClass().getSimpleName(),
+                voucher.getVoucherId().toString(),
+                voucher.getValue().toString(),
+                voucher.getCreatedAt().toString());
+    }
+
+    private Voucher csvDeserialize(String csvLine) {
+        StringTokenizer stringTokenizer = new StringTokenizer(csvLine, ",");
+        VoucherType voucherType = VoucherType.getVoucherTypeByName(stringTokenizer.nextToken().trim());
+        UUID id = UUID.fromString(stringTokenizer.nextToken().trim());
+        long value = Long.parseLong(stringTokenizer.nextToken().trim());
+        LocalDateTime createdAt = LocalDateTime.parse(stringTokenizer.nextToken());
+
+        return voucherType.create(new CreateVoucherDto(id, value, createdAt));
+    }
+
+    private void logAndPrintException(Exception e) {
+        this.logger.error(e.getMessage());
+        System.out.println(e.getMessage());
+    }
+
+    private BufferedReader getBufferedReader(String directory, String target) {
+        try {
+            File db = this.resourceLoader.getResource(directory + target).getFile();
+            FileReader fileReader = new FileReader(db);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            return bufferedReader;
+        } catch (IOException e) {
+            logAndPrintException(e);
+        }
+
+        return (BufferedReader) BufferedReader.nullReader();
+    }
+
+    private BufferedWriter getBufferedWriter(String directory, String target) {
+        try {
+            File db = this.resourceLoader.getResource(directory + target).getFile();
+            FileWriter fileWriter = new FileWriter(db, true);
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+
+            return bufferedWriter;
+        } catch (IOException e) {
+            logAndPrintException(e);
+        }
+
+        return (BufferedWriter) BufferedWriter.nullWriter();
+    }
+
+    private List<String> readAllFromReader(BufferedReader bufferedReader) {
+        List<String> list = new ArrayList<>();
+        String buffer = "";
+        try {
+            while ((buffer = bufferedReader.readLine()) != null) {
+                list.add(buffer);
+            }
+            bufferedReader.close();
+        } catch (IOException e) {
+            logAndPrintException(e);
+        }
+
+        return list;
+    }
 }
