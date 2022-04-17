d49bcaf8d3b329f933c148a64dd2865f356f0016
jki503 <jki503@naver.com>
2022-04-18 05:17:40 +0900

    fix : memberType.java 삭제 refactor : membertype을 임시로 string으로 변환

--- part1/src/main/java/com/programmers/part1/member/entity/Member.java
+++ part1/src/main/java/com/programmers/part1/member/entity/Member.java
@@ -7,9 +7,9 @@ public class Member {
 
     private final Long id;
     private final String name;
-    private final MemberType memberType;
+    private final String memberType;
 
-    public Member(long id, String name, MemberType memberType) {
+    public Member(long id, String name, String memberType) {
         this.id = id;
         this.name = name;
         this.memberType = memberType;
