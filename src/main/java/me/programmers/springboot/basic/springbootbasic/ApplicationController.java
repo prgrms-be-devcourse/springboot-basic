474667d612590ee74740cabf232245cdc21d8c54
Yanjoo <hanjub3@gmail.com>
2022-04-18 04:05:46 +0900

    SpringBootBasicApplicaion 내에 여러 실행 로직을 컨트롤러로 옮김

--- src/main/java/me/programmers/springboot/basic/springbootbasic/ApplicationController.java
+++ src/main/java/me/programmers/springboot/basic/springbootbasic/ApplicationController.java
@@ -1,14 +1,35 @@
 package me.programmers.springboot.basic.springbootbasic;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.io.Resource;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.List;
+
 public class ApplicationController {
 
-    private final CommandLineApplication commandLineApplication;
+    private final ConfigurableApplicationContext context;
 
-    ApplicationController(CommandLineApplication commandLineApplication) {
-        this.commandLineApplication = commandLineApplication;
+    public ApplicationController(SpringApplication springApplication, String[] args) {
+        springApplication.setAdditionalProfiles("default");
+        context = springApplication.run(args);
     }
 
     public void runCommandLineApp() {
+        CommandLineApplication commandLineApplication = context.getBean(CommandLineApplication.class);
         commandLineApplication.run();
+    }
+
+    public void showBlackList() {
+        try {
+            Resource resource = context.getResource("file:customer_blacklist.csv");
+            List<String> blackList = Files.readAllLines(resource.getFile().toPath());
+            System.out.println("customer blacklist " + blackList.stream().reduce("", (a, b) -> a + "\n" + b));
+        } catch (IOException e) {
+
+        }
+
     }
 }
--- src/main/java/me/programmers/springboot/basic/springbootbasic/SpringBootBasicApplication.java
+++ src/main/java/me/programmers/springboot/basic/springbootbasic/SpringBootBasicApplication.java
@@ -2,36 +2,13 @@ package me.programmers.springboot.basic.springbootbasic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.core.io.Resource;
-
-import java.io.IOException;
-import java.nio.file.Files;
-import java.util.List;
 
 @SpringBootApplication
 public class SpringBootBasicApplication {
 
 	public static void main(String[] args) {
-		SpringApplication springApplication = new SpringApplication(SpringBootBasicApplication.class);
-		springApplication.setAdditionalProfiles("default");
-		ConfigurableApplicationContext context = springApplication.run(args);
-
-		showBlackList(context);
-
-		CommandLineApplication app = context.getBean(CommandLineApplication.class);
-		ApplicationController controller = new ApplicationController(app);
+		ApplicationController controller = new ApplicationController(new SpringApplication(SpringBootBasicApplication.class), args);
+		controller.showBlackList();
 		controller.runCommandLineApp();
-	}
-
-	private static void showBlackList(ConfigurableApplicationContext context) {
-		try {
-			Resource resource = context.getResource("file:customer_blacklist.csv");
-			List<String> blackList = Files.readAllLines(resource.getFile().toPath());
-			System.out.println("customer blacklist " + blackList.stream().reduce("", (a, b) -> a + "\n" + b));
-		} catch (IOException e) {
-
-		}
-
 	}
 }
