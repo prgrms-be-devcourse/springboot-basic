3300b04377379325c060e8c922d9cc944218d110
UJ15 <dbwogml15@naver.com>
2022-04-17 22:01:20 +0900

    :recycle:  test : Context 구문에 "create 기능 테스트시" 문장 추가, 전달 받는 인자 명시

--- mission/src/test/java/org/prgrms/voucher/controller/VoucherControllerTest.java
+++ mission/src/test/java/org/prgrms/voucher/controller/VoucherControllerTest.java
@@ -11,7 +11,7 @@ public class VoucherControllerTest {
     class DescribeCreate{
 
         @Nested
-        @DisplayName("바우처 타입을 파라미터로 받으면")
+        @DisplayName("create 기능을 테스트 할 떄 바우처 타입을 파라미터로 받으면")
         class ContextReceiveVoucherTypeAndValue{
 
             @Test
@@ -28,22 +28,22 @@ public class VoucherControllerTest {
         }
 
         @Nested
-        @DisplayName("전달받은 인자가 null이면")
+        @DisplayName("create 기능을 테스트 할 때 전달받은 바우처타입 인자가 null이면")
         class ContextReceiveNull{
 
             @Test
-            @DisplayName("에러메시지를 출력한다.")
+            @DisplayName("널 에러메시지를 출력한다.")
             void itPrintNullError(){
 
             }
         }
 
         @Nested
-        @DisplayName("전달 받은 인자가 공백, 빈문자열이면")
+        @DisplayName("crete 기능을 테스트 할 때 전달 받은 바우처타입 인자가 공백, 빈문자열이면")
         class ContextWhiteSpace{
 
             @Test
-            @DisplayName("에러메시지를 출력한다.")
+            @DisplayName("공백 에러메시지를 출력한다.")
             void itPrintWhiteSpaceError(){
 
             }
