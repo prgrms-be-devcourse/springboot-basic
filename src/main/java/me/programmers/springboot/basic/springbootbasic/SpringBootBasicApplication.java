13571187afe51c2000de76734584d4044eefb282
Yanjoo <hanjub3@gmail.com>
2022-04-17 15:02:17 +0900

    고객 블랙 리스트(csv 파일) 조회

--- src/main/java/me/programmers/springboot/basic/springbootbasic/SpringBootBasicApplication.java
+++ src/main/java/me/programmers/springboot/basic/springbootbasic/SpringBootBasicApplication.java
@@ -3,6 +3,11 @@ package me.programmers.springboot.basic.springbootbasic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.io.Resource;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.List;
 
 @SpringBootApplication
 public class SpringBootBasicApplication {
@@ -11,11 +16,22 @@ public class SpringBootBasicApplication {
 		SpringApplication springApplication = new SpringApplication(SpringBootBasicApplication.class);
 		springApplication.setAdditionalProfiles("default");
 		ConfigurableApplicationContext context = springApplication.run(args);
-		CommandLineApplication app = context.getBean(CommandLineApplication.class);
 
+		showBlackList(context);
+
+		CommandLineApplication app = context.getBean(CommandLineApplication.class);
 		ApplicationController controller = new ApplicationController(app);
 		controller.runCommandLineApp();
 	}
 
+	private static void showBlackList(ConfigurableApplicationContext context) {
+		try {
+			Resource resource = context.getResource("file:customer_blacklist.csv");
+			List<String> blackList = Files.readAllLines(resource.getFile().toPath());
+			System.out.println("customer blacklist " + blackList.stream().reduce("", (a, b) -> a + "\n" + b));
+		} catch (IOException e) {
 
+		}
+
+	}
 }
