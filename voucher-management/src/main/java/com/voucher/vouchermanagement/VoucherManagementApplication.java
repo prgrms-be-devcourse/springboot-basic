14a6c418d55327bac4a60ca4d4792bb7789c58e8
pjh612 <62292492+pjh612@users.noreply.github.com>
2022-04-17 23:35:24 +0900

    refactor : 불필요 throws 삭제

--- voucher-management/src/main/java/com/voucher/vouchermanagement/VoucherManagementApplication.java
+++ voucher-management/src/main/java/com/voucher/vouchermanagement/VoucherManagementApplication.java
@@ -19,7 +19,7 @@ public class VoucherManagementApplication implements CommandLineRunner {
     }
 
     @Override
-    public void run(String... args) throws Exception {
+    public void run(String... args) {
         voucherManager.run();
     }
 }
