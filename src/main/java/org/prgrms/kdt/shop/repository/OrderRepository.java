80f55af57f0d0b12473f532dbb38cca11ba56a2e
HunkiKim <gnsrl76@naver.com>
2022-04-18 03:22:12 +0900

    refactor : delete dead code
    불필요한 코드 삭제.

--- src/main/java/org/prgrms/kdt/shop/repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package org.prgrms.kdt.shop.repository;
-
-import org.prgrms.kdt.shop.domain.Order;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrderRepository {
-    public Order insert(Order order);
-}
