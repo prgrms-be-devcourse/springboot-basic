c245e193bbb2d72e5fdf588267895b4176a90c85
dldydgns530@gmail.com <dldydgns530@gmail.com>
2022-04-17 16:37:41 +0900

    Input Output enum 변경에 따른 로직변경 - enum 의 of 를 통해 값 입력 확인 - Scanner close 부분 추가

--- src/main/java/org/prgrms/kdtspringdemo/console/Input.java
+++ src/main/java/org/prgrms/kdtspringdemo/console/Input.java
@@ -3,101 +3,45 @@ package org.prgrms.kdtspringdemo.console;
 
 import org.prgrms.kdtspringdemo.voucher.voucherdetail.VoucherType;
 
-import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
+@Component
 public class Input {
     private static final Logger logger = LoggerFactory.getLogger(Input.class);
-    Scanner scanner = new Scanner(System.in);
+    private Scanner scanner = new Scanner(System.in);
 
-    // menu 입력을 받는다
+    // Menu 입력을 받는다
     public Menu inputMenu() {
         String inputMenu = scanner.nextLine();
 
-        List<Menu> menus = Arrays.asList(Menu.values());
-        Menu menu = compareInputValueToMenuEnum(inputMenu, menus);
-
-        return menu;
+        return Menu.of(inputMenu);
     }
 
-    // Voucher Type 을 입력 받는다.
+    // VoucherType 입력을 받는다.
     public VoucherType inputVoucherType() {
         String inputVoucherType = scanner.nextLine();
 
-        List<VoucherType> voucherTypes = Arrays.asList(VoucherType.values());
-        VoucherType voucherType = compareInputValueToVoucherTypeEnum(inputVoucherType, voucherTypes);
-
-        return voucherType;
+        return VoucherType.of(inputVoucherType);
     }
 
-    // Menu Enum 과 비교하여 입력값을 비교하고 ERROR 경우도 처리해준다.
-    private VoucherType compareInputValueToVoucherTypeEnum(String inputVoucherType, List<VoucherType> voucherTypes) {
-        for (VoucherType type : voucherTypes) {
-            String typeName = type.getTypeName();
-            String typeNumber = type.getTypeNumber();
-            // 작성자가 error 혹은 0이라고 메뉴를 적는 경우에 대한 대처
-            if (inputVoucherType.equals("0") || inputVoucherType.equalsIgnoreCase("error")) {
-                logger.info("Voucher Type 작성에 있어 error 발생");
-
-                return VoucherType.ERROR;
-
-            } else if (inputVoucherType.equals(typeNumber) || inputVoucherType.equalsIgnoreCase(typeName)) {
-
-                return type;
-            }
-        }
-        // 전체 경우에 해당하지 않는 경우 ERROR 로 처리
-        return VoucherType.ERROR;
-    }
-
-
-    // Menu 선택을 Menu eunm 과 비교하여 입렵값을 비교하고 ERROR 경우도 처리해 준다.
-    private Menu compareInputValueToMenuEnum(String inputMenu, List<Menu> menus) {
-        for (Menu menu : menus) {
-            String menuName = menu.getMenuName();
-            // 작성자가 error 라고 메뉴를 적는 경우에 대한 대처
-            if (menuName.equalsIgnoreCase("Error")) {
-                logger.info("Menu 작성에 있어 error 발생");
-
-                return Menu.ERROR;
-
-            } else if (inputMenu.equalsIgnoreCase(menuName)) {
-
-                return menu;
-            }
-        }
-        // 전체 경우에 해당하지 않는 경우 ERROR 로 처리
-        return Menu.ERROR;
-    }
-
-    // Voucher 타입 선택 후 discount 금액 을 입력받습니다.
-    public int inputAmount(VoucherType voucherType) {
-        switch (voucherType) {
-            case FIXED -> logger.info("할인 금액을 적으시오(0보다 큰 정수값만 적으시오)");
-            case PERCENT -> logger.info("할인 퍼센트를 적으시오(0 과 100 사이의 정수)");
-        }
-
-        // discount 금액을 반환하기 위해서 ERROR 처리를 -1로 처리한다.
-        int discountNumber = -1;
+    // VoucherType 에 맞게 할인 금액을 입력 받는다.
+    public int inputDiscountAmount(VoucherType voucherType) {
+        int discountAmount = 0;
 
         try {
-            discountNumber = scanner.nextInt();
-            scanner.nextLine();
-        } catch (Exception e) {
-            logger.info("정수가 아닌 값을 입력하였습니다.");
-
-            return -1;
-        }
-        if (discountNumber < 0) {
-            logger.info("음수를 입력하였습니다.");
-
-            return -1;
+            discountAmount = Integer.parseInt(scanner.nextLine());
+        } catch (NumberFormatException e) {
+            logger.error("정수가 아닌 값을 입력하였습니다.");
         }
 
-        return discountNumber;
+        return discountAmount;
+    }
 
+    public void closeScanner() {
+        logger.info("Scanner 을 close 합니다.");
+        scanner.close();
     }
 }
--- src/main/java/org/prgrms/kdtspringdemo/console/Output.java
+++ src/main/java/org/prgrms/kdtspringdemo/console/Output.java
@@ -1,29 +1,41 @@
 package org.prgrms.kdtspringdemo.console;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Output {
     public String initMessage() {
-        String message = "=== Voucher Program ===\n" +
-                "Type exit to exit the program.\n" +
-                "Type create to create a new voucher.\n" +
-                "Type list to list all vouchers.\n" +
-                "write one of three word : (exit, create, list)";
-
-        return message;
+        return """
+                === Voucher Program ===
+                Type exit to exit the program.
+                Type create to create a new voucher.
+                Type list to list all vouchers.
+                """;
     }
 
-    public String createListMessage() {
-        String message = "=== Create Voucher ===\n" +
-                "FixedAmountVoucher = 1\n" +
-                "PercentDiscountVoucher = 2\n" +
-                "Write number or voucher name : ";
-
-        return message;
+    public String chooseVoucherTypeMessage() {
+        return """
+                === Choose Voucher ===
+                1. FixedAmountVoucher(Fixed)
+                2. PercentDiscountVoucher(Percent)
+                Type Fixed if you want 1
+                Type Percent if you want 2
+                """;
     }
 
-    public String showAllMessage() {
-        String message = "=== Show all Voucher ===\n" +
-                "These are all vouchers list";
+    public String FixedDiscountAmountMessage() {
+        return """
+                === Write Discount Amount ===
+                Type over 0
+                Type under 1,000,000
+                """;
+    }
 
-        return message;
+    public String PercentDiscountAmountMessage() {
+        return """
+                === Write Percent Amount ===
+                Type over 0
+                Type under 100
+                """;
     }
 }
