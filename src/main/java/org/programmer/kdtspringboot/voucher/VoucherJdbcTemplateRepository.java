44a9968b64c387ed5e7d53b3d24e7dedd8f6464a
unnokid <kimki0609@gmail.com>
2022-04-19 23:05:26 +0900

    Add @Repository

--- src/main/java/org/programmer/kdtspringboot/voucher/VoucherJdbcTemplateRepository.java
+++ src/main/java/org/programmer/kdtspringboot/voucher/VoucherJdbcTemplateRepository.java
@@ -4,12 +4,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
 
 import java.nio.ByteBuffer;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public class VoucherJdbcTemplateRepository implements VoucherRepository {
 
     private static final Logger logger = LoggerFactory.getLogger(VoucherJdbcTemplateRepository.class);
