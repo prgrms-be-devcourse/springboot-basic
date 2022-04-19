1da9c9fc9e594f09d5ac4472fd3b4b289c8bc2b2
dbslzld15 <dbslzld15@naver.com>
2022-04-19 16:13:11 +0900

    feat: 고객 생성 테스트 코드 구현

---
+++ src/test/java/org/prgrms/kdt/domain/customer/model/CustomerTest.java
@@ -0,0 +1,39 @@
+package org.prgrms.kdt.domain.customer.model;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class CustomerTest {
+
+    @Test
+    public void createCustomer() {
+        //given
+        UUID customerId = UUID.randomUUID();
+        String name = "park";
+        String email = "db2@naver.com";
+        LocalDateTime now = LocalDateTime.now();
+        //when
+        Customer customer = new Customer(customerId, name, email, now, now);
+        //then
+        assertThat(customer.getCustomerId()).isEqualTo(customerId);
+    }
+
+    @Test
+    public void createCustomer_exception() {
+        //given
+        UUID customerId = UUID.randomUUID();
+        String name = "park";
+        String email = "d123@";
+        LocalDateTime now = LocalDateTime.now();
+        //when
+        //then
+        assertThatThrownBy(() -> new Customer(customerId, name, email, now, now))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("이메일 주소를 확인해주세요");
+    }
+}
