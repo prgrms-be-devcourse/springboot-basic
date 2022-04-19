36330f8b42f018639aee5ab9b88a3a53efada962
San Kim <waterfog9580@gmail.com>
2022-04-19 23:12:02 +0900

    :fire: Fix: 에러 throw 제거

--- src/main/java/com/waterfogsw/voucher/voucher/service/VoucherService.java
+++ src/main/java/com/waterfogsw/voucher/voucher/service/VoucherService.java
@@ -3,5 +3,5 @@ package com.waterfogsw.voucher.voucher.service;
 import com.waterfogsw.voucher.voucher.domain.Voucher;
 
 public interface VoucherService {
-    Voucher saveVoucher(Voucher voucher) throws IllegalArgumentException;
+    Voucher saveVoucher(Voucher voucher);
 }
