ce1456c5fe987fdb40809879f05f2bd7da031651
Yanjoo <hanjub3@gmail.com>
2022-04-20 23:37:54 +0900

    Console Output Customer List 파라미터 출력 기능 추가

--- src/main/java/me/programmers/springboot/basic/springbootbasic/io/ConsoleOutput.java
+++ src/main/java/me/programmers/springboot/basic/springbootbasic/io/ConsoleOutput.java
@@ -1,6 +1,12 @@
 package me.programmers.springboot.basic.springbootbasic.io;
 
+import me.programmers.springboot.basic.springbootbasic.customer.model.Customer;
+
+import java.util.List;
+
 public interface ConsoleOutput {
 
     void output(String s);
+    void output(List<Customer> list);
+
 }
--- src/main/java/me/programmers/springboot/basic/springbootbasic/io/Out.java
+++ src/main/java/me/programmers/springboot/basic/springbootbasic/io/Out.java
@@ -1,9 +1,23 @@
 package me.programmers.springboot.basic.springbootbasic.io;
 
+import me.programmers.springboot.basic.springbootbasic.customer.model.Customer;
+
+import java.util.List;
+
 public class Out implements ConsoleOutput {
 
     @Override
     public void output(String message) {
         System.out.println(message);
+    }
+
+    @Override
+    public void output(List<Customer> list) {
+        list.forEach(customer -> {
+            System.out.println("Customer Id: " + customer.getCustomerId());
+            System.out.println("Customer name: " + customer.getName());
+            System.out.println("Customer email: " + customer.getEmail());
+            System.out.println();
+        });
     }
 }
