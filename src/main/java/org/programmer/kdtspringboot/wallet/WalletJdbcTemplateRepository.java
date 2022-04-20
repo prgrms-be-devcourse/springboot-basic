905e6f97f557a639006ab14d0196f47afc332ce1
unnokid <kimki0609@gmail.com>
2022-04-20 23:09:33 +0900

    Add `deleteVoucher()` and `findByWalletId()`

--- src/main/java/org/programmer/kdtspringboot/wallet/WalletJdbcTemplateRepository.java
+++ src/main/java/org/programmer/kdtspringboot/wallet/WalletJdbcTemplateRepository.java
@@ -2,14 +2,18 @@ package org.programmer.kdtspringboot.wallet;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
+import java.nio.ByteBuffer;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
-public class WalletJdbcTemplateRepository implements WalletRepository{
+public class WalletJdbcTemplateRepository implements WalletRepository {
 
     private static final Logger logger = LoggerFactory.getLogger(WalletJdbcTemplateRepository.class);
     private final JdbcTemplate jdbcTemplate;
@@ -18,29 +22,75 @@ public class WalletJdbcTemplateRepository implements WalletRepository{
         this.jdbcTemplate = jdbcTemplate;
     }
 
+    private static RowMapper<Wallet> walletRowMapper = (resultSet, rowNum) -> {
+        var walletId = toUUID(resultSet.getBytes("wallet_id"));
+        var customerId = toUUID(resultSet.getBytes("customer_id"));
+        var voucherId = toUUID(resultSet.getBytes("voucher_id"));
+        return new Wallet(walletId, customerId, voucherId);
+    };
 
     @Override
     public Wallet insert(Wallet wallet) {
-        return null;
+        var update = jdbcTemplate.update(
+                "insert into wallet(wallet_id, customer_id, voucher_id ) values (UNHEX(REPLACE(?, '-', '')), UNHEX(REPLACE(?, '-', '')), UNHEX(REPLACE(?, '-', '')))",
+                wallet.getWalletId().toString().getBytes(),
+                wallet.getCustomerId().toString().getBytes(),
+                wallet.getVoucherId().toString().getBytes()
+        );
+        if (update != 1) {
+            throw new RuntimeException("Noting was inserted");
+        }
+        return wallet;
+    }
+
+    @Override
+    public Optional<Wallet> findByWalletId(UUID walletId) {
+        try{
+            return Optional.ofNullable(jdbcTemplate.queryForObject(
+                    "select * from wallet where wallet_id = UNHEX(REPLACE(?, '-', ''))",
+                    walletRowMapper,
+                    walletId.toString().getBytes()));
+        }catch (EmptyResultDataAccessException e){
+            logger.error("Got empty result",e);
+            return Optional.empty();
+        }
     }
 
     @Override
     public List<Wallet> findByCustomerId(UUID customerId) {
-        return null;
+        return jdbcTemplate.query(
+                "select * from wallet where customer_id = UNHEX(REPLACE(?, '-', ''))",
+                walletRowMapper,
+                customerId.toString().getBytes()
+        );
     }
 
     @Override
     public List<Wallet> findByVoucherId(UUID voucherId) {
-        return null;
+        return jdbcTemplate.query(
+                "select * from wallet where voucher_id = UNHEX(REPLACE(?, '-', ''))",
+                walletRowMapper,
+                voucherId.toString().getBytes()
+        );
     }
 
     @Override
     public List<Wallet> findAll() {
-        return null;
+        return jdbcTemplate.query(
+                "select * from wallet",
+                walletRowMapper
+        );
     }
 
     @Override
-    public void deleteVoucher(Wallet wallet) {
+    public void deleteVoucher(UUID voucherId) {
+        jdbcTemplate.update("delete from wallet where voucher_id = UNHEX(REPLACE(?, '-', ''))",
+                voucherId.toString().getBytes()
+        );
+    }
 
+    static UUID toUUID(byte[] bytes) {
+        var byteBuffer = ByteBuffer.wrap(bytes);
+        return new UUID(byteBuffer.getLong(), byteBuffer.getLong());
     }
 }
--- src/main/java/org/programmer/kdtspringboot/wallet/WalletRepository.java
+++ src/main/java/org/programmer/kdtspringboot/wallet/WalletRepository.java
@@ -1,13 +1,15 @@
 package org.programmer.kdtspringboot.wallet;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface WalletRepository {
 
     Wallet insert(Wallet wallet);
+    Optional<Wallet> findByWalletId(UUID walletId);
     List<Wallet> findByCustomerId(UUID customerId);
     List<Wallet> findByVoucherId(UUID voucherId);
     List<Wallet> findAll();
-    void deleteVoucher(Wallet wallet);
+    void deleteVoucher(UUID voucherId);
 }
