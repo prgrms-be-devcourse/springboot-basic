cdfdf572caf1df67eb2409d719083ed9cecf4b10
midas <midas117@knou.ac.kr>
2022-04-20 10:24:08 +0900

    feat: Yaml 파일(application.yml) 정보 읽어올 수 있도록 YamlPropertiesFactory 생성, AppConfiguration 설정 추가

--- src/main/java/org/prgrms/kdt/configuration/AppConfiguration.java
+++ src/main/java/org/prgrms/kdt/configuration/AppConfiguration.java
@@ -1,9 +1,13 @@
 package org.prgrms.kdt.configuration;
 
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @ComponentScan(basePackages = "org.prgrms.kdt")
+@PropertySource(value = "application.yml", factory = YamlPropertiesFactory.class)
+@EnableConfigurationProperties
 public class AppConfiguration {
 }
