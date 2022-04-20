50e178bc21a60afddacff0bad90eb7d763fc97d2
ichoi <ichoi@student.42seoul.kr>
2022-04-21 03:31:13 +0900

    feat:createFile 메소드를 구현하라

--- src/main/java/org/programmers/springbootbasic/voucher/repository/FileVoucherRepository.java
+++ src/main/java/org/programmers/springbootbasic/voucher/repository/FileVoucherRepository.java
@@ -1,13 +1,16 @@
 package org.programmers.springbootbasic.voucher.repository;
 
 import org.programmers.springbootbasic.voucher.model.Voucher;
+import org.programmers.springbootbasic.voucher.model.VoucherDTO;
 import org.programmers.springbootbasic.voucher.model.VoucherType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Repository;
 
 import java.io.*;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -18,25 +21,39 @@ import java.util.UUID;
 public class FileVoucherRepository implements VoucherRepository {
     private static final Logger logger = LoggerFactory.getLogger(FileVoucherRepository.class);
 
-    private final File file = new File("voucher_file_database.csv");
-    VoucherType voucherType;
+    @Value("${path.voucher}")
+    private String file;
+
+    private VoucherType voucherType;
+
+
+    public File createFile(File file) {
+        try (
+                FileOutputStream fileOutputStream = new FileOutputStream(file, true);
+        ) {
+        } catch (IOException e) {
+            logger.error("IOException 에러입니다. {0}", e);
+        }
+        return file;
+    }
+
 
     @Override
     public Optional<Voucher> findById(UUID voucherId) {
         String line;
         try (
-                FileReader fileReader = new FileReader(file);
+                FileReader fileReader = new FileReader(createFile(new File(file)));
                 BufferedReader bufferedReader = new BufferedReader(fileReader);
         ) {
-            while ((line = bufferedReader.readLine()) != null) {
+            while ((line = bufferedReader.readLine())!=null) {
                 String[] voucherInfo = line.split(" ");
                 if (voucherInfo[2].equals(voucherId.toString())) {
                     voucherType = VoucherType.findByType(voucherInfo[0]);
-                    return Optional.of(voucherType.createWithUUID(UUID.fromString(voucherInfo[2]), Long.parseLong(voucherInfo[4])));
+                    return Optional.of(voucherType.create(new VoucherDTO(UUID.fromString(voucherInfo[2]), Long.parseLong(voucherInfo[4]), LocalDateTime.parse(voucherInfo[6]))));
                 }
             }
         } catch (IOException e) {
-            logger.error("아이디로 찾기 IOException 에러입니다. {0}", e);
+            logger.error("IOException 에러입니다. {0}", e);
         }
         return Optional.empty();
     }
@@ -62,16 +79,19 @@ public class FileVoucherRepository implements VoucherRepository {
 
         String line;
         try (
-                FileReader fileReader = new FileReader(file);
+                FileReader fileReader = new FileReader(createFile(new File(file)));
                 BufferedReader bufferedReader = new BufferedReader(fileReader);
         ) {
-            while ((line = bufferedReader.readLine()) != null) {
+            while ((line = bufferedReader.readLine())!=null) {
                 String[] voucherInfo = line.split(" ");
                 voucherType = VoucherType.findByType(voucherInfo[0]);
-                voucherList.add(voucherType.createWithUUID(UUID.fromString(voucherInfo[2]), Long.parseLong(voucherInfo[4])));
+                voucherList.add(voucherType.create(new VoucherDTO(UUID.fromString(voucherInfo[2]), Long.parseLong(voucherInfo[4]), LocalDateTime.parse(voucherInfo[6]))));
             }
-        } catch (IOException e) {
-            logger.error("모두 찾기 IOException 에러입니다. {0}r", e);
+        } catch (FileNotFoundException e) {
+            logger.warn("FileNotFoundException 입니다.", e);
+        }
+        catch (IOException e) {
+            logger.error("모두 찾기 IOException 에러입니다. {0}", e);
         }
         return voucherList;
     }
