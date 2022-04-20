db4da5d27904fbdd1a41d861a36ef6dc7e98e3fb
kimziou77 <kimziou77@naver.com>
2022-04-20 18:23:11 +0900

    (Remove) 바우처 구상체 제거
    * 기존 구체화는 VoucherType(FIXED/PERCENT)에서 처리
    
    Modified : * DiscountVoucher

--- voucher-project/src/main/java/com/example/voucherproject/voucher/domain/PercentDiscountVoucher.java
+++
@@ -1,9 +0,0 @@
-package com.example.voucherproject.voucher.domain;
-import com.example.voucherproject.common.enums.VoucherType;
-import java.util.UUID;
-
-public class PercentDiscountVoucher extends Voucher{
-    public PercentDiscountVoucher(UUID id, VoucherType type) {
-        super(id, type);
-    }
-}
