ac2a683845d845590449b781b6900674bc6ea3aa
beomsic <qjatjr29@khu.ac.kr>
2022-04-20 10:01:17 +0900

    리뷰 반영 : 커스텀 예외 네이밍 변경

--- src/main/java/com/prgrms/voucher_manager/exception/EmptyVoucherException.java
+++
@@ -1,7 +0,0 @@
-package com.prgrms.voucher_manager.exception;
-
-public class EmptyVoucherException extends RuntimeException{
-    public EmptyVoucherException(String message) {
-        super(message);
-    }
-}
