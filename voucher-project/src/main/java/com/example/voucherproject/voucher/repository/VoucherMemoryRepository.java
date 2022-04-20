ecb7ba9b0be98978a797db247af4f20917940a14
kimziou77 <kimziou77@naver.com>
2022-04-20 18:21:11 +0900

    (Feat) 바우처 DB 관리 기능
    # VoucherJdbcRepository 저장기능 추가
    # VoucherRepository 인터페이스 변경
    
    Modified : Voucher * Repository, VoucherService

--- voucher-project/src/main/java/com/example/voucherproject/voucher/repository/VoucherMemoryRepository.java
+++ voucher-project/src/main/java/com/example/voucherproject/voucher/repository/VoucherMemoryRepository.java
@@ -1,5 +1,6 @@
 package com.example.voucherproject.voucher.repository;
 
+import com.example.voucherproject.voucher.enums.VoucherType;
 import com.example.voucherproject.voucher.domain.Voucher;
 
 import java.util.*;
@@ -8,13 +9,33 @@ public class VoucherMemoryRepository implements VoucherRepository{
     private final Map<UUID, Voucher> voucherMap = new HashMap<>();
 
     @Override
-    public Voucher save(Voucher voucher) {
+    public Voucher insert(Voucher voucher) {
         voucherMap.put(voucher.getId(),voucher);
         return voucher;
     }
 
     @Override
-    public List<Voucher> getList() {
+    public List<Voucher> findHavingTypeAll(VoucherType type) {
+        return null;
+    }
+
+    @Override
+    public List<Voucher> findAll() {
         return new ArrayList<>(voucherMap.values());
+    }
+
+    @Override
+    public Optional<Voucher> findById(UUID voucherId) {
+        return Optional.empty();
+    }
+
+    @Override
+    public int deleteAll() {
+        return 0;
+    }
+
+    @Override
+    public long count() {
+        return 0;
     }
 }
--- voucher-project/src/main/java/com/example/voucherproject/voucher/repository/VoucherRepository.java
+++ voucher-project/src/main/java/com/example/voucherproject/voucher/repository/VoucherRepository.java
@@ -1,8 +1,25 @@
 package com.example.voucherproject.voucher.repository;
+import com.example.voucherproject.voucher.enums.VoucherType;
 import com.example.voucherproject.voucher.domain.Voucher;
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 public interface VoucherRepository {
-    List<Voucher> getList();
-    Voucher save(Voucher voucher);
+
+    //create
+    Voucher insert(Voucher voucher);
+
+    //read
+    List<Voucher> findHavingTypeAll(VoucherType type);
+    List<Voucher> findAll();
+
+    Optional<Voucher> findById(UUID voucherId);
+
+    //update
+
+    //delete
+    int deleteAll();
+
+    long count();
 }
