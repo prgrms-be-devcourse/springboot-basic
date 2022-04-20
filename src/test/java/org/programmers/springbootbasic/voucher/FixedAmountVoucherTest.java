f2dbde57ecb1a1eea611ea0d5d36fc08c51bff01
ichoi <ichoi@student.42seoul.kr>
2022-04-21 03:41:58 +0900

    test:테스트를 추가하고 BDD로 변경하라

--- src/test/java/org/programmers/springbootbasic/voucher/FixedAmountVoucherTest.java
+++ src/test/java/org/programmers/springbootbasic/voucher/FixedAmountVoucherTest.java
@@ -1,38 +1,95 @@
 package org.programmers.springbootbasic.voucher;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.programmers.springbootbasic.voucher.model.FixedAmountVoucher;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+
+@DisplayName("FixedAmountVoucher 클래스")
 class FixedAmountVoucherTest {
 
-    @Test
-    @DisplayName("주어진 금액만큼 할인을 해야한다.")
-    void testDiscount() {
-        var fixedAmountVoucher = new FixedAmountVoucher(UUID.randomUUID(), 100);
+    @Nested
+    @DisplayName("discount 메소드는")
+    class Discount_of {
+        private final FixedAmountVoucher fixedAmountVoucher = new FixedAmountVoucher(UUID.randomUUID(), 100, LocalDateTime.now());
 
-        assertEquals(900, fixedAmountVoucher.discount(1000));
+        @Nested
+        @DisplayName("amount가 할인 가능한 값일때.")
+        class Context_with_discount_able {
+
+            @Test
+            @DisplayName("입력 값에서 amount만큼 할인된 값을 반환합니다.")
+            void it_returns_a_discount_value() {
+                assertThat(fixedAmountVoucher.discount(1000)).isEqualTo(900);
+            }
+        }
+
+        @Nested
+        @DisplayName("amount가 할인 가능한 값보다 클 때")
+        class Context_with_discount_unable {
+
+            @Test
+            @DisplayName("예외를 던집니다.")
+            void it_returns_a_throw() {
+                assertThatThrownBy(() -> {
+                    fixedAmountVoucher.discount(90);
+                }).isInstanceOf(ArithmeticException.class)
+                        .hasMessageContaining("결제 금액보다 할인 금액이 더 큽니다.");
+            }
+        }
     }
 
-    @Test
-    @DisplayName("디스 카운트된 금액은 마이너스가 될 수 없다.")
-    void minusDiscountedAmountTest() {
-        var fixedAmountVoucher = new FixedAmountVoucher(UUID.randomUUID(), 1000);
+    @Nested
+    @DisplayName("바우처를 생성할 때")
+    class Context_with_create_voucher {
 
-        assertEquals(0, fixedAmountVoucher.discount(900));
-    }
+        @Nested
+        @DisplayName("amount가 양수값이고 MAX_AMOUNT보다 작다면")
+        class Context_with_able_amount{
+            private final FixedAmountVoucher fixedAmountVoucher = new FixedAmountVoucher(UUID.randomUUID(), 100, LocalDateTime.now());
 
-    @Test
-    @DisplayName("유효한 할인 금액으로만 생성할 수 있다.")
-    void voucherCreationTest() {
-        assertAll("FixedAmountVoucher creation",
-                () ->  assertThrows(IllegalArgumentException.class, () -> new FixedAmountVoucher(UUID.randomUUID(), -100)),
-                () ->  assertThrows(IllegalArgumentException.class, () -> new FixedAmountVoucher(UUID.randomUUID(), 0)),
-                () ->  assertThrows(IllegalArgumentException.class, () -> new FixedAmountVoucher(UUID.randomUUID(), 1000000))
-        );
+            @Test
+            @DisplayName("생성에 성공합니다.")
+            void it_returns_true() {
+                assertThat(new FixedAmountVoucher(fixedAmountVoucher.getVoucherId(), 100, LocalDateTime.now()).getVoucherId()).isEqualTo(fixedAmountVoucher.getVoucherId());
+            }
+        }
+
+        @Nested
+        @DisplayName("amount가 음수값이라면")
+        class Context_with_minus {
+
+            @Test
+            @DisplayName("예외를 던집니다.")
+            void it_returns_a_throw() {
+                assertThatThrownBy(() -> {
+                    new FixedAmountVoucher(UUID.randomUUID(), -100, LocalDateTime.now());
+                })
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessageContaining("할인된 금액은 0보다 커야 합니다.");
+            }
+        }
+        @Nested
+        @DisplayName("amount가 MAX_AMOUNT보다 큰 값이라면")
+        class Context_with_more_than_max_amount {
+            private static final long MAX_VOUCHER_AMOUNT = 10000;
+
+            @Test
+            @DisplayName("예외를 던집니다.")
+            void it_returns_a_throw() {
+                assertThatThrownBy(() -> {
+                    new FixedAmountVoucher(UUID.randomUUID(), 1000000, LocalDateTime.now());
+                })
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessageContaining("할인금액은 %d보다 작아야 합니다.".formatted(MAX_VOUCHER_AMOUNT));
+            }
+        }
     }
 }
