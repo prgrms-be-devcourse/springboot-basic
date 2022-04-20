9a03b18d478401fb49ba4fc20654bdbfe2f5f4e3
ichoi <ichoi@student.42seoul.kr>
2022-04-21 03:22:21 +0900

    feat:file이 없는 경우의 에러를 수정하라
    필요없는 getBufferedReader 메소드 삭제
    createFile 메소드 구현
    file의 경로를 yaml에서 관리하도록 수정

--- src/main/java/org/programmers/springbootbasic/customer/repository/BlackCustomerRepository.java
+++ src/main/java/org/programmers/springbootbasic/customer/repository/BlackCustomerRepository.java
@@ -4,6 +4,7 @@ import org.programmers.springbootbasic.customer.model.BlackListCustomer;
 import org.programmers.springbootbasic.customer.model.Customer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 
 import java.io.*;
@@ -15,16 +16,28 @@ import java.util.UUID;
 public class BlackCustomerRepository implements CustomerRepository {
     private static final Logger logger = LoggerFactory.getLogger(BlackCustomerRepository.class);
 
-    private final File file = new File("customer_blacklist.csv");
-    private BufferedReader bufferedReader;
+    @Value("${path.blacklist}")
+    private String file;
+
+    public File createFile(File file) {
+        try (
+                FileOutputStream fileOutputStream = new FileOutputStream(file, true);
+        ) {
+        } catch (IOException e) {
+            logger.error("IOException 에러입니다. {0}", e);
+        }
+        return file;
+    }
 
     @Override
     public List<Customer> findAll() {
         List<Customer> blackCustomerList = new ArrayList<>();
 
         String line;
-        try {
-            bufferedReader = getBufferedReader(file);
+        try (
+                FileReader fileReader = new FileReader(createFile(new File(file)));
+                BufferedReader bufferedReader = new BufferedReader(fileReader);
+        ) {
             while ((line = bufferedReader.readLine()) != null) {
                 String[] blackListCustomerInfo = line.split(" ");
                 if (blackListCustomerInfo[0].equals("BlackListCustomer")) {
@@ -33,19 +46,12 @@ public class BlackCustomerRepository implements CustomerRepository {
                     blackCustomerList.add(blackListCustomer);
                 }
             }
+        } catch (FileNotFoundException e) {
+            logger.error("FileNotFoundException error");
         } catch (IOException e) {
             logger.error("find IO exception error");
         }
         return blackCustomerList;
-    }
-
-    private BufferedReader getBufferedReader(File file) {
-        try {
-            bufferedReader = new BufferedReader(new FileReader(file));
-        } catch (FileNotFoundException e) {
-            logger.error("FileNotFoundException error");
-        }
-        return bufferedReader;
     }
 }
 
