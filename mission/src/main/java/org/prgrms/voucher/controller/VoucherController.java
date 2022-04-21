e59460f22f41087b0b9fdc403beef3b3d7626df0
UJ15 <dbwogml15@naver.com>
2022-04-22 01:31:50 +0900

    :hammer:  feat :  컨트롤러 create 기능 구현

--- mission/src/main/java/org/prgrms/voucher/controller/VoucherController.java
+++ mission/src/main/java/org/prgrms/voucher/controller/VoucherController.java
@@ -1,11 +1,18 @@
 package org.prgrms.voucher.controller;
 
 import org.prgrms.voucher.dto.VoucherDto;
+import org.prgrms.voucher.models.Voucher;
+import org.prgrms.voucher.models.VoucherType;
+import org.prgrms.voucher.response.ResponseState;
 import org.prgrms.voucher.service.VoucherService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class VoucherController {
+
+    private final Logger logger = LoggerFactory.getLogger(VoucherController.class);
     private final VoucherService voucherService;
 
     public VoucherController(VoucherService voucherService) {
@@ -15,6 +22,16 @@ public class VoucherController {
 
     public VoucherDto.CreateVoucherResponse create(VoucherDto.CreateVoucherRequest requestDto) {
 
-        return null;
+        try {
+            Voucher voucher = voucherService.create(requestDto);
+
+            return new VoucherDto.CreateVoucherResponse(voucher.getVoucherId(),
+                    voucher.getDiscountValue(), voucher.getVoucherType());
+        } catch (NullPointerException nullPointerException) {
+            logger.info("bad Request...");
+
+            return new VoucherDto.CreateVoucherResponse(-1L, -1,
+                    VoucherType.EXCEPTION, ResponseState.BAD_REQUEST);
+        }
     }
 }
