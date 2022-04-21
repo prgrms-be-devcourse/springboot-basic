b88565191496191569b5430349479cb8a136ed4f
ynoolee <heerarea129@gmail.com>
2022-04-21 17:22:42 +0900

    Refactor : 매핑 코드값을 리턴하는 기능을 갖는 것으로CodeMappable 인터페이스 변경

--- src/main/java/com/prgrms/vouchermanagement/commons/CodeMappable.java
+++ src/main/java/com/prgrms/vouchermanagement/commons/CodeMappable.java
@@ -1,5 +1,8 @@
 package com.prgrms.vouchermanagement.commons;
 
+/**
+ * 해당 객체에 매핑하는 코드를 리턴 할 것으로 기대할 수 있다.
+ * */
 public interface CodeMappable {
-	boolean isMappedType(String target);
+	String getMappingCode();
 }
