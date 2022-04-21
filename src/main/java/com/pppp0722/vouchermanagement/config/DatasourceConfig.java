8b1a2958fb41f7182ce65080a72c6569dcd24c30
pppp0722 <poj0722@naver.com>
2022-04-21 23:34:08 +0900

    [add] Datasource 설정

---
+++ src/main/java/com/pppp0722/vouchermanagement/config/DatasourceConfig.java
@@ -0,0 +1,37 @@
+package com.pppp0722.vouchermanagement.config;
+
+import com.zaxxer.hikari.HikariDataSource;
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+
+@Configuration
+public class DatasourceConfig {
+
+    @Value("${database.url}")
+    private String url;
+    @Value("${database.username}")
+    private String username;
+    @Value("${database.password}")
+    private String password;
+
+    @Bean
+    public DataSource dataSource() {
+        HikariDataSource dataSource = DataSourceBuilder.create()
+            .url(url)
+            .username(username)
+            .password(password)
+            .type(HikariDataSource.class)
+            .build();
+
+        return dataSource;
+    }
+
+    @Bean
+    public NamedParameterJdbcTemplate namedParameterJdbcTemplate(DataSource dataSource) {
+        return new NamedParameterJdbcTemplate(dataSource);
+    }
+}
