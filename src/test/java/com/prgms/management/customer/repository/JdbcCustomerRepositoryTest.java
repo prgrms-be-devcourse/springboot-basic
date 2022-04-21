e855b381c82513e21c3b0b15b99a04a1d4fd33b3
moosongsong <songe08@gmail.com>
2022-04-21 23:36:28 +0900

    test: 테스트 설명 구체적으로 수정
    https://github.com/prgrms-be-devcourse/springboot-basic/pull/256#discussion_r852046216

--- src/test/java/com/prgms/management/customer/repository/JdbcCustomerRepositoryTest.java
+++ src/test/java/com/prgms/management/customer/repository/JdbcCustomerRepositoryTest.java
@@ -37,153 +37,159 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @TestClassOrder(ClassOrderer.OrderAnnotation.class)
 @ActiveProfiles("default")
-@DisplayName("JdbcCustomerRepositoryTest 유닛 테스트")
+@DisplayName("JdbcCustomerRepository 유닛 테스트")
 class JdbcCustomerRepositoryTest {
-    @Autowired
-    CustomerRepository customerRepository;
-
-    @Autowired
-    DataSource dataSource;
-
-    EmbeddedMysql embeddedMysql;
-    List<Customer> customers = new ArrayList<>();
-
-    @BeforeAll
-    void setUp() {
-        MysqldConfig config = aMysqldConfig(v5_7_latest)
-                .withCharset(UTF8)
-                .withPort(2215)
-                .withUser("test", "1234")
-                .withTimeZone("Asia/Seoul")
-                .withTimeout(2, TimeUnit.MINUTES)
-                .build();
-
-        embeddedMysql = anEmbeddedMysql(config)
-                .addSchema("demo", ScriptResolver.classPathScript("schema.sql"))
-                .start();
+  @Autowired
+  CustomerRepository customerRepository;
+  
+  @Autowired
+  DataSource dataSource;
+  
+  EmbeddedMysql embeddedMysql;
+  List<Customer> customers = new ArrayList<>();
+  
+  @BeforeAll
+  void setUp() {
+    MysqldConfig config = aMysqldConfig(v5_7_latest)
+      .withCharset(UTF8)
+      .withPort(2215)
+      .withUser("test", "1234")
+      .withTimeZone("Asia/Seoul")
+      .withTimeout(2, TimeUnit.MINUTES)
+      .build();
+    
+    embeddedMysql = anEmbeddedMysql(config)
+      .addSchema("demo", ScriptResolver.classPathScript("schema.sql"))
+      .start();
+  }
+  
+  @AfterAll
+  void cleanUp() {
+    embeddedMysql.stop();
+  }
+  
+  @Configuration
+  @ComponentScan
+  static class Config {
+    @Bean
+    public DataSource dataSource() {
+      return DataSourceBuilder.create()
+        .url("jdbc:mysql://localhost:2215/demo")
+        .username("test")
+        .password("1234")
+        .type(HikariDataSource.class)
+        .build();
     }
-
-    @AfterAll
-    void cleanUp() {
-        embeddedMysql.stop();
+    
+    @Bean
+    public NamedParameterJdbcTemplate namedParameterJdbcTemplate(DataSource dataSource) {
+      return new NamedParameterJdbcTemplate(dataSource);
     }
-
-    @Configuration
-    @ComponentScan
-    static class Config {
-        @Bean
-        public DataSource dataSource() {
-            return DataSourceBuilder.create()
-                    .url("jdbc:mysql://localhost:2215/demo")
-                    .username("test")
-                    .password("1234")
-                    .type(HikariDataSource.class)
-                    .build();
-        }
-
-        @Bean
-        public NamedParameterJdbcTemplate namedParameterJdbcTemplate(DataSource dataSource) {
-            return new NamedParameterJdbcTemplate(dataSource);
-        }
+  }
+  
+  @DisplayName("save() : 고객 저장 테스트")
+  @Nested
+  @Order(1)
+  @TestMethodOrder(MethodOrderer.DisplayName.class)
+  class SaveTest {
+    @DisplayName("성공 : WHITE/BLACK 타입으로 고객 정보가 지정된 경우 저장에 성공합니다.")
+    @ParameterizedTest(name = "{0} 고객 정보 저장 성공")
+    @CsvSource({"kate, WHITE, kate@gmail.com", "jade, WHITE, jade@gmail.com", "sage, BLACK, sage@gmail.com", "haes, " +
+      "BLACK, haes@gmail.com"})
+    void saveSuccess(String name, String typeStr, String email) {
+      Customer newCustomer = new Customer(name, CustomerType.valueOf(typeStr), email);
+      customers.add(customerRepository.save(newCustomer));
+      
+      var retrievedCustomer = customerRepository.findById(newCustomer.getId());
+      assertThat(retrievedCustomer, not(nullValue()));
+      assertThat(retrievedCustomer, equalTo(newCustomer));
     }
-
-    @DisplayName("save() 테스트")
-    @Nested
-    @Order(1)
-    class SaveTest {
-        @DisplayName("고객 정보 저장 성공 사례")
-        @ParameterizedTest(name = "{0} 고객 정보 저장")
-        @CsvSource({"kate, WHITE, kate@gmail.com", "jade, WHITE, jade@gmail.com", "sage, BLACK, sage@gmail.com", "haes, BLACK, haes@gmail.com"})
-        void saveSuccess(String name, String typeStr, String email) {
-            Customer newCustomer = new Customer(name, CustomerType.valueOf(typeStr), email);
-            customers.add(customerRepository.save(newCustomer));
-
-            var retrievedCustomer = customerRepository.findById(newCustomer.getId());
-            assertThat(retrievedCustomer, not(nullValue()));
-            assertThat(retrievedCustomer, equalTo(newCustomer));
-        }
-
-        @DisplayName("고객 정보 저장 실패 사례")
-        @Test
-        void saveFail() {
-            assertThrows(CustomerException.class, () -> {
-                Customer newCustomer = new Customer("test", CustomerType.NONE, "test@test.co");
-                customers.add(customerRepository.save(newCustomer));
-            });
-        }
+    
+    @DisplayName("실패 : NONE 타입으로 고객 정보가 지정된 경우 CustomerException 예외가 발생합니다.")
+    @Test
+    void saveFail() {
+      assertThrows(CustomerException.class, () -> {
+        Customer newCustomer = new Customer("test", CustomerType.NONE, "test@test.co");
+        customers.add(customerRepository.save(newCustomer));
+      });
     }
-
-    @DisplayName("findById() 테스트")
-    @Nested
-    @Order(2)
-    class FindByIdTest {
-        @DisplayName("아이디를 통한 조회 가능 사례")
-        @Test
-        void findSuccess() {
-            for (Customer customer : customers) {
-                var resultCustomer = customerRepository.findById(customer.getId());
-                assertThat(resultCustomer, equalTo(customer));
-            }
-        }
-
-        @DisplayName("아이디를 통한 조회 실패 사례")
-        @Test
-        void findFail() {
-            assertThat(customerRepository.findById(UUID.randomUUID()), is(nullValue()));
-        }
+  }
+  
+  @DisplayName("findById() : ID로 고객 조회 테스트")
+  @Nested
+  @Order(2)
+  @TestMethodOrder(MethodOrderer.DisplayName.class)
+  class FindByIdTest {
+    @DisplayName("성공 : 존재하는 ID로 조회하는 경우 알맞은 고객을 반환합니다.")
+    @Test
+    void findSuccess() {
+      for (Customer customer : customers) {
+        var resultCustomer = customerRepository.findById(customer.getId());
+        assertThat(resultCustomer, equalTo(customer));
+      }
     }
-
-    @DisplayName("findByEmail() 테스트")
-    @Nested
-    @Order(2)
-    class FindByEmailTest {
-        @DisplayName("이메일을 통한 조회 가능 사례")
-        @Test
-        void findSuccess() {
-            for (Customer customer : customers) {
-                var resultCustomer = customerRepository.findByEmail(customer.getEmail());
-                assertThat(resultCustomer, equalTo(customer));
-            }
-        }
-
-        @DisplayName("이메일을 통한 조회 실패 사례")
-        @Test
-        void findFail() {
-            assertThat(customerRepository.findByEmail("fake@email"), is(nullValue()));
-        }
+    
+    @DisplayName("실패 : 존재하지 않는 ID로 조회하는 경우 null이 반환됩니다.")
+    @Test
+    void findFail() {
+      assertThat(customerRepository.findById(UUID.randomUUID()), is(nullValue()));
     }
-
-    @DisplayName("findAll() 테스트")
-    @Nested
-    @Order(3)
-    class FindAllTest {
-        @DisplayName("전체 고객 정보 조회 가능 사례")
-        @Test
-        void findSuccess() {
-            var resultCustomers = customerRepository.findAll();
-            assertThat(resultCustomers.isEmpty(), is(false));
-            assertThat(resultCustomers, hasSize(customers.size()));
-        }
+  }
+  
+  @DisplayName("findByEmail() : 이메일로 고객 조회 테스트")
+  @Nested
+  @Order(2)
+  @TestMethodOrder(MethodOrderer.DisplayName.class)
+  class FindByEmailTest {
+    @DisplayName("성공 : 존재하는 이메일로 조회하는 경우 알맞은 고객을 반환합니다.")
+    @Test
+    void findSuccess() {
+      for (Customer customer : customers) {
+        var resultCustomer = customerRepository.findByEmail(customer.getEmail());
+        assertThat(resultCustomer, equalTo(customer));
+      }
     }
-
-    @DisplayName("findByType() 테스트")
-    @Nested
-    @Order(3)
-    class FindByTypeTest {
-        @DisplayName("타입에 따른 고객 정보 조회 가능 사례")
-        @ParameterizedTest(name = "{0} 고객 정보 저장")
-        @CsvSource({"WHITE, 2", "BLACK, 2"})
-        void findSuccess(String typeStr, int count) {
-            var resultCustomers = customerRepository.findByType(CustomerType.valueOf(typeStr));
-            assertThat(resultCustomers.isEmpty(), is(false));
-            assertThat(resultCustomers, hasSize(count));
-        }
-
-        @DisplayName("타입에 따른 고객 정보 조회 실패 사례")
-        @Test
-        void findFail() {
-            var resultCustomers = customerRepository.findByType(CustomerType.NONE);
-            assertThat(resultCustomers.isEmpty(), is(true));
-        }
+    
+    @DisplayName("실패 : 존재하지 않는 이메일로 조회하는 경우 null이 반환됩니다.")
+    @Test
+    void findFail() {
+      assertThat(customerRepository.findByEmail("fake@email"), is(nullValue()));
     }
+  }
+  
+  @DisplayName("findAll() : 전체 고객 목록 조회 테스트")
+  @Nested
+  @Order(3)
+  @TestMethodOrder(MethodOrderer.DisplayName.class)
+  class FindAllTest {
+    @DisplayName("성공 : 한 개 이상의 고객 정보가 저장되어 있는 경우 전체 고객 리스트가 반환됩니다.")
+    @Test
+    void findSuccess() {
+      var resultCustomers = customerRepository.findAll();
+      assertThat(resultCustomers.isEmpty(), is(false));
+      assertThat(resultCustomers, hasSize(customers.size()));
+    }
+  }
+  
+  @DisplayName("findByType() : 타입으로 고객 목록 조회 테스트")
+  @Nested
+  @Order(3)
+  @TestMethodOrder(MethodOrderer.DisplayName.class)
+  class FindByTypeTest {
+    @DisplayName("성공 : WHITE/BLACK 타입으로 조회하는 경우 해당 타입을 지닌 고객 리스트가 반환됩니다.")
+    @ParameterizedTest(name = "{0} 타입의 고객 목록 조회 성공")
+    @CsvSource({"WHITE, 2", "BLACK, 2"})
+    void findSuccess(String typeStr, int count) {
+      var resultCustomers = customerRepository.findByType(CustomerType.valueOf(typeStr));
+      assertThat(resultCustomers.isEmpty(), is(false));
+      assertThat(resultCustomers, hasSize(count));
+    }
+    
+    @DisplayName("실패 : NONE 타입으로 조회하는 경우 빈 리스트가 반환됩니다.")
+    @Test
+    void findFail() {
+      var resultCustomers = customerRepository.findByType(CustomerType.NONE);
+      assertThat(resultCustomers.isEmpty(), is(true));
+    }
+  }
 }
--- src/test/java/com/prgms/management/voucher/repository/JdbcVoucherRepositoryTest.java
+++ src/test/java/com/prgms/management/voucher/repository/JdbcVoucherRepositoryTest.java
@@ -43,165 +43,172 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 @ActiveProfiles("default")
 @DisplayName("JdbcVoucherRepository 유닛 테스트")
 class JdbcVoucherRepositoryTest {
-    @Autowired
-    VoucherRepository voucherRepository;
-
-    @Autowired
-    DataSource dataSource;
-
-    EmbeddedMysql embeddedMysql;
-    List<Voucher> vouchers = new ArrayList<>();
-
-    @BeforeAll
-    void setUp() {
-        MysqldConfig config = aMysqldConfig(v5_7_latest)
-                .withCharset(UTF8)
-                .withPort(2215)
-                .withUser("test", "1234")
-                .withTimeZone("Asia/Seoul")
-                .withTimeout(2, TimeUnit.MINUTES)
-                .build();
-
-        embeddedMysql = anEmbeddedMysql(config)
-                .addSchema("demo", ScriptResolver.classPathScript("schema.sql"))
-                .start();
+  @Autowired
+  VoucherRepository voucherRepository;
+  
+  @Autowired
+  DataSource dataSource;
+  
+  EmbeddedMysql embeddedMysql;
+  List<Voucher> vouchers = new ArrayList<>();
+  
+  @BeforeAll
+  void setUp() {
+    MysqldConfig config = aMysqldConfig(v5_7_latest)
+      .withCharset(UTF8)
+      .withPort(2215)
+      .withUser("test", "1234")
+      .withTimeZone("Asia/Seoul")
+      .withTimeout(2, TimeUnit.MINUTES)
+      .build();
+    
+    embeddedMysql = anEmbeddedMysql(config)
+      .addSchema("demo", ScriptResolver.classPathScript("schema.sql"))
+      .start();
+  }
+  
+  @AfterAll
+  void cleanUp() {
+    embeddedMysql.stop();
+  }
+  
+  @Configuration
+  @ComponentScan
+  static class Config {
+    @Bean
+    public DataSource dataSource() {
+      return DataSourceBuilder.create()
+        .url("jdbc:mysql://localhost:2215/demo")
+        .username("test")
+        .password("1234")
+        .type(HikariDataSource.class)
+        .build();
     }
-
-    @AfterAll
-    void cleanUp() {
-        embeddedMysql.stop();
+    
+    @Bean
+    public NamedParameterJdbcTemplate namedParameterJdbcTemplate(DataSource dataSource) {
+      return new NamedParameterJdbcTemplate(dataSource);
     }
-
-    @Configuration
-    @ComponentScan
-    static class Config {
-        @Bean
-        public DataSource dataSource() {
-            return DataSourceBuilder.create()
-                    .url("jdbc:mysql://localhost:2215/demo")
-                    .username("test")
-                    .password("1234")
-                    .type(HikariDataSource.class)
-                    .build();
-        }
-
-        @Bean
-        public NamedParameterJdbcTemplate namedParameterJdbcTemplate(DataSource dataSource) {
-            return new NamedParameterJdbcTemplate(dataSource);
-        }
+  }
+  
+  @DisplayName("save() : 바우처 저장 테스트")
+  @Nested
+  @Order(1)
+  @TestMethodOrder(MethodOrderer.DisplayName.class)
+  class SaveTest {
+    @DisplayName("성공 : 0과 100 사이의 할인률이 지정된 경우 저장에 성공합니다.")
+    @ParameterizedTest(name = "{0} 할인 바우처 저장 성공")
+    @CsvSource({"1%, 1", "99%, 99"})
+    void savePercentVoucherSuccess(String name, int percent) {
+      Voucher newVoucher = new PercentDiscountVoucher(UUID.randomUUID(), name, percent,
+        Timestamp.valueOf(LocalDateTime.now()));
+      vouchers.add(voucherRepository.save(newVoucher));
+      
+      var retrievedVoucher = voucherRepository.findById(newVoucher.getVoucherId());
+      assertThat(retrievedVoucher, not(nullValue()));
+      assertThat(retrievedVoucher, equalTo(newVoucher));
     }
-
-    @DisplayName("save() 테스트")
-    @Nested
+    
+    @DisplayName("실패 : 0미만 100초과의 할인률이 지정된 경우 InvalidVoucherParameterException 예외가 발생합니다.")
+    @ParameterizedTest(name = "{0} 할인 바우처 저장 실패")
+    @CsvSource({"-1%, -1", "101%, 101"})
+    void savePercentVoucherFail(String name, int percent) {
+      assertThrows(InvalidVoucherParameterException.class, () -> {
+        Voucher newVoucher = new PercentDiscountVoucher(UUID.randomUUID(), name, percent,
+          Timestamp.valueOf(LocalDateTime.now()));
+        vouchers.add(voucherRepository.save(newVoucher));
+      });
+    }
+    
+    @DisplayName("성공 : 0과 10000 사이의 금액이 지정된 경우 저장에 성공합니다.")
+    @ParameterizedTest(name = "{0} 금액 바우처 저장 성공")
+    @CsvSource({"10000원, 10000", "1원, 1"})
+    void saveAmountVoucherSuccess(String name, int percent) {
+      Voucher newVoucher = new FixedAmountVoucher(UUID.randomUUID(), name, percent,
+        Timestamp.valueOf(LocalDateTime.now()));
+      vouchers.add(voucherRepository.save(newVoucher));
+      
+      var retrievedVoucher = voucherRepository.findById(newVoucher.getVoucherId());
+      assertThat(retrievedVoucher, not(nullValue()));
+      assertThat(retrievedVoucher, equalTo(newVoucher));
+    }
+    
+    @DisplayName("실패 : 0미만 10000초과의 금액이 지정된 경우 InvalidVoucherParameterException 예외가 발생합니다.")
+    @ParameterizedTest(name = "{0} 금액 바우처 저장 실패")
+    @CsvSource({"-1원, -1", "10001원, 10001"})
+    void saveAmountVoucherFail(String name, int percent) {
+      assertThrows(InvalidVoucherParameterException.class, () -> {
+        Voucher newVoucher = new FixedAmountVoucher(UUID.randomUUID(), name, percent,
+          Timestamp.valueOf(LocalDateTime.now()));
+        vouchers.add(voucherRepository.save(newVoucher));
+      });
+    }
+  }
+  
+  @DisplayName("findById() : ID로 바우처 조회 테스트")
+  @Nested
+  @Order(2)
+  @TestMethodOrder(MethodOrderer.DisplayName.class)
+  class FindByIdTest {
+    @DisplayName("성공 : 존재하는 ID로 조회하는 경우 알맞은 바우처를 반환합니다.")
+    @Test
+    void findSuccess() {
+      for (Voucher voucher : vouchers) {
+        Voucher resultVoucher = voucherRepository.findById(voucher.getVoucherId());
+        assertThat(voucher, equalTo(resultVoucher));
+      }
+    }
+    
+    @DisplayName("실패 : 존재하지 않는 ID로 조회하는 경우 null이 반환됩니다.")
+    @Test
+    void findFail() {
+      assertThat(voucherRepository.findById(UUID.randomUUID()), is(nullValue()));
+    }
+  }
+  
+  @DisplayName("findAll() : 전체 바우처 목록 조회 테스트")
+  @Nested
+  @Order(3)
+  @TestMethodOrder(MethodOrderer.DisplayName.class)
+  class FindAllTest {
+    @DisplayName("성공 : 한 개 이상의 바우처가 저장되어 있는 경우 전체 바우처 리스트가 반환됩니다.")
+    @Test
+    void findSuccess() {
+      var retVoucher = voucherRepository.findAll();
+      assertThat(retVoucher.isEmpty(), is(false));
+      assertThat(retVoucher, hasSize(vouchers.size()));
+    }
+  }
+  
+  @DisplayName("removeById() : ID로 바우처 삭제 테스트")
+  @Nested
+  @Order(4)
+  @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+  class RemoveByIdTest {
+    List<UUID> oldVouchers = new ArrayList<>();
+    
+    @DisplayName("성공 : 존재하는 ID로 삭제하는 경우 삭제에 성공합니다.")
+    @Test
     @Order(1)
-    class SaveTest {
-        @DisplayName("할인 바우처 저장 성공 사례")
-        @ParameterizedTest(name = "{0} 할인 바우처 생성")
-        @CsvSource({"1%, 1", "99%, 99"})
-        void savePercentVoucherSuccess(String name, int percent) {
-            Voucher newVoucher = new PercentDiscountVoucher(UUID.randomUUID(), name, percent, Timestamp.valueOf(LocalDateTime.now()));
-            vouchers.add(voucherRepository.save(newVoucher));
-
-            var retrievedVoucher = voucherRepository.findById(newVoucher.getVoucherId());
-            assertThat(retrievedVoucher, not(nullValue()));
-            assertThat(retrievedVoucher, equalTo(newVoucher));
-        }
-
-        @DisplayName("할인 바우처 저장 실패 사례")
-        @ParameterizedTest(name = "{0} 할인 바우처 생성")
-        @CsvSource({"-1%, -1", "101%, 101"})
-        void savePercentVoucherFail(String name, int percent) {
-            assertThrows(InvalidVoucherParameterException.class, () -> {
-                Voucher newVoucher = new PercentDiscountVoucher(UUID.randomUUID(), name, percent, Timestamp.valueOf(LocalDateTime.now()));
-                vouchers.add(voucherRepository.save(newVoucher));
-            });
-        }
-
-        @DisplayName("금액 바우처 저장 성공 사례")
-        @ParameterizedTest(name = "{0} 금액 바우처 생성")
-        @CsvSource({"10000원, 10000", "1원, 1"})
-        void saveAmountVoucherSuccess(String name, int percent) {
-            Voucher newVoucher = new FixedAmountVoucher(UUID.randomUUID(), name, percent, Timestamp.valueOf(LocalDateTime.now()));
-            vouchers.add(voucherRepository.save(newVoucher));
-
-            var retrievedVoucher = voucherRepository.findById(newVoucher.getVoucherId());
-            assertThat(retrievedVoucher, not(nullValue()));
-            assertThat(retrievedVoucher, equalTo(newVoucher));
-        }
-
-        @DisplayName("금액 바우처 저장 실패 사례")
-        @ParameterizedTest(name = "{0} 금액 바우처 생성")
-        @CsvSource({"-1원, -1", "10001원, 10001"})
-        void saveAmountVoucherFail(String name, int percent) {
-            assertThrows(InvalidVoucherParameterException.class, () -> {
-                Voucher newVoucher = new FixedAmountVoucher(UUID.randomUUID(), name, percent, Timestamp.valueOf(LocalDateTime.now()));
-                vouchers.add(voucherRepository.save(newVoucher));
-            });
-        }
+    void removeSuccess() {
+      int count = vouchers.size();
+      
+      for (Voucher voucher : vouchers) {
+        oldVouchers.add(voucher.getVoucherId());
+        voucherRepository.removeById(voucher.getVoucherId());
+        
+        var result = voucherRepository.findAll();
+        assertThat(result, hasSize(--count));
+      }
     }
-
-    @DisplayName("findById() 테스트")
-    @Nested
+    
+    @DisplayName("실패 : 존재하지 않는 ID로 삭제하는 경우 VoucherDeleteFailException 예외가 발생합니다.")
+    @Test
     @Order(2)
-    class FindByIdTest {
-        @DisplayName("아이디를 통한 조회 가능 사례")
-        @Test
-        void findSuccess() {
-            for (Voucher voucher : vouchers) {
-                Voucher resultVoucher = voucherRepository.findById(voucher.getVoucherId());
-                assertThat(voucher, equalTo(resultVoucher));
-            }
-        }
-
-        @DisplayName("아이디를 통한 조회 실패 사례")
-        @Test
-        void findFail() {
-            assertThat(voucherRepository.findById(UUID.randomUUID()), is(nullValue()));
-        }
+    void removeFail() {
+      for (UUID id : oldVouchers) {
+        assertThrows(VoucherDeleteFailException.class, () -> voucherRepository.removeById(id));
+      }
     }
-
-    @DisplayName("findAll() 테스트")
-    @Nested
-    @Order(3)
-    class FindAllTest {
-        @DisplayName("전체 바우처 조회 가능 사례")
-        @Test
-        void findSuccess() {
-            var retVoucher = voucherRepository.findAll();
-            assertThat(retVoucher.isEmpty(), is(false));
-            assertThat(retVoucher, hasSize(vouchers.size()));
-        }
-    }
-
-    @DisplayName("removeById() 테스트")
-    @Nested
-    @Order(4)
-    @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-    class RemoveByIdTest {
-        List<UUID> oldVouchers = new ArrayList<>();
-
-        @DisplayName("아이디를 통한 삭제 가능 사례")
-        @Test
-        @Order(1)
-        void removeSuccess() {
-            int count = vouchers.size();
-
-            for (Voucher voucher : vouchers) {
-                oldVouchers.add(voucher.getVoucherId());
-                voucherRepository.removeById(voucher.getVoucherId());
-
-                var result = voucherRepository.findAll();
-                assertThat(result, hasSize(--count));
-            }
-        }
-
-        @DisplayName("아이디를 통한 삭제 실패 사례")
-        @Test
-        @Order(2)
-        void removeFail() {
-            for (UUID id : oldVouchers) {
-                assertThrows(VoucherDeleteFailException.class, () -> voucherRepository.removeById(id));
-            }
-        }
-    }
+  }
 }
