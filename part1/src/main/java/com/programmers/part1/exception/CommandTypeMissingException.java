91c994b14c1b513dc845511adc6ea940c79d683c
jki503 <jki503@naver.com>
2022-04-22 17:53:39 +0900

    refactor : CommandTypeMissingException 을 TypeMissingException으로 변경 (voucherType에서도 사용하도록) : VoucherListEmptyException을 ListEmptyException을 변경(포괄적인 네이밍으로 예외를 정의)
    feat : NoUpdateException, DB 업데이트 이루어졌는지 예외처리

--- part1/src/main/java/com/programmers/part1/exception/CommandTypeMissingException.java
+++
@@ -1,8 +0,0 @@
-package com.programmers.part1.exception;
-
-public class CommandTypeMissingException extends RuntimeException{
-
-    public CommandTypeMissingException(String message) {
-        super(message);
-    }
-}
--- part1/src/main/java/com/programmers/part1/exception/voucher/VoucherListEmptyException.java
+++
@@ -1,8 +0,0 @@
-package com.programmers.part1.exception.voucher;
-
-public class VoucherListEmptyException extends RuntimeException{
-
-    public VoucherListEmptyException(String message) {
-        super(message);
-    }
-}
