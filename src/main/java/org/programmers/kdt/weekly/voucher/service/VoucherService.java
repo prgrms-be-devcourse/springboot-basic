a275177bc129c5242c7caa6f0216a91bbe04a263
HYEBPARK <hyeqls3742@gmail.com>
2022-04-23 02:16:21 +0900

    refactor : enum class 에서 바우처 생성

--- src/main/java/org/programmers/kdt/weekly/voucher/service/VoucherService.java
+++ src/main/java/org/programmers/kdt/weekly/voucher/service/VoucherService.java
@@ -1,8 +1,11 @@
 package org.programmers.kdt.weekly.voucher.service;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 import org.programmers.kdt.weekly.voucher.model.Voucher;
+import org.programmers.kdt.weekly.voucher.VoucherDto;
 import org.programmers.kdt.weekly.voucher.model.VoucherType;
 import org.programmers.kdt.weekly.voucher.repository.VoucherRepository;
 import org.springframework.stereotype.Service;
@@ -16,15 +19,20 @@ public class VoucherService {
         this.voucherRepository = voucherRepository;
     }
 
-    public Voucher createVoucher(VoucherType voucherType, int value) {
-        Voucher voucher = voucherType.create(UUID.randomUUID(), value);
-        this.voucherRepository.insert(voucher);
+    public Optional<Voucher> createVoucher(VoucherType voucherType, int value) {
+        VoucherDto voucherDto = new VoucherDto(UUID.randomUUID(), value, LocalDateTime.now());
+        Optional<Voucher> maybeVoucher = voucherType.create(voucherDto);
 
-        return voucher;
+        if (maybeVoucher.isPresent()) {
+            this.voucherRepository.insert(maybeVoucher.get());
+
+            return maybeVoucher;
+        }
+
+        return Optional.empty();
     }
 
     public List<Voucher> getVoucherList() {
         return this.voucherRepository.findAll();
     }
-
 }
