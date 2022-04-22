be76e636a2456bd78d70318a524c4df2ccafa982
pppp0722 <poj0722@naver.com>
2022-04-23 04:37:24 +0900

    [add] 요구사항 추가 - 고객 보유한 바우처 제거, 특정 바우처 보유한 고객 조회

--- src/main/java/com/pppp0722/vouchermanagement/engine/CommandLineApplication.java
+++ src/main/java/com/pppp0722/vouchermanagement/engine/CommandLineApplication.java
@@ -1,21 +1,16 @@
 package com.pppp0722.vouchermanagement.engine;
 
-import static com.pppp0722.vouchermanagement.entity.voucher.VoucherType.FIXED_AMOUNT;
-import static com.pppp0722.vouchermanagement.entity.voucher.VoucherType.PERCENT_DISCOUNT;
-
-import com.pppp0722.vouchermanagement.entity.EntityType;
+import com.pppp0722.vouchermanagement.engine.command.CommandType;
+import com.pppp0722.vouchermanagement.engine.command.Create;
+import com.pppp0722.vouchermanagement.engine.command.Delete;
+import com.pppp0722.vouchermanagement.engine.command.Read;
+import com.pppp0722.vouchermanagement.engine.command.Update;
 import com.pppp0722.vouchermanagement.io.Console;
-import com.pppp0722.vouchermanagement.entity.member.Member;
-import com.pppp0722.vouchermanagement.service.member.MemberServiceImpl;
-import com.pppp0722.vouchermanagement.entity.voucher.Voucher;
-import com.pppp0722.vouchermanagement.entity.voucher.VoucherType;
-import com.pppp0722.vouchermanagement.service.voucher.VoucherServiceImpl;
+import com.pppp0722.vouchermanagement.member.service.MemberServiceImpl;
+import com.pppp0722.vouchermanagement.voucher.service.VoucherServiceImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
-
-import java.util.List;
-import java.util.UUID;
 
 @Component
 public class CommandLineApplication {
@@ -24,14 +19,21 @@ public class CommandLineApplication {
     private final Console console = Console.getInstance();
     private final VoucherServiceImpl voucherService;
     private final MemberServiceImpl memberService;
+    private final Create create;
+    private final Read read;
+    private final Update update;
+    private final Delete delete;
 
     public CommandLineApplication(VoucherServiceImpl voucherService,
         MemberServiceImpl memberService) {
         this.voucherService = voucherService;
         this.memberService = memberService;
+        create = new Create(memberService, voucherService);
+        read = new Read(memberService, voucherService);
+        update = new Update(memberService, voucherService);
+        delete = new Delete(memberService, voucherService);
     }
 
-    // 프로그램 실행
     public void run() {
         logger.info("Start voucher management application.");
         console.printLogo();
@@ -43,13 +45,16 @@ public class CommandLineApplication {
             CommandType commandType = CommandType.getCommandType(command);
             switch (commandType) {
                 case CREATE:
-                    create();
+                    create.start();
                     break;
-                case LIST:
-                    printList();
+                case READ:
+                    read.start();
+                    break;
+                case UPDATE:
+                    update.start();
                     break;
                 case DELETE:
-                    delete();
+                    delete.start();
                     break;
                 case EXIT:
                     isExit = true;
@@ -57,152 +62,11 @@ public class CommandLineApplication {
                 default:
                     console.printInputError();
                     logger.error("Invalid command -> {}!", command);
+                    break;
             }
-            console.printEmpty();
+            System.out.println();
         }
 
         logger.info("Terminate voucher management application.");
-    }
-
-    public void create() {
-        EntityType entityType;
-        console.printEntityTypeInputRequest1();
-        entityType = EntityType.getEntityType(console.getCommand("Input : "));
-        if (entityType.equals(EntityType.MEMBER)) {
-            inputMember();
-        } else if (entityType.equals(EntityType.VOUCHER)) {
-            inputVoucher();
-        } else {
-            logger.error("Invalid entity type!");
-            console.printInputError();
-
-            create();
-        }
-    }
-
-    public void inputMember() {
-        String name;
-        console.printNameInputRequest();
-        name = console.getCommand("Input : ");
-        memberService.createMember(UUID.randomUUID(), name);
-    }
-
-    public void inputVoucher() {
-        UUID memberId = inputMemberId();
-        VoucherType voucherType = inputVoucherType();
-        long discountAmount = inputDiscountAmount(voucherType);
-        voucherService.createVoucher(UUID.randomUUID(), voucherType, discountAmount, memberId);
-    }
-
-    public UUID inputMemberId() {
-        UUID memberId;
-        console.printMemberIdInputRequest();
-        memberId = UUID.fromString(console.getCommand("Input : "));
-        return memberId;
-    }
-
-    public VoucherType inputVoucherType() {
-        VoucherType voucherType;
-        console.printVoucherTypeInputRequest();
-        voucherType = VoucherType.getVoucherType(console.getCommand("Input : "));
-
-        if (voucherType.equals(VoucherType.NONE)) {
-            logger.error("Invalid voucher type!");
-            console.printInputError();
-
-            return inputVoucherType();
-        }
-
-        return voucherType;
-    }
-
-    public long inputDiscountAmount(VoucherType voucherType) {
-        long discountAmount;
-        console.printAmountInputRequest();
-        discountAmount = Long.parseLong(console.getCommand("Input : "));
-        if (!isValidAmount(voucherType, discountAmount)) {
-            logger.error("Invalid amount -> {}!", discountAmount);
-            console.printInputError();
-            inputDiscountAmount(voucherType);
-        }
-
-        return discountAmount;
-    }
-
-    public boolean isValidAmount(VoucherType voucherType, long discountAmount) {
-        if (voucherType.equals(FIXED_AMOUNT)) {
-            if (discountAmount < 1) {
-                return false;
-            }
-        }
-
-        if (voucherType.equals(PERCENT_DISCOUNT)) {
-            if (discountAmount < 1 && discountAmount > 100) {
-                return false;
-            }
-        }
-
-        return true;
-    }
-
-    public void printList() {
-        EntityType entityType;
-        console.printEntityTypeInputRequest2();
-        entityType = EntityType.getEntityType(console.getCommand("Input : "));
-        if (entityType.equals(EntityType.MEMBER)) {
-            printMemberList();
-        } else if (entityType.equals(EntityType.VOUCHER)) {
-            printVoucherList();
-        } else if (entityType.equals(EntityType.WALLET)) {
-            printWallet();
-        }
-        else {
-            logger.error("Invalid entity type!");
-            console.printInputError();
-        }
-    }
-
-    public void printVoucherList() {
-        List<Voucher> voucherList = voucherService.getAllVouchers();
-
-        if (voucherList.isEmpty()) {
-            logger.info("Voucher is Empty.");
-            console.printVoucherEmpty();
-        } else {
-            console.printVoucherList(voucherList);
-        }
-    }
-
-    public void printMemberList() {
-        List<Member> memberList = memberService.getAllMembers();
-
-        if (memberList.isEmpty()) {
-            logger.info("Blacklist is Empty.");
-            console.printMemberListEmpty();
-        } else {
-            console.printMemberList(memberList);
-        }
-    }
-
-    public void printWallet() {
-        UUID memberId;
-        console.printMemberIdInputRequest();
-        memberId = UUID.fromString(console.getCommand("Input : "));
-        printVoucherListByMemberId(memberId);
-    }
-
-    public void printVoucherListByMemberId(UUID memberId) {
-        List<Voucher> voucherList = voucherService.getVouchersByMemberId(memberId);
-
-        if (voucherList.isEmpty()) {
-            logger.info("Voucher is Empty.");
-            console.printVoucherEmpty();
-        } else {
-            console.printVoucherList(voucherList);
-        }
-    }
-
-    public void delete() {
-
     }
 }
