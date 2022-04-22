64ae69414f75ab4c3c90ba23c9f273bdec227664
moosongsong <songe08@gmail.com>
2022-04-23 03:34:10 +0900

    feat: 바우처 등록 및 조회 DTO 추가

---
+++ src/main/java/com/prgms/management/voucher/controller/dto/VoucherRequest.java
@@ -0,0 +1,23 @@
+package com.prgms.management.voucher.controller.dto;
+
+import com.prgms.management.common.exception.SaveFailException;
+import com.prgms.management.voucher.model.FixedAmountVoucher;
+import com.prgms.management.voucher.model.PercentDiscountVoucher;
+import com.prgms.management.voucher.model.Voucher;
+import com.prgms.management.voucher.model.VoucherType;
+
+public record VoucherRequest(
+    Integer figure,
+    String type,
+    String name
+) {
+    public Voucher toVoucher() {
+        if (VoucherType.valueOf(type.toUpperCase()) == VoucherType.FIXED) {
+            return new FixedAmountVoucher(name, figure);
+        } else if (VoucherType.valueOf(type.toUpperCase()) == VoucherType.PERCENT) {
+            return new PercentDiscountVoucher(name, figure);
+        } else {
+            throw new SaveFailException("바우처 등록에 실패하였습니다.");
+        }
+    }
+}
