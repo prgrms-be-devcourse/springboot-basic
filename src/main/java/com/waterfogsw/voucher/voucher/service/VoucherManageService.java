2d7e108306bdf67caa424967397e9e498c984602
San Kim <waterfog9580@gmail.com>
2022-04-23 04:30:22 +0900

    :sparkles: Feat: VoucherService addVoucher 프로덕션 코드 작성

--- src/main/java/com/waterfogsw/voucher/voucher/service/VoucherManageService.java
+++ src/main/java/com/waterfogsw/voucher/voucher/service/VoucherManageService.java
@@ -1,12 +1,30 @@
 package com.waterfogsw.voucher.voucher.service;
 
 import com.waterfogsw.voucher.voucher.domain.Voucher;
+import com.waterfogsw.voucher.voucher.repository.VoucherRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class VoucherManageService implements VoucherService {
+
+    private VoucherRepository voucherRepository;
+
     @Override
     public Voucher saveVoucher(Voucher voucher) {
-        return null;
+        validateVoucher(voucher);
+        return voucherRepository.save(voucher);
+    }
+
+    private void validateVoucher(Voucher voucher) {
+        switch (voucher.getType()) {
+            case FIXED_AMOUNT -> {
+                if(voucher.getValue() < 0)
+                    throw new IllegalArgumentException();
+            }
+            case PERCENT_DISCOUNT -> {
+                if(voucher.getValue() < 0 || voucher.getValue() > 100)
+                    throw new IllegalArgumentException();
+            }
+        }
     }
 }
