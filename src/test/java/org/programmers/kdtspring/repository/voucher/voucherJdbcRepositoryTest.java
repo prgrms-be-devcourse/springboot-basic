facde2acdc4491501505a98dd88f98bc23d532bf
wisehero <godjw96@gmail.com>
2022-04-22 19:51:02 +0900

    # test: 바우처 레포지토리 테스트코드 작성

--- src/test/java/org/programmers/kdtspring/repository/voucher/voucherJdbcRepositoryTest.java
+++ src/test/java/org/programmers/kdtspring/repository/voucher/voucherJdbcRepositoryTest.java
@@ -3,8 +3,11 @@ package org.programmers.kdtspring.repository.voucher;
 import com.zaxxer.hikari.HikariDataSource;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.*;
+import org.programmers.kdtspring.entity.user.Customer;
 import org.programmers.kdtspring.entity.voucher.FixedAmountVoucher;
+import org.programmers.kdtspring.entity.voucher.Voucher;
 import org.programmers.kdtspring.entity.voucher.VoucherType;
+import org.programmers.kdtspring.repository.user.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
@@ -14,6 +17,9 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
 import javax.sql.DataSource;
+
+import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -49,7 +55,10 @@ class voucherJdbcRepositoryTest {
     @Autowired
     VoucherJdbcRepository voucherJdbcRepository;
 
-    @AfterEach
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @Test
     void setUp() {
         voucherJdbcRepository.deleteAll();
     }
@@ -70,6 +79,35 @@ class voucherJdbcRepositoryTest {
         var vouchers = voucherJdbcRepository.findAll();
 
         assertThat(vouchers.isEmpty()).isFalse();
+    }
+
+    @Test
+    @DisplayName("고객으로 바우처 찾기")
+    void TestFindByCustomerId() {
+        voucherJdbcRepository.save(new FixedAmountVoucher(1L, 1L, 1000, VoucherType.FixedAmountVoucher.name()));
+        voucherJdbcRepository.save(new FixedAmountVoucher(2L, 1L, 1000, VoucherType.FixedAmountVoucher.name()));
+        voucherJdbcRepository.save(new FixedAmountVoucher(3L, 1L, 1000, VoucherType.FixedAmountVoucher.name()));
+
+        Optional<Customer> customer = customerRepository.findById(1L);
+        List<Voucher> vouchersForCustomer = voucherJdbcRepository.findByCustomer(customer.get());
+
+        assertThat(vouchersForCustomer.size()).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("하나 삭제")
+    void TestDeleteOne() {
+        voucherJdbcRepository.save(new FixedAmountVoucher(1L, 1L, 1000, VoucherType.FixedAmountVoucher.name()));
+        voucherJdbcRepository.save(new FixedAmountVoucher(2L, 1L, 1000, VoucherType.FixedAmountVoucher.name()));
+        voucherJdbcRepository.save(new FixedAmountVoucher(3L, 1L, 1000, VoucherType.FixedAmountVoucher.name()));
+
+        Optional<Voucher> voucher = voucherJdbcRepository.findById(1L);
+
+        voucherJdbcRepository.deleteOne(voucher.get());
+
+
+        List<Voucher> all = voucherJdbcRepository.findAll();
+        assertThat(all.size()).isEqualTo(2);
     }
 
 }
