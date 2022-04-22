8119e8696b1e386ada721e91e1bfa184433a986e
hyuk0309 <bahngeh@naver.com>
2022-04-22 19:18:02 +0900

    chore : xml converter 관련 설정

--- src/main/java/org/prgrms/springbootbasic/config/WebConfig.java
+++ src/main/java/org/prgrms/springbootbasic/config/WebConfig.java
@@ -1,7 +1,11 @@
 package org.prgrms.springbootbasic.config;
 
+import java.util.List;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.xml.MarshallingHttpMessageConverter;
+import org.springframework.oxm.xstream.XStreamMarshaller;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -10,5 +14,14 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addFormatters(FormatterRegistry registry) {
         registry.addConverter(new StringToEnumConverter());
+    }
+
+    @Override
+    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        var messageConverter = new MarshallingHttpMessageConverter();
+        var xStreamMarshaller = new XStreamMarshaller();
+        messageConverter.setMarshaller(xStreamMarshaller);
+        messageConverter.setUnmarshaller(xStreamMarshaller);
+        converters.add(0, messageConverter);
     }
 }
