f89874e6132a377588a9d47b9192520f9fcf6db3
San Kim <waterfog9580@gmail.com>
2022-04-23 04:29:37 +0900

    :white_check_mark: Test: VoucherService addVoucher 테스트 코드 구현

--- src/test/java/com/waterfogsw/voucher/voucher/VoucherServiceTests.java
+++ src/test/java/com/waterfogsw/voucher/voucher/VoucherServiceTests.java
@@ -1,13 +1,33 @@
 package com.waterfogsw.voucher.voucher;
 
+import com.waterfogsw.voucher.voucher.domain.Voucher;
+import com.waterfogsw.voucher.voucher.domain.VoucherType;
+import com.waterfogsw.voucher.voucher.repository.VoucherRepository;
+import com.waterfogsw.voucher.voucher.service.VoucherManageService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class VoucherServiceTests {
+
+    @Mock
+    VoucherRepository voucherRepository;
+
+    @InjectMocks
+    VoucherManageService voucherService;
 
     @Nested
     @DisplayName("addVoucher 메소드는")
@@ -20,7 +40,14 @@ public class VoucherServiceTests {
             @Test
             @DisplayName("IllegalArgumentException 예외를 발생시킨다.")
             void it_throw_IllegalArgumentException() {
+                Voucher voucher = new Voucher(VoucherType.FIXED_AMOUNT, -1000);
 
+                try {
+                    voucherService.saveVoucher(voucher);
+                    fail();
+                } catch (IllegalArgumentException e) {
+                    //pass
+                }
             }
         }
 
@@ -28,21 +55,36 @@ public class VoucherServiceTests {
         @DisplayName("type 이 PERCENT_DISCOUNT 인 Voucher 의 value 가 1 ~ 100 사이의 수가 아니면")
         class Context_with_out_of_range_percent_discount {
 
-            @Test
+            @ParameterizedTest
+            @ValueSource(ints = {-1, 101})
             @DisplayName("IllegalArgumentException 예외를 발생시킨다.")
-            void it_throw_IllegalArgumentException() {
+            void it_throw_IllegalArgumentException(int percent) {
+                Voucher voucher = new Voucher(VoucherType.PERCENT_DISCOUNT, percent);
 
+                try {
+                    voucherService.saveVoucher(voucher);
+                    fail();
+                } catch (IllegalArgumentException e) {
+                    //pass
+                }
             }
         }
 
         @Nested
-        @DisplayName("Voucher 가 정상적으로 저장되면")
+        @DisplayName("Voucher 가 정상적으로 저장되어 생성되면")
         class Context_with_saved_success {
 
             @Test
             @DisplayName("저장한 Voucher 를 return 한다")
             void it_throw_IllegalArgumentException() {
+                Voucher voucher = new Voucher(VoucherType.FIXED_AMOUNT, 1000);
 
+                when(voucherRepository.save(any(Voucher.class))).thenReturn(voucher);
+
+                Voucher saved = voucherService.saveVoucher(voucher);
+
+                assertThat(voucher.getType(), is(saved.getType()));
+                assertThat(voucher.getValue(), is(saved.getValue()));
             }
         }
     }
