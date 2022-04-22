8f434e275f9be277fcc813783a5e0b95c8bee372
ecvheo1 <ecvheo@naver.com>
2022-04-23 05:12:31 +0900

    :bug: fix: 메소드 반환값 & argument 수정

--- src/main/java/kdt/vouchermanagement/domain/voucher/service/VoucherService.java
+++ src/main/java/kdt/vouchermanagement/domain/voucher/service/VoucherService.java
@@ -1,6 +1,8 @@
 package kdt.vouchermanagement.domain.voucher.service;
 
+import kdt.vouchermanagement.domain.voucher.domain.Voucher;
+
 public interface VoucherService {
 
-    void createVoucher(Object any);
+    Voucher createVoucher(Voucher voucher);
 }
