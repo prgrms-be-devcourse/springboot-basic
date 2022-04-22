f9936f170778167d48631b5ade0093d32a262a8b
sdardew <sdardew@gmail.com>
2022-04-23 01:38:03 +0900

    refeat: controller 추가

---
+++ src/main/java/org/prgrms/vouchermanagement/controller/console/ConsoleController.java
@@ -0,0 +1,46 @@
+package org.prgrms.vouchermanagement.controller.console;
+
+import org.prgrms.vouchermanagement.console.Input;
+import org.prgrms.vouchermanagement.console.Output;
+import org.prgrms.vouchermanagement.customer.service.CustomerService;
+import org.prgrms.vouchermanagement.voucher.service.VoucherService;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ConsoleController {
+
+  private final ListController listController;
+  private final VoucherService voucherService;
+  private final CustomerService customerService;
+  private final Input input;
+  private final Output output;
+
+  public ConsoleController(ListController listController, VoucherService voucherService, CustomerService customerService, Input input, Output output) {
+    this.listController = listController;
+    this.voucherService = voucherService;
+    this.customerService = customerService;
+    this.input = input;
+    this.output = output;
+  }
+
+  public void run() {
+    boolean progress = true;
+    while (progress) {
+      switch (input.readCommand()) {
+        case "exit":
+          progress = false;
+          output.printNotExistingCommand();
+          break;
+        case "list":
+
+          break;
+        case "create":
+          break;
+        default:
+
+      }
+    }
+  }
+
+
+}
