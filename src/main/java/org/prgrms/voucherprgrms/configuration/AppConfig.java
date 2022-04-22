5821d8f10d914a7a4eb67983c78791b45c4a8c3f
ㅎㅈ <hgkimmme9@gmail.com>
2022-04-22 18:49:52 +0900

    의존성 추가 및 AppConfig

--- src/main/java/org/prgrms/voucherprgrms/configuration/AppConfig.java
+++ src/main/java/org/prgrms/voucherprgrms/configuration/AppConfig.java
@@ -1,34 +1,13 @@
 package org.prgrms.voucherprgrms.configuration;
 
-import com.zaxxer.hikari.HikariDataSource;
-import org.springframework.boot.jdbc.DataSourceBuilder;
-import org.springframework.context.annotation.Bean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-
-import javax.sql.DataSource;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @ComponentScan
+@PropertySource("application.yaml")
+@EnableConfigurationProperties
 public class AppConfig {
-
-    @Bean
-    public DataSource dataSource() {
-
-        var dataSource = DataSourceBuilder.create()
-                .url("jdbc:mysql://localhost:2210/test-voucher_prgrms")
-                .username("test")
-                .password("test123")
-                .type(HikariDataSource.class)
-                .build();
-
-        return dataSource;
-    }
-
-    @Bean
-    public NamedParameterJdbcTemplate namedParameterJdbcTemplate(DataSource dataSource) {
-        return new NamedParameterJdbcTemplate(dataSource);
-    }
-
 }
