d2792d3e4b35c99ab6d01d0faadd3b090eb40596
jki503 <jki503@naver.com>
2022-04-22 17:38:38 +0900

    chore : pom.xml -> embedded DB, mysql, jdbc connection 설정 feat : AppConfiguration.java -> Mysql DB connection feat : table schema 추가

--- part1/src/main/java/com/programmers/part1/configuration/AppConfiguration.java
+++ part1/src/main/java/com/programmers/part1/configuration/AppConfiguration.java
@@ -1,16 +1,56 @@
 package com.programmers.part1.configuration;
 
-import com.programmers.part1.member.MemberController;
-import com.programmers.part1.member.MemberService;
-import com.programmers.part1.member.repository.FileMemberRepository;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.support.TransactionTemplate;
+
+import javax.sql.DataSource;
 
 @Configuration
 @ComponentScan(
         basePackages = "com.programmers.part1"
 )
+@EnableConfigurationProperties
 public class AppConfiguration {
+    @Bean
+    public DataSource dataSource() {
+        var dataSource = DataSourceBuilder.create()
+                .url("jdbc:mysql://localhost/customer_voucher_management")
+                .username("root")
+                .password("jungki503\\")
+                .type(HikariDataSource.class)
+                .build();
+        dataSource.setMaximumPoolSize(1000);
+        dataSource.setMinimumIdle(100);
 
+        return dataSource;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate(DataSource dataSource){
+        return new JdbcTemplate(dataSource);
+    }
+
+    @Bean
+    public NamedParameterJdbcTemplate namedParameterJdbcTemplate(JdbcTemplate jdbcTemplate) {
+        return new NamedParameterJdbcTemplate(jdbcTemplate);
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(DataSource dataSource){
+        return new DataSourceTransactionManager(dataSource);
+    }
+
+    @Bean
+    public TransactionTemplate transactionTemplate(PlatformTransactionManager platformTransactionManager){
+        return new TransactionTemplate(platformTransactionManager);
+    }
 }
---
+++ part1/src/main/resources/schema.sql
@@ -0,0 +1,26 @@
+CREATE TABLE customers
+(
+    customer_id    BINARY(16) PRIMARY KEY,
+    name           varchar(20) NOT NULL,
+    email          varchar(50) NOT NULL,
+    created_at     datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+    last_login_at     datetime(6) DEFAULT NULL,
+    CONSTRAINT unq_user_email UNIQUE (email)
+);
+
+CREATE TABLE vouchers
+(
+    voucher_id    BINARY(16) PRIMARY KEY,
+    voucher_type  varchar(30) NOT NULL,
+    voucher_amount int NOT NULL
+);
+
+create table voucher_wallets
+(
+    voucher_wallet_id BINARY(16) PRIMARY KEY,
+    customer_id       BINARY(16) NOT NULL,
+    voucher_id        BINARY(16) NOT NULL,
+    CONSTRAINT customer_id UNIQUE (customer_id, voucher_id),
+    CONSTRAINT voucher_wallets_1 FOREIGN KEY (customer_id) REFERENCES customers (customer_id) ON DELETE CASCADE,
+    CONSTRAINT voucher_wallets_2 FOREIGN KEY (voucher_id) REFERENCES vouchers (voucher_id) ON DELETE CASCADE
+);
