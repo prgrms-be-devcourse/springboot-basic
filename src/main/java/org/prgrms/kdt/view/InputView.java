3e8d0e070ee1e17329c4bf8935fdaeb20186e4a5
HyoungUkJJang <guddnr1451@gmail.com>
2022-04-23 16:06:20 +0900

    refactor : InputView 입력 개수 검증 메소드로 통일하기

--- src/main/java/org/prgrms/kdt/view/InputView.java
+++ src/main/java/org/prgrms/kdt/view/InputView.java
@@ -7,27 +7,23 @@ import org.slf4j.LoggerFactory;
 
 public class InputView {
 
-    private static final Logger logger = LoggerFactory.getLogger(InputView.class);
+    private static final Logger log = LoggerFactory.getLogger(InputView.class);
     private static final Scanner SC = new Scanner(System.in);
 
     private static final int VALID_SIZE = 2;
 
-    public static Menu inputTheMenu() {
-        return Menu.findOf(SC.nextLine());
+    public static Command getCommand() {
+        return Command.of(SC.nextLine());
     }
 
     public static String[] inputTheVoucher() throws IllegalArgumentException {
         String[] inputs = SC.nextLine().split(" ");
-
-        validateCount(inputs);
-        return inputs;
-    }
-
-    private static void validateCount(String[] inputs) {
         if (inputs.length != VALID_SIZE) {
-            logger.error("inputTheMenu Size => {}", inputs.length);
+            log.error("inputTheMenu Size => {}", inputs.length);
             throw new IllegalArgumentException(ErrorMessage.INPUT_COUNT_ERROR.getMessage());
         }
+
+        return inputs;
     }
 
 }
