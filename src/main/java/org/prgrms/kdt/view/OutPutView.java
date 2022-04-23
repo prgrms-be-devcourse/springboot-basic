9177ae4ef271faf6fab978813a3af460a75886c2
HyoungUkJJang <guddnr1451@gmail.com>
2022-04-23 16:08:05 +0900

    refactor : OutPutView 메소드 수정

--- src/main/java/org/prgrms/kdt/view/OutPutView.java
+++ src/main/java/org/prgrms/kdt/view/OutPutView.java
@@ -1,15 +1,15 @@
 package org.prgrms.kdt.view;
 
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
+import org.prgrms.kdt.customer.model.Customer;
 import org.prgrms.kdt.voucher.model.Voucher;
 
 public class OutPutView {
 
     public static void showsInitMenu() {
-        System.out.println("==== Voucher Program ====");
-        System.out.println("Type exit to exit the program.");
-        System.out.println("Type create to create a new voucher.");
-        System.out.println("Type list to list all vouchers.");
+        Arrays.stream(Command.values()).forEach(m -> System.out.println(m.getMessage()));
     }
 
     public static void showsRetryMessage() {
@@ -18,12 +18,15 @@ public class OutPutView {
 
     public static void showsVoucherMenu() {
         System.out.println("==== Create Voucher ====");
-        System.out.println("ex_1) fixed 1000");
-        System.out.println("ex_2) percent 15");
+        Arrays.stream(VoucherMenu.values()).forEach(v -> System.out.println(v.getMessage()));
     }
 
     public static void showsVoucherHistory(Collection<Voucher> vouchers) {
         vouchers.forEach(System.out::println);
+    }
+
+    public static void showsBlackList(List<Customer> customers) {
+        customers.forEach(System.out::println);
     }
 
 }
