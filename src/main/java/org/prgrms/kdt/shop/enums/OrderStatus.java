c430a730ca92ccf84b0545d440a469eaadfe5183
HunkiKim <gnsrl76@naver.com>
2022-04-23 18:48:02 +0900

    remove : delete dead file
    쓰지않는 파일 삭제

--- src/main/java/org/prgrms/kdt/shop/enums/OrderStatus.java
+++
@@ -1,5 +0,0 @@
-package org.prgrms.kdt.shop.enums;
-
-public enum OrderStatus {
-    ACCEPTED, PAYMENT_REQUIRED, PAYMENT_CONFIRMED, PAYMENT_REJECTED, READY_FOR_DELIVERY, SHIPPED, SETTLED, CANCELLED
-}
