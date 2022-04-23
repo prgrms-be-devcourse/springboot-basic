b9c18a50d588a9f43e08ca2d7b9c4e6ac967023e
hong yuseok <jericoh@naver.com>
2022-04-23 22:16:18 +0900

    feat: 페이지를 통한 voucher 삭제 구현

--- src/main/java/org/prgms/voucheradmin/domain/voucher/controller/VoucherViewController.java
+++ src/main/java/org/prgms/voucheradmin/domain/voucher/controller/VoucherViewController.java
@@ -10,10 +10,7 @@ import org.prgms.voucheradmin.domain.voucher.entity.vo.VoucherType;
 import org.prgms.voucheradmin.domain.voucher.service.VoucherService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class VoucherViewController {
@@ -53,5 +50,11 @@ public class VoucherViewController {
     public String viewNewVoucherPage(Model model)  {
         model.addAttribute("voucherTypes", VoucherType.values());
         return "views/voucher/new-voucher";
+    }
+
+    @GetMapping("/vouchers/delete/{voucherId}")
+    public String deleteVoucher(@PathVariable UUID voucherId)  {
+        voucherService.deleteVoucher(voucherId);
+        return "redirect:/vouchers";
     }
 }
