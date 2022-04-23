a738e2ad3db2551232268d58480879cb05b00cb1
HunkiKim <gnsrl76@naver.com>
2022-04-23 18:24:59 +0900

    feat : add schema.sql
    스키마 생성 파일 추가

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,13 @@
+CREATE TABLE vouchers(
+                         voucher_id BINARY(16) PRIMARY KEY,
+                         voucher_amount int NOT NULL,
+                         voucher_type varchar(20) NOT NULL
+);
+CREATE TABLE customers(
+                          customer_id BINARY(16) PRIMARY KEY,
+                          name varchar(20) NOT NULL,
+                          email varchar(50) NOT NULL,
+                          last_login_at datetime DEFAULT NULL,
+                          created_at datetime NOT NULL DEFAULT current_timestamp(),
+                          CONSTRAINT unq_user_email UNIQUE (email)
+);
