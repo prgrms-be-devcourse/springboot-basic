7a3e9e5fe4b985feddf824e1e89bc4394fdd68b3
HyoungUkJJang <guddnr1451@gmail.com>
2022-04-23 16:10:14 +0900

    refactor : voucherController 기능 수정

--- src/main/java/org/prgrms/kdt/controller/VoucherController.java
+++ src/main/java/org/prgrms/kdt/controller/VoucherController.java
@@ -1,73 +1,64 @@
 package org.prgrms.kdt.controller;
 
-import java.util.UUID;
+import java.util.Collection;
+import java.util.List;
+import org.prgrms.kdt.customer.model.Customer;
+import org.prgrms.kdt.customer.service.CustomerService;
 import org.prgrms.kdt.view.InputView;
-import org.prgrms.kdt.view.Menu;
+import org.prgrms.kdt.view.Command;
 import org.prgrms.kdt.view.OutPutView;
-import org.prgrms.kdt.voucher.model.FixedAmountVoucher;
-import org.prgrms.kdt.voucher.model.PercentDiscountVoucher;
+import org.prgrms.kdt.voucher.model.Voucher;
 import org.prgrms.kdt.voucher.model.VoucherType;
-import org.prgrms.kdt.voucher.repository.VoucherRepository;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.prgrms.kdt.voucher.service.VoucherService;
 import org.springframework.stereotype.Component;
 
 @Component
 public class VoucherController {
 
-    private static final Logger logger = LoggerFactory.getLogger(VoucherController.class);
     private static final int VOUCHER_KEYWORD_INDEX = 0;
     private static final int VOUCHER_DISCOUNT_INDEX = 1;
 
-    private final VoucherRepository voucherRepository;
+    private final VoucherService voucherService;
+    private final CustomerService customerService;
 
-    public VoucherController(VoucherRepository voucherRepository) {
-        this.voucherRepository = voucherRepository;
+    public VoucherController(VoucherService voucherService,
+        CustomerService customerService) {
+        this.voucherService = voucherService;
+        this.customerService = customerService;
     }
 
     public void run() {
-        while (true) {
+        Command command = Command.INIT;
+
+        while (command != Command.EXIT) {
             OutPutView.showsInitMenu();
-            execute(InputView.inputTheMenu());
+            command = InputView.getCommand();
+            execute(command);
         }
     }
 
-    private void execute(Menu menu) {
-        switch (menu) {
+    private void execute(Command command) {
+        switch (command) {
+            case INIT:
+                OutPutView.showsRetryMessage();
+                break;
             case EXIT:
                 System.exit(0);
             case CREATE:
-                selectTheVoucher();
+                OutPutView.showsVoucherMenu();
+                String[] strings = InputView.inputTheVoucher();
+                VoucherType voucherMenu = VoucherType.of(strings[VOUCHER_KEYWORD_INDEX]);
+                voucherService.makeVoucher(voucherMenu,
+                    Long.parseLong(strings[VOUCHER_DISCOUNT_INDEX]));
                 break;
             case LIST:
-                OutPutView.showsVoucherHistory(voucherRepository.getAll());
+                Collection<Voucher> vouchers = voucherService.getVouchers();
+                OutPutView.showsVoucherHistory(vouchers);
                 break;
-            case NONE:
-                OutPutView.showsRetryMessage();
-                break;
-        }
-    }
-
-    private void selectTheVoucher() {
-        OutPutView.showsVoucherMenu();
-
-        String[] voucherMenu = InputView.inputTheVoucher();
-        VoucherType voucherType = VoucherType.findVoucherMenu(voucherMenu[VOUCHER_KEYWORD_INDEX]);
-        makeVoucher(voucherType, Long.parseLong(voucherMenu[VOUCHER_DISCOUNT_INDEX]));
-    }
-
-    private void makeVoucher(VoucherType voucherMenu, long discountValue) {
-        logger.info("Make a Voucher => [Type : {}] [Value : {}]", voucherMenu, discountValue);
-        switch (voucherMenu) {
-            case FIXED:
-                voucherRepository.insert(new FixedAmountVoucher(UUID.randomUUID(), discountValue));
-                break;
-            case PERCENT:
-                voucherRepository.insert(
-                    new PercentDiscountVoucher(UUID.randomUUID(), discountValue));
-                break;
-            case NONE:
-                OutPutView.showsRetryMessage();
+            case CUSTOMER:
+                List<Customer> customers = customerService.getAll();
+                customers.forEach(System.out::println);
+                OutPutView.showsBlackList(customers);
                 break;
         }
     }
