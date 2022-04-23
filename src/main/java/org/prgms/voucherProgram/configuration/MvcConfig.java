e7923f9329c4485fa24d7906b2438b9a73652f72
yuminhwan <ymh7532@icloud.com>
2022-04-24 00:11:14 +0900

    feat: SpringMVC 설정

---
+++ src/main/java/org/prgms/voucherProgram/configuration/MvcConfig.java
@@ -0,0 +1,29 @@
+package org.prgms.voucherProgram.configuration;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.xml.MarshallingHttpMessageConverter;
+import org.springframework.oxm.xstream.XStreamMarshaller;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("api/**")
+            .allowedOrigins("*");
+    }
+
+    @Override
+    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        MarshallingHttpMessageConverter xmlConverter = new MarshallingHttpMessageConverter();
+        XStreamMarshaller xstreamMarshaller = new XStreamMarshaller();
+        xmlConverter.setMarshaller(xstreamMarshaller);
+        xmlConverter.setUnmarshaller(xstreamMarshaller);
+        converters.add(0, xmlConverter);
+    }
+}
