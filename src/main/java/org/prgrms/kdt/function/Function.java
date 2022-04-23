9d4075812e0b393bf5d5a0fafb41f8fed00e6fd7
yerimmac <yerim.lee.earth@gmail.com>
2022-04-24 02:50:47 +0900

    refactor : Function enum 람다 적용한다

--- src/main/java/org/prgrms/kdt/function/Function.java
+++ src/main/java/org/prgrms/kdt/function/Function.java
@@ -3,69 +3,50 @@ package org.prgrms.kdt.function;
 import org.prgrms.kdt.io.OutputConsole;
 
 public enum Function {
-    exit(" to exit the program") {
-        @Override
-        public boolean execute(FunctionOperator functionOperator) {
-            new OutputConsole().printMessage("exit program ! bye :)");
-            return isEndProgram;
-        }
-    },
-    create(" to create a new voucher") {
-        @Override
-        public boolean execute(FunctionOperator functionOperator) {
-            functionOperator.execute(this.name());
-            return !isEndProgram;
-        }
-    },
-    voucherList(" to list all vouchers") {
-        @Override
-        public boolean execute(FunctionOperator functionOperator) {
-            functionOperator.execute(this.name());
-            return !isEndProgram;
-        }
-    },
-    blackList(" to list all blackList") {
-        @Override
-        public boolean execute(FunctionOperator functionOperator) {
-            functionOperator.execute(this.name());
-            return !isEndProgram;
-        }
-    },
-    add("to create new customer") {
-        @Override
-        public boolean execute(FunctionOperator functionOperator) {
-            functionOperator.execute(this.name());
-            return !isEndProgram;
-        }
-    },
-    provide(" to provide voucher to customer") {
-        @Override
-        public boolean execute(FunctionOperator functionOperator) {
-            functionOperator.execute(this.name());
-            return !isEndProgram;
-        }
-    },
-    manage(" to list/delete voucher customer has") {
-        @Override
-        public boolean execute(FunctionOperator functionOperator) {
-            functionOperator.execute(this.name());
-            return !isEndProgram;
-        }
-    }
-    ;
+    exit(" to exit the program", (functionOperator, functionName) -> {
+        new OutputConsole().printMessage("exit program ! bye :)");
+        return true;
+    }),
+    create(" to create a new voucher", (functionOperator, functionName) -> {
+        functionOperator.execute(functionName);
+        return false;
+    }),
+    voucherList(" to list all vouchers", (functionOperator, functionName) ->{
+        functionOperator.execute(functionName);
+        return false;
+    }),
+    blackList(" to list all blackList", (functionOperator, functionName) -> {
+        functionOperator.execute(functionName);
+        return false;
+    }),
+    add("to create new customer", (functionOperator, functionName) -> {
+        functionOperator.execute(functionName);
+        return false;
+    }),
+    provide(" to provide voucher to customer", (functionOperator, functionName) -> {
+        functionOperator.execute(functionName);
+        return false;
+    }),
+    manage(" to list/delete voucher customer has", (functionOperator, functionName) -> {
+        functionOperator.execute(functionName);
+        return false;
+    });
 
     private final String explain;
 
-    private final static boolean isEndProgram = true;
+    private final java.util.function.BiFunction<FunctionOperator, String,  Boolean> expression;
 
-    Function(String explain) {
+    Function(String explain, java.util.function.BiFunction<FunctionOperator, String, Boolean> expression) {
         this.explain = explain;
+        this.expression = expression;
     }
 
     public String getExplain() {
         return explain;
     }
 
-    public abstract boolean execute(FunctionOperator functionOperator);
+    public boolean execute(FunctionOperator functionOperator) {
+        return  expression.apply(functionOperator, this.name());
+    }
 
 }
