6b897c29b473ce51c01f6320901f1096dad45af9
midas <midas117@knou.ac.kr>
2022-04-23 16:05:26 +0900

    feat: SpringBootApplication Annotation 사용, Jdbc 빈 추가 - Application : `@SpringBootApplication` 사용 - AppConfiguration - Jdbc 빈 추가

--- src/main/java/org/prgrms/kdt/Application.java
+++ src/main/java/org/prgrms/kdt/Application.java
@@ -5,8 +5,10 @@ import org.prgrms.kdt.io.Input;
 import org.prgrms.kdt.io.InputConsole;
 import org.prgrms.kdt.io.Output;
 import org.prgrms.kdt.io.OutputConsole;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+@SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
--- src/main/java/org/prgrms/kdt/configuration/AppConfiguration.java
+++ src/main/java/org/prgrms/kdt/configuration/AppConfiguration.java
@@ -1,13 +1,28 @@
 package org.prgrms.kdt.configuration;
 
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+
+import javax.sql.DataSource;
 
 @Configuration
 @ComponentScan(basePackages = "org.prgrms.kdt")
 @PropertySource(value = "application.yml", factory = YamlPropertiesFactory.class)
 @EnableConfigurationProperties
 public class AppConfiguration {
+
+    @Bean
+    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
+
+    @Bean
+    public NamedParameterJdbcTemplate namedParameterJdbcTemplate(JdbcTemplate jdbcTemplate) {
+        return new NamedParameterJdbcTemplate(jdbcTemplate);
+    }
 }
