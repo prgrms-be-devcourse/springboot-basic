4cf8e69838f935976d668615f18ab7e69ebaa2e8
hong yuseok <jericoh@naver.com>
2022-04-23 23:07:29 +0900

    feat: voucher 수정 구현

--- src/main/java/org/prgms/voucheradmin/domain/voucher/controller/VoucherViewController.java
+++ src/main/java/org/prgms/voucheradmin/domain/voucher/controller/VoucherViewController.java
@@ -5,15 +5,19 @@ import java.util.List;
 import java.util.UUID;
 
 import org.prgms.voucheradmin.domain.voucher.dto.VoucherCreateReqDto;
+import org.prgms.voucheradmin.domain.voucher.dto.VoucherUpdateReqDto;
 import org.prgms.voucheradmin.domain.voucher.entity.Voucher;
 import org.prgms.voucheradmin.domain.voucher.entity.vo.VoucherType;
 import org.prgms.voucheradmin.domain.voucher.service.VoucherService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class VoucherViewController {
+    private static final Logger logger = LoggerFactory.getLogger(VoucherViewController.class);
     private final VoucherService voucherService;
 
     public VoucherViewController(VoucherService voucherService) {
@@ -22,6 +26,7 @@ public class VoucherViewController {
 
     @ExceptionHandler(Exception.class)
     public String except(Exception ex, Model model) {
+        logger.error(ex.getMessage());
         model.addAttribute("message", ex.getMessage());
         return "views/error";
     }
@@ -39,20 +44,34 @@ public class VoucherViewController {
         return "views/voucher/vouchers";
     }
 
-    @GetMapping("/vouchers/{voucherId}")
-    public String viewVouchersPage(@PathVariable UUID voucherId, Model model) throws IOException {
-        Voucher voucher = voucherService.getVoucher(voucherId);
-        model.addAttribute("voucher", voucher);
-        return "views/voucher/voucher";
-    }
-
     @GetMapping("/vouchers/new")
     public String viewNewVoucherPage(Model model)  {
         model.addAttribute("voucherTypes", VoucherType.values());
         return "views/voucher/new-voucher";
     }
 
-    @GetMapping("/vouchers/delete/{voucherId}")
+    @GetMapping("/vouchers/{voucherId}")
+    public String viewVoucherPage(@PathVariable UUID voucherId, Model model) throws IOException {
+        Voucher voucher = voucherService.getVoucher(voucherId);
+        model.addAttribute("voucher", voucher);
+        return "views/voucher/voucher";
+    }
+
+    @GetMapping("/vouchers/{voucherId}/update")
+    public String viewVoucherUpdatePage(@PathVariable UUID voucherId, Model model) throws IOException {
+        Voucher voucher = voucherService.getVoucher(voucherId);
+        model.addAttribute("voucher", voucher);
+        model.addAttribute("voucherTypes", VoucherType.values());
+        return "views/voucher/update-voucher";
+    }
+
+    @PostMapping("/vouchers/update")
+    public String updateVoucher(VoucherUpdateReqDto voucherUpdateReqDto) {
+        voucherService.updateVoucher(voucherUpdateReqDto);
+        return "redirect:/vouchers/"+voucherUpdateReqDto.getVoucherId();
+    }
+
+    @PostMapping("/vouchers/delete/{voucherId}")
     public String deleteVoucher(@PathVariable UUID voucherId)  {
         voucherService.deleteVoucher(voucherId);
         return "redirect:/vouchers";
--- src/main/java/org/prgms/voucheradmin/domain/voucher/dto/VoucherUpdateReqDto.java
+++ src/main/java/org/prgms/voucheradmin/domain/voucher/dto/VoucherUpdateReqDto.java
@@ -3,6 +3,7 @@ package org.prgms.voucheradmin.domain.voucher.dto;
 import java.util.UUID;
 
 import org.prgms.voucheradmin.domain.voucher.entity.vo.VoucherType;
+import org.prgms.voucheradmin.global.exception.WrongInputException;
 
 public class VoucherUpdateReqDto {
     private UUID voucherId;
@@ -10,6 +11,20 @@ public class VoucherUpdateReqDto {
     private long amount;
 
     public VoucherUpdateReqDto(UUID voucherId, VoucherType voucherType, long amount) {
+        switch (voucherType) {
+            case FIXED_AMOUNT:
+                if(amount < 0)  {
+                    throw new WrongInputException("0 <= amount");
+                }
+                break;
+            case PERCENTAGE_DISCOUNT:
+                if(amount < 0 || amount > 100)  {
+                    throw new WrongInputException("0 <= amount <= 100");
+                }
+                break;
+            default:
+                throw new WrongInputException("wrong voucher type");
+        }
         this.voucherId = voucherId;
         this.voucherType = voucherType;
         this.amount = amount;
