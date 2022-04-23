638ac809c3bc317f0c35c75bd43bbf02b0833139
pjh612 <62292492+pjh612@users.noreply.github.com>
2022-04-24 03:33:05 +0900

    feat : VoucherService 프로토타입 추가

--- src/main/java/com/voucher/vouchermanagement/service/voucher/VoucherService.java
+++ src/main/java/com/voucher/vouchermanagement/service/voucher/VoucherService.java
@@ -1,10 +1,11 @@
-package com.voucher.vouchermanagement.service;
+package com.voucher.vouchermanagement.service.voucher;
 
 import com.voucher.vouchermanagement.model.voucher.Voucher;
 import com.voucher.vouchermanagement.model.voucher.VoucherType;
 import com.voucher.vouchermanagement.repository.voucher.VoucherRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.List;
 
 @Service
@@ -15,17 +16,21 @@ public class VoucherService {
     public VoucherService(VoucherRepository voucherRepository) {
         this.voucherRepository = voucherRepository;
     }
-    
+
+    //@Transactional
     public void createVoucher(VoucherType voucherType, Long value) {
-        this.voucherRepository.insert(voucherType.create(UUID.randomUUID(), value, LocalDateTime.now()));
+        //this.voucherRepository.insert(voucherType.create(UUID.randomUUID(), value, LocalDateTime.now()));
     }
 
+    //@Transactional
     public void createVouchers(List<Voucher> vouchers) {
-        vouchers.forEach(voucherRepository::insert);
+        //vouchers.forEach(voucherRepository::insert);
     }
 
+    //@Transactional(readOnly = true)
     public List<Voucher> findAll() {
-        return this.voucherRepository.findAll();
+        //return this.voucherRepository.findAll();
+        return Collections.emptyList();
     }
 
 }
