c7a7adb8145d5dd90a6184d399a10584d189d867
ecvheo1 <ecvheo@naver.com>
2022-04-23 19:18:20 +0900

    :sparkles: feat: VoucherConsoleController에게 create Voucher 요청

--- src/main/java/kdt/vouchermanagement/global/view/ConsoleDispatcher.java
+++ src/main/java/kdt/vouchermanagement/global/view/ConsoleDispatcher.java
@@ -1,8 +1,10 @@
 package kdt.vouchermanagement.global.view;
 
+import kdt.vouchermanagement.domain.voucher.controller.VoucherConsoleController;
 import kdt.vouchermanagement.domain.voucher.converter.VoucherRequestConverter;
 import kdt.vouchermanagement.domain.voucher.dto.VoucherRequest;
 import kdt.vouchermanagement.global.io.Input;
+import kdt.vouchermanagement.global.response.Response;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
@@ -12,11 +14,13 @@ public class ConsoleDispatcher implements ApplicationRunner {
 
     private final Input consoleInput;
     private final VoucherRequestConverter voucherRequestConverter;
+    private final VoucherConsoleController voucherConsoleController;
 
 
-    public ConsoleDispatcher(Input consoleInput, VoucherRequestConverter voucherRequestConverter) {
+    public ConsoleDispatcher(Input consoleInput, VoucherRequestConverter voucherRequestConverter, VoucherConsoleController voucherConsoleController) {
         this.consoleInput = consoleInput;
         this.voucherRequestConverter = voucherRequestConverter;
+        this.voucherConsoleController = voucherConsoleController;
     }
 
     @Override
@@ -24,25 +28,23 @@ public class ConsoleDispatcher implements ApplicationRunner {
         while (true) {
             //TODO output
             Menu menu = findMenu(consoleInput.menuInput());
-            try {
-                switch (menu) {
-                    case EXIT_PROGRAM:
-                        return;
-                    case CREATE_VOUCHER:
-                        //TODO output
-                        int voucherTypeNum = consoleInput.valueInput();
-                        //TODO output
-                        int discountValue = consoleInput.valueInput();
-                        VoucherRequest voucherRequest = voucherRequestConverter.of(voucherTypeNum, discountValue);
-                    case LIST_VOUCHERS:
-                        //TODO output
-                    case BLACKLIST:
-                        //TODO output
-                    case NONE:
-                        //TODO output
-                }
-            } catch (Exception e) {
-                //TODO output
+            switch (menu) {
+                case EXIT_PROGRAM:
+                    return;
+                case CREATE_VOUCHER:
+                    //TODO output
+                    int voucherTypeNum = consoleInput.valueInput();
+                    //TODO output
+                    int discountValue = consoleInput.valueInput();
+                    VoucherRequest voucherRequest = voucherRequestConverter.of(voucherTypeNum, discountValue);
+                    Response response = voucherConsoleController.create(voucherRequest);
+                    //TODO output
+                case LIST_VOUCHERS:
+                    //TODO output
+                case BLACKLIST:
+                    //TODO output
+                case NONE:
+                    //TODO output
             }
         }
     }
