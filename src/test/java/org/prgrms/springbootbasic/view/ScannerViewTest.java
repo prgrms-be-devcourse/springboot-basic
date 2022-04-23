b4ea4c51a98f9d6aa0d98e867a98564b5f40c3aa
hyuk0309 <bahngeh@naver.com>
2022-04-23 20:07:45 +0900

    test : ScannerViewTest 수정

--- src/test/java/org/prgrms/springbootbasic/view/ScannerViewTest.java
+++ src/test/java/org/prgrms/springbootbasic/view/ScannerViewTest.java
@@ -3,12 +3,10 @@ package org.prgrms.springbootbasic.view;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.io.PrintStream;
-import java.util.List;
 import java.util.Scanner;
 import java.util.UUID;
 import org.apache.commons.io.output.ByteArrayOutputStream;
@@ -20,9 +18,6 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
 import org.prgrms.springbootbasic.controller.Menu;
 import org.prgrms.springbootbasic.controller.VoucherType;
-import org.prgrms.springbootbasic.entity.customer.Customer;
-import org.prgrms.springbootbasic.entity.voucher.FixedAmountVoucher;
-import org.prgrms.springbootbasic.entity.voucher.PercentDiscountVoucher;
 import org.prgrms.springbootbasic.exception.InvalidMenuInput;
 import org.prgrms.springbootbasic.exception.InvalidVoucherTypeInput;
 import org.prgrms.springbootbasic.exception.InvalidateUUIDFormat;
@@ -155,32 +150,6 @@ class ScannerViewTest {
         assertThat(percent).isEqualTo(20);
     }
 
-    @DisplayName("voucher list 출력 테스트")
-    @Test
-    void printList() {
-        //given
-        var vouchers = List.of(
-            new FixedAmountVoucher(UUID.randomUUID(), 1000),
-            new PercentDiscountVoucher(UUID.randomUUID(), 20));
-
-        //when
-        new ScannerView(new Scanner(System.in), mock(CustomerBlackList.class)).printList(vouchers);
-    }
-
-    @DisplayName("블랙리스트 출력 테스트")
-    @Test
-    void printCustomerBlackList() {
-        //given
-        var customerBlackList = mock(CustomerBlackList.class);
-        var scannerView = new ScannerView(new Scanner(System.in), customerBlackList);
-
-        //when
-        scannerView.printCustomerBlackList();
-
-        //then
-        verify(customerBlackList).printCustomerBlackList();
-    }
-
     @DisplayName("사용자 이름 입력 테스트")
     @Test
     void selectName() {
@@ -205,30 +174,6 @@ class ScannerViewTest {
 
         //then
         assertThat(email).isEqualTo("test@gmail.com");
-    }
-
-    @DisplayName("사용자 목록 출력 테스트")
-    @Test
-    void printAllCustomers() {
-        //given
-        var customers = List.of(
-            new Customer(UUID.randomUUID(), "test", "test@gmail.com"),
-            new Customer(UUID.randomUUID(), "test2", "test2@gmail.com")
-        );
-
-        //when
-        generateScannerView("").printAllCustomers(customers);
-        //then
-    }
-
-    @DisplayName("비지니스 에러 출력")
-    @Test
-    void printError() {
-        //given
-        //when
-        generateScannerView("").printError("Got Service Error!");
-
-        //then
     }
 
     @DisplayName("바우처 아이디 입력 - 정상")
@@ -281,18 +226,5 @@ class ScannerViewTest {
         //then
         assertThatThrownBy(() -> scannerView.selectCustomerId())
             .isInstanceOf(InvalidateUUIDFormat.class);
-    }
-
-    @DisplayName("특정 손님 바우처 목록 출력")
-    @Test
-    void printCustomerVouchers() {
-        //given
-        var vouchers = List.of(
-            new FixedAmountVoucher(UUID.randomUUID(), 1000),
-            new PercentDiscountVoucher(UUID.randomUUID(), 20)
-        );
-
-        //when
-        generateScannerView("").printCustomerVouchers(vouchers);
     }
 }
