2374373930907098d7d1312c7574b177f78fc904
UJ15 <dbwogml15@naver.com>
2022-04-24 17:50:54 +0900

    :sparkles: feat : 도메인 유효성 검사

--- mission/src/main/java/org/prgrms/voucher/models/FixedAmountVoucher.java
+++ mission/src/main/java/org/prgrms/voucher/models/FixedAmountVoucher.java
@@ -6,9 +6,10 @@ public class FixedAmountVoucher extends Voucher {
     public FixedAmountVoucher(long amount, VoucherType voucherType) {
 
         super(amount, voucherType);
+        validate(amount);
     }
 
-    public FixedAmountVoucher(Long voucherId, long amount, VoucherType voucherType){
+    public FixedAmountVoucher(Long voucherId, long amount, VoucherType voucherType) {
 
         super(voucherId, amount, voucherType);
     }
@@ -17,5 +18,12 @@ public class FixedAmountVoucher extends Voucher {
     public long discount() {
 
         return 0;
+    }
+
+    private void validate(long amount) {
+
+        if (amount < 0) {
+            throw new IllegalArgumentException("bad discountValue..");
+        }
     }
 }
