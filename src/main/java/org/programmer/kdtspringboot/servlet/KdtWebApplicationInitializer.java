cd2ab6e596837889f402129f3455b62103e90f83
unnokid <kimki0609@gmail.com>
2022-04-24 17:45:46 +0900

    Add KdtWebApplicationInitializer

---
+++ src/main/java/org/programmer/kdtspringboot/servlet/KdtWebApplicationInitializer.java
@@ -0,0 +1,119 @@
+package org.programmer.kdtspringboot.servlet;
+
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import com.zaxxer.hikari.HikariDataSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeansException;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.EncodedResourceResolver;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+import javax.sql.DataSource;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+public class KdtWebApplicationInitializer implements WebApplicationInitializer {
+    private static final Logger logger = LoggerFactory.getLogger(KdtWebApplicationInitializer.class);
+
+    @Configuration
+    @EnableWebMvc // webMvc가 필요한 Bean들이 자동으로 등록됨
+    @ComponentScan(basePackages = "org.programmer.kdtspringboot.voucher")
+    static class ServletConfig implements WebMvcConfigurer, ApplicationContextAware {
+
+        ApplicationContext applicationContext;
+
+        @Override
+        public void configureViewResolvers(ViewResolverRegistry registry) {
+            registry.jsp().viewNames(".jsp/*");
+
+            var springResourceTemplateResolver = new SpringResourceTemplateResolver();
+            springResourceTemplateResolver.setApplicationContext(applicationContext);
+            springResourceTemplateResolver.setPrefix("/WEB-INF/");
+            springResourceTemplateResolver.setSuffix(".html");
+            var springTemplateEngine = new SpringTemplateEngine();
+            springTemplateEngine.setTemplateResolver(springResourceTemplateResolver);
+            var thymeleafViewResolver = new ThymeleafViewResolver();
+            thymeleafViewResolver.setTemplateEngine(springTemplateEngine);
+            thymeleafViewResolver.setOrder(1);
+            thymeleafViewResolver.setViewNames(new String[]{"views/*"});
+            registry.viewResolver(thymeleafViewResolver);
+
+        }
+
+        @Override
+        public void addResourceHandlers(ResourceHandlerRegistry registry) {
+            registry.addResourceHandler("/resources/**")
+                    .addResourceLocations("/resources/")
+                    .setCachePeriod(60)
+                    .resourceChain(true)
+                    .addResolver(new EncodedResourceResolver());
+
+
+        }
+
+        //빈으로 datasource 등록하기
+        @Bean
+        public DataSource dataSource() {
+            HikariDataSource dataSource = DataSourceBuilder.create()
+                    .url("jdbc:mysql://localhost/order_mgmt")
+                    .username("root")
+                    .password("root")
+                    .type(HikariDataSource.class) //실제 datasource를 만들때 쓰이는 구현체 넣기가능
+                    .build();
+            return dataSource;
+        }
+
+        @Bean
+        public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+            return new JdbcTemplate(dataSource);
+        }
+
+        @Bean
+        public PlatformTransactionManager platformTransactionManager(DataSource dataSource) {
+            return new DataSourceTransactionManager(dataSource);
+        }
+
+        @Override
+        public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+            this.applicationContext = applicationContext;
+        }
+
+    }
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        logger.info("start server~!");
+        System.out.println("start server");
+        var applicationContext = new AnnotationConfigWebApplicationContext();
+        applicationContext.register(ServletConfig.class);
+
+        DispatcherServlet dispatcherServlet = new DispatcherServlet(applicationContext);
+        var servletRegistration = servletContext.addServlet("test", dispatcherServlet);
+        servletRegistration.addMapping("/");
+        servletRegistration.setLoadOnStartup(1);
+
+    }
+}
