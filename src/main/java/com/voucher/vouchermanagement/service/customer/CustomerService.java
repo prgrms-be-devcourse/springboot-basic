9340a4050f7515ec456d6a111f0347b5cc663ff8
pjh612 <62292492+pjh612@users.noreply.github.com>
2022-04-24 16:31:39 +0900

    fix : Service layer임을 지정

--- src/main/java/com/voucher/vouchermanagement/service/customer/CustomerService.java
+++ src/main/java/com/voucher/vouchermanagement/service/customer/CustomerService.java
@@ -2,10 +2,12 @@ package com.voucher.vouchermanagement.service.customer;
 
 import com.voucher.vouchermanagement.dto.customer.CustomerDto;
 import com.voucher.vouchermanagement.repository.customer.CustomerRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.UUID;
 
+@Service
 public class CustomerService {
 
     private final CustomerRepository customerRepository;
