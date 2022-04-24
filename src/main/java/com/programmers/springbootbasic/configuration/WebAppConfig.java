f915cc9486a198bafb6591f130d1f93af45c85f0
chlwlgns524 <chlwlgns524@naver.com>
2022-04-24 16:16:43 +0900

    FEAT: thymeleaf 환경설정 및 컨트롤러 연동

--- src/main/java/com/programmers/springbootbasic/configuration/WebAppConfig.java
+++ src/main/java/com/programmers/springbootbasic/configuration/WebAppConfig.java
@@ -3,11 +3,17 @@ package com.programmers.springbootbasic.configuration;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+import org.thymeleaf.templatemode.TemplateMode;
 
 @Configuration
 @EnableWebMvc
@@ -17,13 +23,34 @@ public class WebAppConfig implements WebMvcConfigurer, ApplicationContextAware {
     private ApplicationContext applicationContext;
 
     @Override
-    public void configureViewResolvers(ViewResolverRegistry registry) {
-        registry.jsp("/WEB-INF/jsp/",".jsp");
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
     }
 
     @Override
-    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        this.applicationContext = applicationContext;
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+//        registry.jsp().viewNames("jsp/*");
+
+        SpringResourceTemplateResolver springResourceTemplateResolver = new SpringResourceTemplateResolver();
+        springResourceTemplateResolver.setApplicationContext(applicationContext);
+        springResourceTemplateResolver.setPrefix("/WEB-INF/");
+        springResourceTemplateResolver.setSuffix(".html");
+
+        SpringTemplateEngine springTemplateEngine = new SpringTemplateEngine();
+        springTemplateEngine.setTemplateResolver(springResourceTemplateResolver);
+
+        ThymeleafViewResolver thymeleafViewResolver = new ThymeleafViewResolver();
+        thymeleafViewResolver.setTemplateEngine(springTemplateEngine);
+        thymeleafViewResolver.setOrder(1);
+        thymeleafViewResolver.setViewNames(new String[]{"views/*"});
+
+        registry.viewResolver(thymeleafViewResolver);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**")
+                .addResourceLocations("/resources/");
     }
 
 }
--- src/main/java/com/programmers/springbootbasic/controller/HelloController.java
+++ src/main/java/com/programmers/springbootbasic/controller/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String helloController() {
-        return "index";
+        return "views/hello";
     }
 
 }
