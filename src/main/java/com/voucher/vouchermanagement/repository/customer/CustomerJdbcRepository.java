bc8a7bb96f6b74df4a914acde2c53f5e31a21e73
pjh612 <62292492+pjh612@users.noreply.github.com>
2022-04-25 01:07:38 +0900

    feat : CustomerRepository findById 추가

--- src/main/java/com/voucher/vouchermanagement/repository/customer/CustomerJdbcRepository.java
+++ src/main/java/com/voucher/vouchermanagement/repository/customer/CustomerJdbcRepository.java
@@ -1,15 +1,32 @@
 package com.voucher.vouchermanagement.repository.customer;
 
 import com.voucher.vouchermanagement.model.customer.Customer;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public class CustomerJdbcRepository implements CustomerRepository {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public CustomerJdbcRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
     @Override
     public void insert(Customer voucher) {
 
+    }
+
+    @Override
+    public Optional<Customer> findById(UUID id) {
+        return Optional.empty();
     }
 
     @Override
--- src/main/java/com/voucher/vouchermanagement/repository/customer/CustomerRepository.java
+++ src/main/java/com/voucher/vouchermanagement/repository/customer/CustomerRepository.java
@@ -3,9 +3,13 @@ package com.voucher.vouchermanagement.repository.customer;
 import com.voucher.vouchermanagement.model.customer.Customer;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 public interface CustomerRepository {
     void insert(Customer voucher);
+
+    Optional<Customer> findById(UUID id);
 
     List<Customer> findAll();
 
