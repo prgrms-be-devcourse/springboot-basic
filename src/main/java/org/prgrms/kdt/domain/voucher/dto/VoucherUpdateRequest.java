790b128c3872d562f07f56fe2e46d2759c9833e7
dbslzld15 <dbslzld15@naver.com>
2022-04-24 18:24:18 +0900

    fix: 바우처 업데이트 검증 로직 수정

--- src/main/java/org/prgrms/kdt/domain/voucher/dto/VoucherUpdateRequest.java
+++ src/main/java/org/prgrms/kdt/domain/voucher/dto/VoucherUpdateRequest.java
@@ -9,6 +9,7 @@ import java.time.LocalDateTime;
 import java.util.UUID;
 
 public class VoucherUpdateRequest {
+    @NotNull
     private UUID voucherId;
     @NotNull
     private VoucherType voucherType;
