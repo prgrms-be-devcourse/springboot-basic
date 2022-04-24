19d6150b416be180594d396efc7a0ef5cadafb69
hymn-fly <rlfrmsdh1@gmail.com>
2022-04-24 14:53:24 +0900

    Refactoring.  - VoucherServiceTest. 해당 로직들 domain과 repository 에서 이미 테스트 함.  - 새로운 로직에 대한 테스트 작성 필요

--- src/test/java/org/prgms/voucher/service/VoucherServiceTest.java
+++ src/test/java/org/prgms/voucher/service/VoucherServiceTest.java
@@ -1,19 +1,16 @@
 package org.prgms.voucher.service;
 
-import org.assertj.core.api.Assertions;
-import org.hamcrest.MatcherAssert;
-import org.hamcrest.Matchers;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.prgms.voucher.domain.FixedAmountVoucher;
 import org.prgms.voucher.domain.PercentDiscountVoucher;
 import org.prgms.voucher.domain.Voucher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.UUID;
 
+@Transactional
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 class VoucherServiceTest {
 
@@ -28,31 +25,4 @@ class VoucherServiceTest {
     public final Voucher fixedAmountVoucher = new FixedAmountVoucher(UUID.randomUUID(), 500L);
     public final Voucher percentDiscountVoucher = new PercentDiscountVoucher(UUID.randomUUID(), 10L);
 
-    @Test
-    @DisplayName("바우처 만들기 테스트")
-    void createTest() {
-        voucherService.createVoucher(fixedAmountVoucher);
-        MatcherAssert.assertThat(voucherService.listVoucher(), Matchers.hasSize(1));
-    }
-
-    @Test
-    @DisplayName("바우처 조회 테스트")
-    void selectTest() {
-        voucherService.createVoucher(fixedAmountVoucher);
-        voucherService.createVoucher(percentDiscountVoucher);
-        MatcherAssert.assertThat(voucherService.listVoucher(), Matchers.hasSize(2));
-    }
-
-    @Test
-    @DisplayName("바우처 적용 테스트")
-    void voucherUseTest() {
-        voucherService.createVoucher(fixedAmountVoucher);
-        voucherService.createVoucher(percentDiscountVoucher);
-        Assertions.assertThat(voucherService.useVoucher(
-                        1000L, fixedAmountVoucher.getVoucherId()))
-                .isEqualTo(500L);
-        Assertions.assertThat(voucherService.useVoucher(
-                        1000L, percentDiscountVoucher.getVoucherId()))
-                .isEqualTo(900L);
-    }
 }
