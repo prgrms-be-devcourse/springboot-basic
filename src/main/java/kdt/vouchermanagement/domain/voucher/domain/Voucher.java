f9b65f2d8e042f0b27e37189df9ca0dc56157b9e
ecvheo1 <ecvheo@naver.com>
2022-04-25 00:08:20 +0900

    :adhesive_bandage: fix: voucherId long -> Long 변경

--- src/main/java/kdt/vouchermanagement/domain/voucher/domain/Voucher.java
+++ src/main/java/kdt/vouchermanagement/domain/voucher/domain/Voucher.java
@@ -1,17 +1,17 @@
 package kdt.vouchermanagement.domain.voucher.domain;
 
 public abstract class Voucher {
-    private final long voucherId;
+    private final Long voucherId;
     private final VoucherType voucherType;
     private final int discountValue;
 
     public Voucher(VoucherType voucherType, int discountValue) {
-        this.voucherId = 0;
+        this.voucherId = null;
         this.voucherType = voucherType;
         this.discountValue = discountValue;
     }
 
-    public Voucher(long voucherId, VoucherType voucherType, int discountValue) {
+    public Voucher(Long voucherId, VoucherType voucherType, int discountValue) {
         this.voucherId = voucherId;
         this.voucherType = voucherType;
         this.discountValue = discountValue;
