e25437e12888be7a2c61cd66e681fa7e9a03000a
hayoung <>
2022-04-24 12:40:31 +0900

    fix: VoucherMemoryRepository 테스트케이스 추가, 테스트를 위한 일부 코드 구현

--- src/main/java/com/example/voucher/domain/voucher/repository/VoucherMemoryRepository.java
+++ src/main/java/com/example/voucher/domain/voucher/repository/VoucherMemoryRepository.java
@@ -1,10 +1,14 @@
 package com.example.voucher.domain.voucher.repository;
 
 import com.example.voucher.domain.voucher.Voucher;
+import com.example.voucher.domain.voucher.VoucherType;
 import org.springframework.stereotype.Repository;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+
+import static com.example.voucher.domain.voucher.VoucherType.EMPTY;
+import static com.example.voucher.exception.ErrorMessage.SERVER_ERROR;
 
 @Repository
 public class VoucherMemoryRepository implements VoucherRepository {
@@ -12,6 +16,24 @@ public class VoucherMemoryRepository implements VoucherRepository {
 
 	@Override
 	public Voucher save(Voucher voucher) {
-		return null;
+
+		if (voucher == null) {
+			// TODO: 로그 남기기
+			throw new IllegalArgumentException(SERVER_ERROR.name());
+		}
+
+		if (voucher.getVoucherId() == null) {
+			VoucherType voucherType = VoucherType.of(voucher.getClass().getSimpleName());
+
+			if (voucherType == EMPTY) {
+				// TODO: 로그 남기기
+				throw new IllegalArgumentException(SERVER_ERROR.name());
+			}
+
+			voucher = voucherType.create(VoucherIdGenerator.generateVoucherId(), voucher.getDiscountAmount());
+		}
+
+		store.put(voucher.getVoucherId(), voucher);
+		return voucher;
 	}
 }
