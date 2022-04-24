11fc5822d22b0f1218a8a66198c3e166f1f2d534
dojinyou <djyou128@gmail.com>
2022-04-25 00:11:13 +0900

    :sparkles: feat(applicationController): 관련 클래스 수정

--- src/main/java/com/dojinyou/devcourse/voucherapplication/voucher/VoucherController.java
+++ src/main/java/com/dojinyou/devcourse/voucherapplication/voucher/VoucherController.java
@@ -1,5 +1,15 @@
 package com.dojinyou.devcourse.voucherapplication.voucher;
 
-public class VoucherController implements IVoucherController{
-    private IVoucherService voucherService;
+import com.dojinyou.devcourse.voucherapplication.Response;
+import com.dojinyou.devcourse.voucherapplication.voucher.Entity.Voucher;
+
+public class VoucherController{
+    private VoucherService voucherService;
+
+    public Response create(Voucher.Request requestDto) {
+        return null;
+    }
+    public Response findAll() {
+        return null;
+    }
 }
