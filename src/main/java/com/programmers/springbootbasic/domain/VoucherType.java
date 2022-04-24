a758d3b8421afdf16cab98e2b25102ee219c36b6
chlwlgns524 <chlwlgns524@naver.com>
2022-04-24 12:05:25 +0900

    REFACTOR: 할인권 타입에 따른 생성을 담당하도록 VoucherType enum class 기능 강화

--- src/main/java/com/programmers/springbootbasic/domain/VoucherType.java
+++ src/main/java/com/programmers/springbootbasic/domain/VoucherType.java
@@ -4,30 +4,38 @@ import com.programmers.springbootbasic.dto.VoucherDTO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Arrays;
+import java.util.Optional;
 import java.util.Scanner;
 import java.util.UUID;
 
+import static com.programmers.springbootbasic.validation.BusinessValidator.*;
+
 public enum VoucherType {
 
-    FIXED_AMOUNT(1),
-    PERCENT_DISCOUNT(2);
+    FIXED_AMOUNT(1, "FIXED AMOUNT"),
+    PERCENT_DISCOUNT(2, "PERCENT DISCOUNT");
 
     private static final Logger logger = LoggerFactory.getLogger(VoucherType.class);
     private static final Scanner sc = new Scanner(System.in);
 
     private final int type;
+    private final String name;
 
-    VoucherType(int type) {
+    VoucherType(int type, String name) {
         this.type = type;
+        this.name = name;
     }
 
-    public static VoucherDTO createVoucherDTO(VoucherType type) {
+    public static VoucherDTO createVoucherDTO(int type) throws IllegalArgumentException {
+        VoucherType voucherType = findVoucherFromType(type)
+                .orElseThrow(() -> new IllegalArgumentException("해당 할인권이 존재하지 않습니다."));
+
         VoucherDTO voucherDTO = null;
 
-        switch (type) {
+        switch (voucherType) {
             case FIXED_AMOUNT -> voucherDTO = makeFixedAmountVoucher();
             case PERCENT_DISCOUNT -> voucherDTO = makePerCentDiscountVoucher();
-            default -> throw new IllegalArgumentException("해당 종류의 할인권은 존재하지 않습니다.");
         }
 
         return voucherDTO;
@@ -38,10 +46,16 @@ public enum VoucherType {
 
         try {
             Long fixedAmount = Long.valueOf(sc.next());
-            return new VoucherDTO(UUID.randomUUID(), fixedAmount, null, FIXED_AMOUNT.getType());
+            
+            if (validateFixedAmountVoucher(fixedAmount))
+                return new VoucherDTO(UUID.randomUUID(), fixedAmount, null, FIXED_AMOUNT.type);
+            else {
+                logger.info("범위를 벗어난 고정 할인 금액 입력");
+                throw new IllegalArgumentException(FIXED_AMOUNT_OUT_OF_BOUNDS_EXCEPTION_MESSAGE);
+            }
         } catch (NumberFormatException e) {
-            logger.info("잘못된 입력이 들어왔습니다.", e);
-            throw new NumberFormatException(e.getMessage());
+            logger.info("고정 할인 금액에 대한 부적절한 값 입력");
+            throw new NumberFormatException(INVALID_INPUT_NUMBER_EXCEPTION_MESSAGE);
         }
     }
 
@@ -49,16 +63,30 @@ public enum VoucherType {
         System.out.print("할인율을 입력하세요. => ");
 
         try {
-            Double discountPercent = Double.valueOf(sc.next());
-            return new VoucherDTO(UUID.randomUUID(), null, discountPercent, PERCENT_DISCOUNT.getType());
+            int discountPercent = Integer.parseInt(sc.next());
+
+            if (validatePercentDiscountVoucher(discountPercent))
+                return new VoucherDTO(UUID.randomUUID(), null, discountPercent, PERCENT_DISCOUNT.type);
+            else {
+                logger.info("범위를 벗어난 고정 할인율 입력");
+                throw new IllegalArgumentException(PERCENT_DISCOUNT_OUT_OF_BOUNDS_EXCEPTION_MESSAGE);
+            }
         } catch (NumberFormatException e) {
-            logger.info("잘못된 입력이 들어왔습니다.", e);
-            throw new NumberFormatException(e.getMessage());
+            logger.info("고정 할인율에 대한 부적절한 값 입력");
+            throw new NumberFormatException(INVALID_INPUT_NUMBER_EXCEPTION_MESSAGE);
         }
     }
 
-    public int getType() {
-        return type;
+    public static String getFormalName(int type) {
+        Optional<VoucherType> voucherFromType = findVoucherFromType(type);
+
+        return voucherFromType.map(voucherType -> voucherType.name).orElse(null);
+    }
+
+    private static Optional<VoucherType> findVoucherFromType(int type) {
+        return Arrays.stream(values())
+                .filter(voucher -> voucher.type == type)
+                .findFirst();
     }
 
 }
