8e274ee8d5f47edbfecbbd537577f3d3272ab891
San Kim <waterfog9580@gmail.com>
2022-04-24 19:17:47 +0900

    :white_check_mark: Test: VoucherService 테스트 코드 작성

--- src/test/java/com/waterfogsw/voucher/voucher/VoucherServiceTests.java
+++ src/test/java/com/waterfogsw/voucher/voucher/VoucherServiceTests.java
@@ -1,13 +1,32 @@
 package com.waterfogsw.voucher.voucher;
 
+import com.waterfogsw.voucher.voucher.domain.FixedAmountVoucher;
+import com.waterfogsw.voucher.voucher.domain.Voucher;
+import com.waterfogsw.voucher.voucher.exception.RepositoryException;
+import com.waterfogsw.voucher.voucher.repository.VoucherRepository;
+import com.waterfogsw.voucher.voucher.service.VoucherManageService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class VoucherServiceTests {
+    @Mock
+    VoucherRepository voucherRepository;
+
+    @InjectMocks
+    VoucherManageService voucherService;
 
     @Nested
     @DisplayName("addVoucher 메소드는")
@@ -20,7 +39,11 @@ public class VoucherServiceTests {
             @Test
             @DisplayName("RepositoryException 예외를 발생시킨다")
             void it_throw_RepositoryException() {
+                Voucher voucher = new FixedAmountVoucher(100);
 
+                when(voucherRepository.save(any(Voucher.class))).thenThrow(NullPointerException.class);
+
+                assertThrows(RepositoryException.class, () -> voucherService.addVoucher(voucher));
             }
         }
 
@@ -31,7 +54,17 @@ public class VoucherServiceTests {
             @Test
             @DisplayName("저장한 Voucher 를 return 한다")
             void it_throw_IllegalArgumentException() {
+                Voucher voucher = new FixedAmountVoucher(100);
 
+                when(voucherRepository.save(any(Voucher.class))).thenReturn(voucher);
+
+                try {
+                    var savedVoucher = voucherService.addVoucher(voucher);
+                    assertThat(savedVoucher.getType(), is(savedVoucher.getType()));
+                    assertThat(savedVoucher.getValue(), is(savedVoucher.getValue()));
+                } catch (RepositoryException e) {
+                    fail();
+                }
             }
         }
     }
