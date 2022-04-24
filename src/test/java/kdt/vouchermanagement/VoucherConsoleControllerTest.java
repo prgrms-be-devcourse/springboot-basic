eec052c08c968d486620fd680ca32b9c24735e99
ecvheo1 <ecvheo@naver.com>
2022-04-24 16:55:29 +0900

    :white_check_mark: test: create FixedAmountVoucher 기능 테스트 코드 구현

--- src/test/java/kdt/vouchermanagement/VoucherConsoleControllerTest.java
+++ src/test/java/kdt/vouchermanagement/VoucherConsoleControllerTest.java
@@ -1,6 +1,7 @@
 package kdt.vouchermanagement;
 
 import kdt.vouchermanagement.domain.voucher.controller.VoucherConsoleController;
+import kdt.vouchermanagement.domain.voucher.domain.Voucher;
 import kdt.vouchermanagement.domain.voucher.domain.VoucherType;
 import kdt.vouchermanagement.domain.voucher.dto.VoucherRequest;
 import kdt.vouchermanagement.domain.voucher.service.VoucherService;
@@ -15,8 +16,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class VoucherConsoleControllerTest {
@@ -62,5 +62,50 @@ public class VoucherConsoleControllerTest {
 
         //when
         verify(voucherService, times(1)).createVoucher(any());
+    }
+
+    @Test
+    @DisplayName("전달받은 DTO가 NULL이라면 바우처 생성 요청_실패")
+    void NotRequestCreateVoucher() {
+        //given
+        VoucherRequest request = null;
+        Response response = Response.of(400, "애플리케이션이 null을 사용하려고 합니다.");
+
+        //when
+        Response createdResponse = voucherConsoleController.create(request);
+
+        //then
+        verify(voucherService, times(0)).createVoucher(any());
+        assertThat(createdResponse).usingRecursiveComparison().isEqualTo(response);
+    }
+
+    @Test
+    @DisplayName("discountValue 값이 유효하지 않다는 Exception을 받는다면_성공")
+    void responseValueRangeException() {
+        //given
+        VoucherRequest request = new VoucherRequest(VoucherType.FIXED_AMOUNT, 100);
+        Response response = Response.of(400, "입력한 DiscountValue 값이 유효하지 않습니다.");
+        when(voucherService.createVoucher(any(Voucher.class))).thenThrow(new IllegalArgumentException("입력한 DiscountValue 값이 유효하지 않습니다."));
+
+        //when
+        Response createdResponse = voucherConsoleController.create(request);
+
+        //then
+        assertThat(createdResponse).usingRecursiveComparison().isEqualTo(response);
+    }
+
+    @Test
+    @DisplayName("중복된 바우처가 존재한다는 Exception을 받는다면_성공")
+    void responseDuplicateVoucherException() {
+        //given
+        VoucherRequest request = new VoucherRequest(VoucherType.FIXED_AMOUNT, 100);
+        Response response = Response.of(400, "입력한 VoucherType과 Discount 값이 중복되었습니다.");
+        when(voucherService.createVoucher(any(Voucher.class))).thenThrow(new DuplicateVoucherException("입력한 VoucherType과 Discount 값이 중복되었습니다."));
+
+        //when
+        Response createdResponse = voucherConsoleController.create(request);
+
+        //then
+        assertThat(createdResponse).usingRecursiveComparison().isEqualTo(response);
     }
 }
---
+++ src/test/java/kdt/vouchermanagement/VoucherServiceTest.java
@@ -0,0 +1,104 @@
+package kdt.vouchermanagement;
+
+import kdt.vouchermanagement.domain.voucher.domain.FixedAmountVoucher;
+import kdt.vouchermanagement.domain.voucher.domain.Voucher;
+import kdt.vouchermanagement.domain.voucher.domain.VoucherType;
+import kdt.vouchermanagement.domain.voucher.repository.VoucherRepository;
+import kdt.vouchermanagement.domain.voucher.service.VoucherService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class VoucherServiceTest {
+
+    VoucherService voucherService;
+
+    @Mock
+    VoucherRepository voucherRepository;
+
+    @BeforeEach
+    void setup() {
+        MockitoAnnotations.openMocks(this);
+        voucherService = new VoucherServiceImpl(voucherRepository);
+    }
+
+    @Test
+    @DisplayName("FixedAmountVoucher 타입의 할인값이 음수면_실패")
+    void negativeDiscountValueWhenFixedAmountVoucher() {
+        //given
+        VoucherType voucherType = VoucherType.FIXED_AMOUNT;
+        int discountValue = -1;
+        Voucher voucher = new FixedAmountVoucher(voucherType, discountValue);
+
+        //when, then
+        assertThrows(IllegalArgumentException.class, () -> voucherService.createVoucher(voucher));
+    }
+
+    @Test
+    @DisplayName("FixedAmountVoucher 타입의 할인값 0이면_실패")
+    void zeroDiscountValueWhenFixedAmountVoucher() {
+        //given
+        VoucherType voucherType = VoucherType.FIXED_AMOUNT;
+        int discountValue = 0;
+        Voucher voucher = new FixedAmountVoucher(voucherType, discountValue);
+
+        //when, then
+        assertThrows(IllegalArgumentException.class, () -> voucherService.createVoucher(voucher));
+    }
+
+    @Test
+    @DisplayName("생성할 VoucherType과 DiscountValue가 주어질 때 해당 값을 가진 바우처가 이미 존재하면_실패")
+    void isDuplicateVoucher() {
+        //given
+        VoucherType voucherType = VoucherType.FIXED_AMOUNT;
+        int discountValue = 100;
+        Voucher voucher = new FixedAmountVoucher(voucherType, discountValue);
+
+        doReturn(Optional.of(voucher)).when(voucherRepository).findByVoucherTypeAndDiscountValue(voucherType, discountValue);
+
+        //when, then
+        assertThrows(DuplicateVoucherException.class, () -> voucherService.createVoucher(voucher));
+    }
+
+    @Test
+    @DisplayName("전달받은 Voucher가 NULL이라면 FixedAmountVoucher 생성_실패")
+    void notCreateFixedAmountVoucher() {
+        //given
+        Voucher voucher = null;
+
+        //when, then
+        verify(voucherRepository, times(0)).save(any());
+        assertThrows(NullPointerException.class, () -> voucherService.createVoucher(voucher));
+    }
+
+    @Test
+    @DisplayName("FixedAmountVoucher 생성_성공")
+    void createFixedAmountVoucher() {
+        //given
+        VoucherType voucherType = VoucherType.FIXED_AMOUNT;
+        int discountValue = 100;
+        Voucher voucher = new FixedAmountVoucher(voucherType, discountValue);
+
+        doReturn(voucher).when(voucherRepository).save(any(Voucher.class));
+        doReturn(Optional.empty()).when(voucherRepository).findByVoucherTypeAndDiscountValue(voucherType, discountValue);
+
+        //when
+        Voucher createdVoucher = voucherService.createVoucher(voucher);
+
+        //then
+        verify(voucherRepository, times(1)).save(any());
+        assertThat(createdVoucher).usingRecursiveComparison().isEqualTo(voucher);
+    }
+}
