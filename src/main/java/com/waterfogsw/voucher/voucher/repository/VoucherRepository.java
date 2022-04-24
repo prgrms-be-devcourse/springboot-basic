de8b9683d2ee823b1659b1569520bc3f6acf939c
San Kim <waterfog9580@gmail.com>
2022-04-25 00:02:46 +0900

    :sparkles: Feat: findAllVoucher 프로덕션 코드

--- src/main/java/com/waterfogsw/voucher/voucher/repository/VoucherRepository.java
+++ src/main/java/com/waterfogsw/voucher/voucher/repository/VoucherRepository.java
@@ -2,6 +2,10 @@ package com.waterfogsw.voucher.voucher.repository;
 
 import com.waterfogsw.voucher.voucher.domain.Voucher;
 
+import java.util.List;
+
 public interface VoucherRepository {
     Voucher save(Voucher voucher);
+
+    List<Voucher> findAll();
 }
