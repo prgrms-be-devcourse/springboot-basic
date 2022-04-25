c5fd3ce103c095c48ebd893e70cb1f0864153723
dbslzld15 <dbslzld15@naver.com>
2022-04-25 23:59:42 +0900

    fix: 바우처 검증 로직 수정

--- src/main/java/org/prgrms/kdt/domain/voucher/controller/VoucherController.java
+++ src/main/java/org/prgrms/kdt/domain/voucher/controller/VoucherController.java
@@ -1,21 +1,26 @@
 package org.prgrms.kdt.domain.voucher.controller;
 
+import org.prgrms.kdt.domain.common.exception.ExceptionType;
 import org.prgrms.kdt.domain.customer.model.Customer;
 import org.prgrms.kdt.domain.customer.service.CustomerService;
 import org.prgrms.kdt.domain.voucher.dto.VoucherAssignRequest;
 import org.prgrms.kdt.domain.voucher.dto.VoucherCreateRequest;
 import org.prgrms.kdt.domain.voucher.dto.VoucherUpdateRequest;
+import org.prgrms.kdt.domain.voucher.exception.VoucherDataException;
 import org.prgrms.kdt.domain.voucher.model.Voucher;
 import org.prgrms.kdt.domain.voucher.model.VoucherType;
 import org.prgrms.kdt.domain.voucher.service.VoucherService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+
+import static org.prgrms.kdt.domain.common.exception.ExceptionType.NOT_SAVED;
 
 @Controller
 @RequestMapping("vouchers")
@@ -84,14 +89,22 @@ public class VoucherController {
     public String voucherAssignShow(Model model) {
         List<Voucher> vouchers = voucherService.getVouchersNotAssignedToCustomer();
         model.addAttribute("vouchers", vouchers);
+        model.addAttribute("assignForm", new VoucherAssignRequest());
         return "vouchers/assign";
     }
 
     @PostMapping("/assign")
-    public String voucherAssign(VoucherAssignRequest assignRequest) {
-        Optional<Customer> customer = customerService.getCustomerByEmail(assignRequest.getEmail());
-        customer.ifPresent(value ->
-                voucherService.updateCustomerId(assignRequest.getVoucherId(), value.getCustomerId()));
+    public String voucherAssign(
+            @ModelAttribute("assignForm") @Valid VoucherAssignRequest assignRequest,
+            BindingResult bindingResult, Model model) {
+        if(bindingResult.hasErrors()) {
+            List<Voucher> vouchers = voucherService.getVouchersNotAssignedToCustomer();
+            model.addAttribute("vouchers", vouchers);
+            return "/vouchers/assign";
+        }
+        Customer customer = customerService.getCustomerByEmail(assignRequest.getEmail())
+                .orElseThrow(() -> new VoucherDataException(NOT_SAVED));
+        voucherService.updateCustomerId(assignRequest.getVoucherId(), customer.getCustomerId());
         return "redirect:/";
     }
 }
--- src/main/java/org/prgrms/kdt/domain/voucher/dto/VoucherAssignRequest.java
+++ src/main/java/org/prgrms/kdt/domain/voucher/dto/VoucherAssignRequest.java
@@ -1,14 +1,20 @@
 package org.prgrms.kdt.domain.voucher.dto;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.UUID;
 
 public class VoucherAssignRequest {
+    @NotBlank(message = "이메일은 필수입력입니다.")
+    @Email(message = "이메일 형식으로 입력해주세요.")
+    @Size(max = 50, message = "이메일은 50자 이하로 입력해주세요.")
     private String email;
+    @NotNull
     private UUID voucherId;
 
-    public VoucherAssignRequest(String email, UUID voucherId) {
-        this.email = email;
-        this.voucherId = voucherId;
+    public VoucherAssignRequest() {
     }
 
     public String getEmail() {
@@ -17,5 +23,13 @@ public class VoucherAssignRequest {
 
     public UUID getVoucherId() {
         return voucherId;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setVoucherId(UUID voucherId) {
+        this.voucherId = voucherId;
     }
 }
