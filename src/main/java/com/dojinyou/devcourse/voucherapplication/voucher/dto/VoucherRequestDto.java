ee02d71a2ce9099e511cd23b37c1704464f25b40
dojinyou <djyou128@gmail.com>
2022-04-25 22:13:39 +0900

    :sparkles: feat(voucherRequestDto): request dto 구현

--- src/main/java/com/dojinyou/devcourse/voucherapplication/voucher/dto/VoucherRequestDto.java
+++ src/main/java/com/dojinyou/devcourse/voucherapplication/voucher/dto/VoucherRequestDto.java
@@ -1,14 +1,21 @@
 package com.dojinyou.devcourse.voucherapplication.voucher.dto;
 
-import com.dojinyou.devcourse.voucherapplication.voucher.entity.VoucherAmount;
-import com.dojinyou.devcourse.voucherapplication.voucher.entity.VoucherType;
+import com.dojinyou.devcourse.voucherapplication.voucher.domain.VoucherAmount;
+import com.dojinyou.devcourse.voucherapplication.voucher.domain.VoucherType;
 
 public class VoucherRequestDto {
-    private final VoucherType voucherType;
-    private final VoucherAmount voucherAmount;
+    private final VoucherType type;
+    private final VoucherAmount amount;
 
-    public VoucherRequestDto(VoucherType voucherType, VoucherAmount voucherAmount) {
-        this.voucherType = voucherType;
-        this.voucherAmount = voucherAmount;
+    public VoucherRequestDto(VoucherType type, VoucherAmount amount) {
+        this.type = type;
+        this.amount = amount;
+    }
+
+    public VoucherType getVoucherType() {
+        return this.type;
+    }
+    public VoucherAmount getVoucherAmount() {
+        return this.amount;
     }
 }
