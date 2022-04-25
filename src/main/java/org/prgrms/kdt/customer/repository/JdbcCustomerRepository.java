e712ffe88053204e41d677b4191b7f7f1c42de20
HyoungUkJJang <guddnr1451@gmail.com>
2022-04-25 11:27:30 +0900

    feat : JdbcCustomerRepository 기본구성

---
+++ src/main/java/org/prgrms/kdt/customer/repository/JdbcCustomerRepository.java
@@ -0,0 +1,13 @@
+package org.prgrms.kdt.customer.repository;
+
+import java.util.List;
+import org.prgrms.kdt.customer.model.Customer;
+
+public class JdbcCustomerRepository implements CustomerRepository{
+
+    @Override
+    public List<Customer> findAll() {
+        return null;
+    }
+
+}
