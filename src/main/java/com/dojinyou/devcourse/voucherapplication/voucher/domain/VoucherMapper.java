a3b9bd71edf337a196f798fe9586085902cf9509
dojinyou <djyou128@gmail.com>
2022-04-25 23:32:28 +0900

    :sparkles: feat(voucherMapper): voucherMapper 구현

---
+++ src/main/java/com/dojinyou/devcourse/voucherapplication/voucher/domain/VoucherMapper.java
@@ -0,0 +1,79 @@
+package com.dojinyou.devcourse.voucherapplication.voucher.domain;
+
+import com.dojinyou.devcourse.voucherapplication.voucher.dto.VoucherRequestDto;
+import com.dojinyou.devcourse.voucherapplication.voucher.dto.VoucherResponseDto;
+import com.dojinyou.devcourse.voucherapplication.voucher.entity.FixedAmountVoucherEntity;
+import com.dojinyou.devcourse.voucherapplication.voucher.entity.PercentAmountVoucherEntity;
+import com.dojinyou.devcourse.voucherapplication.voucher.entity.VoucherEntity;
+
+public class VoucherMapper {
+    public static final String ERROR_MESSAGE_FOR_NULL = "잘못된 입력입니다.";
+
+    private static Voucher getDomain(Long id, VoucherType type, VoucherAmount amount) {
+        switch (type) {
+            case FIXED:
+                return new FixedAmountVoucher(id, type, (FixedAmount) amount);
+            case PERCENT:
+                return new PercentAmountVoucher(id, type, (PercentAmount) amount);
+        }
+        throw new IllegalArgumentException(ERROR_MESSAGE_FOR_NULL);
+    }
+
+    private static VoucherEntity getEntity(Long id, VoucherType type, VoucherAmount amount) {
+        switch (type) {
+            case FIXED:
+                return new FixedAmountVoucherEntity(id, type, amount);
+            case PERCENT:
+                return new PercentAmountVoucherEntity(id, type, amount);
+        }
+        throw new IllegalArgumentException(ERROR_MESSAGE_FOR_NULL);
+    }
+
+    private static VoucherResponseDto getResponseDto(Long id, VoucherType type, VoucherAmount amount) {
+        return new VoucherResponseDto(id, type, amount);
+    }
+
+    public static Voucher requestDtoToDomain(VoucherRequestDto dto) {
+        nullCheck(dto);
+        VoucherType type = dto.getVoucherType();
+        VoucherAmount amount = dto.getVoucherAmount();
+
+        nullCheck(type, amount);
+        return getDomain(null, type, amount);
+    }
+
+    public static VoucherResponseDto domainToResponseDto(Voucher voucher) {
+        nullCheck(voucher);
+        Long id = voucher.getVoucherId();
+        VoucherType type = voucher.getVoucherType();
+        VoucherAmount amount = voucher.getVoucherAmount();
+
+        nullCheck(id, type, amount);
+        return getResponseDto(id, type, amount);
+
+    }
+
+    public static VoucherEntity domainToEntity(Long id, Voucher voucher) {
+        nullCheck(id, voucher);
+        VoucherType type = voucher.getVoucherType();
+        VoucherAmount amount = voucher.getVoucherAmount();
+        return getEntity(id, type, amount);
+    }
+
+    public static Voucher entityToDomain(VoucherEntity voucherEntity) {
+        nullCheck(voucherEntity);
+        Long id = voucherEntity.getId();
+        VoucherType type = voucherEntity.getType();
+        VoucherAmount amount = voucherEntity.getAmount();
+
+        return getDomain(id, type, amount);
+    }
+
+    private static void nullCheck(Object... objs) {
+        for (Object obj : objs) {
+            if (obj == null) {
+                throw new IllegalArgumentException(ERROR_MESSAGE_FOR_NULL);
+            }
+        }
+    }
+}
