a518192728c3c128ba9ef14fe2a856548e785d30
ichoi <ichoi@student.42seoul.kr>
2022-04-25 16:41:48 +0900

    refactor:Blacklist와 Customer를 분리하라

--- src/main/java/org/programmers/springbootbasic/blacklist/repository/FileBlacklistRepository.java
+++ src/main/java/org/programmers/springbootbasic/blacklist/repository/FileBlacklistRepository.java
@@ -1,4 +1,4 @@
-package org.programmers.springbootbasic.blacklist;
+package org.programmers.springbootbasic.blacklist.repository;
 
 import org.programmers.springbootbasic.blacklist.model.Blacklist;
 import org.slf4j.Logger;
