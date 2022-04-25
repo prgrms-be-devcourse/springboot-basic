30a6d50faa8ee821b0c7e7bbb6612ace3fe3d248
ichoi <ichoi@student.42seoul.kr>
2022-04-25 10:56:13 +0900

    feat:Customer Repository를 추가하라

--- src/main/java/org/programmers/springbootbasic/customer/repository/CustomerRepository.java
+++ src/main/java/org/programmers/springbootbasic/customer/repository/CustomerRepository.java
@@ -3,7 +3,20 @@ package org.programmers.springbootbasic.customer.repository;
 import org.programmers.springbootbasic.customer.model.Customer;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 public interface CustomerRepository {
+
+    Customer insert(Customer customer);
+
     List<Customer> findAll();
+
+    Optional<Customer> findById(UUID customerId);
+
+    Optional<Customer> findByName(String name);
+
+    void deleteById(UUID customerId);
+
+    void deleteAll();
 }
