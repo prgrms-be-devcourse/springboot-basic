ef187d6d9f15d32df964ef16f96d3a1202dee711
UJ15 <dbwogml15@naver.com>
2022-04-25 22:37:29 +0900

    :sparkles: feat : Console Application 구현

--- mission/src/main/java/org/prgrms/voucher/CommandLineApplication.java
+++ mission/src/main/java/org/prgrms/voucher/CommandLineApplication.java
@@ -2,10 +2,17 @@ package org.prgrms.voucher;
 
 import org.prgrms.voucher.io.Input;
 import org.prgrms.voucher.io.Output;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+@SpringBootApplication
 public class CommandLineApplication implements ApplicationRunner {
+
+    private final Logger logger = LoggerFactory.getLogger(CommandLineApplication.class);
 
     private final Input input;
     private final Output output;
@@ -21,5 +28,44 @@ public class CommandLineApplication implements ApplicationRunner {
     @Override
     public void run(ApplicationArguments args) throws Exception {
 
+        while (true) {
+            CommandType commandType;
+            output.printPrompt();
+            String userInput = input.getString();
+
+            try {
+                commandType = CommandType.findByCommand(userInput);
+            } catch (IllegalArgumentException inputException) {
+                logger.error("{} Type menu error", inputException.getMessage(), inputException);
+                output.printInvalidInputError();
+                continue;
+            }
+
+            switch (commandType) {
+                case CREATE -> creatVoucher();
+                case LIST -> showVoucherList();
+                case EXIT -> System.exit(0);
+            }
+        }
+    }
+
+    private void creatVoucher() {
+
+        String response;
+
+        output.printVoucherType();
+        String typeSelect = input.getString();
+        output.printVoucherDiscountType();
+        String discountValue = input.getString();
+        response = converter.createVoucherRequest(typeSelect, discountValue);
+        output.printMessage(response);
+    }
+
+    private void showVoucherList() {
+
+        String response;
+
+        response = converter.showVoucherListRequest();
+        output.printMessage(response);
     }
 }
--- mission/src/main/java/org/prgrms/voucher/Converter.java
+++ mission/src/main/java/org/prgrms/voucher/Converter.java
@@ -1,5 +1,17 @@
 package org.prgrms.voucher;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Converter {
 
+    public String createVoucherRequest(String typeSelect, String discountValue) {
+
+        return null;
+    }
+
+    public String showVoucherListRequest() {
+
+        return null;
+    }
 }
