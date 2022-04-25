ed54d1141630ab89da35bc51cbb40ddb1d164fff
ichoi <ichoi@student.42seoul.kr>
2022-04-25 16:43:49 +0900

    refactor:switch문을 분리하라

--- src/main/java/org/programmers/springbootbasic/CommandLineApplication.java
+++ src/main/java/org/programmers/springbootbasic/CommandLineApplication.java
@@ -1,54 +1,41 @@
 package org.programmers.springbootbasic;
 
-import org.programmers.springbootbasic.customer.service.CustomerService;
+import org.programmers.springbootbasic.command.CommandType;
 import org.programmers.springbootbasic.io.Console;
-import org.programmers.springbootbasic.voucher.model.VoucherType;
 import org.programmers.springbootbasic.voucher.service.VoucherService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Component;
+import org.springframework.context.ApplicationContext;
 
-@Component
 public class CommandLineApplication implements Runnable {
 	private static final Logger logger = LoggerFactory.getLogger(CommandLineApplication.class);
 
 	private final Console console;
-	private final VoucherService voucherService;
-	private final CustomerService customerService;
+	private final ApplicationContext applicationContext;
 
-	public CommandLineApplication(Console console, VoucherService voucherService, CustomerService customerService) {
+	public CommandLineApplication(Console console, ApplicationContext applicationContext) {
 		this.console = console;
-		this.voucherService = voucherService;
-		this.customerService = customerService;
+		this.applicationContext = applicationContext;
 	}
 
 	@Override
 	public void run() {
+		VoucherService voucherService = applicationContext.getBean(VoucherService.class);
+
+		while (execute(voucherService)) ;
+	}
+
+	private boolean execute(VoucherService voucherService) {
 		boolean runProgram = true;
 
-		while (runProgram) {
+		try {
 			console.printMenu();
-			String choice = console.input("");
-
-			switch (choice) {
-				case "exit" -> runProgram = false;
-				case "create" -> {
-					try {
-						long value = Long.parseLong(console.input("Type value"));
-						int voucherNumber = Integer.parseInt(console.input("Type Voucher number\n 1. FixedAmountVoucher\n 2. PercentDiscountVoucher"));
-						VoucherType voucherType = VoucherType.findByNumber(voucherNumber);
-						voucherService.createVoucher(voucherType, value);
-						console.printSuccessMessage();
-					} catch (NumberFormatException e) {
-						logger.error("wrong long type input error", e);
-					} catch (IllegalArgumentException e) {
-						logger.error("creat voucher IllegalArgumentException error", e);
-					}
-				}
-				case "list" -> voucherService.getVoucherList().forEach(System.out::println);
-				case "blacklist" -> customerService.findAll().forEach(System.out::println);
-				default -> logger.error("wrong console choice error");
-			}
+			String choiceCommandType = console.input("");
+			runProgram = CommandType.execute(choiceCommandType, console, voucherService);
+		} catch (IllegalArgumentException exception) {
+			logger.error("Invalid Command Type", exception);
 		}
+
+		return runProgram;
 	}
 }
