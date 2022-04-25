20bb128e246209f84e90013995d3e0a975d9ed6d
beomsic <qjatjr29@khu.ac.kr>
2022-04-25 23:47:16 +0900

    클래스 이름 변경

--- src/main/java/com/prgrms/voucher_manager/infra/Utils.java
+++ src/main/java/com/prgrms/voucher_manager/infra/Utils.java
@@ -3,7 +3,7 @@ package com.prgrms.voucher_manager.infra;
 import java.nio.ByteBuffer;
 import java.util.UUID;
 
-public class ToUuid {
+public class Utils {
 
     public static UUID toUUID(byte[] bytes) {
         ByteBuffer byteBuffer = ByteBuffer.wrap(bytes);
