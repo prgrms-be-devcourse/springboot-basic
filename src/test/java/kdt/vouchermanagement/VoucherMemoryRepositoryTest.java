28ff752d3e7cbc5fac6dea2fd1068c3e81a5f05b
ecvheo1 <ecvheo@naver.com>
2022-04-25 22:33:48 +0900

    :sparkles: feat: 바우처 목록 조회 repository 테스트 코드 구현

--- src/test/java/kdt/vouchermanagement/VoucherMemoryRepositoryTest.java
+++ src/test/java/kdt/vouchermanagement/VoucherMemoryRepositoryTest.java
@@ -5,17 +5,27 @@ import kdt.vouchermanagement.domain.voucher.domain.Voucher;
 import kdt.vouchermanagement.domain.voucher.domain.VoucherType;
 import kdt.vouchermanagement.domain.voucher.repository.VoucherMemoryRepository;
 import kdt.vouchermanagement.domain.voucher.repository.VoucherRepository;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class VoucherMemoryRepositoryTest {
 
-    private VoucherRepository voucherRepository = new VoucherMemoryRepository();
+    VoucherRepository voucherRepository;
+    Voucher firstVoucher = new FixedAmountVoucher(VoucherType.FIXED_AMOUNT, 100);
+
+    @BeforeAll
+    void setup() {
+        voucherRepository = new VoucherMemoryRepository();
+    }
 
     @Test
+    @Order(1)
     @DisplayName("바우처 저장할 때 전달받은 Voucher가 NULL이라면 IllegalArgumentException이 발생한다._실패")
     void nullArgumentWhenSave() {
         //given
@@ -26,16 +36,27 @@ public class VoucherMemoryRepositoryTest {
     }
 
     @Test
+    @Order(2)
     @DisplayName("바우처 저장할 때 전달받은 Voucher가 정상적이라면 저장한 Voucher를 반환_실패")
     void returnSavedVoucherWhenSave() {
-        //given
-        Voucher voucher = new FixedAmountVoucher(VoucherType.FIXED_AMOUNT, 100);
-
-        //when
-        Voucher savedVoucher = voucherRepository.save(voucher);
+        //given, when
+        Voucher savedVoucher = voucherRepository.save(firstVoucher);
 
         //then
-        assertThat(voucher.getVoucherType()).isEqualTo(savedVoucher.getVoucherType());
-        assertThat(voucher.getDiscountValue()).isEqualTo(savedVoucher.getDiscountValue());
+        assertThat(firstVoucher.getVoucherType()).isEqualTo(savedVoucher.getVoucherType());
+        assertThat(firstVoucher.getDiscountValue()).isEqualTo(savedVoucher.getDiscountValue());
+    }
+
+    @Test
+    @Order(3)
+    @DisplayName("저장소에 저장된 모든 바우처들을 조회 후 바우처 리스트 반환_성공")
+    void findAllVouchers() {
+        //given, when
+        List<Voucher> foundVouchers = voucherRepository.findAll();
+
+        //then
+        assertThat(foundVouchers.size()).isEqualTo(1);
+        assertThat(foundVouchers.get(0).getVoucherType()).isEqualTo(firstVoucher.getVoucherType());
+        assertThat(foundVouchers.get(0).getDiscountValue()).isEqualTo(firstVoucher.getDiscountValue());
     }
 }
