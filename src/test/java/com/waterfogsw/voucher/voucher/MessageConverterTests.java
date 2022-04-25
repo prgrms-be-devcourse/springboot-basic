91f0da5a66c6aecae528211e1361cd3d05fa9fe2
San Kim <waterfog9580@gmail.com>
2022-04-25 22:29:05 +0900

    :white_check_mark: Test: 테스트 코드 추가

--- src/test/java/com/waterfogsw/voucher/voucher/MessageConverterTests.java
+++ src/test/java/com/waterfogsw/voucher/voucher/MessageConverterTests.java
@@ -1,18 +1,25 @@
 package com.waterfogsw.voucher.voucher;
 
+import com.waterfogsw.voucher.console.Command;
 import com.waterfogsw.voucher.global.MessageConverter;
+import com.waterfogsw.voucher.global.RequestMessage;
 import com.waterfogsw.voucher.global.VoucherTypeConverter;
+import com.waterfogsw.voucher.voucher.domain.VoucherType;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.EnumSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.when;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 @ExtendWith(MockitoExtension.class)
 public class MessageConverterTests {
@@ -39,14 +46,20 @@ public class MessageConverterTests {
         }
 
         @Nested
-        @DisplayName("존재하는 바우처 타입이 전달되면")
+        @DisplayName("유효한 Command 의 Request 가 전달되면")
         class Context_with_existing_voucher_type {
 
             @ParameterizedTest
-            @ValueSource(strings = {"FIXED_AMOUNT", "PERCENT_DISCOUNT"})
-            @DisplayName("enum 바우처 타입을 반환한다")
-            void it_return_enum(String source) {
+            @EnumSource(Command.class)
+            @DisplayName("DTO 를 생성한다")
+            void it_return_enum(Command command) {
+                var requestMessage = new RequestMessage(command, "FIXED_AMOUNT", "1000");
 
+                when(voucherTypeConverter.convert(any())).thenReturn(VoucherType.FIXED_AMOUNT);
+
+                var voucherDto = converter.convert(requestMessage);
+
+                assertThat(voucherDto.type(), is(VoucherType.FIXED_AMOUNT));
             }
         }
     }
