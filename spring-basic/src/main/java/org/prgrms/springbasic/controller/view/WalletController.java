745faa855593d6827596e2a7a8ff6507af0f6957
Gosh95 <goshk1995@gmail.com>
2022-04-26 15:08:11 +0900

    feat: Wallet Controller 구현

---
+++ spring-basic/src/main/java/org/prgrms/springbasic/controller/view/WalletController.java
@@ -0,0 +1,25 @@
+package org.prgrms.springbasic.controller.view;
+
+import lombok.RequiredArgsConstructor;
+import org.prgrms.springbasic.service.web.WalletService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/wallets")
+@RequiredArgsConstructor
+public class WalletController {
+
+    private final WalletService walletService;
+
+    @GetMapping
+    public String walletList(Model model) {
+        var wallets = walletService.findWallets();
+
+        model.addAttribute("wallets", wallets);
+
+        return "/wallet/wallets";
+    }
+}
