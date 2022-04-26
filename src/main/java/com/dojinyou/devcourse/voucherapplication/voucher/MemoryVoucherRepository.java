8acacdd459ec6111592ceb7434a74a06af34ae14
dojinyou <djyou128@gmail.com>
2022-04-26 18:11:19 +0900

    :sparkles: feat(MemoryVoucherRepository): create 기능 구현

--- src/main/java/com/dojinyou/devcourse/voucherapplication/voucher/MemoryVoucherRepository.java
+++ src/main/java/com/dojinyou/devcourse/voucherapplication/voucher/MemoryVoucherRepository.java
@@ -1,4 +1,32 @@
 package com.dojinyou.devcourse.voucherapplication.voucher;
 
-public class MemoryVoucherRepository implements IVoucherRepository{
+import com.dojinyou.devcourse.voucherapplication.voucher.domain.Voucher;
+import com.dojinyou.devcourse.voucherapplication.voucher.domain.VoucherMapper;
+import com.dojinyou.devcourse.voucherapplication.voucher.entity.VoucherEntity;
+import org.springframework.stereotype.Repository;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+
+@Repository
+public class MemoryVoucherRepository implements VoucherRepository {
+    public static final String ERROR_MESSAGE_FOR_NULL = "잘못된 입력입니다.";
+    private static final AtomicLong idGenerator = new AtomicLong();
+    private static final Map<Long, VoucherEntity> store = new ConcurrentHashMap<>();
+
+    @Override
+    public Voucher create(Voucher voucher) {
+        if (voucher == null) {
+            throw new IllegalArgumentException(ERROR_MESSAGE_FOR_NULL);
+        }
+        long id = idGenerator.incrementAndGet();
+        VoucherEntity voucherEntity = VoucherMapper.domainToEntity(id, voucher);
+        return save(voucherEntity);
+    }
+
+    private Voucher save(VoucherEntity voucherEntity) {
+        store.put(voucherEntity.getId(), voucherEntity);
+        return VoucherMapper.entityToDomain(voucherEntity);
+    }
 }
