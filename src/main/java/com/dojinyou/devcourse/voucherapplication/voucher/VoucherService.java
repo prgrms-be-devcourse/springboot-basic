047ecd6ad113c74b376e7e23a87b094ec608fb3e
DojinYou <djyou128@gmail.com>
2022-04-26 10:55:48 +0900

    Revert "[2기-C] 유도진 - Voucher Service 구현"

--- src/main/java/com/dojinyou/devcourse/voucherapplication/voucher/VoucherService.java
+++ src/main/java/com/dojinyou/devcourse/voucherapplication/voucher/VoucherService.java
@@ -1,28 +1,5 @@
 package com.dojinyou.devcourse.voucherapplication.voucher;
 
-import com.dojinyou.devcourse.voucherapplication.Response;
-import com.dojinyou.devcourse.voucherapplication.voucher.domain.Voucher;
-import com.dojinyou.devcourse.voucherapplication.voucher.domain.VoucherMapper;
-import com.dojinyou.devcourse.voucherapplication.voucher.dto.VoucherResponseDto;
-import org.springframework.stereotype.Service;
-
-@Service
 public class VoucherService {
-    public static final String ERROR_MESSAGE_FOR_NULL = "잘못된 입력입니다.";
-    private VoucherRepository voucherRepository;
-
-    public VoucherService(MemoryVoucherRepository voucherRepository) {
-        this.voucherRepository = voucherRepository;
-    }
-
-    public Response<VoucherResponseDto> create(Voucher voucher) {
-        if (voucher == null) {
-            throw new IllegalArgumentException(ERROR_MESSAGE_FOR_NULL);
-        }
-        Voucher savedVoucher = voucherRepository.create(voucher);
-        VoucherResponseDto voucherResponseDto = VoucherMapper.domainToResponseDto(savedVoucher);
-
-        Response<VoucherResponseDto> response = new Response<>(Response.State.SUCCESS, voucherResponseDto);
-        return response;
-    }
+    private IVoucherRepository voucherRepository;
 }
