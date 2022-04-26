225953ad31202829edfcae795b26ce3fd06b8e03
jeonghaemin <jhm0828@gmail.com>
2022-04-26 18:28:24 +0900

    Feat: VoucherService remove, findVoucherById 메서드 추가

--- src/main/java/com/prgrms/vouchermanagement/voucher/service/VoucherService.java
+++ src/main/java/com/prgrms/vouchermanagement/voucher/service/VoucherService.java
@@ -10,16 +10,17 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
 public class VoucherService {
 
     private final Logger log = LoggerFactory.getLogger(getClass());
-    private final VoucherRepository repository;
+    private final VoucherRepository voucherRepository;
 
     public VoucherService(VoucherRepository repository) {
-        this.repository = repository;
+        this.voucherRepository = repository;
     }
 
     /**
@@ -27,7 +28,7 @@ public class VoucherService {
      */
     public UUID addVoucher(VoucherType voucherType, long amount) throws IllegalArgumentException, DataAccessException {
         Voucher newVoucher = voucherType.constructor(UUID.randomUUID(), amount, LocalDateTime.now());
-        repository.save(newVoucher);
+        voucherRepository.save(newVoucher);
         log.info("voucher is saved - {}", newVoucher);
         return newVoucher.getVoucherId();
     }
@@ -36,7 +37,7 @@ public class VoucherService {
      * @throws DataAccessException : Repository에서 쿼리 실행에 문제가 발생한 경우 던져진다.
      */
     public List<Voucher> findAllVouchers() throws DataAccessException {
-        List<Voucher> allVouchers = repository.findAll();
+        List<Voucher> allVouchers = voucherRepository.findAll();
         log.info("find all vouchers. size={}", allVouchers.size());
         return allVouchers;
     }
@@ -45,6 +46,14 @@ public class VoucherService {
      * @throws DataAccessException : Repository에서 쿼리 실행에 문제가 발생한 경우 던져진다.
      */
     public boolean isRegisteredVoucher(UUID voucherId) throws DataAccessException {
-        return repository.findById(voucherId).isPresent();
+        return voucherRepository.findById(voucherId).isPresent();
+    }
+
+    public Optional<Voucher> findVoucherById(UUID voucherId) {
+        return voucherRepository.findById(voucherId);
+    }
+
+    public void removeVoucher(UUID voucherId) {
+        voucherRepository.remove(voucherId);
     }
 }
