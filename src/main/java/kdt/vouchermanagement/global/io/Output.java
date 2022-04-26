3e9c0da64ff1e38db3a1916dceb31b354ebc3347
ecvheo1 <ecvheo@naver.com>
2022-04-27 03:26:29 +0900

    :adhesive_bandage: fix: 콘솔 출력 구조 수정

--- src/main/java/kdt/vouchermanagement/global/io/Output.java
+++ src/main/java/kdt/vouchermanagement/global/io/Output.java
@@ -1,23 +1,11 @@
 package kdt.vouchermanagement.global.io;
 
-import kdt.vouchermanagement.domain.voucher.domain.Voucher;
-import kdt.vouchermanagement.global.response.Response;
 
-import java.util.List;
+import kdt.vouchermanagement.global.response.Response;
 
 public interface Output {
 
-    void printMenu();
+    void printMenu(String message);
 
-    void printType();
-
-    void printValue();
-
-    void printError(Response<String> response);
-
-    void printCreateVoucher(Response<Voucher> response);
-
-    void printListVouchers(Response<List> response);
-
-    void printNoneMenu();
+    void printResponse(Response response);
 }
