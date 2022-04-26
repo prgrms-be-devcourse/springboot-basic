55c16a2fa900a40dc9a02506ae54e3a0f8c8b8fc
NownS <sksmsx147@gmail.com>
2022-04-27 03:34:12 +0900

    voucherType enum 내 팩토리 메소드 추가, isMatches 구현

--- src/main/java/org/programmers/voucher/domain/VoucherType.java
+++ src/main/java/org/programmers/voucher/domain/VoucherType.java
@@ -1,5 +1,7 @@
 package org.programmers.voucher.domain;
 
+import java.util.UUID;
+
 public enum VoucherType {
     FIXED_AMOUNT_VOUCHER("Fixed"),
     PERCENT_DISCOUNT_VOUCHER("Percent");
@@ -10,8 +12,29 @@ public enum VoucherType {
         this.alias = alias;
     }
 
-    //todo
     public boolean isMatches(String input) {
-        return false;
+        return input.equals(alias);
+    }
+
+    public String getAlias() {
+        return alias;
+    }
+
+    public static Voucher makeVoucher(VoucherType type, long value) {
+        switch (type) {
+            case FIXED_AMOUNT_VOUCHER -> {
+                if (value < 0) {
+                    throw new IllegalArgumentException();
+                }
+                return new FixedAmountVoucher(UUID.randomUUID(), value);
+            }
+            case PERCENT_DISCOUNT_VOUCHER -> {
+                if (value < 0 || value > 100) {
+                    throw new IllegalArgumentException();
+                }
+                return new PercentDiscountVoucher(UUID.randomUUID(), value);
+            }
+        }
+        return null;
     }
 }
