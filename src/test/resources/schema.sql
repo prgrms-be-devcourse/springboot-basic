897440b7e7aca3d486843a072df3b76b2c314e8b
dldydgns530@gmail.com <dldydgns530@gmail.com>
2022-04-26 21:41:45 +0900

    refactor : testcontainer 설정 및 초기화

--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -3,8 +3,8 @@ CREATE TABLE customers
     customer_id BINARY(16) PRIMARY KEY,
     name varchar(20) NOT NULL,
     email varchar(50) NOT NULL,
-    last_login_at datetime DEFAULT NULL,
-    created_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+    last_login_at datetime(6) DEFAULT NULL,
+    created_at datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
     CONSTRAINT unq_user_email UNIQUE (email)
 );
 
