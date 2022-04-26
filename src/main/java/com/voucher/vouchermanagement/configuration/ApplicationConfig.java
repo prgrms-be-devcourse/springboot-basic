5c7956569d68b42bb770c08515f2b6f66dc3256a
pjh612 <62292492+pjh612@users.noreply.github.com>
2022-04-26 21:38:11 +0900

    refactor : Jdbc 설정 변경

--- src/main/java/com/voucher/vouchermanagement/configuration/ApplicationConfig.java
+++ src/main/java/com/voucher/vouchermanagement/configuration/ApplicationConfig.java
@@ -1,38 +1,8 @@
 package com.voucher.vouchermanagement.configuration;
 
-import com.zaxxer.hikari.HikariDataSource;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.jdbc.DataSourceBuilder;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.core.JdbcTemplate;
-
-import javax.sql.DataSource;
 
 @Configuration
 public class ApplicationConfig {
 
-    @Value("${db.connection.name}")
-    private String dbUserName;
-    @Value("${db.connection.password}")
-    private String dbPassword;
-    @Value("${db.connection.url}")
-    private String dbUrl;
-
-    @Bean
-    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
-        return new JdbcTemplate(dataSource);
-    }
-
-    @Bean
-    public DataSource dataSource() {
-        DataSource dataSource = DataSourceBuilder.create()
-                .url(dbUrl)
-                .username(dbUserName)
-                .password(dbPassword)
-                .type(HikariDataSource.class)
-                .build();
-
-        return dataSource;
-    }
 }
