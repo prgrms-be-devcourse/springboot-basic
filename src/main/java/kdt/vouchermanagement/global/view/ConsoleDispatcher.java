b2308fc24aaaada9cb1eac7ae561a33dfe303c24
ecvheo1 <ecvheo@naver.com>
2022-04-28 01:12:36 +0900

    :sparkles: feat: 애플리케이션 명령 & 출력 구현

--- src/main/java/kdt/vouchermanagement/global/view/ConsoleDispatcher.java
+++ src/main/java/kdt/vouchermanagement/global/view/ConsoleDispatcher.java
@@ -4,6 +4,7 @@ import kdt.vouchermanagement.domain.voucher.controller.VoucherConsoleController;
 import kdt.vouchermanagement.domain.voucher.converter.VoucherRequestConverter;
 import kdt.vouchermanagement.domain.voucher.dto.VoucherRequest;
 import kdt.vouchermanagement.global.io.Input;
+import kdt.vouchermanagement.global.io.Output;
 import kdt.vouchermanagement.global.response.Response;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -13,12 +14,14 @@ import org.springframework.stereotype.Component;
 public class ConsoleDispatcher implements ApplicationRunner {
 
     private final Input consoleInput;
+    private final Output consoleOutput;
     private final VoucherRequestConverter voucherRequestConverter;
     private final VoucherConsoleController voucherConsoleController;
 
 
-    public ConsoleDispatcher(Input consoleInput, VoucherRequestConverter voucherRequestConverter, VoucherConsoleController voucherConsoleController) {
+    public ConsoleDispatcher(Input consoleInput, Output consoleOutput, VoucherRequestConverter voucherRequestConverter, VoucherConsoleController voucherConsoleController) {
         this.consoleInput = consoleInput;
+        this.consoleOutput = consoleOutput;
         this.voucherRequestConverter = voucherRequestConverter;
         this.voucherConsoleController = voucherConsoleController;
     }
@@ -26,27 +29,38 @@ public class ConsoleDispatcher implements ApplicationRunner {
     @Override
     public void run(ApplicationArguments args) throws Exception {
         while (true) {
-            //TODO output
+            consoleOutput.printMenu(CommandMessage.MENU.getMessage());
             Menu menu = findMenu(consoleInput.menuInput());
             switch (menu) {
                 case EXIT_PROGRAM:
                     return;
                 case CREATE_VOUCHER:
-                    //TODO output
-                    int voucherTypeNum = consoleInput.valueInput();
-                    //TODO output
-                    int discountValue = consoleInput.valueInput();
-                    VoucherRequest voucherRequest = voucherRequestConverter.of(voucherTypeNum, discountValue);
-                    Response response = voucherConsoleController.create(voucherRequest);
-                    //TODO output
+                    commandCreateVoucher();
+                    break;
                 case LIST_VOUCHERS:
-                    //TODO output
-                case BLACKLIST:
-                    //TODO output
+                    commandListVouchers();
+                    break;
                 case NONE:
-                    //TODO output
+                    consoleOutput.printMenu(CommandMessage.NONE.getMessage());
+                    break;
             }
         }
+    }
+
+    private void commandCreateVoucher() {
+        consoleOutput.printMenu(CommandMessage.VOUCHER_NUM.getMessage());
+        int voucherTypeNum = consoleInput.valueInput();
+        consoleOutput.printMenu(CommandMessage.DISCOUNT_VALUE.getMessage());
+        int discountValue = consoleInput.valueInput();
+        VoucherRequest request = voucherRequestConverter.of(voucherTypeNum, discountValue);
+
+        Response response = voucherConsoleController.create(request);
+        consoleOutput.printResponse(response);
+    }
+
+    private void commandListVouchers() {
+        Response response = voucherConsoleController.getVouchers();
+        consoleOutput.printResponse(response);
     }
 
     private Menu findMenu(String inputMenu) {
