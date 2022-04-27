763dfe3f254f231a179fc117047be0f1c8ae70fc
ecvheo1 <ecvheo@naver.com>
2022-04-28 01:28:29 +0900

    :adhesive_bandage: fix: 라인 구분자 제거

--- src/main/java/kdt/vouchermanagement/global/view/CommandMessage.java
+++ src/main/java/kdt/vouchermanagement/global/view/CommandMessage.java
@@ -2,16 +2,16 @@ package kdt.vouchermanagement.global.view;
 
 public enum CommandMessage {
     MENU("""
-            \nType exit to exit the program
+            Type exit to exit the program
             Type create to create a new voucher
             Type list to list all vouchers
             """),
     VOUCHER_NUM("""
-            \nType Number of Voucher
+            Type Number of Voucher
             1. FixedAmountVoucher
             2. PercentDiscountVoucher
             """),
-    DISCOUNT_VALUE("\nType Discount Value for Voucher\n"),
+    DISCOUNT_VALUE("Type Discount Value for Voucher\n"),
     NONE("Please valid menu!");
 
     private String message;
