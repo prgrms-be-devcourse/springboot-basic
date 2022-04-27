3750bbf988823d190bbad234a62df687dde45a7e
HyoungUkJJang <guddnr1451@gmail.com>
2022-04-28 01:28:27 +0900

    feat : CustomerRepository 메서드 저장, 조회, 삭제 메서드 추가 작성

--- src/main/java/org/prgrms/kdt/customer/repository/BlackListCustomerRepository.java
+++ src/main/java/org/prgrms/kdt/customer/repository/BlackListCustomerRepository.java
@@ -1,10 +1,15 @@
 package org.prgrms.kdt.customer.repository;
 
+import static java.time.format.DateTimeFormatter.ofPattern;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 import java.util.stream.Collectors;
 import org.prgrms.kdt.customer.model.Customer;
@@ -42,7 +47,8 @@ public class BlackListCustomerRepository implements CustomerRepository {
             new InputStreamReader(resource.getInputStream()));) {
             customers = br.lines()
                 .map(l -> l.split(","))
-                .map(s -> new Customer(UUID.fromString(s[0]), s[1]))
+                .map(s -> new Customer(UUID.fromString(s[0]), s[1],
+                    LocalDateTime.parse(s[2], DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS"))))
                 .collect(Collectors.toList());
         } catch (IOException e) {
             log.error("파일에 읽어오는데 문제가 생겼습니다.");
@@ -50,6 +56,21 @@ public class BlackListCustomerRepository implements CustomerRepository {
         }
 
         return customers;
+    }
+
+    @Override
+    public Optional<Customer> findById(UUID customerId) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Customer save(Customer customer) {
+        return null;
+    }
+
+    @Override
+    public void deleteAll() {
+
     }
 
 }
--- src/main/java/org/prgrms/kdt/customer/repository/CustomerRepository.java
+++ src/main/java/org/prgrms/kdt/customer/repository/CustomerRepository.java
@@ -1,10 +1,18 @@
 package org.prgrms.kdt.customer.repository;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 import org.prgrms.kdt.customer.model.Customer;
 
 public interface CustomerRepository {
 
     List<Customer> findAll();
+
+    Optional<Customer> findById(UUID customerId);
+
+    Customer save(Customer customer);
+
+    void deleteAll();
 
 }
