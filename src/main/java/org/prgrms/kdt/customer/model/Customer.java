96becb8bec29e364efa78895d02a823764335703
HyoungUkJJang <guddnr1451@gmail.com>
2022-04-28 01:27:46 +0900

    feat : Customer createAt 필드 추가

--- src/main/java/org/prgrms/kdt/customer/model/Customer.java
+++ src/main/java/org/prgrms/kdt/customer/model/Customer.java
@@ -1,23 +1,39 @@
 package org.prgrms.kdt.customer.model;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 public class Customer {
 
-    private final UUID uuid;
+    private final UUID customerId;
     private final String name;
+    private final LocalDateTime createdAt;
 
-    public Customer(UUID uuid, String name) {
-        this.uuid = uuid;
+    public Customer(UUID customerId, String name, LocalDateTime createdAt) {
+        this.customerId = customerId;
         this.name = name;
+        this.createdAt = createdAt;
     }
 
     @Override
     public String toString() {
         return "Customer{" +
-            "uuid=" + uuid +
+            "customerId=" + customerId +
             ", name='" + name + '\'' +
+            ", createdAt=" + createdAt +
             '}';
+    }
+
+    public UUID getCustomerId() {
+        return customerId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
     }
 
 }
