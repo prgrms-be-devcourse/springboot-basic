9f66df41753b05b97fcee3fd595720cd58b89bab
HyoungUkJJang <guddnr1451@gmail.com>
2022-04-28 01:27:20 +0900

    feat : JdbcVoucherRepository 기능 작성

--- src/main/java/org/prgrms/kdt/voucher/repository/JdbcVoucherRepository.java
+++ src/main/java/org/prgrms/kdt/voucher/repository/JdbcVoucherRepository.java
@@ -1,33 +1,95 @@
 package org.prgrms.kdt.voucher.repository;
 
+import java.nio.ByteBuffer;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import java.util.UUID;
 import org.prgrms.kdt.voucher.model.Voucher;
+import org.prgrms.kdt.voucher.model.VoucherType;
 import org.springframework.context.annotation.Profile;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
 @Profile("jdbc")
 public class JdbcVoucherRepository implements VoucherRepository {
 
+    private final NamedParameterJdbcTemplate jdbcTemplate;
+
+    private static final RowMapper<Voucher> voucherRowMapper = (resultSet, i) -> {
+        UUID voucherId = toUUID(resultSet.getBytes("voucher_id"));
+        long value = resultSet.getLong("value");
+        VoucherType type = VoucherType.valueOf(resultSet.getString("type"));
+        LocalDateTime createdAt = toLocalDateTime(resultSet.getTimestamp("created_at"));
+
+        return type.convert(voucherId, value, createdAt);
+    };
+
+    public JdbcVoucherRepository(NamedParameterJdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
     @Override
     public Voucher save(Voucher voucher) {
-        return null;
+        int update = jdbcTemplate.update(
+            "insert into vouchers(voucher_id, value, type, created_at)"
+                + " values(UUID_TO_BIN(:voucherId), :value, :type, :createdAt)"
+            , toParamMap(voucher));
+        if (update != 1) {
+            throw new RuntimeException("Noting was inserted"); // 커스텀 예외로 수정해보자
+        }
+
+        return voucher;
     }
 
     @Override
     public List<Voucher> findAll() {
-        return null;
+        return jdbcTemplate.query("select * from vouchers", voucherRowMapper);
     }
 
     @Override
-    public Voucher update(UUID voucherId, long value) {
-        return null;
+    public Optional<Voucher> findById(UUID voucherId) {
+        try {
+            return Optional.ofNullable(
+                jdbcTemplate.queryForObject(
+                    "select * from vouchers where voucher_id = UUID_TO_BIN(:voucherId)",
+                    Collections.singletonMap("voucherId", voucherId.toString().getBytes()),
+                    voucherRowMapper)
+            );
+        } catch (EmptyResultDataAccessException e) {
+            return Optional.empty();
+        }
     }
 
     @Override
     public void deleteAll() {
+        jdbcTemplate.update("delete from vouchers", Collections.emptyMap());
+    }
 
+    private Map<String, Object> toParamMap(Voucher voucher) {
+        Map<String, Object> paramMap = new HashMap<>();
+        paramMap.put("voucherId", voucher.getVoucherId().toString().getBytes());
+        paramMap.put("value", voucher.getValue());
+        paramMap.put("type", voucher.getVoucherType().toString());
+        paramMap.put("createdAt", voucher.getCreatedAt());
+
+        return paramMap;
+    }
+
+    private static UUID toUUID(byte[] bytes) {
+        var byteBuffer = ByteBuffer.wrap(bytes);
+        return new UUID(byteBuffer.getLong(), byteBuffer.getLong());
+    }
+
+    private static LocalDateTime toLocalDateTime(Timestamp timestamp) {
+        return timestamp != null ? timestamp.toLocalDateTime() : null;
     }
 
 }
