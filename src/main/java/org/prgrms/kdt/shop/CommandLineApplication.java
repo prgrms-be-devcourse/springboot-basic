f32212e280dc545737e942e58170a51f1336467a
HunkiKim <gnsrl76@naver.com>
2022-04-28 07:11:04 +0900

    refactor : delete component
    웹으로 실행하기 위해 콘솔기능 컴포넌트 제거

--- src/main/java/org/prgrms/kdt/shop/CommandLineApplication.java
+++ src/main/java/org/prgrms/kdt/shop/CommandLineApplication.java
@@ -9,18 +9,17 @@ import org.prgrms.kdt.shop.io.Output;
 import org.prgrms.kdt.shop.service.VoucherService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
-import org.springframework.stereotype.Component;
 
 import java.util.UUID;
 
-@Component
 public class CommandLineApplication implements ApplicationRunner {
     private final Input input;
     private final Output output;
     private final VoucherService voucherService;
-    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    private static final Logger logger = LoggerFactory.getLogger(CommandLineApplication.class);
 
     public CommandLineApplication(Input input, Output output, VoucherService voucherService) {
         this.input = input;
@@ -29,10 +28,10 @@ public class CommandLineApplication implements ApplicationRunner {
     }
 
     @Override
-    public void run(ApplicationArguments args) throws Exception {
+    public void run(ApplicationArguments args) {
         boolean exitFlag = true;
         while (exitFlag) {
-            output.menu();
+            output.printMenu();
             MenuStatus menuStatus = inputMenu();
             switch (menuStatus) {
                 case CREATE:
@@ -43,7 +42,6 @@ public class CommandLineApplication implements ApplicationRunner {
                     break;
                 case EXIT:
                     exitFlag = false;
-
             }
         }
         System.exit(0);
@@ -63,51 +61,51 @@ public class CommandLineApplication implements ApplicationRunner {
 
     private String inputFixedAmount( ) {
         try {
-            output.selectDiscount();
-            String inputDiscount = input.input();
-            voucherService.insert(new FixedAmountVoucher(UUID.randomUUID(), Long.parseLong(inputDiscount)));
+            output.printDiscountInput();
+            String inputDiscount = input.getInput();
+            voucherService.addVoucher(new FixedAmountVoucher(UUID.randomUUID(), Long.parseLong(inputDiscount)));
             return inputDiscount;
         } catch (Exception e) {
 
-            output.inputError();
-            logger.error("할인 입력 에러");
+            output.printInputError();
+            logger.error("할인 입력 에러", e);
         }
         return inputFixedAmount();
     }
 
     private String inputPercentDiscount( ) {
         try {
-            output.selectDiscount();
-            String inputDiscount = input.input();
-            voucherService.insert(new PercentDiscountVoucher(UUID.randomUUID(), Long.parseLong(inputDiscount)));
+            output.printDiscountInput();
+            String inputDiscount = input.getInput();
+            voucherService.addVoucher(new PercentDiscountVoucher(UUID.randomUUID(), Long.parseLong(inputDiscount)));
             return inputDiscount;
         } catch (Exception e) {
-            output.inputError();
-            logger.error("할인 입력 에러");
+            output.printInputError();
+            logger.error("할인 입력 에러", e);
         }
         return inputPercentDiscount();
     }
 
     private VoucherType inputVoucher( ) {
         try {
-            output.selectVoucher();
-            String inputVoucher = input.input();
+            output.printVoucherSelector();
+            String inputVoucher = input.getInput();
             return VoucherType.find(inputVoucher);
         } catch (IllegalArgumentException e) {
-            output.inputError();
-            logger.error("바우처 입력 에러");
+            output.printInputError();
+            logger.error("바우처 입력 에러", e);
         }
         return inputVoucher();
     }
 
     private MenuStatus inputMenu( ) {
         try {
-            output.selectMenu();
-            String inputMenu = input.input();
+            output.printMenuSelector();
+            String inputMenu = input.getInput();
             return MenuStatus.find(inputMenu);
         } catch (IllegalArgumentException e) {
-            output.inputError();
-            logger.error("메뉴 입력 에러");
+            output.printInputError();
+            logger.error("메뉴 입력 에러", e);
         }
         return inputMenu();
     }
