15e65fc968ad1aa84c77714643d0ff44b55ef962
yuminhwan <ymh7532@icloud.com>
2022-04-27 14:44:42 +0900

    refactor: SQL 테이블 수정

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -8,12 +8,13 @@ CREATE TABLE customer
     CONSTRAINT unq_user_email UNIQUE (email)
 );
 
-
 CREATE TABLE voucher
 (
     voucher_id  BINARY(16) PRIMARY KEY,
     customer_id BINARY(16),
     type        TINYINT UNSIGNED                         NOT NULL,
     discount    INT UNSIGNED                             NOT NULL,
-    created_at  datetime(6) default CURRENT_TIMESTAMP(6) not null
+    created_at  DATETIME(6) DEFAULT CURRENT_TIMESTAMP(6) NOT NULL,
+    CONSTRAINT voucher_ibfk_1 FOREIGN KEY (customer_id) REFERENCES customer (customer_id) ON UPDATE CASCADE ON DELETE CASCADE
 );
+
