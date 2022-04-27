1417444db380d59ee29fe75ad626aa3113a7c637
Hyunji <rioreo2022@gmail.com>
2022-04-27 16:27:59 +0900

    test: Product 테스트 코드 작성

--- src/main/java/org/prgrms/vouchermanager/domain/product/domain/Product.java
+++ src/main/java/org/prgrms/vouchermanager/domain/product/domain/Product.java
@@ -1,9 +1,13 @@
 package org.prgrms.vouchermanager.domain.product.domain;
 
 import lombok.Getter;
+import org.apache.logging.log4j.util.Strings;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkNotNull;
 
 @Getter
 public class Product {
@@ -16,14 +20,22 @@ public class Product {
 
     /* 가격 */
     private final long price;
-    /* 생성 일시 */
-    private final LocalDateTime createdAt = LocalDateTime.now();
-    /*  상품 상태 */
-    ProductStatus status;
+
     /* 재고 */
     private long stock;
 
+    /*  상품 상태 */
+    ProductStatus status;
+
+    /* 생성 일시 */
+    private final LocalDateTime createdAt = LocalDateTime.now();
+
     public Product(UUID id, String name, long price, long stock) {
+        checkNotNull(id);
+        checkArgument(Strings.isNotBlank(name), "name은 null 혹은 공백이 될 수 없습니다.");
+        checkArgument(price >= 0, "price는 양수여야 합니다.");
+        checkArgument(stock >= 0, "stock은 음수가 될 수 없습니다.");
+
         this.id = id;
         this.name = name;
         this.price = price;
--- src/test/java/org/prgrms/vouchermanager/domain/product/domain/ProductTest.java
+++ src/test/java/org/prgrms/vouchermanager/domain/product/domain/ProductTest.java
@@ -3,39 +3,63 @@ package org.prgrms.vouchermanager.domain.product.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.*;
 
 class ProductTest {
 
     @Nested
     @DisplayName("Product 생성자 테스트")
-    class create{
+    class create {
         @Test
         void Product_생성_성공() {
+            Product product = new Product(UUID.randomUUID(), "productName", 1000L, 10);
+
+            assertThat(product.getId()).isNotNull();
+            assertThat(product.getName()).isEqualTo("product01");
+            assertThat(product.getPrice()).isEqualTo(1000);
+            assertThat(product.getStock()).isEqualTo(10);
+            assertThat(product.getStatus()).isEqualTo(ProductStatus.FOR_SALE);
         }
 
         @Test
         void id가_null이면_예외를_던진다() {
+            assertThatNullPointerException().isThrownBy(() -> new Product(null, "productName", 1000L, 10));
         }
 
-        @Test
-        void name이_공백이면_예외를_던진다() {
+        @ParameterizedTest
+        @NullAndEmptySource
+        void name이_공백이면_예외를_던진다(String name) {
+            assertThatIllegalArgumentException().isThrownBy(() -> new Product(UUID.randomUUID(), name, 1000L, 10));
         }
 
         @Test
         void price가_음수면_예외를_던진다() {
+            assertThatIllegalArgumentException().isThrownBy(() -> new Product(UUID.randomUUID(), "productName", -1000L, 10));
         }
 
         @Test
         void stock이_음수면_예외를_던진다() {
+            assertThatIllegalArgumentException().isThrownBy(() -> new Product(UUID.randomUUID(), "productName", 1000L, -1));
         }
 
         @Test
         void stock이_0이면_status를_SOLD_OUT으로_설정한다() {
+            Product product = new Product(UUID.randomUUID(), "productName", 1000, 0);
+
+            assertThat(product.getStatus()).isEqualTo(ProductStatus.SOLD_OUT);
         }
 
         @Test
         void stock이_양수이면_status를_FOR_SALE로_설정한다() {
+            Product product = new Product(UUID.randomUUID(), "productName", 1000, 1);
 
+            assertThat(product.getStatus()).isEqualTo(ProductStatus.FOR_SALE);
         }
     }
+
 }
