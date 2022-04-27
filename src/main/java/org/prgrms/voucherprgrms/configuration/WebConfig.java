54de6efb594f9c2fa87a9fc370479c5f8177c901
ㅎㅈ <hgkimmme9@gmail.com>
2022-04-27 23:23:22 +0900

    WebConfig

---
+++ src/main/java/org/prgrms/voucherprgrms/configuration/WebConfig.java
@@ -0,0 +1,44 @@
+package org.prgrms.voucherprgrms.configuration;
+
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.http.converter.xml.MarshallingHttpMessageConverter;
+import org.springframework.oxm.xstream.XStreamMarshaller;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "org.prgrms.voucherprgrms.voucher")
+public class WebConfig implements WebMvcConfigurer {
+
+    //xml to Object mapping
+    @Override
+    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+
+        //json localdatetime 형식 지정
+        var javaTimeModule = new JavaTimeModule();
+        javaTimeModule.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer(DateTimeFormatter.ISO_DATE_TIME));
+
+        var modules = Jackson2ObjectMapperBuilder.json().modules(javaTimeModule);
+        converters.add(0, new MappingJackson2HttpMessageConverter(modules.build()));
+
+        //xml Converter
+        var messageConverter = new MarshallingHttpMessageConverter();
+        var xStreamMarshaller = new XStreamMarshaller();
+        messageConverter.setMarshaller(xStreamMarshaller);
+        messageConverter.setUnmarshaller(xStreamMarshaller);
+        converters.add(1, messageConverter);
+
+    }
+
+}
