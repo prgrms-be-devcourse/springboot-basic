5ee99f14e5da91a2676f0aa39086b44472a95c1a
HyoungUkJJang <guddnr1451@gmail.com>
2022-04-28 01:22:40 +0900

    feat : DataSource, NamedParameterJdbcTemplate 빈 추가

--- src/main/java/org/prgrms/kdt/config/VoucherConfig.java
+++ src/main/java/org/prgrms/kdt/config/VoucherConfig.java
@@ -1,9 +1,13 @@
 package org.prgrms.kdt.config;
 
+import javax.sql.DataSource;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
 @Configuration
 @ComponentScan(basePackages = {"org.prgrms.kdt.voucher",
@@ -12,5 +16,19 @@ import org.springframework.context.annotation.PropertySource;
 @EnableConfigurationProperties
 @PropertySource(value = "application.yaml", factory = YamlPropertiesFactory.class)
 public class VoucherConfig {
+
+    @Bean
+    public DataSource dataSource() {
+       return  DataSourceBuilder.create()
+            .url("jdbc:mysql://localhost:3306/voucher_mgmt")
+            .username("root")
+            .password("asd1234!")
+            .build();
+    }
+
+    @Bean
+    public NamedParameterJdbcTemplate jdbcTemplate() {
+        return new NamedParameterJdbcTemplate(dataSource());
+    }
 
 }
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -3,6 +3,12 @@ create table vouchers
     voucher_id BINARY(16) PRIMARY KEY,
     value bigint NOT NULL,
     type VARCHAR(30) NOT NULL,
-    created_at datetime(6) not null,
-    updated_at datetime(6) DEFAULT null
+    created_at datetime(6) not null
+);
+
+create table customers
+(
+    customer_id BINARY(16) PRIMARY KEY,
+    name varchar(50) NOT NULL,
+    created_at datetime(6) not null
 );
