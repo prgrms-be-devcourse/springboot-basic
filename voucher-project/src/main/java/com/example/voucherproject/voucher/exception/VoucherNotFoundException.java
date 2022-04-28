c7e792ad1155694eaccb9516984e14d57300fa2e
kimziou77 <kimziou77@naver.com>
2022-04-28 20:43:22 +0900

    (Feat) VoucherNotFoundException 제작

---
+++ voucher-project/src/main/java/com/example/voucherproject/voucher/exception/VoucherNotFoundException.java
@@ -0,0 +1,11 @@
+package com.example.voucherproject.voucher.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class VoucherNotFoundException extends RuntimeException{
+    public VoucherNotFoundException(String message){
+        super(message);
+    }
+}
