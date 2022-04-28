93b42b02d541342b36581e7b98ccf38064cf43ef
ynoolee <heerarea129@gmail.com>
2022-04-28 10:54:11 +0900

    Refactor: VoucherRepository 행위 추가 및 행위이름변경

--- src/main/java/com/prgrms/vouchermanagement/voucher/repository/VoucherRepository.java
+++ src/main/java/com/prgrms/vouchermanagement/voucher/repository/VoucherRepository.java
@@ -1,6 +1,7 @@
 package com.prgrms.vouchermanagement.voucher.repository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 import com.prgrms.vouchermanagement.voucher.domain.Voucher;
@@ -8,7 +9,11 @@ import com.prgrms.vouchermanagement.voucher.domain.Voucher;
 public interface VoucherRepository {
 	List<Voucher> findAllVoucher();
 
-	Voucher save(Voucher voucher);
+	Voucher insert(Voucher voucher);
 
-	Voucher findById(UUID id);
+	Optional<Voucher> findById(UUID id);
+
+	boolean deleteById(UUID id);
+
+	boolean deleteAll();
 }
