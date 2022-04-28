20a2e65d989f9c9cd0ca61afe9895ca0ecf6ddf5
wisehero <godjw96@gmail.com>
2022-04-28 22:21:59 +0900

    # 피드백 반영 : FileRepository 주석 제거

--- src/main/java/org/programmers/kdtspring/repository/voucher/FileVoucherRepository.java
+++ src/main/java/org/programmers/kdtspring/repository/voucher/FileVoucherRepository.java
@@ -1,5 +1,6 @@
 package org.programmers.kdtspring.repository.voucher;
 
+import org.programmers.kdtspring.entity.user.Customer;
 import org.programmers.kdtspring.entity.voucher.FixedAmountVoucher;
 import org.programmers.kdtspring.entity.voucher.PercentDiscountVoucher;
 import org.programmers.kdtspring.entity.voucher.Voucher;
@@ -15,54 +16,81 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-//@Repository
-//@Profile("default")
-//public class FileVoucherRepository implements VoucherRepository {
-//
-//    private final String fileName = "voucherList.txt";
-//    private final FileWriter fw = new FileWriter(fileName, true);
-//    private final BufferedWriter bf = new BufferedWriter(fw);
-//    private final FileReader fr = new FileReader(fileName);
-//    private final BufferedReader br = new BufferedReader(fr);
-//    private static final Logger logger = LoggerFactory.getLogger(FileVoucherRepository.class);
-//
-//    public FileVoucherRepository() throws IOException {
-//    }
-//
-//    @Override
-//    public void save(Voucher voucher) {
-//
-//        logger.info("[FileVoucherRepository] save(Voucher voucher) called");
-//
-//        try {
-//            String stringToWrite = voucher.getVoucherId() + ", " +
-//                    voucher.getDiscount() + ", " + voucher.getVoucherType();
-//            bf.write(stringToWrite);
-//            bf.newLine();
-//            bf.flush();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//
-//        logger.info("Voucher saved");
-//    }
-//
-//    @Override
-//    public List<Voucher> findAll() throws IOException {
-//
-//        logger.info("[FileVoucherRepository] findAll() called");
-//        List<Voucher> vouchers = new ArrayList<>();
-//        String readLine = null;
-//        while ((readLine = br.readLine()) != null) {
-//            String[] readLineSplit = readLine.split(",");
-//            if (readLineSplit[2].equalsIgnoreCase(String.valueOf(VoucherType.FixedAmountVoucher))) {
-//                vouchers.add(new FixedAmountVoucher(UUID.fromString(readLineSplit[0]), Long.parseLong(readLineSplit[1].trim())));
-//            } else {
-//                vouchers.add(new PercentDiscountVoucher(UUID.fromString(readLineSplit[0]), Long.parseLong(readLineSplit[1].trim())));
-//            }
-//        }
-//        return vouchers;
-////    }
-//
-//
-//}
+@Repository
+@Profile("default")
+public class FileVoucherRepository implements VoucherRepository {
+
+    private static final Logger logger = LoggerFactory.getLogger(FileVoucherRepository.class);
+    private final String fileName = "voucherList.txt";
+    private final FileWriter fw = new FileWriter(fileName, true);
+    private final BufferedWriter bf = new BufferedWriter(fw);
+    private final FileReader fr = new FileReader(fileName);
+    private final BufferedReader br = new BufferedReader(fr);
+
+    public FileVoucherRepository() throws IOException {
+    }
+
+    @Override
+    public void save(Voucher voucher) {
+
+        logger.info("[FileVoucherRepository] save(Voucher voucher) called");
+
+        try {
+            String stringToWrite = voucher.getVoucherId() + ", " +
+                    voucher.getDiscount() + ", " + voucher.getVoucherType();
+            bf.write(stringToWrite);
+            bf.newLine();
+            bf.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        logger.info("Voucher saved");
+    }
+
+    @Override
+    public Optional<Voucher> findById(UUID voucherId) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Voucher> findAll() {
+
+        logger.info("[FileVoucherRepository] findAll() called");
+        List<Voucher> vouchers = new ArrayList<>();
+        String readLine = null;
+        try {
+            while ((readLine = br.readLine()) != null) {
+                String[] readLineSplit = readLine.split(",");
+                if (readLineSplit[2].equalsIgnoreCase(String.valueOf(VoucherType.FixedAmountVoucher))) {
+                    vouchers.add(new FixedAmountVoucher(UUID.fromString(readLineSplit[0]), Integer.parseInt(readLineSplit[1]), String.valueOf(readLineSplit[2])));
+                } else {
+                    vouchers.add(new PercentDiscountVoucher(UUID.fromString(readLineSplit[0]), Integer.parseInt(readLineSplit[1]), String.valueOf(readLineSplit[2])));
+                }
+            }
+        } catch (IOException e) {
+            logger.error("{} error occurred", IOException.class);
+        }
+        return vouchers;
+    }
+
+    @Override
+    public List<Voucher> findByCustomer(Customer customer) {
+        return null;
+    }
+
+    @Override
+    public Voucher updateCustomerId(Voucher voucher) {
+        return null;
+    }
+
+    @Override
+    public void deleteOne(Voucher voucher) {
+
+    }
+
+    @Override
+    public void deleteAll() {
+
+    }
+}
