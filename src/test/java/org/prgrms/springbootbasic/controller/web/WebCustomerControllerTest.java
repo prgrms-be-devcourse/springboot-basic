2e89a654c74fd88cd6b2924c2b155894032abb14
hyuk0309 <bahngeh@naver.com>
2022-04-28 21:03:07 +0900

    refactor : WebCustomerControllerTest refactor

--- src/test/java/org/prgrms/springbootbasic/controller/web/WebCustomerControllerTest.java
+++ src/test/java/org/prgrms/springbootbasic/controller/web/WebCustomerControllerTest.java
@@ -3,7 +3,7 @@ package org.prgrms.springbootbasic.controller.web;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -30,8 +30,8 @@ class WebCustomerControllerTest {
     private CustomerService customerService;
 
     @Test
-    @DisplayName("손님 전체 조회 페이지")
-    void viewAllVouchers() throws Exception {
+    @DisplayName("전체 손님 페이지 조회")
+    void testViewAllVouchers() throws Exception {
         //given
         given(customerService.findAllCustomers())
             .willReturn(List.of(
@@ -42,12 +42,13 @@ class WebCustomerControllerTest {
         //when
         //then
         mockMvc.perform(get("/customers"))
-            .andExpect(status().isOk());
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.TEXT_HTML + ";charset=UTF-8"));
     }
 
     @Test
-    @DisplayName("손님 상세 페이지")
-    void viewCustomer() throws Exception {
+    @DisplayName("손님 상세 페이지 조회")
+    void testViewCustomer() throws Exception {
         //given
         var customer = new Customer(UUID.randomUUID(), "test", "test@gmail.com");
         given(customerService.findCustomer(customer.getCustomerId()))
@@ -56,22 +57,24 @@ class WebCustomerControllerTest {
         //when
         //then
         mockMvc.perform(get("/customers/" + customer.getCustomerId().toString()))
-            .andExpect(status().isOk());
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.TEXT_HTML + ";charset=UTF-8"));
     }
 
     @Test
-    @DisplayName("회원 등록 폼")
-    void viewRegisterForm() throws Exception {
+    @DisplayName("회원 등록 폼 페이지 조회")
+    void testViewRegisterForm() throws Exception {
         //given
         //when
         //then
         mockMvc.perform(get("/customers/new"))
-            .andExpect(status().isOk());
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.TEXT_HTML + ";charset=UTF-8"));
     }
 
     @Test
-    @DisplayName("회원 등록")
-    void registerCustomer() throws Exception {
+    @DisplayName("회원 등록 요청")
+    void testRegisterCustomer() throws Exception {
         //given
         //when
         //then
@@ -80,12 +83,11 @@ class WebCustomerControllerTest {
                 .param("name", "test")
                 .param("email", "test@gmail.com"))
             .andExpect(status().is3xxRedirection())
-            .andExpect(header().string(HttpHeaders.LOCATION, "/customers"))
-            .andDo(print());
+            .andExpect(header().string(HttpHeaders.LOCATION, "/customers"));
     }
 
     @Test
-    @DisplayName("회원 삭제")
+    @DisplayName("회원 삭제 요청")
     void testDeleteCustomer() throws Exception {
         //given
         var customerId = UUID.randomUUID();
@@ -95,7 +97,6 @@ class WebCustomerControllerTest {
         //then
         mockMvc.perform(post("/customers/" + customerId.toString() + "/delete"))
             .andExpect(status().is3xxRedirection())
-            .andExpect(header().string(HttpHeaders.LOCATION, "/customers"))
-            .andDo(print());
+            .andExpect(header().string(HttpHeaders.LOCATION, "/customers"));
     }
 }
