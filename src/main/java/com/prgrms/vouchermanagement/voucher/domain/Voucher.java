edb63992af41302c7bf6fe901e965cfecfd697b2
ynoolee <heerarea129@gmail.com>
2022-04-28 10:17:31 +0900

    Refactor: Voucher 에 필요한 행위 추가

--- src/main/java/com/prgrms/vouchermanagement/voucher/domain/Voucher.java
+++ src/main/java/com/prgrms/vouchermanagement/voucher/domain/Voucher.java
@@ -1,9 +1,18 @@
 package com.prgrms.vouchermanagement.voucher.domain;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
+
+import com.prgrms.vouchermanagement.voucher.VoucherType;
 
 public interface Voucher {
 	UUID getVoucherId();
+
+	long getDiscountInfo();
+
+	VoucherType getType();
+
+	LocalDateTime getCreatedTime();
 
 	long discount(long beforeDiscount);
 }
