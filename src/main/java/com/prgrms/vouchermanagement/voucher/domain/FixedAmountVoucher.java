f59d7509e1e90b43ef97e1236d01598395b80934
ynoolee <heerarea129@gmail.com>
2022-04-28 10:25:25 +0900

    Refactor: type, createdAt 필드 추가 및 Voucher 에 추가된 요구사항 구현

--- src/main/java/com/prgrms/vouchermanagement/voucher/domain/FixedAmountVoucher.java
+++ src/main/java/com/prgrms/vouchermanagement/voucher/domain/FixedAmountVoucher.java
@@ -1,22 +1,43 @@
 package com.prgrms.vouchermanagement.voucher.domain;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
+import com.prgrms.vouchermanagement.voucher.VoucherType;
+
 public class FixedAmountVoucher implements Voucher {
+	private final VoucherType type = VoucherType.FIXED;
 	private final UUID voucherId;
 	private final long amount;
+	private final LocalDateTime createdAt;
 
-	public FixedAmountVoucher(UUID voucherId, long amount) {
+	public FixedAmountVoucher(UUID voucherId, long amount, LocalDateTime createdAt) {
 		if (amount <= 0) {
 			throw new IllegalArgumentException("FixedAmountVoucher - Amount should be positive");
 		}
 		this.voucherId = voucherId;
 		this.amount = amount;
+		this.createdAt = createdAt;
+	}
+
+	@Override
+	public long getDiscountInfo() {
+		return amount;
+	}
+
+	@Override
+	public VoucherType getType() {
+		return type;
 	}
 
 	@Override
 	public UUID getVoucherId() {
 		return this.voucherId;
+	}
+
+	@Override
+	public LocalDateTime getCreatedTime() {
+		return createdAt;
 	}
 
 	public long discount(long beforeDiscount) {
@@ -31,4 +52,5 @@ public class FixedAmountVoucher implements Voucher {
 			"amount=" + amount +
 			'}';
 	}
+
 }
--- src/main/java/com/prgrms/vouchermanagement/voucher/domain/PercentDiscountVoucher.java
+++ src/main/java/com/prgrms/vouchermanagement/voucher/domain/PercentDiscountVoucher.java
@@ -1,22 +1,43 @@
 package com.prgrms.vouchermanagement.voucher.domain;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
+import com.prgrms.vouchermanagement.voucher.VoucherType;
+
 public class PercentDiscountVoucher implements Voucher {
+	private final VoucherType type = VoucherType.PERCENT;
 	private final UUID voucherId;
 	private final long percent;
+	private final LocalDateTime createdAt;
 
-	public PercentDiscountVoucher(UUID voucherId, long percent) {
-		if (percent <= 0 || percent > 100)
-			throw new IllegalArgumentException("PercentDiscountVoucher - percent should be between 0 and 100");
-
+	public PercentDiscountVoucher(UUID voucherId, long percent, LocalDateTime createdAt) {
+		if (percent <= 0 || percent > 100) {
+			throw new IllegalArgumentException("PercentDiscountVoucher - 퍼센트는 0과 100 사이여야 합니다.");
+		}
 		this.voucherId = voucherId;
 		this.percent = percent;
+		this.createdAt = createdAt;
+	}
+
+	@Override
+	public long getDiscountInfo() {
+		return percent;
+	}
+
+	@Override
+	public VoucherType getType() {
+		return type;
 	}
 
 	@Override
 	public UUID getVoucherId() {
 		return voucherId;
+	}
+
+	@Override
+	public LocalDateTime getCreatedTime() {
+		return createdAt;
 	}
 
 	@Override
