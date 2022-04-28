a6582b9a331d224fe3a7e0b3e338ec25059d1ab6
unnokid <kimki0609@gmail.com>
2022-04-28 16:30:29 +0900

    Remove logger

--- src/main/java/org/programmer/kdtspringboot/voucher/VoucherController.java
+++ src/main/java/org/programmer/kdtspringboot/voucher/VoucherController.java
@@ -1,20 +1,14 @@
 package org.programmer.kdtspringboot.voucher;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.time.LocalDateTime;
-import java.util.List;
-import java.util.UUID;
 
 @Controller
 public class VoucherController {
-    private static final Logger logger = LoggerFactory.getLogger(VoucherController.class);
     private final VoucherService voucherService;
 
     public VoucherController(VoucherService voucherService) {
@@ -23,7 +17,7 @@ public class VoucherController {
 
     //초기 메인 화면
     @GetMapping(value = "/")
-    public String home(){
+    public String home() {
         return "views/home";
     }
 
@@ -32,56 +26,46 @@ public class VoucherController {
     public String viewVouchersPage(Model model) {
         var allCustomers = voucherService.findAllVouchers();
         model.addAttribute("serverTime", LocalDateTime.now());
-        model.addAttribute("vouchers",allCustomers);
+        model.addAttribute("vouchers", allCustomers);
         return "views/vouchers";
     }
 
     //새로 생성
     @GetMapping("/vouchers/new")
-    public String viewNewVoucherPage(){
+    public String viewNewVoucherPage() {
         return "views/new-vouchers";
     }
 
     @GetMapping("/vouchers/search")
-    public String searchVoucherPage(){
+    public String searchVoucherPage() {
         return "views/search-vouchers";
     }
 
     @GetMapping("/vouchers/delete")
-    public String deleteVoucherPage(){
+    public String deleteVoucherPage() {
         return "views/delete-vouchers";
     }
 
     //Voucher 생성
     @PostMapping("/vouchers/new")
-    public String addNewVoucher(CreateVoucherRequest createVoucherRequest){
-        logger.info("CreateVoucherRequest: {} and {}",createVoucherRequest.getType(),createVoucherRequest.getValue());
-        voucherService.createVoucher(createVoucherRequest.getType(),createVoucherRequest.getValue());
+    public String addNewVoucher(CreateVoucherRequest createVoucherRequest) {
+        voucherService.createVoucher(createVoucherRequest.getType(), createVoucherRequest.getValue());
         return "redirect:/";
     }
 
     //Voucher 삭제
     @PostMapping("/vouchers/delete")
-    public String deleteVoucher(SearchDeleteVoucherRequest searchDeleteVoucherRequest){
-        logger.info("SDVoucherRequest: {} ",searchDeleteVoucherRequest.getVoucherId());
+    public String deleteVoucher(SearchDeleteVoucherRequest searchDeleteVoucherRequest) {
         voucherService.deleteVoucher(searchDeleteVoucherRequest.getVoucherId());
         return "redirect:/";
     }
 
     //Voucher 찾기
     @PostMapping("/vouchers/search")
-    public String searchVoucher(Model model,SearchDeleteVoucherRequest searchDeleteVoucherRequest){
-        logger.info("SDVoucherRequest: {} ",searchDeleteVoucherRequest.getVoucherId());
+    public String searchVoucher(Model model, SearchDeleteVoucherRequest searchDeleteVoucherRequest) {
         model.addAttribute("serverTime", LocalDateTime.now());
-        model.addAttribute("vouchers",voucherService.findByIdVoucher(searchDeleteVoucherRequest.getVoucherId()));
+        model.addAttribute("vouchers", voucherService.findByIdVoucher(searchDeleteVoucherRequest.getVoucherId()));
         return "views/vouchers";
     }
-
-    @GetMapping("/api/v1/vouchers")
-    @ResponseBody
-    public List<Voucher> findVouchers(){
-        return voucherService.findAllVouchers();
-    }
-
 
 }
