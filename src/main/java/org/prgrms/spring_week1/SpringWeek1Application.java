b5f1e52bc379ed302338a1dea202cf057c3dcc7c
suy2on <tnwjdrms@naver.com>
2022-04-28 15:58:51 +0900

    # feat : command application에 처음에 고객 만들어지게 수정

--- src/main/java/org/prgrms/spring_week1/SpringWeek1Application.java
+++ src/main/java/org/prgrms/spring_week1/SpringWeek1Application.java
@@ -2,6 +2,11 @@ package org.prgrms.spring_week1;
 
 import static java.lang.System.exit;
 
+import java.util.UUID;
+import org.prgrms.spring_week1.Voucher.model.VoucherType;
+import org.prgrms.spring_week1.customer.model.Customer;
+import org.prgrms.spring_week1.customer.model.Gender;
+import org.prgrms.spring_week1.customer.service.CustomerService;
 import org.prgrms.spring_week1.order.OrderService;
 import org.prgrms.spring_week1.Voucher.VoucherService;
 import org.prgrms.spring_week1.Voucher.model.Voucher;
@@ -23,8 +28,12 @@ public class SpringWeek1Application {
             .run(SpringWeek1Application.class, args);
 
         VoucherService voucherService = ac.getBean(VoucherService.class);
-        OrderService orderService = ac.getBean(OrderService.class);
         ConsoleService consoleService = ac.getBean(ConsoleService.class);
+        CustomerService customerService = ac.getBean(CustomerService.class);
+
+        // 현재 고객 저장
+        Customer customer = new Customer(UUID.randomUUID(), "customer1", "address", Gender.FEMALE, "010-2222-2222" );
+        customerService.insert(customer);
 
         while (true) {
             consoleService.mainMenu();
@@ -37,13 +46,14 @@ public class SpringWeek1Application {
                 if (type.equals("1")) { // fixedVoucher
                     consoleService.output("정해진 할인가격을 입력하세요.");
                     String amount = consoleService.input();
-                    voucherService.createFixedVoucher(Long.parseLong(amount));
+                    voucherService.createVoucher(Long.parseLong(amount), customer.getCustomerId(), VoucherType.FIXEDAMOUNT);
 
 
                 } else if (type.equals("2")) { // percentVoucher
                     consoleService.output("할인율을 입력하세요");
                     String percent = consoleService.input();
-                    voucherService.createPercentVoucher(Long.parseLong(percent));
+                    voucherService.createVoucher(Long.parseLong(percent), customer.getCustomerId(),
+                        VoucherType.PERCENTDISCOUNT);
 
                 } else { // 잘못된 입력
                     logger.info("사용자가 생성할바우처 종류선택에서 기대되지않는 입력을 했습니다.");
