53bc0ed091beb9d1c051a151550782cd03bf4c9b
beomsic <qjatjr29@khu.ac.kr>
2022-04-28 16:01:57 +0900

    [Feat] - REST API - ResponseEntity 사용

--- src/main/java/com/prgrms/voucher_manager/customer/controller/api/RestCustomerController.java
+++ src/main/java/com/prgrms/voucher_manager/customer/controller/api/RestCustomerController.java
@@ -7,14 +7,20 @@ import com.prgrms.voucher_manager.customer.service.CustomerService;
 import com.prgrms.voucher_manager.wallet.service.WalletService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 @RestController()
+@CrossOrigin(origins = "*")
 @RequestMapping("/api/customers")
 public class RestCustomerController {
 
@@ -30,41 +36,55 @@ public class RestCustomerController {
     }
 
     @GetMapping("")
-    public List<CustomerDto> customers() {
+    public ResponseEntity customers() {
         List<Customer> customers = customerService.findAllCustomer();
         List<CustomerDto> customerDtos = new ArrayList<>();
         customers.forEach(v -> customerDtos.add(entityToDto(v)));
-        return customerDtos;
+        return ResponseEntity.ok()
+                .headers(getHttpHeaders())
+                .body(customerDtos);
     }
 
     @GetMapping("{customerId}")
-    public CustomerDto customer(@PathVariable UUID customerId) {
+    public ResponseEntity customer(@PathVariable UUID customerId) {
         Optional<Customer> customer = customerService.findCustomerById(customerId);
-        return customer.map(RestCustomerController::entityToDto).orElse(null);
+        if(customer.isPresent()) {
+            return ResponseEntity.ok()
+                    .headers(getHttpHeaders())
+                    .body(entityToDto(customer.get()));
+        }
+        return ResponseEntity.noContent().build();
     }
 
     @PostMapping("/add")
-    public CustomerDto restAddCustomer(@RequestBody CustomerDto customerDto) {
+    public ResponseEntity restAddCustomer(@RequestBody CustomerDto customerDto) {
         logger.info("save Customer {}", customerDto);
         customerService.createCustomer(customerDto.getName(), customerDto.getEmail());
-        return customerDto;
+        return ResponseEntity.created(URI.create("/api/customers/add")).build();
     }
 
-    @PostMapping("/{customerId}/voucher/{voucherId}")
-    public void restAddVoucher(@PathVariable UUID customerId, @PathVariable UUID voucherId) {
+    @PostMapping()
+    public ResponseEntity restAddVoucher(@PathVariable UUID customerId, @PathVariable UUID voucherId) {
         walletService.createWallet(customerId, voucherId);
+        return ResponseEntity.created(URI.create("/api/customers/{customerId}/voucher/{voucherId}")).build();
     }
 
     @DeleteMapping("/{customerId}")
-    public Customer restDeleteCustomer(@PathVariable UUID customerId) {
+    public ResponseEntity restDeleteCustomer(@PathVariable UUID customerId) {
         Optional<Customer> deleteCustomer = customerService.findCustomerById(customerId);
         deleteCustomer.ifPresent(customerService::deleteCustomer);
-        return deleteCustomer.get();
+        return ResponseEntity.noContent().build();
     }
-
 
     private static CustomerDto entityToDto(Customer customer) {
         return customer.toCustomerDto();
+    }
+
+    private HttpHeaders getHttpHeaders() {
+        HttpHeaders headers = new HttpHeaders();
+        MediaType mediaType = new MediaType("application", "json", StandardCharsets.UTF_8);
+        headers.setContentType(mediaType);
+        return headers;
     }
 
 }
--- src/main/java/com/prgrms/voucher_manager/voucher/controller/api/RestVoucherController.java
+++ src/main/java/com/prgrms/voucher_manager/voucher/controller/api/RestVoucherController.java
@@ -9,13 +9,21 @@ import com.prgrms.voucher_manager.voucher.controller.VoucherDto;
 import com.prgrms.voucher_manager.voucher.service.VoucherService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
 @RestController()
+@CrossOrigin(origins = "*")
 @RequestMapping("/api/vouchers")
 public class RestVoucherController {
 
@@ -30,48 +38,59 @@ public class RestVoucherController {
     }
 
     @GetMapping("")
-    public List<VoucherDto> vouchers() {
+    public ResponseEntity vouchers() {
         List<Voucher> vouchers = voucherService.getFindAllVoucher();
-        return getVoucherDtoList(vouchers);
+        List<VoucherDto> voucherDtos = getVoucherDtoList(vouchers);
+        return ResponseEntity.ok()
+                .headers(getHttpHeaders())
+                .body(voucherDtos);
     }
 
+
     @GetMapping("/{voucherId}")
-    public VoucherDto voucherById(@PathVariable UUID voucherId) {
+    public ResponseEntity voucherById(@PathVariable UUID voucherId) {
         Voucher voucher = voucherService.findById(voucherId);
-        return entityToDto(voucher);
+        return ResponseEntity.ok()
+                .headers(getHttpHeaders())
+                .body(entityToDto(voucher));
     }
 
     @GetMapping("/type/{type}")
-    public List<VoucherDto> voucherByType(@PathVariable String type) {
+    public ResponseEntity voucherByType(@PathVariable String type) {
         List<Voucher> vouchers = voucherService.findByType(type);
-        return getVoucherDtoList(vouchers);
+        List<VoucherDto> voucherDtos = getVoucherDtoList(vouchers);
+        return ResponseEntity.ok()
+                .headers(getHttpHeaders())
+                .body(voucherDtos);
     }
 
     @GetMapping("/customer/{type}")
-    public List<CustomerDto> customerByVoucherType(@PathVariable String type) {
+    public ResponseEntity customerByVoucherType(@PathVariable String type) {
         List<Customer> customers = voucherServiceFacade.findCustomerByVoucherType(type);
         List<CustomerDto> customerDtos = new ArrayList<>();
         customers.forEach(customer -> customerDtos.add(customer.toCustomerDto()));
-        return customerDtos;
+        return ResponseEntity.ok()
+                .headers(getHttpHeaders())
+                .body(customerDtos);
     }
 
 
     @PostMapping("/add")
-    public VoucherDto restAddVoucher(@RequestBody VoucherDto voucherDto) {
+    public ResponseEntity restAddVoucher(@RequestBody VoucherDto voucherDto) {
         logger.info("save Voucher {}", voucherDto);
         voucherService.createVoucher(voucherDto.getType(), voucherDto.getValue());
-        return voucherDto;
+        return ResponseEntity.created(URI.create("/api/vouchers/add")).build();
     }
 
 
     @DeleteMapping("/{voucherId}")
-    public Voucher restDeleteVoucher(@PathVariable UUID voucherId) {
+    public ResponseEntity restDeleteVoucher(@PathVariable UUID voucherId) {
         Voucher deleteVoucher = voucherService.findById(voucherId);
         voucherService.deleteVoucher(deleteVoucher);
-        return deleteVoucher;
+        return ResponseEntity.noContent().build();
     }
 
-    private static VoucherDto entityToDto(Voucher voucher) {
+    private VoucherDto entityToDto(Voucher voucher) {
         return voucher.toVoucherDto();
     }
 
@@ -79,6 +98,13 @@ public class RestVoucherController {
         List<VoucherDto> voucherDtos = new ArrayList<>();
         vouchers.forEach(voucher -> voucherDtos.add(entityToDto(voucher)));
         return voucherDtos;
+    }
+
+    private HttpHeaders getHttpHeaders() {
+        HttpHeaders headers = new HttpHeaders();
+        MediaType mediaType = new MediaType("application", "json", StandardCharsets.UTF_8);
+        headers.setContentType(mediaType);
+        return headers;
     }
 
 }
