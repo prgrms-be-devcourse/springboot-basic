17553be26eefa99376aff3594dbd84e9447fd36d
jki503 <jki503@naver.com>
2022-04-29 20:55:22 +0900

    refactor : percentVoucher validtion 메서드로 나누고, 생성자 두개, percent -> amount
    - fixedvoucher와 통일성 주기 위해 percent -> amount로 변경
    - 생성시 createdAt을 변수로 받거나 받지 않는 경우로 나누기
    - validateAmount를 private method로 빼주기
    
    feat : voucherRepository.java inteface에 바우처타입으로 리스트 받아오는 메서드 추가

--- part1/src/main/java/com/programmers/part1/domain/voucher/PercentAmountVoucher.java
+++ part1/src/main/java/com/programmers/part1/domain/voucher/PercentAmountVoucher.java
@@ -11,44 +11,44 @@ public class PercentAmountVoucher implements Voucher {
 
     private final UUID voucherId;
     private final VoucherType voucherType;
-    private final int percent;
+    private final int amount;
     private final LocalDateTime createdAt;
 
-    public PercentAmountVoucher(UUID voucherId, int percent) throws PercentErrorException{
-        validatePercent(percent);
+    public PercentAmountVoucher(UUID voucherId, int amount) throws PercentErrorException{
+        validatePercent(amount);
         this.voucherId = voucherId;
-        this.percent = percent;
+        this.amount = amount;
         this.voucherType = VoucherType.PERCENT;
         this.createdAt = LocalDateTime.now();
     }
 
-    public PercentAmountVoucher(UUID voucherId, int percent, LocalDateTime createdAt){
-        validatePercent(percent);
+    public PercentAmountVoucher(UUID voucherId, int amount, LocalDateTime createdAt){
+        validatePercent(amount);
         this.voucherId = voucherId;
-        this.percent = percent;
+        this.amount = amount;
         this.voucherType = VoucherType.PERCENT;
         this.createdAt = createdAt;
     }
 
     @Override
     public int getAmount() {
-        return this.percent;
+        return this.amount;
     }
 
     @Override
     public int discount(int beforeDiscount) {
-        return beforeDiscount * (100 - percent) / 100;
+        return beforeDiscount * (100 - amount) / 100;
     }
 
     @Override
     public String toString() {
-        return String.format("%-20s %-6d", "PercentAmountVoucher", this.percent);
+        return String.format("%-20s %-6d", "PercentAmountVoucher", this.amount);
     }
 
-    private void validatePercent(int percent){
-        if(percent > 100)
+    private void validatePercent(int amount){
+        if(amount > 100)
             throw new PercentErrorException("비율은 100%를 넘길 수 없습니다.\n");
-        else if(percent <= 0)
+        else if(amount <= 0)
             throw new PercentErrorException("비율은 0%보다 커야합니다.\n");
     }
 }
