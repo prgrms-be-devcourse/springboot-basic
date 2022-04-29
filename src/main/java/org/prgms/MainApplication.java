045e679c369a4ac5411d1e5b1136c60471bc371f
hymn-fly <rlfrmsdh1@gmail.com>
2022-04-29 14:52:37 +0900

    수정  - voucher-detail.html input tag `disabled`, `readonly` attribute 추가

--- src/main/java/org/prgms/MainApplication.java
+++ src/main/java/org/prgms/MainApplication.java
@@ -2,10 +2,9 @@ package org.prgms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class MainApplication implements WebMvcConfigurer {
+public class MainApplication {
     public static void main(String[] args) throws Exception {
         SpringApplication.run(MainApplication.class, args);
     }
