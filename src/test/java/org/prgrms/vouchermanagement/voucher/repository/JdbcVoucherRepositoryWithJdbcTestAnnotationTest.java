26014746d18c62e22e4722d75cca669d31613229
sdardew <sdardew@gmail.com>
2022-04-30 20:54:18 +0900

    Chore: 연결 성공

--- src/test/java/org/prgrms/vouchermanagement/voucher/repository/JdbcVoucherRepositoryWithJdbcTestAnnotationTest.java
+++ src/test/java/org/prgrms/vouchermanagement/voucher/repository/JdbcVoucherRepositoryWithJdbcTestAnnotationTest.java
@@ -1,8 +1,10 @@
 package org.prgrms.vouchermanagement.voucher.repository;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,10 +13,9 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
-//@SpringBootTest
-//@ExtendWith(SpringExtension.class)
-@JdbcTest
+@ImportAutoConfiguration(DataSourceConfig.class)
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@JdbcTest
 class JdbcVoucherRepositoryWithJdbcTestAnnotationTest {
 
   @Mock
