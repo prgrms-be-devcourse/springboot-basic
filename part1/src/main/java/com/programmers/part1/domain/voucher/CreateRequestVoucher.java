22cd03c99e2a38f2d014da1bc14804619628af2f
jki503 <jki503@naver.com>
2022-04-30 15:38:26 +0900

    fix! : 생성자 추가, 생성자 미기입으로 인해 템플릿으로부터 받아오지 못하는 문제 해결

--- part1/src/main/java/com/programmers/part1/domain/voucher/CreateRequestVoucher.java
+++ part1/src/main/java/com/programmers/part1/domain/voucher/CreateRequestVoucher.java
@@ -4,6 +4,11 @@ import lombok.Getter;
 
 @Getter
 public class CreateRequestVoucher {
-    private String voucherType;
-    private int amount;
+    private final String voucherType;
+    private final int amount;
+
+    public CreateRequestVoucher(String voucherType, int amount) {
+        this.voucherType = voucherType;
+        this.amount = amount;
+    }
 }
