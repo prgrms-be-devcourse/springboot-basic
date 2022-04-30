b26a2578d9a9d6d24bfbf50c8e0e47cb62cea73f
wisehero <godjw96@gmail.com>
2022-04-30 22:40:03 +0900

    # 피드백 반영 : command Enum으로 관리

--- src/main/java/org/programmers/kdtspring/ConsoleIO/CommandType.java
+++ src/main/java/org/programmers/kdtspring/ConsoleIO/CommandType.java
@@ -1,9 +1,15 @@
 package org.programmers.kdtspring.ConsoleIO;
 
 public enum CommandType {
-    EXIT,
-    CREATE,
-    LIST,
-    APPEND,
-    LIST_FOR_CUSTOMER
+    EXIT("exit"), CREATE("create"), LIST("list"), APPEND("append"), LIST_FOR_CUSTOMER("listForCustomer");
+
+    private final String command;
+
+    CommandType(String command) {
+        this.command = command;
+    }
+
+    public String getCommand() {
+        return command;
+    }
 }
---
+++ src/main/java/org/programmers/kdtspring/controller/VoucherController.java
@@ -0,0 +1,49 @@
+package org.programmers.kdtspring.controller;
+
+import org.programmers.kdtspring.ConsoleIO.*;
+import org.programmers.kdtspring.repository.user.CustomerRepository;
+import org.programmers.kdtspring.repository.voucher.VoucherRepository;
+import org.programmers.kdtspring.service.CustomerService;
+import org.programmers.kdtspring.service.VoucherService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.util.*;
+
+import static org.programmers.kdtspring.ConsoleIO.CommandType.*;
+
+@Component
+public class VoucherController implements Runnable {
+
+    private static final Logger logger = LoggerFactory.getLogger(VoucherController.class);
+    private final Input input;
+    private final Output output;
+    private final VoucherService voucherService;
+    private final CustomerService customerService;
+    private Map<CommandType, CommandStrategy> commandStrategy = new HashMap<>();
+
+    public VoucherController(Input input, Output output, VoucherService voucherService, CustomerService customerService) {
+        this.input = input;
+        this.output = output;
+        this.voucherService = voucherService;
+        this.customerService = customerService;
+    }
+
+    @Override
+    public void run() {
+        logger.info("Voucher Application run...");
+        putStrategy();
+        while (true) {
+            String selectedOption = input.showOption();
+            commandStrategy.get(Enum.valueOf(CommandType.class, selectedOption)).runCommand();
+        }
+    }
+
+    private void putStrategy() {
+        commandStrategy.put(EXIT, new ExitCommandStrategy(output));
+        commandStrategy.put(CREATE, new CreateCommandStrategy(input, output, voucherService));
+        commandStrategy.put(LIST, new ListCommandStrategy(output, voucherService));
+        commandStrategy.put(LIST_FOR_CUSTOMER, new ListVoucherForCustomer(input, output, customerService));
+    }
+}
