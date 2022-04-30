120528835956189365b78a660d727621ffeebe07
hymn-fly <rlfrmsdh1@gmail.com>
2022-04-30 18:42:24 +0900

    수정  - 코멘트 반영

--- src/main/java/org/prgms/controller/VoucherController.java
+++ src/main/java/org/prgms/controller/VoucherController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import javax.servlet.http.HttpServletResponse;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -30,7 +29,7 @@ public class VoucherController {
     }
 
     @GetMapping("/vouchers/{voucherId}")
-    public String vouchers(@PathVariable UUID voucherId, Model model, HttpServletResponse httpServletResponse) {
+    public String vouchers(@PathVariable UUID voucherId, Model model) {
         Optional<Voucher> voucherOptional = voucherService.getVoucher(voucherId);
 
         if (voucherOptional.isEmpty()) {
