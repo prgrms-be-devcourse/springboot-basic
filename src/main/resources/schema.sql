3ed3a319eaa751843a3ef2bf7f7809daa91995b9
ecvheo1 <ecvheo@naver.com>
2022-05-01 23:45:45 +0900

    :wrench: chore: Create Table sql 파일 수정

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -2,5 +2,7 @@ CREATE TABLE voucher
 (
     voucher_id       BIGINT PRIMARY KEY AUTO_INCREMENT,
     voucher_type     VARCHAR(30) NOT NULL,
-    discount_value   INT NOT NULL
+    discount_value   INT NOT NULL,
+    created_at       TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at       TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
