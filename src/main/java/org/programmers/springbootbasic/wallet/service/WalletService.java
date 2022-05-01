23e0dafb5f680879404c518e5de70fd68693bb07
ICCHOI <cicblck@gmail.com>
2022-05-02 01:11:24 +0900

    feat:checkWalletExist 기능을 구현하라

--- src/main/java/org/programmers/springbootbasic/wallet/service/WalletService.java
+++ src/main/java/org/programmers/springbootbasic/wallet/service/WalletService.java
@@ -1,6 +1,7 @@
 package org.programmers.springbootbasic.wallet.service;
 
 import org.programmers.springbootbasic.customer.model.Customer;
+import org.programmers.springbootbasic.exception.DuplicateObjectKeyException;
 import org.programmers.springbootbasic.voucher.model.Voucher;
 import org.programmers.springbootbasic.wallet.domain.Wallet;
 import org.programmers.springbootbasic.wallet.repository.WalletRepository;
@@ -26,10 +27,17 @@ public class WalletService {
     }
 
     public Wallet allocateVoucher(Wallet wallet) {
+        if (checkWalletExist(wallet.getWalletId())) {
+            throw new DuplicateObjectKeyException("월렛 키 중복입니다.");
+        }
         return walletRepository.insert(wallet);
     }
 
     public void deleteWallet(UUID walletId) {
         walletRepository.deleteById(walletId);
+    }
+
+    private boolean checkWalletExist(UUID walletId) {
+        return walletRepository.getCountByWalletId(walletId) > 0;
     }
 }
