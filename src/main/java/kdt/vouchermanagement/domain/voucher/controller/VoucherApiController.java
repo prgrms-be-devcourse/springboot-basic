e9c5a0c96a47ee5bce00d15a8a5942eb086cd600
ecvheo1 <ecvheo@naver.com>
2022-05-01 23:00:43 +0900

    :adhesive_bandage: fix: endpoint 수정

--- src/main/java/kdt/vouchermanagement/domain/voucher/controller/VoucherApiController.java
+++ src/main/java/kdt/vouchermanagement/domain/voucher/controller/VoucherApiController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/voucher")
+@RequestMapping("/api/v1/vouchers")
 public class VoucherApiController {
 
     private final VoucherService voucherService;
