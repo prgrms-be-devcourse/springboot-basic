7aaada73360169fa179e16cb63f8ea9377b58240
UJ15 <dbwogml15@naver.com>
2022-05-01 16:00:10 +0900

    :white_check_mark: add 바우처 createdAt 필드 추가에 따른 변경

--- mission/src/main/resources/schema.sql
+++ mission/src/main/resources/schema.sql
@@ -1,7 +1,7 @@
 -- auto-generated definition
 create table voucher
 (
-    voucher_id     bigint auto_increment primary key,
+    voucher_id     bigint primary key auto_increment,
     discount_value decimal(20) not null,
     voucher_type   varchar(50) not null,
     created_at      datetime(6) not null
