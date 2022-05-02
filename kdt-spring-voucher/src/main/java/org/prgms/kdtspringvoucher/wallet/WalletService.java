b86f05304617a7f9eb158feb49e8584b98057751
Yongsu Kang <dksktm15@naver.com>
2022-05-02 12:12:14 +0900

    수정: WalletService 각 고객, 바우처 서비스를 사용하도록 수정

--- kdt-spring-voucher/src/main/java/org/prgms/kdtspringvoucher/wallet/WalletService.java
+++ kdt-spring-voucher/src/main/java/org/prgms/kdtspringvoucher/wallet/WalletService.java
@@ -2,35 +2,62 @@ package org.prgms.kdtspringvoucher.wallet;
 
 import org.prgms.kdtspringvoucher.customer.domain.Customer;
 import org.prgms.kdtspringvoucher.customer.repository.CustomerRepository;
+import org.prgms.kdtspringvoucher.customer.service.CustomerService;
+import org.prgms.kdtspringvoucher.voucher.domain.FixedAmountVoucher;
+import org.prgms.kdtspringvoucher.voucher.domain.PercentDiscountVoucher;
 import org.prgms.kdtspringvoucher.voucher.domain.Voucher;
+import org.prgms.kdtspringvoucher.voucher.dto.AssignVoucherRequest;
 import org.prgms.kdtspringvoucher.voucher.repository.VoucherRepository;
+import org.prgms.kdtspringvoucher.voucher.service.VoucherService;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
 
 @Service
 public class WalletService {
 
-    private final CustomerRepository customerRepository;
-    private final VoucherRepository voucherRepository;
+    private final CustomerService customerService;
+    private final VoucherService voucherService;
 
-    public WalletService(CustomerRepository customerRepository, VoucherRepository voucherRepository) {
-        this.customerRepository = customerRepository;
-        this.voucherRepository = voucherRepository;
+    public WalletService(CustomerService customerService, VoucherService voucherService) {
+        this.customerService = customerService;
+        this.voucherService = voucherService;
     }
 
+    //commandLine Application
     public Voucher assignVoucherToCustomer(Customer customer, Voucher voucher) {
-        voucher.assignVoucherToCustomer(customer);
-        return voucherRepository.update(voucher);
+        voucher.assignVoucherToCustomer(customer.getCustomerId());
+        return voucherService.updateVoucher(voucher);
     }
 
     public void showVouchersAssignedToCustomer(Customer customer){
-        voucherRepository.findByCustomerId(customer.getCustomerId()).forEach(System.out::println);
+        voucherService.getVouchersByCustomerId(customer.getCustomerId()).forEach(System.out::println);
     }
 
-    public void deleteVoucherAssignedToCustomer(Customer customer) {
-        voucherRepository.deleteByCustomerId(customer.getCustomerId());
+    public void deleteVoucherAssignedToCustomer(UUID voucherId) {
+        voucherService.deleteVoucherById(voucherId);
     }
-
     public void showCustomerByVoucherId(Voucher voucher){
-        System.out.println(customerRepository.findById(voucher.getCustomerId()).orElse(null));
+        System.out.println(customerService.getCustomerById(voucher.getCustomerId()));
+    }
+
+    //MVC
+    public List<Customer> getCustomers() {
+        return customerService.getCustomers();
+    }
+
+    public List<Voucher> getVouchersByCustomerId(UUID customerId) {
+        return voucherService.getVouchersByCustomerId(customerId);
+    }
+
+    public List<Voucher> getVoucherToAssign() {
+        return voucherService.getVouchers();
+    }
+
+    public Voucher assignVoucherToCustomer(UUID customerId, UUID voucherId) {
+        Voucher voucher = voucherService.getVoucherById(voucherId).orElse(null);
+        voucher.assignVoucherToCustomer(customerId);
+        return voucherService.updateVoucher(voucher);
     }
 }
