c139b0f051ff66e0fd0acedd39e9beccc953e2df
HunkiKim <gnsrl76@naver.com>
2022-05-10 05:49:07 +0900

    refactor : add created_at
    생성일 스키마 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,13 +1,16 @@
-CREATE TABLE vouchers(
-                         voucher_id BINARY(16) PRIMARY KEY,
-                         voucher_amount int NOT NULL,
-                         voucher_type varchar(20) NOT NULL
+CREATE TABLE vouchers
+(
+    voucher_id     BINARY(16) PRIMARY KEY,
+    voucher_amount int         NOT NULL,
+    voucher_type   varchar(20) NOT NULL,
+    created_at     datetime    NOT NULL DEFAULT current_timestamp()
 );
-CREATE TABLE customers(
-                          customer_id BINARY(16) PRIMARY KEY,
-                          name varchar(20) NOT NULL,
-                          email varchar(50) NOT NULL,
-                          last_login_at datetime DEFAULT NULL,
-                          created_at datetime NOT NULL DEFAULT current_timestamp(),
-                          CONSTRAINT unq_user_email UNIQUE (email)
+CREATE TABLE customers
+(
+    customer_id   BINARY(16) PRIMARY KEY,
+    name          varchar(20) NOT NULL,
+    email         varchar(50) NOT NULL,
+    last_login_at datetime             DEFAULT NULL,
+    created_at    datetime    NOT NULL DEFAULT current_timestamp(),
+    CONSTRAINT unq_user_email UNIQUE (email)
 );
