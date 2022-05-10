455114ff4a012ad63756c57b24bda3a488f6f435
Yanjoo <hanjub3@gmail.com>
2022-05-10 16:39:23 +0900

    CORS 설정

---
+++ src/main/java/me/programmers/springboot/basic/springbootbasic/config/MvcConfig.java
@@ -0,0 +1,14 @@
+package me.programmers.springboot.basic.springbootbasic.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**").allowedOrigins("*");
+    }
+}
