88dc643934cabeeb6ab0659b4557d97cb8b9b73a
Yanjoo <hanjub3@gmail.com>
2022-05-12 20:48:16 +0900

    JdbcVoucherService: getAllVouchers 테스트

--- src/test/java/me/programmers/springboot/basic/springbootbasic/voucher/service/JdbcVoucherServiceTest.java
+++ src/test/java/me/programmers/springboot/basic/springbootbasic/voucher/service/JdbcVoucherServiceTest.java
@@ -1,6 +1,7 @@
 package me.programmers.springboot.basic.springbootbasic.voucher.service;
 
 import me.programmers.springboot.basic.springbootbasic.voucher.model.FixedAmountVoucher;
+import me.programmers.springboot.basic.springbootbasic.voucher.model.PercentAmountVoucher;
 import me.programmers.springboot.basic.springbootbasic.voucher.model.Voucher;
 import me.programmers.springboot.basic.springbootbasic.voucher.repository.JdbcTemplateVoucherRepository;
 import org.junit.jupiter.api.Test;
@@ -9,9 +10,11 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.List;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 
@@ -32,6 +35,22 @@ class JdbcVoucherServiceTest {
         Voucher savedVoucher = voucherService.save(voucher);
 
         assertThat(savedVoucher.getVoucherId()).isEqualTo(voucher.getVoucherId());
+    }
+
+    @Test
+    void getAllVouchersTest() {
+        Voucher fixVoucher = new FixedAmountVoucher(UUID.randomUUID(), 2000);
+        Voucher percentVoucher = new PercentAmountVoucher(UUID.randomUUID(), 20);
+        List<Voucher> vouchers = List.of(fixVoucher, percentVoucher);
+
+        given(voucherRepository.findAll()).willReturn(vouchers);
+
+        List<Voucher> allVouchers = voucherService.getAllVouchers();
+
+        assertAll(
+                () -> assertThat(allVouchers).hasSize(2),
+                () -> assertThat(allVouchers.get(0).getVoucherId()).isEqualTo(fixVoucher.getVoucherId())
+        );
     }
 
 }
