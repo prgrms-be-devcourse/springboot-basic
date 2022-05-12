a511452772cf4b101c5436f03f66e3d338fc17d8
pjh612 <pjhjh612@gmail.com>
2022-05-13 01:02:54 +0900

    feat : Voucher 전체 조회 페이지
    Form 태그에서 DeleteMapping 사용할 수 있도록 설정

--- src/main/java/com/voucher/vouchermanagement/configuration/ApplicationConfig.java
+++ src/main/java/com/voucher/vouchermanagement/configuration/ApplicationConfig.java
@@ -1,8 +1,13 @@
 package com.voucher.vouchermanagement.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 
 @Configuration
 public class ApplicationConfig {
-
+	@Bean
+	public HiddenHttpMethodFilter hiddenHttpMethodFilter() {
+		return new HiddenHttpMethodFilter();
+	}
 }
