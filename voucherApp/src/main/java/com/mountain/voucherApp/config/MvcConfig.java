78a64792579aab9da2c300a659fec54f60e8c3c0
taesan <pawer02023@gmail.com>
2022-05-14 02:31:04 +0900

    chore: 스페이스 제거
    스페이스 제거

--- voucherApp/src/main/java/com/mountain/voucherApp/config/MvcConfig.java
+++ voucherApp/src/main/java/com/mountain/voucherApp/config/MvcConfig.java
@@ -14,7 +14,7 @@ public class MvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedMethods("GET", "POST" )
+                .allowedMethods("GET", "POST")
                 .allowedOrigins("*");
     }
 
