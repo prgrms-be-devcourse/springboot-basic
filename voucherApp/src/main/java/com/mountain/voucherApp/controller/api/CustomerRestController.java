d5d2590396f22c38977becb83bd1139e8ade28e6
taesan <pawer02023@gmail.com>
2022-05-14 03:17:58 +0900

    refactor: 반환타입 ResponseEntity 변경
    반환타입 ResponseEntity 변경

--- voucherApp/src/main/java/com/mountain/voucherApp/controller/api/CustomerRestController.java
+++ voucherApp/src/main/java/com/mountain/voucherApp/controller/api/CustomerRestController.java
@@ -1,14 +1,13 @@
 package com.mountain.voucherApp.controller.api;
 
-import com.mountain.voucherApp.dto.CustomerDto;
 import com.mountain.voucherApp.service.VoucherAppService;
 import com.mountain.voucherApp.service.customer.CustomerService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -24,12 +23,12 @@ public class CustomerRestController {
     }
 
     @GetMapping
-    public List<CustomerDto> customers() {
-        return customerService.findAll();
+    public ResponseEntity customers() {
+        return ResponseEntity.ok(customerService.findAll());
     }
 
     @GetMapping("/{voucherId}")
-    public List<CustomerDto> customersByVoucherId(@PathVariable String voucherId) {
-        return voucherAppService.showByVoucher(UUID.fromString(voucherId));
+    public ResponseEntity customersByVoucherId(@PathVariable String voucherId) {
+        return ResponseEntity.ok(voucherAppService.showByVoucher(UUID.fromString(voucherId)));
     }
 }
