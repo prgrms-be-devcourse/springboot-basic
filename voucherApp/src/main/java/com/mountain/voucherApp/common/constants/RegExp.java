0bdaf958c708ecbc7f76519d296892de616de2c8
taesan <pawer02023@gmail.com>
2022-05-14 01:21:56 +0900

    refactor: 불필요한 분리 제거
    불필요한 분리 제거

--- voucherApp/src/main/java/com/mountain/voucherApp/common/constants/RegExp.java
+++ voucherApp/src/main/java/com/mountain/voucherApp/common/constants/RegExp.java
@@ -5,7 +5,7 @@ import java.util.regex.Pattern;
 import static com.mountain.voucherApp.common.constants.ErrorMessage.EMAIL_NOT_VALID;
 
 public enum RegExp {
-    EMAIL(Expression.EMAIL_EXP, EMAIL_NOT_VALID.getMessage());
+    EMAIL("\\b[\\w\\.-]+@[\\w\\.-]+\\.\\w{2,4}\\b", EMAIL_NOT_VALID.getMessage());
 
     private final String expression;
     private final String errorMsg;
@@ -22,9 +22,5 @@ public enum RegExp {
 
     public boolean validate(String source) {
         return Pattern.matches(expression, source);
-    }
-
-    public static class Expression {
-        public static final String EMAIL_EXP = "\\b[\\w\\.-]+@[\\w\\.-]+\\.\\w{2,4}\\b";
     }
 }
