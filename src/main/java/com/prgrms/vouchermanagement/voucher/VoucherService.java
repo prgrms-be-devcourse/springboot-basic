7f503a145e749b30f6d86afb98c26911a17ea397
ynoolee <heerarea129@gmail.com>
2022-05-13 18:40:52 +0900

    fix: 잘못된 검증로직 수정 및 컨벤션 적용

--- src/main/java/com/prgrms/vouchermanagement/voucher/VoucherService.java
+++ src/main/java/com/prgrms/vouchermanagement/voucher/VoucherService.java
@@ -2,7 +2,6 @@ package com.prgrms.vouchermanagement.voucher;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 import org.slf4j.Logger;
@@ -12,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.prgrms.vouchermanagement.commons.exception.AlreadyExistException;
 import com.prgrms.vouchermanagement.commons.exception.CreationFailException;
+import com.prgrms.vouchermanagement.commons.exception.NotExistException;
 import com.prgrms.vouchermanagement.voucher.domain.Voucher;
 import com.prgrms.vouchermanagement.voucher.repository.VoucherRepository;
 
@@ -29,9 +29,11 @@ public class VoucherService {
 	}
 
 	@Transactional
-	public Voucher create(VoucherType voucherType, long voucherDiscountInfo) throws CreationFailException, AlreadyExistException {
+	public Voucher create(VoucherType voucherType, long voucherDiscountInfo) throws
+		CreationFailException,
+		AlreadyExistException {
 		checkNull(voucherType, "voucherType 는 null 이 올 수 없습니다");
-		checkNull(voucherDiscountInfo, "voucherDiscountInfo 는 null 이 올 수 없습니다");
+		checkPositive(voucherDiscountInfo, "voucherDiscountInfo 는 0 보다 큰 정수여야 합니다");
 
 		UUID id = UUID.randomUUID();
 
@@ -46,25 +48,11 @@ public class VoucherService {
 	}
 
 	@Transactional
-	public Optional<Voucher> getById(UUID id) {
+	public Voucher getById(UUID id) {
 		checkNull(id, "id 는 null 이 올 수 없습니다");
-		checkDuplicatedId(id);
 
-		return voucherRepository.findById(id);
-	}
-
-	public void checkNull(Object obj, String message) {
-		if (obj == null) {
-			throw new IllegalArgumentException(message);
-		}
-	}
-
-	@Transactional(readOnly = true)
-	void checkDuplicatedId(UUID id) {
-		if (voucherRepository.findById(id)
-			.isPresent()) {
-			throw new AlreadyExistException();
-		}
+		return voucherRepository.findById(id)
+			.orElseThrow(NotExistException::new);
 	}
 
 	@Transactional
@@ -75,4 +63,23 @@ public class VoucherService {
 		voucherRepository.deleteById(voucherId);
 	}
 
+	@Transactional(readOnly = true)
+	void checkDuplicatedId(UUID id) {
+		if (voucherRepository.findById(id)
+			.isPresent()) {
+			throw new AlreadyExistException();
+		}
+	}
+
+	private void checkNull(Object obj, String message) {
+		if (obj == null) {
+			throw new IllegalArgumentException(message);
+		}
+	}
+
+	private void checkPositive(long numb, String message) {
+		if (numb <= 0) {
+			throw new IllegalArgumentException(message);
+		}
+	}
 }
