287f62468684d09631565823c34ebb01f6d57c86
sdardew <sdardew@gmail.com>
2022-05-15 01:57:46 +0900

    사용하지 않는 import 제거

--- src/main/java/org/prgrms/vouchermanagement/customer/service/CustomerService.java
+++ src/main/java/org/prgrms/vouchermanagement/customer/service/CustomerService.java
@@ -2,13 +2,9 @@ package org.prgrms.vouchermanagement.customer.service;
 
 import org.prgrms.vouchermanagement.customer.Customer;
 import org.prgrms.vouchermanagement.customer.repository.CustomerRepository;
-import org.prgrms.vouchermanagement.customer.wallet.WalletRepository;
-import org.prgrms.vouchermanagement.voucher.repository.VoucherRepository;
-import org.prgrms.vouchermanagement.voucher.voucher.Voucher;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.UUID;
 
 @Service
 public class CustomerService {
