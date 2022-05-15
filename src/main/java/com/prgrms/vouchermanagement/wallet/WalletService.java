9a8fc13524149eb5c410073ffcec9e97a4492eae
ynoolee <heerarea129@gmail.com>
2022-05-15 19:40:20 +0900

    refactor: WalletService 인자 검증 코드 추가 및 메소드 이동

--- src/main/java/com/prgrms/vouchermanagement/wallet/WalletService.java
+++ src/main/java/com/prgrms/vouchermanagement/wallet/WalletService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.UUID;
 
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.Assert;
 
 import com.prgrms.vouchermanagement.commons.exception.AlreadyExistException;
 import com.prgrms.vouchermanagement.commons.exception.NotExistException;
@@ -13,49 +14,29 @@ import com.prgrms.vouchermanagement.voucher.VoucherService;
 import com.prgrms.vouchermanagement.voucher.domain.Voucher;
 
 public class WalletService {
-	private final WalletRepository walletRepository;
+	private final WalletJdbcRepository walletRepository;
 	private final CustomerService customerService;
 	private final VoucherService voucherService;
 
-	public WalletService(WalletRepository walletRepository,
+	public WalletService(WalletJdbcRepository walletRepository,
 		CustomerService customerService, VoucherService voucherService) {
+
 		this.walletRepository = walletRepository;
 		this.customerService = customerService;
 		this.voucherService = voucherService;
 	}
 
+	@Transactional
 	public List<Voucher> getAllVouchersOfCustomer(UUID customerId) {
-		checkEmpty(customerId);
+		checkEmptyCustomerId(customerId);
 
 		return walletRepository.findAllVouchersOfCustomer(customerId);
 	}
 
-	// 해당 커스토머가 이 바우처를 갖고 있지 않으면 예외를 던진다
-	@Transactional(readOnly = true)
-	protected void checkCustomerHavingThisVoucher(UUID customerId, UUID voucherId) {
-		walletRepository.findVoucherOfCustomer(customerId, voucherId)
-			.orElseThrow(NotExistException::new);
-	}
-
-	// 해당 커스토머가 이 바우처를 이미 갖고 있으면 예외를 던진다
-	@Transactional(readOnly = true)
-	protected void checkCustomerNotHavingThisVoucher(UUID customerId, UUID voucherId) {
-		if (walletRepository.findVoucherOfCustomer(customerId, voucherId)
-			.isPresent()) {
-			throw new AlreadyExistException();
-		}
-	}
-
-	private void checkEmpty(UUID id) {
-		if (id == null) {
-			throw new IllegalArgumentException("id 는 null 이 오면 안됩니다");
-		}
-	}
-
 	@Transactional
 	public void disableVoucherOfCustomer(UUID customerId, UUID voucherId) throws NotExistException {
-		checkEmpty(customerId);
-		checkEmpty(voucherId);
+		checkEmptyCustomerId(customerId);
+		checkEmptyVoucherId(voucherId);
 
 		checkCustomerHavingThisVoucher(customerId, voucherId);
 
@@ -64,19 +45,49 @@ public class WalletService {
 
 	@Transactional
 	public void publishVoucherToCustomer(UUID customerId, UUID voucherId) {
-		checkEmpty(customerId);
-		checkEmpty(voucherId);
+		checkEmptyCustomerId(customerId);
+		checkEmptyVoucherId(voucherId);
 
+		voucherService.getById(voucherId);
+		customerService.getById(customerId);
 		checkCustomerNotHavingThisVoucher(customerId, voucherId);
-		voucherService.getById(voucherId)
-			.orElseThrow(NotExistException::new);
 
 		walletRepository.insertRecord(customerId, voucherId);
 	}
 
+	@Transactional
 	public List<Customer> getAllOwnersOfVoucher(UUID voucherId) {
-		checkEmpty(voucherId);
+		checkEmptyVoucherId(voucherId);
 
 		return walletRepository.findAllCustomersOfVoucher(voucherId);
+	}
+
+	// 해당 커스토머가 이 바우처를 갖고 있지 않으면 예외를 던진다
+	@Transactional(readOnly = true)
+	void checkCustomerHavingThisVoucher(UUID customerId, UUID voucherId) {
+		walletRepository.findVoucherOfCustomer(customerId, voucherId)
+			.orElseThrow(NotExistException::new);
+	}
+
+	// 해당 커스토머가 이 바우처를 이미 갖고 있으면 예외를 던진다
+	@Transactional(readOnly = true)
+	void checkCustomerNotHavingThisVoucher(UUID customerId, UUID voucherId) {
+		if (walletRepository.findVoucherOfCustomer(customerId, voucherId)
+			.isPresent()) {
+			throw new AlreadyExistException();
+		}
+	}
+
+	@Transactional(readOnly = true)
+	void checkExistingCustomer(UUID customerId) {
+		customerService.getById(customerId);
+	}
+
+	private void checkEmptyCustomerId(UUID customerId) {
+		Assert.notNull(customerId, "customerId 는 null 일 수 없습니다");
+	}
+
+	private void checkEmptyVoucherId(UUID voucherId) {
+		Assert.notNull(voucherId, "voucherId 는 null 일 수 없습니다");
 	}
 }
