c1f32eb5fbd844988fb70baf9af96d58073c3db0
ICCHOI <cicblck@gmail.com>
2022-05-16 17:37:12 +0900

    feat:Exception Handler를 구현하라

--- src/main/java/org/programmers/springbootbasic/voucher/controller/GlobalExceptionHandler.java
+++ src/main/java/org/programmers/springbootbasic/voucher/controller/GlobalExceptionHandler.java
@@ -3,10 +3,9 @@ package org.programmers.springbootbasic.voucher.controller;
 import javassist.NotFoundException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestController;
 
-@ControllerAdvice(annotations = RestController.class)
-public class VoucherControllerAdvice {
+@ControllerAdvice
+public class GlobalExceptionHandler {
 
     @ExceptionHandler(NotFoundException.class)
     public String notFoundHandler() {
