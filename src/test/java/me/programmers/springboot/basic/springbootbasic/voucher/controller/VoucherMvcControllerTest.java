0ce0ee968282bf9761b5d4260eab5c81b44d5b62
Yanjoo <hanjub3@gmail.com>
2022-05-16 15:49:45 +0900

    VoucherMvcController: 바우처 생성 테스트 추가

--- src/test/java/me/programmers/springboot/basic/springbootbasic/voucher/controller/VoucherMvcControllerTest.java
+++ src/test/java/me/programmers/springboot/basic/springbootbasic/voucher/controller/VoucherMvcControllerTest.java
@@ -1,5 +1,9 @@
 package me.programmers.springboot.basic.springbootbasic.voucher.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import me.programmers.springboot.basic.springbootbasic.voucher.dto.VoucherCreateRequestDto;
+import me.programmers.springboot.basic.springbootbasic.voucher.model.FixedAmountVoucher;
+import me.programmers.springboot.basic.springbootbasic.voucher.model.Voucher;
 import me.programmers.springboot.basic.springbootbasic.voucher.service.JdbcVoucherService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,7 +12,13 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.UUID;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -20,6 +30,9 @@ class VoucherMvcControllerTest {
 
     @MockBean
     JdbcVoucherService voucherService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @Test
     void showVoucherPageTest() throws Exception {
@@ -35,6 +48,24 @@ class VoucherMvcControllerTest {
         mockMvc.perform(get(url))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.valueOf("text/html;charset=UTF-8")));
+    }
+
+
+
+    @Test
+    void createVoucherTest() throws Exception {
+        String url = "/vouchers/new";
+
+        VoucherCreateRequestDto createRequest = new VoucherCreateRequestDto("fixed", 1000, 0);
+
+        mockMvc.perform(post(url)
+                        .contentType("application/x-www-form-urlencoded")
+                        .accept("application/x-www-form-urlencoded")
+                        .param("type", "fixed")
+                        .param("discountPrice", "1000")
+                        .param("discountPercent", "0"))
+                .andDo(print())
+                .andExpect(status().is3xxRedirection());
     }
 
 }
