3f1a2c7c5c9a3c2dc616205194e956ca8a8261ea
ynoolee <heerarea129@gmail.com>
2022-05-17 21:03:35 +0900

    feat: GlobalExceptionHandler 에 예기치 못한 예외에 대한 처리기능을 추가한다

--- src/main/java/com/prgrms/vouchermanagement/commons/api/GlobalExceptionHandler.java
+++ src/main/java/com/prgrms/vouchermanagement/commons/api/GlobalExceptionHandler.java
@@ -16,4 +16,19 @@ public class GlobalExceptionHandler {
 
 		return ApiResult.error(HttpStatus.BAD_REQUEST, e);
 	}
+
+	@ExceptionHandler({RuntimeException.class})
+	public ApiResult<ApiError> handleRuntimeException(RuntimeException e) {
+		logger.error("알 수 없는 런타임 예외 {}", e.getMessage(), e);
+
+		return ApiResult.error(HttpStatus.INTERNAL_SERVER_ERROR, e);
+	}
+
+	@ExceptionHandler({Exception.class})
+	public ApiResult<ApiError> handleRuntimeException(Exception e) {
+		logger.error("알 수 없는 예외 {}", e.getMessage(), e);
+
+		return ApiResult.error(HttpStatus.INTERNAL_SERVER_ERROR, e);
+	}
+
 }
