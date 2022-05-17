cdf67ea5b943c56875159aea77a74f2cb005e044
ynoolee <heerarea129@gmail.com>
2022-05-17 20:58:39 +0900

    feat: RestController 에 Voucher 다건 조회,단건 삭제,단건 조회 기능을 추가한다
    - deleteVoucher 의 경우, 삭제 성공시 empty result 를 주고 싶은데 제네릭 ApiResult 에서 parameter type 을 무엇으로 줘야 하는건지 모르겠어서 raw use of parameterized class 를 하였음

--- src/main/java/com/prgrms/vouchermanagement/voucher/controller/VoucherRestController.java
+++ src/main/java/com/prgrms/vouchermanagement/voucher/controller/VoucherRestController.java
@@ -1,11 +1,19 @@
 package com.prgrms.vouchermanagement.voucher.controller;
 
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.prgrms.vouchermanagement.commons.page.Pageable;
 import com.prgrms.vouchermanagement.commons.api.ApiResult;
 import com.prgrms.vouchermanagement.voucher.VoucherService;
 import com.prgrms.vouchermanagement.voucher.dto.VoucherInfo;
@@ -19,7 +27,7 @@ public class VoucherRestController {
 
 	private final VoucherService voucherService;
 
-	@PostMapping("/vouchers/")
+	@PostMapping("/vouchers")
 	public ApiResult<VoucherInfo> newVoucher(@RequestBody CreateVoucherRequest request) {
 		return ApiResult.success(
 			HttpStatus.OK,
@@ -28,6 +36,31 @@ public class VoucherRestController {
 					request.getType(),
 					request.getDiscountInfo())
 			)
+		);
+	}
+
+	@GetMapping("/vouchers")
+	public ApiResult<List<VoucherInfo>> showVouchers(Pageable page) {
+		return ApiResult.success(
+			HttpStatus.OK,
+			voucherService.getAll(page).stream()
+				.map(VoucherInfo::fromEntity)
+				.collect(Collectors.toList())
+		);
+	}
+
+	@DeleteMapping("/vouchers/{voucherId}")
+	public ApiResult deleteVoucher(@PathVariable UUID voucherId) {
+		voucherService.removeById(voucherId);
+
+		return ApiResult.success(HttpStatus.NO_CONTENT, null);
+	}
+
+	@GetMapping("/vouchers/{voucherId}")
+	public ApiResult<VoucherInfo> showVoucher(@PathVariable UUID voucherId) {
+		return ApiResult.success(
+			HttpStatus.OK,
+			VoucherInfo.fromEntity(voucherService.getById(voucherId))
 		);
 	}
 }
