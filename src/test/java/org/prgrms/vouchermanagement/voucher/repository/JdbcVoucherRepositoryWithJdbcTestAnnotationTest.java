2152b41ccf1a078e6c44915c95f50cba69f213ef
sdardew <sdardew@gmail.com>
2022-05-17 17:34:51 +0900

    사용하지 않는 import 제거

--- src/test/java/org/prgrms/vouchermanagement/voucher/repository/JdbcVoucherRepositoryWithJdbcTestAnnotationTest.java
+++ src/test/java/org/prgrms/vouchermanagement/voucher/repository/JdbcVoucherRepositoryWithJdbcTestAnnotationTest.java
@@ -3,26 +3,14 @@ package org.prgrms.vouchermanagement.voucher.repository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.prgrms.vouchermanagement.voucher.voucher.Voucher;
 import org.prgrms.vouchermanagement.voucher.voucher.VoucherFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
-import org.springframework.context.annotation.Import;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.test.context.jdbc.Sql;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
-
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.not;
 
 //@Sql("/schema.sql")
 @JdbcTest
