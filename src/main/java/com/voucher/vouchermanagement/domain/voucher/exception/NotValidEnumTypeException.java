553ea15546c603eff9e1214f7d15440c3f34884c
pjh <pjh_jn@naver.com>
2022-05-21 20:12:55 +0900

    refactor : NotValidEnumTypeException 패키지 이동

--- src/main/java/com/voucher/vouchermanagement/domain/voucher/exception/NotValidEnumTypeException.java
+++
@@ -1,7 +0,0 @@
-package com.voucher.vouchermanagement.domain.voucher.exception;
-
-public class NotValidEnumTypeException extends IllegalArgumentException{
-	public NotValidEnumTypeException(String message, Throwable cause) {
-		super(message, cause);
-	}
-}
---
+++ src/main/java/com/voucher/vouchermanagement/exception/NotValidEnumTypeException.java
@@ -0,0 +1,7 @@
+package com.voucher.vouchermanagement.exception;
+
+public class NotValidEnumTypeException extends IllegalArgumentException {
+	public NotValidEnumTypeException(String message, Throwable cause) {
+		super(message, cause);
+	}
+}
