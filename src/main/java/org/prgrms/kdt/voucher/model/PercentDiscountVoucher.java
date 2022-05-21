673981c5df6b7d25504471d8a21a1161f1f6282c
HyoungUkJJang <guddnr1451@gmail.com>
2022-05-21 15:58:31 +0900

    refactor : customer, voucher 필드 네이밍 수정

--- src/main/java/org/prgrms/kdt/voucher/model/PercentDiscountVoucher.java
+++ src/main/java/org/prgrms/kdt/voucher/model/PercentDiscountVoucher.java
@@ -8,47 +8,47 @@ public class PercentDiscountVoucher implements Voucher {
     private static final int MIN_PERCENT = 0;
     private static final int MAX_PERCENT = 100;
 
-    private final UUID voucherId;
-    private final long percent;
+    private final UUID id;
+    private long value;
     private final LocalDateTime createdAt;
     private final VoucherType type = VoucherType.PERCENT;
 
-    public PercentDiscountVoucher(UUID voucherId, long percent, LocalDateTime createdAt) {
-        if (percent < MIN_PERCENT || percent > MAX_PERCENT) {
+    public PercentDiscountVoucher(UUID id, long value, LocalDateTime createdAt) {
+        if (value < MIN_PERCENT || value > MAX_PERCENT) {
             throw new IllegalArgumentException("퍼센트는 1~100 사이의 값을 입력해주세요.");
         }
 
-        this.voucherId = voucherId;
-        this.percent = percent;
+        this.id = id;
+        this.value = value;
         this.createdAt = createdAt;
     }
 
-    public PercentDiscountVoucher(long percent) {
-        if (percent < MIN_PERCENT || percent > MAX_PERCENT) {
+    public PercentDiscountVoucher(long value) {
+        if (value < MIN_PERCENT || value > MAX_PERCENT) {
             throw new IllegalArgumentException("퍼센트는 1~100 사이의 값을 입력해주세요.");
         }
 
-        this.voucherId = UUID.randomUUID();
-        this.percent = percent;
+        this.id = UUID.randomUUID();
+        this.value = value;
         this.createdAt = LocalDateTime.now();
     }
 
     @Override
     public String toString() {
         return "PercentDiscountVoucher{" +
-            "voucherId=" + voucherId +
-            ", percent=" + percent +
+            "id=" + id +
+            ", percent=" + value +
             '}';
     }
 
     @Override
-    public UUID getVoucherId() {
-        return this.voucherId;
+    public UUID getId() {
+        return this.id;
     }
 
     @Override
     public long discount(long beforeDiscount) {
-        return beforeDiscount * (this.percent / 100);
+        return beforeDiscount * (this.value / 100);
     }
 
     @Override
@@ -58,12 +58,16 @@ public class PercentDiscountVoucher implements Voucher {
 
     @Override
     public long getValue() {
-        return this.percent;
+        return this.value;
     }
 
     @Override
     public LocalDateTime getCreatedAt() {
         return this.createdAt;
+    }
+
+    public VoucherType getType() {
+        return type;
     }
 
 }
