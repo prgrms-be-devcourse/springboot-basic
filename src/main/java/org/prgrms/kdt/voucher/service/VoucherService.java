7342f24cb6a9f0417cf01f85ceda4f573107fa51
HyoungUkJJang <guddnr1451@gmail.com>
2022-05-21 16:04:27 +0900

    feat : 바우처 수정, 삭제 기능 추가

--- src/main/java/org/prgrms/kdt/voucher/service/VoucherService.java
+++ src/main/java/org/prgrms/kdt/voucher/service/VoucherService.java
@@ -3,14 +3,17 @@ package org.prgrms.kdt.voucher.service;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.UUID;
+드import org.prgrms.kdt.error.VoucherNotFoundException;
 import org.prgrms.kdt.voucher.model.Voucher;
 import org.prgrms.kdt.voucher.model.VoucherType;
 import org.prgrms.kdt.voucher.repository.VoucherRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
-
+import org.springframework.transaction.annotation.Transactional;
+성
 @Service
+@Transactional(readOnly = true)
 public class VoucherService {
 
     private static final Logger log = LoggerFactory.getLogger(VoucherService.class);
@@ -20,6 +23,7 @@ public class VoucherService {
         this.voucherRepository = voucherRepository;
     }
 
+    @Transactional
     public Voucher makeVoucher(VoucherType voucherMenu, long discountValue) {
         log.info("바우처를 생성합니다. => [타입 : {}] [값 : {}]", voucherMenu, discountValue);
         Voucher voucher = voucherMenu.create(discountValue);
@@ -27,15 +31,26 @@ public class VoucherService {
         return voucher;
     }
 
-    public Voucher getVoucher(UUID voucherId) {
-        return voucherRepository.findById(voucherId)
-            .orElseThrow(NoSuchElementException::new);
+    @Transactional
+    public void updateVoucher(UUID id, long value) {
+        voucherRepository.update(id, value);
+    }
+
+    public Voucher getVoucher(UUID id) {
+        return voucherRepository.findById(id)
+            .orElseThrow(VoucherNotFoundException::new);
     }
 
     public List<Voucher> getVouchers() {
         return voucherRepository.findAll();
     }
 
+    @Transactional
+    public void deleteVoucher(UUID id) {
+        voucherRepository.deleteById(id);
+    }
+
+    @Transactional
     public void deleteVouchers() {
         voucherRepository.deleteAll();
     }
