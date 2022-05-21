cda3f04207e49704a214ea5f0149ef9a5a2ae75c
HyoungUkJJang <guddnr1451@gmail.com>
2022-05-21 16:06:34 +0900

    feat : 바우처 웹 컨트롤러 작성

---
+++ src/main/java/org/prgrms/kdt/controller/VoucherWebController.java
@@ -0,0 +1,90 @@
+package org.prgrms.kdt.controller;
+
+import java.util.List;
+import java.util.UUID;
+import org.prgrms.kdt.controller.dto.CreateVoucherRequest;
+import org.prgrms.kdt.controller.dto.VoucherResponse;
+import org.prgrms.kdt.controller.dto.VouchersResponse;
+import org.prgrms.kdt.voucher.model.Voucher;
+import org.prgrms.kdt.voucher.model.VoucherType;
+import org.prgrms.kdt.voucher.service.VoucherService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/vouchers")
+public class VoucherWebController {
+
+    private final VoucherService voucherService;
+
+    public VoucherWebController(VoucherService voucherService) {
+        this.voucherService = voucherService;
+    }
+
+    @GetMapping
+    public String vouchersPage(Model model) {
+        List<VouchersResponse> vouchers = voucherService.getVouchers().stream()
+            .map(voucher -> new VouchersResponse(
+                voucher.getId(),
+                voucher.getValue(),
+                voucher.getVoucherType(),
+                voucher.getCreatedAt()))
+            .toList();
+        model.addAttribute("vouchers", vouchers);
+
+        return "vouchers";
+    }
+
+    @GetMapping("/{id}")
+    public String voucherPage(@PathVariable UUID id, Model model) {
+        Voucher findVoucher = voucherService.getVoucher(id);
+        VoucherResponse voucherResponse = new VoucherResponse(
+            findVoucher.getId(),
+            findVoucher.getValue(),
+            findVoucher.getVoucherType(),
+            findVoucher.getCreatedAt()
+        );
+        model.addAttribute("voucher", voucherResponse);
+
+        return "voucher";
+    }
+
+    @GetMapping("/new-voucher")
+    public String newVoucherPage() {
+        return "new-voucher";
+    }
+
+    @PostMapping("/new-voucher")
+    public String newVoucher(CreateVoucherRequest voucherRequest) {
+        voucherService.makeVoucher(VoucherType.of(voucherRequest.getType()),
+            voucherRequest.getValue());
+
+        return "redirect:/vouchers";
+    }
+
+    @GetMapping("/{id}/update")
+    public String updateVoucherPage(@PathVariable UUID id, Model model) {
+        model.addAttribute("voucher", voucherService.getVoucher(id));
+
+        return "update-voucher";
+    }
+
+    @PostMapping("/{id}/update")
+    public String updateVoucher(@PathVariable UUID id, long value) {
+        voucherService.updateVoucher(id, value);
+
+        return "redirect:/vouchers/" + id;
+    }
+
+    @PostMapping("/{id}/delete")
+    public String deleteVoucher(@PathVariable UUID id) {
+        voucherService.deleteVoucher(id);
+
+        return "redirect:/vouchers";
+    }
+
+}
