b5b57a7b5124c5553e47fcaaa219ca1a8bf31528
pjh <pjh_jn@naver.com>
2022-05-21 20:22:43 +0900

    feat : 예외 처리 핸들러, 로깅 추가
    - DataNotFoundException
    - RuntimeException
    - Exception

--- src/main/java/com/voucher/vouchermanagement/web/controller/DefaultControllerAdvice.java
+++ src/main/java/com/voucher/vouchermanagement/web/controller/DefaultControllerAdvice.java
@@ -1,13 +1,43 @@
 package com.voucher.vouchermanagement.web.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.voucher.vouchermanagement.exception.DataNotFoundException;
+
 @ControllerAdvice
 public class DefaultControllerAdvice {
+
+	private final Logger log = LoggerFactory.getLogger(getClass());
+
 	@ExceptionHandler(IllegalArgumentException.class)
 	public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException exception) {
+		log.debug(exception.getMessage(), exception);
+
 		return ResponseEntity.badRequest().body(exception.getMessage());
+	}
+
+	@ExceptionHandler(DataNotFoundException.class)
+	public ResponseEntity<String> handleDataNotFoundException(DataNotFoundException exception) {
+		log.debug(exception.getMessage(), exception);
+
+		return ResponseEntity.badRequest().build();
+	}
+
+	@ExceptionHandler(RuntimeException.class)
+	public ResponseEntity<String> handleRuntimeException(RuntimeException exception) {
+		log.warn(exception.getMessage(), exception);
+
+		return ResponseEntity.internalServerError().build();
+	}
+
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<String> handleException(Exception exception) {
+		log.error("unexpected Exception {}", exception.getMessage(), exception);
+
+		return ResponseEntity.internalServerError().build();
 	}
 }
