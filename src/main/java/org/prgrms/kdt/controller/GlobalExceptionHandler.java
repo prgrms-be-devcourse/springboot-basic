9545a56f4a2084d05cb9b2a9c718df429a9a26fd
midasworld <midas117@knou.ac.kr>
2022-05-23 21:49:58 +0900

    feat: GlobalExceptionHandler - 전역 예외 처리 클래스 생성

---
+++ src/main/java/org/prgrms/kdt/controller/GlobalExceptionHandler.java
@@ -0,0 +1,33 @@
+package org.prgrms.kdt.controller;
+
+import org.prgrms.kdt.error.InputException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import static org.prgrms.kdt.controller.ApiResponse.ERROR;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+    private ResponseEntity<ApiResponse<?>> newResponse(Throwable throwable, HttpStatus status) {
+        return new ResponseEntity<>(ERROR(throwable, status), status);
+    }
+
+    @ExceptionHandler({InputException.class, IllegalArgumentException.class})
+    private ResponseEntity<?> handleBadRequestException(Exception e) {
+        logger.warn("Bad request exception occurred: {}", e.getMessage(), e);
+        return newResponse(e, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler({Exception.class, RuntimeException.class})
+    public ResponseEntity<?> handleAllException(Exception e) {
+        logger.error("Unexpected exception occurred: {}", e.getMessage(), e);
+        return newResponse(e, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
