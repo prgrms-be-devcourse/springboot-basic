3038bad118f4c7be134a8251af76534e61e2d1f9
HYEBPARK <hyeqls3742@gmail.com>
2022-05-24 01:52:16 +0900

    refactor : create, update, response 로 dto 분리

--- src/main/java/org/programmers/kdt/weekly/voucher/controller/restController/VoucherDto.java
+++ src/main/java/org/programmers/kdt/weekly/voucher/controller/restController/VoucherDto.java
@@ -3,14 +3,29 @@ package org.programmers.kdt.weekly.voucher.controller.restController;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+import javax.validation.constraints.NotNull;
+
 import org.programmers.kdt.weekly.voucher.model.VoucherType;
 
-public record VoucherDto(UUID id, VoucherType type, int value, LocalDateTime createdAt) {
-
-	public VoucherDto(UUID id, VoucherType type, int value, LocalDateTime createdAt) {
-		this.id = id;
-		this.type = type;
-		this.value = value;
-		this.createdAt = createdAt;
+public class VoucherDto {
+	public record VoucherUpdateRequest(
+		@NotNull
+		int value) {
 	}
+
+	public record VoucherCreateRequest(
+		@NotNull
+		VoucherType voucherType,
+
+		@NotNull
+		int value) {
+	}
+
+	public record VoucherResponse(
+		UUID id,
+		VoucherType type,
+		int value,
+		LocalDateTime createdAt) {
+	}
+
 }
