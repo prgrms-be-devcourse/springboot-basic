416f9dd27c54f5f285fa814eea4963d49b004e7e
yongckim <zxcv9203@naver.com>
2022-06-10 12:28:08 +0900

    feat : dto class default constructor modify

--- src/main/java/org/devcourse/voucher/application/voucher/controller/dto/VoucherRequest.java
+++ src/main/java/org/devcourse/voucher/application/voucher/controller/dto/VoucherRequest.java
@@ -1,16 +1,17 @@
 package org.devcourse.voucher.application.voucher.controller.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import org.devcourse.voucher.application.voucher.model.VoucherType;
 
 public class VoucherRequest {
 
-    private final VoucherType voucherType;
+    private VoucherType voucherType;
 
-    private final long discount;
+    private long discount;
 
-    public VoucherRequest(@JsonProperty("voucherType") VoucherType voucherType,
-                          @JsonProperty("discount") long discount) {
+    public VoucherRequest() {
+    }
+
+    public VoucherRequest(VoucherType voucherType, long discount) {
         this.voucherType = voucherType;
         this.discount = discount;
     }
