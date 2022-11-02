83c034beb3be443449935cc3de941d8f1b72a7f8
Kong <rhdtn311@naver.com>
2022-11-02 17:58:07 +0900

    init : 프로젝트 초기 세팅

---
+++ src/test/java/org/prgrms/vouchermanagement/VoucherManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package org.prgrms.vouchermanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoucherManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
