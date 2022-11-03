8dcb04100d6b1ed456528954a553f6f692d847aa
cloudwi <cloudwi@naver.com>
2022-11-04 01:42:10 +0900

    @SpringBootApplication를 활용해서 ApplicationContext로 빈을 가지고 온다.
    따라서 바우처어플리케이션을 가지고 와서 실행한다.

--- command-lind/src/main/java/com/programmers/commandLine/CommandLindApplication.java
+++ command-lind/src/main/java/com/programmers/commandLine/CommandLindApplication.java
@@ -4,9 +4,6 @@ import com.programmers.commandLine.domain.voucher.application.VoucherApplication
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
 /**
  *
