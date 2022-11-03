4d6a8507f10ba212792ee74050a6352f35154301
ttaehee <ttaehee1105@gmail.com>
2022-11-04 02:34:19 +0900

    initial commit

---
+++ src/main/java/com/programmers/devcoursevoucher/DevcourseVoucherApplication.java
@@ -0,0 +1,13 @@
+package com.programmers.devcoursevoucher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevcourseVoucherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevcourseVoucherApplication.class, args);
+	}
+
+}
