fd084c35c7e2479bbca1b8734ecdb22271aa98df
김기서 <5rne@naver.com>
2022-11-05 23:10:44 +0900

    feat: HashMap에 Voucher를 저장하는 Repository 구현

---
+++ src/main/java/com/programmers/springvoucherservice/repository/HashMapVoucherRepository.java
@@ -0,0 +1,31 @@
+package com.programmers.springvoucherservice.repository;
+
+import com.programmers.springvoucherservice.domain.voucher.Voucher;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Repository;
+
+import java.util.*;
+
+@Profile("local")
+@Repository
+public class HashMapVoucherRepository implements VoucherRepository {
+    Map<UUID, Voucher> map = new HashMap<>();
+
+    @Override
+    public Optional<Voucher> findById(UUID id) {
+        return Optional.ofNullable(map.get(id));
+    }
+
+    @Override
+    public List<Voucher> findAllVouchers() {
+        return new LinkedList<>(map.values());
+    }
+
+    @Override
+    public UUID registerVoucher(Voucher voucher) {
+        UUID voucherId = voucher.getVoucherId();
+        map.put(voucherId, voucher);
+
+        return voucherId;
+    }
+}
