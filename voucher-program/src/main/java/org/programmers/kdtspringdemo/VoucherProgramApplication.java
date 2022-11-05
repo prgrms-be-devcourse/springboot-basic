cd95259999466629aeed595fa246fc6549315b50
김소현 <thgus345@naver.com>
2022-11-05 12:46:05 +0900

    [UPDATE] 스프링부트 방식으로 실행 로직 변경

--- voucher-program/src/main/java/org/programmers/kdtspringdemo/VoucherProgramApplication.java
+++ voucher-program/src/main/java/org/programmers/kdtspringdemo/VoucherProgramApplication.java
@@ -1,12 +1,14 @@
 package org.programmers.kdtspringdemo;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class VoucherProgramApplication {
 	public static void main(String[] args) {
-//		SpringApplication.run(VoucherProgramApplication.class, args);
-		VoucherProgram voucherProgram = new VoucherProgram();
+		ConfigurableApplicationContext applicationContext = SpringApplication.run(VoucherProgramApplication.class, args);
+		VoucherProgram voucherProgram = applicationContext.getBean(VoucherProgram.class);
 		voucherProgram.run();
 	}
 
