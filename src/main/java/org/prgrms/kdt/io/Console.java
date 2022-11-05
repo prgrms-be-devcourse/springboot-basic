127430e059c6d3507413dbf95e856ba63c40847f
dlxortmd987 <dlxortmd987@gmail.com>
2022-11-06 05:04:13 +0900

    refactor: 입출력과 관련된 상수 Console로 이동

--- src/main/java/org/prgrms/kdt/io/Console.java
+++ src/main/java/org/prgrms/kdt/io/Console.java
@@ -2,11 +2,14 @@ package org.prgrms.kdt.io;
 
 import org.springframework.stereotype.Service;
 
-import static org.prgrms.kdt.utils.Constants.COMMAND_ERROR_PROMPT;
-import static org.prgrms.kdt.utils.Constants.COMMAND_LIST_PROMPT;
-
 @Service
 public class Console {
+    public static final String COMMAND_LIST_PROMPT = "=== Voucher Program ===" + System.lineSeparator() +
+            "Type exit to exit the program." + System.lineSeparator() +
+            "Type create to create a new voucher." + System.lineSeparator() +
+            "Type list to list all vouchers" + System.lineSeparator();
+    public static final String COMMAND_ERROR_PROMPT = "지원하지 않는 명령어입니다." + System.lineSeparator();
+
     private final Input input;
     private final Output output;
 
