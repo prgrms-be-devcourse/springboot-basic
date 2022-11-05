de579dd326f3409abd58ae03bdcf526322254714
Bosub Kim <39071638+boompatron@users.noreply.github.com>
2022-11-05 18:40:57 +0900

    다시 커밋 git ignore가 이상하게 들어가있어서 소스파일이 안올라감... 아니 다른것도 아니고 소스파일이 안올라가면 안되지..

---
+++ src/main/java/org/programmers/spbw1/SpringbootBasicW1Application.java
@@ -0,0 +1,13 @@
+package org.programmers.spbw1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBasicW1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBasicW1Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/programmers/spbw1/SpringbootBasicW1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.programmers.spbw1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBasicW1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
