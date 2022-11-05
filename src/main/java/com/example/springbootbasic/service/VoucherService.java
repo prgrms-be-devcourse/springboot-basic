d09906856c958403190f921d547af69ad2bed2e3
hyunseo <goatsfish@gmail.com>
2022-11-05 21:38:38 +0900

    refactor : 바우처 전체 검색, 바우처 저장 기능 리팩터링

--- src/main/java/com/example/springbootbasic/service/VoucherService.java
+++ src/main/java/com/example/springbootbasic/service/VoucherService.java
@@ -2,9 +2,14 @@ package com.example.springbootbasic.service;
 
 import com.example.springbootbasic.repository.VoucherRepository;
 import com.example.springbootbasic.domain.voucher.Voucher;
-import com.example.springbootbasic.domain.voucher.VoucherGeneratorEnum;
+import com.example.springbootbasic.domain.voucher.VoucherEnum;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+import static com.example.springbootbasic.domain.voucher.VoucherEnum.*;
+import static com.example.springbootbasic.util.CharacterUnit.*;
 
 @Service
 public class VoucherService {
@@ -15,10 +20,30 @@ public class VoucherService {
         this.voucherRepository = voucherRepository;
     }
 
-    public Long saveVoucher(String voucherType, Long amount) {
+    public Long saveVoucher(VoucherEnum voucherType, Long amount) {
         Long newVoucherId = voucherRepository.getSequence();
-        Voucher generatedVoucher = VoucherGeneratorEnum.generateVoucher(newVoucherId, amount, voucherType);
+        Voucher generatedVoucher = generateVoucher(newVoucherId, amount, voucherType);
         voucherRepository.save(newVoucherId, generatedVoucher);
         return newVoucherId;
+    }
+
+    public String findAllVouchers() {
+        return buildResponseBodyBy(voucherRepository.findAllVouchers());
+    }
+
+    private String buildResponseBodyBy(List<Voucher> findAllVouchers) {
+        StringBuilder responseBodyBuilder = new StringBuilder();
+        findAllVouchers.forEach(voucher -> {
+            responseBodyBuilder
+                    .append(voucher.getVoucherEnum().getVoucherType())
+                    .append(SPACE.getUnit())
+                    .append(voucher.getDiscountValue());
+            if (voucher.getVoucherEnum() == PERCENT_DISCOUNT_VOUCHER) {
+                responseBodyBuilder.append(PERCENT.getUnit());
+            }
+            responseBodyBuilder.append(ENTER.getUnit());
+        });
+
+        return responseBodyBuilder.toString();
     }
 }
