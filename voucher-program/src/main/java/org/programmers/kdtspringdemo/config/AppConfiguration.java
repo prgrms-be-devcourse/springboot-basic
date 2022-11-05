fc7089610d8df4e6ccb55e58d0ef0020535d5790
김소현 <thgus345@naver.com>
2022-11-05 12:43:29 +0900

    [ADD] basePackage 추가 (특정 bean 을 찾지 못하는 에러 수정)

--- voucher-program/src/main/java/org/programmers/kdtspringdemo/config/AppConfiguration.java
+++ voucher-program/src/main/java/org/programmers/kdtspringdemo/config/AppConfiguration.java
@@ -1,10 +1,11 @@
 package org.programmers.kdtspringdemo.config;
 
+import org.programmers.kdtspringdemo.voucher.model.Voucher;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan
+@ComponentScan(basePackages = "org.programmers.kdtspringdemo.voucher")
 public class AppConfiguration {
 
 }
