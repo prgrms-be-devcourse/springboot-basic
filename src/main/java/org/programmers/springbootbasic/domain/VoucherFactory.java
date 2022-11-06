b25ff1a9d4ed9654fd218eb65358618e85b30ade
ybkim-dev <beanybigggy@gmail.com>
2022-11-06 18:13:38 +0900

    fix : DefaultVoucherFactory를 제거
    새로운 Voucher가 추가되면 VoucherFactory를 추가하지 않고 VoucherFactory 내의 case 문을 통해 추가하기 위해 VoucherFactory만 유지합니다.

--- src/main/java/org/programmers/springbootbasic/domain/VoucherFactory.java
+++ src/main/java/org/programmers/springbootbasic/domain/VoucherFactory.java
@@ -1,7 +1,19 @@
 package org.programmers.springbootbasic.domain;
 
 import org.programmers.springbootbasic.data.VoucherType;
+import org.springframework.stereotype.Component;
 
-public interface VoucherFactory {
-    Voucher getVoucher(VoucherType type, long amount);
+import java.util.UUID;
+
+/**
+ * Voucher가 추가될 경우 case 문에 추가해서 처리 가능.
+ */
+@Component
+public class VoucherFactory{
+    public Voucher getVoucher(VoucherType type, long amount) {
+        return switch (type) {
+            case FIXED -> new FixedAmountVoucher(UUID.randomUUID(), amount);
+            case PERCENT -> new PercentDiscountVoucher(UUID.randomUUID(), amount);
+        };
+    }
 }
