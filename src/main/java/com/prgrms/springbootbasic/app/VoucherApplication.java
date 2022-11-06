ac9b4d2901b4e044479118e54c9e886de896046d
Hwan Kim <hwankim123@naver.com>
2022-11-06 19:13:47 +0900

    [VOU-01] refactor: MenuHandler에서 처리하던 I/O 작업을 VoucherApplication에게 넘김
    - VoucherApplication에서 while문을 돌며 Command 입력을 받음
    - Command에 따른 Console 출력과 과 MenuHandler의 메서드를 호출

--- src/main/java/com/prgrms/springbootbasic/app/VoucherApplication.java
+++ src/main/java/com/prgrms/springbootbasic/app/VoucherApplication.java
@@ -1,20 +1,56 @@
 package com.prgrms.springbootbasic.app;
 
+import com.prgrms.springbootbasic.console.Console;
+import com.prgrms.springbootbasic.handler.menu.CommandType;
 import com.prgrms.springbootbasic.handler.menu.MenuHandler;
+import com.prgrms.springbootbasic.handler.menu.dto.MenuInputResult;
 import org.springframework.stereotype.Component;
 
 @Component
 public class VoucherApplication {
-    private final MenuHandler menuHandler;
 
-    public VoucherApplication(MenuHandler menuHandler) {
+    private static final String COMMAND_NOT_SUPPORTER = "command not supported yet.";
+    private static final String MENU = "=== Voucher Program ===\n" +
+            "Type **exit** to exit the program.\n" +
+            "Type **create** to create a new voucher.\n" +
+            "Type **list** to list all vouchers.";
+    private static final String EXIT_MESSAGE = "Exit program. Bye.";
+
+    private final MenuHandler menuHandler;
+    private final ApplicationStatus applicationStatus;
+    private final Console console;
+
+    public VoucherApplication(MenuHandler menuHandler, ApplicationStatus applicationStatus, Console console) {
         this.menuHandler = menuHandler;
+        this.applicationStatus = applicationStatus;
+        this.console = console;
     }
 
     public void runLifecycle() {
-        boolean readyToExit = false;
-        while (!readyToExit) {
-            readyToExit = menuHandler.handleBeforeExit();
+        while (applicationStatus.isRunning()) {
+            getCommand();
+        }
+    }
+
+    private void getCommand() {
+        MenuInputResult inputResult = console.printAndGetCommand(MENU);
+        try {
+            CommandType commandType = CommandType.findByCommand(inputResult.getCommand());
+            controlMenu(commandType);
+        } catch (IllegalArgumentException e) {
+            console.printMessage(e.getMessage());
+        }
+    }
+
+    private void controlMenu(CommandType command) {
+        switch (command) {
+            case EXIT -> {
+                applicationStatus.exit();
+                console.printMessage(EXIT_MESSAGE);
+            }
+            case CREATE -> menuHandler.create();
+            case LIST -> menuHandler.list();
+            default -> console.printMessage(COMMAND_NOT_SUPPORTER);
         }
     }
 }
--- src/main/java/com/prgrms/springbootbasic/handler/menu/MenuHandler.java
+++ src/main/java/com/prgrms/springbootbasic/handler/menu/MenuHandler.java
@@ -1,53 +1,18 @@
 package com.prgrms.springbootbasic.handler.menu;
 
-import com.prgrms.springbootbasic.console.Console;
-import com.prgrms.springbootbasic.handler.menu.dto.MenuInputResult;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MenuHandler {
-    private static final String COMMAND_NOT_SUPPORTER = "command not supported yet.";
-    private static final String MENU = "=== Voucher Program ===\n" +
-            "Type **exit** to exit the program.\n" +
-            "Type **create** to create a new voucher.\n" +
-            "Type **list** to list all vouchers.";
-    private static final String EXIT = "Exit program. Bye.";
-    private final Console console;
 
-    public MenuHandler(Console console) {
-        this.console = console;
+    public MenuHandler() {
     }
 
-    public boolean handleBeforeExit() {
-        MenuInputResult inputResult = console.printAndGetCommand(MENU);
-        try {
-            CommandType commandType = CommandType.findByCommand(inputResult.getCommand());
-            handleMenu(commandType);
-            return commandType.isExit();
-        } catch (IllegalArgumentException e) {
-            console.printMessage(e.getMessage());
-            return false;
-        }
-    }
-
-    private void handleMenu(CommandType command) {
-        switch (command) {
-            case EXIT -> exit();
-            case CREATE -> create();
-            case LIST -> list();
-            default -> console.printMessage(COMMAND_NOT_SUPPORTER);
-        }
-    }
-
-    private void exit() {
-        console.printMessage(EXIT);
-    }
-
-    private void create() {
+    public void create() {
         System.out.println("Create Voucher. coming soon");
     }
 
-    private void list() {
+    public void list() {
         System.out.println("Voucher List. coming soon");
     }
 }
