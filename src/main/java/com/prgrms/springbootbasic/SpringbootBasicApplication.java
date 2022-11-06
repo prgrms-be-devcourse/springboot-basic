044ee4f7668092ffb0bc6c16dff573ec46bfc2e2
Hwan Kim <hwankim123@naver.com>
2022-11-06 17:22:49 +0900

    [VOU-01] refactor: 바우처 어플리케이션이 더이상 SpringBoot 기반에서 실행되지 않도록 수정
    - Application Context를 이용하여 Bean으로 등록한 VoucherApplication을 직접 사용하기 때문

--- src/main/java/com/prgrms/springbootbasic/SpringbootBasicApplication.java
+++ src/main/java/com/prgrms/springbootbasic/SpringbootBasicApplication.java
@@ -2,17 +2,11 @@ package com.prgrms.springbootbasic;
 
 import com.prgrms.springbootbasic.app.VoucherApplication;
 import com.prgrms.springbootbasic.common.config.AppConfiguration;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-@SpringBootApplication
 public class SpringbootBasicApplication {
 
     public static void main(String[] args) {
-
-        SpringApplication.run(SpringbootBasicApplication.class, args);
-
         AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfiguration.class);
 
         VoucherApplication console = applicationContext.getBean(VoucherApplication.class);
