6e4ad7ee6f4428bd591fc59e05c5f55dfdb8d88d
Kim Changgyu <kimc980106@naver.com>
2022-11-08 00:09:57 +0900

    feat: Voucher(VO) 및 구상체 추가
    Voucher.java: V.O. 인터페이스
    FixedAmountVoucher.java: 고정 금액 할인 바우처 구상체
    PercentDiscountVoucher.java: 비율 할인 바우처 구상체

---
+++ src/main/java/org/prgrms/java/domain/voucher/FixedAmountVoucher.java
@@ -0,0 +1,28 @@
+package org.prgrms.java.domain.voucher;
+
+import java.util.UUID;
+
+public class FixedAmountVoucher implements Voucher {
+    private final UUID voucherId;
+    private final long amount;
+
+    public FixedAmountVoucher(UUID voucherId, long amount) {
+        this.voucherId = voucherId;
+        this.amount = amount;
+    }
+
+    @Override
+    public UUID getVoucherId() {
+        return voucherId;
+    }
+
+    @Override
+    public long discount(long beforeDiscount) {
+        return beforeDiscount - amount;
+    }
+
+    @Override
+    public String toString() {
+        return voucherId + ", " + amount;
+    }
+}
---
+++ src/main/java/org/prgrms/java/domain/voucher/Voucher.java
@@ -0,0 +1,9 @@
+package org.prgrms.java.domain.voucher;
+
+import java.util.UUID;
+
+public interface Voucher {
+    UUID getVoucherId();
+
+    long discount(long beforeDiscount);
+}
