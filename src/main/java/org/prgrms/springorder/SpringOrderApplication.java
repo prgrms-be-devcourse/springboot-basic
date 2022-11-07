7336dd19aa1cc6fd1e932b8dc1cfee6439c87ad0
ysk <kim206gh@naver.com>
2022-11-07 12:36:19 +0900

    project init

---
+++ src/main/java/org/prgrms/springorder/SpringOrderApplication.java
@@ -0,0 +1,13 @@
+package org.prgrms.springorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringOrderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/prgrms/springorder/SpringOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package org.prgrms.springorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
