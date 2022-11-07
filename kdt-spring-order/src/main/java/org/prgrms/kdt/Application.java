638da7ba7205a996db975e77b8f3ffafb08a7bf4
Lee Suyoung <78334910+twotwobread@users.noreply.github.com>
2022-11-07 23:08:27 +0900

    fix: CommandLineApplication을 SpringBoot를 실행하는 Application과 명령어를 입력받고 이에 맞는 service logic을 수행하는 CommandLineController로 나누다. [Week1.Day4]

---
+++ kdt-spring-order/src/main/java/org/prgrms/kdt/Application.java
@@ -0,0 +1,14 @@
+package org.prgrms.kdt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        var springApplication = new SpringApplication(Application.class);
+        springApplication.setAdditionalProfiles("dev");
+        springApplication.run(args);
+    }
+}
---
+++ kdt-spring-order/src/main/java/org/prgrms/kdt/CommandLineController.java
@@ -0,0 +1,57 @@
+package org.prgrms.kdt;
+
+import org.prgrms.kdt.io.ConsoleIO;
+import org.prgrms.kdt.service.BlackListService;
+import org.prgrms.kdt.service.VoucherService;
+import org.prgrms.kdt.voucher.VoucherValidator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.stream.Collectors;
+
+@Component
+public class CommandLineController implements CommandLineRunner {
+
+    private static final Logger logger = LoggerFactory.getLogger(CommandLineController.class);
+    private final ConsoleIO consoleIO;
+    private final VoucherService voucherService;
+    private final BlackListService blackListService;
+
+    public CommandLineController(ConsoleIO consoleIO, VoucherService voucherService, BlackListService blackListService) {
+        this.consoleIO = consoleIO;
+        this.voucherService = voucherService;
+        this.blackListService = blackListService;
+    }
+
+    public void run(String[] args) {
+        while (true) {
+            consoleIO.printEnableCommandList();
+            try {
+                CommandType command = CommandType.of(consoleIO.inputCommand());
+                switch (command) {
+                    case CREATE -> {
+                        String voucherType = consoleIO.inputVoucherType();
+                        String voucherDiscountValue = consoleIO.inputVoucherDiscountValue();
+                        VoucherValidator.validateVoucherTypeAndDiscountValue(voucherType, voucherDiscountValue);
+                        voucherService.save(voucherService.create(voucherType, voucherDiscountValue));
+                    }
+                    case LIST -> consoleIO.printItems(voucherService.getAllVouchers().stream()
+                            .map(voucher -> voucher.toString())
+                            .collect(Collectors.toList()));
+                    case BLACKLIST -> consoleIO.printItems(blackListService.getAllBlackList().stream()
+                            .map(voucher -> voucher.toString())
+                            .collect(Collectors.toList()));
+                    case EXIT -> {
+                        consoleIO.terminate();
+                        System.exit(0);
+                    }
+                }
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+                logger.warn("{} {}", e.getMessage(), e.getStackTrace());
+            }
+        }
+    }
+}
