7e56951e24d12c5c05a15d0f88fc7b24cc9aa371
Kong <rhdtn311@naver.com>
2022-11-08 12:07:51 +0900

    feat : BlackList 조회 기능 추가

--- src/main/java/org/prgrms/vouchermanagement/customer/repository/BlackListCustomerRepository.java
+++ src/main/java/org/prgrms/vouchermanagement/customer/repository/BlackListCustomerRepository.java
@@ -1,10 +1,16 @@
 package org.prgrms.vouchermanagement.customer.repository;
 
+import org.prgrms.vouchermanagement.customer.domain.BlackCustomer;
 import org.prgrms.vouchermanagement.customer.domain.Customer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 @Repository
 public class BlackListCustomerRepository implements CustomerRepository {
@@ -17,10 +23,20 @@ public class BlackListCustomerRepository implements CustomerRepository {
 
     @Override
     public List<Customer> findAll() {
-        return null;
+        List<Customer> customers = new ArrayList<>();
+        try {
+            List<String> blackList = Files.readAllLines(Paths.get(path));
+            blackList.forEach(customerInfo -> customers.add(createCustomer(customerInfo.split(","))));
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        return customers;
     }
 
     private Customer createCustomer(String[] customerInfos) {
-        return null;
+        UUID customerId = UUID.fromString(customerInfos[0]);
+        String name = customerInfos[1];
+
+        return new BlackCustomer(customerId, name);
     }
 }
