6b4dff34ed7688422d4026488ceb0c971e20a3aa
ybkim-dev <beanybigggy@gmail.com>
2022-11-09 01:45:55 +0900

    test : 바우처 생성 실패 케이스 추가

--- src/test/java/org/programmers/springbootbasic/service/VoucherServiceTest.java
+++ src/test/java/org/programmers/springbootbasic/service/VoucherServiceTest.java
@@ -1,6 +1,7 @@
 package org.programmers.springbootbasic.service;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.programmers.springbootbasic.data.VoucherType;
 import org.programmers.springbootbasic.domain.FixedAmountVoucher;
@@ -8,15 +9,21 @@ import org.programmers.springbootbasic.domain.PercentDiscountVoucher;
 import org.programmers.springbootbasic.domain.Voucher;
 import org.programmers.springbootbasic.domain.VoucherFactory;
 import org.programmers.springbootbasic.dto.VoucherDto;
+import org.programmers.springbootbasic.exception.WrongTypeInputException;
 import org.programmers.springbootbasic.repository.MemoryVoucherRepository;
 
 class VoucherServiceTest {
+    VoucherService voucherService;
+
+    @BeforeEach
+    void init() {
+        voucherService = new VoucherService(new MemoryVoucherRepository(), new VoucherFactory());
+    }
 
 
     @Test
     public void 바우처_생성_테스트() throws Exception {
         //given
-        VoucherService voucherService = new VoucherService(new MemoryVoucherRepository(), new VoucherFactory());
 
         // when
         Voucher fixedVoucher = voucherService.createVoucher(new VoucherDto(VoucherType.FIXED, 10));
@@ -29,9 +36,20 @@ class VoucherServiceTest {
     }
 
     @Test
+    public void 바우처_생성_실패_테스트() throws Exception {
+        //given
+        VoucherDto voucherDto = new VoucherDto(VoucherType.WRONG_INPUT, 20);
+        // when
+
+        //then
+        Assertions.assertThatThrownBy(() -> {
+            voucherService.createVoucher(voucherDto);
+        }).isInstanceOf(WrongTypeInputException.class);
+    }
+
+    @Test
     public void 바우처_저장_테스트() throws Exception {
         //given
-        VoucherService voucherService = new VoucherService(new MemoryVoucherRepository(), new VoucherFactory());
 
         // when
         Voucher percentVoucher = voucherService.createVoucher(new VoucherDto(VoucherType.PERCENT, 20));
