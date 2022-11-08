4e824b548b19d0dc5aebd80c4a71c444cbb11196
Kong <rhdtn311@naver.com>
2022-11-08 11:48:20 +0900

    feat : Voucher 메모리 저장 클래스 생성

---
+++ src/main/java/org/prgrms/vouchermanagement/voucher/repository/MemoryVoucherRepository.java
@@ -0,0 +1,28 @@
+package org.prgrms.vouchermanagement.voucher.repository;
+
+import org.prgrms.vouchermanagement.voucher.domain.Voucher;
+import org.springframework.stereotype.Repository;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Repository
+public class MemoryVoucherRepository implements VoucherRepository {
+
+    private final Map<UUID, Voucher> storage = new ConcurrentHashMap<>();
+
+    @Override
+    public Voucher save(Voucher voucher) {
+        return storage.put(voucher.getVoucherId(), voucher);
+    }
+
+    @Override
+    public Optional<Voucher> findById(UUID voucherId) {
+        return Optional.ofNullable(storage.get(voucherId));
+    }
+
+    @Override
+    public List<Voucher> findAll() {
+        return new ArrayList<>(storage.values());
+    }
+}
---
+++ src/main/java/org/prgrms/vouchermanagement/voucher/repository/VoucherRepository.java
@@ -0,0 +1,15 @@
+package org.prgrms.vouchermanagement.voucher.repository;
+
+import org.prgrms.vouchermanagement.voucher.domain.Voucher;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface VoucherRepository {
+    Voucher save(Voucher voucher);
+
+    Optional<Voucher> findById(UUID voucherId);
+
+    List<Voucher> findAll();
+}
