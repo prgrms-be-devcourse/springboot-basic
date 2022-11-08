f7c53da9bf0e0a178b1aac40f03a7a1103017737
김기서 <5rne@naver.com>
2022-11-09 01:59:32 +0900

    Refactor: CommandLineApplication을 빈으로 등록. SpringVoucherServiceApplication에서 CommandLineApplication만 수동으로 빈 검색 후 실행

--- src/main/java/com/programmers/SpringVoucherServiceApplication.java
+++ src/main/java/com/programmers/SpringVoucherServiceApplication.java
@@ -1,8 +1,6 @@
 package com.programmers;
 
 import com.programmers.voucher.CommandLineApplication;
-import com.programmers.voucher.service.VoucherService;
-import com.programmers.voucher.view.View;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
@@ -15,12 +13,12 @@ public class SpringVoucherServiceApplication {
 
     public static void main(String[] args) {
         ConfigurableApplicationContext ac = SpringApplication.run(SpringVoucherServiceApplication.class, args);
-        VoucherService voucherService = ac.getBean(VoucherService.class);
-        View view = ac.getBean(View.class);
+
+        CommandLineApplication commandLineApplication = ac.getBean(CommandLineApplication.class);
 
         logger.info("애플리케이션 기동");
+        commandLineApplication.run();
 
-        new CommandLineApplication(view, voucherService).run();
     }
 
 }
