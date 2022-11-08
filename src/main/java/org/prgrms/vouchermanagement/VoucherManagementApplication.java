33a5018b118bc481cf38dbaa8c6dba9602d3fa28
Kong <rhdtn311@naver.com>
2022-11-08 12:20:02 +0900

    feat : 스프링부트 실행 시 바우처 관리 어플리케이션 실행

--- src/main/java/org/prgrms/vouchermanagement/VoucherManagementApplication.java
+++ src/main/java/org/prgrms/vouchermanagement/VoucherManagementApplication.java
@@ -2,12 +2,14 @@ package org.prgrms.vouchermanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class VoucherManagementApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(VoucherManagementApplication.class, args);
+		ConfigurableApplicationContext ac = SpringApplication.run(VoucherManagementApplication.class, args);
+		ac.getBean(CommandLineApplication.class).run();
 	}
 
 }
