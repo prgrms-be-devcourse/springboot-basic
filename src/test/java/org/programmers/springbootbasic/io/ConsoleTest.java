5c873f69f15f8daa3c87f7ea6532aeb8fb67a27b
ybkim-dev <beanybigggy@gmail.com>
2022-11-09 01:16:51 +0900

    test : 사용자 입력 성공케이스 실패케이스 추가

--- src/test/java/org/programmers/springbootbasic/io/ConsoleTest.java
+++ src/test/java/org/programmers/springbootbasic/io/ConsoleTest.java
@@ -1,6 +1,8 @@
 package org.programmers.springbootbasic.io;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.programmers.springbootbasic.data.VoucherMainMenuCommand;
 
 class ConsoleTest {
 
@@ -10,12 +12,22 @@ class ConsoleTest {
         String input1 = "exit";
         String input2 = "create";
         String input3 = "list";
+        String wrongInput1 = "aas";
+        String wrongInput2 = "123123";
+        String wrongInput3 = "!#!#@#12312#@!!#@3";
+        String wrongInput4 = "EXIT";
+
         // when
-
         //then
-    }
 
-    @Test
-    void printMenu() {
+        Assertions.assertThat(VoucherMainMenuCommand.EXIT).isEqualTo(VoucherMainMenuCommand.valueOfCommand(input1));
+        Assertions.assertThat(VoucherMainMenuCommand.CREATE).isEqualTo(VoucherMainMenuCommand.valueOfCommand(input2));
+        Assertions.assertThat(VoucherMainMenuCommand.LIST).isEqualTo(VoucherMainMenuCommand.valueOfCommand(input3));
+
+        // wrong input.
+        Assertions.assertThat(VoucherMainMenuCommand.WRONG_INPUT).isEqualTo(VoucherMainMenuCommand.valueOfCommand(wrongInput1));
+        Assertions.assertThat(VoucherMainMenuCommand.WRONG_INPUT).isEqualTo(VoucherMainMenuCommand.valueOfCommand(wrongInput2));
+        Assertions.assertThat(VoucherMainMenuCommand.WRONG_INPUT).isEqualTo(VoucherMainMenuCommand.valueOfCommand(wrongInput3));
+        Assertions.assertThat(VoucherMainMenuCommand.WRONG_INPUT).isEqualTo(VoucherMainMenuCommand.valueOfCommand(wrongInput4));
     }
 }
