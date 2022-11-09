7c5710e1e662aa9bfb406fdb5610c224ec4d9635
Rose <82176176+geonwoo0215@users.noreply.github.com>
2022-11-09 20:01:53 +0900

    feat : main에서 controller 실행

--- src/main/java/org/prgrms/springorder/SpringorderApplication.java
+++ src/main/java/org/prgrms/springorder/SpringorderApplication.java
@@ -1,13 +1,16 @@
 package org.prgrms.springorder;
 
+import org.prgrms.springorder.controller.VoucherController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringorderApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringorderApplication.class, args);
+		ApplicationContext applicationContext = SpringApplication.run(SpringorderApplication.class, args);
+		applicationContext.getBean(VoucherController.class).run();
 	}
 
 }
