753a8e40876adb6fc48a6f6b6ddf7b4bbc188fde
Kim Changgyu <kimc980106@naver.com>
2022-11-10 01:56:20 +0900

    style: CLI 가이드 색상 및 볼드체 적용

--- src/main/java/org/prgrms/java/common/MessageGuide.java
+++ src/main/java/org/prgrms/java/common/MessageGuide.java
@@ -3,10 +3,10 @@ package org.prgrms.java.common;
 public enum MessageGuide {
     COMMAND_OPTION(
             "=== Voucher Program ===\n" +
-            "Type exit to exit the program.\n" +
-            "Type create to create a new voucher.\n" +
-            "Type list to list all vouchers.\n" +
-            "Type blacklist to list all blocked users."
+            "Type " + MessageGuide.ANSI_BOLD + MessageGuide.ANSI_RED + "exit" + MessageGuide.ANSI_RESET + " to " + MessageGuide.ANSI_BOLD + MessageGuide.ANSI_RED + "exit" + MessageGuide.ANSI_RESET + " the program.\n" +
+            "Type " + MessageGuide.ANSI_BOLD + "create" + MessageGuide.ANSI_RESET + " to " + MessageGuide.ANSI_BOLD + "create" + MessageGuide.ANSI_RESET + " a new voucher.\n" +
+            "Type " + MessageGuide.ANSI_BOLD + "list" + MessageGuide.ANSI_RESET + " to " + MessageGuide.ANSI_BOLD + "list" + MessageGuide.ANSI_RESET + " all vouchers.\n" +
+            "Type " + MessageGuide.ANSI_BOLD + "blacklist" + MessageGuide.ANSI_RESET + " to " + MessageGuide.ANSI_BOLD + "list" + MessageGuide.ANSI_RESET + " all blocked users.\n"
     ),
     VOUCHER_TYPE(
             "=== Select Voucher Type ===\n" +
@@ -18,6 +18,10 @@ public enum MessageGuide {
             "If you choose 'Fixed' : 1000\n" +
             "If you choose 'Percentage' : 10% -> 10"
     );
+
+    private static final String ANSI_RESET = "\u001B[0m";
+    private static final String ANSI_BOLD = "\u001B[1m";
+    private static final String ANSI_RED = "\u001B[31m";
 
     private final String message;
 
