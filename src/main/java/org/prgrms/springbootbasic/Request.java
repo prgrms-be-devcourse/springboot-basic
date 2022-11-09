a53baea8669841973bb46bfe58696e20677df5fd
bufflect <bndgc@naver.com>
2022-11-10 01:38:11 +0900

    feat: Request 추가

---
+++ src/main/java/org/prgrms/springbootbasic/Request.java
@@ -0,0 +1,48 @@
+package org.prgrms.springbootbasic;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+public class Request {
+
+    private final Menu menuItem;
+    private TypeOption option;
+
+    private final ConcurrentHashMap<String, Long> arguments = new ConcurrentHashMap<>();
+
+    private Request(Menu menuItem) {
+        this.menuItem = menuItem;
+    }
+
+    public static Request GenerateRequest(String input) {
+        Menu menuItem = Menu.valueOf(input.toUpperCase());
+        return new Request(menuItem);
+    }
+
+    public Menu getMenuItem() {
+        return menuItem;
+    }
+
+    public Long getArgument(String argument) {
+        return arguments.get(argument);
+    }
+
+    public TypeOption getOption() {
+        return option;
+    }
+
+    public void setOption(TypeOption option) {
+        this.option = option;
+    }
+
+    public void insertArgument(String argument, long value) {
+        arguments.put(argument, value);
+    }
+
+    public boolean isCreateRequest() {
+        return menuItem == Menu.CREATE;
+    }
+
+    public boolean isLookupRequest() {
+        return menuItem == Menu.LIST;
+    }
+}
