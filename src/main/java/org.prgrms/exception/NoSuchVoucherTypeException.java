3bd465a43c9ad5e43271def7fe5e5e8fe8b164d5
yewon9609 <dodnjs1241@naver.com>
2022-11-09 23:56:46 +0900

    refactor: 오류 메세지 영어로 수정

--- src/main/java/org.prgrms/exception/NoSuchVoucherTypeException.java
+++ src/main/java/org.prgrms/exception/NoSuchVoucherTypeException.java
@@ -3,6 +3,6 @@ package org.prgrms.exception;
 public class NoSuchVoucherTypeException extends RuntimeException {
 
   public NoSuchVoucherTypeException(String voucherType) {
-    super(voucherType + "은 존재하지 않습니다. 다시입력해주세요");
+    super("' " + voucherType + " ' does not exist. Please re-enter");
   }
 }
--- src/main/java/org.prgrms/exception/PaymentCannotBeNegativeException.java
+++ src/main/java/org.prgrms/exception/PaymentCannotBeNegativeException.java
@@ -3,7 +3,10 @@ package org.prgrms.exception;
 public class PaymentCannotBeNegativeException extends RuntimeException {
 
   public PaymentCannotBeNegativeException(Long payment) {
-    super("결제금액은 0원 이상이어야 합니다. *현재 결제금액 :" + payment + "원");
+    super("The payment amount must be at least 0 won."
+        + System.lineSeparator()
+        + "* Current payment amount :" + payment + "won");
+
   }
 
 }
