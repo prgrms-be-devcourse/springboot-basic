b7fde2283c25aec0afb7e91f89d22459a352976f
Rose <82176176+geonwoo0215@users.noreply.github.com>
2022-11-09 18:42:03 +0900

    initial commit

---
+++ src/main/java/org/prgrms/springorder/SpringorderApplication.java
@@ -0,0 +1,13 @@
+package org.prgrms.springorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringorderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringorderApplication.class, args);
+	}
+
+}
