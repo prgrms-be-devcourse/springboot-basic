7b6113e0a600290bb65158cca6e110f10a5c3393
bufflect <bndgc@naver.com>
2022-11-10 01:37:56 +0900

    feat: TypeOption

---
+++ src/main/java/org/prgrms/springbootbasic/TypeOption.java
@@ -0,0 +1,31 @@
+package org.prgrms.springbootbasic;
+
+public enum TypeOption {
+    FIXED("1", "fixedAmountVoucherFactory"), PERCENT("2", "PercentAmountVoucherFactory");
+
+    private final String number;
+    private final String beanName;
+
+    TypeOption(String number, String beanName) {
+        this.number = number;
+        this.beanName = beanName;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public String getBeanName() {
+        return beanName;
+    }
+
+
+    public static boolean isFixed(String option) {
+        return FIXED.getNumber().equals(option);
+    }
+
+    public static boolean isPercent(String option) {
+        return PERCENT.getNumber().equals(option);
+    }
+
+}
