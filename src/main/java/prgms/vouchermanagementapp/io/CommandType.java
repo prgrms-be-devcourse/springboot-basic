9315fd06cf7c6c1e5b9d78e3d2de8c2dc8d35406
preferKim <shineecard5@gmail.com>
2022-11-10 15:59:55 +0900

    Refactor(command): 입력받은 커맨드를 확인하는 로직 변경
    (전) stream을 이용하여 검증 ->
    (후) Enum.valueOf() 메서드를 사용하여 검증

--- src/main/java/prgms/vouchermanagementapp/io/CommandType.java
+++ src/main/java/prgms/vouchermanagementapp/io/CommandType.java
@@ -1,23 +1,17 @@
 package prgms.vouchermanagementapp.io;
 
-import prgms.vouchermanagementapp.io.message.ExceptionMessage;
-
-import java.util.Arrays;
 import java.util.List;
 
 public enum CommandType {
 
-    // TODO: UpperCase를 사용하여 리팩토링
-    // 출력 역할 입장에서 생각해봐야한다.
-    EXIT("exit", "Type exit to exit the program."),
-    CREATE("create", "Type create to create a new voucher."),
-    LIST("list", "Type list to list all vouchers.");
+    // TODO: 출력 역할 입장에서 생각해봐야한다.
+    EXIT("Type exit to exit the program."),
+    CREATE("Type create to create a new voucher."),
+    LIST("Type list to list all vouchers.");
 
-    private final String command;
     private final String message;
 
-    CommandType(String command, String message) {
-        this.command = command;
+    CommandType(String message) {
         this.message = message;
     }
 
@@ -25,11 +19,8 @@ public enum CommandType {
         return List.of(CommandType.values());
     }
 
-    public static CommandType of(String menu) {
-        return Arrays.stream(CommandType.values())
-                .filter(commandType -> commandType.command.equals(menu))
-                .findFirst()
-                .orElseThrow(() -> new NoSuchFieldError(ExceptionMessage.NO_MENU_EXISTS.getMessage() + menu));
+    public static CommandType of(String command) throws IllegalArgumentException {
+        return CommandType.valueOf(command.toUpperCase());
     }
 
     public boolean is(CommandType commandType) {
