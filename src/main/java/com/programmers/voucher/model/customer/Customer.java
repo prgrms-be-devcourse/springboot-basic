296fab4b4df46c46405f3ed1d37cc92a9dabbff2
ttaehee <ttaehee1105@gmail.com>
2022-11-10 23:30:42 +0900

    feat : customer entity를 생성하라

--- src/main/java/com/programmers/voucher/model/customer/Customer.java
+++ src/main/java/com/programmers/voucher/model/customer/Customer.java
@@ -1,4 +1,11 @@
 package com.programmers.voucher.model.customer;
 
 public class Customer {
+    private final String customerId;
+    private final String customerName;
+
+    public Customer(String customerId, String customerName) {
+        this.customerId = customerId;
+        this.customerName = customerName;
+    }
 }
