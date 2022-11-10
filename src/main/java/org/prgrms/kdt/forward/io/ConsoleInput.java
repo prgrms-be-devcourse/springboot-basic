b8865ab3b941de3b7724a395a25def6e554e144e
fortune00 <sinyoungbok99@gmail.com>
2022-11-10 18:10:16 +0900

    feat: io 인터페이스와 콘솔 구현체 생성

---
+++ src/main/java/org/prgrms/kdt/forward/io/ConsoleInput.java
@@ -0,0 +1,29 @@
+package org.prgrms.kdt.forward.io;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class ConsoleInput implements Input {
+
+    private final BufferedReader reader;
+
+    public ConsoleInput(BufferedReader reader) {
+        this.reader = reader;
+    }
+
+    public String readLine() {
+        String input = "";
+        try {
+            input = reader.readLine();
+        } catch (IOException ignored) {
+        }
+        return input;
+    }
+
+    public void close() {
+        try {
+            reader.close();
+        } catch (IOException ignored) {
+        }
+    }
+}
---
+++ src/main/java/org/prgrms/kdt/forward/io/Input.java
@@ -0,0 +1,6 @@
+package org.prgrms.kdt.forward.io;
+
+public interface Input {
+    String readLine();
+    void close();
+}
