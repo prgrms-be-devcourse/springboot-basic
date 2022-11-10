3a1da0d594dd7b4c1ab03fcb6f6feb53a42a12b0
권주성 <99165624+JoosungKwon@users.noreply.github.com>
2022-11-10 16:39:53 +0900

    feat: 명령어를 관리하기 위한 enum 클래스 생성
    * 명령어를 enum으로 관리하기

---
+++ src/main/java/com/programmers/kwonjoosung/springbootbasicjoosung/controller/CommandType.java
@@ -0,0 +1,31 @@
+package com.programmers.kwonjoosung.springbootbasicjoosung.controller;
+
+import java.util.stream.Stream;
+
+public enum CommandType {
+    EXIT("Type exit to exit the program.", "exit"),
+    CREATE("Type create to create a new voucher.", "create"),
+    LIST("Type list to list all vouchers.", "list"),
+
+    HELP("Type help to show all command.", "help");
+
+    private final String explanation;
+    private final String command;
+
+    CommandType(String explanation, String command) {
+        this.explanation = explanation;
+        this.command = command;
+    }
+
+    public static CommandType getCommand(String input) {
+        return Stream.of(CommandType.values())
+                .filter(commandType -> commandType.command.equals(input))
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException(input));
+    }
+
+    public String getExplanation() {
+        return explanation;
+    }
+}
+
