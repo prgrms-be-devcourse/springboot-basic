45de450dbc9ea984d628c8fcd15a60727632672b
권성준 <tjdwns1865@naver.com>
2022-11-11 04:36:46 +0900

    test: @ParameterizedTest 적용

--- src/test/java/com/programmers/voucher/domain/voucher/util/VoucherFactoryTest.java
+++ src/test/java/com/programmers/voucher/domain/voucher/util/VoucherFactoryTest.java
@@ -1,12 +1,12 @@
 package com.programmers.voucher.domain.voucher.util;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.UUID;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import com.programmers.voucher.domain.voucher.model.FixedDiscountVoucher;
 import com.programmers.voucher.domain.voucher.model.PercentDiscountVoucher;
@@ -33,35 +33,21 @@ class VoucherFactoryTest {
 		Assertions.assertThat(percentVoucher).isInstanceOf(PercentDiscountVoucher.class);
 	}
 
-	@Test
+	@ParameterizedTest
+	@ValueSource(strings = {"   ", "이십", "twenty", "20.."})
 	@DisplayName("할인값이 숫자가 아니면 예외가 발생한다.")
-	void validateDiscountIsNumber() {
-		List<String> discounts = new ArrayList<>();
-		discounts.add("   ");
-		discounts.add("이십");
-		discounts.add("twenty");
-		discounts.add("20.");
-
+	void validateDiscountIsNumber(String discount) {
 		Assertions.assertThatExceptionOfType(IllegalArgumentException.class)
-			.isThrownBy(() -> {
-				discounts.stream()
-					.forEach(i -> factory.makeVoucher(VoucherType.FIXED, UUID.randomUUID(), i));
-			})
+			.isThrownBy(() -> factory.makeVoucher(VoucherType.FIXED, UUID.randomUUID(), discount))
 			.withMessageContaining(ExceptionMessage.WRONG_DISCOUNT_TYPE.getMessage());
 	}
 
-	@Test
+	@ParameterizedTest
+	@ValueSource(strings = {"120", "-5"})
 	@DisplayName("Percent 바우처일 때, 할인 정도가 0~100 범위 밖이면 예외가 발생한다.")
-	void validateDiscountPercentRange() {
-		List<String> discounts = new ArrayList<>();
-		discounts.add("120");
-		discounts.add("-5");
-
+	void validateDiscountPercentRange(String discount) {
 		Assertions.assertThatExceptionOfType(IllegalArgumentException.class)
-			.isThrownBy(() -> {
-				discounts.stream()
-					.forEach(i -> factory.makeVoucher(VoucherType.PERCENT, UUID.randomUUID(), i));
-			})
+			.isThrownBy(() -> factory.makeVoucher(VoucherType.PERCENT, UUID.randomUUID(), discount))
 			.withMessageContaining(ExceptionMessage.OUT_OF_DISCOUNT_PERCENT_RANGE.getMessage());
 	}
 }
