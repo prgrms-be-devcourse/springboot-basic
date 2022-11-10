6f61ddc02eb26e203db98ed4619e10c3c8fce9fc
권주성 <99165624+JoosungKwon@users.noreply.github.com>
2022-11-10 17:08:38 +0900

    feat: 트랜잭션 관리 및 도메인 비지니스 로직을 담당하는 VoucherService 생성
    * 트랜잭션 관리 및 도메인 비지니스 로직을 수행하는 역할
    * Voucher를 생성한다
    * Reporotory를 통해서 저장된 Voucher를 가져온다

---
+++ src/main/java/com/programmers/kwonjoosung/springbootbasicjoosung/service/VoucherService.java
@@ -0,0 +1,39 @@
+package com.programmers.kwonjoosung.springbootbasicjoosung.service;
+
+import com.programmers.kwonjoosung.springbootbasicjoosung.model.voucher.FixedAmountDiscountVoucher;
+import com.programmers.kwonjoosung.springbootbasicjoosung.model.voucher.PercentDiscountVoucher;
+import com.programmers.kwonjoosung.springbootbasicjoosung.model.voucher.Voucher;
+import com.programmers.kwonjoosung.springbootbasicjoosung.model.voucher.VoucherType;
+import com.programmers.kwonjoosung.springbootbasicjoosung.repository.voucher.VoucherRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class VoucherService {
+    private final VoucherRepository voucherRepository;
+
+    public VoucherService(VoucherRepository voucherRepository){
+        this.voucherRepository = voucherRepository;
+    }
+
+    public List<Voucher> getVoucherList() {
+        return voucherRepository.findAll();
+    }
+
+    public Voucher createAndSave(VoucherType voucherType, long discount) {
+        return saveVoucher(createVoucher(voucherType,discount));
+    }
+
+    private Voucher createVoucher(VoucherType voucherType,long discount){
+        return switch (voucherType) {
+            case FIXED -> new FixedAmountDiscountVoucher(discount);
+            case PERCENT -> new PercentDiscountVoucher(discount);
+        };
+    }
+
+    private Voucher saveVoucher(Voucher voucher){
+        return voucherRepository.insert(voucher.getVoucherId(),voucher);
+    }
+
+}
