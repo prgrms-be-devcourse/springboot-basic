8d13a378459190058dbfc1ea9dcb942fd7dcb957
bufflect <bndgc@naver.com>
2022-11-10 12:26:11 +0900

    remove: 불필요한 import문, 디버깅 코드 제거

--- src/main/java/org/prgrms/springbootbasic/SpringbootBasicApplication.java
+++ src/main/java/org/prgrms/springbootbasic/SpringbootBasicApplication.java
@@ -1,35 +1,18 @@
 package org.prgrms.springbootbasic;
 
-import org.prgrms.springbootbasic.factory.VoucherFactory;
-import org.prgrms.springbootbasic.repository.VoucherRepository;
-import org.prgrms.springbootbasic.service.VoucherService;
-import org.prgrms.springbootbasic.type.TypeOption;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
-import java.util.Map;
-
 @SpringBootApplication
 public class SpringbootBasicApplication {
 
-	public static void main(String[] args) {
-//		SpringApplication.run(SpringbootBasicApplication.class, args);
-		ConfigurableApplicationContext configurableApplicationContext = SpringApplication
-				.run(SpringbootBasicApplication.class, args);
-//				.getBean();
-//		.process();
+    public static void main(String[] args) {
+        ConfigurableApplicationContext configurableApplicationContext = SpringApplication
+                .run(SpringbootBasicApplication.class, args);
 
-//		Console console = configurableApplicationContext.getBean(Console.class);
-//		console.run();
-
-		VoucherService voucherService = configurableApplicationContext.getBean(VoucherService.class);
-
-		Map<String, VoucherFactory> map = voucherService.getVoucherFactoryMap();
-		System.out.println(map.get(TypeOption.FIXED.getBeanName()));
-		System.out.println(map.get("fixedAmountVoucherFactory"));
-		System.out.println(map.get("percentAmountVoucherFactory"));
-
-	}
+        Console console = configurableApplicationContext.getBean(Console.class);
+        console.run();
+    }
 
 }
