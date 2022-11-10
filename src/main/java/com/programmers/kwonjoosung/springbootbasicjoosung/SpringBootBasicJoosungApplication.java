0ffd97d96e427ab612eda101977fd65a501c3821
권주성 <99165624+JoosungKwon@users.noreply.github.com>
2022-11-10 16:25:13 +0900

    init: Spring Boot CLI를 통해서 프로젝트 생성
    * 빌드 툴: Maven
    * 자바 버전: 17
    * 프로젝트 버전: v1.0.0

---
+++ src/main/java/com/programmers/kwonjoosung/springbootbasicjoosung/SpringBootBasicJoosungApplication.java
@@ -0,0 +1,13 @@
+package com.programmers.kwonjoosung.springbootbasicjoosung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBasicJoosungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBasicJoosungApplication.class, args);
+	}
+
+}
