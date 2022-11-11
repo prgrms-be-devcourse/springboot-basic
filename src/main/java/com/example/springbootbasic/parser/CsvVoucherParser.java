695141fe3f3a52eea988d76e2184162247550969
hyunseo <goatsfish@gmail.com>
2022-11-11 23:27:12 +0900

    refactor : 바우처 CSV 파싱, 리포지터리 리팩터링

--- src/main/java/com/example/springbootbasic/parser/CsvVoucherParser.java
+++ src/main/java/com/example/springbootbasic/parser/CsvVoucherParser.java
@@ -1,35 +1,31 @@
-//package com.example.springbootbasic.parser;
-//
-//import com.example.springbootbasic.domain.voucher.Voucher;
-//import com.example.springbootbasic.domain.voucher.VoucherFactory;
-//
-//import static com.example.springbootbasic.exception.ParserException.CSV_PARSER_EXCEPTION;
-//import static com.example.springbootbasic.util.CharacterUnit.ENTER;
-//import static com.example.springbootbasic.util.CharacterUnit.SPACE;
-//
-//public class CsvVoucherParser {
-//    private static final int VOUCHER_ID_INDEX = 0;
-//    private static final int VOUCHER_TYPE_INDEX = 1;
-//    private static final int VOUCHER_DISCOUNT_VALUE_INDEX = 2;
-//
-//    public Voucher parseToVoucherFrom(String voucherCsv) {
-//        String[] voucherPiece = voucherCsv.split(SPACE.getUnit());
-//        Long voucherId = Long.parseLong(voucherPiece[VOUCHER_ID_INDEX]);
-//        String voucherType = voucherPiece[VOUCHER_TYPE_INDEX];
-//        Long voucherValue = Long.parseLong(voucherPiece[VOUCHER_DISCOUNT_VALUE_INDEX]);
-//        return VoucherFactory.generate(
-//                voucherId,
-//                voucherValue,
-//                VoucherFactory.findVoucherBy(voucherType)
-//                        .orElseThrow(() -> new IllegalArgumentException(CSV_PARSER_EXCEPTION.getMessage())));
-//    }
-//
-//    public String parseToCsvFrom(Long voucherId, Voucher voucher) {
-//        return voucherId +
-//                SPACE.getUnit() +
-//                voucher.getVoucherEnum().getVoucherType() +
-//                SPACE.getUnit() +
-//                voucher.getDiscountValue() +
-//                ENTER.getUnit();
-//    }
-//}
+package com.example.springbootbasic.parser;
+
+import com.example.springbootbasic.domain.voucher.Voucher;
+import com.example.springbootbasic.domain.voucher.VoucherFactory;
+import com.example.springbootbasic.domain.voucher.VoucherType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static com.example.springbootbasic.util.CharacterUnit.ENTER;
+import static com.example.springbootbasic.util.CharacterUnit.SPACE;
+
+public class CsvVoucherParser {
+
+    private static final Logger logger = LoggerFactory.getLogger(CsvVoucherParser.class);
+    private static final int VOUCHER_ID_INDEX = 0;
+    private static final int VOUCHER_TYPE_INDEX = 1;
+    private static final int VOUCHER_DISCOUNT_VALUE_INDEX = 2;
+
+    public Voucher parseToVoucherFrom(String voucherCsv) {
+        String[] voucherPiece = voucherCsv.split(SPACE.unit());
+        long voucherId = Long.parseLong(voucherPiece[VOUCHER_ID_INDEX].trim());
+        VoucherType voucherType = VoucherType.findVoucherType(voucherPiece[VOUCHER_TYPE_INDEX].trim());
+        long discountValue = Long.parseLong(voucherPiece[VOUCHER_DISCOUNT_VALUE_INDEX].trim());
+        return VoucherFactory.generateVoucher(voucherId, discountValue, voucherType);
+    }
+
+    public String parseToCsvFrom(Voucher voucher) {
+        return String.format("%s %s %s %s",
+                voucher.getVoucherId(), voucher.getVoucherType(), voucher.getDiscountValue(), ENTER.unit());
+    }
+}
--- src/main/java/com/example/springbootbasic/repository/CsvVoucherRepository.java
+++ src/main/java/com/example/springbootbasic/repository/CsvVoucherRepository.java
@@ -1,60 +1,71 @@
-//package com.example.springbootbasic.repository;
-//
-//import com.example.springbootbasic.config.AppConfiguration;
-//import com.example.springbootbasic.domain.voucher.Voucher;
-//import com.example.springbootbasic.parser.CsvVoucherParser;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.annotation.Profile;
-//import org.springframework.stereotype.Repository;
-//
-//import java.io.FileWriter;
-//import java.io.IOException;
-//import java.io.Writer;
-//import java.nio.file.Files;
-//import java.nio.file.Path;
-//import java.nio.file.Paths;
-//import java.util.List;
-//import java.util.stream.Collectors;
-//
-//@Repository
-//@Profile("dev")
-//public class CsvVoucherRepository implements VoucherRepository {
-//    private static Long sequence = 0L;
-//    private final AppConfiguration appConfiguration;
-//    private final CsvVoucherParser csvParser = new CsvVoucherParser();
-//
-//
-//    @Autowired
-//    public CsvVoucherRepository(AppConfiguration appConfiguration) {
-//        this.appConfiguration = appConfiguration;
-//    }
-//
-//    @Override
-//    public Long save(Long voucherId, Voucher voucher) {
-//        try (Writer writer = new FileWriter(appConfiguration.getVoucherCsvResource(), true)) {
-//            writer.write(csvParser.parseToCsvFrom(voucherId, voucher));
-//            writer.flush();
-//        } catch (IOException e) {
-//            throw new RuntimeException(e);
-//        }
-//        return voucherId;
-//    }
-//
-//    @Override
-//    public List<Voucher> findAllVouchers() {
-//        Path csvPath = Paths.get(appConfiguration.getVoucherCsvResource());
-//        try {
-//            List<String> voucherTexts = Files.readAllLines(csvPath);
-//            return voucherTexts.stream()
-//                    .map(csvParser::parseToVoucherFrom)
-//                    .collect(Collectors.toList());
-//        } catch (IOException e) {
-//            throw new RuntimeException(e);
-//        }
-//    }
-//
-//    @Override
-//    public Long getSequence() {
-//        return sequence++;
-//    }
-//}
+package com.example.springbootbasic.repository;
+
+import com.example.springbootbasic.config.AppConfiguration;
+import com.example.springbootbasic.domain.voucher.Voucher;
+import com.example.springbootbasic.domain.voucher.VoucherFactory;
+import com.example.springbootbasic.parser.CsvVoucherParser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Repository;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Repository
+@Profile("dev")
+public class CsvVoucherRepository implements VoucherRepository {
+
+    private static final Logger logger = LoggerFactory.getLogger(CsvVoucherRepository.class);
+    private static long sequence;
+    private final AppConfiguration appConfiguration;
+    private final CsvVoucherParser csvParser = new CsvVoucherParser();
+
+    @Autowired
+    public CsvVoucherRepository(AppConfiguration appConfiguration) {
+        this.appConfiguration = appConfiguration;
+        initSequence();
+    }
+
+    private void initSequence() {
+        List<Voucher> allVouchers = findAllVouchers();
+        if (!allVouchers.isEmpty()) {
+            sequence = allVouchers.get(allVouchers.size() - 1).getVoucherId();
+        }
+    }
+
+    @Override
+    public synchronized Voucher save(Voucher voucher) {
+        try (Writer writer = new FileWriter(appConfiguration.getVoucherCsvResource(), true)) {
+            Voucher generatedVoucher = VoucherFactory.generateVoucher(
+                    ++sequence, voucher.getDiscountValue(), voucher.getVoucherType());
+            writer.write(csvParser.parseToCsvFrom(generatedVoucher));
+            writer.flush();
+        } catch (IOException e) {
+            logger.error("");
+        }
+        return voucher;
+    }
+
+    @Override
+    public List<Voucher> findAllVouchers() {
+        Path csvPath = Paths.get(appConfiguration.getVoucherCsvResource());
+        List<String> voucherTexts = Collections.emptyList();
+        try {
+            voucherTexts = Files.readAllLines(csvPath);
+        } catch (IOException e) {
+            logger.error("");
+        }
+        return voucherTexts.stream()
+                .map(csvParser::parseToVoucherFrom)
+                .collect(Collectors.toList());
+    }
+}
