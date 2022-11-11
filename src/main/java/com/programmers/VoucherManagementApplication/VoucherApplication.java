405ee02c85c69beca19c9e1362427f0049fdfd77
hikarigin99 <hikarigin99@gmail.com>
2022-11-11 22:07:34 +0900

    fix: 주석 변경

--- src/main/java/com/programmers/VoucherManagementApplication/VoucherApplication.java
+++ src/main/java/com/programmers/VoucherManagementApplication/VoucherApplication.java
@@ -9,13 +9,10 @@ import com.programmers.VoucherManagementApplication.service.VoucherService;
 
 public class VoucherApplication{
     public static void main(String[] args) {
-        // IoC
+        // new 연산이 너무 많은 것 같은데 괜찮은가요? 최대한 클라이언트쪽에서 할당하려 하다보니 길어집니다..
         new VoucherController(
                 new Console(new Input(), new Output()),
                 new VoucherService(new MemoryVoucherRepository())
         ).run();
-        // new 연산이 너무 많은 것 같은데 괜찮은가요? 최대한 클라이언트쪽에서 할당하려 하다보니 길어집니다..
-        // 스프링은 직접 주입하지 않아도 IoC
-        // IoC, DI(의존성 주입)
     }
 }
