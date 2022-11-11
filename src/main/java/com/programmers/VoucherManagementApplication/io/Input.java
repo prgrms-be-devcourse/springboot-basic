13bd7a57ba6d598c07edce773fdf8b9fde3565d4
hikarigin99 <hikarigin99@gmail.com>
2022-11-11 21:54:20 +0900

    add: Input, Output Class

---
+++ src/main/java/com/programmers/VoucherManagementApplication/io/Input.java
@@ -0,0 +1,11 @@
+package com.programmers.VoucherManagementApplication.io;
+
+import java.util.Scanner;
+
+public class Input {
+    Scanner scanner = new Scanner(System.in);
+
+    public String input() {
+        return scanner.nextLine();
+    }
+}
