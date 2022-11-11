209cb0fd7142f3e2ae813b7ef38c8646d5408d46
Rose <82176176+geonwoo0215@users.noreply.github.com>
2022-11-11 15:41:34 +0900

    refactor : 팩토리로 바우처 생성

--- src/main/java/org/prgrms/springorder/service/VoucherService.java
+++ src/main/java/org/prgrms/springorder/service/VoucherService.java
@@ -1,15 +1,15 @@
 package org.prgrms.springorder.service;
 
 import java.util.List;
-import java.util.UUID;
 import java.util.stream.Collectors;
 
-import org.prgrms.springorder.domain.Voucher;
-import org.prgrms.springorder.domain.VoucherType;
-import org.prgrms.springorder.repository.VoucherRepository;
-import org.springframework.stereotype.Service;
+import org.prgrms.springorder.domain.VoucherFactory;
+import org.prgrms.springorder.domain.voucher.Voucher;
+import org.prgrms.springorder.domain.voucher.VoucherType;
+import org.prgrms.springorder.repository.voucher.VoucherRepository;
+import org.springframework.stereotype.Component;
 
-@Service
+@Component
 public class VoucherService {
 
 	private final VoucherRepository voucherRepository;
@@ -18,8 +18,8 @@ public class VoucherService {
 		this.voucherRepository = voucherRepository;
 	}
 
-	public void createVoucher(VoucherType voucherType, long value) {
-		Voucher voucher = voucherType.voucherMaker(UUID.randomUUID(), value);
+	public void createVoucher(VoucherType voucherType, double value) {
+		Voucher voucher = VoucherFactory.createVoucher(voucherType, value);
 		voucherRepository.save(voucher);
 	}
 
