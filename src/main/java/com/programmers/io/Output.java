488a5a9f7f2b43c3c0e248aa61c88dd69f3e7c99
Albatross3 <superstring77@gmail.com>
2022-11-12 03:48:25 +0900

    feat : 입력을 담당하는 Input 과 출력을 담당하는 Output 작성

---
+++ src/main/java/com/programmers/io/Output.java
@@ -0,0 +1,45 @@
+package com.programmers.io;
+
+import com.programmers.TypeOfVoucher;
+import com.programmers.voucher.Voucher;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+import java.util.UUID;
+
+@Component
+public class Output {
+
+    public void printDescription() {
+        System.out.println("=== Voucher Program ===");
+        System.out.println("Type create to create a new voucher.");
+        System.out.println("Type list to list all vouchers.");
+        System.out.println("Type exit to exit the program.");
+    }
+
+    public void printTermination() {
+        System.out.println("Exit application");
+    }
+
+    public void printStartOrder() {
+        System.out.println("Type order");
+    }
+
+    public void printSelectVoucher() {
+        System.out.println("1. FixedAmountVoucher");
+        System.out.println("2. PercentDiscountVoucher");
+        System.out.println("Type number to select voucher");
+    }
+
+    public void printSelectDiscount(TypeOfVoucher typeOfVoucher) {
+        switch (typeOfVoucher) {
+            case FixedAmountVoucher -> System.out.println("Type fixed amount");
+            case PercentDiscountVoucher -> System.out.println("Type percent discount");
+        }
+    }
+
+    public void printStorage(Map<UUID, Voucher> history) {
+        history.forEach((key, value) -> System.out.println("ID -> " + key + " Type -> " + value.getType() + " Discount -> " + value.getDiscount()));
+
+    }
+}
