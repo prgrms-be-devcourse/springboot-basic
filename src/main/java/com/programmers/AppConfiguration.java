cbe61d3d2f7f601ac9acbec4e6c27e28a517e33f
Albatross3 <superstring77@gmail.com>
2022-11-12 03:22:15 +0900

    feat : configuration 설정

---
+++ src/main/java/com/programmers/AppConfiguration.java
@@ -0,0 +1,9 @@
+package com.programmers;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan()
+public class AppConfiguration {
+}
