1e479280e17f13b8125582f0116f4f1e5784b7c9
kivv00ng <rldnd2637@naver.com>
2022-11-12 19:16:48 +0900

    ErrorCode status 제거

--- commandline/src/main/java/org/prgrms/kdt/exception/ErrorCode.java
+++ commandline/src/main/java/org/prgrms/kdt/exception/ErrorCode.java
@@ -6,12 +6,11 @@ import lombok.Getter;
 @AllArgsConstructor
 @Getter
 public enum ErrorCode {
-    IS_NOT_NUMBER(400, "숫자가 아닙니다."),
-    NOT_FIND_VOUCHER_TYPE(400, "잘못된 voucher 타입을 입력하셨습니다."),
-    WRONG_RANGE_INPUT(400, "유효한 범위를 벗어났습니다."),
-    WRONG_COMMAND(400, "잘못된 명령어입니다."),
-    IOEXCEPTION(400, "잘못된 입력입니다.");
-
-    private final int status;
+    IS_NOT_NUMBER("숫자가 아닙니다."),
+    NOT_FIND_VOUCHER_TYPE("잘못된 voucher 타입을 입력하셨습니다."),
+    WRONG_RANGE_INPUT("유효한 범위를 벗어났습니다."),
+    WRONG_COMMAND("잘못된 명령어입니다."),
+    IOEXCEPTION("잘못된 입력입니다.");
+    
     private final String message;
 }
