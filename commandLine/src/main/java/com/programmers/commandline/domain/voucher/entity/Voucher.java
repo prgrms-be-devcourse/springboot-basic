c10dd8f31b72d4bc4463f0e7964c36f6bb3714c5
cloudwi <cloudwi@naver.com>
2022-11-12 21:28:33 +0900

    파일 프로젝트명을 바꾸다가 오류가 발생하였습니다. 따라서 다음과 같은 파일을 변경합니다....

--- commandLine/src/main/java/com/programmers/commandline/domain/voucher/entity/Voucher.java
+++
@@ -1,9 +0,0 @@
-package com.programmers.commandline.domain.voucher.entity;
-
-import java.util.UUID;
-
-public interface Voucher {
-    UUID getVoucherId();
-    VoucherType getType();
-    Long getDiscount();
-}
