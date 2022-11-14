08b19d3fb8172c2a3292f1601661c269bb162747
cloudwi <cloudwi@naver.com>
2022-11-14 21:43:00 +0900

    abstract를 사용하는데 미숙함이 있었습니다. 따라서 추가적인 학습으로 필드와 매서드를 정의 했습니다.

--- src/main/java/com/programmers/commandline/domain/voucher/entity/Voucher.java
+++ src/main/java/com/programmers/commandline/domain/voucher/entity/Voucher.java
@@ -3,8 +3,24 @@ package com.programmers.commandline.domain.voucher.entity;
 import java.util.UUID;
 
 public abstract class Voucher {
-    public abstract UUID getVoucherId();
-    public abstract VoucherType getType();
-    public abstract Long getDiscount();
-    public abstract String getAmountUnit();
+    private final UUID voucherId;
+    private final Long discount;
+    private final VoucherType vouchertype;
+    private final String unit;
+
+    protected Voucher(UUID voucherId, Long discount, VoucherType vouchertype, String unit) {
+        this.voucherId = voucherId;
+        this.discount = discount;
+        this.vouchertype = vouchertype;
+        this.unit = unit;
+    }
+
+    public UUID getVoucherId() {
+        return this.voucherId;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Id: %s Type: %s Discount: %d %s", this.voucherId, this.vouchertype, this.discount, this.unit);
+    }
 }
