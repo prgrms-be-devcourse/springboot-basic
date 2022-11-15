e5743e7583e8fbfa1305997beeb85d13a92a204d
tinajeong <migujeong@gmail.com>
2022-11-15 12:17:57 +0900

    [refact] DTO record로 변경

--- src/main/java/org/prgms/springbootbasic/domain/VoucherChoiceDTO.java
+++ src/main/java/org/prgms/springbootbasic/domain/VoucherChoiceDTO.java
@@ -1,22 +1,6 @@
 package org.prgms.springbootbasic.domain;
 
-public class VoucherChoiceDTO {
-    private final VoucherType voucherType;
-    private final long amount;
-
-    public VoucherChoiceDTO(VoucherType voucherType, long amount) {
-        this.voucherType = voucherType;
-        this.amount = amount;
-    }
-
-
-    public VoucherType getVoucherType() {
-        return voucherType;
-    }
-
-    public long getAmount() {
-        return amount;
-    }
+public record VoucherChoiceDTO(VoucherType voucherType, long amount) {
 
     @Override
     public String toString() {
