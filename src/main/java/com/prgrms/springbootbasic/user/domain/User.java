49e9eed170d46a7008859616383adee47493b89a
Hwan Kim <hwankim1941@gmail.com>
2022-11-16 02:37:36 +0900

    [VOU-05] refactor: User 도메인의 필드 접근 제어자 지정

--- src/main/java/com/prgrms/springbootbasic/user/domain/User.java
+++ src/main/java/com/prgrms/springbootbasic/user/domain/User.java
@@ -3,19 +3,19 @@ package com.prgrms.springbootbasic.user.domain;
 import java.util.UUID;
 
 public class User {
-  UUID id;
-  String name;
+    private final UUID id;
+    private final String name;
 
-  public User(UUID id, String name) {
-    this.id = id;
-    this.name = name;
-  }
+    public User(UUID id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 
-  public UUID getId() {
-    return id;
-  }
+    public UUID getId() {
+        return id;
+    }
 
-  public String getName() {
-    return name;
-  }
+    public String getName() {
+        return name;
+    }
 }
