00b3ae063321961f766acba92c28c2daf32ccf04
박세준 <tpwns6693@gmail.com>
2022-11-15 21:28:58 +0900

    test : 데이터베이스로 Embeded mysql 대신 Test Container를 사용 - Embeded mysql의 window 관련 오류가 수정됐음에도 새로운 릴리즈가 없었기에 리드미를 읽어봤더니 DEPRECATED 되었으니 Test Container를 이용하라고 적혀 있었음(UUID_TO_BIN 관련 오류) - embeded mysql의 지원이 끊겼기 때문에, 이를 계속 사용한다면 UUID_TO_BIN 관련 오류를 계속 가진채로 테스트를 진행해야 했기 때문에, Test Container를 적용시키기로 결정 - testContainer 관련 종속성 추가 - mysql java connector 종속성 추가 ( 안해주니까 드라이버를 찾을 수 없다고 에러를 뿜음)

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,12 @@
+create table IF NOT EXISTS customers_demo
+    (
+        customer_id   BINARY(16) PRIMARY KEY,
+        name          varchar(20) NOT NULL,
+        birth_date    date NOT NULL,
+        email         varchar(50) NOT NULL,
+        black_list    boolean NOT NULL DEFAULT false,
+        last_login_at datetime(6) DEFAULT NULL,
+        created_at    datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+        CONSTRAINT unq_user_email UNIQUE (email)
+);
+
--- src/test/java/org/prgrms/kdtspringdemo/customer/repository/CustomerRepositoryTest.java
+++ src/test/java/org/prgrms/kdtspringdemo/customer/repository/CustomerRepositoryTest.java
@@ -1,50 +1,103 @@
 package org.prgrms.kdtspringdemo.customer.repository;
 
-import com.wix.mysql.EmbeddedMysql;
-import org.assertj.core.api.Assertions;
+import com.zaxxer.hikari.HikariDataSource;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.prgrms.kdtspringdemo.customer.Customer;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import javax.sql.DataSource;
-
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-import static com.wix.mysql.EmbeddedMysql.anEmbeddedMysql;
+
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+@Testcontainers
+@SpringJUnitConfig
 class CustomerRepositoryTest {
-//    @Configuration
-//    @ComponentScan(basePackages = {"org.prgrms.kdtspringdemo.customer"})
-//    static class Config{
-//    }
+    @Configuration
+    @ComponentScan(basePackages = {"org.prgrms.kdtspringdemo.customer.repository"})
+    static class Config{
+        @Bean
+//        @ConfigurationProperties(prefix = "")
+        public DataSource dataSource(){
+            return DataSourceBuilder.create()
+                    .url(mySQLContainer.getJdbcUrl())
+                    .username(mySQLContainer.getUsername())
+                    .password(mySQLContainer.getPassword())
+                    .type(HikariDataSource.class)
+                    .build();
+        }
+        @Bean
+        public JdbcTemplate jdbcTemplate(DataSource dataSource){
+            return new JdbcTemplate(dataSource);
+        }
+        @Bean
+        public NamedParameterJdbcTemplate namedParameterJdbcTemplate(JdbcTemplate jdbcTemplate){
+            return new NamedParameterJdbcTemplate(jdbcTemplate);
+        }
+    }
     @Autowired
     CustomerRepository customerRepository;
-
-    EmbeddedMysql embeddedMysql;
-    Customer newCustomer;
-    List<Customer> blackListCustomers;
-    List<Customer> nonBlackLIstCustomers;
+    @Autowired
+    DataSource dataSource;
+    @Container
+    static MySQLContainer mySQLContainer = new MySQLContainer<>("mysql:8").withInitScript("schema.sql");
+    static Customer newCustomer;
+    static List<Customer> blackListCustomers = new ArrayList<>();
+    static List<Customer> nonBlackListCustomers= new ArrayList<>();
 
     @BeforeAll
-    void setup(){
+    static void setup(){
         newCustomer = new Customer(UUID.randomUUID(),
                 "test-user",
                 LocalDate.parse("1999-02-04"),
                 "test-user@gmail.com",
-                LocalDateTime.now().truncatedTo(ChronoUnit.MILLIS),
-                false);
-
+                LocalDateTime.now().truncatedTo(ChronoUnit.MILLIS)
+                );
+        for (int i = 0; i < 10; i++) {
+            Customer nonBlackCustomer = new Customer(UUID.randomUUID(),
+                    "test-user%d".formatted(i),
+                    LocalDate.parse("1999-02-04"),
+                    "test-uesr%d@gmail.com".formatted(i),
+                    LocalDateTime.now().truncatedTo(ChronoUnit.MILLIS)
+            );
+            Customer blackCustomer = new Customer(UUID.randomUUID(),
+                    "test-user%s%d".formatted("-black",i),
+                    LocalDate.parse("1999-02-04"),
+                    "test-uesr%s%d@gmail.com".formatted("-black",i),
+                    LocalDateTime.now().truncatedTo(ChronoUnit.MILLIS),
+                    true
+            );
+            blackListCustomers.add(blackCustomer);
+            nonBlackListCustomers.add(nonBlackCustomer);
+        }
 //        embeddedMysql = anEmbeddedMysql().addSchema("order_mgnt",StringResclassPath("schema.sql")).start()
 
+    }
+    @Test
+    public void testHikariConnectionPool(){
+        assertThat(dataSource.getClass().getName()).isEqualTo("com.zaxxer.hikari.HikariDataSource");
     }
 
     //CREATE
@@ -54,6 +107,24 @@ class CustomerRepositoryTest {
         customerRepository.insert(newCustomer);
         Optional<Customer> addedCustomer = customerRepository.findById(newCustomer.getCustomerId());
         assertThat(addedCustomer.get()).isEqualTo(newCustomer);
+    }
+    @Test
+    @DisplayName("이메일이 같은 고객은 넣지 못한다.")
+    public void insertSameEmail() throws Exception{
+        //given
+
+        //when
+
+        //then
+    }
+    @Test
+    @DisplayName("ID가 같은 고객은 넣지 못한다.")
+    public void insertSameId() throws Exception{
+        //given
+        
+        //when
+        
+        //then
     }
     //READ
     @Test
