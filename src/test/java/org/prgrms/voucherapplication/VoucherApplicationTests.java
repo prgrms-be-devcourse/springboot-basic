5ddf9ddc8dc2a99a2205c3439f463d615cf28818
yeezze <yeeeze22@gmail.com>
2022-11-15 13:58:22 +0900

    test: yaml 내용 변경에 따른 VoucherApplicationTests 수정
    - yaml 설정값 구조 변경

--- src/test/java/org/prgrms/voucherapplication/VoucherApplicationTests.java
+++ src/test/java/org/prgrms/voucherapplication/VoucherApplicationTests.java
@@ -1,13 +1,15 @@
 package org.prgrms.voucherapplication;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.prgrms.voucherapplication.config.VoucherProperties;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.ActiveProfiles;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @ActiveProfiles("prod")
@@ -16,19 +18,20 @@ class VoucherApplicationTests {
 	@Autowired
 	private ApplicationContext applicationContext;
 
-	final String description = "=== Voucher Program ===\n" +
-			"Type exit to exit the program.\n" +
-			"Type create to create a new voucher.\n" +
-			"Type list to list all vouchers.\n" +
-			"Type blacklist to list all customer_blacklist.";
+	@Value("${file-path.voucher}")
+	String voucher;
+	@Value("${file-path.blacklist}")
+	String blacklist;
 
 	@DisplayName("yaml 값 주입받기 성공")
 	@Test
 	void contextLoads() {
 		VoucherProperties bean = applicationContext.getBean(VoucherProperties.class);
 
-		String beanDescription = bean.getDescription();
+		String voucherFilePath = bean.getVoucher();
+		String blacklistFilePath = bean.getBlacklist();
 
-		Assertions.assertThat(beanDescription).isEqualTo(description);
+		assertThat(voucherFilePath).isEqualTo(voucher);
+		assertThat(blacklistFilePath).isEqualTo(blacklist);
 	}
 }
