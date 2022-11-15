3fa75b05577b14fd62424b5c99eb6386ea0d8680
ysk <kim206gh@naver.com>
2022-11-15 14:05:52 +0900

    feat : jdbc 테스트 로그 설정 및 스키마 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -6,11 +6,13 @@ CREATE TABLE IF NOT EXISTS vouchers
 
 );
 
+
 CREATE TABLE IF NOT EXISTS customers
 (
     customer_id   varchar(36) PRIMARY KEY,
     name          varchar(20) NOT NULL,
     email         varchar(50) NOT NULL,
+    customer_status varchar(10) NOT NULL DEFAULT 'NORMAL',
     last_login_at datetime             DEFAULT NULL,
     created_at    datetime    NOT NULL DEFAULT CURRENT_TIMESTAMP(),
     CONSTRAINT unq_user_email UNIQUE (email)
