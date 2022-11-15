5c51012a9fbc908cf1f2601c78d3678cabd32916
bufflect <bndgc@naver.com>
2022-11-15 18:26:23 +0900

    feat: FixedAmountVoucherFactory amount 예외 조건 추가

--- src/main/java/org/prgrms/springbootbasic/factory/FixedAmountVoucherFactory.java
+++ src/main/java/org/prgrms/springbootbasic/factory/FixedAmountVoucherFactory.java
@@ -1,12 +1,14 @@
 package org.prgrms.springbootbasic.factory;
 
-import org.prgrms.springbootbasic.voucher.FixedAmountVoucher;
-import org.prgrms.springbootbasic.voucher.Voucher;
+import org.prgrms.springbootbasic.entity.voucher.FixedAmountVoucher;
+import org.prgrms.springbootbasic.entity.voucher.Voucher;
 
 public class FixedAmountVoucherFactory implements VoucherFactory {
-
     @Override
-    public Voucher createVoucher(long factor) {
-        return new FixedAmountVoucher(factor);
+    public Voucher createVoucher(long quantity) {
+        if (quantity < 0) {
+            throw new IllegalArgumentException("amount > 0");
+        }
+        return new FixedAmountVoucher(quantity);
     }
 }
