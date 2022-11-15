e39a19930615ae84cd04a718ae61d166aff7d32d
Kim Changgyu <kimc980106@naver.com>
2022-11-15 21:51:42 +0900

    Refactor: 바우처 할인 범위 상수화

--- src/main/java/org/prgrms/java/domain/voucher/FixedAmountVoucher.java
+++ src/main/java/org/prgrms/java/domain/voucher/FixedAmountVoucher.java
@@ -5,9 +5,11 @@ import org.prgrms.java.exception.VoucherException;
 import java.util.UUID;
 
 public class FixedAmountVoucher extends Voucher {
+    private static final long MIN_AMOUNT = 0L;
+
     public FixedAmountVoucher(UUID voucherId, long amount) {
         super(voucherId, amount);
-        if (amount <= 0) throw new VoucherException("Voucher discount amount should be positive.");
+        if (amount <= MIN_AMOUNT) throw new VoucherException("Voucher discount amount should be positive.");
     }
 
     @Override
--- src/main/java/org/prgrms/java/domain/voucher/PercentDiscountVoucher.java
+++ src/main/java/org/prgrms/java/domain/voucher/PercentDiscountVoucher.java
@@ -5,10 +5,13 @@ import org.prgrms.java.exception.VoucherException;
 import java.util.UUID;
 
 public class PercentDiscountVoucher extends Voucher {
+    private static final long MAX_AMOUNT = 100L;
+    private static final long MIN_AMOUNT = 0L;
+
     public PercentDiscountVoucher(UUID voucherId, long amount) {
         super(voucherId, amount);
-        if (amount <= 0) throw new VoucherException("Voucher discount percent should be positive.");
-        if (amount > 100) throw new VoucherException("Voucher discount percent cannot be bigger than 100.");
+        if (amount <= MIN_AMOUNT) throw new VoucherException("Voucher discount percent should be positive.");
+        if (amount > MAX_AMOUNT) throw new VoucherException("Voucher discount percent cannot be bigger than 100.");
     }
 
     @Override
