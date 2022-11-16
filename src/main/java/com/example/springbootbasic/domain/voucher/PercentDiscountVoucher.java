27a2e070203e6bce4f86f0652eabd53d36254f56
hyunseo <goatsfish@gmail.com>
2022-11-17 00:09:17 +0900

    feat : 할인율 생성 검증 기능 추가

--- src/main/java/com/example/springbootbasic/domain/voucher/PercentDiscountVoucher.java
+++ src/main/java/com/example/springbootbasic/domain/voucher/PercentDiscountVoucher.java
@@ -1,14 +1,33 @@
 package com.example.springbootbasic.domain.voucher;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.text.MessageFormat;
+
 import static com.example.springbootbasic.domain.voucher.VoucherType.PERCENT_DISCOUNT;
+import static com.example.springbootbasic.exception.PercentDiscountVoucherExceptionMessage.PERCENT_DISCOUNT_RANGE_EXCEPTION;
 
 public class PercentDiscountVoucher extends Voucher {
 
+    private static final Logger logger = LoggerFactory.getLogger(PercentDiscountVoucher.class);
+    private static final Long MAX_DISCOUNT_PERCENT = 100L;
+    private static final Long MIN_DISCOUNT_PERCENT = 0L;
+
     public PercentDiscountVoucher(Long voucherId, Long discountValue) {
         super(voucherId, discountValue, PERCENT_DISCOUNT);
+        validateDiscountValue(discountValue);
     }
 
     public VoucherType getVoucherType() {
         return PERCENT_DISCOUNT;
+    }
+
+    private void validateDiscountValue(Long discountValue) {
+        if (MIN_DISCOUNT_PERCENT > discountValue || discountValue > MAX_DISCOUNT_PERCENT) {
+            logger.error("Fail - {}", MessageFormat.format(PERCENT_DISCOUNT_RANGE_EXCEPTION.getMessage(), MIN_DISCOUNT_PERCENT, MAX_DISCOUNT_PERCENT));
+            throw new IllegalArgumentException(
+                    MessageFormat.format(PERCENT_DISCOUNT_RANGE_EXCEPTION.getMessage(), MIN_DISCOUNT_PERCENT, MAX_DISCOUNT_PERCENT));
+        }
     }
 }
