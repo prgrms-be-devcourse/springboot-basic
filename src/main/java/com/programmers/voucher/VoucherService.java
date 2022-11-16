269ff385661bbfd50f9effccf33732b6715597e8
Albatross3 <superstring77@gmail.com>
2022-11-17 01:03:00 +0900

    refactor : VoucherService 는 voucher 저장과 조회만 담당하게 하였습니다

--- src/main/java/com/programmers/voucher/VoucherService.java
+++ src/main/java/com/programmers/voucher/VoucherService.java
@@ -1,7 +1,5 @@
 package com.programmers.voucher;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
@@ -11,24 +9,17 @@ import java.util.UUID;
 public class VoucherService {
     private final VoucherRepository voucherRepository;
 
-    private static final Logger logger = LoggerFactory.getLogger(VoucherService.class);
     public VoucherService(VoucherRepository voucherRepository) {
         this.voucherRepository = voucherRepository;
     }
 
-    public void createFixedAmountVoucher(long discountAmount) {
-        Voucher voucher = new FixedAmountVoucher(UUID.randomUUID(), discountAmount);
-        logger.info("FixedAmountVoucher 가 생성되었습니다.");
-        voucherRepository.insert(voucher);
-    }
 
-    public void createPercentDiscountVoucher(long discountPercent) {
-        Voucher voucher = new PercentDiscountVoucher(UUID.randomUUID(), discountPercent);
-        logger.info("PercentDiscountVoucher 가 생성되었습니다.");
+    public void saveVoucher(Voucher voucher) {
         voucherRepository.insert(voucher);
     }
 
     public Map<UUID, Voucher> findAll() {
+
         return voucherRepository.findAll();
     }
 }
