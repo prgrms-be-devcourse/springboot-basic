0b618cb944ff7524751715358a26e19b3108182f
yewon9609 <dodnjs1241@naver.com>
2022-11-16 17:08:47 +0900

    refactor: 검증로직 분리

--- src/main/java/org.prgrms/voucher/discountType/DiscountAmount.java
+++ src/main/java/org.prgrms/voucher/discountType/DiscountAmount.java
@@ -4,25 +4,13 @@ public class DiscountAmount implements Amount {
 
   private final long value;
 
-  public DiscountAmount(String input) {
-    this.value = getTarget(input);
+  public DiscountAmount(long input) {
+    validateAmount(input);
+    this.value = input;
   }
 
   public long getValue() {
     return value;
-  }
-
-  private long getTarget(String input) {
-    long target;
-    try {
-      target = Long.parseLong(input);
-    } catch (NumberFormatException e) {
-      throw new NumberFormatException("Only numbers can be entered");
-    }
-    if (target < 1) {
-      throw new IllegalStateException("Wrong discount amount range");
-    }
-    return target;
   }
 
 }
