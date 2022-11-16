61ee2fd9e224c885c2f4a4aecb214de19976eac6
fortune00 <sinyoungbok99@gmail.com>
2022-11-16 21:44:40 +0900

    test: 바우처 서비스와 컨트롤러 테스트

---
+++ src/test/java/org/prgrms/kdt/service/VoucherServiceTest.java
@@ -0,0 +1,70 @@
+package org.prgrms.kdt.service;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.prgrms.kdt.domain.Voucher;
+import org.prgrms.kdt.domain.VoucherType;
+import org.prgrms.kdt.repository.VoucherRepository;
+import org.prgrms.kdt.service.dto.CreateVoucherDto;
+
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class VoucherServiceTest {
+
+    private VoucherService voucherService;
+    private VoucherRepository voucherRepository;
+
+    @BeforeEach
+    void setUp() {
+        this.voucherRepository = mock(VoucherRepository.class);
+        this.voucherService = new VoucherService(voucherRepository);
+    }
+
+    @Test
+    @DisplayName("[성공] 바우처 저장하기")
+    void createVoucherTest() {
+        when(voucherRepository.save(any())).thenReturn(true);
+
+        VoucherType voucherType = VoucherType.PERCENT_DISCOUNT_VOUCHER;
+        double discountAmount = 10;
+        CreateVoucherDto dto = new CreateVoucherDto(voucherType, discountAmount);
+
+        boolean result = voucherService.createVoucher(dto);
+
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    @DisplayName("[실패] 바우처 저장 실패한 경우")
+    void createVoucherTest_fail() {
+        when(voucherRepository.save(any())).thenReturn(false);
+
+        VoucherType voucherType = VoucherType.PERCENT_DISCOUNT_VOUCHER;
+        double discountAmount = 10;
+        CreateVoucherDto dto = new CreateVoucherDto(voucherType, discountAmount);
+
+        boolean result = voucherService.createVoucher(dto);
+
+        Assertions.assertFalse(result);
+    }
+
+    @Test
+    @DisplayName("[성공] 모든 바우처 가져오기")
+    void getAllVoucherTest() {
+        VoucherType voucherType = VoucherType.PERCENT_DISCOUNT_VOUCHER;
+        double discountAmount = 10;
+        Voucher voucher = new Voucher(voucherType, discountAmount);
+
+        when(voucherRepository.getAll()).thenReturn(List.of(voucher));
+
+        List<Voucher> allVouchers = voucherService.getAllVouchers();
+
+        Assertions.assertEquals(allVouchers.size(), 1);
+    }
+}
