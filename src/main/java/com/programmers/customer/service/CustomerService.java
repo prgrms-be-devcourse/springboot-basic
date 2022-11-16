c4b9f4a32d5cd0cc8f9859d5ac647b2798de1158
김기서 <5rne@naver.com>
2022-11-16 19:39:55 +0900

    feat: - [CustomerService] 구현 - [Customer] 클래스에 wallet 필드 추가

---
+++ src/main/java/com/programmers/customer/service/CustomerService.java
@@ -0,0 +1,21 @@
+package com.programmers.customer.service;
+
+import com.programmers.customer.Customer;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface CustomerService {
+    void join(Customer customer);
+
+    Customer findById(UUID customerId);
+
+    Customer findByName(String name);
+
+    Customer findByEmail(String email);
+
+    Customer update(Customer customer);
+
+    List<Customer> findAll();
+}
+
---
+++ src/main/java/com/programmers/customer/service/CustomerServiceImpl.java
@@ -0,0 +1,56 @@
+package com.programmers.customer.service;
+
+import com.programmers.customer.Customer;
+import com.programmers.customer.repository.CustomerRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@Transactional(readOnly = true)
+public class CustomerServiceImpl implements CustomerService {
+    private final CustomerRepository customerRepository;
+
+    public CustomerServiceImpl(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    @Override
+    @Transactional
+    public void join(Customer customer) {
+        customerRepository.insert(customer);
+    }
+
+    @Override
+    @Transactional
+    public Customer update(Customer customer) {
+        return customerRepository.update(customer);
+    }
+
+    @Override
+    public Customer findById(UUID customerId) {
+        return customerRepository.findById(customerId)
+                .orElseThrow(() -> new RuntimeException("사용자를 찾을 수 없습니다."));
+    }
+
+    @Override
+    public Customer findByName(String name) {
+        return customerRepository.findByName(name)
+                .orElseThrow(() -> new RuntimeException("사용자를 찾을 수 없습니다."));
+    }
+
+    @Override
+    public Customer findByEmail(String email) {
+        return customerRepository.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("사용자를 찾을 수 없습니다."));
+
+    }
+
+    @Override
+    public List<Customer> findAll() {
+        return customerRepository.findAll();
+    }
+
+}
