345c4d62559d2301ecf68574c57fad2470e3c46f
yewon9609 <dodnjs1241@naver.com>
2022-11-16 17:10:56 +0900

    refactor: enum VoucherType을 Voucher팩토리로 사용하도록 수정

--- src/main/java/org.prgrms/voucher/voucherType/VoucherType.java
+++ src/main/java/org.prgrms/voucher/voucherType/VoucherType.java
@@ -1,6 +1,7 @@
 package org.prgrms.voucher.voucherType;
 
 import java.util.UUID;
+import java.util.function.BiFunction;
 import java.util.function.Function;
 import java.util.stream.Stream;
 import org.prgrms.exception.NoSuchVoucherTypeException;
@@ -10,40 +11,50 @@ import org.prgrms.voucher.discountType.Amount;
 
 public enum VoucherType {
 
-  FIXED("1", (Amount amount) -> new FixedAmountVoucher(UUID.randomUUID(), amount),
-      DiscountAmount::new),
-  PERCENT("2", (Amount amount) -> new PercentDiscountVoucher(UUID.randomUUID(), amount),
-      DiscountRate::new);
+  FIXED(1, FixedAmountVoucher::new, DiscountAmount::new),
+  PERCENT(2, PercentDiscountVoucher::new, DiscountRate::new);
 
-  private final String type;
+  private final long type;
 
-  private final Function<Amount, Voucher> voucher;
+  private final BiFunction<UUID ,Amount, Voucher> voucher;
 
-  private final Function<String, Amount> amount;
+  private final Function<Long , Amount> amount;
 
-  VoucherType(String type, Function<Amount, Voucher> voucher,
-      Function<String, Amount> amount) {
+  VoucherType(int type, BiFunction<UUID, Amount, Voucher> voucher,
+      Function<Long, Amount> amount) {
     this.type = type;
     this.voucher = voucher;
     this.amount = amount;
   }
 
+  public static VoucherType of(long choice) {
+    return Stream.of(VoucherType.values())
+        .filter(voucher -> voucher.type == choice)
+        .findAny()
+        .orElseThrow(() -> new NoSuchVoucherTypeException(choice));
+  }
+
   public static VoucherType of(String choice) {
     return Stream.of(VoucherType.values())
-        .filter(voucher -> voucher.type.equals(choice))
+        .filter(voucher -> choice.contains(voucher.name()))
         .findAny()
         .orElseThrow(() -> new NoSuchVoucherTypeException(choice));
   }
 
   public Voucher generateVoucher(Amount discount) {
-    return this.voucher.apply(discount);
+    return this.voucher.apply(UUID.randomUUID(),discount);
   }
 
-  public Amount generateAmount(String value) {
+  public Voucher generateFileVoucher(UUID id, Amount discount) {
+    return this.voucher.apply(id, discount);
+  }
+
+  public Amount generateAmount(long value) {
     return this.amount.apply(value);
   }
 
-  public String getType() {
+  public long getType() {
     return type;
   }
+
 }
