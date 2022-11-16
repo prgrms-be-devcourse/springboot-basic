5a2ca464889f49748406123fa0e775cc296888db
Albatross3 <superstring77@gmail.com>
2022-11-17 01:05:27 +0900

    refactor : 생성자에서 예외를 던지게 하고 type 변수를 없애고 의미 있는 변수 값의 이름을 할당하였습니다

--- src/main/java/com/programmers/voucher/PercentDiscountVoucher.java
+++ src/main/java/com/programmers/voucher/PercentDiscountVoucher.java
@@ -1,17 +1,27 @@
 package com.programmers.voucher;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.UUID;
 
 public class PercentDiscountVoucher implements Voucher{
 
-    private final String type = "PercentDiscount";
     private final UUID voucherID;
     private final long discountPercent;
 
+    private final long MIN_PERCENT = 0;
+    private final long MAX_PERCENT = 100;
+
+    private static final Logger logger = LoggerFactory.getLogger(PercentDiscountVoucher.class);
+
     public PercentDiscountVoucher(UUID voucherID, long discountPercent) {
-        if(discountPercent<0 || discountPercent>100) throw new IllegalArgumentException();
+        if (discountPercent < MIN_PERCENT || discountPercent > MAX_PERCENT) {
+            throw new IllegalArgumentException("잘못된 범위의 할인율입니다.");
+        }
         this.voucherID = voucherID;
         this.discountPercent = discountPercent;
+        logger.info("PercentDiscountVoucher 가 생성되었습니다.");
     }
 
     @Override
@@ -23,9 +33,9 @@ public class PercentDiscountVoucher implements Voucher{
     public long discount(long beforeDiscount) {
         return beforeDiscount * (100 - discountPercent) / 100;
     }
-    
+
     @Override
     public String toString() {
-        return "Voucher ID -> " + voucherID + " Voucher Type -> " + type + " Discount -> " + discountPercent;
+        return "Voucher ID -> " + voucherID + " Voucher Type -> PercentDiscount" + " Discount -> " + discountPercent;
     }
 }
