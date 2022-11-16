7562b5abdb12e99f588da38384c3c688d5585a14
Albatross3 <superstring77@gmail.com>
2022-11-17 01:01:31 +0900

    refactor : TypeOfVoucher Enum 클래스에서 생성을 책임, 예외 던지기

--- src/main/java/com/programmers/TypeOfVoucher.java
+++ src/main/java/com/programmers/TypeOfVoucher.java
@@ -1,25 +1,39 @@
 package com.programmers;
 
+import com.programmers.voucher.FixedAmountVoucher;
+import com.programmers.voucher.PercentDiscountVoucher;
+import com.programmers.voucher.Voucher;
+
 import java.util.Arrays;
-import java.util.Optional;
+import java.util.UUID;
+import java.util.function.Function;
 
 public enum TypeOfVoucher {
-    FIXED_AMOUNT_VOUCHER("1"),
-    PERCENT_DISCOUNT_VOUCHER("2"),
-    ERROR_VOUCHER("0");
+    FIXED_AMOUNT_VOUCHER("1","Insert amount of fixed discount",(discount)->new FixedAmountVoucher(UUID.randomUUID(),discount)),
+    PERCENT_DISCOUNT_VOUCHER("2","Insert ratio of discount" ,(discount) -> new PercentDiscountVoucher(UUID.randomUUID(), discount));
+
 
     private final String type;
-    TypeOfVoucher(String type) {
+    private final String  guideMessage;
+    private final Function<Long, Voucher> function;
+    TypeOfVoucher(String type,String guideMessage, Function<Long,Voucher> function) {
         this.type = type;
+        this.guideMessage = guideMessage;
+        this.function = function;
+    }
+
+    public static String getGuideMessage(TypeOfVoucher typeOfVoucher) {
+        return typeOfVoucher.guideMessage;
+    }
+
+    public static Voucher createVoucher(TypeOfVoucher typeOfVoucher, long discount) {
+        return typeOfVoucher.function.apply(discount);
     }
 
     public static TypeOfVoucher getType(String typeNumber) {
-        Optional<TypeOfVoucher> optional = Arrays.stream(TypeOfVoucher.values())
-                .filter(o -> o.type.equals(typeNumber))
-                .findFirst();
-        if (optional.isEmpty()) {
-            return ERROR_VOUCHER;
-        }
-        return optional.get();
+        return Arrays.stream(TypeOfVoucher.values())
+                .filter(typeOfVoucher -> typeOfVoucher.type.equals(typeNumber))
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("존재하지 않는 바우처입니다."));
     }
 }
