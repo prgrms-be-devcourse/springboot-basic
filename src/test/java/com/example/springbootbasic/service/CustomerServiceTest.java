e73f93402efd54484459d4271509602a4f1c49ed
hyunseo <goatsfish@gmail.com>
2022-11-17 00:21:46 +0900

    test : 고객 서비스 조회 성공 테스트

---
+++ src/test/java/com/example/springbootbasic/service/CustomerServiceTest.java
@@ -0,0 +1,37 @@
+package com.example.springbootbasic.service;
+
+import com.example.springbootbasic.domain.customer.Customer;
+import com.example.springbootbasic.repository.CustomerRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.TestInstance;
+import org.mockito.Mockito;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.jupiter.api.TestInstance.Lifecycle;
+
+@TestInstance(Lifecycle.PER_CLASS)
+@ActiveProfiles("dev")
+class CustomerServiceTest {
+
+    private final CustomerService customerService;
+
+    public CustomerServiceTest() {
+        CustomerRepository customerRepository = Mockito.mock(CustomerRepository.class);
+        this.customerService = new CustomerService(customerRepository);
+    }
+
+    @RepeatedTest(100)
+    @DisplayName("다수 블랙리스트 고객 검색 성공")
+    void whenFindBlackCustomersThenSuccessTest() {
+        // when
+        List<Customer> allBlackCustomers = customerService.findAllBlackCustomers();
+
+        // then
+        assertThat(allBlackCustomers, notNullValue());
+    }
+}
