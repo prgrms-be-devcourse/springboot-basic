dd77b2cea4135b2115890cf5bf75a00c12382d35
hyunseo <goatsfish@gmail.com>
2022-11-17 00:16:42 +0900

    feat : 콘솔, 손님 상태, 파싱, 할인율, 고정할인, 바우처 예외 메시지 추가

---
+++ src/main/java/com/example/springbootbasic/exception/FixedAmountVoucherExceptionMessage.java
@@ -0,0 +1,16 @@
+package com.example.springbootbasic.exception;
+
+public enum FixedAmountVoucherExceptionMessage {
+
+    FIXED_AMOUNT_DISCOUNT_RANGE_EXCEPTION("고정 할인의 가능한 금액 범위는 {0}원 ~ {1}원 입니다.");
+
+    private final String message;
+
+    FixedAmountVoucherExceptionMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
