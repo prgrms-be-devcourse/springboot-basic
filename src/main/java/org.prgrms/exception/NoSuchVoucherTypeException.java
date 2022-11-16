cc754df62a506753a4daba1e593792d32f8b4f7b
yewon9609 <dodnjs1241@naver.com>
2022-11-16 17:10:32 +0900

    feat: 파라미터값에 따른 오버로딩

--- src/main/java/org.prgrms/exception/NoSuchVoucherTypeException.java
+++ src/main/java/org.prgrms/exception/NoSuchVoucherTypeException.java
@@ -2,6 +2,10 @@ package org.prgrms.exception;
 
 public class NoSuchVoucherTypeException extends RuntimeException {
 
+  public NoSuchVoucherTypeException(long voucherType) {
+    super("number ' " + voucherType + " ' does not exist. Please re-enter");
+  }
+
   public NoSuchVoucherTypeException(String voucherType) {
     super("' " + voucherType + " ' does not exist. Please re-enter");
   }
