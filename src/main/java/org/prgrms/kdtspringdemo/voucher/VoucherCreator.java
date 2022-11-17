2a22a9dd8cb89b7311f683303e8cb76b4b1bbaaf
박세준 <tpwns6693@gmail.com>
2022-11-17 14:42:35 +0900

    refactor : creator의 switch case문을 함수형 인터페이스로 Enum 내부로 넣음 - 바우쳐를 생성하는것을 Enum에서 만들도록 변경했습니다.

--- src/main/java/org/prgrms/kdtspringdemo/voucher/VoucherCreator.java
+++ src/main/java/org/prgrms/kdtspringdemo/voucher/VoucherCreator.java
@@ -1,7 +1,5 @@
 package org.prgrms.kdtspringdemo.voucher;
 
-import org.prgrms.kdtspringdemo.voucher.model.FixedAmountVoucher;
-import org.prgrms.kdtspringdemo.voucher.model.PercentDiscountVoucher;
 import org.prgrms.kdtspringdemo.voucher.model.Voucher;
 import org.prgrms.kdtspringdemo.voucher.model.VoucherType;
 import org.slf4j.Logger;
@@ -15,21 +13,13 @@ public class VoucherCreator {
     private static final Logger logger = LoggerFactory.getLogger(VoucherCreator.class);
 
     public Voucher createVoucher(VoucherType voucherType, Long percentOrAmount) throws IllegalArgumentException {
-
-        Voucher newVoucher = switch (voucherType) {
-            case FIXED -> new FixedAmountVoucher(UUID.randomUUID(), percentOrAmount);
-            case PERCENT -> new PercentDiscountVoucher(UUID.randomUUID(), percentOrAmount);
-        };
+        Voucher newVoucher = voucherType.newInstance(UUID.randomUUID(), percentOrAmount);
         logger.info("바우쳐 생성 성공. 타입 :{} 값 :{}", voucherType.name(), percentOrAmount);
         return newVoucher;
     }
 
     public Voucher createVoucher(UUID voucherId, VoucherType voucherType, Long percentOrAmount) throws IllegalArgumentException {
-
-        Voucher newVoucher = switch (voucherType) {
-            case FIXED -> new FixedAmountVoucher(voucherId, percentOrAmount);
-            case PERCENT -> new PercentDiscountVoucher(voucherId, percentOrAmount);
-        };
+        Voucher newVoucher = voucherType.newInstance(voucherId, percentOrAmount);
         logger.info("바우쳐 생성 성공. 타입 :{} 값 :{}", voucherType.name(), percentOrAmount);
         return newVoucher;
     }
--- src/main/java/org/prgrms/kdtspringdemo/voucher/model/VoucherType.java
+++ src/main/java/org/prgrms/kdtspringdemo/voucher/model/VoucherType.java
@@ -1,7 +1,16 @@
 package org.prgrms.kdtspringdemo.voucher.model;
 
+import java.util.UUID;
+import java.util.function.BiFunction;
+
 public enum VoucherType {
-    FIXED, PERCENT;
+    FIXED(FixedAmountVoucher::new), PERCENT(PercentDiscountVoucher::new);
+
+    final BiFunction<UUID, Long, Voucher> constructor;
+
+    VoucherType(BiFunction<UUID, Long, Voucher> constructor) {
+        this.constructor = constructor;
+    }
 
     public static VoucherType getTypeByName(String string) throws IllegalArgumentException {
         try {
@@ -9,5 +18,9 @@ public enum VoucherType {
         } catch (IllegalArgumentException e) {
             throw new IllegalArgumentException("잘못된 바우쳐 타입 입니다.");
         }
+    }
+
+    public Voucher newInstance(UUID id, Long value) {
+        return this.constructor.apply(id, value);
     }
 }
