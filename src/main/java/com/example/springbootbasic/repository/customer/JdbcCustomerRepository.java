b811a8cefb842dde31f57d5937cdcb6a42beca8c
hyunseo <goatsfish@gmail.com>
2022-11-17 15:22:11 +0900

    feat : DB를 이용한 customer 전체 검색, customer 타입별 전체 검색 기능 추가

---
+++ src/main/java/com/example/springbootbasic/repository/customer/JdbcCustomerRepository.java
@@ -0,0 +1,57 @@
+package com.example.springbootbasic.repository.customer;
+
+import com.example.springbootbasic.domain.customer.Customer;
+import com.example.springbootbasic.domain.customer.CustomerStatus;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Profile;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.text.MessageFormat;
+import java.util.Collections;
+import java.util.List;
+
+@Repository
+@Profile("jdbc")
+public class JdbcCustomerRepository implements CustomerRepository {
+
+    private static final Logger logger = LoggerFactory.getLogger(JdbcCustomerRepository.class);
+    private static final String FIND_ALL_CUSTOMERS_SQL = "SELECT * FROM CUSTOMER";
+    private static final String FIND_ALL_BLACK_CUSTOMERS_SQL = "SELECT * FROM CUSTOMER WHERE customer_status = {0}";
+
+    private final NamedParameterJdbcTemplate jdbcTemplate;
+
+    public JdbcCustomerRepository(NamedParameterJdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    private static RowMapper<Customer> customerRowMapper = (resultSet, i) -> {
+        long customerId = resultSet.getLong("customer_id");
+        String customerStatus = resultSet.getString("customer_status");
+        return new Customer(customerId, CustomerStatus.of(customerStatus));
+    };
+
+    @Override
+    public List<Customer> findAllCustomers() {
+        try {
+            return jdbcTemplate.query(FIND_ALL_CUSTOMERS_SQL, customerRowMapper);
+        } catch (EmptyResultDataAccessException e) {
+            logger.error("Fail - {}", e.getMessage());
+            return Collections.emptyList();
+        }
+    }
+
+    @Override
+    public List<Customer> findCustomersByStatus(CustomerStatus status) {
+        try {
+            return jdbcTemplate.query(MessageFormat.format(FIND_ALL_BLACK_CUSTOMERS_SQL, ":customerStatus"),
+                    Collections.singletonMap("customerStatus", status.getType()), customerRowMapper);
+        } catch (EmptyResultDataAccessException e) {
+            logger.error("Fail - {}", e.getMessage());
+            return Collections.emptyList();
+        }
+    }
+}
