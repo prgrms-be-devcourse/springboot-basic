2560da8d8fde89bf034f11cf41094a96fb3bb3c5
hikarigin99 <hikarigin99@gmail.com>
2022-11-17 22:19:18 +0900

    [FIX] VoucherController swich delete

--- src/main/java/com/programmers/VoucherManagementApplication/controller/VoucherController.java
+++ src/main/java/com/programmers/VoucherManagementApplication/controller/VoucherController.java
@@ -1,2 +1,28 @@
-package com.programmers.VoucherManagementApplication.controller;public class VoucherController {
+package com.programmers.VoucherManagementApplication.controller;
+
+import com.programmers.VoucherManagementApplication.vo.Amount;
+import com.programmers.VoucherManagementApplication.vo.VoucherType;
+import com.programmers.VoucherManagementApplication.service.VoucherService;
+import com.programmers.VoucherManagementApplication.voucher.Voucher;
+import org.springframework.stereotype.Controller;
+
+import java.util.Map;
+import java.util.UUID;
+
+@Controller
+public class VoucherController {
+
+    private final VoucherService voucherService;
+
+    public VoucherController(VoucherService voucherService) {
+        this.voucherService = voucherService;
+    }
+
+    public Voucher create(VoucherType voucherType, Amount amount) {
+        return voucherService.create(voucherType, amount);
+    }
+
+    public Map<UUID, Voucher> findAll() {
+        return voucherService.findAll();
+    }
 }
