c94a8d948c590a7d7ad7ecb8ea0aa91a9d240d0a
Lee Suyoung <78334910+twotwobread@users.noreply.github.com>
2022-11-18 03:12:42 +0900

    fix: 테스트를 MatcherAssert 이용해서 개선하다.

--- src/test/java/org/prgrms/kdt/repository/voucher/MemoryVoucherRepositoryTest.java
+++ src/test/java/org/prgrms/kdt/repository/voucher/MemoryVoucherRepositoryTest.java
@@ -3,7 +3,6 @@ package org.prgrms.kdt.repository.voucher;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.prgrms.kdt.dao.entity.voucher.FixedAmountVoucher;
 import org.prgrms.kdt.dao.entity.voucher.Voucher;
 import org.prgrms.kdt.dao.repository.voucher.MemoryVoucherRepository;
 import org.prgrms.kdt.dao.repository.voucher.VoucherRepository;
@@ -14,6 +13,9 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
 class MemoryVoucherRepositoryTest {
+
+    private static final String FIXED_TYPE = "FixedAmountVoucher";
+    private static final String PERCENT_TYPE = "PercentDiscountVoucher";
 
     VoucherRepository voucherRepository = new MemoryVoucherRepository();
 
@@ -26,7 +28,7 @@ class MemoryVoucherRepositoryTest {
     @DisplayName("저장된 바우처를 voucherId를 이용해 찾는 기능 검증")
     void voucherId_이용한_바우처찾기() {
         // given
-        Voucher voucher = new FixedAmountVoucher(UUID.randomUUID(), 1000L);
+        Voucher voucher = new Voucher(UUID.randomUUID(), "1000", FIXED_TYPE);
 
         // when
         voucherRepository.insert(voucher);
@@ -40,7 +42,7 @@ class MemoryVoucherRepositoryTest {
     @DisplayName("바우처가 제대로 들어갔는지 repository 내부 바우처의 갯수로 검증")
     void insert() {
         // given
-        Voucher voucher = new FixedAmountVoucher(UUID.randomUUID(), 1000L);
+        Voucher voucher = new Voucher(UUID.randomUUID(), "1000", FIXED_TYPE);
 
         // when
         voucherRepository.insert(voucher);
@@ -54,9 +56,9 @@ class MemoryVoucherRepositoryTest {
     @DisplayName("저장된 바우처들이 제대로 나오는지 바우처 갯수를 이용한 검증")
     void getAllStoredVoucher() {
         // given
-        voucherRepository.insert(new FixedAmountVoucher(UUID.randomUUID(), 1000L));
-        voucherRepository.insert(new FixedAmountVoucher(UUID.randomUUID(), 2000L));
-        voucherRepository.insert(new FixedAmountVoucher(UUID.randomUUID(), 3000L));
+        voucherRepository.insert(new Voucher(UUID.randomUUID(), "1000", FIXED_TYPE));
+        voucherRepository.insert(new Voucher(UUID.randomUUID(), "2000", FIXED_TYPE));
+        voucherRepository.insert(new Voucher(UUID.randomUUID(), "3000", FIXED_TYPE));
 
         // when
         int getVoucherCount = voucherRepository.getAllStoredVoucher().size();
