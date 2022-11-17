21c184ed503f939ec91327442324f089bf766c8d
Rose <82176176+geonwoo0215@users.noreply.github.com>
2022-11-17 17:42:58 +0900

    refactor : Customer 필드 변경, Customers 테이블 추가

--- src/main/java/org/prgrms/springorder/domain/customer/Customer.java
+++ src/main/java/org/prgrms/springorder/domain/customer/Customer.java
@@ -1,21 +1,29 @@
 package org.prgrms.springorder.domain.customer;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 public class Customer {
 
-	private final UUID uuid;
+	private final UUID customerId;
 	private final String name;
+	private final String email;
+	private final LocalDateTime createdAt;
 	private final CustomerType customerType;
 
-	public Customer(UUID uuid, String name, CustomerType customerType) {
-		this.uuid = uuid;
+	public Customer(UUID customerId, String name, String email, LocalDateTime createdAt,
+		CustomerType customerType) {
+		this.customerId = customerId;
 		this.name = name;
+		this.email = email;
+		this.createdAt = createdAt;
 		this.customerType = customerType;
 	}
 
 	@Override
 	public String toString() {
-		return String.format("ID : %s Name : %s", uuid, name);
+		return String.format("ID : %s Name : %s", customerId, name);
 	}
+
+
 }
