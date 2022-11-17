8f85f070ce1ec521e57d7bb9749249fb33abe719
Lee Suyoung <78334910+twotwobread@users.noreply.github.com>
2022-11-18 03:11:43 +0900

    fix: FileVoucherRepository에 콜백 함수들을 추가하여 코드를 개선하다.

--- src/main/java/org/prgrms/kdt/dao/repository/voucher/FileVoucherRepository.java
+++ src/main/java/org/prgrms/kdt/dao/repository/voucher/FileVoucherRepository.java
@@ -3,13 +3,15 @@ package org.prgrms.kdt.dao.repository.voucher;
 
 import org.prgrms.kdt.dao.entity.voucher.Voucher;
 import org.prgrms.kdt.dao.entity.voucher.VoucherFactory;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.prgrms.kdt.exception.io.WrongInputDataException;
+import org.prgrms.kdt.exception.io.WrongOutputDataException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Repository;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -25,93 +27,76 @@ import java.util.UUID;
 @Profile({"dev", "test"})
 public class FileVoucherRepository implements VoucherRepository {
 
-    private static final Logger logger = LoggerFactory.getLogger(FileVoucherRepository.class);
+    private static final List<Voucher> voucherRepository = new ArrayList<>();
     private final VoucherFactory voucherFactory;
 
-    private final String filePath;
     private final ClassPathResource resource;
 
     public FileVoucherRepository(VoucherFactory voucherFactory, @Value("${kdt.log-file}") String filePath) {
         this.voucherFactory = voucherFactory;
-        this.filePath = filePath;
         this.resource = new ClassPathResource(filePath);
     }
 
+    @PostConstruct
+    void setUp() {
+        try {
+            for (String content : getAllLineFromFile()) {
+                Voucher voucher = getVoucherStatus(parse(content));
+                voucherRepository.add(voucher);
+            }
+        } catch (IOException e) {
+            throw new WrongOutputDataException("바우처 파일을 읽어오는데 실패했습니다.", e);
+        }
+    }
+
+    private List<String> getAllLineFromFile() throws IOException {
+        Path pathResource = Paths.get(resource.getURI());
+        return Files.readAllLines(pathResource);
+    }
+
+    private Voucher getVoucherStatus(String[] fields) {
+        String voucherId = fields[0];
+        String discountValue = fields[1];
+        String voucherType = fields[2];
+
+        return voucherFactory.createVoucher(UUID.fromString(voucherId), discountValue, voucherType);
+    }
+
+    private String[] parse(String line) {
+        return line.split(",");
+    }
+
+    @PreDestroy
+    void exit() {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(resource.getFile(), false))) {
+            for (Voucher voucher : voucherRepository) {
+                bw.write(voucher.getVoucherId().toString() + "," + voucher.getDiscountAmount() + "," + voucher.getVoucherType());
+                bw.newLine();
+            }
+        } catch (IOException e) {
+            throw new WrongInputDataException("바우처를 삽입하기에 실패했습니다.", e);
+        }
+    }
 
     @Override
     public Optional<Voucher> findById(UUID voucherId) {
-        try {
-            Path pathResource = Paths.get(resource.getURI());
-            List<String> repositoryAllContent = Files.readAllLines(pathResource);
-
-
-            String voucherIdString = voucherId.toString();
-            List<Voucher> voucherList = new ArrayList<>();
-            for (String str : repositoryAllContent) {
-                String[] fields = str.split(",");
-
-                if (voucherIdString.equals(fields[1])) {
-                    return Optional.of(getVoucherStatus(fields));
-                }
-            }
-        } catch (IOException e) {
-            logger.error("{} {}", e.getMessage(), e.getStackTrace());
-        }
-        return Optional.empty();
+        return voucherRepository.stream()
+                .filter(voucher -> voucher.getVoucherId().equals(voucherId))
+                .findFirst();
     }
 
     @Override
     public void insert(Voucher voucher) {
-        try {
-            BufferedWriter bw = new BufferedWriter(new FileWriter(resource.getFile(), true));
-
-            String voucherTypeString = voucherFactory.createVoucherClassNameString(voucher);
-
-            bw.write(voucherTypeString + "," + voucher.getVoucherId().toString() + "," + voucher.getDiscountValue());
-            bw.newLine();
-            bw.close();
-        } catch (IOException e) {
-            logger.error("{} {}", e.getMessage(), e.getStackTrace());
-        }
+        voucherRepository.add(voucher);
     }
 
     @Override
     public List<Voucher> getAllStoredVoucher() {
-        List<Voucher> voucherList = new ArrayList<>();
-        try {
-            Path pathResource = Paths.get(resource.getURI());
-            List<String> repositoryAllContent = Files.readAllLines(pathResource);
-
-
-            for (String str : repositoryAllContent) {
-                String[] fields = str.split(",");
-
-                Voucher voucher = getVoucherStatus(fields);
-
-                voucherList.add(voucher);
-            }
-        } catch (IOException e) {
-            logger.error("{} {}", e.getMessage(), e.getStackTrace());
-        }
-        return voucherList;
-    }
-
-    private Voucher getVoucherStatus(String[] fields) {
-        String voucherType = fields[0];
-        String voucherId = fields[1];
-        String discountValue = fields[2];
-
-        Voucher voucher = voucherFactory.createVoucher(voucherType, voucherId, discountValue);
-
-        return voucher;
+        return voucherRepository;
     }
 
     @Override
     public void clear() {
-        try {
-            new FileWriter(resource.getFile(), false).close();
-        } catch (IOException e) {
-            logger.error("{} {}", e.getMessage(), e.getStackTrace());
-        }
+        voucherRepository.clear();
     }
 }
