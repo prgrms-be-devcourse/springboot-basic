b073507632ffeaf65cace695ef1e8915de6d2e2d
Rose <82176176+geonwoo0215@users.noreply.github.com>
2022-11-19 00:45:16 +0900

    refactor : Command 추가

--- src/main/java/org/prgrms/springorder/controller/Command.java
+++ src/main/java/org/prgrms/springorder/controller/Command.java
@@ -8,8 +8,15 @@ import org.prgrms.springorder.exception.NoSuchCommandException;
 public enum Command {
 	LIST("list"),
 	CREATE("create"),
+	JOIN("join"),
 	EXIT("exit"),
-	BLACK_LIST("blacklist");
+	BLACK_LIST("blacklist"),
+	ALLOCATE("allocate"),
+	GET("get"),
+	DELETE("delete"),
+	SEARCH("search");
+
+
 
 	private final String orderContext;
 
@@ -21,6 +28,6 @@ public enum Command {
 		return Arrays.stream(Command.values())
 			.filter(a -> a.orderContext.equals(s))
 			.findAny()
-			.orElseThrow(() -> new NoSuchCommandException(ErrorMessage.No_SUCH_COMMAND_MESSAGE));
+			.orElseThrow(() -> new NoSuchCommandException(ErrorMessage.NO_SUCH_COMMAND_MESSAGE));
 	}
 }
