b04333d33d86cb171db9249832f5b840ae55ff44
ybkim-dev <beanybigggy@gmail.com>
2022-11-18 17:36:26 +0900

    feat : 실행환경 분리
    local, dev, file 환경에 맞추어 VoucherRepository를 다르게 주입하도록 환경 분리

--- src/main/java/org/programmers/springbootbasic/domain/voucher/repository/CSVFileVoucherRepository.java
+++ src/main/java/org/programmers/springbootbasic/domain/voucher/repository/CSVFileVoucherRepository.java
@@ -18,7 +18,7 @@ import java.util.List;
 import java.util.UUID;
 
 @Repository
-@Profile("local")
+@Profile("file")
 public class CSVFileVoucherRepository implements VoucherRepository {
 
     private static final Logger logger = LoggerFactory.getLogger(CSVFileVoucherRepository.class);
