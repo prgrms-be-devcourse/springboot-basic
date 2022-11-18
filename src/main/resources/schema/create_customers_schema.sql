a54b719ad21dd08b26877532960f7378b2e9f30c
Kong <rhdtn311@naver.com>
2022-11-18 21:19:36 +0900

    test : customer 테스트 테이블 name 필드 길이 변경

--- src/main/resources/schema/create_customers_schema.sql
+++ src/main/resources/schema/create_customers_schema.sql
@@ -1,7 +1,7 @@
 CREATE TABLE customers
 (
     customer_id BINARY(16) PRIMARY KEY,
-    name        varchar(20) NOT NULL,
+    name        varchar(30) NOT NULL,
     email       varchar(50) NOT NULL,
     createdAt   datetime,
     CONSTRAINT unq_user_email UNIQUE (email)
