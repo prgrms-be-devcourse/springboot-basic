b64a9edb19838bce6014b04adb155bb59ad2bd4f
hyunseo <goatsfish@gmail.com>
2022-11-18 14:52:06 +0900

    test : 고객 관리 jdbc 레포지터리 테스트 추가

--- src/test/java/com/example/springbootbasic/repository/customer/JdbcCustomerRepositoryTest.java
+++ src/test/java/com/example/springbootbasic/repository/customer/JdbcCustomerRepositoryTest.java
@@ -1,91 +1,64 @@
 package com.example.springbootbasic.repository.customer;
 
+import com.example.springbootbasic.config.TestConfig;
 import com.example.springbootbasic.domain.customer.Customer;
-import com.zaxxer.hikari.HikariDataSource;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.jdbc.DataSourceBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-import javax.sql.DataSource;
 import java.util.Collections;
 import java.util.List;
 
 import static com.example.springbootbasic.domain.customer.CustomerStatus.BLACK;
 import static com.example.springbootbasic.domain.customer.CustomerStatus.NORMAL;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.hasSize;
-import static org.hamcrest.Matchers.is;
+import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringJUnitConfig
-//@ActiveProfiles("dev")
-class JdbcCustomerRepositoryTest {
-
-    @Configuration
-//    @ComponentScan(basePackages = {"com.example.springbootbasic.repository.customer"})
-    static class Config {
-
-        @Bean
-        DataSource dataSource() {
-            return DataSourceBuilder.create()
-                    .url("jdbc:mysql://localhost:3306/spb_basic")
-                    .username("root")
-                    .password("root1234!")
-                    .type(HikariDataSource.class)
-                    .build();
-        }
-
-        @Bean
-        JdbcTemplate jdbcTemplate() {
-            return new JdbcTemplate(dataSource());
-        }
-
-        @Bean
-        NamedParameterJdbcTemplate namedParameterJdbcTemplate() {
-            return new NamedParameterJdbcTemplate(jdbcTemplate());
-        }
-
-        @Bean
-        JdbcCustomerRepository jdbcCustomerRepository() {
-            return new JdbcCustomerRepository(namedParameterJdbcTemplate());
-        }
-    }
+@SpringBootTest
+@ActiveProfiles("dev")
+class JdbcCustomerRepositoryTest extends TestConfig {
 
     @Autowired
-    private JdbcCustomerRepository jdbcCustomerRepository;
+    private JdbcCustomerRepository customerRepository;
 
     @Test
-    @DisplayName("손님 전체 검색 성공")
+    @DisplayName("모든 타입의 고객 검색을 성공한다.")
     void whenFindAllCustomersThenSuccessTest() {
         // when
-        List<Customer> allCustomers = jdbcCustomerRepository.findAllCustomers();
+        List<Customer> allCustomers = customerRepository.findAllCustomers();
 
         // then
-        assertThat(allCustomers, hasSize(3));
+        assertThat(allCustomers).hasSize(3);
     }
 
     @Test
-    @DisplayName("일반 손님 전체 검색 성공")
+    @DisplayName("일반 타입으로 등록된 모든 고객 검색을 성공한다.")
     void whenFindAllNormalStatusCustomersThenSuccessTest() {
         // when
-        List<Customer> normalCustomers = jdbcCustomerRepository.findCustomersByStatus(NORMAL);
+        List<Customer> normalCustomers = customerRepository.findCustomersByStatus(NORMAL);
 
         // then
-        assertThat(normalCustomers, is(Collections.emptyList()));
+        assertThat(normalCustomers).isEqualTo(Collections.emptyList());
     }
 
     @Test
-    @DisplayName("블랙된 손님 전체 검색 성공")
+    @DisplayName("블랙리스트로 등록된 모든 고객 검색을 성공한다.")
     void whenFindAllBlackStatusCustomersThenSuccessTest() {
         // when
-        List<Customer> blackCustomers = jdbcCustomerRepository.findCustomersByStatus(BLACK);
+        List<Customer> blackCustomers = customerRepository.findCustomersByStatus(BLACK);
 
         // then
-        assertThat(blackCustomers, hasSize(3));
+        assertThat(blackCustomers).hasSize(3);
+    }
+
+    @Test
+    @DisplayName("타입을 통한 고객 검색 중 타입에 null이 들어갔을 경우 Collection.emptyList()를 반환한다.")
+    void whenFindCustomersByStatusNullThenSuccessTest() {
+        // when
+        List<Customer> findAllCustomers = customerRepository.findCustomersByStatus(null);
+
+        // then
+        assertThat(findAllCustomers).isEqualTo(Collections.emptyList());
     }
 }
