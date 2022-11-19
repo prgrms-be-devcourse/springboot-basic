63548f4f0023165e96bad3f56b122f9900ae96de
ybkim-dev <beanybigggy@gmail.com>
2022-11-19 14:47:57 +0900

    fix : VoucherInputDto 에 불필요한 String type 제거

--- src/main/java/org/programmers/springbootbasic/domain/voucher/dto/VoucherInputDto.java
+++ src/main/java/org/programmers/springbootbasic/domain/voucher/dto/VoucherInputDto.java
@@ -4,17 +4,15 @@ import org.programmers.springbootbasic.data.VoucherType;
 
 
 public class VoucherInputDto {
-    private String type;
     private VoucherType voucherType;
     private long amount;
 
     public VoucherInputDto(String type, long amount) {
-        this.type = type;
-        this.voucherType = convertToVoucherType();
+        this.voucherType = convertToVoucherType(type);
         this.amount = amount;
     }
 
-    private VoucherType convertToVoucherType() {
+    private VoucherType convertToVoucherType(String type) {
         // valid 하다면 VoucherType 으로 convert.
         return VoucherType.valueOfType(type);
     }
