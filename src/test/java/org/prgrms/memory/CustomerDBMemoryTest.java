5d1e19c42d4f71f411bd0d9a328c6291affc3dc7
yewon9609 <dodnjs1241@naver.com>
2022-11-20 03:17:21 +0900

    style: logger삭제

--- src/test/java/org/prgrms/memory/CustomerDBMemoryTest.java
+++ src/test/java/org/prgrms/memory/CustomerDBMemoryTest.java
@@ -15,8 +15,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestInstance.Lifecycle;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
