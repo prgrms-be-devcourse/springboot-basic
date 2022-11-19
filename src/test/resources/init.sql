03b8d3f4fa0ecb7bf7481206c09b5024e697a294
Lee Suyoung <78334910+twotwobread@users.noreply.github.com>
2022-11-19 21:33:29 +0900

    feat: 설정 파일 수정 및 추가하다.

---
+++ src/test/resources/init.sql
@@ -0,0 +1,29 @@
+DROP TABLE IF EXISTS vouchers;
+CREATE TABLE vouchers
+(
+    voucher_id        BINARY(16) PRIMARY KEY,
+    discount_value    varchar(20) NOT NULL,
+    voucher_type      varchar(30) NOT NULL,
+    owned_customer_id BINARY(16)           DEFAULT NULL,
+    created_at        datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6)
+);
+
+DROP TABLE IF EXISTS customers;
+CREATE TABLE customers
+(
+    customer_id   BINARY(16) PRIMARY KEY,
+    name          varchar(20) NOT NULL,
+    email         varchar(50) NOT NULL,
+    last_login_at datetime(6)          DEFAULT NULL,
+    created_at    datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+    is_blacklist  BOOLEAN              DEFAULT FALSE,
+    CONSTRAINT unq_user_email UNIQUE (email)
+);
+
+DROP TABLE IF EXISTS User;
+CREATE TABLE User
+(
+    seq  BIGINT AUTO_INCREMENT,
+    name VARCHAR(50),
+    PRIMARY KEY (seq)
+);
