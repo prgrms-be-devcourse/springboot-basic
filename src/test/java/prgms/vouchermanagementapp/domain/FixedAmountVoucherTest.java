f35a1c4f005f09101df332ad25704b8746698010
preferKim <shineecard5@gmail.com>
2022-11-21 02:44:48 +0900

    Refactor(test): 불필요한 프로파일 설정 로직 삭제

--- src/test/java/prgms/vouchermanagementapp/domain/FixedAmountVoucherTest.java
+++ src/test/java/prgms/vouchermanagementapp/domain/FixedAmountVoucherTest.java
@@ -1,29 +1,12 @@
 package prgms.vouchermanagementapp.domain;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.core.env.ConfigurableEnvironment;
-import prgms.vouchermanagementapp.VoucherManagementApp;
 import prgms.vouchermanagementapp.domain.value.Amount;
 import prgms.vouchermanagementapp.service.VoucherCreationFactory;
 
 class FixedAmountVoucherTest {
-    
-    static AnnotationConfigApplicationContext applicationContext;
-
-    @DisplayName("프로파일 설정")
-    @BeforeAll
-    static void beforeEach() {
-        applicationContext = new AnnotationConfigApplicationContext();
-        applicationContext.register(VoucherManagementApp.class);
-
-        ConfigurableEnvironment environment = applicationContext.getEnvironment();
-        environment.setActiveProfiles("release");
-        applicationContext.refresh();
-    }
 
     @DisplayName("할인 전 금액이 고정 할인 금액보다 작은 경우 IllegalStateException 발생")
     @Test
