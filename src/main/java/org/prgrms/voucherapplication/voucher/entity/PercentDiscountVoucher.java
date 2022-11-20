65a82b52b2732366879ff78ea7cabe5af85ad8b7
yeezze <yeeeze22@gmail.com>
2022-11-20 13:22:08 +0900

    style: 필드명 변경

--- src/main/java/org/prgrms/voucherapplication/voucher/entity/PercentDiscountVoucher.java
+++ src/main/java/org/prgrms/voucherapplication/voucher/entity/PercentDiscountVoucher.java
@@ -9,15 +9,15 @@ import java.util.UUID;
 public class PercentDiscountVoucher extends Voucher{
 
     private final Logger logger = LoggerFactory.getLogger(PercentDiscountVoucher.class);
-    private static final String PERCENT_NOT_100 = "할인 퍼센트는 100% 이하만 가능합니다.";
+    private static final String PERCENT_MAX = "할인 퍼센트는 100% 이하만 가능합니다.";
 
     public PercentDiscountVoucher(UUID uuid, int percent) {
         super(uuid, percent);
 
         final int MAX_PERCENT = 100;
         if (percent > MAX_PERCENT) {
-            logger.error(PERCENT_NOT_100);
-            throw new VoucherException(PERCENT_NOT_100);
+            logger.error(PERCENT_MAX);
+            throw new VoucherException(PERCENT_MAX);
         }
     }
 
