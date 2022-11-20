c7091e55bf998e745b762ce1e476359dad1037b6
권주성 <99165624+JoosungKwon@users.noreply.github.com>
2022-11-21 05:33:50 +0900

    feat: VoucherService 요구사항에 맞는 기능 추가
    - Transaction 미적용
    - 아직 까지는 단순 CRUD 제공
    - 테스트 코드 X

--- src/main/java/com/programmers/kwonjoosung/springbootbasicjoosung/service/VoucherService.java
+++ src/main/java/com/programmers/kwonjoosung/springbootbasicjoosung/service/VoucherService.java
@@ -1,14 +1,12 @@
 package com.programmers.kwonjoosung.springbootbasicjoosung.service;
 
-import com.programmers.kwonjoosung.springbootbasicjoosung.model.voucher.FixedAmountDiscountVoucher;
-import com.programmers.kwonjoosung.springbootbasicjoosung.model.voucher.PercentDiscountVoucher;
-import com.programmers.kwonjoosung.springbootbasicjoosung.model.voucher.Voucher;
-import com.programmers.kwonjoosung.springbootbasicjoosung.model.voucher.VoucherType;
+import com.programmers.kwonjoosung.springbootbasicjoosung.model.voucher.*;
 import com.programmers.kwonjoosung.springbootbasicjoosung.repository.voucher.VoucherRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class VoucherService {
@@ -18,23 +16,35 @@ public class VoucherService {
         this.voucherRepository = voucherRepository;
     }
 
-    public List<Voucher> getVoucherList() {
+    public Optional<Voucher> createVoucher(VoucherType voucherType, long discount) {
+        Voucher voucher = VoucherFactory.createVoucher(voucherType, discount);
+        boolean result = saveVoucher(voucher);
+        if(result) return Optional.of(voucher);
+        return Optional.empty();
+    }
+    private boolean saveVoucher(Voucher voucher) {
+        return voucherRepository.insert(voucher);
+    }
+
+    public Optional<Voucher> findVoucher(UUID voucherId) {
+        return voucherRepository.findById(voucherId);
+    }
+
+    public List<Voucher> getAllVouchers() {
         return voucherRepository.findAll();
     }
 
-    public Optional<Voucher> createAndSave(VoucherType voucherType, long discount) {
-        return Optional.ofNullable(saveVoucher(createVoucher(voucherType, discount)));
+    public boolean updateVoucher(UUID voucherId, VoucherType voucherType, long discount) {
+        if(findVoucher(voucherId).isPresent()){
+            Voucher newVoucher = VoucherFactory.createVoucher(voucherType,voucherId, discount);
+            return voucherRepository.update(newVoucher);
+        }
+        return false;
     }
 
-    private Voucher createVoucher(VoucherType voucherType, long discount) {
-        return switch (voucherType) {
-            case FIXED -> new FixedAmountDiscountVoucher(discount);
-            case PERCENT -> new PercentDiscountVoucher(discount);
-        };
+    public boolean deleteVoucher(UUID voucherId) {
+        return voucherRepository.deleteById(voucherId);
     }
 
-    private Voucher saveVoucher(Voucher voucher) {
-        return voucherRepository.insert(voucher) ? voucher : null;
-    }
 
 }
