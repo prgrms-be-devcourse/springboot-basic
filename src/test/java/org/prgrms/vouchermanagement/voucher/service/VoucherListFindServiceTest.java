16406ebb1fd56e5619d8711aca7b17cdbccb0cd6
Kong <rhdtn311@naver.com>
2022-11-20 14:07:18 +0900

    test : 특정 바우처를 보유한 고객 조회 기능 테스트 코드 작성

--- src/test/java/org/prgrms/vouchermanagement/voucher/service/VoucherListFindServiceTest.java
+++ src/test/java/org/prgrms/vouchermanagement/voucher/service/VoucherListFindServiceTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.prgrms.vouchermanagement.customer.domain.Customer;
+import org.prgrms.vouchermanagement.exception.voucher.VoucherNotFoundException;
 import org.prgrms.vouchermanagement.voucher.domain.FixedAmountVoucher;
 import org.prgrms.vouchermanagement.voucher.domain.PercentDiscountVoucher;
 import org.prgrms.vouchermanagement.voucher.domain.Voucher;
@@ -11,10 +12,12 @@ import org.prgrms.vouchermanagement.voucher.repository.VoucherRepository;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.tuple;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
 class VoucherListFindServiceTest {
@@ -76,5 +79,49 @@ class VoucherListFindServiceTest {
                 .extracting("voucherId", "discountAmount", "voucherType", "customerId")
                 .contains(tuple(fixedAmountVoucher.getVoucherId(), fixedAmountVoucher.getDiscountAmount(), fixedAmountVoucher.getVoucherType(), fixedAmountVoucher.getCustomerId()))
                 .contains(tuple(percentDiscountVoucher.getVoucherId(), percentDiscountVoucher.getDiscountAmount(), percentDiscountVoucher.getVoucherType(), percentDiscountVoucher.getCustomerId()));
+    }
+
+    @Test
+    @DisplayName("VoucherId로 Voucher 찾기")
+    void findVoucherByVoucherId() {
+        // given
+        Voucher fixedAmountVoucher = new FixedAmountVoucher(UUID.randomUUID(), 1000, UUID.randomUUID());
+        Voucher percentDiscountVoucher = new PercentDiscountVoucher(UUID.randomUUID(), 30, UUID.randomUUID());
+
+        when(voucherRepository.findById(fixedAmountVoucher.getVoucherId()))
+                .thenReturn(Optional.of(fixedAmountVoucher));
+
+        when(voucherRepository.findById(percentDiscountVoucher.getVoucherId()))
+                .thenReturn(Optional.of(percentDiscountVoucher));
+
+        // when
+        Voucher findFixedAmountVoucher = voucherListFindService.findVoucherByVoucherId(fixedAmountVoucher.getVoucherId());
+        Voucher findPercentDiscountVoucher = voucherListFindService.findVoucherByVoucherId(percentDiscountVoucher.getVoucherId());
+
+        // then
+        verify(voucherRepository).findById(fixedAmountVoucher.getVoucherId());
+        verify(voucherRepository).findById(percentDiscountVoucher.getVoucherId());
+
+        assertThat(findFixedAmountVoucher)
+                .usingRecursiveComparison()
+                .isEqualTo(findFixedAmountVoucher);
+
+        assertThat(percentDiscountVoucher)
+                .usingRecursiveComparison()
+                .isEqualTo(findPercentDiscountVoucher);
+    }
+
+    @Test
+    @DisplayName("VoucherId로 Voucher를 찾을 때 DB에 Voucher가 존재하지 않는 경우")
+    void findVoucherByVoucherIdWhenVoucherNotExist() {
+        // given
+        UUID voucherId = UUID.randomUUID();
+        Voucher fixedAmountVoucher = new FixedAmountVoucher(UUID.randomUUID(), 1000, voucherId);
+
+        when(voucherRepository.findById(fixedAmountVoucher.getVoucherId()))
+                .thenThrow(VoucherNotFoundException.class);
+
+        // when, then
+        assertThrows(VoucherNotFoundException.class, () -> voucherListFindService.findVoucherByVoucherId(voucherId));
     }
 }
