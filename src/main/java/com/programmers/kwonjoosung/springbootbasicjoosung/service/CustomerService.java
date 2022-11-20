c531c6366ac1c4d702585d22c477fb0a79c5fe27
권주성 <99165624+JoosungKwon@users.noreply.github.com>
2022-11-21 05:36:36 +0900

    feat: CustomerService 요구사항에 맞게 기능 변경
    - Transaction 미적용
    - 아직 까지는 단순 CRUD 제공
    - 테스트 코드 X

--- src/main/java/com/programmers/kwonjoosung/springbootbasicjoosung/service/CustomerService.java
+++ src/main/java/com/programmers/kwonjoosung/springbootbasicjoosung/service/CustomerService.java
@@ -1,20 +1,45 @@
 package com.programmers.kwonjoosung.springbootbasicjoosung.service;
 
 import com.programmers.kwonjoosung.springbootbasicjoosung.model.customer.Customer;
-import com.programmers.kwonjoosung.springbootbasicjoosung.repository.customer.FileCustomerRepository;
+import com.programmers.kwonjoosung.springbootbasicjoosung.repository.customer.CustomerRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class CustomerService {
-    private final FileCustomerRepository fileCustomerRepository;
 
-    public CustomerService(FileCustomerRepository fileCustomerRepository) {
-        this.fileCustomerRepository = fileCustomerRepository;
+    private final CustomerRepository customerRepository;
+
+    public CustomerService(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    public Optional<Customer> createCustomer(String name) {
+        Customer newCustomer = new Customer(UUID.randomUUID(), name);
+        return customerRepository.insert(newCustomer) ? Optional.of(newCustomer) : Optional.empty();
+    }
+
+    public Optional<Customer> findCustomerByCustomerId(UUID customerId) {
+        return customerRepository.findById(customerId);
+    }
+
+    public List<Customer> getAllCustomers() {
+        return customerRepository.findAll();
+    }
+
+    public boolean updateCustomer(UUID customerId, String customerName) {
+        Customer customer = new Customer(customerId, customerName);
+        return customerRepository.update(customer);
+    }
+
+    public boolean deleteCustomer(UUID customerId) {
+        return customerRepository.delete(customerId);
     }
 
     public List<Customer> getBlackList() {
-        return fileCustomerRepository.findAll();
+        return customerRepository.findAllBlockCustomer();
     }
 }
