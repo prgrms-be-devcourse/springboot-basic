b88bd414b2753e6c8b4aa93f9323537018a70852
hyunseo <goatsfish@gmail.com>
2022-11-21 21:04:59 +0900

    feat : 고객 바우처 보유 기능 추가

--- src/main/java/com/example/springbootbasic/domain/customer/Customer.java
+++ src/main/java/com/example/springbootbasic/domain/customer/Customer.java
@@ -1,8 +1,25 @@
 package com.example.springbootbasic.domain.customer;
 
+import com.example.springbootbasic.domain.voucher.Voucher;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 public class Customer {
-    private final long customerId;
-    private final CustomerStatus status;
+    private long customerId;
+
+    private CustomerStatus status;
+
+    private List<Voucher> vouchers = new ArrayList<>();
+
+    public Customer(long customerId) {
+        this.customerId = customerId;
+    }
+
+    public Customer(CustomerStatus status) {
+        this.status = status;
+    }
 
     public Customer(long customerId, CustomerStatus status) {
         this.customerId = customerId;
@@ -22,4 +39,21 @@ public class Customer {
     }
 
     public boolean isNormal() { return status.isNormal(); }
+
+    public void receiveFrom(Voucher voucher) {
+        vouchers.add(voucher);
+    }
+
+    public boolean hasVoucher(Voucher findVoucher) {
+        return vouchers.stream().anyMatch(voucher -> Objects.equals(voucher.getVoucherId(), findVoucher.getVoucherId()));
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "customerId=" + customerId +
+                ", status=" + status +
+                ", vouchers=" + vouchers +
+                '}';
+    }
 }
