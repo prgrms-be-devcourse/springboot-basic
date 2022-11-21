66d658db6718f629666ab20c85e778eb70140ef8
hyunseo <goatsfish@gmail.com>
2022-11-22 00:05:50 +0900

    test : 고객 서비스 기능 테스트

--- src/test/java/com/example/springbootbasic/service/customer/CustomerServiceTest.java
+++ src/test/java/com/example/springbootbasic/service/customer/CustomerServiceTest.java
@@ -1,13 +1,10 @@
 package com.example.springbootbasic.service.customer;
 
-import com.example.springbootbasic.config.TestConfig;
 import com.example.springbootbasic.domain.customer.Customer;
+import com.example.springbootbasic.repository.customer.CsvCustomerRepository;
 import com.example.springbootbasic.repository.customer.CustomerRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
-import org.mockito.Mockito;
-import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
 
@@ -15,18 +12,12 @@ import static com.example.springbootbasic.domain.customer.CustomerStatus.BLACK;
 import static com.example.springbootbasic.domain.customer.CustomerStatus.NORMAL;
 import static java.util.Collections.EMPTY_LIST;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.TestInstance.Lifecycle;
+import static org.mockito.Mockito.mock;
 
-@TestInstance(Lifecycle.PER_CLASS)
-@ActiveProfiles("dev")
-class CustomerServiceTest extends TestConfig {
+class CustomerServiceTest {
 
-    private final CustomerService customerService;
-
-    public CustomerServiceTest() {
-        CustomerRepository customerRepository = Mockito.mock(CustomerRepository.class);
-        this.customerService = new CustomerService(customerRepository);
-    }
+    private final CustomerRepository customerRepository = mock(CsvCustomerRepository.class);
+    private final CustomerService customerService = new CustomerService(customerRepository);
 
     @Test
     @DisplayName("블랙리스트로 등록된 모든 고객 검색에 성공한다.")
