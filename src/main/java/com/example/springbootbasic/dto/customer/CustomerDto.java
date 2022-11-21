8d83ae071c0374941eeec786466d940078d2e319
hyunseo <goatsfish@gmail.com>
2022-11-22 02:48:55 +0900

    feat : 고객 jdbc 컨트롤러 기능 구현

--- src/main/java/com/example/springbootbasic/dto/customer/CustomerDto.java
+++ src/main/java/com/example/springbootbasic/dto/customer/CustomerDto.java
@@ -2,19 +2,29 @@ package com.example.springbootbasic.dto.customer;
 
 import com.example.springbootbasic.domain.customer.Customer;
 import com.example.springbootbasic.domain.customer.CustomerStatus;
+import com.example.springbootbasic.dto.voucher.VoucherDto;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class CustomerDto {
 
-    private Long customerId;
-    private CustomerStatus status;
+    private final Long customerId;
+    private final CustomerStatus status;
+    private final List<VoucherDto> vouchers;
 
-    public CustomerDto(Long customerId, CustomerStatus status) {
+    private CustomerDto(Long customerId, CustomerStatus status, List<VoucherDto> vouchers) {
         this.customerId = customerId;
         this.status = status;
+        this.vouchers = vouchers;
     }
 
-    public static CustomerDto toDto(Customer customer) {
-        return new CustomerDto(customer.getCustomerId(), customer.getStatus());
+    public static CustomerDto of(Customer customer) {
+        List<VoucherDto> voucherDtos = customer.getVouchers()
+                .stream()
+                .map(VoucherDto::of)
+                .collect(Collectors.toList());
+        return new CustomerDto(customer.getCustomerId(), customer.getStatus(), voucherDtos);
     }
 
     public Long getCustomerId() {
@@ -23,5 +33,9 @@ public class CustomerDto {
 
     public CustomerStatus getStatus() {
         return status;
+    }
+
+    public List<VoucherDto> getVouchers() {
+        return vouchers;
     }
 }
