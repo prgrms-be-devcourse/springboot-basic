9748b1cfc393b31b7420f87cfcccc03d36570cfa
yeezze <yeeeze22@gmail.com>
2022-11-21 15:39:31 +0900

    refactor: input 입력상황에서 에러 발생 시 해당 메소드 내에서 로직을 다시 실행하도록 변경 (while문 사용)

--- src/main/java/org/prgrms/voucherapplication/voucher/controller/VoucherController.java
+++ src/main/java/org/prgrms/voucherapplication/voucher/controller/VoucherController.java
@@ -6,7 +6,9 @@ import org.prgrms.voucherapplication.console.Output;
 import org.prgrms.voucherapplication.customer.service.CustomerService;
 import org.prgrms.voucherapplication.customer.dto.ResponseBlacklist;
 import org.prgrms.voucherapplication.voucher.entity.Voucher;
+import org.prgrms.voucherapplication.voucher.entity.VoucherConstructorException;
 import org.prgrms.voucherapplication.voucher.entity.VoucherType;
+import org.prgrms.voucherapplication.voucher.entity.VoucherTypeOfException;
 import org.prgrms.voucherapplication.voucher.service.VoucherService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -82,36 +84,42 @@ public class VoucherController {
     public void create() {
         VoucherType voucherType = getVoucherType();
 
-        String discount;
-        Voucher voucher;
-        try {
+        boolean isSuccess = false;
+        while (!isSuccess) {
+            String discount;
+            Voucher voucher;
+
             discount = input.command();
-            voucher = voucherType.createVoucher(UUID.randomUUID(), Integer.parseInt(discount), LocalDateTime.now());
-        } catch (RuntimeException e) {
-            logger.error(e.getMessage());
-            discount = input.command();
-            voucher = voucherType.createVoucher(UUID.randomUUID(), Integer.parseInt(discount), LocalDateTime.now());
+            try {
+                voucher = voucherType.createVoucher(UUID.randomUUID(), Integer.parseInt(discount), LocalDateTime.now());
+            } catch (VoucherConstructorException e) {
+                continue;
+            }
+
+            voucherService.create(voucher);
+            isSuccess = true;
         }
-        voucherService.create(voucher);
     }
 
     public VoucherType getVoucherType() {
-        output.display(REQUEST_MESSAGE_VOUCHER_TYPE);
-        String voucherNames = VoucherType.getNames();
-        output.display(voucherNames);
-        String voucherNameInput = input.command();
-        VoucherType voucherType;
+        VoucherType voucherType = null;
 
-        try {
-            voucherType = VoucherType.of(voucherNameInput);
-        } catch (RuntimeException e) {
-            logger.error(e.getMessage());
+        boolean isSuccess = false;
+        while (!isSuccess) {
+            output.display(REQUEST_MESSAGE_VOUCHER_TYPE);
+            String voucherNames = VoucherType.getNames();
             output.display(voucherNames);
-            voucherNameInput = input.command();
-            voucherType = VoucherType.of(voucherNameInput);
-        }
+            String voucherNameInput = input.command();
 
-        output.display(voucherType.getDiscountGuide());
+            try {
+                voucherType = VoucherType.of(voucherNameInput);
+            } catch (VoucherTypeOfException e) {
+                continue;
+            }
+
+            output.display(voucherType.getDiscountGuide());
+            isSuccess = true;
+        }
         return voucherType;
     }
 }
