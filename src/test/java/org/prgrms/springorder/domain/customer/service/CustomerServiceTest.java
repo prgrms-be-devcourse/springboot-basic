abaac53fbd48c079903544894c2f4913389ac980
ysk <kim206gh@naver.com>
2022-11-21 13:18:32 +0900

    refactor : wallet 필드 수정

--- src/test/java/org/prgrms/springorder/domain/customer/service/CustomerServiceTest.java
+++ src/test/java/org/prgrms/springorder/domain/customer/service/CustomerServiceTest.java
@@ -68,7 +68,7 @@ class CustomerServiceTest {
         UUID percentVoucherId = UUID.randomUUID();
         Voucher percentAmountVoucher = new PercentDiscountVoucher(percentVoucherId, amount);
         List<Voucher> vouchers = List.of(fixedAmountVoucher, percentAmountVoucher);
-        Wallet wallet = new Wallet(customer, vouchers);
+        Wallet wallet = new Wallet(customer, vouchers, customer1);
 
         when(customerRepository.findByIdWithVouchers(customerId))
             .thenReturn(Optional.of(wallet));
