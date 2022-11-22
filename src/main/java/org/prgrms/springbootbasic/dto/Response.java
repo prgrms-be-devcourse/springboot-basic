22c856611bde2f5400a55a9d3af5de9e67af4187
bufflect <bndgc@naver.com>
2022-11-23 01:00:44 +0900

    refactor : Response 변경   - methodType이 update, delete일 때 문구 추가   - 리스트의 원소들이 줄바꿈되어 출력되도록 변경

--- src/main/java/org/prgrms/springbootbasic/dto/Response.java
+++ src/main/java/org/prgrms/springbootbasic/dto/Response.java
@@ -5,13 +5,14 @@ import org.prgrms.springbootbasic.type.MethodType;
 import java.text.MessageFormat;
 import java.util.List;
 
-import static org.prgrms.springbootbasic.type.MethodType.CREATE;
-import static org.prgrms.springbootbasic.type.MethodType.LOOKUP;
+import static org.prgrms.springbootbasic.type.MethodType.*;
 
 public class Response {
-    public static final String DOES_NOT_EXIST = "does not exist";
     public static final String GENERATED = "is generated";
+    public static final String DOES_NOT_EXIST = "does not exist";
     public static final String EXIST = "exist";
+    public static final String UPDATED = "updated";
+    public static final String DELETED = "deleted";
     private final MethodType methodType;
     private final List<?> list;
 
@@ -22,13 +23,24 @@ public class Response {
 
     @Override
     public String toString() {
-        if (methodType == CREATE) return MessageFormat.format("{0} {1}", list, GENERATED);
-        if (methodType == LOOKUP) {
+        if (methodType == CREATE) {
+            return MessageFormat.format("{0} {1}", list2String(), GENERATED);
+        } else if (methodType == LOOKUP) {
             if (list.isEmpty()) {
                 return DOES_NOT_EXIST;
             }
-            return MessageFormat.format("{0} {1}", list, EXIST);
+            return MessageFormat.format("{0} {1}", list2String(), EXIST);
+        } else if (methodType == UPDATE) {
+            return MessageFormat.format("{0} {1}", list2String(), UPDATED);
+        } else if (methodType == DELETE) {
+            return MessageFormat.format("{0} {1}", list2String(), DELETED);
         }
         return list.toString();
+    }
+
+    private String list2String() {
+        StringBuffer sb = new StringBuffer();
+        list.forEach((e) -> sb.append(e).append("\n"));
+        return sb.toString();
     }
 }
