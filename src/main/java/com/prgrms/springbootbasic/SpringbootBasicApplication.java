3071e64b871d30ed6b4e1a800bdf9d1539d33818
Hwan Kim <hwankim1941@gmail.com>
2022-11-24 02:08:28 +0900

    [VOU-07] feat: SpringBoot 기반으로 동작하도록 설정

--- src/main/java/com/prgrms/springbootbasic/SpringbootBasicApplication.java
+++ src/main/java/com/prgrms/springbootbasic/SpringbootBasicApplication.java
@@ -1,16 +1,13 @@
 package com.prgrms.springbootbasic;
 
-import com.prgrms.springbootbasic.app.VoucherApplication;
-import com.prgrms.springbootbasic.common.config.AppConfiguration;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class SpringbootBasicApplication {
 
     public static void main(String[] args) {
-        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfiguration.class);
-
-        VoucherApplication console = applicationContext.getBean(VoucherApplication.class);
-        console.runLifecycle();
+        SpringApplication.run(SpringbootBasicApplication.class, args);
     }
 
 }
