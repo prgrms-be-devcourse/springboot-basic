55127ae20f5da70ae25505a0a1875427dedef8d8
ttaehee <ttaehee1105@gmail.com>
2022-11-24 00:00:29 +0900

    refactor : VoucherRepository unit test, order 지정으로 수정

--- src/test/java/com/programmers/voucher/repository/voucher/VoucherRepositoryTest.java
+++ src/test/java/com/programmers/voucher/repository/voucher/VoucherRepositoryTest.java
@@ -8,8 +8,7 @@ import com.programmers.voucher.model.voucher.PercentDiscountVoucher;
 import com.programmers.voucher.model.voucher.Voucher;
 import com.programmers.voucher.model.voucher.VoucherType;
 import com.programmers.voucher.repository.customer.CustomerRepository;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
@@ -17,6 +16,7 @@ import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class VoucherRepositoryTest extends JdbcConfig {
     @Autowired
     private VoucherRepository voucherRepository;
@@ -60,11 +60,12 @@ class VoucherRepositoryTest extends JdbcConfig {
     }
 
     @Test
+    @Order(1)
     @DisplayName("데이터베이스에 바우처를 저장한다.")
     void save() {
         //given
-        VoucherType voucherType = getVoucherType();
-        Voucher newVoucher = getVoucher();
+        VoucherType voucherType = VoucherType.toVoucherType("1");
+        Voucher newVoucher = new FixedAmountVoucher(UUID.randomUUID(), 5000);
         newVoucher.setVoucherType(voucherType);
 
         //when
@@ -76,6 +77,7 @@ class VoucherRepositoryTest extends JdbcConfig {
     }
 
     @Test
+    @Order(2)
     @DisplayName("데이터베이스에서 모든 바우처 목록을 조회한다.")
     void findAll() {
         //given
@@ -86,27 +88,39 @@ class VoucherRepositoryTest extends JdbcConfig {
 
         //then
         assertThat(result.size())
-                .isEqualTo(2);
+                .isEqualTo(3);
     }
 
     @Test
+    @Order(3)
+    @DisplayName("데이터베이스에 저장된 바우처를 고객에게 할당한다.")
+    void assign() {
+        //given
+        Voucher voucher = insertSingleVoucherData();
+        Customer customer = insertSingleCustomerData();
+        voucher.setCustomer(customer);
+
+        //when
+        voucherRepository.assign(voucher);
+
+        //then
+        assertThat(voucherRepository.findAllByEmail("taehee@gmail.com").size())
+                .isEqualTo(1);
+    }
+
+    @Test
+    @Order(4)
     @DisplayName("데이터베이스에서 해당 고객이 가진 모든 바우처 목록을 조회한다.")
     void findAllByEmail() {
         //given
-        insertAllVouchersData();
-        Customer customer = insertSingleCustomerData();
-        List<Voucher> vouchers = voucherRepository.findAll();
-        for (Voucher voucher : vouchers) {
-            voucher.setCustomer(customer);
-            voucherRepository.assign(voucher);
-        }
+        String email = "taehee@gmail.com";
 
         //when
-        List<Voucher> result = voucherRepository.findAllByEmail("taehee@gmail.com");
+        List<Voucher> result = voucherRepository.findAllByEmail(email);
 
         //then
         assertThat(result.size())
-                .isEqualTo(2);
+                .isEqualTo(1);
     }
 
     @Test
@@ -142,10 +156,10 @@ class VoucherRepositoryTest extends JdbcConfig {
     }
 
     @Test
+    @Order(5)
     @DisplayName("데이터베이스에서 모든 바우처 목록을 삭제한다.")
     void deleteALL() {
         //given
-        insertAllVouchersData();
 
         //when
         voucherRepository.deleteAll();
@@ -153,21 +167,5 @@ class VoucherRepositoryTest extends JdbcConfig {
         //then
         assertThat(voucherRepository.findAll().isEmpty())
                 .isTrue();
-    }
-
-    @Test
-    @DisplayName("데이터베이스에 저장된 바우처를 고객에게 할당한다.")
-    void assign() {
-        //given
-        Voucher voucher = insertSingleVoucherData();
-        Customer customer = insertSingleCustomerData();
-        voucher.setCustomer(customer);
-
-        //when
-        voucherRepository.assign(voucher);
-
-        //then
-        assertThat(voucherRepository.findAllByEmail("taehee@gmail.com").size())
-                .isEqualTo(1);
     }
 }
