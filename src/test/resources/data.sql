f9cb99a3c6c6093b899f8ecf366f658e531ef226
Fortune00 <sinyoungbok99@gmail.com>
2022-11-24 01:17:28 +0900

    test: 유저 저장소 테스트

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -7,3 +7,8 @@ create table if not exists voucher(
     discount_amount DOUBLE(7,2) not null,
     voucher_type varchar(255)
 ) engine=MyISAM;
+
+create table if not exists customer(
+    id BIGINT not null auto_increment primary key,
+    email VARCHAR(255) not null
+) engine=MyISAM;
