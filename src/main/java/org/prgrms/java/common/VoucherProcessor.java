243ba4daf6e2df0e0d91935cbc2113f9ad9b6621
Kim Changgyu <kimc980106@naver.com>
2022-11-24 13:39:32 +0900

    Feat: Wallet 기능 추가 (+ 명령어 전면 재개편)

--- src/main/java/org/prgrms/java/common/VoucherProcessor.java
+++ src/main/java/org/prgrms/java/common/VoucherProcessor.java
@@ -1,36 +1,36 @@
 package org.prgrms.java.common;
 
-import org.prgrms.java.domain.voucher.FixedAmountVoucher;
-import org.prgrms.java.domain.voucher.PercentDiscountVoucher;
+import org.prgrms.java.controller.CustomerController;
+import org.prgrms.java.controller.VoucherController;
+import org.prgrms.java.domain.customer.Customer;
 import org.prgrms.java.domain.voucher.Voucher;
 import org.prgrms.java.exception.CommandException;
 import org.prgrms.java.exception.CustomerException;
 import org.prgrms.java.exception.VoucherException;
-import org.prgrms.java.service.customer.CustomerService;
-import org.prgrms.java.service.voucher.VoucherService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Component
-public class VoucherProcessor implements CommandLineRunner {
-    private final CustomerService customerService;
-    private final VoucherService voucherService;
+public class VoucherProcessor implements ApplicationRunner {
+    private final CustomerController customerController;
+    private final VoucherController voucherController;
     private final View view;
     private static final Logger logger = LoggerFactory.getLogger(VoucherProcessor.class);
 
-    public VoucherProcessor(CustomerService customerService, VoucherService voucherService, View view) {
-        this.customerService = customerService;
-        this.voucherService = voucherService;
+    public VoucherProcessor(CustomerController customerController, VoucherController voucherController, View view) {
+        this.customerController = customerController;
+        this.voucherController = voucherController;
         this.view = view;
     }
 
     @Override
-    public void run(String... args) {
+    public void run(ApplicationArguments args) {
         boolean isRunning = true;
         while (isRunning) {
             try {
@@ -51,17 +51,15 @@ public class VoucherProcessor implements CommandLineRunner {
                 case EXIT:
                     view.print("Terminates the program...");
                     return isRunning = false;
-                case CREATE:
-                    createVoucher();
+                case VOUCHER:
+                    processVoucherCommand();
                     break;
-                case LIST:
-                    getVouchers();
+                case CUSTOMER:
+                    processCustomerCommand();
                     break;
-                case CUSTOMERS:
-                    getCustomers();
+                case WALLET:
+                    processWalletCommand();
                     break;
-                case BLACKLIST:
-                    getBlackCustomers();
             }
             return true;
         } catch (CommandException e) {
@@ -71,63 +69,244 @@ public class VoucherProcessor implements CommandLineRunner {
         return isRunning;
     }
 
-    private void createVoucher() {
-        view.print(MessageGuide.VOUCHER_TYPE);
+    private void processVoucherCommand() {
+        view.print(MessageGuide.VOUCHER_COMMAND_OPTION);
+        try {
+            switch (Command.get(view.read())) {
+                case CREATE:
+                    invokeCreateVoucher();
+                    break;
+                case FIND:
+                    invokeFindVoucher();
+                    break;
+                case LIST:
+                    invokeFindVouchers();
+                    break;
+                case DELETE:
+                    invokeDeleteVoucher();
+                    break;
+                case DELETE_ALL:
+                    invokeDeleteVouchers();
+                    break;
+            }
+        } catch (CommandException e) {
+            // Nothing
+        } catch (VoucherException | IllegalArgumentException e) {
+            view.print(e.getMessage());
+        }
+    }
+
+    private void invokeCreateVoucher() {
+        view.print(MessageGuide.REQUIRE_VOUCHER_TYPE);
         String voucherType = view.read();
 
-        view.print(MessageGuide.VOUCHER_DISCOUNT_AMOUNT);
+        view.print(MessageGuide.REQUIRE_VOUCHER_DISCOUNT_AMOUNT);
         long voucherAmount;
         try {
             voucherAmount = Long.parseLong(view.read());
         } catch (IllegalArgumentException e) {
-            view.print(e.getMessage());
-            logger.warn(e.getMessage());
-            return;
+            throw new IllegalArgumentException("Please enter a number.");
         }
 
-        Voucher voucher;
         switch (voucherType) {
             case "1":
-                voucher = new FixedAmountVoucher(UUID.randomUUID(), voucherAmount, LocalDateTime.now().plusDays(7));
+                voucherType = "FIXED";
                 break;
             case "2":
-                voucher = new PercentDiscountVoucher(UUID.randomUUID(), voucherAmount, LocalDateTime.now().plusDays(7));
+                voucherType = "PERCENT";
                 break;
             default:
-                view.print("Invalid Voucher Type");
+                view.print(voucherType + " is an Invalid Voucher Type.");
                 return;
         }
 
-        try {
-            voucherService.createVoucher(voucher);
-        } catch (VoucherException e) {
-            view.print(e.getMessage());
-            logger.warn(e.getMessage());
+        voucherController.createVoucher(voucherAmount, voucherType, LocalDateTime.now(), LocalDateTime.now().plusDays(7));
+    }
+
+    private void invokeFindVoucher() {
+        view.print(MessageGuide.VOUCHER_FIND_COMMAND_OPTION);
+
+        switch (view.read()) {
+            case "1":
+                view.print(MessageGuide.REQUIRE_VOUCHER_ID);
+                view.print(voucherController.findVoucherById(UUID.fromString(view.read())));
+                break;
+            case "2":
+                view.print(MessageGuide.REQUIRE_CUSTOMER_ID);
+                voucherController.findVouchersByOwner(UUID.fromString(view.read()))
+                        .forEach(view::print);
+                break;
         }
     }
 
-    private void getCustomers() {
-        try {
-            customerService.getAllCustomers()
-                    .forEach(view::print);
-        } catch (CustomerException e) {
-            view.print(e.getMessage());
-            logger.warn(e.getMessage());
-        }
-    }
-
-    private void getBlackCustomers() {
-        try {
-            customerService.getAllBlackCustomers()
-                    .forEach(view::print);
-        } catch (CustomerException e) {
-            view.print(e.getMessage());
-            logger.warn(e.getMessage());
-        }
-    }
-
-    private void getVouchers() {
-        voucherService.getAllVouchers()
+    private void invokeFindVouchers() {
+        voucherController.findVouchers()
                 .forEach(view::print);
+    }
+
+    private void invokeDeleteVoucher() {
+        view.print(MessageGuide.REQUIRE_VOUCHER_ID);
+
+        voucherController.deleteVoucher(UUID.fromString(view.read()));
+        view.print(MessageGuide.SUCCESS_MESSAGE);
+    }
+
+    private void invokeDeleteVouchers() {
+        view.print("Deleted " + voucherController.deleteVouchers() + " vouchers.");
+    }
+
+    private void processCustomerCommand() {
+        view.print(MessageGuide.CUSTOMER_COMMAND_OPTION);
+        try {
+            switch (Command.get(view.read())) {
+                case CREATE:
+                    invokeCreateCustomer();
+                    break;
+                case FIND:
+                    invokeFindCustomer();
+                    break;
+                case LIST:
+                    invokeFindCustomers();
+                    break;
+                case BLACKLIST:
+                    invokeFindBlacklist();
+                    break;
+                case DELETE:
+                    invokeDeleteCustomer();
+                    break;
+                case DELETE_ALL:
+                    invokeDeleteCustomers();
+                    break;
+            }
+        } catch (CommandException e) {
+            // Nothing
+        } catch (CustomerException | IllegalArgumentException e) {
+            view.print(e.getMessage());
+        }
+    }
+
+    private void invokeCreateCustomer() {
+        view.print(MessageGuide.REQUIRE_CUSTOMER_NAME);
+        String name = view.read();
+
+        view.print(MessageGuide.REQUIRE_CUSTOMER_EMAIL);
+        String email = view.read();
+
+        customerController.createCustomer(name, email);
+        view.print(MessageGuide.SUCCESS_MESSAGE);
+    }
+
+    private void invokeFindCustomer() {
+        view.print(MessageGuide.CUSTOMER_FIND_COMMAND_OPTION);
+
+        switch (view.read()) {
+            case "1":
+                view.print(MessageGuide.REQUIRE_CUSTOMER_ID);
+                view.print(customerController.findCustomerById(UUID.fromString(view.read())));
+                break;
+            case "2":
+                view.print(MessageGuide.REQUIRE_CUSTOMER_NAME);
+                view.print(customerController.findCustomerByName(view.read()));
+                break;
+            case "3":
+                view.print(MessageGuide.REQUIRE_CUSTOMER_EMAIL);
+                view.print(customerController.findCustomerByEmail(view.read()));
+                break;
+        }
+    }
+
+    private void invokeFindCustomers() {
+        customerController.findCustomers()
+                .forEach(view::print);
+    }
+
+    private void invokeFindBlacklist() {
+        customerController.findBlacklist()
+                .forEach(view::print);
+    }
+
+    private void invokeDeleteCustomer() {
+        view.print(MessageGuide.REQUIRE_CUSTOMER_ID);
+
+        customerController.deleteCustomer(UUID.fromString(view.read()));
+        view.print(MessageGuide.SUCCESS_MESSAGE);
+    }
+
+    private void invokeDeleteCustomers() {
+        view.print("Deleted " + customerController.deleteCustomers() + " customers.");
+    }
+
+    private void processWalletCommand() {
+        view.print(MessageGuide.WALLET_COMMAND_OPTION);
+
+        try {
+            switch (Command.get(view.read())) {
+                case ALLOCATE:
+                    invokeAllocateVoucherToWallet();
+                    break;
+                case SHOW:
+                    invokeShowCustomerWallet();
+                    break;
+                case FIND:
+                    invokeFindCustomerHavingVoucher();
+                    break;
+                case DELETE:
+                    invokeRemoveVoucherFromWallet();
+                    break;
+                case DELETE_ALL:
+                    invokeRemoveAllVouchersFromWallet();
+                    break;
+            }
+        } catch (CommandException e) {
+            // Nothing
+        } catch (CustomerException | VoucherException | IllegalArgumentException e) {
+            view.print(e.getMessage());
+        }
+    }
+
+    private void invokeAllocateVoucherToWallet() {
+        view.print(MessageGuide.REQUIRE_VOUCHER_ID);
+        Voucher voucher = voucherController.findVoucherById(UUID.fromString(view.read()));
+
+        view.print(MessageGuide.REQUIRE_CUSTOMER_ID);
+        Customer customer = customerController.findCustomerById(UUID.fromString(view.read()));
+
+        voucherController.allocateVoucher(voucher.getVoucherId(), customer.getCustomerId());
+        view.print(MessageGuide.SUCCESS_MESSAGE);
+    }
+
+    private void invokeFindCustomerHavingVoucher() {
+        view.print(MessageGuide.REQUIRE_VOUCHER_ID);
+        Voucher voucher = voucherController.findVoucherById(UUID.fromString(view.read()));
+        Customer customer = customerController.findCustomerById(voucher.getOwnerId());
+
+        view.print(customer);
+    }
+
+    private void invokeShowCustomerWallet() {
+        view.print(MessageGuide.REQUIRE_CUSTOMER_ID);
+        Customer customer = customerController.findCustomerById(UUID.fromString(view.read()));
+
+        voucherController.findVouchers().stream()
+                .filter(voucher -> customer.getCustomerId().equals(voucher.getOwnerId()))
+                .forEach(view::print);
+    }
+
+    private void invokeRemoveVoucherFromWallet() {
+        view.print(MessageGuide.REQUIRE_VOUCHER_ID);
+        Voucher voucher = voucherController.findVoucherById(UUID.fromString(view.read()));
+
+        voucherController.detachOwnerFromVoucher(voucher.getVoucherId());
+        view.print(MessageGuide.SUCCESS_MESSAGE);
+    }
+
+    private void invokeRemoveAllVouchersFromWallet() {
+        view.print(MessageGuide.REQUIRE_CUSTOMER_ID);
+        Customer customer = customerController.findCustomerById(UUID.fromString(view.read()));
+
+        voucherController.findVouchersByOwner(customer.getCustomerId())
+                .forEach(voucher -> voucherController.detachOwnerFromVoucher(voucher.getVoucherId()));
+
+        view.print(MessageGuide.SUCCESS_MESSAGE);
     }
 }
