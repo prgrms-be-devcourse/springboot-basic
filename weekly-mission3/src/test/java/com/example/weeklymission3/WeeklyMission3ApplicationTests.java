42f651331e6302c462d7bfc6d9d1bc36490e3d04
김소현 <thgus345@naver.com>
2022-11-25 00:02:34 +0900

    [INIT] 프로젝트 초기 세팅

---
+++ weekly-mission3/src/test/java/com/example/weeklymission3/WeeklyMission3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.weeklymission3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeeklyMission3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
