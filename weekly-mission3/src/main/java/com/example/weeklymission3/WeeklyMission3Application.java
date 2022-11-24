c14d77823c54e19b04b6e3192010bbf34f0c2db2
김소현 <thgus345@naver.com>
2022-11-25 00:11:12 +0900

    [INIT] 프로젝트 초기 세팅

--- weekly-mission3/src/main/java/com/example/weeklymission3/WeeklyMission3Application.java
+++ weekly-mission3/src/main/java/com/example/weeklymission3/WeeklyMission3Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WeeklyMission3Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WeeklyMission3Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WeeklyMission3Application.class, args);
+    }
 
 }
