444182eed7b183fcdf83c2e426a027fdb68a6988
bufflect <bndgc@naver.com>
2022-11-24 16:59:43 +0900

    remove : web과 관련없는 모듈 제거   - Console 관련 모듈   - blackListService 관련 모듈   - Voucher 관련 모듈(리팩토링 예정)     - Voucher들     - VoucherFactory들   - Web 관련 의존성 추가

--- src/main/java/org/prgrms/springbootbasic/factory/VoucherFactory.java
+++
@@ -1,9 +0,0 @@
-package org.prgrms.springbootbasic.factory;
-
-
-import org.prgrms.springbootbasic.entity.voucher.Voucher;
-
-public interface VoucherFactory {
-
-    Voucher createVoucher(long quantity);
-}
--- src/main/java/org/prgrms/springbootbasic/processor/Processor.java
+++
@@ -1,7 +0,0 @@
-package org.prgrms.springbootbasic.processor;
-
-import org.prgrms.springbootbasic.dto.Response;
-
-public interface Processor {
-    Response process();
-}
