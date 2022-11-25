41f1cd8976b3b70a910ace9addd01733ee85cf11
ttaehee <ttaehee1105@gmail.com>
2022-11-26 02:23:20 +0900

    feat : 조회페이지 관련 구현

--- src/main/java/com/programmers/voucher/controller/VoucherWebController.java
+++ src/main/java/com/programmers/voucher/controller/VoucherWebController.java
@@ -1,11 +1,15 @@
 package com.programmers.voucher.controller;
 
 import com.programmers.voucher.controller.dto.VoucherCreateRequest;
+import com.programmers.voucher.model.voucher.Voucher;
 import com.programmers.voucher.service.VoucherService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/voucher/")
@@ -31,5 +35,12 @@ public class VoucherWebController {
     public String create(VoucherCreateRequest voucherCreateRequest) {
         voucherService.create(voucherCreateRequest);
         return "redirect:/voucher/";
+    }
+
+    @GetMapping("voucherlist")
+    public String findAll(Model model) {
+        List<Voucher> vouchers = voucherService.findAll();
+        model.addAttribute("vouchers", vouchers);
+        return "voucher/listVoucher";
     }
 }
