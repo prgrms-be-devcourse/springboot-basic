32eee019913767a654c55e432c8d3ce784596473
박세준 <tpwns6693@gmail.com>
2022-11-25 23:38:47 +0900

    feat : 바우쳐 / 고객 관리 페이지로 나뉘는 페이지 생성 - 메인 컨트롤러에서 바우쳐관리로 갈지 고객 관리로 갈지 정해준다.(index.html) -

---
+++ src/main/java/org/prgrms/kdtspringdemo/MainController.java
@@ -0,0 +1,12 @@
+package org.prgrms.kdtspringdemo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    @GetMapping("/")
+    public String mainPage() {
+        return "index";
+    }
+}
--- src/main/java/org/prgrms/kdtspringdemo/domain/voucher/VoucherController.java
+++ src/main/java/org/prgrms/kdtspringdemo/domain/voucher/VoucherController.java
@@ -7,10 +7,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.UUID;
 
 @Controller
+@RequestMapping("/vouchers")
 public class VoucherController {
     private final VoucherService voucherService;
 
@@ -18,33 +20,28 @@ public class VoucherController {
         this.voucherService = voucherService;
     }
 
-    @GetMapping("/")
-    public String StartPage() {
-        return "redirect:/vouchers";
-    }
-
-    @GetMapping("/vouchers")
+    @GetMapping("")
     public String VouchersPage(Model model) {
         var vouchers = voucherService.getAllVoucherList();
         model.addAttribute("vouchers", vouchers);
         return "vouchers";
     }
 
-    @GetMapping("/vouchers/{voucherId}")
+    @GetMapping("/{voucherId}")
     public String VoucherDetailPage(@PathVariable("voucherId") UUID voucherId, Model model) {
         var voucher = voucherService.getVoucher(voucherId);
         model.addAttribute("voucher", voucher);
         return "voucher-detail";
     }
 
-    @GetMapping("/vouchers/new")
+    @GetMapping("/new")
     public String createVoucherPage(Model model) {
         var voucherTypes = VoucherType.values();
         model.addAttribute("voucherTypes", voucherTypes);
         return "voucher-new";
     }
 
-    @PostMapping("/vouchers/new")
+    @PostMapping("/new")
     public String CreateVoucher(Model model, CreateVoucherRequest createVoucherRequest) {
         try {
             voucherService.createVoucher(VoucherType.getTypeByName(createVoucherRequest.voucherType()),
