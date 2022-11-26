768178492c8eaf189713ed01e6928b376ec50481
권성준 <tjdwns1865@naver.com>
2022-11-27 02:27:40 +0900

    refactor: 웹 환경으로 변경

--- src/main/java/com/programmers/voucher/VoucherApplication.java
+++ src/main/java/com/programmers/voucher/VoucherApplication.java
@@ -2,15 +2,11 @@ package com.programmers.voucher;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-
-import com.programmers.voucher.console.ConsoleVoucherApp;
 
 @SpringBootApplication
 public class VoucherApplication {
 
 	public static void main(String[] args) {
-		ApplicationContext applicationContext = SpringApplication.run(VoucherApplication.class, args);
-		applicationContext.getBean(ConsoleVoucherApp.class).run();
+		SpringApplication.run(VoucherApplication.class, args);
 	}
 }
