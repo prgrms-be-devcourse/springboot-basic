74a079c1a3c05cd99a584371fc4b7a105410bf95
Seonho Kim <sunhowold@naver.com>
2022-11-27 21:08:11 +0900

    Feat(controller): 개별 바우처 삭제 기능 구현

--- src/main/java/prgms/vouchermanagementapp/controller/WebVoucherController.java
+++ src/main/java/prgms/vouchermanagementapp/controller/WebVoucherController.java
@@ -14,16 +14,16 @@ import java.util.List;
 import java.util.UUID;
 
 @Controller
-@RequestMapping("/voucher/vouchers")
+@RequestMapping("/voucher/voucherList")
 public class WebVoucherController {
 
     /**
      * URI Prefix: "voucher/vouchers"
-     * 바우처 전체 조회 페이지 vouchers(): GET, ""
-     * 바우처 상세 조회 페이지 voucher(): GET, "/{voucherId}"
-     * 바우처 생성 페이지 addForm() : GET, "/add"
+     * 바우처 전체 조회 페이지 voucherList(): GET, ""
+     * 바우처 상세 조회 페이지 voucherDetail(): GET, "/{voucherId}"
+     * 바우처 생성 페이지 voucherAddForm() : GET, "/add"
      * 바우처 생성 addVoucher() : POST, "/add"
-     * 바우처 수정 페이지 editForm() : GET, "/{voucherId}/edit
+     * 바우처 수정 페이지 voucherEditForm() : GET, "/{voucherId}/edit
      * 바우처 수정 editVoucher() : POST, "/{voucherId}/edit
      * 바우처 삭제 deleteVoucher() : POST, "/{voucherId}/delete
      */
@@ -35,18 +35,18 @@ public class WebVoucherController {
     }
 
     @GetMapping
-    public String vouchers(Model model) {
+    public String voucherList(Model model) {
         List<Voucher> vouchers = voucherRepository.findAll();
         List<VoucherViewDTO> voucherViewDTOs = vouchers.stream()
                 .map(VoucherViewDTO::new)
                 .toList();
 
         model.addAttribute("vouchers", voucherViewDTOs);
-        return "voucher/vouchers";
+        return "voucher/voucherList";
     }
 
     @GetMapping("/{voucherId}")
-    public String voucher(@PathVariable UUID voucherId, Model model) {
+    public String voucherDetail(@PathVariable UUID voucherId, Model model) {
         Voucher foundVoucher = voucherRepository.findById(voucherId)
                 .orElseThrow(() ->
                         new EmptyResultDataAccessException("cannot find voucher for voucherId=" + voucherId, 1)
@@ -54,6 +54,12 @@ public class WebVoucherController {
         VoucherViewDTO voucherViewDTO = new VoucherViewDTO(foundVoucher);
 
         model.addAttribute("voucher", voucherViewDTO);
-        return "voucher/voucher";
+        return "voucher/voucherDetail";
+    }
+
+    @GetMapping("/{voucherId}/delete")
+    public String deleteVoucher(@PathVariable UUID voucherId) {
+        voucherRepository.deleteById(voucherId);
+        return "redirect:/voucher/voucherList";
     }
 }
