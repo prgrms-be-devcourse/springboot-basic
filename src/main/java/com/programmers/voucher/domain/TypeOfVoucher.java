77d10ddd971bddee07d1360923b7fdc0aa4d8015
Albatross3 <superstring77@gmail.com>
2022-11-29 00:48:22 +0900

    feat: Voucher 로 통일 및 , TypeOfVoucher 수정

--- src/main/java/com/programmers/voucher/domain/TypeOfVoucher.java
+++ src/main/java/com/programmers/voucher/domain/TypeOfVoucher.java
@@ -1,35 +1,66 @@
 package com.programmers.voucher.domain;
 
+import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.UUID;
-import java.util.function.Function;
+import java.util.function.Consumer;
 
 public enum TypeOfVoucher {
-    FIXED_AMOUNT_VOUCHER("1","Insert amount of fixed discount",(discount)-> new FixedAmountVoucher(UUID.randomUUID(),discount)),
-    PERCENT_DISCOUNT_VOUCHER("2","Insert ratio of discount" ,(discount) -> new PercentDiscountVoucher(UUID.randomUUID(), discount));
-
+    FIXED_AMOUNT_VOUCHER("FixedAmount", "Insert amount of fixed discount", 6, (amount) -> validateFixedAmount(amount)),
+    PERCENT_DISCOUNT_VOUCHER("PercentDiscount", "Insert ratio of discount", 3, (discountPercent) -> validatePercentDiscount(discountPercent)),;
 
     private final String type;
     private final String  guideMessage;
-    private final Function<Long, Voucher> function;
-    TypeOfVoucher(String type,String guideMessage, Function<Long,Voucher> function) {
+    private final int expirationPeriod;
+    private final Consumer<Long> consumer;
+
+    TypeOfVoucher(String type, String guideMessage, int expirationPeriod, Consumer<Long> consumer) {
         this.type = type;
         this.guideMessage = guideMessage;
-        this.function = function;
+        this.expirationPeriod = expirationPeriod;
+        this.consumer = consumer;
+    }
+
+
+    public static Voucher createVoucher(String email ,TypeOfVoucher typeOfVoucher, long discount) {
+        return new Voucher(UUID.randomUUID(), typeOfVoucher, discount, LocalDateTime.now(), LocalDateTime.now().plusMonths(typeOfVoucher.expirationPeriod), email);
+    }
+    public static void validateVoucher(TypeOfVoucher typeOfVoucher, long discount) {
+        typeOfVoucher.consumer.accept(discount);
+    }
+
+    public static TypeOfVoucher getType(String typeName) {
+        return Arrays.stream(TypeOfVoucher.values())
+                .filter(typeOfVoucher -> typeOfVoucher.type.equals(typeName))
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("존재하지 않는 바우처입니다."));
+    }
+
+    public static int getExpirationPeriod(TypeOfVoucher typeOfVoucher) {
+        return typeOfVoucher.expirationPeriod;
+    }
+
+    public static String getType(TypeOfVoucher typeOfVoucher) {
+        return typeOfVoucher.type;
     }
 
     public static String getGuideMessage(TypeOfVoucher typeOfVoucher) {
         return typeOfVoucher.guideMessage;
     }
-
-    public static Voucher createVoucher(TypeOfVoucher typeOfVoucher, long discount) {
-        return typeOfVoucher.function.apply(discount);
+    public static void validateFixedAmount(long amount) {
+        final long MIN_VOUCHER_AMOUNT = 0L;
+        final long MAX_VOUCHER_AMOUNT = 10000L;
+        if (amount <= MIN_VOUCHER_AMOUNT || amount > MAX_VOUCHER_AMOUNT) {
+            throw new IllegalArgumentException("잘못된 범위의 할인 금액입니다.");
+        }
     }
 
-    public static TypeOfVoucher getType(String typeNumber) {
-        return Arrays.stream(TypeOfVoucher.values())
-                .filter(typeOfVoucher -> typeOfVoucher.type.equals(typeNumber))
-                .findFirst()
-                .orElseThrow(() -> new RuntimeException("존재하지 않는 바우처입니다."));
+    public static void validatePercentDiscount(long discountPercent) {
+        final long MIN_PERCENT = 0;
+        final long MAX_PERCENT = 100;
+        if (discountPercent <= MIN_PERCENT || discountPercent > MAX_PERCENT) {
+            throw new IllegalArgumentException("잘못된 범위의 할인율입니다.");
+        }
     }
+
 }
--- src/main/java/com/programmers/voucher/domain/Voucher.java
+++ src/main/java/com/programmers/voucher/domain/Voucher.java
@@ -1,11 +1,58 @@
 package com.programmers.voucher.domain;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
-public interface Voucher {
+public class Voucher {
+    private final UUID voucherId;
+    private final TypeOfVoucher typeOfVoucher;
+    private long discount;
+    private final LocalDateTime createdAt;
+    private final LocalDateTime expiredAt;
+    private final String customerEmail;
 
-    UUID getVoucherId();
+    public Voucher(UUID voucherId, TypeOfVoucher typeOfVoucher, long discount, LocalDateTime createdAt, LocalDateTime expiredAt, String customerEmail) {
+        this.voucherId = voucherId;
+        this.typeOfVoucher = typeOfVoucher;
+        this.discount = discount;
+        this.createdAt = createdAt;
+        this.expiredAt = expiredAt;
+        this.customerEmail = customerEmail;
+    }
 
-    long discount(long beforeDiscount);
+    public UUID getVoucherId() {
+        return voucherId;
+    }
 
+    public TypeOfVoucher getTypeOfVoucher() {
+        return typeOfVoucher;
+    }
+
+    public long getDiscount() {
+        return discount;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public LocalDateTime getExpiredAt() {
+        return expiredAt;
+    }
+
+    public String getCustomerEmail() {
+        return customerEmail;
+    }
+
+    @Override
+    public String toString() {
+        return "Voucher{" +
+                "voucherId=" + voucherId +
+                ", typeOfVoucher=" + typeOfVoucher +
+                ", discount=" + discount +
+                ", createdAt=" + createdAt +
+                ", expiredAt=" + expiredAt +
+                ", customerEmail='" + customerEmail + '\'' +
+                '}';
+    }
 }
