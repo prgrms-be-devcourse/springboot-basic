8dd0c07f8daf756928b8b69705a179d879962867
goseungwon <knmy0925@gmail.com>
2022-11-29 17:31:35 +0900

    [Fix] 잘못된 머지로 생긴 파일들 삭제

--- src/main/java/com/programmers/assignment/voucher/engine/io/Input.java
+++
@@ -1,9 +0,0 @@
-package com.programmers.assignment.voucher.engine.io;
-
-public interface Input {
-    String inputCommand(String commandMessage);
-
-    String selectVoucher(String voucher);
-
-    String inputVoucherInfo(String voucherInfo);
-}
--- src/main/java/com/programmers/assignment/voucher/engine/voucher/Voucher.java
+++
@@ -1,8 +0,0 @@
-package com.programmers.assignment.voucher.engine.voucher;
-
-import java.util.UUID;
-
-public interface Voucher {
-    UUID getVoucherId();
-    long discount(long beforeDiscount);
-}
