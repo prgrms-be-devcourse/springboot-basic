dce43dde5be73c6379e84a413b8a87531141fe80
Hwan Kim <hwankim1941@gmail.com>
2022-11-29 22:24:29 +0900

    [VOU-07] refactor: ErrorHandler를 위한 ErrorCode 및 Exception 코드 변경 적용

---
+++ src/main/java/com/prgrms/springbootbasic/common/dto/ErrorResponse.java
@@ -0,0 +1,20 @@
+package com.prgrms.springbootbasic.common.dto;
+
+public class ErrorResponse {
+
+    private final String code;
+    private final String message;
+
+    public ErrorResponse(String code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
--- src/main/java/com/prgrms/springbootbasic/common/exception/DataModifyingException.java
+++ src/main/java/com/prgrms/springbootbasic/common/exception/DataModifyingException.java
@@ -1,7 +1,19 @@
 package com.prgrms.springbootbasic.common.exception;
 
 public class DataModifyingException extends RuntimeException {
+    private final ErrorCode errorCode;
+
     public DataModifyingException(String message) {
         super(message);
+        this.errorCode = CommonErrorCode.DATA_MODIFYING_ERROR;
+    }
+
+    public DataModifyingException(String message, ErrorCode errorCode) {
+        super(message);
+        this.errorCode = errorCode;
+    }
+
+    public ErrorCode getErrorCode() {
+        return errorCode;
     }
 }
