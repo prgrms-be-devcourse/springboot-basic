76e5187938adddcb60730beb660cbd5f442b5f42
yeezze <yeeeze22@gmail.com>
2022-11-29 21:32:13 +0900

    refactor: json DTO 생성자 삭제

--- src/main/java/org/prgrms/voucherapplication/domain/voucher/controller/dto/CreateVoucherRequest.java
+++ src/main/java/org/prgrms/voucherapplication/domain/voucher/controller/dto/CreateVoucherRequest.java
@@ -2,22 +2,9 @@ package org.prgrms.voucherapplication.domain.voucher.controller.dto;
 
 import org.prgrms.voucherapplication.domain.voucher.entity.VoucherType;
 
-import java.util.UUID;
-
 public class CreateVoucherRequest {
-    private final UUID voucherId;
-    private final int discount;
-    private final String voucherType;
-
-    public CreateVoucherRequest(int discount, String voucherType) {
-        this.voucherId = UUID.randomUUID();
-        this.discount = discount;
-        this.voucherType = voucherType;
-    }
-
-    public UUID getVoucherId() {
-        return voucherId;
-    }
+    private int discount;
+    private String voucherType;
 
     public int getDiscount() {
         return discount;
