d5822063effa559c5f51017f983966dbdd6ba2ea
권주성 <99165624+JoosungKwon@users.noreply.github.com>
2022-11-30 23:35:32 +0900

    refactor: Service 레이어 테스트 코드 수정

--- src/test/java/com/programmers/kwonjoosung/springbootbasicjoosung/service/CustomerServiceTest.java
+++ src/test/java/com/programmers/kwonjoosung/springbootbasicjoosung/service/CustomerServiceTest.java
@@ -12,7 +12,6 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
 
 public class CustomerServiceTest {
@@ -24,69 +23,34 @@ public class CustomerServiceTest {
     @DisplayName("[성공] 고객을 생성할 수 있다")
     void createCustomer() {
         //given
-        UUID customerId = UUID.randomUUID();
-        String customerName = "test1";
-        Customer customer = new Customer(customerId, customerName);
-        Optional<Customer> createdCustomer;
-        //when
-        try (MockedStatic<UUID> utilities = mockStatic(UUID.class)) {
-            utilities.when(UUID::randomUUID).thenReturn(customerId);
-            when(customerRepositoryMock.insert(customer)).thenReturn(true);
-            createdCustomer = customerService.createCustomer(customerName);
-        }
-        //then
-        assertThat(createdCustomer).isNotEmpty();
-        assertThat(createdCustomer.get()).isEqualTo(customer);
-        verify(customerRepositoryMock).insert(customer);
-    }
-
-    @Test
-    @DisplayName("[실패] 동일한 id의 고객을 생성할 수 없다")
-    void createAlreadyExistCustomer() {
-        //given
-        Customer customer = new Customer(UUID.randomUUID(), "test2");
-        when(customerRepositoryMock.insert(customer)).thenReturn(false);
-        Optional<Customer> createdCustomer;
+        Customer customer = new Customer(UUID.randomUUID(), "test1");
         //when
         try (MockedStatic<UUID> utilities = mockStatic(UUID.class)) {
             utilities.when(UUID::randomUUID).thenReturn(customer.getCustomerId());
-            createdCustomer = customerService.createCustomer(customer.getName());
+            when(customerRepositoryMock.insert(customer)).thenReturn(customer);
+            customerService.createCustomer(customer.getName());
+            utilities.verify(UUID::randomUUID);
         }
         //then
-        assertThat(createdCustomer).isEmpty();
         verify(customerRepositoryMock).insert(customer);
     }
 
+
     @Test
-    @DisplayName("[성공] 고객을 Id로 조회할 수 있다")
+    @DisplayName("[성공] 고객을 Id로 조회하는 기능을 테스트 한다")
     void findCustomerByCustomerId() {
         //given
         UUID customerId = UUID.randomUUID();
         Customer customer = new Customer(customerId, "test3");
         when(customerRepositoryMock.findById(customerId)).thenReturn(Optional.of(customer));
         //when
-        Optional<Customer> foundCustomerById = customerService.findCustomerByCustomerId(customerId);
+        customerService.findCustomerByCustomerId(customerId);
         //then
-        assertThat(foundCustomerById).isNotEmpty();
-        assertThat(foundCustomerById.get()).isEqualTo(customer);
         verify(customerRepositoryMock).findById(customerId);
     }
 
     @Test
-    @DisplayName("[실패] 고객테이블에 없는 고객은 Id로 조회할 수 없다")
-    void findNotExistCustomerByCustomerId() {
-        //given
-        UUID customerId = UUID.randomUUID();
-        when(customerRepositoryMock.findById(customerId)).thenReturn(Optional.empty());
-        //when
-        Optional<Customer> foundCustomerById = customerService.findCustomerByCustomerId(customerId);
-        //then
-        assertThat(foundCustomerById).isEmpty();
-        verify(customerRepositoryMock).findById(customerId);
-    }
-
-    @Test
-    @DisplayName("[성공] 모든 고객을 조회할 수 있다")
+    @DisplayName("[성공] 모든 고객을 조회하는 기능을 테스트 한다")
     void findAllCustomers() {
         //given
         UUID customerId1 = UUID.randomUUID();
@@ -95,84 +59,33 @@ public class CustomerServiceTest {
         Customer customer2 = new Customer(customerId2, "test5");
         when(customerRepositoryMock.findAll()).thenReturn(List.of(customer1, customer2));
         //when
-        List<Customer> customers = customerService.getAllCustomers();
+        customerService.getAllCustomers();
         //then
-        assertThat(customers).isNotEmpty();
-        assertThat(customers).hasSize(2);
-        assertThat(customers).contains(customer1, customer2);
-        assertThat(customers.get(0)).isEqualTo(customer1);
-        assertThat(customers.get(1)).isEqualTo(customer2);
         verify(customerRepositoryMock).findAll();
     }
 
     @Test
-    @DisplayName("[실패] 고객 테이블이 비어 있다면 빈 리스트가 반환된다")
-    void findAllCustomersFromEmpty() {
-        //given
-        when(customerRepositoryMock.findAll()).thenReturn(List.of());
-        //when
-        List<Customer> customers = customerService.getAllCustomers();
-        //then
-        assertThat(customers).isEmpty();
-        verify(customerRepositoryMock).findAll();
-    }
-
-    @Test
-    @DisplayName("[성공] 고객 정보를 수정할 수 있다.")
+    @DisplayName("[성공] 고객 정보를 수정하는 기능을 테스트 한다")
     void updateCustomer() {
         //given
         UUID customerId = UUID.randomUUID();
         Customer customer = new Customer(customerId, "test6");
-        Customer newCustomer = new Customer(customerId, "test7");
-        when(customerRepositoryMock.findById(customerId)).thenReturn(Optional.of(customer));
-        when(customerRepositoryMock.update(newCustomer)).thenReturn(true);
+        when(customerRepositoryMock.update(customer)).thenReturn(customer);
         //when
-        boolean updateResult = customerService.updateCustomer(customerId, newCustomer.getName());
+        customerService.updateCustomer(customerId, customer.getName());
         //then
-        assertThat(updateResult).isTrue();
-        verify(customerRepositoryMock).findById(customerId);
-        verify(customerRepositoryMock).update(newCustomer);
+        verify(customerRepositoryMock).update(customer);
     }
 
     @Test
-    @DisplayName("[실패] 고객테이블에 없는 고객은 정보를 수정할 수 없다.")
-    void updateNotExistCustomer() {
-        //given
-        UUID customerId = UUID.randomUUID();
-        when(customerRepositoryMock.findById(customerId)).thenReturn(Optional.empty());
-        //when
-        boolean updateResult = customerService.updateCustomer(customerId, "test8");
-        //then
-        assertThat(updateResult).isFalse();
-        verify(customerRepositoryMock).findById(customerId);
-    }
-
-    @Test
-    @DisplayName("[성공] 고객을 삭제할 수 있다.")
+    @DisplayName("[성공] 고객을 삭제하는 기능을 테스트 한다")
     void deleteCustomer() {
         //given
         UUID customerId = UUID.randomUUID();
-        Customer customer = new Customer(customerId, "test10");
-        when(customerRepositoryMock.findById(customerId)).thenReturn(Optional.of(customer));
-        when(customerRepositoryMock.delete(customerId)).thenReturn(true);
         //when
-        boolean deleteResult = customerService.deleteCustomer(customerId);
+        customerService.deleteCustomer(customerId);
         //then
-        assertThat(deleteResult).isTrue();
-        verify(customerRepositoryMock).findById(customerId);
         verify(customerRepositoryMock).delete(customerId);
     }
 
-    @Test
-    @DisplayName("[실패] 고객테이블에 없는 고객은 삭제할 수 없다.")
-    void deleteNotExistCustomer() {
-        //given
-        UUID customerId = UUID.randomUUID();
-        when(customerRepositoryMock.findById(customerId)).thenReturn(Optional.empty());
-        //when
-        boolean deleteResult = customerService.deleteCustomer(customerId);
-        //then
-        assertThat(deleteResult).isFalse();
-        verify(customerRepositoryMock).findById(customerId);
-    }
 }
--- src/test/java/com/programmers/kwonjoosung/springbootbasicjoosung/service/VoucherServiceTest.java
+++ src/test/java/com/programmers/kwonjoosung/springbootbasicjoosung/service/VoucherServiceTest.java
@@ -1,5 +1,6 @@
 package com.programmers.kwonjoosung.springbootbasicjoosung.service;
 
+import com.programmers.kwonjoosung.springbootbasicjoosung.controller.voucher.request.CreateVoucherRequest;
 import com.programmers.kwonjoosung.springbootbasicjoosung.model.voucher.Voucher;
 import com.programmers.kwonjoosung.springbootbasicjoosung.model.voucher.VoucherFactory;
 import com.programmers.kwonjoosung.springbootbasicjoosung.model.voucher.VoucherType;
@@ -14,7 +15,6 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
 
 class VoucherServiceTest {
@@ -23,72 +23,37 @@ class VoucherServiceTest {
     private final VoucherService voucherService = new VoucherService(voucherRepositoryMock);
 
     @Test
-    @DisplayName("[성공] 바우처를 생성하고 저장할 수 있다")
+    @DisplayName("[성공] 바우처를 생성하고 저장하는 기능을 테스트 한다")
     void createAndSaveVoucherTest() {
         //given
         UUID voucherId = UUID.randomUUID();
         Voucher voucher = VoucherFactory.createVoucher(VoucherType.FIXED, voucherId, 1000);
-        Optional<Voucher> savedVoucher;
         //when
         try (MockedStatic<VoucherFactory> utilities = mockStatic(VoucherFactory.class)) {
             utilities.when(() -> VoucherFactory.createVoucher(VoucherType.FIXED, 1000)).thenReturn(voucher);
-            when(voucherRepositoryMock.insert(voucher)).thenReturn(true);
-            savedVoucher = voucherService.createVoucher(VoucherType.FIXED, 1000);
+            when(voucherRepositoryMock.insert(voucher)).thenReturn(voucher);
+            voucherService.saveVoucher(new CreateVoucherRequest(VoucherType.FIXED.name(), 1000));
+            utilities.verify(() -> VoucherFactory.createVoucher(VoucherType.FIXED, 1000));
         }
         //then
-        verify(voucherRepositoryMock).insert(voucher);
-        assertThat(savedVoucher).isNotEmpty();
-        assertThat(savedVoucher.get()).isEqualTo(voucher);
-    }
-
-    @Test
-    @DisplayName("[실패] 같은 바우처를 중복해서 저장할 수 없다")
-    void createAndSaveSameVoucherTest() {
-        //given
-        UUID voucherId = UUID.randomUUID();
-        Voucher voucher = VoucherFactory.createVoucher(VoucherType.FIXED, voucherId, 1000);
-        Optional<Voucher> savedVoucher;
-        //when
-        try (MockedStatic<VoucherFactory> utilities = mockStatic(VoucherFactory.class)) {
-            utilities.when(() -> VoucherFactory.createVoucher(VoucherType.FIXED, 1000)).thenReturn(voucher);
-            when(voucherRepositoryMock.insert(voucher)).thenReturn(false);
-            savedVoucher = voucherService.createVoucher(VoucherType.FIXED, 1000);
-        }
-        //then
-        assertThat(savedVoucher).isEmpty();
         verify(voucherRepositoryMock).insert(voucher);
     }
 
     @Test
-    @DisplayName("[성공] 바우처id를 통해 바우처 정보를 조회할 수 있다")
+    @DisplayName("[성공] 바우처id를 통해 바우처 정보를 조회하는 기능을 테스트 한다")
     void findVoucherByVoucherId() {
         //given
         UUID voucherId = UUID.randomUUID();
         Voucher voucher = VoucherFactory.createVoucher(VoucherType.FIXED, voucherId, 1000);
         when(voucherRepositoryMock.findById(voucherId)).thenReturn(Optional.of(voucher));
         //when
-        Optional<Voucher> foundVoucher = voucherService.findVoucher(voucherId);
+        voucherService.findVoucher(voucherId);
         //then
-        assertThat(foundVoucher).isNotEmpty();
-        assertThat(foundVoucher.get()).isEqualTo(voucher);
         verify(voucherRepositoryMock).findById(voucherId);
     }
 
     @Test
-    @DisplayName("[실패] 바우처id가 없다면 바우처 정보를 조회할 수 없다")
-    void findVoucherByNotExistVoucherId() {
-        //given
-        UUID voucherId = UUID.randomUUID();
-        when(voucherRepositoryMock.findById(voucherId)).thenReturn(Optional.empty());
-        //when
-        Optional<Voucher> foundVoucher = voucherService.findVoucher(voucherId);
-        //then
-        assertThat(foundVoucher).isEmpty();
-        verify(voucherRepositoryMock).findById(voucherId);
-    }
-
-    @Test
-    @DisplayName("[성공] 현재 테이블에 저장된 모든 바우처를 조회할 수 있다")
+    @DisplayName("[성공] 현재 테이블에 저장된 모든 바우처를 조회하는 기능을 테스트 한다")
     void getAllVoucherFromVoucherTable() {
         //given
         UUID voucherId1 = UUID.randomUUID();
@@ -97,86 +62,37 @@ class VoucherServiceTest {
         Voucher voucher2 = VoucherFactory.createVoucher(VoucherType.PERCENT, voucherId2, 10);
         when(voucherRepositoryMock.findAll()).thenReturn(List.of(voucher1, voucher2));
         //when
-        List<Voucher> vouchers = voucherService.getAllVouchers();
+        voucherService.getAllVouchers();
         //then
-        assertThat(vouchers).isNotEmpty();
-        assertThat(vouchers).containsExactly(voucher1, voucher2);
-        assertThat(voucher1).isEqualTo(vouchers.get(0));
-        assertThat(voucher2).isEqualTo(vouchers.get(1));
         verify(voucherRepositoryMock).findAll();
     }
 
     @Test
-    @DisplayName("[성공] 현재 테이블에 저장된 바우처가 없다면 바우처를 조회할 수 없다")
-    void getAllVoucherFromEmpty() {
-        //given
-        when(voucherRepositoryMock.findAll()).thenReturn(List.of());
-        //when
-        List<Voucher> vouchers = voucherService.getAllVouchers();
-        //then
-        assertThat(vouchers).isEmpty();
-        verify(voucherRepositoryMock).findAll();
-    }
-
-    @Test
-    @DisplayName("[성공] 바우처 테이블에 존재하는 바우처는 수정할 수 있다")
+    @DisplayName("[성공] 바우처 테이블에 존재하는 바우처는 수정하는 기능을 테스트 한다.")
     void updateVoucherInfo() {
         //given
         UUID voucherId = UUID.randomUUID();
         Voucher voucher = VoucherFactory.createVoucher(VoucherType.PERCENT, voucherId, 30);
-        when(voucherRepositoryMock.findById(voucherId)).thenReturn(Optional.of(voucher));
-        when(voucherRepositoryMock.update(voucher)).thenReturn(true);
+        when(voucherRepositoryMock.update(voucher)).thenReturn(voucher);
         //when
-        boolean updateResult;
+
         try (MockedStatic<VoucherFactory> utilities = mockStatic(VoucherFactory.class)) {
             utilities.when(() -> VoucherFactory.createVoucher(VoucherType.FIXED,voucherId, 1000)).thenReturn(voucher);
-            updateResult = voucherService.updateVoucher(voucher.getVoucherId(), VoucherType.FIXED, 1000);
+            voucherService.updateVoucher(voucher.getVoucherId(), VoucherType.FIXED, 1000);
         }
         //then
-        assertThat(updateResult).isTrue();
-        verify(voucherRepositoryMock).findById(voucherId);
         verify(voucherRepositoryMock).update(voucher);
     }
 
     @Test
-    @DisplayName("[실패] 바우처 테이블에 존재하지 않는 바우처는 수정할 수 없다")
-    void updateNotExistVoucher() {
-        //given
-        UUID voucherId = UUID.randomUUID();
-        when(voucherRepositoryMock.findById(voucherId)).thenReturn(Optional.empty());
-        //when
-        boolean updateResult= voucherService.updateVoucher(voucherId, VoucherType.FIXED, 1000);
-        //then
-        assertThat(updateResult).isFalse();
-        verify(voucherRepositoryMock).findById(voucherId);
-    }
-
-    @Test
-    @DisplayName("[성공] 바우처를 삭제할 수 있다")
+    @DisplayName("[성공] 바우처를 삭제하는 기능을 테스트 한다.")
     void deleteVoucher() {
         //given
         UUID voucherId = UUID.randomUUID();
-        Voucher voucher = VoucherFactory.createVoucher(VoucherType.FIXED, voucherId, 1000);
-        when(voucherRepositoryMock.findById(voucherId)).thenReturn(Optional.of(voucher));
-        when(voucherRepositoryMock.deleteById(voucherId)).thenReturn(true);
         //when
-        boolean deleteResult = voucherService.deleteVoucher(voucherId);
+        voucherService.deleteVoucher(voucherId);
         //then
-        assertThat(deleteResult).isTrue();
         verify(voucherRepositoryMock).deleteById(voucherId);
-    }
-
-    @Test
-    @DisplayName("[실패] 존재하지 않는 바우처를 삭제할 수 없다")
-    void deleteVoucherNotExist() {
-        //given
-        UUID voucherId = UUID.randomUUID();
-        when(voucherRepositoryMock.findById(voucherId)).thenReturn(Optional.empty());
-        //when
-        boolean deleteResult = voucherService.deleteVoucher(voucherId);
-        //then
-        assertThat(deleteResult).isFalse();
-        verify(voucherRepositoryMock).findById(voucherId);
     }
 
 }
