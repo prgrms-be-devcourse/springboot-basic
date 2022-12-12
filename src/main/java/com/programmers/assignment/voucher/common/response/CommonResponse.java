ae139e0b180ebbce12b96e619018b6021d5f4667
goseungwon <knmy0925@gmail.com>
2022-12-12 22:03:45 +0900

    [Refactor] response code 제거

--- src/main/java/com/programmers/assignment/voucher/common/response/CommonResponse.java
+++ src/main/java/com/programmers/assignment/voucher/common/response/CommonResponse.java
@@ -6,21 +6,18 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import java.time.LocalDateTime;
 
 public class CommonResponse<T> {
-    private int code;
-
     @JsonInclude(JsonInclude.Include.NON_NULL)
     private T result;
-
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
     private LocalDateTime serverDatetime;
 
     public CommonResponse(T result) {
-        this.code = ResponseCode.SUCCESS.getCode();
         this.result = result;
+        serverDatetime = LocalDateTime.now();
     }
 
-    public CommonResponse(ResponseCode errCode) {
-        this.code = errCode.getCode();
+    public CommonResponse(ResponseCode responseCode) {
+        serverDatetime = LocalDateTime.now();
     }
 
 }
--- src/main/java/com/programmers/assignment/voucher/common/response/ResponseCode.java
+++ src/main/java/com/programmers/assignment/voucher/common/response/ResponseCode.java
@@ -1,22 +1,10 @@
 package com.programmers.assignment.voucher.common.response;
 
-import org.springframework.http.HttpStatus;
-
 public enum ResponseCode {
-    SUCCESS(HttpStatus.OK.value()),
+    SUCCESS(),
 
-    NOT_FOUND_CUSTOMER(HttpStatus.NOT_FOUND.value()),
-    NOT_FOUND_VOUCHER(HttpStatus.NOT_FOUND.value()),
-    INVALID_DISCOUNT_VALUE(HttpStatus.BAD_REQUEST.value()),
-    NOT_AVAILABLE_VOUCHER(HttpStatus.BAD_REQUEST.value());
-
-    private int code;
-
-    ResponseCode(int code) {
-        this.code = code;
-    }
-
-    public int getCode() {
-        return code;
-    }
+    NOT_FOUND_CUSTOMER(),
+    NOT_FOUND_VOUCHER(),
+    INVALID_DISCOUNT_VALUE(),
+    NOT_AVAILABLE_VOUCHER();
 }
