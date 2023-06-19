407d45e4388c2e7b0466c68e51547b6503a0c778
hongxeob <sjun2918@naver.com>
2023-06-19 18:13:04 +0900

    init: 프로젝트 생성

---
+++ src/test/java/org/promgrammers/springbootbasic/SpringbootBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package org.promgrammers.springbootbasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBasicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
