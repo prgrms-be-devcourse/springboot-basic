6d726d0d516a0506e81cd79d9b8e532029cce32e
bona <ra_ari@naver.com>
2023-06-19 20:58:24 +0900

    Initial commit

---
+++ src/main/java/org/prgrms/kdt/OrderTester.java
@@ -0,0 +1,19 @@
+package org.prgrms.kdt;
+
+import org.springframework.util.Assert;
+
+import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.UUID;
+
+public class OrderTester {
+    public static void main(String[] args) {
+        var customerId = UUID.randomUUID();
+        var orderItems = new ArrayList<OrderItem>(){{
+            add(new OrderItem(UUID.randomUUID(), 100L, 1));
+        }};
+        var fixedAmountVoucher = new FixedAmountVoucher(UUID.randomUUID(), 10L);
+        var order = new Order(UUID.randomUUID(), customerId, orderItems, fixedAmountVoucher);
+        Assert.isTrue(order.totalAmount() == 90L, MessageFormat.format("total Amount {0} is not 90L", order.totalAmount()));
+    }
+}
