37ae3a049026084b0d6b1cdba5db5fa8319d326e
weonest <geonhee33@naver.com>
2023-06-20 17:40:08 +0900

    feat: OutputView 추가

---
+++ src/main/java/com/programmers/voucher/view/OutputView.java
@@ -0,0 +1,13 @@
+package com.programmers.voucher.view;
+
+public class OutputView {
+
+    public void println(String message) {
+        System.out.println(message);
+    }
+
+    public void print(String message) {
+        System.out.println(message);
+        
+    }
+}
