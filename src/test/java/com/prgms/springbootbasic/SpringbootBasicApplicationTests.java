de2c2f6828b51f662c2e574f40bf577545cfebaf
heenahan <mby1997s@gmail.com>
2023-06-20 17:35:15 +0900

    init : 스프링 프로젝트 초기화

---
+++ src/test/java/com/prgms/springbootbasic/SpringbootBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prgms.springbootbasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
