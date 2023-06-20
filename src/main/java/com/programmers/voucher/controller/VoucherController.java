c282939035aa683a210c54c8ebcb03aeb51266f6
weonest <geonhee33@naver.com>
2023-06-20 17:43:57 +0900

    feat: VoucherController 추가

---
+++ src/main/java/com/programmers/voucher/controller/VoucherController.java
@@ -0,0 +1,12 @@
+package com.programmers.voucher.controller;
+
+public class VoucherController {
+    private boolean isRunning = true;
+
+
+    public void run() {
+        while (isRunning) {
+
+        }
+    }
+}
