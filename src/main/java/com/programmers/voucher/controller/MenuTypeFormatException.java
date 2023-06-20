95fd0ef788bd34158aebb63156dedd493cbe8dbd
weonest <geonhee33@naver.com>
2023-06-20 18:24:09 +0900

    feat: MenuTypeFormatException 추가

---
+++ src/main/java/com/programmers/voucher/controller/MenuTypeFormatException.java
@@ -0,0 +1,9 @@
+package com.programmers.voucher.controller;
+
+public class MenuTypeFormatException extends RuntimeException {
+    private static final String MESSAGE = "[ERROR] 올바른 메뉴 형식을 입력해주세요. ";
+
+    public MenuTypeFormatException() {
+        super(MESSAGE);
+    }
+}
