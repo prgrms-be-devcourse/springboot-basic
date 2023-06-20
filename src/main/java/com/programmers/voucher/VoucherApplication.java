6711558301187257a74a12ac121b42b8fe4954ae
weonest <geonhee33@naver.com>
2023-06-20 18:18:24 +0900

    feat: VoucherController 동작하도록 ApplicationContext 추가

--- src/main/java/com/programmers/voucher/VoucherApplication.java
+++ src/main/java/com/programmers/voucher/VoucherApplication.java
@@ -1,13 +1,16 @@
 package com.programmers.voucher;
 
+import com.programmers.voucher.controller.VoucherController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class VoucherApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(VoucherApplication.class, args);
+        ApplicationContext applicationContext = SpringApplication.run(VoucherApplication.class, args);
+        applicationContext.getBean(VoucherController.class).run();
     }
 
 }
--- src/main/java/com/programmers/voucher/controller/VoucherController.java
+++ src/main/java/com/programmers/voucher/controller/VoucherController.java
@@ -1,12 +1,17 @@
 package com.programmers.voucher.controller;
 
+import com.programmers.voucher.uitl.MenuType;
+import com.programmers.voucher.view.Console;
+import org.springframework.stereotype.Component;
+
+@Component
 public class VoucherController {
     private boolean isRunning = true;
-
+    private Console console = new Console();
 
     public void run() {
         while (isRunning) {
-
+            MenuType.getCommand(console.getRequest());
         }
     }
 }
