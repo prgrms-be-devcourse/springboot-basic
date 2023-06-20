adb70aba817bd5b23241d390ab37bd3f6ece975b
SY97P <onetuks@naver.com>
2023-06-20 22:58:03 +0900

    feat: Main 메소드 연결

--- src/main/java/com/devcourse/springbootbasic/SpringbootBasicApplication.java
+++ src/main/java/com/devcourse/springbootbasic/SpringbootBasicApplication.java
@@ -1,5 +1,8 @@
 package com.devcourse.springbootbasic;
 
+import com.devcourse.springbootbasic.engine.Platform;
+import com.devcourse.springbootbasic.engine.io.InputConsole;
+import com.devcourse.springbootbasic.engine.io.OutputConsole;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +10,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootBasicApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootBasicApplication.class, args);
+//		SpringApplication.run(SpringbootBasicApplication.class, args);
+		new Platform(
+				new InputConsole(),
+				new OutputConsole()
+		).run();
 	}
 
 }
