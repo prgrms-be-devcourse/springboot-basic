7afdc9b2ef8008edb46cb1f9c5a07b8ecdebbe2c
weonest <geonhee33@naver.com>
2023-06-20 18:14:47 +0900

    test: 메뉴타입 반환 확인 테스트

---
+++ src/test/java/com/programmers/voucher/uitl/MenuTypeTest.java
@@ -0,0 +1,26 @@
+package com.programmers.voucher.uitl;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MenuTypeTest {
+
+    @DisplayName("MenuType 반환 테스트")
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "create"
+            , "CREATE"
+            , "list"
+            , "List"
+            , "EXIT"
+            , "exit"
+            , "abc"
+    })
+    void 메뉴타입_반환_테스트(String command) {
+        assertThatThrownBy(() -> MenuType.getCommand(command))
+                .isInstanceOf(RuntimeException.class);
+    }
+}
