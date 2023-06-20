15922cd74e15ce57c71684a25f4b028191ce21f4
weonest <geonhee33@naver.com>
2023-06-20 17:37:49 +0900

    refactor: ConsoleMessage -> Menu 이름 변경

--- src/main/java/com/programmers/voucher/uitl/Menu.java
+++ src/main/java/com/programmers/voucher/uitl/Menu.java
@@ -1,16 +1,16 @@
-package com.programmers.voucher;
+package com.programmers.voucher.uitl;
 
-public enum ConsoleMessage {
+public enum Menu {
     COMMAND_MESSAGE("=== Voucher Program ===\n" +
             "Type **exit** to exit the program.\n" +
             "Type **create** to create a new voucher.\n" +
             "Type **list** to list all vouchers."),
     CREATE_VOUCHER_TYPE_MESSAGE("Select a voucher type\n" +
-            "1:FixedAmountVoucher 2:PercentDiscountVoucher");
+            "1:FixedAmountVoucher 2:PKercentDiscountVoucher");
 
     private final String message;
 
-    ConsoleMessage(String message) {
+    Menu(String message) {
         this.message = message;
     }
 }
