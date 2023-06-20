af27d1c465895017f7a91508b6cd8c3380276bb0
changhyeon <changhyeon.h@kakao.com>
2023-06-20 15:09:04 +0900

    feat: 프로젝트 초기화

---
+++ src/main/java/com/programmers/springweekly/SpringWeeklyApplication.java
@@ -0,0 +1,13 @@
+package com.programmers.springweekly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWeeklyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWeeklyApplication.class, args);
+	}
+
+}
