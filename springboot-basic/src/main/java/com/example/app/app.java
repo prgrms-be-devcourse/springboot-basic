5d9e40af8a0aa2b84aabd940396b145cef7e60bf
baejunil <bjo6300@naver.com>
2023-06-20 22:53:16 +0900

    chore : springboot 초기 세팅, .gitignore 설정

---
+++ springboot-basic/src/main/java/com/example/app/app.java
@@ -0,0 +1,13 @@
+package com.example.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class app {
+
+	public static void main(String[] args) {
+		SpringApplication.run(app.class, args);
+	}
+
+}
