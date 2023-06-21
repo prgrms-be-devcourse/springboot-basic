692216ca6e68756992cc7610b96c7ffc00ec2b57
jun <alap_u@naver.com>
2023-06-21 18:33:25 +0900

    style 예외 메시지 상수화

--- src/main/java/com/devmission/springbootbasic/view/console/Command.java
+++ src/main/java/com/devmission/springbootbasic/view/console/Command.java
@@ -8,10 +8,12 @@ public enum Command {
     LIST,
     EXIT;
 
+    private static final String INVALID_COMMAND_MESSAGE = "선택할 수 없는 명령입니다.";
+
     public static Command from(String name) {
         return Arrays.stream(values()).filter(command -> command.name().equals(name.toUpperCase()))
                 .findFirst()
-                .orElseThrow(() -> new IllegalArgumentException("선택할 수 없는 명령입니다."));
+                .orElseThrow(() -> new IllegalArgumentException(INVALID_COMMAND_MESSAGE));
     }
 
 }
--- src/main/java/com/devmission/springbootbasic/voucher/VoucherType.java
+++ src/main/java/com/devmission/springbootbasic/voucher/VoucherType.java
@@ -7,10 +7,12 @@ public enum VoucherType {
     FIXED,
     PERCENT;
 
+    private static final String INVALID_VOUCHER_TYPE_MESSAGE = "선택할 수 없는 타입입니다.";
+
     public static VoucherType from(String name) {
         return Arrays.stream(values()).filter(voucherType -> voucherType.name().equals(name.toUpperCase()))
                 .findFirst()
-                .orElseThrow(() -> new IllegalArgumentException("선택할 수 없는 타입입니다."));
+                .orElseThrow(() -> new IllegalArgumentException(INVALID_VOUCHER_TYPE_MESSAGE));
     }
 
 }
