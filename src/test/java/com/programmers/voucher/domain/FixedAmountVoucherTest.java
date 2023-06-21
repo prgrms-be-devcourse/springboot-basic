c71f1b6abd2c50b1e51c1ac6c63c801165b64043
yumyeonghan <mgtmh991013@gmail.com>
2023-06-21 15:24:46 +0900

    test: Voucher 테스트 생성
    - discount(): 해당 Voucher에 맞트 할인된 금액 반환 테스

---
+++ src/test/java/com/programmers/voucher/domain/FixedAmountVoucherTest.java
@@ -0,0 +1,25 @@
+package com.programmers.voucher.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class FixedAmountVoucherTest {
+    public static final int FIXED_DISCOUNT_AMOUNT = 100;
+    public static final int ORIGINAL_AMOUNT = 1000;
+    public static final int FINAL_AMOUNT = 900;
+    Voucher voucher;
+
+    @BeforeEach
+    void 초기화() {
+        voucher = new FixedAmountVoucher(UUID.randomUUID(), FIXED_DISCOUNT_AMOUNT);
+    }
+
+    @Test
+    void discount() {
+        assertThat(voucher.discount(ORIGINAL_AMOUNT)).isEqualTo(FINAL_AMOUNT);
+    }
+}
---
+++ src/test/java/com/programmers/voucher/domain/PercentDiscountVoucherTest.java
@@ -0,0 +1,25 @@
+package com.programmers.voucher.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class PercentDiscountVoucherTest {
+    public static final int PERCENT_DISCOUNT_AMOUNT = 10;
+    public static final int ORIGINAL_AMOUNT = 1000;
+    public static final int FINAL_AMOUNT = 900;
+    Voucher voucher;
+
+    @BeforeEach
+    void 초기화() {
+        voucher = new PercentDiscountVoucher(UUID.randomUUID(), PERCENT_DISCOUNT_AMOUNT);
+    }
+
+    @Test
+    void discount() {
+        assertThat(voucher.discount(ORIGINAL_AMOUNT)).isEqualTo(FINAL_AMOUNT);
+    }
+}
