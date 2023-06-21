1ee78afe4f5f77fb9c2f2ba23c577b1b11f35ad1
player-geun <player_geun@naver.com>
2023-06-21 17:09:16 +0900

    feat : 프로젝트를 CLI를 통해 초기화

---
+++ src/test/java/org/programers/vouchermanagement/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.programers.vouchermanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
