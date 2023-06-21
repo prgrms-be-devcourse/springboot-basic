616d191ea10e2e8ba8d4e4361d26705bbe2a7aad
bombo-dev <bombo96@chungwoon.ac.kr>
2023-06-21 23:11:18 +0900

    init : 프로젝트 생성

---
+++ src/test/java/com/prgms/VoucherApp/VoucherAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prgms.VoucherApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoucherAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
