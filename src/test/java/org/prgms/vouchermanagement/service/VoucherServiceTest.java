b607b06e31324fd4e294277116e5293a98503dad
ymkim97 <rladudaud97@naver.com>
2023-06-21 20:24:07 +0900

    test: VoucherInputVaildator 테스트 코드 작성
    - VoucherService createNewVoucher 테스트 코드 작성 예정

---
+++ src/test/java/org/prgms/vouchermanagement/service/VoucherServiceTest.java
@@ -0,0 +1,5 @@
+package org.prgms.vouchermanagement.service;
+
+class VoucherServiceTest {
+
+}
--- src/test/java/org/prgms/vouchermanagement/validator/VoucherInputValidatorTest.java
+++ src/test/java/org/prgms/vouchermanagement/validator/VoucherInputValidatorTest.java
@@ -1,11 +1,13 @@
 package org.prgms.vouchermanagement.validator;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.prgms.vouchermanagement.constant.ExceptionMessageConstant;
 
 import java.util.InputMismatchException;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class VoucherInputValidatorTest {
 
@@ -20,15 +22,15 @@ class VoucherInputValidatorTest {
         String input3 = "";
 
         //when, then
-        Assertions.assertThatThrownBy(() -> validator.checkVoucherTypeInput(input1))
+        assertThatThrownBy(() -> validator.checkVoucherTypeInput(input1))
                 .isInstanceOf(InputMismatchException.class)
                 .hasMessageContaining(ExceptionMessageConstant.VOUCHER_TYPE_INPUT_EXCEPTION);
 
-        Assertions.assertThatThrownBy(() -> validator.checkVoucherTypeInput(input2))
+        assertThatThrownBy(() -> validator.checkVoucherTypeInput(input2))
                 .isInstanceOf(InputMismatchException.class)
                 .hasMessageContaining(ExceptionMessageConstant.VOUCHER_TYPE_INPUT_EXCEPTION);
 
-        Assertions.assertThatThrownBy(() -> validator.checkVoucherTypeInput(input3))
+        assertThatThrownBy(() -> validator.checkVoucherTypeInput(input3))
                 .isInstanceOf(InputMismatchException.class)
                 .hasMessageContaining(ExceptionMessageConstant.VOUCHER_TYPE_INPUT_EXCEPTION);
 
@@ -42,10 +44,106 @@ class VoucherInputValidatorTest {
         String input2 = "2";
 
         //when, then
-        Assertions.assertThatCode(() -> validator.checkVoucherTypeInput(input1))
+        assertThatCode(() -> validator.checkVoucherTypeInput(input1))
                 .doesNotThrowAnyException();
 
-        Assertions.assertThatCode(() -> validator.checkVoucherTypeInput(input2))
+        assertThatCode(() -> validator.checkVoucherTypeInput(input2))
+                .doesNotThrowAnyException();
+
+    }
+
+    @Test
+    @DisplayName("FixedAmountVoucher Amount 입력 Exception")
+    void checkFixedAmount_Exception_Test() {
+        //given
+        String input1 = "";
+        String input2 = "-30";
+        String input3 = "abc";
+
+        //when, then
+        assertThatThrownBy(() -> validator.checkFixedAmount(input1))
+                .isInstanceOf(InputMismatchException.class)
+                .hasMessageContaining(ExceptionMessageConstant.FIXED_VOUCHER_AMOUNT_INPUT_EXCEPTION);
+
+        assertThatThrownBy(() -> validator.checkFixedAmount(input2))
+                .isInstanceOf(InputMismatchException.class)
+                .hasMessageContaining(ExceptionMessageConstant.FIXED_VOUCHER_AMOUNT_INPUT_EXCEPTION);
+
+        assertThatThrownBy(() -> validator.checkFixedAmount(input3))
+                .isInstanceOf(InputMismatchException.class)
+                .hasMessageContaining(ExceptionMessageConstant.FIXED_VOUCHER_AMOUNT_INPUT_EXCEPTION);
+
+    }
+
+    @Test
+    @DisplayName("FixedAmountVoucher Amount 입력 Test")
+    void checkFixedAmount_Test() {
+        //given
+        String input1 = "1000";
+        String input2 = "5000";
+        String input3 = "300";
+
+        //when, then
+        assertThatCode(() -> validator.checkFixedAmount(input1))
+                .doesNotThrowAnyException();
+
+        assertThatCode(() -> validator.checkFixedAmount(input2))
+                .doesNotThrowAnyException();
+
+        assertThatCode(() -> validator.checkFixedAmount(input3))
+                .doesNotThrowAnyException();
+
+
+    }
+
+    @Test
+    @DisplayName("PercentDiscountVoucher Percent 입력 Exception")
+    void checkPercent_Exception_Test() {
+        //given
+        String input1 = "";
+        String input2 = "-30";
+        String input3 = "abc";
+        String input4 = "150";
+
+        //when, then
+        assertThatThrownBy(() -> validator.checkPercent(input1))
+                .isInstanceOf(InputMismatchException.class)
+                .hasMessageContaining(ExceptionMessageConstant.PERCENT_DISCOUNT_INPUT_EXCEPTION);
+
+        assertThatThrownBy(() -> validator.checkPercent(input2))
+                .isInstanceOf(InputMismatchException.class)
+                .hasMessageContaining(ExceptionMessageConstant.PERCENT_DISCOUNT_INPUT_EXCEPTION);
+
+        assertThatThrownBy(() -> validator.checkPercent(input3))
+                .isInstanceOf(InputMismatchException.class)
+                .hasMessageContaining(ExceptionMessageConstant.PERCENT_DISCOUNT_INPUT_EXCEPTION);
+
+        assertThatThrownBy(() -> validator.checkPercent(input4))
+                .isInstanceOf(InputMismatchException.class)
+                .hasMessageContaining(ExceptionMessageConstant.PERCENT_DISCOUNT_INPUT_EXCEPTION);
+
+    }
+
+    @Test
+    @DisplayName("PercentDiscountVoucher Percent 입력 Test")
+    void checkPercent_Test() {
+        //given
+        String input1 = "1";
+        String input2 = "99";
+        String input3 = "49";
+        String input4 = "30";
+
+        //when, then
+        assertThatCode(() -> validator.checkPercent(input1))
+                .doesNotThrowAnyException();
+
+        assertThatCode(() -> validator.checkPercent(input2))
+                .doesNotThrowAnyException();
+
+        assertThatCode(() -> validator.checkPercent(input3))
+                .doesNotThrowAnyException();
+
+        assertThatCode(() -> validator.checkPercent(input4))
                 .doesNotThrowAnyException();
 
     }
