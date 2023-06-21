48ccee150781a666f6993ff3f526004aa4120f2b
hyee0715 <hyee0715@naver.com>
2023-06-21 23:01:57 +0900

    Initial commit

---
+++ src/test/java/com/programmers/ApplicationTest.java
@@ -0,0 +1,15 @@
+package com.programmers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
