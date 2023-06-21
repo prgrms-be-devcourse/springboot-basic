23899b2feeff857ac4003226eea96b6d29562bfe
Juwoong <wwong7247@gmail.com>
2023-06-21 21:30:12 +0900

    chore : 초기 프로젝트 생성

---
+++ voucher/src/test/java/com/example/voucher/VoucherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.voucher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoucherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
