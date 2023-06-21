5c28f125701855b99419a4f9ad8792e660d45525
yumyeonghan <mgtmh991013@gmail.com>
2023-06-21 16:13:01 +0900

    test: VoucherRepository 테스트
    - save(): Voucher 저장 기능 테스트
    - findAll(): 모든 Voucher 조회 기능 테스트

---
+++ src/test/java/com/programmers/voucher/repository/MemoryVoucherRepositoryTest.java
@@ -0,0 +1,54 @@
+package com.programmers.voucher.repository;
+
+import com.programmers.voucher.domain.FixedAmountVoucher;
+import com.programmers.voucher.domain.PercentDiscountVoucher;
+import com.programmers.voucher.domain.Voucher;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.UUID;
+
+class MemoryVoucherRepositoryTest {
+    public static final int FIXED_DISCOUNT_AMOUNT = 100;
+    public static final int PERCENT_DISCOUNT_AMOUNT = 10;
+    VoucherRepository voucherRepository;
+
+    @BeforeEach
+    void 초기화() {
+        voucherRepository = new MemoryVoucherRepository();
+    }
+
+    @Test
+    void save() {
+        //give
+        UUID voucherId = UUID.randomUUID();
+        Voucher voucher = new FixedAmountVoucher(voucherId, FIXED_DISCOUNT_AMOUNT);
+
+        //when
+        voucherRepository.save(voucher);
+
+        //then
+        Voucher savedVoucher = voucherRepository.findByVoucherId(voucherId).get();
+        Assertions.assertThat(voucher).isEqualTo(savedVoucher);
+    }
+
+    @Test
+    void findAll() {
+        //give
+        UUID voucherId1 = UUID.randomUUID();
+        Voucher voucher1 = new FixedAmountVoucher(voucherId1, FIXED_DISCOUNT_AMOUNT);
+
+        UUID voucherId2 = UUID.randomUUID();
+        Voucher voucher2 = new PercentDiscountVoucher(voucherId2, PERCENT_DISCOUNT_AMOUNT);
+
+        //when
+        voucherRepository.save(voucher1);
+        voucherRepository.save(voucher2);
+        List<Voucher> voucherList = voucherRepository.findAll();
+
+        //then
+        Assertions.assertThat(voucherList.size()).isEqualTo(2);
+    }
+}
