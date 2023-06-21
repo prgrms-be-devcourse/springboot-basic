9e16c040834a86a3812426b6d9da649d15f9644a
SY97P <onetuks@naver.com>
2023-06-21 18:28:03 +0900

    refactor: 출력 책임 OutputConsole 이동

--- src/main/java/com/devcourse/springbootbasic/engine/io/InputConsole.java
+++ src/main/java/com/devcourse/springbootbasic/engine/io/InputConsole.java
@@ -1,29 +1,18 @@
 package com.devcourse.springbootbasic.engine.io;
 
-import com.devcourse.springbootbasic.engine.exception.InvalidDataException;
 import com.devcourse.springbootbasic.engine.model.Menu;
-import org.beryx.textio.InputReader;
 import org.beryx.textio.TextIO;
 import org.beryx.textio.TextIoFactory;
-import org.beryx.textio.TextTerminal;
+import org.springframework.stereotype.Component;
 
-import java.util.List;
-import java.util.Scanner;
-
+@Component
 public class InputConsole {
 
     private TextIO input = TextIoFactory.getTextIO();
 
     public Menu inputMenu() {
-        Menu menu = input.newEnumInputReader(Menu.class)
-                .read("=== Voucher Program ===");
-//        System.out.println();
-//        System.out.println("Type exit to exit the program.");
-//        System.out.println("Type create to create a new voucher.");
-//        System.out.println("Type list to list all vouchers.");
-//        System.out.println("=======================");
-//        System.out.print("Menu Type : ");
-        return menu;
+        return input.newEnumInputReader(Menu.class)
+                .read();
     }
 
     public String inputVoucher() {
--- src/main/java/com/devcourse/springbootbasic/engine/io/OutputConsole.java
+++ src/main/java/com/devcourse/springbootbasic/engine/io/OutputConsole.java
@@ -4,13 +4,22 @@ import com.devcourse.springbootbasic.engine.exception.InvalidDataException;
 import com.devcourse.springbootbasic.engine.voucher.Voucher;
 import org.beryx.textio.TextIoFactory;
 import org.beryx.textio.TextTerminal;
+import org.springframework.stereotype.Component;
 
+@Component
 public class OutputConsole {
 
     private TextTerminal output = TextIoFactory.getTextTerminal();
 
     public void printLine() {
         output.println();
+    }
+
+    public void printMenus() {
+        printMessage("=== Voucher Program ===");
+        printMessage("Type exit to exit the program.");
+        printMessage("Type create to create a new voucher.");
+        printMessage("Type list to list all vouchers.");
     }
 
     public void printMessage(String message) {
