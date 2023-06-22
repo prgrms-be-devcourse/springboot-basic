cb75f604ad5aae0a65bd9ffbd1fe8a63eb6edf80
HyuckJuneHong <jjj8337721@naver.com>
2023-06-22 22:47:18 +0900

    feat : Spring Boot CLI를 통해 프로젝트 초기화

---
+++ src/main/java/kr/co/springbootweeklymission/VoucherApplication.java
@@ -0,0 +1,12 @@
+package kr.co.springbootweeklymission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoucherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VoucherApplication.class, args);
+    }
+}
