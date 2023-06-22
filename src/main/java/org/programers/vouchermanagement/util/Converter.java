18f3409f8f2253953485b8b24285d7deee83472c
player-geun <player_geun@naver.com>
2023-06-22 16:55:55 +0900

    refactor : Converter 를 더 범용적으로 사용하기 위해 이름 변경

--- src/main/java/org/programers/vouchermanagement/util/Converter.java
+++ src/main/java/org/programers/vouchermanagement/util/Converter.java
@@ -1,5 +1,8 @@
 package org.programers.vouchermanagement.util;
 
+import org.programers.vouchermanagement.member.domain.Member;
+import org.programers.vouchermanagement.member.domain.MemberStatus;
+import org.programers.vouchermanagement.member.dto.MemberResponse;
 import org.programers.vouchermanagement.voucher.domain.FixedAmountPolicy;
 import org.programers.vouchermanagement.voucher.domain.PercentDiscountPolicy;
 import org.programers.vouchermanagement.voucher.domain.Voucher;
@@ -7,16 +10,20 @@ import org.programers.vouchermanagement.voucher.dto.VoucherResponse;
 
 import java.util.UUID;
 
-public class VoucherConverter {
+public class Converter {
 
-    private static String DELIMINATOR = " ";
+    private static String DELIMINATOR = " |,";
 
     public static String toString(Voucher voucher) {
-        return voucher.getId() + DELIMINATOR + voucher.getPolicy().toString();
+        return voucher.getId() + " " + voucher.getPolicy().toString();
     }
 
     public static String toString(VoucherResponse voucher) {
-        return voucher.getId() + DELIMINATOR + voucher.getPolicy().toString();
+        return voucher.getId() + " " + voucher.getPolicy().toString();
+    }
+
+    public static String toString(MemberResponse member) {
+        return member.getId() + " " + member.getStatus();
     }
 
     public static Voucher toVoucher(String text) {
@@ -25,5 +32,10 @@ public class VoucherConverter {
             return new Voucher(UUID.fromString(result[0]), new FixedAmountPolicy());
         }
         return new Voucher(UUID.fromString(result[0]), new PercentDiscountPolicy());
+    }
+
+    public static Member toMember(String text) {
+        String[] result = text.split(DELIMINATOR);
+        return new Member(UUID.fromString(result[0]), MemberStatus.valueOf(result[1]));
     }
 }
