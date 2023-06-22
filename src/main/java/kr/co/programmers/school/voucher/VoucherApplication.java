3f8df5ecbd894f2d8caee988edf893726d205629
Jeong-w-o-o <irmnels@gmail.com>
2023-06-22 14:47:28 +0900

    :tada: Initial Commit

---
+++ src/main/java/kr/co/programmers/school/voucher/VoucherApplication.java
@@ -0,0 +1,13 @@
+package kr.co.programmers.school.voucher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoucherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VoucherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/co/programmers/school/voucher/VoucherApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.programmers.school.voucher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoucherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
