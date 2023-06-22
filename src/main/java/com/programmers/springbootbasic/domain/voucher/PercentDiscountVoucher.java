7d7e66aae67c351130c88f3802bdae46acf5c88d
sangminnim <sangmins930@gmail.com>
2023-06-23 01:27:40 +0900

    feat: PercentDiscountVoucher 생성

---
+++ src/main/java/com/programmers/springbootbasic/domain/voucher/PercentDiscountVoucher.java
@@ -0,0 +1,23 @@
+package com.programmers.springbootbasic.domain.voucher;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public class PercentDiscountVoucher extends Voucher {
+    private final int percent;
+
+    public PercentDiscountVoucher(UUID voucherId, String name, LocalDateTime expirationDate, int percent) {
+        super(voucherId, name, expirationDate);
+        this.percent = percent;
+    }
+
+    public PercentDiscountVoucher(UUID voucherId, String name, Long condition, LocalDateTime expirationDate, int percent) {
+        super(voucherId, name, condition, expirationDate);
+        this.percent = percent;
+    }
+
+    @Override
+    public Long discount(Long priceBeforeDiscount) {
+        return Math.round(priceBeforeDiscount * ((100 - percent) / 100d) / 10) * 10;
+    }
+}
