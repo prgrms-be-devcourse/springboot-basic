b7d7f508a62a709a2d3b2fbf28d553d31dd3e1ea
changhyeon <changhyeon.h@kakao.com>
2023-06-22 23:30:39 +0900

    refactor: ProgramMenu, VoucherMenu enum 탐색 조건 추가

--- src/main/java/com/programmers/springweekly/domain/ProgramMenu.java
+++ src/main/java/com/programmers/springweekly/domain/ProgramMenu.java
@@ -1,5 +1,20 @@
 package com.programmers.springweekly.domain;
 
+import java.util.Arrays;
+
 public enum ProgramMenu {
-    EXIT, CREATE, LIST
+    EXIT("exit"), CREATE("create"), LIST("list");
+
+    private String menu;
+
+    ProgramMenu(String menu) {
+        this.menu = menu;
+    }
+
+    public ProgramMenu findProgramMenu(ProgramMenu programMenu){
+        return Arrays.stream(ProgramMenu.values())
+                .filter(program -> program.menu.equals(programMenu))
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException("찾으시는 메뉴가 없습니다"));
+    }
 }
--- src/main/java/com/programmers/springweekly/domain/VoucherMenu.java
+++ src/main/java/com/programmers/springweekly/domain/VoucherMenu.java
@@ -1,5 +1,20 @@
 package com.programmers.springweekly.domain;
 
+import java.util.Arrays;
+
 public enum VoucherMenu {
-    FIXED, PERCENT
+    FIXED("fixed"), PERCENT("percent");
+
+    private String menu;
+
+    VoucherMenu(String voucherMenu) {
+        this.menu = voucherMenu;
+    }
+
+    public VoucherMenu findVoucherMenu(VoucherMenu voucherMenu){
+        return Arrays.stream(VoucherMenu.values())
+                .filter(voucher -> voucher.menu.equals(voucherMenu))
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException("찾는 바우처 종류가 없습니다."));
+    }
 }
