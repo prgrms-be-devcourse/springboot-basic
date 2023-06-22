530792e4d88524d40a1446048c12415f5a04ff72
jun <alap_u@naver.com>
2023-06-23 02:32:52 +0900

    test 백분율 할인 바우처 할인 범위 유효성 검증 테스트

---
+++ src/test/java/com/dev/bootbasic/voucher/domain/PercentDiscountVoucherTest.java
@@ -0,0 +1,39 @@
+package com.dev.bootbasic.voucher.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.UUID;
+
+import static com.dev.bootbasic.voucher.domain.PercentDiscountVoucher.PERCENT_DISCOUNT_AMOUNT_VALIDATION_EXCEPTION_MESSAGE;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class PercentDiscountVoucherTest {
+
+    @DisplayName("백분율 할인 바우처의 할인금액 성공 테스트")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 50, 100})
+    void validateDiscountAmountSuccessTest(int discountAmount) {
+        UUID id = UUID.randomUUID();
+
+        AbstractVoucher voucher = PercentDiscountVoucher.of(id, discountAmount);
+
+        assertThat(voucher)
+                .extracting(AbstractVoucher::getId, AbstractVoucher::getDiscountAmount)
+                .containsExactly(id, discountAmount);
+    }
+
+    @DisplayName("백분율 할인 바우처의 할인금액 예외 테스트")
+    @ParameterizedTest
+    @ValueSource(ints = {0, 101})
+    void validateDiscountAmountFailTest(int discountAmount) {
+        UUID id = UUID.randomUUID();
+
+        assertThatThrownBy(() -> PercentDiscountVoucher.of(id, discountAmount))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(PERCENT_DISCOUNT_AMOUNT_VALIDATION_EXCEPTION_MESSAGE);
+    }
+
+}
