a80eb4029e0710eeef43390a63c7385bcffc465e
Juwoong <wwong7247@gmail.com>
2023-06-22 16:39:05 +0900

    feat : main 메서드 작성
    * 에너테이션 기반 ApplicationContext를 통해 CommandLineApplication 빈을 꺼내오고 run 메서드를 호출하여 프로그램을 실행함

--- voucher/src/main/java/com/example/voucher/VoucherApplication.java
+++ voucher/src/main/java/com/example/voucher/VoucherApplication.java
@@ -2,12 +2,18 @@ package com.example.voucher;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.example.voucher.config.AppConfiguration;
 
 @SpringBootApplication
 public class VoucherApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(VoucherApplication.class, args);
+		ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfiguration.class);
+		CommandLineApplication commandLineApplication = applicationContext.getBean(CommandLineApplication.class);
+		commandLineApplication.run();
 	}
 
 }
