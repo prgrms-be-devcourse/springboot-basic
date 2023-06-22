19b43eada6f02d368275000d956e550debd1601e
SY97P <onetuks@naver.com>
2023-06-22 21:52:01 +0900

    test: VoucherType 테스트

--- src/test/java/com/devcourse/springbootbasic/voucher/VoucherTest.java
+++ src/test/java/com/devcourse/springbootbasic/voucher/VoucherTest.java
@@ -1,5 +1,7 @@
 package com.devcourse.springbootbasic.voucher;
 
+import com.devcourse.springbootbasic.engine.exception.InvalidDataException;
+import com.devcourse.springbootbasic.engine.io.InputConsole;
 import com.devcourse.springbootbasic.engine.model.VoucherType;
 import com.devcourse.springbootbasic.engine.voucher.domain.FixedAmountVoucher;
 import com.devcourse.springbootbasic.engine.voucher.domain.PercentDiscountVoucher;
@@ -10,10 +12,14 @@ import com.devcourse.springbootbasic.engine.voucher.service.VoucherService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
+import java.io.ByteArrayInputStream;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.UUID;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
@@ -43,5 +49,29 @@ public class VoucherTest {
         voucherRepository.setVoucherMap(input);
         VoucherService voucherService = new VoucherService(voucherRepository);
         Assertions.assertEquals(expected, voucherService.getAllVouchers().size());
+    }
+
+    public static Stream<Arguments> provideVoucherType() {
+        return Stream.of(
+                Arguments.of("1", VoucherType.FIXED_AMOUNT),
+                Arguments.of("2", VoucherType.PERCENT_DISCOUNT)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideVoucherType")
+    void voucherTypeTest(String input, VoucherType expected) {
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+        InputConsole inputConsole = new InputConsole();
+        VoucherType voucherType = inputConsole.inputVoucherType();
+        Assertions.assertEquals(expected, voucherType);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"4", "-1"})
+    void voucherTypeExceptionTest(String input) {
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+        InputConsole inputConsole = new InputConsole();
+        Assertions.assertThrows(InvalidDataException.class, inputConsole::inputVoucherType);
     }
 }
