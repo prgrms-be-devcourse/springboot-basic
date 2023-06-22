c59f7441515e073b78e45c5701d859af95886099
sangminnim <sangmins930@gmail.com>
2023-06-23 01:22:51 +0900

    feat: Voucher 모델 생성

---
+++ src/main/java/com/programmers/springbootbasic/domain/voucher/Voucher.java
@@ -0,0 +1,64 @@
+package com.programmers.springbootbasic.domain.voucher;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public abstract class Voucher {
+    private final UUID voucherId;
+    private final String name;
+    private final Long condition;
+    private final LocalDateTime expirationDate;
+    private VoucherState state = VoucherState.AVAILABLE;
+
+    public Voucher(UUID voucherId, String name, LocalDateTime expirationDate) {
+        this.voucherId = voucherId;
+        this.name = name;
+        this.condition = 0L;
+        this.expirationDate = expirationDate;
+    }
+
+    public Voucher(UUID voucherId, String name, Long condition, LocalDateTime expirationDate) {
+        this.voucherId = voucherId;
+        this.name = name;
+        this.condition = condition;
+        this.expirationDate = expirationDate;
+    }
+
+    public UUID getVoucherId() {
+        return voucherId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Long getCondition() {
+        return condition;
+    }
+
+    public LocalDateTime getExpirationDate() {
+        return expirationDate;
+    }
+
+    public VoucherState getState() {
+        if (isExpiration()) {
+            setVoucherState(VoucherState.EXPIRE);
+        }
+        return state;
+    }
+
+    public void use() {
+        setVoucherState(VoucherState.USED);
+    }
+
+    protected boolean isExpiration() {
+        LocalDateTime now = LocalDateTime.now();
+        return !now.isBefore(expirationDate);
+    }
+
+    protected void setVoucherState(VoucherState toChange) {
+        state = toChange;
+    }
+
+    protected abstract Long discount(Long priceBeforeDiscount);
+}
