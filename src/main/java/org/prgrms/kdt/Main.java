900be3d42af06af82b4e944e252ef03da09b8602
bona <ra_ari@naver.com>
2023-06-22 23:36:40 +0900

    [UPDATE] 코드 라인 정리

--- src/main/java/org/prgrms/kdt/Main.java
+++ src/main/java/org/prgrms/kdt/Main.java
@@ -1,25 +1,12 @@
 package org.prgrms.kdt;
 
-import org.prgrms.kdt.AppConfiguration;
 import org.prgrms.kdt.voucher.VoucherManagement;
-import org.prgrms.kdt.voucher.domain.FixedAmountVoucher;
-import org.prgrms.kdt.voucher.domain.Voucher;
-import org.prgrms.kdt.voucher.repository.VoucherRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.core.env.ConfigurableEnvironment;
-import org.springframework.core.io.Resource;
-
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.List;
-import java.util.UUID;
 
 @SpringBootApplication
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfiguration.class);
         applicationContext.getBean(VoucherManagement.class).run();
 //        ConfigurableEnvironment environment = applicationContext.getEnvironment();
--- src/main/java/org/prgrms/kdt/voucher/utils/Validator.java
+++ src/main/java/org/prgrms/kdt/voucher/utils/Validator.java
@@ -1,6 +1,5 @@
 package org.prgrms.kdt.voucher.utils;
 
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Component;
 
 @Component
