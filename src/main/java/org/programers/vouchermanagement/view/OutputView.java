d7a5cb96e75df6993ed03effc7c33e8381f93bc2
player-geun <player_geun@naver.com>
2023-06-22 16:56:30 +0900

    feat : 블랙리스트를 출력하는 기능 구현

--- src/main/java/org/programers/vouchermanagement/view/OutputView.java
+++ src/main/java/org/programers/vouchermanagement/view/OutputView.java
@@ -1,8 +1,10 @@
 package org.programers.vouchermanagement.view;
 
+import org.programers.vouchermanagement.member.dto.MemberResponse;
+import org.programers.vouchermanagement.member.dto.MembersResponse;
 import org.programers.vouchermanagement.voucher.dto.VoucherResponse;
 import org.programers.vouchermanagement.voucher.dto.VouchersResponse;
-import org.programers.vouchermanagement.util.VoucherConverter;
+import org.programers.vouchermanagement.util.Converter;
 
 public class OutputView {
 
@@ -23,7 +25,13 @@ public class OutputView {
 
     public static void outputVouchers(VouchersResponse response) {
         for (VoucherResponse voucher : response.getVouchers()) {
-            System.out.println(VoucherConverter.toString(voucher));
+            System.out.println(Converter.toString(voucher));
+        }
+    }
+
+    public static void outputMembers(MembersResponse response) {
+        for (MemberResponse member : response.getMembers()) {
+            System.out.println(Converter.toString(member));
         }
     }
 }
