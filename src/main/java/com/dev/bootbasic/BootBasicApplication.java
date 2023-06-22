688a5916c5e82b8984bbc802fc21fc4879ad4b90
jun <alap_u@naver.com>
2023-06-23 02:31:08 +0900

    프로젝트 초기 셋팅

---
+++ src/main/java/com/dev/bootbasic/BootBasicApplication.java
@@ -0,0 +1,13 @@
+package com.dev.bootbasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootBasicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootBasicApplication.class, args);
+    }
+
+}
