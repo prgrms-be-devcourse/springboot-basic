a337008a7eec4f5f8b188b3561cf7d4f80dd82dd
Na Yeongkyeong <102007066+na-yk@users.noreply.github.com>
2023-06-23 02:45:53 +0900

    refactor: DemoApplication을 CommandLineApplication으로 수정

--- voucher/src/main/java/org/devcourse/voucher/CommandLineApplication.java
+++ voucher/src/main/java/org/devcourse/voucher/CommandLineApplication.java
@@ -1,15 +1,23 @@
 package org.devcourse.voucher;
 
-
 import org.devcourse.voucher.console.Console;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-public class CommandLineApplication implements Runnable{
-    private final Console console = new Console();
+@SpringBootApplication
+public class CommandLineApplication {
 
-    @Override
-    public void run() {
-        console.printMenu();
-        String option = console.getOption();
-        System.out.println(option);
-    }
+	private final Console console = new Console();
+
+	public static void main(String[] args) {
+		SpringApplication.run(org.devcourse.voucher.CommandLineApplication.class, args);
+	}
+
+	public void run() {
+		console.printMenu();
+		String option = console.getOption();
+		System.out.println(option);
+	}
 }
+
+
