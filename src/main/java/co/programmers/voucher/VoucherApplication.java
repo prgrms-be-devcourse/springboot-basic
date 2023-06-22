ff458f5755b9489a27e896ad944625b22a3c041c
SW-H <hsw08@naver.com>
2023-06-23 02:36:03 +0900

    init : 프로젝트 생성

---
+++ src/main/java/co/programmers/voucher/VoucherApplication.java
@@ -0,0 +1,13 @@
+package co.programmers.voucher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoucherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoucherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/programmers/voucher/VoucherApplicationTests.java
@@ -0,0 +1,13 @@
+package co.programmers.voucher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoucherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
