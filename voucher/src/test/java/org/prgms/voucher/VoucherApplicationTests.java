bab90eae057364d835d28f83c274f7c99ee484f6
dingding-21 <phj8221@naver.com>
2023-06-22 22:54:40 +0900

    init: 프로젝트 세팅

---
+++ voucher/src/test/java/org/prgms/voucher/VoucherApplicationTests.java
@@ -0,0 +1,13 @@
+package org.prgms.voucher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoucherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
