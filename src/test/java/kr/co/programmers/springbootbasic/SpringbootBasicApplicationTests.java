75962e2858aa8814a68089d0f4a71323a1f9956f
ysng_ysng <yeasung67@gmail.com>
2023-06-23 00:54:50 +0900

    setUp project

---
+++ src/test/java/kr/co/programmers/springbootbasic/SpringbootBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.programmers.springbootbasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBasicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
