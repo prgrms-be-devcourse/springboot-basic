986d9d4c8da83180102502cf0af44a50839667b2
SY97P <onetuks@naver.com>
2023-06-24 02:21:41 +0900

    style: 불필요 import 제거

--- src/main/java/com/devcourse/springbootbasic/engine/voucher/domain/factory/VoucherFactory.java
+++ src/main/java/com/devcourse/springbootbasic/engine/voucher/domain/factory/VoucherFactory.java
@@ -1,9 +1,7 @@
 package com.devcourse.springbootbasic.engine.voucher.domain.factory;
 
-import com.devcourse.springbootbasic.engine.model.VoucherType;
 import com.devcourse.springbootbasic.engine.voucher.domain.Voucher;
 
 public interface VoucherFactory {
-
     Voucher create(double voucherDiscount);
 }
