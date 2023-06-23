8886f76a78c0ba0cca6e0a0c2aa2cf8913ce539a
parksey <parkseyeon99@naver.com>
2023-06-23 18:46:55 +0900

    test: 사용자 입력에 대한 예외 테스트 추가

---
+++ src/test/java/org/weekly/weekly/ui/ReadExceptionTest.java
@@ -0,0 +1,22 @@
+package org.weekly.weekly.ui;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class ReadExceptionTest {
+    @ParameterizedTest
+    @ValueSource(strings = {"", " "})
+    void 사용자가_빈값이나_입력오류났을때_예외발생(String userInput) {
+        assertThatThrownBy(ReadException.isEmpty())
+                .isInstanceOf(RuntimeException.class);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {""})
+    void 메뉴외의_숫자값을_입력하면_예외발생(String userInput) {
+        assertThatThrownBy(RuntimeException.notInputFormat)
+                .isInstanceOf(RuntimeException.class);
+    }
+}
