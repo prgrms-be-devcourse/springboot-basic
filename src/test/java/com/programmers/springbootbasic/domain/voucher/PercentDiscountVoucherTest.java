252e72584292c465254f5c84c25e1b0bdcdaf055
sangminnim <sangmins930@gmail.com>
2023-06-23 18:57:21 +0900

    chore: 중복된 테스트 제거

--- src/test/java/com/programmers/springbootbasic/domain/voucher/PercentDiscountVoucherTest.java
+++ src/test/java/com/programmers/springbootbasic/domain/voucher/PercentDiscountVoucherTest.java
@@ -10,27 +10,6 @@ import java.util.UUID;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class PercentDiscountVoucherTest {
-
-    @Test
-    void discount() {
-        // given
-        Long price = 4550L;
-        int percent = 33;
-        PercentDiscountVoucher voucher = new PercentDiscountVoucher(
-                UUID.randomUUID(),
-                percent + "% 할인권",
-                LocalDateTime.MAX,
-                percent);
-
-        Long expectedPrice = Math.round(price * ((100 - percent) / 100d) / 10) * 10;
-
-        // when
-        Long discountedPrice = voucher.discount(price);
-
-        // then
-        assertThat(discountedPrice).isEqualTo(expectedPrice);
-    }
-
     @ParameterizedTest
     @CsvSource(value = {"4550,33,3050", "5500,0,5500"})
     void discount(Long price, int percent, Long expectedPrice) {
