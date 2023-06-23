e9fa0d63e1df6fa9d898873e02acbb75b5ccb35e
hseong3243 <hseong3243@gmail.com>
2023-06-24 01:04:45 +0900

    test: 스프링 부트 테스트 임시적으로 비활성화

--- src/test/java/com/programmers/voucher/VoucherApplicationTests.java
+++ src/test/java/com/programmers/voucher/VoucherApplicationTests.java
@@ -1,9 +1,11 @@
 package com.programmers.voucher;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Disabled("콘솔 애플리케이션 무한 루프로 인해 스프링 부트 테스트 일시적으로 비활성화")
 class VoucherApplicationTests {
 
     @Test
