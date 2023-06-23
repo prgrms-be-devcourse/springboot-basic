14dcb6fb2efdc1be73c9d867f37b747fbe709c83
hseong3243 <hseong3243@gmail.com>
2023-06-23 23:48:09 +0900

    log: 콘솔 커맨드 입력값 검증시 warn 로깅 추가

--- src/main/java/com/programmers/voucher/enumtype/ConsoleCommandType.java
+++ src/main/java/com/programmers/voucher/enumtype/ConsoleCommandType.java
@@ -1,5 +1,8 @@
 package com.programmers.voucher.enumtype;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.Arrays;
 import java.util.Objects;
 
@@ -8,6 +11,8 @@ public enum ConsoleCommandType {
     LIST("list"),
     HELP("help"),
     EXIT("exit");
+
+    private static final Logger log = LoggerFactory.getLogger(ConsoleCommandType.class);
 
     private final String input;
 
@@ -24,6 +29,7 @@ public enum ConsoleCommandType {
                             .append(" Current input: ")
                             .append(input);
 
+                    log.warn("Invalid command type: {}", sb);
                     return new IllegalArgumentException(sb.toString());
                 });
     }
