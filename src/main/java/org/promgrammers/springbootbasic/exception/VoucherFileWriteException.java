8a7e7944d4fde48b52617f689807cbea7549217b
hongxeob <sjun2918@naver.com>
2023-06-23 18:16:51 +0900

    feat: File방식으로 Voucher 저장 및 조회 구현

---
+++ src/main/java/org/promgrammers/springbootbasic/exception/VoucherFileWriteException.java
@@ -0,0 +1,8 @@
+package org.promgrammers.springbootbasic.exception;
+
+public class VoucherFileWriteException extends RuntimeException {
+
+    public VoucherFileWriteException(String message) {
+        super(message);
+    }
+}
