1c59ccf1512ae49d5fe6d587d82c6cc171005267
yumyeonghan <mgtmh991013@gmail.com>
2023-06-23 22:32:47 +0900

    refactor: 코드 리뷰 반영
    1. VoucherType의 필드명 의미있게 수정

--- src/main/java/com/programmers/voucher/enums/VoucherType.java
+++ src/main/java/com/programmers/voucher/enums/VoucherType.java
@@ -15,33 +15,32 @@ import java.util.function.BiFunction;
 public enum VoucherType {
     FIXED_AMOUNT_VOUCHER("fixed", FixedAmountVoucher::new, 1000),
     PERCENT_DISCOUNT_VOUCHER("percent", PercentDiscountVoucher::new, 100);
-
-    public static final int NO_COUNT = 0;
-    private final String type;
-    private final BiFunction<UUID, Long, Voucher> biFunction;
+    
+    private final String voucherType;
+    private final BiFunction<UUID, Long, Voucher> voucherCreationFunction;
     private final Integer maximumDiscountAmount;
 
-    VoucherType(String type, BiFunction<UUID, Long, Voucher> biFunction, Integer maximumDiscountAmount) {
-        this.type = type;
-        this.biFunction = biFunction;
+    VoucherType(String voucherType, BiFunction<UUID, Long, Voucher> voucherCreationFunction, Integer maximumDiscountAmount) {
+        this.voucherType = voucherType;
+        this.voucherCreationFunction = voucherCreationFunction;
         this.maximumDiscountAmount = maximumDiscountAmount;
     }
 
     public static VoucherType getVoucherType(String type) {
         return Arrays.stream(VoucherType.values())
-                .filter(voucherType -> Objects.equals(voucherType.type, type))
+                .filter(voucherType -> Objects.equals(voucherType.voucherType, type))
                 .findAny()
                 .orElseThrow(() -> new VoucherException(VoucherErrorCode.NOT_SUPPORTED_TYPE));
     }
 
     public Voucher createVoucher(VoucherCreationRequest voucherCreationRequest) {
         validateOverDiscountAmount(voucherCreationRequest);
-        return biFunction.apply(UUID.randomUUID(), voucherCreationRequest.getAmount());
+        return voucherCreationFunction.apply(UUID.randomUUID(), voucherCreationRequest.getAmount());
     }
 
     private static void validateOverDiscountAmount(VoucherCreationRequest voucherCreationRequest) {
         Arrays.stream(VoucherType.values())
-                .filter(voucherType -> Objects.equals(voucherType.type, voucherCreationRequest.getType())
+                .filter(voucherType -> Objects.equals(voucherType.voucherType, voucherCreationRequest.getType())
                         && voucherType.maximumDiscountAmount > voucherCreationRequest.getAmount())
                 .findAny()
                 .orElseThrow(() -> new VoucherException(VoucherErrorCode.EXCEED_MAXIMUM_DISCOUNT_AMOUNT));
