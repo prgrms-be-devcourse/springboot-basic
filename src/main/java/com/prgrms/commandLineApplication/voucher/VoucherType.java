b0536d4a32d25293dd5c65a14b6336a992ba274d
박은지 <qkrdmswl1018@naver.com>
2023-06-23 12:35:59 +0900

    feat: VoucherType 계산 기능 추가

--- src/main/java/com/prgrms/commandLineApplication/voucher/VoucherType.java
+++ src/main/java/com/prgrms/commandLineApplication/voucher/VoucherType.java
@@ -1,25 +1,34 @@
 package com.prgrms.commandLineApplication.voucher;
 
 import java.util.Arrays;
+import java.util.UUID;
+import java.util.function.BiFunction;
 
 public enum VoucherType {
-  FIXED("fixed"),
-  PERCENT("percent");
+  FIXED("fixed", (voucherId, discountValue) -> new FixedAmountVoucher(voucherId, discountValue)),
+  PERCENT("percent", (voucherId, discountValue) -> new PercentDiscountVoucher(voucherId, discountValue));
 
   private final String type;
+  private final BiFunction<UUID, Long, Voucher> createVoucher;
 
-  VoucherType(String type) {
+  VoucherType(String type, BiFunction<UUID, Long, Voucher> createVoucher) {
     this.type = type;
+    this.createVoucher = createVoucher;
   }
 
   public String getType() {
     return type;
   }
 
-  public void checkType(String voucherType) {
+  public Voucher create(UUID voucherId, Long discountValue, String inputVoucherType) {
+    checkType(inputVoucherType);
+    return createVoucher.apply(voucherId, discountValue);
+  }
+
+  public void checkType(String inputVoucherType) {
     Arrays.stream(VoucherType.values())
-            .filter(voucher -> voucher.getType().equals(voucherType))
+            .filter(voucher -> voucher.getType().equals(inputVoucherType))
             .findFirst()
-            .orElseThrow(() -> new IllegalArgumentException("Type " + voucherType + " does not exist. \n Please select the type in the view."));
+            .orElseThrow(() -> new IllegalArgumentException("Type " + inputVoucherType + " does not exist. \n Please select the type in the view."));
   }
 }
