e0128274345e82309268edcaa85f8ca39c0cf6d4
sangminnim <sangmins930@gmail.com>
2023-06-24 00:36:09 +0900

    feat: Voucher 구체 클래스 입력값 검증 로직 구현

--- src/main/java/com/programmers/springbootbasic/domain/voucher/FixedAmountVoucher.java
+++ src/main/java/com/programmers/springbootbasic/domain/voucher/FixedAmountVoucher.java
@@ -4,15 +4,23 @@ import java.time.LocalDateTime;
 import java.util.UUID;
 
 public class FixedAmountVoucher extends Voucher {
+    private final Long MIN_AMOUNT = 10L;
+    private final Long MAX_AMOUNT = 10_000_000L;
     private final Long amount;
 
     public FixedAmountVoucher(UUID voucherId, String name, LocalDateTime expirationDate, Long amount) {
         super(voucherId, name, expirationDate);
+        if (isInvalidateAmount(amount)) {
+            throw new IllegalArgumentException("잘못된 할인 금액");
+        }
         this.amount = amount;
     }
 
     public FixedAmountVoucher(UUID voucherId, String name, Long minimumPrice, LocalDateTime expirationDate, Long amount) {
         super(voucherId, name, minimumPrice, expirationDate);
+        if (isInvalidateAmount(amount)) {
+            throw new IllegalArgumentException("잘못된 할인 금액");
+        }
         this.amount = amount;
     }
 
@@ -22,5 +30,9 @@ public class FixedAmountVoucher extends Voucher {
             return 0L;
         }
         return priceBeforeDiscount - amount;
+    }
+
+    private Boolean isInvalidateAmount(Long amount) {
+        return amount < MIN_AMOUNT || MAX_AMOUNT < amount;
     }
 }
--- src/main/java/com/programmers/springbootbasic/domain/voucher/PercentDiscountVoucher.java
+++ src/main/java/com/programmers/springbootbasic/domain/voucher/PercentDiscountVoucher.java
@@ -4,20 +4,32 @@ import java.time.LocalDateTime;
 import java.util.UUID;
 
 public class PercentDiscountVoucher extends Voucher {
+    private final int MIN_PERCENT = 1;
+    private final int MAX_PERCENT = 100;
     private final int percent;
 
     public PercentDiscountVoucher(UUID voucherId, String name, LocalDateTime expirationDate, int percent) {
         super(voucherId, name, expirationDate);
+        if (isInvalidateRange(percent)) {
+            throw new IllegalArgumentException("잘못된 할인 범위");
+        }
         this.percent = percent;
     }
 
     public PercentDiscountVoucher(UUID voucherId, String name, Long minimumPrice, LocalDateTime expirationDate, int percent) {
         super(voucherId, name, minimumPrice, expirationDate);
+        if (isInvalidateRange(percent)) {
+            throw new IllegalArgumentException("잘못된 할인 범위");
+        }
         this.percent = percent;
     }
 
     @Override
     public Long discount(Long priceBeforeDiscount) {
         return Math.round((priceBeforeDiscount - (priceBeforeDiscount * percent / 100d)) / 10) * 10;
+    }
+
+    private Boolean isInvalidateRange(int percent) {
+        return percent < MIN_PERCENT || MAX_PERCENT < percent;
     }
 }
