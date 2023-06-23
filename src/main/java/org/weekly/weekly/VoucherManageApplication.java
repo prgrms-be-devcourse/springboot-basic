0fbaa5fdf8063ee51d09f784e20e33b08e388673
parksey <parkseyeon99@naver.com>
2023-06-23 11:02:26 +0900

    feat: 프로젝트 초기화

---
+++ src/main/java/org/weekly/weekly/VoucherManageApplication.java
@@ -0,0 +1,16 @@
+package org.weekly.weekly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Scanner;
+
+@SpringBootApplication
+public class VoucherManageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoucherManageApplication.class, args);
+		new Scanner(System.in);
+	}
+
+}
---
+++ src/test/java/org/weekly/weekly/WeeklyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.weekly.weekly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeeklyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
