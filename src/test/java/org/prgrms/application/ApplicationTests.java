9ec8eb5511f979ccc82dadc5c4a829d1b64a4fe4
Continue <joje1003@gmail.com>
2023-06-23 17:14:55 +0900

    feat project init

---
+++ src/test/java/org/prgrms/application/ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.prgrms.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
