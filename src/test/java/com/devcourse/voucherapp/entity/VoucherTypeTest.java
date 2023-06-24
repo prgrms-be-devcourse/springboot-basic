75615ef21ad21b447f3b406f089680fe6af79959
kylekim2123 <kyle2123@naver.com>
2023-06-25 02:58:48 +0900

    test: 고정, 비율 할인권 객체 생성에 대한 예외처리 테스트 추가

--- src/test/java/com/devcourse/voucherapp/entity/VoucherTypeTest.java
+++ src/test/java/com/devcourse/voucherapp/entity/VoucherTypeTest.java
@@ -7,6 +7,8 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import com.devcourse.voucherapp.exception.VoucherInputException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class VoucherTypeTest {
 
@@ -25,5 +27,19 @@ class VoucherTypeTest {
     void selectNotExistedVoucherTypeTest() {
         assertThrows(VoucherInputException.class, () -> getVoucherType("10000"));
         assertThrows(VoucherInputException.class, () -> getVoucherType("string"));
+    }
+
+    @DisplayName("고정 금액 할인권을 생성할 때, 잘못된 금액을 입력한 경우 예외처리 수행 테스트")
+    @ParameterizedTest
+    @ValueSource(strings = {"0", "-1", "10.1", "string", "", " ", "\n"})
+    void invalidFixDiscountPriceTest(String discountPrice) {
+        assertThrows(VoucherInputException.class, () -> VoucherType.FIX.makeVoucher(discountPrice));
+    }
+
+    @DisplayName("비율 퍼센트 할인권을 생성할 때, 잘못된 퍼센트를 입력한 경우 예외처리 수행 테스트")
+    @ParameterizedTest
+    @ValueSource(strings = {"0", "-1", "10.1", "string", "101", "", " ", "\n"})
+    void invalidPercentDiscountRateTest(String discountRate) {
+        assertThrows(VoucherInputException.class, () -> VoucherType.PERCENT.makeVoucher(discountRate));
     }
 }
