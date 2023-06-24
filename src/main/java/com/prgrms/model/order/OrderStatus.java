c67cc7b8a954f6ba640881f25cc5a8433d340c1e
byeolhaha <j35635aa@gmail.com>
2023-06-24 18:16:42 +0900

    feat : 주문 상태에 대해 Enum으로 분리 delete : 주문 서비스에 대한 클래스 삭제

---
+++ src/main/java/com/prgrms/model/order/OrderStatus.java
@@ -0,0 +1,12 @@
+package com.prgrms.model.order;
+
+public enum OrderStatus {
+    ACCEPTED,
+    PAYMENT_REQUIRED,
+    PAYMENT_CONFIRMED,
+    PAYMENT_REJECTED,
+    READY_FOR_DELIVERY,
+    SHIPPED,
+    CANCELED
+
+}
