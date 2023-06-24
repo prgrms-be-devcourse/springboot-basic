74888427ad5026e0b952ac575cd3322f32f39cb5
jun <alap_u@naver.com>
2023-06-25 02:30:52 +0900

    test 중복테스트 파리미터로 변경

--- src/test/java/com/dev/bootbasic/voucher/repository/InMemoryVoucherRepositoryTest.java
+++ src/test/java/com/dev/bootbasic/voucher/repository/InMemoryVoucherRepositoryTest.java
@@ -2,12 +2,16 @@ package com.dev.bootbasic.voucher.repository;
 
 import com.dev.bootbasic.voucher.domain.PercentDiscountVoucher;
 import com.dev.bootbasic.voucher.domain.Voucher;
+import com.dev.bootbasic.voucher.domain.VoucherFactory;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.Optional;
 import java.util.UUID;
 
+import static java.util.UUID.randomUUID;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class InMemoryVoucherRepositoryTest {
@@ -15,18 +19,21 @@ class InMemoryVoucherRepositoryTest {
     private final VoucherRepository voucherRepository = new InMemoryVoucherRepository();
 
     @DisplayName("바우처를 저장할 수 있다.")
-    @Test
-    void saveVoucherTest() {
-        PercentDiscountVoucher percentVoucher = createPercentVoucher(50);
-
-        UUID percentVoucherId = voucherRepository.saveVoucher(percentVoucher);
-
-        Optional<Voucher> foundPercentVoucher = voucherRepository.findVoucher(percentVoucherId);
-        assertThat(foundPercentVoucher.get()).isEqualTo(percentVoucher);
+    @ParameterizedTest
+    @CsvSource({
+            "fixed,5000",
+            "percent,50"
     }
+    )
+    void saveVoucherTest(String type, int discountAmount) {
+        VoucherFactory voucherFactory = new VoucherFactory();
+        Voucher voucher = voucherFactory.create(randomUUID(), type, discountAmount);
 
-    private PercentDiscountVoucher createPercentVoucher(int discountAmount) {
-        return PercentDiscountVoucher.of(UUID.randomUUID(), discountAmount);
+
+        UUID foundVoucherId = voucherRepository.saveVoucher(voucher);
+
+        Optional<Voucher> foundPercentVoucher = voucherRepository.findVoucher(foundVoucherId);
+        assertThat(foundPercentVoucher.get()).isEqualTo(voucher);
     }
 
 }
