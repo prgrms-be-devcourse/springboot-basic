281d5963bcdecf13e48473360f7a80dccdd7310e
hyee0715 <hyee0715@naver.com>
2023-06-24 23:08:57 +0900

    refactor: Application 클래스 코드 수정

--- src/main/java/com/programmers/Application.java
+++ src/main/java/com/programmers/Application.java
@@ -2,14 +2,15 @@ package com.programmers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(Application.class, args);
 
-        VoucherController voucherController = new VoucherController();
+        VoucherController voucherController = applicationContext.getBean(VoucherController.class);
         voucherController.run();
     }
 }
