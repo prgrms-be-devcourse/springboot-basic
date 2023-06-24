d805dcf71c083f2d92be5c336a908890cc2a5e7b
dingding-21 <phj8221@naver.com>
2023-06-25 02:45:50 +0900

    feat: Command-line Application 클래스 생성

---
+++ voucher/src/main/java/org/prgms/voucher/CommandLineApplication.java
@@ -0,0 +1,13 @@
+package org.prgms.voucher;
+
+public class CommandLineApplication implements Application {
+    @Override
+    public void printCommands() {
+
+    }
+
+    @Override
+    public void exitProgram() {
+
+    }
+}
