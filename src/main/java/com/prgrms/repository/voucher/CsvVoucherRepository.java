0b12f56dde77a5f3a05574abbf37a53be5cac129
byeolhaha <j35635aa@gmail.com>
2023-06-24 17:50:59 +0900

    feat : 비휘발성 바우처 저장소 틀 구현

---
+++ src/main/java/com/prgrms/repository/voucher/CsvVoucherRepository.java
@@ -0,0 +1,25 @@
+package com.prgrms.repository.voucher;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public class CsvVoucherRepository implements VoucherRepository {
+    @Override
+    public Optional<Voucher> findById(UUID voucherId) {
+        return Optional.empty();
+    }
+    @Override
+    public Voucher insert(Voucher voucher) {
+        return null;
+    }
+
+    @Override
+    public List<Voucher> getAllVoucherList() {
+        return null;
+    }
+}
+
