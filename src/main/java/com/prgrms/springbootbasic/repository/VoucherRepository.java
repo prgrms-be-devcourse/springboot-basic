dd13728523961bb43b4093dbbb6fc391e1680682
jay-so <merry9504@naver.com>
2023-06-25 07:21:17 +0900

    Feat:VoucherRepository 구현

---
+++ src/main/java/com/prgrms/springbootbasic/repository/VoucherRepository.java
@@ -0,0 +1,11 @@
+package com.prgrms.springbootbasic.repository;
+
+import com.prgrms.springbootbasic.domain.Voucher;
+
+import java.util.List;
+
+public interface VoucherRepository {
+    Voucher insert(Voucher voucher);
+
+    List<Voucher> getAllVouchersList();
+}
