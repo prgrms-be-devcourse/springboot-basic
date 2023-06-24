78b2ce98d6e87820a786a761559a8153cac06c2b
dingding-21 <phj8221@naver.com>
2023-06-25 02:43:34 +0900

    feat: VoucherService에 명령어 기능 메서드 추가

--- voucher/src/main/java/org/prgms/voucher/VoucherService.java
+++ voucher/src/main/java/org/prgms/voucher/VoucherService.java
@@ -1,9 +1,19 @@
 package org.prgms.voucher;
 
+import java.util.List;
+
 public class VoucherService {
     private final VoucherRepository voucherRepository;
 
     public VoucherService(VoucherRepository voucherRepository) {
         this.voucherRepository = voucherRepository;
+    }
+
+    void createVoucher(AmountType amountType) {
+
+    }
+
+    List<Voucher> listVoucher() {
+
     }
 }
