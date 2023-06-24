0289f3a1570bce9b074aaea9430a9a86a9c3db03
kimihiqq <kimihiqq@gmail.com>
2023-06-24 17:17:19 +0900

    feat: VoucherManagementApplication 클래스 작성 및 실행 흐름 제어

--- src/main/java/me/kimihiqq/vouchermanagement/VoucherManagementApplication.java
+++ src/main/java/me/kimihiqq/vouchermanagement/VoucherManagementApplication.java
@@ -1,13 +1,17 @@
 package me.kimihiqq.vouchermanagement;
 
+import me.kimihiqq.vouchermanagement.controller.VoucherController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class VoucherManagementApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(VoucherManagementApplication.class, args);
+        ApplicationContext applicationContext = SpringApplication.run(VoucherManagementApplication.class, args);
+        applicationContext.getBean(VoucherController.class).run();
     }
-
 }
+
