70ae066e5fa002b6aa7c1bc2c574e0750b401d9e
kylekim2123 <kyle2123@naver.com>
2023-06-24 13:58:00 +0900

    build: 프로젝트 초기 빌드 파일 생성

---
+++ src/test/java/com/devcourse/voucherapp/VoucherappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devcourse.voucherapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoucherappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
