46da157259d0852c49b92f1855d25df62f065da0
player-geun <player_geun@naver.com>
2023-06-24 18:44:48 +0900

    refactor : File 클래스를 Path 클래스로 변경하고, 저수준의 IO 를 Files 클래스를 통해 개선

--- src/main/java/org/programers/vouchermanagement/voucher/domain/InFileVoucherRepository.java
+++ src/main/java/org/programers/vouchermanagement/voucher/domain/InFileVoucherRepository.java
@@ -7,25 +7,35 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Repository;
 
 import java.io.*;
-import java.util.ArrayList;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Repository
 @Primary
 @Profile("dev")
 public class InFileVoucherRepository implements VoucherRepository {
 
-    private static final File file = new File("src/main/resources/voucher.txt");
+    private static final Path file = Paths.get("src/main/resources/voucher.txt");
+
+    static {
+        try {
+            if (!Files.exists(file)) {
+                Files.createFile(file);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     @Override
     public Voucher save(Voucher voucher) {
         try {
-            Writer writer = new FileWriter(file, true);
-            writer.write(Converter.toString(voucher));
-            writer.flush();
-            writer.close();
+            Files.writeString(file, Converter.toString(voucher));
             return voucher;
         } catch (IOException e) {
             throw new NoSuchVoucherException("IO 문제로 바우처가 저장되지 않았습니다.");
@@ -35,15 +45,13 @@ public class InFileVoucherRepository implements VoucherRepository {
     @Override
     public Optional<Voucher> findById(UUID id) {
         try {
-            BufferedReader reader = new BufferedReader(new FileReader(file));
-            String line;
-            while ((line = reader.readLine()) != null) {
+            List<String> lines = Files.readAllLines(file);
+            for (String line : lines) {
                 Voucher voucher = Converter.toVoucher(line);
                 if (voucher.getId() == id) {
                     return Optional.of(voucher);
                 }
             }
-            reader.close();
             return Optional.empty();
         } catch (IOException e) {
             throw new NoSuchVoucherException("IO 문제로 바우처가 조회되지 않았습니다.");
@@ -52,16 +60,10 @@ public class InFileVoucherRepository implements VoucherRepository {
 
     @Override
     public List<Voucher> findAll() {
-        List<Voucher> result = new ArrayList<>();
         try {
-            BufferedReader reader = new BufferedReader(new FileReader(file));
-            String line;
-            while ((line = reader.readLine()) != null) {
-                Voucher voucher = Converter.toVoucher(line);
-                result.add(voucher);
-            }
-            reader.close();
-            return result;
+            return Files.readAllLines(file).stream()
+                    .map(Converter::toVoucher)
+                    .collect(Collectors.toList());
         } catch (IOException e) {
             throw new NoSuchVoucherException("IO 문제로 바우처가 조회되지 않았습니다.");
         }
