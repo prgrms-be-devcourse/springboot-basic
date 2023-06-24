0dca5a22b3dde01072b6808616d5c87aa4595119
hongxeob <sjun2918@naver.com>
2023-06-24 18:01:13 +0900

    test: 중복되는 테스트 하나로 통합

--- src/test/java/org/promgrammers/springbootbasic/controller/CommandTest.java
+++ src/test/java/org/promgrammers/springbootbasic/controller/CommandTest.java
@@ -1,8 +1,8 @@
 package org.promgrammers.springbootbasic.controller;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.promgrammers.springbootbasic.domain.voucher.model.Command;
 
@@ -12,65 +12,22 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class CommandTest {
 
-    @Test
-    @DisplayName("생성 성공 - 입력값 exit -> Command.EXIT")
-    void createCommandExitTest() throws Exception {
-
-        //given
-        String inputCommand = "exit";
-
-        //when
-        Command createdCommand = assertDoesNotThrow(() -> Command.of(inputCommand));
-
-        //then
-        assertThat(createdCommand).isEqualTo(Command.EXIT);
-    }
-
-    @Test
-    @DisplayName("생성 성공 - 입력값 exit -> Command.CREATE")
-    void createCommandCREATETest() throws Exception {
-
-        //given
-        String inputCommand = "create";
-
-        //when
-        Command createdCommand = assertDoesNotThrow(() -> Command.of(inputCommand));
-
-        //then
-        assertThat(createdCommand).isEqualTo(Command.CREATE);
-    }
-
-    @Test
-    @DisplayName("생성 성공 - 입력값 list -> Command.LIST")
-    void createCommandListTest() throws Exception {
-
-        //given
-        String inputCommand = "list";
-
-        //when
-        Command createdCommand = assertDoesNotThrow(() -> Command.of(inputCommand));
-
-        //then
-        assertThat(createdCommand).isEqualTo(Command.LIST);
-    }
-
-    @Test
-    @DisplayName("생성 성공 - 입력값 blackList -> Command.BLACKLIST")
-    void createCommandBlackListTest() throws Exception {
-
-        //given
-        String inputCommand = "blackList";
-
-        //when
-        Command createdCommand = assertDoesNotThrow(() -> Command.of(inputCommand));
-
-        //then
-        assertThat(createdCommand).isEqualTo(Command.BLACKLIST);
-    }
-
-    @DisplayName("생성 실패 - 입력값이 커맨드 타입에 없는 값")
     @ParameterizedTest
-    @ValueSource(strings = {"exi", "reate", "listttt"})
+    @DisplayName("생성 성공 - 올바른 커맨합드 입력")
+    @CsvSource(
+            {"exit, EXIT", "create, CREATE", "list,LIST", "blackList,BLACKLIST"})
+    void createCommandSuccessTest(String inputCommand, Command expectedCommand) {
+
+        //given -> when
+        Command createdCommand = assertDoesNotThrow(() -> Command.of(inputCommand));
+
+        //then
+        assertThat(createdCommand).isEqualTo(expectedCommand);
+    }
+
+    @DisplayName("생성 실패 - 올바르지 않은 커맨드 입력")
+    @ParameterizedTest
+    @ValueSource(strings = {"exi", "reate", "listttt", "blackListtt"})
     void createCommandFailTest(String input) throws Exception {
 
         assertThrows(IllegalArgumentException.class, () -> Command.of(input));
