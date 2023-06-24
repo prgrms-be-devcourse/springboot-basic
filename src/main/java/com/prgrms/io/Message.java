488e6de9f903d45ce208c4f861e7b434dafd0399
byeolhaha <j35635aa@gmail.com>
2023-06-24 18:03:29 +0900

    feat : 사용자에게 보여지는 메세지에 대해 Enum 분리

---
+++ src/main/java/com/prgrms/io/Message.java
@@ -0,0 +1,23 @@
+package com.prgrms.io;
+
+public enum Message {
+    START("=== Voucher Program === \n" +
+            "Type exit to exit the program.\n" +
+            "Type create to create a new voucher.\n" +
+            "Type list to list all vouchers."),
+    ERROR("잘못된 형태로 입력하였습니다. 다시 시작합니다."),
+    END("바우처 어플리케이션을 종료합니다."),
+    COMPLETE_CREATE("바우처가 등록되었습니다."),
+    EMPTY_REPOSITORY("만들어진 바우처 정책이 없습니다.");
+
+    private final String message;
+
+    Message(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return message;
+    }
+}
