d2b905eda87993e7ef7acc7e0168fddfb156fc9a
hi-june <wnsgur735@gmail.com>
2023-06-25 00:46:47 +0900

    feat: spring boot application 정의

--- springbasic/src/main/java/com/programmers/springbasic/SpringbasicApplication.java
+++ springbasic/src/main/java/com/programmers/springbasic/SpringbasicApplication.java
@@ -1,13 +1,16 @@
 package com.programmers.springbasic;
 
+import com.programmers.springbasic.domain.voucher.controller.VoucherController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringbasicApplication {
+	public static void main(String[] args) throws Exception {
+		ApplicationContext applicationContext = new SpringApplication(SpringbasicApplication.class).run(args);
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbasicApplication.class, args);
+		VoucherController voucherController = applicationContext.getBean(VoucherController.class);
+		voucherController.run();
 	}
-
 }
