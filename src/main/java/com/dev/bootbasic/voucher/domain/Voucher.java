39bd68058d749f50681a6c63a9de0a86e8a4b52b
jun <alap_u@naver.com>
2023-06-24 19:00:00 +0900

    refactor 바우처를 상속하는 구현체는 바우처타입을 갖고있는다.

--- src/main/java/com/dev/bootbasic/voucher/domain/Voucher.java
+++ src/main/java/com/dev/bootbasic/voucher/domain/Voucher.java
@@ -6,11 +6,13 @@ public abstract class Voucher {
 
     private static final String NOT_ASSIGNED_VOUCHER_ID_MESSAGE = "바우처 ID가 할당되지 않았습니다.";
     private final UUID id;
+    private final VoucherType voucherType;
     private final int discountAmount;
 
-    protected Voucher(UUID id, int discountAmount) {
+    protected Voucher(UUID id, VoucherType voucherType, int discountAmount) {
         validateId(id);
         this.id = id;
+        this.voucherType = voucherType;
         this.discountAmount = discountAmount;
     }
 
@@ -22,6 +24,10 @@ public abstract class Voucher {
 
     public UUID getId() {
         return id;
+    }
+
+    public VoucherType getVoucherType() {
+        return voucherType;
     }
 
     public int getDiscountAmount() {
