89f44f3fb1225a933b3081ee67b57c3ea3aca1fe
박은지 <qkrdmswl1018@naver.com>
2023-06-24 17:04:16 +0900

    test: 테스트코드 수정

--- src/test/java/com/prgrms/commandLineApplication/voucher/FixedAmountVoucherTest.java
+++ src/test/java/com/prgrms/commandLineApplication/voucher/FixedAmountVoucherTest.java
@@ -6,8 +6,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-import java.util.UUID;
-
 class FixedAmountVoucherTest {
 
   Voucher fixedAmountVoucher;
@@ -15,16 +13,19 @@ class FixedAmountVoucherTest {
   @ParameterizedTest
   @CsvSource(value = {"30000|70000", "5000|95000", "10000|90000"}, delimiter = '|')
   @DisplayName("FixedAmountVoucher 할인 계산 결과 일치 테스트")
-  void fixedAmountVoucher_계산_성공(double discountValue, double result) {
-    fixedAmountVoucher = new FixedAmountVoucher(discountValue);
+  void fixedAmountVoucher_계산_성공(double discountAmount, double result) {
+    String voucherType = "fixed";
+    fixedAmountVoucher = new FixedAmountVoucher(voucherType, discountAmount);
     assertThat(fixedAmountVoucher.discount(100000)).isEqualTo(result);
   }
 
   @ParameterizedTest
   @CsvSource(value = {"30000|10, 50000|20, 10010|30"}, delimiter = '|')
   @DisplayName("FixedAmountVoucher 할인 계산 결과 불일치 테스트")
-  void fixedAmountVoucher_계산_실패(double discountValue, double result) {
-    fixedAmountVoucher = new FixedAmountVoucher(discountValue);
+  void fixedAmountVoucher_계산_실패(double discountAmount, double result) {
+    String voucherType = "fixed";
+    fixedAmountVoucher = new FixedAmountVoucher(voucherType, discountAmount);
     assertThat(fixedAmountVoucher.discount(10000)).isEqualTo(result);
   }
+
 }
--- src/test/java/com/prgrms/commandLineApplication/voucher/PercentDiscountVoucherTest.java
+++ src/test/java/com/prgrms/commandLineApplication/voucher/PercentDiscountVoucherTest.java
@@ -4,8 +4,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-import java.util.UUID;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 class PercentDiscountVoucherTest {
@@ -15,16 +13,19 @@ class PercentDiscountVoucherTest {
   @ParameterizedTest
   @CsvSource(value = {"20|80000", "30|70000", "50|50000"}, delimiter = '|')
   @DisplayName("percentDiscountVoucher 할인 계산 결과 일치 테스트")
-  void percentDiscountVoucher_계산_성공(Long discountValue, Long result) {
-    percentDiscountVoucher = new PercentDiscountVoucher(discountValue);
-    assertThat(percentDiscountVoucher.discount(100000L)).isEqualTo(result);
+  void percentDiscountVoucher_계산_성공(double discountValue, double result) {
+    String voucherType = "percent";
+    percentDiscountVoucher = new PercentDiscountVoucher(voucherType, discountValue);
+    assertThat(percentDiscountVoucher.discount(100000)).isEqualTo(result);
   }
 
   @ParameterizedTest
   @CsvSource(value = {"101|0, 200|0, 220|0"}, delimiter = '|')
   @DisplayName("percentDiscountVoucher 할인 계산 결과 불일치 테스트")
-  void percentDiscountVoucher_계산_실패(Long discountValue, Long result) {
-    percentDiscountVoucher = new PercentDiscountVoucher(discountValue);
-    assertThat(percentDiscountVoucher.discount(100000L)).isEqualTo(result);
+  void percentDiscountVoucher_계산_실패(double discountValue, double result) {
+    String voucherType = "percent";
+    percentDiscountVoucher = new PercentDiscountVoucher(voucherType, discountValue);
+    assertThat(percentDiscountVoucher.discount(100000)).isEqualTo(result);
   }
+
 }
