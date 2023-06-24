0dadaf1bed3b23759e45f83ce40e3f07ca75951b
yumyeonghan <mgtmh991013@gmail.com>
2023-06-24 15:09:55 +0900

    feat: VoucherCreationRequest record 타입 적용

--- src/main/java/com/programmers/voucher/domain/VoucherFactory.java
+++ src/main/java/com/programmers/voucher/domain/VoucherFactory.java
@@ -6,10 +6,10 @@ import java.util.UUID;
 
 public class VoucherFactory {
     public static Voucher createVoucher(VoucherCreationRequest voucherCreationRequest) {
-        VoucherType voucherType = VoucherType.valueOf(voucherCreationRequest.getVoucherType());
+        VoucherType voucherType = VoucherType.valueOf(voucherCreationRequest.voucherType());
         return switch (voucherType) {
-            case FIXED -> FixedAmountVoucher.of(UUID.randomUUID(), voucherCreationRequest.getDiscountAmount());
-            case PERCENT -> PercentDiscountVoucher.of(UUID.randomUUID(), voucherCreationRequest.getDiscountAmount());
+            case FIXED -> FixedAmountVoucher.of(UUID.randomUUID(), voucherCreationRequest.discountAmount());
+            case PERCENT -> PercentDiscountVoucher.of(UUID.randomUUID(), voucherCreationRequest.discountAmount());
         };
     }
 
--- src/main/java/com/programmers/voucher/request/VoucherCreationRequest.java
+++ src/main/java/com/programmers/voucher/request/VoucherCreationRequest.java
@@ -2,28 +2,15 @@ package com.programmers.voucher.request;
 
 import java.util.Objects;
 
-public class VoucherCreationRequest {
-    private final String voucherType;
-    private final long discountAmount;
-
-    public VoucherCreationRequest(String voucherType, long discountAmount) {
+public record VoucherCreationRequest(String voucherType, long discountAmount) {
+    public VoucherCreationRequest {
         validateVoucherCreationRequest(voucherType, discountAmount);
-        this.voucherType = voucherType;
-        this.discountAmount = discountAmount;
     }
 
     private static void validateVoucherCreationRequest(String voucherType, long discountAmount) {
-        if(Objects.isNull(voucherType) || discountAmount == 0) {
+        if (Objects.isNull(voucherType) || discountAmount == 0) {
             throw new IllegalArgumentException("바우처 타입과 할인양을 입력해주세요.");
         }
-    }
-
-    public String getVoucherType() {
-        return voucherType;
-    }
-
-    public long getDiscountAmount() {
-        return discountAmount;
     }
 
 }
