83b3ab99b0f1f1a8ea12608a5a23626c96687234
weonest <geonhee33@naver.com>
2023-06-24 21:30:37 +0900

    refactor: 퍼센트 바우처 할인 테스트 PR 반영

--- src/test/java/com/programmers/voucher/domain/PercentDiscountVoucherTest.java
+++ src/test/java/com/programmers/voucher/domain/PercentDiscountVoucherTest.java
@@ -13,23 +13,22 @@ import static org.junit.jupiter.api.Assertions.*;
 class PercentDiscountVoucherTest {
 
 
-    @DisplayName("퍼센트 바우처 유효기간 테스트")
-    @Test
-    void PercentVoucherAvailableTest() {
-        long amount = 10;
-        LocalDate localDateTime = LocalDate.of(2023,6, 17);
-
-        PercentDiscountVoucher percentDiscountVoucher = new PercentDiscountVoucher(UUID.randomUUID(), amount, localDateTime);
-        assertTrue(percentDiscountVoucher.available());
-    }
-
-    @DisplayName("퍼센트 바우처 할인 테스트")
+    @DisplayName("유효하지 않은 퍼센트 바우처 할인 테스트")
     @ParameterizedTest
-    @CsvSource(value = {"100, 50", "100, 0", "100, 100", "50, -1"})
-    void discountPercentTest(long beforeDiscount, long amount) {
-        LocalDate localDate = LocalDate.of(2023, 6, 20);
+    @CsvSource(value = {"100, 90", "50, 2", "5, 3", "126346, 100", "1235123511, 14353", "100, 99"})
+    void notAvailableVoucherDiscountTest(long beforeDiscount, long amount) {
+        LocalDate localDate = LocalDate.of(2023,6, 13);
 
         PercentDiscountVoucher percentDiscountVoucher = new PercentDiscountVoucher(UUID.randomUUID(), amount, localDate);
-        assertEquals(50, percentDiscountVoucher.discount(beforeDiscount));
+        assertEquals(beforeDiscount - amount, percentDiscountVoucher.discount(beforeDiscount));
+    }
+
+    @DisplayName("유효한 퍼센트 바우처 할인 테스트")
+    @ParameterizedTest
+    @CsvSource(value = {"100, 90", "50, 2", "5, 3", "126346, 100", "1235123511, 143532343", "100, 99"})
+    void availableVoucherDiscountTest(long beforeDiscount, long amount) {
+
+        PercentDiscountVoucher percentDiscountVoucher = new PercentDiscountVoucher(UUID.randomUUID(), amount, LocalDate.now());
+        assertEquals(beforeDiscount - amount, percentDiscountVoucher.discount(beforeDiscount));
     }
 }
