aa6fc8de2a370566d079ede395d192eab7f45ae6
heenahan <mby1997s@gmail.com>
2023-06-25 00:21:26 +0900

    feat : voucher type에 팩토리 메서드 추가

--- src/main/java/com/prgms/springbootbasic/util/VoucherType.java
+++ src/main/java/com/prgms/springbootbasic/util/VoucherType.java
@@ -8,27 +8,32 @@ import com.prgms.springbootbasic.exception.NoSuchVoucherTypeException;
 import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
+import java.util.function.BiFunction;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public enum VoucherType {
 	
-	FIXED("fixed", (amount) -> new FixedAmountVoucher(UUID.randomUUID(), amount)),
-	PERCENT("percent", (percent) -> new PercentAmountVoucher(UUID.randomUUID(), percent));
+	FIXED("fixed", (amount) -> new FixedAmountVoucher(UUID.randomUUID(), amount), FixedAmountVoucher::new),
+	PERCENT("percent", (percent) -> new PercentAmountVoucher(UUID.randomUUID(), percent), PercentAmountVoucher::new);
 	
 	private static final String ERROR_NO_SUCH_VOUCHER_TYPE = "[Error] Can't Find Such Voucher Type";
 	private static final List<VoucherType> VOUCHER_TYPE = Arrays.stream(VoucherType.values()).collect(Collectors.toList());
 	private final String type;
 	private final Function<Long, Voucher> voucherFunction;
+	private final BiFunction<UUID, Long, Voucher> voucherBiFunction;
 	
-	VoucherType(String type, Function<Long, Voucher> voucherFunction) {
+	VoucherType(String type, Function<Long, Voucher> voucherFunction, BiFunction<UUID, Long, Voucher> voucherBiFunction) {
 		this.type = type;
 		this.voucherFunction = voucherFunction;
+		this.voucherBiFunction = voucherBiFunction;
 	}
 	
 	public Voucher createVoucher(long number) {
 		return voucherFunction.apply(number);
 	}
+	
+	public Voucher exsistVoucher(UUID voucherId, long number) { return voucherBiFunction.apply(voucherId, number); }
 	
 	public static VoucherType of(String type) {
 		return VOUCHER_TYPE.stream()
