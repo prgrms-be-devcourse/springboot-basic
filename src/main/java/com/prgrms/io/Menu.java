83b3b3bf2de674732e573ae34ebb40c1f052504f
byeolhaha <j35635aa@gmail.com>
2023-06-25 00:25:12 +0900

    fix : 메뉴 Enum에서 불필요한 속성 제거

--- src/main/java/com/prgrms/io/Menu.java
+++ src/main/java/com/prgrms/io/Menu.java
@@ -4,19 +4,13 @@ import java.util.Arrays;
 import java.util.Optional;
 
 public enum Menu {
-    EXIT("exit"),
-    CREATE("create"),
-    LIST("list");
-
-    private final String value;
-
-    Menu(String value) {
-        this.value = value;
-    }
+    EXIT,
+    CREATE,
+    LIST;
 
     public static Optional<Menu> findBySelectedMenu(String selectedMenu) {
-        return Arrays.stream(Menu.values())
-                .filter(m -> m.value.equals(selectedMenu))
+        return Arrays.stream(values())
+                .filter(m -> m.name().equalsIgnoreCase(selectedMenu))
                 .findFirst();
     }
 
