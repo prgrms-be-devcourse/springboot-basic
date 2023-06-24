b90611ba3b088b568ff714317e20fe78665565d6
dingding-21 <phj8221@naver.com>
2023-06-25 02:42:05 +0900

    feat: 메모리에 저장하는 VoucherRepository 클래스 생성

---
+++ voucher/src/main/java/org/prgms/voucher/MemoryVoucherRepository.java
@@ -0,0 +1,13 @@
+package org.prgms.voucher;
+
+import java.util.List;
+
+public class MemoryVoucherRepository implements VoucherRepository {
+    @Override
+    public Voucher create(AmountType amountType) {
+    }
+
+    @Override
+    public List<Voucher> findAll() {
+    }
+}
