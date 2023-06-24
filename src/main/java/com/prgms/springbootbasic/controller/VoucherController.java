60f44d0dfd42e2074c0b969fb0afe614ff0a7304
heenahan <mby1997s@gmail.com>
2023-06-25 00:22:40 +0900

    refactor : 컨트롤러에서 바우처 저장소 의존

--- src/main/java/com/prgms/springbootbasic/controller/VoucherController.java
+++ src/main/java/com/prgms/springbootbasic/controller/VoucherController.java
@@ -2,43 +2,48 @@ package com.prgms.springbootbasic.controller;
 
 import com.prgms.springbootbasic.model.Voucher;
 import com.prgms.springbootbasic.model.VouchersInMemory;
+import com.prgms.springbootbasic.model.VouchersStorage;
 import com.prgms.springbootbasic.ui.Console;
 import com.prgms.springbootbasic.util.Menu;
 import com.prgms.springbootbasic.util.VoucherType;
+import org.springframework.stereotype.Controller;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Controller
 public class VoucherController {
 	
 	private final Console console;
+	private final VouchersStorage vouchersStorage;
 	
-	public VoucherController(Console console) {
+	public VoucherController(Console console, VouchersStorage vouchersStorage) {
 		this.console = console;
+		this.vouchersStorage = vouchersStorage;
 	}
 	
 	public boolean run() {
 		try {
 			String command = console.init();
 			Menu menu = Menu.of(command);
-			VouchersInMemory vouchersInMemory = new VouchersInMemory();
 			if (menu == Menu.EXIT) {
 				exit();
 				return false;
 			}
-			init(menu, vouchersInMemory);
+			init(menu);
 		} catch (Exception e) {
 			console.showExceptionMessage(e.getMessage());
 		}
 		return true;
 	}
 	
-	private void init(Menu menu, VouchersInMemory vouchersInMemory) {
+	private void init(Menu menu) throws IOException {
 		if (menu == Menu.CREATE) {
 			Voucher voucher = create();
-			vouchersInMemory.save(voucher);
+			vouchersStorage.save(voucher);
 		} else if (menu == Menu.LIST) {
-			List<Voucher> vouchers = vouchersInMemory.findAll();
+			List<Voucher> vouchers = vouchersStorage.findAll();
 			list(vouchers);
 		}
 	}
@@ -79,8 +84,8 @@ public class VoucherController {
 	
 	private void list(List<Voucher> vouchers) {
 		List<String> vouchersOfString = vouchers.stream()
-				                        .map(Voucher::toString)
-				                        .collect(Collectors.toList());
+				.map(Voucher::toString)
+				.collect(Collectors.toList());
 		console.showVoucherList(vouchersOfString);
 	}
 	
