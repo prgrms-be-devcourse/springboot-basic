97a906df77ac8f8a7326e32a0255435eaa12fb0e
wonu606 <wonu606@gmail.com>
2023-06-25 04:52:24 +0900

    init: 스프링 부트 프로젝트 초기화

---
+++ voucher-manager/src/main/java/com/wonu606/vouchermanager/VoucherManagerApplication.java
@@ -0,0 +1,13 @@
+package com.wonu606.vouchermanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoucherManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoucherManagerApplication.class, args);
+	}
+
+}
---
+++ voucher-manager/src/test/java/com/wonu606/vouchermanager/VoucherManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wonu606.vouchermanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoucherManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
