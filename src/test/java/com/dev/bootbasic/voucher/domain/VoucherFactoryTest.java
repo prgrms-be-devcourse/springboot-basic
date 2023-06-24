9afe80fc4bf6ce68c31b70a3f2c6a57a827b2c5b
jun <alap_u@naver.com>
2023-06-25 01:40:40 +0900

    test 바우처를 생성하지 못하면 예외를 발생한다.

--- src/test/java/com/dev/bootbasic/voucher/domain/VoucherFactoryTest.java
+++ src/test/java/com/dev/bootbasic/voucher/domain/VoucherFactoryTest.java
@@ -1,11 +1,13 @@
 package com.dev.bootbasic.voucher.domain;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class VoucherFactoryTest {
 
@@ -20,9 +22,21 @@ class VoucherFactoryTest {
     void createVoucherTest(String type, int amount, VoucherType expectedVoucherType) {
         Voucher voucher = voucherFactory.create(UUID.randomUUID(), type, amount);
 
-        Assertions.assertThat(voucher)
+        assertThat(voucher)
                 .extracting(Voucher::getVoucherType, Voucher::getDiscountAmount)
                 .containsExactly(expectedVoucherType, amount);
+    }
+
+    @DisplayName("바우처를 생성하지 못하면 예외를 발생한다.")
+    @ParameterizedTest
+    @CsvSource({
+            "asd, 5000, NoSuchElementException",
+            ", 50, NoSuchElementException",
+    })
+    void createVoucherFailTest(String type, int amount, RuntimeException exception) {
+
+        assertThatThrownBy(() -> voucherFactory.create(UUID.randomUUID(), type, amount))
+                .isInstanceOf(exception.getClass());
     }
 
 }
