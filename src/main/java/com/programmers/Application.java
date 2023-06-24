d1f7922af2d4f7c1aaf2a0e3ea3ee41ee2e1dd86
hyee0715 <hyee0715@naver.com>
2023-06-24 15:21:09 +0900

    chore: Application 클래스 주석 해제

--- src/main/java/com/programmers/Application.java
+++ src/main/java/com/programmers/Application.java
@@ -1,12 +1,13 @@
 package com.programmers;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
-        //SpringApplication.run(Application.class, args);
+        SpringApplication.run(Application.class, args);
 
         VoucherController voucherController = new VoucherController();
         voucherController.run();
