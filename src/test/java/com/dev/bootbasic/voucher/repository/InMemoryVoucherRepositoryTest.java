502ed91302a23b5b26b6a337e7aa22bc371049db
jun <alap_u@naver.com>
2023-06-25 02:18:40 +0900

    test 불필요한 중복테스트 제거

--- src/test/java/com/dev/bootbasic/voucher/repository/InMemoryVoucherRepositoryTest.java
+++ src/test/java/com/dev/bootbasic/voucher/repository/InMemoryVoucherRepositoryTest.java
@@ -1,6 +1,5 @@
 package com.dev.bootbasic.voucher.repository;
 
-import com.dev.bootbasic.voucher.domain.FixedAmountVoucher;
 import com.dev.bootbasic.voucher.domain.PercentDiscountVoucher;
 import com.dev.bootbasic.voucher.domain.Voucher;
 import org.junit.jupiter.api.DisplayName;
@@ -17,22 +16,13 @@ class InMemoryVoucherRepositoryTest {
 
     @DisplayName("바우처를 저장할 수 있다.")
     @Test
-    void saveVoucherTest(){
-        FixedAmountVoucher fixedVoucher = createFixedVoucher(5000);
+    void saveVoucherTest() {
         PercentDiscountVoucher percentVoucher = createPercentVoucher(50);
 
-        UUID fixedVoucherId = voucherRepository.saveVoucher(fixedVoucher);
         UUID percentVoucherId = voucherRepository.saveVoucher(percentVoucher);
 
-        Optional<Voucher> foundFixedVoucher = voucherRepository.findVoucher(fixedVoucherId);
         Optional<Voucher> foundPercentVoucher = voucherRepository.findVoucher(percentVoucherId);
-
-        assertThat(foundFixedVoucher.get()).isEqualTo(fixedVoucher);
         assertThat(foundPercentVoucher.get()).isEqualTo(percentVoucher);
-     }
-
-    private FixedAmountVoucher createFixedVoucher(int discountAmount) {
-        return FixedAmountVoucher.of(UUID.randomUUID(), discountAmount);
     }
 
     private PercentDiscountVoucher createPercentVoucher(int discountAmount) {
