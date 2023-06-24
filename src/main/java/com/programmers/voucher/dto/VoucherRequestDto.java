e780e954d5be8e64c278681b6ccc80157aedf59a
weonest <geonhee33@naver.com>
2023-06-24 20:34:54 +0900

    refactor: VoucherRequestDto record 클래스로 변경

--- src/main/java/com/programmers/voucher/dto/VoucherRequestDto.java
+++ src/main/java/com/programmers/voucher/dto/VoucherRequestDto.java
@@ -1,12 +1,8 @@
 package com.programmers.voucher.dto;
 
-public class VoucherRequestDto {
-    private String voucherType;
-    private long discountAmount;
+public record VoucherRequestDto(String voucherType, long discountAmount) {
 
-    public VoucherRequestDto(String voucherType, long discountAmount) {
-        this.voucherType = voucherType;
-        this.discountAmount = discountAmount;
+    public VoucherRequestDto {
     }
 
     public String getVoucherType() {
