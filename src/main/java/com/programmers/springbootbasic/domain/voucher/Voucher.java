324dd4c72233996ab7a55660f586c578db3f4e9b
sangminnim <sangmins930@gmail.com>
2023-06-24 22:55:40 +0900

    feat: voucherId ,name, expirationDate에 대한 검증로직 구현

--- src/main/java/com/programmers/springbootbasic/domain/voucher/Voucher.java
+++ src/main/java/com/programmers/springbootbasic/domain/voucher/Voucher.java
@@ -11,16 +11,34 @@ public abstract class Voucher {
     private VoucherState state = VoucherState.AVAILABLE;
 
     protected Voucher(UUID voucherId, String name, LocalDateTime expirationDate) {
+        if (isInvalidVoucherId(voucherId)) {
+            throw new IllegalArgumentException("잘못된 아이디 값");
+        }
         this.voucherId = voucherId;
+        if (isInvalidName(name)) {
+            throw new IllegalArgumentException("잘못된 이름");
+        }
         this.name = name;
         this.minimumPriceCondition = 0L;
+        if (isInvalidExpirationDate(expirationDate)) {
+            throw new IllegalArgumentException("잘못된 유효기간");
+        }
         this.expirationDate = expirationDate;
     }
 
     protected Voucher(UUID voucherId, String name, Long minimumPriceCondition, LocalDateTime expirationDate) {
+        if (isInvalidVoucherId(voucherId)) {
+            throw new IllegalArgumentException("잘못된 아이디 값");
+        }
         this.voucherId = voucherId;
+        if (isInvalidName(name)) {
+            throw new IllegalArgumentException("잘못된 이름");
+        }
         this.name = name;
         this.minimumPriceCondition = minimumPriceCondition;
+        if (isInvalidExpirationDate(expirationDate)) {
+            throw new IllegalArgumentException("잘못된 유효기간");
+        }
         this.expirationDate = expirationDate;
     }
 
@@ -39,6 +57,20 @@ public abstract class Voucher {
     public VoucherState getState() {
         return state;
     }
+
+    private boolean isInvalidVoucherId(UUID voucherId) {
+        return voucherId == null;
+    }
+
+    private boolean isInvalidName(String name) {
+        return name == null || name.isEmpty();
+    }
+
+    private boolean isInvalidExpirationDate(LocalDateTime expirationDate) {
+        LocalDateTime now = LocalDateTime.now();
+        return now.isAfter(expirationDate);
+    }
+
 
     protected boolean isExpiration(LocalDateTime now) {
         return now.isAfter(expirationDate);
