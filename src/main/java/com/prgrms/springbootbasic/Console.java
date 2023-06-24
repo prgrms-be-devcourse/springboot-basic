b56b67f37d3e6a91e2998f8a358167d7339d57ad
jay-so <merry9504@naver.com>
2023-06-25 08:40:51 +0900

    Feat: Console클래스에 Text.io를 이용하여 입,출력관련 메서드 구현

--- src/main/java/com/prgrms/springbootbasic/Console.java
+++ src/main/java/com/prgrms/springbootbasic/Console.java
@@ -2,22 +2,27 @@ package com.prgrms.springbootbasic;
 
 import com.prgrms.springbootbasic.io.Input;
 import com.prgrms.springbootbasic.io.Output;
+import jline.Terminal;
+import org.beryx.textio.TextIO;
+import org.beryx.textio.TextTerminal;
 
 
-public class Console implements Input, Output {
+public class Console<T extends TextTerminal>  implements Input, Output {
+    private TextIO textIO;
+    private T terminal;
 
     @Override
     public String readCommand(String message) {
-        return null;
+        return textIO.newStringInputReader().read(message);
     }
 
     @Override
     public String readString(String message) {
-        return null;
+        return textIO.newStringInputReader().read(message);
     }
 
     @Override
     public void println(String message) {
-        return;
+        terminal.println(message);
     }
 }
