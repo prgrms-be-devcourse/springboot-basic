cb864a8d45cea82f95c3e8437a895d4d8fe6c34a
hi-june <wnsgur735@gmail.com>
2023-06-25 00:38:47 +0900

    feat: main menu 문구 정의

---
+++ springbasic/src/main/java/com/programmers/springbasic/domain/voucher/model/VoucherMenu.java
@@ -0,0 +1,19 @@
+package com.programmers.springbasic.domain.voucher.model;
+
+public enum VoucherMenu {
+    MAIN_MENU_MESSAGE("=== Voucher Program ==="),
+    EXIT_MENU_MESSAGE("Type exit to exit the program."),
+    CREATE_MENU_MESSAGE("Type create to create a new voucher."),
+    LIST_MENU_MESSAGE("Type list to list all vouchers.");
+
+    String message;
+
+    VoucherMenu(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return message;
+    }
+}
