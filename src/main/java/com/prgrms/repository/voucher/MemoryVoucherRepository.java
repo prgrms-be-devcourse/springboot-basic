da95f7086ce4b3636a401d07a502274cdc23c087
byeolhaha <j35635aa@gmail.com>
2023-06-24 17:50:32 +0900

    feat : 휘발성 바우처 저장소 구현

---
+++ src/main/java/com/prgrms/repository/voucher/MemoryVoucherRepository.java
@@ -0,0 +1,34 @@
+package com.prgrms.repository.voucher;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
+
+@Repository
+@Primary
+public class MemoryVoucherRepository implements VoucherRepository {
+    private final Map<UUID, Voucher> storage = new ConcurrentHashMap<>();
+
+    @Override
+    public Optional<Voucher> findById(UUID voucherId) {
+        return Optional.ofNullable(storage.get(voucherId));
+    }
+
+    @Override
+    public Voucher insert(Voucher voucher) {
+        storage.put(voucher.getVoucherId(), voucher);
+        return voucher;
+    }
+
+    @Override
+    public List<Voucher> getAllVoucherList() {
+        return storage.values().stream()
+                .collect(Collectors.toList());
+    }
+}
