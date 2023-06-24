304919af4f338f855c71d1fe9bc6d698a7993a2c
jay-so <merry9504@naver.com>
2023-06-25 07:21:53 +0900

    Feat: MemoryVoucherRepository 구현

---
+++ src/main/java/com/prgrms/springbootbasic/repository/MemoryVoucherRepository.java
@@ -0,0 +1,25 @@
+package com.prgrms.springbootbasic.repository;
+
+import com.prgrms.springbootbasic.domain.Voucher;
+import com.prgrms.springbootbasic.service.VoucherService;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class MemoryVoucherRepository implements VoucherRepository {
+    private final Map<UUID,Voucher> storage = new ConcurrentHashMap<>();
+
+    @Override
+    public Voucher insert(Voucher voucher) {
+        storage.put(voucher.getVoucherId(), voucher);
+        return voucher;
+    }
+
+    @Override
+    public List<Voucher> getAllVouchersList() {
+        return new ArrayList<>(storage.values());
+    }
+}
