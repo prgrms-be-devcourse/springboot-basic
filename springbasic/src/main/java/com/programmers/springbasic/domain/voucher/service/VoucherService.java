a70dafb6b67e736ca7108e116884d80bd6592b5e
hi-june <wnsgur735@gmail.com>
2023-06-25 00:31:35 +0900

    feat: voucher service 정의

---
+++ springbasic/src/main/java/com/programmers/springbasic/domain/voucher/service/VoucherService.java
@@ -0,0 +1,47 @@
+package com.programmers.springbasic.domain.voucher.service;
+
+import com.programmers.springbasic.domain.voucher.entity.FixedAmountVoucher;
+import com.programmers.springbasic.domain.voucher.entity.PercentDiscountVoucher;
+import com.programmers.springbasic.domain.voucher.repository.FixedAmountVoucherRepository;
+import com.programmers.springbasic.domain.voucher.repository.PercentDiscountVoucherRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+@Service
+@RequiredArgsConstructor
+public class VoucherService {
+    private final FixedAmountVoucherRepository fixedVoucherRepository;
+    private final PercentDiscountVoucherRepository percentVoucherRepository;
+
+    public void createFixedAmountVoucher(double fixedAmount) {
+        FixedAmountVoucher voucher = new FixedAmountVoucher(fixedAmount);
+
+        fixedVoucherRepository.save(voucher);
+    }
+
+    public void createPercentDiscountVoucher(double percentDiscount) {
+        PercentDiscountVoucher voucher = new PercentDiscountVoucher(percentDiscount);
+
+        percentVoucherRepository.save(voucher);
+    }
+
+    public List<String> getAllFixedAmountVoucherInfo() {
+        List<FixedAmountVoucher> fixedAmountVouchers = fixedVoucherRepository.findAll();
+
+        return fixedAmountVouchers.stream()
+                .map(v -> v.getInfo())
+                .collect(Collectors.toList());
+    }
+
+    public List<String> getAllPercentDiscountVoucherInfo() {
+        List<PercentDiscountVoucher> percentDiscountVouchers = percentVoucherRepository.findAll();
+
+        return percentDiscountVouchers.stream()
+                .map(v -> v.getInfo())
+                .collect(Collectors.toList());
+    }
+}
