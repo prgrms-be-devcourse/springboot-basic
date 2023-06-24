863ffdc55f32abcf51684bb57ac7110c3ed6857b
juno-junho <ssmm0205@naver.com>
2023-06-25 05:42:00 +0900

    docs: 프로젝트 생성 및 gitignore 작성

---
+++ voucher/src/test/java/com/ray/junho/voucher/VoucherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ray.junho.voucher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoucherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
