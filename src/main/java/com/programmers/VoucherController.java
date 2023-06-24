5be7a8d61513bf63b1ed980962ace3777db4f7e8
hyee0715 <hyee0715@naver.com>
2023-06-24 19:22:19 +0900

    refactor: AppConfiguration 제거

--- src/main/java/com/programmers/VoucherController.java
+++ src/main/java/com/programmers/VoucherController.java
@@ -5,7 +5,6 @@ import com.programmers.io.Console;
 import com.programmers.service.VoucherService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.ansi.AnsiOutput;
 import org.springframework.stereotype.Controller;
 
 import java.util.List;
