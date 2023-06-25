b430eebdf52a1bbb0d2be9a429dd704aeda67f44
byeolhaha <j35635aa@gmail.com>
2023-06-26 00:48:59 +0900

    fix : Message Enum과 VoucherPolicy Enum 메서드 수정에 따른 Input 수정

--- src/main/java/com/prgrms/io/Output.java
+++ src/main/java/com/prgrms/io/Output.java
@@ -8,25 +8,25 @@ import java.util.List;
 
 public class Output {
     public void viewStartMessage() {
-        System.out.println(Message.START.toString());
+        System.out.println(Message.START.getMessage());
     }
 
     public void viewInputError() {
-        System.out.println(Message.ERROR.toString());
+        System.out.println(Message.ERROR.getMessage());
     }
 
     public void viewEndMessage() {
-        System.out.println(Message.END.toString());
+        System.out.println(Message.END.getMessage());
     }
 
     public void viewVoucherOption() {
         for (VoucherPolicy voucherPolicy : VoucherPolicy.values()) {
-            System.out.println(voucherPolicy.toString());
+            System.out.println(voucherPolicy.voucherPolicyOptionGuid());
         }
     }
 
     public static void viewDiscountGuide(VoucherPolicy voucherPolicy) {
-        System.out.println(voucherPolicy.getDiscountGuide());
+        System.out.println(voucherPolicy.discountGuide());
     }
 
     public static void viewVoucherList(List<VoucherResponse> voucherList) {
