54276f37faa1ad4eac2b072105a47294de36c559
jay-so <merry9504@naver.com>
2023-06-25 09:02:17 +0900

    rename: FixedDiscountController -> FixedDiscountController, PercentDiscountController-> PercentDiscountVoucherController로 클래스명 변경

--- src/main/java/com/prgrms/springbootbasic/controller/FixedDiscountVoucherController.java
+++ src/main/java/com/prgrms/springbootbasic/controller/FixedDiscountVoucherController.java
@@ -4,12 +4,12 @@ import com.prgrms.springbootbasic.io.Input;
 import com.prgrms.springbootbasic.io.Output;
 import com.prgrms.springbootbasic.service.VoucherService;
 
-public class FixedDiscountController {
+public class FixedDiscountVoucherController {
     private final Input input;
     private final Output output;
     private final VoucherService voucherService;
 
-    public FixedDiscountController(Input input, Output output, VoucherService voucherService) {
+    public FixedDiscountVoucherController(Input input, Output output, VoucherService voucherService) {
         this.input = input;
         this.output = output;
         this.voucherService = voucherService;
--- src/main/java/com/prgrms/springbootbasic/controller/PercentDiscountVoucherController.java
+++ src/main/java/com/prgrms/springbootbasic/controller/PercentDiscountVoucherController.java
@@ -4,12 +4,12 @@ import com.prgrms.springbootbasic.io.Input;
 import com.prgrms.springbootbasic.io.Output;
 import com.prgrms.springbootbasic.service.VoucherService;
 
-public class PercentDiscountController {
+public class PercentDiscountVoucherController {
     private final Input input;
     private final Output output;
     private final VoucherService voucherService;
 
-    public PercentDiscountController(Input input, Output output, VoucherService voucherService) {
+    public PercentDiscountVoucherController(Input input, Output output, VoucherService voucherService) {
         this.input = input;
         this.output = output;
         this.voucherService = voucherService;
