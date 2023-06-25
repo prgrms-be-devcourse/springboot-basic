832affcffb649ed794810e67989d876297c36280
jay-so <merry9504@naver.com>
2023-06-25 20:54:52 +0900

    feat: PercentDiscountVoucher의 입력 범위 오류, 숫자 외의 타입 입력할 시 오류, 생성 시 발생한 오류 로그 구현

--- src/main/java/com/prgrms/springbootbasic/controller/PercentDiscountVoucherController.java
+++ src/main/java/com/prgrms/springbootbasic/controller/PercentDiscountVoucherController.java
@@ -6,6 +6,7 @@ import com.prgrms.springbootbasic.io.Input;
 import com.prgrms.springbootbasic.io.Output;
 import com.prgrms.springbootbasic.service.VoucherService;
 import java.util.UUID;
+import static com.prgrms.springbootbasic.Console.logger;
 
 public class PercentDiscountVoucherController {
     private final Input input;
@@ -20,21 +21,24 @@ public class PercentDiscountVoucherController {
 
     public void createPercentDiscountVoucher() {
         try {
-            String percentStr = input.readString("PercentDiscountVoucher의 percent를 입력해주세요.(할인 퍼센트만 입력,%표시 제외)");
+            String percentStr = input.readString("생성할 PercentDiscountVoucher의 percent를 입력해주세요.(할인 퍼센트만 입력,%표시 제외)");
             double percent = Double.parseDouble(percentStr);
             if (percent < 0 || percent > 100) {
                 output.println("허용되지 않는 퍼센트 값입니다. 0과 100 사이의 값으로 입력해주세요.");
+                logger.error("PercentDiscountVoucher 입력 범위 오류");
                 return;
             }
             Voucher voucher = new PercentDiscountVoucher(UUID.randomUUID(), (long) percent);
             voucherService.createPercentDiscountVoucher(percent);
 
             output.println("새로운 PercentDiscountVoucher가 생성되었습니다.");
-            output.println("생성된 PercentDiscountVoucher: " + voucher.getDiscount());
+            output.println("생성된 PercentDiscountVoucher: " + voucher.getDiscount() + "입니다.");
         } catch (NumberFormatException e) {
             output.println("유효하지 않은 입력입니다. 숫자로 입력해주세요.");
+            logger.error("PercentDiscountVoucher 입력 타입 오류");
         } catch (Exception e) {
             output.println("PercentDiscountVoucher 생성 중 오류가 발생했습니다.");
+            logger.error("PercentDiscountVoucher 생성 오류");
         }
     }
 }
