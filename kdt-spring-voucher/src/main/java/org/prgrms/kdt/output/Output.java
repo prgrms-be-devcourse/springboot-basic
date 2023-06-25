9a017af21d9eb70c8b21eb1a937bfcb18ea58b45
KimJaeUng <78838534+kju2405@users.noreply.github.com>
2023-06-25 18:23:28 +0900

    feat: 각 단계에 따른 console 출력

--- kdt-spring-voucher/src/main/java/org/prgrms/kdt/output/Output.java
+++ kdt-spring-voucher/src/main/java/org/prgrms/kdt/output/Output.java
@@ -3,4 +3,9 @@ package org.prgrms.kdt.output;
 public interface Output {
     void displayMenu();
     void displayUserInputLine();
+
+    void displayVoucherCreateMenu();
+    void displayFixedAmountInputValue();
+    void displayPercentDiscountInputValue();
+    void userInputWrongValue();
 }
--- kdt-spring-voucher/src/main/java/org/prgrms/kdt/output/OutputConsole.java
+++ kdt-spring-voucher/src/main/java/org/prgrms/kdt/output/OutputConsole.java
@@ -8,9 +8,9 @@ public class OutputConsole implements Output {
     @Override
     public void displayMenu() {
         System.out.println("=== Voucher Program ===");
-        printMenuLine("exit", "the program");
-        printMenuLine("create", "a new voucher");
-        printMenuLine("list", "all vouchers");
+        printMenuLine("exit", "exit the program");
+        printMenuLine("create", "create a new voucher");
+        printMenuLine("list", "list all vouchers");
         System.out.println();
     }
 
@@ -19,7 +19,30 @@ public class OutputConsole implements Output {
         System.out.print("Please Type command : ");
     }
 
-    private void printMenuLine(String userInput,String inputExplanation){
-        System.out.printf("Type %s to %s %s.%n",userInput,userInput,inputExplanation);
+    @Override
+    public void displayVoucherCreateMenu() {
+        System.out.println("=== What voucher do you want to make? ===");
+        printMenuLine("fixedAmount", "create FixedAmountVoucher");
+        printMenuLine("percentDiscount", "create PercentDiscountVoucher");
+        System.out.println();
+    }
+
+    @Override
+    public void displayFixedAmountInputValue() {
+        printMenuLine("FixedAmount value", "create FixedAmountVoucher");
+    }
+
+    @Override
+    public void displayPercentDiscountInputValue() {
+        printMenuLine("PercentDiscount value", "create PercentDiscountVoucher");
+    }
+
+    @Override
+    public void userInputWrongValue() {
+        System.out.println("Please check the input value.");
+    }
+
+    private void printMenuLine(String userInput, String inputExplanation) {
+        System.out.printf("Type %s to %s.%n", userInput, userInput, inputExplanation);
     }
 }
