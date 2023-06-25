96221bec06f6da561d4255a633b15a3c996991c6
ddongpuri <fnffnfkffk1112@gmail.com>
2023-06-25 23:33:25 +0900

    init : 프로젝트 생성

---
+++ voucher-mission/src/main/java/org/prgrms/vouchermission/VoucherMissionApplication.java
@@ -0,0 +1,13 @@
+package org.prgrms.vouchermission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoucherMissionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VoucherMissionApplication.class, args);
+    }
+
+}
