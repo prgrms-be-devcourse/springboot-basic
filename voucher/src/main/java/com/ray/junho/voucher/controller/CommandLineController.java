3e8ac698c216b6c49308cb8496c62f1426d89358
juno-junho <ssmm0205@naver.com>
2023-06-25 09:56:31 +0900

    feat(view): Console 인터페이스 생성 및 컨트롤러 생성

---
+++ voucher/src/main/java/com/ray/junho/voucher/controller/CommandLineController.java
@@ -0,0 +1,24 @@
+package com.ray.junho.voucher.controller;
+
+import com.ray.junho.voucher.repository.VoucherRepository;
+import com.ray.junho.voucher.service.VoucherService;
+import com.ray.junho.voucher.view.Console;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class CommandLineController {
+
+    private final VoucherService voucherService;
+    private final VoucherRepository voucherRepository;
+    private final Console console;
+
+    public CommandLineController(VoucherService voucherService, VoucherRepository voucherRepository, Console console) {
+        this.voucherService = voucherService;
+        this.voucherRepository = voucherRepository;
+        this.console = console;
+    }
+
+
+
+
+}
