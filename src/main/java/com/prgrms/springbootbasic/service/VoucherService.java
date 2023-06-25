434696cc8ef204618be0d1fda5f4f1a6886e7b1c
jay-so <merry9504@naver.com>
2023-06-25 18:09:14 +0900

    refactor: VoucherService를 통해 FixedDiscountVoucher와 DiscountVoucher를 생성할 수 있도록 비즈니스 로직 수정

--- src/main/java/com/prgrms/springbootbasic/service/VoucherService.java
+++ src/main/java/com/prgrms/springbootbasic/service/VoucherService.java
@@ -1,21 +1,29 @@
 package com.prgrms.springbootbasic.service;
 
+import com.prgrms.springbootbasic.domain.FixedDiscountVoucher;
+import com.prgrms.springbootbasic.domain.PercentDiscountVoucher;
 import com.prgrms.springbootbasic.domain.Voucher;
 import com.prgrms.springbootbasic.repository.VoucherRepository;
-
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 public class VoucherService {
     private final VoucherRepository voucherRepository;
-
     public VoucherService(VoucherRepository voucherRepository) {
         this.voucherRepository = voucherRepository;
     }
 
-    public Voucher createVoucher(Voucher voucher) {
+    public Voucher createFixedDiscountVoucher(long amount) {
+        Voucher voucher = new FixedDiscountVoucher(UUID.randomUUID(), amount);
         return voucherRepository.insert(voucher);
     }
+
+    public Voucher createPercentDiscountVoucher(double percent) {
+        Voucher voucher = new PercentDiscountVoucher(UUID.randomUUID(), (long) percent);
+        return voucherRepository.insert(voucher);
+    }
+
     public List<Voucher> fetchAllVouchers() {
         return new ArrayList<>(voucherRepository.getAllVouchersList());
     }
