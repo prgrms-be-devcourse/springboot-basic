45c5a9c70c239aff7aef9237d0480a097c072b6a
sujjangOvO <pnk7038@gmail.com>
2023-06-26 06:19:52 +0900

    feat : voucher service 구현

---
+++ src/main/java/com/prgms/voucher/voucherproject/service/VoucherService.java
@@ -0,0 +1,24 @@
+package com.prgms.voucher.voucherproject.service;
+
+import com.prgms.voucher.voucherproject.domain.Voucher;
+import com.prgms.voucher.voucherproject.repository.VoucherRepository;
+import org.springframework.stereotype.Service;
+
+import java.text.MessageFormat;
+import java.util.UUID;
+
+@Service
+public class VoucherService {
+    private final VoucherRepository voucherRepository;
+
+    public VoucherService(VoucherRepository voucherRepository) {
+        this.voucherRepository = voucherRepository;
+    }
+
+    public Voucher getVoucher(UUID voucherId) {
+        return voucherRepository
+                .findById(voucherId)
+                .orElseThrow(() -> new RuntimeException(MessageFormat.format("Can not find a voucher for {0}", voucherId)));
+    }
+
+}
