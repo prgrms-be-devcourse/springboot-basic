2fc2cff1425a0e4be44b59736a55ac450c96d07a
SW-H <hsw08@naver.com>
2023-06-26 08:38:09 +0900

    feat : Response 출력

--- src/main/java/co/programmers/voucher/view/Console.java
+++ src/main/java/co/programmers/voucher/view/Console.java
@@ -3,16 +3,21 @@ package co.programmers.voucher.view;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.text.MessageFormat;
+import java.util.List;
 
 import org.springframework.stereotype.Component;
+
+import co.programmers.voucher.dto.Response;
+import co.programmers.voucher.dto.VoucherInquiryResponseDTO;
 
 @Component
 public class Console implements InputView, OutputView {
 	private static final String STARTER_MESSAGE =
 			"=== Voucher Program ===\n"
-					+ "Type exit to exit the program.\n"
-					+ "Type create to create a new voucher.\n"
-					+ "Type list to list all vouchers.";
+					+ "1. Type exit or 'x' to exit  the program.\n"
+					+ "2. Type create or 'c' to create a new voucher.\n"
+					+ "3. Type list or 'l' to list all vouchers.";
 	private static final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
 	@Override
@@ -26,12 +31,47 @@ public class Console implements InputView, OutputView {
 	}
 
 	@Override
-	public void println(String content) {
+	public void println(Object content) {
 		System.out.println(content);
 	}
 
 	@Override
-	public void print(String content) {
+	public void print(Object content) {
 		System.out.print(content);
+	}
+
+	@Override
+	public void print(Response contents) {
+		if (contents.getState() == Response.State.FAILED) {
+			System.out.println(contents.getResponseData());
+			return;
+		}
+		if (contents.getResponseData() == null) {
+			return;
+		}
+		Object responseData = contents.getResponseData();
+		if (!(responseData instanceof List)) {
+			System.out.println(contents);
+			return;
+		}
+		for (var content : (List)responseData) {
+			if (content instanceof VoucherInquiryResponseDTO) {
+				System.out.println(MessageFormat.format("id : {0}", ((VoucherInquiryResponseDTO)content).getId()));
+				System.out.println(MessageFormat.format("name : {0}", ((VoucherInquiryResponseDTO)content).getName()));
+				System.out.println(MessageFormat.format("description : {0}",
+						((VoucherInquiryResponseDTO)content).getDescription()));
+				System.out.println(MessageFormat.format("discountType : {0}",
+						((VoucherInquiryResponseDTO)content).getDiscountType()));
+				System.out.println(MessageFormat.format("discountAmount : {0}",
+						((VoucherInquiryResponseDTO)content).getDiscountAmount()));
+				System.out.println(
+						MessageFormat.format("create At : {0}", ((VoucherInquiryResponseDTO)content).getCreatedAt()));
+				System.out.println(
+						MessageFormat.format("expire At : {0}", ((VoucherInquiryResponseDTO)content).getExpiredAt()));
+			} else {
+				System.out.println(content);
+			}
+		}
+
 	}
 }
