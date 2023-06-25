15990a6431fb73f3a39b18aaee2a4f91aafc2d93
박은지 <qkrdmswl1018@naver.com>
2023-06-25 19:48:05 +0900

    test:  테스트코드 수정 및 설명 추가

--- src/test/java/com/prgrms/commandLineApplication/voucher/FixedAmountVoucherTest.java
+++ src/test/java/com/prgrms/commandLineApplication/voucher/FixedAmountVoucherTest.java
@@ -6,26 +6,30 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
+import java.util.UUID;
+
 class FixedAmountVoucherTest {
 
   Voucher fixedAmountVoucher;
 
   @ParameterizedTest
   @CsvSource(value = {"30000|70000", "5000|95000", "10000|90000"}, delimiter = '|')
-  @DisplayName("FixedAmountVoucher 할인 계산 결과 일치 테스트")
-  void fixedAmountVoucher_계산_성공(double discountAmount, double result) {
+  @DisplayName("범위 내의 할인값을 입력했을 경우 Fixed Amount 계산 성공")
+  void fixedAmountVoucher_계산_성공(double discountAmount, double expected) {
     String voucherType = "fixed";
-    fixedAmountVoucher = new FixedAmountVoucher(voucherType, discountAmount);
-    assertThat(fixedAmountVoucher.discount(100000)).isEqualTo(result);
+    fixedAmountVoucher = new FixedAmountVoucher(UUID.randomUUID(), voucherType, discountAmount);
+    double result = fixedAmountVoucher.discount(100000);
+    assertThat(result).isEqualTo(expected);
   }
 
   @ParameterizedTest
   @CsvSource(value = {"30000|10, 50000|20, 10010|30"}, delimiter = '|')
-  @DisplayName("FixedAmountVoucher 할인 계산 결과 불일치 테스트")
-  void fixedAmountVoucher_계산_실패(double discountAmount, double result) {
+  @DisplayName("범위를 벗어난 할인값을 입력햇을 경우 Fixed Amount 계산 실패")
+  void fixedAmountVoucher_계산_실패(double discountAmount, double expected) {
     String voucherType = "fixed";
-    fixedAmountVoucher = new FixedAmountVoucher(voucherType, discountAmount);
-    assertThat(fixedAmountVoucher.discount(10000)).isEqualTo(result);
+    fixedAmountVoucher = new FixedAmountVoucher(UUID.randomUUID(), voucherType, discountAmount);
+    double result = fixedAmountVoucher.discount(100000);
+    assertThat(result).isEqualTo(expected);
   }
 
 }
