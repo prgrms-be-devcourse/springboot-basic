9149adc0d2cf79a536d8be7a7f1d4f52a5a868af
SW-H <hsw08@naver.com>
2023-06-25 11:38:46 +0900

    feat : main()

--- src/main/java/co/programmers/voucher/VoucherApplication.java
+++ src/main/java/co/programmers/voucher/VoucherApplication.java
@@ -1,13 +1,17 @@
 package co.programmers.voucher;
 
+import java.io.IOException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import co.programmers.voucher.Controller.VoucherApplicationRunner;
 
 @SpringBootApplication
 public class VoucherApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(VoucherApplication.class, args);
+	public static void main(String[] args) throws IOException {
+		ConfigurableApplicationContext applicationContext = SpringApplication.run(VoucherApplication.class, args);
+		applicationContext.getBean(VoucherApplicationRunner.class).run();
 	}
-
 }
