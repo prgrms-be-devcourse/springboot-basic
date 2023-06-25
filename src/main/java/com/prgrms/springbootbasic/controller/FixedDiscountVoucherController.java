98285c1d197505a50b3f05f1d4adf9394c11ae00
jay-so <merry9504@naver.com>
2023-06-25 20:56:05 +0900

    feat: FixedDiscountVoucher의 숫자 외 타입 입력할 시 오류, 생성 시 발생한 오류 로그 구현

--- src/main/java/com/prgrms/springbootbasic/controller/FixedDiscountVoucherController.java
+++ src/main/java/com/prgrms/springbootbasic/controller/FixedDiscountVoucherController.java
@@ -4,6 +4,7 @@ import com.prgrms.springbootbasic.domain.Voucher;
 import com.prgrms.springbootbasic.io.Input;
 import com.prgrms.springbootbasic.io.Output;
 import com.prgrms.springbootbasic.service.VoucherService;
+import static com.prgrms.springbootbasic.Console.logger;
 
 public class FixedDiscountVoucherController {
     private final Input input;
@@ -18,16 +19,18 @@ public class FixedDiscountVoucherController {
 
     public void createFixedDiscountVoucher() {
         try {
-            String amountStr = input.readString("FixedAmountVoucher의 Discount를 입력해주세요.(할인 금액만 입력, ₩표시 제외)");
+            String amountStr = input.readString("생성할 FixedDiscountVoucher의 Discount를 입력해주세요.(할인 금액만 입력, ₩표시 제외)");
             long amount = Long.parseLong(amountStr);
             Voucher voucher = voucherService.createFixedDiscountVoucher(amount);
 
-            output.println("새로운 FixedAmountVoucher가 생성되었습니다.");
-            output.println("생성된 FixedVoucher: " + voucher.getDiscount() + "입니다.");
+            output.println("새로운 FixedDiscountVoucher가 생성되었습니다.");
+            output.println("생성된 FixedDiscountVoucher는: " + voucher.getDiscount() + "입니다.");
         } catch (NumberFormatException e) {
             output.println("유효하지 않은 입력입니다. 숫자로 입력해주세요.");
+            logger.error("FixedDiscountVoucher 입력 타입 오류");
         } catch (Exception e) {
-            output.println("FixedAmountVoucher 생성 중 오류가 발생했습니다.");
+            output.println("FixedDiscountVoucher 생성 중 오류가 발생했습니다.");
+            logger.error("FixedDiscountVoucher 생성 오류");
         }
     }
 }
