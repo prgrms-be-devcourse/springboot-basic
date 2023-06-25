15c39beaacd677dc3db35f9dbb5078d1245994c0
jay-so <merry9504@naver.com>
2023-06-25 17:38:08 +0900

    feat: 입력한 명령어에 따른 voucher 생성(create),목록 불러오기(list) 메소드 구현

--- src/main/java/com/prgrms/springbootbasic/Console.java
+++ src/main/java/com/prgrms/springbootbasic/Console.java
@@ -2,13 +2,16 @@ package com.prgrms.springbootbasic;
 
 import com.prgrms.springbootbasic.controller.FixedDiscountVoucherController;
 import com.prgrms.springbootbasic.controller.PercentDiscountVoucherController;
+import com.prgrms.springbootbasic.domain.FixedDiscountVoucher;
+import com.prgrms.springbootbasic.domain.PercentDiscountVoucher;
+import com.prgrms.springbootbasic.domain.Voucher;
 import com.prgrms.springbootbasic.io.Input;
 import com.prgrms.springbootbasic.io.Output;
 import com.prgrms.springbootbasic.repository.VoucherRepository;
 import com.prgrms.springbootbasic.service.VoucherService;
 import org.beryx.textio.TextIO;
 import org.beryx.textio.TextTerminal;
-
+import java.util.List;
 
 public class Console<T extends TextTerminal>  implements Input, Output {
     private TextIO textIO;
@@ -60,13 +63,41 @@ public class Console<T extends TextTerminal>  implements Input, Output {
             }
         }
     }
-
-    private void list() {
-
+    private void create() {
+        String voucherType = readString("생성할 voucher의 종류를 입력해주세요.(FixedAmountVoucher, PercentDiscountVoucher)");
+        switch (voucherType) {
+            case "FixedAmountVoucher":
+                fixedDiscountVoucherController.createFixedDiscountVoucher();
+                break;
+            case "PercentDiscountVoucher":
+                percentDiscountVoucherController.createPercentDiscountVoucher();
+                break;
+            default:
+                println("입력가능한 Voucher Type은 고정 금액 할인과 퍼센트 금액 할인 타입 입니다.");
+                println("FixedAmountVoucher과 PercentDiscountVoucher 중 하나를 선택하여 생성해주세요.");
+                break;
+        }
     }
 
-    private void create() {
+    private void list() {
+        try {
+            List<Voucher> vouchers = voucherService.fetchAllVouchers();
 
+            if (vouchers.isEmpty()) {
+                println("생성된 Voucher가 없습니다.");
+            } else {
+                println("생성된 Voucher 목록입니다.");
+                for (Voucher voucher : vouchers) {
+                    if (voucher instanceof FixedDiscountVoucher) {
+                        println("FixedAmountVoucher: " + voucher.getDiscount());
+                    } else if (voucher instanceof PercentDiscountVoucher) {
+                        println("PercentDiscountVoucher: " + voucher.getDiscount());
+                    }
+                }
+            }
+        } catch (Exception e) {
+            println("Voucher 목록을 가져오는 중 오류가 발생했습니다.");
+        }
     }
 
     public void consoleMenu(){
@@ -75,5 +106,4 @@ public class Console<T extends TextTerminal>  implements Input, Output {
         terminal.println("Type create to create a new voucher.");
         terminal.println("Type list to list all vouchers.");
     }
-
 }
