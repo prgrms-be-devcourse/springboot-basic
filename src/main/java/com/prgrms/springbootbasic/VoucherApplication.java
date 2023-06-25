e37e5399a8752478c0ed910f8608c80b40e6ae0f
jay-so <merry9504@naver.com>
2023-06-25 19:25:11 +0900

    rename: SpringBootApplication-> VoucherApplication, SpringBootApplicationTest->VoucherApplicationTest로 클래스 명 변경

--- src/main/java/com/prgrms/springbootbasic/VoucherApplication.java
+++ src/main/java/com/prgrms/springbootbasic/VoucherApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootBasicApplication {
+public class VoucherApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringbootBasicApplication.class, args);
+        SpringApplication.run(VoucherApplication.class, args);
     }
-
 }
---
+++ src/test/java/com/prgrms/springbootbasic/VoucherApplicationTest.java
@@ -0,0 +1,7 @@
+package com.prgrms.springbootbasic;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoucherApplicationTest {
+}
