c5a2a3b301c742ba7d95f18dac70ed7093b5fbe6
bumwoo <gld55602@gmail.com>
2023-06-25 16:40:54 +0900

    feat: 프로그램 시작 시 명령어 출력

--- src/main/java/com/example/voucher/VoucherApplication.java
+++ src/main/java/com/example/voucher/VoucherApplication.java
@@ -1,13 +1,23 @@
 package com.example.voucher;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class VoucherApplication {
+public class VoucherApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(VoucherApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(VoucherApplication.class, args);
+    }
 
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("=== Voucher Program ===");
+        System.out.println("Type **exit** to exit the program.");
+        System.out.println("Type **create** to create a new voucher.");
+        System.out.println("Type **list** to list all vouchers.");
+
+        //TODO: 사용자 입력 및 명령어 처리 로직 구현
+    }
 }
