d0cd05b6d3584c15165285ca7221e446931b7381
changhyeon <changhyeon.h@kakao.com>
2023-06-25 20:01:09 +0900

    refactor: Validator 유틸 클래스 불변으로 변경

--- src/main/java/com/programmers/springweekly/util/Validator.java
+++ src/main/java/com/programmers/springweekly/util/Validator.java
@@ -1,19 +1,17 @@
 package com.programmers.springweekly.util;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class Validator {
+public final class Validator {
 
-    private static final Logger logger = LoggerFactory.getLogger(Validator.class);
-    private static final Pattern pattern = Pattern.compile("[0-9]+");
+    private static final Pattern pattern = Pattern.compile("\\d+");
     private static final String IS_NOT_A_NUMBER = "Is not a number";
     private static final String A_NUMBER_THAT_IS_OUT_OF_RANGE = "A number that is out of range";
     private static final int PERCENT_MAX = 100;
     private static final int PERCENT_MIN = 0;
+
+    private Validator(){}
 
     public static void fixedAmountValidate(String fixedAmount){
         isNumber(fixedAmount);
