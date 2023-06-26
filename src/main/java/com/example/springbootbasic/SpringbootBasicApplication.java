514ea3a0399feebe05cf5cf0bee6c993603704f5
Choi Seongwon <vlflalektzja@naver.com>
2023-06-27 00:59:09 +0900

    feat: 전체적인 main 메소드 흐름 작성

--- src/main/java/com/example/springbootbasic/SpringbootBasicApplication.java
+++ src/main/java/com/example/springbootbasic/SpringbootBasicApplication.java
@@ -1,14 +1,21 @@
 package com.example.springbootbasic;
 
+import com.example.springbootbasic.voucher.MemoryVoucherRepository;
+import com.example.springbootbasic.voucher.VoucherRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringbootBasicApplication {
+	private final static VoucherRepository voucherRepository = new MemoryVoucherRepository();
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootBasicApplication.class, args);
 		System.out.println("Hello Springboot");
+
+		Console console = new Console();
+		VoucherManager voucherManager = new CliVoucherManager(console, console, voucherRepository);
+		voucherManager.run();
 	}
 
 }
