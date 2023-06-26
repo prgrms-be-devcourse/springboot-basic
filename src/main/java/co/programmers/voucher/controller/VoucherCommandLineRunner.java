f865b3cce9824e0d3d8fb32298b218d9f8ea2e5e
SW-H <hsw08@naver.com>
2023-06-26 09:27:47 +0900

    feat : 메뉴에 따른 선택 로직 추가

--- src/main/java/co/programmers/voucher/controller/VoucherCommandLineRunner.java
+++ src/main/java/co/programmers/voucher/controller/VoucherCommandLineRunner.java
@@ -1,62 +1,81 @@
 package co.programmers.voucher.controller;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
+import java.text.MessageFormat;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
-import co.programmers.voucher.service.Launcher;
-import co.programmers.voucher.service.MenuLauncher;
+import co.programmers.voucher.dto.Response;
+import co.programmers.voucher.dto.VoucherCreationRequestDTO;
+import co.programmers.voucher.service.CreatingMenuService;
+import co.programmers.voucher.service.ListingMenuService;
 import co.programmers.voucher.view.InputView;
 import co.programmers.voucher.view.OutputView;
 
 @Controller
 public class VoucherCommandLineRunner implements VoucherApplicationRunner {
+	private final CreatingMenuService creatingMenuService;
+	private final ListingMenuService listingMenuService;
 	private final OutputView outputView;
 	private final InputView inputView;
 
 	@Autowired
-	public VoucherCommandLineRunner(OutputView outputView, InputView inputView) {
+	public VoucherCommandLineRunner(CreatingMenuService creatingMenuService, ListingMenuService listingMenuService,
+			OutputView outputView, InputView inputView) {
+		this.creatingMenuService = creatingMenuService;
+		this.listingMenuService = listingMenuService;
 		this.outputView = outputView;
 		this.inputView = inputView;
 	}
 
 	public void run() throws IOException {
 		String menu;
+		Response response;
 		do {
 			outputView.printGuideMessage();
 			menu = (String)inputView.input();
-			try {
-				Launcher launcher = MenuLauncher.from(menu);
-				Map<String, Object> demandedData = collectDemandedData(launcher.getRequestBody());
-				launcher.run(demandedData);
-			} catch (IllegalArgumentException illegalArgumentException) {
-				outputView.println(illegalArgumentException.getMessage());
+			switch (menu.toLowerCase()) {
+				case ("exit"):
+				case ("x"):
+					return;
+				case ("create"):
+				case ("c"):
+					VoucherCreationRequestDTO voucherCreationRequestDTO = request();
+					response = creatingMenuService.run(voucherCreationRequestDTO);
+					break;
+				case ("list"):
+				case ("l"):
+					response = listingMenuService.run(new VoucherCreationRequestDTO());
+					break;
+				default:
+					throw new IllegalArgumentException("* Invalid input for menu *");
 			}
+			outputView.print(response);
 		} while (!"EXIT".equalsIgnoreCase(menu));
 	}
 
-	Map<String, Object> collectDemandedData(Map<String, Object> requestBody) throws IOException {
-		Map<String, Object> responseBody = new HashMap<>();
-		for (var value : requestBody.entrySet()) {
-			String instructionMessage = makeMessage(value);
-			outputView.print(instructionMessage);
-			Object userData = inputView.input();
-			responseBody.put(value.getKey(), userData);
-		}
-		return responseBody;
-	}
+	VoucherCreationRequestDTO request() throws IOException {
+		String REQUEST_MESSAGE_FORMAT = "Input {0} >> ";
 
-	private String makeMessage(Map.Entry<String, Object> variableFactor) {
-		StringBuffer message = new StringBuffer();
-		String factorName = variableFactor.getKey();
-		String explanation = (String)variableFactor.getValue();
-		message.append("Input ");
-		message.append(factorName);
-		message.append("\n" + explanation);
-		message.append(">> ");
-		return message.toString();
+		outputView.print(MessageFormat.format(REQUEST_MESSAGE_FORMAT, "amount"));
+		int amount = Integer.parseInt((String)inputView.input());
+
+		outputView.print(MessageFormat.format(REQUEST_MESSAGE_FORMAT, "name"));
+		String name = (String)inputView.input();
+
+		outputView.print(MessageFormat.format(REQUEST_MESSAGE_FORMAT, "description"));
+		String description = (String)inputView.input();
+
+		outputView.print(MessageFormat.format(REQUEST_MESSAGE_FORMAT,
+				"discount type. Fixed(for FixedAmountVoucher) or Percent(for PercentDiscountVoucher)"));
+		String type = (String)inputView.input();
+
+		return VoucherCreationRequestDTO.builder()
+				.discountAmount(amount)
+				.name(name)
+				.description(description)
+				.discountStrategy(type)
+				.build();
 	}
 }
