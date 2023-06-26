fba2c7cefdf6b5e80cb04f88ad6d9ea53307ead9
jun <alap_u@naver.com>
2023-06-26 21:42:55 +0900

    feat 바우처의 정보를 입력받아 바우처를 생성한다.

--- src/main/java/com/dev/bootbasic/CommandLineApplication.java
+++ src/main/java/com/dev/bootbasic/CommandLineApplication.java
@@ -2,21 +2,40 @@ package com.dev.bootbasic;
 
 import com.dev.bootbasic.view.Command;
 import com.dev.bootbasic.view.ViewManager;
+import com.dev.bootbasic.voucher.controller.VoucherController;
+import com.dev.bootbasic.voucher.dto.VoucherCreateRequest;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.UUID;
 
 @Component
 public class CommandLineApplication implements CommandLineRunner {
 
     private final ViewManager viewManager;
+    private final VoucherController voucherController;
 
-    public CommandLineApplication(ViewManager viewManager) {
+    public CommandLineApplication(ViewManager viewManager, VoucherController voucherController) {
         this.viewManager = viewManager;
+        this.voucherController = voucherController;
     }
 
     @Override
     public void run(String... args) throws Exception {
         Command command = viewManager.readCommand();
+
+        switch (command) {
+            case CREATE -> createVoucher();
+            case LIST -> System.out.println("todo");
+            case EXIT -> System.out.println("todo");
+        }
+    }
+
+    private void createVoucher() throws IOException {
+        VoucherCreateRequest request = viewManager.readVoucherCreateInfo();
+        UUID voucher = voucherController.createVoucher(request);
+        viewManager.showMessage(voucher.toString());
     }
 
 }
--- src/main/java/com/dev/bootbasic/view/ViewManager.java
+++ src/main/java/com/dev/bootbasic/view/ViewManager.java
@@ -1,8 +1,10 @@
 package com.dev.bootbasic.view;
 
+import com.dev.bootbasic.voucher.dto.VoucherCreateRequest;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.UUID;
 
 @Component
 public class ViewManager {
@@ -11,6 +13,12 @@ public class ViewManager {
             "Type exit to exit the program.\n" +
             "Type create to create a new voucher.\n" +
             "Type list to list all vouchers.\n";
+    private static final String VOUCHER_TYPE_MESSAGE = "=== Voucher Type Choice ===\n" +
+            "Type fixed {Amount}\n" +
+            "Type percent {1 ~ 100}\n";
+    private static final int VOUCHER_TYPE_INDEX = 0;
+    private static final int VOUCHER_AMOUNT_INDEX = 1;
+    private static final String SPACE = " ";
     private final InputView inputView;
     private final OutputView outputView;
 
@@ -22,6 +30,21 @@ public class ViewManager {
     public Command readCommand() throws IOException {
         outputView.showMessage(INPUT_COMMAND_MESSAGE);
         return Command.from(inputView.inputLine());
+    }
+
+    public VoucherCreateRequest readVoucherCreateInfo() throws IOException {
+        outputView.showMessage(VOUCHER_TYPE_MESSAGE);
+
+        String voucher = inputView.inputLine();
+        String[] voucherInfo = voucher.split(SPACE);
+
+        String voucherType = voucherInfo[VOUCHER_TYPE_INDEX];
+        int amount = Integer.parseInt(voucherInfo[VOUCHER_AMOUNT_INDEX]);
+        return new VoucherCreateRequest(voucherType, amount);
+    }
+
+    public void showMessage(String message) {
+        outputView.showMessage(message);
     }
 
 }
