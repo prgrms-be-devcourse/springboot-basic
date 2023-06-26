1c617c6439d3c7b221a310a8c3f2c4a68b16c1ec
byeolhaha <j35635aa@gmail.com>
2023-06-26 17:30:03 +0900

    refactor : 할인 정책에 대한 단위 속성 추가

--- src/main/java/com/prgrms/model/voucher/VoucherPolicy.java
+++ src/main/java/com/prgrms/model/voucher/VoucherPolicy.java
@@ -4,17 +4,25 @@ import java.util.Arrays;
 import java.util.Optional;
 
 public enum VoucherPolicy {
-    FixedAmountVoucher("1", "고정 금액 할인에 따른 바우처", "얼만큼 할인 받고 싶은지 입력하세요 (단위: 원):"),
-    PercentDiscountVoucher("2", "할인율에 따른 바우처", "할인율을 입력하세요 (0~100 사이의 값):");
+    FixedAmountVoucher("1",
+            "고정 금액 할인에 따른 바우처",
+            "얼만큼 할인 받고 싶은지 입력하세요 (단위: 원):",
+            "원"),
+    PercentDiscountVoucher("2",
+            "할인율에 따른 바우처",
+            "할인율을 입력하세요 (0~100 사이의 값):",
+            "%");
 
     private final String number;
     private final String policy;
     private final String discountGuide;
+    private final String unit;
 
-    VoucherPolicy(String number, String policy, String discountGuide) {
+    VoucherPolicy(String number, String policy, String discountGuide, String unit) {
         this.number = number;
         this.policy = policy;
         this.discountGuide = discountGuide;
+        this.unit = unit;
     }
 
     public static Optional<VoucherPolicy> findBySelectedPolicy(String selectedPolicy) {
@@ -29,5 +37,9 @@ public enum VoucherPolicy {
 
     public String discountGuide() {
         return discountGuide;
+    }
+
+    public String getUnit() {
+        return unit;
     }
 }
