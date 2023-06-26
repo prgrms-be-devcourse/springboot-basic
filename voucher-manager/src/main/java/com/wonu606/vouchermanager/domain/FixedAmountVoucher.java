3138fea00a76f555b3ad1801a9c4d3cb8fb840d6
wonu606 <wonu606@gmail.com>
2023-06-27 01:24:42 +0900

    fix: FixedAmountVoucher가 양수만 받도록 수정
    - FixedAmountVoucherTest 통과

--- voucher-manager/src/main/java/com/wonu606/vouchermanager/domain/FixedAmountVoucher.java
+++ voucher-manager/src/main/java/com/wonu606/vouchermanager/domain/FixedAmountVoucher.java
@@ -8,11 +8,22 @@ public class FixedAmountVoucher extends Voucher {
 
     public FixedAmountVoucher(UUID uuid, double fixedAmount) {
         super(uuid);
+        validateFixedAmount(fixedAmount);
         this.fixedAmount = fixedAmount;
     }
 
     @Override
     public double calculateDiscountedPrice(double originalPrice) {
         return Math.max(originalPrice - fixedAmount, 0);
+    }
+
+    private void validateFixedAmount(double fixedAmount) {
+        if (!isFixedAmountValid(fixedAmount)) {
+            throw new IllegalArgumentException("할인할 금액은 양수여야 합니다.");
+        }
+    }
+
+    private boolean isFixedAmountValid(double fixedAmount) {
+        return fixedAmount >= 0;
     }
 }
