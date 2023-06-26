65474250d56462dd179733d2ea8e1c83906aad18
Yebin Lee <lyb2325@gmail.com>
2023-06-26 14:29:45 +0900

    initial comit

---
+++ src/test/java/voucher/app/voucher/VoucherApplicationTests.java
@@ -0,0 +1,13 @@
+package voucher.app.voucher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoucherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
