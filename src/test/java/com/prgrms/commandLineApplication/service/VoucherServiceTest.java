548b7c72752081e1caca0d1e386914ca3b94c79f
박은지 <qkrdmswl1018@naver.com>
2023-06-26 15:09:38 +0900

    test: VoucherService 에외 발생 테스트코드 작성

---
+++ src/test/java/com/prgrms/commandLineApplication/service/VoucherServiceTest.java
@@ -0,0 +1,24 @@
+package com.prgrms.commandLineApplication.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class VoucherServiceTest {
+
+  VoucherService voucherService = new VoucherService();
+
+  @ParameterizedTest
+  @ValueSource(strings = {"fix", "pe", "hello", "test"})
+  @DisplayName("잘못된 타입을 입력했을 경우 예외 발생 성공")
+  void fixedAmountVoucher_할인값_예외_발생(String voucherType) {
+    int discountValue = 10;
+    Throwable exception = assertThrows(IllegalArgumentException.class, () -> {
+      voucherService.create(voucherType, discountValue);
+    });
+    assertEquals("Invalid Type", exception.getMessage());
+  }
+
+}
