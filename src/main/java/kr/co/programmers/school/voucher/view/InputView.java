a389f87d28498d9d90ba5c3696b4463b35567311
Jeong-w-o-o <irmnels@gmail.com>
2023-06-27 02:14:04 +0900

    Feat: 입력 및 출력 기능 구현

---
+++ src/main/java/kr/co/programmers/school/voucher/view/InputView.java
@@ -0,0 +1,13 @@
+package kr.co.programmers.school.voucher.view;
+
+import java.util.Scanner;
+
+public class InputView {
+    private static final Scanner SCANNER = new Scanner(System.in);
+
+    public String input() {
+        String input = SCANNER.nextLine();
+
+        return input;
+    }
+}
---
+++ src/main/java/kr/co/programmers/school/voucher/view/OutputView.java
@@ -0,0 +1,29 @@
+package kr.co.programmers.school.voucher.view;
+
+public class OutputView {
+    public void printCommand() {
+        System.out.println("=== Voucher Program ===");
+        System.out.println("1. 바우처 생성\n 2. 바우처 조회\n 3. 종료");
+        System.out.println("입력 : ");
+    }
+
+    public void printVoucherList() {
+
+    }
+
+    public void printVoucherType() {
+
+    }
+
+    public void printPercentDiscountVoucherMessage() {
+
+    }
+
+    public void printFixedDiscountVoucherMessage() {
+
+    }
+
+    public void printSuccessMessage() {
+        System.out.println("바우처를 생성하였습니다.");
+    }
+}
