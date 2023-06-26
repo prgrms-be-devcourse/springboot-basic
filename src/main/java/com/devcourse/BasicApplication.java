e38c90a651c7dc3151ee1b3ee9ebbfd24c2daf8a
Hejow <gmlwh124@naver.com>
2023-06-27 03:16:49 +0900

    init: project

---
+++ src/main/java/com/devcourse/BasicApplication.java
@@ -0,0 +1,11 @@
+package com.devcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BasicApplication.class, args);
+    }
+}
