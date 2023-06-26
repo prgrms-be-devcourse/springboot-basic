b77b8d09f2a74088e64c8a42c87e8c24155432e8
JHS <wnghdtjr129@naver.com>
2023-06-26 20:14:36 +0900

    fix: percent 음수 검증

--- src/main/java/org/devcourse/voucher/domain/voucher/Money.java
+++ src/main/java/org/devcourse/voucher/domain/voucher/Money.java
@@ -14,16 +14,16 @@ public record Money(int amount) {
 
     public Money minus(Money discountAmount) {
         int calculatedResultAmount = this.amount - discountAmount.amount;
-
         if (calculatedResultAmount < 0) {
             return ZERO_AMOUNT;
         }
+
         return new Money(calculatedResultAmount);
     }
 
     public Money percent(int percentAmount) {
         int calculatedResultAmount = this.amount * percentAmount / 100;
 
-        return new Money(calculatedResultAmount);
+        return Money.of(calculatedResultAmount);
     }
 }
