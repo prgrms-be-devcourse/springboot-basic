247d059cbc6f46fb3834e34e2a9d227d9e3b3430
kmebin <blowhuiee@gmail.com>
2023-06-26 17:37:25 +0900

    chore: 불필요한 줄바꿈 제거

--- src/main/java/com/programmers/voucher/constant/ErrorMessage.java
+++ src/main/java/com/programmers/voucher/constant/ErrorMessage.java
@@ -4,7 +4,6 @@ public class ErrorMessage {
     public static final String INVALID_COMMAND = "존재하지 않는 명령어입니다.";
     public static final String INVALID_DISCOUNT_AMOUNT = "할인 값은 양수만 가능합니다.";
     public static final String INVALID_DISCOUNT_PERCENT = "할인율은 100을 넘을 수 없습니다.";
-
-
+    
     private ErrorMessage() {}
 }
