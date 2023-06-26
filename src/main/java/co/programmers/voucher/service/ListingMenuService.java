e10adfa5fdf25921c827e3c06fb99916ddfaa143
SW-H <hsw08@naver.com>
2023-06-26 09:24:46 +0900

    feat : 바우처 관련 메소드 매개변수 변경(dto), getInstance 삭제

--- src/main/java/co/programmers/voucher/service/ListingMenuService.java
+++ src/main/java/co/programmers/voucher/service/ListingMenuService.java
@@ -1,42 +1,30 @@
 package co.programmers.voucher.service;
 
-import java.io.IOException;
-import java.util.List;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import co.programmers.voucher.dto.Response;
+import co.programmers.voucher.dto.VoucherCreationRequestDTO;
 import co.programmers.voucher.repository.VoucherRepository;
-import co.programmers.voucher.view.OutputView;
 
 @Service
 public class ListingMenuService implements Launcher {
-	private static final ListingMenuService LISTING_MENU_LAUNCHER = new ListingMenuService();
 	private static VoucherRepository REPOSITORY;
-	private static OutputView outputView;
 
 	private ListingMenuService() {
 	}
 
 	@Autowired
-	private ListingMenuService(VoucherRepository REPOSITORY, OutputView outputView) {
+	private ListingMenuService(VoucherRepository REPOSITORY) {
 		ListingMenuService.REPOSITORY = REPOSITORY;
-		ListingMenuService.outputView = outputView;
-	}
-
-	public static Launcher getInstance() {
-		return LISTING_MENU_LAUNCHER;
 	}
 
 	@Override
-	public void run(Map<String, Object> requestBody) throws IOException {
-		List<Map<String, Object>> inquiredResult = REPOSITORY.inquire();
-		outputView.println(inquiredResult.toString());
+	public Response run(VoucherCreationRequestDTO voucherCreationRequestDTO) {
+		return Response.builder()
+				.state(Response.State.SUCCESS)
+				.responseData(REPOSITORY.findAll())
+				.build();
 	}
 
-	@Override
-	public Map<String, Object> getRequestBody() {
-		return Map.of();
-	}
 }
