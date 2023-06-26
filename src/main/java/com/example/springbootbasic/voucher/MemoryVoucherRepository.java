d3d250992378ca9bfc3f2ecd1db60fc03fb902c2
Choi Seongwon <vlflalektzja@naver.com>
2023-06-27 00:58:33 +0900

    feat: 메모리에 저장하는 바우처 저장소 구현체 생성

---
+++ src/main/java/com/example/springbootbasic/voucher/MemoryVoucherRepository.java
@@ -0,0 +1,15 @@
+package com.example.springbootbasic.voucher;
+
+import java.util.List;
+
+public class MemoryVoucherRepository implements VoucherRepository {
+    @Override
+    public void save(Voucher voucher) {
+
+    }
+
+    @Override
+    public List<Voucher> findAll() {
+        return null;
+    }
+}
