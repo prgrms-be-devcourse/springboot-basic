e90cb789bbdc31463b620baf516634923f815ad5
byeolhaha <j35635aa@gmail.com>
2023-06-26 18:04:27 +0900

    refactor : 누락을 방지하기 위해서 @NoArgsConstructor에 access레벨을 protected로 설정 및 기본생성자를 막아두어 @AllArgsConstructor 어노테이션 추가

--- src/main/java/com/prgrms/model/dto/VoucherResponse.java
+++ src/main/java/com/prgrms/model/dto/VoucherResponse.java
@@ -4,12 +4,14 @@ import com.prgrms.model.voucher.Discount;
 import com.prgrms.model.voucher.Voucher;
 import com.prgrms.model.voucher.VoucherPolicy;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
 
 @Getter
 @Builder
+@AllArgsConstructor
 public class VoucherResponse {
     private VoucherPolicy voucherPolicy;
     private Discount discount;
