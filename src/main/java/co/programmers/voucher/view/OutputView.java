4f4a2a612b6915b9b250738f31328d8ef2211ef3
SW-H <hsw08@naver.com>
2023-06-26 09:25:37 +0900

    feat : 바우처 관련 메소드 매개변수 변경(dto)

--- src/main/java/co/programmers/voucher/view/OutputView.java
+++ src/main/java/co/programmers/voucher/view/OutputView.java
@@ -2,10 +2,14 @@ package co.programmers.voucher.view;
 
 import java.io.IOException;
 
+import co.programmers.voucher.dto.Response;
+
 public interface OutputView {
 	void printGuideMessage() throws IOException;
 
-	void println(String content) throws IOException;
+	void println(Object content) throws IOException;
 
-	void print(String content) throws IOException;
+	void print(Object content) throws IOException;
+
+	void print(Response contents);
 }
