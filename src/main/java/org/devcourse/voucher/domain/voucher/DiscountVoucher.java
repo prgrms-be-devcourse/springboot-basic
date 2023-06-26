27175bfc96d8ef4e1bb7fc78bf76d35fa4eb31db
JHS <wnghdtjr129@naver.com>
2023-06-26 20:13:27 +0900

    feat: VoucherAmount제거

--- src/main/java/org/devcourse/voucher/domain/voucher/DiscountVoucher.java
+++ src/main/java/org/devcourse/voucher/domain/voucher/DiscountVoucher.java
@@ -4,13 +4,25 @@ import org.devcourse.voucher.domain.voucher.amount.VoucherAmount;
 
 public class DiscountVoucher extends Voucher{
 
-    private final VoucherAmount amount;
+    private final int amount;
     private final DiscountPolicy discountPolicy;
 
     protected DiscountVoucher(long id, VoucherType type, int amount, DiscountPolicy discountPolicy) {
         super(id, type);
-        this.amount = VoucherAmount.of(type, amount);
+        validateAmount(type, amount);
+        this.amount = amount;
         this.discountPolicy = discountPolicy;
+    }
+
+    private void validateAmount(VoucherType type, int amount) {
+        boolean incorrectAmountRange = switch (type) {
+            case PERCENT -> amount < 0 || amount > 100_000;
+            case FIX -> amount < 0 || amount > 100;
+        };
+
+        if (incorrectAmountRange) {
+            throw new RuntimeException("바우처 금액 범위 오류");
+        }
     }
 
     @Override
