9494104083af074a477b5d0a059610709dc06b8e
박은지 <qkrdmswl1018@naver.com>
2023-06-26 10:48:15 +0900

    refactor: 검증 로직 분리

--- src/main/java/com/prgrms/commandLineApplication/validation/VoucherValidation.java
+++ src/main/java/com/prgrms/commandLineApplication/validation/VoucherValidation.java
@@ -1,2 +1,32 @@
-package com.prgrms.commandLineApplication.validation;public class VoucherValidation {
+package com.prgrms.commandLineApplication.validation;
+
+import java.util.UUID;
+
+public class VoucherValidation {
+  private static final int MINIMUM_VALUE = 0;
+  private static final int PERCENT_RATE_BASE = 100;
+
+  public static void checkId(UUID voucherId) {
+    if (voucherId == null) {
+      throw new IllegalArgumentException("Invalid ID");
+    }
+  }
+
+  public static void checkFixedDiscountAmount(double discountAmount) {
+    if (discountAmount < MINIMUM_VALUE) {
+      throw new IllegalArgumentException("Invalid discount amount range (0 ~ )");
+    }
+  }
+
+  public static void checkPercentDiscountAmount(double discountAmount) {
+    if (PERCENT_RATE_BASE < discountAmount || discountAmount < MINIMUM_VALUE) {
+      throw new IllegalArgumentException("Invalid discount amount range (0 ~ 100)");
+    }
+  }
+
+  public static void checkType(String voucherType) {
+    if (!voucherType.equals("fixed") || !voucherType.equals("percent")) {
+      throw new IllegalArgumentException("Invalid Type");
+    }
+  }
 }
