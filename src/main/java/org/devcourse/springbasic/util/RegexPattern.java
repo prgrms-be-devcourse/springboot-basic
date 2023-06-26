0ac69e47118e907fc165b980220b9700dade0ce6
seongju <lsj40413@gmail.com>
2023-06-27 07:20:07 +0900

    refactor: private 생성자를 통한 util클래스 인스턴스화 방지

--- src/main/java/org/devcourse/springbasic/util/RegexPattern.java
+++ src/main/java/org/devcourse/springbasic/util/RegexPattern.java
@@ -4,6 +4,8 @@ import java.util.regex.Pattern;
 
 public class RegexPattern {
 
+    private RegexPattern() {}
+
     public static final Pattern NOT_INPUT_MENU_PATTERN = Pattern.compile("^[ㄱ-ㅎㅏ-ㅣ가-힣a-zA-Z0-9]");
     public static final Pattern VOUCHER_MENU_PATTERN = Pattern.compile("[0-9]");
 }
