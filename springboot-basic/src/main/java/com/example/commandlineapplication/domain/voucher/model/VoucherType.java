dab8d72dc52cb4ac220ea2de7aa3b6bfe4819f07
baejunil <bjo6300@naver.com>
2023-06-27 00:19:45 +0900

    refactor : voucherType 검사하는 함수 분리

--- springboot-basic/src/main/java/com/example/commandlineapplication/domain/voucher/model/VoucherType.java
+++ springboot-basic/src/main/java/com/example/commandlineapplication/domain/voucher/model/VoucherType.java
@@ -6,12 +6,16 @@ public enum VoucherType {
 
   FIXED,
   PERCENT;
-  
+
   public static VoucherType of(String inputVoucherType) {
     return Arrays.stream(values())
-        .filter(voucher -> voucher.getLowerCaseVoucherType().equals(inputVoucherType))
+        .filter(voucher -> voucher.isEquals(inputVoucherType, voucher))
         .findFirst()
         .orElseThrow(() -> new IllegalArgumentException("잘못된 바우처 타입입니다."));
+  }
+
+  private boolean isEquals(String inputVoucherType, VoucherType voucher) {
+    return voucher.getLowerCaseVoucherType().equals(inputVoucherType);
   }
 
   public String getLowerCaseVoucherType() {
