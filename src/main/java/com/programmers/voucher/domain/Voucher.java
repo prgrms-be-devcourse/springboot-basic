8fc7e5062418634a6db213cef9fd11ab98a8ace9
weonest <geonhee33@naver.com>
2023-06-26 17:44:55 +0900

    feat: 바우처 아이디 null 검증 추가

--- src/main/java/com/programmers/voucher/domain/Voucher.java
+++ src/main/java/com/programmers/voucher/domain/Voucher.java
@@ -1,5 +1,7 @@
 package com.programmers.voucher.domain;
 
+import com.programmers.global.exception.NotFoundException;
+
 import java.time.LocalDateTime;
 import java.util.UUID;
 
@@ -9,7 +11,10 @@ public class Voucher {
     private final Discount discount;
     private final LocalDateTime createdDate;
 
+    private static final String VOUCHER_NULL_MESSAGE = "[ERROR] Voucher ID를 찾을 수 없습니다.";
+
     public Voucher(UUID voucherId, Discount discount, LocalDateTime createdDate) {
+        validateVoucherId(voucherId);
         this.voucherId = voucherId;
         this.discount = discount;
         this.createdDate = createdDate;
@@ -29,5 +34,9 @@ public class Voucher {
 
     public LocalDateTime getCreatedDate() {
         return createdDate;
+    }
+
+    private void validateVoucherId(UUID voucherId) {
+        if (voucherId == null) throw new NotFoundException(VOUCHER_NULL_MESSAGE);
     }
 }
