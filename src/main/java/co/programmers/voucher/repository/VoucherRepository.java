f37ba25e2c0d06093b10150405192fc317dd0781
SW-H <hsw08@naver.com>
2023-06-26 09:28:08 +0900

    feat : 전체 조회

--- src/main/java/co/programmers/voucher/repository/VoucherRepository.java
+++ src/main/java/co/programmers/voucher/repository/VoucherRepository.java
@@ -1,13 +1,13 @@
 package co.programmers.voucher.repository;
 
 import java.util.List;
-import java.util.Map;
 
+import co.programmers.voucher.dto.VoucherInquiryResponseDTO;
 import co.programmers.voucher.entity.Voucher;
 
 public interface VoucherRepository {
 
 	void save(Voucher voucher);
 
-	List<Map<String, Object>> inquire();
+	List<VoucherInquiryResponseDTO> findAll();
 }
