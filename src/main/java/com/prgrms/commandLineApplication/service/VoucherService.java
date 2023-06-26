5892b1313e455c33b3b2a54fd4deef42d875360c
박은지 <qkrdmswl1018@naver.com>
2023-06-26 15:09:00 +0900

    feat: VoucherService 구현

--- src/main/java/com/prgrms/commandLineApplication/service/VoucherService.java
+++ src/main/java/com/prgrms/commandLineApplication/service/VoucherService.java
@@ -1,2 +1,29 @@
-package com.prgrms.commandLineApplication.service;public class VoucherService {
+package com.prgrms.commandLineApplication.service;
+
+import com.prgrms.commandLineApplication.repository.VoucherRepository;
+import com.prgrms.commandLineApplication.voucher.Voucher;
+import com.prgrms.commandLineApplication.voucher.VoucherFactory;
+
+import java.util.Map;
+import java.util.UUID;
+
+public class VoucherService {
+
+  private final VoucherRepository voucherRepository;
+  private final VoucherFactory voucherFactory;
+
+  public VoucherService(VoucherRepository voucherRepository, VoucherFactory voucherFactory) {
+    this.voucherRepository = voucherRepository;
+    this.voucherFactory = voucherFactory;
+  }
+
+  public Map<UUID, Voucher> viewList() {
+    return voucherRepository.findAll();
+  }
+
+  public void create(String voucherType, int discountAmount) {
+    Voucher createdVoucher = voucherFactory.createVoucher(voucherType, discountAmount);
+    voucherRepository.save(createdVoucher);
+  }
+
 }
