fb9209f4263687dc87e77df7db594e59f84d8dba
seongju <lsj40413@gmail.com>
2023-06-27 06:00:11 +0900

    refactor: 스프링 의존성 제거 및 simple factory 적용
    기존에 모든 클래스들이 스프링에 의존적이었다는 피드백을 반영.
    Bean을 굳이 등록하지 않아도 되는 객체들에 대한 의존성을 제거.
    simple factory를 적용하여 객체간 결합도를 낮춤.

--- src/main/java/org/devcourse/springbasic/repository/FileVoucherRepository.java
+++ src/main/java/org/devcourse/springbasic/repository/FileVoucherRepository.java
@@ -1,8 +1,8 @@
-package org.devcourse.springbasic.voucher.repository;
+package org.devcourse.springbasic.repository;
 
-import org.devcourse.springbasic.voucher.FixedAmountVoucher;
-import org.devcourse.springbasic.voucher.PercentDiscountVoucher;
+import org.devcourse.springbasic.io.ErrorMsgPrinter;
 import org.devcourse.springbasic.voucher.Voucher;
+import org.devcourse.springbasic.voucher.VoucherType;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Repository;
@@ -20,75 +20,53 @@ public class FileVoucherRepository implements VoucherRepository {
 
     @Override
     public Optional<Voucher> findById(UUID voucherId) {
-
         return findAll().stream().filter(voucher -> voucher.getVoucherId() == voucherId)
                 .findAny();
-
     }
 
     @Override
     public List<Voucher> findAll() {
 
-        try {
-
-            FileReader fileReader = new FileReader(FILE_PATH);
-            BufferedReader reader = new BufferedReader(fileReader);
+        try (FileReader fileReader = new FileReader(FILE_PATH);
+             BufferedReader reader = new BufferedReader(fileReader)){
 
             String data = "";
             while ((data = reader.readLine()) != null) {
-
-                // voucher 종류, voucher ID, voucher 할인율
                 String[] fileData = data.split(",");
                 Voucher voucher = fileDataToVoucher(fileData);
                 vouchers.add(voucher);
-
             }
 
             return vouchers;
 
         } catch (IOException e) {
-            System.out.println(e.getMessage());
+            ErrorMsgPrinter.print(e.getMessage());
         }
-
         return new ArrayList<>();
     }
 
     @Override
-    public UUID create(Voucher voucher) {
+    public UUID save(Voucher voucher) {
 
         try {
-
             FileWriter fileWriter = new FileWriter(FILE_PATH, true);
             BufferedWriter writer = new BufferedWriter(fileWriter);
-
             writer.write(voucher.toString());
             writer.newLine();
-
             writer.flush();
             writer.close();
 
         } catch (IOException e) {
-            System.out.println(e.getMessage());
-
+            ErrorMsgPrinter.print(e.getMessage());
         }
-
         return voucher.getVoucherId();
     }
 
     private Voucher fileDataToVoucher(String[] fileData) {
-
-        Voucher newVoucher = null;
-        String voucherType = fileData[0];
+        VoucherType voucherType = VoucherType.valueOf(fileData[0]);
         UUID voucherId = UUID.fromString(fileData[1]);
-        long voucherDiscountPolicy = Long.parseLong(fileData[2]);
+        long discountRate = Long.parseLong(fileData[2]);
 
-        if (voucherType.equals(FixedAmountVoucher.class.getSimpleName())) {
-            newVoucher = new FixedAmountVoucher(voucherId, voucherDiscountPolicy);
-
-        } else if (voucherType.equals(PercentDiscountVoucher.class.getSimpleName())) {
-            newVoucher = new PercentDiscountVoucher(voucherId, voucherDiscountPolicy);
-        }
-
-        return newVoucher;
+        return voucherType.getFunctionToSelectVoucher().apply(voucherId, discountRate);
     }
 }
