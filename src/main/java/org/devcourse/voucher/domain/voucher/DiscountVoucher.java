8c196342530aa4a5969fc03a605fb288da228cdc
JHS <wnghdtjr129@naver.com>
2023-06-26 20:16:01 +0900

    refactor: 정책 선택 팩토리 메소드 -> voucher 이동

--- src/main/java/org/devcourse/voucher/domain/voucher/DiscountVoucher.java
+++ src/main/java/org/devcourse/voucher/domain/voucher/DiscountVoucher.java
@@ -1,28 +1,33 @@
 package org.devcourse.voucher.domain.voucher;
 
-import org.devcourse.voucher.domain.voucher.amount.VoucherAmount;
-
-public class DiscountVoucher extends Voucher{
+public class DiscountVoucher extends Voucher {
 
     private final int amount;
     private final DiscountPolicy discountPolicy;
 
-    protected DiscountVoucher(long id, VoucherType type, int amount, DiscountPolicy discountPolicy) {
+    protected DiscountVoucher(long id, VoucherType type, int amount) {
         super(id, type);
         validateAmount(type, amount);
         this.amount = amount;
-        this.discountPolicy = discountPolicy;
+        this.discountPolicy = selectPolicy(type);
     }
 
     private void validateAmount(VoucherType type, int amount) {
         boolean incorrectAmountRange = switch (type) {
-            case PERCENT -> amount < 0 || amount > 100_000;
-            case FIX -> amount < 0 || amount > 100;
+            case PERCENT -> amount < 0 || amount > 100;
+            case FIX -> amount < 0 || amount > 100_000;
         };
 
         if (incorrectAmountRange) {
             throw new RuntimeException("바우처 금액 범위 오류");
         }
+    }
+
+    private DiscountPolicy selectPolicy(VoucherType type) {
+        return switch (type) {
+            case PERCENT -> originPrice -> originPrice.minus(originPrice.percent(amount));
+            case FIX -> originPrice -> originPrice.minus(Money.of(amount));
+        };
     }
 
     @Override
--- src/main/java/org/devcourse/voucher/domain/voucher/VoucherFactory.java
+++ src/main/java/org/devcourse/voucher/domain/voucher/VoucherFactory.java
@@ -2,20 +2,9 @@ package org.devcourse.voucher.domain.voucher;
 
 public class VoucherFactory {
 
-    public static Voucher of(VoucherType type, int amount) {
+    public static Voucher discountVoucher(long id, VoucherType type, int amount) {
         validateType(type);
-        return switch (type) {
-            case PERCENT, FIX -> discountVoucher(0L, type, amount);
-            default -> throw new RuntimeException("사용 불가능한 타입입니다");
-        };
-    }
-
-    private static Voucher discountVoucher(long id, VoucherType type, int amount) {
-        return switch (type) {
-            case PERCENT -> new DiscountVoucher(id, type, amount, originPrice -> originPrice.minus(originPrice.percent(amount)));
-            case FIX -> new DiscountVoucher(id, type, amount, originPrice -> originPrice.minus(Money.of(amount)));
-            default -> throw new RuntimeException("사용 불가능한 타입입니다");
-        };
+        return new DiscountVoucher(id, type, amount);
     }
 
     private static void validateType(VoucherType type) {
