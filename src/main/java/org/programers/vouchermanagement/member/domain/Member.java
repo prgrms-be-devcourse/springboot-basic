336ca3e12519245bbfe70bb0ae25a330a6761df8
player-geun <player_geun@naver.com>
2023-06-26 13:59:15 +0900

    refactor : 빈으로 관리될 필요가 없는 클래스 수정

--- src/main/java/org/programers/vouchermanagement/member/domain/Member.java
+++ src/main/java/org/programers/vouchermanagement/member/domain/Member.java
@@ -1,18 +1,13 @@
 package org.programers.vouchermanagement.member.domain;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
 import java.util.UUID;
 
-@Component
 public class Member {
 
     private final UUID id;
 
     private MemberStatus status;
 
-    @Autowired
     public Member() {
         this(UUID.randomUUID(), MemberStatus.NORMAL);
     }
