b3790bcc31b5cd65e856506fcb7e217e1461aba4
Continue <joje1003@gmail.com>
2023-06-26 16:36:58 +0900

    feat logback 관련 설정추가

--- src/main/java/org/prgrms/application/Command.java
+++ src/main/java/org/prgrms/application/Command.java
@@ -8,8 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
-import java.util.Optional;
-
 @Component
 public class Command implements CommandLineRunner {
 
