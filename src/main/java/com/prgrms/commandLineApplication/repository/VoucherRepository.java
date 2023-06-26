2073f00c42fdbbd8e33d55d39f63779133317507
박은지 <qkrdmswl1018@naver.com>
2023-06-26 15:11:20 +0900

    feat: VoucherRepository 구현

--- src/main/java/com/prgrms/commandLineApplication/repository/VoucherRepository.java
+++ src/main/java/com/prgrms/commandLineApplication/repository/VoucherRepository.java
@@ -1,2 +1,14 @@
-package com.prgrms.commandLineApplication.repository;public interface VoucherRepository {
+package com.prgrms.commandLineApplication.repository;
+
+import com.prgrms.commandLineApplication.voucher.Voucher;
+
+import java.util.Map;
+import java.util.UUID;
+
+public interface VoucherRepository {
+
+  void save(Voucher voucher);
+
+  Map<UUID, Voucher> findAll();
+
 }
