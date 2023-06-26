5fbe1d6e602523d35a7a032fd759dab54f7bd633
byeolhaha <j35635aa@gmail.com>
2023-06-26 18:09:24 +0900

    refactor : controller가 repository에 의존하던 것을 service에서 의존하도록 리스트 출력 메서드 추가

--- src/main/java/com/prgrms/service/voucher/VoucherService.java
+++ src/main/java/com/prgrms/service/voucher/VoucherService.java
@@ -1,17 +1,16 @@
 package com.prgrms.service.voucher;
 
 import com.prgrms.model.dto.VoucherResponse;
-import com.prgrms.model.voucher.FixedAmountVoucher;
-import com.prgrms.model.voucher.PercentDiscountVoucher;
-import com.prgrms.model.voucher.Voucher;
-import com.prgrms.model.voucher.VoucherPolicy;
+import com.prgrms.model.voucher.*;
 import com.prgrms.repository.voucher.VoucherRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.security.DigestInputStream;
 import java.text.MessageFormat;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -24,17 +23,23 @@ public class VoucherService {
                 .orElseThrow(() -> new RuntimeException(MessageFormat.format("can not find a voucher for {0}", voucherId)));
     }
 
-    public Voucher createVoucher(VoucherPolicy voucherPolicy, long discount) {
+    public Voucher createVoucher(VoucherPolicy voucherPolicy, Discount discount) {
         Voucher voucher = null;
         UUID id = UUID.randomUUID();
 
         switch (voucherPolicy) {
-            case FixedAmountVoucher -> voucher = new FixedAmountVoucher(id, discount, voucherPolicy.name());
-            case PercentDiscountVoucher -> voucher = new PercentDiscountVoucher(id, discount, voucherPolicy.name());
+            case FixedAmountVoucher -> voucher = new FixedAmountVoucher(id, discount, voucherPolicy);
+            case PercentDiscountVoucher -> voucher = new PercentDiscountVoucher(id, discount, voucherPolicy);
         }
 
         voucherRepository.insert(voucher);
         return voucher;
+    }
+
+    public List<VoucherResponse> getAllVoucherList() {
+        return voucherRepository.getAllVoucherList().stream()
+                .map(VoucherResponse::of)
+                .collect(Collectors.toList());
     }
 
     public boolean isEmptyRepository(List<VoucherResponse> voucherList) {
