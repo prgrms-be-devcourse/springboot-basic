cb8790f29f3c1e68e9ede776496f91afe65e8f69
SW-H <hsw08@naver.com>
2023-06-26 09:26:52 +0900

    refactor : 패키지명 수정

--- src/main/java/co/programmers/voucher/VoucherApplication.java
+++ src/main/java/co/programmers/voucher/VoucherApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
-import co.programmers.voucher.Controller.VoucherApplicationRunner;
+import co.programmers.voucher.controller.VoucherApplicationRunner;
 
 @SpringBootApplication
 public class VoucherApplication {
