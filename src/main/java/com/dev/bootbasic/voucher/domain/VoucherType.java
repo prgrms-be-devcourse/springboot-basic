b27b8d8e778f781fa3d57d65b3ff5b6978141587
jun <alap_u@naver.com>
2023-06-26 16:42:54 +0900

    refactor 예외 메시지에 사용자의 값을 같이 출력한다.

--- src/main/java/com/dev/bootbasic/voucher/domain/VoucherType.java
+++ src/main/java/com/dev/bootbasic/voucher/domain/VoucherType.java
@@ -6,12 +6,13 @@ public enum VoucherType {
     PERCENT;
 
     private static final String INVALID_VOUCHER_TYPE_MESSAGE = "생성할 수 없는 바우처 타입입니다.";
+    private static final String INVALID_VALUE_MESSAGE = " Invalid value: ";
 
     public static VoucherType from(String name) {
         try {
             return valueOf(name.toUpperCase());
         } catch (Exception e) {
-            throw new IllegalArgumentException(INVALID_VOUCHER_TYPE_MESSAGE);
+            throw new IllegalArgumentException(INVALID_VOUCHER_TYPE_MESSAGE + INVALID_VALUE_MESSAGE + name);
         }
     }
 
