437aa1dd1941bcd627f1488838818d7c4c462dbb
kimihiqq <kimihiqq@gmail.com>
2023-06-27 14:18:05 +0900

    refactor: VoucherControllerImpl 내 중복된 코드 제거

--- src/main/java/me/kimihiqq/vouchermanagement/controller/VoucherControllerImpl.java
+++ src/main/java/me/kimihiqq/vouchermanagement/controller/VoucherControllerImpl.java
@@ -51,29 +51,4 @@ public class VoucherControllerImpl implements VoucherController {
             }
         }
     }
-
-    private void handleInput(String input) throws IOException {
-        log.info("Received input: " + input);
-        if (input.equalsIgnoreCase("create")) {
-            String type = console.readLine("Enter voucher type (fixed or percent): ");
-            long discount = console.readDiscount("Enter discount amount or rate: ");
-            if (!type.equalsIgnoreCase("fixed") && !type.equalsIgnoreCase("percent")) {
-                log.error("Invalid voucher type: " + type);
-                return;
-            }
-            log.info("Creating voucher with type: " + type + ", and discount: " + discount);
-            VoucherDto voucherDto = new VoucherDto(type, discount);
-            Voucher voucher = voucherService.createVoucher(voucherDto);
-            console.printLine(voucher.getVoucherId() + ": " + voucher.getType() + " - " + voucher.getDiscount());
-            log.info("Created voucher with ID: " + voucher.getVoucherId());
-        } else if (input.equalsIgnoreCase("list")) {
-            log.info("Listing all vouchers");
-            voucherService.listVouchers().forEach(voucher ->
-                    console.printLine(voucher.getVoucherId() + ": " + voucher.getType() + " - " + voucher.getDiscount())
-            );
-        } else {
-            log.error("Invalid input: " + input);
-            console.printLine("잘못된 명령입니다. 다시 시도하세요.");
-        }
-    }
 }
