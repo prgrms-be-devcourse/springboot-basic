bafa39e9fe6a4be00d47be27b95b0c029ce410ce
Jeong-w-o-o <irmnels@gmail.com>
2023-06-28 04:46:47 +0900

    Feat: VoucherRequest 입력값 검증 추가

--- src/main/java/kr/co/programmers/school/voucher/domain/voucher/dto/VoucherRequest.java
+++ src/main/java/kr/co/programmers/school/voucher/domain/voucher/dto/VoucherRequest.java
@@ -1,14 +1,36 @@
 package kr.co.programmers.school.voucher.domain.voucher.dto;
 
 import kr.co.programmers.school.voucher.domain.voucher.enums.VoucherType;
+import kr.co.programmers.school.voucher.domain.voucher.exception.ApplicationException;
+import kr.co.programmers.school.voucher.domain.voucher.exception.ErrorMessage;
+
+import java.util.regex.Pattern;
 
 public class VoucherRequest {
+    private static final Pattern PERCENT_INPUT_RANGE_PATTERN = Pattern.compile("^[1-9][0-9]?$|^100$");
+    private static final Pattern FIXED_INPUT_PATTERN = Pattern.compile("^[1-9]\\d*$");
+
     private final int amount;
     private final VoucherType voucherType;
 
     public VoucherRequest(int amount, VoucherType voucherType) {
         this.amount = amount;
         this.voucherType = voucherType;
+    }
+
+    public static VoucherRequest of(String amount, VoucherType voucherType) {
+        if (!validate(amount, voucherType)) {
+            throw new ApplicationException(ErrorMessage.WRONG_RANGE);
+        }
+        return new VoucherRequest(Integer.parseInt(amount), voucherType);
+    }
+
+    private static boolean validate(String amount, VoucherType voucherType) {
+        if (voucherType.isPercentAmount()) {
+            return PERCENT_INPUT_RANGE_PATTERN.matcher(amount).matches();
+        }
+
+        return FIXED_INPUT_PATTERN.matcher(amount).matches();
     }
 
     public VoucherType getVoucherType() {
@@ -19,7 +41,7 @@ public class VoucherRequest {
         return this.amount;
     }
 
-    public static VoucherRequest of(int amount, VoucherType voucherType) {
-        return new VoucherRequest(amount, voucherType);
+    public String toString() {
+        return this.voucherType + " " + this.amount;
     }
 }
