c61dd7164b08b53801e6c2aba69fbd5f62cade6a
sujjangOvO <pnk7038@gmail.com>
2023-06-28 03:34:38 +0900

    feat : VoucherApp 구현

--- src/main/java/com/prgms/voucher/voucherproject/VoucherApp.java
+++ src/main/java/com/prgms/voucher/voucherproject/VoucherApp.java
@@ -1,23 +1,60 @@
 package com.prgms.voucher.voucherproject;
 
-import com.prgms.voucher.voucherproject.config.AppConfiguration;
+import com.prgms.voucher.voucherproject.domain.VoucherType;
 import com.prgms.voucher.voucherproject.io.Console;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import com.prgms.voucher.voucherproject.repository.MemoryVoucherRepository;
+import com.prgms.voucher.voucherproject.repository.VoucherRepository;
+import com.prgms.voucher.voucherproject.service.VoucherService;
 
 public class VoucherApp implements Runnable{
-
     private final Console console = new Console();
+    private final VoucherService voucherService;
+    private static VoucherRepository voucherRepository;
+
+    public VoucherApp(VoucherService voucherService) {
+        this.voucherService = voucherService;
+        this.voucherRepository = voucherRepository;
+    }
+
 
     public static void main(String[] args) {
-        var applicationContext = new AnnotationConfigApplicationContext(AppConfiguration.class);
-        applicationContext.register(AppConfiguration.class);
-
-        VoucherApp voucherApp = new VoucherApp();
-        voucherApp.run();
+        voucherRepository = new MemoryVoucherRepository();
+        new VoucherApp(new VoucherService(voucherRepository)).run();
     }
 
     @Override
     public void run() {
-        console.printMenu();
+        while(true){
+            console.printMenu();
+            switch (console.inputCommand().toLowerCase()){
+                case "exit":
+                    return;
+
+                case "create":
+                    console.printKindOfVoucher();
+                    int selectedNum = console.inputSelectedVoucherType();
+                    VoucherType voucherType = VoucherType.getSelectedVoucherType(selectedNum);
+                    if(voucherType != null){
+                        try{
+                            voucherService.create(voucherType);
+                        }
+                        catch (Exception e){
+                            e.getMessage();
+                        }
+                    }
+                    else {
+                        console.printErrorMsg();
+                    }
+                    break;
+
+                case "list":
+                    voucherService.list();
+                    break;
+
+                default:
+                    console.printErrorMsg();
+            }
+        }
+
     }
 }
