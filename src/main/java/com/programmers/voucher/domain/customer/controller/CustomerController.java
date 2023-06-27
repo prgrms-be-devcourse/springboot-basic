aa52e29ec63b069245be27d4e61fe7bdfa8959cf
hseong3243 <hseong3243@gmail.com>
2023-06-28 00:18:33 +0900

    feat: 고객 조회 기능을 제공하는 CustomerController 추가

---
+++ src/main/java/com/programmers/voucher/domain/customer/controller/CustomerController.java
@@ -0,0 +1,20 @@
+package com.programmers.voucher.domain.customer.controller;
+
+import com.programmers.voucher.domain.customer.domain.Customer;
+import com.programmers.voucher.domain.customer.service.CustomerService;
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
+
+@Controller
+public class CustomerController {
+    private final CustomerService customerService;
+
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    public List<Customer> findCustomers() {
+        return customerService.findCustomers();
+    }
+}
