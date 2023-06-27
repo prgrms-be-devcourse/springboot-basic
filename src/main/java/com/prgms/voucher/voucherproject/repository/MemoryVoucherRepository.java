cb58d1ca9a95bd20681dc50eef059acd1d559f58
sujjangOvO <pnk7038@gmail.com>
2023-06-28 03:33:21 +0900

    feat : VoucherRepository 구현

--- src/main/java/com/prgms/voucher/voucherproject/repository/MemoryVoucherRepository.java
+++ src/main/java/com/prgms/voucher/voucherproject/repository/MemoryVoucherRepository.java
@@ -2,22 +2,25 @@ package com.prgms.voucher.voucherproject.repository;
 
 import com.prgms.voucher.voucherproject.domain.Voucher;
 
-import java.util.Map;
-import java.util.Optional;
-import java.util.UUID;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class MemoryVoucherRepository implements VoucherRepository{
-    private final Map<UUID, Voucher> storage = new ConcurrentHashMap<>(); // thread safe을 위해 ConcurrentHashMap 사용
+    private final Map<UUID, Voucher> storage = new ConcurrentHashMap<>();
+
     @Override
     public Optional<Voucher> findById(UUID voucherId) {
         return Optional.ofNullable(storage.get(voucherId)); // null일 경우 empty 반환
     }
 
     @Override
-    public Voucher insert(Voucher voucher) {
+    public void save(Voucher voucher) {
         storage.put(voucher.getVoucherId(), voucher);
-        return voucher;
     }
+
+    @Override
+    public ArrayList<Voucher> findAll() {
+        return new ArrayList<Voucher>(storage.values());
+     }
 
 }
--- src/main/java/com/prgms/voucher/voucherproject/repository/VoucherRepository.java
+++ src/main/java/com/prgms/voucher/voucherproject/repository/VoucherRepository.java
@@ -2,12 +2,15 @@ package com.prgms.voucher.voucherproject.repository;
 
 import com.prgms.voucher.voucherproject.domain.Voucher;
 
+import java.util.ArrayList;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface VoucherRepository {
     public Optional<Voucher> findById(UUID voucherId); // voucher가 존재하지 않을 수 있음을 Optional로
-    Voucher insert(Voucher voucher);
+    void save(Voucher voucher);
+
+    ArrayList<Voucher> findAll();
 
 
 }
