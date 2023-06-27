b247e573e1cb9dbab071bed4872c7825c4e0ecf8
KimMinheee <99alsgmlalsl@naver.com>
2023-06-27 18:26:05 +0900

    프로젝트 세팅

---
+++ src/main/java/org/programmers/VoucherManagement/VoucherManagement.java
@@ -0,0 +1,12 @@
+package org.programmers.VoucherManagement;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoucherManagement {
+	public static void main(String[] args) {
+		SpringApplication.run(VoucherManagement.class, args);
+	}
+}
