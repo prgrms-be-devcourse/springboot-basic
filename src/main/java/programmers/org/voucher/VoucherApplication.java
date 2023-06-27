6e412a818858dbcb6e1d4787f129f500d344b433
kys0411 <32180846@dankook.ac.kr>
2023-06-27 21:52:28 +0900

    refactor: main 로직 변경

--- src/main/java/programmers/org/voucher/VoucherApplication.java
+++ src/main/java/programmers/org/voucher/VoucherApplication.java
@@ -1,18 +1,15 @@
 package programmers.org.voucher;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import programmers.org.voucher.config.AppConfig;
 import programmers.org.voucher.controller.VoucherController;
 
 @SpringBootApplication
 public class VoucherApplication {
 
 	public static void main(String[] args) {
-		ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
-		VoucherController voucherController = applicationContext.getBean("voucherController", VoucherController.class);
-
-		voucherController.run();
+		ApplicationContext applicationContext = SpringApplication.run(VoucherApplication.class, args);
+		applicationContext.getBean(VoucherController.class).run();
 	}
 }
