86cf724d1ebc0a6bfa98c838105a6624e1cf8abb
jun <alap_u@naver.com>
2023-06-27 16:03:10 +0900

    style 예외 메시지 폼변경

--- src/main/java/com/dev/bootbasic/view/Command.java
+++ src/main/java/com/dev/bootbasic/view/Command.java
@@ -7,13 +7,13 @@ public enum Command {
     EXIT
     ;
 
-    private static final String INVALID_COMMAND_MESSAGE = "는(은) 실행할 수 없는 명령입니다.";
+    private static final String INVALID_COMMAND_MESSAGE = "%s 는(은) 실행할 수 없는 명령입니다.";
 
     public static Command from(String name) {
         try {
             return valueOf(name.toUpperCase());
         } catch (Exception e) {
-            throw new IllegalArgumentException(name + INVALID_COMMAND_MESSAGE);
+            throw new IllegalArgumentException(String.format(INVALID_COMMAND_MESSAGE, name));
         }
     }
 
