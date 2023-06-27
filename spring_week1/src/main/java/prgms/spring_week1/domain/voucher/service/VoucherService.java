a91bb17193004d98216c2ff37b5a8af5db378d7f
2jie0516 <2jie0516>
2023-06-27 14:44:03 +0900

    refactor : 바우처 타입 매칭 처리 로직 수정

--- spring_week1/src/main/java/prgms/spring_week1/domain/voucher/service/VoucherService.java
+++ spring_week1/src/main/java/prgms/spring_week1/domain/voucher/service/VoucherService.java
@@ -1,6 +1,5 @@
 package prgms.spring_week1.domain.voucher.service;
 
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import prgms.spring_week1.domain.voucher.model.Voucher;
 import prgms.spring_week1.domain.voucher.model.impl.FixedAmountVoucher;
@@ -8,7 +7,6 @@ import prgms.spring_week1.domain.voucher.model.impl.PercentDiscountVoucher;
 import prgms.spring_week1.domain.voucher.model.type.VoucherType;
 import prgms.spring_week1.domain.voucher.repository.VoucherRepository;
 import prgms.spring_week1.exception.NoSuchVoucherType;
-
 import java.util.Optional;
 import java.util.UUID;
 import java.util.stream.Stream;
@@ -22,25 +20,22 @@ public class VoucherService {
     }
 
     public VoucherType matchVoucherType(String inputSelectText) throws NoSuchVoucherType {
-        VoucherType selectedVoucherType = matchVoucherTypeFilter(inputSelectText);
+        VoucherType selectedVoucherType = makeVoucherTypeStream(inputSelectText);
 
         return selectedVoucherType;
     }
 
-    private VoucherType matchVoucherTypeFilter(String inputSelectText) {
-        Optional<VoucherType> selectedVoucherType = getMatchVoucherType(Stream.of(VoucherType.values()), inputSelectText);
-        if(selectedVoucherType.isEmpty()) {
+    private VoucherType makeVoucherTypeStream(String inputSelectText) {
+        Optional<VoucherType> matchedVoucherType = getMatchVoucherTypeFilter(Stream.of(VoucherType.values()), inputSelectText);
+        if(matchedVoucherType.isEmpty()) {
             throw new NoSuchVoucherType();
         }
-        return selectedVoucherType.get();
+        return matchedVoucherType.get();
     }
 
-    private Optional<VoucherType> getMatchVoucherType(Stream<VoucherType> voucherType, String inputSelectText) {
-        VoucherType matchedVoucherType = voucherType.filter(v -> v.getVoucherType().equalsIgnoreCase(inputSelectText))
-                                        .findFirst()
-                                        .orElseThrow(() -> new NoSuchVoucherType("해당 바우처 타입이 존재하지 않습니다."));
-
-        return Optional.ofNullable(matchedVoucherType);
+    private Optional<VoucherType> getMatchVoucherTypeFilter(Stream<VoucherType> voucherType, String inputSelectText) {
+        return voucherType.filter(v -> v.getVoucherType().equalsIgnoreCase(inputSelectText))
+                .findFirst();
     }
 
     public Voucher insertFixedAmountVoucher(Long discountAmount) {
