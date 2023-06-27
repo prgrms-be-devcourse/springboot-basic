a22c4bb95487854a568e500d479d6cf4bd66f945
Jeong-w-o-o <irmnels@gmail.com>
2023-06-28 04:47:03 +0900

    Feat: 에러 메시지 추가

--- src/main/java/kr/co/programmers/school/voucher/domain/voucher/exception/ErrorMessage.java
+++ src/main/java/kr/co/programmers/school/voucher/domain/voucher/exception/ErrorMessage.java
@@ -2,7 +2,8 @@ package kr.co.programmers.school.voucher.domain.voucher.exception;
 
 public enum ErrorMessage {
     NOT_FOUND_OPTION("[ERROR] 해당 명령이 존재하지 않습니다."),
-    NOT_FOUND_VOUCHER_TYPE("[ERROR] 해당 바우처 타입이 존재하지 않습니다.")
+    NOT_FOUND_VOUCHER_TYPE("[ERROR] 해당 바우처 타입이 존재하지 않습니다."),
+    WRONG_RANGE("[ERROR] 잘못된 입력값 범위입니다.")
     ;
 
     private final String message;
