867d3cad4f0eff90ebd269d50329d4d46c05529b
Na Yeongkyeong <102007066+na-yk@users.noreply.github.com>
2023-06-27 16:52:51 +0900

    refactor: commandInput 처리 추가

--- voucher/src/main/java/org/devcourse/voucher/console/CommandLineApplication.java
+++ voucher/src/main/java/org/devcourse/voucher/console/CommandLineApplication.java
@@ -1,16 +1,45 @@
 package org.devcourse.voucher.console;
 
-import org.devcourse.voucher.console.Console;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
+import static java.lang.System.exit;
+
 @Component
 public class CommandLineApplication implements ApplicationRunner {
+    public static final String CREATE_COMMAND_STRING = "create";
+    public static final String LIST_COMMAND_STRING = "list";
+    public static final String EXIT_COMMAND_STRING = "exit";
+
+    private final Console console = new Console();
+
+
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        Console console = new Console();
-        console.printMenu();
-        String option = console.getOption();
+        String commandInput;
+        while (true) {
+            console.printMenu();
+            commandInput = console.input();
+            execute(commandInput);
+        }
+    }
+
+    protected void execute(String command) throws IllegalArgumentException {
+        switch (command) {
+            case CREATE_COMMAND_STRING:
+                // 바우처 생성
+                System.out.println("Create Voucher");
+                break;
+            case LIST_COMMAND_STRING:
+                // 바우처 조회
+                System.out.println("List Voucher");
+                break;
+            case EXIT_COMMAND_STRING:
+                System.out.println("Terminate Application");
+                System.exit(0);
+            default:
+                throw new IllegalArgumentException("Unsupported command: " + command);
+        }
     }
 }
