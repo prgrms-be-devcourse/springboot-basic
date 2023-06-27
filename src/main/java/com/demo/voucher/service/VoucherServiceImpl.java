af21f76d60f78c69ddbdab7897be43f3df7ddd18
Yebin Lee <lyb2325@gmail.com>
2023-06-27 12:41:02 +0900

    feat: Voucher Service에서 Voucher Type에 따라 새로운 바우처 생성하고 Repository에 추가하는 로직 추가 - Voucher Type이 FIXED_AMOUNT인 경우 FixedAmountVoucher 구현체 생성 후 repository에 추가 - Voucher Type이 PERCENT_DISCOUNT인 경우 PercentDiscountVoucher 구현체 생성 후 repository에 추가

--- src/main/java/com/demo/voucher/service/VoucherServiceImpl.java
+++ src/main/java/com/demo/voucher/service/VoucherServiceImpl.java
@@ -1,6 +1,9 @@
 package com.demo.voucher.service;
 
+import com.demo.voucher.domain.FixedAmountVoucher;
+import com.demo.voucher.domain.PercentDiscountVoucher;
 import com.demo.voucher.domain.Voucher;
+import com.demo.voucher.domain.VoucherType;
 import com.demo.voucher.repository.VoucherRepository;
 import org.springframework.stereotype.Service;
 
@@ -10,6 +13,7 @@ import java.util.UUID;
 @Service
 public class VoucherServiceImpl implements VoucherService {
     private static final String NOT_FOUND_VOUCHER = "바우처를 찾을 수 없습니다.";
+    private static final String VOUCHER_NOT_CREATED_ERROR = "바우처가 생성되지 않았습니다.";
     private final VoucherRepository voucherRepository;
 
 
@@ -30,7 +34,17 @@ public class VoucherServiceImpl implements VoucherService {
     }
 
     @Override
-    public Voucher createVoucher(Voucher voucher) {
-        return voucherRepository.insert(voucher);
+    public Voucher createVoucher(VoucherType voucherType, String amount) {
+        UUID uuid = UUID.randomUUID();
+        int amountValue = Integer.parseInt(amount);
+        switch (voucherType) {
+            case FIXED_AMOUNT -> {
+                return voucherRepository.insert(new FixedAmountVoucher(uuid, amountValue));
+            }
+            case PERCENT_DISCOUNT -> {
+                return voucherRepository.insert(new PercentDiscountVoucher(uuid, amountValue));
+            }
+            default -> throw new RuntimeException(VOUCHER_NOT_CREATED_ERROR);
+        }
     }
 }
