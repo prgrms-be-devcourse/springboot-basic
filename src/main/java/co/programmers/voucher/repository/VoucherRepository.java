0ec1ac3894f3177da7f93f5df72e6b466d78a9da
SW-H <hsw08@naver.com>
2023-06-27 10:45:12 +0900

    refactor : 네이밍 변경

--- src/main/java/co/programmers/voucher/repository/VoucherRepository.java
+++ src/main/java/co/programmers/voucher/repository/VoucherRepository.java
@@ -2,12 +2,12 @@ package co.programmers.voucher.repository;
 
 import java.util.List;
 
-import co.programmers.voucher.dto.VoucherInquiryResponseDTO;
+import co.programmers.voucher.dto.VoucherResponseDTO;
 import co.programmers.voucher.entity.Voucher;
 
 public interface VoucherRepository {
 
 	void save(Voucher voucher);
 
-	List<VoucherInquiryResponseDTO> findAll();
+	List<VoucherResponseDTO> findAll();
 }
