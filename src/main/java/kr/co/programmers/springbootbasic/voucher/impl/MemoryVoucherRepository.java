91ad8ee63725006605548342df5844a52bfe93b0
ysng_ysng <yeasung67@gmail.com>
2023-06-27 15:10:28 +0900

    feat: findByVoucherId 구현
    UUID로 바우처 하나를 조회할 수 있는 findByVoucherId를 구현했습니다.

--- src/main/java/kr/co/programmers/springbootbasic/voucher/impl/MemoryVoucherRepository.java
+++ src/main/java/kr/co/programmers/springbootbasic/voucher/impl/MemoryVoucherRepository.java
@@ -4,25 +4,33 @@ import kr.co.programmers.springbootbasic.voucher.Voucher;
 import kr.co.programmers.springbootbasic.voucher.VoucherRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Repository;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.UUID;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Repository
+//@Primary
+//@Profile("dev")
 public class MemoryVoucherRepository implements VoucherRepository {
     private static final Logger logger = LoggerFactory.getLogger(MemoryVoucherRepository.class);
     private final Map<UUID, Voucher> repository;
 
     public MemoryVoucherRepository() {
-        this.repository = new HashMap<>();
+        this.repository = new ConcurrentHashMap<>();
+    }
+
+    @Override
+    public Optional<Voucher> findByVoucherId(UUID voucherId) {
+        logger.info("MemoryVoucherRepository에서 ID가 {}인 바우처를 조회합니다...", voucherId);
+        return Optional.ofNullable(repository.get(voucherId));
     }
 
     @Override
     public List<Voucher> listAll() {
-        logger.info("MemoryVoucherRepository에서 바우처 조회를 시작합니다...");
+        logger.info("MemoryVoucherRepository에서 모든 바우처를 조회합니다...");
         return repository.values()
                 .stream()
                 .toList();
