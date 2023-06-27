c0dbcccc5989bcaf91bf1941f828e0e702442a78
hongxeob <sjun2918@naver.com>
2023-06-27 12:52:02 +0900

    refactor: VoucherType에 Unchecked 예외를 Try Catch로 잡은 부분 수정

--- src/main/java/org/promgrammers/springbootbasic/domain/voucher/model/VoucherType.java
+++ src/main/java/org/promgrammers/springbootbasic/domain/voucher/model/VoucherType.java
@@ -7,26 +7,22 @@ import java.util.Arrays;
 
 public enum VoucherType {
 
-    FIXED("FIXED","1"),
-    PERCENT("PERCENT","2");
+    FIXED("FIXED", "1"),
+    PERCENT("PERCENT", "2");
 
     private final String type;
     private final String typeNumber;
     private static final Logger logger = LoggerFactory.getLogger(VoucherType.class);
 
-    VoucherType(String type,String typeNumber) {
+    VoucherType(String type, String typeNumber) {
         this.type = type;
         this.typeNumber = typeNumber;
     }
 
     public static VoucherType of(String voucherTypeNumber) {
-        try {
-            return Arrays.stream(values())
-                    .filter(voucher -> voucher.typeNumber.equals(voucherTypeNumber))
-                    .findFirst()
-                    .orElseThrow(() -> new IllegalArgumentException("잘못된 Voucher 타입입니다. => " + voucherTypeNumber));
-        } catch (IllegalArgumentException e) {
-            throw e;
-        }
+        return Arrays.stream(values())
+                .filter(voucher -> voucher.typeNumber.equals(voucherTypeNumber))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("잘못된 Voucher 타입입니다. => " + voucherTypeNumber));
     }
 }
