b7a938155ace4b764d3de6df61f346b4f893ae57
Yebin Lee <lyb2325@gmail.com>
2023-06-27 13:53:53 +0900

    test: FixedAmountVoucher, PercentDiscountVoucher 구현체 유닛 테스트

---
+++ src/test/java/com/demo/voucher/domain/FixedAmountVoucherTest.java
@@ -0,0 +1,47 @@
+package com.demo.voucher.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class FixedAmountVoucherTest {
+    // given
+    private static final Voucher fixedAmountVoucher = new FixedAmountVoucher(UUID.randomUUID(), 2000);
+
+    @Test
+    @DisplayName("Fixed Voucher이 voucherId 값을 정상적으로 가져오는지 확인하는 테스트")
+    void getVoucherId() {
+        // given
+        UUID uuid = UUID.randomUUID();
+        FixedAmountVoucher voucher = new FixedAmountVoucher(uuid, 2000);
+
+        // when
+        UUID voucherId = voucher.getVoucherId();
+
+        // then
+        assertEquals(voucherId, uuid);
+    }
+
+
+    @Test
+    @DisplayName("Fixed Voucher이 amount 값을 잘 가져오는지 확인하는 테스트")
+    void getAmount() {
+        assertEquals(2000, fixedAmountVoucher.getAmount());
+    }
+
+    @Test
+    @DisplayName("Fixed Voucher이 바우처 타입 설명을 제대로 리턴하는지 확인하는 테스트")
+    void getVoucherTypeDescription() {
+        assertEquals("고정 할인 바우처", fixedAmountVoucher.getVoucherTypeDescription());
+    }
+
+    @Test
+    @DisplayName("Fixed Voucher이 할인 단위와 함께 할인 amount를 잘 리턴하는지 확인하는 테스트")
+    void getDiscountInfo() {
+        assertEquals("2000원", fixedAmountVoucher.getDiscountInfo());
+
+    }
+}
