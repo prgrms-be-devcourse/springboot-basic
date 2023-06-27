93d3ffaa35c386060561051d958e19a80a86b778
heenahan <mby1997s@gmail.com>
2023-06-27 18:29:11 +0900

    refactor : VoucherApplication으로부터 파일 자원 가져와 Files로 읽고 쓰기

--- src/main/java/com/prgms/springbootbasic/model/VouchersInFile.java
+++ src/main/java/com/prgms/springbootbasic/model/VouchersInFile.java
@@ -1,51 +1,43 @@
 package com.prgms.springbootbasic.model;
 
+import com.prgms.springbootbasic.util.VoucherApplication;
 import com.prgms.springbootbasic.util.VoucherType;
-import org.springframework.context.annotation.Primary;
-import org.springframework.stereotype.Repository;
 
-import java.io.*;
-import java.util.ArrayList;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardOpenOption;
 import java.util.List;
 import java.util.UUID;
 
-@Repository
-@Primary
 public class VouchersInFile implements VouchersStorage {
 
-	private static File file = new File("/Users/user/github/springboot-basic/src/main/resources/data.csv");
-	
+	private final File file;
+
+	public VouchersInFile() throws IOException {
+		VoucherApplication voucherApplication = new VoucherApplication();
+		this.file = voucherApplication.getFile();
+	}
+
 	@Override
 	public void save(Voucher voucher) throws IOException {
-		BufferedWriter bw = new BufferedWriter(new FileWriter(file, true));
-		String voucherOfCsv = voucher.formatOfCSV();
-		bw.write(voucherOfCsv);
-		bw.newLine();
-		if (bw != null) {
-			bw.flush();
-			bw.close();
-		}
+		Files.write(file.toPath(), voucher.formatOfCSV(), StandardOpenOption.APPEND);
 	}
 	
 	@Override
 	public List<Voucher> findAll() throws IOException {
-		BufferedReader br = new BufferedReader(new FileReader(file));
-		String line = null;
-		List<Voucher> result = new ArrayList<>();
-		while ((line = br.readLine()) != null) {
-			String[] voucherOfCSV= line.split(" ");
-			Voucher voucher = changeToVoucher(voucherOfCSV);
-			result.add(voucher);
-		}
-		br.close();
-		return result;
+		return Files.readAllLines(file.toPath())
+										.stream()
+										.map(s -> changeToVoucher(s.split(",")))
+										.toList();
 	}
 	
 	private Voucher changeToVoucher(String[] voucherOfCSV) {
 		String type = voucherOfCSV[0];
 		UUID voucherId = UUID.fromString(voucherOfCSV[1]);
-		Long number = Long.parseLong(voucherOfCSV[2]);
+		long number = Long.parseLong(voucherOfCSV[2]);
 		VoucherType voucherType = VoucherType.of(type);
 		return voucherType.exsistVoucher(voucherId, number);
 	}
+
 }
