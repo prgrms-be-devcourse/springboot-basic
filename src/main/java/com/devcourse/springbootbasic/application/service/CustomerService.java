6235431bad68dbe2373ce0386bbd0f37d0d8d9e2
SY97P <onetuks@naver.com>
2023-06-27 23:41:19 +0900

    feat: 블랙고객 조회 기능 Customer domain으로 변경

---
+++ src/main/java/com/devcourse/springbootbasic/application/service/CustomerService.java
@@ -0,0 +1,20 @@
+package com.devcourse.springbootbasic.application.service;
+
+import com.devcourse.springbootbasic.application.repository.CustomerRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CustomerService {
+
+    private final CustomerRepository customerRepository;
+
+    public CustomerService(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    public List<String> getBlackCustomers() {
+        return customerRepository.findAllBlackCustomers();
+    }
+}
