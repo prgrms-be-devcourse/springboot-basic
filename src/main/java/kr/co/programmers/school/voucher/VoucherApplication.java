dc274e9d29a20061cd137f7b8eb5f9b058621d0e
Jeong-w-o-o <irmnels@gmail.com>
2023-06-28 00:53:07 +0900

    Feat: 할인 amount 검증 후 재입력 받기 추가

--- src/main/java/kr/co/programmers/school/voucher/VoucherApplication.java
+++ src/main/java/kr/co/programmers/school/voucher/VoucherApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class VoucherApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(VoucherApplication.class, args);
     }
-
 }
--- src/main/java/kr/co/programmers/school/voucher/domain/voucher/controller/VoucherController.java
+++ src/main/java/kr/co/programmers/school/voucher/domain/voucher/controller/VoucherController.java
@@ -1,20 +1,30 @@
 package kr.co.programmers.school.voucher.domain.voucher.controller;
 
+import kr.co.programmers.school.voucher.domain.voucher.dto.VoucherListResponse;
 import kr.co.programmers.school.voucher.domain.voucher.enums.Command;
 import kr.co.programmers.school.voucher.domain.voucher.dto.VoucherRequest;
 import kr.co.programmers.school.voucher.domain.voucher.enums.VoucherType;
 import kr.co.programmers.school.voucher.domain.voucher.service.VoucherService;
 import kr.co.programmers.school.voucher.view.InputView;
 import kr.co.programmers.school.voucher.view.OutputView;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Controller;
+
+import java.util.regex.Pattern;
 
 
 @Controller
 public class VoucherController implements CommandLineRunner {
+    private static final Logger LOGGER = LoggerFactory.getLogger(VoucherController.class);
+
     private final InputView inputView;
     private final OutputView outputView;
     private final VoucherService voucherService;
+
+    private static final Pattern PERCENT_INPUT_RANGE_PATTERN = Pattern.compile("^[1-9][0-9]?$|^100$");
+    private static final Pattern FIXED_INPUT_PATTERN = Pattern.compile("^[1-9]\\d*$");
 
     public VoucherController(InputView inputView, OutputView outputView, VoucherService voucherService) {
         this.inputView = inputView;
@@ -23,16 +33,20 @@ public class VoucherController implements CommandLineRunner {
     }
 
     @Override
-    public void run(String... args) throws Exception {
-        Command command;
-        do {
-            outputView.printCommand();
-            String input = inputView.input();
-            System.out.println();
-            command = Command.from(input);
-            processCommand(command);
+    public void run(String... args) {
+        try {
+            Command command;
+            do {
+                outputView.printCommand();
+                String input = inputView.input();
+                command = Command.from(input);
+                processCommand(command);
+            }
+            while (isContinue(command));
         }
-        while (isContinue(command));
+        catch (Exception e) {
+            LOGGER.error("[ERROR] : {}", e.getMessage());
+        }
     }
 
     private void processCommand(Command command) {
@@ -43,12 +57,15 @@ public class VoucherController implements CommandLineRunner {
             VoucherType voucherType = VoucherType.from(input);
             printVoucherMessage(voucherType);
 
-            int amount = inputView.inputInt();
-            VoucherRequest voucherRequest = VoucherRequest.of(amount, voucherType);
-
+            String amount = inputView.input();
+            amount = checkInputAmount(amount, voucherType);
+            VoucherRequest voucherRequest = VoucherRequest.of(Integer.parseInt(amount), voucherType);
             voucherService.createVoucher(voucherRequest);
+            outputView.printSuccessMessage();
         }
         else if (command.isInquiry()) {
+            VoucherListResponse voucherListResponse = voucherService.getAllVoucher();
+            outputView.printVoucherList(voucherListResponse);
         }
     }
 
@@ -56,7 +73,18 @@ public class VoucherController implements CommandLineRunner {
         if (command.isExit()) {
             return false;
         }
+
         return true;
+    }
+
+    private String checkInputAmount(String amount, VoucherType voucherType) {
+        while (!isAmountValid(amount, voucherType)) {
+            outputView.printInvalidRangeMessage();
+            printVoucherMessage(voucherType);
+            amount = inputView.input();
+        }
+
+        return amount;
     }
 
     private void printVoucherMessage(VoucherType voucherType) {
@@ -64,6 +92,15 @@ public class VoucherController implements CommandLineRunner {
             outputView.printFixedDiscountVoucherMessage();
             return;
         }
+
         outputView.printPercentDiscountVoucherMessage();
+    }
+
+    private boolean isAmountValid(String amount, VoucherType voucherType) {
+        if (voucherType.isPercentAmount()) {
+            return PERCENT_INPUT_RANGE_PATTERN.matcher(amount).matches();
+        }
+
+        return FIXED_INPUT_PATTERN.matcher(amount).matches();
     }
 }
