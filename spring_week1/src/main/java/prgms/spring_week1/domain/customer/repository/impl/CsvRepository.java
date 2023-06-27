b32eda327105d3817c6078056f7725c8f082be8c
2jie0516 <2jie0516>
2023-06-28 00:15:00 +0900

    feat : application.yaml 적용

--- spring_week1/src/main/java/prgms/spring_week1/domain/customer/repository/impl/CsvRepository.java
+++ spring_week1/src/main/java/prgms/spring_week1/domain/customer/repository/impl/CsvRepository.java
@@ -1,6 +1,9 @@
 package prgms.spring_week1.domain.customer.repository.impl;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Repository;
+import org.springframework.validation.annotation.Validated;
 import prgms.spring_week1.domain.customer.model.BlackConsumer;
 import prgms.spring_week1.domain.customer.repository.BlackListRepository;
 import java.io.*;
@@ -9,11 +12,16 @@ import java.util.List;
 
 @Repository
 public class CsvRepository implements BlackListRepository {
+    private final String CsvFilePath;
+
+    public CsvRepository(@Value("${file.blackList}") String csvFilePath) {
+        CsvFilePath = csvFilePath;
+    }
 
     @Override
     public List<BlackConsumer> getBlackConsumerList(){
         try {
-            BufferedReader br = new BufferedReader(new FileReader(new File("/home/osh/Downloads/java_week1/spring_week1/src/main/resources/BlackListCsv.csv")));
+            BufferedReader br = new BufferedReader(new FileReader(new File(CsvFilePath)));
             List<BlackConsumer> blackConsumerList = new ArrayList<>();
             String line = "";
             while((line = br.readLine()) != null){
