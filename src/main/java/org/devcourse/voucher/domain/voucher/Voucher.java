d1aa94b217f4dba6921907dff66675c49104753f
Juhongseok <wnghdtjr129@naver.com>
2023-06-27 16:49:30 +0900

    feat: 추상클래스 -> 단일 클래스 변경, 필드 변경

--- src/main/java/org/devcourse/voucher/domain/voucher/Voucher.java
+++ src/main/java/org/devcourse/voucher/domain/voucher/Voucher.java
@@ -1,12 +1,18 @@
 package org.devcourse.voucher.domain.voucher;
 
-public abstract class Voucher {
+import org.devcourse.voucher.domain.voucher.policy.Policy;
+
+public class Voucher {
     private final long id;
     private final VoucherType type;
+    private final Policy policy;
 
-    protected Voucher(long id, VoucherType type) {
+    public Voucher(long id, VoucherType type, Policy policy) {
+        validate(type, policy);
+        validateType(type);
         this.id = id;
         this.type = type;
+        this.policy = policy;
     }
 
     public long getId() {
@@ -17,7 +23,28 @@ public abstract class Voucher {
         return type;
     }
 
-    public abstract Money retrievePostBalance(Money money);
+    public Policy getPolicy() {
+        return policy;
+    }
 
-    public abstract int getProvidedAmount();
+    public int useVoucherAndRetrieve(int receivedAmount) {
+        return policy.execute(receivedAmount);
+    }
+
+    private void validate(VoucherType type, Policy policy) {
+        validateType(type);
+        validatePolicy(policy);
+    }
+
+    private void validateType(VoucherType type) {
+        if (type == null) {
+            throw new RuntimeException("바우처 타입은 빈 값일 수 없습니다");
+        }
+    }
+
+    private void validatePolicy(Policy policy) {
+        if (policy == null) {
+            throw new RuntimeException("바우처 정책은 빈 값일 수 없습니다");
+        }
+    }
 }
