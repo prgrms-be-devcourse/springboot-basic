9ee056e535657b735df6c1488a1c4e66050a7a58
JHS <wnghdtjr129@naver.com>
2023-06-27 22:27:46 +0900

    fix: 조건 반대 수정, static 메소드

--- src/main/java/org/devcourse/voucher/domain/voucher/policy/Policy.java
+++ src/main/java/org/devcourse/voucher/domain/voucher/policy/Policy.java
@@ -11,15 +11,15 @@ public abstract class Policy {
         this.providedAmount = providedAmount;
     }
 
-    public Policy of(VoucherType type, int providedAmount) {
+    public static Policy of(VoucherType type, int providedAmount) {
         validateType(type);
         return switch (type) {
-            case PERCENT -> new FixedDiscountPolicy(providedAmount);
-            case FIX -> new PercentDiscountPolicy(providedAmount);
+            case PERCENT -> new PercentDiscountPolicy(providedAmount);
+            case FIX -> new FixedDiscountPolicy(providedAmount);
         };
     }
 
-    private void validateType(VoucherType type) {
+    private static void validateType(VoucherType type) {
         if (type == null) {
             throw new RuntimeException("바우처 타입은 빈 값일 수 없습니다");
         }
