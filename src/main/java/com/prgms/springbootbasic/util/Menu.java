fdf04a6d14a1c8a5cc61ac417f7e607d88758577
heenahan <mby1997s@gmail.com>
2023-06-27 18:12:07 +0900

    feat : 메뉴마다 컨트롤러 생성하여 반환

--- src/main/java/com/prgms/springbootbasic/util/Menu.java
+++ src/main/java/com/prgms/springbootbasic/util/Menu.java
@@ -1,30 +1,41 @@
 package com.prgms.springbootbasic.util;
 
+import com.prgms.springbootbasic.controller.VoucherController;
+import com.prgms.springbootbasic.controller.ExitController;
+import com.prgms.springbootbasic.controller.VoucherCreateController;
+import com.prgms.springbootbasic.controller.VoucherListController;
 import com.prgms.springbootbasic.exception.NoSuchMenuException;
+import com.prgms.springbootbasic.ui.Console;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.function.Function;
 
 public enum Menu {
 	
-	EXIT("exit"),
-	CREATE("create"),
-	LIST("list");
+	EXIT("exit", ExitController::new),
+	CREATE("create", VoucherCreateController::new),
+	LIST("list", VoucherListController::new);
 	
 	private static final String ERROR_NO_SUCH_MENU = "[Error] Can't Find Such Menu";
-	private static final List<Menu> MENU = Arrays.stream(Menu.values()).collect(Collectors.toList());
+	private static final List<Menu> MENU_VALUES = Arrays.stream(Menu.values()).toList();
 	private final String command;
+	private final Function<Console, VoucherController> controllerFactory;
 	
-	Menu(String command) {
+	Menu(String command, Function<Console, VoucherController> controllerFactory) {
 		this.command = command;
+		this.controllerFactory = controllerFactory;
+	}
+
+	public VoucherController getCommandLineController(Console console) {
+		return controllerFactory.apply(console);
 	}
 	
 	public static Menu of(String command) {
-		return MENU.stream()
-			       .filter(m -> m.command.equals(command))
-			       .findFirst()
-			       .orElseThrow(() -> new NoSuchMenuException(ERROR_NO_SUCH_MENU));
+		return MENU_VALUES.stream()
+					.filter(m -> m.command.equals(command))
+					.findFirst()
+					.orElseThrow(() -> new NoSuchMenuException(ERROR_NO_SUCH_MENU));
 	}
 	
 }
