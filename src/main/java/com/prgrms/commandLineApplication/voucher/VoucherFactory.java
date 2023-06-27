ed9952d683ea4a3b886e252986e7b0bcefe31073
박은지 <qkrdmswl1018@naver.com>
2023-06-28 00:48:13 +0900

    refactor: VoucherType 구현

--- src/main/java/com/prgrms/commandLineApplication/voucher/VoucherFactory.java
+++ src/main/java/com/prgrms/commandLineApplication/voucher/VoucherFactory.java
@@ -1,16 +1,13 @@
 package com.prgrms.commandLineApplication.voucher;
 
-import com.prgrms.commandLineApplication.validation.VoucherValidation;
-
 import java.util.UUID;
 
 public class VoucherFactory {
 
   public static Voucher createVoucher(String voucherType, int discountAmount) {
-    VoucherValidation.checkType(voucherType);
-    return switch (voucherType) {
-      case "fixed" -> FixedAmountVoucher.of(UUID.randomUUID(), voucherType, discountAmount);
-      default -> PercentDiscountVoucher.of(UUID.randomUUID(), voucherType, discountAmount);
+    return switch (VoucherType.valueOfType(voucherType)) {
+      case FIXED -> FixedAmountVoucher.of(UUID.randomUUID(), voucherType, discountAmount);
+      case PERCENT -> PercentDiscountVoucher.of(UUID.randomUUID(), voucherType, discountAmount);
     };
   }
 
