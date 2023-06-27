a7b710da5f96a5a23c8c766ba1b90e00f5dc0e18
ysng_ysng <yeasung67@gmail.com>
2023-06-28 01:16:08 +0900

    chore: 사용하지 않는 파일 삭제
    사용하지 않는 파일을 삭제했습니다.

--- src/main/java/kr/co/programmers/springbootbasic/exception/FileRepoInitException.java
+++
@@ -1,7 +0,0 @@
-package kr.co.programmers.springbootbasic.exception;
-
-public class FileRepoInitException extends RuntimeException {
-    public FileRepoInitException(String message) {
-        super(message);
-    }
-}
