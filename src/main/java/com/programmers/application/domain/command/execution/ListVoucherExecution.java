b41cbbd0c0039ebf3efa2961877ebf9204081aed
yumyeonghan <mgtmh991013@gmail.com>
2023-06-27 15:43:25 +0900

    feat: ListVoucherExecution run() 구현
    1. run(): 조회된 바우처 정보들을 출력

--- src/main/java/com/programmers/application/domain/command/execution/ListVoucherExecution.java
+++ src/main/java/com/programmers/application/domain/command/execution/ListVoucherExecution.java
@@ -1,11 +1,24 @@
 package com.programmers.application.domain.command.execution;
 
+import com.programmers.application.dto.reponse.VoucherInfoResponse;
 import com.programmers.application.io.IO;
 import com.programmers.application.service.VoucherService;
 
-public class ListVoucherExecution implements VoucherExecution {
-    @Override
-    public void run(VoucherService voucherService, IO io) {
+import java.util.List;
 
+public class ListVoucherExecution implements VoucherExecution {
+    private final VoucherService voucherService;
+    private final IO io;
+
+    public ListVoucherExecution(VoucherService voucherService, IO io) {
+        this.voucherService = voucherService;
+        this.io = io;
+    }
+
+    @Override
+    public void run() {
+        List<VoucherInfoResponse> voucherList = voucherService.findVoucherList();
+        voucherList.stream()
+                .forEach(voucherInfoResponse -> io.write(voucherInfoResponse.toString()));
     }
 }
