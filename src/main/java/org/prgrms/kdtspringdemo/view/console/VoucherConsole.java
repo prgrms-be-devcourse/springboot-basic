1d3df3f926d6583f9d3b68d0bf1dd847364809fb
beombu <beombu13@gmail.com>
2023-06-29 07:45:31 +0900

    Refactor : 피드백 수정
    - String 블록 사용

--- src/main/java/org/prgrms/kdtspringdemo/view/console/VoucherConsole.java
+++ src/main/java/org/prgrms/kdtspringdemo/view/console/VoucherConsole.java
@@ -6,10 +6,12 @@ import org.prgrms.kdtspringdemo.voucher.constant.VoucherType;
 import org.prgrms.kdtspringdemo.voucher.model.dto.VoucherDto;
 
 public class VoucherConsole {
-    private static final String INIT_MESSAGE = "=== Voucher Program ===\n" +
-            "Type exit to exit the program.\n" +
-            "Type create to create a new voucher.\n" +
-            "Type list to list all vouchers.\n";
+    private static final String INIT_MESSAGE = """
+            === Voucher Program ===
+            Type exit to exit the program.
+            Type create to create a new voucher.
+            Type list to list all vouchers.
+            """;
     private static final String SYSTEM_SHUTDOWN_MESSAGE = "시스템을 종료합니다.\n";
     private static final String INVALID_COMMAND_MESSAGE = "잘못된 명령입니다.\n" +
             "다시 입력 부탁드립니다.\n";
@@ -17,6 +19,7 @@ public class VoucherConsole {
     private static final String FIXED_DISCOUNT_VOUCHER_MESSAGE = "할인 금액을 입력하세요.\n";
     private static final String SUCCESS_CREATED_FIXED_VOUCHER = "type : %s\ndiscount : %s 원\n";
     private static final String SUCCESS_CREATED_PERCENT_VOUCHER = "type : %s\ndiscount : %s 퍼센트\n";
+
     private final Input input;
     private final Output output;
 
