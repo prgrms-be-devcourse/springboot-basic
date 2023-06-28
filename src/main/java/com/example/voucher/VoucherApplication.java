d01410353e909740b1cce34f962e3188ab06ba5b
bumwoo <gld55602@gmail.com>
2023-06-28 13:57:36 +0900

    feat: VoucherApplication 로깅 추가

--- src/main/java/com/example/voucher/VoucherApplication.java
+++ src/main/java/com/example/voucher/VoucherApplication.java
@@ -1,5 +1,7 @@
 package com.example.voucher;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import com.example.voucher.ui.Output;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -7,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class VoucherApplication implements CommandLineRunner {
+    private static final Logger logger = LoggerFactory.getLogger(VoucherApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(VoucherApplication.class, args);
@@ -14,6 +17,10 @@ public class VoucherApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        Output.printProgramInfo();
+        try {
+            Output.printProgramInfo();
+        } catch (Exception e) {
+            logger.error("예외 발생: ", e);
+        }
     }
 }
