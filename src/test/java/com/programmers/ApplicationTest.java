80693dacfaf6fb0bbef80f07c3aa738e064abb3b
hyee0715 <hyee0715@naver.com>
2023-06-28 17:57:04 +0900

    test: ApplicationTest 애노테이션 제거

--- src/test/java/com/programmers/ApplicationTest.java
+++ src/test/java/com/programmers/ApplicationTest.java
@@ -1,15 +1,13 @@
-//package com.programmers;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.test.context.ActiveProfiles;
-//
-//@SpringBootTest
-//@ActiveProfiles("local")
-//class ApplicationTest {
-//
-//    @Test
-//    void contextLoads() {
-//    }
-//
-//}
+package com.programmers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("local")
+class ApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
