4b586f181f82f7ba1960b61f2c4ded8008754154
byeolhaha <j35635aa@gmail.com>
2023-06-28 17:51:46 +0900

    refactor : 확장성을 고려하고 예외메세지를 매개변수에서 제외하고 상수로 전환

--- src/main/java/com/prgrms/model/voucher/Discount.java
+++ src/main/java/com/prgrms/model/voucher/Discount.java
@@ -1,16 +1,17 @@
 package com.prgrms.model.voucher;
 
 public class Discount {
+    private final String negativeException = "할인에 대한 인자값은 양수여야 합니다.";
     private final long discount;
 
     public Discount(long discount) {
-        validDiscount(discount < 0, "할인에 대한 인자값은 양수여야 합니다.");
+        validDiscount(discount < 0);
         this.discount = discount;
     }
 
-    private void validDiscount (boolean expression, String exceptionMessage){
-        if(expression) {
-            throw new IllegalArgumentException(exceptionMessage);
+    private void validDiscount(boolean expression) {
+        if (expression) {
+            throw new IllegalArgumentException(negativeException);
         }
     }
 
