8676b34c29303739e33c9f16ca0d772646794911
SY97P <onetuks@naver.com>
2023-06-28 18:01:02 +0900

    test: DTO 테스트

---
+++ src/test/java/com/devcourse/springbootbasic/application/dto/ListMenuTest.java
@@ -0,0 +1,25 @@
+package com.devcourse.springbootbasic.application.dto;
+
+import com.devcourse.springbootbasic.application.exception.InvalidDataException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ListMenuTest {
+
+    @ParameterizedTest
+    @DisplayName("올바른 출력 메뉴 선택하면 성공")
+    @ValueSource(strings = {"1", "2"})
+    void 출력메뉴테스트(String input) {
+        assertDoesNotThrow(() -> ListMenu.getListMenu(input));
+    }
+
+    @ParameterizedTest
+    @DisplayName("부적절한 출력 메뉴 선택하면 실패")
+    @ValueSource(strings = {"0", "3", "12", "-1"})
+    void 출력메뉴예외테스트(String input) {
+        assertThrows(InvalidDataException.class, () -> ListMenu.getListMenu(input));
+    }
+}
