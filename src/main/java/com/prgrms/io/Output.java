9ba5d86741bf7fe36776c5ba4455c8e70d14dbd3
byeolhaha <j35635aa@gmail.com>
2023-06-28 17:59:29 +0900

    refactor : view에 대한 관할자 객체를 만들어 Output을 빈에 등록하고 역할이 단순 출력의 역할로 축소됨

--- src/main/java/com/prgrms/io/Output.java
+++ src/main/java/com/prgrms/io/Output.java
@@ -1,47 +1,10 @@
 package com.prgrms.io;
 
-import com.prgrms.model.dto.VoucherResponse;
-import com.prgrms.model.voucher.Voucher;
-import com.prgrms.model.voucher.VoucherPolicy;
+import org.springframework.stereotype.Component;
 
-import java.util.List;
-
+@Component
 public class Output {
-    public static void viewStartMessage() {
-        System.out.println(Message.START.getMessage());
-    }
-
-    public static void viewInputError() {
-        System.out.println(Message.ERROR.getMessage());
-    }
-
-    public static void viewEndMessage() {
-        System.out.println(Message.END.getMessage());
-    }
-
-    public static void viewVoucherOption() {
-        for (VoucherPolicy voucherPolicy : VoucherPolicy.values()) {
-            System.out.println(voucherPolicy.voucherPolicyOptionGuid());
-        }
-    }
-
-    public static void viewDiscountGuide(VoucherPolicy voucherPolicy) {
-        System.out.println(voucherPolicy.discountGuide());
-    }
-
-    public static void viewVoucherList(List<VoucherResponse> voucherList) {
-        for (VoucherResponse voucherResponse : voucherList) {
-            System.out.println(voucherResponse.getVoucherPolicy().name() + " : "
-                    + voucherResponse.getDiscount().getDiscount()
-                    + voucherResponse.getVoucherPolicy().getUnit());
-        }
-    }
-
-    public static void viewCompleteVoucher() {
-        System.out.println(Message.COMPLETE_CREATE.getMessage());
-    }
-
-    public static void viewEmptyRepository() {
-        System.out.println(Message.EMPTY_REPOSITORY.getMessage());
+    public void outputView(String message) {
+        System.out.println(message);
     }
 }
