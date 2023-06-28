8ffdd41079b703d0c382d2d06084625ba6c36b8e
SY97P <onetuks@naver.com>
2023-06-28 23:28:38 +0900

    test: FileVoucherRepository 테스트

--- src/test/java/com/devcourse/springbootbasic/application/repository/voucher/FileVoucherRepositoryTest.java
+++ src/test/java/com/devcourse/springbootbasic/application/repository/voucher/FileVoucherRepositoryTest.java
@@ -1,10 +1,31 @@
 package com.devcourse.springbootbasic.application.repository.voucher;
 
+import com.devcourse.springbootbasic.application.constant.YamlProperties;
+import com.devcourse.springbootbasic.application.domain.FixedAmountVoucher;
+import com.devcourse.springbootbasic.application.dto.VoucherType;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+import org.yaml.snakeyaml.Yaml;
+
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringJUnitConfig
 class FileVoucherRepositoryTest {
+
+    FileVoucherRepository fileVoucherRepository;
+    YamlProperties yamlProperties;
+
+    @BeforeEach
+    void init() {
+        yamlProperties = new YamlProperties();
+        fileVoucherRepository = new FileVoucherRepository(yamlProperties);
+    }
+
+    @Test
+    void test() {
+        assertNull(yamlProperties.getVoucherRecordPath());
+    }
 
 }
