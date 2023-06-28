c54b8eb7908717a4556a46a5d130e4722fb47cb4
KimMinheee <99alsgmlalsl@naver.com>
2023-06-28 17:06:08 +0900

    [Fix] : 기존 바우처 관련 예외 메시지 클래스명 변경

--- src/main/java/org/programmers/VoucherManagement/exception/VoucherExceptionMessage.java
+++ src/main/java/org/programmers/VoucherManagement/exception/VoucherExceptionMessage.java
@@ -1,6 +1,6 @@
 package org.programmers.VoucherManagement.exception;
 
-public enum ExceptionMessage {
+public enum VoucherExceptionMessage {
     NOT_INCLUDE_1_TO_100("할인율은 1부터 100사이의 값이여야 합니다."),
     VOUCHER_AMOUNT_IS_NOT_NUMBER("할인 금액은 숫자만 입력 가능합니다."),
     NOT_EXIST_COMMAND("해당하는 Command가 존재하지 않습니다."),
@@ -8,11 +8,11 @@ public enum ExceptionMessage {
 
     private final String message;
 
-    ExceptionMessage(String message) {
+    VoucherExceptionMessage(String message) {
         this.message = message;
     }
 
-    public String getMessage(){
+    public String getMessage() {
         return this.message;
     }
 }
