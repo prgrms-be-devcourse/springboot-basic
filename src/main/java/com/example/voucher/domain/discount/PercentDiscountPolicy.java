8675a4138c6fecc6534c64bd65dd4d3c29c1d2e9
bumwoo <gld55602@gmail.com>
2023-06-28 13:13:39 +0900

    refactor: 내부적으로 할인 적용된 금액을 저장하도록 PercentDiscountPolicy 수정

--- src/main/java/com/example/voucher/domain/discount/PercentDiscountPolicy.java
+++ src/main/java/com/example/voucher/domain/discount/PercentDiscountPolicy.java
@@ -3,7 +3,8 @@ package com.example.voucher.domain.discount;
 import com.example.voucher.domain.Voucher;
 
 public class PercentDiscountPolicy implements DiscountPolicy {
-    private double percent;
+    private final double percent;
+    private double discountedAmount;  // 할인 적용된 값을 저장하는 변수
 
     public PercentDiscountPolicy(double percent) {
         this.percent = percent;
@@ -12,5 +13,11 @@ public class PercentDiscountPolicy implements DiscountPolicy {
     @Override
     public void applyDiscount(Voucher voucher) {
         // 할인율 적용 로직 구현
+        double originalAmount = voucher.getAmount();  // 원래 금액 가져오기
+        discountedAmount = originalAmount * percent;  // 할인 적용된 금액 계산하여 저장
+    }
+
+    public double getDiscountedAmount() {
+        return discountedAmount;  // 할인 적용된 값을 반환하는 메서드
     }
 }
