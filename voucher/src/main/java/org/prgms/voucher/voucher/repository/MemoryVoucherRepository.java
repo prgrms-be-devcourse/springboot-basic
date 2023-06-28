2948d596d90e8949292147a05cf923cc009d5d3b
dingding-21 <phj8221@naver.com>
2023-06-28 20:06:29 +0900

    feat: MemoryVouhcerRepository 메서드 구현

--- voucher/src/main/java/org/prgms/voucher/voucher/repository/MemoryVoucherRepository.java
+++ voucher/src/main/java/org/prgms/voucher/voucher/repository/MemoryVoucherRepository.java
@@ -1,17 +1,25 @@
 package org.prgms.voucher.voucher.repository;
 
 import org.prgms.voucher.voucher.Voucher;
-import org.prgms.voucher.voucher.repository.VoucherRepository;
+import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import java.util.*;
 
+@Repository
 public class MemoryVoucherRepository implements VoucherRepository {
+    Map<UUID, Voucher> memoryStorage = new HashMap<>();
 
     @Override
     public Voucher save(Voucher voucher) {
+        memoryStorage.put(voucher.getVoucherId(), voucher);
+        return voucher;
     }
 
     @Override
     public List<Voucher> findAll() {
+        List<Voucher> vouchers = new ArrayList<>(memoryStorage.values());
+        vouchers.sort(Comparator.comparing(Voucher::getExpirationDate).reversed()
+                .thenComparing(Voucher::getCreatedAt));
+        return vouchers;
     }
 }
