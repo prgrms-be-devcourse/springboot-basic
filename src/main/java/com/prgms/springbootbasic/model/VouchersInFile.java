e6c73c4bcf5f1668d4750a1008c784dacdedbc9b
heenahan <mby1997s@gmail.com>
2023-06-28 13:44:54 +0900

    feat : 로깅 및 파일 쓰기 & 읽기 실패 시 커스텀 예외 던짐

--- src/main/java/com/prgms/springbootbasic/model/VouchersInFile.java
+++ src/main/java/com/prgms/springbootbasic/model/VouchersInFile.java
@@ -1,35 +1,52 @@
 package com.prgms.springbootbasic.model;
 
+import com.prgms.springbootbasic.exception.CantReadFileException;
+import com.prgms.springbootbasic.exception.CantWriteFileException;
+import com.prgms.springbootbasic.util.ExceptionMessage;
 import com.prgms.springbootbasic.util.VoucherApplication;
 import com.prgms.springbootbasic.util.VoucherType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Repository;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.StandardOpenOption;
 import java.util.List;
 import java.util.UUID;
 
+@Repository
 public class VouchersInFile implements VouchersStorage {
 
-	private final File file;
-
-	public VouchersInFile() throws IOException {
-		VoucherApplication voucherApplication = new VoucherApplication();
-		this.file = voucherApplication.getFile();
-	}
+	private static final String FILE_PATH = "data.csv";
+	private final Logger logger = LoggerFactory.getLogger(VouchersInFile.class);
 
 	@Override
 	public void save(Voucher voucher) throws IOException {
-		Files.write(file.toPath(), voucher.formatOfCSV(), StandardOpenOption.APPEND);
+		File file = VoucherApplication.file(FILE_PATH);
+		try {
+			Files.write(file.toPath(), voucher.formatOfCSV().getBytes(), )
+		} catch (IOException e) {
+			logger.error("파일을 쓸 수 없습니다. file path : {}", file.toPath());
+			throw new CantWriteFileException(ExceptionMessage.CANT_WRITE_FILE);
+		}
 	}
 	
 	@Override
 	public List<Voucher> findAll() throws IOException {
-		return Files.readAllLines(file.toPath())
-										.stream()
-										.map(s -> changeToVoucher(s.split(",")))
-										.toList();
+		try {
+			File file = VoucherApplication.file(FILE_PATH);
+			return Files.readAllLines(file.toPath())
+					.stream()
+					.map(s -> changeToVoucher(s.split(",")))
+					.toList();
+		} catch (IOException e) {
+			logger.error("파일을 읽을 수 없습니다. file path : {}", FILE_PATH);
+			throw new CantReadFileException(ExceptionMessage.CANT_READ_FILE);
+		}
 	}
 	
 	private Voucher changeToVoucher(String[] voucherOfCSV) {
