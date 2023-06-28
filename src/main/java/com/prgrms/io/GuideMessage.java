c6b233f87356e7e97b61b1c88382fa288d919150
byeolhaha <j35635aa@gmail.com>
2023-06-28 17:37:36 +0900

    refactor : toString 오버라이팅

---
+++ src/main/java/com/prgrms/io/GuideMessage.java
@@ -0,0 +1,21 @@
+package com.prgrms.io;
+
+public enum GuideMessage {
+    START("=== Voucher Program === \n" +
+            "Type exit to exit the program.\n" +
+            "Type create to create a new voucher.\n" +
+            "Type list to list all vouchers."),
+    CLOSE("바우처 어플리케이션을 종료합니다."),
+    COMPLETE_CREATE("바우처가 등록되었습니다.");
+
+    private final String message;
+
+    GuideMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return message;
+    }
+}
