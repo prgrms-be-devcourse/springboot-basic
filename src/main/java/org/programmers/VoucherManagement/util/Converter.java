0c5511d4a9ff2b23709a3095e8d22ded6b0dc221
KimMinheee <99alsgmlalsl@naver.com>
2023-06-28 17:11:13 +0900

    [Feat] : util 패키지 내 Converter 클래스 구현 - 라인을 읽고 separator에 따라 구분하여 Member화 하여 반환하는 메소드 구현

---
+++ src/main/java/org/programmers/VoucherManagement/util/Converter.java
@@ -0,0 +1,19 @@
+package org.programmers.VoucherManagement.util;
+
+import org.programmers.VoucherManagement.member.domain.Member;
+import org.programmers.VoucherManagement.member.domain.MemberStatus;
+
+import java.util.UUID;
+
+public class Converter {
+    private static final String SEPARATOR = ",";
+    public static Member toMember(String line){
+        String[] chunks = line.split(SEPARATOR);
+
+        UUID uuid = UUID.fromString(chunks[0]);
+        String name = chunks[1];
+        MemberStatus memberStatus = MemberStatus.from(chunks[2]);
+
+        return new Member(uuid,name,memberStatus);
+    }
+}
