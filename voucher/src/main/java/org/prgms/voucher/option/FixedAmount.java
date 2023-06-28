7a8771d9cd54d5c4dc559865794a9c770b931111
dingding-21 <phj8221@naver.com>
2023-06-28 20:01:16 +0900

    feat: 옵션 종류 구현체 변수 및 메서드 구현

--- voucher/src/main/java/org/prgms/voucher/option/FixedAmount.java
+++ voucher/src/main/java/org/prgms/voucher/option/FixedAmount.java
@@ -1,11 +1,54 @@
 package org.prgms.voucher.option;
 
+import org.prgms.voucher.type.OptionType;
 import org.prgms.voucher.voucher.Voucher;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
 public class FixedAmount implements Voucher {
+    private final UUID fixedAmountId;
+    private final int fixedAmount;
+    private final LocalDate publishDate;
+    private final LocalDate expirationDate;
+    private final LocalDateTime createdAt;
+
+    public FixedAmount(UUID fixedAmountId, int fixedAmount) {
+        this.fixedAmountId = fixedAmountId;
+        this.fixedAmount = fixedAmount;
+        this.publishDate = LocalDate.now();
+        this.expirationDate = publishDate.plusMonths(6);
+        this.createdAt = LocalDateTime.now();
+    }
 
     @Override
-    public void discount() {
+    public UUID getVoucherId() {
+        return fixedAmountId;
+    }
 
+    @Override
+    public String getOptionType() {
+        return OptionType.FIXED_AMOUNT.getOptionType();
+    }
+
+    @Override
+    public LocalDate getPublishDate() {
+        return publishDate;
+    }
+
+
+    @Override
+    public LocalDate getExpirationDate() {
+        return expirationDate;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    @Override
+    public int discount(int amountBeforeDiscount) {
+        return amountBeforeDiscount - fixedAmount;
     }
 }
--- voucher/src/main/java/org/prgms/voucher/option/PercentAmount.java
+++ voucher/src/main/java/org/prgms/voucher/option/PercentAmount.java
@@ -1,11 +1,54 @@
 package org.prgms.voucher.option;
 
+import org.prgms.voucher.type.OptionType;
 import org.prgms.voucher.voucher.Voucher;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
 public class PercentAmount implements Voucher {
+    private final UUID fixedAmountId;
+    private final int percentAmount;
+    private final LocalDate publishDate;
+    private final LocalDate expirationDate;
+    private final LocalDateTime createdAt;
+
+    public PercentAmount(UUID fixedAmountId, int percentAmount) {
+        this.fixedAmountId = fixedAmountId;
+        this.percentAmount = percentAmount;
+        this.publishDate = LocalDate.now();
+        this.expirationDate = publishDate.plusMonths(6);
+        this.createdAt = LocalDateTime.now();
+    }
 
     @Override
-    public void discount() {
+    public UUID getVoucherId() {
+        return fixedAmountId;
+    }
 
+    @Override
+    public String getOptionType() {
+        return OptionType.PERCENT_AMOUNT.getOptionType();
+    }
+
+    @Override
+    public LocalDate getPublishDate() {
+        return publishDate;
+    }
+
+    @Override
+    public LocalDate getExpirationDate() {
+        return expirationDate;
+    }
+
+    @Override
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    @Override
+    public int discount(int amountBeforeDiscount) {
+        return amountBeforeDiscount - (amountBeforeDiscount * percentAmount / 100);
     }
 }
