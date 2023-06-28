a707d4f7dcc9b7accebab62f7e0eb5f7232f628a
byeolhaha <j35635aa@gmail.com>
2023-06-28 17:53:51 +0900

    refactor : 불필요한 주석과 같은 속성이 존재하여 Enum에서 삭제하고 관련 메서드 수정

--- src/main/java/com/prgrms/model/voucher/VoucherPolicy.java
+++ src/main/java/com/prgrms/model/voucher/VoucherPolicy.java
@@ -5,34 +5,30 @@ import java.util.Optional;
 
 public enum VoucherPolicy {
     FixedAmountVoucher("1",
-            "고정 금액 할인에 따른 바우처",
             "얼만큼 할인 받고 싶은지 입력하세요 (단위: 원):",
             "원"),
     PercentDiscountVoucher("2",
-            "할인율에 따른 바우처",
             "할인율을 입력하세요 (0~100 사이의 값):",
             "%");
 
     private final String number;
-    private final String policy;
     private final String discountGuide;
     private final String unit;
 
-    VoucherPolicy(String number, String policy, String discountGuide, String unit) {
+    VoucherPolicy(String number, String discountGuide, String unit) {
         this.number = number;
-        this.policy = policy;
         this.discountGuide = discountGuide;
         this.unit = unit;
     }
 
-    public static Optional<VoucherPolicy> findBySelectedPolicy(String selectedPolicy) {
+    public static Optional<VoucherPolicy> findByPolicy(String policy) {
         return Arrays.stream(VoucherPolicy.values())
-                .filter(p -> p.number.equals(selectedPolicy))
+                .filter(p -> p.number.equals(policy))
                 .findFirst();
     }
 
-    public String voucherPolicyOptionGuid() {
-        return number + "번 : " + policy;
+    public String voucherPolicyOptionGuide() {
+        return number + "번 : " + name();
     }
 
     public String discountGuide() {
