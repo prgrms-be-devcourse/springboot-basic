c6995170a1b160694771c1c92080b720129ca150
JHS <wnghdtjr129@naver.com>
2023-06-29 13:13:39 +0900

    feat: 바우처 repo 생성

---
+++ src/main/java/org/devcourse/voucher/repository/InMemoryVoucherRepository.java
@@ -0,0 +1,38 @@
+package org.devcourse.voucher.repository;
+
+import org.devcourse.voucher.domain.voucher.Voucher;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
+
+public class InMemoryVoucherRepository implements VoucherRepository{
+
+    private final static Map<Long, Voucher> voucherStorage = new HashMap<>();
+    private final static AtomicLong idGenerator = new AtomicLong(1);
+
+    @Override
+    public Voucher save(Voucher voucher) {
+        if (voucher == null) {
+            throw new RuntimeException("빈 값을 저장 할 수 없습니다");
+        }
+
+        Voucher saveVoucher = voucher;
+        if (voucher.getId() == 0) {
+            long id = idGenerator.getAndIncrement();
+            saveVoucher = new Voucher(id, voucher.getType(), voucher.getPolicyAmount());
+        }
+
+        voucherStorage.put(saveVoucher.getId(), saveVoucher);
+
+        return voucher;
+    }
+
+    @Override
+    public List<Voucher> findAll() {
+        return new ArrayList<>(voucherStorage.values());
+    }
+
+}
