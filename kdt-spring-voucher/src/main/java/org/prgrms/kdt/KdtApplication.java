b04315ae2b79aaee8949a87fdfd5766c47b235c5
KimJaeUng <78838534+kju2405@users.noreply.github.com>
2023-06-29 18:19:46 +0900

    refector: 스프링부트 사용

--- kdt-spring-voucher/src/main/java/org/prgrms/kdt/KdtApplication.java
+++ kdt-spring-voucher/src/main/java/org/prgrms/kdt/KdtApplication.java
@@ -1,13 +1,41 @@
 package org.prgrms.kdt;
 
+import org.prgrms.kdt.controller.VoucherController;
+import org.prgrms.kdt.input.MenuCommand;
+import org.prgrms.kdt.input.UserInput;
+import org.prgrms.kdt.input.VoucherCommand;
+import org.prgrms.kdt.output.Output;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class KdtApplication {
+    public static void main(String[] args) {
+        ConfigurableApplicationContext ac = SpringApplication.run(KdtApplication.class, args);
+        Output outputConsole = ac.getBean(Output.class);
+        UserInput userInputMenu = ac.getBean(UserInput.class);
+        VoucherController voucherController = ac.getBean(VoucherController.class);
 
-	public static void main(String[] args) {
-		SpringApplication.run(KdtApplication.class, args);
-	}
-
+        while (true) {
+            outputConsole.displayMenu();
+            outputConsole.displayUserInputLine();
+            String userInputMenuCommand = userInputMenu.userInputMenuCommand();
+            MenuCommand UserInputMenuCommand = MenuCommand.findByUserInputMenuCommand(userInputMenuCommand);
+            switch (UserInputMenuCommand) {
+                case EXIT -> {
+                    return;
+                }
+                case CREATE -> {
+                    outputConsole.displayVoucherCreateMenu();
+                    outputConsole.displayUserInputLine();
+                    String userInputVoucherCreateMenuCommand = userInputMenu.userInputVoucherCreateMenuCommand();
+                    VoucherCommand UserInputVoucherCommand = VoucherCommand.findByUserInputVoucherCommand(userInputVoucherCreateMenuCommand);
+                    voucherController.createVoucher(UserInputVoucherCommand);
+                }
+                case LIST -> outputConsole.displayAllVoucherList();
+                case WRONG -> outputConsole.userInputWrongValue();
+            }
+        }
+    }
 }
