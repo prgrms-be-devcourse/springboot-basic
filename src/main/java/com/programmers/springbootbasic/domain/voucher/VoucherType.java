1efd32a9bb3352e2580e6ee8f3c761c50095c476
sangminnim <sangmins930@gmail.com>
2023-06-29 16:38:39 +0900

    feat: VoucherType

---
+++ src/main/java/com/programmers/springbootbasic/domain/voucher/VoucherType.java
@@ -0,0 +1,21 @@
+package com.programmers.springbootbasic.domain.voucher;
+
+import java.util.Arrays;
+
+public enum VoucherType {
+    RATE("정률 할인"),
+    FIX("정액 할인");
+
+    private final String voucherType;
+
+    VoucherType(String voucherType) {
+        this.voucherType = voucherType;
+    }
+
+    public static VoucherType from(String type) {
+        return Arrays.stream(values())
+                .filter(o -> type.equals(o.voucherType))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("잘못된 쿠폰 유형"));
+    }
+}
