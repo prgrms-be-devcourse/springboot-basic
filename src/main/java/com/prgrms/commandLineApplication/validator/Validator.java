10d635b4bf5523e9d090e9a35e7fa756df2b456d
박은지 <qkrdmswl1018@naver.com>
2023-06-29 23:55:56 +0900

    rename: 패키지 이동

---
+++ src/main/java/com/prgrms/commandLineApplication/validator/Validator.java
@@ -0,0 +1,32 @@
+package com.prgrms.commandLineApplication.validator;
+
+import com.prgrms.commandLineApplication.discount.FixedDiscount;
+import com.prgrms.commandLineApplication.discount.PercentDiscount;
+
+import java.util.UUID;
+
+public class Validator {
+
+  private static final String INVALID_ID_ERROR = "Invalid ID";
+  private static final String INVALID_FIXED_AMOUNT_RANGE_ERROR = "Invalid discount amount range (0 ~ 10000)";
+  private static final String INVALID_PERCENT_AMOUNT_ERROR = "Invalid discount amount range (0 ~ 100)";
+
+  public static void checkId(UUID id) {
+    if (id == null) {
+      throw new IllegalArgumentException(INVALID_ID_ERROR);
+    }
+  }
+
+  public static void checkFixedDiscount(int discountAmount) {
+    if (discountAmount < FixedDiscount.MINIMUM_VALUE || discountAmount > FixedDiscount.MAXIMUM_VALUE) {
+      throw new IllegalArgumentException(INVALID_FIXED_AMOUNT_RANGE_ERROR);
+    }
+  }
+
+  public static void checkPercentDiscount(int discountAmount) {
+    if (PercentDiscount.PERCENT_RATE_BASE < discountAmount || discountAmount < PercentDiscount.MINIMUM_VALUE) {
+      throw new IllegalArgumentException(INVALID_PERCENT_AMOUNT_ERROR);
+    }
+  }
+
+}
