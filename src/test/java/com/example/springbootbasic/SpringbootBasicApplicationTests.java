987ea0b07fbe9a9975fe7b102a63ada2c981091e
Choi Seongwon <vlflalektzja@naver.com>
2023-06-29 17:03:37 +0900

    test: 불필요한 테스트 코드 삭제

--- src/test/java/com/example/springbootbasic/SpringbootBasicApplicationTests.java
+++ src/test/java/com/example/springbootbasic/SpringbootBasicApplicationTests.java
@@ -1,13 +1,9 @@
 package com.example.springbootbasic;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringbootBasicApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
