05f2e7274ba23e30210dd4a15424e08b7bed4710
wonu606 <wonu606@gmail.com>
2023-06-29 12:17:02 +0900

    refactor: 팩토리에서 바우처 생성시 저장까지 담당하도록 수정

--- voucher-manager/src/main/java/com/wonu606/vouchermanager/service/VoucherFactory.java
+++ voucher-manager/src/main/java/com/wonu606/vouchermanager/service/VoucherFactory.java
@@ -3,11 +3,21 @@ package com.wonu606.vouchermanager.service;
 import com.wonu606.vouchermanager.domain.FixedAmountVoucher;
 import com.wonu606.vouchermanager.domain.PercentageVoucher;
 import com.wonu606.vouchermanager.domain.Voucher;
+import com.wonu606.vouchermanager.repository.VoucherRepository;
 import java.util.UUID;
+import lombok.AllArgsConstructor;
 
+@AllArgsConstructor
 public class VoucherFactory {
 
-    public Voucher createVoucher(VoucherType type, UUID uuid, double discount) {
+    private final VoucherRepository repository;
+
+    public Voucher create(VoucherType type, UUID uuid, double discount) {
+        Voucher voucher = createVoucher(type, uuid, discount);
+        return repository.save(voucher);
+    }
+
+    private Voucher createVoucher(VoucherType type, UUID uuid, double discount) {
         switch (type) {
             case FIXED:
                 return new FixedAmountVoucher(uuid, discount);
--- voucher-manager/src/main/java/com/wonu606/vouchermanager/service/VoucherService.java
+++ voucher-manager/src/main/java/com/wonu606/vouchermanager/service/VoucherService.java
@@ -4,24 +4,27 @@ import com.wonu606.vouchermanager.domain.Voucher;
 import com.wonu606.vouchermanager.repository.VoucherRepository;
 import java.util.List;
 import java.util.UUID;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
 public class VoucherService {
 
-    private final VoucherFactory factory = new VoucherFactory();
-    private final VoucherRepository voucherRepository;
+    private final VoucherFactory factory;
+    private final VoucherRepository repository;
+
+    public VoucherService(VoucherRepository repository) {
+        this.repository = repository;
+        this.factory = new VoucherFactory(repository);
+    }
 
     public Voucher createVoucher(String typeName, UUID uuid, double discount) {
         VoucherType type = VoucherType.getVoucherTypeByName(typeName)
                 .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 바우처 타입입니다."));
 
-        return factory.createVoucher(type, uuid, discount);
+        return factory.create(type, uuid, discount);
     }
 
     public List<Voucher> getVoucherList() {
-        return voucherRepository.findAll();
+        return repository.findAll();
     }
 }
