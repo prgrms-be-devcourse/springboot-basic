29c8adc1b484f29203f3754f79545a52286cb0d0
heenahan <mby1997s@gmail.com>
2023-06-29 23:19:18 +0900

    feat : yaml에서 파일 path 관리

--- src/main/java/com/prgms/springbootbasic/AppConfig.java
+++ src/main/java/com/prgms/springbootbasic/AppConfig.java
@@ -2,9 +2,11 @@ package com.prgms.springbootbasic;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @ComponentScan
+@PropertySource(value = "application.yaml", factory = YamlPropertyFactory.class)
 public class AppConfig {
 
 }
