10ec3bc063c64ac4186728ef400dd143d7118620
JHS <wnghdtjr129@naver.com>
2023-06-29 09:21:55 +0900

    feat: 바우처 서비스 로직 구현

---
+++ src/main/java/org/devcourse/voucher/app/VoucherManagerService.java
@@ -0,0 +1,29 @@
+package org.devcourse.voucher.app;
+
+import org.devcourse.voucher.domain.voucher.Voucher;
+import org.devcourse.voucher.repository.VoucherRepository;
+
+import java.util.List;
+
+public class VoucherManagerService implements VoucherService{
+
+    private final VoucherRepository voucherRepository;
+
+    public VoucherManagerService(VoucherRepository voucherRepository) {
+        this.voucherRepository = voucherRepository;
+    }
+
+    @Override
+    public Voucher save(Voucher voucher) {
+        if (voucher == null) {
+            throw new RuntimeException("저장하고자 하는 바우처가 빈 값일 수 없습니다");
+        }
+
+        return voucherRepository.save(voucher);
+    }
+
+    @Override
+    public List<Voucher> findAll() {
+        return voucherRepository.findAll();
+    }
+}
