37bdd293dbdc7b2ffb1f13ffd4bb6a9a4cec62ae
Juwoong <wwong7247@gmail.com>
2023-06-30 02:07:50 +0900

    style : validate 출력문 띄어쓰기 변경

--- voucher/src/main/java/com/example/voucher/utils/validator/VoucherValidator.java
+++ voucher/src/main/java/com/example/voucher/utils/validator/VoucherValidator.java
@@ -4,13 +4,19 @@ public class VoucherValidator {
 
 	public static void validatePercent(long percent) {
 		if (percent < 0 || percent > 100) {
-			throw new IllegalArgumentException("패센트 값은 0 과 100사이 여야합니다");
+			throw new IllegalArgumentException("패센트 값은 0 과 100사이여야 합니다");
 		}
 	}
 
 	public static void validateNonZero(long value) {
 		if (value == 0) {
-			throw new IllegalArgumentException("값은 0이 아니여야합니다");
+			throw new IllegalArgumentException("값은 0이 아니여야 합니다");
+		}
+	}
+
+	public static void validatePositive(long value) {
+		if (value <= 0) {
+			throw new IllegalArgumentException("값은 양수여야 합니다");
 		}
 	}
 
