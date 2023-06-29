5efba9ebe482fdb6d5a16d1a4ca6d6821433e337
heenahan <mby1997s@gmail.com>
2023-06-29 22:44:48 +0900

    refactor : 메인 controller에 종료기능 넣음

--- src/main/java/com/prgms/springbootbasic/controller/MenuController.java
+++ src/main/java/com/prgms/springbootbasic/controller/MenuController.java
@@ -1,8 +1,13 @@
 package com.prgms.springbootbasic.controller;
 
+import com.prgms.springbootbasic.exception.NoSuchMenuException;
 import com.prgms.springbootbasic.ui.Console;
+import com.prgms.springbootbasic.util.MemberMenu;
 import com.prgms.springbootbasic.util.Menu;
+import com.prgms.springbootbasic.util.VoucherMenu;
 import org.springframework.stereotype.Controller;
+
+import java.io.IOException;
 
 @Controller
 public class MenuController {
@@ -16,12 +21,31 @@ public class MenuController {
         try {
             String command = console.init();
             Menu menu = Menu.of(command);
-            VoucherController commandLineController = menu.getCommandLineController();
-            return commandLineController.run();
+            return runExit(menu);
         } catch (Exception e) {
             console.showExceptionMessage(e.getMessage());
         }
         return true;
+    }
+
+    private boolean runExit(Menu menu) throws IOException {
+        if (menu == Menu.EXIT) {
+            console.exit();
+            return false;
+        }
+        return runApplication(menu);
+    }
+
+    private boolean runApplication(Menu menu) throws IOException {
+        String command = console.initApplication(menu);
+        if (menu == Menu.MEMBER) {
+            MemberMenu memberMenu = MemberMenu.of(command);
+            MemberController memberController = memberMenu.getMemberController();
+            return memberController.run();
+        }
+        VoucherMenu voucherMenu = VoucherMenu.of(command);
+        VoucherController voucherController = voucherMenu.getCommandLineController();
+        return voucherController.run();
     }
 
 }
