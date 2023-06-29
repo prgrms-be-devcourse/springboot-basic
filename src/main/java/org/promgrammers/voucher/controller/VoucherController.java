90527df2e2c03d781f053115118c5cefddca65b0
seongHyun-Min <steve6133@naver.com>
2023-06-30 03:55:57 +0900

    feat : 바우처 컨트롤러 구현

---
+++ src/main/java/org/promgrammers/voucher/controller/VoucherController.java
@@ -0,0 +1,58 @@
+package org.promgrammers.voucher.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.promgrammers.voucher.domain.dto.VoucherListResponseDto;
+import org.promgrammers.voucher.domain.dto.VoucherRequestDto;
+import org.promgrammers.voucher.domain.dto.VoucherResponseDto;
+import org.promgrammers.voucher.service.VoucherService;
+import org.promgrammers.voucher.view.Console;
+import org.promgrammers.voucher.view.Option;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class VoucherController {
+
+    private final VoucherService voucherService;
+    private final Console console;
+
+    private boolean isRunning = true;
+
+    public void run() {
+        while (isRunning) {
+            try {
+                console.display();
+                String input = console.input();
+                Option option = Option.fromOption(input);
+                execute(option);
+            } catch (RuntimeException e) {
+                console.print(e.getMessage());
+            }
+        }
+    }
+
+    private void execute(Option option) {
+        if (option == Option.CREATE) {
+            createVoucher();
+        } else if (option == Option.LIST) {
+            listVouchers();
+        } else if (option == Option.EXIT) {
+            exitProgram();
+        }
+    }
+
+    private void createVoucher() {
+        VoucherRequestDto voucherDto = console.createVoucherDto();
+        VoucherResponseDto createdVoucher = voucherService.createVoucher(voucherDto);
+        console.print("Voucher 생성. : " + createdVoucher.toString());
+    }
+
+    private void listVouchers() {
+        VoucherListResponseDto voucherList = voucherService.findAll();
+        console.print(voucherList.toString());
+    }
+
+    private void exitProgram() {
+        isRunning = false;
+    }
+}
