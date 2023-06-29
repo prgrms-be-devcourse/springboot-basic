2748f930aaae32671046b099e9f24048dbb220a7
kys0411 <32180846@dankook.ac.kr>
2023-06-29 17:11:50 +0900

    add: error message 추가

--- src/main/java/programmers/org/voucher/exception/ErrorMessage.java
+++ src/main/java/programmers/org/voucher/exception/ErrorMessage.java
@@ -2,7 +2,8 @@ package programmers.org.voucher.exception;
 
 public enum ErrorMessage {
     COMMAND_ERROR_MESSAGE("유효하지 않은 커맨드입니다."),
-    VOUCHER_ERROR_MESSAGE("유효하지 않은 바우처 타입입니다.");
+    VOUCHER_ERROR_MESSAGE("유효하지 않은 바우처 타입입니다."),
+    DISCOUNT_ERROR_MESSAGE("유효한 할인 범위가 아닙니다.");
 
     private final String message;
 
