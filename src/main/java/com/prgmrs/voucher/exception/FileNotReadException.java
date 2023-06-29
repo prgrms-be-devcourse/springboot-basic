1cadca3cd04b3d4e4f211d2387810d93873da989
JaeyoungAhn <quaki@naver.com>
2023-06-30 04:22:32 +0900

    feat : 커스텀 익셉션 클래스 생성

---
+++ src/main/java/com/prgmrs/voucher/exception/FileNotReadException.java
@@ -0,0 +1,7 @@
+package com.prgmrs.voucher.exception;
+
+public class FileNotReadException extends RuntimeException {
+    public FileNotReadException(String message) {
+        super(message);
+    }
+}
