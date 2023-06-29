4640b85368c032503942d5342d434ea495eca6c6
Choi Seongwon <vlflalektzja@naver.com>
2023-06-29 17:15:17 +0900

    feat: 메모리에 저장하는 바우처 저장소 구현

--- src/main/java/com/example/springbootbasic/voucher/MemoryVoucherRepository.java
+++ src/main/java/com/example/springbootbasic/voucher/MemoryVoucherRepository.java
@@ -1,15 +1,23 @@
 package com.example.springbootbasic.voucher;
 
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
+@Repository
 public class MemoryVoucherRepository implements VoucherRepository {
+
+    private static final List<Voucher> storage = new ArrayList<>();
+
     @Override
     public void save(Voucher voucher) {
-
+        storage.add(voucher);
     }
 
     @Override
     public List<Voucher> findAll() {
-        return null;
+        return Collections.unmodifiableList(storage);
     }
 }
