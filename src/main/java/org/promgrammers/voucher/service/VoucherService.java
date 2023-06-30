5fe23138b7e6f8cc55530f11cd55321a85dcbf0e
seongHyun-Min <steve6133@naver.com>
2023-06-30 19:25:25 +0900

    refactor : 미사용 매서드 삭제

--- src/main/java/org/promgrammers/voucher/service/VoucherService.java
+++ src/main/java/org/promgrammers/voucher/service/VoucherService.java
@@ -10,9 +10,6 @@ import org.promgrammers.voucher.repository.VoucherRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.NoSuchElementException;
-import java.util.Optional;
-import java.util.UUID;
 import java.util.stream.Collectors;
 
 @Service
@@ -29,16 +26,6 @@ public class VoucherService {
         // 로그 메시지: Voucher 생성 로그
         log.info("Voucher 생성 - ID: {}, Amount: {}, VoucherType: {}", voucher.getId(), voucher.getAmount(), voucher.getVoucherType());
         return new VoucherResponseDto(voucher.getId(), voucher.getAmount(), voucher.getVoucherType());
-    }
-
-    public Optional<VoucherResponseDto> findById(UUID id) {
-        return Optional.ofNullable(voucherRepository.findById(id)
-                .map(voucher -> new VoucherResponseDto(voucher.getId(), voucher.getAmount(), voucher.getVoucherType()))
-                .orElseThrow(() -> {
-                    // 로그 메시지: ID를 찾을 수 없음 에러
-                    log.error("ID를 찾을 수 없음 - ID: {}", id);
-                    return new NoSuchElementException("해당 id는 존재하지 않습니다");
-                }));
     }
 
     public VoucherListResponseDto findAll() {
