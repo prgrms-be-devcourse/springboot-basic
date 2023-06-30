3215ac330b15fd928de1891ecfb8e4d819cc43ef
SW-H <hsw08@naver.com>
2023-06-30 23:26:02 +0900

    refactor : 메소드(할인 방식) 네이밍 수정 DiscountByFixedAmount -> FixedDiscount, DiscountByPercentage,  PercentageDiscount
    feat : checked exception 제거

--- src/main/java/co/programmers/voucher_management/voucher/service/DiscountTypeGenerator.java
+++ src/main/java/co/programmers/voucher_management/voucher/service/DiscountTypeGenerator.java
@@ -2,24 +2,25 @@ package co.programmers.voucher_management.voucher.service;
 
 import org.springframework.stereotype.Service;
 
-import co.programmers.voucher_management.voucher.entity.DiscountByFixedAmount;
-import co.programmers.voucher_management.voucher.entity.DiscountByPercentage;
+import co.programmers.voucher_management.exception.InvalidUserInputException;
 import co.programmers.voucher_management.voucher.entity.DiscountStrategy;
+import co.programmers.voucher_management.voucher.entity.FixedDiscount;
+import co.programmers.voucher_management.voucher.entity.PercentageDiscount;
 
 @Service
 public class DiscountTypeGenerator {
 	static DiscountStrategy discountStrategy;
 
-	public static DiscountStrategy of(String type, Integer amount) throws IllegalArgumentException {
+	public static DiscountStrategy of(String type, Integer amount) {
 		switch (type.toLowerCase()) {
 			case ("fixed"):
-				discountStrategy = new DiscountByFixedAmount(amount);
+				discountStrategy = new FixedDiscount(amount);
 				break;
 			case ("percent"):
-				discountStrategy = new DiscountByPercentage(amount);
+				discountStrategy = new PercentageDiscount(amount);
 				break;
 			default:
-				throw new IllegalArgumentException("Invalid type of discount");
+				throw new InvalidUserInputException("Invalid type of discount");
 		}
 		return discountStrategy;
 	}
