91e60bdf10c2bf3891d9195149380ee294bda425
charlesuu <tiger0192004@gmail.com>
2023-06-30 21:45:16 +0900

    feat: 흐름 제어 로직 구현

--- src/main/java/com/example/demo/VoucherApplication.java
+++ src/main/java/com/example/demo/VoucherApplication.java
@@ -1,14 +1,51 @@
 package com.example.demo;
 
 
+import com.example.demo.controller.VoucherController;
+import com.example.demo.dto.VoucherDto;
+import com.example.demo.util.CommandType;
+import com.example.demo.util.VoucherType;
+import com.example.demo.view.VoucherView;
+import java.util.List;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Component;
 
-@Controller
+@Component
 public class VoucherApplication implements CommandLineRunner {
 
+    private final VoucherController voucherController;
+    private final VoucherView voucherView;
+
+
+    public VoucherApplication(VoucherController voucherController, VoucherView voucherView) {
+        this.voucherController = voucherController;
+        this.voucherView = voucherView;
+    }
+
+
+    //흐름 제어
     @Override
     public void run(String... args) throws Exception {
-        System.out.println("Hi");
+        voucherView.printStartingMessage();
+
+        Boolean shouldContinue = true;
+        while (shouldContinue) {
+            CommandType commandType = voucherView.readCommandOption();
+
+            switch (commandType) {
+                case CREATE -> {
+                    VoucherType voucherType = voucherView.readVoucherOption();
+                    Integer amount = voucherView.readVoucherAmount(voucherType);
+                    voucherController.createVoucher(voucherType, amount);
+                    voucherView.printVoucherCreateMessage(voucherType, amount);
+                }
+                case LIST -> {
+                    List<VoucherDto> voucherDtoList = voucherController.readVoucherList();
+                    voucherView.printVoucherList(voucherDtoList);
+                }
+                case EXIT -> shouldContinue = false;
+                default -> throw new IllegalArgumentException("올바르지 않은 커맨드입니다.");
+            }
+        }
     }
 }
