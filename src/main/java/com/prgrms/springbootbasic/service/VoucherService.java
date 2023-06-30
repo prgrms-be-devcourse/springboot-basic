ac16d18dc97523534a1d76f3c050e121014bc936
jay-so <merry9504@naver.com>
2023-06-30 17:54:55 +0900

    refactor:Service 클래스의 createFixedDiscountVoucher,createPercentDiscountVoucher -> createVoucher 메소드로 리팩토링

--- src/main/java/com/prgrms/springbootbasic/service/VoucherService.java
+++ src/main/java/com/prgrms/springbootbasic/service/VoucherService.java
@@ -3,9 +3,9 @@ package com.prgrms.springbootbasic.service;
 import com.prgrms.springbootbasic.domain.FixedDiscountVoucher;
 import com.prgrms.springbootbasic.domain.PercentDiscountVoucher;
 import com.prgrms.springbootbasic.domain.Voucher;
+import com.prgrms.springbootbasic.enums.VoucherType;
 import com.prgrms.springbootbasic.repository.VoucherRepository;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 import org.springframework.stereotype.Service;
 
@@ -18,17 +18,17 @@ public class VoucherService {
         this.voucherRepository = voucherRepository;
     }
 
-    public Voucher createFixedDiscountVoucher(long amount) {
-        Voucher voucher = new FixedDiscountVoucher(UUID.randomUUID(), amount);
+    public Voucher createVoucher(VoucherType type, long discount) {
+        Voucher voucher;
+        switch (type) {
+            case FIXED -> voucher = new FixedDiscountVoucher(UUID.randomUUID(), discount);
+            case PERCENT -> voucher = new PercentDiscountVoucher(UUID.randomUUID(), discount);
+            default -> throw new IllegalStateException("입력가능한 Voucher Type은 " + type + "입니다.");
+        }
         return voucherRepository.insert(voucher);
     }
 
-    public Voucher createPercentDiscountVoucher(double percent) {
-        Voucher voucher = new PercentDiscountVoucher(UUID.randomUUID(), (long) percent);
-        return voucherRepository.insert(voucher);
-    }
-
-    public List<Voucher> fetchAllVouchers() {
-        return new ArrayList<>(voucherRepository.getAllVouchersList());
+    public Map<UUID, Voucher> fetchAllVouchers() {
+        return voucherRepository.getAllVouchersList();
     }
 }
