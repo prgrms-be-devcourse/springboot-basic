7b7e5e351c4414a0dfb23b6679cf10a4c39db433
jay-so <merry9504@naver.com>
2023-06-30 17:29:12 +0900

    refactor:VoucherType 메소드 추가

--- src/main/java/com/prgrms/springbootbasic/domain/Voucher.java
+++ src/main/java/com/prgrms/springbootbasic/domain/Voucher.java
@@ -1,8 +1,13 @@
 package com.prgrms.springbootbasic.domain;
 
+import com.prgrms.springbootbasic.enums.VoucherType;
 import java.util.UUID;
 
 public interface Voucher {
+
     UUID getVoucherId();
+
     long getDiscount();
+
+    VoucherType getVoucherType();
 }
