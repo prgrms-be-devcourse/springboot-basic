5b8a668c051db909f0aba27a9f1ca20827685618
jay-so <merry9504@naver.com>
2023-06-30 22:57:22 +0900

    refactor:Command enum &  올바르지 않은 명령어 입력시 오류 출력 개선

--- src/main/java/com/prgrms/springbootbasic/enums/Command.java
+++ src/main/java/com/prgrms/springbootbasic/enums/Command.java
@@ -1,12 +1,21 @@
 package com.prgrms.springbootbasic.enums;
 
+import java.util.Arrays;
+
 public enum Command {
     EXIT("exit"),
     CREATE("create"),
     LIST("list");
-    private final String command;
+    private final String inputCommand;
 
-    Command(String command) {
-        this.command = command;
+    Command(String inputCommand) {
+        this.inputCommand = inputCommand;
+    }
+
+    public static Command of(String inputCommand) {
+        return Arrays.stream(values())
+                .filter(e -> e.inputCommand.equalsIgnoreCase(inputCommand))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("잘못된 명령어 입력입니다. 다시 입력해주세요." + inputCommand));
     }
 }
