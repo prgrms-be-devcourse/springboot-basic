bcbbdf34d12a8305d5f75a38476cf3b528e9358c
sunny <rosa7536@naver.com>
2023-06-30 14:54:03 +0900

    FEAT : logger 추가

--- src/main/java/com/prgrms/spring/AppRunner.java
+++ src/main/java/com/prgrms/spring/AppRunner.java
@@ -5,6 +5,8 @@ import com.prgrms.spring.domain.menu.MenuType;
 import com.prgrms.spring.exception.Error;
 import com.prgrms.spring.io.ConsoleView;
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -16,10 +18,13 @@ public class AppRunner implements CommandLineRunner {
 
     private final ConsoleView consoleView;
 
+    private static final Logger logger = LoggerFactory.getLogger(AppRunner.class);
+
     @Override
     public void run(String... args) throws Exception {
         boolean isExecute = true;
         while (isExecute) {
+            logger.info("프로그램 시작.");
             consoleView.showMenu();
             MenuType menuType = MenuType.matchType(consoleView.getMenu());
             switch (menuType) {
--- src/main/java/com/prgrms/spring/controller/voucher/VoucherController.java
+++ src/main/java/com/prgrms/spring/controller/voucher/VoucherController.java
@@ -7,8 +7,9 @@ import com.prgrms.spring.exception.Success;
 import com.prgrms.spring.io.ConsoleView;
 import com.prgrms.spring.service.voucher.VoucherService;
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-
 
 
 @Controller
@@ -17,6 +18,7 @@ public class VoucherController {
 
     private final VoucherService voucherService;
     private final ConsoleView consoleView;
+    private static final Logger logger = LoggerFactory.getLogger(VoucherController.class);
 
     public void createVoucher() {
 
@@ -27,6 +29,7 @@ public class VoucherController {
         Voucher voucher = voucherService.createVoucher(type, discount);
         if (voucher == null) {
             consoleView.showErrorMsg(Error.CREATE_VOUCHER_EXCEPTION);
+            logger.error("바우처 생성 실패");
             return;
         }
         consoleView.showSuccessMsg(Success.CREATE_VOUCHER_SUCCESS);
