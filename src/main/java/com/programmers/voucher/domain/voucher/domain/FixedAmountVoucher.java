a2d2d702e01365e7ba9138ce6291ec6a7309a985
hseong3243 <hseong3243@gmail.com>
2023-07-01 00:15:40 +0900

    refactor: `Voucher` 객체 생성시점에 할인값에 대한 검증을 수행하도록 변경

--- src/main/java/com/programmers/voucher/domain/voucher/domain/FixedAmountVoucher.java
+++ src/main/java/com/programmers/voucher/domain/voucher/domain/FixedAmountVoucher.java
@@ -1,15 +1,32 @@
 package com.programmers.voucher.domain.voucher.domain;
 
 import com.programmers.voucher.domain.voucher.dto.VoucherDto;
+import com.programmers.voucher.global.util.CommonErrorMessages;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.UUID;
 
+import static com.programmers.voucher.domain.voucher.util.VoucherDiscountRange.FIXED_AMOUNT_MIN;
+import static com.programmers.voucher.domain.voucher.util.VoucherErrorMessages.INVALID_FIXED_AMOUNT;
+
 public class FixedAmountVoucher extends Voucher {
+    private static final Logger LOG = LoggerFactory.getLogger(FixedAmountVoucher.class);
+
     private final long amount;
 
     public FixedAmountVoucher(UUID voucherId, long amount) {
         super(voucherId);
+        validateAmount(amount);
         this.amount = amount;
+    }
+
+    private void validateAmount(long amount) {
+        if (amount <= FIXED_AMOUNT_MIN) {
+            String errorMessage = CommonErrorMessages.addCurrentInput(INVALID_FIXED_AMOUNT, amount);
+            LOG.warn(errorMessage);
+            throw new IllegalStateException(errorMessage);
+        }
     }
 
     @Override
--- src/main/java/com/programmers/voucher/domain/voucher/domain/PercentDiscountVoucher.java
+++ src/main/java/com/programmers/voucher/domain/voucher/domain/PercentDiscountVoucher.java
@@ -1,15 +1,33 @@
 package com.programmers.voucher.domain.voucher.domain;
 
 import com.programmers.voucher.domain.voucher.dto.VoucherDto;
+import com.programmers.voucher.global.util.CommonErrorMessages;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.UUID;
 
+import static com.programmers.voucher.domain.voucher.util.VoucherDiscountRange.PERCENT_DISCOUNT_MAX;
+import static com.programmers.voucher.domain.voucher.util.VoucherDiscountRange.PERCENT_DISCOUNT_MIN;
+import static com.programmers.voucher.domain.voucher.util.VoucherErrorMessages.INVALID_PERCENT_DISCOUNT;
+
 public class PercentDiscountVoucher extends Voucher {
+    private static final Logger LOG = LoggerFactory.getLogger(PercentDiscountVoucher.class);
+
     private final long percent;
 
     public PercentDiscountVoucher(UUID voucherId, long percent) {
         super(voucherId);
+        validatePercent(percent);
         this.percent = percent;
+    }
+
+    private void validatePercent(long percent) {
+        if (percent <= PERCENT_DISCOUNT_MIN || percent >= PERCENT_DISCOUNT_MAX) {
+            String errorMessage = CommonErrorMessages.addCurrentInput(INVALID_PERCENT_DISCOUNT, percent);
+            LOG.warn(errorMessage);
+            throw new IllegalArgumentException(errorMessage);
+        }
     }
 
     @Override
