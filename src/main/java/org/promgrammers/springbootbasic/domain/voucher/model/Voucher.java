4085d64cf92529b5d119d78c87f697c583c27955
hongxeob <sjun2918@naver.com>
2023-06-30 13:05:39 +0900

    refactor: Voucher lombok 설정

--- src/main/java/org/promgrammers/springbootbasic/domain/voucher/model/Voucher.java
+++ src/main/java/org/promgrammers/springbootbasic/domain/voucher/model/Voucher.java
@@ -1,15 +1,17 @@
 package org.promgrammers.springbootbasic.domain.voucher.model;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+
 import java.util.UUID;
 
+@Getter
+@EqualsAndHashCode
 public abstract class Voucher {
 
     private final UUID voucherId;
+
     private final long amount;
-
-    public abstract VoucherType getVoucherType();
-
-    public abstract long discount(long beforeDiscount);
 
     public UUID getVoucherId() {
         return voucherId;
@@ -21,9 +23,11 @@ public abstract class Voucher {
         this.amount = amount;
     }
 
-    protected abstract void validateAmount(long discountAmount);
+    protected abstract void validateAmount(long amount);
 
-    public long getAmount() {
-        return amount;
-    }
+    public abstract VoucherType getVoucherType();
+
+    public abstract long discount(long beforeDiscount);
+
+
 }
