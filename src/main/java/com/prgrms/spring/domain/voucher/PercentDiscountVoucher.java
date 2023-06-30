ce5bceedfc0f97e8aadd01305c75ec6a85da3ef7
sunny <rosa7536@naver.com>
2023-06-30 15:22:12 +0900

    FEAT : discount 값 범위 검증

--- src/main/java/com/prgrms/spring/domain/voucher/PercentDiscountVoucher.java
+++ src/main/java/com/prgrms/spring/domain/voucher/PercentDiscountVoucher.java
@@ -13,6 +13,7 @@ public class PercentDiscountVoucher implements Voucher {
     private final String discountUnit = "%";
 
     public static PercentDiscountVoucher newInstance(UUID voucherId, long discount) {
+        if (discount <= 0 || discount >= 100) throw new IllegalArgumentException("유효하지 않은 할인 금액");
         return new PercentDiscountVoucher(voucherId, discount);
     }
 
