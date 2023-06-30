3a149048042d8555440c416eafff71caec182b1e
Dev Uni <wlwhsvkdlxh@gmail.com>
2023-06-30 22:47:39 +0900

    fix: 커밋 개행

--- src/main/java/com/programmers/springmission/VoucherApplication.java
+++ src/main/java/com/programmers/springmission/VoucherApplication.java
@@ -6,8 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class VoucherApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(VoucherApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(VoucherApplication.class, args);
+    }
 
 }
+
--- src/test/java/com/programmers/springmission/VoucherApplicationTest.java
+++
@@ -1,7 +0,0 @@
-package com.programmers.springmission;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class VoucherApplicationTest {
-
-}
