ff6035a1053695e2fd1bef6447c99f8c766ed8c9
jay-so <merry9504@naver.com>
2023-06-30 14:17:37 +0900

    refactor: Input 인터페이스 리팩토링

--- src/main/java/com/prgrms/springbootbasic/io/Input.java
+++ src/main/java/com/prgrms/springbootbasic/io/Input.java
@@ -1,6 +1,13 @@
 package com.prgrms.springbootbasic.io;
 
 public interface Input {
+
+    //콘솔 명령어 입력
     String readCommand(String message);
-    String readString(String message);
+
+    //바우처 타입
+    String readVoucherType(String message);
+
+    //바우처 금액
+    String readVoucherValue(String message);
 }
