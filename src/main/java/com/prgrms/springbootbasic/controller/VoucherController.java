ae4e3f1cc7f529d2d7a251b21652532387e1198e
jay-so <merry9504@naver.com>
2023-06-30 22:59:21 +0900

    feat: VoucherController 구현

--- src/main/java/com/prgrms/springbootbasic/controller/VoucherController.java
+++ src/main/java/com/prgrms/springbootbasic/controller/VoucherController.java
@@ -1,8 +1,24 @@
 package com.prgrms.springbootbasic.controller;
 
+import com.prgrms.springbootbasic.domain.Voucher;
+import com.prgrms.springbootbasic.enums.VoucherType;
+import com.prgrms.springbootbasic.service.VoucherService;
+import java.util.Map;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 
 @Controller
+@AllArgsConstructor
 public class VoucherController {
 
+    private final VoucherService voucherService;
+
+    public void createVoucher(VoucherType type, long discount) {
+        voucherService.createVoucher(type, discount);
+    }
+
+    public Map<UUID, Voucher> printVoucherList() {
+        return voucherService.fetchAllVouchers();
+    }
 }
