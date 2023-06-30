ce70ce018e680f19a03271e6f4c5dc6785ff0d19
HyuckJuneHong <jjj8337721@naver.com>
2023-06-30 17:17:09 +0900

    feat : DB 관리를 위한 YAML, DI 설정 및 Table 설계

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,30 @@
+use voucher_schema;
+
+drop table if exists tbl_vouchers_members;
+drop table if exists tbl_vouchers;
+drop table if exists tbl_members;
+
+create table tbl_vouchers (
+    voucher_id varchar(50) not null unique ,
+    voucher_amount int not null ,
+    voucher_policy varchar(50) not null ,
+
+    primary key (voucher_id)
+);
+
+create table tbl_members (
+    member_id varchar(50) not null unique ,
+    member_status varchar(50) not null ,
+
+    primary key (member_id)
+);
+
+create table tbl_vouchers_members (
+    voucher_member_id varchar(50) not null unique ,
+    voucher_id varchar(50) not null ,
+    member_id varchar(5) not null ,
+
+    primary key (voucher_member_id) ,
+    foreign key (voucher_id) references tbl_vouchers (voucher_id) on delete cascade on update cascade ,
+    foreign key (member_id) references tbl_members (member_id) on delete cascade on update cascade
+);
