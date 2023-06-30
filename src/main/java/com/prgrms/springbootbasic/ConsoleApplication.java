8e4dd187280f14985ee13023378298b46617ff4d
jay-so <merry9504@naver.com>
2023-07-01 00:38:50 +0900

    refactor: ConsoleApplication 리팩토링

--- src/main/java/com/prgrms/springbootbasic/ConsoleApplication.java
+++ src/main/java/com/prgrms/springbootbasic/ConsoleApplication.java
@@ -1,148 +1,56 @@
 package com.prgrms.springbootbasic;
 
-import com.prgrms.springbootbasic.controller.FixedDiscountVoucherController;
-import com.prgrms.springbootbasic.controller.PercentDiscountVoucherController;
-import com.prgrms.springbootbasic.domain.FixedDiscountVoucher;
-import com.prgrms.springbootbasic.domain.PercentDiscountVoucher;
+import com.prgrms.springbootbasic.controller.VoucherController;
 import com.prgrms.springbootbasic.domain.Voucher;
-import com.prgrms.springbootbasic.repository.VoucherRepository;
-import com.prgrms.springbootbasic.service.VoucherService;
-import java.io.Console;
-import java.util.List;
+import com.prgrms.springbootbasic.enums.Command;
+import com.prgrms.springbootbasic.enums.VoucherType;
+import com.prgrms.springbootbasic.view.Console;
 import java.util.Map;
 import java.util.UUID;
-import org.beryx.textio.TextIO;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
 
+@Slf4j
+@Component
+@AllArgsConstructor
 public class ConsoleApplication implements CommandLineRunner {
 
-    private TextIO textIO;
-    private T terminal;
-    private final FixedDiscountVoucherController fixedDiscountVoucherController;
-    private final PercentDiscountVoucherController percentDiscountVoucherController;
-    private final VoucherService voucherService;
-    public static final Logger logger = LoggerFactory.getLogger(Console.class);
-
-    public Console(TextIO textIO, T terminal, VoucherRepository voucherRepository) {
-        this.textIO = textIO;
-        this.terminal = terminal;
-        this.voucherService = new VoucherService(voucherRepository);
-        this.fixedDiscountVoucherController = new FixedDiscountVoucherController(this, this, voucherService);
-        this.percentDiscountVoucherController = new PercentDiscountVoucherController(this, this, voucherService);
-    }
-
-    @Override
-    public String readCommand(String message) {
-        return textIO.newStringInputReader().read(message);
-    }
-
-    @Override
-    public String readVoucherType(String message) {
-        return null;
-    }
-
-    @Override
-    public String readVoucherValue(String message) {
-        return null;
-    }
-
-    @Override
-    public String readString(String message) {
-        return textIO.newStringInputReader().read(message);
-    }
-
-    @Override
-    public void printlnCommand(String message) {
-        terminal.println(message);
-    }
-
-    @Override
-    public void printCreateVoucherType() {
-
-    }
-
-    @Override
-    public void printlnVoucherList(Map<UUID, Voucher> voucherMap) {
-
-    }
-
-    @Override
-    public void errorMessage(String errorMessage) {
-
-    }
-
-    public void run() {
-        consoleMenu();
-        while (true) {
-            String inputCommand = readCommand("명령어를 입력해주세요.(exit, create, list)");
-            switch (inputCommand) {
-                case "exit":
-                    printlnCommand("프로그램을 종료합니다.");
-                    return;
-                case "create":
-                    create();
-                    break;
-                case "list":
-                    list();
-                    break;
-                default:
-                    printlnCommand("허용된 명령어가 아닙니다. exit(종료), create(바우처 생성), list(바우처 목록)중 하나를 선택하세요.");
-                    logger.error("명령어 입력 오류");
-                    break;
-            }
-        }
-    }
-
-    private void create() {
-        String voucherType = readString("생성할 voucher의 종류를 입력해주세요.(FixedAmountVoucher, PercentDiscountVoucher)");
-        switch (voucherType) {
-            case "FixedAmountVoucher":
-                fixedDiscountVoucherController.createFixedDiscountVoucher();
-                break;
-            case "PercentDiscountVoucher":
-                percentDiscountVoucherController.createPercentDiscountVoucher();
-                break;
-            default:
-                printlnCommand("입력가능한 Voucher Type은 고정 금액 할인과 퍼센트 금액 할인 타입 입니다.");
-                printlnCommand("FixedAmountVoucher과 PercentDiscountVoucher 중 하나를 선택하여 생성해주세요.");
-                logger.error("Voucher Type입력 오류");
-                break;
-        }
-    }
-
-    private void list() {
-        try {
-            List<Voucher> vouchers = voucherService.fetchAllVouchers();
-
-            if (vouchers.isEmpty()) {
-                printlnCommand("생성된 Voucher가 없습니다.");
-            } else {
-                printlnCommand("생성된 Voucher 목록입니다.");
-                for (Voucher voucher : vouchers) {
-                    if (voucher instanceof FixedDiscountVoucher) {
-                        printlnCommand("FixedAmountVoucher: " + voucher.getDiscount());
-                    } else if (voucher instanceof PercentDiscountVoucher) {
-                        printlnCommand("PercentDiscountVoucher: " + voucher.getDiscount());
-                    }
-                }
-            }
-        } catch (Exception e) {
-            printlnCommand("Voucher 목록을 가져오는 중 오류가 발생했습니다.");
-            logger.error("Voucher 목록 불러오기 오류");
-        }
-    }
-
-    public void consoleMenu() {
-        terminal.println("=== Voucher Program ===");
-        terminal.println("Type exit to exit the program.");
-        terminal.println("Type create to create a new voucher.");
-        terminal.println("Type list to list all vouchers.");
-    }
+    private final Console console;
+    private final VoucherController voucherController;
 
     @Override
     public void run(String... args) throws Exception {
+        console.consoleMenu();
 
+        while (true) {
+            String command = console.inputCommand();
+            Command inputCommand = Command.checkInputCommand(command);
+
+            switch (inputCommand) {
+                case CREATE -> createVoucher();
+                case LIST -> getVoucherList();
+                case EXIT -> {
+                    console.printMessage("프로그램을 종료합니다.");
+                    return;
+                }
+            }
+        }
+    }
+
+    private void createVoucher() {
+        String voucherTypeInput = console.inputVoucherType();
+        VoucherType voucherType = VoucherType.checkVoucherType(voucherTypeInput);
+
+        long voucherDiscount = console.inputVoucherDiscount();
+
+        voucherController.createVoucher(voucherType, voucherDiscount);
+        console.printMessage("바우처가 생성되었습니다!");
+    }
+
+    private void getVoucherList() {
+        Map<UUID, Voucher> voucherMap = voucherController.printVoucherList();
+        console.printlnVoucherList(voucherMap);
     }
 }
