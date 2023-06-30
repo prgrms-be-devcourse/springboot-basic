63c507fb6765204a5b4fef74f73a8650bfd2396f
HyuckJuneHong <jjj8337721@naver.com>
2023-06-30 19:43:23 +0900

    refactor : 기존 Repository Profile 설정 변경

--- src/main/java/kr/co/springbootweeklymission/voucher/domain/repository/InFileVoucherRepository.java
+++ src/main/java/kr/co/springbootweeklymission/voucher/domain/repository/InFileVoucherRepository.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-@Profile("dev")
+@Profile("file")
 @Repository
 public class InFileVoucherRepository implements VoucherRepository {
     private static final File VOUCHER_FILE = new File("src/main/resources/files/voucher_file.txt");
