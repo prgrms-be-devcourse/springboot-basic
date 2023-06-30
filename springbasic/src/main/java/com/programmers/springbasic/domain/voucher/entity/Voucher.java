f0b4bc200c9fa69103481c4c94cced2f4f7f9fb1
hi-june <wnsgur735@gmail.com>
2023-06-30 15:42:15 +0900

    refactor: LocalDate로 변경

--- springbasic/src/main/java/com/programmers/springbasic/domain/voucher/entity/Voucher.java
+++ springbasic/src/main/java/com/programmers/springbasic/domain/voucher/entity/Voucher.java
@@ -1,33 +1,32 @@
 package com.programmers.springbasic.domain.voucher.entity;
 
+import com.programmers.springbasic.domain.voucher.model.VoucherOption;
 import lombok.Getter;
 
 import java.io.Serializable;
-import java.util.Calendar;
-import java.util.Date;
+import java.time.LocalDate;
 import java.util.UUID;
 
 @Getter
 public abstract class Voucher implements Serializable {
-    private static final int EXPIRE_DURATION = 7;   // voucher 기본 유효 기간
+    private static final long EXPIRE_DURATION = 7;   // voucher 기본 유효 기간
 
     protected UUID code;  // 각 voucher의 고유한 코드
     protected double value;   // voucher의 가치
-    protected Date expirationDate;    // voucher의 만료일
+    protected VoucherOption voucherType;    // voucher 타입
+    protected LocalDate expirationDate;    // voucher의 만료일
     protected boolean isActive;   // voucher의 활성화 상태
 
     Voucher() {
         this.code = UUID.randomUUID();
-        this.expirationDate = getExpirationDate();
+        this.expirationDate = setExpirationDate();
         this.isActive = true;
     }
 
-    public abstract String getInfo();
+    private LocalDate setExpirationDate() {
+        LocalDate date = LocalDate.now();
+        date.plusDays(EXPIRE_DURATION);
 
-    private Date getExpirationDate() {
-        Calendar calendar = Calendar.getInstance();
-        calendar.add(Calendar.DAY_OF_MONTH, EXPIRE_DURATION);
-
-        return calendar.getTime();
+        return date;
     }
 }
