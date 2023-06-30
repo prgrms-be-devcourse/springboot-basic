027edd37c3d32cb95a0735f52f3229f0a597a1d9
charlesuu <tiger0192004@gmail.com>
2023-06-30 21:47:22 +0900

    feat: 바우처 Repository 구현

--- src/main/java/com/example/demo/domain/voucher/MemoryVoucherRepository.java
+++ src/main/java/com/example/demo/domain/voucher/MemoryVoucherRepository.java
@@ -1,16 +1,33 @@
 package com.example.demo.domain.voucher;
 
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.UUID;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class MemoryVoucherRepository implements VoucherRepository {
+
+    private final Map<UUID, Voucher> voucherHashMap = new LinkedHashMap<>();
 
     @Override
     public void save(Voucher voucher) {
-
+        voucherHashMap.put(voucher.getUUID(), voucher);
     }
 
     @Override
-    public List<Voucher> readVoucherList() {
-        return null;
+    public Optional<Voucher> findById(UUID id) {
+        Voucher voucher = voucherHashMap.get(id);
+        return Optional.ofNullable(voucher);
+    }
+
+    @Override
+    public List<Voucher> findAll() {
+        return voucherHashMap
+                .values()
+                .stream()
+                .toList();
     }
 }
--- src/main/java/com/example/demo/domain/voucher/VoucherRepository.java
+++ src/main/java/com/example/demo/domain/voucher/VoucherRepository.java
@@ -1,10 +1,14 @@
 package com.example.demo.domain.voucher;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 public interface VoucherRepository {
 
     void save(Voucher voucher);
 
-    List<Voucher> readVoucherList();
+    Optional<Voucher> findById(UUID id);
+
+    List<Voucher> findAll();
 }
