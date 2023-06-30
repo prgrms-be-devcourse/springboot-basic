02771ce602dea307624e90c4b665ae20cdf12c0a
sujjangOvO <pnk7038@gmail.com>
2023-07-01 01:43:48 +0900

    feat : logback 설정으로 에러 파일 기록

--- src/main/java/com/prgms/voucher/voucherproject/VoucherApp.java
+++ src/main/java/com/prgms/voucher/voucherproject/VoucherApp.java
@@ -5,9 +5,15 @@ import com.prgms.voucher.voucherproject.domain.VoucherType;
 import com.prgms.voucher.voucherproject.io.Console;
 import com.prgms.voucher.voucherproject.repository.MemoryVoucherRepository;
 import com.prgms.voucher.voucherproject.service.VoucherService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.InputMismatchException;
 
+import static com.prgms.voucher.voucherproject.domain.MenuType.EXIT;
+
 public class VoucherApp implements Runnable {
+    private static final Logger logger = LoggerFactory.getLogger(VoucherApp.class);
     private int EXIT_NUM = 1;
     private final Console console = new Console();
 
@@ -23,38 +29,37 @@ public class VoucherApp implements Runnable {
         new VoucherApp(new VoucherService(new MemoryVoucherRepository())).run();
     }
 
+    String menuName;
+
     @Override
     public void run() {
 
         while (EXIT_NUM > 0) {
             console.printMenu();
+            menuName = console.inputCommand().toLowerCase();
 
             try {
-                switch (MenuType.getSelectedMenuType(console.inputCommand().toLowerCase())) {
-                    case EXIT:
+                switch (MenuType.getSelectedMenuType(menuName)) {
+                    case EXIT -> {
                         EXIT_NUM = -1;
                         console.printMsg("프로그램을 종료합니다.");
-                        break;
-
-                    case CREATE:
+                    }
+                    case CREATE -> {
                         console.printKindOfVoucher();
                         int selectedNum = console.inputSelectedVoucherType();
                         console.bufferDeleted();
-
                         try {
                             VoucherType voucherType = VoucherType.getSelectedVoucherType(selectedNum);
                             voucherService.create(voucherType);
                         } catch (InputMismatchException e) {
+                            logger.error("VoucherType InputMismatchException -> {}", selectedNum);
                             System.out.println(e.getLocalizedMessage());
                         }
-
-                        break;
-
-                    case LIST:
-                        voucherService.list();
-                        break;
+                    }
+                    case LIST -> voucherService.list();
                 }
             } catch (InputMismatchException e) {
+                logger.error("MenuType InputMismatchException -> {}", menuName);
                 console.printErrorMsg();
             }
 
--- src/main/java/com/prgms/voucher/voucherproject/service/VoucherService.java
+++ src/main/java/com/prgms/voucher/voucherproject/service/VoucherService.java
@@ -6,6 +6,8 @@ import com.prgms.voucher.voucherproject.domain.Voucher;
 import com.prgms.voucher.voucherproject.domain.VoucherType;
 import com.prgms.voucher.voucherproject.io.Console;
 import com.prgms.voucher.voucherproject.repository.VoucherRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.text.MessageFormat;
@@ -14,10 +16,13 @@ import java.util.UUID;
 
 @Service
 public class VoucherService {
+    private static final Logger logger = LoggerFactory.getLogger(VoucherService.class);
     private final VoucherRepository voucherRepository;
     private final Console console = new Console();
     private final String CREATE_FIXED_MSG = "고정 할인 금액을 입력하세요. (1이상)";
     private final String CREATE_PERCENT_MSG = "퍼센트 할인 금액을 입력하세요. (1~99)";
+    private long discount;
+    private long percent;
 
     public VoucherService(VoucherRepository voucherRepository) {
         this.voucherRepository = voucherRepository;
@@ -25,7 +30,7 @@ public class VoucherService {
 
     private void createFixedVoucher() {
         console.printMsg(CREATE_FIXED_MSG);
-        long discount = console.inputDiscountAmount();
+        discount = console.inputDiscountAmount();
         console.bufferDeleted();
         Voucher fixedAmountVoucher = new FixedAmountVoucher(UUID.randomUUID(), discount);
         voucherRepository.save(fixedAmountVoucher);
@@ -33,7 +38,7 @@ public class VoucherService {
 
     private void createPercentVoucher() {
         console.printMsg(CREATE_PERCENT_MSG);
-        long percent = console.inputDiscountAmount();
+        percent = console.inputDiscountAmount();
         console.bufferDeleted();
         Voucher percentDiscountVoucher = new PercentDiscountVoucher(UUID.randomUUID(), percent);
         voucherRepository.save(percentDiscountVoucher);
@@ -53,21 +58,24 @@ public class VoucherService {
 
     public void create(VoucherType voucherType) {
         switch (voucherType) {
-            case FIXED:
+            case FIXED -> {
                 try {
                     createFixedVoucher();
                 } catch (IllegalArgumentException e) {
+                    logger.error("FixedAmountVoucher IllegalArgumentException -> {}", discount);
                     System.out.println(e.getLocalizedMessage());
                 }
-                break;
+            }
 
-            case PERCENT:
+
+            case PERCENT -> {
                 try {
                     createPercentVoucher();
                 } catch (IllegalArgumentException e) {
+                    logger.error("PercentDiscountVoucher IllegalArgumentException -> {}", percent);
                     System.out.println(e.getLocalizedMessage());
                 }
-                break;
+            }
         }
     }
 
