4966eaaf2bc5ff0e1597a5beef1b114c65127012
jay-so <merry9504@naver.com>
2023-06-30 22:58:34 +0900

    refactor: VoucherType enum & 올바르지 못한 타입 입력시 오류 출력 개선

--- src/main/java/com/prgrms/springbootbasic/enums/VoucherType.java
+++ src/main/java/com/prgrms/springbootbasic/enums/VoucherType.java
@@ -1,12 +1,20 @@
 package com.prgrms.springbootbasic.enums;
 
+import java.util.Arrays;
+
 public enum VoucherType {
     FIXED("Fixed"),
     PERCENT("Percent");
-    private final String voucherType;
+    private final String inputVoucherType;
 
+    VoucherType(String inputVoucherType) {
+        this.inputVoucherType = inputVoucherType;
+    }
 
-    VoucherType(String voucherType) {
-        this.voucherType = voucherType;
+    public static VoucherType checkVoucherType(String voucherType) {
+        return Arrays.stream(values())
+                .filter(type -> type.inputVoucherType.equalsIgnoreCase(voucherType))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("잘못된 바우처 타입입니다."));
     }
 }
