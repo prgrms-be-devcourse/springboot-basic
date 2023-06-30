2c6110bf3094773b989b71c5c640d6b2fa6783d6
baejunil <bjo6300@naver.com>
2023-06-30 15:12:14 +0900

    refactor : controller의 책임을 service로 이동

--- springboot-basic/src/main/java/com/example/commandlineapplication/domain/voucher/controller/VoucherController.java
+++ springboot-basic/src/main/java/com/example/commandlineapplication/domain/voucher/controller/VoucherController.java
@@ -1,9 +1,5 @@
 package com.example.commandlineapplication.domain.voucher.controller;
 
-import com.example.commandlineapplication.domain.voucher.dto.VoucherCreateRequest;
-import com.example.commandlineapplication.domain.voucher.model.Voucher;
-import com.example.commandlineapplication.domain.voucher.model.VoucherType;
-import com.example.commandlineapplication.domain.voucher.service.VoucherFactory;
 import com.example.commandlineapplication.domain.voucher.service.VoucherService;
 import com.example.commandlineapplication.global.io.Command;
 import com.example.commandlineapplication.global.io.Input;
@@ -20,7 +16,6 @@ public class VoucherController implements Runnable {
   private static final Logger LOG = LoggerFactory.getLogger(VoucherController.class);
   private final Input input;
   private final Output output;
-  private final VoucherFactory voucherFactory;
   private final VoucherService voucherService;
 
   @Override
@@ -34,18 +29,7 @@ public class VoucherController implements Runnable {
 
         switch (command) {
           case CREATE:
-            output.printCreateOption();
-
-            VoucherType inputVoucherType = VoucherType.of(input.selectOption());
-
-            output.printDiscount();
-
-            Integer discount = input.getDiscount();
-
-            VoucherCreateRequest voucherCreateRequest = new VoucherCreateRequest(inputVoucherType,
-                discount);
-            Voucher voucher = voucherFactory.create(voucherCreateRequest);
-            voucherService.insert(voucher);
+            voucherService.createVoucher();
             continue;
           case LIST:
             voucherService.history();
--- springboot-basic/src/main/java/com/example/commandlineapplication/domain/voucher/service/VoucherService.java
+++ springboot-basic/src/main/java/com/example/commandlineapplication/domain/voucher/service/VoucherService.java
@@ -1,7 +1,10 @@
 package com.example.commandlineapplication.domain.voucher.service;
 
+import com.example.commandlineapplication.domain.voucher.dto.VoucherCreateRequest;
 import com.example.commandlineapplication.domain.voucher.model.Voucher;
+import com.example.commandlineapplication.domain.voucher.model.VoucherType;
 import com.example.commandlineapplication.domain.voucher.repository.MemoryVoucherRepository;
+import com.example.commandlineapplication.global.io.Input;
 import com.example.commandlineapplication.global.io.Output;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
@@ -15,8 +18,25 @@ public class VoucherService {
   private final static Logger LOG = LoggerFactory.getLogger(VoucherService.class);
   private final MemoryVoucherRepository memoryVoucherRepository;
   private final Output output;
+  private final Input input;
+  private final VoucherFactory voucherFactory;
 
-  public Voucher insert(Voucher voucher) {
+  public void createVoucher() {
+    output.printCreateOption();
+    VoucherType inputVoucherType = VoucherType.of(input.selectOption());
+
+    output.printDiscount();
+    Integer discount = input.getDiscount();
+
+    VoucherCreateRequest voucherCreateRequest = new VoucherCreateRequest(inputVoucherType,
+        discount);
+
+    Voucher voucher = voucherFactory.create(voucherCreateRequest);
+
+    save(voucher);
+  }
+
+  public Voucher save(Voucher voucher) {
     LOG.info("Voucher가 저장되었습니다.");
     return memoryVoucherRepository.insert(voucher);
   }
