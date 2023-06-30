e8fab1c10d91fe8a10e8cbf2c37f3506e5553b8f
jay-so <merry9504@naver.com>
2023-06-30 20:30:49 +0900

    refacotor: VoucherApplication 클래스 리팩토링

--- src/main/java/com/prgrms/springbootbasic/VoucherApplication.java
+++ src/main/java/com/prgrms/springbootbasic/VoucherApplication.java
@@ -1,33 +1,12 @@
 package com.prgrms.springbootbasic;
 
-import com.prgrms.springbootbasic.repository.MemoryVoucherRepository;
-import com.prgrms.springbootbasic.repository.VoucherRepository;
-import org.beryx.textio.TextIO;
-import org.beryx.textio.TextIoFactory;
-import org.beryx.textio.TextTerminal;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class VoucherApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(VoucherApplication.class, args);
-    }
-    @Bean
-    public CommandLineRunner consoleRunner() {
-        return args -> {
-            TextIO textIO = TextIoFactory.getTextIO();
-            try {
-                TextTerminal<?> terminal = textIO.getTextTerminal();
-                VoucherRepository voucherRepository = new MemoryVoucherRepository();
-                Console<TextTerminal<?>> console = new Console<>(textIO, terminal, voucherRepository);
-                console.run();
-            } finally {
-                textIO.dispose();
-            }
-        };
     }
 }
