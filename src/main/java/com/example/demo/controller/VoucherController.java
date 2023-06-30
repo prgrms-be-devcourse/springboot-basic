2021844412668d777c2e735a434b4c79ef3b8333
charlesuu <tiger0192004@gmail.com>
2023-06-30 21:48:00 +0900

    feat: Voucher 컨트롤러 구현

--- src/main/java/com/example/demo/controller/VoucherController.java
+++ src/main/java/com/example/demo/controller/VoucherController.java
@@ -1,5 +1,31 @@
 package com.example.demo.controller;
 
+import com.example.demo.dto.VoucherDto;
+import com.example.demo.service.VoucherService;
+import com.example.demo.util.VoucherType;
+import java.util.List;
+import java.util.UUID;
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class VoucherController {
+
+    private final VoucherService voucherService;
+
+    public VoucherController(VoucherService voucherService) {
+        this.voucherService = voucherService;
+    }
+
+    public void createVoucher(VoucherType voucherType, int amount) {
+        voucherService.createVoucher(voucherType, amount);
+    }
     
+    public List<VoucherDto> readVoucherList() {
+        return voucherService.readVoucherList();
+    }
+
+    public VoucherDto readVoucher(UUID id) {
+        return voucherService.readVoucher(id);
+    }
 }
+
