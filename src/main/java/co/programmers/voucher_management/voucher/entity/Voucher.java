47a6177df9089eaf4f7be692ecf85627594a3814
SW-H <hsw08@naver.com>
2023-06-30 23:28:08 +0900

    feat : lombok-builder 추가

--- src/main/java/co/programmers/voucher_management/voucher/entity/Voucher.java
+++ src/main/java/co/programmers/voucher_management/voucher/entity/Voucher.java
@@ -1,9 +1,12 @@
 package co.programmers.voucher_management.voucher.entity;
 
+import lombok.Builder;
+
 public class Voucher {
 	private final int id;
 	private final DiscountStrategy discountStrategy;
 
+	@Builder
 	public Voucher(int id, DiscountStrategy discountStrategy) {
 		this.id = id;
 		this.discountStrategy = discountStrategy;
