e70fba05d7ab858298560e04f4d5b40cc8fcdfd3
hongxeob <sjun2918@naver.com>
2023-07-01 15:20:54 +0900

    test: Wallet Service 테스트 코드 작성

---
+++ src/test/java/org/promgrammers/springbootbasic/domain/wallet/service/WalletServiceTest.java
@@ -0,0 +1,250 @@
+package org.promgrammers.springbootbasic.domain.wallet.service;
+
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.promgrammers.springbootbasic.controller.CommandLineController;
+import org.promgrammers.springbootbasic.domain.customer.model.Customer;
+import org.promgrammers.springbootbasic.domain.customer.repository.impl.JdbcCustomerRepository;
+import org.promgrammers.springbootbasic.domain.voucher.model.FixedAmountVoucher;
+import org.promgrammers.springbootbasic.domain.voucher.model.Voucher;
+import org.promgrammers.springbootbasic.domain.voucher.repository.impl.JdbcVoucherRepository;
+import org.promgrammers.springbootbasic.domain.wallet.dto.request.CreateWalletRequest;
+import org.promgrammers.springbootbasic.domain.wallet.dto.response.WalletListResponse;
+import org.promgrammers.springbootbasic.domain.wallet.dto.response.WalletResponse;
+import org.promgrammers.springbootbasic.domain.wallet.model.Wallet;
+import org.promgrammers.springbootbasic.domain.wallet.repository.impl.JdbcWalletRepository;
+import org.promgrammers.springbootbasic.exception.BusinessException;
+import org.promgrammers.springbootbasic.exception.repository.NonExistentDomainException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.List;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+@ActiveProfiles("jdbc")
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class WalletServiceTest {
+
+    @MockBean
+    private CommandLineController controller;
+    @Autowired
+    private WalletService walletService;
+    @Autowired
+    private JdbcWalletRepository walletRepository;
+    @Autowired
+    JdbcCustomerRepository customerRepository;
+    @Autowired
+    JdbcVoucherRepository voucherRepository;
+
+    private Wallet wallet;
+
+    private Voucher voucher;
+
+    private Customer customer;
+
+    @BeforeAll
+    void setUp() {
+        walletService = new WalletService(walletRepository);
+    }
+
+    @BeforeEach
+    void init() {
+        customerRepository.deleteAll();
+        voucherRepository.deleteAll();
+        walletRepository.deleteAll();
+        voucher = new FixedAmountVoucher(UUID.randomUUID(), 100);
+        customer = new Customer(UUID.randomUUID(), "HONG");
+        voucherRepository.insert(voucher);
+        customerRepository.save(customer);
+    }
+
+    @Test
+    @DisplayName("지갑 저장 성공")
+    void createWalletSuccessTest() throws Exception {
+
+        //given
+        CreateWalletRequest createWalletRequest = new CreateWalletRequest(voucher, customer);
+
+        //when
+        WalletResponse walletResponse = walletService.create(createWalletRequest);
+
+        //then
+        assertNotNull(walletResponse.walletId());
+        assertThat(walletResponse.customer().getCustomerId()).isEqualTo(customer.getCustomerId());
+        assertThat(walletResponse.voucher().getVoucherId()).isEqualTo(voucher.getVoucherId());
+    }
+
+    @Test
+    @DisplayName("전체 지갑 조회 성공")
+    void findAllWalletsSuccessTest() {
+
+        //given
+        List<CreateWalletRequest> walletRequests = List.of(
+                new CreateWalletRequest(voucher, customer),
+                new CreateWalletRequest(voucher, customer)
+        );
+        walletRequests.forEach(walletService::create);
+
+
+        //when
+        WalletListResponse walletListResponse = walletService.findAll();
+
+        //then
+        assertNotNull(walletListResponse);
+        assertThat(walletListResponse.walletList().size()).isEqualTo(2);
+        List<WalletResponse> walletResponses = walletListResponse.walletList();
+
+        for (int i = 0; i < walletRequests.size(); i++) {
+            CreateWalletRequest walletRequest = walletRequests.get(i);
+            WalletResponse walletResponse = walletResponses.get(i);
+
+            assertThat(walletRequest.voucher()).isEqualTo(walletResponse.voucher());
+            assertThat(walletRequest.customer()).isEqualTo(walletResponse.customer());
+        }
+    }
+
+    @Test
+    @DisplayName("모든 지갑 조회 - 지갑 없음")
+    void testFindAllWallets_NoWalletsExist() {
+
+        //when -> Then
+        assertThrows(BusinessException.class, () -> walletService.findAll());
+    }
+
+    @Test
+    @DisplayName("지갑ID로 조회 성공 - 지갑 존재")
+    void findWalletByIdSuccessTest() throws Exception {
+
+        //given
+        CreateWalletRequest walletRequest = new CreateWalletRequest(voucher, customer);
+        WalletResponse createdWallet = walletService.create(walletRequest);
+
+        //when
+        WalletResponse foundWallet = walletService.findById(createdWallet.walletId());
+
+        //then
+        assertThat(createdWallet.walletId()).isEqualTo(foundWallet.walletId());
+        assertThat(walletRequest.voucher()).isEqualTo(foundWallet.voucher());
+        assertThat(walletRequest.customer()).isEqualTo(foundWallet.customer());
+    }
+
+    @Test
+    @DisplayName("지갑ID로 조회 실패 - 존재하지 않는 지갑")
+    void findWalletByIdFailTest() throws Exception {
+
+        //when -> then
+        assertThrows(BusinessException.class, () -> walletService.findById(UUID.randomUUID()));
+    }
+
+    @Test
+    @DisplayName("고객ID로 지갑 조회 성공 - 존재하는 지갑")
+    void findWalletsByCustomerIdSuccessTest() throws Exception {
+
+        //given
+        List<CreateWalletRequest> walletRequests = List.of(
+                new CreateWalletRequest(voucher, customer),
+                new CreateWalletRequest(voucher, customer)
+        );
+        walletRequests.forEach(walletService::create);
+
+        //when
+        WalletListResponse walletListResponse = walletService.findAllWalletByCustomerId(customer.getCustomerId());
+
+        //then
+        assertNotNull(walletListResponse.walletList());
+        assertThat(walletListResponse.walletList().size()).isEqualTo(2);
+        assertThat(walletListResponse.walletList().get(0).customer()).isEqualTo(customer);
+        assertThat(walletListResponse.walletList().get(1).voucher()).isEqualTo(voucher);
+    }
+
+    @Test
+    @DisplayName("고객ID로 지갑 조회 실패 - 지갑 없음")
+    void findAllWalletsByCustomerIdFailTest() throws Exception {
+        //given
+        UUID customerId = UUID.randomUUID();
+
+        //when -> then
+        assertThrows(BusinessException.class, () -> walletService.findAllWalletByCustomerId(customerId));
+    }
+
+    @Test
+    @DisplayName("바우처ID로 지갑 조회 성공 - 존재하는 지갑")
+    void findWalletsByVoucherIdSuccessTest() throws Exception {
+
+        //given
+        List<CreateWalletRequest> walletRequests = List.of(
+                new CreateWalletRequest(voucher, customer),
+                new CreateWalletRequest(voucher, customer)
+        );
+        walletRequests.forEach(walletService::create);
+
+        //when
+        WalletListResponse walletListResponse = walletService.findAllWalletByVoucherId(voucher.getVoucherId());
+
+        //then
+        assertNotNull(walletListResponse.walletList());
+        assertThat(walletListResponse.walletList().size()).isEqualTo(2);
+        assertThat(walletListResponse.walletList().get(0).customer()).isEqualTo(customer);
+        assertThat(walletListResponse.walletList().get(1).voucher()).isEqualTo(voucher);
+    }
+
+    @Test
+    @DisplayName("바우처ID로 지갑 조회 실패 - 지갑 없음")
+    void findAllWalletsByVoucherIdFailTest() throws Exception {
+        //given
+        UUID voucherId = UUID.randomUUID();
+
+        //when -> then
+        assertThrows(BusinessException.class, () -> walletService.findAllWalletByCustomerId(voucherId));
+    }
+
+    @Test
+    @DisplayName("지갑 전체 삭제 성공")
+    void DeleteAllWalletsSuccessTest() {
+
+        //given
+        List<CreateWalletRequest> walletRequests = List.of(
+                new CreateWalletRequest(voucher, customer),
+                new CreateWalletRequest(voucher, customer)
+        );
+        walletRequests.forEach(walletService::create);
+
+        // when -> then
+        assertDoesNotThrow(() -> walletService.deleteAll());
+    }
+
+    @Test
+    @DisplayName("지갑 ID로 삭제 - 지갑 존재")
+    void testDeleteWalletById_WalletExists() {
+
+        // given
+        CreateWalletRequest walletRequest = new CreateWalletRequest(voucher, customer);
+        WalletResponse createdWallet = walletService.create(walletRequest);
+
+        // when
+        walletService.deleteById(createdWallet.walletId());
+
+        // then
+        assertThrows(BusinessException.class, () -> walletService.findById(createdWallet.walletId()));
+    }
+
+    @Test
+    @DisplayName("지갑 ID로 삭제 - 지갑 존재하지 않음")
+    void testDeleteWalletById_WalletNotExists() {
+
+        // when & then
+        assertThrows(NonExistentDomainException.class, () -> walletService.deleteById(UUID.randomUUID()));
+    }
+}
