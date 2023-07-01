c5c19e7a6d5e6975cce951a48139f22566f23878
byeolhaha <j35635aa@gmail.com>
2023-07-02 00:03:38 +0900

    style : 더 직관적인 메서드명으로 변경

--- src/main/java/com/prgrms/io/Output.java
+++ src/main/java/com/prgrms/io/Output.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Output {
-    public void outputView(String message) {
+    public void write(String message) {
         System.out.println(message);
     }
 }
