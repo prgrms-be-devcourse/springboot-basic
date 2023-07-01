624aaddacfa0a588f9e7c0142b653528d404ae23
byeolhaha <j35635aa@gmail.com>
2023-07-01 23:15:28 +0900

    refactor : Input의 기능이 입력만 담당하도록 Menu, VoucherType, Discount에 대해서 알지 못하도록 변경

--- src/main/java/com/prgrms/io/Input.java
+++ src/main/java/com/prgrms/io/Input.java
@@ -3,25 +3,20 @@ package com.prgrms.io;
 import com.prgrms.model.voucher.VoucherType;
 import org.springframework.stereotype.Component;
 
+import java.util.Date;
 import java.util.Optional;
 import java.util.Scanner;
 
 @Component
 public class Input {
+
     private final Scanner sc = new Scanner(System.in);
 
-    public Optional<Menu> enterMenu() {
-        String selectedMenu = sc.next();
-        return Menu.findByMenu(selectedMenu);
+    public String enterOption() {
+        return sc.next();
     }
 
-    public Optional<VoucherType> enterVoucherPolicy() {
-        String selectedPolicy = sc.next();
-        return VoucherType.findByPolicy(selectedPolicy);
-    }
-
-    public Long enterDiscount() {
-        Long discount = sc.nextLong();
-        return discount;
+    public Double enterDiscount() {
+        return sc.nextDouble();
     }
 }
