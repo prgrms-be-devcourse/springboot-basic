e0ddfd74732e30cf51d0dc692ad7267e4b3555da
sangminnim <sangmins930@gmail.com>
2023-07-02 00:15:25 +0900

    refactor: 예외 메세지 수정 및 상수로 변경

--- src/main/java/com/programmers/springbootbasic/domain/voucher/VoucherType.java
+++ src/main/java/com/programmers/springbootbasic/domain/voucher/VoucherType.java
@@ -6,6 +6,8 @@ public enum VoucherType {
     PERCENT("정률 할인"),
     FIX("정액 할인");
 
+    private static final String INVALID_VOUCHER_TYPE = "잘못된 바우처 유형. 현재 입력 값: ";
+
     private final String voucherType;
 
     VoucherType(String voucherType) {
@@ -16,6 +18,6 @@ public enum VoucherType {
         return Arrays.stream(values())
                 .filter(o -> type.equals(o.voucherType))
                 .findFirst()
-                .orElseThrow(() -> new IllegalArgumentException("잘못된 쿠폰 유형"));
+                .orElseThrow(() -> new IllegalArgumentException(INVALID_VOUCHER_TYPE + type));
     }
 }
