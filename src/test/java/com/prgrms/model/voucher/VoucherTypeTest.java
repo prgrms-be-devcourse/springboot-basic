2e962741266228c1d4c50f1d68af94d1782f4048
byeolhaha <j35635aa@gmail.com>
2023-07-02 01:41:38 +0900

    test : 등록된 바우처 정책이 없는 경우 true를 반환하는지 테스트 코드 추가 및 assertj로 변경

--- src/test/java/com/prgrms/model/voucher/VoucherTypeTest.java
+++ src/test/java/com/prgrms/model/voucher/VoucherTypeTest.java
@@ -12,25 +12,25 @@ class VoucherTypeTest {
     @DisplayName("find 고정된 금액의 정책")
     void findByPolicyFixedVoucherPolicy() {
         String policy = "1";
-        Optional<VoucherType> voucherPolicy = VoucherType.findByPolicy(policy);
+        VoucherType voucherPolicy = VoucherType.findByPolicy(policy);
 
-        assertEquals(VoucherType.FIXED_AMOUNT_VOUCHER, voucherPolicy.get());
+        assertEquals(VoucherType.FIXED_AMOUNT_VOUCHER, voucherPolicy);
     }
 
     @Test
     @DisplayName("find 할인율 정책")
     void findByPolicyPercentVoucherPolicy() {
         String policy = "2";
-        Optional<VoucherType> voucherPolicy = VoucherType.findByPolicy(policy);
+        VoucherType voucherPolicy = VoucherType.findByPolicy(policy);
 
-        assertEquals(VoucherType.PERCENT_DISCOUNT_VOUCHER, voucherPolicy.get());
+        assertEquals(VoucherType.PERCENT_DISCOUNT_VOUCHER, voucherPolicy);
     }
 
     @Test
     @DisplayName("없는 할인 정책에 대한 예외 테스트")
     void findByPolicyNotExistVoucherPolicy() {
         String policy = "3";
-        Optional<VoucherType> voucherPolicy = VoucherType.findByPolicy(policy);
+        VoucherType voucherPolicy = VoucherType.findByPolicy(policy);
 
         assertEquals(Optional.empty(), voucherPolicy);
     }
