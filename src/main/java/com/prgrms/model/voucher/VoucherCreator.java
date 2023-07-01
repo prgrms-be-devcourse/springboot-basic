96c6bc20d0069c6069181fea8728e464182f8240
byeolhaha <j35635aa@gmail.com>
2023-07-01 12:24:49 +0900

    refactor : 초기화된 변수에 재할당하지 않도록 수정 및 voucher id를 테스트할 때 조절할 수 있도록 voucher id를 만드는 메서드 분리

--- src/main/java/com/prgrms/model/voucher/VoucherCreator.java
+++ src/main/java/com/prgrms/model/voucher/VoucherCreator.java
@@ -7,18 +7,17 @@ import java.util.UUID;
 @Component
 public class VoucherCreator {
 
-    private final String errorMessage = "지원되지 않는 바우처 정책입니다.";
-
     public Voucher createVoucher(Discount discount, VoucherType voucherType) {
 
-        Voucher voucher;
-        UUID id = UUID.randomUUID();
+        UUID id = getVoucherID();
 
-        switch (voucherType) {
-            case FIXED_AMOUNT_VOUCHER -> voucher = new FixedAmountVoucher(id, discount, voucherType);
-            case PERCENT_DISCOUNT_VOUCHER -> voucher = new PercentDiscountVoucher(id, discount, voucherType);
-            default -> throw new IllegalArgumentException(errorMessage);
-        }
-        return voucher;
+        return switch (voucherType) {
+            case FIXED_AMOUNT_VOUCHER -> new FixedAmountVoucher(id, discount, voucherType);
+            case PERCENT_DISCOUNT_VOUCHER -> new PercentDiscountVoucher(id, discount, voucherType);
+        };
+    }
+
+    public UUID getVoucherID() {
+        return UUID.randomUUID();
     }
 }
