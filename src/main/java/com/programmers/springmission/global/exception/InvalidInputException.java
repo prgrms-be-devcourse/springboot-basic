9f0191e3a4ab065ef6eee8d95694ce5bccb74527
Dev Uni <wlwhsvkdlxh@gmail.com>
2023-07-01 21:10:23 +0900

    refactor: Exception 네이밍 구체화

--- src/main/java/com/programmers/springmission/global/exception/InvalidInputException.java
+++ src/main/java/com/programmers/springmission/global/exception/InvalidInputException.java
@@ -2,11 +2,8 @@ package com.programmers.springmission.global.exception;
 
 public class VoucherException extends RuntimeException {
 
-    private final ErrorMessage errorMessage;
-
     public VoucherException(ErrorMessage errorMessage) {
         super(errorMessage.getMessage());
-        this.errorMessage = errorMessage;
     }
 }
 
