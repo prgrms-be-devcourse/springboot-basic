5db27213ff2b2326a1f824631d24aec0ee085db1
bombo-dev <bombo96@chungwoon.ac.kr>
2023-07-01 17:24:03 +0900

    refactor : 발생하지 않는 예외 케이스 제거와 역할이 없는 메서드 제거

--- src/main/java/com/prgms/VoucherApp/domain/voucher/PercentDiscountVoucher.java
+++ src/main/java/com/prgms/VoucherApp/domain/voucher/PercentDiscountVoucher.java
@@ -1,17 +1,12 @@
 package com.prgms.VoucherApp.domain.voucher;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.math.BigDecimal;
 import java.math.MathContext;
-import java.math.RoundingMode;
 import java.util.UUID;
 
 public class PercentDiscountVoucher implements Voucher {
 
-    private static final Long DISCOUNT_PERCENTAGE_MAX_VALUE = 100L;
-    private static final Logger log = LoggerFactory.getLogger(PercentDiscountVoucher.class);
+    private static final Long PERCENT_CONVERT_VALUE = 100L;
     private final UUID voucherId;
     private final BigDecimal percent;
     private final VoucherType voucherType;
@@ -24,19 +19,8 @@ public class PercentDiscountVoucher implements Voucher {
 
     @Override
     public BigDecimal discount(BigDecimal beforeAmount) {
-        return calculateAmount(beforeAmount);
-    }
-
-    private BigDecimal calculateAmount(BigDecimal beforeAmount) {
-        BigDecimal discountAmount;
-        try {
-            discountAmount = beforeAmount.multiply(percent)
-                    .divide(BigDecimal.valueOf(DISCOUNT_PERCENTAGE_MAX_VALUE), MathContext.UNLIMITED);
-        } catch (ArithmeticException e) {
-            log.warn("무한 소수 예외가 발생하였습니다. 20번째 자리에서 반올림합니다.");
-            discountAmount = beforeAmount.multiply(percent)
-                    .divide(BigDecimal.valueOf(DISCOUNT_PERCENTAGE_MAX_VALUE), 20, RoundingMode.HALF_UP);
-        }
+        BigDecimal discountAmount = beforeAmount.multiply(percent)
+                .divide(BigDecimal.valueOf(PERCENT_CONVERT_VALUE), MathContext.UNLIMITED);
         return beforeAmount.subtract(discountAmount);
     }
 
--- src/test/java/com/prgms/VoucherApp/domain/voucher/PercentDiscountVoucherTest.java
+++ src/test/java/com/prgms/VoucherApp/domain/voucher/PercentDiscountVoucherTest.java
@@ -1,8 +1,6 @@
 package com.prgms.VoucherApp.domain.voucher;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -16,22 +14,10 @@ class PercentDiscountVoucherTest {
     Voucher percentDiscountVoucher;
 
     @ParameterizedTest
-    @CsvSource(value = {"10:9000", "15:8500", "100:0", "0:10000", "50:5000"}, delimiter = ':')
+    @CsvSource(value = {"10:9000", "15:8500", "100:0", "0:10000", "50:5000", "33.3:6670.0"}, delimiter = ':')
     @DisplayName("비율 값 할인")
     void percentDiscount(BigDecimal percent, BigDecimal result) {
         percentDiscountVoucher = new PercentDiscountVoucher(UUID.randomUUID(), percent, VoucherType.PERCENT_VOUCHER);
         assertThat(percentDiscountVoucher.discount(BigDecimal.valueOf(10000L))).isEqualTo(result);
-    }
-
-    @Test
-    @DisplayName("비율 값 할인 무한 소수 예외 테스트")
-    void percentDiscountException() {
-        percentDiscountVoucher = new PercentDiscountVoucher(UUID.randomUUID(), BigDecimal.valueOf(3.33), VoucherType.PERCENT_VOUCHER);
-        try {
-            percentDiscountVoucher.discount(BigDecimal.valueOf(1));
-        } catch (ArithmeticException e) {
-            Assertions.assertThatExceptionOfType(ArithmeticException.class)
-                    .isThrownBy(() -> percentDiscountVoucher.discount(BigDecimal.valueOf(1)));
-        }
     }
 }
