841864b61f18e37964b4f4b6a52755ed63000f53
jay-so <merry9504@naver.com>
2023-07-02 00:03:29 +0900

    refactor: FixedDiscountVoucher클래스 Lombok 리팩토링(@RequiredArgsConstructor 제거), 내부에서 UUID 생성되도록 변경

--- src/main/java/com/prgrms/springbootbasic/domain/FixedDiscountVoucher.java
+++ src/main/java/com/prgrms/springbootbasic/domain/FixedDiscountVoucher.java
@@ -3,14 +3,17 @@ package com.prgrms.springbootbasic.domain;
 import com.prgrms.springbootbasic.enums.VoucherType;
 import java.util.UUID;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 @Getter
-@RequiredArgsConstructor
 public class FixedDiscountVoucher implements Voucher {
 
     private final UUID voucherId;
     private final long fixedDiscount;
+
+    public FixedDiscountVoucher(long fixedDiscount) {
+        this.voucherId = UUID.randomUUID();
+        this.fixedDiscount = fixedDiscount;
+    }
 
     @Override
     public long getDiscount() {
