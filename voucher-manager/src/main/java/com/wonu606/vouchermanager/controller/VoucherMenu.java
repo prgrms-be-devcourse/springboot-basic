4ef7f881ce34decf8abce3422c41e12fc993842e
wonu606 <wonu606@gmail.com>
2023-07-03 02:02:19 +0900

    refactor: VoucherMenu에서 getVoucherTypeByName() 예외 처리 추가

--- voucher-manager/src/main/java/com/wonu606/vouchermanager/controller/VoucherMenu.java
+++ voucher-manager/src/main/java/com/wonu606/vouchermanager/controller/VoucherMenu.java
@@ -2,7 +2,7 @@ package com.wonu606.vouchermanager.controller;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 public enum VoucherMenu {
@@ -11,12 +11,11 @@ public enum VoucherMenu {
     CREATE,
     LIST;
 
-    public static String menuDescription;
-
-    public static Optional<VoucherMenu> getVoucherTypeByName(String name) {
-        return Arrays.stream(VoucherMenu.values())
-                .filter(v -> v.name().equals(name.toUpperCase()))
-                .findFirst();
+    public static VoucherMenu getVoucherTypeByName(String name) {
+        return Arrays.stream(values())
+                .filter(v -> Objects.equals(v.name(), name.toUpperCase()))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 메뉴입니다: " + name));
     }
 
     public static List<String> getAllNames() {
