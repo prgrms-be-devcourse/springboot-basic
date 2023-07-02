53bc1144de8b4db6b83afa14381d81e01614f5fc
Juwoong <wwong7247@gmail.com>
2023-07-02 18:33:14 +0900

    feat : 문자열 상수를 ConstantStrings에서 한번에 관리

--- voucher/src/main/java/com/example/voucher/io/Console.java
+++ voucher/src/main/java/com/example/voucher/io/Console.java
@@ -1,7 +1,9 @@
 package com.example.voucher.io;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import com.example.voucher.constant.ConstantStrings;
 import com.example.voucher.domain.dto.VoucherDTO;
 import com.example.voucher.domain.enums.VoucherType;
 
@@ -9,42 +11,24 @@ public class Console {
 
 	private static Scanner scanner = new Scanner(System.in);
 
-	private static final String APPLICATION_MODE_TYPE_MESSAGE = """
-		=== Voucher Program ===
-		Type exit to exit the program.
-		Type create to create a new voucher.
-		Type list to list all vouchers.
-		""";
-	private static final String VOUCHER_CREATE_TYPE_MESSAGE = """
-		Input Number for select VoucherType
-		1. FixedAmount
-		2. PercentDiscount
-    	""";
-	private static final String DISCOUNT_PRICE_MESSAGE = """
- 		input price for discount
- 		""";
-	private static final String DISCOUNT_PERCENT_MESSAGE = """
-		input percent for discount
-		""";
-
 	private Console() {
 
 	}
 
 	public static void printModeType() {
-		System.out.println(APPLICATION_MODE_TYPE_MESSAGE);
+		System.out.println(ConstantStrings.MESSAGE_PRINT_MODE_SELECTION);
 	}
 
 	public static void printVoucherType() {
-		System.out.println(VOUCHER_CREATE_TYPE_MESSAGE);
+		System.out.println(ConstantStrings.MESSAGE_PRINT_TYPE_SELECTION);
 	}
 
 	public static void printDiscountAmount() {
-		System.out.println(DISCOUNT_PRICE_MESSAGE);
+		System.out.println(ConstantStrings.MESSAGE_PRINT_DISCOUNT_PRICE);
 	}
 
 	public static void printDiscountPercent() {
-		System.out.println(DISCOUNT_PERCENT_MESSAGE);
+		System.out.println(ConstantStrings.MESSAGE_PRINT_DISCOUNT_PERCENT);
 	}
 
 	public static void printVoucherInfo(VoucherDTO voucherDTO) {
@@ -52,9 +36,11 @@ public class Console {
 
 		switch (voucherType) {
 			case FixedAmountDiscount -> System.out.println(
-				String.format("VoucherType : %s, discountAmount : %d", voucherDTO.voucherType(), voucherDTO.value()));
+				String.format(ConstantStrings.FORMAT_PRINT_FIXED_AMOUNT_VOUCHER_INFO, voucherDTO.voucherType(),
+					voucherDTO.value()));
 			case PercentDiscount -> System.out.println(
-				String.format("VoucherType : %s, discountPercent : %d", voucherDTO.voucherType(), voucherDTO.value()));
+				String.format(ConstantStrings.FORMAT_PRINT_PERCENT_VOUCHER_INFO, voucherDTO.voucherType(),
+					voucherDTO.value()));
 		}
 	}
 
@@ -67,17 +53,25 @@ public class Console {
 	}
 
 	public static Integer readVoucherType() {
-		Integer type = scanner.nextInt();
-		scanner.nextLine();
-
-		return type;
+		try {
+			Integer type = scanner.nextInt();
+			return type;
+		} catch (InputMismatchException e) {
+			throw new InputMismatchException(ConstantStrings.MESSAGE_ERROR_INPUT_NUMBER);
+		} finally {
+			scanner.nextLine();
+		}
 	}
 
 	public static long readDiscount() {
-		Long discount = scanner.nextLong();
-		scanner.nextLine();
-
-		return discount;
+		try {
+			Long discount = scanner.nextLong();
+			return discount;
+		} catch (InputMismatchException e) {
+			throw new InputMismatchException(ConstantStrings.MESSAGE_ERROR_INPUT_NUMBER);
+		} finally {
+			scanner.nextLine();
+		}
 	}
 
 }
--- voucher/src/main/java/com/example/voucher/utils/validator/VoucherValidator.java
+++ voucher/src/main/java/com/example/voucher/utils/validator/VoucherValidator.java
@@ -1,11 +1,8 @@
 package com.example.voucher.utils.validator;
 
-public class VoucherValidator {
+import com.example.voucher.constant.ConstantStrings;
 
-	private static final String RANGE_CONSTRAINT = "패센트 값은 0과 100 사이여야 합니다";
-	private static final String NON_ZERO_CONSTRAINT = "값은 0이 아니여야 합니다";
-	private static final String POSITIVE_CONSTRAINT = "값은 양수여야 합니다";
-	private static final String GREATER_THAN_CONSTRAINT = "값은 threshold 보다 커야합니다.";
+public class VoucherValidator {
 
 	private VoucherValidator() {
 
@@ -13,25 +10,26 @@ public class VoucherValidator {
 
 	public static void validatePercent(long percent) {
 		if (percent < 0 || percent > 100) {
-			throw new IllegalArgumentException(RANGE_CONSTRAINT);
+			throw new IllegalArgumentException(ConstantStrings.MESSAGE_ERROR_RANGE_CONSTRAINT);
 		}
 	}
 
 	public static void validateNonZero(long value) {
 		if (value == 0) {
-			throw new IllegalArgumentException(NON_ZERO_CONSTRAINT);
+			throw new IllegalArgumentException(ConstantStrings.MESSAGE_ERROR_NON_ZERO_CONSTRAINT);
 		}
 	}
 
 	public static void validatePositive(long value) {
 		if (value <= 0) {
-			throw new IllegalArgumentException(POSITIVE_CONSTRAINT);
+			throw new IllegalArgumentException(ConstantStrings.MESSAGE_ERROR_POSITIVE_CONSTRAINT);
 		}
 	}
 
 	public static void validateGreaterThan(long value, long threshold) {
 		if (value <= threshold) {
-			throw new IllegalArgumentException(String.format("{} {}", GREATER_THAN_CONSTRAINT, threshold));
+			throw new IllegalArgumentException(
+				String.format("{} {}", ConstantStrings.FORMAT_ERROR_GREATER_THAN_CONSTRAINT, threshold));
 		}
 	}
 
