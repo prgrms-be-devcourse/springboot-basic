1b69e402f4adc797f394696fb79706af19426515
2jie0516 <2jie0516>
2023-07-02 22:13:40 +0900

    refactor : input 메소드 통일

--- src/main/java/prgms/spring_week1/io/Input.java
+++ src/main/java/prgms/spring_week1/io/Input.java
@@ -11,20 +11,7 @@ public class Input {
     private static final Logger log = LoggerFactory.getLogger(Input.class);
     Scanner sc = new Scanner(System.in);
 
-    public String inputTextOption() {
+    public String input() {
         return sc.nextLine();
     }
-
-    public String inputVoucherType() {
-        return sc.nextLine();
-    }
-
-    public Long insertDiscountAmountVoucher() {
-        return sc.nextLong();
-    }
-
-    public int insertDiscountPercentVoucher() {
-        return sc.nextInt();
-    }
-
 }
