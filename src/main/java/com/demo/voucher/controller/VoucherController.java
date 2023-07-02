2ae935db52b5d6906494fd8ca13061590699e22b
Yebin Lee <lyb2325@gmail.com>
2023-07-02 23:05:56 +0900

    fix: VoucherController는 controller 역할을 하도록 변경

--- src/main/java/com/demo/voucher/controller/VoucherController.java
+++ src/main/java/com/demo/voucher/controller/VoucherController.java
@@ -1,55 +1,24 @@
 package com.demo.voucher.controller;
 
+import com.demo.voucher.domain.Voucher;
 import com.demo.voucher.domain.VoucherType;
-import com.demo.voucher.io.CommandType;
-import com.demo.voucher.io.ConsoleView;
 import com.demo.voucher.service.VoucherService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 
+import java.util.List;
+
 @Controller
-public class VoucherController implements Runnable {
-    private final ConsoleView consoleView;
+@RequiredArgsConstructor
+public class VoucherController {
     private final VoucherService voucherService;
 
-    public VoucherController(ConsoleView consoleView, VoucherService voucherService) {
-        this.consoleView = consoleView;
-        this.voucherService = voucherService;
+    public Voucher createVoucher(VoucherType voucherType, String amount) {
+        return voucherService.createVoucher(voucherType, amount);
     }
 
-    @Override
-    public void run() {
-        consoleView.showMenu();
-
-        boolean isProgramRunnable = true;
-        while (isProgramRunnable) {
-            String inputCommand = consoleView.requestMenu();
-            if (!CommandType.isValidCommandInput(inputCommand)) {
-                consoleView.showCommandError();
-                continue;
-            }
-
-            CommandType commandType = CommandType.getCommandType(inputCommand);
-            switch (commandType) {
-                case EXIT -> isProgramRunnable = false;
-                case CREATE -> {
-                    consoleView.showVoucherType();
-
-                    String inputVoucherType = consoleView.requestVoucherType();
-                    if (!VoucherType.isValidVoucherTypeInput(inputVoucherType)) {
-                        consoleView.showVoucherTypeError();
-                        continue;
-                    }
-
-                    VoucherType voucherType = VoucherType.getVoucherTypeByCommand(inputVoucherType);
-                    String inputAmount = consoleView.getAmount(voucherType);
-                    if (!voucherType.validateAmount(inputAmount)) {
-                        consoleView.showAmountError();
-                        continue;
-                    }
-                    voucherService.createVoucher(voucherType, inputAmount);
-                }
-                case LIST -> consoleView.showAllVouchers(voucherService.findAllVouchers());
-            }
-        }
+    public List<Voucher> getAllVouchers() {
+        return voucherService.findAllVouchers();
     }
+
 }
