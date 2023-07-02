20939f637e34a94e41d71a03db5cce51038599c2
sujjangOvO <pnk7038@gmail.com>
2023-07-03 04:08:47 +0900

    refactor : PercentDiscountVoucher 상수 네임 변경

--- src/main/java/com/prgms/voucher/voucherproject/domain/PercentDiscountVoucher.java
+++ src/main/java/com/prgms/voucher/voucherproject/domain/PercentDiscountVoucher.java
@@ -3,14 +3,14 @@ package com.prgms.voucher.voucherproject.domain;
 import java.util.UUID;
 
 public class PercentDiscountVoucher implements Voucher {
-    private final static long MIN_AMOUNT = 1;
-    private final static long MAX_AMOUNT = 99;
+    private final static long MIN_PERCENT = 1;
+    private final static long MAX_PERCENT = 99;
     private final UUID voucherId;
     private final long percent;
 
 
     public PercentDiscountVoucher(UUID voucherId, long percent) {
-        if (percent < MIN_AMOUNT || percent > MAX_AMOUNT) {
+        if (percent < MIN_PERCENT || percent > MAX_PERCENT) {
             throw new IllegalArgumentException("잘못된 퍼센트 할인 금액입니다.");
         }
         this.voucherId = voucherId;
