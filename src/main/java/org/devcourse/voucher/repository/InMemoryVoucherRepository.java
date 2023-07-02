6e81329876ab3ecaf42170d3ced55ef7fa2613bf
JHS <wnghdtjr129@naver.com>
2023-07-02 22:11:36 +0900

    feat: voucherStorage 자료구조 변경

--- src/main/java/org/devcourse/voucher/repository/InMemoryVoucherRepository.java
+++ src/main/java/org/devcourse/voucher/repository/InMemoryVoucherRepository.java
@@ -3,14 +3,14 @@ package org.devcourse.voucher.repository;
 import org.devcourse.voucher.domain.voucher.Voucher;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicLong;
 
 public class InMemoryVoucherRepository implements VoucherRepository {
 
-    private final Map<Long, Voucher> voucherStorage = new HashMap<>();
+    private final Map<Long, Voucher> voucherStorage = new ConcurrentHashMap<>();
     private final AtomicLong idGenerator = new AtomicLong(1);
 
     @Override
