c61b597a0235a2968d706674465a57498c8733c6
Juwoong <wwong7247@gmail.com>
2023-07-02 23:26:53 +0900

    refactor : 스프링 빈 애너테이션 기반 설정 추가 작업
    * 주석 처리한 설정 파일 삭제
    * @ComponentScan 애너테이션 삭제 -> @SpringBootApplication이 해당 기능을 포함함

--- voucher/src/main/java/com/example/voucher/VoucherApplication.java
+++ voucher/src/main/java/com/example/voucher/VoucherApplication.java
@@ -2,10 +2,8 @@ package com.example.voucher;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "com.example")
 public class VoucherApplication {
 
 	public static void main(String[] args) {
