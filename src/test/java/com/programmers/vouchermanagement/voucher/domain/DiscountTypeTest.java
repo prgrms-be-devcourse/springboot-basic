32005470db422f582c5c2208056be677f90cde94
Yiseull <omjl5123@catholic.ac.kr>
2023-07-02 19:24:42 +0900

    test: DiscountType 테스트 추가

---
+++ src/test/java/com/programmers/vouchermanagement/voucher/domain/DiscountTypeTest.java
@@ -0,0 +1,56 @@
+package com.programmers.vouchermanagement.voucher.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class DiscountTypeTest {
+
+    @Nested
+    @DisplayName("할인 유형에 적합한 DiscountType 을 반환한다.")
+    class returnAppropriateDiscountType {
+
+        @Test
+        @DisplayName("fix 가 들어온 경우")
+        void WhenFixComeInto() {
+            // when
+            String name = "fix";
+
+            // given
+            DiscountType result = DiscountType.from(name);
+
+            // then
+            assertThat(result).isEqualTo(DiscountType.FIX);
+        }
+
+        @Test
+        @DisplayName("percent 가 들어온 경우")
+        void WhenPercentComeInto() {
+            // when
+            String name = "percent";
+
+            // given
+            DiscountType result = DiscountType.from(name);
+
+            // then
+            assertThat(result).isEqualTo(DiscountType.PERCENT);
+        }
+    }
+
+    @Test
+    @DisplayName("존재하지 않는 할인 유형이 들어오면 에외가 발생한다.")
+    void occurExceptionWhenDiscountTypeDoesNotExist() {
+        // when
+        String name = "fixx";
+
+        // given & then
+        assertThatThrownBy(() -> {
+            DiscountType.from(name);
+        })
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("This discount type does not exist.");
+    }
+}
