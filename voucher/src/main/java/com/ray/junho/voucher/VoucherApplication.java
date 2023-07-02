6c4d8d716a3790b213410adea2bac0cc3cdd162b
juno-junho <ssmm0205@naver.com>
2023-07-02 19:35:27 +0900

    feat: 게임을 실행하는 컨트롤러 run 메서드 생성

--- voucher/src/main/java/com/ray/junho/voucher/VoucherApplication.java
+++ voucher/src/main/java/com/ray/junho/voucher/VoucherApplication.java
@@ -1,5 +1,6 @@
 package com.ray.junho.voucher;
 
+import com.ray.junho.voucher.controller.CommandLineController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class VoucherApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(VoucherApplication.class, args);
+        SpringApplication
+                .run(VoucherApplication.class, args)
+                .getBean(CommandLineController.class)
+                .run();
     }
-
 }
