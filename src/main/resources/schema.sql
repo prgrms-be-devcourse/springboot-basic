7b561d35020f8caf525f74c65fa851f649f68384
HyuckJuneHong <jjj8337721@naver.com>
2023-07-02 10:25:40 +0900

    refactor : 바우처와 회원 간 다대다 설계 (매핑 테이블)

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,5 +1,6 @@
 drop table if exists tbl_vouchers;
 drop table if exists tbl_members;
+drop table if exists tbl_vouchers_members;
 
 create table tbl_members(
     member_id     varchar(50) not null unique,
@@ -13,8 +14,19 @@ create table tbl_vouchers
     voucher_id     varchar(50) not null unique,
     voucher_amount int         not null,
     voucher_policy varchar(50) not null,
-    member_id      varchar(50),
 
-    primary key (voucher_id),
-    foreign key (member_id) references tbl_members (member_id)
+    primary key (voucher_id)
 );
+
+create table tbl_vouchers_members(
+    voucher_member_id varchar(50) not null unique,
+    voucher_id varchar(50) not null ,
+    member_id varchar(50) not null ,
+
+    foreign key (voucher_id)
+        references tbl_vouchers (voucher_id)
+        on delete cascade on update cascade ,
+    foreign key (member_id)
+        references tbl_members (member_id)
+        on delete cascade on update cascade
+)
