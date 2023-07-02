0a195673d547f18895f434109d178bc542e91087
Juwoong <wwong7247@gmail.com>
2023-07-03 02:00:19 +0900

    refactor :  enum 불필요한 멤버 삭제
    * enum에서 기본 제공하는 메서드를 통해  멤버의 기능을 수행함으로 삭제함

--- voucher/src/main/java/com/example/voucher/constant/ModeType.java
+++ voucher/src/main/java/com/example/voucher/constant/ModeType.java
@@ -5,25 +5,17 @@ import java.util.NoSuchElementException;
 
 public enum ModeType {
 
-	Exit("exit"),
-	Create("create"),
-	List("list");
-
-	private String typeName;
-
-	ModeType(String typeName) {
-		this.typeName = typeName;
-	}
-
-	public String getTypeName() {
-		return typeName;
-	}
+	Exit,
+	Create,
+	List;
 
 	public static ModeType getTypeMode(String typeName) {
 		return Arrays.stream(ModeType.values())
-					.filter(e->typeName.equals(e.getTypeName()))
-					.findAny()
-					.orElseThrow(() -> new NoSuchElementException(ConstantStrings.MESSAGE_PRINT_RETRY_VOUCHER_TYPE_SELECTION_PROMPT));
+			.filter(e -> typeName.equals(e.name().toLowerCase()))
+			.findAny()
+			.orElseThrow(
+				() -> new NoSuchElementException(ConstantStrings.MESSAGE_PRINT_RETRY_VOUCHER_TYPE_SELECTION_PROMPT));
 	}
 
 }
+
--- voucher/src/main/java/com/example/voucher/constant/VoucherType.java
+++ voucher/src/main/java/com/example/voucher/constant/VoucherType.java
@@ -5,30 +5,25 @@ import java.util.NoSuchElementException;
 
 public enum VoucherType {
 
-	FixedAmountDiscount("FixedAmount", 1),
-	PercentDiscount("PercentDiscount", 2);
+	FixedAmountDiscount(1),
+	PercentDiscount(2);
 
-	private final String typeName;
 	private final Integer inputNum;
 
-	VoucherType(String typeName, Integer inputNum) {
-		this.typeName = typeName;
+	VoucherType(Integer inputNum) {
 		this.inputNum = inputNum;
 	}
 
-	public String getTypeName() {
-		return typeName;
-	}
-
-	public Integer getInputNum(){
+	public Integer getInputNum() {
 		return inputNum;
 	}
 
 	public static VoucherType getVouchersType(Integer readVoucherType) {
 		return Arrays.stream(VoucherType.values())
-				.filter( e -> readVoucherType == e.getInputNum())
-				.findAny()
-				.orElseThrow(() -> new NoSuchElementException(ConstantStrings.MESSAGE_PRINT_RETRY_VOUCHER_TYPE_SELECTION_PROMPT));
+			.filter(e -> readVoucherType == e.getInputNum())
+			.findAny()
+			.orElseThrow(
+				() -> new NoSuchElementException(ConstantStrings.MESSAGE_PRINT_RETRY_VOUCHER_TYPE_SELECTION_PROMPT));
 	}
 
 }
