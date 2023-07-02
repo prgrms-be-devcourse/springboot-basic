c2fbfd871edb13a17a2704bc21b9f53896d895c7
2jie0516 <2jie0516>
2023-07-02 22:49:57 +0900

    refactor : output 클래서에서 출력 메소드 통일

--- src/main/java/prgms/spring_week1/io/Output.java
+++ src/main/java/prgms/spring_week1/io/Output.java
@@ -5,39 +5,21 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import prgms.spring_week1.domain.customer.model.BlackConsumer;
 import prgms.spring_week1.domain.voucher.model.Voucher;
+import prgms.spring_week1.io.message.ConsoleOutputMessage;
 
 import java.util.List;
 
 @Component
 public class Output {
     private static final Logger log = LoggerFactory.getLogger(Output.class);
-    private final String printMenuListMessage =
-            """
-                    === Voucher Program ===
-                    Type exit to exit the program.
-                    Type create to create a new voucher.
-                    Type list to list all vouchers.
-                    Type black to list all blackList
-                    """;
 
-    private final String printTypeSelectMessage =
-            """
-                    === Voucher Select ===
-                    Fixed Amount Voucher 을 생성하려면 띄어쓰기 없이 입력하세요. -> Fixed       
-                    Percent Discount Voucher 을 생성하려면 띄어쓰기 없이 입력하세요. -> Percent
-                    """;
-
-    public void printMenuList() {
-        System.out.println(printMenuListMessage);
-    }
-
-    public void printTypeSelectMessage() {
-        System.out.println(printTypeSelectMessage);
+    public void output(String outputMessage){
+        System.out.println(outputMessage);
     }
 
     public void printAllVoucher(List<Voucher> voucherList) {
         if (voucherList.isEmpty()) {
-            printEmptyListMessage();
+            output(ConsoleOutputMessage.NO_VOUCHER_LIST_MESSAGE);
             return;
         }
 
@@ -49,36 +31,12 @@ public class Output {
         }
     }
 
-    public void printEmptyListMessage() {
-        System.out.println("조회된 바우처 리스트가 없습니다.");
-    }
-
-    public void printWrongMenuMessage() {
-        System.out.println("해당 메뉴가 존재하지 않습니다.");
-    }
-
-    public void printInsertFixedVoucherMessage() {
-        System.out.print("할인 가격을 입력하세요 : ");
-    }
-
-    public void printInsertPercentVoucherMessage() {
-        System.out.print("할인율을 입력하세요 : ");
-    }
-
-    public void printInsertVoucherInfo(Voucher voucher) {
-        System.out.println("상품권 등록이 완료되었습니다.");
-    }
-
     public void printBlackConsumerList(List<BlackConsumer> blackConsumerList) {
         if (blackConsumerList.isEmpty()) {
-            printEmptyBlackListMessage();
+            output(ConsoleOutputMessage.EMPTY_BLACK_LIST_MESSAGE);
             return;
         }
         blackConsumerList.forEach(bl -> System.out.println(bl.getName() + " " + bl.getAge()));
-    }
-
-    public void printNoSuchVoucherType() {
-        System.out.println("해당 바우처 타입이 존재하지 않습니다.");
     }
 
     public void printDiscountFixedVoucherInfo(Voucher fixedAmountVoucher) {
@@ -89,9 +47,5 @@ public class Output {
     public void printDiscountAmountVoucherInfo(Voucher percentDiscountVoucher) {
         System.out.println("상품권 종류 : 고정 가격 할인 상품권 " +
                 "할인률 :" + percentDiscountVoucher.getDiscount() + " 퍼센트");
-    }
-
-    public void printEmptyBlackListMessage() {
-        System.out.println("블랙리스트 목록이 없습니다.");
     }
 }
