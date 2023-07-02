1b7c236431b0a175aa7bbb40c3836ef018e9eed9
beombu <beombu13@gmail.com>
2023-07-02 23:55:02 +0900

    Refactor : View layer 범위 검증 로직 삭제
    - View와 Domain에서 중복 예외 발생 시 Domain에서 다른 리소스를 사용해 해당 값을 변경하는지 확인하고 사용할 것!

--- src/main/java/org/prgrms/kdtspringdemo/view/console/VoucherConsole.java
+++ src/main/java/org/prgrms/kdtspringdemo/view/console/VoucherConsole.java
@@ -16,13 +16,8 @@ public class VoucherConsole {
     private static final String INVALID_COMMAND_MESSAGE = "잘못된 명령입니다.\n";
     private static final String CHOICE_VOUCHER_TYPE_MESSAGE = "바우처 타입을 입력하세요.(ex : FIXED or PERCENT)\n";
     private static final String FIXED_AMOUNT_VOUCHER_MESSAGE = "고정 할인 금액을 입력하세요.\n";
-    private static final long FIXED_MIN_AMOUNT = 0L;
-    private static final long FIXED_MAX_AMOUNT = 100000L;
     private static final String IS_NUMERIC_EXCEPTION_MESSAGE = "바우처의 할인 정책은 숫자를 입력해주세요.\n";
-    private static final String OUT_OF_AMOUNT_RANGE_MESSAGE = "범위를 초과 하였습니다.\n";
     private static final String PERCENT_AMOUNT_VOUCHER_MESSAGE = "퍼센트 할인 금액을 입력하세요.\n";
-    private static final long PERCENT_MIN_AMOUNT = 0L;
-    private static final long PERCENT_MAX_AMOUNT = 100L;
     private static final String SUCCESS_CREATED_FIXED_VOUCHER = """
             type : %s
             discount : %s 원
@@ -54,51 +49,38 @@ public class VoucherConsole {
 
     public String chooseVoucherType() {
         output.write(CHOICE_VOUCHER_TYPE_MESSAGE);
+
         return input.read();
     }
 
     public Long inputAmountByVoucher(VoucherType voucherType) {
         long amount = 0L;
-        if (voucherType == VoucherType.FIXED) {
-            output.write(FIXED_AMOUNT_VOUCHER_MESSAGE);
-            String inputAmount = input.read();
-            validateAmount(inputAmount, FIXED_MIN_AMOUNT, FIXED_MAX_AMOUNT);
-            amount = Long.parseLong(inputAmount);
-        }
-
-        if (voucherType == VoucherType.PERCENT) {
-            output.write(PERCENT_AMOUNT_VOUCHER_MESSAGE);
-            String inputAmount = input.read();
-            validateAmount(inputAmount, PERCENT_MIN_AMOUNT, PERCENT_MAX_AMOUNT);
-            amount = Long.parseLong(inputAmount);
+        switch (voucherType) {
+            case FIXED -> {
+                amount = inputAmountEachVoucher(FIXED_AMOUNT_VOUCHER_MESSAGE);
+            }
+            case PERCENT -> {
+                amount = inputAmountEachVoucher(PERCENT_AMOUNT_VOUCHER_MESSAGE);
+            }
         }
 
         return amount;
     }
 
-    private void validateAmount(String input, final long minAmount, final long maxAmount) {
-        if (!isNumeric(input)) {
-            return;
-        }
+    private long inputAmountEachVoucher(final String message) {
+        output.write(message);
+        String inputAmount = input.read();
+        validateAmountIsNumeric(inputAmount);
 
-        long parsedInput = Long.parseLong(input);
-        if (checkAmountRange(parsedInput, minAmount, maxAmount)) {
-            throw new IllegalArgumentException(OUT_OF_AMOUNT_RANGE_MESSAGE);
-        }
+        return Long.parseLong(inputAmount);
     }
 
-    private boolean isNumeric(String amount) {
+    private void validateAmountIsNumeric(String amount) {
         try {
             Long.parseLong(amount);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(IS_NUMERIC_EXCEPTION_MESSAGE);
         }
-
-        return true;
-    }
-
-    private boolean checkAmountRange(long input, final long minAmount, final long maxAmount) {
-        return input <= minAmount && input > maxAmount;
     }
 
     public void printCreatedVoucher(VoucherDto voucher) {
