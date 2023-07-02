15e76dd256b6f85e13bd4aa2671ac292791ee6e6
beombu <beombu13@gmail.com>
2023-07-03 01:37:15 +0900

    Feat : 바우처 저장 실패 테스트 추가

--- src/test/java/org/prgrms/kdtspringdemo/voucher/ropository/VoucherRepositoryTest.java
+++ src/test/java/org/prgrms/kdtspringdemo/voucher/ropository/VoucherRepositoryTest.java
@@ -1,19 +1,22 @@
 package org.prgrms.kdtspringdemo.voucher.ropository;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.prgrms.kdtspringdemo.voucher.constant.VoucherType;
 import org.prgrms.kdtspringdemo.voucher.model.entity.FixedAmountVoucher;
 import org.prgrms.kdtspringdemo.voucher.model.entity.Voucher;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
 import java.util.UUID;
 
-@SpringBootTest
 public class VoucherRepositoryTest {
-    @Autowired
     VoucherRepository voucherRepository;
+
+    @BeforeEach
+    void beforeEach() {
+        voucherRepository = new MemoryVoucherRepository();
+    }
 
     @Test
     void 바우처_저장_테스트() {
@@ -27,5 +30,22 @@ public class VoucherRepositoryTest {
 
         //then
         Assertions.assertThat(savedVoucher.toString()).isEqualTo(findFixed.toString());
+    }
+
+    @Test
+    void 바우처_저장_실패_테스트() {
+        //given
+        long amount1 = 1000;
+        long amount2 = 2000;
+        FixedAmountVoucher fixedAmountVoucher1 = new FixedAmountVoucher(new UUID(10, 10), VoucherType.FIXED, amount1);
+        FixedAmountVoucher fixedAmountVoucher2 = new FixedAmountVoucher(new UUID(10, 10), VoucherType.FIXED, amount2);
+
+        //when
+        voucherRepository.save(fixedAmountVoucher1);
+        voucherRepository.save(fixedAmountVoucher2);
+        List<Voucher> voucherList = voucherRepository.findAll();
+
+        //then
+        Assertions.assertThat(voucherList).hasSize(1);
     }
 }
