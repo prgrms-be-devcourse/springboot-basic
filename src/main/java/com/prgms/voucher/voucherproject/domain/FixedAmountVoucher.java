6fdaf74d6ad6931e06a453c9114278f542522326
sujjangOvO <pnk7038@gmail.com>
2023-07-03 04:06:39 +0900

    refactor : Voucher 구현체 매직넘버 제거

--- src/main/java/com/prgms/voucher/voucherproject/domain/FixedAmountVoucher.java
+++ src/main/java/com/prgms/voucher/voucherproject/domain/FixedAmountVoucher.java
@@ -3,11 +3,12 @@ package com.prgms.voucher.voucherproject.domain;
 import java.util.UUID;
 
 public class FixedAmountVoucher implements Voucher {
+    private final static long MIN_AMOUNT = 0;
     private final UUID voucherId;
     private final long amount;
 
     public FixedAmountVoucher(UUID voucherId, long amount) {
-        if(amount <= 0){
+        if (amount <= MIN_AMOUNT) {
             throw new IllegalArgumentException("잘못된 고정 할인 금액입니다.");
         }
         this.amount = amount;
--- src/main/java/com/prgms/voucher/voucherproject/domain/PercentDiscountVoucher.java
+++ src/main/java/com/prgms/voucher/voucherproject/domain/PercentDiscountVoucher.java
@@ -1,15 +1,16 @@
 package com.prgms.voucher.voucherproject.domain;
 
-
 import java.util.UUID;
 
 public class PercentDiscountVoucher implements Voucher {
+    private final static long MIN_AMOUNT = 1;
+    private final static long MAX_AMOUNT = 99;
     private final UUID voucherId;
     private final long percent;
 
 
     public PercentDiscountVoucher(UUID voucherId, long percent) {
-        if(percent < 1 || percent > 99){
+        if (percent < MIN_AMOUNT || percent > MAX_AMOUNT) {
             throw new IllegalArgumentException("잘못된 퍼센트 할인 금액입니다.");
         }
         this.voucherId = voucherId;
