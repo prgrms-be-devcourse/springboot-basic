43680767d3eb751ea4f5d95ebf91dc8fc9e46714
kimihiqq <kimihiqq@gmail.com>
2023-07-03 17:00:01 +0900

    feat: CustomerRepository 수정 및 JDBCCustomerRepository 생성

--- src/main/java/me/kimihiqq/vouchermanagement/domain/customer/repository/CustomerRepository.java
+++ src/main/java/me/kimihiqq/vouchermanagement/domain/customer/repository/CustomerRepository.java
@@ -3,11 +3,20 @@ package me.kimihiqq.vouchermanagement.domain.customer.repository;
 import me.kimihiqq.vouchermanagement.domain.customer.Customer;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 public interface CustomerRepository {
 
-    Customer findCustomer(String customerId);
+    Customer save(Customer customer);
 
-    List<Customer> findBlacklistedCustomers();
+    Optional<Customer> findById(UUID customerId);
+
+    List<Customer> findAll();
+
+    void deleteById(UUID customerId);
+
+    void update(Customer customer);
+
 
 }
