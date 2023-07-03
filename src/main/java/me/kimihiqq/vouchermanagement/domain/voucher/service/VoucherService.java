bfd210a821b9af4f20941027d4e6b2ab6415c883
kimihiqq <kimihiqq@gmail.com>
2023-07-03 17:08:03 +0900

    feat: VoucherService에 findVoucherById 메서드 추가

--- src/main/java/me/kimihiqq/vouchermanagement/domain/voucher/service/VoucherService.java
+++ src/main/java/me/kimihiqq/vouchermanagement/domain/voucher/service/VoucherService.java
@@ -4,8 +4,11 @@ import me.kimihiqq.vouchermanagement.domain.voucher.Voucher;
 import me.kimihiqq.vouchermanagement.domain.voucher.dto.VoucherDto;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 public interface VoucherService {
     Voucher createVoucher(VoucherDto voucherDto);
     List<Voucher> listVouchers();
+    Optional<Voucher> findVoucherById(UUID voucherId); // 추가된 메소드
 }
