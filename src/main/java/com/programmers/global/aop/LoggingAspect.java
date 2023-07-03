9c1fc7cc7f96678708d8068207ebc43cddabb41a
weonest <geonhee33@naver.com>
2023-07-03 20:25:53 +0900

    feat: 4차PR

--- src/main/java/com/programmers/global/aop/LoggingAspect.java
+++ src/main/java/com/programmers/global/aop/LoggingAspect.java
@@ -1,6 +1,8 @@
 package com.programmers.global.aop;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.slf4j.Logger;
@@ -13,10 +15,10 @@ public class LoggingAspect {
 
     private static final Logger logger = LoggerFactory.getLogger(LoggingAspect.class);
 
-//    @AfterThrowing(value = "execution(* com.programmers..*.*(..))", throwing = "ex")
-//    public void log(JoinPoint joinPoint, Exception ex) throws Throwable {
-//        logger.error("에러는 여기서 발생 -> {}", joinPoint.getSignature().getName());
-//    }
+    @AfterThrowing(value = "execution(* com.programmers..*.*(..))", throwing = "ex")
+    public void log(JoinPoint joinPoint, Throwable ex) throws Throwable {
+        logger.error("에러는 여기서 발생 -> {}", joinPoint.getSignature().getName());
+    }
 
 // 로깅을 AOP 적용하여 처리하고 싶었는데, private 메서드에는 동작하지 않는다는 것을 알게 됐습니다.
 // private 메서드에도 적용하려면 Spring AOP 방식이 아닌 Assertj 방식의 CTW를 이용해야 한다는 것을
@@ -25,12 +27,14 @@ public class LoggingAspect {
 // +++ 추가
 // @AfterThrowing을 통해 예외인 경우에만 로그를 남기고 싶었는데, 이게 구조가 잘못짜여서 에러를 잡지 못하는 것인지
 // private메서드 때문인지 이유는 잘 모르겠습니다만 예외가 발생해도 아무런 로그를 남기지 않습니다..
+// +++ 추가
+// CommandLineApplication의 모든 try catch문을 삭제할 시에는 error 로그가 남겨지긴 합니다만,
+// 이렇게 하는 경우 애플리케이션이 멈춰버립니다.
 
-
-//    @Around("execution(* com.programmers..*.*(..))")
-//    public Object log(ProceedingJoinPoint joinPoint) throws Throwable {
-//        Object result = joinPoint.proceed();
-//        logger.info("Method called with result -> {}, {}", joinPoint.getSignature(), result);
-//        return result;
-//    }
+    @Around("execution(* com.programmers..*.*(..))")
+    public Object log(ProceedingJoinPoint joinPoint) throws Throwable {
+        Object result = joinPoint.proceed();
+        logger.info("Method called with result -> {}, {}", joinPoint.getSignature(), result);
+        return result;
+    }
 }
