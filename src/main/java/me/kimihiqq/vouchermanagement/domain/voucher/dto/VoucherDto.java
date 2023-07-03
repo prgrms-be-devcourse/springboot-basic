1dca23abb1bc2de82ca7e37bc53f056d897ca391
kimihiqq <kimihiqq@gmail.com>
2023-07-03 17:05:51 +0900

    feat: VoucherDto에 toVoucher 메서드 추가

--- src/main/java/me/kimihiqq/vouchermanagement/domain/voucher/dto/VoucherDto.java
+++ src/main/java/me/kimihiqq/vouchermanagement/domain/voucher/dto/VoucherDto.java
@@ -1,4 +1,24 @@
 package me.kimihiqq.vouchermanagement.domain.voucher.dto;
 
+import me.kimihiqq.vouchermanagement.domain.voucher.FixedAmountVoucher;
+import me.kimihiqq.vouchermanagement.domain.voucher.PercentDiscountVoucher;
+import me.kimihiqq.vouchermanagement.domain.voucher.Voucher;
+import me.kimihiqq.vouchermanagement.option.VoucherTypeOption;
+
+import java.util.UUID;
+
 public record VoucherDto(String type, long discount) {
+    public Voucher toVoucher() {
+        VoucherTypeOption voucherTypeOption = VoucherTypeOption.valueOf(this.type.toUpperCase());
+        UUID id = UUID.randomUUID();
+
+        switch (voucherTypeOption) {
+            case FIXED:
+                return new FixedAmountVoucher(id, this.discount);
+            case PERCENT:
+                return new PercentDiscountVoucher(id, this.discount);
+            default:
+                throw new IllegalArgumentException("Invalid voucher type: " + this.type);
+        }
+    }
 }
