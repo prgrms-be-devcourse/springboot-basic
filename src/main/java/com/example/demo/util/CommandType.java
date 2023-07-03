e96218e4fa1fba595037270d938c606ed3c0e858
charlesuu <tiger0192004@gmail.com>
2023-07-03 14:50:34 +0900

    refactor: VoucherType에서 Enum 객체를 찾는 로직 단순화

--- src/main/java/com/example/demo/util/CommandType.java
+++ src/main/java/com/example/demo/util/CommandType.java
@@ -4,12 +4,12 @@ public enum CommandType {
     EXIT,
     CREATE,
     LIST;
-    
+
     public static CommandType find(String input) {
         try {
             return valueOf(input.toUpperCase());
         } catch (IllegalArgumentException e) {
-            throw new IllegalArgumentException("입력하신 " + input + "는 유효한 커멘드가 아닙니다.");
+            throw new IllegalArgumentException("입력하신 " + input + "는 유효한 커멘드가 아닙니다. \n exit, create, list 중 하나를 입력하세요.\n");
         }
     }
 }
--- src/main/java/com/example/demo/util/VoucherType.java
+++ src/main/java/com/example/demo/util/VoucherType.java
@@ -1,34 +1,15 @@
 package com.example.demo.util;
 
-import java.util.Collections;
-import java.util.Map;
-import java.util.function.Function;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
 public enum VoucherType {
-    FIXED_AMOUNT_VOUCHER("fix"),
-    PERCENT_DISCOUNT_VOUCHER("percent");
+    FIX,
+    PERCENT;
 
-
-    private static final Map<String, VoucherType> VOUCHER_TYPE_MAP =
-            Collections.unmodifiableMap(Stream.of(values())
-                    .collect(Collectors.toMap(VoucherType::getVoucher, Function.identity())));
-
-    private final String voucher;
-
-    VoucherType(String voucher) {
-        this.voucher = voucher;
-    }
-
-    public String getVoucher() {
-        return voucher;
-    }
 
     public static VoucherType find(String input) {
-        if (VOUCHER_TYPE_MAP.containsKey(input)) {
-            return VOUCHER_TYPE_MAP.get(input);
+        try {
+            return valueOf(input.toUpperCase());
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("입력하신 " + input + "는 유효한 바우처 종류가 아닙니다.\n fix 또는 percent를 입력하세요.\n");
         }
-        throw new IllegalArgumentException("잘 못된 명령어 입니다.");
     }
 }
