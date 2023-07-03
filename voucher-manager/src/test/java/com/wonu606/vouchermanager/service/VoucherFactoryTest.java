ceac38cf538ae52b88dfd933337722d212abbd90
wonu606 <wonu606@gmail.com>
2023-07-04 02:53:35 +0900

    test: VoucherFactory 테스트 작성 및 통과

--- voucher-manager/src/test/java/com/wonu606/vouchermanager/service/VoucherFactoryTest.java
+++ voucher-manager/src/test/java/com/wonu606/vouchermanager/service/VoucherFactoryTest.java
@@ -1,42 +1,69 @@
 package com.wonu606.vouchermanager.service;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import com.wonu606.vouchermanager.domain.voucher.FixedAmountVoucher;
 import com.wonu606.vouchermanager.domain.voucher.PercentageVoucher;
 import com.wonu606.vouchermanager.domain.voucher.Voucher;
+import com.wonu606.vouchermanager.domain.voucher.VoucherDto;
 import java.util.UUID;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-class VoucherFactoryTest {
+@DisplayName("VoucherFactory 테스트")
+public class VoucherFactoryTest {
 
-    VoucherFactory factory = new VoucherFactory();
-
+    @DisplayName("create_type이 FIXED이면_FixedAmountVoucher를 생성한다.")
     @Test
-    void FIXED타입이면_FixedAmountVoucher_생성해야_한다() {
+    public void Create_TypeIsFIXED_CreatesFixedAmountVoucher() {
         // given
-        VoucherType type = VoucherType.FIXED;
+        VoucherFactory factory = new VoucherFactory();
         UUID uuid = UUID.randomUUID();
-        double discount = 5000;
+        double discountValue = 10.0;
+        VoucherDto voucherDto = new VoucherDto("FIXED", uuid, discountValue);
 
-        // when
-        Voucher voucher = factory.create(type, uuid, discount);
+        // Act
+        Voucher voucher = factory.create(voucherDto);
 
-        // then
-        assertEquals(FixedAmountVoucher.class, voucher.getClass());
+        // Assert
+        assertThat(voucher).isInstanceOf(FixedAmountVoucher.class);
+        assertThat(voucher.getUuid()).isEqualTo(uuid);
+        assertThat(voucher.getDiscountValue().getValue())
+                .isEqualTo(discountValue);
     }
 
+    @DisplayName("create_type이 PERCENT이면_PercentageVoucher를 생성한다.")
     @Test
-    void PERCENT타입이면_PercentageVoucher_생성해야_한다() {
-        // given
-        VoucherType type = VoucherType.PERCENT;
+    public void Create_TypeIsPERCENT_CreatesPercentageVoucher() {
+        // Arrange
+        VoucherFactory factory = new VoucherFactory();
         UUID uuid = UUID.randomUUID();
-        double discount = 50;
+        double discountValue = 10.0;
+        VoucherDto voucherDto = new VoucherDto("PERCENT", uuid, discountValue);
 
-        // when
-        Voucher voucher = factory.create(type, uuid, discount);
+        // Act
+        Voucher voucher = factory.create(voucherDto);
 
-        // then
-        assertEquals(PercentageVoucher.class, voucher.getClass());
+        // Assert
+        assertThat(voucher).isInstanceOf(PercentageVoucher.class);
+        assertThat(voucher.getUuid()).isEqualTo(uuid);
+        assertThat(voucher.getDiscountValue().getValue())
+                .isEqualTo(discountValue);
+    }
+
+    @DisplayName("create_type이 존재하지 않는다면_예외가 발생한다.")
+    @Test
+    public void Create_TypeDoesNotExist_ThrowsException() {
+        // Arrange
+        VoucherFactory factory = new VoucherFactory();
+        UUID uuid = UUID.randomUUID();
+        double discountValue = 10.0;
+        VoucherDto voucherDto = new VoucherDto("INVALID", uuid, discountValue);
+
+        // Act and Assert
+        assertThatThrownBy(() -> factory.create(voucherDto))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("존재하지 않는 바우처 타입입니다.");
     }
 }
