7ebbcb7fd793ca3c19245696925400864d6cdcb5
kimihiqq <kimihiqq@gmail.com>
2023-07-03 17:11:03 +0900

    feat: VoucherWalletRepository 인터페이스 생성

---
+++ src/main/java/me/kimihiqq/vouchermanagement/domain/voucherwallet/repository/VoucherWalletRepository.java
@@ -0,0 +1,11 @@
+package me.kimihiqq.vouchermanagement.domain.voucherwallet.repository;
+
+import java.util.Set;
+import java.util.UUID;
+
+public interface VoucherWalletRepository {
+    void addVoucherToWallet(UUID customerId, UUID voucherId);
+    void removeVoucherFromWallet(UUID customerId, UUID voucherId);
+    Set<UUID> findVoucherIdsByCustomerId(UUID customerId);
+    void deleteByCustomerId(UUID customerId);
+}
