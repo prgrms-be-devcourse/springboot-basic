fc01edb49409eceaddcf95419f90ebca7e2bafa0
charlesuu <tiger0192004@gmail.com>
2023-07-03 14:44:19 +0900

    refactor: CommandType에서 Enum 객체를 찾는 로직 단순화

--- src/main/java/com/example/demo/util/CommandType.java
+++ src/main/java/com/example/demo/util/CommandType.java
@@ -1,35 +1,15 @@
 package com.example.demo.util;
 
-import java.util.Collections;
-import java.util.Map;
-import java.util.function.Function;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
 public enum CommandType {
-    EXIT("exit"),
-    CREATE("create"),
-    LIST("list");
-
-    private static final Map<String, CommandType> COMMAND_TYPE_MAP =
-            Collections.unmodifiableMap(Stream.of(values())
-                    .collect(Collectors.toMap(CommandType::getCommand, Function.identity())));
-
-    private final String command;
-
-    CommandType(String command) {
-        this.command = command;
-    }
-
-    public String getCommand() {
-        return command;
-    }
-
-    public static CommandType find(String input) {
-        if (COMMAND_TYPE_MAP.containsKey(input)) {
-            return COMMAND_TYPE_MAP.get(input);
-        }
-        throw new IllegalArgumentException("잘 못된 명령어 입니다.");
-    }
+    EXIT,
+    CREATE,
+    LIST;
     
+    public static CommandType find(String input) {
+        try {
+            return valueOf(input.toUpperCase());
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("입력하신 " + input + "는 유효한 커멘드가 아닙니다.");
+        }
+    }
 }
