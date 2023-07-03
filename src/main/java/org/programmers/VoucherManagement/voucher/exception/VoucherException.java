b8e0941b383e2f87aaaee138ea42f25612171bea
KimMinheee <99alsgmlalsl@naver.com>
2023-07-03 16:51:42 +0900

    [Refractor] : VoucherException 클래스 내 ExceptionMessage 객체 final로 변경

--- src/main/java/org/programmers/VoucherManagement/voucher/exception/VoucherException.java
+++ src/main/java/org/programmers/VoucherManagement/voucher/exception/VoucherException.java
@@ -2,13 +2,15 @@ package org.programmers.VoucherManagement.voucher.exception;
 
 public class VoucherException extends RuntimeException {
 
-    private VoucherExceptionMessage exceptionMessage;
+    private final VoucherExceptionMessage exceptionMessage;
 
     public VoucherException(VoucherExceptionMessage exceptionMessage) {
         super(exceptionMessage.getMessage());
+        this.exceptionMessage = exceptionMessage;
     }
 
     public VoucherException(VoucherExceptionMessage exceptionMessage, Throwable cause) {
         super(exceptionMessage.getMessage(), cause);
+        this.exceptionMessage = exceptionMessage;
     }
 }
