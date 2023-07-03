656ceaaac1095f9ee39a8796e7c0ad1d95c9a994
JHS <wnghdtjr129@naver.com>
2023-07-03 22:08:16 +0900

    feat: 에러 처리 제거

--- src/main/java/org/devcourse/voucher/controller/console/Command.java
+++ src/main/java/org/devcourse/voucher/controller/console/Command.java
@@ -6,10 +6,6 @@ public enum Command {
     EXIT;
 
     public static Command find(String commandName) {
-        try {
-            return valueOf(commandName.toUpperCase());
-        } catch (NullPointerException e) {
-            throw new RuntimeException("지원하지 않는 명령 입니다");
-        }
+        return valueOf(commandName.toUpperCase());
     }
 }
