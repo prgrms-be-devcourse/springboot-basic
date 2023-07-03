5e5db791ad6d1ce4abc8c2b6aaddbf5325c38960
kimihiqq <kimihiqq@gmail.com>
2023-07-03 17:02:08 +0900

    refactor: Voucher 인터페이스 추상클래스로 변경

--- src/main/java/me/kimihiqq/vouchermanagement/domain/voucher/Voucher.java
+++ src/main/java/me/kimihiqq/vouchermanagement/domain/voucher/Voucher.java
@@ -1,14 +1,42 @@
 package me.kimihiqq.vouchermanagement.domain.voucher;
 
+import lombok.extern.slf4j.Slf4j;
+import me.kimihiqq.vouchermanagement.option.VoucherTypeOption;
+
 import java.util.UUID;
 
-public interface Voucher {
+@Slf4j
+public abstract class Voucher {
 
-    UUID getVoucherId();
+    private final UUID voucherId;
+    private final VoucherTypeOption type;
+    private final long discount;
 
-    String getType();
+    public Voucher(UUID voucherId, VoucherTypeOption type, long discount) {
+        validateDiscount(discount);
+        this.voucherId = voucherId;
+        this.type = type;
+        this.discount = discount;
+    }
 
-    long getDiscount();
+    private void validateDiscount(long discount) {
+        if (discount <= 0) {
+            throw new IllegalArgumentException("Discount must be positive.");
+        }
+    }
 
-    long discount(long beforeDiscount);
+    public UUID getVoucherId() {
+        return voucherId;
+    }
+
+    public String getType() {
+        return type.name();
+    }
+
+    public long getDiscount() {
+        return discount;
+    }
+
+    public abstract long discount(long beforeDiscount);
+
 }
