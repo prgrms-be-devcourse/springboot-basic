28ff740761c5f5f1777b3c68d797bb4bd499b8d5
weonest <geonhee33@naver.com>
2023-07-03 17:17:22 +0900

    feat: logback 추가 및 CTW 설정 주석

--- src/test/java/com/programmers/voucher/service/VoucherServiceTest.java
+++ src/test/java/com/programmers/voucher/service/VoucherServiceTest.java
@@ -12,12 +12,14 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@EnableAspectJAutoProxy
 class VoucherServiceTest {
 
     private VoucherService voucherService;
