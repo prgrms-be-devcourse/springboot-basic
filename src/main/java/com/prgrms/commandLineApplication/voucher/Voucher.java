37a4c55fd8f94ea86827999a54f240edfa672835
박은지 <qkrdmswl1018@naver.com>
2023-07-04 16:53:23 +0900

    refactor: Discount 수정

--- src/main/java/com/prgrms/commandLineApplication/voucher/Voucher.java
+++ src/main/java/com/prgrms/commandLineApplication/voucher/Voucher.java
@@ -1,6 +1,7 @@
 package com.prgrms.commandLineApplication.voucher;
 
 import com.prgrms.commandLineApplication.voucher.discount.Discount;
+import com.prgrms.commandLineApplication.voucher.discount.DiscountType;
 import com.prgrms.commandLineApplication.voucher.validator.VoucherValidator;
 import com.prgrms.commandLineApplication.voucher.discount.validator.DiscountValidator;
 
@@ -11,15 +12,11 @@ public class Voucher {
   private final UUID voucherId;
   private final Discount discount;
 
-  public Voucher(UUID voucherId, Discount discount) {
+  protected Voucher(UUID voucherId, Discount discount) {
     VoucherValidator.checkId(voucherId);
     DiscountValidator.checkDiscount(discount);
     this.voucherId = voucherId;
     this.discount = discount;
-  }
-
-  public int supplyDiscount(int price) {
-    return discount.executeDiscount(price);
   }
 
   public UUID getVoucherId() {
@@ -30,8 +27,16 @@ public class Voucher {
     return discount;
   }
 
+  public int supplyDiscount(int price) {
+    return discount.executeDiscount(price);
+  }
+
   public int supplyDiscountAmount() {
     return discount.getDiscountAmount();
+  }
+
+  public DiscountType supplyDiscountType() {
+    return discount.getDiscountType();
   }
 
 }
--- src/main/java/com/prgrms/commandLineApplication/voucher/discount/Discount.java
+++ src/main/java/com/prgrms/commandLineApplication/voucher/discount/Discount.java
@@ -2,9 +2,17 @@ package com.prgrms.commandLineApplication.voucher.discount;
 
 public abstract class Discount {
 
-  public abstract int executeDiscount(int price);
+  private final DiscountType discountType;
 
-  public abstract DiscountType getDiscountType();
+  protected Discount(DiscountType discountType) {
+    this.discountType = discountType;
+  }
+
+  public DiscountType getDiscountType() {
+    return discountType;
+  }
+
+  public abstract int executeDiscount(int price);
 
   public abstract int getDiscountAmount();
 
