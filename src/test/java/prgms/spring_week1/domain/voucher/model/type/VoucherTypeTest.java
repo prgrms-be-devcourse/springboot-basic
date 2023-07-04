565067901cbbd986ec8d494959163148e44f294a
2jie0516 <2jie0516>
2023-07-04 20:28:53 +0900

    바우처 타입 조회 테스트 코드 구현

--- src/test/java/prgms/spring_week1/domain/voucher/model/type/VoucherTypeTest.java
+++ src/test/java/prgms/spring_week1/domain/voucher/model/type/VoucherTypeTest.java
@@ -1,7 +1,25 @@
 package prgms.spring_week1.domain.voucher.model.type;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import prgms.spring_week1.exception.NoSuchVoucherTypeException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class VoucherTypeTest {
+
+    @ParameterizedTest
+    @ValueSource(strings = {"FIXED", "PERCENT"})
+    void findVoucherType(String inputSelectText) {
+        assertDoesNotThrow(() -> VoucherType.findVoucherType(inputSelectText));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"FIXD", "ERCENT"})
+    void findVoucherType_fail(String inputSelectText) {
+        Throwable exception = assertThrows(NoSuchVoucherTypeException.class,() -> VoucherType.findVoucherType(inputSelectText));
+        assertEquals("해당 바우처 타입이 존재하지 않습니다.",exception.getMessage());
+    }
 
 }
