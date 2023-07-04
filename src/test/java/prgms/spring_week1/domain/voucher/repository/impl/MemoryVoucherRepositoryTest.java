574e391d57fc4646e612c5c23f12fccd8a552afd
2jie0516 <2jie0516>
2023-07-04 17:08:31 +0900

    바우처 저장소 조회 , 추가에 대한 테스트 코드 생성

--- src/test/java/prgms/spring_week1/domain/voucher/repository/impl/MemoryVoucherRepositoryTest.java
+++ src/test/java/prgms/spring_week1/domain/voucher/repository/impl/MemoryVoucherRepositoryTest.java
@@ -1,7 +1,46 @@
 package prgms.spring_week1.domain.voucher.repository.impl;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import prgms.spring_week1.domain.voucher.model.Voucher;
+import prgms.spring_week1.domain.voucher.model.impl.FixedAmountVoucher;
+import prgms.spring_week1.domain.voucher.model.impl.PercentDiscountVoucher;
+import prgms.spring_week1.domain.voucher.model.type.VoucherType;
+import prgms.spring_week1.domain.voucher.repository.VoucherRepository;
+
+import java.util.Map;
+import java.util.UUID;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MemoryVoucherRepositoryTest {
+    private VoucherRepository voucherRepository;
 
+    @BeforeEach
+    void setUp(){
+        voucherRepository = new MemoryVoucherRepository();
+        voucherRepository.insert(new FixedAmountVoucher(UUID.randomUUID(), VoucherType.FIXED,10000));
+        voucherRepository.insert(new PercentDiscountVoucher(UUID.randomUUID(), VoucherType.PERCENT,30));
+    }
+
+    @Test
+    void insert() {
+        //given
+        Map<VoucherType,Long> voucherList = voucherRepository.findAll();
+        //when
+        int voucherListSize = voucherList.size();
+        //then
+        assertEquals(2,voucherListSize);
+    }
+
+    @Test
+    void findAll() {
+        //given
+        Voucher newVoucher = new FixedAmountVoucher(UUID.randomUUID(), VoucherType.FIXED,10000);
+        //when
+        voucherRepository.insert(newVoucher);
+        //then
+        assertEquals(3,voucherRepository.findAll().size());
+    }
 }
