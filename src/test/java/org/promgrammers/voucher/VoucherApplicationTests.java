6cff6475233be52f270d571767d1e1bb3f831275
seongHyun-Min <steve6133@naver.com>
2023-07-04 17:46:04 +0900

    test : 커밋 스쿼시

--- src/test/java/org/promgrammers/voucher/VoucherApplicationTests.java
+++ src/test/java/org/promgrammers/voucher/VoucherApplicationTests.java
@@ -6,7 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class VoucherApplicationTests {
 
-    //test
+    //testoo
     @Test
     void contextLoads() {
     }
