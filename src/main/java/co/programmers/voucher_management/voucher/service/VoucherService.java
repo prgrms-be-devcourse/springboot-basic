a1f8f0fd9b72d16d7cc07208a5694c9605b930ef
SW-H <hsw08@naver.com>
2023-07-05 01:12:56 +0900

    feat : 조회와 생성으로 분리했던 service 를 합침 VoucherInquiryService + VoucherCreationService -> VoucherService

--- src/main/java/co/programmers/voucher_management/voucher/service/VoucherService.java
+++ src/main/java/co/programmers/voucher_management/voucher/service/VoucherService.java
@@ -1,25 +1,29 @@
 package co.programmers.voucher_management.voucher.service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import co.programmers.voucher_management.common.Response;
 import co.programmers.voucher_management.voucher.dto.VoucherRequestDTO;
+import co.programmers.voucher_management.voucher.dto.VoucherResponseDTO;
 import co.programmers.voucher_management.voucher.entity.DiscountStrategy;
 import co.programmers.voucher_management.voucher.entity.Voucher;
 import co.programmers.voucher_management.voucher.repository.VoucherRepository;
 
 @Service
-public class VoucherCreationService {
-	private static final Logger logger = LoggerFactory.getLogger(VoucherCreationService.class);
+public class VoucherService {
+	private static final Logger logger = LoggerFactory.getLogger(VoucherService.class);
 	private final VoucherRepository voucherRepository;
 
-	public VoucherCreationService(VoucherRepository voucherRepository) {
+	public VoucherService(VoucherRepository voucherRepository) {
 		this.voucherRepository = voucherRepository;
 	}
 
-	public Response run(VoucherRequestDTO voucherRequestDTO) {
+	public Response create(VoucherRequestDTO voucherRequestDTO) {
 		DiscountStrategy discountStrategy;
 		String discountTypeName = voucherRequestDTO.getDiscountStrategy();
 		int amount = voucherRequestDTO.getDiscountAmount();
@@ -32,5 +36,19 @@ public class VoucherCreationService {
 				.build();
 	}
 
-
+	public Response inquiry() {
+		List<VoucherResponseDTO> voucherResponseDTOs;
+		try {
+			List<Voucher> inquiredData = voucherRepository.findAll();
+			voucherResponseDTOs = inquiredData.stream()
+					.map(VoucherResponseDTO::new)
+					.collect(Collectors.toList());
+		} catch (RuntimeException runtimeException) {
+			return new Response(Response.State.FAILED, runtimeException.getMessage());
+		}
+		return Response.builder()
+				.state(Response.State.SUCCESS)
+				.responseData(voucherResponseDTOs)
+				.build();
+	}
 }
