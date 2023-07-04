0959e64c7ef1496b46d37fd4db195440b5f6f43c
2jie0516 <2jie0516>
2023-07-04 10:29:06 +0900

    Meny enum 클래스에서 menuName 필드 제거 후 name 메소드 적용

--- src/main/java/prgms/spring_week1/menu/Menu.java
+++ src/main/java/prgms/spring_week1/menu/Menu.java
@@ -5,21 +5,15 @@ import prgms.spring_week1.exception.NoSuchOptionValueException;
 import java.util.stream.Stream;
 
 public enum Menu {
-    INVALID(""),
-    EXIT("exit"),
-    CREATE("create"),
-    LIST("list"),
-    BLACK("black");
-
-    private final String menuName;
-
-    Menu(String menuName) {
-        this.menuName = menuName;
-    }
+    INVALID,
+    EXIT,
+    CREATE,
+    LIST,
+    BLACK;
 
     public static Menu findMenuType(String inputText) throws NoSuchOptionValueException {
         return Stream.of(Menu.values())
-                .filter(menu -> menu.menuName.equalsIgnoreCase(inputText))
+                .filter(menu -> menu.name().equalsIgnoreCase(inputText))
                 .findFirst()
                 .orElseThrow(() -> new NoSuchOptionValueException("해당 메뉴 타입이 존재하지 않습니다."));
     }
