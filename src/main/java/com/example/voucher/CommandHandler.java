e7db40ff2d3b65c33f57f4c2ee9b586c851f9ea2
bumwoo <gld55602@gmail.com>
2023-07-04 15:15:08 +0900

    feat: CommandHandler 구현

--- src/main/java/com/example/voucher/CommandHandler.java
+++ src/main/java/com/example/voucher/CommandHandler.java
@@ -2,5 +2,22 @@ package com.example.voucher;
 
 public class CommandHandler {
     public void handleCommand(String command) {
+        String[] parts = command.split(" ");
+        String action = parts[0];
+
+        if (action.equalsIgnoreCase("create")) {
+            handleCreateCommand(parts);
+        }
+        if (action.equalsIgnoreCase("list")) {
+            handleListCommand();
+        }
+    }
+
+    private void handleListCommand() {
+        // List 커맨드 구현
+    }
+
+    private void handleCreateCommand(String[] parts) {
+        // Handle 커맨드 구현
     }
 }
