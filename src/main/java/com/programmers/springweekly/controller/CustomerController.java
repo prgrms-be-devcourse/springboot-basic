00f312568eb02ed356a2acdd1479b819287d0300
changhyeon <changhyeon.h@kakao.com>
2023-07-04 17:43:27 +0900

    feat: Customer CRUD 관련된 컨트롤러 로직 구현

--- src/main/java/com/programmers/springweekly/controller/CustomerController.java
+++ src/main/java/com/programmers/springweekly/controller/CustomerController.java
@@ -1,10 +1,15 @@
 package com.programmers.springweekly.controller;
 
 import com.programmers.springweekly.domain.customer.Customer;
+import com.programmers.springweekly.dto.CustomerCreateDto;
+import com.programmers.springweekly.dto.CustomerUpdateDto;
 import com.programmers.springweekly.service.CustomerService;
-import java.util.List;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Controller
 @AllArgsConstructor
@@ -12,7 +17,31 @@ public class CustomerController {
 
     private final CustomerService customerService;
 
+    public void save(CustomerCreateDto customerCreateDto) {
+        customerService.save(customerCreateDto);
+    }
+
+    public void update(CustomerUpdateDto customerUpdateDto) {
+        customerService.update(customerUpdateDto);
+    }
+
+    public Optional<Customer> findById(UUID customerId) {
+        return customerService.findById(customerId);
+    }
+
+    public List<Customer> findAll() {
+        return customerService.findAll();
+    }
+
     public List<Customer> getBlackList() {
         return customerService.getBlackList();
+    }
+
+    public void deleteById(UUID customerId) {
+        customerService.deleteById(customerId);
+    }
+
+    public void deleteAll() {
+        customerService.deleteAll();
     }
 }
