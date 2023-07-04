559663f52cd277a3899d591d89a8d702c9493966
ddongpuri <fnffnfkffk1112@gmail.com>
2023-07-04 22:42:56 +0900

    refactor : VoucherVO 추가

--- voucher-mission/src/main/java/org/prgrms/vouchermission/VoucherFactory.java
+++ voucher-mission/src/main/java/org/prgrms/vouchermission/VoucherFactory.java
@@ -8,13 +8,13 @@ public enum VoucherFactory {
         private final static long TEMP_VOUCHER_ID = Long.MIN_VALUE;
 
         @Override
-        public Voucher createVoucher(long voucherId, long percent, LocalDate createdDate, LocalDate expirationDate) {
-            return new PercentDiscountVoucher(voucherId, percent, createdDate, expirationDate);
+        public Voucher createVoucher(long voucherId, VoucherVO voucherVO) {
+            return new PercentDiscountVoucher(voucherId, voucherVO.getValue(), voucherVO.getCreatedDate(), voucherVO.getExpirationDate());
         }
 
         @Override
-        public Voucher createTempVoucher(long percent, LocalDate createdDate, LocalDate expirationDate) {
-            return new PercentDiscountVoucher(TEMP_VOUCHER_ID, percent, createdDate, expirationDate);
+        public Voucher createTempVoucher(VoucherVO voucherVO) {
+            return new PercentDiscountVoucher(TEMP_VOUCHER_ID, voucherVO.getValue(), voucherVO.getCreatedDate(), voucherVO.getExpirationDate());
         }
 
     },
@@ -22,17 +22,17 @@ public enum VoucherFactory {
         private final static long TEMP_VOUCHER_ID = Long.MIN_VALUE;
 
         @Override
-        public Voucher createVoucher(long voucherId, long amount, LocalDate createdDate, LocalDate expirationDate) {
-            return new FixedAmountVoucher(voucherId, amount, createdDate, expirationDate);
+        public Voucher createVoucher(long voucherId, VoucherVO voucherVO) {
+            return new FixedAmountVoucher(voucherId, voucherVO.getValue(), voucherVO.getCreatedDate(), voucherVO.getExpirationDate());
         }
 
         @Override
-        public Voucher createTempVoucher(long amount, LocalDate createdDate, LocalDate expirationDate) {
-            return new PercentDiscountVoucher(TEMP_VOUCHER_ID, amount, createdDate, expirationDate);
+        public Voucher createTempVoucher(VoucherVO voucherVO) {
+            return new PercentDiscountVoucher(TEMP_VOUCHER_ID, voucherVO.getValue(), voucherVO.getCreatedDate(), voucherVO.getExpirationDate());
         }
     };
 
-    public abstract Voucher createVoucher(long voucherId, long value, LocalDate createdDate, LocalDate expirationDate);
-    public abstract Voucher createTempVoucher(long value, LocalDate createdDate, LocalDate expirationDate);
+    public abstract Voucher createVoucher(long voucherId, VoucherVO voucherVO);
+    public abstract Voucher createTempVoucher(VoucherVO voucherVO);
 }
 
