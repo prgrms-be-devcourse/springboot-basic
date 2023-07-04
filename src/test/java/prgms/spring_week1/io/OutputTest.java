30401ebd69df41c0a1a5c3aa01c01f4f2cac5d5d
2jie0516 <2jie0516>
2023-07-04 17:45:44 +0900

    바우처 정보 출력에 대한 테스트 구현

--- src/test/java/prgms/spring_week1/io/OutputTest.java
+++ src/test/java/prgms/spring_week1/io/OutputTest.java
@@ -1,7 +1,41 @@
 package prgms.spring_week1.io;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import prgms.spring_week1.domain.voucher.model.Voucher;
+import prgms.spring_week1.domain.voucher.model.impl.FixedAmountVoucher;
+import prgms.spring_week1.domain.voucher.model.type.VoucherType;
+import prgms.spring_week1.domain.voucher.repository.VoucherRepository;
+import prgms.spring_week1.domain.voucher.repository.impl.MemoryVoucherRepository;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.UUID;
+import java.util.stream.Stream;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class OutputTest {
+    private static ByteArrayOutputStream outputMessage;
+    private static VoucherRepository voucherRepository;
+    private static Output output;
 
+    @BeforeEach
+    void setUp(){
+        outputMessage = new ByteArrayOutputStream();
+        voucherRepository = new MemoryVoucherRepository();
+        output = new Output();
+        voucherRepository.insert(new FixedAmountVoucher(UUID.randomUUID(), VoucherType.FIXED,10000));
+        voucherRepository.insert(new FixedAmountVoucher(UUID.randomUUID(), VoucherType.PERCENT,10));
+        System.setOut(new PrintStream(outputMessage));
+    }
+
+    @Test
+    void printVoucherInfo() {
+        output.printDiscountFixedVoucherInfo(10000);
+        output.printDiscountAmountVoucherInfo(10);
+
+        assertEquals("상품권 종류 : 고정 가격 할인 상품권 할인 가격 :10000원\n" +
+                            "상품권 종류 : 고정 가격 할인 상품권 할인률 :10 퍼센트",outputMessage.toString().strip());
+    }
 }
