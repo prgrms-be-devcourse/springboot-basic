76cd25c38f142c96dec53ea214a1279887f00720
2jie0516 <2jie0516>
2023-07-04 22:21:36 +0900

    given/when/then 형식으로 출력 테스트 클래스 형식 변경

--- src/test/java/prgms/spring_week1/io/OutputTest.java
+++ src/test/java/prgms/spring_week1/io/OutputTest.java
@@ -27,17 +27,21 @@ class OutputTest {
         outputMessage = new ByteArrayOutputStream();
         voucherRepository = new MemoryVoucherRepository();
         output = new Output();
-        voucherRepository.insert(new FixedAmountVoucher(UUID.randomUUID(), VoucherType.FIXED, 10000));
-        voucherRepository.insert(new FixedAmountVoucher(UUID.randomUUID(), VoucherType.PERCENT, 10));
         System.setOut(new PrintStream(outputMessage));
     }
 
     @Test
     @DisplayName("바우처를 조회 했을 때 제대로 출력되는 지 확인")
     void printVoucherInfo() {
+        //given
+        voucherRepository.insert(new FixedAmountVoucher(UUID.randomUUID(), VoucherType.FIXED, 10000));
+        voucherRepository.insert(new FixedAmountVoucher(UUID.randomUUID(), VoucherType.PERCENT, 10));
+
+        //when
         output.printDiscountFixedVoucherInfo(10000);
         output.printDiscountAmountVoucherInfo(10);
 
+        //then
         assertEquals("상품권 종류 : 고정 가격 할인 상품권 할인 가격 :10000원\n" +
                 "상품권 종류 : 고정 가격 할인 상품권 할인률 :10 퍼센트", outputMessage.toString().strip());
     }
@@ -45,13 +49,16 @@ class OutputTest {
     @Test
     @DisplayName("블랙리스트를 조회 했을 때 제대로 출력되는 지 확인")
     void printBlackConsumerList() {
+        //given
         List<BlackConsumer> blackConsumerList = new ArrayList<>();
         blackConsumerList.add(new BlackConsumer("오세한", "20"));
         blackConsumerList.add(new BlackConsumer("세오한", "21"));
         blackConsumerList.add(new BlackConsumer("세한오", "22"));
 
+        //when
         output.printBlackConsumerList(blackConsumerList);
 
+        //then
         assertEquals("오세한 20\n" +
                 "세오한 21\n" +
                 "세한오 22", outputMessage.toString().strip());
