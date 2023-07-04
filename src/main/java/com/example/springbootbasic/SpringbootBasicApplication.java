1c0704e2eb2919568b858cd65f94c07a3c4dceff
Choi Seongwon <vlflalektzja@naver.com>
2023-07-05 00:42:05 +0900

    feat: main class 상 불필요한 필드 제거

--- src/main/java/com/example/springbootbasic/SpringbootBasicApplication.java
+++ src/main/java/com/example/springbootbasic/SpringbootBasicApplication.java
@@ -1,20 +1,13 @@
 package com.example.springbootbasic;
 
-import com.example.springbootbasic.voucher.CliVoucherApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringbootBasicApplication {
-	private final CliVoucherApplication cliVoucherApplication;
-
-	public SpringbootBasicApplication(CliVoucherApplication cliVoucherApplication) {
-		this.cliVoucherApplication = cliVoucherApplication;
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootBasicApplication.class, args);
-//		cliVoucherApplication.run();
 	}
 
 }
