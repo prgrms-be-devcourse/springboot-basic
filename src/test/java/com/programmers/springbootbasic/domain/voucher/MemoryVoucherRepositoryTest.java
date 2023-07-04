6b5a069618283591a4b783e43b41cbf5659eb295
sangminnim <sangmins930@gmail.com>
2023-07-05 03:05:57 +0900

    test: ParameterizedTest로 변경해서 FixAmountVoucher, PercentDiscountVoucher 둘 다 테스트

--- src/test/java/com/programmers/springbootbasic/domain/voucher/MemoryVoucherRepositoryTest.java
+++ src/test/java/com/programmers/springbootbasic/domain/voucher/MemoryVoucherRepositoryTest.java
@@ -2,13 +2,14 @@ package com.programmers.springbootbasic.domain.voucher;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
 
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class MemoryVoucherRepositoryTest {
     private final VoucherRepository voucherRepository = new MemoryVoucherRepository();
@@ -18,17 +19,28 @@ class MemoryVoucherRepositoryTest {
         voucherRepository.deleteAll();
     }
 
-    @Test
-    void Voucher생성_save_성공() {
+
+    @ParameterizedTest
+    @EnumSource(VoucherType.class)
+    void Voucher생성_save_성공(VoucherType voucherType) {
         // given
         UUID voucherId = UUID.randomUUID();
-        String name = "회원가입 5000원 할인 쿠폰";
+        String name = "회원가입 쿠폰";
         LocalDateTime createdDate = LocalDateTime.now();
         LocalDateTime expirationDate = createdDate.plusMonths(3);
-        int amount = 5_000;
-        Voucher voucher = new FixedAmountVoucher(voucherId, name, createdDate, expirationDate, amount);
+
 
         // when
+        Voucher voucher = switch (voucherType) {
+            case FIX -> {
+                int amount = 5_000;
+                yield new FixedAmountVoucher(voucherId, name, createdDate, expirationDate, amount);
+            }
+            case PERCENT -> {
+                int percent = 30;
+                yield new PercentDiscountVoucher(voucherId, name, createdDate, expirationDate, percent);
+            }
+        };
         Voucher saved = voucherRepository.save(voucher);
 
         // then
