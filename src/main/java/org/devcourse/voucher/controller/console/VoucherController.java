58a966ec8912aaa676c7f4aac8594406667d0b0a
JHS <wnghdtjr129@naver.com>
2023-07-04 12:19:43 +0900

    feat: 컨트롤러 기능 구현

--- src/main/java/org/devcourse/voucher/controller/console/VoucherController.java
+++ src/main/java/org/devcourse/voucher/controller/console/VoucherController.java
@@ -3,6 +3,8 @@ package org.devcourse.voucher.controller.console;
 import org.devcourse.voucher.app.VoucherService;
 import org.devcourse.voucher.controller.console.dto.VoucherInfoResponse;
 import org.devcourse.voucher.controller.console.dto.VoucherSaveRequest;
+import org.devcourse.voucher.domain.voucher.Voucher;
+import org.devcourse.voucher.domain.voucher.vo.VoucherVO;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,10 +18,24 @@ public class VoucherController {
     }
 
     public VoucherInfoResponse createVoucher(VoucherSaveRequest request) {
-        return new VoucherInfoResponse(0, "", 0);
+        Voucher voucher = new Voucher(0, request.voucherType(), request.amount());
+        Voucher savedVoucher = voucherService.save(voucher);
+
+        VoucherVO value = savedVoucher.values();
+
+        return new VoucherInfoResponse(value);
     }
 
     public List<VoucherInfoResponse> listVoucher() {
-        return new ArrayList<>();
+        List<Voucher> vouchers = voucherService.findAll();
+
+        List<VoucherInfoResponse> response = new ArrayList<>();
+        for (Voucher voucher : vouchers) {
+            VoucherVO value = voucher.values();
+
+            response.add(new VoucherInfoResponse(value));
+        }
+
+        return response;
     }
 }
