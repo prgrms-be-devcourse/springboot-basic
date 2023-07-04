5a1495d9bb74ea0cf46a9e9aeae616e5c730f493
2jie0516 <2jie0516>
2023-07-04 18:47:41 +0900

    메뉴 타입 조회 테스트 코드 구현

--- src/test/java/prgms/spring_week1/menu/MenuTest.java
+++ src/test/java/prgms/spring_week1/menu/MenuTest.java
@@ -1,7 +1,24 @@
 package prgms.spring_week1.menu;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+import prgms.spring_week1.exception.NoSuchOptionValueException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MenuTest {
 
+    @ParameterizedTest
+    @ValueSource(strings = {"EXIT", "exit", "creaTE"})
+    void findMenuType(String inputText) {
+        assertDoesNotThrow(() -> Menu.findMenuType(inputText));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"EXI", "crrea", "EXwww"})
+    void MenuType_fail(String inputText) {
+        assertThrows(NoSuchOptionValueException.class, () -> Menu.findMenuType(inputText));
+    }
 }
