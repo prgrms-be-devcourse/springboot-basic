807704bf87e4ef4c3a5f99fcc71394fc333c9d01
박은지 <qkrdmswl1018@naver.com>
2023-07-04 16:45:56 +0900

    refactor: valueOf 사용

--- src/main/java/com/prgrms/commandLineApplication/io/MenuType.java
+++ src/main/java/com/prgrms/commandLineApplication/io/MenuType.java
@@ -1,26 +1,18 @@
 package com.prgrms.commandLineApplication.io;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
-
 public enum MenuType {
   EXIT,
   LIST,
   CREATE;
 
   private static final String ERROR_MESSAGE = "Invalid Menu Type";
-  private static final Map<String, MenuType> MENU_TYPES = new HashMap<>();
-
-  static {
-    Arrays.stream(values())
-            .forEach(menu -> MENU_TYPES.put(menu.name(), menu));
-  }
 
   public static MenuType valueOfType(String menu) {
-    return Optional.ofNullable(MENU_TYPES.get(menu.toUpperCase()))
-            .orElseThrow(() -> new IllegalArgumentException(ERROR_MESSAGE + " -> " + menu));
+    try {
+      return MenuType.valueOf(menu.toUpperCase());
+    } catch (Exception e) {
+      throw new IllegalArgumentException(ERROR_MESSAGE);
+    }
   }
 
 }
--- src/main/java/com/prgrms/commandLineApplication/voucher/discount/DiscountType.java
+++ src/main/java/com/prgrms/commandLineApplication/voucher/discount/DiscountType.java
@@ -1,25 +1,17 @@
 package com.prgrms.commandLineApplication.voucher.discount;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
-
 public enum DiscountType {
   FIXED,
   PERCENT;
 
   private static final String ERROR_MESSAGE = "Invalid Voucher Type";
-  private static final Map<String, DiscountType> VOUCHER_TYPES = new HashMap<>();
-
-  static {
-    Arrays.stream(values())
-            .forEach(voucher -> VOUCHER_TYPES.put(voucher.name(), voucher));
-  }
 
   public static DiscountType valueOfType(String type) {
-    return Optional.ofNullable(VOUCHER_TYPES.get(type.toUpperCase()))
-            .orElseThrow(() -> new IllegalArgumentException(ERROR_MESSAGE + " -> " + type));
+    try {
+      return DiscountType.valueOf(type.toUpperCase());
+    } catch (Exception e) {
+      throw new IllegalArgumentException(ERROR_MESSAGE);
+    }
   }
 
 }
