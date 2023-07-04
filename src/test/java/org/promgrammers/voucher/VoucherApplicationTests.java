87e172b3f3cc2351d00aeda72375121f3d0a4073
seongHyun-Min <steve6133@naver.com>
2023-07-04 17:47:56 +0900

    "test: new branch"

--- src/test/java/org/promgrammers/voucher/VoucherApplicationTests.java
+++ src/test/java/org/promgrammers/voucher/VoucherApplicationTests.java
@@ -6,7 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class VoucherApplicationTests {
 
-    //testoo
+    //test
     @Test
     void contextLoads() {
     }
