be6a14239a6955a67c6c9a9377867d8fbd96db79
bumwoo <gld55602@gmail.com>
2023-07-04 15:18:52 +0900

    feat: readInput 구현

--- src/main/java/com/example/voucher/ui/Input.java
+++ src/main/java/com/example/voucher/ui/Input.java
@@ -1,8 +1,21 @@
 package com.example.voucher.ui;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Input {
     public String readInput() {
-        return null;
+        String input = "";
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        try {
+            input = reader.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return input;
     }
 }
 
