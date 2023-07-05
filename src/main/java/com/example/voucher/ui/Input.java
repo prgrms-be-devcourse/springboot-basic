f69af7d84decf6a509936328ffe9fb05a0b66649
bumwoo <gld55602@gmail.com>
2023-07-05 15:28:39 +0900

    refactor: readInput Scanner로 수정

--- src/main/java/com/example/voucher/ui/Input.java
+++ src/main/java/com/example/voucher/ui/Input.java
@@ -1,21 +1,11 @@
 package com.example.voucher.ui;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class Input {
     public String readInput() {
-        String input = "";
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-
-        try {
-            input = reader.readLine();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return input;
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
     }
 }
 
