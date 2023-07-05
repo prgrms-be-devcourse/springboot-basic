edbd107a966cf514672eace5d5e6a68930efa2c4
doyeon <kimsk3113@naver.com>
2023-07-05 21:25:22 +0900

    reafactor: 첫 번째 피드백 이후 테스트 코드 변경사항 작성

--- src/main/java/com/example/demo/voucher/infrastructure/FileRepository.java
+++ src/main/java/com/example/demo/voucher/infrastructure/FileRepository.java
@@ -6,8 +6,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Repository;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
@@ -18,12 +16,14 @@ import java.util.UUID;
 public class FileRepository implements VoucherRepository {
 
     private final VoucherInfo vouchers;
+    private final VoucherFileWriter writer;
 
     @Value("${file.path}")
     private String filePath;
 
-    public FileRepository(VoucherInfo vouchers) {
+    public FileRepository(VoucherInfo vouchers, VoucherFileWriter writer) {
         this.vouchers = vouchers;
+        this.writer = writer;
     }
 
     @Override
@@ -43,21 +43,11 @@ public class FileRepository implements VoucherRepository {
     @Override
     public void insert(Voucher voucher) {
         try {
-            write(voucher);
+            writer.write(voucher, filePath);
             vouchers.add(voucher);
         } catch (IOException e) {
             throw new RuntimeException("File writing failed", e);
         }
     }
 
-    private void write(Voucher voucher) throws IOException {
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath, true))) {
-            writer.write(voucher.getName());
-            writer.write(",");
-            writer.write(voucher.getVoucherId().toString());
-            writer.write(",");
-            writer.write(Long.toString(voucher.getValue()));
-            writer.newLine();
-        }
-    }
 }
