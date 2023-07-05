8de276458dbf4dc9e3783279d29ac5ccf2d1f61c
juno-junho <ssmm0205@naver.com>
2023-07-06 00:10:03 +0900

    feat: input을 uppercase로 변환해 메서드에 전달 추가

--- voucher/src/main/java/com/ray/junho/voucher/controller/Command.java
+++ voucher/src/main/java/com/ray/junho/voucher/controller/Command.java
@@ -6,8 +6,10 @@ public enum Command {
     EXIT,CREATE, LIST;
 
     public static Command find(String input) {
+        String inputToUpperCase = input.toUpperCase();
+
         return Arrays.stream(values())
-                .filter(command -> command.toString().equals(input))
+                .filter(command -> command.toString().equals(inputToUpperCase))
                 .findAny()
                 .orElseThrow(() -> new IllegalArgumentException("해당 명령어가 존재하지 않습니다."));
     }
