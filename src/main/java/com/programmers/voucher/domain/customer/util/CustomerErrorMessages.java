463b6bd40dc89a878a071411144be8d8c26eea63
hseong3243 <hseong3243@gmail.com>
2023-07-05 22:37:58 +0900

    fix: 메시지 관리 유틸 클래스 `private` 생성자 추가

--- src/main/java/com/programmers/voucher/domain/customer/util/CustomerErrorMessages.java
+++ src/main/java/com/programmers/voucher/domain/customer/util/CustomerErrorMessages.java
@@ -2,4 +2,7 @@ package com.programmers.voucher.domain.customer.util;
 
 public final class CustomerErrorMessages {
     public static final String DUPLICATE_EMAIL = "Duplicate email exist: Email {0}";
+
+    private CustomerErrorMessages() {
+    }
 }
--- src/main/java/com/programmers/voucher/global/util/DataErrorMessages.java
+++ src/main/java/com/programmers/voucher/global/util/DataErrorMessages.java
@@ -4,4 +4,6 @@ public final class DataErrorMessages {
     public static final String NO_SUCH_ELEMENT
             = "The element does not exist";
 
+    private DataErrorMessages() {
+    }
 }
