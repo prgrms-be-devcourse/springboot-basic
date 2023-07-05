126534b2f426e904c2a37633092a8be6c5c03cda
juno-junho <ssmm0205@naver.com>
2023-07-05 23:56:51 +0900

    feat: voucherType에 순서 코드 필드 추가

--- voucher/src/main/java/com/ray/junho/voucher/domain/VoucherType.java
+++ voucher/src/main/java/com/ray/junho/voucher/domain/VoucherType.java
@@ -1,6 +1,22 @@
 package com.ray.junho.voucher.domain;
 
+import java.util.Arrays;
+
 public enum VoucherType {
-    FIXED_AMOUNT,
-    PERCENT_DISCOUNT,
+    FIXED_AMOUNT(1),
+    PERCENT_DISCOUNT(2),
+    ;
+
+    private final int voucherTypeOrder;
+
+    VoucherType(int voucherTypeOrder) {
+        this.voucherTypeOrder = voucherTypeOrder;
+    }
+
+    public static VoucherType findVoucherType(int voucherTypeOrder) {
+        return Arrays.stream(values())
+                .filter(voucherType -> voucherType.voucherTypeOrder == voucherTypeOrder)
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException("해당하는 바우처 타입이 없습니다."));
+    }
 }
