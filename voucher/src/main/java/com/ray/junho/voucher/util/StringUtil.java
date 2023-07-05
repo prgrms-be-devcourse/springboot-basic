8eb836c01165438cdefd36db88c5f49176a08133
juno-junho <ssmm0205@naver.com>
2023-07-06 00:23:54 +0900

    chore(util): 사용하지 않는 메서드 삭제

--- voucher/src/main/java/com/ray/junho/voucher/util/StringUtil.java
+++ voucher/src/main/java/com/ray/junho/voucher/util/StringUtil.java
@@ -1,10 +1,5 @@
 package com.ray.junho.voucher.util;
 
-import com.ray.junho.voucher.domain.Voucher;
-
-import java.util.List;
-import java.util.stream.Collectors;
-
 public final class StringUtil {
 
     private StringUtil() {
@@ -17,11 +12,5 @@ public final class StringUtil {
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("잘못된 값을 입력하셨습니다. 숫자를 입력해 주세요.", e);
         }
-    }
-
-    public static List<String> convertVouchersToList(List<Voucher> vouchers) {
-        return vouchers.stream()
-                .map(Object::toString)
-                .collect(Collectors.toList());
     }
 }
