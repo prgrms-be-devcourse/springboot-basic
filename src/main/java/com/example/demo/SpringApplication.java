ddfb234aadd28f395f7854eff347c724a1d4955a
charlesuu <tiger0192004@gmail.com>
2023-07-05 18:33:59 +0900

    refactor: 클래스명 변경 DemoApplication -> SpringApplication

--- src/main/java/com/example/demo/SpringApplication.java
+++ src/main/java/com/example/demo/SpringApplication.java
@@ -1,12 +1,11 @@
 package com.example.demo;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class SpringApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        org.springframework.boot.SpringApplication.run(SpringApplication.class, args);
     }
 }
