f1fde757183957fc635a1779397ccc9217673c47
JHS <wnghdtjr129@naver.com>
2023-07-05 19:58:06 +0900

    refactor: 타입 검증 위치 변경

--- src/main/java/org/devcourse/voucher/controller/console/dto/VoucherSaveRequest.java
+++ src/main/java/org/devcourse/voucher/controller/console/dto/VoucherSaveRequest.java
@@ -3,4 +3,8 @@ package org.devcourse.voucher.controller.console.dto;
 import org.devcourse.voucher.domain.voucher.VoucherType;
 
 public record VoucherSaveRequest(VoucherType voucherType, int amount) {
+
+    public VoucherSaveRequest(String voucherType, String amount) {
+        this(VoucherType.find(voucherType), Integer.parseInt(amount));
+    }
 }
--- src/main/java/org/devcourse/voucher/controller/console/runner/CreateCommandRunner.java
+++ src/main/java/org/devcourse/voucher/controller/console/runner/CreateCommandRunner.java
@@ -5,7 +5,6 @@ import org.devcourse.voucher.controller.console.dto.Response;
 import org.devcourse.voucher.controller.console.dto.Status;
 import org.devcourse.voucher.controller.console.dto.VoucherInfoResponse;
 import org.devcourse.voucher.controller.console.dto.VoucherSaveRequest;
-import org.devcourse.voucher.domain.voucher.VoucherType;
 import org.devcourse.voucher.view.Input;
 
 public class CreateCommandRunner implements CommandRunner {
@@ -21,17 +20,12 @@ public class CreateCommandRunner implements CommandRunner {
 
     @Override
     public Response run() {
-        VoucherSaveRequest request = createRequest();
+        String type = input.getUserInput();
+        String amount = input.getUserInput();
+        VoucherSaveRequest request = new VoucherSaveRequest(type, amount);
         VoucherInfoResponse response = controller.createVoucher(request);
 
         return postProcessResponse(response);
-    }
-
-    private VoucherSaveRequest createRequest() {
-        VoucherType voucherType = VoucherType.find(input.getUserInput());
-        int inputAmount = Integer.parseInt(input.getUserInput());
-
-        return new VoucherSaveRequest(voucherType, inputAmount);
     }
 
     private Response postProcessResponse(VoucherInfoResponse response) {
