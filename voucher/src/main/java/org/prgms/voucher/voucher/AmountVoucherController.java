1e56e740fe1f8f3117b5e8ec29ccaefaf9e09c33
hyeon-z <phj8221@naver.com>
2023-07-06 01:17:32 +0900

    refactor: AmountVoucherController의 바우처 조회 메서드 개선

--- voucher/src/main/java/org/prgms/voucher/voucher/AmountVoucherController.java
+++ voucher/src/main/java/org/prgms/voucher/voucher/AmountVoucherController.java
@@ -8,7 +8,6 @@ import java.util.stream.Collectors;
 @Controller
 public class AmountVoucherController {
     private final AmountVoucherService amountVoucherService;
-    private static final int INITIAL_MONEY = 100_000;
 
     public AmountVoucherController(AmountVoucherService amountVoucherService) {
         this.amountVoucherService = amountVoucherService;
@@ -26,11 +25,7 @@ public class AmountVoucherController {
         List<AmountVoucher> amountVouchers = amountVoucherService.listAmountVoucher();
 
         return amountVouchers.stream()
-                .map(voucher -> new VoucherPrintDto(
-                        voucher.getOptionTypeName(),
-                        voucher.discount(INITIAL_MONEY),
-                        voucher.getPublishDate(),
-                        voucher.getExpirationDate()))
+                .map(VoucherPrintDto::new)
                 .map(VoucherPrintDto::getVoucherPrint)
                 .collect(Collectors.joining("\n"));
     }
--- voucher/src/main/java/org/prgms/voucher/voucher/VoucherPrintDto.java
+++ voucher/src/main/java/org/prgms/voucher/voucher/VoucherPrintDto.java
@@ -2,28 +2,21 @@ package org.prgms.voucher.voucher;
 
 import org.prgms.voucher.util.MessageUtils;
 
-import java.time.LocalDate;
-
 import static org.prgms.voucher.voucher.VoucherPrintType.*;
 
 public class VoucherPrintDto {
-    private final String voucherType;
-    private final int afterDiscountValue;
-    private final LocalDate publishDate;
-    private final LocalDate expirationDate;
+    private static final int INITIAL_MONEY = 100_000;
+    private final AmountVoucher amountVoucher;
 
-    public VoucherPrintDto(String voucherType, int afterDiscountValue, LocalDate publishDate, LocalDate expirationDate) {
-        this.voucherType = voucherType;
-        this.afterDiscountValue = afterDiscountValue;
-        this.publishDate = publishDate;
-        this.expirationDate = expirationDate;
+    public VoucherPrintDto(AmountVoucher amountVoucher) {
+        this.amountVoucher = amountVoucher;
     }
 
     public String getVoucherPrint() {
-        return formatVoucherOutput(VOUCHER_TYPE, voucherType)
-                + formatVoucherOutput(AFTER_DISCOUNT_VALUE, String.valueOf(afterDiscountValue))
-                + formatVoucherOutput(PUBLISH_DATE, String.valueOf(publishDate))
-                + formatVoucherOutput(EXPIRATION_DATE, String.valueOf(expirationDate));
+        return formatVoucherOutput(VOUCHER_TYPE, amountVoucher.getOptionTypeName())
+                + formatVoucherOutput(AFTER_DISCOUNT_VALUE, String.valueOf(amountVoucher.discount(INITIAL_MONEY)))
+                + formatVoucherOutput(PUBLISH_DATE, String.valueOf(amountVoucher.getPublishDate()))
+                + formatVoucherOutput(EXPIRATION_DATE, String.valueOf(amountVoucher.getExpirationDate()));
     }
 
     private String formatVoucherOutput(VoucherPrintType voucherPrintType, String value) {
