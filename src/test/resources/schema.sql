7fa21ec2371267ced39700844474d1ed90503438
hseong3243 <hseong3243@gmail.com>
2023-07-06 02:51:06 +0900

    chore: `Voucher` 도메인 모델의 데이터 타입에 맞게 스키마 변경

--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -2,7 +2,7 @@ drop table if exists voucher;
 create table voucher (
     voucher_id uuid primary key,
     voucher_type varchar(20) not null,
-    amount int not null
+    amount bigint not null
 );
 
 drop table if exists customer;
