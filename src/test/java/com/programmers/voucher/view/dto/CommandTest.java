1c408a3c2b871188919172326b860b7fcec22bb8
kmebin <blowhuiee@gmail.com>
2023-07-05 22:32:44 +0900

    test: 변경된 명령어 메뉴 테스트

--- src/test/java/com/programmers/voucher/view/dto/CommandTest.java
+++ src/test/java/com/programmers/voucher/view/dto/CommandTest.java
@@ -10,18 +10,18 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 class CommandTest {
     @ParameterizedTest
-    @DisplayName("올바른 명령어인 경우 성공한다.")
-    @ValueSource(strings = {"exit", "create", "list"})
-    void 올바른_명령어(String command) {
-        assertThatCode(() -> Command.findByName(command))
+    @DisplayName("올바른 명령어 메뉴인 경우 성공한다.")
+    @ValueSource(ints = {1, 2, 3})
+    void 올바른_명령어_메뉴(int number) {
+        assertThatCode(() -> Command.findByNumber(number))
                 .doesNotThrowAnyException();
     }
 
     @ParameterizedTest
-    @DisplayName("잘못된 명령어인 경우 예외가 발생한다.")
-    @ValueSource(strings = {"a", "123", ""})
-    void 잘못된_명령어(String command) {
-        assertThatThrownBy(() -> Command.findByName(command))
+    @DisplayName("잘못된 명령어 메뉴인 경우 예외가 발생한다.")
+    @ValueSource(ints = {0, 999, -10})
+    void 잘못된_명령어_메뉴(int command) {
+        assertThatThrownBy(() -> Command.findByNumber(command))
                 .isInstanceOf(InvalidCommandException.class)
                 .hasMessage("존재하지 않는 명령어입니다.");
     }
