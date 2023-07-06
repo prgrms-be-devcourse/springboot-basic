78814d114c20adc09b277d11105cc113a17b36bb
kmebin <blowhuiee@gmail.com>
2023-07-06 22:58:27 +0900

    test: 바우처, 고객 관리 명령어 테스트

--- src/test/java/com/programmers/voucher/view/command/VoucherCommandTest.java
+++ src/test/java/com/programmers/voucher/view/command/VoucherCommandTest.java
@@ -8,20 +8,20 @@ import org.junit.jupiter.params.provider.ValueSource;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
-class CommandTest {
+class VoucherCommandTest {
     @ParameterizedTest
-    @DisplayName("올바른 명령어 메뉴인 경우 성공한다.")
-    @ValueSource(ints = {1, 2, 3})
-    void 올바른_명령어_메뉴(int number) {
-        assertThatCode(() -> Command.findByNumber(number))
+    @DisplayName("올바른 바우처 관리 메뉴인 경우 성공한다.")
+    @ValueSource(ints = {1, 2, 3, 4, 5})
+    void 올바른_바우처_관리_메뉴(int number) {
+        assertThatCode(() -> VoucherCommand.findByNumber(number))
                 .doesNotThrowAnyException();
     }
 
     @ParameterizedTest
-    @DisplayName("잘못된 명령어 메뉴인 경우 예외가 발생한다.")
+    @DisplayName("잘못된 바우처 관리 메뉴인 경우 예외가 발생한다.")
     @ValueSource(ints = {0, 999, -10})
-    void 잘못된_명령어_메뉴(int number) {
-        assertThatThrownBy(() -> Command.findByNumber(number))
+    void 잘못된_바우처_관리_메뉴(int command) {
+        assertThatThrownBy(() -> VoucherCommand.findByNumber(command))
                 .isInstanceOf(InvalidCommandException.class)
                 .hasMessage("존재하지 않는 명령어입니다.");
     }
