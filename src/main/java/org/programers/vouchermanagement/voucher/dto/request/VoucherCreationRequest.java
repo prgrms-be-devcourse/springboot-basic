ee4310ba9ea8d2391697c342d10876e66123b363
player-geun <player_geun@naver.com>
2023-07-06 20:11:31 +0900

    feat : 바우처를 조회, 상세, 입력 페이지 구현

--- src/main/java/org/programers/vouchermanagement/voucher/dto/request/VoucherCreationRequest.java
+++ src/main/java/org/programers/vouchermanagement/voucher/dto/request/VoucherCreationRequest.java
@@ -2,11 +2,9 @@ package org.programers.vouchermanagement.voucher.dto.request;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import org.programers.vouchermanagement.voucher.domain.VoucherType;
 
 @AllArgsConstructor
-@NoArgsConstructor
 @Getter
 public class VoucherCreationRequest {
 
