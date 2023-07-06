2e9707c480ffa4c2f6f47f030f6d6c97ecc2c28a
SW-H <hsw08@naver.com>
2023-07-07 03:16:48 +0900

    feat : findById() 구현

--- src/main/java/co/programmers/voucher_management/customer/repository/CustomerFileRepository.java
+++ src/main/java/co/programmers/voucher_management/customer/repository/CustomerFileRepository.java
@@ -1,5 +1,7 @@
 package co.programmers.voucher_management.customer.repository;
 
+import static co.programmers.voucher_management.voucher.entity.Voucher.STATUS.*;
+
 import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
@@ -7,27 +9,49 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Repository;
 
 import com.opencsv.CSVReader;
 
 import co.programmers.voucher_management.customer.entity.Customer;
 
+@Repository
 @Profile("file")
-public class CustomerFileRepository {
+public class CustomerFileRepository implements CustomerRepository {
 	private final Path path;
 
 	public CustomerFileRepository(@Value(value = "${file.blacklist.path}") String filePath) {
 		path = Paths.get(filePath);
 	}
 
+	@Override
 	public List<Customer> findByRating(String rating) {
 		if (Customer.Rating.BLACKLIST.name().equals(rating)) {
 			return findBlackList();
 		}
 		return List.of();
+	}
+
+	@Override
+	public Optional<Customer> findById(long customerId) {
+		String[] line;
+		try (Reader reader = Files.newBufferedReader(path);
+			 CSVReader csvReader = new CSVReader(reader)) {
+			while((line = csvReader.readNext())!=null){
+				long id = Long.parseLong(line[CustomerProperty.ID.index]);
+				String status = line[CustomerProperty.STATUS.index];
+				if((customerId == id) && (status.equals(NORMAL.toString()))){
+					return Optional.of(new Customer(line));
+				}
+			}
+		}catch (IOException ioException) {
+			throw new RuntimeException("File Reader Failed");
+		}
+		return Optional.empty();
 	}
 
 	private List<Customer> findBlackList() {
@@ -39,19 +63,23 @@ public class CustomerFileRepository {
 				customers.add(maptoCustomer(blackList));
 			}
 			return customers;
-		} catch (IOException e) {
+		} catch (IOException ioException) {
 			throw new RuntimeException("File Reader Failed");
 		}
 	}
 
 	private Customer maptoCustomer(String[] oneLine) {
 		return Customer.builder()
-				.name(oneLine[CustomerProperty.Name.index])
+				.name(oneLine[CustomerProperty.NAME.index])
 				.build();
 	}
 
 	public enum CustomerProperty {
-		Name(0);
+		ID(0),
+		NAME(1),
+		RATING(2),
+		PHONE_NUMBER(3),
+		STATUS(4);
 
 		final int index;
 
