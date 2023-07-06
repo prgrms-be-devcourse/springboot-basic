ccbc86014c38bdf58f6b6b512fdfa1d1e7df9a3a
byeolhaha <j35635aa@gmail.com>
2023-07-06 15:31:23 +0900

    refactor : 일급컬렉션을 핵심 비즈니스 로직만 알도록 mapper를dto로 변환하는 객체 추가하며 로직 수정

--- src/main/java/com/prgrms/service/voucher/VoucherService.java
+++ src/main/java/com/prgrms/service/voucher/VoucherService.java
@@ -1,29 +1,42 @@
 package com.prgrms.service.voucher;
 
+import com.prgrms.model.dto.mapper.DtoConverter;
 import com.prgrms.model.dto.VoucherRequest;
+import com.prgrms.model.dto.VoucherResponse;
 import com.prgrms.model.voucher.*;
+import com.prgrms.model.voucher.discount.Discount;
 import com.prgrms.repository.voucher.VoucherRepository;
-import lombok.RequiredArgsConstructor;
+import com.prgrms.model.voucher.VoucherCreator;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-@RequiredArgsConstructor
 public class VoucherService {
 
     private final VoucherRepository voucherRepository;
     private final VoucherCreator voucherCreator;
+    private final DtoConverter dtoConverter;
+
+    public VoucherService(VoucherRepository voucherRepository, VoucherCreator voucherCreator, DtoConverter dtoConverter) {
+        this.voucherRepository = voucherRepository;
+        this.voucherCreator = voucherCreator;
+        this.dtoConverter = dtoConverter;
+    }
 
     public Voucher createVoucher(VoucherRequest voucherRequest) {
         Discount discount = voucherRequest.getDiscount();
         VoucherType voucherType = voucherRequest.getVoucherType();
-
         Voucher voucher = voucherCreator.createVoucher(discount, voucherType);
 
         return voucherRepository.insert(voucher);
     }
 
-    public VoucherRegistry getAllVoucherList() {
-        return voucherRepository.getAllVoucherList();
+    public List<VoucherResponse> getAllVoucherList() {
+        VoucherRegistry vouchers = voucherRepository.getAllVoucher();
+        return dtoConverter.convertVoucherResponse(vouchers);
     }
 
 }
+
+
