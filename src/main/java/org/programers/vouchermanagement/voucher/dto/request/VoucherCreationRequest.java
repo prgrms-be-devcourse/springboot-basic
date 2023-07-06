5a777b7b3067a8d4dc15ebc0bf825a94478781ec
player-geun <player_geun@naver.com>
2023-07-06 10:41:06 +0900

    feat : 바우처를 추가하는 API 구현

--- src/main/java/org/programers/vouchermanagement/voucher/dto/request/VoucherCreationRequest.java
+++ src/main/java/org/programers/vouchermanagement/voucher/dto/request/VoucherCreationRequest.java
@@ -1,12 +1,14 @@
 package org.programers.vouchermanagement.voucher.dto.request;
 
+import lombok.NoArgsConstructor;
 import org.programers.vouchermanagement.voucher.domain.VoucherPolicy;
 import org.programers.vouchermanagement.voucher.domain.VoucherType;
 
+@NoArgsConstructor
 public class VoucherCreationRequest {
 
-    private final VoucherPolicy policy;
-    private final VoucherType type;
+    private VoucherPolicy policy;
+    private VoucherType type;
 
     public VoucherCreationRequest(VoucherPolicy policy, VoucherType type) {
         this.policy = policy;
