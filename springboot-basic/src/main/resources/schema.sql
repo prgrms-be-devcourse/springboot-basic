ced474ba4cf0dd0762fea953116bf88ab632b50f
baejunil <bjo6300@naver.com>
2023-07-06 22:26:07 +0900

    feat : voucher 테이블 정의

--- springboot-basic/src/main/resources/schema.sql
+++ springboot-basic/src/main/resources/schema.sql
@@ -5,3 +5,12 @@ create table customer
     customer_name  varchar(30) not null,
     customer_email varchar(50) not null
 );
+
+create table vouchers
+(
+    voucher_id  binary(10) primary key,
+    discount    integer(10) NOT NULL,
+    type        varchar(20) NOT NULL,
+    customer_id binary(10),
+    foreign key (customer_id) references customer (customer_id)
+);
