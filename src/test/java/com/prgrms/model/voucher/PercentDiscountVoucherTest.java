8db67ed0334b597183c3e46f16edd4c8bcd206d4
byeolhaha <j35635aa@gmail.com>
2023-07-06 15:09:49 +0900

    test : 할인율에 따라 세일된 금액이 나오는지 테스트 구현

--- src/test/java/com/prgrms/model/voucher/PercentDiscountVoucherTest.java
+++ src/test/java/com/prgrms/model/voucher/PercentDiscountVoucherTest.java
@@ -1,20 +1,34 @@
 package com.prgrms.model.voucher;
 
 import com.prgrms.model.order.OrderItem;
+import com.prgrms.model.voucher.discount.Discount;
+import com.prgrms.model.voucher.discount.PercentDiscount;
+import com.prgrms.model.voucher.discount.Price;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class PercentDiscountVoucherTest {
-    @Test
-    public void salePriceBiggerThanDiscountTest() {
-        UUID voucherId = UUID.randomUUID();
-        Voucher createdVoucher = new PercentDiscountVoucher(voucherId, new Discount(20), VoucherType.FIXED_AMOUNT_VOUCHER);
-        OrderItem orderItem = new OrderItem(UUID.randomUUID(), 1000, 1);
-        long price = 1000;
+    private OrderItem orderItem;
 
-        assertEquals(800, createdVoucher.getRealPrice(orderItem));
+    @BeforeEach
+    public void setUp() {
+        orderItem = new OrderItem(UUID.randomUUID(), 1000, 1);
+    }
+    @Test
+    @DisplayName("할인된 금액이 제대로 나오는지 확인한다.")
+    public void discountPrice_DiscountedPrice_Equal() {
+        //given
+        Voucher createdVoucher = new PercentDiscountVoucher(UUID.randomUUID(), new PercentDiscount(20), VoucherType.FIXED_AMOUNT_VOUCHER);
+        OrderItem orderItem = new OrderItem(UUID.randomUUID(), 1000, 1);
+        //when
+        Price discountedPrice = createdVoucher.discountPrice(orderItem);
+        //then
+        assertThat(discountedPrice.getValue()).isEqualTo(800);
     }
 }
