7434e6f2b9bd256dd536530140ea267dab6fb05c
byeolhaha <j35635aa@gmail.com>
2023-07-06 14:42:33 +0900

    feat : 에러 메시지 Enum으로 분리

---
+++ src/main/java/com/prgrms/io/ErrorMessage.java
@@ -0,0 +1,18 @@
+package com.prgrms.io;
+
+public enum ErrorMessage {
+
+    INVALID_SELECTION("올바른 선택지가 아닙니다."),
+    Negative_ARGUMENT ("양수여야 합니다.");
+
+    private final String message;
+
+    ErrorMessage(String message){
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+}
