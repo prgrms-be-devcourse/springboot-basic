595b3e222a54e9e1c750e394e8f508d42d402e7f
sunny <rosa7536@naver.com>
2023-07-06 14:50:59 +0900

    [Feat] Customer Service

---
+++ src/main/java/com/prgrms/spring/controller/dto/request/CustomerCreateRequestDto.java
@@ -0,0 +1,14 @@
+package com.prgrms.spring.controller.dto.request;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class CustomerCreateRequestDto {
+    private String name;
+    private String email;
+}
--- src/main/java/com/prgrms/spring/service/customer/CustomerService.java
+++ src/main/java/com/prgrms/spring/service/customer/CustomerService.java
@@ -1,8 +1,15 @@
 package com.prgrms.spring.service.customer;
 
+import com.prgrms.spring.controller.dto.request.CustomerCreateRequestDto;
+import com.prgrms.spring.domain.customer.Customer;
 import com.prgrms.spring.repository.customer.CustomerRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.UUID;
 
 @Service
 @AllArgsConstructor
@@ -10,6 +17,21 @@ public class CustomerService {
 
     private final CustomerRepository customerRepository;
 
-    
+    @Transactional
+    public Customer createCustomer(CustomerCreateRequestDto customerCreateRequestDto) {
+        var newCustomer = Customer.newInstance(UUID.randomUUID(), customerCreateRequestDto.getName(), customerCreateRequestDto.getEmail(), LocalDateTime.now());
+        customerRepository.insert(newCustomer);
+        return newCustomer;
+    }
 
+    @Transactional
+    public Customer getCustomerById(UUID customerId) {
+        return customerRepository.findById(customerId)
+                .orElseThrow(() -> new RuntimeException("고객 조회 실패"));
+    }
+
+    @Transactional
+    public Collection<Customer> getAllCustomers() {
+        return customerRepository.findAll();
+    }
 }
