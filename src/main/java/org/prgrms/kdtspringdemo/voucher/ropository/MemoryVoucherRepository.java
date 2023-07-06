acbe5f5e8bd88f02d6b3a4b17a829ac250b4fc6d
beombu <beombu13@gmail.com>
2023-07-07 04:31:48 +0900

    Refactor : repository findById 삭제

--- src/main/java/org/prgrms/kdtspringdemo/voucher/ropository/MemoryVoucherRepository.java
+++ src/main/java/org/prgrms/kdtspringdemo/voucher/ropository/MemoryVoucherRepository.java
@@ -7,18 +7,12 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Repository
 public class MemoryVoucherRepository implements VoucherRepository {
     private final Map<UUID, Voucher> storage = new ConcurrentHashMap<>();
-
-    @Override
-    public Optional<Voucher> findById(UUID voucherId) {
-        return Optional.ofNullable(storage.get(voucherId));
-    }
 
     @Override
     public Voucher save(Voucher voucher) {
--- src/test/java/org/prgrms/kdtspringdemo/voucher/ropository/VoucherRepositoryTest.java
+++ src/test/java/org/prgrms/kdtspringdemo/voucher/ropository/VoucherRepositoryTest.java
@@ -24,23 +24,19 @@ public class VoucherRepositoryTest {
         //when
         FixedAmountVoucher fixedAmountVoucher = new FixedAmountVoucher(discount);
         Voucher savedVoucher = voucherRepository.save(fixedAmountVoucher);
-        Voucher findFixed = voucherRepository.findById(fixedAmountVoucher.getVoucherId()).get();
 
         //then
-        Assertions.assertThat(savedVoucher.toString()).isEqualTo(findFixed.toString());
+        Assertions.assertThat(savedVoucher).isEqualTo(savedVoucher);
     }
 
     @Test
-    void 바우처_저장_실패_테스트() {
+    void 동일한_id_바우처_저장_실패_테스트() {
         //given
         long amount1 = 1000;
-        long amount2 = 2000;
         FixedAmountVoucher fixedAmountVoucher1 = new FixedAmountVoucher(amount1);
-        FixedAmountVoucher fixedAmountVoucher2 = new FixedAmountVoucher(amount2);
-
         //when
         voucherRepository.save(fixedAmountVoucher1);
-        voucherRepository.save(fixedAmountVoucher2);
+        voucherRepository.save(fixedAmountVoucher1);
         List<Voucher> voucherList = voucherRepository.findAll();
 
         //then
