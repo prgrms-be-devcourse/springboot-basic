adecc70db9aa460d1cefcc5065604ab2175f8719
kmebin <blowhuiee@gmail.com>
2023-07-06 22:41:10 +0900

    test: mockito를 통한 VoucherService 테스트

--- src/test/java/com/programmers/voucher/service/VoucherServiceTest.java
+++ src/test/java/com/programmers/voucher/service/VoucherServiceTest.java
@@ -1,50 +1,106 @@
 package com.programmers.voucher.service;
 
-import com.programmers.voucher.domain.FixedAmountVoucher;
-import com.programmers.voucher.domain.PercentDiscountVoucher;
-import com.programmers.voucher.domain.Voucher;
-import com.programmers.voucher.repository.MemoryVoucherRepository;
-import com.programmers.voucher.view.dto.DiscountAmount;
-import com.programmers.voucher.view.dto.VoucherType;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import com.programmers.voucher.controller.voucher.dto.VoucherCreateRequest;
+import com.programmers.voucher.controller.voucher.dto.VoucherResponse;
+import com.programmers.voucher.controller.voucher.dto.VoucherUpdateRequest;
+import com.programmers.voucher.entity.voucher.Voucher;
+import com.programmers.voucher.entity.voucher.VoucherType;
+import com.programmers.voucher.repository.voucher.VoucherRepository;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.stream.Stream;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.params.provider.Arguments.arguments;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 
+@ExtendWith(MockitoExtension.class)
 class VoucherServiceTest {
-    VoucherService voucherService;
-    MemoryVoucherRepository voucherRepository;
+    @Mock
+    private VoucherRepository voucherRepository;
+    @InjectMocks
+    private VoucherService voucherService;
 
-    @BeforeEach
-    void beforeEach() {
-        voucherRepository = new MemoryVoucherRepository();
-        voucherService = new VoucherService(voucherRepository);
+    @Test
+    @DisplayName("바우처 생성에 성공한다.")
+    void 바우처_생성_성공() {
+        // given
+        VoucherCreateRequest request = new VoucherCreateRequest(VoucherType.FIXED, 100);
+        Voucher voucher = Voucher.create(VoucherType.FIXED, 100);
+        given(voucherRepository.insert(any(Voucher.class))).willReturn(voucher);
+
+        // when
+        VoucherResponse response = voucherService.createVoucher(request);
+
+        // then
+        assertThat(response.voucherId()).isEqualTo(voucher.getId());
     }
 
-    @AfterEach
-    void afterEach() {
-        voucherRepository.clear();
+    @Test
+    @DisplayName("모든 바우처 조회에 성공한다.")
+    void 모든_바우처_조회_성공() {
+        // given
+        Voucher voucher1 = Voucher.create(VoucherType.FIXED, 100);
+        Voucher voucher2 = Voucher.create(VoucherType.PERCENT, 20);
+        List<Voucher> vouchers = List.of(voucher1, voucher2);
+        given(voucherRepository.findAll()).willReturn(vouchers);
+
+        // when
+        List<VoucherResponse> response = voucherService.getAllVouchers();
+
+        // then
+        assertThat(response).hasSize(2);
     }
 
-    @ParameterizedTest
-    @DisplayName("바우처 타입에 따른 바우처 생성에 성공한다.")
-    @MethodSource("createVoucherArguments")
-    void 바우처_생성(VoucherType voucherType, long amount, Class<? extends Voucher> expected) {
-        Voucher voucher = voucherService.createVoucher(voucherType, new DiscountAmount(voucherType, amount));
-        assertThat(voucher.getClass()).isEqualTo(expected);
+    @Test
+    @DisplayName("바우처 조회에 성공한다.")
+    void 바우처_조회_성공() {
+        // given
+        Voucher voucher = Voucher.create(VoucherType.FIXED, 100);
+        given(voucherRepository.findById(voucher.getId())).willReturn(Optional.of(voucher));
+
+        // when
+        VoucherResponse response = voucherService.getVoucher(voucher.getId());
+
+        // then
+        assertThat(response.voucherType()).isEqualTo(voucher.getType());
+        assertThat(response.amount()).isEqualTo(voucher.getAmount());
     }
 
-    private static Stream<Arguments> createVoucherArguments() {
-        return Stream.of(
-                arguments(VoucherType.FIXED_AMOUNT, 3000L, FixedAmountVoucher.class),
-                arguments(VoucherType.PERCENT_DISCOUNT, 30L, PercentDiscountVoucher.class)
-        );
+    @Test
+    @DisplayName("존재하지 않는 바우처를 조회한 경우 예외가 발생한다.")
+    void 바우처_조회_예외() {
+        // given
+        UUID voucherId = UUID.randomUUID();
+        given(voucherRepository.findById(voucherId)).willReturn(Optional.empty());
+
+        // when & then
+        assertThatThrownBy(() -> voucherService.getVoucher(voucherId))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("존재하는 바우처가 없습니다.");
+    }
+
+    @Test
+    @DisplayName("바우처 수정에 성공한다.")
+    void 바우처_수정_성공() {
+        // given
+        VoucherUpdateRequest request = new VoucherUpdateRequest(VoucherType.PERCENT, 20);
+        Voucher voucher = Voucher.create(VoucherType.FIXED, 100);
+        given(voucherRepository.findById(voucher.getId())).willReturn(Optional.of(voucher));
+
+        // when
+        VoucherResponse response = voucherService.updateVoucher(voucher.getId(), request);
+
+        // then
+        assertThat(response.voucherType()).isEqualTo(VoucherType.PERCENT);
+        assertThat(response.amount()).isEqualTo(20);
     }
 }
