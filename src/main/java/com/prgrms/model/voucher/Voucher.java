6d44ce1462f656203c9fdc7ac6eca05aada70b45
byeolhaha <j35635aa@gmail.com>
2023-07-06 11:48:38 +0900

    style : 의미가 잘 드러나도록 메서드명 변경

--- src/main/java/com/prgrms/model/voucher/Voucher.java
+++ src/main/java/com/prgrms/model/voucher/Voucher.java
@@ -1,13 +1,14 @@
 package com.prgrms.model.voucher;
 
 import com.prgrms.model.order.OrderItem;
+import com.prgrms.model.voucher.discount.Discount;
 
 import java.util.UUID;
 
 public abstract class Voucher {
-    public UUID voucherId;
-    public Discount discount;
-    public VoucherType voucherType;
+    private final UUID voucherId;
+    private final Discount discount;
+    private final VoucherType voucherType;
 
     public Voucher(UUID voucherId, Discount discount, VoucherType voucherType) {
         this.voucherId = voucherId;
@@ -27,7 +28,7 @@ public abstract class Voucher {
         return voucherType;
     }
 
-    public double getRealPrice(OrderItem orderItem) {
+    public double getDiscountedPrice(OrderItem orderItem) {
         return orderItem.productPrice() - sale(orderItem.productPrice());
     }
 
