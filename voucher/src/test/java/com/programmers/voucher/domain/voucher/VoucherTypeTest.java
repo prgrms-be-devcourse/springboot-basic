c6413bd45c4fa161266d1c5e954ab1142428444a
BeomSeogKim <kbs4520@daum.net>
2023-07-06 10:24:51 +0900

    @ValueSource -> @CsvSource 변경 및 테스트 코드 순서 명시화

--- voucher/src/test/java/com/programmers/voucher/domain/voucher/VoucherTypeTest.java
+++ voucher/src/test/java/com/programmers/voucher/domain/voucher/VoucherTypeTest.java
@@ -4,6 +4,7 @@ import com.programmers.voucher.domain.enums.VoucherType;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.Optional;
@@ -14,18 +15,22 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 class VoucherTypeTest {
 
     @ParameterizedTest
-    @ValueSource(ints = {1, 2})
-    void decideVoucherType(int number) {
-        VoucherType voucherType = VoucherType.decideVoucherType(number).get();
-        switch (voucherType) {
-            case FIXED -> assertThat(voucherType).isSameAs(VoucherType.FIXED);
-            case PERCENT -> assertThat(voucherType).isSameAs(VoucherType.PERCENT);
-        }
+    @CsvSource({"1,FIXED", "2,PERCENT"})
+    void decideVoucherType(int number, VoucherType expeceted) {
+        VoucherType actual = VoucherType.decideVoucherType(number).get();
+        assertThat(actual).isEqualTo(expeceted);
     }
 
     @Test
-    @DisplayName("유요하지 않은 숫자가 들어올 경우 Optional.empty()를 반환한다.")
+    @DisplayName("유효하지 않은 숫자가 들어올 경우 Optional.empty()를 반환한다.")
     void decideVoucherTypeEmpty() {
-        assertThat(VoucherType.decideVoucherType(100)).isEqualTo(Optional.empty());
+        // given
+        int number = 100;
+
+        // when
+        Optional<VoucherType> optionalVoucherType = VoucherType.decideVoucherType(number);
+
+        // then
+        assertThat(optionalVoucherType).isEmpty();
     }
 }
