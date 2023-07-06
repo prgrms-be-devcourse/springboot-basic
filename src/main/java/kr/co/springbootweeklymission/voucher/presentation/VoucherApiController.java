fd4adf0956a5772d6f26c962d49c52639e1eb79a
HyuckJuneHong <jjj8337721@naver.com>
2023-07-07 01:17:19 +0900

    feat : 특정 바우처를 조회하는 기능 구현

--- src/main/java/kr/co/springbootweeklymission/voucher/presentation/VoucherApiController.java
+++ src/main/java/kr/co/springbootweeklymission/voucher/presentation/VoucherApiController.java
@@ -4,12 +4,12 @@ import kr.co.springbootweeklymission.common.response.ResponseFormat;
 import kr.co.springbootweeklymission.common.response.ResponseStatus;
 import kr.co.springbootweeklymission.voucher.application.VoucherService;
 import kr.co.springbootweeklymission.voucher.presentation.dto.request.VoucherReqDTO;
+import kr.co.springbootweeklymission.voucher.presentation.dto.response.VoucherResDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,5 +21,10 @@ public class VoucherApiController {
     public ResponseFormat<Void> createVoucher(@RequestBody @Validated VoucherReqDTO.CREATE create) {
         voucherService.createVoucher(create);
         return ResponseFormat.success(ResponseStatus.SUCCESS_CREATE);
+    }
+
+    @GetMapping("/{voucher_id}")
+    public ResponseFormat<VoucherResDTO.READ> getVoucherById(@PathVariable(name = "voucher_id") UUID voucherId) {
+        return ResponseFormat.successWithData(ResponseStatus.SUCCESS_OK, voucherService.getVoucherById(voucherId));
     }
 }
