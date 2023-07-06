955655dca258ff65fdeb2d84d15939569325c7bf
baejunil <bjo6300@naver.com>
2023-07-06 16:54:45 +0900

    feat : customer model 생성

---
+++ springboot-basic/src/main/java/com/example/commandlineapplication/domain/customer/model/Customer.java
@@ -0,0 +1,39 @@
+package com.example.commandlineapplication.domain.customer.model;
+
+import java.util.UUID;
+
+public class Customer {
+
+  private final UUID customerId;
+  private String name;
+  private final String email;
+
+  public Customer(UUID customerId, String name, String email) {
+    this.customerId = customerId;
+    this.name = name;
+    this.email = email;
+  }
+
+  public UUID getCustomerId() {
+    return this.customerId;
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public String getEmail() {
+    return this.email;
+  }
+
+  public String updateName(String newName) {
+    this.name = newName;
+    return newName;
+  }
+
+  private void checkName(String name) {
+    if (name.isBlank()) {
+      throw new RuntimeException("이름은 빈칸이 될 수 없습니다.");
+    }
+  }
+}
