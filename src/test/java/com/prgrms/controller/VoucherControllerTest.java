c1e47b5f8220ed563c6960a1107492750aee8085
byeolhaha <j35635aa@gmail.com>
2023-07-06 15:24:18 +0900

    test : assertj 적용 및 테스트명 컨벤션 통일

--- src/test/java/com/prgrms/controller/VoucherControllerTest.java
+++ src/test/java/com/prgrms/controller/VoucherControllerTest.java
@@ -1,9 +1,12 @@
 package com.prgrms.controller;
 
+import com.prgrms.model.dto.mapper.DtoConverter;
 import com.prgrms.model.dto.VoucherRequest;
+import com.prgrms.model.dto.VoucherResponse;
 import com.prgrms.model.voucher.*;
+import com.prgrms.model.voucher.discount.FixedDiscount;
+import com.prgrms.model.voucher.discount.PercentDiscount;
 import com.prgrms.service.voucher.VoucherService;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -14,12 +17,17 @@ import org.mockito.MockitoAnnotations;
 import java.util.List;
 import java.util.UUID;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class VoucherControllerTest {
 
     @Mock
     private VoucherService voucherService;
 
     private VoucherController voucherController;
+
+    private Voucher createdVoucher1;
+    private Voucher createdVoucher2;
     private UUID voucherId1;
     private UUID voucherId2;
 
@@ -30,55 +38,64 @@ public class VoucherControllerTest {
         voucherId1 = UUID.randomUUID();
         voucherId2 = UUID.randomUUID();
 
+        createdVoucher1 = new FixedAmountVoucher(voucherId1, new FixedDiscount(20), VoucherType.FIXED_AMOUNT_VOUCHER);
+        createdVoucher2 = new PercentDiscountVoucher(voucherId2, new PercentDiscount(20), VoucherType.PERCENT_DISCOUNT_VOUCHER);
+
+
     }
 
     @Test
-    public void testCreateVoucher() {
+    @DisplayName("값 누락 없이 바우처 정책을 잘 만드는지 테스트한다.")
+    public void CreateVoucher_CreatedVoucher_Equal() {
         // Given
-        VoucherRequest voucherRequest = new VoucherRequest(VoucherType.FIXED_AMOUNT_VOUCHER, new Discount(20));
-        Voucher createdVoucher = new FixedAmountVoucher(voucherId1, new Discount(20), VoucherType.FIXED_AMOUNT_VOUCHER);
+        VoucherRequest voucherRequest = new VoucherRequest(VoucherType.FIXED_AMOUNT_VOUCHER, new FixedDiscount(20));
+        Voucher createdVoucher = new FixedAmountVoucher(voucherId1, new FixedDiscount(20), VoucherType.FIXED_AMOUNT_VOUCHER);
         Mockito.when(voucherService.createVoucher(voucherRequest)).thenReturn(createdVoucher);
 
         // When
         Voucher result = voucherController.createVoucher(voucherRequest);
 
         // Then
-        Assertions.assertEquals(createdVoucher, result);
+        assertThat(result).isEqualTo(createdVoucher);
         Mockito.verify(voucherService, Mockito.times(1)).createVoucher(voucherRequest);
     }
 
 
     @Test
     @DisplayName("voucherId 가 다른 두 바우처 정책의 리스트를 잘 만드는지 테스트합니다.")
-    public void testListVoucher_DifferentVoucherId() {
+    public void ListVoucher_DifferentVoucherId_Equal() {
         // Given
-        Voucher createdVoucher1 = new FixedAmountVoucher(voucherId1, new Discount(20), VoucherType.FIXED_AMOUNT_VOUCHER);
-        Voucher createdVoucher2 = new PercentDiscountVoucher(voucherId2, new Discount(20), VoucherType.PERCENT_DISCOUNT_VOUCHER);
         List<Voucher> list = List.of(createdVoucher1, createdVoucher2);
         VoucherRegistry voucherRegistry = new VoucherRegistry(list);
-        Mockito.when(voucherService.getAllVoucherList()).thenReturn(voucherRegistry);
+
+        DtoConverter dtoConverter = new DtoConverter();
+        List<VoucherResponse> expected = dtoConverter.convertVoucherResponse(voucherRegistry);
+        Mockito.when(voucherService.getAllVoucherList()).thenReturn(expected);
 
         // When
-        VoucherRegistry result = voucherController.listVoucher();
+        List<VoucherResponse> result = voucherController.listVoucher();
 
         // Then
-        Assertions.assertEquals(voucherRegistry, result);
+        assertThat(result).isEqualTo(expected);
         Mockito.verify(voucherService, Mockito.times(1)).getAllVoucherList();
     }
 
     @Test
     @DisplayName("voucherId 가 같은 두 바우처 정책의 리스트를 잘 만드는지 테스트합니다. 통과되므로 추후에 중복의 경우를 만들지 못하도록 예외로")
-    public void testListVoucher_SameVoucherId() {
+    public void ListVoucher_SameVoucherId_Equal() {
         // Given
-        Voucher createdVoucher1 = new FixedAmountVoucher(voucherId1, new Discount(20), VoucherType.FIXED_AMOUNT_VOUCHER);
-        Voucher createdVoucher2 = new PercentDiscountVoucher(voucherId1, new Discount(20), VoucherType.PERCENT_DISCOUNT_VOUCHER);
         List<Voucher> list = List.of(createdVoucher1, createdVoucher2);
         VoucherRegistry voucherRegistry = new VoucherRegistry(list);
-        Mockito.when(voucherService.getAllVoucherList()).thenReturn(voucherRegistry);
+
+        DtoConverter dtoConverter = new DtoConverter();
+        List<VoucherResponse> expected = dtoConverter.convertVoucherResponse(voucherRegistry);
+        Mockito.when(voucherService.getAllVoucherList()).thenReturn(expected);
+
         // When
-        VoucherRegistry result = voucherController.listVoucher();
+        List<VoucherResponse> result = voucherController.listVoucher();
+
         // Then
-        Assertions.assertEquals(voucherRegistry, result);
+        assertThat(result).isEqualTo(expected);
         Mockito.verify(voucherService, Mockito.times(1)).getAllVoucherList();
     }
 }
