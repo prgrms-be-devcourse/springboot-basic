e4fd26c2054a745a28fab987380d153a92d6cc12
byeolhaha <j35635aa@gmail.com>
2023-07-06 15:46:01 +0900

    test : 바우처 서비스 테스트 구현

--- src/test/java/com/prgrms/service/voucher/VoucherServiceTest.java
+++ src/test/java/com/prgrms/service/voucher/VoucherServiceTest.java
@@ -1,9 +1,16 @@
 package com.prgrms.service.voucher;
 
+import com.prgrms.model.dto.mapper.DtoConverter;
 import com.prgrms.model.dto.VoucherRequest;
+import com.prgrms.model.dto.VoucherResponse;
 import com.prgrms.model.voucher.*;
+import com.prgrms.model.voucher.discount.Discount;
+import com.prgrms.model.voucher.discount.FixedDiscount;
+import com.prgrms.model.voucher.discount.PercentDiscount;
 import com.prgrms.repository.voucher.VoucherRepository;
+import com.prgrms.model.voucher.VoucherCreator;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
@@ -11,8 +18,8 @@ import org.mockito.MockitoAnnotations;
 import java.util.List;
 import java.util.UUID;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
-import static org.junit.jupiter.api.Assertions.*;
 
 class VoucherServiceTest {
 
@@ -21,6 +28,8 @@ class VoucherServiceTest {
 
     @Mock
     private VoucherCreator voucherCreator;
+    @Mock
+    private DtoConverter dtoConverter;
 
     private VoucherService voucherService;
     private UUID voucherId1;
@@ -29,43 +38,54 @@ class VoucherServiceTest {
     @BeforeEach
     void setUp() {
         MockitoAnnotations.openMocks(this);
-        voucherService = new VoucherService(voucherRepository, voucherCreator);
+        voucherService = new VoucherService(voucherRepository, voucherCreator, dtoConverter);
         voucherId1 = UUID.randomUUID();
         voucherId2 = UUID.randomUUID();
 
     }
 
     @Test
-    public void testCreateVoucherCreatedVoucher() {
-        VoucherRequest voucherRequest = new VoucherRequest(VoucherType.FIXED_AMOUNT_VOUCHER, new Discount(10));
-        Voucher createdVoucher = new FixedAmountVoucher(voucherId1, new Discount(20), VoucherType.FIXED_AMOUNT_VOUCHER);
-        ;
-
+    @DisplayName("전달받은 바우처를 레파지토리에 잘 전달하여 저장소에 저장되는지 확인한다.")
+    public void createVoucher_RepositoryInsertVoucher_Equals() {
+        //given
+        VoucherRequest voucherRequest = new VoucherRequest(VoucherType.FIXED_AMOUNT_VOUCHER, new FixedDiscount(10));
+        Voucher createdVoucher = new FixedAmountVoucher(voucherId1, new FixedDiscount(20), VoucherType.FIXED_AMOUNT_VOUCHER);
         when(voucherCreator.createVoucher(any(Discount.class), any(VoucherType.class)))
                 .thenReturn(createdVoucher);
         when(voucherRepository.insert(any(Voucher.class))).thenReturn(createdVoucher);
 
+        //when
         Voucher result = voucherService.createVoucher(voucherRequest);
 
-        assertNotNull(result);
-        assertEquals(createdVoucher, result);
+        //then
+        assertThat(result)
+                .isNotNull()
+                .isEqualTo(createdVoucher);
         verify(voucherCreator, times(1)).createVoucher(any(Discount.class), any(VoucherType.class));
         verify(voucherRepository, times(1)).insert(any(Voucher.class));
     }
 
     @Test
-    public void testGetAllVoucherListVoucherList() {
-        Voucher createdVoucher1 = new FixedAmountVoucher(voucherId1, new Discount(20), VoucherType.FIXED_AMOUNT_VOUCHER);
-        Voucher createdVoucher2 = new PercentDiscountVoucher(voucherId2, new Discount(20), VoucherType.PERCENT_DISCOUNT_VOUCHER);
+    @DisplayName("저장된 바우처 정책을 잘 출력하는지 확인한다.")
+    public void getAllVoucherList_RepositoryListVoucherList_Equals() {
+        //given
+        Voucher createdVoucher1 = new FixedAmountVoucher(voucherId1, new FixedDiscount(20), VoucherType.FIXED_AMOUNT_VOUCHER);
+        Voucher createdVoucher2 = new PercentDiscountVoucher(voucherId2, new PercentDiscount(20), VoucherType.PERCENT_DISCOUNT_VOUCHER);
+        VoucherResponse voucherResponse1 = new VoucherResponse(createdVoucher1);
+        VoucherResponse voucherResponse2 = new VoucherResponse(createdVoucher2);
+        List<VoucherResponse> expected = List.of(voucherResponse1, voucherResponse2);
         List<Voucher> list = List.of(createdVoucher1, createdVoucher2);
         VoucherRegistry voucherRegistry = new VoucherRegistry(list);
+        when(voucherRepository.getAllVoucher()).thenReturn(voucherRegistry);
+        when(dtoConverter.convertVoucherResponse(any(VoucherRegistry.class))).thenReturn(expected);
 
-        when(voucherRepository.getAllVoucherList()).thenReturn(voucherRegistry);
+        //when
+        List<VoucherResponse> result = voucherService.getAllVoucherList();
 
-        VoucherRegistry result = voucherService.getAllVoucherList();
-
-        assertNotNull(result);
-        assertEquals(voucherRegistry, result);
-        verify(voucherRepository, times(1)).getAllVoucherList();
+        //then
+        assertThat(result)
+                .isNotNull()
+                .containsOnly(voucherResponse1,voucherResponse2);
+        verify(voucherRepository, times(1)).getAllVoucher();
     }
 }
