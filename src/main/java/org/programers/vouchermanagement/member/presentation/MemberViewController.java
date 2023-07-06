9e3d5537e2e969a9af5df141906718f747848750
player-geun <player_geun@naver.com>
2023-07-06 16:59:09 +0900

    feat : 회원을 조회, 상세, 입력 페이지를 구현

---
+++ src/main/java/org/programers/vouchermanagement/member/presentation/MemberViewController.java
@@ -0,0 +1,54 @@
+package org.programers.vouchermanagement.member.presentation;
+
+import lombok.RequiredArgsConstructor;
+import org.programers.vouchermanagement.member.application.MemberService;
+import org.programers.vouchermanagement.member.dto.request.MemberCreationRequest;
+import org.programers.vouchermanagement.member.dto.response.MemberResponse;
+import org.programers.vouchermanagement.member.dto.response.MembersResponse;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.UUID;
+
+@RequestMapping("/view/members")
+@RequiredArgsConstructor
+@Controller
+public class MemberViewController {
+
+    private final MemberService memberService;
+
+    @GetMapping("/add")
+    public String save(Model model) {
+        model.addAttribute("member", new MemberResponse());
+        return "members/addMember";
+    }
+
+    @PostMapping("/add")
+    public String save(@ModelAttribute MemberCreationRequest request, RedirectAttributes redirectAttributes) {
+        MemberResponse response = memberService.save(request);
+        redirectAttributes.addAttribute("id", response.getId());
+        return "redirect:/view/members/{id}";
+    }
+
+    @GetMapping("/{id}")
+    public String findById(@PathVariable UUID id, Model model) {
+        MemberResponse response = memberService.findById(id);
+        model.addAttribute("member", response);
+        return "members/member";
+    }
+
+    @GetMapping("/all")
+    public String findAll(Model model) {
+        MembersResponse response = memberService.findAll();
+        model.addAttribute("members", response);
+        return "members/members";
+    }
+
+    @PostMapping("/{id}")
+    public String deleteById(@PathVariable UUID id) {
+        memberService.deleteById(id);
+        return "redirect:/view/members/all";
+    }
+}
