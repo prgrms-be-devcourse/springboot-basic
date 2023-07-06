d307811bed22c2869a95e621c83f9b07109f3977
SW-H <hsw08@naver.com>
2023-07-06 15:06:20 +0900

    feat : voucherDTO -> voucher 생성은 voucher 클래스 내에서 하도록 수정

--- src/main/java/co/programmers/voucher_management/voucher/entity/Voucher.java
+++ src/main/java/co/programmers/voucher_management/voucher/entity/Voucher.java
@@ -1,23 +1,53 @@
 package co.programmers.voucher_management.voucher.entity;
 
+import java.time.LocalDateTime;
+
+import co.programmers.voucher_management.exception.VoucherReassignmentException;
+import co.programmers.voucher_management.voucher.dto.VoucherRequestDTO;
+import co.programmers.voucher_management.voucher.dto.VoucherUpdateDTO;
+import co.programmers.voucher_management.voucher.service.DiscountTypeGenerator;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class Voucher {
 	private long id;
 	private DiscountStrategy discountStrategy;
+	private long customerId;
 	private char status;
-
-	public Voucher(DiscountStrategy discountStrategy) {
-		this.discountStrategy = discountStrategy;
-	}
+	private LocalDateTime createdAt;
+	private LocalDateTime updatedAt;
 
 	@Builder
-	public Voucher(int id, DiscountStrategy discountStrategy) {
+	public Voucher(long id, DiscountStrategy discountStrategy) {
 		this.id = id;
 		this.discountStrategy = discountStrategy;
 		status = STATUS.NORMAL.symbol;
+	}
+
+	public Voucher(VoucherRequestDTO voucherRequestDTO) {
+		String discountType = voucherRequestDTO.getDiscountType();
+		int discountAmount = voucherRequestDTO.getDiscountAmount();
+		DiscountStrategy discountStrategy = DiscountTypeGenerator.of(discountType, discountAmount);
+		this.discountStrategy = discountStrategy;
+	}
+
+	public Voucher(VoucherUpdateDTO voucherUpdateDTO) {
+		long id = voucherUpdateDTO.getId();
+		String discountType = voucherUpdateDTO.getDiscountType();
+		int discountAmount = voucherUpdateDTO.getDiscountAmount();
+		DiscountStrategy discountStrategy = DiscountTypeGenerator.of(discountType, discountAmount);
+		this.discountStrategy = discountStrategy;
+		this.id = id;
+	}
+
+	public void assignCustomer(long customerId) {
+		if (customerId > 0) {
+			throw new VoucherReassignmentException("Already assigned voucher");
+		}
+		this.customerId = customerId;
 	}
 
 	public void delete() {
