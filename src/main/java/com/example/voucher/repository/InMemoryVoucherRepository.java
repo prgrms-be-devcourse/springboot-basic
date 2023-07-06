e2721ecebfdbb567a7265e5cafe65bbbdd3b36e0
bumwoo <gld55602@gmail.com>
2023-07-06 17:21:33 +0900

    feat: repository findAll 메소드 추가

--- src/main/java/com/example/voucher/repository/InMemoryVoucherRepository.java
+++ src/main/java/com/example/voucher/repository/InMemoryVoucherRepository.java
@@ -1,13 +1,13 @@
 package com.example.voucher.repository;
 
 import com.example.voucher.domain.Voucher;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public class InMemoryVoucherRepository implements VoucherRepository {
 
     private final Map<UUID, Voucher> storage = new ConcurrentHashMap<>();
@@ -16,6 +16,12 @@ public class InMemoryVoucherRepository implements VoucherRepository {
     public Optional<Voucher> findById(UUID voucherId) {
         return Optional.ofNullable(storage.get(voucherId));
     }
+
+    @Override
+    public List<Voucher> findAll() {
+        return new ArrayList<>(storage.values());
+    }
+
 
     @Override
     public Voucher insert(Voucher voucher) {
--- src/main/java/com/example/voucher/repository/VoucherRepository.java
+++ src/main/java/com/example/voucher/repository/VoucherRepository.java
@@ -1,10 +1,12 @@
 package com.example.voucher.repository;
 
 import com.example.voucher.domain.Voucher;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface VoucherRepository {
     Optional<Voucher> findById(UUID voucherId);
+    List<Voucher> findAll();
     Voucher insert(Voucher voucher);
 }
