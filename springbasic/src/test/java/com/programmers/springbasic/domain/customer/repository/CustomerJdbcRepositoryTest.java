3f0a786586a125dbb7feebd4347c408b2857e426
hi-june <wnsgur735@gmail.com>
2023-07-06 22:15:01 +0900

    tmp commit

---
+++ springbasic/src/test/java/com/programmers/springbasic/domain/customer/repository/CustomerJdbcRepositoryTest.java
@@ -0,0 +1,103 @@
+package com.programmers.springbasic.domain.customer.repository;
+
+import com.programmers.springbasic.domain.customer.entity.Customer;
+import com.wix.mysql.EmbeddedMysql;
+import com.wix.mysql.config.MysqldConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+import javax.sql.DataSource;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+import java.util.UUID;
+
+import static com.wix.mysql.EmbeddedMysql.anEmbeddedMysql;
+import static com.wix.mysql.ScriptResolver.classPathScript;
+import static com.wix.mysql.config.Charset.UTF8;
+import static com.wix.mysql.config.MysqldConfig.aMysqldConfig;
+import static com.wix.mysql.distribution.Version.v8_0_11;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.samePropertyValuesAs;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringJUnitConfig
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class CustomerJdbcRepositoryTest {
+
+    @Configuration
+    @ComponentScan(basePackages = {"com.programmers.springbasic.domain.customer.repository"})
+    static class Config {
+        @Bean
+        public DataSource dataSource() {
+            var dataSource = DataSourceBuilder.create()
+                    .url("jdbc:mysql://localhost:2215/testdb")
+                    .username("test")
+                    .password("test1234!")
+                    .type(HikariDataSource.class)
+                    .build();
+
+            return dataSource;
+        }
+
+        @Bean
+        public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+            return new JdbcTemplate(dataSource);
+        }
+    }
+
+    @Autowired
+    CustomerJdbcRepository customerJdbcRepository;
+
+    @Autowired
+    DataSource dataSource;
+
+    EmbeddedMysql embeddedMysql;
+
+    Customer newCustomer;
+
+    @BeforeAll
+    void setUp() {
+        MysqldConfig mysqlConfig = aMysqldConfig(v8_0_11)
+                .withCharset(UTF8)
+                .withPort(2215)
+                .withUser("test", "test1234!")
+                .withTimeZone("Asia/Seoul")
+                .build();
+
+        embeddedMysql = anEmbeddedMysql(mysqlConfig)
+                .addSchema("test-order_mgmt", classPathScript("schema.sql"))
+                .start();
+
+        newCustomer = new Customer(UUID.randomUUID(), "test", "test@gmail.com");
+    }
+
+    @AfterAll
+    void cleanUp() {
+        embeddedMysql.stop();
+    }
+
+    @Test
+    @Order(1)
+    public void testHikariConnectionPool() {
+        assertThat(dataSource.getClass().getName(), is("com.zaxxer.hikari.HikariDataSource"));
+    }
+
+    @Test
+    @Order(2)
+    void save() {
+        customerJdbcRepository.save(newCustomer);
+        Optional<Customer> retrievedCustomer = customerJdbcRepository.findById(newCustomer.getCustomerId());
+
+        assertThat(retrievedCustomer.isEmpty(), is(true));
+        // assertThat(retrievedCustomer.get(), samePropertyValuesAs(newCustomer));
+    }
+}
