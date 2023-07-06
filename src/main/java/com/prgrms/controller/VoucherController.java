b376915b3f4110f9f4d68e2b5761b251047cb911
byeolhaha <j35635aa@gmail.com>
2023-07-06 15:20:17 +0900

    refactor : 일급 컬렉션에 대해서 핵심 비즈니스 로직만 알게 controller에서 분리

--- src/main/java/com/prgrms/controller/VoucherController.java
+++ src/main/java/com/prgrms/controller/VoucherController.java
@@ -1,24 +1,28 @@
 package com.prgrms.controller;
 
 import com.prgrms.model.dto.VoucherRequest;
+import com.prgrms.model.dto.VoucherResponse;
 import com.prgrms.model.voucher.Voucher;
-import com.prgrms.model.voucher.VoucherRegistry;
 import com.prgrms.service.voucher.VoucherService;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
-@RequiredArgsConstructor
 public class VoucherController {
 
     private final VoucherService voucherService;
+
+    public VoucherController(VoucherService voucherService) {
+        this.voucherService = voucherService;
+    }
 
     public Voucher createVoucher(VoucherRequest voucherRequest) {
         return voucherService.createVoucher(voucherRequest);
     }
 
-    public VoucherRegistry listVoucher() {
+    public List<VoucherResponse> listVoucher() {
         return voucherService.getAllVoucherList();
     }
 }
