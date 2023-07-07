b74388d9976ebe0adf498b6feb072d1c23dded05
changhyeon <changhyeon.h@kakao.com>
2023-07-07 20:34:26 +0900

    test: Voucher CRUD 관련 서비스 테스트 구현

--- src/test/java/com/programmers/springweekly/domain/voucher/VoucherTest.java
+++ src/test/java/com/programmers/springweekly/domain/voucher/VoucherTest.java
@@ -1,15 +1,12 @@
 package com.programmers.springweekly.domain.voucher;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.UUID;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
-
-import java.util.UUID;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class VoucherTest {
 
@@ -17,7 +14,7 @@ public class VoucherTest {
     @DisplayName("팩토리에서 고정 할인 클래스를 생성한다.")
     void createFixedDiscountOfFactory() {
         // given && when
-        Voucher voucher = VoucherFactory.createVoucher(UUID.randomUUID(), VoucherType.FIXED, "1000");
+        Voucher voucher = VoucherFactory.createVoucher(UUID.randomUUID(), VoucherType.FIXED, 1000L);
 
         // then
         assertThat(voucher).isInstanceOf(FixedAmountVoucher.class);
@@ -42,7 +39,7 @@ public class VoucherTest {
     @DisplayName("팩토리에서 퍼센트 할인 클래스를 생성한다.")
     void createPercentDiscountOfFactory() {
         // given && when
-        Voucher voucher = VoucherFactory.createVoucher(UUID.randomUUID(), VoucherType.PERCENT, "100");
+        Voucher voucher = VoucherFactory.createVoucher(UUID.randomUUID(), VoucherType.PERCENT, 100L);
 
         // then
         assertThat(voucher).isInstanceOf(PercentDiscountVoucher.class);
@@ -60,32 +57,5 @@ public class VoucherTest {
 
         // then
         assertThat(discountPrice).isEqualTo(expectedDiscount);
-    }
-
-    @ParameterizedTest
-    @ValueSource(strings = {"abc", "!@#", "1입니다"})
-    @DisplayName("고정 할인 바우처를 생성할 때 입력값이 숫자가 아니면 예외가 발생한다.")
-    void fixedDiscountNumberException(String input) {
-        assertThatThrownBy(() -> VoucherFactory.createVoucher(UUID.randomUUID(), VoucherType.FIXED, input))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("Input : " + input + ", 입력하신 것은 숫자가 아닙니다.");
-    }
-
-    @ParameterizedTest
-    @ValueSource(strings = {"abc", "!@#", "1입니다"})
-    @DisplayName("퍼센트 할인 바우처를 생성할 때 입력값이 숫자가 아니면 예외가 발생한다.")
-    void percentDiscountNumberException(String input) {
-        assertThatThrownBy(() -> VoucherFactory.createVoucher(UUID.randomUUID(), VoucherType.PERCENT, input))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("Input : " + input + ", 입력하신 것은 숫자가 아닙니다.");
-    }
-
-    @ParameterizedTest
-    @ValueSource(strings = {"101"})
-    @DisplayName("퍼센트 할인 바우처를 생성할 때 입력값이 0에서 100사이가 아닌 경우 예외가 발생한다.")
-    void percentDiscountNumberRangeException(String input) {
-        assertThatThrownBy(() -> VoucherFactory.createVoucher(UUID.randomUUID(), VoucherType.PERCENT, input))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("Input : " + input + ", 입력하신 숫자는 범위를 벗어납니다.");
     }
 }
--- src/test/java/com/programmers/springweekly/service/voucher/VoucherServiceTest.java
+++ src/test/java/com/programmers/springweekly/service/voucher/VoucherServiceTest.java
@@ -1,66 +1,123 @@
 package com.programmers.springweekly.service.voucher;
 
-import com.programmers.springweekly.domain.voucher.Voucher;
-import com.programmers.springweekly.domain.voucher.VoucherFactory;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 import com.programmers.springweekly.domain.voucher.VoucherType;
-import com.programmers.springweekly.repository.voucher.VoucherRepository;
+import com.programmers.springweekly.dto.voucher.request.VoucherCreateRequest;
+import com.programmers.springweekly.dto.voucher.request.VoucherUpdateRequest;
+import com.programmers.springweekly.dto.voucher.response.VoucherListResponse;
+import com.programmers.springweekly.dto.voucher.response.VoucherResponse;
 import com.programmers.springweekly.service.VoucherService;
+import java.util.NoSuchElementException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-import java.util.UUID;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.BDDMockito.given;
-import static org.mockito.BDDMockito.then;
-import static org.mockito.Mockito.times;
-
-@ExtendWith(MockitoExtension.class)
+@ActiveProfiles("test")
+@Transactional
+@SpringBootTest
 public class VoucherServiceTest {
 
-    @Mock
-    private VoucherRepository voucherRepository;
-
-    @InjectMocks
+    @Autowired
     private VoucherService voucherService;
+
+    @Test
+    @DisplayName("바우처를 저장한다.")
+    void save() {
+        // given
+        VoucherCreateRequest voucherExpected = new VoucherCreateRequest(3000, VoucherType.FIXED);
+
+        // when
+        voucherService.save(voucherExpected);
+        VoucherListResponse voucherActual = voucherService.findAll();
+
+        // then
+        assertThat(voucherActual.getVoucherList().get(0).getDiscountAmount()).isEqualTo(3000);
+    }
+
+    @Test
+    @DisplayName("바우처를 업데이트 한다.")
+    void update() {
+        // given
+        VoucherCreateRequest voucher = new VoucherCreateRequest(3000, VoucherType.FIXED);
+        voucherService.save(voucher);
+        VoucherListResponse voucherList = voucherService.findAll();
+
+        // when
+        voucherService.update(new VoucherUpdateRequest(voucherList.getVoucherList().get(0).getVoucherId(), 1000, VoucherType.PERCENT));
+        VoucherListResponse voucherListUpdated = voucherService.findAll();
+
+        // then
+        assertThat(voucherListUpdated.getVoucherList().get(0).getDiscountAmount()).isEqualTo(1000);
+    }
+
+    @Test
+    @DisplayName("바우처 ID를 가지고 바우처를 가져온다.")
+    void findById() {
+        // given
+        VoucherCreateRequest voucher = new VoucherCreateRequest(3000, VoucherType.FIXED);
+        voucherService.save(voucher);
+        VoucherListResponse voucherList = voucherService.findAll();
+
+        // when
+        VoucherResponse voucherActual = voucherService.findById(voucherList.getVoucherList().get(0).getVoucherId());
+
+        // then
+        assertThat(voucherActual.getDiscountAmount()).isEqualTo(3000);
+    }
 
     @Test
     @DisplayName("바우처를 모두 조회한다.")
     void getVoucherList() {
         // given
-        Voucher voucher1 = VoucherFactory.createVoucher(UUID.randomUUID(), VoucherType.FIXED, "1000");
-        Voucher voucher2 = VoucherFactory.createVoucher(UUID.randomUUID(), VoucherType.PERCENT, "10");
-
-        List<Voucher> voucherList = List.of(voucher1, voucher2);
-
-        given(voucherRepository.findAll()).willReturn(voucherList);
+        VoucherCreateRequest voucher1 = new VoucherCreateRequest(3000, VoucherType.FIXED);
+        VoucherCreateRequest voucher2 = new VoucherCreateRequest(2000, VoucherType.PERCENT);
+        voucherService.save(voucher1);
+        voucherService.save(voucher2);
 
         // when
-        List<Voucher> voucherAll = voucherService.findVoucherAll();
+        VoucherListResponse voucherList = voucherService.findAll();
 
         // then
-        assertThat(voucherAll).usingRecursiveFieldByFieldElementComparator().isEqualTo(voucherList);
-
-        then(voucherRepository).should(times(1)).findAll();
+        assertThat(voucherList.getVoucherList().get(0).getDiscountAmount()).isEqualTo(3000);
+        assertThat(voucherList.getVoucherList().get(1).getDiscountAmount()).isEqualTo(2000);
     }
 
-    /*
     @Test
-    @DisplayName("바우처를 저장한다.")
-    void saveVoucher() {
+    @DisplayName("바우처 ID를 가지고 바우처를 삭제한다.")
+    void deleteById() {
         // given
-        Voucher voucher = VoucherFactory.createVoucher(VoucherType.FIXED, "1000");
+        VoucherCreateRequest voucher = new VoucherCreateRequest(3000, VoucherType.FIXED);
+        voucherService.save(voucher);
+        VoucherListResponse voucherList = voucherService.findAll();
 
         // when
-        voucherService.saveVoucher(VoucherType.FIXED, "1000");
+        voucherService.deleteById(voucherList.getVoucherList().get(0).getVoucherId());
 
         // then
-        verify(voucherRepository).save(voucher);
+        assertThatThrownBy(() -> voucherService.findById(voucherList.getVoucherList().get(0).getVoucherId()))
+                .isInstanceOf(NoSuchElementException.class);
     }
-    */
+
+    @Test
+    @DisplayName("바우처를 모두 삭제한다.")
+    void deleteAll() {
+        // given
+        VoucherCreateRequest voucher1 = new VoucherCreateRequest(3000, VoucherType.FIXED);
+        VoucherCreateRequest voucher2 = new VoucherCreateRequest(2000, VoucherType.PERCENT);
+
+        voucherService.save(voucher1);
+        voucherService.save(voucher2);
+
+        // when
+        voucherService.deleteAll();
+
+        // then
+        assertThat(voucherService.findAll().getVoucherList().size()).isEqualTo(0);
+    }
+
 }
