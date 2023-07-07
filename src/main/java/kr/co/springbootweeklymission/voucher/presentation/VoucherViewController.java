17bfa4701e000577fe59c4fb1b7b375f8c1bc6e7
HyuckJuneHong <jjj8337721@naver.com>
2023-07-07 23:08:17 +0900

    feat : 특정 바우처를 조회하는 페이지 구현

--- src/main/java/kr/co/springbootweeklymission/voucher/presentation/VoucherViewController.java
+++ src/main/java/kr/co/springbootweeklymission/voucher/presentation/VoucherViewController.java
@@ -6,9 +6,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
+import java.util.UUID;
 
 @Controller
 @RequiredArgsConstructor
@@ -21,5 +23,13 @@ public class VoucherViewController {
         List<VoucherResDTO.READ> reads = voucherService.getVouchersAll();
         model.addAttribute("vouchers", reads);
         return "voucher/getVouchersAll";
+    }
+
+    @GetMapping("/{voucher_id}")
+    public String getVoucherById(Model model,
+                                 @PathVariable(name = "voucher_id") UUID voucherId) {
+        VoucherResDTO.READ read = voucherService.getVoucherById(voucherId);
+        model.addAttribute("voucher", read);
+        return "voucher/getVoucherById";
     }
 }
