4b7a59ffbe162b2bfd7800ae0e6f60dc8fd7bb77
HyuckJuneHong <jjj8337721@naver.com>
2023-07-07 11:09:53 +0900

    fix : record class에서 어노테이션 제거

--- src/main/java/kr/co/springbootweeklymission/common/response/ResponseError.java
+++ src/main/java/kr/co/springbootweeklymission/common/response/ResponseError.java
@@ -1,22 +1,19 @@
 package kr.co.springbootweeklymission.common.response;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import lombok.*;
+import lombok.Builder;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.FieldError;
 
 import java.util.List;
 
 @Builder
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public record ResponseError(String exception,
                             String message,
                             HttpStatus httpStatus,
                             @JsonInclude(JsonInclude.Include.NON_EMPTY)
                             List<ValidationException> validationExceptions) {
-    @Getter
     @Builder
-    @RequiredArgsConstructor
     public record ValidationException(String message,
                                       String field) {
         public static ValidationException of(final FieldError fieldError) {
