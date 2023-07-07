2babad14cdd747472abe7698d18793106d6b1041
byeolhaha <j35635aa@gmail.com>
2023-07-07 16:43:49 +0900

    refactor : 전략패턴 적용

--- src/main/java/com/prgrms/model/voucher/VoucherType.java
+++ src/main/java/com/prgrms/model/voucher/VoucherType.java
@@ -1,22 +1,30 @@
 package com.prgrms.model.voucher;
 
 import com.prgrms.io.ErrorMessage;
+import com.prgrms.model.voucher.discount.Discount;
 
 import java.util.Arrays;
 import java.util.Optional;
+import java.util.UUID;
+import java.util.function.BiFunction;
+import java.util.function.Function;
 
 public enum VoucherType {
     FIXED_AMOUNT_VOUCHER("1",
-            "얼만큼 할인 받고 싶은지 입력하세요 :"),
+            "얼만큼 할인 받고 싶은지 입력하세요 :",
+                          FixedAmountVoucher::new),
     PERCENT_DISCOUNT_VOUCHER("2",
-            "할인율을 입력하세요 :");
+            "할인율을 입력하세요 :",
+            PercentDiscountVoucher::new);
 
     private final String number;
     private final String discountGuide;
+    private final BiFunction<Discount,VoucherType, Voucher> voucherFunction ;
 
-    VoucherType(String number, String discountGuide) {
+    VoucherType(String number, String discountGuide, BiFunction<Discount,VoucherType, Voucher> voucherFunction) {
         this.number = number;
         this.discountGuide = discountGuide;
+        this.voucherFunction = voucherFunction;
     }
 
     public static VoucherType findByPolicy(String policy) {
@@ -32,6 +40,10 @@ public enum VoucherType {
 
     public String discountGuide() {
         return discountGuide;
+    }
+
+    public Voucher createPolicy(Discount amount) {
+        return voucherFunction.apply(amount, this);
     }
 
 }
