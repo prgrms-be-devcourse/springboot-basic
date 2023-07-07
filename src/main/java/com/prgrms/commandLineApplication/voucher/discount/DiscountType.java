803a76b5c8af7f66592ec7cfaa16f95a33116dae
박은지 <qkrdmswl1018@naver.com>
2023-07-07 18:15:08 +0900

    feat: discount 단위 출력 메소드 추가

--- src/main/java/com/prgrms/commandLineApplication/voucher/discount/DiscountType.java
+++ src/main/java/com/prgrms/commandLineApplication/voucher/discount/DiscountType.java
@@ -1,23 +1,23 @@
 package com.prgrms.commandLineApplication.voucher.discount;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import java.io.IOException;
 
 public enum DiscountType {
   FIXED,
   PERCENT;
 
-  private static final Logger LOGGER = LoggerFactory.getLogger(DiscountType.class);
-
   private static final String VOUCHER_TYPE_ERROR = "Invalid Voucher Type";
 
-  public static DiscountType valueOfType(String type) {
+  public static DiscountType valueOfType(String type) throws IOException {
     try {
       return DiscountType.valueOf(type.toUpperCase());
     } catch (Exception e) {
-      LOGGER.error("Menu Error Message => {}", VOUCHER_TYPE_ERROR);
-      throw new IllegalArgumentException(VOUCHER_TYPE_ERROR);
+      throw new IOException(VOUCHER_TYPE_ERROR);
     }
+  }
+
+  public static String getUnit(DiscountType discountType) {
+    return DiscountType.FIXED == discountType ? "₩" : "%";
   }
 
 }
