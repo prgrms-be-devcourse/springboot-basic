b53f7894416eaf9c6a13df614c19cf08ed29668f
charlesuu <tiger0192004@gmail.com>
2023-07-08 00:34:43 +0900

    refactor: 상수 네이밍 변경, 메서드 순서 변경

--- src/main/java/com/example/demo/view/validate/VoucherAmountValidator.java
+++ src/main/java/com/example/demo/view/validate/VoucherAmountValidator.java
@@ -5,7 +5,7 @@ import java.util.regex.Pattern;
 
 public final class VoucherAmountValidator {
 
-    private static final String ERROR_MESSAGE_NOT_INTEGER_VALUE = "[ERROR] 1이상의 숫자를 입력해주세요.";
+    private static final String ERROR_MESSAGE_NOT_POSITIVE_INTEGER_VALUE = "[ERROR] 1이상의 숫자를 입력해주세요.";
     private static final String ERROR_MESSAGE_NOT_PERCENT_VALUE = "[ERROR] 1이상 ~ 100 이하의 숫자를 입력해주세요.";
 
     private static final Pattern IS_NUMERIC_REGEX = Pattern.compile("^[1-9]\\d*$");
@@ -13,18 +13,6 @@ public final class VoucherAmountValidator {
 
 
     private VoucherAmountValidator() {
-    }
-
-    private static void validatePositiveNumber(String input) {
-        if (!IS_NUMERIC_REGEX.matcher(input).matches()) {
-            throw new IllegalArgumentException(ERROR_MESSAGE_NOT_INTEGER_VALUE);
-        }
-    }
-
-    private static void validatePercentageNumber(String input) {
-        if (!IS_PERCENT_REGEX.matcher(input).matches()) {
-            throw new IllegalArgumentException(ERROR_MESSAGE_NOT_PERCENT_VALUE);
-        }
     }
 
     public static void validateAmount(VoucherType voucherType, String input) {
@@ -35,6 +23,18 @@ public final class VoucherAmountValidator {
                 VoucherAmountValidator.validatePercentageNumber(input);
             }
             default -> throw new IllegalArgumentException(String.format("입력하신 %s는 유효한 바우처 타입이 아닙니다.", voucherType.name()));
+        }
+    }
+
+    private static void validatePositiveNumber(String input) {
+        if (!IS_NUMERIC_REGEX.matcher(input).matches()) {
+            throw new IllegalArgumentException(ERROR_MESSAGE_NOT_POSITIVE_INTEGER_VALUE);
+        }
+    }
+
+    private static void validatePercentageNumber(String input) {
+        if (!IS_PERCENT_REGEX.matcher(input).matches()) {
+            throw new IllegalArgumentException(ERROR_MESSAGE_NOT_PERCENT_VALUE);
         }
     }
 }
