1a969dac22e68183700d86bb25a214b6b07a53dd
kmebin <blowhuiee@gmail.com>
2023-07-07 14:27:08 +0900

    test: Voucher 테스트로 변경

--- src/test/java/com/programmers/voucher/domain/voucher/entity/VoucherTest.java
+++ src/test/java/com/programmers/voucher/domain/voucher/entity/VoucherTest.java
@@ -1,31 +1,30 @@
 package com.programmers.voucher.domain.voucher.entity;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 
-class DiscountAmountTest {
+class VoucherTest {
     @ParameterizedTest
-    @DisplayName("바우처 타입과 상관없이, 할인 값이 양수가 아닌 경우 예외가 발생한다.")
+    @DisplayName("할인 값이 양수가 아닌 경우 예외가 발생한다.")
     @MethodSource("invalidDiscountAmountArguments")
-    void 할인_값_양수_X(VoucherType voucherType, int amount) {
-        assertThatThrownBy(() -> new DiscountAmount(voucherType, amount))
+    void 바우처_생성_예외(VoucherType voucherType, int amount) {
+        assertThatThrownBy(() -> Voucher.create(voucherType, amount))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("할인 값은 양수만 가능합니다.");
     }
 
-    @ParameterizedTest
+    @Test
     @DisplayName("할인율이 100 초과인 경우 예외가 발생한다.")
-    @ValueSource(ints = {105, 2000})
-    void 할인율_100_초과(int percent) {
-        assertThatThrownBy(() -> new DiscountAmount(VoucherType.PERCENT, percent))
+    void 퍼센트_할인_바우처_생성_예외() {
+        assertThatThrownBy(() -> Voucher.create(VoucherType.PERCENT, 1000))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("할인율은 100을 넘을 수 없습니다.");
     }
