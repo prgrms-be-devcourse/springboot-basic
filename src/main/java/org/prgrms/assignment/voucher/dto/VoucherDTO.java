1a4458ff5522bab96eeeff223bb0b8e753fb3c07
BeommoKoo <rnqjaah1536@naver.com>
2023-07-07 11:52:27 +0900

    refactor : VoucherDTO class -> record로 리팩터링

--- src/main/java/org/prgrms/assignment/voucher/dto/VoucherDTO.java
+++ src/main/java/org/prgrms/assignment/voucher/dto/VoucherDTO.java
@@ -4,26 +4,6 @@ import org.prgrms.assignment.voucher.model.VoucherType;
 
 import java.time.LocalDateTime;
 
-public class VoucherDTO {
-    private final VoucherType voucherType;
-    private final long benefit;
-    private final LocalDateTime createdAt;
-
-    public VoucherDTO(VoucherType voucherType, long benefit, LocalDateTime createdAt) {
-        this.voucherType = voucherType;
-        this.benefit = benefit;
-        this.createdAt = createdAt;
-    }
-
-    public VoucherType getVoucherType() {
-        return voucherType;
-    }
-
-    public long getBenefit() {
-        return benefit;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
+public record VoucherDTO(VoucherType voucherType, long benefit, LocalDateTime createdAt,
+                         LocalDateTime expireDate) {
 }
