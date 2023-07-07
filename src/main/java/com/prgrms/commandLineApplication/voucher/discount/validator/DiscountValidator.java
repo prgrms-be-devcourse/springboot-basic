6b54f7f8d9436f2854b756c15dcb533dd86d3b42
박은지 <qkrdmswl1018@naver.com>
2023-07-07 18:14:01 +0900

    refactor: 검증 로직에 로그 제거

--- src/main/java/com/prgrms/commandLineApplication/voucher/discount/validator/DiscountValidator.java
+++ src/main/java/com/prgrms/commandLineApplication/voucher/discount/validator/DiscountValidator.java
@@ -8,29 +8,24 @@ import org.slf4j.LoggerFactory;
 
 public class DiscountValidator {
 
-  private static final Logger LOGGER = LoggerFactory.getLogger(DiscountValidator.class);
-
   private static final String INVALID_DISCOUNT_ERROR = "Invalid Discount";
   private static final String INVALID_FIXED_AMOUNT_RANGE_ERROR = "Invalid discount amount range (0 ~ 10000)";
   private static final String INVALID_PERCENT_AMOUNT_ERROR = "Invalid discount amount range (0 ~ 100)";
 
   public static void checkDiscount(Discount discount) {
     if (discount == null) {
-      LOGGER.error("Discount Error Message => {}", INVALID_DISCOUNT_ERROR);
       throw new IllegalArgumentException(INVALID_DISCOUNT_ERROR);
     }
   }
 
   public static void checkFixedDiscount(int discountAmount) {
     if (discountAmount < FixedDiscount.MINIMUM_VALUE || discountAmount > FixedDiscount.MAXIMUM_VALUE) {
-      LOGGER.error("Discount Error Message => {}", INVALID_FIXED_AMOUNT_RANGE_ERROR);
       throw new IllegalArgumentException(INVALID_FIXED_AMOUNT_RANGE_ERROR);
     }
   }
 
   public static void checkPercentDiscount(int discountAmount) {
     if (PercentDiscount.PERCENT_RATE_BASE < discountAmount || discountAmount < PercentDiscount.MINIMUM_VALUE) {
-      LOGGER.error("Discount Error Message => {}", INVALID_PERCENT_AMOUNT_ERROR);
       throw new IllegalArgumentException(INVALID_PERCENT_AMOUNT_ERROR);
     }
   }
--- src/main/java/com/prgrms/commandLineApplication/voucher/validator/VoucherValidator.java
+++ src/main/java/com/prgrms/commandLineApplication/voucher/validator/VoucherValidator.java
@@ -1,19 +1,13 @@
 package com.prgrms.commandLineApplication.voucher.validator;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.util.UUID;
 
 public class VoucherValidator {
-
-  private static final Logger LOGGER = LoggerFactory.getLogger(VoucherValidator.class);
 
   private static final String INVALID_ID_ERROR = "Invalid ID";
 
   public static void checkId(UUID id) {
     if (id == null) {
-      LOGGER.error("Voucher Error Message => {}", INVALID_ID_ERROR);
       throw new IllegalArgumentException(INVALID_ID_ERROR);
     }
   }
