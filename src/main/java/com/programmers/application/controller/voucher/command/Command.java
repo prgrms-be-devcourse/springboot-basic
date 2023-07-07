52dd1e2b838acc3cc18e7ff15f1e16dc5fd0f9ec
yumyeonghan <mgtmh991013@gmail.com>
2023-07-07 13:41:56 +0900

    feat: CommandExecution 생성
    1. Command를 인자로 받아 적절한 실행 객체를 찾아 실행시켜주는 클래스 생성
    2. enum 타입에 너무 많은 책임 부여 지양

--- src/main/java/com/programmers/application/controller/voucher/command/Command.java
+++ src/main/java/com/programmers/application/controller/voucher/command/Command.java
@@ -1,23 +1,8 @@
 package com.programmers.application.controller.voucher.command;
 
-import com.programmers.application.controller.voucher.command.execution.VoucherExecution;
-
-import java.io.IOException;
-
-
 public enum Command {
     EXIT,
     CREATE,
     LIST,
     ;
-
-    private VoucherExecution voucherExecution;
-
-    public void executeVoucher() throws IOException {
-        voucherExecution.execute();
-    }
-
-    protected void setVoucherExecution(VoucherExecution voucherExecution) {
-        this.voucherExecution = voucherExecution;
-    }
 }
--- src/main/java/com/programmers/application/controller/voucher/command/CommandExecution.java
+++ src/main/java/com/programmers/application/controller/voucher/command/CommandExecution.java
@@ -3,34 +3,32 @@ package com.programmers.application.controller.voucher.command;
 import com.programmers.application.controller.voucher.command.execution.CreateVoucherExecution;
 import com.programmers.application.controller.voucher.command.execution.ExitVoucherExecution;
 import com.programmers.application.controller.voucher.command.execution.ListVoucherExecution;
-import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
+
 @Component
-public class VoucherExecutionDependencyInjector {
+public class CommandExecution {
 
     private final CreateVoucherExecution createVoucherExecution;
     private final ExitVoucherExecution exitVoucherExecution;
     private final ListVoucherExecution listVoucherExecution;
 
-    public VoucherExecutionDependencyInjector(CreateVoucherExecution createVoucherExecution, ExitVoucherExecution exitVoucherExecution, ListVoucherExecution listVoucherExecution) {
+    public CommandExecution(CreateVoucherExecution createVoucherExecution, ExitVoucherExecution exitVoucherExecution, ListVoucherExecution listVoucherExecution) {
         this.createVoucherExecution = createVoucherExecution;
         this.exitVoucherExecution = exitVoucherExecution;
         this.listVoucherExecution = listVoucherExecution;
     }
 
-    @PostConstruct
-    public void inject() {
-        for (Command command : Command.values()) {
-            if (command.equals(Command.CREATE)) {
-                command.setVoucherExecution(createVoucherExecution);
-            }
-            if (command.equals(Command.EXIT)) {
-                command.setVoucherExecution(exitVoucherExecution);
-            }
-            if (command.equals(Command.LIST)) {
-                command.setVoucherExecution(listVoucherExecution);
-            }
+    public void executeVoucher(Command command) throws IOException {
+        if (command == Command.CREATE) {
+            createVoucherExecution.execute();
+        }
+        if (command == Command.EXIT) {
+            exitVoucherExecution.execute();
+        }
+        if (command == Command.LIST) {
+            listVoucherExecution.execute();
         }
     }
 }
