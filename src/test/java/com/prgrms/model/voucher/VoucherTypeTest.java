017fd5b4e8735ecd7d564a8bd14b81340522c3a5
byeolhaha <j35635aa@gmail.com>
2023-07-07 16:56:20 +0900

    feat : 재사용성을 고려하여 key 생성 클래스 분리

--- src/test/java/com/prgrms/model/voucher/VoucherTypeTest.java
+++ src/test/java/com/prgrms/model/voucher/VoucherTypeTest.java
@@ -1,38 +1,59 @@
 package com.prgrms.model.voucher;
 
+import com.prgrms.model.voucher.discount.Discount;
+import com.prgrms.model.voucher.discount.FixedDiscount;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Optional;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class VoucherTypeTest {
     @Test
     @DisplayName("find 고정된 금액의 정책")
-    void findByPolicyFixedVoucherPolicy() {
+    void findByPolicy_FixedVoucherPolicy_Equals() {
+        //given
         String policy = "1";
+        //when
         VoucherType voucherPolicy = VoucherType.findByPolicy(policy);
-
-        assertEquals(VoucherType.FIXED_AMOUNT_VOUCHER, voucherPolicy);
+        //then
+        assertThat(voucherPolicy).isEqualTo(VoucherType.FIXED_AMOUNT_VOUCHER);
     }
 
     @Test
     @DisplayName("find 할인율 정책")
-    void findByPolicyPercentVoucherPolicy() {
+    void findByPolicy_PercentVoucherPolicy_Equals() {
+        //given
         String policy = "2";
+        //when
         VoucherType voucherPolicy = VoucherType.findByPolicy(policy);
-
-        assertEquals(VoucherType.PERCENT_DISCOUNT_VOUCHER, voucherPolicy);
+        //then
+        assertThat(voucherPolicy).isEqualTo(VoucherType.PERCENT_DISCOUNT_VOUCHER);
     }
 
     @Test
     @DisplayName("없는 할인 정책에 대한 예외 테스트")
-    void findByPolicyNotExistVoucherPolicy() {
+    void findByPolicy_NotExistVoucherPolicy_Empty() {
+        //given
         String policy = "3";
+        //when
         VoucherType voucherPolicy = VoucherType.findByPolicy(policy);
+        //then
+        assertThat(voucherPolicy).isEqualTo(Optional.empty());
+    }
 
-        assertEquals(Optional.empty(), voucherPolicy);
+    @Test
+    @DisplayName("없는 할인 정책에 대한 예외 테스트")
+    void createVoucher_() {
+        // given
+        Discount discount = new FixedDiscount(10); // Example discount value
+        // when
+        Voucher voucher = VoucherType.FIXED_AMOUNT_VOUCHER.createVoucher(discount);
+        //then
+        assertThat(voucher).isNotNull()
+                .isInstanceOf(FixedAmountVoucher.class);
     }
 
 }
