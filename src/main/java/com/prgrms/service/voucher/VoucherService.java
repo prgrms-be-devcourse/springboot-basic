431e3c052c1211a39a153faaa710e14145159cfb
byeolhaha <j35635aa@gmail.com>
2023-07-07 17:12:01 +0900

    refactor : createVoucher 메서드 로직 수정 VoucherCreator의 switch case문을 없애고 VoucherType을 통해서 객체 생성

--- src/main/java/com/prgrms/service/voucher/VoucherService.java
+++ src/main/java/com/prgrms/service/voucher/VoucherService.java
@@ -6,7 +6,6 @@ import com.prgrms.model.dto.VoucherResponse;
 import com.prgrms.model.voucher.*;
 import com.prgrms.model.voucher.discount.Discount;
 import com.prgrms.repository.voucher.VoucherRepository;
-import com.prgrms.model.voucher.VoucherCreator;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,20 +14,18 @@ import java.util.List;
 public class VoucherService {
 
     private final VoucherRepository voucherRepository;
-    private final VoucherCreator voucherCreator;
     private final DtoConverter dtoConverter;
 
-    public VoucherService(VoucherRepository voucherRepository, VoucherCreator voucherCreator, DtoConverter dtoConverter) {
+    public VoucherService(VoucherRepository voucherRepository, DtoConverter dtoConverter) {
         this.voucherRepository = voucherRepository;
-        this.voucherCreator = voucherCreator;
         this.dtoConverter = dtoConverter;
     }
 
     public Voucher createVoucher(VoucherRequest voucherRequest) {
         Discount discount = voucherRequest.getDiscount();
         VoucherType voucherType = voucherRequest.getVoucherType();
-        Voucher voucher = voucherCreator.createVoucher(discount, voucherType);
-
+        Voucher voucher = voucherType.createVoucher(discount);
+        
         return voucherRepository.insert(voucher);
     }
 
@@ -36,7 +33,4 @@ public class VoucherService {
         VoucherRegistry vouchers = voucherRepository.getAllVoucher();
         return dtoConverter.convertVoucherResponse(vouchers);
     }
-
 }
-
-
