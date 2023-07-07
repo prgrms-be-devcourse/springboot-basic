06efefff58f2e41b92b9141cecf919c86d2a153f
changhyeon <changhyeon.h@kakao.com>
2023-07-07 15:50:29 +0900

    feat: 바우처 CRUD 관련 컨트롤러 기능 로직 구현

--- src/main/java/com/programmers/springweekly/controller/VoucherController.java
+++ src/main/java/com/programmers/springweekly/controller/VoucherController.java
@@ -1,26 +1,41 @@
 package com.programmers.springweekly.controller;
 
-import com.programmers.springweekly.domain.voucher.Voucher;
-import com.programmers.springweekly.domain.voucher.VoucherType;
+import com.programmers.springweekly.dto.voucher.request.VoucherCreateRequest;
+import com.programmers.springweekly.dto.voucher.request.VoucherUpdateRequest;
+import com.programmers.springweekly.dto.voucher.response.VoucherListResponse;
+import com.programmers.springweekly.dto.voucher.response.VoucherResponse;
 import com.programmers.springweekly.service.VoucherService;
-import lombok.AllArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 
-import java.util.List;
-
 @Controller
-@AllArgsConstructor
-@Slf4j
+@RequiredArgsConstructor
 public class VoucherController {
 
     private final VoucherService voucherService;
 
-    public void createVoucher(VoucherType voucherType, String inputNumber) {
-        voucherService.saveVoucher(voucherType, inputNumber);
+    public void save(VoucherCreateRequest voucherCreateRequest) {
+        voucherService.save(voucherCreateRequest);
     }
 
-    public List<Voucher> getVoucherList() {
-        return voucherService.findVoucherAll();
+    public void update(VoucherUpdateRequest voucherUpdateRequest) {
+        voucherService.update(voucherUpdateRequest);
+    }
+
+    public VoucherResponse findById(UUID voucherId) {
+        return voucherService.findById(voucherId);
+    }
+
+    public VoucherListResponse findAll() {
+        return voucherService.findAll();
+    }
+
+    public void deleteById(UUID voucherId) {
+        voucherService.deleteById(voucherId);
+    }
+
+    public void deleteAll() {
+        voucherService.deleteAll();
     }
 }
