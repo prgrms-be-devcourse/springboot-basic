48611a1ae43f9c9127c9e74502776b1e9c1ac952
Juwoong <wwong7247@gmail.com>
2023-07-08 20:58:01 +0900

    VoucherConrtroller 불필요한 예외처리 제거

--- src/main/java/com/example/voucher/controller/VoucherController.java
+++ src/main/java/com/example/voucher/controller/VoucherController.java
@@ -1,19 +1,11 @@
 package com.example.voucher.controller;
 
-import static com.example.voucher.utils.ExceptionMessage.INVALID_ARGUMENT_CANT_CREATE;
-import static com.example.voucher.utils.ExceptionMessage.EXCEPTION_CANT_CREATE;
-
 import java.util.List;
 import java.util.stream.Collectors;
-
 import org.springframework.stereotype.Controller;
-
 import com.example.voucher.constant.VoucherType;
-import com.example.voucher.domain.Voucher;
 import com.example.voucher.domain.dto.VoucherDTO;
 import com.example.voucher.service.VoucherService;
-import com.example.voucher.utils.ExceptionHandler;
-import com.example.voucher.utils.Validator;
 
 @Controller
 public class VoucherController {
@@ -25,26 +17,14 @@ public class VoucherController {
     }
 
     public void createVoucher(VoucherType voucherType, long discountValue) {
-        try {
-            voucherService.createVoucher(voucherType, discountValue);
-
-        } catch (IllegalArgumentException e) {
-            ExceptionHandler.handleException(new IllegalArgumentException(INVALID_ARGUMENT_CANT_CREATE));
-
-        } catch (Exception e) {
-            ExceptionHandler.handleException(new Exception(EXCEPTION_CANT_CREATE));
-
-        }
+        voucherService.createVoucher(voucherType, discountValue);
     }
 
     public List<VoucherDTO> getVouchers() {
-        List<Voucher> vouchers = voucherService.getVouchers();
-
-        List<VoucherDTO> voucherDTOS = vouchers.stream()
+        return voucherService.getVouchers()
+            .stream()
             .map(o -> new VoucherDTO(o.getValue(), o.getVoucherType()))
             .collect(Collectors.toList());
-
-        return voucherDTOS;
     }
 
 }
--- src/main/java/com/example/voucher/controller/VoucherCreateRequest.java
+++
@@ -1,4 +0,0 @@
-package com.example.voucher.controller;
-
-public record VoucherCreateRequest(Integer voucherType, Long discountValue) {
-}
