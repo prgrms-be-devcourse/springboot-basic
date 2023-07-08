b6d9c8206e5d9e8eca822c223a1144dd9ed0de97
jay-so <merry9504@naver.com>
2023-07-08 19:21:27 +0900

    feat: CustomerRepository 인터페이스 구현

---
+++ src/main/java/com/prgrms/springbootbasic/repository/customer/CustomerRepository.java
@@ -0,0 +1,23 @@
+package com.prgrms.springbootbasic.repository.customer;
+
+import com.prgrms.springbootbasic.domain.customer.Customer;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface CustomerRepository {
+
+    Customer insert(Customer customer);
+
+    Customer update(Customer customer);
+
+    List<Customer> findAll();
+
+    Optional<Customer> findById(UUID customerId);
+
+    Optional<Customer> findByName(String name);
+
+    Optional<Customer> findByEmail(String email);
+
+    void deleteAll();
+}
