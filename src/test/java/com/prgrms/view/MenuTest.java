93622dff4f9388f88d74b9a92958af92fc9811a4
byeolhaha <j35635aa@gmail.com>
2023-07-08 23:34:00 +0900

    test : 바우처 생성 테스트 추가

--- src/test/java/com/prgrms/view/MenuTest.java
+++ src/test/java/com/prgrms/view/MenuTest.java
@@ -1,18 +1,25 @@
-package com.prgrms.io;
+package com.prgrms.view;
 
-//Junit
-
+import com.prgrms.view.command.Command;
+import com.prgrms.view.command.CommandFactory;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 //assertJ
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.*;
 
 
 class MenuTest {
 
+    @Mock
+    private CommandFactory commandFactory;
+
     @Test
     @DisplayName("소문자 list")
-    public void findLowerList() {
+    public void FindByMenu_LowerList_Equals() {
         String selectMenu = "list";
         Menu menu = Menu.findByMenu(selectMenu);
 
@@ -21,7 +28,7 @@ class MenuTest {
 
     @Test
     @DisplayName("대문자 LIST")
-    public void findUpperList() {
+    public void FindByMenu_UpperList_Equals() {
         String selectMenu = "LIST";
         Menu menu = Menu.findByMenu(selectMenu);
 
@@ -30,7 +37,7 @@ class MenuTest {
 
     @Test
     @DisplayName("소문자 create")
-    public void findLowerCreate() {
+    public void FindByMenu_LowerCreate_Equals() {
         String selectMenu = "create";
         Menu menu = Menu.findByMenu(selectMenu);
 
@@ -39,7 +46,7 @@ class MenuTest {
 
     @Test
     @DisplayName("대문자 CREATE")
-    public void findUpperCREATE() {
+    public void FindByMenu_UpperCreate_Equals() {
         String selectMenu = "CREATE";
         Menu menu = Menu.findByMenu(selectMenu);
 
@@ -48,7 +55,7 @@ class MenuTest {
 
     @Test
     @DisplayName("소문자 exit")
-    public void findLowerExit() {
+    public void FindByMenu_LowerExit_Equals() {
         String selectMenu = "exit";
         Menu menu = Menu.findByMenu(selectMenu);
 
@@ -57,7 +64,7 @@ class MenuTest {
 
     @Test
     @DisplayName("대문자 EXIT")
-    public void findUpperExit() {
+    public void FindByMenu_UpperExit_Equals() {
         String selectMenu = "EXIT";
         Menu menu = Menu.findByMenu(selectMenu);
 
@@ -66,7 +73,7 @@ class MenuTest {
 
     @Test
     @DisplayName("대소문자 섞인 메뉴")
-    public void findMixedMenu() {
+    public void FindByMenu_MixedMenu_Equals() {
         String selectMenu = "ExIt";
         Menu menu = Menu.findByMenu(selectMenu);
 
@@ -75,7 +82,7 @@ class MenuTest {
 
     @Test
     @DisplayName("없는 메뉴에 대한 예외 테스트")
-    void findByPolicyNotExistVoucherPolicy() {
+    public void findByMenu_NotExistMenu_ThrowsException() {
         String menu = "cccc";
 
         assertThatThrownBy(() -> Menu.findByMenu(menu))
@@ -83,4 +90,30 @@ class MenuTest {
                 .hasMessage("올바른 선택지가 아닙니다.");
     }
 
+    @Test
+    void createCommand_ShouldReturnCorrectCommandInstanceBasedOnMenu() {
+        //given
+        MockitoAnnotations.openMocks(this);
+        Menu menu = Menu.CREATE;
+        Command mockCommand = mock(Command.class);
+        when(commandFactory.createCreateCommand()).thenReturn(mockCommand);
+
+        //when
+        Command result = menu.createCommand(commandFactory);
+
+        // then
+        switch (menu) {
+            case EXIT:
+                verify(commandFactory, times(1)).createExitCommand();
+                break;
+            case CREATE:
+                verify(commandFactory, times(1)).createCreateCommand();
+                break;
+            case LIST:
+                verify(commandFactory, times(1)).createListCommand();
+                break;
+        }
+
+        assertThat(result).isEqualTo(mockCommand);
+    }
 }
