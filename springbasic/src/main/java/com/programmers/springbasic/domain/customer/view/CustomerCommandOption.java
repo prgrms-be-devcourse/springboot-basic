d3231ec8606c95a3449f85d77ce326e3a183fb10
hi-june <wnsgur735@gmail.com>
2023-07-08 20:34:03 +0900

    refactor: CustomerCommandOption 파싱

--- springbasic/src/main/java/com/programmers/springbasic/domain/customer/view/CustomerCommandOption.java
+++ springbasic/src/main/java/com/programmers/springbasic/domain/customer/view/CustomerCommandOption.java
@@ -2,6 +2,8 @@ package com.programmers.springbasic.domain.customer.view;
 
 import lombok.Getter;
 
+import java.util.Arrays;
+
 @Getter
 public enum CustomerCommandOption {
     EXIT("exit"), CREATE("create"), LIST("list"), READ("read"), UPDATE("update"), DELETE("delete"), SHOW("show");
@@ -10,5 +12,12 @@ public enum CustomerCommandOption {
 
     CustomerCommandOption(String command) {
         this.command = command;
+    }
+
+    public static CustomerCommandOption of(String inputCommand) {
+        return Arrays.stream(values())
+                .filter(customerCommandOption -> customerCommandOption.name().equalsIgnoreCase(inputCommand))
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("Wrong Command!"));
     }
 }
