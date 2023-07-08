c5fbc8ca43d487e8e02791e3882fc12e40ceb877
jay-so <merry9504@naver.com>
2023-07-09 00:39:20 +0900

    refactor:CustomerJdbcRepository 클래스 수정

--- src/main/java/com/prgrms/springbootbasic/repository/customer/CustomerJdbcRepository.java
+++ src/main/java/com/prgrms/springbootbasic/repository/customer/CustomerJdbcRepository.java
@@ -2,8 +2,6 @@ package com.prgrms.springbootbasic.repository.customer;
 
 import com.prgrms.springbootbasic.domain.customer.Customer;
 import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
 
 public class CustomerJdbcRepository implements CustomerRepository {
 
@@ -13,28 +11,13 @@ public class CustomerJdbcRepository implements CustomerRepository {
     }
 
     @Override
-    public Customer update(Customer customer) {
-        return null;
+    public void update(Customer customer) {
+
     }
 
     @Override
     public List<Customer> findAll() {
         return null;
-    }
-
-    @Override
-    public Optional<Customer> findById(UUID customerId) {
-        return Optional.empty();
-    }
-
-    @Override
-    public Optional<Customer> findByName(String name) {
-        return Optional.empty();
-    }
-
-    @Override
-    public Optional<Customer> findByEmail(String email) {
-        return Optional.empty();
     }
 
     @Override
