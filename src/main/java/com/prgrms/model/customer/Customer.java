82b6f53a27ca87825dc9abaf5fcaf91d3a70f440
byeolhaha <j35635aa@gmail.com>
2023-07-08 23:51:42 +0900

    feat : 怨좉컼 媛앹껜 援ы쁽

---
+++ src/main/java/com/prgrms/model/customer/Customer.java
@@ -0,0 +1,64 @@
+package com.prgrms.model.customer;
+
+import java.time.LocalDateTime;
+
+public class Customer {
+
+    private final int customerId;
+    private final String email;
+    private final LocalDateTime createdAt;
+    private String name;
+    private LocalDateTime lastLoginAt;
+
+    public Customer(int customerId, String name, String email, LocalDateTime createdAt) {
+        validateName(name);
+        this.customerId = customerId;
+        this.name = name;
+        this.email = email;
+        this.createdAt = createdAt;
+    }
+
+    private void validateName(String name) {
+        if (name.isBlank()) {
+            throw new RuntimeException("Name should not be blank");
+        }
+    }
+
+    public Customer(int customerId, String name, String email, LocalDateTime lastLoginAt, LocalDateTime createdAt) {
+        validateName(name);
+        this.customerId = customerId;
+        this.name = name;
+        this.email = email;
+        this.lastLoginAt = lastLoginAt;
+        this.createdAt = createdAt;
+    }
+
+    public void changeName(String name) {
+        validateName(name);
+        this.name = name;
+    }
+
+    public void login() {
+        this.lastLoginAt = LocalDateTime.now();
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public LocalDateTime getLastLoginAt() {
+        return lastLoginAt;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+}
