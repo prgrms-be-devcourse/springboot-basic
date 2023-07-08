a693f2d0528eeadd06bbadfcdeceaa9a163776b2
byeolhaha <j35635aa@gmail.com>
2023-07-08 21:19:09 +0900

    test : 생성자 수정으로 인해 테스트 로직 수정

--- src/test/java/com/prgrms/model/voucher/FixedAmountVoucherTest.java
+++ src/test/java/com/prgrms/model/voucher/FixedAmountVoucherTest.java
@@ -1,34 +1,34 @@
 package com.prgrms.model.voucher;
 
 import com.prgrms.model.order.OrderItem;
-import com.prgrms.model.voucher.discount.Discount;
-import com.prgrms.model.voucher.discount.FixedDiscount;
-import com.prgrms.model.voucher.discount.PercentDiscount;
-import com.prgrms.model.voucher.discount.Price;
+import com.prgrms.model.voucher.dto.discount.FixedDiscount;
+import com.prgrms.model.voucher.dto.price.Price;
+import com.prgrms.util.KeyGenerator;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.UUID;
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
 
 class FixedAmountVoucherTest {
     private OrderItem orderItem;
+    private int id = KeyGenerator.make();
 
     @BeforeEach
     public void setUp() {
-        orderItem = new OrderItem(UUID.randomUUID(), 1000, 1);
+        orderItem = new OrderItem(1, 1000, 1);
     }
+
     @Test
     @DisplayName("할인된 금액이 제대로 나오는지 확인한다.")
     public void discountPrice_DiscountedPrice_Equal() {
         //given
-        Voucher createdVoucher = new FixedAmountVoucher(new FixedDiscount(20), VoucherType.FIXED_AMOUNT_VOUCHER);
+        Voucher createdVoucher = new FixedAmountVoucher(id, new FixedDiscount(20), VoucherType.FIXED_AMOUNT_VOUCHER);
+
         //when
         Price discountedPrice = createdVoucher.discountPrice(orderItem);
+
         //then
         assertThat(discountedPrice.getValue()).isEqualTo(980);
     }
@@ -37,9 +37,10 @@ class FixedAmountVoucherTest {
     @DisplayName("할인금액이 원가보다 커 할인된 금액이 음수가 나오는 경우 예외를 던지는지 확인한다.")
     public void discountPrice_NegativeDiscountedPrice_ThrowsException() {
         //given
-        Voucher createdVoucher = new FixedAmountVoucher(new FixedDiscount(2000), VoucherType.FIXED_AMOUNT_VOUCHER);
+        Voucher createdVoucher = new FixedAmountVoucher(id, new FixedDiscount(2000), VoucherType.FIXED_AMOUNT_VOUCHER);
+
         //when_then
-        assertThatThrownBy(() ->createdVoucher.discountPrice(orderItem))
+        assertThatThrownBy(() -> createdVoucher.discountPrice(orderItem))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
--- src/test/java/com/prgrms/model/voucher/PercentDiscountVoucherTest.java
+++ src/test/java/com/prgrms/model/voucher/PercentDiscountVoucherTest.java
@@ -1,32 +1,33 @@
 package com.prgrms.model.voucher;
 
 import com.prgrms.model.order.OrderItem;
-import com.prgrms.model.voucher.discount.Discount;
-import com.prgrms.model.voucher.discount.PercentDiscount;
-import com.prgrms.model.voucher.discount.Price;
+import com.prgrms.model.voucher.dto.discount.PercentDiscount;
+import com.prgrms.model.voucher.dto.price.Price;
+import com.prgrms.util.KeyGenerator;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.UUID;
-
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class PercentDiscountVoucherTest {
     private OrderItem orderItem;
+    private int id = KeyGenerator.make();
 
     @BeforeEach
     public void setUp() {
-        orderItem = new OrderItem(UUID.randomUUID(), 1000, 1);
+        orderItem = new OrderItem(1, 1000, 1);
     }
+
     @Test
     @DisplayName("할인된 금액이 제대로 나오는지 확인한다.")
     public void discountPrice_DiscountedPrice_Equal() {
         //given
-        Voucher createdVoucher = new PercentDiscountVoucher(new PercentDiscount(20), VoucherType.PERCENT_DISCOUNT_VOUCHER);
+        Voucher createdVoucher = new PercentDiscountVoucher(id, new PercentDiscount(20), VoucherType.PERCENT_DISCOUNT_VOUCHER);
+
         //when
         Price discountedPrice = createdVoucher.discountPrice(orderItem);
+
         //then
         assertThat(discountedPrice.getValue()).isEqualTo(800);
     }
