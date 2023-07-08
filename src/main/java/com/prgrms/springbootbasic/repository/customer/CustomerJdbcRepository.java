55a476b2116306e726a5a57f76f59a021fea53a4
jay-so <merry9504@naver.com>
2023-07-08 19:22:58 +0900

    feat:CustomerRepository의 구현체로 CustomerJdbcRepository가 되도록 변경

--- src/main/java/com/prgrms/springbootbasic/repository/customer/CustomerJdbcRepository.java
+++ src/main/java/com/prgrms/springbootbasic/repository/customer/CustomerJdbcRepository.java
@@ -1,5 +1,44 @@
 package com.prgrms.springbootbasic.repository.customer;
 
-public class CustomerJdbcRepository {
+import com.prgrms.springbootbasic.domain.customer.Customer;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
+public class CustomerJdbcRepository implements CustomerRepository {
+
+    @Override
+    public Customer insert(Customer customer) {
+        return null;
+    }
+
+    @Override
+    public Customer update(Customer customer) {
+        return null;
+    }
+
+    @Override
+    public List<Customer> findAll() {
+        return null;
+    }
+
+    @Override
+    public Optional<Customer> findById(UUID customerId) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Customer> findByName(String name) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Customer> findByEmail(String email) {
+        return Optional.empty();
+    }
+
+    @Override
+    public void deleteAll() {
+
+    }
 }
