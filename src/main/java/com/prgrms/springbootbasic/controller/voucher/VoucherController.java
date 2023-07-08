c3ebd7817e0df6a0b92eeb7a399d25f6c4b8c91e
jay-so <merry9504@naver.com>
2023-07-09 00:58:13 +0900

    refactor:VocherController에 추가할 메소드 명 생성

--- src/main/java/com/prgrms/springbootbasic/controller/voucher/VoucherController.java
+++ src/main/java/com/prgrms/springbootbasic/controller/voucher/VoucherController.java
@@ -14,11 +14,33 @@ public class VoucherController {
 
     private final VoucherService voucherService;
 
+    //(Create)바우처 생성
     public void createVoucher(VoucherType type, long discount) {
         voucherService.createVoucher(type, discount);
     }
 
+    //(Read) 바우처의 모든 리스트 출력
     public Map<UUID, Voucher> printVoucherList() {
         return voucherService.fetchAllVouchers();
+    }
+
+    //(Read) 특정 바우처 id의 내용을 출력
+    public void findVoucherByid() {
+
+    }
+
+    //(Update) 생성된 바우처 수정
+    public void updateVoucher() {
+
+    }
+
+    //(Delete) 특정 바우처를 찾아 삭제
+    public void deleteVoucherByid() {
+
+    }
+
+    //(Delete) 모든 바우처의 내용 삭제
+    public void deleteVoucherAll() {
+
     }
 }
