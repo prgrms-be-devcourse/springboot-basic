9efaf1d6990c22630e2d11ac03295bcf8c93eef2
baejunil <bjo6300@naver.com>
2023-07-08 22:10:52 +0900

    fix : voucher, customer PK type 변경

--- springboot-basic/src/main/resources/schema.sql
+++ springboot-basic/src/main/resources/schema.sql
@@ -2,14 +2,14 @@ drop table if exists voucher cascade;
 drop table if exists customer cascade;
 create table customer
 (
-    customer_id    binary(100) primary key,
+    customer_id    varchar(36) default (UUID()) primary key,
     customer_name  varchar(30) not null,
     customer_email varchar(50) not null
 );
 
 create table voucher
 (
-    voucher_id       binary(100) primary key,
+    voucher_id       varchar(36) default (UUID()) primary key,
     voucher_discount integer(10) NOT NULL,
     voucher_type     varchar(20) NOT NULL
 #     customer_id binary(10),
