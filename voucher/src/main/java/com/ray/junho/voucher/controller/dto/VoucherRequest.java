ad07382f3fd99d411fdded78685bd44c8fddb4fc
juno-junho <ssmm0205@naver.com>
2023-07-08 20:01:14 +0900

    refactor: dto record로 변환

--- voucher/src/main/java/com/ray/junho/voucher/controller/dto/VoucherRequest.java
+++ voucher/src/main/java/com/ray/junho/voucher/controller/dto/VoucherRequest.java
@@ -2,27 +2,6 @@ package com.ray.junho.voucher.controller.dto;
 
 import com.ray.junho.voucher.domain.VoucherType;
 
-public class VoucherRequest {
+public record VoucherRequest(VoucherType voucherType, int discountValue, int amount) {
 
-    private final VoucherType voucherType;
-    private final int discountValue;
-    private final int amount;
-
-    public VoucherRequest(VoucherType voucherType, int discountValue, int amount) {
-        this.voucherType = voucherType;
-        this.discountValue = discountValue;
-        this.amount = amount;
-    }
-
-    public int getDiscountValue() {
-        return discountValue;
-    }
-
-    public VoucherType getVoucherType() {
-        return voucherType;
-    }
-
-    public int getAmount() {
-        return amount;
-    }
 }
--- voucher/src/main/java/com/ray/junho/voucher/controller/dto/VoucherResponse.java
+++ voucher/src/main/java/com/ray/junho/voucher/controller/dto/VoucherResponse.java
@@ -1,31 +1,8 @@
 package com.ray.junho.voucher.controller.dto;
 
 import com.ray.junho.voucher.domain.VoucherType;
-import org.apache.logging.log4j.message.Message;
 
-public class VoucherResponse {
-
-    private final long id;
-    private final VoucherType voucherType;
-    private final int discountValue;
-
-    public VoucherResponse(long id, VoucherType voucherType, int discountValue) {
-        this.id = id;
-        this.voucherType = voucherType;
-        this.discountValue = discountValue;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public int getDiscountValue() {
-        return discountValue;
-    }
-
-    public VoucherType getVoucherType() {
-        return voucherType;
-    }
+public record VoucherResponse(long id, VoucherType voucherType, int discountValue) {
 
     public String generateMessage() {
         return String.format("Voucher 번호: %d, 타입: %s, 할인 값: %d", id, voucherType, discountValue);
