8c485a58a335179caf78ffc450ca96190b238d42
byeolhaha <j35635aa@gmail.com>
2023-07-08 23:38:48 +0900

    test : 메서드명 통일 및 정책 생성 로직 추가로 인해 테스트 추가

--- src/test/java/com/prgrms/model/voucher/VoucherTypeTest.java
+++ src/test/java/com/prgrms/model/voucher/VoucherTypeTest.java
@@ -1,14 +1,14 @@
 package com.prgrms.model.voucher;
 
-import com.prgrms.model.voucher.discount.Discount;
-import com.prgrms.model.voucher.discount.FixedDiscount;
+import com.prgrms.model.voucher.dto.discount.Discount;
+import com.prgrms.model.voucher.dto.discount.FixedDiscount;
+import com.prgrms.util.KeyGenerator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class VoucherTypeTest {
     @Test
@@ -16,8 +16,10 @@ class VoucherTypeTest {
     void findByPolicy_FixedVoucherPolicy_Equals() {
         //given
         String policy = "1";
+
         //when
         VoucherType voucherPolicy = VoucherType.findByPolicy(policy);
+
         //then
         assertThat(voucherPolicy).isEqualTo(VoucherType.FIXED_AMOUNT_VOUCHER);
     }
@@ -27,8 +29,10 @@ class VoucherTypeTest {
     void findByPolicy_PercentVoucherPolicy_Equals() {
         //given
         String policy = "2";
+
         //when
         VoucherType voucherPolicy = VoucherType.findByPolicy(policy);
+
         //then
         assertThat(voucherPolicy).isEqualTo(VoucherType.PERCENT_DISCOUNT_VOUCHER);
     }
@@ -38,19 +42,24 @@ class VoucherTypeTest {
     void findByPolicy_NotExistVoucherPolicy_Empty() {
         //given
         String policy = "3";
+
         //when
         VoucherType voucherPolicy = VoucherType.findByPolicy(policy);
+
         //then
         assertThat(voucherPolicy).isEqualTo(Optional.empty());
     }
 
     @Test
     @DisplayName("없는 할인 정책에 대한 예외 테스트")
-    void createVoucher_() {
+    void createVoucher_FixedVoucher_ReturnEqualInstanceOf() {
         // given
+        int voucherId = KeyGenerator.make();
         Discount discount = new FixedDiscount(10); // Example discount value
+
         // when
-        Voucher voucher = VoucherType.FIXED_AMOUNT_VOUCHER.createVoucher(discount);
+        Voucher voucher = VoucherType.FIXED_AMOUNT_VOUCHER.createVoucher(voucherId, discount);
+
         //then
         assertThat(voucher).isNotNull()
                 .isInstanceOf(FixedAmountVoucher.class);
