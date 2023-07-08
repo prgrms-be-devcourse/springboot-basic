0164beb7f5e7ebccef62ece37fefb0d4cb783cdd
byeolhaha <j35635aa@gmail.com>
2023-07-08 21:31:26 +0900

    refactor : CommandFactory를 불러올 수 있도록 수정

---
+++ src/main/java/com/prgrms/view/Menu.java
@@ -0,0 +1,31 @@
+package com.prgrms.view;
+
+import com.prgrms.view.command.Command;
+import com.prgrms.view.command.CommandFactory;
+import com.prgrms.view.message.ErrorMessage;
+
+import java.util.Arrays;
+import java.util.function.Function;
+
+public enum Menu {
+    EXIT(CommandFactory:: createExitCommand),
+    CREATE(CommandFactory::createCreateCommand),
+    LIST(CommandFactory::createListCommand);
+
+    private final Function<CommandFactory,Command> commandFunction;
+
+    Menu(Function<CommandFactory,Command> commandFunction) {
+        this.commandFunction = commandFunction;
+    }
+
+    public static Menu findByMenu(String menu) {
+        return Arrays.stream(values())
+                .filter(m -> m.name().equalsIgnoreCase(menu))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException(ErrorMessage.INVALID_SELECTION.getMessage()));
+    }
+
+    public Command createCommand(CommandFactory commandFactory){
+        return commandFunction.apply(commandFactory);
+    }
+}
