bb171671272a06818a879963fb41ac2cb63e72aa
Dev Uni <wlwhsvkdlxh@gmail.com>
2023-07-09 06:52:15 +0900

    refactor: sql 스키마 수정

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -15,7 +15,7 @@ CREATE TABLE vouchers
     voucher_id  VARCHAR(50) PRIMARY KEY,
     policy      VARCHAR(50) NOT NULL,
     amount      INT NOT NULL,
-    customer_id VARCHAR(50) UNIQUE,
+    customer_id VARCHAR(50),
 
     FOREIGN KEY (customer_id)
         references customers(customer_id)
