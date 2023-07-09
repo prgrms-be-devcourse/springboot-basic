cf98a294b1773fc0b6331f4ad776f805668fef2d
KimMinheee <99alsgmlalsl@naver.com>
2023-07-09 23:19:56 +0900

    [Feat] : schema.sql에 voucher 테이블 ddl 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,9 +1,23 @@
 drop table if exists member_table;
+drop table if exists voucher_table;
 
-create table member_table(
-    member_id varchar(60) not null,
+create table member_table
+(
+    member_id     varchar(60) not null,
     member_status varchar(10) not null,
-    name varchar(20) not null,
+    name          varchar(20) not null,
 
     primary key (member_id)
 );
+
+create table voucher_table
+(
+    voucher_id    varchar(60) not null,
+    voucher_value int         not null,
+    voucher_type  varchar(20) not null,
+
+    primary key (voucher_id)
+);
+
+
+
