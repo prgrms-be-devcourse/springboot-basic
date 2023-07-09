7617a7809c22df3a5c83ee9e6eb79db266b78311
byeolhaha <j35635aa@gmail.com>
2023-07-09 18:54:35 +0900

    refactor : 상수표기법으로 수정

--- src/main/java/com/prgrms/view/message/ErrorMessage.java
+++ src/main/java/com/prgrms/view/message/ErrorMessage.java
@@ -3,7 +3,9 @@ package com.prgrms.view.message;
 public enum ErrorMessage {
 
     INVALID_SELECTION("올바른 선택지가 아닙니다."),
-    Negative_ARGUMENT("양수여야 합니다.");
+    NEGATIVE_ARGUMENT("양수여야 합니다."),
+    NULL_ARGUMENT("null이 발생하였습니다."),
+    NO_CONSTRUCTOR("생성자를 호출할 수 없습니다");
 
     private final String message;
 
