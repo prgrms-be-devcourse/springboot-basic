30130ff84a06f4220c45bfbe3f2a5f0be8ec6831
2jie0516 <2jie0516>
2023-07-09 16:03:58 +0900

    바우처 서비스 클래스에서 percent 입력값에 대한 검증 추가

--- src/main/java/prgms/spring_week1/domain/voucher/service/VoucherService.java
+++ src/main/java/prgms/spring_week1/domain/voucher/service/VoucherService.java
@@ -1,15 +1,19 @@
 package prgms.spring_week1.domain.voucher.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import prgms.spring_week1.domain.voucher.model.Voucher;
 
 import prgms.spring_week1.domain.voucher.model.type.VoucherType;
 import prgms.spring_week1.domain.voucher.repository.VoucherRepository;
+import prgms.spring_week1.io.Input;
 
 import java.util.List;
 
 @Service
 public class VoucherService {
+    private final Logger logger = LoggerFactory.getLogger(VoucherService.class);
     private final VoucherRepository voucherRepository;
 
     public VoucherService(VoucherRepository voucherRepository) {
@@ -17,6 +21,11 @@ public class VoucherService {
     }
 
     public void insertNewVoucher(VoucherType voucherType, int discountAmount) {
+        if(voucherType == VoucherType.PERCENT && discountAmount > 100){
+            logger.warn("100 퍼센트 이상의 값은 넣을 수 없습니다.");
+            throw new NumberFormatException();
+        }
+
         Voucher voucher = new Voucher(voucherType, discountAmount);
         voucherRepository.insert(voucher);
     }
