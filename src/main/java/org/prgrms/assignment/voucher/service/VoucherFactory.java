5cb825921872e5a6142f326d6fecd95053cf2733
BeommoKoo <rnqjaah1536@naver.com>
2023-07-10 01:12:12 +0900

    refactor : fixed / percent Voucher 생성자 파라미터 타입 변경

--- src/main/java/org/prgrms/assignment/voucher/service/VoucherFactory.java
+++ src/main/java/org/prgrms/assignment/voucher/service/VoucherFactory.java
@@ -1,9 +1,6 @@
 package org.prgrms.assignment.voucher.service;
 
-import org.prgrms.assignment.voucher.model.FixedAmountVoucher;
-import org.prgrms.assignment.voucher.model.PercentDiscountVoucher;
-import org.prgrms.assignment.voucher.model.Voucher;
-import org.prgrms.assignment.voucher.model.VoucherType;
+import org.prgrms.assignment.voucher.model.*;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
@@ -12,15 +9,15 @@ import java.util.UUID;
 @Component
 public class VoucherFactory {
 
-
     public Voucher createVoucher(VoucherType voucherType, UUID voucherId, Long benefit, long durationDate) {
         LocalDateTime now = LocalDateTime.now();
+        LocalDateTime expireDate = now.plusDays(durationDate);
         switch (voucherType) {
             case FIXED -> {
-                return new FixedAmountVoucher(voucherId, benefit, now, now.plusDays(durationDate));
+                return new FixedAmountVoucher(voucherId, benefit, now, expireDate);
             }
             case PERCENT -> {
-                return new PercentDiscountVoucher(voucherId, benefit, now, now.plusDays(durationDate));
+                return new PercentDiscountVoucher(voucherId, benefit, now, expireDate);
             }
         }
         throw new RuntimeException("Failed to create Voucher!");
