89e2214e3a7aeb5e9c195f100c18e1a35b7e4b49
hseong3243 <hseong3243@gmail.com>
2023-07-10 00:15:59 +0900

    feat: 바우처 관리 웹을 위한 `VoucherController` 추가

--- src/main/java/com/programmers/voucher/domain/voucher/dto/request/VoucherCreateRequest.java
+++ src/main/java/com/programmers/voucher/domain/voucher/dto/request/VoucherCreateRequest.java
@@ -6,8 +6,8 @@ public class VoucherCreateRequest {
     private final VoucherType voucherType;
     private final long amount;
 
-    public VoucherCreateRequest(VoucherType type, long amount) {
-        this.voucherType = type;
+    public VoucherCreateRequest(VoucherType voucherType, long amount) {
+        this.voucherType = voucherType;
         this.amount = amount;
     }
 
