dfacff6de5a85d8cfbd9ebaeca7b673924551c31
KimMinheee <99alsgmlalsl@naver.com>
2023-07-09 22:57:51 +0900

    [Refactor] : 이전 테스트 코드 일부 수정 - file 테스트 코드 bean 주입 - menu 입력 테스트

--- src/test/java/org/programmers/VoucherManagement/io/MenuTypeTest.java
+++ src/test/java/org/programmers/VoucherManagement/io/MenuTypeTest.java
@@ -10,26 +10,26 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 public class MenuTypeTest {
     @Test
     @DisplayName("입력값에 따라 CommandType을 반환하는 테스트 - 성공")
-    void 입력값에따라_Command를_반환_성공(){
+    void 입력값에따라_Command를_반환_성공() {
         //given
-        String blackListInput = "blacklist";
-        String createInput = "create";
+        int listInput = 8;
+        int createInput = 7;
 
         //when
-        MenuType blackListTypeExpect = MenuType.from(blackListInput);
+        MenuType listTypeExpect = MenuType.from(listInput);
         MenuType createTypeExpect = MenuType.from(createInput);
 
         //then
-        assertThat(blackListTypeExpect).isEqualTo(MenuType.BLACKLIST);
+        assertThat(listTypeExpect).isEqualTo(MenuType.LIST);
         assertThat(createTypeExpect).isEqualTo(MenuType.CREATE);
     }
 
     @Test
     @DisplayName("입력값에 따라 CommandType을 반환하는 테스트 - 실패")
-    void 입력값에따라_Command를_반환_실패(){
-        String input ="yellowlist";
+    void 입력값에따라_Command를_반환_실패() {
+        int input = 43;
 
-        assertThatThrownBy(()-> MenuType.from(input))
+        assertThatThrownBy(() -> MenuType.from(input))
                 .isInstanceOf(VoucherException.class)
                 .hasMessage("해당하는 Command가 존재하지 않습니다.");
     }
--- src/test/java/org/programmers/VoucherManagement/member/infrastructure/FileMemberRepositoryTest.java
+++ src/test/java/org/programmers/VoucherManagement/member/infrastructure/FileMemberRepositoryTest.java
@@ -2,9 +2,7 @@ package org.programmers.VoucherManagement.member.infrastructure;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
 import org.programmers.VoucherManagement.member.domain.Member;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -13,15 +11,15 @@ import java.util.List;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-
 @SpringBootTest(classes = {FileMemberRepository.class})
 public class FileMemberRepositoryTest {
+
     @Autowired
-    MemberRepository memberRepository;
+    FileMemberRepository memberRepository;
 
     @Test
     @DisplayName("csv파일을읽고 Member리스트화하여 반환 테스트 - 성공")
-    void csv파일을읽고_Member리스트화하여_반환_성공(){
+    void csv파일을읽고_Member리스트화하여_반환_성공() {
         assertThatCode(() -> {
             List<Member> members = memberRepository.findAllByMemberStatus();
             assertTrue(!members.isEmpty());
