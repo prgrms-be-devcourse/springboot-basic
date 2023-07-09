4aa4f5a253b7d5d2b4ceff0bf94295fbc218aa15
weonest <geonhee33@naver.com>
2023-07-09 23:59:50 +0900

    feat: JdbcCustomerRepository 모든 기능 구현 및 테스트

--- src/main/java/com/programmers/customer/repository/JdbcCustomerRepository.java
+++ src/main/java/com/programmers/customer/repository/JdbcCustomerRepository.java
@@ -1,6 +1,7 @@
 package com.programmers.customer.repository;
 
 import com.programmers.customer.domain.Customer;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
@@ -8,13 +9,13 @@ import org.springframework.stereotype.Repository;
 import javax.sql.DataSource;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.UUID;
+import java.util.*;
 
 @Repository
 public class JdbcCustomerRepository implements CustomerRepository {
+
+    private static final String NOT_FOUND_ERROR_MESSAGE = "[ERROR] 해당 요청에 대한 결과를 찾을 수 없습니다.";
+    private static final String UPDATE_FAIL_MESSAGE = "[ERROR] 수정 요청에 대한 값을 찾지 못했습니다..";
 
     private NamedParameterJdbcTemplate jdbcTemplate;
 
@@ -32,22 +33,36 @@ public class JdbcCustomerRepository implements CustomerRepository {
 
     @Override
     public Customer update(Customer customer) {
-        return null;
+        String sql = "update customer set name = :name , modified_at = :modifiedAt where id = :id";
+        int update = jdbcTemplate.update(sql, converParameterToMap(customer));
+        if (update != 1) {
+            throw new RuntimeException(UPDATE_FAIL_MESSAGE);
+        }
+        return customer;
     }
 
     @Override
     public List<Customer> findAll() {
-        return null;
+        String sql = "select * from customer";
+        return jdbcTemplate.query(sql, customerRowMapper());
     }
 
     @Override
     public Customer findById(UUID customerId) {
-        return null;
+        String sql = "select * from customer where id = :id";
+        try {
+            return jdbcTemplate.queryForObject(sql,
+                    Collections.singletonMap("id", customerId.toString()),
+                    customerRowMapper());
+        } catch (EmptyResultDataAccessException e) {
+            throw new NoSuchElementException(NOT_FOUND_ERROR_MESSAGE);
+        }
     }
 
     @Override
     public void deleteById(UUID customerId) {
-
+        String sql = "delete from customer where id = :id";
+        jdbcTemplate.update(sql, Collections.singletonMap("id", customerId.toString()));
     }
 
     private Map<String, Object> converParameterToMap (Customer customer) {
--- src/test/java/com/programmers/customer/repository/JdbcCustomerRepositoryTest.java
+++ src/test/java/com/programmers/customer/repository/JdbcCustomerRepositoryTest.java
@@ -12,9 +12,12 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @ActiveProfiles("test")
 @Transactional
@@ -33,11 +36,64 @@ class JdbcCustomerRepositoryTest {
     @DisplayName("DB에 고객 정보를 저장할 수 있다.")
     @Test
     void insertVoucherTest() {
-
         Customer testCustomer = new Customer(UUID.randomUUID(), "고객", LocalDateTime.now());
 
         Customer storedCustomer = jdbcCustomerRepository.save(testCustomer);
 
         assertThat(testCustomer.getCustomerId()).isEqualTo(storedCustomer.getCustomerId());
+    }
+
+    @DisplayName("저장된 고객의 정보를 수정할 수 있다.")
+    @Test
+    void updateCustomerTest() {
+        UUID id = UUID.randomUUID();
+        Customer oldCustomer = new Customer(id, "고객", LocalDateTime.now());
+        jdbcCustomerRepository.save(oldCustomer);
+        Customer newCustomer = new Customer(id, "손님", LocalDateTime.now(), LocalDateTime.now());
+
+        Customer updatedCustomer = jdbcCustomerRepository.update(newCustomer);
+
+        assertThat(oldCustomer.getCustomerId()).isEqualTo(updatedCustomer.getCustomerId());
+        assertThat(oldCustomer.getName()).isNotEqualTo(updatedCustomer.getName());
+    }
+
+    @DisplayName("저장된 모든 고객을 조회할 수 있다.")
+    @Test
+    void findAllCustomersTest() {
+        Customer testCustomer = new Customer(UUID.randomUUID(), "고객", LocalDateTime.now());
+        Customer testCustomer2 = new Customer(UUID.randomUUID(), "고객", LocalDateTime.now());
+        Customer testCustomer3 = new Customer(UUID.randomUUID(), "고객", LocalDateTime.now());
+        jdbcCustomerRepository.save(testCustomer);
+        jdbcCustomerRepository.save(testCustomer2);
+        jdbcCustomerRepository.save(testCustomer3);
+
+        List<Customer> customers = jdbcCustomerRepository.findAll();
+
+        assertThat(customers.size()).isEqualTo(3);
+    }
+
+    @DisplayName("저장된 고객을 ID로 조회할 수 있다.")
+    @Test
+    void findCustomerByIdTest() {
+        UUID id = UUID.randomUUID();
+        Customer testCustomer = new Customer(id, "고객", LocalDateTime.now());
+        jdbcCustomerRepository.save(testCustomer);
+
+        Customer storedCustomer = jdbcCustomerRepository.findById(id);
+
+        assertThat(testCustomer.getCustomerId()).isEqualTo(storedCustomer.getCustomerId());
+    }
+
+    @DisplayName("저장된 고객을 ID를 통해 삭제할 수 있다.")
+    @Test
+    void deleteCustomerByIdTest() {
+        UUID id = UUID.randomUUID();
+        Customer testCustomer = new Customer(id, "고객", LocalDateTime.now());
+        jdbcCustomerRepository.save(testCustomer);
+
+        jdbcCustomerRepository.deleteById(id);
+
+        assertThatThrownBy(()-> jdbcCustomerRepository.findById(id))
+                .isInstanceOf(NoSuchElementException.class);
     }
 }
