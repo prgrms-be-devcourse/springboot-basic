2b2b2c619e022944b23d7a290c47742be34e9a06
byeolhaha <j35635aa@gmail.com>
2023-07-09 16:50:38 +0900

    refactor : 기본생성자를 호출하지 못하도록 예외 던지도록 수정

--- src/main/java/com/prgrms/model/voucher/dto/mapper/DtoConverter.java
+++ src/main/java/com/prgrms/model/voucher/dto/mapper/DtoConverter.java
@@ -1,15 +1,20 @@
 package com.prgrms.model.voucher.dto.mapper;
 
+import com.prgrms.model.voucher.Vouchers;
 import com.prgrms.model.voucher.dto.VoucherResponse;
-import com.prgrms.model.voucher.VoucherRegistry;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Component
 public class DtoConverter {
-    public List<VoucherResponse> convertVoucherResponse(VoucherRegistry vouchers) {
-        return vouchers.getVoucherRegistry()
+
+    private DtoConverter() {
+        throw new AssertionError("생성자를 호출할 수 없습니다.");
+    }
+
+    public List<VoucherResponse> convertVoucherResponse(Vouchers vouchers) {
+        return vouchers.getVouchers()
                 .stream()
                 .map(VoucherResponse::new)
                 .toList();
