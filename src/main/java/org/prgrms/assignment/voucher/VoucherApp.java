ec232d19376c8fa42d58a8104574fc0f3bd4e421
BeommoKoo <rnqjaah1536@naver.com>
2023-07-10 01:02:36 +0900

    chore : VoucherApp SpringBootApp변경에 따른 Config추가

--- src/main/java/org/prgrms/assignment/voucher/VoucherApp.java
+++ src/main/java/org/prgrms/assignment/voucher/VoucherApp.java
@@ -1,26 +1,17 @@
 package org.prgrms.assignment.voucher;
 
-import org.prgrms.assignment.voucher.controller.VoucherController;
-import org.prgrms.kdt.KdtApplication;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Profile;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-@Profile({"dev"})
-public class VoucherApp {
+@ComponentScan(basePackages = {"org.prgrms.assignment.voucher.configuration"})
+//@Profile({"dev"})
+public class VoucherApp implements WebMvcConfigurer {
 
     public static void main(String[] args) {
-        ConfigurableApplicationContext applicationContext = SpringApplication.run(VoucherApp.class, args);
-//        applicationContext.getBean(MyController.class).run();
-//        applicationContext.register(VoucherAppConfiguration.class);
-//        ConfigurableEnvironment environment = applicationContext.getEnvironment();
-//        environment.setActiveProfiles("local");
-//        applicationContext.refresh();
-        applicationContext.getBean(VoucherController.class).run();
+        SpringApplication.run(VoucherApp.class, args);
     }
+
 }
