224b149ac660094a6b5596f126ad3e62152b3292
박은지 <qkrdmswl1018@naver.com>
2023-07-09 19:53:43 +0900

    refactor: PATTERN 변수명 변경

--- src/main/java/com/prgrms/commandLineApplication/customer/validator/CustomerValidator.java
+++ src/main/java/com/prgrms/commandLineApplication/customer/validator/CustomerValidator.java
@@ -6,8 +6,8 @@ import java.util.regex.Pattern;
 
 public class CustomerValidator {
 
-  private static final String EMAIL_FORMAT = "^[_a-z0-9-]+(.[_a-z0-9-]+)*@(?:\\w+\\.)+\\w+$";
-  private static final Pattern PATTERN = Pattern.compile(EMAIL_FORMAT);
+  private static final String REG_EXP_EMAIL = "^[_a-z0-9-]+(.[_a-z0-9-]+)*@(?:\\w+\\.)+\\w+$";
+  private static final Pattern EMAIL_PATTERN= Pattern.compile(REG_EXP_EMAIL);
 
   private static final String INVALID_ID_ERROR = "Invalid ID";
   private static final String INVALID_EMAIL_ERROR = "Invalid Email";
@@ -19,7 +19,7 @@ public class CustomerValidator {
   }
 
   public static void checkEmail(String email) {
-    Matcher matchers = PATTERN.matcher(email);
+    Matcher matchers = EMAIL_PATTERN.matcher(email);
 
     if (email == null || !matchers.matches()) {
       throw new IllegalArgumentException(INVALID_EMAIL_ERROR);
