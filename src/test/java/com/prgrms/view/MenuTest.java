b64b570f64f3939e9349cd23469ec1c91fcfba69
byeolhaha <j35635aa@gmail.com>
2023-07-09 18:46:50 +0900

    test : 로직 수정으로 인해 수정

--- src/test/java/com/prgrms/view/MenuTest.java
+++ src/test/java/com/prgrms/view/MenuTest.java
@@ -1,24 +1,25 @@
 package com.prgrms.view;
 
+import com.prgrms.controller.VoucherController;
 import com.prgrms.view.command.Command;
-import com.prgrms.view.command.CommandFactory;
+import com.prgrms.view.command.CreateCommand;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-//assertJ
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.mockito.Mockito.*;
 
 
 class MenuTest {
 
     @Mock
-    private CommandFactory commandFactory;
+    private Command command;
+    private VoucherController voucherController;
+    private ViewManager viewManager;
 
     @Test
-    @DisplayName("소문자 list")
+    @DisplayName("소문자 list를 입력했을 때 제대로 된 메뉴 LIST를 반환하는지 확인한다.")
     public void FindByMenu_LowerList_Equals() {
         String selectMenu = "list";
         Menu menu = Menu.findByMenu(selectMenu);
@@ -27,7 +28,7 @@ class MenuTest {
     }
 
     @Test
-    @DisplayName("대문자 LIST")
+    @DisplayName("대문자 LIST를 입력했을 때 제대로 된 메뉴 LIST를 반환하는지 확인한다.")
     public void FindByMenu_UpperList_Equals() {
         String selectMenu = "LIST";
         Menu menu = Menu.findByMenu(selectMenu);
@@ -36,7 +37,7 @@ class MenuTest {
     }
 
     @Test
-    @DisplayName("소문자 create")
+    @DisplayName("소문자 create를 입력했을 때 제대로 된 메뉴 CREATE를 반환하는지 확인한다.")
     public void FindByMenu_LowerCreate_Equals() {
         String selectMenu = "create";
         Menu menu = Menu.findByMenu(selectMenu);
@@ -45,7 +46,7 @@ class MenuTest {
     }
 
     @Test
-    @DisplayName("대문자 CREATE")
+    @DisplayName("대문자 CREATE를 입력했을 때 제대로 된 메뉴 CREATE를 반환하는지 확인한다.")
     public void FindByMenu_UpperCreate_Equals() {
         String selectMenu = "CREATE";
         Menu menu = Menu.findByMenu(selectMenu);
@@ -54,7 +55,7 @@ class MenuTest {
     }
 
     @Test
-    @DisplayName("소문자 exit")
+    @DisplayName("소문자 exit를 입력했을 때 제대로 된 메뉴 EXIT를 반환하는지 확인한다.")
     public void FindByMenu_LowerExit_Equals() {
         String selectMenu = "exit";
         Menu menu = Menu.findByMenu(selectMenu);
@@ -63,7 +64,7 @@ class MenuTest {
     }
 
     @Test
-    @DisplayName("대문자 EXIT")
+    @DisplayName("대문자 EXIT를 입력했을 때 제대로 된 메뉴 EXIT를 반환하는지 확인한다.")
     public void FindByMenu_UpperExit_Equals() {
         String selectMenu = "EXIT";
         Menu menu = Menu.findByMenu(selectMenu);
@@ -72,7 +73,7 @@ class MenuTest {
     }
 
     @Test
-    @DisplayName("대소문자 섞인 메뉴")
+    @DisplayName("대소문자 섞인 메뉴를 입력했을 때 제대로 된 대문자 메뉴를 반환하는지 확인한다.")
     public void FindByMenu_MixedMenu_Equals() {
         String selectMenu = "ExIt";
         Menu menu = Menu.findByMenu(selectMenu);
@@ -81,7 +82,7 @@ class MenuTest {
     }
 
     @Test
-    @DisplayName("없는 메뉴에 대한 예외 테스트")
+    @DisplayName("없는 메뉴에 대한 예외를 반환한다.")
     public void findByMenu_NotExistMenu_ThrowsException() {
         String menu = "cccc";
 
@@ -91,29 +92,16 @@ class MenuTest {
     }
 
     @Test
+    @DisplayName("create 메뉴가 CreateCommand를 반환하는지 확인한다.")
     void createCommand_CorrectCommandInstanceBasedOnMenu() {
         //given
-        MockitoAnnotations.openMocks(this);
         Menu menu = Menu.CREATE;
-        Command mockCommand = mock(Command.class);
-        when(commandFactory.createCreateCommand()).thenReturn(mockCommand);
 
         //when
-        Command result = menu.createCommand(commandFactory);
+        Command command = menu.createCommand(voucherController, viewManager);
 
-        // then
-        switch (menu) {
-            case EXIT:
-                verify(commandFactory, times(1)).createExitCommand();
-                break;
-            case CREATE:
-                verify(commandFactory, times(1)).createCreateCommand();
-                break;
-            case LIST:
-                verify(commandFactory, times(1)).createListCommand();
-                break;
-        }
-
-        assertThat(result).isEqualTo(mockCommand);
+        //then
+        assertThat(command).isInstanceOf(CreateCommand.class);
     }
+
 }
