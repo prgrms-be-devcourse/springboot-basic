e5bcf545ca7a467c64a32ae31d967cd0cf6ffbc0
weonest <geonhee33@naver.com>
2023-07-10 22:41:57 +0900

    refactor: final 설정

--- src/main/java/com/programmers/customer/service/CustomerServiceImpl.java
+++ src/main/java/com/programmers/customer/service/CustomerServiceImpl.java
@@ -16,7 +16,7 @@ import java.util.UUID;
 @Service
 public class CustomerServiceImpl implements CustomerService {
 
-    private CustomerRepository customerRepository;
+    private final CustomerRepository customerRepository;
 
     public CustomerServiceImpl(CustomerRepository customerRepository) {
         this.customerRepository = customerRepository;
