36c9e50dc875f08347c7b4fbd93fa6f2cf1b540c
kimihiqq <kimihiqq@gmail.com>
2023-07-10 16:54:48 +0900

    feat: thymeleaf를 이용한 바우처 서비스 관리페이지 생성

--- src/main/java/me/kimihiqq/vouchermanagement/VoucherManagementApplication.java
+++ src/main/java/me/kimihiqq/vouchermanagement/VoucherManagementApplication.java
@@ -1,21 +1,24 @@
 package me.kimihiqq.vouchermanagement;
 
 import lombok.extern.slf4j.Slf4j;
-import me.kimihiqq.vouchermanagement.controller.VoucherConsoleController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 
 @Slf4j
 @SpringBootApplication
 public class VoucherManagementApplication {
 
+//    public static void main(String[] args) {
+//        log.info("Starting Voucher Management Application");
+//        ApplicationContext applicationContext = SpringApplication.run(VoucherManagementApplication.class, args);
+//        applicationContext.getBean(VoucherConsoleController.class).run();
+//        log.info("Voucher Management Application has stopped");
+//    }
     public static void main(String[] args) {
         log.info("Starting Voucher Management Application");
-        ApplicationContext applicationContext = SpringApplication.run(VoucherManagementApplication.class, args);
-        applicationContext.getBean(VoucherConsoleController.class).run();
-        log.info("Voucher Management Application has stopped");
+        SpringApplication.run(VoucherManagementApplication.class, args);
+        log.info("Voucher Management Application has started");
     }
 }
 
