c58a87f4330c251f99c187ccc4afb2a0ee464eff
sujjangOvO <pnk7038@gmail.com>
2023-07-10 19:41:20 +0900

    VoucherType enum의 매핑데이터를 숫자로 다시 변경

--- src/main/java/com/prgms/voucher/voucherproject/domain/VoucherType.java
+++ src/main/java/com/prgms/voucher/voucherproject/domain/VoucherType.java
@@ -5,18 +5,18 @@ import java.util.Objects;
 import java.util.stream.Stream;
 
 public enum VoucherType {
-    FIXED("1"),
-    PERCENT("2");
+    FIXED(1),
+    PERCENT(2);
 
-    private String voucherNum;
+    private int voucherNum;
 
-    VoucherType(String voucherNum) {
+    VoucherType(int voucherNum) {
         this.voucherNum = voucherNum;
     }
 
-    public static VoucherType getSelectedVoucherType(String selectedNum) {
+    public static VoucherType getSelectedVoucherType(int selectedNum) {
         return Stream.of(VoucherType.values())
-                .filter(voucherType -> Objects.equals(voucherType.voucherNum, selectedNum))
+                .filter(voucherType -> voucherType.voucherNum == selectedNum)
                 .findFirst()
                 .orElseThrow(() -> new InputMismatchException("존재하지 않는 바우처 타입입니다."));
     }
