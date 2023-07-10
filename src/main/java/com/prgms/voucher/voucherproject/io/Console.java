3e5a2cae307a17d9113bcee291d7c72f8bae6a11
sujjangOvO <pnk7038@gmail.com>
2023-07-10 19:42:36 +0900

    Console 기본 생성자 삭제 및 숫자값 입력 검증 부분(미완성)

--- src/main/java/com/prgms/voucher/voucherproject/io/Console.java
+++ src/main/java/com/prgms/voucher/voucherproject/io/Console.java
@@ -4,15 +4,15 @@ package com.prgms.voucher.voucherproject.io;
 import com.prgms.voucher.voucherproject.domain.FixedAmountVoucher;
 import com.prgms.voucher.voucherproject.domain.PercentDiscountVoucher;
 import com.prgms.voucher.voucherproject.domain.Voucher;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Console implements Input, Output {
-
-    public Console() {
-    }
-
     private static final Scanner sc = new Scanner(System.in);
+    private static final Logger logger = LoggerFactory.getLogger(Console.class);
 
     @Override
     public void printErrorMsg() {
@@ -48,6 +48,18 @@ public class Console implements Input, Output {
     @Override
     public String inputCommand() {
         return sc.nextLine();
+    }
+
+    public int inputIntType() {
+        int selectedNum = -1;
+        try {
+            selectedNum = sc.nextInt();
+        } catch (InputMismatchException e) {
+            logger.error("VoucherType InputMismatchException");
+            System.out.println(e.getLocalizedMessage());
+        }
+        sc.nextLine();
+        return selectedNum;
     }
 
     @Override
