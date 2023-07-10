9a86536597217c6b1fb93c2a4bfce2d23625903d
sujjangOvO <pnk7038@gmail.com>
2023-07-10 19:39:19 +0900

    문자열 비교를 equlasIgnoreCase로 변경하여 대소문자 구분 제거

--- src/main/java/com/prgms/voucher/voucherproject/domain/MenuType.java
+++ src/main/java/com/prgms/voucher/voucherproject/domain/MenuType.java
@@ -2,8 +2,6 @@ package com.prgms.voucher.voucherproject.domain;
 
 import java.util.Arrays;
 import java.util.InputMismatchException;
-import java.util.Objects;
-
 public enum MenuType {
 
     EXIT,
@@ -12,7 +10,7 @@ public enum MenuType {
 
     public static MenuType getSelectedMenuType(String menuName) {
         return Arrays.stream(MenuType.values())
-                .filter(menuType -> Objects.equals(menuType.name().toLowerCase(), menuName))
+                .filter(menuType -> menuType.name().equalsIgnoreCase(menuName))
                 .findAny()
                 .orElseThrow(() -> new InputMismatchException("잘못된 메뉴 명령어입니다."));
     }
