6b679e11e11e239559d84909661ee2295f4faa0f
Juwoong <wwong7247@gmail.com>
2023-07-10 15:01:33 +0900

    데이터 길이를 줄이기 위해 VoucherId를 VARCHAR 형식으로 변경

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,8 +1,8 @@
 DROP TABLE IF EXISTS voucher;
 
 CREATE TABLE voucher (
-    voucher_id BINARY(16) NOT NULL,
-    value BIGINT NOT NULL,
-    voucher_type VARCHAR(255) NOT NULL,
+    voucher_id VARCHAR(200) NOT NULL,
+    discount_value BIGINT NOT NULL,
+    voucher_type VARCHAR(200) NOT NULL,
     PRIMARY KEY (voucher_id)
 );
