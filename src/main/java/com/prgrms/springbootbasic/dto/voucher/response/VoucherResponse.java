00486b78b2f1f7640dbfa3c0d4d06b98beb12cbe
jay-so <merry9504@naver.com>
2023-07-11 00:07:51 +0900

    feat: Voucher 응답 Dto 생성

---
+++ src/main/java/com/prgrms/springbootbasic/dto/voucher/response/VoucherResponse.java
@@ -0,0 +1,17 @@
+package com.prgrms.springbootbasic.dto.voucher.response;
+
+import com.prgrms.springbootbasic.enums.VoucherType;
+import java.time.LocalDateTime;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class VoucherResponse {
+
+    private final UUID voucherId;
+    private long discount;
+    private VoucherType type;
+    private final LocalDateTime createAt;
+}
