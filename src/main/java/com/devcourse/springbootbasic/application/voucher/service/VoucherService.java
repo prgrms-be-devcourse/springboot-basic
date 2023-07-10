00a03cbda7fb8a5f82373a6859ce6834fb3e8843
SY97P <onetuks@naver.com>
2023-07-10 23:07:50 +0900

    feat: VoucherService DB 대응

--- src/main/java/com/devcourse/springbootbasic/application/voucher/service/VoucherService.java
+++ src/main/java/com/devcourse/springbootbasic/application/voucher/service/VoucherService.java
@@ -1,5 +1,7 @@
 package com.devcourse.springbootbasic.application.voucher.service;
 
+import com.devcourse.springbootbasic.application.global.exception.ErrorMessage;
+import com.devcourse.springbootbasic.application.global.exception.InvalidDataException;
 import com.devcourse.springbootbasic.application.voucher.model.Voucher;
 import com.devcourse.springbootbasic.application.voucher.repository.VoucherRepository;
 import org.springframework.stereotype.Service;
@@ -19,7 +21,27 @@ public class VoucherService {
         return voucherRepository.insert(voucher);
     }
 
+    public Voucher updateVoucher(Voucher voucher) {
+        return voucherRepository.update(voucher);
+    }
+
     public List<Voucher> getVouchers() {
         return voucherRepository.findAllVouchers();
     }
+
+    public Voucher findVoucherById(Voucher voucher) {
+        return voucherRepository.findById(voucher.getVoucherId())
+                .orElseThrow(() -> new InvalidDataException(ErrorMessage.INVALID_VOUCHER_INFO.getMessageText()));
+    }
+
+    public void deleteAllVouchers() {
+        voucherRepository.deleteAll();
+    }
+
+    public Voucher deleteVoucherById(Voucher voucher) {
+        var foundVoucher = voucherRepository.findById(voucher.getVoucherId());
+        voucherRepository.deleteById(voucher.getVoucherId());
+        return foundVoucher.orElseThrow(() -> new InvalidDataException(ErrorMessage.INVALID_VOUCHER_INFO.getMessageText()));
+    }
+
 }
--- src/test/java/com/devcourse/springbootbasic/application/voucher/service/VoucherServiceTest.java
+++ src/test/java/com/devcourse/springbootbasic/application/voucher/service/VoucherServiceTest.java
@@ -1,17 +1,23 @@
 package com.devcourse.springbootbasic.application.voucher.service;
 
+import com.devcourse.springbootbasic.application.global.exception.InvalidDataException;
 import com.devcourse.springbootbasic.application.voucher.model.Voucher;
 import com.devcourse.springbootbasic.application.voucher.vo.DiscountValue;
 import com.devcourse.springbootbasic.application.voucher.vo.VoucherType;
 import com.devcourse.springbootbasic.application.voucher.repository.VoucherRepository;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import com.wix.mysql.EmbeddedMysql;
+import com.wix.mysql.ScriptResolver;
+import com.wix.mysql.config.Charset;
+import com.wix.mysql.config.MysqldConfig;
+import com.wix.mysql.distribution.Version;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
@@ -21,45 +27,120 @@ import java.util.stream.Stream;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
 
-@ActiveProfiles("dev")
+@SpringBootTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@ActiveProfiles("default")
 class VoucherServiceTest {
 
-    @Mock
-    VoucherRepository voucherRepository;
+    @Autowired
     VoucherService voucherService;
 
-    static Stream<Arguments> provideVouchers() {
-        return vouchers.stream()
-                .map(Arguments::of);
+    EmbeddedMysql embeddedMysql;
+
+    @BeforeAll
+    void init() {
+        var mysqlConfig = MysqldConfig.aMysqldConfig(Version.v8_0_17)
+                .withCharset(Charset.UTF8)
+                .withTimeZone("Asia/Seoul")
+                .withUser("test", "test1234!")
+                .withPort(8070)
+                .build();
+        embeddedMysql = EmbeddedMysql.anEmbeddedMysql(mysqlConfig)
+                .addSchema("test-voucher_system", ScriptResolver.classPathScript("test-voucher_schema.sql"))
+                .start();
     }
 
     @BeforeEach
-    void init() {
-        MockitoAnnotations.openMocks(this);
-        voucherService = new VoucherService(voucherRepository);
+    void cleanup() {
+        voucherService.deleteAllVouchers();
+    }
+
+    @AfterAll
+    void destroy() {
+        embeddedMysql.stop();
     }
 
     @ParameterizedTest
     @DisplayName("새로운 바우처가 추가되면 성공한다.")
     @MethodSource("provideVouchers")
-    void CreateVoucher_VoucherParam_InsertAndReturnVoucher(Voucher voucher) {
-        given(voucherRepository.insert(voucher)).willReturn(voucher);
-        var result = voucherService.createVoucher(voucher);
+    void createVoucher_VoucherParam_InsertAndReturnVoucher(Voucher voucher) {
+        voucherService.createVoucher(voucher);
+        var result = voucherService.findVoucherById(voucher);
         assertThat(result, notNullValue());
         assertThat(result, instanceOf(Voucher.class));
         assertThat(result.getVoucherId(), is(voucher.getVoucherId()));
     }
 
+    @ParameterizedTest
+    @DisplayName("이미 존재하는 바우처를 또 생성하려고 하면 실패한다.")
+    @MethodSource("provideVouchers")
+    void createVoucher_ParamExistVoucher_Exception(Voucher voucher) {
+        voucherService.createVoucher(voucher);
+        Assertions.assertThrows(InvalidDataException.class, () -> voucherService.createVoucher(voucher));
+    }
+
+    @ParameterizedTest
+    @DisplayName("존재하는 바우처를 갱신하려고 하면 성공한다.")
+    @MethodSource("provideVouchers")
+    void updateVoucher_ParamExistVoucher_UpdateAndReturnVoucher(Voucher voucher) {
+        voucherService.createVoucher(voucher);
+        var newVoucher = new Voucher(voucher.getVoucherId(), VoucherType.FIXED_AMOUNT, DiscountValue.from(VoucherType.FIXED_AMOUNT, 124));
+        voucherService.updateVoucher(newVoucher);
+        var result = voucherService.findVoucherById(newVoucher);
+        assertThat(result, samePropertyValuesAs(newVoucher));
+    }
+
+    @ParameterizedTest
+    @DisplayName("존재하지 않는 바우처를 갱신하려고 하면 실패한다.")
+    @MethodSource("provideVouchers")
+    void updateVoucher_ParamNotExistVoucher_UpdateAndReturnVoucher(Voucher voucher) {
+        Assertions.assertThrows(InvalidDataException.class, () -> voucherService.updateVoucher(voucher));
+    }
+
     @Test
     @DisplayName("생성된 바우처가 리스트 형태로 반환되면 성공한다.")
-    void GetVouchers_VoucherMap_ReturnVouchers() {
-        given(voucherRepository.findAllVouchers()).willReturn(vouchers);
+    void getVouchers_VoucherMap_ReturnVouchers() {
+        voucherService.createVoucher(vouchers.get(0));
         var result = voucherService.getVouchers();
         assertThat(result, notNullValue());
         assertThat(result, not(empty()));
         assertThat(result, instanceOf(List.class));
         assertThat(result.get(0), instanceOf(Voucher.class));
+    }
+
+    @ParameterizedTest
+    @DisplayName("존재하는 바우처를 아이디로 검색하는 경우 성공한다.")
+    @MethodSource("provideVouchers")
+    void findVoucherById_ParamExistVoucher_ReturnVoucher(Voucher voucher) {
+        voucherService.createVoucher(voucher);
+        var foundVoucher = voucherService.findVoucherById(voucher);
+        assertThat(foundVoucher, samePropertyValuesAs(voucher));
+    }
+
+    @ParameterizedTest
+    @DisplayName("존재하지 않는 바우처를 아이디로 검색하는 경우 실패한다.")
+    @MethodSource("provideVouchers")
+    void findVoucherById_ParamNotExistVoucher_Exception(Voucher voucher) {
+        Assertions.assertThrows(InvalidDataException.class, () -> voucherService.findVoucherById(voucher));
+    }
+
+    @Test
+    @DisplayName("모든 바우처를 제거한다.")
+    void deleteAllVouchers_ParamVoid_DeleteAllVouchers() {
+        voucherService.deleteAllVouchers();
+        var vouchers = voucherService.getVouchers();
+        assertThat(vouchers.isEmpty(), is(true));
+    }
+
+    @ParameterizedTest
+    @DisplayName("아이디로 바우처 제거한다.")
+    @MethodSource("provideVouchers")
+    void deleteVoucherById_PararmVoucher_DeleteVoucher(Voucher voucher) {
+        voucherService.createVoucher(voucher);
+        var deletedVoucher = voucherService.deleteVoucherById(voucher);
+        assertThat(deletedVoucher, samePropertyValuesAs(voucher));
     }
 
     static List<Voucher> vouchers = List.of(
@@ -68,5 +149,10 @@ class VoucherServiceTest {
             new Voucher(UUID.randomUUID(), VoucherType.FIXED_AMOUNT, DiscountValue.from(VoucherType.FIXED_AMOUNT, "1240")),
             new Voucher(UUID.randomUUID(), VoucherType.PERCENT_DISCOUNT, DiscountValue.from(VoucherType.PERCENT_DISCOUNT, "10"))
     );
+
+    static Stream<Arguments> provideVouchers() {
+        return vouchers.stream()
+                .map(Arguments::of);
+    }
 
 }
