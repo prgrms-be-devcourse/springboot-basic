a9b835b197e742f71ff668c303964167720e6a3c
hseong3243 <hseong3243@gmail.com>
2023-07-10 15:26:37 +0900

    feat: `Customer` 생성 시간을 나타내는 `createdAt` 필드 추가

--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -1,15 +1,18 @@
 drop table if exists voucher;
-create table voucher (
-    voucher_id uuid primary key,
-    voucher_type varchar(20) not null,
-    amount bigint not null,
-    created_at timestamp(9) not null
+create table voucher
+(
+    voucher_id   uuid primary key,
+    voucher_type varchar(20)  not null,
+    amount       bigint       not null,
+    created_at   timestamp(9) not null
 );
 
 drop table if exists customer;
-create table customer (
+create table customer
+(
     customer_id uuid primary key,
-    email varchar(50) not null,
-    name varchar(20) not null,
-    banned boolean not null
+    email       varchar(50)  not null,
+    name        varchar(20)  not null,
+    banned      boolean      not null,
+    created_at  timestamp(9) not null
 )
