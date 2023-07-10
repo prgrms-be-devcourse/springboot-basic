8eb2131a6c28fe02b7e9e1c66dfd0e4ec0a31c4a
jay-so <merry9504@naver.com>
2023-07-11 00:03:58 +0900

    feat:CreateRequsetDto 생성

--- src/main/java/com/prgrms/springbootbasic/dto/voucher/request/VoucherCreateRequest.java
+++ src/main/java/com/prgrms/springbootbasic/dto/voucher/request/VoucherCreateRequest.java
@@ -2,16 +2,14 @@ package com.prgrms.springbootbasic.dto.voucher.request;
 
 import com.prgrms.springbootbasic.enums.VoucherType;
 import java.time.LocalDateTime;
-import java.util.UUID;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 @Getter
-@RequiredArgsConstructor
+@AllArgsConstructor
 public class VoucherCreateRequest {
 
-    private final UUID id;
-    private final VoucherType type;
-    private final long discount;
-    private final LocalDateTime createAt;
+    private long discount;
+    private VoucherType type;
+    private LocalDateTime createAt;
 }
