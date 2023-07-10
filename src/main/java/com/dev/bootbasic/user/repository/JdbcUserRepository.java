d8709db9f9ae03b080f31af0694f9a57bb2cd929
jun <alap_u@naver.com>
2023-07-10 22:16:06 +0900

    feat: DB에서 유저의 이름을 검색해 유저를 삭제한다.

--- src/main/java/com/dev/bootbasic/user/repository/JdbcUserRepository.java
+++ src/main/java/com/dev/bootbasic/user/repository/JdbcUserRepository.java
@@ -1,17 +1,21 @@
 package com.dev.bootbasic.user.repository;
 
 import com.dev.bootbasic.user.domain.User;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.sql.Timestamp;
 import java.util.Optional;
+import java.util.UUID;
 
 @Repository
 public class JdbcUserRepository implements UserRepository {
 
     private static final String INSERT_USER_COMMAND = "INSERT INTO users (id, name, created_at) VALUES (:id, :name, :createdAt)";
+    private static final String SELECT_USER__BY_NAME_QUERY = "SELECT * FROM users WHERE name = :name";
+    private static final String DELETE_USER_BY_NAME_COMMAND = "DELETE FROM users WHERE name = :name";
     private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 
     public JdbcUserRepository(NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
@@ -30,12 +34,24 @@ public class JdbcUserRepository implements UserRepository {
 
     @Override
     public Optional<User> findByName(String name) {
-        return Optional.empty();
+        MapSqlParameterSource params = new MapSqlParameterSource();
+        params.addValue("name", name);
+        User user = namedParameterJdbcTemplate.queryForObject(SELECT_USER__BY_NAME_QUERY, params, getUserRowMapper());
+        return Optional.ofNullable(user);
     }
 
     @Override
     public void deleteByName(String name) {
+        MapSqlParameterSource params = new MapSqlParameterSource();
+        params.addValue("name", name);
+        namedParameterJdbcTemplate.update(DELETE_USER_BY_NAME_COMMAND, params);
+    }
 
+    private RowMapper<User> getUserRowMapper() {
+        return (rs, rowNum) -> User.of(
+                UUID.fromString(rs.getString("id")),
+                rs.getString("name"),
+                rs.getTimestamp("created_at").toLocalDateTime());
     }
 }
 
