20fac2dfd428ee7ef7e3b30a4de0f9ce737bb4a9
yumyeonghan <mgtmh991013@gmail.com>
2023-07-11 01:25:16 +0900

    feat: profile 수정
    1. !test => cli
    - test 프로파일이 아닐 웹 애플리케이션 실행시, 불필요한 cli 관련 빈들이 등록되는 것을 방지

--- src/main/java/com/programmers/application/CLIVoucherApplicationRunner.java
+++ src/main/java/com/programmers/application/CLIVoucherApplicationRunner.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Component
-@Profile("!test")
+@Profile("cli")
 public class CLIVoucherApplicationRunner implements ApplicationRunner {
     private final FrontController frontController;
 
