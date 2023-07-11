d17482c67b3721dc1b847a215a50f97050338e04
beombu <beombu13@gmail.com>
2023-07-12 05:58:30 +0900

    Test : 저장 성공 테스트 구현

---
+++ src/test/java/org/prgrms/kdtspringdemo/voucher/Service/VoucherServiceTest.java
@@ -0,0 +1,40 @@
+package org.prgrms.kdtspringdemo.voucher.Service;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.prgrms.kdtspringdemo.voucher.model.dto.VoucherResponseDto;
+import org.prgrms.kdtspringdemo.voucher.model.entity.FixedAmountVoucher;
+import org.prgrms.kdtspringdemo.voucher.ropository.VoucherRepository;
+import org.prgrms.kdtspringdemo.voucher.service.VoucherServiceImpl;
+
+import static org.mockito.BDDMockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class VoucherServiceTest {
+
+    @InjectMocks
+    private VoucherServiceImpl voucherService;
+
+    @Mock
+    private VoucherRepository voucherRepository;
+
+    @Test
+    void 바우처_생성_성공_테스트() {
+        //given
+        long amount = 1000;
+        FixedAmountVoucher fixedAmountVoucher = new FixedAmountVoucher(1000);
+
+        //mocking
+        given(voucherRepository.save(any())).willReturn(fixedAmountVoucher);
+
+        //when
+        VoucherResponseDto responseDto = voucherService.create(fixedAmountVoucher.getVoucherType(), fixedAmountVoucher.getAmount());
+
+        //then
+        Assertions.assertThat(responseDto.getVoucherId()).isEqualTo(fixedAmountVoucher.getVoucherId());
+    }
+}
