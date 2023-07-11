5767929720308639345cc2ac96c47303ea7f1403
sangminnim <sangmins930@gmail.com>
2023-07-11 15:01:59 +0900

    feat: 에러메세지 추가

--- src/main/java/com/programmers/springbootbasic/domain/voucher/VoucherDateTime.java
+++ src/main/java/com/programmers/springbootbasic/domain/voucher/VoucherDateTime.java
@@ -4,6 +4,8 @@ import java.time.LocalDateTime;
 
 public class VoucherDateTime {
     private static final String INVALID_EXPIRED_AT = "생성일 이전을 만료일로 설정할 수 없습니다. 현재 값: ";
+    private static final String CREATED_AT_IS_NULL = "빈 값이나 공백을 생성일로 지정할 수 없습니다. 현재 값: ";
+    private static final String EXPIRED_AT_IS_NULL = "빈 값이나 공백을 만료일로 지정할 수 없습니다. 현재 값: ";
     private final LocalDateTime createdAt;
     private final LocalDateTime expiredAt;
 
@@ -34,10 +36,10 @@ public class VoucherDateTime {
 
     private void checkNullValue(LocalDateTime createdAt, LocalDateTime expiredAt) {
         if (isNullCreatedAt(createdAt)) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(CREATED_AT_IS_NULL + createdAt);
         }
         if (isNullExpiredAt(expiredAt)) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(EXPIRED_AT_IS_NULL + expiredAt);
         }
     }
 
