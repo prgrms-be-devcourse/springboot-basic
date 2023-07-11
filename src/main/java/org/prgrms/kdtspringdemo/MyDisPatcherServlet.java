313f152c759ecfa811be65b2beb8832d6867ba25
beombu <beombu13@gmail.com>
2023-07-11 15:23:49 +0900

    Chore : Console Message 복구

--- src/main/java/org/prgrms/kdtspringdemo/MyDisPatcherServlet.java
+++ src/main/java/org/prgrms/kdtspringdemo/MyDisPatcherServlet.java
@@ -11,10 +11,30 @@ import org.springframework.stereotype.Component;
 
 import java.util.List;
 
-import static org.prgrms.kdtspringdemo.view.constant.ConsoleMessage.*;
-
 @Component
 public class MyDisPatcherServlet implements CommandLineRunner {
+    private static final String MAIN_PROGRAM_INIT_MESSAGE = """
+            === Voucher Program ===
+            EXIT -> 프로그램 종료
+            VOUCHER -> 바우처 관련 서비스
+            CUSTOMER -> 소비자 관련 서비스
+            """;
+    private static final String SYSTEM_SHUTDOWN_MESSAGE = "시스템을 종료합니다.\n";
+    private static final String VOUCHER_SERVICE_INIT_MESSAGE = """
+            === Voucher Program ===
+            CREATE -> 바우처 생성
+            LIST -> 특정 바우처 조회
+            LIST_ALL -> 바우처 전체 조회
+            UPDATE -> 특정 바우처 업데이트
+            DELETE -> 특정 바우처 삭제
+            """;
+    private static final String CHOICE_VOUCHER_TYPE_MESSAGE = "바우처 타입을 입력하세요.(ex : FIXED or PERCENT)\n";
+    private static final String AMOUNT_VOUCHER_MESSAGE = "할인 금액을 입력하세요.\n";
+    private static final String SUCCESS_CREATED_VOUCHER = """
+            type : %s
+            discount amount : %s
+            """;
+
     private final VoucherConsole voucherConsole = new VoucherConsole();
     private final VoucherService voucherService;
 
