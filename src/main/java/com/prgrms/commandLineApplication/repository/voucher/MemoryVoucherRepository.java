1d93e485a53d425cc4008a700c10cc630ff21946
박은지 <qkrdmswl1018@naver.com>
2023-07-12 00:56:07 +0900

    refactor: findById 예외 처리 수정

--- src/main/java/com/prgrms/commandLineApplication/repository/voucher/MemoryVoucherRepository.java
+++ src/main/java/com/prgrms/commandLineApplication/repository/voucher/MemoryVoucherRepository.java
@@ -1,23 +1,22 @@
-package com.prgrms.commandLineApplication.repository;
+package com.prgrms.commandLineApplication.repository.voucher;
 
 import com.prgrms.commandLineApplication.voucher.Voucher;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
+import java.util.NoSuchElementException;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 
-@Repository
 public class MemoryVoucherRepository implements VoucherRepository {
 
   private static final String NO_EXIST_VOUCHER = "It doesn't exist";
   private static final Map<UUID, Voucher> voucherStorage = new ConcurrentHashMap<>();
 
   @Override
-  public void save(Voucher voucher) {
+  public Voucher save(Voucher voucher) {
     voucherStorage.put(voucher.getVoucherId(), voucher);
+    return voucher;
   }
 
   @Override
@@ -29,8 +28,11 @@ public class MemoryVoucherRepository implements VoucherRepository {
 
   @Override
   public Voucher findById(UUID id) {
-    return Optional.ofNullable(voucherStorage.get(id))
-            .orElseThrow(() -> new IllegalArgumentException(NO_EXIST_VOUCHER + " -> " + id));
+    try {
+      return voucherStorage.get(id);
+    } catch (Exception e) {
+      throw new NoSuchElementException(NO_EXIST_VOUCHER + " -> " + id);
+    }
   }
 
 }
