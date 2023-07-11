388eca4722122bef470612b9140a4ea57b905608
KimMinheee <99alsgmlalsl@naver.com>
2023-07-12 03:10:49 +0900

    [Refactor] : VoucherFactory 코드 수정

--- src/main/java/org/programmers/VoucherManagement/voucher/domain/VoucherFactory.java
+++ src/main/java/org/programmers/VoucherManagement/voucher/domain/VoucherFactory.java
@@ -1,30 +1,26 @@
 package org.programmers.VoucherManagement.voucher.domain;
 
 import org.programmers.VoucherManagement.voucher.dto.CreateVoucherRequest;
-import org.springframework.stereotype.Component;
 
 import java.util.UUID;
 
-@Component
+
 public class VoucherFactory {
     public static Voucher createVoucher(CreateVoucherRequest createVoucherRequest) {
         DiscountType discountType = createVoucherRequest.getDiscountType();
 
-        Voucher voucher = switch (discountType) {
+        return switch (discountType) {
             case FIXED ->
                     new FixedAmountVoucher(UUID.randomUUID(), discountType, new DiscountValue(createVoucherRequest.getDiscountValue()));
             case PERCENT ->
                     new PercentAmountVoucher(UUID.randomUUID(), discountType, new DiscountValue(createVoucherRequest.getDiscountValue()));
         };
-
-        return voucher;
     }
 
     public static Voucher mapVoucher(UUID voucherId, int discountValue, DiscountType discountType) {
-        Voucher voucher = switch (discountType) {
+        return switch (discountType) {
             case FIXED -> new FixedAmountVoucher(voucherId, discountType, new DiscountValue(discountValue));
             case PERCENT -> new PercentAmountVoucher(voucherId, discountType, new DiscountValue(discountValue));
         };
-        return voucher;
     }
 }
