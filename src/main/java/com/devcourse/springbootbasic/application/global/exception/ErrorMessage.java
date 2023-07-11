71a05a8a36a1899c5d3be4a5afed4d930cc8c6e9
SY97P <onetuks@naver.com>
2023-07-12 00:40:12 +0900

    style: Message 정리

--- src/main/java/com/devcourse/springbootbasic/application/global/exception/ErrorMessage.java
+++ src/main/java/com/devcourse/springbootbasic/application/global/exception/ErrorMessage.java
@@ -3,20 +3,12 @@ package com.devcourse.springbootbasic.application.global.exception;
 public enum ErrorMessage {
 
     INVALID_MENU("올바른 메뉴가 아닙니다."),
-    INVALID_VOUCHER_TYPE("올바른 바우처 메뉴가 아닙니다."),
-    INVALID_LIST_MENU("올바른 목록 메뉴가 아닙니다."),
-
-    INVALID_VOUCHER_INFO("올바른 바우처 정보가 아닙니다."),
-    INVALID_CUSTOMER_INFO("올바른 고객 정보가 아닙니다."),
-
-    INAVLID_VOUCHER_CREATION("바우처 생성이 실패했습니다."),
-    INVALID_CUSTOMER_CREATION("고객 추가에 실패했습니다."),
-
+    INVALID_PROPERTY("해당 정보가 없습니다."),
+    INVALID_CREATION("생성이 실패했습니다."),
+    INVALID_UPDATE("갱신이 실패했습니다."),
     INVALID_DISCOUNT_VALUE("부적절한 바우처 할인값입니다."),
-
     INVALID_FILE_ACCESS("부적절한 파일 접근입니다."),
-    INVALID_SQL_QUERY("부적절한 쿼리 사용입니다."),
-    INVALID_SQL_RESULT("쿼리 결과가 없습니다.");
+    INVALID_SQL("부적절한 SQL 사용입니다.");
 
     private final String messageText;
 
--- src/main/java/com/devcourse/springbootbasic/application/global/io/OutputMessage.java
+++ src/main/java/com/devcourse/springbootbasic/application/global/io/OutputMessage.java
@@ -2,13 +2,16 @@ package com.devcourse.springbootbasic.application.global.io;
 
 public enum OutputMessage {
 
-    START_GAME_PROMPT("=== Voucher Program ==="),
-    END_GAME_PROMPT("프로그램을 종료합니다."),
-    CREATION_DONE_PROMPT("바우처가 생성되었습니다."),
-    VOUCHER_TYPE_PROMPT("--- 바우처 옵션 선택 ---"),
-    LIST_MENU_PROMPT("--- 출력 옵션 선택 ---"),
-    LIST_VOUCHERS_PROMPT("- 생성한 바우처 목록입니다. -"),
-    BLACK_CUSTOMER_PROMPT("진상 목록입니다.");
+    START_GAME("=== Voucher Program ==="),
+    END_GAME("프로그램을 종료합니다."),
+    VOUCHER_CREATION_DONE("바우처가 생성되었습니다."),
+    CUSTOMER_REGISTER_DONE("고객이 등록되었습니다."),
+    DOMAIN_MENU("--- 도메인 옵션 선택 ---"),
+    VOUCHER_TYPE("--- 바우처 옵션 선택 ---"),
+    PROPERTY_MENU("--- 속성 선택 ---"),
+    LIST_VOUCHERS("- 생성한 바우처 목록입니다. -"),
+    LIST_CUSTOMERS("- 고객 목록입니다. -"),
+    LIST_BLACK_CUSTOMERS("- 진상 목록입니다. -");
 
     private final String messageText;
 
