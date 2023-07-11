1781f481d42a2a45e160f756ecc1ec0c4f0b0945
sangminnim <sangmins930@gmail.com>
2023-07-12 01:00:16 +0900

    chore: 에러메시지 수정

--- src/main/java/com/programmers/springbootbasic/domain/voucher/VoucherType.java
+++ src/main/java/com/programmers/springbootbasic/domain/voucher/VoucherType.java
@@ -6,7 +6,7 @@ public enum VoucherType {
     PERCENT("정률 할인"),
     FIX("정액 할인");
 
-    private static final String INVALID_VOUCHER_TYPE = "잘못된 바우처 유형. 현재 입력 값: ";
+    private static final String INVALID_VOUCHER_TYPE = "잘못된 바우처 유형입니다. 현재 입력 값: ";
 
     private final String description;
 
