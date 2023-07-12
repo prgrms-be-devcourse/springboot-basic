bba06db217e916de63c21a90149cdab330bea584
Bombo <bombo96@chungwoon.ac.kr>
2023-07-12 16:58:40 +0900

    feat : 고객 조회 기능을 추가

--- src/main/java/com/prgms/VoucherApp/domain/customer/controller/CustomerController.java
+++ src/main/java/com/prgms/VoucherApp/domain/customer/controller/CustomerController.java
@@ -1,15 +1,30 @@
 package com.prgms.VoucherApp.domain.customer.controller;
 
+import com.prgms.VoucherApp.domain.customer.dto.CustomerResponse;
+import com.prgms.VoucherApp.domain.customer.dto.CustomersResponse;
+import com.prgms.VoucherApp.domain.customer.model.CustomerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
-@RequestMapping("/customer")
+@RequestMapping("/customers")
 public class CustomerController {
 
-    @GetMapping("/main")
-    public String mainPage() {
-        return "customer/main";
+    private final CustomerService customerService;
+
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @GetMapping
+    public String customers(Model model) {
+        CustomersResponse response = customerService.findAll();
+        List<CustomerResponse> customers = response.customers();
+        model.addAttribute("customers", customers);
+        return "customer/customers";
     }
 }
