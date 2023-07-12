5ad3973a41e581cf39a1b8ed737c44d9eeeee6d0
2jie0516 <2jie0516>
2023-07-13 03:38:27 +0900

    jdbc로 관리하는 바우처 클래스 추가

--- src/main/java/prgms/spring_week1/domain/voucher/model/Voucher.java
+++ src/main/java/prgms/spring_week1/domain/voucher/model/Voucher.java
@@ -2,12 +2,22 @@ package prgms.spring_week1.domain.voucher.model;
 
 import prgms.spring_week1.domain.voucher.model.type.VoucherType;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 public class Voucher {
     protected final UUID voucherId = UUID.randomUUID();
     protected final VoucherType voucherType;
     protected final int discount;
+    protected final LocalDateTime createdAt = LocalDateTime.now();
+
+    public VoucherType getVoucherType() {
+        return voucherType;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
 
     public Voucher(VoucherType voucherType, int discount) {
         this.voucherType = voucherType;
--- src/main/java/prgms/spring_week1/domain/voucher/repository/impl/JdbcVoucherRepository.java
+++ src/main/java/prgms/spring_week1/domain/voucher/repository/impl/JdbcVoucherRepository.java
@@ -1,2 +1,69 @@
-package prgms.spring_week1.domain.voucher.repository.impl;public class JdbcVoucherRepository {
+package prgms.spring_week1.domain.voucher.repository.impl;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import prgms.spring_week1.domain.voucher.model.Voucher;
+import prgms.spring_week1.domain.voucher.model.type.VoucherType;
+import prgms.spring_week1.domain.voucher.repository.VoucherRepository;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.UUID;
+
+public class JdbcVoucherRepository implements VoucherRepository {
+    private static final Logger logger = LoggerFactory.getLogger(JdbcVoucherRepository.class);
+
+    private final NamedParameterJdbcTemplate jdbcTemplate;
+
+    private static final RowMapper<Voucher> voucherRowMapper = (resultset, i) -> {
+        var voucherType = VoucherType.valueOf(resultset.getString("voucher_type"));
+        var discount = resultset.getInt("discount");
+        return new Voucher(voucherType,discount);
+    };
+
+    public JdbcVoucherRepository(NamedParameterJdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    private Map<String, Object> toParamMap(Voucher voucher) {
+        return new HashMap<>() {{
+            put("voucherId", voucher.getVoucherId().toString().getBytes());
+            put("voucherType", String.valueOf(voucher.getVoucherType()));
+            put("discount", voucher.getDiscount());
+            put("createdAt",voucher.getCreatedAt());
+        }};
+    }
+    @Override
+    public void insert(Voucher voucher) {
+        var update = jdbcTemplate.update("INSERT INTO voucher(voucher_type, discount, created_at) VALUES (:voucherType, :discount ,:createdAt)",
+                toParamMap(voucher));
+        if (update != 1) {
+            throw new RuntimeException("Noting was inserted");
+        }
+    }
+    @Override
+    public List<Voucher> findAll() {
+        return jdbcTemplate.query("select * from voucher", voucherRowMapper);
+    }
+
+    public Optional<Voucher> findById(UUID voucherId) {
+        try {
+            return Optional.ofNullable(jdbcTemplate.queryForObject("select * from voucher WHERE voucher_id = UUID_TO_BIN(:voucher_id)",
+                    Collections.singletonMap("voucherId", voucherId.toString().getBytes()),
+                    voucherRowMapper));
+        } catch (EmptyResultDataAccessException e) {
+            logger.error("Got empty result", e);
+            return Optional.empty();
+        }
+    }
+
+    public void delete(String voucherType) {
+        jdbcTemplate.update("DELETE FROM voucher WHERE voucher_type = :voucherType ", Collections.singletonMap("voucherType",voucherType));
+    }
 }
