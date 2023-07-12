1ba78c19d9c5222551fa397df1ad388d9fb9e044
hyee0715 <hyee0715@naver.com>
2023-07-13 00:24:05 +0900

    feat: 바우처 생성 페이지 구현

--- src/main/java/com/programmers/springbootbasic/voucher/controller/VoucherViewController.java
+++ src/main/java/com/programmers/springbootbasic/voucher/controller/VoucherViewController.java
@@ -1,10 +1,13 @@
 package com.programmers.springbootbasic.voucher.controller;
 
+import com.programmers.springbootbasic.voucher.dto.VoucherCreateRequestDto;
 import com.programmers.springbootbasic.voucher.dto.VouchersResponseDto;
 import com.programmers.springbootbasic.voucher.service.VoucherService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("voucher")
@@ -23,5 +26,17 @@ public class VoucherViewController {
         model.addAttribute("vouchers", vouchersResponseDto);
 
         return "voucher/voucher-list";
+    }
+
+    @GetMapping("/create")
+    public String create() {
+        return "voucher/voucher-form";
+    }
+
+    @PostMapping("/create")
+    public String save(@ModelAttribute VoucherCreateRequestDto voucherCreateRequestDto) {
+        voucherService.save(voucherCreateRequestDto);
+
+        return "redirect:/voucher/list";
     }
 }
