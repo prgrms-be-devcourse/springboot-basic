80d6da675d25cd53950e322c81ee738c9aa9301b
jay-so <merry9504@naver.com>
2023-07-12 15:30:31 +0900

    rename:FixedDiscountVoucher -> FixedVoucher로 클래스명 변경

--- src/main/java/com/prgrms/springbootbasic/domain/voucher/FixedVoucher.java
+++ src/main/java/com/prgrms/springbootbasic/domain/voucher/FixedVoucher.java
@@ -3,29 +3,33 @@ package com.prgrms.springbootbasic.domain.voucher;
 import com.prgrms.springbootbasic.enums.VoucherType;
 import java.time.LocalDateTime;
 import java.util.UUID;
-import lombok.Getter;
 
-@Getter
-public class FixedDiscountVoucher implements Voucher {
+public class FixedVoucher implements Voucher {
 
     private final UUID voucherId;
     private long discount;
     private final LocalDateTime createAt;
     private final VoucherType type;
 
-    public FixedDiscountVoucher(long discount) {
+    public FixedVoucher(long discount) {
         this.voucherId = UUID.randomUUID();
         this.discount = ValidDiscount(discount);
-        this.createAt = getcreatedAt();
+        this.createAt = getCreatedAt();
         this.type = getVoucherType();
     }
 
 
     public long ValidDiscount(long discount) {
+
         if (discount <= 0) {
             throw new IllegalArgumentException("고정 할인 바우처의 입력 금액은 0 이하를 입력할 수 없습니다.");
         }
         return discount;
+    }
+
+    @Override
+    public UUID getVoucherId() {
+        return voucherId;
     }
 
     @Override
@@ -34,7 +38,7 @@ public class FixedDiscountVoucher implements Voucher {
     }
 
     @Override
-    public LocalDateTime getcreatedAt() {
+    public LocalDateTime getCreatedAt() {
         return createAt;
     }
 }
