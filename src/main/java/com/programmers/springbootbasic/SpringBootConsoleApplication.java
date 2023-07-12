2b5972922b4eb1e5515a633216b23e4baae004fd
sangminnim <sangmins930@gmail.com>
2023-07-12 14:59:57 +0900

    feat: SpringBootApplication (runner 구현), application.properties 설정

---
+++ src/main/java/com/programmers/springbootbasic/SpringBootConsoleApplication.java
@@ -0,0 +1,62 @@
+package com.programmers.springbootbasic;
+
+import com.programmers.springbootbasic.presentation.Command;
+import com.programmers.springbootbasic.presentation.controller.VoucherController;
+import com.programmers.springbootbasic.presentation.view.ConsoleApplicationView;
+import com.programmers.springbootbasic.service.dto.VoucherCreationRequest;
+import com.programmers.springbootbasic.service.dto.VoucherResponse;
+import com.programmers.springbootbasic.service.dto.VoucherResponses;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
+
+@SpringBootApplication
+public class SpringBootConsoleApplication implements CommandLineRunner {
+    private final VoucherController voucherController;
+    private final ConsoleApplicationView applicationView;
+
+    public SpringBootConsoleApplication(VoucherController voucherController, ConsoleApplicationView applicationView) {
+        this.voucherController = voucherController;
+        this.applicationView = applicationView;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootConsoleApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        while (true) {
+            try {
+                Command command = applicationView.selectCommand();
+                switch (command) {
+                    case CREATE -> create();
+                    case LIST -> list();
+                    case EXIT -> {
+                        applicationView.exit();
+                        return;
+                    }
+                }
+            } catch (IllegalArgumentException | IllegalStateException e) {
+                applicationView.printErrorMessage(e.getMessage());
+            }
+        }
+    }
+
+    private void create() throws IOException {
+        VoucherCreationRequest request = applicationView.createVoucherCreationRequestFromInput();
+        VoucherResponse response = voucherController.createVoucher(request);
+        applicationView.printCreatedVoucher(response);
+    }
+
+    private void list() throws IOException {
+        VoucherResponses voucherResponses = voucherController.listVouchers();
+        applicationView.listVouchers(voucherResponses);
+    }
+
+    private void exit() throws IOException {
+        applicationView.exit();
+    }
+}
