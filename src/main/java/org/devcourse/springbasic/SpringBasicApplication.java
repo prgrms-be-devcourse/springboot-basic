e59fd996ff385f072ff25a83799844950d2ec84a
seongju <lsj40413@gmail.com>
2023-07-12 15:44:39 +0900

    feat(app): customer 도메인 추가에 따른 app 전체 동작기능 수정

--- src/main/java/org/devcourse/springbasic/SpringBasicApplication.java
+++ src/main/java/org/devcourse/springbasic/SpringBasicApplication.java
@@ -1,16 +1,15 @@
 package org.devcourse.springbasic;
 
-import org.devcourse.springbasic.controller.VoucherApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SpringBasicApplication {
-	public static void main(String[] args) {
+    public static void main(String[] args) {
+
 		ConfigurableApplicationContext ac = SpringApplication.run(SpringBasicApplication.class, args);
-		VoucherApplication voucherApp = ac.getBean(VoucherApplication.class);
-		String deviceType = "ConsoleDevice";
-		voucherApp.run(deviceType);
-	}
+		VoucherAppController voucherApp = ac.getBean(VoucherAppController.class);
+		voucherApp.run();
+    }
 }
---
+++ src/main/java/org/devcourse/springbasic/VoucherAppController.java
@@ -0,0 +1,60 @@
+package org.devcourse.springbasic;
+
+import lombok.RequiredArgsConstructor;
+import org.devcourse.springbasic.domain.DomainType;
+import org.devcourse.springbasic.domain.customer.presentation.CustomerController;
+import org.devcourse.springbasic.domain.voucher.presentation.VoucherController;
+import org.devcourse.springbasic.global.io.ErrorMsgPrinter;
+import org.devcourse.springbasic.global.io.input.AppInput;
+import org.devcourse.springbasic.global.io.input.AppInputType;
+import org.devcourse.springbasic.global.io.input.customer.CustomerInput;
+import org.devcourse.springbasic.global.io.input.customer.CustomerInputType;
+import org.devcourse.springbasic.global.io.input.voucher.VoucherInput;
+import org.devcourse.springbasic.global.io.input.voucher.VoucherInputType;
+import org.devcourse.springbasic.global.io.output.AppOutput;
+import org.devcourse.springbasic.global.io.output.AppOutputType;
+import org.devcourse.springbasic.global.io.output.customer.CustomerOutput;
+import org.devcourse.springbasic.global.io.output.customer.CustomerOutputType;
+import org.devcourse.springbasic.global.io.output.voucher.VoucherOutput;
+import org.devcourse.springbasic.global.io.output.voucher.VoucherOutputType;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequiredArgsConstructor
+public class VoucherAppController {
+
+    private final CustomerController customerController;
+    private final VoucherController voucherController;
+
+    public void run() {
+
+        String environment = getEnvironment();
+        AppInput input = AppInputType.valueOf(environment).getInput();
+        AppOutput output = AppOutputType.valueOf(environment).getOutput();
+        CustomerInput customerInput = CustomerInputType.valueOf(environment).getInput();
+        CustomerOutput customerOutput = CustomerOutputType.valueOf(environment).getOutput();
+        VoucherInput voucherInput = VoucherInputType.valueOf(environment).getInput();
+        VoucherOutput voucherOutput = VoucherOutputType.valueOf(environment).getOutput();
+
+        try {
+            output.menus();
+            DomainType selectedMenu = input.menu();
+
+            switch (selectedMenu) {
+                case CUSTOMER:
+                    customerController.run(customerInput, customerOutput);
+                    break;
+
+                case VOUCHER:
+                    voucherController.run(voucherInput, voucherOutput);
+                    break;
+            }
+        } catch (IllegalArgumentException e) {
+            ErrorMsgPrinter.print(e.getMessage());
+        }
+    }
+
+    private String getEnvironment() {
+        return "CONSOLE";
+    }
+}
