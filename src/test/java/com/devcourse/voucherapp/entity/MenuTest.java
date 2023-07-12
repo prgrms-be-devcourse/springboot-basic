e675be26bf96eae8c5319d3d34d9aae9ce32c5eb
kylekim2123 <kyle2123@naver.com>
2023-07-12 15:18:17 +0900

    test: ParameterizedTest를 이용한 테스트 코드 간소화

--- src/test/java/com/devcourse/voucherapp/entity/MenuTest.java
+++ src/test/java/com/devcourse/voucherapp/entity/MenuTest.java
@@ -5,30 +5,23 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import com.devcourse.voucherapp.exception.MenuInputException;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class MenuTest {
 
     @DisplayName("존재하는 메뉴 번호 입력 시, 해당 메뉴 객체가 반환된다.")
-    @Test
-    void selectExistedMenuTest() {
-        Menu createMenu = Menu.from("1");
-        Menu listMenu = Menu.from("2");
-        Menu updateMenu = Menu.from("3");
-        Menu deleteMenu = Menu.from("4");
-        Menu quitMenu = Menu.from("5");
-
-        assertEquals(Menu.CREATE, createMenu);
-        assertEquals(Menu.READ, listMenu);
-        assertEquals(Menu.UPDATE, updateMenu);
-        assertEquals(Menu.DELETE, deleteMenu);
-        assertEquals(Menu.QUIT, quitMenu);
+    @ParameterizedTest
+    @CsvSource(value = {"1,CREATE", "2,READ", "3,UPDATE", "4,DELETE", "5,QUIT"})
+    void selectExistedMenuTest(String menuNumber, Menu menu) {
+        assertEquals(menu, Menu.from(menuNumber));
     }
 
     @DisplayName("존재하지 않는 메뉴 번호 입력 시, MenuInputException 예외가 발생한다.")
-    @Test
-    void selectNotExistedMenuTest() {
-        assertThrows(MenuInputException.class, () -> Menu.from("10000"));
-        assertThrows(MenuInputException.class, () -> Menu.from("string"));
+    @ParameterizedTest
+    @ValueSource(strings = {"10000", "string"})
+    void selectNotExistedMenuTest(String invalidMenuNumber) {
+        assertThrows(MenuInputException.class, () -> Menu.from(invalidMenuNumber));
     }
 }
