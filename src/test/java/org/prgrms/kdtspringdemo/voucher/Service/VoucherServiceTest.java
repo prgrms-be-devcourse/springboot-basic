018d6c76c30a282f92e5da335847a9f2cf8219f6
beombu <beombu13@gmail.com>
2023-07-12 23:06:04 +0900

    Test : 바우처 수정 성공 테스트 구현

--- src/test/java/org/prgrms/kdtspringdemo/voucher/Service/VoucherServiceTest.java
+++ src/test/java/org/prgrms/kdtspringdemo/voucher/Service/VoucherServiceTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.prgrms.kdtspringdemo.voucher.constant.VoucherType;
 import org.prgrms.kdtspringdemo.voucher.model.dto.VoucherResponseDto;
 import org.prgrms.kdtspringdemo.voucher.model.entity.FixedAmountVoucher;
 import org.prgrms.kdtspringdemo.voucher.model.entity.PercentAmountVoucher;
@@ -29,8 +30,7 @@ public class VoucherServiceTest {
     @Test
     void 바우처_생성_성공_테스트() {
         //given
-        long amount = 1000;
-        FixedAmountVoucher fixedAmountVoucher = new FixedAmountVoucher(amount);
+        FixedAmountVoucher fixedAmountVoucher = new FixedAmountVoucher(1000);
 
         //mocking
         given(voucherRepository.save(any())).willReturn(fixedAmountVoucher);
@@ -45,8 +45,7 @@ public class VoucherServiceTest {
     @Test
     void 바우처_조회_성공_테스트() {
         //given
-        long amount = 1000;
-        FixedAmountVoucher fixedAmountVoucher = new FixedAmountVoucher(amount);
+        FixedAmountVoucher fixedAmountVoucher = new FixedAmountVoucher(1000);
 
         //mocking
         given(voucherRepository.findById(any())).willReturn(fixedAmountVoucher);
@@ -72,5 +71,29 @@ public class VoucherServiceTest {
 
         //then
         Assertions.assertThat(responseDtos).hasSize(2);
+    }
+
+    @Test
+    void 바우처_수정_성공_테스트() {
+        //given
+        Voucher voucher1 = new FixedAmountVoucher(1000);
+        given(voucherRepository.save(any())).willReturn(voucher1);
+        VoucherResponseDto voucher2 = voucherService.create(voucher1.getVoucherType(), voucher1.getAmount());
+
+        VoucherType percent = VoucherType.PERCENT;
+        long amount = 10;
+        PercentAmountVoucher voucher3 = new PercentAmountVoucher(voucher2.getVoucherId(), amount);
+
+        //mocking
+        given(voucherRepository.update(any())).willReturn(voucher3);
+
+        //when
+        VoucherResponseDto responseDto = voucherService.update(voucher2.getVoucherId(), percent, amount);
+
+        //then
+        Assertions.assertThat(responseDto.getVoucherId()).isEqualTo(voucher1.getVoucherId());
+        Assertions.assertThat(responseDto.getVoucherType()).isEqualTo(percent);
+        Assertions.assertThat(responseDto.getAmount()).isEqualTo(amount);
+
     }
 }
