47767650980080d663a3122b4d7e366083d8f641
jay-so <merry9504@naver.com>
2023-07-12 17:55:14 +0900

    refactor:바우처 Controller refactoring

--- src/main/java/com/prgrms/springbootbasic/controller/voucher/VoucherController.java
+++ src/main/java/com/prgrms/springbootbasic/controller/voucher/VoucherController.java
@@ -1,13 +1,12 @@
 package com.prgrms.springbootbasic.controller.voucher;
 
-import com.prgrms.springbootbasic.domain.voucher.Voucher;
 import com.prgrms.springbootbasic.dto.voucher.request.VoucherCreateRequest;
 import com.prgrms.springbootbasic.dto.voucher.request.VoucherUpdateRequest;
+import com.prgrms.springbootbasic.dto.voucher.response.VoucherListResponse;
 import com.prgrms.springbootbasic.dto.voucher.response.VoucherResponse;
 import com.prgrms.springbootbasic.enums.VoucherType;
 import com.prgrms.springbootbasic.service.voucher.VoucherService;
 import java.time.LocalDateTime;
-import java.util.Map;
 import java.util.UUID;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -25,18 +24,13 @@ public class VoucherController {
 
 
     //(Read) 바우처의 모든 리스트 출력
-    public Map<UUID, Voucher> findAllList() {
-        return voucherService.fetchAllVouchers();
+    public VoucherListResponse findAllList() {
+        return voucherService.findAllVouchers();
     }
 
     //(Read) 특정 바우처 id의 내용을 출력
-    public VoucherResponse findByid(UUID voucherId) {
+    public VoucherResponse findById(UUID voucherId) {
         return voucherService.findById(voucherId);
-    }
-
-    //(Read) 바우처 타입  내용을 출력
-    public VoucherResponse findByType(VoucherType voucherType) {
-        return voucherService.finByType(VoucherType);
     }
 
     //(Read) 바우처 생성기간 출력
@@ -44,14 +38,19 @@ public class VoucherController {
         return voucherService.findByCreateAt(createAt);
     }
 
+    //(Read) 바우처 타입별 리스트 출력
+    public VoucherListResponse findByType(VoucherType type) {
+        return voucherService.findByType(type);
+    }
+
     //(Update) 생성된 바우처 수정
     public void update(VoucherUpdateRequest voucherUpdateRequest) {
-        voucherService.update(voucherUpdateRequest);
+        voucherService.updateVoucher(voucherUpdateRequest);
     }
 
     //(Delete) 특정 바우처를 찾아 삭제
-    public void deleteByid() {
-        voucherService.deleteById();
+    public void deleteById(UUID voucherId) {
+        voucherService.deleteById(voucherId);
     }
 
     //(Delete) 모든 바우처의 내용 삭제
