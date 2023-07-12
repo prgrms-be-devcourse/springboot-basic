ae47fcc16183bd97970a19c83c6d6f8048326ad9
Choi Seongwon <vlflalektzja@naver.com>
2023-07-12 15:01:10 +0900

    feat: VoucherJdbcRepository 작성, autowire 대상 변경 명시

--- src/main/java/com/example/springbootbasic/voucher/VoucherService.java
+++ src/main/java/com/example/springbootbasic/voucher/VoucherService.java
@@ -1,5 +1,6 @@
 package com.example.springbootbasic.voucher;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -8,7 +9,7 @@ import java.util.List;
 public class VoucherService {
     private final VoucherRepository voucherRepository;
 
-    public VoucherService(VoucherRepository voucherRepository) {
+    public VoucherService(@Qualifier("voucherJdbcRepository") VoucherRepository voucherRepository) {
         this.voucherRepository = voucherRepository;
     }
 
