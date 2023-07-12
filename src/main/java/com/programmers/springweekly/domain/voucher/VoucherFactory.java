410e4c252ed6002f1896cdcf5eb9acc63e97fc7f
changhyeon <changhyeon.h@kakao.com>
2023-07-12 18:26:58 +0900

    refactor: voucherFactory에서 switch문법 개선

--- src/main/java/com/programmers/springweekly/domain/voucher/VoucherFactory.java
+++ src/main/java/com/programmers/springweekly/domain/voucher/VoucherFactory.java
@@ -6,15 +6,9 @@ public class VoucherFactory {
 
     public static Voucher createVoucher(UUID voucherId, VoucherType voucherType, long discount) {
 
-        switch (voucherType) {
-            case FIXED -> {
-                return new FixedAmountVoucher(voucherId, discount);
-            }
-            case PERCENT -> {
-                return new PercentDiscountVoucher(voucherId, discount);
-            }
-            default -> throw new IllegalArgumentException("Input: " + voucherType + "바우처 타입이 없습니다.");
-        }
+        return switch (voucherType) {
+            case FIXED -> new FixedAmountVoucher(voucherId, discount);
+            case PERCENT -> new PercentDiscountVoucher(voucherId, discount);
+        };
     }
-    
 }
