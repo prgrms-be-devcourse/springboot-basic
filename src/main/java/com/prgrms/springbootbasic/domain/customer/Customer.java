32f5bc9b027e47f4a395a8cda66c9893b316bc9b
jay-so <merry9504@naver.com>
2023-07-12 14:41:42 +0900

    refactor: Custeor domain 리팩토링

--- src/main/java/com/prgrms/springbootbasic/domain/customer/Customer.java
+++ src/main/java/com/prgrms/springbootbasic/domain/customer/Customer.java
@@ -1,14 +1,21 @@
 package com.prgrms.springbootbasic.domain.customer;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 @Getter
-@RequiredArgsConstructor
 public class Customer {
 
     private final UUID customerId;
     private final String name;
     private final String email;
+    private LocalDateTime createAt;
+
+    public Customer(UUID customerId, String name, String email, LocalDateTime createAt) {
+        this.customerId = customerId;
+        this.name = name;
+        this.email = email;
+        this.createAt = createAt;
+    }
 }
