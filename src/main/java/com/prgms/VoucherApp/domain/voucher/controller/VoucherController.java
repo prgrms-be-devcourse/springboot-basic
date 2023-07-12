2e8d175849a7768bfaef53eeb784edf8e1330414
Bombo <bombo96@chungwoon.ac.kr>
2023-07-12 16:58:51 +0900

    feat : 할인권 조회 기능을 추가

--- src/main/java/com/prgms/VoucherApp/domain/voucher/controller/VoucherController.java
+++ src/main/java/com/prgms/VoucherApp/domain/voucher/controller/VoucherController.java
@@ -1,15 +1,30 @@
 package com.prgms.VoucherApp.domain.voucher.controller;
 
+import com.prgms.VoucherApp.domain.voucher.dto.VoucherResponse;
+import com.prgms.VoucherApp.domain.voucher.dto.VouchersResponse;
+import com.prgms.VoucherApp.domain.voucher.model.VoucherService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
-@RequestMapping("/voucher")
+@RequestMapping("/vouchers")
 public class VoucherController {
 
-    @GetMapping("/main")
-    public String mainPage() {
-        return "voucher/main";
+    private final VoucherService voucherService;
+
+    public VoucherController(VoucherService voucherService) {
+        this.voucherService = voucherService;
+    }
+
+    @GetMapping
+    public String vouchers(Model model) {
+        VouchersResponse vouchersResponse = voucherService.findAll();
+        List<VoucherResponse> vouchers = vouchersResponse.getVouchers();
+        model.addAttribute("vouchers", vouchers);
+        return "voucher/vouchers";
     }
 }
