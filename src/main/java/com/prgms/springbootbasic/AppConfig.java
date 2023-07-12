839d025a751e23408a8810c46453a8ee925411c8
heenahan <mby1997s@gmail.com>
2023-07-12 16:53:32 +0900

    chore : web관련 설정 추가

--- src/main/java/com/prgms/springbootbasic/AppConfig.java
+++ src/main/java/com/prgms/springbootbasic/AppConfig.java
@@ -2,22 +2,53 @@ package com.prgms.springbootbasic;
 
 import com.prgms.springbootbasic.util.YamlPropertyFactory;
 import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 
 import javax.sql.DataSource;
 
 @Configuration
 @ComponentScan
-@PropertySource(value = "application.yaml", factory = YamlPropertyFactory.class)
-public class AppConfig {
+@EnableTransactionManagement
+@EnableWebMvc
+@PropertySource(value = "classpath:application.yaml", factory = YamlPropertyFactory.class)
+public class AppConfig implements WebMvcConfigurer, ApplicationContextAware {
+
+    private ApplicationContext applicationContext;
+
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        var springResourceTemplateResolver = new SpringResourceTemplateResolver();
+        springResourceTemplateResolver.setApplicationContext(applicationContext);
+        springResourceTemplateResolver.setPrefix("/WEB-INF/");
+        springResourceTemplateResolver.setSuffix(".html");
+
+        var springTemplateEngine = new SpringTemplateEngine();
+        springTemplateEngine.setTemplateResolver(springResourceTemplateResolver);
+
+        var thymeleafViewResolver = new ThymeleafViewResolver();
+        thymeleafViewResolver.setTemplateEngine(springTemplateEngine);
+        thymeleafViewResolver.setViewNames(new String[]{"views/*"});
+        registry.viewResolver(thymeleafViewResolver);
+    }
 
     @Bean
     public DataSource dataSource(@Value("${database.url}") String url,
@@ -41,6 +72,16 @@ public class AppConfig {
     @Bean
     public NamedParameterJdbcTemplate namedParameterJdbcTemplate(JdbcTemplate jdbcTemplate) {
         return new NamedParameterJdbcTemplate(jdbcTemplate);
+    }
+
+    @Bean
+    public PlatformTransactionManager platformTransactionManager(DataSource dataSource) {
+        return new DataSourceTransactionManager(dataSource);
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
     }
 
 }
