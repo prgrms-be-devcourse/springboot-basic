db6bae138da851655d929f5510295898d6009cb9
jay-so <merry9504@naver.com>
2023-07-13 22:10:04 +0900

    refactor: CustomerRepository interface refactoring(find - Id,crateAt, all, update Method add)

--- src/main/java/com/prgrms/springbootbasic/repository/customer/CustomerRepository.java
+++ src/main/java/com/prgrms/springbootbasic/repository/customer/CustomerRepository.java
@@ -1,18 +1,31 @@
 package com.prgrms.springbootbasic.repository.customer;
 
 import com.prgrms.springbootbasic.domain.customer.Customer;
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 public interface CustomerRepository {
 
     //생성(create)
-    Customer insert(Customer customer);
+    Customer save(Customer customer);
+
+    //read-id
+    Optional<Customer> findById(UUID customerId);
+
+    //read-create at
+    Optional<Customer> findByCreateAt(LocalDateTime createAt);
+
+    //조회(Read) - all
+    List<Customer> findAll();
 
     //업데이트(update)
-    void update(Customer customer);
+    Optional<Customer> update(Customer customer);
 
-    //조회(Read) - 리스트 출력
-    List<Customer> findAll();
+    //delete-id
+    Optional<Customer> deleteById(UUID customerId);
+
 
     //customer 모두 삭제(Delete)
     void deleteAll();
