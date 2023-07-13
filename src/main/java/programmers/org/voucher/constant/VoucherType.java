8303b5524bbc13db8e117b1b0e911fd62c6d216c
kys0411 <32180846@dankook.ac.kr>
2023-07-14 03:21:41 +0900

    voucher의 discountAmount 값을 검증하는 로직 추가

--- src/main/java/programmers/org/voucher/constant/VoucherType.java
+++ src/main/java/programmers/org/voucher/constant/VoucherType.java
@@ -2,17 +2,28 @@ package programmers.org.voucher.constant;
 
 import java.util.Arrays;
 import java.util.NoSuchElementException;
+import java.util.function.Predicate;
 
 import static programmers.org.voucher.exception.ErrorMessage.VOUCHER_ERROR_MESSAGE;
 
 public enum VoucherType {
-    FIXED,
-    PERCENT;
+    FIXED((amount) -> amount > 0),
+    PERCENT((amount) -> amount > 0 && amount <= 100);
+
+    private final Predicate<Integer> validation;
+
+    VoucherType(Predicate<Integer> validation) {
+        this.validation = validation;
+    }
 
     public static VoucherType find(String type) {
         return Arrays.stream(VoucherType.values())
                 .filter(voucherType -> voucherType.name().equals(type.toUpperCase()))
                 .findAny()
                 .orElseThrow(() -> new NoSuchElementException(VOUCHER_ERROR_MESSAGE.getMessage()));
+    }
+
+    public boolean isInvalidAmount(int amount) {
+        return !validation.test(amount);
     }
 }
--- src/main/java/programmers/org/voucher/domain/Voucher.java
+++ src/main/java/programmers/org/voucher/domain/Voucher.java
@@ -2,6 +2,8 @@ package programmers.org.voucher.domain;
 
 import programmers.org.voucher.constant.VoucherType;
 
+import static programmers.org.voucher.exception.ErrorMessage.DISCOUNT_ERROR_MESSAGE;
+
 public class Voucher {
     private Long id;
 
@@ -10,11 +12,13 @@ public class Voucher {
     private final VoucherType type;
 
     public Voucher(int discountAmount, VoucherType type) {
+        validateVoucher(discountAmount, type);
         this.discountAmount = discountAmount;
         this.type = type;
     }
 
     public Voucher(Long id, int discountAmount, VoucherType type) {
+        validateVoucher(discountAmount, type);
         this.id = id;
         this.discountAmount = discountAmount;
         this.type = type;
@@ -30,5 +34,11 @@ public class Voucher {
 
     public VoucherType getType() {
         return type;
+    }
+
+    private void validateVoucher(int discountAmount, VoucherType type) {
+        if (type.isInvalidAmount(discountAmount)) {
+            throw new IllegalArgumentException(DISCOUNT_ERROR_MESSAGE.getMessage());
+        }
     }
 }
