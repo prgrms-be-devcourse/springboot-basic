347eb4e9065a041d624ad55b6483e53f3ae5b1a2
PHJ <legowww@gmail.com>
2023-07-13 12:24:41 +0900

    feat: Web 설정 추가

--- src/main/java/com/dev/voucherproject/controller/web/config/WebConfig.java
+++ src/main/java/com/dev/voucherproject/controller/web/config/WebConfig.java
@@ -1,31 +1,15 @@
 package com.dev.voucherproject.controller.web.config;
 
 
-import com.dev.voucherproject.controller.web.converter.VoucherPolicyConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import org.springframework.format.FormatterRegistry;
 import org.springframework.web.filter.HiddenHttpMethodFilter;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
-    @Override
-    public void addFormatters(FormatterRegistry registry) {
-        registry.addConverter(new VoucherPolicyConverter());
-    }
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry
-            .addMapping("/**")
-            .allowedOrigins("http://localhost:3000");
-    }
-
     @Bean
     public HiddenHttpMethodFilter hiddenHttpMethodFilter(){
         return new HiddenHttpMethodFilter();
