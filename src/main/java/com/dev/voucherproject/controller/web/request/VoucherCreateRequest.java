e74e0591e8d091742a299500c1609a890eb95d88
PHJ <legowww@gmail.com>
2023-07-13 12:24:57 +0900

    feat: 요청 및 응답 객체 구현

---
+++ src/main/java/com/dev/voucherproject/controller/web/request/VoucherCreateRequest.java
@@ -0,0 +1,4 @@
+package com.dev.voucherproject.controller.web.request;
+import com.dev.voucherproject.model.voucher.VoucherPolicy;
+public record VoucherCreateRequest(VoucherPolicy voucherPolicy, long discountFigure) {
+}
