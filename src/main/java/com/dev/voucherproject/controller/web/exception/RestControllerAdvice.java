c8d091ecbdec3c7fbea9206d82178cb82adea67c
PHJ <legowww@gmail.com>
2023-07-13 12:25:18 +0900

    feat: 예외처리를 위해 @ExceptionHandler 사용

---
+++ src/main/java/com/dev/voucherproject/controller/web/exception/RestControllerAdvice.java
@@ -0,0 +1,21 @@
+package com.dev.voucherproject.controller.web.exception;
+
+
+import com.dev.voucherproject.controller.web.response.Response;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@org.springframework.web.bind.annotation.RestControllerAdvice
+public class RestControllerAdvice {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<Response<String>> runtimeExceptionHandler(RuntimeException e) {
+        return ResponseEntity.badRequest().body(Response.error(e.getMessage()));
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<Response<String>> illegalArgumentExceptionHandler(IllegalArgumentException e) {
+        return ResponseEntity.badRequest().body(Response.error(e.getMessage()));
+    }
+}
