080540eed5d49e1bcfb46f8b32158b370835a065
weonest <geonhee33@naver.com>
2023-07-13 13:06:10 +0900

    feat: 바우처 삭제 및 타입으로 조회 기능 추가

--- src/test/java/com/programmers/voucher/repository/JdbcVoucherRepositoryTest.java
+++ src/test/java/com/programmers/voucher/repository/JdbcVoucherRepositoryTest.java
@@ -1,9 +1,12 @@
 package com.programmers.voucher.repository;
 
 import com.programmers.voucher.domain.FixedDiscount;
+import com.programmers.voucher.domain.PercentDiscount;
 import com.programmers.voucher.domain.Voucher;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
@@ -69,5 +72,37 @@ class JdbcVoucherRepositoryTest {
         List<Voucher> vouchers = jdbcVoucherRepository.findAll();
 
         assertThat(vouchers.size()).isEqualTo(3);
+    }
+
+    @DisplayName("할인 타입으로 바우처를 조회할 수 있다.")
+    @ParameterizedTest
+    @CsvSource(value = {"fixed", "FIXED", "PERCENT", "percent"})
+    void findVouchersByTypeTest(String type) {
+        for (int i = 0; i < 4; i++) {
+            if (i % 2 == 0) {
+                Voucher testVoucher = new Voucher(UUID.randomUUID(), new FixedDiscount(100), LocalDateTime.now());
+                jdbcVoucherRepository.save(testVoucher);
+                continue;
+            }
+            Voucher testVoucher = new Voucher(UUID.randomUUID(), new PercentDiscount(100), LocalDateTime.now());
+            jdbcVoucherRepository.save(testVoucher);
+        }
+
+        List<Voucher> vouchers = jdbcVoucherRepository.findByType(type);
+
+        assertThat(vouchers.size()).isEqualTo(2);
+    }
+
+    @DisplayName("ID로 바우처를 삭제할 수 있다.")
+    @Test
+    void deleteVoucherByIdTest() {
+        UUID id = UUID.randomUUID();
+        Voucher testVoucher = new Voucher(id, new FixedDiscount(100), LocalDateTime.now());
+        jdbcVoucherRepository.save(testVoucher);
+
+        jdbcVoucherRepository.deleteById(id);
+
+        List<Voucher> vouchers = jdbcVoucherRepository.findAll();
+        assertThat(vouchers.size()).isEqualTo(0);
     }
 }
