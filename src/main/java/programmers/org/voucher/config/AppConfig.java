399681898d96f8b044bdec42ffa9fa609b1508c7
kys0411 <32180846@dankook.ac.kr>
2023-07-14 02:26:15 +0900

    voucher repository타입 빈을 jdbc repository로 변경

--- src/main/java/programmers/org/voucher/config/AppConfig.java
+++ src/main/java/programmers/org/voucher/config/AppConfig.java
@@ -2,14 +2,16 @@ package programmers.org.voucher.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import programmers.org.voucher.repository.MemoryVoucherRepository;
+import programmers.org.voucher.repository.JdbcVoucherRepository;
 import programmers.org.voucher.repository.VoucherRepository;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class AppConfig {
 
     @Bean
-    public VoucherRepository voucherRepository() {
-        return new MemoryVoucherRepository();
+    public VoucherRepository voucherRepository(DataSource dataSource) {
+        return new JdbcVoucherRepository(dataSource);
     }
 }
