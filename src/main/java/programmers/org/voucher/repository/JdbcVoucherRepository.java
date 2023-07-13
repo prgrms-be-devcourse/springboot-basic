e3f02a64c8d61f41a58ac08a60baafc7aba7be4d
kys0411 <32180846@dankook.ac.kr>
2023-07-14 02:24:13 +0900

    jdbc voucher repository 구현

---
+++ src/main/java/programmers/org/voucher/repository/JdbcVoucherRepository.java
@@ -0,0 +1,79 @@
+package programmers.org.voucher.repository;
+
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import programmers.org.voucher.constant.VoucherType;
+import programmers.org.voucher.domain.Voucher;
+import programmers.org.voucher.dto.VoucherRequest;
+
+import javax.sql.DataSource;
+import java.sql.PreparedStatement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class JdbcVoucherRepository implements VoucherRepository {
+    private final JdbcTemplate jdbcTemplate;
+
+    public JdbcVoucherRepository(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    @Override
+    public void save(Voucher voucher) {
+        String sql = "insert into vouchers(discount_amount, type) values(?, ?)";
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+
+        jdbcTemplate.update(connection -> {
+            PreparedStatement statement = connection.prepareStatement(sql, new String[]{"id"});
+            statement.setInt(1, voucher.getDiscountAmount());
+            statement.setString(2, voucher.getType().toString());
+            return statement;
+        }, keyHolder);
+    }
+
+    @Override
+    public List<Voucher> getAll() {
+        String sql = "select * from vouchers";
+        List<Voucher> voucherList = new ArrayList<>();
+
+        return jdbcTemplate.query(sql, voucherRowMapper(), voucherList.toArray());
+    }
+
+    @Override
+    public void update(Long id, VoucherRequest request) {
+        String sql = "update vouchers set discount_amount=? where voucher_id=?";
+        jdbcTemplate.update(sql, request.getDiscountAmount(), id);
+    }
+
+    @Override
+    public Optional<Voucher> findById(Long id) {
+        String sql = "select * from vouchers where voucher_id = ?";
+
+        try {
+            Voucher voucher = jdbcTemplate.queryForObject(sql, voucherRowMapper(), id);
+            return Optional.of(voucher);
+        } catch (EmptyResultDataAccessException e) {
+            return Optional.empty();
+        }
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        String sql = "delete from vouchers where voucher_id=?";
+        jdbcTemplate.update(sql, id);
+    }
+
+    private RowMapper<Voucher> voucherRowMapper() {
+        return (resultSet, rowNum) -> {
+            long id = resultSet.getLong("voucher_id");
+            int discountAmount = resultSet.getInt("discount_amount");
+            VoucherType type = VoucherType.find(resultSet.getString("type"));
+
+            return new Voucher(id, discountAmount, type);
+        };
+    }
+}
