7c47aa9d9bb473fec9d05f63ad554ae5f7a4bdae
jay-so <merry9504@naver.com>
2023-07-13 22:14:41 +0900

    refactor: schema.sql refacotring(add customer table)

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,9 +1,17 @@
-drop table if exists vouchers;
-use vouchers;
+use springboot_basic;
+# drop table if exists vouchers;
 create table vouchers
 (
     voucher_id varchar(50) primary key,
     discount int not null,
     voucher_type varchar(30) not null,
     voucher_createdAt TIMESTAMP not null
+);
+# drop table if exists customers;
+create table customers
+(
+    customer_id varchar(50) primary key,
+    customer_name varchar(50) not null,
+    customer_email varchar(100) not null,
+    customer_createAt TIMESTAMP not null
 )
