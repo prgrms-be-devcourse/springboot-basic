70f60378fb6942026ac72bc974ead7e195593ee9
Hongseob Lee <sjun2918@naver.com>
2023-07-13 15:04:22 +0900

    test: RestController 테스트 @WebMvcTest 로 변경

--- src/test/java/org/promgrammers/springbootbasic/domain/voucher/controller/VoucherApiControllerTest.java
+++ src/test/java/org/promgrammers/springbootbasic/domain/voucher/controller/VoucherApiControllerTest.java
@@ -4,44 +4,52 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.promgrammers.springbootbasic.domain.voucher.dto.request.CreateVoucherRequest;
-import org.promgrammers.springbootbasic.domain.voucher.model.FixedAmountVoucher;
+import org.promgrammers.springbootbasic.domain.voucher.dto.response.VoucherListResponse;
+import org.promgrammers.springbootbasic.domain.voucher.dto.response.VoucherResponse;
+import org.promgrammers.springbootbasic.domain.voucher.model.PercentDiscountVoucher;
 import org.promgrammers.springbootbasic.domain.voucher.model.Voucher;
 import org.promgrammers.springbootbasic.domain.voucher.model.VoucherType;
-import org.promgrammers.springbootbasic.domain.voucher.repository.impl.JdbcVoucherRepository;
+import org.promgrammers.springbootbasic.domain.voucher.service.VoucherService;
+import org.promgrammers.springbootbasic.global.error.exception.BusinessException;
+import org.promgrammers.springbootbasic.global.error.exception.ErrorCode;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.UUID;
 
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.doThrow;
+import static org.promgrammers.springbootbasic.global.error.exception.ErrorCode.NOT_FOUND_VOUCHER;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@Transactional
-@AutoConfigureMockMvc
-@SpringBootTest
+@WebMvcTest(VoucherApiController.class)
 class VoucherApiControllerTest {
 
     @Autowired
     MockMvc mockMvc;
+
     @Autowired
     ObjectMapper objectMapper;
-    @Autowired
-    JdbcVoucherRepository voucherRepository;
+
+    @MockBean
+    VoucherService voucherService;
 
     @Test
     @DisplayName("바우처 생성 성공")
     void createVoucherSuccessTest() throws Exception {
 
+        //given
         CreateVoucherRequest createVoucherRequest = new CreateVoucherRequest(VoucherType.PERCENT, 10);
 
+        //when -> then
         mockMvc.perform(post("/api/vouchers/create")
                         .contentType(APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(createVoucherRequest)))
@@ -53,8 +61,14 @@ class VoucherApiControllerTest {
     @DisplayName("바우처 생성 실패 - 잘못된 금액")
     void createVoucherFailTest_Amount() throws Exception {
 
-        CreateVoucherRequest createVoucherRequest = new CreateVoucherRequest(VoucherType.PERCENT, 1200);
+        //given
+        CreateVoucherRequest createVoucherRequest = new CreateVoucherRequest(VoucherType.PERCENT, 2000);
 
+        //when
+        given(voucherService.create(createVoucherRequest))
+                .willThrow(new BusinessException(ErrorCode.INVALID_PERCENT_VOUCHER_AMOUNT));
+
+        //then
         mockMvc.perform(post("/api/vouchers/create")
                         .contentType(APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(createVoucherRequest)))
@@ -66,8 +80,19 @@ class VoucherApiControllerTest {
     @DisplayName("바우처 전체 조회")
     void findAllSuccessTest() throws Exception {
 
-        voucherRepository.insert(new FixedAmountVoucher(UUID.randomUUID(), 10));
+        //given
+        List<Voucher> vouchers = Arrays.asList(
+                new PercentDiscountVoucher(UUID.randomUUID(), 10),
+                new PercentDiscountVoucher(UUID.randomUUID(), 20)
+        );
 
+        //when
+        given(voucherService.findAll())
+                .willReturn(new VoucherListResponse(vouchers.stream()
+                        .map(VoucherResponse::new)
+                        .toList()));
+
+        //then
         mockMvc.perform(get("/api/vouchers/list")
                         .contentType(APPLICATION_JSON))
                 .andExpect(status().isOk()).andDo(print());
@@ -77,6 +102,11 @@ class VoucherApiControllerTest {
     @DisplayName("바우처 전체 조회 실패 - 바우처 존재하지 않음")
     void findAllFailTest() throws Exception {
 
+        //given -> when
+        given(voucherService.findAll())
+                .willThrow(new BusinessException(NOT_FOUND_VOUCHER));
+
+        //then
         mockMvc.perform(get("/api/vouchers/list")
                         .contentType(APPLICATION_JSON))
                 .andExpect(status().isNotFound()).andDo(print());
@@ -86,13 +116,22 @@ class VoucherApiControllerTest {
     @DisplayName("바우처 타입으로 조회 성공")
     void findByTypeSuccessTest() throws Exception {
 
-        Voucher voucher = voucherRepository.insert(new FixedAmountVoucher(UUID.randomUUID(), 10));
-        VoucherType voucherType = voucher.getVoucherType();
+        //given
+        List<Voucher> vouchers = Arrays.asList(
+                new PercentDiscountVoucher(UUID.randomUUID(), 10),
+                new PercentDiscountVoucher(UUID.randomUUID(), 20)
+        );
 
-        mockMvc.perform(get("/api/vouchers").param("voucherType", voucherType.getValue())
+        //when
+        given(voucherService.findByType(VoucherType.PERCENT))
+                .willReturn(new VoucherListResponse(vouchers.stream()
+                        .map(VoucherResponse::new)
+                        .toList()));
+
+        //then
+        mockMvc.perform(get("/api/vouchers").param("voucherType", "percent")
                         .contentType(APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.msg").value("조회 성공"))
                 .andDo(print());
     }
 
@@ -100,10 +139,18 @@ class VoucherApiControllerTest {
     @DisplayName("바우처 타입으로 조회 실패 - 저장되지 않은 타입의 바우처")
     void findByTypeFailTest() throws Exception {
 
-        Voucher voucher = voucherRepository.insert(new FixedAmountVoucher(UUID.randomUUID(), 10));
-        VoucherType voucherType = voucher.getVoucherType();
+        //given
+        List<Voucher> vouchers = Arrays.asList(
+                new PercentDiscountVoucher(UUID.randomUUID(), 10),
+                new PercentDiscountVoucher(UUID.randomUUID(), 20)
+        );
 
-        mockMvc.perform(get("/api/vouchers").param("voucherType", "percent")
+        //when
+        given(voucherService.findByType(VoucherType.FIXED))
+                .willThrow(new BusinessException(NOT_FOUND_VOUCHER));
+
+        //then
+        mockMvc.perform(get("/api/vouchers").param("voucherType", "fixed")
                         .contentType(APPLICATION_JSON))
                 .andExpect(status().isNotFound())
                 .andDo(print());
@@ -113,13 +160,20 @@ class VoucherApiControllerTest {
     @DisplayName("바우처 ID로 조회 성공")
     void findByIdSuccessTest() throws Exception {
 
-        Voucher voucher = voucherRepository.insert(new FixedAmountVoucher(UUID.randomUUID(), 10));
+        //given
+        UUID voucherId = UUID.randomUUID();
+        Voucher voucher = new PercentDiscountVoucher(voucherId, 10);
 
+        //when
+        given(voucherService.findById(voucherId))
+                .willReturn(new VoucherResponse(voucher));
+
+        //then
         mockMvc.perform(get("/api/vouchers/{id}", voucher.getVoucherId())
                         .contentType(APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.body.voucherId").value(voucher.getVoucherId().toString()))
-                .andExpect(jsonPath("$.body.voucherType").value(voucher.getVoucherType().getValue()))
+                .andExpect(jsonPath("$.body.voucherType").value(voucher.getVoucherType().toString()))
                 .andDo(print());
     }
 
@@ -127,9 +181,15 @@ class VoucherApiControllerTest {
     @DisplayName("바우처 ID로 조회 실패 - 존재하지 않는 ID")
     void findByIdFailTest() throws Exception {
 
-        UUID id = UUID.randomUUID();
+        //given
+        UUID unknownId = UUID.randomUUID();
 
-        mockMvc.perform(get("/api/vouchers/{id}", id)
+        //when
+        given(voucherService.findById(unknownId))
+                .willThrow(new BusinessException(NOT_FOUND_VOUCHER));
+
+        //then
+        mockMvc.perform(get("/api/vouchers/{id}", unknownId)
                         .contentType(APPLICATION_JSON))
                 .andExpect(status().isNotFound())
                 .andDo(print());
@@ -139,12 +199,15 @@ class VoucherApiControllerTest {
     @DisplayName("바우처 아이디로 단건 삭제")
     void deleteByIdSuccessTest() throws Exception {
 
-        Voucher voucher = voucherRepository.insert(new FixedAmountVoucher(UUID.randomUUID(), 10));
+        //given
+        UUID voucherId = UUID.randomUUID();
+        Voucher voucher = new PercentDiscountVoucher(voucherId, 10);
 
+        //when -> then
         mockMvc.perform(delete("/api/vouchers/{id}", voucher.getVoucherId())
                         .contentType(APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.msg").value("삭제 완료"))
+                .andExpect(jsonPath("$.message").value("삭제 완료"))
                 .andDo(print());
     }
 
@@ -152,8 +215,14 @@ class VoucherApiControllerTest {
     @DisplayName("바우처 ID로 단건 삭제 실패 - 존재하지 않는 ID")
     void deleteByIdFailTest() throws Exception {
 
+        //given
         UUID id = UUID.randomUUID();
 
+        //when
+        doThrow(new BusinessException(NOT_FOUND_VOUCHER))
+                .when(voucherService).deleteById(id);
+
+        //then
         mockMvc.perform(delete("/api/vouchers/{id}", id)
                         .contentType(APPLICATION_JSON))
                 .andExpect(status().isNotFound())
@@ -164,9 +233,7 @@ class VoucherApiControllerTest {
     @DisplayName("바우처 전체 삭제 성공")
     void deleteAllSuccessTest() throws Exception {
 
-        voucherRepository.insert(new FixedAmountVoucher(UUID.randomUUID(), 10));
-        voucherRepository.insert(new FixedAmountVoucher(UUID.randomUUID(), 10));
-
+        //given -> when -> then
         mockMvc.perform(delete("/api/vouchers"))
                 .andExpect(status().isOk())
                 .andDo(print());
@@ -176,6 +243,11 @@ class VoucherApiControllerTest {
     @DisplayName("바우처 전체 삭제 실패 - 존재하는 바우처 없음")
     void deleteAllFailTest() throws Exception {
 
+        //given -> when
+        doThrow(new BusinessException(NOT_FOUND_VOUCHER))
+                .when(voucherService).deleteAll();
+
+        //then
         mockMvc.perform(delete("/api/vouchers"))
                 .andExpect(status().isNotFound())
                 .andDo(print());
