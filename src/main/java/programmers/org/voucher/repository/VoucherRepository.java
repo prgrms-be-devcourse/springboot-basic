73778626fdb47e543f2a5c3f87c725fb8ccb7981
kys0411 <32180846@dankook.ac.kr>
2023-07-14 02:01:49 +0900

    voucher repository에 update, get, delete 추가

--- src/main/java/programmers/org/voucher/repository/VoucherRepository.java
+++ src/main/java/programmers/org/voucher/repository/VoucherRepository.java
@@ -2,8 +2,10 @@ package programmers.org.voucher.repository;
 
 import org.springframework.stereotype.Repository;
 import programmers.org.voucher.domain.Voucher;
+import programmers.org.voucher.dto.VoucherRequest;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface VoucherRepository {
@@ -11,4 +13,10 @@ public interface VoucherRepository {
     void save(Voucher voucher);
 
     List<Voucher> getAll();
+
+    void update(Long id, VoucherRequest request);
+
+    Optional<Voucher> findById(Long id);
+
+    void deleteById(Long id);
 }
