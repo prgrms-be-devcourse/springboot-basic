1e0735eb837aa470604b79611ebec8a3c7dab6dc
Dev Uni <wlwhsvkdlxh@gmail.com>
2023-07-14 00:26:58 +0900

    refactor: @see 활용하여 LoggerAspect 명시

--- src/main/java/com/programmers/springmission/ManagementController.java
+++ src/main/java/com/programmers/springmission/ManagementController.java
@@ -10,6 +10,13 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Component;
 
+/**
+ * 최상위 단인 컨트롤러에서 내부적으로 동작이 실행될 때
+ * 실행된 클래스, 메서드 이름, 발생 시각을 로그로 남긴다.
+ *
+ * @see com.programmers.springmission.global.aop.LoggerAspect
+ */
+
 @Slf4j
 @Component
 public class ManagementController implements CommandLineRunner {
--- src/main/java/com/programmers/springmission/global/exception/InvalidInputException.java
+++ src/main/java/com/programmers/springmission/global/exception/InvalidInputException.java
@@ -1,5 +1,11 @@
 package com.programmers.springmission.global.exception;
 
+/**
+ * 잘못된 입력으로 인하여 발생하는 예외를 로그로 남긴다.
+ *
+ * @see com.programmers.springmission.global.aop.LoggerAspect
+ */
+
 public class InvalidInputException extends RuntimeException {
 
     public InvalidInputException(ErrorMessage errorMessage) {
