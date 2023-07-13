2765fd2e60cbb33fa37ab37bedf61586912f9b69
hyeon-z <phj8221@naver.com>
2023-07-13 18:18:56 +0900

    feat: AmountVoucherVo에 equals(), hashCode() 재정의

--- voucher/src/main/java/org/prgms/voucher/voucher/AmountVoucherVo.java
+++ voucher/src/main/java/org/prgms/voucher/voucher/AmountVoucherVo.java
@@ -1,5 +1,7 @@
 package org.prgms.voucher.voucher;
 
+import java.util.Objects;
+
 public class AmountVoucherVo {
     private final AmountVoucherCreationType amountVoucherCreationType;
     private final int discountAmount;
@@ -15,5 +17,18 @@ public class AmountVoucherVo {
 
     public AmountVoucherCreationType getAmountVoucherCreationType() {
         return amountVoucherCreationType;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AmountVoucherVo amountVoucherVo = (AmountVoucherVo) o;
+        return discountAmount == amountVoucherVo.discountAmount && amountVoucherCreationType == amountVoucherVo.amountVoucherCreationType;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(amountVoucherCreationType, discountAmount);
     }
 }
