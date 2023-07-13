6d4ecc677c0e9a74b3efc3ab1ad6c9dfb3cb07b9
kys0411 <32180846@dankook.ac.kr>
2023-07-14 02:07:15 +0900

    memory repository에서 update, get, delete 메서드 구현

--- src/main/java/programmers/org/voucher/repository/MemoryVoucherRepository.java
+++ src/main/java/programmers/org/voucher/repository/MemoryVoucherRepository.java
@@ -1,8 +1,11 @@
 package programmers.org.voucher.repository;
 
 import programmers.org.voucher.domain.Voucher;
+import programmers.org.voucher.dto.VoucherRequest;
+
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class MemoryVoucherRepository implements VoucherRepository {
@@ -17,5 +20,23 @@ public class MemoryVoucherRepository implements VoucherRepository {
     @Override
     public List<Voucher> getAll() {
         return List.copyOf(voucherStorage.values());
+    }
+
+    @Override
+    public void update(Long id, VoucherRequest request) {
+        voucherStorage.put(id, request.toEntity());
+    }
+
+    @Override
+    public Optional<Voucher> findById(Long id) {
+        if (voucherStorage.get(id) == null) {
+            return Optional.empty();
+        }
+        return Optional.of(voucherStorage.get(id));
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        voucherStorage.remove(id);
     }
 }
