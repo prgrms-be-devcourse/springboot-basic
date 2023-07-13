84cbef3a7f6f74680a45267d21c9d52ca3becdd8
kys0411 <32180846@dankook.ac.kr>
2023-07-14 03:35:58 +0900

    jdbc customer repository 빈 등록

--- src/main/java/programmers/org/voucher/config/AppConfig.java
+++ src/main/java/programmers/org/voucher/config/AppConfig.java
@@ -2,6 +2,8 @@ package programmers.org.voucher.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import programmers.org.voucher.repository.CustomerRepository;
+import programmers.org.voucher.repository.JdbcCustomerRepository;
 import programmers.org.voucher.repository.JdbcVoucherRepository;
 import programmers.org.voucher.repository.VoucherRepository;
 
@@ -13,5 +15,10 @@ public class AppConfig {
     @Bean
     public VoucherRepository voucherRepository(DataSource dataSource) {
         return new JdbcVoucherRepository(dataSource);
+    }
+
+    @Bean
+    public CustomerRepository customerRepository(DataSource dataSource) {
+        return new JdbcCustomerRepository(dataSource);
     }
 }
