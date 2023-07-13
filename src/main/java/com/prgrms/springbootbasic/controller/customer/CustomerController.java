4a273f66b6c0f786bb0b9e8b4d79f6c9fc0db0e6
jay-so <merry9504@naver.com>
2023-07-13 22:15:14 +0900

    feat:CustomerController(Create,Update,Read,Delete)

--- src/main/java/com/prgrms/springbootbasic/controller/customer/CustomerController.java
+++ src/main/java/com/prgrms/springbootbasic/controller/customer/CustomerController.java
@@ -1,8 +1,50 @@
 package com.prgrms.springbootbasic.controller.customer;
 
+import com.prgrms.springbootbasic.dto.customer.request.CustomerCreateRequest;
+import com.prgrms.springbootbasic.dto.customer.request.CustomerUpdateRequest;
+import com.prgrms.springbootbasic.dto.customer.response.CustomerListResponse;
+import com.prgrms.springbootbasic.dto.customer.response.CustomerResponse;
+import com.prgrms.springbootbasic.service.customer.CustomerService;
+import java.time.LocalDateTime;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 
 @Controller
+@AllArgsConstructor
 public class CustomerController {
 
+    private final CustomerService customerService;
+
+
+    //(Create)고객 생성
+    public void create(CustomerCreateRequest customerCreateRequest) {
+        customerService.createCustomer(customerCreateRequest);
+    }
+
+
+    //(Read) 고객의 모든 리스트 출력
+    public CustomerListResponse findAllList() {
+        return customerService.findAllCustomers();
+    }
+
+    //(Read) 고객 생성기간 출력
+    public CustomerResponse findByCreateAt(LocalDateTime createAt) {
+        return customerService.findByCreateAt(createAt);
+    }
+
+    //(Update) 고객 수정
+    public void update(CustomerUpdateRequest customerUpdateRequset) {
+        customerService.updateCustomer(customerUpdateRequset);
+    }
+
+    //(Delete) 특정 고객을 찾아 삭제
+    public void deleteById(UUID customerId) {
+        customerService.deleteById(customerId);
+    }
+
+    //(Delete) 모든 고객 내용 삭제
+    public void deleteAll() {
+        customerService.deleteAllCustomer();
+    }
 }
