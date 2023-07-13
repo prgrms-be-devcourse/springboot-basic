3175e9c2ce95ec5cb1ab978ab1cfcca1eb096200
hseong3243 <hseong3243@gmail.com>
2023-07-13 22:42:23 +0900

    refactor: `VoucherConsoleController`가 `Console`을 모르도록 변경

--- src/test/java/com/programmers/voucher/global/io/menu/ConsoleVoucherMenuTest.java
+++ src/test/java/com/programmers/voucher/global/io/menu/ConsoleVoucherMenuTest.java
@@ -1,6 +1,7 @@
 package com.programmers.voucher.global.io.menu;
 
 import com.programmers.voucher.domain.voucher.controller.VoucherConsoleController;
+import com.programmers.voucher.domain.voucher.dto.VoucherDto;
 import com.programmers.voucher.global.io.Console;
 import com.programmers.voucher.global.io.command.VoucherCommandType;
 import org.junit.jupiter.api.DisplayName;
@@ -10,6 +11,12 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.List;
+import java.util.UUID;
+
+import static com.programmers.voucher.testutil.VoucherTestUtil.createFixedVoucherDto;
+import static com.programmers.voucher.testutil.VoucherTestUtil.createPercentVoucherDto;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.times;
@@ -27,37 +34,44 @@ class ConsoleVoucherMenuTest {
     private VoucherConsoleController voucherController;
 
     @Test
-    @DisplayName("성공: create 명령 입력 - exit 명령 입력")
+    @DisplayName("성공: create 명령 실행 - exit 명령 실행")
     public void voucherCommandTypeCreate() {
         //given
         given(console.inputVoucherCommandType())
                 .willReturn(VoucherCommandType.CREATE, VoucherCommandType.EXIT);
+        given(voucherController.createVoucher(any())).willReturn(UUID.randomUUID());
 
         //when
         consoleVoucherMenu.runningVoucherService();
 
         //then
-        then(console).should(times(2)).inputVoucherCommandType();
-        then(voucherController).should().createVoucher();
+        then(console).should().inputVoucherCreateInfo();
+        then(voucherController).should().createVoucher(any());
+        then(console).should().print(any());
     }
 
     @Test
-    @DisplayName("성공: list 명령 입력 - exit 명령 입력")
+    @DisplayName("성공: list 명령 실행 - exit 명령 실행")
     void voucherCommandTypeList() {
         //given
+        VoucherDto fixedVoucher = createFixedVoucherDto(UUID.randomUUID(), 10);
+        VoucherDto percentVoucher = createPercentVoucherDto(UUID.randomUUID(), 10);
+        List<VoucherDto> vouchers = List.of(fixedVoucher, percentVoucher);
+
         given(console.inputVoucherCommandType())
                 .willReturn(VoucherCommandType.LIST, VoucherCommandType.EXIT);
+        given(voucherController.findVouchers()).willReturn(vouchers);
 
         //when
         consoleVoucherMenu.runningVoucherService();
 
         //then
-        then(console).should(times(2)).inputVoucherCommandType();
         then(voucherController).should().findVouchers();
+        then(console).should().printVouchers(any());
     }
 
     @Test
-    @DisplayName("성공: voucher 명령 입력 - delete 명령 입력 - exit 명령 입력")
+    @DisplayName("성공: delete 명령 실행 - exit 명령 실행")
     void voucherCommandTypeDelete() {
         //given
         given(console.inputVoucherCommandType())
@@ -67,12 +81,13 @@ class ConsoleVoucherMenuTest {
         consoleVoucherMenu.runningVoucherService();
 
         //then
-        then(console).should(times(2)).inputVoucherCommandType();
-        then(voucherController).should().deleteVoucher();
+        then(console).should().inputUUID();
+        then(voucherController).should().deleteVoucher(any());
+        then(console).should().print(any());
     }
 
     @Test
-    @DisplayName("성공: help 명령 입력 - exit 명령 입력")
+    @DisplayName("성공: help 명령 실행 - exit 명령 실행")
     void voucherCommandTypeHelp() {
         //given
         given(console.inputVoucherCommandType())
@@ -82,7 +97,6 @@ class ConsoleVoucherMenuTest {
         consoleVoucherMenu.runningVoucherService();
 
         //then
-        then(console).should(times(2)).inputVoucherCommandType();
         then(console).should(times(2)).printVoucherCommandSet();
     }
 }
