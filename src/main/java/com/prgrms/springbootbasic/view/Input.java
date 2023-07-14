42f649b2a8b2587800bc25bf1210f95a18dbd1eb
jay-so <merry9504@naver.com>
2023-07-14 15:01:58 +0900

    refactor:InputView refactoring

--- src/main/java/com/prgrms/springbootbasic/view/Input.java
+++ src/main/java/com/prgrms/springbootbasic/view/Input.java
@@ -1,10 +1,30 @@
 package com.prgrms.springbootbasic.view;
 
+import com.prgrms.springbootbasic.dto.customer.request.CustomerCreateRequest;
+import com.prgrms.springbootbasic.dto.customer.request.CustomerUpdateRequest;
+import com.prgrms.springbootbasic.dto.voucher.request.VoucherCreateRequest;
+import com.prgrms.springbootbasic.dto.voucher.request.VoucherUpdateRequest;
+import com.prgrms.springbootbasic.enums.VoucherType;
+import java.util.UUID;
+
 public interface Input {
 
+    //콘솔 명령어 입력
     String inputCommand();
 
-    String inputVoucherType();
+    //바우처 생성(Create)
+    VoucherCreateRequest voucherCrateMessage(VoucherType type);
 
-    long inputVoucherDiscount();
+    //바우처 변경(Update)
+    VoucherUpdateRequest voucherUpdateMessage(UUID voucherId);
+
+    //고객 생성(Create)
+    CustomerCreateRequest customerCreateMessage();
+
+    //고객 변경(Update)
+    CustomerUpdateRequest customerUpdateMessage(UUID cusomterId);
+
+    //ID값 입력
+    UUID inputUUID();
+
 }
