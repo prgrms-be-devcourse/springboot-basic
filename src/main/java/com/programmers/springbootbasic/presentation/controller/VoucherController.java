924270b4a51b667e4299e2f0ed67f98612b569f5
sangminnim <sangmins930@gmail.com>
2023-07-14 20:17:19 +0900

    feat: controller 검증로직 추가 및 파라미터 변경

--- src/main/java/com/programmers/springbootbasic/presentation/controller/VoucherController.java
+++ src/main/java/com/programmers/springbootbasic/presentation/controller/VoucherController.java
@@ -1,10 +1,13 @@
 package com.programmers.springbootbasic.presentation.controller;
 
+import com.programmers.springbootbasic.common.util.Validator;
 import com.programmers.springbootbasic.service.VoucherService;
 import com.programmers.springbootbasic.service.dto.VoucherCreationRequest;
 import com.programmers.springbootbasic.service.dto.VoucherResponse;
 import com.programmers.springbootbasic.service.dto.VoucherResponses;
 import org.springframework.stereotype.Controller;
+
+import java.time.LocalDateTime;
 
 @Controller
 public class VoucherController {
@@ -14,11 +17,21 @@ public class VoucherController {
         this.voucherService = voucherService;
     }
 
-    public VoucherResponse createVoucher(VoucherCreationRequest request) {
+    public VoucherResponse createVoucher(String voucherType, String name, Long minimumPriceCondition, LocalDateTime expiredAt, Integer amountOrPercent) {
+        checkValid(voucherType, name, expiredAt, amountOrPercent);
+        VoucherCreationRequest request = new VoucherCreationRequest(voucherType, name, minimumPriceCondition, expiredAt, amountOrPercent);
         return voucherService.createVoucher(request);
     }
 
     public VoucherResponses listVouchers() {
         return voucherService.list();
+    }
+
+
+    private void checkValid(String voucherType, String name, LocalDateTime expiredAt, Integer amountOrPercent) {
+        Validator.checkNullOrBlank(voucherType);
+        Validator.checkNullOrBlank(name);
+        Validator.checkNullDateTime(expiredAt);
+        Validator.checkNullNumber(amountOrPercent);
     }
 }
