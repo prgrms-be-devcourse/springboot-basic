c3d73009669cf90a7353bcd72afb8122a100f7b8
yumyeonghan <mgtmh991013@gmail.com>
2023-07-14 16:22:08 +0900

    feat: handleException() 추가
    1. 예기치 못한 상황에서 발생하는 Exception 응답

--- src/main/java/com/programmers/application/controller/ApiExceptionHandler.java
+++ src/main/java/com/programmers/application/controller/ApiExceptionHandler.java
@@ -2,20 +2,26 @@ package com.programmers.application.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.util.NoSuchElementException;
 
-@RestControllerAdvice(basePackages = "com.programmers.application.controller.api")
+@ControllerAdvice(basePackages = "com.programmers.application.controller.api")
 public class ApiExceptionHandler {
 
-    @org.springframework.web.bind.annotation.ExceptionHandler
+    @ExceptionHandler
     public ResponseEntity<ErrorResult> handleIllegalArgumentException(IllegalArgumentException e) {
         return ResponseEntity.badRequest().body(ErrorResult.of(HttpStatus.BAD_REQUEST.getReasonPhrase(), e.getMessage()));
     }
 
-    @org.springframework.web.bind.annotation.ExceptionHandler
+    @ExceptionHandler
     public ResponseEntity<ErrorResult> handleNoSuchElementException(NoSuchElementException e) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ErrorResult.of(HttpStatus.NOT_FOUND.getReasonPhrase(), e.getMessage()));
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorResult> handleException(Exception e) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ErrorResult.of(HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase(), e.getMessage()));
     }
 }
