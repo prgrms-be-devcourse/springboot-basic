38ce4a371c0ef1399945c44c913cbbc194c8f901
hi-june <wnsgur735@gmail.com>
2023-07-14 21:30:38 +0900

    refactor: voucher console controller 수정

--- springbasic/src/main/java/com/programmers/springbasic/domain/voucher/controller/VoucherController.java
+++ springbasic/src/main/java/com/programmers/springbasic/domain/voucher/controller/VoucherController.java
@@ -1,15 +1,21 @@
 package com.programmers.springbasic.domain.voucher.controller;
 
 import com.programmers.springbasic.domain.io.IOConsole;
+import com.programmers.springbasic.domain.voucher.dto.request.VoucherCreateRequestDTO;
 import com.programmers.springbasic.domain.voucher.validator.*;
 import com.programmers.springbasic.domain.voucher.view.VoucherCommandOption;
-import com.programmers.springbasic.domain.voucher.view.VoucherOption;
+import com.programmers.springbasic.domain.voucher.model.VoucherType;
 import com.programmers.springbasic.domain.voucher.service.VoucherService;
 import com.programmers.springbasic.domain.voucher.view.VoucherCommandMessage;
+import com.programmers.springbasic.domain.voucher.view.VoucherConsoleResponse;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 
 @Controller
 @RequiredArgsConstructor
@@ -24,104 +30,96 @@ public class VoucherController {
         ioConsole.showVoucherMenu();
 
         String inputCommand = ioConsole.getInput();
-        VoucherCommandValidator voucherCommandValidator = new VoucherCommandValidator(inputCommand);
+        VoucherCommandValidator.validateCommand(inputCommand);
 
-        handleCommand(voucherCommandValidator);
+        handleCommand(inputCommand);
     }
 
-    private void handleCommand(VoucherCommandValidator voucherCommandValidator) {
-        String command = voucherCommandValidator.getInputCommand();
-
-        if (command.equals(VoucherCommandOption.CREATE.getCommand())) {
-            executeCreateVoucher();
-            return;
-        }
-
-        if (command.equals(VoucherCommandOption.LIST.getCommand())) {
-            executeListVoucher();
-            return;
-        }
-
-        if (command.equals(VoucherCommandOption.READ.getCommand())) {
-            executeRaedVoucher();
-            return;
-        }
-
-        if (command.equals(VoucherCommandOption.UPDATE.getCommand())) { // TODO: update voucher
-            executeUpdateVoucher();
-            return;
-        }
-
-        if (command.equals(VoucherCommandOption.DELETE.getCommand())) {
-            executeDeleteVoucher();
-            return;
-        }
-
-        if (command.equals(VoucherCommandOption.SHOW.getCommand())) {
-            executeShowVoucher();
-            return;
-        }
-
-        if (command.equals(VoucherCommandOption.EXIT.getCommand())) {
-            ioConsole.printSingleOutput("Go back to Main Menu!");
+    private void handleCommand(String command) {
+        switch (VoucherCommandOption.of(command)) {
+            case CREATE: {
+                executeCreateVoucher();
+                break;
+            }
+            case LIST: {
+                executeListVoucher();
+                break;
+            }
+            case READ: {
+                executeRaedVoucher();
+                break;
+            }
+            case UPDATE: {
+                executeUpdateVoucher();
+                break;
+            }
+            case DELETE: {
+                executeDeleteVoucher();
+                break;
+            }
+            case SHOW: {
+                executeShowVoucher();
+                break;
+            }
+            case EXIT: {
+                ioConsole.printSingleOutput("Go back to Main Menu!");
+            }
         }
     }
 
     private void executeCreateVoucher() {
         ioConsole.printSingleOutput(VoucherCommandMessage.VOUCHER_OPTION_MESSAGE.getMessage());
 
-        String inputVoucherOption = ioConsole.getInput();
-        VoucherOptionValidator voucherOptionValidator = new VoucherOptionValidator(inputVoucherOption);
-
-        String voucherOption = voucherOptionValidator.getVoucherOption();
+        String voucherType = ioConsole.getInput();
+        VoucherOptionValidator.validateOption(voucherType);
 
         ioConsole.printSingleOutput("Voucher를 할당할 Customer Id를 입력해주세요");
         String inputCustomerId = ioConsole.getInput();
-        VoucherCodeValidator voucherCodeValidator = new VoucherCodeValidator(inputCustomerId);
+        VoucherCodeValidator.validateVoucherCode(inputCustomerId);
 
-        if (voucherOption.equals(VoucherOption.FIXED_AMOUNT_VOUCHER.getVoucherOption())) {
-            ioConsole.printSingleOutput(VoucherCommandMessage.FIXED_AMOUNT_INPUT_MESSAGE.getMessage());
-            String fixedAmountInput = ioConsole.getInput();
-            FixedAmountVoucherCreateRequestValidator fixedAmountVoucherCreateRequestValidator = new FixedAmountVoucherCreateRequestValidator(fixedAmountInput);
+        switch (VoucherType.of(voucherType)) {
+            case FIXED_AMOUNT_VOUCHER: {
+                ioConsole.printSingleOutput(VoucherCommandMessage.FIXED_AMOUNT_INPUT_MESSAGE.getMessage());
+                String fixedAmountInput = ioConsole.getInput();
+                FixedAmountVoucherCreateRequestValidator.validateFixedAmount(fixedAmountInput);
 
-            voucherService.createFixedAmountVoucher(fixedAmountVoucherCreateRequestValidator, voucherCodeValidator);
-            ioConsole.printSingleOutput(VoucherCommandMessage.FIXED_AMOUNT_VOUCHER_CREATE_MESSAGE.getMessage());
-        }
+                VoucherCreateRequestDTO createRequestDTO = new VoucherCreateRequestDTO(inputCustomerId, voucherType, Double.parseDouble(fixedAmountInput));
+                voucherService.createVoucher(createRequestDTO);
+                ioConsole.printSingleOutput(VoucherCommandMessage.FIXED_AMOUNT_VOUCHER_CREATE_MESSAGE.getMessage());
+                break;
+            }
+            case PERCENT_DISCOUNT_VOUCHER: {
+                ioConsole.printSingleOutput(VoucherCommandMessage.PERCENT_DISCOUNT_INPUT_MESSAGE.getMessage());
+                String percentDiscountInput = ioConsole.getInput();
+                PercentDiscountVoucherCreateRequestValidator.validatePercentDiscount(percentDiscountInput);
 
-        if (voucherOption.equals(VoucherOption.PERCENT_DISCOUNT_VOUCHER.getVoucherOption())) {
-            ioConsole.printSingleOutput(VoucherCommandMessage.PERCENT_DISCOUNT_INPUT_MESSAGE.getMessage());
-            String percentDiscountInput = ioConsole.getInput();
-            PercentDiscountVoucherCreateRequestValidator percentDiscountVoucherCreateRequestValidator = new PercentDiscountVoucherCreateRequestValidator(percentDiscountInput);
-
-            voucherService.createPercentDiscountVoucher(percentDiscountVoucherCreateRequestValidator, voucherCodeValidator);
-            ioConsole.printSingleOutput(VoucherCommandMessage.PERCENT_DISCOUNT_VOUCHER_CREATE_MESSAGE.getMessage());
+                VoucherCreateRequestDTO createRequestDTO = new VoucherCreateRequestDTO(inputCustomerId, voucherType, Double.parseDouble(percentDiscountInput));
+                voucherService.createVoucher(createRequestDTO);
+                ioConsole.printSingleOutput(VoucherCommandMessage.PERCENT_DISCOUNT_VOUCHER_CREATE_MESSAGE.getMessage());
+            }
         }
     }
 
     private void executeListVoucher() {
         ioConsole.printSingleOutput(VoucherCommandMessage.VOUCHER_OPTION_MESSAGE.getMessage());
 
-        String inputVoucherOption = ioConsole.getInput();
-        VoucherOptionValidator voucherOptionValidator = new VoucherOptionValidator(inputVoucherOption);
+        String inputVoucherType = ioConsole.getInput();
+        VoucherOptionValidator.validateOption(inputVoucherType);
 
-        String voucherOption = voucherOptionValidator.getVoucherOption();
+        List<String> voucherInfos = voucherService.getAllVoucherInfo(inputVoucherType).stream()
+                .map(VoucherConsoleResponse::getVoucherInfo)
+                .collect(Collectors.toList());
 
-        if (voucherOption.equals(VoucherOption.FIXED_AMOUNT_VOUCHER.getVoucherOption())) {
-            ioConsole.printListOutput(voucherService.getAllFixedAmountVoucherInfo());
-        }
-
-        if (voucherOption.equals(VoucherOption.PERCENT_DISCOUNT_VOUCHER.getVoucherOption())) {
-            ioConsole.printListOutput(voucherService.getAllPercentDiscountVoucherInfo());
-        }
+        ioConsole.printListOutput(voucherInfos);
     }
 
     private void executeRaedVoucher() {
         ioConsole.printSingleOutput("조회하고자 하는 voucher의 code를 입력해주세요.");
 
         String voucherCodeInput = ioConsole.getInput();
-        VoucherCodeValidator voucherCodeValidator = new VoucherCodeValidator(voucherCodeInput);
+        VoucherCodeValidator.validateVoucherCode(voucherCodeInput);
 
-        ioConsole.printSingleOutput(voucherService.findVoucher(voucherCodeValidator));
+        ioConsole.printSingleOutput(VoucherConsoleResponse.getVoucherInfo(voucherService.findVoucher(voucherCodeInput)));
     }
 
     private void executeUpdateVoucher() {   // TODO: update voucher
@@ -131,17 +129,17 @@ public class VoucherController {
         ioConsole.printSingleOutput("삭제하고자 하는 voucher의 code를 입력해주세요.");
 
         String voucherCodeInput = ioConsole.getInput();
-        VoucherCodeValidator voucherCodeValidator = new VoucherCodeValidator(voucherCodeInput);
+        VoucherCodeValidator.validateVoucherCode(voucherCodeInput);
 
-        voucherService.removeVoucher(voucherCodeValidator);
+        voucherService.removeVoucher(voucherCodeInput);
     }
 
     private void executeShowVoucher() {
         ioConsole.printSingleOutput("특정 voucher를 소지한 고객의 명단을 보여줍니다. voucher type을 입력해주세요. (FIXED or PERCENT)");
 
         String inputVoucherOption = ioConsole.getInput();
-        VoucherOptionValidator voucherOptionValidator = new VoucherOptionValidator(inputVoucherOption);
+        VoucherOptionValidator.validateOption(inputVoucherOption);
 
-        ioConsole.printListOutput(voucherService.getAllCustomerIdByVoucherType(voucherOptionValidator));
+        // ioConsole.printListOutput(voucherService.getAllCustomerIdByVoucherType(inputVoucherOption));
     }
 }
