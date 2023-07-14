020c3d1920b55b941bc727d0aeca9587404ccc45
yumyeonghan <mgtmh991013@gmail.com>
2023-07-14 15:10:37 +0900

    feat: @RequestMapping 추가
    1. @RequestMapping 통해 공통된 URI("api")를 하나로 묶음

--- src/main/java/com/programmers/application/controller/api/VoucherApiController.java
+++ src/main/java/com/programmers/application/controller/api/VoucherApiController.java
@@ -6,15 +6,13 @@ import com.programmers.application.service.VoucherService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
 
 @Controller
+@RequestMapping("/api")
 public class VoucherApiController {
 
     private final VoucherService voucherService;
@@ -23,19 +21,19 @@ public class VoucherApiController {
         this.voucherService = voucherService;
     }
 
-    @GetMapping("/api/v1/vouchers")
+    @GetMapping("/v1/vouchers")
     public ResponseEntity<List<VoucherInfoResponse>> findVouchers() {
         List<VoucherInfoResponse> voucherList = voucherService.findVoucherList();
         return ResponseEntity.ok().body(voucherList);
     }
 
-    @GetMapping("/api/v1/voucher/{voucherId}")
+    @GetMapping("/v1/voucher/{voucherId}")
     public ResponseEntity<VoucherInfoResponse> findVoucherByVoucherId(@PathVariable UUID voucherId) {
         VoucherInfoResponse voucher = voucherService.findVoucherByVoucherId(voucherId);
         return ResponseEntity.ok().body(voucher);
     }
 
-    @PostMapping("/api/v1/voucher")
+    @PostMapping("/v1/voucher")
     public ResponseEntity<VoucherInfoResponse> createVoucher(@RequestBody VoucherCreationRequest voucherCreationRequest) {
         UUID voucherId = voucherService.createVoucher(voucherCreationRequest);
         VoucherInfoResponse voucher = voucherService.findVoucherByVoucherId(voucherId);
