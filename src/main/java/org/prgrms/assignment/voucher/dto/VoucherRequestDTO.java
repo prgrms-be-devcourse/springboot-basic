752f21887a6c6a5b5e08e79c7e7a1e86732e53b2
BeommoKoo <rnqjaah1536@naver.com>
2023-07-14 12:41:51 +0900

    refactor : 서비스에서 DTO->Entity로 변경시키는 책임을 각각의 record에서 가지도록 변경, 각 레이어별로 이동할 때 DTO를 가지도록 변경

--- src/main/java/org/prgrms/assignment/voucher/dto/VoucherRequestDTO.java
+++
@@ -1,8 +0,0 @@
-package org.prgrms.assignment.voucher.dto;
-
-import org.prgrms.assignment.voucher.model.VoucherType;
-
-
-public record VoucherRequestDTO (VoucherType voucherType, long benefit,
-                                long durationDate){
-}
--- src/main/java/org/prgrms/assignment/voucher/dto/VoucherResponseDTO.java
+++ src/main/java/org/prgrms/assignment/voucher/dto/VoucherResponseDTO.java
@@ -1,10 +1,26 @@
 package org.prgrms.assignment.voucher.dto;
 
+import org.prgrms.assignment.voucher.entity.VoucherEntity;
 import org.prgrms.assignment.voucher.model.VoucherType;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-public record VoucherResponseDTO(UUID voucherId,  VoucherType voucherType, long benefit, LocalDateTime createdAt,
-                                 LocalDateTime expireDate) {
+public record VoucherResponseDTO(
+    UUID voucherId,
+    VoucherType voucherType,
+    long benefit,
+    LocalDateTime createdAt,
+    LocalDateTime expireDate
+) {
+
+    public static VoucherResponseDTO of(VoucherEntity voucherEntity) {
+        return new VoucherResponseDTO(
+            voucherEntity.voucherId(),
+            voucherEntity.voucherType(),
+            voucherEntity.benefit(),
+            voucherEntity.createdAt(),
+            voucherEntity.expireDate()
+        );
+    }
 }
