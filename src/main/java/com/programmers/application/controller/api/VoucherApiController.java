cacf90d3dc13456072185cedec6715fbb48b85f3
yumyeonghan <mgtmh991013@gmail.com>
2023-07-14 14:52:38 +0900

    feat: @RestController => @Controller 수정
    1. ResponseEntity를 사용하므로 같은 동작을 하는 @ReponseBody를 사용하지 않아도 됨

--- src/main/java/com/programmers/application/controller/api/VoucherApiController.java
+++ src/main/java/com/programmers/application/controller/api/VoucherApiController.java
@@ -5,12 +5,16 @@ import com.programmers.application.dto.request.VoucherCreationRequest;
 import com.programmers.application.service.VoucherService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 import java.util.UUID;
 
-@RestController
+@Controller
 public class VoucherApiController {
 
     private final VoucherService voucherService;
