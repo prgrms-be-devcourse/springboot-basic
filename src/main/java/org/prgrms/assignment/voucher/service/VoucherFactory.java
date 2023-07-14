77ec988c5b439d24bc01db463ccfe934d67fd7f2
BeommoKoo <rnqjaah1536@naver.com>
2023-07-14 12:47:16 +0900

    refactor : VoucherFactory에서 DTO인차를 기반으로 바우처객체 생성

--- src/main/java/org/prgrms/assignment/voucher/service/VoucherFactory.java
+++ src/main/java/org/prgrms/assignment/voucher/service/VoucherFactory.java
@@ -1,5 +1,6 @@
 package org.prgrms.assignment.voucher.service;
 
+import org.prgrms.assignment.voucher.dto.VoucherServiceRequestDTO;
 import org.prgrms.assignment.voucher.model.*;
 import org.springframework.stereotype.Component;
 
@@ -9,17 +10,27 @@ import java.util.UUID;
 @Component
 public class VoucherFactory {
 
-    public Voucher createVoucher(VoucherType voucherType, UUID voucherId, Long benefit, long durationDate) {
-        LocalDateTime now = LocalDateTime.now();
-        LocalDateTime expireDate = now.plusDays(durationDate);
+    public Voucher createVoucher(VoucherServiceRequestDTO voucherServiceRequestDTO) {
+        VoucherType voucherType = voucherServiceRequestDTO.voucherType();
         switch (voucherType) {
             case FIXED -> {
-                return new FixedAmountVoucher(voucherId, benefit, now, expireDate);
+                return new FixedAmountVoucher(
+                    voucherServiceRequestDTO.voucherId(),
+                    voucherServiceRequestDTO.benefit(),
+                    voucherServiceRequestDTO.createdAt(),
+                    voucherServiceRequestDTO.expireDate()
+                );
             }
             case PERCENT -> {
-                return new PercentDiscountVoucher(voucherId, benefit, now, expireDate);
+                return new PercentDiscountVoucher(
+                    voucherServiceRequestDTO.voucherId(),
+                    voucherServiceRequestDTO.benefit(),
+                    voucherServiceRequestDTO.createdAt(),
+                    voucherServiceRequestDTO.expireDate()
+                );
             }
         }
         throw new RuntimeException("Failed to create Voucher!");
     }
+
 }
