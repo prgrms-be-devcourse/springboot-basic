e9f27abef07f6195836cb64cdbdc56e18cb47633
sangminnim <sangmins930@gmail.com>
2023-07-14 22:48:18 +0900

    feat: Email 확인 기능 추가

--- src/main/java/com/programmers/springbootbasic/common/util/PatternUtils.java
+++ src/main/java/com/programmers/springbootbasic/common/util/PatternUtils.java
@@ -3,18 +3,22 @@ package com.programmers.springbootbasic.common.util;
 import java.util.regex.Pattern;
 
 public final class PatternUtils {
-    private static final Pattern DATE_PATTERN = Pattern.compile("^\\d{4}-\\d{2}-\\d{2}$");
-    private static final Pattern DATETIME_PATTERN = Pattern.compile("^\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2}$");
+    private static final Pattern DATE = Pattern.compile("^\\d{4}-\\d{2}-\\d{2}$");
+    private static final Pattern DATETIME = Pattern.compile("^\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2}$");
+    private static final Pattern EMAIL = Pattern.compile("\\b[\\w\\.-]+@[\\w\\.-]+\\.\\w{2,4}\\b");
 
     private PatternUtils() {
     }
 
     public static boolean isDate(String input) {
-        return DATE_PATTERN.matcher(input).matches();
+        return DATE.matcher(input).matches();
     }
 
     public static boolean isDateTime(String input) {
-        return DATETIME_PATTERN.matcher(input).matches();
+        return DATETIME.matcher(input).matches();
     }
 
+    public static boolean isEmail(String input) {
+        return EMAIL.matcher(input).matches();
+    }
 }
