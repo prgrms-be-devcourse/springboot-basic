7e599f9afe65b15f392e654907221bf618e8643f
BeommoKoo <rnqjaah1536@naver.com>
2023-07-14 13:03:13 +0900

    feat : deleteMapping어노테이션 사용을 위한 빈 추가

--- src/main/java/org/prgrms/assignment/voucher/VoucherApp.java
+++ src/main/java/org/prgrms/assignment/voucher/VoucherApp.java
@@ -2,16 +2,21 @@ package org.prgrms.assignment.voucher;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"org.prgrms.assignment.voucher.configuration"})
-//@Profile({"dev"})
 public class VoucherApp implements WebMvcConfigurer {
 
     public static void main(String[] args) {
         SpringApplication.run(VoucherApp.class, args);
     }
 
+    @Bean
+    public HiddenHttpMethodFilter hiddenHttpMethodFilter(){
+        return new HiddenHttpMethodFilter();
+    }
 }
