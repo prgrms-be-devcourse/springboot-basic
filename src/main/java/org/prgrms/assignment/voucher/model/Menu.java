6bf35565f54bf5aeb1c955e8fb64d53dce9216c3
BeommoKoo <rnqjaah1536@naver.com>
2023-07-14 12:46:30 +0900

    refactor : Valueof에서 예외를 터트리므로, 검증삭제

--- src/main/java/org/prgrms/assignment/voucher/model/Menu.java
+++ src/main/java/org/prgrms/assignment/voucher/model/Menu.java
@@ -1,28 +1,14 @@
 package org.prgrms.assignment.voucher.model;
 
-import org.aspectj.lang.annotation.Pointcut;
-
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.stream.Stream;
+import org.prgrms.kdt.voucher.model.Command;
 
 public enum Menu {
+
     EXIT("exit"),
     CREATE("create"),
     LIST("list");
 
-    private static final String TOO_LONG_COMMAND_ERROR = "Your COMMAND length is over than 1!";
-    private static final String NO_MENU_NAMED_ERROR = "There is no menu named ";
-    private static final Map<String, Menu> commandToMenuMap = new HashMap<>();
-    static {
-        for(Menu menu : Menu.values()) {
-            commandToMenuMap.put(menu.menuName, menu);
-        }
-    }
     private final String menuName;
-
-    private static final Menu[] menus = Menu.values();
 
     Menu(String menuName) {
         this.menuName = menuName;
@@ -32,28 +18,8 @@ public enum Menu {
         return menuName;
     }
 
-    public static boolean isValidMenu(String command) {
-        for (Menu menu : menus) {
-            if (menu.getMenuName().equals(command)) {
-                return true;
-            }
-        }
-        return false;
+    public static Menu of(Command command) {
+        return Menu.valueOf(command.getCommand());
     }
 
-    private static boolean isLongerThanOne(String command) {
-        String[] parsedCommand = command.
-            split(" ");
-        return parsedCommand.length >= 2;
-    }
-
-    public static Menu of(String command) {
-        if(!isValidMenu(command)) {
-            throw new IllegalArgumentException(NO_MENU_NAMED_ERROR + command);
-        }
-        if(isLongerThanOne(command)) {
-            throw new IllegalArgumentException(TOO_LONG_COMMAND_ERROR);
-        }
-        return commandToMenuMap.get(command);
-    }
 }
--- src/main/java/org/prgrms/assignment/voucher/model/VoucherType.java
+++ src/main/java/org/prgrms/assignment/voucher/model/VoucherType.java
@@ -4,42 +4,19 @@ import java.util.HashMap;
 import java.util.Map;
 
 public enum VoucherType {
-    FIXED(1, "FixedAmountVoucher", "Type your discount amount"),
 
-    PERCENT(2, "PercentDiscountVoucher", "Type your discount percent");
+    FIXED( "FixedAmountVoucher"),
 
-    VoucherType(int voucherTypeNum, String voucherTypeName, String benefitMessage) {
-        this.voucherTypeNum = voucherTypeNum;
+    PERCENT("PercentDiscountVoucher");
+
+    VoucherType(String voucherTypeName) {
         this.voucherTypeName = voucherTypeName;
-        this.benefitMessage = benefitMessage;
     }
-
-    private final int voucherTypeNum;
 
     private final String voucherTypeName;
-
-    private final String benefitMessage;
-
-    private static final Map<Integer, VoucherType> voucherTypeNameMap = new HashMap<>();
-    static {
-        for(VoucherType voucherType : VoucherType.values()) {
-            voucherTypeNameMap.put(voucherType.voucherTypeNum, voucherType);
-        }
-    }
-
-    public String getVoucherTypeName() {
-        return voucherTypeName;
-    }
-
-    public static VoucherType of(Integer voucherTypeNum) {
-        return voucherTypeNameMap.get(voucherTypeNum);
-    }
 
     public static VoucherType of(String voucherTypeName) {
         return VoucherType.valueOf(voucherTypeName.toUpperCase());
     }
 
-    public String getBenefitMessage() {
-        return benefitMessage;
-    }
 }
