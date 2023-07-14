a6f365a25540d1b7021af5983dcddc11847072e5
sangminnim <sangmins930@gmail.com>
2023-07-15 02:26:43 +0900

    feat: CustomerMapper 생성

---
+++ src/main/java/com/programmers/springbootbasic/service/CustomerMapper.java
@@ -0,0 +1,25 @@
+package com.programmers.springbootbasic.service;
+
+import com.programmers.springbootbasic.domain.customer.Customer;
+import com.programmers.springbootbasic.service.dto.Customer.CustomerCreationRequest;
+import com.programmers.springbootbasic.service.dto.Customer.CustomerResponse;
+import com.programmers.springbootbasic.service.dto.Customer.CustomerUpdateRequest;
+
+import java.util.UUID;
+
+public final class CustomerMapper {
+    private CustomerMapper() {
+    }
+
+    public static Customer toCustomer(CustomerCreationRequest request) {
+        return new Customer(UUID.randomUUID(), request.email(), request.name());
+    }
+
+    public static Customer toCustomer(CustomerUpdateRequest request) {
+        return new Customer(UUID.randomUUID(), request.email(), request.name());
+    }
+
+    public static CustomerResponse toCustomerResponse(Customer customer) {
+        return new CustomerResponse(customer.getName(), customer.getEmail());
+    }
+}
