7d2d389c24eac3df28cac735f49e5d459cd2a006
changhyeon <changhyeon.h@kakao.com>
2023-07-14 18:58:48 +0900

    refactor: Exception 명시적으로 세분화

--- src/main/java/com/programmers/springweekly/ConsoleApplication.java
+++ src/main/java/com/programmers/springweekly/ConsoleApplication.java
@@ -43,6 +43,8 @@ public class ConsoleApplication implements CommandLineRunner {
                 console.outputErrorMessage("입력 값이 잘못되었습니다. 자세한 메시지 : " + e.getMessage());
             } catch (NoSuchElementException e) {
                 console.outputErrorMessage("찾는 값이 존재하지 않습니다. 자세한 메시지 : " + e.getMessage());
+            } catch (IndexOutOfBoundsException e) {
+                console.outputErrorMessage("파일안 내용의 열의 개수가 잘못되었거나 불러올 수 없는 값을 읽어들였습니다. 자세한 메시지 :  " + e.getMessage());
             } catch (Exception e) {
                 console.outputErrorMessage("알 수 없는 에러가 발생되었습니다, 에러 메시지 : " + e.getMessage());
                 log.error("콘솔에서 알 수 없는 에러를 잡았습니다." + e);
