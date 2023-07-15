39bdc7c75749be298dec971ab8cf9f4da3e31ff8
KimMinheee <99alsgmlalsl@naver.com>
2023-07-15 18:12:16 +0900

    [Refactor] : 사용하지 않는 필드 및 메소드 삭제

--- src/main/java/org/programmers/VoucherManagement/io/MenuType.java
+++ src/main/java/org/programmers/VoucherManagement/io/MenuType.java
@@ -12,44 +12,35 @@ import static org.programmers.VoucherManagement.voucher.exception.VoucherExcepti
 
 public enum MenuType {
     //Member
-    INSERT_MEMBER("insertmember", 1),
-    UPDATE_MEMBER("updatemember", 2),
-    DELETE_MEMBER("deletemember", 3),
-    BLACK_MEMBER_LIST("blackmemberlist", 4),
-    MEMBER_LIST("memberlist", 5),
-
+    INSERT_MEMBER(1),
+    UPDATE_MEMBER(2),
+    DELETE_MEMBER(3),
+    BLACK_MEMBER_LIST(4),
+    MEMBER_LIST(5),
 
     //Voucher
-    INSERT_VOUCHER("insertvoucher", 6),
-    UPDATE_VOUCHER("updatevoucher", 7),
-    DELETE_VOUCHER("deletevoucher", 8),
-    VOUCHER_LIST("voucherlist", 9),
+    INSERT_VOUCHER(6),
+    UPDATE_VOUCHER(7),
+    DELETE_VOUCHER(8),
+    VOUCHER_LIST(9),
 
     //System
-    EXIT("exit", 10);
-
+    EXIT(10);
 
     private static final Map<Integer, MenuType> COMMAND_TYPE_MAP =
             Collections.unmodifiableMap(Arrays
                     .stream(values())
                     .collect(Collectors.toMap(MenuType::getNumber, Function.identity())));
 
-    private final String type;
     private final int number;
 
-    MenuType(String type, int number) {
-        this.type = type;
+    MenuType(int number) {
         this.number = number;
-    }
-
-    private String getType() {
-        return type;
     }
 
     private int getNumber() {
         return number;
     }
-
 
     public static MenuType from(int type) {
         if (!COMMAND_TYPE_MAP.containsKey(type)) {
