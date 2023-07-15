e26ce08e54ffe331199a2330e7de12d619913c66
seongHyun-Min <steve6133@naver.com>
2023-07-16 02:59:31 +0900

    feat : CommandLineRunner 구현 (추가)

--- src/main/java/org/promgrammers/voucher/VoucherApplication.java
+++ src/main/java/org/promgrammers/voucher/VoucherApplication.java
@@ -1,17 +1,33 @@
 package org.promgrammers.voucher;
 
+import lombok.RequiredArgsConstructor;
+import org.promgrammers.voucher.controller.CustomerController;
 import org.promgrammers.voucher.controller.VoucherController;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Scanner;
+
 @SpringBootApplication
+@RequiredArgsConstructor
 public class VoucherApplication implements CommandLineRunner {
 
-    private final VoucherController controller;
+    private final VoucherController voucherController;
+    private final CustomerController customerController;
 
-    public VoucherApplication(VoucherController controller) {
-        this.controller = controller;
+    private void execute(String command) {
+        switch (command) {
+            case "VOUCHER":
+                voucherController.userPostController();
+                break;
+            case "CUSTOMER":
+                customerController.userPostController();
+                break;
+            default:
+                System.out.println("Invalid command: " + command);
+                break;
+        }
     }
 
     public static void main(String[] args) {
@@ -20,6 +36,10 @@ public class VoucherApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        controller.userPostController();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the command (VOUCHER or CUSTOMER):");
+        String command = scanner.nextLine().toUpperCase();
+        execute(command);
     }
 }
+
