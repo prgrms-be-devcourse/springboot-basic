2eece502549448c64093cf0fd353cf33de64b937
KimMinheee <99alsgmlalsl@naver.com>
2023-07-16 03:51:01 +0900

    [Refactor] : Javadoc 이용해 주석 달기

--- src/main/java/org/programmers/VoucherManagement/member/infrastructure/MemberRepository.java
+++ src/main/java/org/programmers/VoucherManagement/member/infrastructure/MemberRepository.java
@@ -7,17 +7,47 @@ import java.util.Optional;
 import java.util.UUID;
 
 public interface MemberRepository {
+    /**
+     * 전체 회원 조회
+     *
+     * @return List<Member> - 등록된 전체 회원
+     */
     List<Member> findAll();
 
+    /**
+     * MemberStatus에 따른 회원 조회
+     *
+     * @return List<Member> - status에 해당하는 전체 회원
+     */
     List<Member> findAllByMemberStatus();
 
-    Optional<Member> findById(UUID MemberId); //조회
+    /**
+     * 회원 ID를 이용해 회원 조회
+     *
+     * @param memberId
+     * @return Optional<Member> - memberId값을 이용해 조회한 회원
+     */
+    Optional<Member> findById(UUID memberId); //조회
 
+    /**
+     * db에 회원 저장
+     *
+     * @param member
+     * @return Member - 저장 완료한 회원
+     */
     Member insert(Member member); //저장
 
+    /**
+     * db에 저장된 회원 수정
+     *
+     * @param member
+     */
     void update(Member member); //수정
 
+    /**
+     * db에 저장된 회원 삭제
+     *
+     * @param member
+     */
     void delete(Member member); //삭제
-
-    void deleteAll();  //전체 삭제
 }
--- src/main/java/org/programmers/VoucherManagement/voucher/infrastructure/VoucherRepository.java
+++ src/main/java/org/programmers/VoucherManagement/voucher/infrastructure/VoucherRepository.java
@@ -7,15 +7,40 @@ import java.util.Optional;
 import java.util.UUID;
 
 public interface VoucherRepository {
+    /**
+     * db에 바우처 저장
+     *
+     * @param voucher
+     * @return Voucher - db에 저장한 voucher
+     */
     Voucher insert(Voucher voucher); //저장
 
+    /**
+     * db에 저장된 바우처 정보 수정
+     *
+     * @param voucher
+     */
     void update(Voucher voucher); //수정
 
+    /**
+     * db에 저장된 바우처 삭제
+     *
+     * @param voucher
+     */
     void delete(Voucher voucher); //삭제
 
-    void deleteAll(); //전체 삭제
-
+    /**
+     * db에 저장된 전체 바우처 조회
+     *
+     * @return List<Voucher> - 등록된 전체 바우처
+     */
     List<Voucher> findAll(); //전체 조회
 
+    /**
+     * voucherId를 이용해 바우처 조회
+     *
+     * @param voucherId
+     * @return Optional<Voucher> - voucherId를 이용해 조회한 바우처
+     */
     Optional<Voucher> findById(UUID voucherId); //조회
 }
