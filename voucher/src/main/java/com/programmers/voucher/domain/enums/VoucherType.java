58d114f27b961dec8c9495bedd1b7d6fce76212f
BeomSeogKim <kbs4520@daum.net>
2023-07-15 14:31:15 +0900

    JdbcVoucherStream => String 대신 enum 활용

--- voucher/src/main/java/com/programmers/voucher/domain/enums/VoucherType.java
+++ voucher/src/main/java/com/programmers/voucher/domain/enums/VoucherType.java
@@ -4,17 +4,25 @@ import java.util.Arrays;
 import java.util.Optional;
 
 public enum VoucherType {
-    FIXED(1), PERCENT(2);
+    FIXED(1, "FixedAmountVoucher"), PERCENT(2, "PercentDiscountVoucher");
 
     private final int number;
+    private final String stringValue;
 
-    VoucherType(int number) {
+    VoucherType(int number, String stringValue) {
         this.number = number;
+        this.stringValue = stringValue;
     }
 
     public static Optional<VoucherType> decideVoucherType(int number) {
         return Arrays.stream(VoucherType.values())
                 .filter(v -> v.number == number)
+                .findAny();
+    }
+
+    public static Optional<VoucherType> decideVoucherType(String stringValue) {
+        return Arrays.stream(VoucherType.values())
+                .filter(v -> v.stringValue.equals(stringValue))
                 .findAny();
     }
 }
