13d2f13bae6cb1c333dada88c0792e2a0283cce6
KimMinheee <99alsgmlalsl@naver.com>
2023-07-16 02:44:09 +0900

    [Refactor] : DiscountTypeTest given/when/then 추가

--- src/test/java/org/programmers/VoucherManagement/voucher/domain/DiscountTypeTest.java
+++ src/test/java/org/programmers/VoucherManagement/voucher/domain/DiscountTypeTest.java
@@ -11,11 +11,15 @@ public class DiscountTypeTest {
     @Test
     @DisplayName("입력값에 따라 DiscountType을 반환하는 테스트 - 성공")
     void from_InputTypeString_EqualsDiscountType() {
+        //given
         String fixedInput = "fixed";
         String percentInput = "percent";
 
+        //when
         DiscountType fixedTypeExpect = DiscountType.from(fixedInput);
         DiscountType percentTypeExpect = DiscountType.from(percentInput);
+
+        //then
         assertThat(fixedTypeExpect).isEqualTo(DiscountType.FIXED);
         assertThat(percentTypeExpect).isEqualTo(DiscountType.PERCENT);
     }
@@ -23,8 +27,10 @@ public class DiscountTypeTest {
     @Test
     @DisplayName("입력값에 따라 DiscountType을 반환하는 테스트 - 실패")
     void from_InputTypeString_ThrowVoucherException() {
+        //given
         String input = "fixed123";
 
+        //when&then
         assertThatThrownBy(() -> DiscountType.from(input))
                 .isInstanceOf(VoucherException.class)
                 .hasMessage("해당하는 유형의 바우처가 존재하지 않습니다.");
