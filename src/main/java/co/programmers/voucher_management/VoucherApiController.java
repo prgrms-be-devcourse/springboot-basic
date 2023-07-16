05c3629e30f1f0da5ce13031af19c0d17e8a6d58
SW-H <hsw08@naver.com>
2023-07-16 19:48:49 +0900

    feat : API의 URI 수정

--- src/main/java/co/programmers/voucher_management/VoucherApiController.java
+++ src/main/java/co/programmers/voucher_management/VoucherApiController.java
@@ -1,11 +1,11 @@
 package co.programmers.voucher_management;
 
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
 
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,7 +19,6 @@ import co.programmers.voucher_management.voucher.dto.VoucherResponseDTO;
 import co.programmers.voucher_management.voucher.service.VoucherService;
 
 @RequestMapping("/api/v1/vouchers")
-@Controller
 @RestController
 public class VoucherApiController {
 	private final VoucherService voucherService;
@@ -28,42 +27,50 @@ public class VoucherApiController {
 		this.voucherService = voucherService;
 	}
 
-	@GetMapping("/all")
+	@GetMapping
+	public ResponseEntity<List<VoucherResponseDTO>> find(
+			@RequestParam(required = false) String type,
+			@RequestParam(required = false, defaultValue = "-1") Long id
+	) {
+		if (id > 0) {
+			return findById(id);
+		}
+		if (type != null) {
+			return findByType(type);
+		}
+		return findAll();
+	}
+
 	public ResponseEntity<List<VoucherResponseDTO>> findAll() {
 		List<VoucherResponseDTO> response = voucherService.inquiryVoucherOf();
 		return ResponseEntity.ok(response);
 	}
 
-	@GetMapping("/{id}")
-	public ResponseEntity<VoucherResponseDTO> findById(@PathVariable long id) {
+	private ResponseEntity<List<VoucherResponseDTO>> findById(@PathVariable long id) {
 		VoucherResponseDTO response = voucherService.findById(id);
-		return ResponseEntity.ok(response);
+		return ResponseEntity.ok(List.of(response));
 	}
 
-	@GetMapping()
-	public ResponseEntity<List<VoucherResponseDTO>> findByType(@RequestParam String discountType) {
+	private ResponseEntity<List<VoucherResponseDTO>> findByType(@RequestParam String discountType) {
 		List<VoucherResponseDTO> response = voucherService.findByType(discountType);
 		return ResponseEntity.ok(response);
 	}
 
-	@GetMapping("/search")
-	public ResponseEntity<List<VoucherResponseDTO>> findByDate(@RequestParam String startDate,
-			@RequestParam String endDate) {
-		DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-		LocalDate startLocalDate = LocalDate.parse(startDate, dateTimeFormatter);
-		LocalDate endLocalDate = LocalDate.parse(endDate, dateTimeFormatter);
-
-		List<VoucherResponseDTO> response = voucherService.findByDate(startLocalDate, endLocalDate);
+	@GetMapping(params = {"startDate", "endDate"})
+	public ResponseEntity<List<VoucherResponseDTO>> findByDate(
+			@RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate startDate,
+			@RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate endDate) {
+		List<VoucherResponseDTO> response = voucherService.findByDate(startDate, endDate);
 		return ResponseEntity.ok(response);
 	}
 
-	@PostMapping()
+	@PostMapping
 	public ResponseEntity<VoucherResponseDTO> create(@RequestBody VoucherRequestDTO voucherRequestDTO) {
 		VoucherResponseDTO response = voucherService.create(voucherRequestDTO);
 		return ResponseEntity.ok(response);
 	}
 
-	@PostMapping(value = "/{id}/delete")
+	@DeleteMapping("/{id}")
 	public ResponseEntity deleteById(@PathVariable long id) {
 		voucherService.deleteById(id);
 		return ResponseEntity.ok("");
