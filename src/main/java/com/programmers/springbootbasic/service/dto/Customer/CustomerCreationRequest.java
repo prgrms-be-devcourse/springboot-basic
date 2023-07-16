dd6855f53e0c8a21655db77c3a0c307f76a6287b
sangminnim <sangmins930@gmail.com>
2023-07-16 17:25:58 +0900

    chore: 질문 삭제

--- src/main/java/com/programmers/springbootbasic/service/dto/Customer/CustomerCreationRequest.java
+++ src/main/java/com/programmers/springbootbasic/service/dto/Customer/CustomerCreationRequest.java
@@ -1,9 +1,6 @@
 package com.programmers.springbootbasic.service.dto.Customer;
 
-/*
-    사실 상 dto 내부는 다 똑같은데 class 이름 가진 의미가 있다고 생각해서 3개 만들었는데
-    어떻게 생각하시나요?
- */
+₩
 public record CustomerCreationRequest(
         String name,
         String email
