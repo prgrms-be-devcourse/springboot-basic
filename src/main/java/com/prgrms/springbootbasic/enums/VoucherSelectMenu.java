b591334c9173415cf539a3f9f70f98fa3ead46df
jay-so <merry9504@naver.com>
2023-07-16 17:22:56 +0900

    feat: Voucher 조회 메뉴(all, type, ID, createdAt) enum 생성

---
+++ src/main/java/com/prgrms/springbootbasic/enums/VoucherSelectMenu.java
@@ -0,0 +1,17 @@
+package com.prgrms.springbootbasic.enums;
+
+//모든 바우처를 조회하려면 All,타입별로 조회를 하려면 Type, ID로 조회하려면 ID, 생성일로 조회하려면 CreateAt을 입력해주세요
+public enum VoucherSelectMenu {
+    ID,
+    TYPE,
+    CREATEAT,
+    ALL;
+
+    public static VoucherSelectMenu of(String readMenu) {
+        try {
+            return valueOf(readMenu.toUpperCase());
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("잘못된 명령어를 입력하셨습니다! 실행하고자 하는 바우처 명령어를 다시 확인 해주세요!");
+        }
+    }
+}
