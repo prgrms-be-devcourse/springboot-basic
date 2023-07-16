09c583ed4d51349ffbfc2d4453c006c226d42d6f
2jie0516 <2jie0516>
2023-07-16 16:25:32 +0900

    email 클래스에서 checkaddress 메소드 네이밍 변경 및 문자열 체크 메소드 분리 (각 메소드에서 static 키워드 제거)

--- src/main/java/prgms/spring_week1/domain/customer/model/embeddedType/Email.java
+++ src/main/java/prgms/spring_week1/domain/customer/model/embeddedType/Email.java
@@ -9,12 +9,16 @@ public class Email {
 
     public Email(String address) {
         Assert.notNull(address, "이메일은 필수 입력값입니다.");
-        Assert.isTrue(address.length() >= 4 && address.length() <= 50, "이메일은 4자 이상 50자 이하여야합니다.");
-        Assert.isTrue(checkAddress(address), "이메일 형식이 올바르지 않습니다.");
+        Assert.isTrue(checkAddressLength(address), "이메일은 4자 이상 50자 이하여야합니다.");
+        Assert.isTrue(checkAddressFormat(address), "이메일 형식이 올바르지 않습니다.");
         this.address = address;
     }
 
-    private static boolean checkAddress(String address) {
+    private boolean checkAddressLength(String address) {
+        return address.length() >= 4 && address.length() <= 50;
+    }
+
+    private boolean checkAddressFormat(String address) {
         return Pattern.matches("\\b[\\w\\.-]+@[\\w\\.-]+\\.\\w{2,4}\\b", address);
     }
 
