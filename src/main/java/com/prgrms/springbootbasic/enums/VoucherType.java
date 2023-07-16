c938213471a0e5b1561410cd29475273572bdf04
jay-so <merry9504@naver.com>
2023-07-16 17:18:57 +0900

    refactor: VoucherType refactoring

--- src/main/java/com/prgrms/springbootbasic/enums/VoucherType.java
+++ src/main/java/com/prgrms/springbootbasic/enums/VoucherType.java
@@ -4,12 +4,11 @@ public enum VoucherType {
     FIXED,
     RATE;
 
-
     public static VoucherType of(String voucherType) {
         try {
             return VoucherType.valueOf(voucherType.toUpperCase());
         } catch (IllegalArgumentException e) {
-            throw new IllegalArgumentException("잘못된 바우처 타입을 입력하셨습니다. 고정 할인 바우처(fixed) 또는 비율 할인 바우처(percent)를 선택해서 다시 입력해주세요!");
+            throw new IllegalArgumentException("잘못된 바우처 타입을 입력하셨습니다. 고정 할인 바우처(Fixed) 또는 비율 할인 바우처(Rate)를 선택해서 다시 입력해주세요!");
         }
     }
 }
