7226b2f4f5f7277eb32cadbfd7bfebe8911b8b13
KimMinheee <99alsgmlalsl@naver.com>
2023-07-17 18:10:44 +0900

    [Refractor] : FileInfo 내 setter 삭제

--- src/main/java/org/programmers/VoucherManagement/global/file/FileInfo.java
+++ src/main/java/org/programmers/VoucherManagement/global/file/FileInfo.java
@@ -1,18 +1,16 @@
 package org.programmers.VoucherManagement.global.file;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
 
-@Component
 @ConfigurationProperties(prefix = "app.path")
 public class FileInfo {
-    private String filePath;
+    private final String filePath;
+
+    public FileInfo(String filePath) {
+        this.filePath = filePath;
+    }
 
     public String getFilePath() {
         return filePath;
-    }
-
-    public void setFilePath(String filePath) {
-        this.filePath = filePath;
     }
 }
