734e7bf1c1bd9393d1fae8e463d7f6370cdb28f2
Curry4182 <kangbk4182@naver.com>
2023-07-17 21:03:05 +0900

    feat: voucher_id로 바우처 조회 api 기능 추가

--- src/main/java/org/prgrms/kdt/controller/VoucherAPIController.java
+++ src/main/java/org/prgrms/kdt/controller/VoucherAPIController.java
@@ -1,15 +1,19 @@
 package org.prgrms.kdt.controller;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.prgrms.kdt.enums.VoucherType;
 import org.prgrms.kdt.model.dto.VoucherDTO;
+import org.prgrms.kdt.model.entity.VoucherEntity;
 import org.prgrms.kdt.service.VoucherService;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -26,7 +30,7 @@ public class VoucherAPIController {
 		return voucherService.getVouchers();
 	}
 
-	@GetMapping("/api/v1/vouchers/{voucherTypeIdx}")
+	@GetMapping("/api/v1/vouchers/voucher-type/{voucherTypeIdx}")
 	@ResponseBody
 	public List<VoucherDTO> findVouchers(@PathVariable("voucherTypeIdx")int voucherTypeIdx, Model model) {
 		VoucherType targetVoucherType = VoucherType.valueOf(voucherTypeIdx);
@@ -34,5 +38,11 @@ public class VoucherAPIController {
 			.stream()
 			.filter(voucherDTO -> voucherDTO.getVoucherType() == targetVoucherType)
 			.collect(Collectors.toList());
+	}
+
+	@GetMapping("/api/v1/vouchers/{voucherId}")
+	@ResponseBody
+	public VoucherDTO findVoucherById(@PathVariable("voucherId")Long voucherId, Model model) {
+		return voucherService.findVoucherById(voucherId).orElse(null);
 	}
 }
