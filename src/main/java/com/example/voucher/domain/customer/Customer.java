f4bdfcc2297fe46003981af64b1489e26ac10e6e
Juwoong <wwong7247@gmail.com>
2023-07-17 21:59:54 +0900

    고객 도메인 추가 및 테이블 생성

---
+++ src/main/java/com/example/voucher/domain/customer/Customer.java
@@ -0,0 +1,51 @@
+package com.example.voucher.domain.customer;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+import com.example.voucher.constant.CustomerType;
+
+public class Customer {
+
+    private final UUID customerId;
+    private String name;
+    private final String email;
+    private final CustomerType customerType;
+    private final LocalDateTime createdAt;
+
+    public Customer(String name, String email, CustomerType customerType) {
+        this.customerId = UUID.randomUUID();
+        this.name = name;
+        this.email = email;
+        this.customerType = customerType;
+        this.createdAt = LocalDateTime.now();
+    }
+
+    public Customer(UUID customerId, String name, String email, CustomerType customerType, LocalDateTime createdAt) {
+        this.customerId = customerId;
+        this.name = name;
+        this.email = email;
+        this.customerType = customerType;
+        this.createdAt = createdAt;
+    }
+
+    public UUID getCustomerId() {
+        return customerId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public CustomerType getCustomerType() {
+        return customerType;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+}
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -7,3 +7,16 @@ CREATE TABLE voucher
     voucher_type   VARCHAR(200) NOT NULL,
     PRIMARY KEY (voucher_id)
 );
+
+
+DROP TABLE IF EXISTS customer;
+
+create table customer
+(
+    customer_id    VARCHAR(200) NOT NULL,
+    customer_name  VARCHAR(200) NOT NULL,
+    customer_email VARCHAR(200) NOT NULL,
+    customer_type  VARCHAR(200) NOT NULL,
+    created_at     TIMESTAMP    NOT NULL,
+    PRIMARY KEY (customer_id)
+);
