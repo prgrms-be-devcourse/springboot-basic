a66bf7303bafef8a2760973c331a7464bdd1c0e3
PHJ <legowww@gmail.com>
2023-07-17 12:29:40 +0900

    feat: CORS 설정 추가

--- src/main/java/com/dev/voucherproject/controller/web/config/WebConfig.java
+++ src/main/java/com/dev/voucherproject/controller/web/config/WebConfig.java
@@ -5,11 +5,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import org.springframework.web.filter.HiddenHttpMethodFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry
+                .addMapping("/**")
+                .allowedOrigins("http://localhost:3000");
+    }
     @Bean
     public HiddenHttpMethodFilter hiddenHttpMethodFilter(){
         return new HiddenHttpMethodFilter();
