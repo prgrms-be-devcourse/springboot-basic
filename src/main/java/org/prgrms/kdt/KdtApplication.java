48aabc1bdec0974cbeb4d1924d3e1e21a6d10a23
Curry4182 <kangbk4182@naver.com>
2023-07-17 20:41:12 +0900

    feat: spring boot로 내장 톰켓이 실행되도록 전환

--- src/main/java/org/prgrms/kdt/KdtApplication.java
+++ src/main/java/org/prgrms/kdt/KdtApplication.java
@@ -2,6 +2,12 @@ package org.prgrms.kdt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
 
 @SpringBootApplication
 public class KdtApplication {
