7bd6258bc0f0d2a66c74d87cf6531e6b52862019
kylekim2123 <kyle2123@naver.com>
2023-07-18 14:36:23 +0900

    feat: Thymeleaf를 이용한 Voucher Create 기능 추가

--- src/main/java/com/devcourse/voucherapp/controller/web/VoucherController.java
+++ src/main/java/com/devcourse/voucherapp/controller/web/VoucherController.java
@@ -1,10 +1,32 @@
 package com.devcourse.voucherapp.controller.web;
 
+import com.devcourse.voucherapp.entity.voucher.dto.VoucherCreateRequestDto;
+import com.devcourse.voucherapp.service.VoucherService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequiredArgsConstructor
+@RequestMapping("/vouchers")
 @Profile("dev")
 public class VoucherController {
 
+    private final VoucherService voucherService;
+
+    @GetMapping("/create")
+    public String getCreateForm() {
+        return "voucher/createForm";
+    }
+
+    @PostMapping("/create")
+    public String create(VoucherCreateRequestDto request, Model model) {
+        voucherService.create(request);
+
+        return "voucher/vouchers";
+    }
 }
