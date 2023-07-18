3ddd5bdbe87f7be9364c7d0738b1c2232fe74ab2
kylekim2123 <kyle2123@naver.com>
2023-07-18 23:06:43 +0900

    feat: Thymeleaf를 이용한 Customer Create 기능 추가

--- src/main/java/com/devcourse/voucherapp/controller/web/CustomerController.java
+++ src/main/java/com/devcourse/voucherapp/controller/web/CustomerController.java
@@ -1,10 +1,32 @@
 package com.devcourse.voucherapp.controller.web;
 
+import com.devcourse.voucherapp.entity.customer.dto.CustomerCreateRequestDto;
+import com.devcourse.voucherapp.service.CustomerService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequiredArgsConstructor
+@RequestMapping("/customers")
 @Profile("dev")
 public class CustomerController {
 
+    private final CustomerService customerService;
+
+    @GetMapping("/create")
+    public String getCreateForm() {
+        return "customer/createForm";
+    }
+
+    @PostMapping("/create")
+    public String create(@ModelAttribute CustomerCreateRequestDto request) {
+        customerService.create(request);
+
+        return "redirect:/customers";
+    }
 }
