727ab00853eb855bd46d522ad02403d7c2d272af
kylekim2123 <kyle2123@naver.com>
2023-07-18 23:11:31 +0900

    feat: Thymeleaf를 이용한 Customer Read All 기능 추가

--- src/main/java/com/devcourse/voucherapp/controller/web/CustomerController.java
+++ src/main/java/com/devcourse/voucherapp/controller/web/CustomerController.java
@@ -1,10 +1,13 @@
 package com.devcourse.voucherapp.controller.web;
 
 import com.devcourse.voucherapp.entity.customer.dto.CustomerCreateRequestDto;
+import com.devcourse.voucherapp.entity.customer.dto.CustomerResponseDto;
 import com.devcourse.voucherapp.service.CustomerService;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -28,5 +31,13 @@ public class CustomerController {
         customerService.create(request);
 
         return "redirect:/customers";
+    }
+
+    @GetMapping
+    public String findAllCustomers(Model model) {
+        List<CustomerResponseDto> customers = customerService.findAllCustomers();
+        model.addAttribute("customers", customers);
+
+        return "customer/customers";
     }
 }
