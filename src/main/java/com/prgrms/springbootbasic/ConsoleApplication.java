1ac32b06389bc4df38c5bfae16728b855c887445
jay-so <merry9504@naver.com>
2023-07-18 22:39:06 +0900

    feat: Profile 설정 추가

--- src/main/java/com/prgrms/springbootbasic/ConsoleApplication.java
+++ src/main/java/com/prgrms/springbootbasic/ConsoleApplication.java
@@ -5,8 +5,10 @@ import com.prgrms.springbootbasic.view.Console;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
+@Profile("!test")
 @Slf4j
 @Component
 @RequiredArgsConstructor
