8342a4481b02b1732ab37d1509cb0dbb3ff8c751
2jie0516 <2jie0516>
2023-07-19 15:51:40 +0900

    email 클래스에서 Assert를 사용하지 않고 예외 발생

--- src/main/java/prgms/spring_week1/domain/customer/model/embeddedType/Email.java
+++ src/main/java/prgms/spring_week1/domain/customer/model/embeddedType/Email.java
@@ -8,18 +8,32 @@ public class Email {
     private final String address;
 
     public Email(String address) {
-        Assert.notNull(address, "이메일은 필수 입력값입니다.");
-        Assert.isTrue(checkAddressLength(address), "이메일은 4자 이상 50자 이하여야합니다.");
-        Assert.isTrue(checkAddressFormat(address), "이메일 형식이 올바르지 않습니다.");
+        checkAddress(address);
         this.address = address;
     }
 
-    private boolean checkAddressLength(String address) {
-        return address.length() >= 4 && address.length() <= 50;
+    private void checkAddress(String address){
+        checkAddressIsNull(address);
+        checkAddressLength(address);
+        checkAddressFormat(address);
     }
 
-    private boolean checkAddressFormat(String address) {
-        return Pattern.matches("\\b[\\w\\.-]+@[\\w\\.-]+\\.\\w{2,4}\\b", address);
+    private void checkAddressIsNull(String address) {
+        if (address == null) {
+            throw new IllegalArgumentException("이메일은 필수 입력값입니다.");
+        }
+    }
+
+    private void checkAddressLength(String address) {
+        if (address.length() >= 4 && address.length() <= 50) {
+            throw new IllegalArgumentException("이메일은 4자 이상 50자 이하여야합니다.");
+        }
+    }
+
+    private void checkAddressFormat(String address) {
+        if (Pattern.matches("\\b[\\w\\.-]+@[\\w\\.-]+\\.\\w{2,4}\\b", address)) {
+            throw new IllegalArgumentException("이메일 형식이 올바르지 않습니다.");
+        }
     }
 
     public String getAddress() {
