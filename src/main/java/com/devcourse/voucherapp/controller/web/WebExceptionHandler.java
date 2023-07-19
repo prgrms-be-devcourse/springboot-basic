15eb6c586ea7b7896e25593def0f2505980513a1
kylekim2123 <kyle2123@naver.com>
2023-07-19 17:13:27 +0900

    fix: 405 Method Not Allowed에 대한 처리 추가

--- src/main/java/com/devcourse/voucherapp/controller/web/WebExceptionHandler.java
+++ src/main/java/com/devcourse/voucherapp/controller/web/WebExceptionHandler.java
@@ -2,14 +2,18 @@ package com.devcourse.voucherapp.controller.web;
 
 import static com.devcourse.voucherapp.entity.HomeMenu.CUSTOMER;
 import static com.devcourse.voucherapp.entity.HomeMenu.VOUCHER;
-import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+import static com.devcourse.voucherapp.exception.ExceptionRule.NOT_ALLOWED_HTTP_METHOD;
+import static com.devcourse.voucherapp.exception.ExceptionRule.UNEXPECTED_ERROR;
 
 import com.devcourse.voucherapp.entity.HomeMenu;
 import com.devcourse.voucherapp.exception.BusinessException;
 import com.devcourse.voucherapp.exception.CustomerException;
+import com.devcourse.voucherapp.exception.ExceptionRule;
 import com.devcourse.voucherapp.exception.VoucherException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.ui.Model;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -19,42 +23,58 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ControllerAdvice
 public class WebExceptionHandler {
 
-    private static final String UNEXPECTED_EXCEPTION_MESSAGE = "알 수 없는 에러가 발생했습니다. 이용에 불편함을 드려 죄송합니다.";
-    private static final String CUSTOM_ERROR_PAGE = "error/customError";
+    private static final String ERROR_PAGE = "error/error";
 
     @ExceptionHandler(VoucherException.class)
     public String handleVoucherException(@ModelAttribute VoucherException e, Model model) {
-        logError(e, VOUCHER);
-        setExceptionAttributes(e, model);
+        logBusinessError(e, VOUCHER);
+        setBusinessErrorAttributes(e, model);
 
-        return CUSTOM_ERROR_PAGE;
+        return ERROR_PAGE;
     }
 
     @ExceptionHandler(CustomerException.class)
     public String handleCustomerException(@ModelAttribute CustomerException e, Model model) {
-        logError(e, CUSTOMER);
-        setExceptionAttributes(e, model);
+        logBusinessError(e, CUSTOMER);
+        setBusinessErrorAttributes(e, model);
 
-        return CUSTOM_ERROR_PAGE;
+        return ERROR_PAGE;
+    }
+
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    @ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
+    public String handleHttpRequestMethodNotSupportedException(@ModelAttribute HttpRequestMethodNotSupportedException e, Model model) {
+        logError(e, NOT_ALLOWED_HTTP_METHOD);
+        setErrorAttributes(NOT_ALLOWED_HTTP_METHOD, model);
+
+        return ERROR_PAGE;
     }
 
     @ExceptionHandler(Exception.class)
     @ResponseStatus
-    public String handleUnexpectedException(@ModelAttribute Exception e, Model model) {
-        log.error("원인 불명의 예외 발생 - {} | '{}'", INTERNAL_SERVER_ERROR, e.getMessage(), e);
-        model.addAttribute("status", INTERNAL_SERVER_ERROR);
-        model.addAttribute("message", UNEXPECTED_EXCEPTION_MESSAGE);
+    public String handleException(@ModelAttribute Exception e, Model model) {
+        logError(e, UNEXPECTED_ERROR);
+        setErrorAttributes(UNEXPECTED_ERROR, model);
 
-        return CUSTOM_ERROR_PAGE;
+        return ERROR_PAGE;
     }
 
-    private void logError(BusinessException e, HomeMenu menu) {
-        log.error("{}에서 예외 발생 - {} | '{}' | 사용자 입력 : {}", menu.getName(), e.getStatus(), e.getMessage(), e.getCauseInput(), e);
+    private void logBusinessError(BusinessException e, HomeMenu menu) {
+        log.error("{} 발생 - {} | '{}' | 사용자 입력 : {}", e.getStatus(), menu.getName(), e.getMessage(), e.getCauseInput(), e);
     }
 
-    private void setExceptionAttributes(BusinessException e, Model model) {
+    private void logError(Exception e, ExceptionRule rule) {
+        log.error("{} 발생 - '{}'", rule.getStatus(), rule.getMessage(), e);
+    }
+
+    private void setBusinessErrorAttributes(BusinessException e, Model model) {
         model.addAttribute("status", e.getStatus());
         model.addAttribute("message", e.getMessage());
         model.addAttribute("causeInput", e.getCauseInput());
+    }
+
+    private void setErrorAttributes(ExceptionRule rule, Model model) {
+        model.addAttribute("status", rule.getStatus());
+        model.addAttribute("message", rule.getMessage());
     }
 }
