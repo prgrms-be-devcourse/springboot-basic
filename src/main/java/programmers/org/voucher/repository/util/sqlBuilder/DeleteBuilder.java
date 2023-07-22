c1ae71639172b0a717f52338e3bc7c6973b6147b
kys0411 <32180846@dankook.ac.kr>
2023-07-22 19:47:15 +0900

    DeleteBuilder 추가

---
+++ src/main/java/programmers/org/voucher/repository/util/sqlBuilder/DeleteBuilder.java
@@ -0,0 +1,29 @@
+package programmers.org.voucher.repository.util.sqlBuilder;
+
+import programmers.org.voucher.repository.util.statement.Delete;
+import programmers.org.voucher.repository.util.statement.Where;
+
+import static programmers.org.voucher.repository.util.constant.Symbol.BLANK;
+
+public class DeleteBuilder {
+    private static final String DELETE = "DELETE FROM";
+
+    private StringBuilder query = new StringBuilder();
+
+    public DeleteBuilder delete(Delete delete) {
+        query.append(DELETE)
+                .append(BLANK.getSymbol())
+                .append(delete.getTable());
+
+        return this;
+    }
+
+    public DeleteBuilder where(Where where) {
+        query.append(where.getQuery());
+        return this;
+    }
+
+    public String build() {
+        return query.toString();
+    }
+}
--- src/main/java/programmers/org/voucher/repository/util/statement/Delete.java
+++ src/main/java/programmers/org/voucher/repository/util/statement/Delete.java
@@ -2,31 +2,15 @@ package programmers.org.voucher.repository.util.statement;
 
 import programmers.org.voucher.repository.util.constant.Table;
 
-import static programmers.org.voucher.repository.util.constant.Symbol.BLANK;
+public class Delete {
 
-public class Delete extends Statement {
-    private static final String DELETE = "DELETE FROM";
+    private Table table;
 
-    public Delete(StringBuilder query) {
-        this.query = query;
+    public Delete(Table table) {
+        this.table = table;
     }
 
-    static public class Builder {
-        private StringBuilder query = new StringBuilder();
-
-        public Builder() {
-            query.append(DELETE);
-        }
-
-        public Builder query(Table table) {
-            query.append(BLANK.getSymbol())
-                    .append(table);
-
-            return this;
-        }
-
-        public Delete build() {
-            return new Delete(query);
-        }
+    public Table getTable() {
+        return table;
     }
 }
