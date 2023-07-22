8538ef99736894f204777cf2b1c19367b860307d
kys0411 <32180846@dankook.ac.kr>
2023-07-22 19:47:33 +0900

    UpdateBuilder 추가

--- src/main/java/programmers/org/voucher/repository/util/statement/Update.java
+++ src/main/java/programmers/org/voucher/repository/util/statement/Update.java
@@ -2,31 +2,15 @@ package programmers.org.voucher.repository.util.statement;
 
 import programmers.org.voucher.repository.util.constant.Table;
 
-import static programmers.org.voucher.repository.util.constant.Symbol.BLANK;
+public class Update {
 
-public class Update extends Statement {
-    private static final String UPDATE = "UPDATE";
+    private Table table;
 
-    public Update(StringBuilder query) {
-        this.query = query;
+    public Update(Table table) {
+        this.table = table;
     }
 
-    static public class Builder {
-        private StringBuilder query = new StringBuilder();
-
-        public Builder() {
-            query.append(UPDATE);
-        }
-
-        public Builder query(Table table) {
-            query.append(BLANK.getSymbol())
-                    .append(table);
-
-            return this;
-        }
-
-        public Update build() {
-            return new Update(query);
-        }
+    public Table getTable() {
+        return table;
     }
 }
