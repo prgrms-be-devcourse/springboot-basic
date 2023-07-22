4a281d6a0622bc1d1a6258b18a99fc9e21c231f1
kys0411 <32180846@dankook.ac.kr>
2023-07-22 18:36:38 +0900

    sql statement 클래스의 상위클래스인 Statement 추가

---
+++ src/main/java/programmers/org/voucher/repository/util/statement/Statement.java
@@ -0,0 +1,9 @@
+package programmers.org.voucher.repository.util.statement;
+
+abstract public class Statement {
+    StringBuilder query;
+
+    public StringBuilder getQuery() {
+        return query;
+    }
+}
