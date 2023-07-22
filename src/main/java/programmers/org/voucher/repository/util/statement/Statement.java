46b7a299914a64051092f770ab0300ef7192c56d
kys0411 <32180846@dankook.ac.kr>
2023-07-22 19:41:58 +0900

    불필요한 클래스 삭제

--- src/main/java/programmers/org/voucher/repository/util/statement/Statement.java
+++
@@ -1,9 +0,0 @@
-package programmers.org.voucher.repository.util.statement;
-
-abstract public class Statement {
-    StringBuilder query;
-
-    public StringBuilder getQuery() {
-        return query;
-    }
-}
