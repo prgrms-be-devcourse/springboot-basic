38ba569723d385570782a2c0db9d01f4c0c50e97
kys0411 <32180846@dankook.ac.kr>
2023-07-22 19:46:46 +0900

    selectBuilder 추가

---
+++ src/main/java/programmers/org/voucher/repository/util/sqlBuilder/SelectBuilder.java
@@ -0,0 +1,36 @@
+package programmers.org.voucher.repository.util.sqlBuilder;
+
+import programmers.org.voucher.repository.util.statement.From;
+import programmers.org.voucher.repository.util.statement.Select;
+import programmers.org.voucher.repository.util.statement.Where;
+
+import static programmers.org.voucher.repository.util.constant.Symbol.BLANK;
+
+public class SelectBuilder {
+    private static final String FROM = "FROM";
+
+    private StringBuilder query = new StringBuilder();
+
+    public SelectBuilder select(Select select) {
+        query.append(select.getQuery());
+        return this;
+    }
+
+    public SelectBuilder from(From from) {
+        query.append(BLANK.getSymbol())
+                .append(FROM)
+                .append(BLANK.getSymbol())
+                .append(from.getTable());
+
+        return this;
+    }
+
+    public SelectBuilder where(Where where) {
+        query.append(where.getQuery());
+        return this;
+    }
+
+    public String build() {
+        return query.toString();
+    }
+}
--- src/main/java/programmers/org/voucher/repository/util/statement/From.java
+++ src/main/java/programmers/org/voucher/repository/util/statement/From.java
@@ -2,29 +2,15 @@ package programmers.org.voucher.repository.util.statement;
 
 import programmers.org.voucher.repository.util.constant.Table;
 
-import static programmers.org.voucher.repository.util.constant.Symbol.BLANK;
+public class From {
 
-public class From extends Statement {
-    private static final String FROM = "FROM";
+    private Table table;
 
-    public From(StringBuilder query) {
-        this.query = query;
+    public From(Table table) {
+        this.table = table;
     }
 
-    static public class Builder {
-        private StringBuilder query = new StringBuilder();
-
-        public Builder() {
-            query.append(BLANK.getSymbol()).append(FROM);
-        }
-
-        public Builder query(Table table) {
-            query.append(BLANK.getSymbol()).append(table);
-            return this;
-        }
-
-        public From build() {
-            return new From(query);
-        }
+    public Table getTable() {
+        return table;
     }
 }
