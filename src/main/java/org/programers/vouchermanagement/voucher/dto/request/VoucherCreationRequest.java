c3bcba48ca4c32f2fece9dff4b972f9ec0f90d0a
player-geun <player_geun@naver.com>
2023-07-23 19:29:07 +0900

    fix : 값 범위에 대한 검증 로직에 따른 에러 해결

--- src/main/java/org/programers/vouchermanagement/voucher/dto/request/VoucherCreationRequest.java
+++ src/main/java/org/programers/vouchermanagement/voucher/dto/request/VoucherCreationRequest.java
@@ -3,7 +3,6 @@ package org.programers.vouchermanagement.voucher.dto.request;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import org.hibernate.validator.constraints.Range;
 import org.programers.vouchermanagement.voucher.domain.VoucherType;
 
 @AllArgsConstructor
@@ -13,7 +12,6 @@ public class VoucherCreationRequest {
     @NotNull
     private VoucherType type;
 
-    @Range(min = 1, max = 99)
     @NotNull
     private int value;
 }
