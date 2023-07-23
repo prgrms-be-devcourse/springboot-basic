9441b3168a0f8c96858047739f26198761b69571
hyee0715 <hyee0715@naver.com>
2023-07-23 20:38:26 +0900

    refactor: RequestMapping 슬래시 추가

--- src/main/java/com/programmers/springbootbasic/customer/controller/CustomerViewController.java
+++ src/main/java/com/programmers/springbootbasic/customer/controller/CustomerViewController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
-@RequestMapping("customer")
+@RequestMapping("/customer")
 @Controller
 public class CustomerViewController {
 
--- src/main/java/com/programmers/springbootbasic/voucher/controller/VoucherViewController.java
+++ src/main/java/com/programmers/springbootbasic/voucher/controller/VoucherViewController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
-@RequestMapping("voucher")
+@RequestMapping("/voucher")
 @Controller
 public class VoucherViewController {
 
