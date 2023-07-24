6e717412777635cdd73bdadc636766259d3c0d89
changhyeon <changhyeon.h@kakao.com>
2023-07-24 23:50:16 +0900

    refactor: errorController 형 변환 수정

--- src/main/java/com/programmers/springweekly/controller/CustomErrorController.java
+++ src/main/java/com/programmers/springweekly/controller/CustomErrorController.java
@@ -13,12 +13,12 @@ public class CustomErrorController implements ErrorController {
     // ControllerAdvice로 처리되지 않는 서블릿 수준의 예외와 404 에러 처리
     @GetMapping("/error")
     public String errorThrow(HttpServletRequest httpServletRequest, Model model) {
-        Object errorStatusCode = httpServletRequest.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
-        Object errorMessage = httpServletRequest.getAttribute(RequestDispatcher.ERROR_MESSAGE);
+        Integer errorStatusCode = (Integer) httpServletRequest.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        String errorMessage = (String) httpServletRequest.getAttribute(RequestDispatcher.ERROR_MESSAGE);
 
         model.addAttribute("errorCode", errorStatusCode);
 
-        if ((Integer) errorStatusCode == 404) {
+        if (errorStatusCode == 404) {
             model.addAttribute("errorMsg", "요청하신 페이지를 찾을 수 없습니다.");
             return "errorPage";
         }
