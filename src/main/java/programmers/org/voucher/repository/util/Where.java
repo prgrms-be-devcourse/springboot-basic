ece6dc703dd5f6cd851c9ac80ece68eb4d0669e6
kys0411 <32180846@dankook.ac.kr>
2023-07-26 14:48:31 +0900

    Where Builder 추가

--- src/main/java/programmers/org/voucher/repository/util/Where.java
+++ src/main/java/programmers/org/voucher/repository/util/Where.java
@@ -1,40 +1,59 @@
 package programmers.org.voucher.repository.util;
 
-import static programmers.org.voucher.repository.util.constant.Symbol.*;
+import programmers.org.voucher.repository.util.constant.Operator;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Where {
-    private static final String WHERE = "WHERE";
+    private String query;
 
-    private StringBuilder query;
-
-    private Where(StringBuilder query) {
+    private Where(String query) {
         this.query = query;
     }
 
-    StringBuilder getQuery() {
+    String getQuery() {
         return query;
     }
 
-    static public class Builder {
-        private StringBuilder query = new StringBuilder();
+    public static Builder builder(String column, Operator operator, Object value) {
+        return new Builder(column, operator, value);
+    }
 
-        public Builder() {
-           query.append(BLANK.getSymbol()).append(WHERE);
+    public static class Builder {
+        private static final String WHERE = "WHERE";
+
+        private List<String> query = new ArrayList<>();
+
+        private Builder() {
         }
 
-        public Builder query(String column) {
-            query.append(BLANK.getSymbol())
-                    .append(column)
-                    .append(EQUAL.getSymbol())
-                    .append(QUESTION_MARK.getSymbol())
-                    .append(COMMA.getSymbol());
+        Builder(String column, Operator operator, Object value) {
+            query.add(generateCondition(column, operator, value));
+        }
+
+        public Builder and(String column, Operator operator, Object value) {
+            String statement = String.format("AND %s", generateCondition(column, operator, value));
+            query.add(statement);
+
+            return this;
+        }
+
+        public Builder or(String column, Operator operator, Object value) {
+            String statement = String.format("OR %s %s %s", column, operator.getSymbol(), value);
+            query.add(statement);
 
             return this;
         }
 
         public Where build() {
-            query.deleteCharAt(query.length() - 1);
+            String queryJoin = String.join(" ", query);
+            String query = String.format("%s %s", WHERE, queryJoin);
             return new Where(query);
+        }
+
+        private String generateCondition(String column, Operator operator, Object value) {
+            return String.format("%s %s %s", column, operator.getSymbol(), value);
         }
     }
 }
