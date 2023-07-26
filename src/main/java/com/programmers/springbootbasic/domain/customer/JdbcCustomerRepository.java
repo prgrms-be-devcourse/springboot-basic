b60ab04a542318deb4ea6d337a646b7fef497d2f
sangminnim <sangmins930@gmail.com>
2023-07-26 18:44:16 +0900

    feat: Customer jdbc repository 구현

---
+++ src/main/java/com/programmers/springbootbasic/domain/customer/JdbcCustomerRepository.java
@@ -0,0 +1,76 @@
+package com.programmers.springbootbasic.domain.customer;
+
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public class JdbcCustomerRepository implements CustomerRepository {
+    private static final String CUSTOMER_ID_SNAKE = "customer_id";
+    private static final String CUSTOMER_ID_CAMEL = "customerId";
+    private static final String NAME = "name";
+    private static final String EMAIL = "email";
+    private final NamedParameterJdbcTemplate jdbcTemplate;
+    private final RowMapper<Customer> rowMapper = (rs, rowNum) -> {
+        UUID customerId = UUID.fromString(rs.getString(CUSTOMER_ID_SNAKE));
+        String email = rs.getString(EMAIL);
+        String name = rs.getString(NAME);
+        return new Customer(customerId, email, name);
+    };
+
+    public JdbcCustomerRepository(NamedParameterJdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+
+    private Map<String, Object> toParamMap(Customer customer) {
+        return Map.of(
+                CUSTOMER_ID_CAMEL, customer.getCustomerId().toString(),
+                NAME, customer.getName(),
+                EMAIL, customer.getEmail()
+        );
+    }
+
+    @Override
+    public Optional<Customer> save(Customer customer) {
+        int affectedRow = jdbcTemplate.update(
+                "INSERT INTO customer(customer_id, name, email) VALUES(:customerId, :name, :email)",
+                toParamMap(customer));
+        if (affectedRow != 1) {
+            return Optional.empty();
+        }
+        return Optional.of(customer);
+    }
+
+    @Override
+    public Optional<Customer> update(Customer customer) {
+        int affectedRow = jdbcTemplate.update(
+                "UPDATE customer SET name = :name, email = :email WHERE customer_id = :cusomterId",
+                toParamMap(customer)
+        );
+        if (affectedRow != 1) {
+            return Optional.empty();
+        }
+        return Optional.of(customer);
+    }
+
+    @Override
+    public Optional<Customer> findByEmail(String email) {
+        try {
+            Customer customer = jdbcTemplate.queryForObject(
+                    "SELECT * FROM customer WHERE email = :email",
+                    Collections.singletonMap(EMAIL, email),
+                    rowMapper
+            );
+            return Optional.ofNullable(customer);
+        } catch (DataAccessException e) {
+            return Optional.empty();
+        }
+    }
+}
