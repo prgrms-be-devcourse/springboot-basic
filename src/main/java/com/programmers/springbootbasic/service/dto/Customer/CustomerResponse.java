ba63e5333b390cf770fbf522588f696fab8b9c9b
sangminnim <sangmins930@gmail.com>
2023-07-26 16:11:45 +0900

    feat: ID값 반환하도록 변경

--- src/main/java/com/programmers/springbootbasic/service/dto/Customer/CustomerResponse.java
+++ src/main/java/com/programmers/springbootbasic/service/dto/Customer/CustomerResponse.java
@@ -1,6 +1,9 @@
 package com.programmers.springbootbasic.service.dto.Customer;
 
+import java.util.UUID;
+
 public record CustomerResponse(
+        UUID customerId,
         String name,
         String email
 ) {
