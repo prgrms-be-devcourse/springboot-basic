315af9868def39b79af9d0793c302f92a9c3e922
sangminnim <sangmins930@gmail.com>
2023-07-26 22:12:30 +0900

    feat: Voucher 인스턴스 변수 변경에 따른 검증로직 및 변환 삭제

--- src/main/java/com/programmers/springbootbasic/common/util/Validator.java
+++ src/main/java/com/programmers/springbootbasic/common/util/Validator.java
@@ -1,12 +1,10 @@
 package com.programmers.springbootbasic.common.util;
 
-import java.time.LocalDateTime;
 import java.util.UUID;
 
 public final class Validator {
     private static final String ID_IS_NULL = "빈 값이나 공백을 아이디로 사용할 수 없습니다. 현재 값: ";
     private static final String AMOUNT_OR_DISCOUNT_IS_NULL = "빈 값이나 공백을 할인률/할인액으로 지정할 수 없습니다. 현재 값: ";
-    private static final String DATETIME_IS_NULL = "빈 값이나 공백을 날짜/시간으로 지정할 수 없습니다. 현재 값: ";
     private static final String NULL_OR_BLANK = "빈 값이나 공백을 사용할 수 없습니다. 현재 값: ";
     private static final String EMAIL_IS_WRONG_OR_NULL = "빈 값이나 유효하지 않은 이메일입니다. 현재 값: ";
 
@@ -23,12 +21,6 @@ public final class Validator {
     public static void checkNullOrBlank(String input) {
         if (input == null || input.isBlank()) {
             throw new IllegalArgumentException(NULL_OR_BLANK + input);
-        }
-    }
-
-    public static void checkNullDateTime(LocalDateTime input) {
-        if (input == null) {
-            throw new IllegalArgumentException(DATETIME_IS_NULL + input);
         }
     }
 
