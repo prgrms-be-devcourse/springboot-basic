c93dd488423a32c0210df5420f4fbe5c3d1d93fd
sangminnim <sangmins930@gmail.com>
2023-07-26 16:06:05 +0900

    feat: 바우처 인스턴스 변수 변경에 따른 수정

--- src/main/java/com/programmers/springbootbasic/service/VoucherMapper.java
+++ src/main/java/com/programmers/springbootbasic/service/VoucherMapper.java
@@ -1,13 +1,11 @@
 package com.programmers.springbootbasic.service;
 
-import com.programmers.springbootbasic.domain.model.Duration;
 import com.programmers.springbootbasic.domain.voucher.Voucher;
 import com.programmers.springbootbasic.domain.voucher.VoucherType;
 import com.programmers.springbootbasic.service.dto.Voucher.VoucherCreationRequest;
 import com.programmers.springbootbasic.service.dto.Voucher.VoucherResponse;
 import com.programmers.springbootbasic.service.dto.Voucher.VoucherResponses;
 
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
@@ -17,14 +15,10 @@ public final class VoucherMapper {
     }
 
     public static Voucher toVoucher(VoucherCreationRequest request) {
-        Duration duration = new Duration(LocalDateTime.now(), request.expirationDate());
-        VoucherType voucherType = VoucherType.from(request.voucherType());
-        boolean used = false;
+        VoucherType voucherType = request.voucherType();
         return switch (voucherType) {
-            case FIX ->
-                    Voucher.createFixedAmount(UUID.randomUUID(), voucherType, request.name(), request.minimumPriceCondition(), duration, request.amountOrPercent(), used);
-            case PERCENT ->
-                    Voucher.createPercentDiscount(UUID.randomUUID(), voucherType, request.name(), request.minimumPriceCondition(), duration, request.amountOrPercent(), used);
+            case FIX -> Voucher.createFixedAmount(UUID.randomUUID(), voucherType, request.amountOrPercent());
+            case PERCENT -> Voucher.createPercentDiscount(UUID.randomUUID(), voucherType, request.amountOrPercent());
         };
     }
 
@@ -36,10 +30,12 @@ public final class VoucherMapper {
     }
 
     public static VoucherResponse toVoucherResponse(Voucher voucher) {
+        voucher.getAmountOrPercent();
         return new VoucherResponse(
-                voucher.getName(),
-                voucher.getMinimumPriceCondition(),
-                voucher.getDuration()
+                voucher.getVoucherId(),
+                voucher.getVoucherType(),
+                voucher.getAmountOrPercent()
         );
     }
+
 }
