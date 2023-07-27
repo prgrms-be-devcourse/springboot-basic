9dbcf4d465d093a911176c62b17d4b24f08f22cc
sangminnim <sangmins930@gmail.com>
2023-07-27 13:32:25 +0900

    feat: 패턴검증에 먼저 걸리기 때문에 isBlank 제거

--- src/main/java/com/programmers/springbootbasic/common/util/Validator.java
+++ src/main/java/com/programmers/springbootbasic/common/util/Validator.java
@@ -31,7 +31,7 @@ public final class Validator {
     }
 
     public static void checkNullOrWrongEmail(String input) {
-        if (input == null || !PatternUtils.isEmail(input) || input.isBlank()) {
+        if (input == null || !PatternUtils.isEmail(input)) {
             throw new IllegalArgumentException(EMAIL_IS_WRONG_OR_NULL + input);
         }
     }
