ae07ec60cb0de322e76b67ed133bbe32a370b8eb
Seongju <lsj40413@gmail.com>
2023-07-27 20:29:32 +0900

    feat: voucher DTO 내 entity -> dto 변환로직 처리

--- src/main/java/org/devcourse/springbasic/domain/voucher/dto/VoucherDto.java
+++ src/main/java/org/devcourse/springbasic/domain/voucher/dto/VoucherDto.java
@@ -3,22 +3,35 @@ package org.devcourse.springbasic.domain.voucher.dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.devcourse.springbasic.domain.voucher.domain.VoucherType;
+import org.springframework.format.annotation.DateTimeFormat;
 
+import java.time.LocalDate;
 import java.util.UUID;
 
 public class VoucherDto {
 
     @AllArgsConstructor
     @Getter
-    public static class SaveRequestDto {
+    public static class SaveRequest {
         private final VoucherType voucherType;
+        private final Long discountRate;
     }
 
     @AllArgsConstructor
     @Getter
-    public static class ResponseDto {
+    public static class Response {
         private final UUID voucherId;
-        private final VoucherType voucherType;
-        private final long discountRate;
+        private final String voucherType;
+        private final String discountRate;
+    }
+
+    @AllArgsConstructor
+    @Getter
+    public static class Request{
+        @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+        private final LocalDate creationStartDate;
+        @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+        private final LocalDate creationEndDate;
+        private final String discountType;
     }
 }
--- src/main/java/org/devcourse/springbasic/domain/voucher/service/VoucherService.java
+++ src/main/java/org/devcourse/springbasic/domain/voucher/service/VoucherService.java
@@ -3,12 +3,14 @@ package org.devcourse.springbasic.domain.voucher.service;
 import lombok.RequiredArgsConstructor;
 import org.devcourse.springbasic.domain.voucher.dao.VoucherRepository;
 import org.devcourse.springbasic.domain.voucher.domain.Voucher;
+import org.devcourse.springbasic.domain.voucher.domain.VoucherFactory;
 import org.devcourse.springbasic.domain.voucher.domain.VoucherType;
 import org.devcourse.springbasic.domain.voucher.dto.VoucherDto;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
@@ -20,21 +22,43 @@ public class VoucherService {
 
     private final VoucherRepository voucherRepository;
 
-    public UUID save(VoucherDto.SaveRequestDto voucherDto) {
-        VoucherType voucherType = voucherDto.getVoucherType();
-        Voucher voucher = voucherType.getVoucherSupplier().get();
+    @Transactional
+    public UUID save(VoucherDto.SaveRequest request) {
+        VoucherType voucherType = request.getVoucherType();
+        Voucher voucher = VoucherFactory.createVoucher(voucherType, request.getDiscountRate());
         return voucherRepository.save(voucher);
     }
 
-    public List<VoucherDto.ResponseDto> findAll() {
-        List<Voucher> vouchers = voucherRepository.findAll();
+    @Transactional
+    public void deleteById(UUID voucherId) {
+        voucherRepository.deleteById(voucherId);
+    }
+
+    public List<VoucherDto.Response> findByCriteria(VoucherDto.Request request) {
+
+        List<Voucher> vouchers = voucherRepository.findByCriteria(
+                request.getCreationStartDate(),
+                request.getCreationEndDate(),
+                request.getDiscountType() == null ? null : VoucherType.findVoucherType(request.getDiscountType())
+        );
+
         return vouchers.stream()
-                .map(voucher -> new VoucherDto.ResponseDto(voucher.getVoucherId(), voucher.getVoucherType(), voucher.getDiscountRate()))
+                .map(voucher -> new VoucherDto.Response(
+                        voucher.getVoucherId(),
+                        voucher.getVoucherType().getVoucherName(),
+                        voucher.getDiscountRate() + voucher.getVoucherType().getUnit())
+                )
                 .collect(Collectors.toList());
     }
 
-    public VoucherDto.ResponseDto findById(UUID voucherId) {
-        Voucher voucher = voucherRepository.findById(voucherId);
-        return new VoucherDto.ResponseDto(voucher.getVoucherId(), voucher.getVoucherType(), voucher.getDiscountRate());
+    public VoucherDto.Response findById(UUID voucherId) {
+        Voucher voucher = voucherRepository.findById(voucherId)
+                .orElseThrow(() -> new NoSuchElementException("존재하지 않는 바우처입니다."));
+        return new VoucherDto.Response(
+                voucher.getVoucherId(),
+                voucher.getVoucherType().getVoucherName(),
+                voucher.getDiscountRate() + voucher.getVoucherType().getUnit()
+        );
     }
+
 }
