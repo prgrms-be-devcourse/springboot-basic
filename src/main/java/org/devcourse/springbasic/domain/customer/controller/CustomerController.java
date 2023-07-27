2e85713e8958fe29c03b7a5456951bd2c3e1d395
Seongju <lsj40413@gmail.com>
2023-07-27 20:20:54 +0900

    feat: Customer Controller 및 view

--- src/main/java/org/devcourse/springbasic/domain/customer/controller/CustomerController.java
+++ src/main/java/org/devcourse/springbasic/domain/customer/controller/CustomerController.java
@@ -1,38 +1,61 @@
 package org.devcourse.springbasic.domain.customer.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.devcourse.springbasic.domain.customer.domain.CustomerMenuType;
 import org.devcourse.springbasic.domain.customer.dto.CustomerDto;
 import org.devcourse.springbasic.domain.customer.service.CustomerService;
-import org.devcourse.springbasic.global.io.ErrorMsgPrinter;
-import org.devcourse.springbasic.global.io.input.customer.CustomerInput;
-import org.devcourse.springbasic.global.io.output.customer.CustomerOutput;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.UUID;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/customers")
 public class CustomerController {
 
     private final CustomerService customerService;
 
-    public void run(CustomerInput customerInput, CustomerOutput customerOutput) {
-        CustomerMenuType menu = CustomerMenuType.EXIT;
-        do {
-            try {
-                customerOutput.menus();
-                menu = customerInput.menu();
-                if (menu.isSave()) {
-                    signUp(customerInput);
-                }
-
-            } catch (IllegalArgumentException e) {
-                ErrorMsgPrinter.print(e.getMessage());
-            }
-        } while (!menu.isExit());
+    @GetMapping()
+    public String findByCriteria(
+            @ModelAttribute CustomerDto.Request request,
+            Model model
+    ) {
+        List<CustomerDto.Response> customers = customerService.findByCriteria(request);
+        model.addAttribute("customers", customers);
+        return "customer-list";
     }
 
-    private void signUp(CustomerInput customerInput) {
-        CustomerDto.SaveRequestDto saveRequestDto = customerInput.signUp();
-        customerService.save(saveRequestDto);
+    @GetMapping("/create")
+    public String createVoucherForm() {
+        return "customer-create-form";
+    }
+
+    @PostMapping()
+    public String createNewVoucher(
+            @Valid CustomerDto.SaveRequest request
+    ) {
+        customerService.save(request);
+        return "redirect:/customers";
+    }
+
+    @GetMapping("/{customerId}")
+    public String findById(
+            @PathVariable UUID customerId,
+            Model model
+    ) {
+        CustomerDto.Response response = customerService.findById(customerId);
+        model.addAttribute("customer", response);
+        return "customer-details";
+    }
+
+    @DeleteMapping("/{customerId}")
+    public String deleteById(
+            @PathVariable UUID customerId
+    ) {
+        customerService.deleteById(customerId);
+        return "redirect:/customers";
     }
 }
