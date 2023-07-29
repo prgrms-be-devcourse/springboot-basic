9d63671d49cac982547e485525739a6ff4c3346c
SY97P <onetuks@naver.com>
2023-07-30 03:13:56 +0900

    feat: ExceptionHandler 로그 기능 추가

--- src/main/java/com/tangerine/voucher_system/application/global/exception/VoucherSystemExceptionHandler.java
+++ src/main/java/com/tangerine/voucher_system/application/global/exception/VoucherSystemExceptionHandler.java
@@ -1,25 +1,38 @@
 package com.tangerine.voucher_system.application.global.exception;
 
+import com.tangerine.voucher_system.application.voucher.service.mapper.VoucherServiceMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.text.MessageFormat;
+
 @RestControllerAdvice
 public class VoucherSystemExceptionHandler {
 
+    private final Logger logger = LoggerFactory.getLogger(VoucherSystemExceptionHandler.class);
+
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> internetServerErrorExceptionHandler(Exception e) {
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Internal Server Error");
+        String errorMessage = "Internal Server Error";
+        logger.error(errorMessage);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorMessage);
     }
 
     @ExceptionHandler(InvalidDataException.class)
     public ResponseEntity<String> invalidDataExceptionHandler(InvalidDataException e) {
-        return ResponseEntity.badRequest().body("Invalid Data: " + e.getMessage());
+        String errorMessage = MessageFormat.format("Invalid Data: {0}", e.getMessage());
+        logger.error(errorMessage);
+        return ResponseEntity.badRequest().body(errorMessage);
     }
 
     @ExceptionHandler(SqlException.class)
     public ResponseEntity<String> sqlErrorExceptionHandler(SqlException e) {
-        return ResponseEntity.badRequest().body("SQL Error: " + e.getMessage());
+        String errorMessage = MessageFormat.format("SQL Error: {0}", e.getMessage());
+        logger.error(errorMessage);
+        return ResponseEntity.badRequest().body(errorMessage);
     }
 }
