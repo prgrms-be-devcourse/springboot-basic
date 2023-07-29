88445a23e9e849768c040c61498fbeb67f4a6884
2jie0516 <2jie0516>
2023-07-30 00:15:48 +0900

    findByEmail에 대한 테스트 코드 로직 수정

--- src/test/java/prgms/spring_week1/domain/customer/repository/impl/JdbcCustomerRepositoryTest.java
+++ src/test/java/prgms/spring_week1/domain/customer/repository/impl/JdbcCustomerRepositoryTest.java
@@ -1,24 +1,17 @@
 package prgms.spring_week1.domain.customer.repository.impl;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
-import org.springframework.dao.DataAccessException;
-import org.springframework.util.Assert;
-import org.yaml.snakeyaml.events.CollectionEndEvent;
 import prgms.spring_week1.domain.customer.repository.CustomerRepository;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.empty;
-import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.nullValue;
 
 import javax.sql.DataSource;
 import java.util.Collections;
-import java.util.Optional;
 
 @JdbcTest
 class JdbcCustomerRepositoryTest {
@@ -43,7 +36,7 @@ class JdbcCustomerRepositoryTest {
     @Test
     void findByEmail() {
         assertThat(jdbcCustomerRepository.findByEmail("1sehan@naver.com").getName(),is("일세한"));
-        Assertions.assertThrows(DataAccessException.class,() -> jdbcCustomerRepository.findByEmail("3sehan@naver.com"));
+        assertThat(jdbcCustomerRepository.findByEmail("3sehan@naver.com"),is(nullValue()));
     }
 
     @Test
