a61e66a30d746ed2302994a9fca692d00abb9721
BeomSeogKim <kbs4520@daum.net>
2023-07-30 23:18:22 +0900

    생성 책임 Controller -> Factory

--- voucher/src/main/java/com/programmers/voucher/web/VoucherController.java
+++ voucher/src/main/java/com/programmers/voucher/web/VoucherController.java
@@ -1,8 +1,7 @@
 package com.programmers.voucher.web;
 
-import com.programmers.voucher.domain.voucher.FixedAmountVoucher;
-import com.programmers.voucher.domain.voucher.PercentDiscountVoucher;
 import com.programmers.voucher.domain.voucher.Voucher;
+import com.programmers.voucher.domain.voucher.VoucherFactory;
 import com.programmers.voucher.stream.voucher.VoucherStream;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,15 +9,16 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.Map;
-import java.util.UUID;
 
 @Controller
 @RequestMapping("/vouchers")
 public class VoucherController {
     private final VoucherStream voucherStream;
+    private final VoucherFactory voucherFactory;
 
-    public VoucherController(VoucherStream voucherStream) {
+    public VoucherController(VoucherStream voucherStream, VoucherFactory voucherFactory) {
         this.voucherStream = voucherStream;
+        this.voucherFactory = voucherFactory;
     }
 
     @GetMapping
@@ -43,20 +43,7 @@ public class VoucherController {
     public String createVoucher(@RequestParam Integer discount,
                                 @RequestParam String type,
                                 Model model) {
-
-        if ("FixedAmountVoucher".equals(type)) {
-            FixedAmountVoucher voucher = new FixedAmountVoucher(UUID.randomUUID().toString().substring(0, 7), discount);
-            voucherStream.save(voucher);
-            model.addAttribute("voucher", voucher);
-            return "redirect:/vouchers/" + voucher.getVoucherId();
-        } else if ("PercentDiscountVoucher".equals(type)) {
-            PercentDiscountVoucher voucher = new PercentDiscountVoucher(UUID.randomUUID().toString().substring(0, 7), discount);
-            voucherStream.save(voucher);
-            model.addAttribute("voucher", voucher);
-            return "redirect:/vouchers/" + voucher.getVoucherId();
-        } else {
-            return "redirect:/vouchers";
-        }
+        return voucherFactory.create(discount, type, model);
     }
 
     @GetMapping("/{voucherId}")
