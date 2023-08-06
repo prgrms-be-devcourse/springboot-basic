20ee601dabd754de6527edda2ffbce57f11294c2
sangminnim <sangmins930@gmail.com>
2023-08-07 06:54:23 +0900

    feat: Pagenation 구현 및 save 시 UUID 반환

--- src/main/java/com/programmers/springbootbasic/domain/voucher/Repository/JdbcVoucherRepository.java
+++ src/main/java/com/programmers/springbootbasic/domain/voucher/Repository/JdbcVoucherRepository.java
@@ -1,5 +1,7 @@
 package com.programmers.springbootbasic.domain.voucher.Repository;
 
+import com.programmers.springbootbasic.common.domain.Page;
+import com.programmers.springbootbasic.common.domain.Pageable;
 import com.programmers.springbootbasic.domain.voucher.Voucher;
 import com.programmers.springbootbasic.domain.voucher.VoucherType;
 import org.springframework.context.annotation.Primary;
@@ -7,14 +9,15 @@ import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 @Primary
 public class JdbcVoucherRepository implements VoucherRepository {
+    private static final String FAIL_TO_CREATE = "바우처 생성에 실패했습니다. 입력값을 확인해주세요";
     private static final String DATABASE_VOUCHER_ID = "voucher_id";
     private static final String VOUCHER_ID = "voucherId";
     private static final String DATABASE_VOUCHER_TYPE = "voucher_type";
@@ -22,11 +25,9 @@ public class JdbcVoucherRepository implements VoucherRepository {
     private static final String DATABASE_AMOUNT_OR_PERCENT = "amount_or_percent";
     private static final String AMOUNT_OR_PERCENT = "amountOrPercent";
     private final RowMapper<Voucher> rowMapper = (rs, rowNum) -> {
-
         UUID voucherId = UUID.fromString(rs.getString(DATABASE_VOUCHER_ID));
         VoucherType voucherType = VoucherType.valueOf(rs.getString(DATABASE_VOUCHER_TYPE));
         int amountOrPercent = rs.getInt(DATABASE_AMOUNT_OR_PERCENT);
-
         return Voucher.createVoucher(voucherId, voucherType, amountOrPercent);
     };
     private final NamedParameterJdbcTemplate jdbcTemplate;
@@ -36,13 +37,13 @@ public class JdbcVoucherRepository implements VoucherRepository {
     }
 
     @Override
-    public Optional<Voucher> save(Voucher voucher) {
+    public UUID save(Voucher voucher) {
         int affectedRow = jdbcTemplate.update("INSERT INTO voucher(voucher_id, voucher_type, amount_or_percent) VALUES (:voucherId, :voucherType, :amountOrPercent)"
                 , toParamMap(voucher));
         if (affectedRow != 1) {
-            return Optional.empty();
+            throw new IllegalArgumentException(FAIL_TO_CREATE);
         }
-        return Optional.of(voucher);
+        return voucher.getVoucherId();
     }
 
     private Map<String, Object> toParamMap(Voucher voucher) {
@@ -58,5 +59,23 @@ public class JdbcVoucherRepository implements VoucherRepository {
         return jdbcTemplate.query(
                 "SELECT * FROM voucher",
                 rowMapper);
+    }
+
+    @Override
+    public long countTotalVoucher() {
+        return jdbcTemplate.queryForObject(
+                "SELECT COUNT(*) FROM voucher",
+                Collections.emptyMap(),
+                Long.class);
+    }
+
+    @Override
+    public Page<Voucher> findAllWithPagination(Pageable pageable) {
+        long voucherCount = countTotalVoucher();
+        List<Voucher> vouchers = jdbcTemplate.query(
+                "SELECT * FROM voucher LIMIT :size OFFSET :offset",
+                Map.of("size", pageable.size(), "offset", pageable.getOffSet())
+                , rowMapper);
+        return new Page<>(vouchers, pageable, voucherCount);
     }
 }
--- src/main/java/com/programmers/springbootbasic/domain/voucher/Repository/MemoryVoucherRepository.java
+++ src/main/java/com/programmers/springbootbasic/domain/voucher/Repository/MemoryVoucherRepository.java
@@ -5,18 +5,21 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Repository
 public class MemoryVoucherRepository implements VoucherRepository {
+    private static final String ALREADY_EXISTS = "이미 해당 아이디의 바우처가 존재합니다.";
     private final Map<UUID, Voucher> storage = new ConcurrentHashMap<>();
 
     @Override
-    public Optional<Voucher> save(Voucher voucher) {
+    public UUID save(Voucher voucher) {
+        if (storage.containsKey(voucher.getVoucherId())) {
+            throw new IllegalStateException(ALREADY_EXISTS);
+        }
         storage.put(voucher.getVoucherId(), voucher);
-        return Optional.of(voucher);
+        return voucher.getVoucherId();
     }
 
     @Override
@@ -24,4 +27,8 @@ public class MemoryVoucherRepository implements VoucherRepository {
         return List.copyOf(storage.values());
     }
 
+    @Override
+    public long countTotalVoucher() {
+        return storage.size();
+    }
 }
