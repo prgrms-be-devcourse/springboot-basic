99360d1983eb7b2f91a36a176c669a2a41eb21cb
sangminnim <sangmins930@gmail.com>
2023-08-06 23:11:33 +0900

    feat: 검증 어노테이션 추가

--- src/main/java/com/programmers/springbootbasic/presentation/controller/dto/Customer/CustomerUpdateRequest.java
+++ src/main/java/com/programmers/springbootbasic/presentation/controller/dto/Customer/CustomerUpdateRequest.java
@@ -1,7 +1,16 @@
 package com.programmers.springbootbasic.presentation.controller.dto.Customer;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 public record CustomerUpdateRequest(
+        @NotBlank
+        @Size(min = 1, max = 20, message = "이름은 1자 이상 20자 이하여야 합니다.")
         String name,
+        @NotNull
+        @Email(message = "이메일 형식이 아닙니다.")
         String email
 ) {
 }
--- src/main/java/com/programmers/springbootbasic/presentation/controller/dto/Voucher/VoucherCreationRequest.java
+++ src/main/java/com/programmers/springbootbasic/presentation/controller/dto/Voucher/VoucherCreationRequest.java
@@ -1,10 +1,13 @@
 package com.programmers.springbootbasic.presentation.controller.dto.Voucher;
 
 import com.programmers.springbootbasic.domain.voucher.VoucherType;
+import jakarta.validation.constraints.NotNull;
 
 
 public record VoucherCreationRequest(
+        @NotNull
         VoucherType voucherType,
+        @NotNull
         int amountOrPercent
 ) {
 }
