c4168d44f821daa3410463979f9d33b5cf2d2556
kys0411 <32180846@dankook.ac.kr>
2023-08-07 01:20:41 +0900

    🎨 Select query 순서 유지하도록 기능 변경

--- src/main/java/programmers/org/voucher/repository/util/Select.java
+++ src/main/java/programmers/org/voucher/repository/util/Select.java
@@ -1,11 +1,12 @@
 package programmers.org.voucher.repository.util;
 
-import programmers.org.voucher.repository.util.constant.Table;
-
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Select {
+    private static final List<String> sql = new ArrayList<>();
+
     private String query;
 
     private Select(String query) {
@@ -16,48 +17,90 @@ public class Select {
         return query;
     }
 
-    public static Builder builder() {
-        return new Builder();
+    public static SelectBuilder builder() {
+        return new SelectBuilder();
+    }
+
+    public static class SelectBuilder {
+        private SelectBuilder() {
+
+        }
+
+        public FromBuilder select(Class column) {
+
+            List<String> subQuery = new ArrayList<>();
+            Field[] columns = column.getDeclaredFields();
+
+            for (Field field : columns) {
+                String statement = String.format("%s", field.getName());
+                subQuery.add(statement);
+            }
+
+            String join = String.join(", ", subQuery);
+            sql.add(join);
+
+            return new FromBuilder();
+        }
+
+        public FromBuilder select() {
+            sql.add("*");
+            return new FromBuilder();
+        }
+    }
+
+    public static class FromBuilder {
+        private final static String FROM = "FROM";
+
+        private FromBuilder() {
+
+        }
+
+        public WhereBuilder from(Class table) {
+            String statement = String.format("%s %s", FROM, table.getSimpleName());
+            sql.add(statement);
+            return new WhereBuilder();
+        }
+    }
+
+    public static class WhereBuilder {
+        private WhereBuilder() {
+        }
+
+        public OrderBuilder where(Where where) {
+            sql.add(where.getQuery());
+            return new OrderBuilder();
+        }
+
+        public Select build() {
+            return new Builder().build();
+        }
+    }
+
+    public static class OrderBuilder {
+        private OrderBuilder() {
+        }
+
+        public Builder orderBy(Order order) {
+            sql.add(order.getQuery());
+            return new Builder();
+        }
+
+        public Select build() {
+            return new Builder().build();
+        }
     }
 
     public static class Builder {
 
         private final static String SELECT = "SELECT";
-        private final static String FROM = "FROM";
-
-        private List<String> query = new ArrayList<>();
 
         private Builder() {
         }
 
-        public Builder select(String column) {
-            String statement = String.format("%s", column);
-            query.add(statement);
-
-            return this;
-        }
-
-        public Builder from(Table table) {
-            String statement = String.format("%s %s", FROM, table);
-            query.add(statement);
-
-            return this;
-        }
-
-        public Builder where(Where where) {
-            query.add(where.getQuery());
-            return this;
-        }
-
-        public Builder orderBy(Order order) {
-            query.add(order.getQuery());
-            return this;
-        }
-
         public Select build() {
-            String queryJoin = String.join(" ", query);
-            String query = String.format("%s %s", SELECT, queryJoin);
-            return new Select(query);
+            String join = String.join(" ", sql);
+            sql.clear();
+            return new Select("%s %s".formatted(SELECT, join));
         }
     }
 }
