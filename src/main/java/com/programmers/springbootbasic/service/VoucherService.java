51f5bc44d774e964d7b778a39aec424922aca283
sangminnim <sangmins930@gmail.com>
2023-08-07 06:54:55 +0900

    feat: Pagenation 구현 및 save 시 UUID 반환 in Service

--- src/main/java/com/programmers/springbootbasic/service/VoucherService.java
+++ src/main/java/com/programmers/springbootbasic/service/VoucherService.java
@@ -1,5 +1,7 @@
 package com.programmers.springbootbasic.service;
 
+import com.programmers.springbootbasic.common.domain.Page;
+import com.programmers.springbootbasic.common.domain.Pageable;
 import com.programmers.springbootbasic.domain.voucher.Repository.VoucherRepository;
 import com.programmers.springbootbasic.domain.voucher.Voucher;
 import com.programmers.springbootbasic.presentation.controller.dto.Voucher.VoucherCreationRequest;
@@ -7,23 +9,27 @@ import com.programmers.springbootbasic.service.dto.Voucher.VoucherResponse;
 import com.programmers.springbootbasic.service.dto.Voucher.VoucherResponses;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
 @Service
 public class VoucherService {
-    private static final String FAIL_TO_CREATE = "바우처 생성에 실패했습니다. 입력값을 확인해주세요";
     private final VoucherRepository voucherRepository;
 
     public VoucherService(VoucherRepository voucherRepository) {
         this.voucherRepository = voucherRepository;
     }
 
-    public VoucherResponse createVoucher(VoucherCreationRequest request) {
+    public UUID createVoucher(VoucherCreationRequest request) {
         Voucher voucher = VoucherMapper.toVoucher(request);
-        return voucherRepository.save(voucher)
-                .map(VoucherMapper::toVoucherResponse)
-                .orElseThrow(() -> new IllegalArgumentException(FAIL_TO_CREATE));
+        return voucherRepository.save(voucher);
     }
 
     public VoucherResponses list() {
         return VoucherMapper.toVoucherResponseList(voucherRepository.findAll());
+    }
+
+    public Page<VoucherResponse> findVoucherByPage(Pageable pageable) {
+        return voucherRepository.findAllWithPagination(pageable)
+                .map(VoucherMapper::toVoucherResponse);
     }
 }
