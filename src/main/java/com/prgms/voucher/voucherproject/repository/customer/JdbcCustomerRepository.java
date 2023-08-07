cacbcb42c4c4cb0d4f2e7408a9ed03e67f3e6df7
sujjangOvO <ssu7038@yu.ac.kr>
2023-08-08 03:26:30 +0900

    :sparkles: Jdbc 고객 저장소 쿼리 빌더 기능 적용

--- src/main/java/com/prgms/voucher/voucherproject/repository/customer/JdbcCustomerRepository.java
+++ src/main/java/com/prgms/voucher/voucherproject/repository/customer/JdbcCustomerRepository.java
@@ -1,27 +1,31 @@
 package com.prgms.voucher.voucherproject.repository.customer;
 
+import builder.Delete;
+import builder.Insert;
+import builder.Select;
+import builder.Where;
 import com.prgms.voucher.voucherproject.domain.customer.Customer;
+import com.prgms.voucher.voucherproject.domain.customer.CustomerResponse;
 import com.prgms.voucher.voucherproject.exception.NotFoundCustomerException;
 
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
 
+import java.sql.Timestamp;
 import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
 
+import static builder.Operator.Type.*;
 import static com.prgms.voucher.voucherproject.util.JdbcUtils.toUUID;
 
 @Component
 public class JdbcCustomerRepository implements CustomerRepository {
-	private static final String SQL_INSERT = "INSERT INTO customer(customer_id, email, name, created_at) VALUES (UUID_TO_BIN(?), ?, ?, ?)";
-	private static final String SQL_FINDALL = "SELECT * FROM customer";
-	private static final String SQL_FINDBYID = "SELECT * FROM customer WHERE email = ?";
-	private static final String SQL_DELETEBYID = "DELETE FROM customer WHERE email = ?";
 	public static final int SUCCESS_QUERY = 1;
-
 	private final JdbcTemplate jdbcTemplate;
 
 	public JdbcCustomerRepository(JdbcTemplate jdbcTemplate) {
@@ -30,8 +34,17 @@ public class JdbcCustomerRepository implements CustomerRepository {
 
 	@Override
 	public void save(Customer customer) {
-		int save = jdbcTemplate.update(SQL_INSERT, customer.getCustomerId().toString().getBytes(), customer.getEmail(),
-			customer.getName(), customer.getCreatedAt());
+		Map<String, Object> values = new HashMap<>();
+		values.put("customer_id", customer.getCustomerId());
+		values.put("email", customer.getEmail());
+		values.put("name", customer.getName());
+		values.put("created_at", Timestamp.valueOf(customer.getCreatedAt()));
+
+		Insert insert = Insert.into(Customer.class)
+				.values(values)
+				.build();
+
+		int save = jdbcTemplate.update(insert.getQuery());
 
 		if (save != SUCCESS_QUERY) {
 			throw new IllegalArgumentException("고객 저장에 실패하였습니다.");
@@ -40,22 +53,43 @@ public class JdbcCustomerRepository implements CustomerRepository {
 
 	@Override
 	public List<Customer> findAll() {
-		List<Customer> customers = jdbcTemplate.query(SQL_FINDALL, customerRowMapper);
+		Select selectQueryAll = Select.builder()
+				.select(CustomerResponse.class)
+				.from(Customer.class)
+				.build();
+
+		List<Customer> customers = jdbcTemplate.query(selectQueryAll.getQuery(), customerRowMapper);
 		return customers;
 	}
 
 	@Override
 	public Optional<Customer> findByEmail(String email) {
-		return jdbcTemplate.query(SQL_FINDBYID, customerRowMapper)
+		Select selectQuery = Select.builder()
+				.select(CustomerResponse.class)
+				.from(Customer.class)
+				.where(
+						Where.builder("email", EQ, email)
+								.build()
+				)
+				.build();
+
+		return jdbcTemplate.query(selectQuery.getQuery(), customerRowMapper)
 			.stream()
 			.findFirst();
 	}
 
 	@Override
 	public void deleteByEmail(String email) {
-		int delete = jdbcTemplate.update(SQL_DELETEBYID, email);
+		Delete delete = Delete.from(Customer.class)
+				.where(
+						Where.builder("email", EQ, email)
+								.build()
+				)
+				.build();
 
-		if (delete != SUCCESS_QUERY) {
+		int deleteResult = jdbcTemplate.update(delete.getQuery());
+
+		if (deleteResult != SUCCESS_QUERY) {
 			throw new NotFoundCustomerException("삭제할 고객이 존재하지 않습니다.");
 		}
 	}
