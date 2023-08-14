3cef7be3c9d8f8ba0deddaa82583f9e896f23684
Curry4182 <kangbk4182@naver.com>
2023-08-14 16:00:22 +0900

    refactor: VoucherAPIController 반환 형식 통일

--- src/main/java/org/prgrms/kdt/controller/VoucherAPIController.java
+++ src/main/java/org/prgrms/kdt/controller/VoucherAPIController.java
@@ -6,18 +6,15 @@ import org.prgrms.kdt.enums.VoucherType;
 import org.prgrms.kdt.model.dto.VoucherRequest;
 import org.prgrms.kdt.model.dto.VoucherResponse;
 import org.prgrms.kdt.service.VoucherService;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class VoucherAPIController {
 	private final VoucherService voucherService;
 
@@ -26,36 +23,29 @@ public class VoucherAPIController {
 	}
 
 	@GetMapping("/api/vouchers")
-	@ResponseBody
 	public List<VoucherResponse> findVouchers() {
 		return voucherService.getVouchers();
 	}
 
 	@GetMapping("/api/vouchers/search")
-	@ResponseBody
 	public List<VoucherResponse> findVouchers(
-		@RequestParam(value = "voucher-type", required = false, defaultValue = "") String voucherType
+		@RequestParam(value = "voucher-type") String voucherType
 	) {
 		return voucherService.findVoucherByVoucherType(VoucherType.valueOf(voucherType));
 	}
 
 	@GetMapping("/api/vouchers/{voucher-id}")
-	@ResponseBody
-	public VoucherResponse findVoucherById(@PathVariable("voucher-id")Long voucherId, Model model) {
+	public VoucherResponse findVoucherById(@PathVariable("voucher-id")Long voucherId) {
 		return voucherService.findVoucherById(voucherId);
 	}
 
 	@DeleteMapping("/api/vouchers/{voucher-id}")
-	@ResponseBody
-	public ResponseEntity deleteVoucherById(@PathVariable("voucher-id")Long voucherId) {
+	public void deleteVoucherById(@PathVariable("voucher-id")Long voucherId) {
 		voucherService.deleteVoucherById(voucherId);
-		return ResponseEntity.ok("Voucher deleted");
 	}
 
 	@PostMapping("/api/vouchers/new")
-	@ResponseBody
-	public ResponseEntity saveVoucher(@RequestBody VoucherRequest voucherRequest) {
-		voucherService.saveVoucher(voucherRequest);
-		return ResponseEntity.ok("Voucher created");
+	public Long saveVoucher(@RequestBody VoucherRequest voucherRequest) {
+		return voucherService.saveVoucher(voucherRequest);
 	}
 }
