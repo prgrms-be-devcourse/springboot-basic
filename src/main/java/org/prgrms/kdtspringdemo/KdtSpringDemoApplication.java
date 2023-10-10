a76b9627b25ad565ec98e4d36158d05df4cc2b01
eugene <81062608+eugene225@users.noreply.github.com>
2023-10-10 13:49:23 +0900

    [init] 프로젝트 생성 및 ignore 설정

---
+++ src/main/java/org/prgrms/kdtspringdemo/KdtSpringDemoApplication.java
@@ -0,0 +1,13 @@
+package org.prgrms.kdtspringdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KdtSpringDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KdtSpringDemoApplication.class, args);
+	}
+
+}
