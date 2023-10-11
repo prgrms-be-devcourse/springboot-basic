e1a76f5326d12bfb45a40f5ee517651cca45a93d
Hanna <hanna@trdst.com>
2023-10-11 14:07:27 +0900

    refactor: create 커맨드 컨트롤러 리팩터링

--- src/main/java/com/programmers/springbootbasic/controller/Controller.java
+++ src/main/java/com/programmers/springbootbasic/controller/Controller.java
@@ -2,13 +2,13 @@ package com.programmers.springbootbasic.controller;
 
 import com.programmers.springbootbasic.domain.voucher.Voucher;
 import com.programmers.springbootbasic.domain.voucher.VoucherType;
+import com.programmers.springbootbasic.infra.InputReader;
 import com.programmers.springbootbasic.service.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
 
 import java.util.List;
-import java.util.Scanner;
 
 @ShellComponent
 public class Controller {
@@ -26,19 +26,14 @@ public class Controller {
 
     @ShellMethod(key = "create")
     public void create() {
-        Scanner scanner = new Scanner(System.in);
-        VoucherType.printAllDescriptionsToConsole();
+        InputReader inputReader = new InputReader();
 
-        System.out.print("Please select a voucher type > ");
-        int voucherTypeId = Integer.parseInt(scanner.nextLine());
-        VoucherType voucherType = VoucherType.select(voucherTypeId);
-
-        System.out.print("Please enter the amount > ");
-        Long amount = Long.parseLong(scanner.nextLine());
+        VoucherType voucherType = VoucherType.select(inputReader.readVoucherTypeId());
+        Long amount = inputReader.readAmount();
 
         service.create(voucherType, amount);
 
-        scanner.close();
+        inputReader.close();
     }
 
     @ShellMethod(key = "list")
