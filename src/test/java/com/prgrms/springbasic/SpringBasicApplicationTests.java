4dd1ccff2ab0a6445099390ce5b9f6c6271fc9c4
byulcode <bybit0408@gmail.com>
2023-10-12 19:13:21 +0900

    initial commit

---
+++ src/test/java/com/prgrms/springbasic/SpringBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prgrms.springbasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBasicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
