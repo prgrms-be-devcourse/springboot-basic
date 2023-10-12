380ea2c5c408009d191c8e86b5c39a9be7e7e697
letskuku <yujung9288@naver.com>
2023-10-13 02:34:12 +0900

    feat: 바우처 controller 생성 및 createVoucher 메서드 구현

---
+++ src/main/java/com/programmers/vouchermanagement/voucher/presentation/VoucherController.java
@@ -0,0 +1,50 @@
+package com.programmers.vouchermanagement.voucher.presentation;
+
+import com.programmers.vouchermanagement.utils.ConsoleInputManager;
+import com.programmers.vouchermanagement.utils.ConsoleOutputManager;
+import com.programmers.vouchermanagement.voucher.application.VoucherService;
+import com.programmers.vouchermanagement.voucher.domain.VoucherType;
+import com.programmers.vouchermanagement.voucher.dto.VoucherRequestDto;
+import com.programmers.vouchermanagement.voucher.exception.IllegalDiscountException;
+import com.programmers.vouchermanagement.voucher.exception.VoucherTypeNotFoundException;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class VoucherController {
+
+    private final ConsoleInputManager consoleInputManager;
+    private final ConsoleOutputManager consoleOutputManager;
+    private final VoucherService voucherService;
+
+    public VoucherController(ConsoleInputManager consoleInputManager, ConsoleOutputManager consoleOutputManager, VoucherService voucherService) {
+        this.consoleInputManager = consoleInputManager;
+        this.consoleOutputManager = consoleOutputManager;
+        this.voucherService = voucherService;
+    }
+
+    public void createVoucher() {
+
+        consoleOutputManager.printVoucherTypeMenu();
+
+        VoucherType voucherType;
+
+        try {
+            voucherType = VoucherType.getVoucherTypeByName(consoleInputManager.inputString());
+        } catch (VoucherTypeNotFoundException e) {
+            consoleOutputManager.printReturnMain(e.getMessage());
+            return;
+        }
+
+        consoleOutputManager.printDiscountRequest();
+        Long discount = consoleInputManager.inputDiscount();
+
+        try {
+            voucherService.createVoucher(new VoucherRequestDto(voucherType, discount));
+        } catch (IllegalDiscountException e) {
+            consoleOutputManager.printReturnMain(e.getMessage());
+        }
+    }
+
+    public void readAllVoucher() {
+    }
+}
