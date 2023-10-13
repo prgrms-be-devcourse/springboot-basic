d8206b484a28260e97d423d03ba780dacebfafff
uijin-j <uijin0531@gmail.com>
2023-10-13 16:30:44 +0900

    chore: 프로젝트 설정

---
+++ voucher-management-system/src/main/java/team/marco/vouchermanagementsystem/VoucherManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package team.marco.vouchermanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoucherManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoucherManagementSystemApplication.class, args);
+	}
+
+}
