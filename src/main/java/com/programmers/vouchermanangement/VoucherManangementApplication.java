6616581a0365776aa3e679eb1edb2b44c57fe897
Jay <injaesong0@gmail.com>
2023-10-13 15:00:47 +0900

    init: initialize the project to clarify collaboration without .mvn

---
+++ src/main/java/com/programmers/vouchermanangement/VoucherManangementApplication.java
@@ -0,0 +1,13 @@
+package com.programmers.vouchermanangement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoucherManangementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoucherManangementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/programmers/vouchermanangement/VoucherManangementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.programmers.vouchermanangement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoucherManangementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
