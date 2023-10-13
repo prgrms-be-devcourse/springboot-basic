64003914d91b55b4bd2c51bb5b22fd578f580619
suzzingv <84590065+suzzingv@users.noreply.github.com>
2023-10-13 11:44:56 +0900

    init

---
+++ src/main/java/com/example/vouchermanager/VoucherManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.vouchermanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoucherManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VoucherManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/vouchermanager/VoucherManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vouchermanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoucherManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
