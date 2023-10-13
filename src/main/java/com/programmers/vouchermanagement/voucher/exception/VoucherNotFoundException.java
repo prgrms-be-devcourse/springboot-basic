2f01e59b1a283f3064b58deb7957b39f42a22b24
letskuku <yujung9288@naver.com>
2023-10-14 02:11:13 +0900

    refactor: 예외 메시지 수정

--- src/main/java/com/programmers/vouchermanagement/voucher/exception/VoucherNotFoundException.java
+++ src/main/java/com/programmers/vouchermanagement/voucher/exception/VoucherNotFoundException.java
@@ -3,6 +3,6 @@ package com.programmers.vouchermanagement.voucher.exception;
 public class VoucherNotFoundException extends RuntimeException {
 
     public VoucherNotFoundException() {
-        super("No appropriate voucher has been found. ");
+        super("Voucher not found. ");
     }
 }
