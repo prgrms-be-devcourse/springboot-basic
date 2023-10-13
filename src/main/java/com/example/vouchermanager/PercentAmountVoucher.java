c9e384e703ddc97d78a1437eeae07687641e63e8
suzzingv <84590065+suzzingv@users.noreply.github.com>
2023-10-13 13:40:11 +0900

    feat : PercentAmountVoucher 생ㅅ어

---
+++ src/main/java/com/example/vouchermanager/PercentAmountVoucher.java
@@ -0,0 +1,15 @@
+package com.example.vouchermanager;
+
+public class PercentAmountVoucher implements Voucher {
+
+    private final long percent;
+
+    public PercentAmountVoucher(long percent) {
+        this.percent = percent;
+    }
+
+    @Override
+    public long discount(long beforeDiscount) {
+        return beforeDiscount - beforeDiscount * (percent / 100);
+    }
+}
