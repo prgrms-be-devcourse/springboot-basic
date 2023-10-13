04b1db4a152e0eff9137b163d63f761fb1cd849e
EunChanNam <namhm23@gmail.com>
2023-10-14 00:36:01 +0900

    feat: 바우처 생성 비즈니스 로직 구현

--- src/main/java/study/dev/spring/voucher/exception/VoucherErrorCode.java
+++ src/main/java/study/dev/spring/voucher/exception/VoucherErrorCode.java
@@ -7,7 +7,8 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum VoucherErrorCode {
 
-	;
+	NEGATIVE_DISCOUNT_AMOUNT("할인가는 양수만 가능합니다"),
+	INVALID_RANGE_DISCOUNT_AMOUNT("0~100 사이의 할인률만 가능합니다");
 
 	private final String message;
 }
