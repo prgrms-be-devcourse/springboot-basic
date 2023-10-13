18ff4de53e4bee7d3d2f786abe308fb81f64f60c
letskuku <yujung9288@naver.com>
2023-10-14 02:27:55 +0900

    feat: 파일 관련 예외 생성

---
+++ src/main/java/com/programmers/vouchermanagement/voucher/exception/FileIOException.java
@@ -0,0 +1,8 @@
+package com.programmers.vouchermanagement.voucher.exception;
+
+public class FileIOException extends RuntimeException {
+
+    public FileIOException(String message) {
+        super(message);
+    }
+}
