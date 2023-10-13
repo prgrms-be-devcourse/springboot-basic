37906a818ef0212175dffb46153da455784f203d
Hanna <hanna@trdst.com>
2023-10-14 00:53:17 +0900

    feat: MemoryVoucherRepository local 프로파일 에서만 동작하도록 수정

--- src/main/java/com/programmers/springbootbasic/repository/voucher/MemoryVoucherRepository.java
+++ src/main/java/com/programmers/springbootbasic/repository/voucher/MemoryVoucherRepository.java
@@ -1,11 +1,13 @@
 package com.programmers.springbootbasic.repository.voucher;
 
 import com.programmers.springbootbasic.domain.voucher.Voucher;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
 @Repository
+@Profile("local")
 public class MemoryVoucherRepository implements VoucherRepository {
     Map<UUID, Voucher> storage = new HashMap<>();
 
