7351a2291713c2df9d5537a028dc1d5a6cb2682d
dlswns2480 <dlswns2480@naver.com>
2023-10-14 20:00:30 +0900

    프로젝트 생성

---
+++ src/main/java/org/prgrms/vouchermanager/VouchermanagerApplication.java
@@ -0,0 +1,13 @@
+package org.prgrms.vouchermanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VouchermanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VouchermanagerApplication.class, args);
+	}
+
+}
