732eb3788c12eb2f6841c52ef8a395c2d159e098
JeongeunChoi <77786996+JeongeunChoi@users.noreply.github.com>
2023-10-14 15:11:19 +0900

    chore: 프로젝트 초기 설정

---
+++ src/main/java/org/prgms/kdtspringweek1/KdtSpringWeek1Application.java
@@ -0,0 +1,13 @@
+package org.prgms.kdtspringweek1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KdtSpringWeek1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KdtSpringWeek1Application.class, args);
+	}
+
+}
