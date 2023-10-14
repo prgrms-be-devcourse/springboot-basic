de8e9ab5d0f5792ef3dbc2f98c4456e421fde8d5
suzzingv <84590065+suzzingv@users.noreply.github.com>
2023-10-15 02:06:11 +0900

    faet : controller 분리

--- src/main/java/com/example/vouchermanager/VoucherManagerApplication.java
+++ src/main/java/com/example/vouchermanager/VoucherManagerApplication.java
@@ -3,12 +3,14 @@ package com.example.vouchermanager;
 import com.example.vouchermanager.console.Command;
 import com.example.vouchermanager.console.ConsolePrint;
 import com.example.vouchermanager.console.VoucherType;
+import com.example.vouchermanager.controller.VoucherController;
 import com.example.vouchermanager.exception.NotCorrectForm;
 import com.example.vouchermanager.exception.NotCorrectScope;
 import com.example.vouchermanager.message.ConsoleMessage;
 import com.example.vouchermanager.message.LogMessage;
 import com.example.vouchermanager.service.VoucherService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -16,13 +18,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @Slf4j
 public class VoucherManagerApplication implements CommandLineRunner {
-    private final ConsolePrint consolePrint;
-    private final VoucherService service;
 
-    public VoucherManagerApplication(ConsolePrint consolePrint, VoucherService service) {
-        this.consolePrint = consolePrint;
-        this.service = service;
+    private final VoucherController controller;
+
+    public VoucherManagerApplication(VoucherController controller) {
+        this.controller = controller;
     }
+
+    @Autowired
 
     public static void main(String[] args) {
         SpringApplication.run(VoucherManagerApplication.class, args);
@@ -30,32 +33,6 @@ public class VoucherManagerApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) {
-        try {
-            Command command = consolePrint.run();
-
-            if(command == Command.CREATE) {
-                log.info(LogMessage.CREATE_START.getMessage());
-
-                VoucherType voucherType = consolePrint.getVoucherType();
-                long discount = consolePrint.getVoucherDiscount(voucherType);
-
-                log.info(LogMessage.VOUCHER_INFO.getMessage(), voucherType.getType(), discount);
-
-                service.create(voucherType, discount);
-                System.out.println(ConsoleMessage.COMPLETE_CREATE_VOUCHER.getMessage());
-            } else if(command == Command.LIST) {
-                log.info(LogMessage.LIST_START.getMessage());
-
-                consolePrint.printList(service.list());
-            }
-        } catch (NotCorrectForm e) {
-            System.out.println(ConsoleMessage.NOT_CORRECT_FORM.getMessage());
-            System.out.println(ConsoleMessage.FINISH_PROGRAM.getMessage());
-            System.exit(0);
-        } catch (NotCorrectScope e) {
-            System.out.println(ConsoleMessage.NOT_CORRECT_SCOPE.getMessage());
-            System.out.println(ConsoleMessage.FINISH_PROGRAM.getMessage());
-            System.exit(0);
-        }
+        controller.run();
     }
 }
---
+++ src/main/java/com/example/vouchermanager/controller/VoucherController.java
@@ -0,0 +1,54 @@
+package com.example.vouchermanager.controller;
+
+import com.example.vouchermanager.console.Command;
+import com.example.vouchermanager.console.ConsolePrint;
+import com.example.vouchermanager.console.VoucherType;
+import com.example.vouchermanager.exception.NotCorrectForm;
+import com.example.vouchermanager.exception.NotCorrectScope;
+import com.example.vouchermanager.message.ConsoleMessage;
+import com.example.vouchermanager.message.LogMessage;
+import com.example.vouchermanager.service.VoucherService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@Slf4j
+public class VoucherController {
+    private final ConsolePrint consolePrint;
+    private final VoucherService service;
+
+    public VoucherController(ConsolePrint consolePrint, VoucherService service) {
+        this.consolePrint = consolePrint;
+        this.service = service;
+    }
+
+    public void run() {
+        try {
+            Command command = consolePrint.run();
+
+            if(command == Command.CREATE) {
+                log.info(LogMessage.CREATE_START.getMessage());
+
+                VoucherType voucherType = consolePrint.getVoucherType();
+                long discount = consolePrint.getVoucherDiscount(voucherType);
+
+                log.info(LogMessage.VOUCHER_INFO.getMessage(), voucherType.getType(), discount);
+
+                service.create(voucherType, discount);
+                System.out.println(ConsoleMessage.COMPLETE_CREATE_VOUCHER.getMessage());
+            } else if(command == Command.LIST) {
+                log.info(LogMessage.LIST_START.getMessage());
+
+                consolePrint.printList(service.list());
+            }
+        } catch (NotCorrectForm e) {
+            System.out.println(ConsoleMessage.NOT_CORRECT_FORM.getMessage());
+            System.out.println(ConsoleMessage.FINISH_PROGRAM.getMessage());
+            System.exit(0);
+        } catch (NotCorrectScope e) {
+            System.out.println(ConsoleMessage.NOT_CORRECT_SCOPE.getMessage());
+            System.out.println(ConsoleMessage.FINISH_PROGRAM.getMessage());
+            System.exit(0);
+        }
+    }
+}
