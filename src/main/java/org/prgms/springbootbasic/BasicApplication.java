b40bdf885cd72f81f5802e7bc7534b00d8328e50
ZZAMBAs <rksidksrksi@naver.com>
2023-10-15 14:05:25 +0900

    Feat: 최초로 프로젝트를 생성한다.

---
+++ src/main/java/org/prgms/springbootbasic/BasicApplication.java
@@ -0,0 +1,13 @@
+package org.prgms.springbootbasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicApplication.class, args);
+	}
+
+}
