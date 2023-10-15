a91b78700b80f6132cb3d4c5b83ee1b900ca6379
park0jae <okvv26@naver.com>
2023-10-16 02:00:26 +0900

    feat : enum -> MenuType 구현

---
+++ voucher/src/main/java/com/zerozae/voucher/common/MenuType.java
@@ -0,0 +1,26 @@
+package com.zerozae.voucher.common;
+
+import com.zerozae.voucher.exception.ExceptionHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static com.zerozae.voucher.common.message.MessageConverter.getMessage;
+
+public enum MenuType {
+    EXIT,
+    CREATE,
+    LIST,
+    BLACKLIST,
+    VOUCHER,
+    CUSTOMER,
+    BACK;
+    private static final Logger logger = LoggerFactory.getLogger(MenuType.class);
+    public static MenuType of(String menuType){
+        try {
+            return MenuType.valueOf(menuType.toUpperCase());
+        }catch (RuntimeException e){
+            logger.error("Error Message = {}", e.getMessage());
+            throw ExceptionHandler.err(getMessage("NOT_EXIST_MENU.MSG"));
+        }
+    }
+}
