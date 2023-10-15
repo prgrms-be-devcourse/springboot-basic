7f4679f64fd9aacb1c4e31e6609e32a43cac11e0
kimnamgyu <lethargic.hood@gmail.com>
2023-10-15 21:48:40 +0900

    Refactor : 기능 Repository로 이관 및 load 기능 추가

--- src/main/java/org/prgrms/vouchermanagement/service/VoucherService.java
+++ src/main/java/org/prgrms/vouchermanagement/service/VoucherService.java
@@ -15,30 +15,15 @@ public class VoucherService {
 
     public VoucherService(VoucherRepository voucherRepository) {
         this.voucherRepository = voucherRepository;
+        voucherRepository.load();
     }
 
     public void createVoucher(PolicyStatus policy, long amountOrPercent) {
-        UUID uuid = UUID.randomUUID();
-        DiscountPolicy discountPolicy = null;
-        if (policy == PolicyStatus.FIXED) {
-            discountPolicy = new FixedAmountVoucher(uuid, amountOrPercent, policy);
-        } else if (policy == PolicyStatus.PERCENT) {
-            validateAmountOrPercentRange(amountOrPercent);
-            discountPolicy = new PercentDiscountVoucher(uuid, amountOrPercent, policy);
-        }
-
-        Voucher voucher = new Voucher(uuid, discountPolicy);
-        voucherRepository.create(voucher);
+        UUID voucherId = UUID.randomUUID();
+        voucherRepository.create(voucherId, amountOrPercent, policy);
     }
 
     public List<Voucher> voucherLists() {
         return voucherRepository.voucherLists();
     }
-
-    private void validateAmountOrPercentRange(long amountOrPercent) {
-        if (amountOrPercent < 0 || amountOrPercent > 100) {
-            throw new InvalidRangeException("PercentDiscountPolicy는 0~100 사이의 값을 가져야 합니다.");
-        }
-    }
-
 }
