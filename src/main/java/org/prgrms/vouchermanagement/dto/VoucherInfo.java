64ed06f9c6eaadb14aa376b52be29e5196be9e43
kimnamgyu <lethargic.hood@gmail.com>
2023-10-15 15:40:11 +0900

    Feat : 바우처 필드를 전달하는 바우처 dto

---
+++ src/main/java/org/prgrms/vouchermanagement/dto/VoucherInfo.java
@@ -0,0 +1,7 @@
+package org.prgrms.vouchermanagement.dto;
+
+import org.prgrms.vouchermanagement.voucher.PolicyStatus;
+
+public record VoucherInfo(
+        PolicyStatus policy,
+        long amountOrPercent) {}
