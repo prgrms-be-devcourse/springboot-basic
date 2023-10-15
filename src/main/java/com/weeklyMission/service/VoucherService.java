37487514af1a77eab032e5c909cf90f8f21b3a02
kkangh00n <kig2454@gmail.com>
2023-10-16 00:09:36 +0900

    feat: Voucher 서비스

---
+++ src/main/java/com/weeklyMission/service/VoucherService.java
@@ -0,0 +1,22 @@
+package com.weeklyMission.service;
+
+import com.weeklyMission.domain.Voucher;
+import com.weeklyMission.repository.VoucherRepository;
+import java.util.List;
+
+public class VoucherService {
+
+    private final VoucherRepository voucherRepository;
+
+    public VoucherService(VoucherRepository voucherRepository) {
+        this.voucherRepository = voucherRepository;
+    }
+
+    public Voucher createVoucher(Voucher voucher){
+        return voucherRepository.createVoucher(voucher);
+    }
+
+    public List<Voucher> getVoucherList(){
+        return voucherRepository.getVoucherList();
+    }
+}
