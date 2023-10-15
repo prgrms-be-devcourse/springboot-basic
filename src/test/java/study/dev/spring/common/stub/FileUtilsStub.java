67c4b712cfdfeba806ac33fcd901ad599e20613c
EunChanNam <namhm23@gmail.com>
2023-10-15 23:38:39 +0900

    feat: VoucherRepository 에 findAll 메소드를 추가 구현한다

--- src/test/java/study/dev/spring/common/stub/FileUtilsStub.java
+++ src/test/java/study/dev/spring/common/stub/FileUtilsStub.java
@@ -1,11 +1,9 @@
 package study.dev.spring.common.stub;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
 
 import study.dev.spring.common.utils.FileUtils;
-import study.dev.spring.voucher.domain.VoucherType;
-import study.dev.spring.voucher.infrastructure.dto.VoucherInfo;
 
 public class FileUtilsStub implements FileUtils {
 
@@ -16,14 +14,7 @@ public class FileUtilsStub implements FileUtils {
 
 	@Override
 	public <T> List<Object> readFile(final String filePath, final Class<T> type) {
-		return List.of(
-			new VoucherInfo(
-				UUID.randomUUID(),
-				"voucher",
-				VoucherType.FIXED,
-				1000
-			)
-		);
+		return new ArrayList<>();
 	}
 
 	@Override
