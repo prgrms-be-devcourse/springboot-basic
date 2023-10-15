d0bc9c1e8d854d6d601e85f76bf6c63b06ba3f0b
park0jae <okvv26@naver.com>
2023-10-16 01:54:46 +0900

    feat : 컨트롤러 반환 포맷 Response 구현 및 VoucherController 구현

---
+++ voucher/src/main/java/com/zerozae/voucher/common/response/Response.java
@@ -0,0 +1,31 @@
+package com.zerozae.voucher.common.response;
+
+import com.zerozae.voucher.common.message.MessageConverter;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+public class Response<T> {
+    private boolean isSuccess;
+    private String message;
+    private List<T> data;
+
+    public Response(boolean isSuccess, String message){
+        this.isSuccess = isSuccess;
+        this.message = message;
+    }
+    public Response(boolean isSuccess, List<T> data){
+        this.isSuccess = isSuccess;
+        this.data = data;
+    }
+    public static Response success(){
+        return new Response(true, MessageConverter.getMessage("SUCCESS.MSG"));
+    }
+    public static Response failure(String message){
+        return new Response(false, message);
+    }
+    public static <T> Response success(List<T> data) {
+        return new Response(true,  data);
+    }
+}
