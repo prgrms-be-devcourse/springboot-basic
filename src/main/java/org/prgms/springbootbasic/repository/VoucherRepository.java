31c6f97d80a696aec2aa3fbc6914d906287e4542
ZZAMBAs <rksidksrksi@naver.com>
2023-10-15 16:31:46 +0900

    Feat: Voucher의 CRUD를 담당하는 VoucherRepository 인터페이스를 추가한다.

---
+++ src/main/java/org/prgms/springbootbasic/repository/VoucherRepository.java
@@ -0,0 +1,12 @@
+package org.prgms.springbootbasic.repository;
+
+import org.prgms.springbootbasic.domain.Voucher;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface VoucherRepository {
+    Voucher findById(UUID voucherId);
+    List<Voucher> findAll();
+    Voucher create(int type, int val);
+}
