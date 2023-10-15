4714b73523a1bd19d9014fb0771183d22cac92fc
kimnamgyu <lethargic.hood@gmail.com>
2023-10-15 21:48:54 +0900

    Feat : load() 메서드 추가

--- src/main/java/org/prgrms/vouchermanagement/repository/VoucherRepository.java
+++ src/main/java/org/prgrms/vouchermanagement/repository/VoucherRepository.java
@@ -1,12 +1,15 @@
 package org.prgrms.vouchermanagement.repository;
 
+import org.prgrms.vouchermanagement.voucher.PolicyStatus;
 import org.prgrms.vouchermanagement.voucher.Voucher;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface VoucherRepository {
-    public void create(Voucher voucher);
+    public void load();
+
+    public void create(UUID voucherId, long amountOrPercent, PolicyStatus policy);
 
     public Voucher getById(UUID voucherId);
 
