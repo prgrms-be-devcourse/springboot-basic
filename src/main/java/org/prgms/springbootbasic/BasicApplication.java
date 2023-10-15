924971bc374d214b22f417349e8e2f65034e6474
ZZAMBAs <rksidksrksi@naver.com>
2023-10-15 19:45:48 +0900

    Feat: 최초 실행 main 메서드에서 애플리케이션을 실행한다.

--- src/main/java/org/prgms/springbootbasic/BasicApplication.java
+++ src/main/java/org/prgms/springbootbasic/BasicApplication.java
@@ -1,13 +1,17 @@
 package org.prgms.springbootbasic;
 
+import org.prgms.springbootbasic.common.Console;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class BasicApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BasicApplication.class, args);
+		ConfigurableApplicationContext applicationContext = SpringApplication.run(BasicApplication.class, args);
+		Console console = applicationContext.getBean(Console.class);
+		console.run();
 	}
 
 }
