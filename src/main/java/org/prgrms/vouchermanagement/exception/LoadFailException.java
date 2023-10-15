60f2ab6bc8b2e9f80b13f0c992cfa88e16921f14
kimnamgyu <lethargic.hood@gmail.com>
2023-10-15 20:14:10 +0900

    Feat : 파일로부터 가져오지 못할 경우 발생하는 예외

---
+++ src/main/java/org/prgrms/vouchermanagement/exception/LoadFailException.java
@@ -0,0 +1,7 @@
+package org.prgrms.vouchermanagement.exception;
+
+public class LoadFailException extends RuntimeException {
+    public LoadFailException(String message) {
+        super(message);
+    }
+}
