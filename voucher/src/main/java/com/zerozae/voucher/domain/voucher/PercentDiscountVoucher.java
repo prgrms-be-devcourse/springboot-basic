c994935c33c248c2b66f29bf4f4a454b2674c787
park0jae <okvv26@naver.com>
2023-10-16 01:43:28 +0900

    feat : Voucher 인터페이스 및 Fixed, Percent 구현체 구현

---
+++ voucher/src/main/java/com/zerozae/voucher/domain/voucher/PercentDiscountVoucher.java
@@ -0,0 +1,25 @@
+package com.zerozae.voucher.domain.voucher;
+
+import lombok.Getter;
+
+import java.util.UUID;
+
+@Getter
+public class PercentDiscountVoucher implements Voucher{
+    private final UUID voucherId;
+    private final long discount;
+    private final VoucherType voucherType;
+    private UseStatusType useStatusType;
+    public PercentDiscountVoucher(Long discount) {
+        this.voucherId = UUID.randomUUID();
+        this.discount = discount;
+        this.voucherType = VoucherType.PERCENT;
+        this.useStatusType = UseStatusType.AVAILABLE;
+    }
+    public PercentDiscountVoucher(UUID voucherId, Long discount, UseStatusType useStatusType){
+        this.voucherId = voucherId;
+        this.discount = discount;
+        this.voucherType = VoucherType.PERCENT;
+        this.useStatusType = useStatusType;
+    }
+}
---
+++ voucher/src/main/java/com/zerozae/voucher/domain/voucher/VoucherType.java
@@ -0,0 +1,20 @@
+package com.zerozae.voucher.domain.voucher;
+
+import com.zerozae.voucher.common.message.MessageConverter;
+import com.zerozae.voucher.exception.ExceptionHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public enum VoucherType {
+    FIXED,
+    PERCENT;
+    private static final Logger logger = LoggerFactory.getLogger(VoucherType.class);
+    public static VoucherType of(String voucherType){
+        try{
+            return VoucherType.valueOf(voucherType.toUpperCase());
+        }catch (RuntimeException e){
+            logger.error("Error Message = {}", e.getMessage());
+            throw ExceptionHandler.err(MessageConverter.getMessage("INVALID_VOUCHER_TYPE.MSG"));
+        }
+    }
+}
