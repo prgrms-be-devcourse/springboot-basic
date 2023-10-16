68591d9d23dc04daae6e3dfe119c257252e54182
EunChanNam <namhm23@gmail.com>
2023-10-16 23:00:26 +0900

    feat: 콘솔 로그 설정 및 예외처리시 에외로그 추가하며 파일에 기록하는 설정 추가

--- src/main/java/study/dev/spring/app/exception/VoucherExceptionHandler.java
+++ src/main/java/study/dev/spring/app/exception/VoucherExceptionHandler.java
@@ -1,9 +1,14 @@
 package study.dev.spring.app.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import study.dev.spring.app.VoucherApplicationRunner;
 import study.dev.spring.common.exception.CommonException;
 
 public class VoucherExceptionHandler extends VoucherApplicationRunner {
+
+	private static final Logger logger = LoggerFactory.getLogger(VoucherExceptionHandler.class);
 
 	private final VoucherApplicationRunner target;
 
@@ -19,10 +24,13 @@ public class VoucherExceptionHandler extends VoucherApplicationRunner {
 		try {
 			target.run();
 		} catch (CommonException e) {
-			// todo : 예외 로그 추가
+			logger.error("custom exception : ", e);
 			System.out.println(e.getMessage());
 		} catch (ExitException e) {
 			System.exit(0);
+		} catch (Exception e) {
+			logger.error("system exception : ", e);
+			System.out.println("애플리케이션 내부 오류입니다. 다시 시도해주세요");
 		}
 	}
 }
