ff6278e819ef97b6cd5e880b37ae235b39312fde
hyoguoo <hyoguoo@gmail.com>
2023-10-16 22:47:24 +0900

    feat: InMemoryUserRepository 초기 데이터 생성
    - 등록 로직이 없어 csv와 동일하게 추가

--- src/main/java/devcourse/springbootbasic/repository/user/InMemoryUserRepository.java
+++ src/main/java/devcourse/springbootbasic/repository/user/InMemoryUserRepository.java
@@ -1,6 +1,7 @@
 package devcourse.springbootbasic.repository.user;
 
 import devcourse.springbootbasic.domain.user.User;
+import jakarta.annotation.PostConstruct;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Repository;
 
@@ -21,5 +22,13 @@ public class InMemoryUserRepository implements UserRepository {
                 .stream()
                 .filter(User::isBlacklisted)
                 .toList();
+    }
+
+    @PostConstruct
+    public void init() {
+        User user1 = User.createUser(UUID.randomUUID(), "not blacklist ogu", false);
+        User user2 = User.createUser(UUID.randomUUID(), "blacklist platypus", true);
+        userDatabase.put(user1.getId(), user1);
+        userDatabase.put(user2.getId(), user2);
     }
 }
