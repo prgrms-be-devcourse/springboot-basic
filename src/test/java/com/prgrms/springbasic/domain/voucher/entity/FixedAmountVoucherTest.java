86921399c06d8dc909070acad9e19edb9270995b
byulcode <bybit0408@gmail.com>
2023-10-16 20:13:57 +0900

    test : FixedAmountVoucher 테스트코드 구현

---
+++ src/test/java/com/prgrms/springbasic/domain/voucher/entity/FixedAmountVoucherTest.java
@@ -0,0 +1,31 @@
+package com.prgrms.springbasic.domain.voucher.entity;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FixedAmountVoucherTest {
+
+    @Test
+    @DisplayName("주어진 금액만큼 할인")
+    void testDiscount() {
+        Voucher sut = new FixedAmountVoucher(UUID.randomUUID(), "fixed", 100);
+        assertEquals(900, sut.discount(1000));
+    }
+
+    @Test
+    @DisplayName("할인된 금액은 마이너스가 될 수 없다")
+    void testMinusDiscountedValue() {
+        Voucher sut = new FixedAmountVoucher(UUID.randomUUID(), "fixed", 100);
+        assertEquals(0, sut.discount(90));
+    }
+
+    @Test
+    @DisplayName("할인 금액은 마이너스가 될 수 없다")
+    void testDiscountWithMinus() {
+        assertThrows(IllegalArgumentException.class, () -> FixedAmountVoucher.create("fixed", -100));
+    }
+}
