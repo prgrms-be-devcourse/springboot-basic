33d5d4ad38272b5e06acada73cde77c8c9afd58e
EunChanNam <namhm23@gmail.com>
2023-10-16 17:42:37 +0900

    feat: 바우처 ExceptionHandler 구현

---
+++ src/main/java/study/dev/spring/app/exception/VoucherExceptionHandler.java
@@ -0,0 +1,28 @@
+package study.dev.spring.app.exception;
+
+import study.dev.spring.app.VoucherApplicationRunner;
+import study.dev.spring.common.exception.CommonException;
+
+public class VoucherExceptionHandler extends VoucherApplicationRunner {
+
+	private final VoucherApplicationRunner target;
+
+	public VoucherExceptionHandler(
+		final VoucherApplicationRunner target
+	) {
+		super(null, null, null);
+		this.target = target;
+	}
+
+	@Override
+	public void run() {
+		try {
+			target.run();
+		} catch (CommonException e) {
+			// todo : 예외 로그 추가
+			System.out.println(e.getMessage());
+		} catch (ExitException e) {
+			System.exit(0);
+		}
+	}
+}
