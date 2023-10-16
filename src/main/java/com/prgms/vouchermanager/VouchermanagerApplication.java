06dfbed09167a79e2b8a26a5f66cf188a58e5ed6
YongNyeo <won05121@gmail.com>
2023-10-17 01:22:44 +0900

    init: 프로젝트 초기 설정

---
+++ src/main/java/com/prgms/vouchermanager/VouchermanagerApplication.java
@@ -0,0 +1,13 @@
+package com.prgms.vouchermanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VouchermanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VouchermanagerApplication.class, args);
+	}
+
+}
