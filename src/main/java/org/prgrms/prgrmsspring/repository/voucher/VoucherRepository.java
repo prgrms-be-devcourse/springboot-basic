b0edf5ced43a26872abaf9c7d97b9a4992e75353
voidmelody <sss9073@naver.com>
2023-10-16 22:47:48 +0900

    Feat: "Voucher Repository 인터페이스 선언"

---
+++ src/main/java/org/prgrms/prgrmsspring/repository/voucher/VoucherRepository.java
@@ -0,0 +1,17 @@
+package org.prgrms.prgrmsspring.repository.voucher;
+
+import org.prgrms.prgrmsspring.entity.voucher.Voucher;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface VoucherRepository {
+    Voucher insert(Voucher voucher);
+
+    List<Voucher> findAll();
+
+    Optional<Voucher> findById(UUID voucherId);
+}
