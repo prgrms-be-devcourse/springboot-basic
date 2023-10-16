e59a01ac6eb55983b4dc89b942da6da4bdf1d8bc
hs12 <loveanna1228@gmail.com>
2023-10-16 17:07:42 +0900

    [Feat] Customer 엔티티 추가

---
+++ src/main/java/com/prgrms/voucher_manage/domain/customer/entity/Customer.java
@@ -0,0 +1,12 @@
+package com.prgrms.voucher_manage.domain.customer.entity;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.UUID;
+
+@RequiredArgsConstructor @Getter
+public class Customer {
+    private final UUID customerId;
+    private final String name;
+}
