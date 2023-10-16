aad6d2cc9ee096263e55c9ef097f5e775df6e814
hyoguoo <hyoguoo@gmail.com>
2023-10-16 13:45:46 +0900

    feat: Console에서 출력 될 상수 유틸리티 클래스 추가

---
+++ src/main/java/devcourse/springbootbasic/commandline/console/constant/ConsoleConstants.java
@@ -0,0 +1,10 @@
+package devcourse.springbootbasic.commandline.console.constant;
+
+public final class ConsoleConstants {
+
+    public static final String VOUCHER = "VOUCHER";
+
+    private ConsoleConstants() {
+        throw new AssertionError();
+    }
+}
