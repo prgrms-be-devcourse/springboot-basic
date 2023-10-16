ad74ad269fd1e9abeb34dd322cdc894c5dd8ea13
EunChanNam <namhm23@gmail.com>
2023-10-16 17:43:10 +0900

    feat: 바우처 애플리케이션 구현

--- src/main/java/study/dev/spring/VoucherApplication.java
+++ src/main/java/study/dev/spring/VoucherApplication.java
@@ -2,11 +2,20 @@ package study.dev.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import study.dev.spring.app.VoucherApplicationRunner;
 
 @SpringBootApplication
 public class VoucherApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(VoucherApplication.class, args);
+		ApplicationContext applicationContext = SpringApplication.run(VoucherApplication.class, args);
+
+		VoucherApplicationRunner runner = applicationContext.getBean(VoucherApplicationRunner.class);
+
+		while (true) {
+			runner.run();
+		}
 	}
 }
