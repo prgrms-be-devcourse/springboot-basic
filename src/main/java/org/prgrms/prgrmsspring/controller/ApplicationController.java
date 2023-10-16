6b8bf9572ac9e9bbd196c12e756aa7db00c0a75e
voidmelody <sss9073@naver.com>
2023-10-16 22:45:43 +0900

    Feat: "Controller 기능 구현"

---
+++ src/main/java/org/prgrms/prgrmsspring/controller/ApplicationController.java
@@ -0,0 +1,53 @@
+package org.prgrms.prgrmsspring.controller;
+
+import org.prgrms.prgrmsspring.domain.Command;
+import org.prgrms.prgrmsspring.entity.user.User;
+import org.prgrms.prgrmsspring.entity.voucher.Voucher;
+import org.prgrms.prgrmsspring.service.UserService;
+import org.prgrms.prgrmsspring.service.VoucherService;
+import org.prgrms.prgrmsspring.view.CommandLineView;
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
+
+@Controller
+public class ApplicationController {
+
+    private final VoucherService voucherService;
+    private final CommandLineView commandLineView;
+    private final UserService userService;
+
+    public ApplicationController(VoucherService voucherService, CommandLineView commandLineView, UserService userService) {
+        this.voucherService = voucherService;
+        this.commandLineView = commandLineView;
+        this.userService = userService;
+    }
+
+    public void start() {
+        Command command = getCommand();
+        command.run(this);
+    }
+
+    public void exit() {
+        voucherService.exit();
+    }
+
+    public void create() {
+        Voucher voucher = commandLineView.createVoucher();
+        voucherService.create(voucher);
+    }
+
+    public void list() {
+        List<Voucher> vouchers = voucherService.list();
+        commandLineView.printAll(vouchers);
+    }
+
+    public void showBlackList() {
+        List<User> users = userService.findAll();
+        commandLineView.printAll(users);
+    }
+
+    public Command getCommand() {
+        return commandLineView.getCommand();
+    }
+}
