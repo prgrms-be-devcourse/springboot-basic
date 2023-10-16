bf90525f53ee51da41dd4f1f68843805dcfcbfc3
hyoguoo <hyoguoo@gmail.com>
2023-10-16 14:17:40 +0900

    feat: command line 기능을 위한 Function Enum 추가

---
+++ src/main/java/devcourse/springbootbasic/commandline/function/Function.java
@@ -0,0 +1,36 @@
+package devcourse.springbootbasic.commandline.function;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.Arrays;
+import java.util.Optional;
+import java.util.function.Consumer;
+
+@RequiredArgsConstructor
+public enum Function {
+
+    EXIT("exit", "Type exit to exit the program.", FunctionHandler::exit),
+    CREATE_VOUCHER("create", "Type create to create a new voucher.", FunctionHandler::createVoucher),
+    LIST_VOUCHERS("list", "Type list to list all vouchers.", FunctionHandler::listAllVouchers);
+
+    private final String functionString;
+    @Getter
+    private final String description;
+    private final Consumer<FunctionHandler> functionConsumer;
+
+    public static Optional<Function> fromString(String functionString) {
+        return Arrays.stream(values())
+                .filter(function -> function.functionString.equals(functionString))
+                .findFirst();
+    }
+
+    public void execute(FunctionHandler functionHandler) {
+        this.functionConsumer.accept(functionHandler);
+    }
+
+    @Override
+    public String toString() {
+        return this.getDescription();
+    }
+}
