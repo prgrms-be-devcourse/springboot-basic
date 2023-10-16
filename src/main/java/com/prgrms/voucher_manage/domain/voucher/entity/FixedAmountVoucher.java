ba8752948961f8325ffcbd4e7210addd8ab62ccd
hs12 <loveanna1228@gmail.com>
2023-10-16 09:17:55 +0900

    [Feat] Voucher 엔티티 생성자 수정

--- src/main/java/com/prgrms/voucher_manage/domain/voucher/entity/FixedAmountVoucher.java
+++ src/main/java/com/prgrms/voucher_manage/domain/voucher/entity/FixedAmountVoucher.java
@@ -1,13 +1,15 @@
 package com.prgrms.voucher_manage.domain.voucher.entity;
 
-import lombok.RequiredArgsConstructor;
-
 import java.util.UUID;
 
-@RequiredArgsConstructor
 public class FixedAmountVoucher implements Voucher {
     private final UUID voucherId;
     private final Long discountPrice;
+
+    public FixedAmountVoucher(Long discountPrice){
+        this.voucherId = UUID.randomUUID();
+        this.discountPrice = discountPrice;
+    }
 
 
     @Override
--- src/main/java/com/prgrms/voucher_manage/domain/voucher/entity/PercentAmountVoucher.java
+++ src/main/java/com/prgrms/voucher_manage/domain/voucher/entity/PercentAmountVoucher.java
@@ -1,15 +1,15 @@
 package com.prgrms.voucher_manage.domain.voucher.entity;
 
-import lombok.RequiredArgsConstructor;
-
 import java.util.UUID;
 
-@RequiredArgsConstructor
 public class PercentAmountVoucher implements Voucher {
-    private static final Long MIN_DISCOUNT_PERCENT = 0L;
-    private static final Long MAX_DISCOUNT_PERCENT = 100L;
     private final UUID voucherId;
     private final Long discountPercent;
+
+    public PercentAmountVoucher(Long discountPercent){
+        this.voucherId = UUID.randomUUID();
+        this.discountPercent = discountPercent;
+    }
 
     @Override
     public UUID getVoucherId() {
@@ -21,7 +21,4 @@ public class PercentAmountVoucher implements Voucher {
         return discountPercent;
     }
 
-    public boolean isValidDiscountPercent() {
-        return (this.discountPercent >= MIN_DISCOUNT_PERCENT) && (this.discountPercent <= MAX_DISCOUNT_PERCENT);
-    }
 }
