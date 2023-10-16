4afc824487eef783fd863715f4be1a3b3e61113f
byulcode <bybit0408@gmail.com>
2023-10-16 20:23:31 +0900

    refactor : 예외 처리 로직 추가

--- src/main/java/com/prgrms/springbasic/common/VoucherRunner.java
+++ src/main/java/com/prgrms/springbasic/common/VoucherRunner.java
@@ -1,12 +1,16 @@
 package com.prgrms.springbasic.common;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
 
 
 @Component
 public class VoucherRunner implements CommandLineRunner {
     private final MenuHandler menuHandler;
+    private final Logger logger = LoggerFactory.getLogger(VoucherRunner.class);
 
     public VoucherRunner(MenuHandler menuHandler) {
         this.menuHandler = menuHandler;
@@ -17,12 +21,16 @@ public class VoucherRunner implements CommandLineRunner {
         boolean isRunning = true;
         MenuType menuType;
         while (isRunning) {
-            menuType = MenuType.find(menuHandler.chooseMode());
-            switch (menuType) {
-                case EXIT -> isRunning = menuHandler.exit();
-                case CREATE -> menuHandler.createVoucher();
-                case LIST -> menuHandler.showAllVouchers();
-                case BLACK_LIST -> menuHandler.showAllBlackLists();
+            try {
+                menuType = MenuType.find(menuHandler.chooseMode());
+                switch (menuType) {
+                    case EXIT -> isRunning = menuHandler.exit();
+                    case CREATE -> menuHandler.createVoucher();
+                    case LIST -> menuHandler.showAllVouchers();
+                    case BLACK_LIST -> menuHandler.showAllBlackLists();
+                }
+            } catch (Exception e) {
+                logger.error(e.getMessage());
             }
         }
     }
