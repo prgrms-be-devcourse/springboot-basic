c84dc019a1ad416952a692247b8b53e706ad53af
ZZAMBAs <rksidksrksi@naver.com>
2023-10-16 22:52:53 +0900

    Feat: VoucherType에 displayName을 추가하고 BiFunction을 사용하도록 수정한다.

--- src/main/java/org/prgms/springbootbasic/common/VoucherType.java
+++ src/main/java/org/prgms/springbootbasic/common/VoucherType.java
@@ -1,24 +1,29 @@
 package org.prgms.springbootbasic.common;
 
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.prgms.springbootbasic.domain.FixedAmountVoucher;
 import org.prgms.springbootbasic.domain.PercentDiscountVoucher;
 import org.prgms.springbootbasic.domain.Voucher;
 
 import java.util.UUID;
+import java.util.function.BiFunction;
 import java.util.function.Function;
 
 @Slf4j
 public enum VoucherType {
-    FIXED_AMOUNT(1, val -> new FixedAmountVoucher(UUID.randomUUID(), val)),
-    PERCENT_DISCOUNT(2, val -> new PercentDiscountVoucher(UUID.randomUUID(), val));
+    FIXED_AMOUNT(1, FixedAmountVoucher::new, "FixedAmountVoucher"),
+    PERCENT_DISCOUNT(2, PercentDiscountVoucher::new, "PercentDiscountVoucher");
 
     private final int seq;
-    private final Function<Long, Voucher> consumer;
+    private final BiFunction<UUID, Long, Voucher> biFunction;
+    @Getter
+    private final String displayName;
 
-    VoucherType(int seq, Function<Long, Voucher> consumer) {
+    VoucherType(int seq, BiFunction<UUID, Long, Voucher> biFunction, String displayName) {
         this.seq = seq;
-        this.consumer = consumer;
+        this.biFunction = biFunction;
+        this.displayName = displayName;
     }
 
     public static VoucherType getTypeFromSeq(int seq){
@@ -31,6 +36,10 @@ public enum VoucherType {
     }
 
     public Voucher create(long val){
-        return this.consumer.apply(val);
+        return this.biFunction.apply(UUID.randomUUID(), val);
+    }
+
+    public Voucher create(UUID uuid, long val){
+        return this.biFunction.apply(uuid, val);
     }
 }
