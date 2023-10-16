99dc5b58fec4be0bc735d475c4e418109cb4890e
ZZAMBAs <rksidksrksi@naver.com>
2023-10-16 22:49:34 +0900

    Feat: 파일로 관리하는 FileVoucherRepository를 생성한다.

---
+++ src/main/java/org/prgms/springbootbasic/repository/FileVoucherRepository.java
@@ -0,0 +1,61 @@
+package org.prgms.springbootbasic.repository;
+
+import lombok.extern.slf4j.Slf4j;
+import org.prgms.springbootbasic.common.CsvVoucherFileManager;
+import org.prgms.springbootbasic.common.VoucherType;
+import org.prgms.springbootbasic.domain.Voucher;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Repository;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+@Profile({"dev", "prod"})
+@Primary
+@Slf4j
+public class FileVoucherRepository implements VoucherRepository {
+    private final CsvVoucherFileManager csvVoucherFileManager;
+    private final VoucherRepository voucherRepository;
+
+    public FileVoucherRepository(CsvVoucherFileManager csvVoucherFileManager, MemoryVoucherRepository voucherRepository) {
+        log.debug("FileVoucherRepository started.");
+
+        this.csvVoucherFileManager = csvVoucherFileManager;
+        this.voucherRepository = voucherRepository;
+    }
+
+    @Override
+    public Voucher findById(UUID voucherId) {
+        return voucherRepository.findById(voucherId);
+    }
+
+    @Override
+    public List<Voucher> findAll() {
+        return voucherRepository.findAll();
+    }
+
+    @Override
+    public Voucher create(VoucherType type, int val) {
+        return voucherRepository.create(type, val);
+    }
+
+    @Override
+    public Voucher create(Voucher voucher) {
+        return voucherRepository.create(voucher);
+    }
+
+    @PostConstruct
+    private void fileRead(){
+        List<Voucher> vouchers = csvVoucherFileManager.read();
+        vouchers.forEach(voucherRepository::create);
+    }
+
+    @PreDestroy
+    private void fileWrite(){
+        csvVoucherFileManager.write(voucherRepository.findAll());
+    }
+}
