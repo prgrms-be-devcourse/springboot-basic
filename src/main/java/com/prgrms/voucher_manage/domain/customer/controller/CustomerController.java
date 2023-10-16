0f8d139058444c3525b48846d8ac89e63f464242
hs12 <loveanna1228@gmail.com>
2023-10-16 20:59:45 +0900

    [Feat] controller 추가

---
+++ src/main/java/com/prgrms/voucher_manage/domain/customer/controller/CustomerController.java
@@ -0,0 +1,14 @@
+package com.prgrms.voucher_manage.domain.customer.controller;
+
+import com.prgrms.voucher_manage.domain.customer.service.CustomerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequiredArgsConstructor
+public class CustomerController {
+    private final CustomerService customerService;
+    public void showBlackListCustomers(){
+        customerService.showCustomerBlackList();
+    }
+}
