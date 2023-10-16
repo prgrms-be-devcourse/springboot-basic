5852e42da7e82eac72dd5a2383de6f8ef54997de
shoeone96 <dlwnddnjs96@gmail.com>
2023-10-16 15:02:36 +0900

    feat: VoucherApplication 생성 실제 서비스가 구동될 VoucherApplication class 생성 및 SpringOrderApplication에 CommandLinner로 연동

---
+++ src/main/java/org/programmers/springorder/VoucherApplication.java
@@ -0,0 +1,10 @@
+package org.programmers.springorder;
+
+import org.springframework.boot.CommandLineRunner;
+
+public class VoucherApplication implements CommandLineRunner {
+    @Override
+    public void run(String... args) {
+
+    }
+}
