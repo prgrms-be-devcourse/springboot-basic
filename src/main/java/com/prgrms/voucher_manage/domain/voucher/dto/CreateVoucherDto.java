84f106b75579aa61c34486d9120be8f285b5685e
hs12 <loveanna1228@gmail.com>
2023-10-16 15:05:39 +0900

    [Feat] DTO 레코드로 변경

--- src/main/java/com/prgrms/voucher_manage/domain/voucher/dto/CreateVoucherDto.java
+++ src/main/java/com/prgrms/voucher_manage/domain/voucher/dto/CreateVoucherDto.java
@@ -1,17 +1,10 @@
 package com.prgrms.voucher_manage.domain.voucher.dto;
 
 import com.prgrms.voucher_manage.console.VoucherType;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
-@RequiredArgsConstructor
-@Getter
-public class CreateVoucherDto {
+public record CreateVoucherDto(VoucherType voucherType, Long discountAmount) {
     private static final Long MIN_DISCOUNT_PERCENT = 0L;
     private static final Long MAX_DISCOUNT_PERCENT = 0L;
-
-    private final VoucherType voucherType;
-    private final Long discountAmount;
 
     public boolean isValidPercent() {
         return (discountAmount >= MIN_DISCOUNT_PERCENT) && (discountAmount <= MAX_DISCOUNT_PERCENT);
