71a83cbc09961d27e20d337991def6b6564dc4a2
EomYerim <djadpfa1002@naver.com>
2023-10-16 17:25:34 +0900

    feat: VoucherRepository 생성 바우처 저장, 전체 조회, 번호로 조회, 초기화 메서드 생성

---
+++ src/main/java/org/programmers/springorder/repository/VoucherRepository.java
@@ -0,0 +1,14 @@
+package org.programmers.springorder.repository;
+
+import org.programmers.springorder.model.Voucher;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface VoucherRepository {
+    UUID save(Voucher voucher);
+    List<Voucher> findAll();
+    Optional<Voucher> findById(UUID voucherId);
+    void clear();
+}
