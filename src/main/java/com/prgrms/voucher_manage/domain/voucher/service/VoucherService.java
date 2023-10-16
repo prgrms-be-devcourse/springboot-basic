da03f51b34d6d8577cfbe1cbb6bf633e7f581aef
hs12 <loveanna1228@gmail.com>
2023-10-16 09:15:20 +0900

    [Feat] VoucherService 추가

---
+++ src/main/java/com/prgrms/voucher_manage/domain/voucher/service/VoucherService.java
@@ -0,0 +1,8 @@
+package com.prgrms.voucher_manage.domain.voucher.service;
+
+import com.prgrms.voucher_manage.domain.voucher.dto.CreateVoucherDto;
+
+public interface VoucherService {
+    void createVoucher(CreateVoucherDto dto);
+    void showVoucherList();
+}
---
+++ src/main/java/com/prgrms/voucher_manage/domain/voucher/service/VoucherServiceImpl.java
@@ -0,0 +1,40 @@
+package com.prgrms.voucher_manage.domain.voucher.service;
+
+import com.prgrms.voucher_manage.console.OutputUtil;
+import com.prgrms.voucher_manage.console.VoucherType;
+import com.prgrms.voucher_manage.domain.voucher.dto.CreateVoucherDto;
+import com.prgrms.voucher_manage.domain.voucher.entity.FixedAmountVoucher;
+import com.prgrms.voucher_manage.domain.voucher.entity.PercentAmountVoucher;
+import com.prgrms.voucher_manage.domain.voucher.entity.Voucher;
+import com.prgrms.voucher_manage.domain.voucher.repository.VoucherRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class VoucherServiceImpl implements VoucherService {
+    private final VoucherRepository voucherRepository;
+    private final OutputUtil outputUtil;
+
+
+    @Override
+    public void createVoucher(CreateVoucherDto dto) {
+        VoucherType voucherType = dto.getVoucherType();
+        switch (voucherType) {
+            case FIXED
+                    -> voucherRepository.insert(new FixedAmountVoucher(dto.getDiscountAmount()));
+            case PERCENT
+                    -> voucherRepository.insert(new PercentAmountVoucher(dto.getDiscountAmount()));
+        }
+    }
+
+    @Override
+    public void showVoucherList() {
+        List<Voucher> vouchers = voucherRepository.findAll();
+        for (Voucher voucher : vouchers){
+            outputUtil.printVoucherInfo(voucher);
+        }
+    }
+}
