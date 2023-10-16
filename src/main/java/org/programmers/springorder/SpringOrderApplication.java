4c4f3dc46e6986c3b13e5bc34342b1221d9efe89
shoeone96 <dlwnddnjs96@gmail.com>
2023-10-16 14:39:38 +0900

    init: 초기 설정

---
+++ src/main/java/org/programmers/springorder/SpringOrderApplication.java
@@ -0,0 +1,13 @@
+package org.programmers.springorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringOrderApplication.class, args);
+	}
+
+}
