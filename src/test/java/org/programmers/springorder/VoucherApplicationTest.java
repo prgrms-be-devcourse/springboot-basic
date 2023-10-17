92850484815664af91c0f912c528ed422e9d2610
EomYerim <djadpfa1002@naver.com>
2023-10-17 11:24:20 +0900

    test: Application 구동 테스트 수행

--- src/test/java/org/programmers/springorder/VoucherApplicationTest.java
+++ src/test/java/org/programmers/springorder/VoucherApplicationTest.java
@@ -2,16 +2,23 @@ package org.programmers.springorder;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+@SpringBootTest
 class VoucherApplicationTest {
+
+    @Autowired
+    private VoucherApplication voucherApplication;
 
     @Test
     @DisplayName("Voucher Application 구동 테스트")
     void startVoucherApplicationTest() {
-        VoucherApplication voucherApplication = new VoucherApplication();
         voucherApplication.run();
     }
+
+    //TODO 입출력 관련 테스트 추가
 
 }
