de480ef5bcc5bfd8333067c47fa3e8b1f43cab90
hs12 <loveanna1228@gmail.com>
2023-10-17 12:06:28 +0900

    [Feat] 어플리케이션 실행 예외 처리 삭제

--- src/main/java/com/prgrms/voucher_manage/VoucherApplication.java
+++ src/main/java/com/prgrms/voucher_manage/VoucherApplication.java
@@ -6,11 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class VoucherApplication {
     public static void main(String[] args) {
-        try {
-            SpringApplication.run(VoucherApplication.class, args);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
+        SpringApplication.run(VoucherApplication.class, args);
     }
 }
