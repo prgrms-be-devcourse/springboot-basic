05a827920f791d333359b6b1d11c7c2b1227d8b8
Hanna <hanna@trdst.com>
2023-10-17 16:24:33 +0900

    fix: PercentDiscountVoucher amount validate 로직 추가

--- src/main/java/com/programmers/vouchermanagement/domain/voucher/PercentDiscountVoucher.java
+++ src/main/java/com/programmers/vouchermanagement/domain/voucher/PercentDiscountVoucher.java
@@ -1,17 +1,24 @@
 package com.programmers.vouchermanagement.domain.voucher;
 
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 
 import java.util.UUID;
 
 @Getter
 @ToString
-@RequiredArgsConstructor
 public class PercentDiscountVoucher implements Voucher {
     private final UUID id;
     private final long amount;
+
+    public PercentDiscountVoucher(UUID id, long amount) {
+        if(amount < 0 || amount > 100) {
+            throw new IllegalArgumentException("Percent discount amount should be between 0 and 100");
+        }
+
+        this.id = id;
+        this.amount = amount;
+    }
 
     @Override
     public VoucherType getType() {
