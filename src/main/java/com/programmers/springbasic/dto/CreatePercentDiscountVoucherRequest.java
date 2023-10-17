49b0bf10b5afc9c8e0cb1783378f2f65b31db204
ebcho <joeunbi514@naver.com>
2023-10-17 10:36:44 +0900

    refactor: 체크할 범위 static final 상수로 변경

--- src/main/java/com/programmers/springbasic/dto/CreatePercentDiscountVoucherRequest.java
+++ src/main/java/com/programmers/springbasic/dto/CreatePercentDiscountVoucherRequest.java
@@ -6,10 +6,12 @@ import com.programmers.springbasic.entity.voucher.PercentDiscountVoucher;
 import com.programmers.springbasic.enums.ErrorCode;
 
 public class CreatePercentDiscountVoucherRequest {
+	private static final long MINIMUM_PERCENT = 0;
+	private static final long MAXIMUM_PERCENT = 100;
 	private final long percent;
 
 	public CreatePercentDiscountVoucherRequest(long percent) {
-		if (percent <= 0 || percent >= 100) {
+		if (percent < MINIMUM_PERCENT || percent > MAXIMUM_PERCENT) {
 			throw new IllegalArgumentException(ErrorCode.PERCENT_OUT_OF_RANGE.getMessage());
 		}
 		this.percent = percent;
