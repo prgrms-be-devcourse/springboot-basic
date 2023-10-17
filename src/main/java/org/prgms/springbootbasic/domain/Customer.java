a16dfae342ac380860e79dde7fde68fee6c56dfd
ZZAMBAs <rksidksrksi@naver.com>
2023-10-17 17:14:53 +0900

    Feat: Customer 클래스를 생성한다.

---
+++ src/main/java/org/prgms/springbootbasic/domain/Customer.java
@@ -0,0 +1,22 @@
+package org.prgms.springbootbasic.domain;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public class Customer {
+    private UUID id;
+    private String name;
+    private String email;
+    private LocalDateTime createdAt;
+    private LocalDateTime lastLoginAt;
+    private boolean isBlacked;
+
+    public Customer(UUID id, String name, String email, LocalDateTime createdAt, LocalDateTime lastLoginAt, boolean isBlacked) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.createdAt = createdAt;
+        this.lastLoginAt = lastLoginAt;
+        this.isBlacked = isBlacked;
+    }
+}
