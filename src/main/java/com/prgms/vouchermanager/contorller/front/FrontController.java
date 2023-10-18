e0c05c6e533b6a8fe8281d7af39b5d8ad10a6688
YongNyeo <won05121@gmail.com>
2023-10-19 03:20:27 +0900

    feat: 최상단 컨트롤러 생성

---
+++ src/main/java/com/prgms/vouchermanager/contorller/front/FrontController.java
@@ -0,0 +1,58 @@
+package com.prgms.vouchermanager.contorller.front;
+
+
+import com.prgms.vouchermanager.contorller.customer.CustomerController;
+import com.prgms.vouchermanager.contorller.voucher.VoucherController;
+import com.prgms.vouchermanager.util.io.ConsoleInput;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+
+import static com.prgms.vouchermanager.contorller.front.FrontMenuType.*;
+
+
+@Controller
+public class FrontController {
+
+    private final ConsoleInput consoleInput;
+
+    private final VoucherController voucherController;
+
+    private final CustomerController customerController;
+
+    private final static Logger logger = LoggerFactory.getLogger(FrontController.class);
+
+    public FrontController(ConsoleInput consoleInput, VoucherController voucherController, CustomerController customerController) {
+
+        this.consoleInput = consoleInput;
+        this.voucherController = voucherController;
+        this.customerController = customerController;
+
+    }
+
+    public void run() {
+
+        boolean end = false;
+        int menu;
+
+        while (!end) {
+
+            try {
+                menu = consoleInput.inputFrontMenu();
+            } catch (RuntimeException e) {
+                System.out.println(e.getMessage());
+                logger.warn(e.getMessage());
+                continue;
+            }
+
+            if (menu == VOUCHER.getNumber()) {
+                voucherController.run();
+            } else if (menu == CUSTOMER.getNumber()) {
+                customerController.run();
+            } else {
+                end = true;
+            }
+
+        }
+    }
+}
