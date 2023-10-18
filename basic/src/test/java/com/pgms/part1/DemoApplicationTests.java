b4acdcd91b5fb63acb2b6375a6a06dcf6144fd28
KarmaPol <kkhkr98@gmail.com>
2023-10-18 19:23:49 +0900

    chore : 테스트 코드 주석 처리

--- basic/src/test/java/com/pgms/part1/DemoApplicationTests.java
+++ basic/src/test/java/com/pgms/part1/DemoApplicationTests.java
@@ -1,13 +1,10 @@
 package com.pgms.part1;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class DemoApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//@SpringBootTest
+//class DemoApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
