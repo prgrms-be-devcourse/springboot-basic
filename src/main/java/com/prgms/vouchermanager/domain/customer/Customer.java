6dc0f9196c6194251fbdfc4e95ecee4fb9eb7a5b
YongNyeo <won05121@gmail.com>
2023-10-19 03:24:00 +0900

    feat: 고객 도메인 생성

---
+++ src/main/java/com/prgms/vouchermanager/domain/customer/Customer.java
@@ -0,0 +1,32 @@
+package com.prgms.vouchermanager.domain.customer;
+
+
+import java.util.UUID;
+
+public class Customer {
+
+    private final Long id;
+
+    private String name;
+
+    private String email;
+
+    public Customer(Long id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
