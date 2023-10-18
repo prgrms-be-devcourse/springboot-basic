2e5229d531371e6c0d6c1426911900452276becb
EomYerim <djadpfa1002@naver.com>
2023-10-19 00:04:05 +0900

    refactor: Console에서 Input과 Output 분리 SRP 원칙에 따라 기존 Console에서의 입출력을 Input과 Output을 사용해 구성으로 리팩토링

--- src/main/java/org/programmers/springorder/console/Console.java
+++ src/main/java/org/programmers/springorder/console/Console.java
@@ -1,5 +1,7 @@
 package org.programmers.springorder.console;
 
+import org.programmers.springorder.consts.ErrorMessage;
+import org.programmers.springorder.consts.Message;
 import org.programmers.springorder.dto.VoucherRequestDto;
 import org.programmers.springorder.dto.VoucherResponseDto;
 import org.programmers.springorder.model.VoucherType;
@@ -7,76 +9,58 @@ import org.programmers.springorder.utils.MenuType;
 import org.programmers.springorder.utils.Validation;
 import org.springframework.stereotype.Component;
 
-import java.text.MessageFormat;
 import java.util.List;
-import java.util.Scanner;
 
 @Component
 public class Console {
 
-    private final Scanner scanner = new Scanner(System.in);
+    private final Input input;
+    private final Output output;
 
-    private static final String MENU_SELECT_MESSAGE = """
-            === Voucher Program ===
-            1. Exit the program.
-            2. Create a new voucher.
-            3. List all vouchers.
-            """;
+    public Console() {
+        this.input = new Input();
+        this.output = new Output();
+    }
 
-    private static final String VOUCHER_SELECT_MESSAGE = """
-            Q. 원하는 바우처 타입을 입력해주세요.
-            1. Fixed Amount Voucher
-            2. Percent Discount Voucher
-            """;
-
-    private static final String INPUT_DISCOUNT_VALUE_MESSAGE = """
-            Q. 할인 금액을 입력해주세요.
-            """;
-
-    // Output
     public void printMessage(String message) {
-        System.out.println(message);
+        output.printMessage(message);
     }
 
-    public void printPrompt() {
-        System.out.print("> ");
-    }
-
-    // Input
     public MenuType inputMenu() {
-        printMessage(MENU_SELECT_MESSAGE);
-        printPrompt();
+        printMessage(Message.MENU_SELECT_MESSAGE);
+        output.printPrompt();
 
-        String menuNum = Validation.validateString(scanner.nextLine());
+        String menuNum = Validation.validateString(input.getInput());
         return MenuType.selectMenu(menuNum); // TODO: Console Validation
     }
 
     private VoucherType inputVoucherType() {
-        printMessage(VOUCHER_SELECT_MESSAGE);
-        printPrompt();
+        printMessage(Message.VOUCHER_SELECT_MESSAGE);
+        output.printPrompt();
 
-        int voucherNum = Validation.validateInt(scanner.nextLine());
+        int voucherNum = Validation.validateInt(input.getInput());
         return VoucherType.selectVoucherType(voucherNum);
     }
 
-    private long inputVoucherValue() {
-        printMessage(INPUT_DISCOUNT_VALUE_MESSAGE);
-        printPrompt();
+    private long inputVoucherValue(VoucherType voucherType) {
+        String discountValueMessage = voucherType == VoucherType.FIXED ? Message.INPUT_FIXED_DISCOUNT_VALUE_MESSAGE : Message.INPUT_PERCENT_DISCOUNT_VALUE_MESSAGE;
+        printMessage(discountValueMessage);
+        output.printPrompt();
 
-        return Validation.validateLong(scanner.nextLine());
+        return Validation.validateLong(input.getInput());
     }
 
 
     public VoucherRequestDto inputVoucherInfo() {
         VoucherType voucherType = inputVoucherType();
-        long discountValue = inputVoucherValue();
+        long discountValue = inputVoucherValue(voucherType);
 
         return new VoucherRequestDto(discountValue, voucherType);
     }
 
     public void showList(List<VoucherResponseDto> allVoucher) {
         if(allVoucher.size() == 0) {
-            printMessage("[SYSTEM] 등록된 바우처가 존재하지 않습니다.");
+            printMessage(ErrorMessage.VOUCHER_NOT_EXIST_MESSAGE);
         } else {
             allVoucher.forEach(System.out::println);
         }
---
+++ src/main/java/org/programmers/springorder/console/Output.java
@@ -0,0 +1,11 @@
+package org.programmers.springorder.console;
+
+public class Output {
+    public void printMessage(String message) {
+        System.out.println(message);
+    }
+
+    public void printPrompt() {
+        System.out.print("> ");
+    }
+}
