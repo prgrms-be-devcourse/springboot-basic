ce5ab480329b32ff3dda8fd2baf71777e0386f23
JIN-076 <andy230@naver.com>
2023-10-19 02:02:00 +0900

    feature: 애플리케이션에서 사용되는 명시적인 의미의 상수들을 다루는 Constants 클래스 추가

---
+++ src/main/java/org/programmers/springboot/basic/AppConstants.java
@@ -0,0 +1,12 @@
+package org.programmers.springboot.basic;
+
+public final class AppConstants {
+
+    public static final Long MIN_FIXED_DISCOUNT = 0L;
+    public static final Long MIN_PERCENT_DISCOUNT = 0L;
+    public static final Long MAX_PERCENT_DISCOUNT = 100L;
+    public static final int NONE = 0;
+    public static final int FIXED_AMOUNT = 1;
+    public static final int PERCENT_DISCOUNT = 2;
+    public static final String CSV_SEPARATOR = ",";
+}
