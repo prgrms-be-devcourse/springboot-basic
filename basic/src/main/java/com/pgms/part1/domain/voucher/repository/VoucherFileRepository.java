ac93d13e87dc274df73dae2e1a1adb313ced4620
KarmaPol <kkhkr98@gmail.com>
2023-10-20 00:27:08 +0900

    feat : add logger in voucherFileRepository

--- basic/src/main/java/com/pgms/part1/domain/voucher/repository/VoucherFileRepository.java
+++ basic/src/main/java/com/pgms/part1/domain/voucher/repository/VoucherFileRepository.java
@@ -2,6 +2,8 @@ package com.pgms.part1.domain.voucher.repository;
 
 import com.pgms.part1.domain.voucher.entity.Voucher;
 import com.pgms.part1.domain.voucher.entity.VoucherDiscountType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Repository;
@@ -12,6 +14,7 @@ import java.util.*;
 @Profile("file")
 @Repository
 public class VoucherFileRepository implements VoucherRepository {
+    private final Logger log = LoggerFactory.getLogger(VoucherFileRepository.class);
     private final static Map<UUID, Voucher> voucherMap = new HashMap<>();
     private String FILE_PATH;
     private File file;
@@ -40,7 +43,9 @@ public class VoucherFileRepository implements VoucherRepository {
                 else if(discountType == VoucherDiscountType.FIXED_AMOUNT_DISCOUNT)
                     voucherMap.put(id, Voucher.newFixedAmountDiscountVoucher(id, discount));
             }
+            log.info("file loaded");
         } catch (IOException e) {
+            log.error("load file error");
             throw new RuntimeException("can not load file!!");
         }
     }
@@ -48,8 +53,10 @@ public class VoucherFileRepository implements VoucherRepository {
     private void createFile() {
         try {
             file.createNewFile();
+            log.info("new file created");
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            log.error("create file error");
+            throw new RuntimeException("can not create file!!");
         }
     }
 
@@ -60,8 +67,10 @@ public class VoucherFileRepository implements VoucherRepository {
                         voucher.getDiscount() + "," +
                         voucher.getVoucherDiscountType() + "\n");
             }
+            log.info("file saved");
         }
         catch (IOException e){
+            log.error("save file error");
             throw new RuntimeException("can not save file!!");
         }
     }
