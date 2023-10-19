2acf694786fdd9d9353cf2eba038085e7b31b375
shoeone96 <dlwnddnjs96@gmail.com>
2023-10-19 22:23:04 +0900

    refactor: 프로그램 종료 메세지 추가

--- src/main/java/org/programmers/springorder/consts/Message.java
+++ src/main/java/org/programmers/springorder/consts/Message.java
@@ -7,6 +7,7 @@ public class Message {
             1. Exit the program.
             2. Create a new voucher.
             3. List all vouchers.
+            4. Show all blacklist.
             """;
 
     public static final String VOUCHER_SELECT_MESSAGE = """
@@ -14,12 +15,10 @@ public class Message {
             1. Fixed Amount Voucher
             2. Percent Discount Voucher
             """;
-
-    public static final String INPUT_DISCOUNT_VALUE_MESSAGE = """
-            Q. 할인 금액을 입력해주세요.
-            """;
+    
 
     public static final String INPUT_FIXED_DISCOUNT_VALUE_MESSAGE = "Q. 할인 금액(₩)을 입력해주세요.";
     public static final String INPUT_PERCENT_DISCOUNT_VALUE_MESSAGE = "Q. 할인율(%)을 입력해주세요.";
     public static final String VOUCHER_REGISTERED = "바우처가 등록되었습니다.";
+    public static final String EXIT_PROGRAM_MESSAGE = "프로그램이 종료되었습니다.";
 }
