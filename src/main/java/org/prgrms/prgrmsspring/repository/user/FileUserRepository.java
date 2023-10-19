5199538eb70f8ba89fc434006b44df24ae49ed50
voidmelody <sss9073@naver.com>
2023-10-19 09:46:35 +0900

    Refactor: "File 읽기 함수 및 경로 통일"

--- src/main/java/org/prgrms/prgrmsspring/repository/user/FileUserRepository.java
+++ src/main/java/org/prgrms/prgrmsspring/repository/user/FileUserRepository.java
@@ -4,13 +4,10 @@ import org.prgrms.prgrmsspring.entity.user.User;
 import org.prgrms.prgrmsspring.exception.ExceptionMessage;
 import org.prgrms.prgrmsspring.exception.NotFoundException;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Repository;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
+import java.io.*;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
@@ -24,20 +21,30 @@ public class FileUserRepository implements UserRepository {
 
     public FileUserRepository(@Value("${file.path.blacklist}") String filePath) {
         this.filePath = filePath;
-        read();
+        readAndStoreInMemory();
     }
 
-    private void read() {
-        Resource resource = new ClassPathResource(filePath);
+    private void readAndStoreInMemory() {
         try {
-            File file = resource.getFile();
-            List<String> strings = Files.readAllLines(file.toPath());
-            strings.stream().filter(s -> !s.equals(strings.get(0)))
+            FileInputStream inputStream = new FileInputStream(filePath);
+            List<String> fileStrings = readFile(inputStream);
+            fileStrings.stream().filter(s -> !s.equals(fileStrings.get(0)))
                     .map(name -> new User(UUID.randomUUID(), name))
                     .forEach(user -> store.put(user.getUserId(), user));
         } catch (IOException e) {
             throw new NotFoundException(ExceptionMessage.NOT_FOUND_FILE.getMessage());
         }
+    }
+
+    private List<String> readFile(InputStream inputStream) throws IOException {
+        List<String> lines = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                lines.add(line);
+            }
+        }
+        return lines;
     }
 
     @Override
