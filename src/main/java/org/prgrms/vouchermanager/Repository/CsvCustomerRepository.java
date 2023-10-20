bef18023678756b07a1093a698c7a5ea92d0fc18
dlswns2480 <dlswns2480@naver.com>
2023-10-20 16:59:00 +0900

    2차 과제 시작 전으로 reset

--- src/main/java/org/prgrms/vouchermanager/Repository/CsvCustomerRepository.java
+++ src/main/java/org/prgrms/vouchermanager/Repository/CsvCustomerRepository.java
@@ -5,7 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.prgrms.vouchermanager.domain.customer.Customer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
-import org.springframework.jdbc.core.JdbcTemplate;
+
 import org.springframework.stereotype.Repository;
 
 import java.io.BufferedReader;
