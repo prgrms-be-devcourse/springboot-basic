2be09375e4864de9d0eacc7ea8cf2d354ebae125
dlswns2480 <dlswns2480@naver.com>
2023-10-20 17:01:13 +0900

    toString 메서드 롬복활용으로 변경

--- src/main/java/org/prgrms/vouchermanager/domain/customer/Customer.java
+++ src/main/java/org/prgrms/vouchermanager/domain/customer/Customer.java
@@ -2,20 +2,16 @@ package org.prgrms.vouchermanager.domain.customer;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.ToString;
 import org.springframework.stereotype.Component;
 
 import java.util.UUID;
 @RequiredArgsConstructor
 @Getter
+@ToString
 public class Customer {
     private final String customerId;
     private final String name;
 
-    @Override
-    public String toString() {
-        return "Customer{" +
-                "customerId=" + customerId +
-                ", name='" + name + '\'' +
-                '}';
-    }
+
 }
