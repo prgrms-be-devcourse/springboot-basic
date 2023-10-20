6946738f4bcaf7e0cae55b11c033c40dbb573994
YongNyeo <won05121@gmail.com>
2023-10-20 17:40:12 +0900

    refactor: CustomerRepository 내 불필요한 필드 제거

--- src/main/java/com/prgms/vouchermanager/repository/customer/CustomerRepository.java
+++ src/main/java/com/prgms/vouchermanager/repository/customer/CustomerRepository.java
@@ -4,26 +4,22 @@ import com.prgms.vouchermanager.domain.customer.Customer;
 import com.prgms.vouchermanager.util.file.FileManager;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
 @Repository
 public class CustomerRepository {
-
-    private final FileManager fileManager;
     private Map<Long, Customer> blackList;
 
     public CustomerRepository(FileManager fileManager) {
 
-        this.fileManager = fileManager;
         blackList = fileManager.readBlackListCsv();
     }
 
     public List<Customer> getBlackList() {
-         List<Customer>customerList= new ArrayList<>(blackList.values());
-         return customerList;
+
+        return blackList.values()
+                .stream().toList();
     }
 
 }
