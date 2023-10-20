c527d129138936edc652fac15fcc87236d456f2a
kkangh00n <kig2454@gmail.com>
2023-10-20 14:54:02 +0900

    fix: 도미엔 기능 변경

--- src/main/java/com/weeklyMission/voucher/domain/FixedAmountVoucher.java
+++ src/main/java/com/weeklyMission/voucher/domain/FixedAmountVoucher.java
@@ -3,11 +3,14 @@ package com.weeklyMission.voucher.domain;
 import java.util.UUID;
 
 public class FixedAmountVoucher implements Voucher{
-
+    private static final long MAX_FIXED_VOUCHER_AMOUNT = 10000;
     private final UUID voucherId;
     private final long amount;
 
     public FixedAmountVoucher(UUID voucherId, long amount) {
+        if(amount<0) throw new IllegalArgumentException("음수는 될 수 없음");
+        if(amount==0) throw new IllegalArgumentException("0이 될 수 없음");
+        if(amount>MAX_FIXED_VOUCHER_AMOUNT) throw new IllegalArgumentException("%d보다 작아야 함".formatted(MAX_FIXED_VOUCHER_AMOUNT));
         this.voucherId = voucherId;
         this.amount = amount;
     }
@@ -24,6 +27,6 @@ public class FixedAmountVoucher implements Voucher{
 
     @Override
     public long discount(long beforeDiscount) {
-        return beforeDiscount-amount;
+        return beforeDiscount-amount<0?0:beforeDiscount-amount;
     }
 }
--- src/main/java/com/weeklyMission/voucher/domain/PercentDiscountVoucher.java
+++ src/main/java/com/weeklyMission/voucher/domain/PercentDiscountVoucher.java
@@ -4,10 +4,15 @@ import java.util.UUID;
 
 public class PercentDiscountVoucher implements Voucher{
 
+    private static final long MAX_PERCENT_VOUCHER_AMOUNT = 99;
+
     private final UUID voucherId;
     private final long amount;
 
     public PercentDiscountVoucher(UUID voucherId, long amount) {
+        if(amount<0) throw new IllegalArgumentException("음수는 될 수 없음");
+        if(amount==0) throw new ArithmeticException("0이 될 수 없음");
+        if(amount>MAX_PERCENT_VOUCHER_AMOUNT) throw new IllegalArgumentException("%d보다 작아야 함".formatted(MAX_PERCENT_VOUCHER_AMOUNT));
         this.voucherId = voucherId;
         this.amount = amount;
     }
@@ -24,6 +29,6 @@ public class PercentDiscountVoucher implements Voucher{
 
     @Override
     public long discount(long beforeDiscount) {
-        return beforeDiscount/amount;
+        return beforeDiscount-(beforeDiscount*(amount/100));
     }
 }
