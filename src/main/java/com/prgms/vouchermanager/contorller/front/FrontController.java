d0bb01919d71a948b17ff385b8002c9855705db9
YongNyeo <won05121@gmail.com>
2023-10-20 20:38:52 +0900

    refactor: FrontController에서 Voucher메뉴와 Customer메뉴를 모두 담당하도록 수정

--- src/main/java/com/prgms/vouchermanager/contorller/front/FrontController.java
+++ src/main/java/com/prgms/vouchermanager/contorller/front/FrontController.java
@@ -4,11 +4,18 @@ package com.prgms.vouchermanager.contorller.front;
 import com.prgms.vouchermanager.contorller.customer.CustomerController;
 import com.prgms.vouchermanager.contorller.voucher.VoucherController;
 import com.prgms.vouchermanager.util.io.ConsoleInput;
+import com.prgms.vouchermanager.util.io.ConsoleOutput;
+import com.prgms.vouchermanager.validation.InputValidation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 
+import static com.prgms.vouchermanager.contorller.customer.CustomerMenuType.BLACK_LIST;
 import static com.prgms.vouchermanager.contorller.front.FrontMenuType.*;
+import static com.prgms.vouchermanager.contorller.voucher.VoucherMenuType.CREATE;
+import static com.prgms.vouchermanager.contorller.voucher.VoucherMenuType.LIST;
+import static com.prgms.vouchermanager.exception.ExceptionType.*;
+import static com.prgms.vouchermanager.exception.ExceptionType.INVALID_CUSTOMER_MENU;
 
 
 @Controller
@@ -18,14 +25,20 @@ public class FrontController {
 
     private final ConsoleInput consoleInput;
 
+    private final ConsoleOutput consoleOutput;
+
+    private final InputValidation inputValidation;
+
     private final VoucherController voucherController;
 
     private final CustomerController customerController;
 
 
-    public FrontController(ConsoleInput consoleInput, VoucherController voucherController, CustomerController customerController) {
+    public FrontController(ConsoleInput consoleInput, ConsoleOutput consoleOutput, InputValidation inputValidation, VoucherController voucherController, CustomerController customerController) {
 
         this.consoleInput = consoleInput;
+        this.consoleOutput = consoleOutput;
+        this.inputValidation = inputValidation;
         this.voucherController = voucherController;
         this.customerController = customerController;
 
@@ -34,12 +47,16 @@ public class FrontController {
     public void run() {
 
         boolean end = false;
-        int menu;
+        int menu =0;
 
         while (!end) {
 
             try {
+                consoleOutput.printFrontMenu();
                 menu = consoleInput.inputFrontMenu();
+                if (!inputValidation.validFrontMenu(menu)) {
+                    throw new RuntimeException(INVALID_FRONT_MENU.getMessage());
+                }
             } catch (RuntimeException e) {
                 System.out.println(e.getMessage());
                 logger.warn(e.getMessage());
@@ -47,13 +64,60 @@ public class FrontController {
             }
 
             if (menu == VOUCHER.getNumber()) {
-                voucherController.run();
+                runVoucherController();
             } else if (menu == CUSTOMER.getNumber()) {
-                customerController.run();
+                runCustomerContoller();
             } else {
                 end = true;
             }
 
+        }
+    }
+    private void runVoucherController() {
+
+        consoleOutput.printVoucherMenu();
+        int menu = 0;
+        try {
+            menu = consoleInput.inputVoucherMenu();
+
+            if (!inputValidation.validVoucherMenu(menu)) {
+                logger.warn(INVALID_VOUCHER_MENU.getMessage());
+                throw new RuntimeException(INVALID_VOUCHER_MENU.getMessage());
+            }
+
+        } catch (RuntimeException e) {
+
+            System.out.println(e.getMessage());
+            logger.warn(e.getMessage());
+            return;
+        }
+
+        if (menu == CREATE.getNumber()) {
+            voucherController.create();
+        } else if (menu == LIST.getNumber()) {
+            voucherController.getList();
+        }
+    }
+
+    private void runCustomerContoller() {
+
+        consoleOutput.printCustomerMenu();
+        int menu = 0;
+        try {
+            menu = consoleInput.inputCustomerMenu();
+
+            if (!inputValidation.validCustomerMenu(menu)) {
+                logger.warn(INVALID_CUSTOMER_MENU.getMessage());
+                throw new RuntimeException(INVALID_CUSTOMER_MENU.getMessage());
+            }
+
+        } catch (RuntimeException e) {
+            System.out.println(e.getMessage());
+            logger.warn(e.getMessage());
+            return;
+        }
+        if (menu == BLACK_LIST.getNumber()) {
+            customerController.getBlackList();
         }
     }
 }
