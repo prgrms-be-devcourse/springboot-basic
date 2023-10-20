9114d187809c70aec5d51fe0f52aed147c3f3230
happyjamy <78072370+happyjamy@users.noreply.github.com>
2023-10-20 23:49:16 +0900

    [fix] 환경변수 설정이 안되는 오류 해결

--- src/main/java/com/programmers/springbootbasic/domain/user/application/UserService.java
+++ src/main/java/com/programmers/springbootbasic/domain/user/application/UserService.java
@@ -3,22 +3,24 @@ package com.programmers.springbootbasic.domain.user.application;
 import com.programmers.springbootbasic.domain.user.domain.entity.User;
 import com.programmers.springbootbasic.domain.user.presentation.dto.UserResponse;
 import com.programmers.springbootbasic.util.FileManager;
-import com.programmers.springbootbasic.util.FileProperties;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
 
     private final FileManager fileManager;
+    private String fileName;
 
-    public UserService(FileManager fileManager) {
+    public UserService(FileManager fileManager, @Value("${file.user.path}") String fileName) {
+        this.fileName = fileName;
         this.fileManager = fileManager;
     }
 
     public List<UserResponse> findBlacklistedUsers() {
         return fileManager
-            .read(FileProperties.getUserFilePath(), User.class)
+            .read(fileName, User.class)
             .stream()
             .map(UserResponse::of)
             .toList();
--- src/main/java/com/programmers/springbootbasic/domain/voucher/infrastructure/FilePersistenceVoucherRepository.java
+++ src/main/java/com/programmers/springbootbasic/domain/voucher/infrastructure/FilePersistenceVoucherRepository.java
@@ -6,8 +6,8 @@ import com.programmers.springbootbasic.domain.voucher.domain.VoucherRepository;
 import com.programmers.springbootbasic.domain.voucher.domain.entity.Voucher;
 import com.programmers.springbootbasic.exception.exceptionClass.VoucherException;
 import com.programmers.springbootbasic.util.FileManager;
-import com.programmers.springbootbasic.util.FileProperties;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Repository;
@@ -19,10 +19,12 @@ public class FilePersistenceVoucherRepository implements VoucherRepository {
 
     private final List<FileManager> fileManagerList;
     private FileManager fileManager;
+    private String fileName;
 
-    public FilePersistenceVoucherRepository(List<FileManager> fileManagerList) {
+    public FilePersistenceVoucherRepository(List<FileManager> fileManagerList, @Value("${file.voucher.path}") String fileName) {
         this.fileManagerList = fileManagerList;
-        fileManagerList.stream().filter((fm) -> fm.supports(FileProperties.getVoucherFilePath()))
+        this.fileName = fileName;
+        fileManagerList.stream().filter((fm) -> fm.supports(fileName))
             .findFirst()
             .ifPresentOrElse((fm) -> this.fileManager = fm, () -> {
                 throw new VoucherException(INVALID_FILE_PATH);
@@ -31,13 +33,13 @@ public class FilePersistenceVoucherRepository implements VoucherRepository {
 
     @Override
     public Voucher save(Voucher voucher) {
-        fileManager.write(CsvVoucher.of(voucher), FileProperties.getVoucherFilePath());
+        fileManager.write(CsvVoucher.of(voucher), fileName);
         return voucher;
     }
 
     @Override
     public List<Voucher> findAll() {
-        var csvVouchers = fileManager.read(FileProperties.getVoucherFilePath(), CsvVoucher.class);
+        var csvVouchers = fileManager.read(fileName, CsvVoucher.class);
         return csvVouchers.stream()
             .map(CsvVoucher::toEntity)
             .toList();
