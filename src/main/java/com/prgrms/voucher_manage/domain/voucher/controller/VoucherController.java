bc9ee2444404fb0e3059c608dc5126e7981ac121
hs12 <loveanna1228@gmail.com>
2023-10-20 22:14:30 +0900

    [Feat] 예외처리 로직 위치 수정

--- src/main/java/com/prgrms/voucher_manage/domain/voucher/controller/VoucherController.java
+++ src/main/java/com/prgrms/voucher_manage/domain/voucher/controller/VoucherController.java
@@ -3,19 +3,39 @@ package com.prgrms.voucher_manage.domain.voucher.controller;
 import com.prgrms.voucher_manage.domain.voucher.entity.VoucherType;
 import com.prgrms.voucher_manage.domain.voucher.dto.CreateVoucherDto;
 import com.prgrms.voucher_manage.domain.voucher.service.VoucherService;
+import com.prgrms.voucher_manage.exception.InvalidDiscountRangeException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+
+import static com.prgrms.voucher_manage.domain.voucher.entity.VoucherType.FIXED;
+import static com.prgrms.voucher_manage.domain.voucher.entity.VoucherType.PERCENT;
 
 @Controller
 @RequiredArgsConstructor
 public class VoucherController {
+    private static final Long MIN_DISCOUNT_PRICE = 0L;
+    private static final Long MIN_DISCOUNT_PERCENT = 0L;
+    private static final Long MAX_DISCOUNT_PERCENT = 100L;
     private final VoucherService voucherService;
 
     public void createVoucher(VoucherType voucherType, Long discountAmount) {
+        if (voucherType == FIXED && isInvalidPrice(discountAmount)) {
+            throw new InvalidDiscountRangeException();
+        } else if (voucherType == PERCENT && isInvalidPercent(discountAmount)) {
+            throw new InvalidDiscountRangeException();
+        }
         voucherService.createVoucher(new CreateVoucherDto(voucherType, discountAmount));
     }
 
     public void findVouchers() {
         voucherService.findVouchers();
+    }
+
+    public boolean isInvalidPercent(Long discountAmount) {
+        return (discountAmount < MIN_DISCOUNT_PERCENT) || (discountAmount > MAX_DISCOUNT_PERCENT);
+    }
+
+    public boolean isInvalidPrice(Long discountAmount) {
+        return discountAmount < MIN_DISCOUNT_PRICE;
     }
 }
--- src/main/java/com/prgrms/voucher_manage/domain/voucher/dto/CreateVoucherDto.java
+++ src/main/java/com/prgrms/voucher_manage/domain/voucher/dto/CreateVoucherDto.java
@@ -6,18 +6,6 @@ import com.prgrms.voucher_manage.domain.voucher.entity.Voucher;
 import com.prgrms.voucher_manage.domain.voucher.entity.VoucherType;
 
 public record CreateVoucherDto(VoucherType voucherType, Long discountAmount) {
-    private static final Long MIN_DISCOUNT_PRICE = 0L;
-    private static final Long MIN_DISCOUNT_PERCENT = 0L;
-    private static final Long MAX_DISCOUNT_PERCENT = 100L;
-
-    public boolean isInvalidPercent() {
-        return (discountAmount < MIN_DISCOUNT_PERCENT) || (discountAmount > MAX_DISCOUNT_PERCENT);
-    }
-
-    public boolean isInValidPrice() {
-        return discountAmount < MIN_DISCOUNT_PRICE;
-    }
-
     public Voucher of(){
         if (voucherType==VoucherType.FIXED)
             return new FixedAmountVoucher(discountAmount);
