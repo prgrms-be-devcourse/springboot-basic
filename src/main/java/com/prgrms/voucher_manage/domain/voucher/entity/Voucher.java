5768c92da8deccfbe0fd478d50316b70fa24f3e3
hs12 <loveanna1228@gmail.com>
2023-10-21 18:36:05 +0900

    [Feat] lombok 사용

--- src/main/java/com/prgrms/voucher_manage/domain/voucher/entity/Voucher.java
+++ src/main/java/com/prgrms/voucher_manage/domain/voucher/entity/Voucher.java
@@ -1,18 +1,13 @@
 package com.prgrms.voucher_manage.domain.voucher.entity;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 import java.util.UUID;
 
-@Getter
+@Getter @RequiredArgsConstructor
 public abstract class Voucher {
-    private final UUID voucherId;
+    private final UUID voucherId = UUID.randomUUID();
     private final Long discountAmount;
     private final VoucherType voucherType;
-
-    public Voucher(Long discountAmount, VoucherType voucherType){
-        voucherId = UUID.randomUUID();
-        this.discountAmount = discountAmount;
-        this.voucherType = voucherType;
-    }
 }
