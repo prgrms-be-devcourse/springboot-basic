c6d7df13f0f23929b66e9b7b37f3df0c5863b8c6
ebcho <joeunbi514@naver.com>
2023-10-21 10:55:48 +0900

    fix: toString 수정

--- src/main/java/com/programmers/springbasic/dto/GetBlacklistCustomersResponse.java
+++ src/main/java/com/programmers/springbasic/dto/GetBlacklistCustomersResponse.java
@@ -1,7 +1,5 @@
 package com.programmers.springbasic.dto;
 
-import java.text.MessageFormat;
-
 import com.programmers.springbasic.entity.customer.Customer;
 
 public class GetBlacklistCustomersResponse {
@@ -15,6 +13,7 @@ public class GetBlacklistCustomersResponse {
 
 	@Override
 	public String toString() {
-		return MessageFormat.format("고객 아이디 = {0}, 고객 이름 = {1}", id, name);
+		return "고객 아이디 = " + id + ", 고객 이름 = " + name;
 	}
+
 }
