dda826f1850cc618ce2f915101f415a11e4755c0
suzzingv <84590065+suzzingv@users.noreply.github.com>
2023-10-22 18:26:29 +0900

    test : CustomerFileRepository 테스트

---
+++ src/test/java/com/prgrms/vouchermanager/repository/CustomerFileRepositoryTest.java
@@ -0,0 +1,22 @@
+package com.prgrms.vouchermanager.repository;
+
+import com.prgrms.vouchermanager.domain.Customer;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+
+class CustomerFileRepositoryTest {
+
+    private final CustomerFileRepository repository = new CustomerFileRepository("src/main/resources/customer_blacklist.csv");
+
+    @Test
+    @DisplayName("블랙리스트 조회")
+    void blackList() {
+        List<Customer> blacklist = repository.blacklist();
+        Assertions.assertThat(blacklist.size()).isEqualTo(3);
+    }
+
+}
