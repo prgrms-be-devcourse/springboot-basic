1b94312b83d33c27bdf1cb51f91ed411ef63e6ef
happyjamy <78072370+happyjamy@users.noreply.github.com>
2023-10-23 00:11:42 +0900

    [change] 필드 final 적용 + TextBlock 적용

--- src/main/java/com/programmers/springbootbasic/domain/voucher/presentation/dto/VoucherResponse.java
+++ src/main/java/com/programmers/springbootbasic/domain/voucher/presentation/dto/VoucherResponse.java
@@ -4,9 +4,10 @@ import com.programmers.springbootbasic.domain.voucher.domain.entity.Voucher;
 import java.util.UUID;
 
 public class VoucherResponse {
-    private UUID id;
-    private String voucherType;
-    private Integer benefitValue;
+
+    private final UUID id;
+    private final String voucherType;
+    private final Integer benefitValue;
 
     public VoucherResponse(UUID id, String voucherType, Integer benefitValue) {
         this.id = id;
@@ -16,15 +17,17 @@ public class VoucherResponse {
 
     @Override
     public String toString() {
-        return
-            "Voucher id =" + id + System.lineSeparator() +
-                "Type =" + voucherType + System.lineSeparator() +
-                "benefitValue =" + benefitValue + System.lineSeparator() +
-                "===============================" + System.lineSeparator();
+        return """
+            Voucher id = %s
+            Type = %s
+            benefitValue = %s
+            ===============================
+            """.formatted(id, voucherType, benefitValue);
     }
 
     public static VoucherResponse of(Voucher voucher) {
-        return new VoucherResponse(voucher.getId(), voucher.getVoucherType().getVoucherTypeName(), voucher.getBenefitValue());
+        return new VoucherResponse(voucher.getId(), voucher.getVoucherType().getVoucherTypeName(),
+            voucher.getBenefitValue());
     }
 
 }
