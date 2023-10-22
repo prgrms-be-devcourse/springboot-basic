6a2c6e7ded64f21e3f341fe64bc62399c83282c2
suzzingv <84590065+suzzingv@users.noreply.github.com>
2023-10-22 16:10:51 +0900

    test: VouhcerMemoryRepository 테스트

---
+++ src/test/java/com/prgrms/vouchermanager/repository/VoucherMemoryRepositoryTest.java
@@ -0,0 +1,42 @@
+package com.prgrms.vouchermanager.repository;
+
+import com.prgrms.vouchermanager.domain.PercentAmountVoucher;
+import com.prgrms.vouchermanager.domain.Voucher;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+class VoucherMemoryRepositoryTest {
+
+    private VoucherMemoryRepository repository;
+
+    @BeforeEach
+    void setup() {
+        repository = new VoucherMemoryRepository();
+    }
+    @Test
+    @DisplayName("바우처 생성")
+    void createTest() {
+        PercentAmountVoucher voucher = new PercentAmountVoucher(20);
+        repository.create(voucher);
+
+        List<Voucher> list = repository.list();
+        Assertions.assertThat(list.size()).isEqualTo(1);
+        Assertions.assertThat(list.get(0)).isSameAs(voucher);
+    }
+
+    @Test
+    @DisplayName("바우처 목록")
+    void listTest() {
+        PercentAmountVoucher voucher = new PercentAmountVoucher(20);
+        repository.create(voucher);
+
+        List<Voucher> list = repository.list();
+        Assertions.assertThat(list.size()).isEqualTo(1);
+        Assertions.assertThat(list.get(0)).isSameAs(voucher);
+    }
+
+}
