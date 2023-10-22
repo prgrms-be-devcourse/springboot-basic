c8f42c43f034bb28d90187f7c99dfec5606781cd
수지니 <sssssjin99@naver.com>
2023-10-23 06:09:48 +0900

    [feat]: VoucherService 추가

---
+++ src/main/java/com/prgrms/vouchermanagement/core/voucher/service/VoucherService.java
@@ -0,0 +1,34 @@
+package com.prgrms.vouchermanagement.core.voucher.service;
+
+import com.prgrms.vouchermanagement.core.voucher.controller.request.VoucherCreateRequest;
+import com.prgrms.vouchermanagement.core.voucher.domain.Voucher;
+import com.prgrms.vouchermanagement.core.voucher.dto.VoucherDto;
+import com.prgrms.vouchermanagement.core.voucher.repository.VoucherRepository;
+import com.prgrms.vouchermanagement.core.voucher.utils.mapper.Mapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class VoucherService {
+
+    private final VoucherRepository voucherRepository;
+
+    @Autowired
+    public VoucherService(VoucherRepository voucherRepository) {
+        this.voucherRepository = voucherRepository;
+    }
+
+    public void createVoucher(VoucherCreateRequest voucherCreateRequest) {
+        voucherRepository.save(Mapper.toVoucher(voucherCreateRequest));
+    }
+
+    public List<VoucherDto> findAllVoucher() {
+        List<Voucher> voucherList = voucherRepository.findAll();
+        return voucherList.stream()
+                .map(it -> new VoucherDto(it.getVoucherID(), it.getName(), it.getAmount(), it.getVoucherType()))
+                .collect(Collectors.toList());
+    }
+}
