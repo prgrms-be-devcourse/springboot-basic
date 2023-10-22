c3ef87bb6443d4c500ac815dbd051fc1509d7b23
수지니 <sssssjin99@naver.com>
2023-10-23 06:08:10 +0900

    [feat]: Voucher 도메인, VoucherType 추가

--- src/main/java/com/prgrms/vouchermanagement/core/voucher/domain/Voucher.java
+++ src/main/java/com/prgrms/vouchermanagement/core/voucher/domain/Voucher.java
@@ -1,16 +1,42 @@
-package com.prgrms.vouchermanagement.core.domain;
-
-import java.util.UUID;
+package com.prgrms.vouchermanagement.core.voucher.domain;
 
 public class Voucher {
 
-    private final UUID voucherID;
+    private long voucherID;
+    private final String name;
     private final long amount;
     private final VoucherType voucherType;
 
-    public Voucher(UUID voucherID, long amount, VoucherType voucherType) {
+    public Voucher(long voucherID, String name, long amount, VoucherType voucherType) {
         this.voucherID = voucherID;
+        this.name = name;
         this.amount = amount;
         this.voucherType = voucherType;
+    }
+
+    public Voucher(String name, long amount, VoucherType voucherType) {
+        this.name = name;
+        this.amount = amount;
+        this.voucherType = voucherType;
+    }
+
+    public void setVoucherID(long voucherID) {
+        this.voucherID = voucherID;
+    }
+
+    public long getVoucherID() {
+        return voucherID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public long getAmount() {
+        return amount;
+    }
+
+    public VoucherType getVoucherType() {
+        return voucherType;
     }
 }
--- src/main/java/com/prgrms/vouchermanagement/core/voucher/domain/VoucherType.java
+++ src/main/java/com/prgrms/vouchermanagement/core/voucher/domain/VoucherType.java
@@ -1,7 +1,26 @@
-package com.prgrms.vouchermanagement.core.domain;
+package com.prgrms.vouchermanagement.core.voucher.domain;
+
+import java.util.Arrays;
 
 public enum VoucherType {
 
-    FIXED,
-    RATE
+    FIXED("fixed"),
+    PERCENT("rate");
+
+    private String value;
+
+    VoucherType(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public static VoucherType getType(String value) {
+        return Arrays.stream(VoucherType.values())
+                .filter(type -> type.value.equalsIgnoreCase(value))
+                .findFirst()
+                .orElse(null);
+    }
 }
