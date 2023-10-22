352eb8a03ea4e65bde246219f9421e6d495e21d0
happyjamy <78072370+happyjamy@users.noreply.github.com>
2023-10-22 23:44:19 +0900

    [change] message 필드 final 처리 + TextBlock 적용

--- src/main/java/com/programmers/springbootbasic/util/Messages.java
+++ src/main/java/com/programmers/springbootbasic/util/Messages.java
@@ -2,12 +2,13 @@ package com.programmers.springbootbasic.util;
 
 public enum Messages {
 
-    SELECT_MENU("=== Voucher Program ===" + System.lineSeparator()
-        + "종료 : exit" + System.lineSeparator()
-        + "바우처 등록 : create voucher" + System.lineSeparator()
-        + "바우처 조회 : list voucher" + System.lineSeparator()
-        + "유저 블랙리스트 조회 : list black user" + System.lineSeparator()
-    ),
+    SELECT_MENU("""
+        === Voucher Program ===
+        종료 : exit
+        바우처 등록 : create voucher
+        바우처 조회 : list voucher
+        유저 블랙리스트 조회 : list black user
+        """),
     VOUCHER_REGISTER_TYPE("등록할 바우처의 타입을 입력하세요.(FIXED, PERCENT)"),
     VOUCHER_REGISTER_AMOUNT("등록할 바우처의 금액을 입력하세요.(숫자)"),
     SUCCESS_VOUCHER_REGISTER("바우처가 등록되었습니다."),
@@ -19,7 +20,7 @@ public enum Messages {
         this.message = Message;
     }
 
-    private String message;
+    private final String message;
 
     public String getMessage() {
         return message;
