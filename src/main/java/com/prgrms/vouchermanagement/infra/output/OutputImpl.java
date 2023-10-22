9780a92c1901f71c2aee117e98fbbae583a3b142
수지니 <sssssjin99@naver.com>
2023-10-23 06:05:00 +0900

    [feat]: OutputProvider, OutputProvider 구현체 추가

--- src/main/java/com/prgrms/vouchermanagement/infra/output/OutputImpl.java
+++ src/main/java/com/prgrms/vouchermanagement/infra/output/OutputImpl.java
@@ -1,15 +1,34 @@
 package com.prgrms.vouchermanagement.infra.output;
 
+import com.prgrms.vouchermanagement.core.voucher.controller.response.VouchersResponse;
+import com.prgrms.vouchermanagement.core.voucher.domain.VoucherType;
+import com.prgrms.vouchermanagement.infra.utils.OutputMessage;
 import org.springframework.stereotype.Component;
 
 @Component
 public class OutputImpl implements OutputProvider {
+
     @Override
-    public void showMenu() {
-        System.out.println("=== Voucher Program ===");
-        System.out.println("Type exit to exit the program.");
-        System.out.println("Type create to create a new voucher.");
-        System.out.println("Type list to list all vouchers.");
+    public void printMessage(String message) {
+        System.out.print(message);
     }
 
+    @Override
+    public void printMessage(VoucherType voucherType) {
+        switch(voucherType) {
+            case FIXED:
+                System.out.print(OutputMessage.FIXAMOUNT_VOUCHER_AMOUNT);
+                break;
+            case PERCENT:
+                System.out.print(OutputMessage.PERCENTDISCOUNT_VOUCHER_AMOUNT);
+                break;
+        }
+    }
+
+    @Override
+    public void printMessage(VouchersResponse vouchersResponse) {
+        vouchersResponse.getVoucherResponses()
+                .stream()
+                .forEach(it -> System.out.println(it));
+    }
 }
--- src/main/java/com/prgrms/vouchermanagement/infra/output/OutputProvider.java
+++ src/main/java/com/prgrms/vouchermanagement/infra/output/OutputProvider.java
@@ -1,6 +1,12 @@
 package com.prgrms.vouchermanagement.infra.output;
 
+import com.prgrms.vouchermanagement.core.voucher.controller.response.VouchersResponse;
+import com.prgrms.vouchermanagement.core.voucher.domain.VoucherType;
+
 public interface OutputProvider {
 
-    void showMenu();
+    void printMessage(String message);
+    void printMessage(VoucherType voucherType);
+    void printMessage(VouchersResponse vouchersResponse);
+
 }
