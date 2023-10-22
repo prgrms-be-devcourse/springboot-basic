4ff89565d0adef9a556abd134db4b67db9a13343
수지니 <sssssjin99@naver.com>
2023-10-23 06:06:13 +0900

    [feat]: input 시 InvalidFormatException 추가

--- src/main/java/com/prgrms/vouchermanagement/infra/exception/InvalidFormatException.java
+++ src/main/java/com/prgrms/vouchermanagement/infra/exception/InvalidFormatException.java
@@ -1,15 +1,8 @@
 package com.prgrms.vouchermanagement.infra.exception;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+public class InvalidFormatException extends IllegalArgumentException {
 
-public class MenuTypeFormatException extends IllegalArgumentException {
-
-    private final Logger logger = LoggerFactory.getLogger(this.getClass());
-
-    public MenuTypeFormatException() {
-        super("[exit, create, list] 중 하나를 입력해야합니다.");
-        String msg = "[exit, create, list] 중 하나를 입력해야합니다.";
-        logger.warn(msg);
+    public InvalidFormatException(String message) {
+        super(message);
     }
 }
