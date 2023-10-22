cfd8342803fbc1e30975680e72bd0388a6fd91e6
ParkJuHan94 <1004sunye@naver.com>
2023-10-23 00:31:27 +0900

    feat: outputhandler 구현

--- src/main/java/org/prgrms/kdt/app/configuration/io/ConsoleOutputHandler.java
+++ src/main/java/org/prgrms/kdt/app/configuration/io/ConsoleOutputHandler.java
@@ -6,16 +6,26 @@ import org.springframework.stereotype.Component;
 @Component
 public class ConsoleOutputHandler implements OutputHandler{
 
+    private static final String lineSeparator = System.lineSeparator();
+
     @Override
     public void outputStartMessage() {
-        System.out.println("=== Voucher Program ===");
-        System.out.println("Type exit to exit the program.");
-        System.out.println("Type create to create a new voucher.");
-        System.out.println("Type list to list all vouchers.");
+        StringBuilder sb = new StringBuilder();
+        sb.append("=== Voucher Program ===");
+        sb.append(lineSeparator);
+        sb.append("Type exit to exit the program.");
+        sb.append(lineSeparator);
+        sb.append("Type create to create a new voucher.");
+        sb.append(lineSeparator);
+        sb.append("Type list to list all vouchers.");
+        sb.append(lineSeparator);
+        sb.append(lineSeparator);
+
+        System.out.println(sb.toString());
     }
 
     @Override
     public void outputSystemMessage(String message) {
-
+        System.out.println(message);
     }
 }
