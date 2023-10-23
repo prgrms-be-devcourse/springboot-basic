634e538f93d57f688fdb25286e23ce442b6eeef7
수지니 <sssssjin99@naver.com>
2023-10-24 02:01:48 +0900

    [refactor]: Profile 설정 - prod, dev 추가

--- src/main/java/com/prgrms/vouchermanagement/core/voucher/repository/MemoryVoucherRepository.java
+++ src/main/java/com/prgrms/vouchermanagement/core/voucher/repository/MemoryVoucherRepository.java
@@ -1,12 +1,14 @@
 package com.prgrms.vouchermanagement.core.voucher.repository;
 
 import com.prgrms.vouchermanagement.core.voucher.domain.Voucher;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.atomic.AtomicLong;
 
+@Profile("dev")
 @Repository
 public class MemoryVoucherRepository implements VoucherRepository {
 
