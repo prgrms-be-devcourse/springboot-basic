742eec6c482655dafff5e02444c85351611503a9
hs12 <loveanna1228@gmail.com>
2023-10-23 13:18:02 +0900

    [Fix] 테스트 케이스 삭제

--- src/test/java/com/prgrms/voucher_manage/voucher/VoucherFileRepositoryTest.java
+++ src/test/java/com/prgrms/voucher_manage/voucher/VoucherFileRepositoryTest.java
@@ -11,33 +11,16 @@ import java.util.List;
 import static com.prgrms.voucher_manage.domain.voucher.entity.VoucherType.PERCENT;
 import static org.assertj.core.api.Assertions.assertThat;
 
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class VoucherFileRepositoryTest {
     FileVoucherRepository repository = new FileVoucherRepository(new VoucherFileManager("/src/test/resources/voucher_test.csv"));
-    static FileVoucherRepository static_repository = new FileVoucherRepository(new VoucherFileManager("/src/test/resources/voucher_test.csv"));
-
-    @AfterAll()
-    public static void saveDummyData(){
-        Voucher voucher = new PercentDiscountVoucher(30L);
-        static_repository.save(voucher);
-    }
 
     @AfterEach
     public void clear(){
         repository.clear();
     }
 
-    @Test
-    @Order(1)
-    @DisplayName("파일에 저장된 Voucher를 가져올 수 있다.")
-    public void fileVoucherRepository_getFileData(){
-        Voucher savedVoucher = repository.findAll().get(0);
-        assertThat(savedVoucher.getVoucherType()).isEqualTo(PERCENT);
-        assertThat(savedVoucher.getDiscountAmount()).isEqualTo(30L);
-    }
 
     @Test
-    @Order(2)
     @DisplayName("Voucher를 저장할 수 있다.")
     public void fileVoucherRepository_save(){
         //given
@@ -48,11 +31,9 @@ public class VoucherFileRepositoryTest {
         assertThat(savedVoucher).isNotNull();
         assertThat(savedVoucher.getVoucherType()).isEqualTo(PERCENT);
         assertThat(savedVoucher.getDiscountAmount()).isEqualTo(10L);
-        repository.clear();
     }
 
     @Test
-    @Order(3)
     @DisplayName("Voucher 리스트를 반환받을 수 있다.")
     public void fileVoucherRepository_findAll(){
         //given
@@ -66,6 +47,5 @@ public class VoucherFileRepositoryTest {
         List<Voucher> voucherList = repository.findAll();
         //then
         assertThat(voucherList.size()).isEqualTo(2);
-        repository.clear();
     }
 }
