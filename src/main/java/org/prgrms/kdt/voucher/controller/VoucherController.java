733abd4c7be47f9a862160176e993cee7da7c457
ParkJuHan94 <1004sunye@naver.com>
2023-10-23 19:09:33 +0900

    fix: VoucherController 함수 분리

--- src/main/java/org/prgrms/kdt/voucher/controller/VoucherController.java
+++ src/main/java/org/prgrms/kdt/voucher/controller/VoucherController.java
@@ -37,70 +37,91 @@ public class VoucherController {
         outputHandler.outputStartMessage();
         var menu = inputHandler.inputString();
 
-        if (menu.equals(EXIT)) {
-            outputHandler.outputSystemMessage(EXIT_PROGRAM.getMessage());
-            return false;
-        } else if (menu.equals(CREATE)) {
-            outputHandler.outputSystemMessage(CREATE_BOUCHER_TYPE.getMessage());
-            var createVoucherType = inputHandler.inputString();
-
-            while (true) {
-                if (createVoucherType.equals(FIXED)) {
-                    outputHandler.outputSystemMessage(CREATE_FIXED_BOUCHER.getMessage());
-
-                    var amount = 0;
-                    while (true) {
-                        amount = inputHandler.inputInt();
-
-                        if (amount <= 0) {
-                            outputHandler.outputSystemMessage(EXCEPTION_FIXED_AMOUNT_MINUS.getMessage());
-                            continue;
-                        }
-                        if (amount >= 100_000) {
-                            outputHandler.outputSystemMessage(EXCEPTION_FIXED_AMOUNT_OVER.getMessage());
-                            continue;
-                        }
-                        break;
-                    }
-
-                    var fixedAmountVoucherDto = new FixedAmountVoucherDto(UUID.randomUUID(), amount);
-                    voucherService.createVoucher(fixedAmountVoucherDto);
-                    break;
-                } else if (createVoucherType.equals(PERCENT)) {
-                    outputHandler.outputSystemMessage(CREATE_PERCENT_BOUCHER.getMessage());
-
-                    var percent = 0;
-                    while (true) {
-                        percent = inputHandler.inputInt();
-
-                        if (percent <= 0) {
-                            outputHandler.outputSystemMessage(EXCEPTION_PERCENT_MINUS.getMessage());
-                            continue;
-                        }
-                        if (percent >= 100) {
-                            outputHandler.outputSystemMessage(EXCEPTION_PERCENT_OVER.getMessage());
-                            continue;
-                        }
-                        break;
-                    }
-
-                    var percentDiscountVoucherDto = new PercentDiscountVoucherDto(UUID.randomUUID(), percent);
-                    voucherService.createVoucher(percentDiscountVoucherDto);
-                    break;
-                } else {
-                    outputHandler.outputSystemMessage(EXCEPTION_VOUCHER_TYPE.getMessage());
-                }
-            }
-        } else if (menu.equals(LIST)) {
-            List<Voucher> voucherList = voucherService.getAllVouchers();
-            outputHandler.outputVouchers(voucherList);
-            return true;
-        } else {
-            outputHandler.outputSystemMessage(EXCEPTION_VOUCHER_TYPE.getMessage());
-            return true;
+        switch (menu) {
+            case EXIT:
+                outputHandler.outputSystemMessage(EXIT_PROGRAM.getMessage());
+                return false;
+            case CREATE:
+                createVoucher();
+                break;
+            case LIST:
+                List<Voucher> voucherList = voucherService.getAllVouchers();
+                outputHandler.outputVouchers(voucherList);
+                break;
+            default:
+                outputHandler.outputSystemMessage(EXCEPTION_VOUCHER_TYPE.getMessage());
+                break;
         }
 
         return true;
+    }
+
+
+    private void createVoucher() throws IOException {
+        while (true) {
+            outputHandler.outputSystemMessage(CREATE_BOUCHER_TYPE.getMessage());
+            var createVoucherType = inputHandler.inputString();
+
+            var isRepeat = true;
+
+            if (createVoucherType.equals(FIXED)) {
+                isRepeat = createFixedAmountVoucher();
+                if(!isRepeat)
+                    break;
+            } else if (createVoucherType.equals(PERCENT)) {
+                isRepeat = createPercentDiscountVoucher();
+                if(!isRepeat)
+                    break;
+            } else {
+                outputHandler.outputSystemMessage(EXCEPTION_VOUCHER_TYPE.getMessage());
+            }
+        }
+    }
+
+    private boolean createFixedAmountVoucher() throws IOException {
+        outputHandler.outputSystemMessage(CREATE_FIXED_BOUCHER.getMessage());
+
+        var amount = 0;
+        while (true) {
+            amount = inputHandler.inputInt();
+
+            if (amount <= 0) {
+                outputHandler.outputSystemMessage(EXCEPTION_FIXED_AMOUNT_MINUS.getMessage());
+                continue;
+            }
+            if (amount >= 100_000) {
+                outputHandler.outputSystemMessage(EXCEPTION_FIXED_AMOUNT_OVER.getMessage());
+                continue;
+            }
+            break;
+        }
+
+        var fixedAmountVoucherDto = new FixedAmountVoucherDto(UUID.randomUUID(), amount);
+        voucherService.createVoucher(fixedAmountVoucherDto);
+        return false;
+    }
+
+    private boolean createPercentDiscountVoucher() throws IOException {
+        outputHandler.outputSystemMessage(CREATE_PERCENT_BOUCHER.getMessage());
+
+        var percent = 0;
+        while (true) {
+            percent = inputHandler.inputInt();
+
+            if (percent <= 0) {
+                outputHandler.outputSystemMessage(EXCEPTION_PERCENT_MINUS.getMessage());
+                continue;
+            }
+            if (percent >= 100) {
+                outputHandler.outputSystemMessage(EXCEPTION_PERCENT_OVER.getMessage());
+                continue;
+            }
+            break;
+        }
+
+        var percentDiscountVoucherDto = new PercentDiscountVoucherDto(UUID.randomUUID(), percent);
+        voucherService.createVoucher(percentDiscountVoucherDto);
+        return false;
     }
 
     //    var orderProperties = applicationContext.getBean(OrderProperties.class);
