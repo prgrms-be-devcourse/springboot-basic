07bf069b431296265e84c449c5b1b8aa052ee09c
Jay <injaesong0@gmail.com>
2023-10-23 16:13:52 +0900

    refactor: add VoucherType getter

--- src/main/java/com/programmers/vouchermanagement/voucher/domain/Voucher.java
+++ src/main/java/com/programmers/vouchermanagement/voucher/domain/Voucher.java
@@ -1,30 +1,32 @@
 package com.programmers.vouchermanagement.voucher.domain;
 
-import com.programmers.vouchermanagement.voucher.dto.GeneralVoucherDTO;
-
 import java.math.BigDecimal;
 import java.util.UUID;
 
 public class Voucher {
     private static final String INVALID_DISCOUNT_INPUT_MESSAGE =
             "Input should be a number greater than 0 and smaller than 100";
-    private final UUID voucherID;
+    private final UUID voucherId;
     private final BigDecimal discountValue;
     private final VoucherType voucherType;
 
-    public Voucher(UUID voucherID, VoucherType voucherType, BigDecimal discountValue) {
-        this.voucherID = voucherID;
+    public Voucher(UUID voucherId, BigDecimal discountValue, VoucherType voucherType) {
+        this.voucherId = voucherId;
         this.voucherType = voucherType;
         this.voucherType.validateDiscountValue(discountValue);
         this.discountValue = discountValue;
     }
 
     public UUID getVoucherId() {
-        return voucherID;
+        return voucherId;
     }
 
     public BigDecimal getDiscountValue() {
         return discountValue;
+    }
+
+    public VoucherType getVoucherType() {
+        return voucherType;
     }
 
     public BigDecimal discount(BigDecimal priceBeforeDiscount) {
