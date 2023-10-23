60055d0c6e0c81366ef7abf5fd1449a99e65e156
happyjamy <78072370+happyjamy@users.noreply.github.com>
2023-10-23 10:59:57 +0900

    [change] 콘솔 유효성 검사 부분 분리

--- src/main/java/com/programmers/springbootbasic/infrastructure/IO/Console.java
+++ src/main/java/com/programmers/springbootbasic/infrastructure/IO/Console.java
@@ -2,52 +2,31 @@ package com.programmers.springbootbasic.infrastructure.IO;
 
 import com.programmers.springbootbasic.presentation.IOManager;
 import com.programmers.springbootbasic.presentation.validation.InputValidator;
-import jakarta.annotation.PostConstruct;
 import java.util.Scanner;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Console implements IOManager {
-    private final Scanner scanner = new Scanner(System.in);
-    private InputValidator<String> stringValidator;
-    private InputValidator<String> integerValidator;
 
-    @PostConstruct
-    private void setValidator() {
-        InputValidator<String> stringValidator = new InputValidator<>();
-        stringValidator.addValidator(input -> {
-            if(input == null || input.isEmpty()) {
-                return "입력 값이 없습니다.";
-            }
-            return null;
-        });
+    private final Scanner scanner;
+    private final InputValidator<String> stringValidator;
+    private final InputValidator<String> integerValidator;
+
+    public Console(@Qualifier("getStringValidator") InputValidator<String> stringValidator,
+        @Qualifier("getIntegerValidator") InputValidator<String> integerValidator) {
+        this.scanner = new Scanner(System.in);
         this.stringValidator = stringValidator;
-
-        InputValidator<String> integerValidator = new InputValidator<>();
-        integerValidator.addValidator(input -> {
-            if(input == null || input.isEmpty()) {
-                return "입력 값이 없습니다.";
-            }
-            return null;
-        });
-        integerValidator.addValidator(input -> {
-            try {
-                Integer.parseInt(input);
-                return null;
-            } catch (NumberFormatException e) {
-                return "숫자가 아닙니다.";
-            }
-        });
         this.integerValidator = integerValidator;
     }
 
     public String collectStringInput(String message) {
-        while(true) {
+        while (true) {
             print(message);
             var result = scanner.nextLine().trim();
             var errors = stringValidator.validate(result);
 
-            if(errors.isEmpty()) {
+            if (errors.isEmpty()) {
                 return result;
             }
             errors.get().forEach(this::print);
@@ -55,12 +34,12 @@ public class Console implements IOManager {
     }
 
     public int collectIntegerInput(String message) {
-        while(true) {
+        while (true) {
             print(message);
             var result = scanner.nextLine().trim();
             var errors = integerValidator.validate(result);
 
-            if(errors.isEmpty()) {
+            if (errors.isEmpty()) {
                 return Integer.parseInt(result);
             }
             errors.get().forEach(this::print);
