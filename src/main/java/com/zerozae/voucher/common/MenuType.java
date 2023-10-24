bdc9065c4095ef870475ec47f2e479767811ef1b
park0jae <okvv26@naver.com>
2023-10-24 20:08:57 +0900

    feat : MenuType의 메뉴 리스트 추가

--- src/main/java/com/zerozae/voucher/common/MenuType.java
+++ src/main/java/com/zerozae/voucher/common/MenuType.java
@@ -1,8 +1,6 @@
 package com.zerozae.voucher.common;
 
 import com.zerozae.voucher.exception.ErrorMessage;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 
 public enum MenuType {
@@ -13,15 +11,20 @@ public enum MenuType {
     BLACKLIST,
     VOUCHER,
     CUSTOMER,
+    REGISTER,
+    REMOVE,
+    VOUCHER_LIST,
+    OWNER,
+    SEARCH,
+    DELETE,
+    DELETE_ALL,
+    UPDATE,
     BACK;
-
-    private static final Logger logger = LoggerFactory.getLogger(MenuType.class);
 
     public static MenuType of(String input){
         try {
             return MenuType.valueOf(input.toUpperCase());
         }catch (RuntimeException e){
-            logger.warn("Error Message = {}", e.getMessage());
             throw ErrorMessage.error("존재하지 않는 메뉴입니다. 다시 입력해주세요.");
         }
     }
