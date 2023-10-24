d2a18c97299807ff5975e4115261e4771939ff27
park0jae <okvv26@naver.com>
2023-10-24 20:00:26 +0900

    feat : VoucherService CRUD 관련 메서드 및 지갑 기능 메서드 추가 구현

--- src/main/java/com/zerozae/voucher/service/voucher/VoucherService.java
+++ src/main/java/com/zerozae/voucher/service/voucher/VoucherService.java
@@ -6,18 +6,19 @@ import com.zerozae.voucher.domain.voucher.Voucher;
 import com.zerozae.voucher.domain.voucher.VoucherType;
 import com.zerozae.voucher.dto.voucher.VoucherRequest;
 import com.zerozae.voucher.dto.voucher.VoucherResponse;
+import com.zerozae.voucher.dto.voucher.VoucherUpdateRequest;
 import com.zerozae.voucher.exception.ErrorMessage;
 import com.zerozae.voucher.repository.voucher.VoucherRepository;
-import lombok.extern.flogger.Flogger;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class VoucherService {
 
+    private static final String VOUCHER_NOT_FOUND_MESSAGE = "바우처가 존재하지 않습니다.";
     private final VoucherRepository voucherRepository;
 
     public VoucherService(VoucherRepository voucherRepository) {
@@ -38,10 +39,61 @@ public class VoucherService {
         }
     }
 
+    public VoucherResponse findById(UUID voucherId) {
+        Voucher voucher = voucherRepository.findById(voucherId).orElseThrow(() -> ErrorMessage.error(VOUCHER_NOT_FOUND_MESSAGE));
+        return VoucherResponse.toDto(voucher);
+    }
+
+    public UUID findVoucherOwnerId(UUID voucherId) {
+        voucherRepository.findById(voucherId).orElseThrow(() -> ErrorMessage.error(VOUCHER_NOT_FOUND_MESSAGE));
+        return voucherRepository.findVoucherOwner(voucherId).orElseThrow(() -> ErrorMessage.error("바우처 소유자가 존재하지 않습니다."));
+    }
+
     public List<VoucherResponse> findAllVouchers(){
         return voucherRepository.findAll()
                 .stream()
                 .map(VoucherResponse::toDto)
                 .toList();
     }
+
+    public List<VoucherResponse> findHaveVouchers(UUID customerId) {
+        List<Voucher> vouchers = voucherRepository.findVouchersByCustomerId(customerId);
+
+        return vouchers.stream()
+                .map(VoucherResponse::toDto)
+                .toList();
+    }
+
+    public void registerVoucher(UUID customerId, UUID voucherId) {
+        voucherRepository.findById(voucherId).orElseThrow(() -> ErrorMessage.error(VOUCHER_NOT_FOUND_MESSAGE));
+        if(isAlreadyRegisterVoucher(voucherId)){
+            throw ErrorMessage.error("이미 다른 회원에게 등록된 바우처입니다.");
+        };
+        voucherRepository.registerVoucher(customerId, voucherId);
+    }
+
+    public void removeVoucherFromCustomer(UUID voucherId) {
+        voucherRepository.findById(voucherId).orElseThrow(() -> ErrorMessage.error(VOUCHER_NOT_FOUND_MESSAGE));
+        voucherRepository.removeVoucher(voucherId);
+    }
+
+    public void update(UUID voucherId, VoucherUpdateRequest voucherUpdateRequest) {
+        voucherRepository.findById(voucherId).orElseThrow(() -> ErrorMessage.error(VOUCHER_NOT_FOUND_MESSAGE));
+        voucherRepository.update(voucherId, voucherUpdateRequest);
+    }
+
+    public void deleteById(UUID voucherId){
+        voucherRepository.findById(voucherId).orElseThrow(() -> ErrorMessage.error(VOUCHER_NOT_FOUND_MESSAGE));
+        voucherRepository.deleteById(voucherId);
+    }
+
+    public void deleteAll() {
+        voucherRepository.deleteAll();
+    }
+
+    private boolean isAlreadyRegisterVoucher(UUID voucherId) {
+        Optional<UUID> owner = voucherRepository.findVoucherOwner(voucherId);
+        return owner.isPresent();
+    }
 }
+
