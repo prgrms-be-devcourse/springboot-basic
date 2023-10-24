3e6c0fec3d4ca82b58e3aee1f057510cb7b70fff
park0jae <okvv26@naver.com>
2023-10-24 19:54:19 +0900

    feat : JdbcCustomerRepository, JdbcVoucherRepository 구현

---
+++ src/main/java/com/zerozae/voucher/repository/customer/JdbcCustomerRepository.java
@@ -0,0 +1,114 @@
+package com.zerozae.voucher.repository.customer;
+
+import com.zerozae.voucher.domain.customer.Customer;
+import com.zerozae.voucher.domain.customer.CustomerType;
+import com.zerozae.voucher.dto.customer.CustomerRequest;
+import com.zerozae.voucher.exception.ErrorMessage;
+import org.springframework.context.annotation.Profile;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.UUID;
+
+import static com.zerozae.voucher.util.UuidConverter.*;
+import static com.zerozae.voucher.util.UuidConverter.toUUID;
+
+
+@Profile("prod")
+@Repository
+public class JdbcCustomerRepository implements CustomerRepository{
+
+    private static final String CUSTOMER_ID = "customerId";
+    private static final String CUSTOMER_NAME = "customerName";
+    private static final String CUSTOMER_TYPE = "customerType";
+    private static final RowMapper<Customer> customerRowMapper = (resultSet, i) -> {
+        UUID customerId = toUUID(resultSet.getBytes("customer_id"));
+        String customerName = resultSet.getString("customer_name");
+        CustomerType customerType = CustomerType.valueOf(resultSet.getString("customer_type"));
+
+        return new Customer(customerId, customerName, customerType);
+    };
+    private final NamedParameterJdbcTemplate jdbcTemplate;
+
+    public JdbcCustomerRepository(NamedParameterJdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    @Transactional
+    public Customer save(Customer customer) {
+        int updated = jdbcTemplate.update(
+                "insert into customers(customer_id,customer_name , customer_type) values (UUID_TO_BIN(:customerId), :customerName, :customerType)",
+                toParamMap(customer));
+
+        if(updated != 1){
+            String SAVE_FAILED_MESSAGE = "저장에 실패했습니다.";
+            throw ErrorMessage.error(SAVE_FAILED_MESSAGE);
+        }
+        return customer;
+    }
+
+    @Override
+    public List<Customer> findAll() {
+        return jdbcTemplate.query("select * from customers", customerRowMapper);
+    }
+
+    @Override
+    public Optional<Customer> findById(UUID customerId) {
+        try {
+            Customer customer = jdbcTemplate.queryForObject(
+                    "select * from customers where customer_id = UUID_TO_BIN(:customerId)",
+                    Collections.singletonMap(CUSTOMER_ID, customerId.toString().getBytes()),
+                    customerRowMapper);
+            return Optional.of(customer);
+        }catch (EmptyResultDataAccessException e){
+            return Optional.empty();
+        }
+    }
+
+    @Transactional
+    @Override
+    public void deleteById(UUID customerId) {
+        jdbcTemplate.update(
+                "delete from customers where customer_id = UUID_TO_BIN(:customerId)",
+                Collections.singletonMap(CUSTOMER_ID, customerId.toString().getBytes()));
+    }
+
+    @Transactional
+    @Override
+    public void deleteAll() {
+        jdbcTemplate.getJdbcOperations().update("delete from customers");
+    }
+
+    @Transactional
+    @Override
+    public void update(UUID customerId, CustomerRequest customerRequest) {
+        int update = jdbcTemplate.update(
+                "update customers set customer_name = :customerName, customer_type = :customerType where customer_id = UUID_TO_BIN(:customerId)",
+                Map.of(
+                        CUSTOMER_NAME, customerRequest.getCustomerName(),
+                        CUSTOMER_TYPE, customerRequest.getCustomerType().toString(),
+                        CUSTOMER_ID, customerId.toString().getBytes()));
+
+        if(update != 1){
+            String UPDATE_FAILED_MESSAGE = "업데이트에 실패했습니다.";
+            throw ErrorMessage.error(UPDATE_FAILED_MESSAGE);
+        }
+    }
+
+    private Map<String, Object> toParamMap(Customer customer) {
+        return new HashMap<>() {{
+            put(CUSTOMER_ID, customer.getCustomerId().toString().getBytes());
+            put(CUSTOMER_NAME, customer.getCustomerName());
+            put(CUSTOMER_TYPE, customer.getCustomerType().toString());
+        }};
+    }
+}
---
+++ src/main/java/com/zerozae/voucher/repository/voucher/JdbcVoucherRepository.java
@@ -0,0 +1,171 @@
+package com.zerozae.voucher.repository.voucher;
+
+import com.zerozae.voucher.domain.voucher.FixedDiscountVoucher;
+import com.zerozae.voucher.domain.voucher.PercentDiscountVoucher;
+import com.zerozae.voucher.domain.voucher.UseStatusType;
+import com.zerozae.voucher.domain.voucher.Voucher;
+import com.zerozae.voucher.domain.voucher.VoucherType;
+import com.zerozae.voucher.dto.voucher.VoucherUpdateRequest;
+import com.zerozae.voucher.exception.ErrorMessage;
+import com.zerozae.voucher.util.UuidConverter;
+import org.springframework.context.annotation.Profile;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.UUID;
+
+import static com.zerozae.voucher.domain.voucher.VoucherType.FIXED;
+import static com.zerozae.voucher.util.UuidConverter.*;
+import static com.zerozae.voucher.util.UuidConverter.toUUID;
+
+@Profile("prod")
+@Repository
+public class JdbcVoucherRepository implements VoucherRepository {
+
+    private static final String VOUCHER_ID = "voucherId";
+    private static final String CUSTOMER_ID = "customerId";
+    private static final String DISCOUNT = "discount";
+    private static final String VOUCHER_TYPE = "voucherType";
+    private static final String USE_STATUS_TYPE = "useStatusType";
+
+    private static final RowMapper<Voucher> voucherRowMapper = (resultSet, i) -> {
+        UUID voucherId = toUUID(resultSet.getBytes("voucher_id"));
+        long discount = resultSet.getLong(DISCOUNT);
+        VoucherType voucherType = VoucherType.valueOf(resultSet.getString("voucher_type"));
+        UseStatusType useStatusType = UseStatusType.valueOf(resultSet.getString("use_status_type"));
+
+        if (voucherType.equals(FIXED)) {
+            return new FixedDiscountVoucher(voucherId, discount, useStatusType);
+        } else {
+            return new PercentDiscountVoucher(voucherId, discount, useStatusType);
+        }
+    };
+
+    private final NamedParameterJdbcTemplate jdbcTemplate;
+
+    public JdbcVoucherRepository(NamedParameterJdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Transactional
+    @Override
+    public Voucher save(Voucher voucher) {
+        int result = jdbcTemplate.update(
+                "insert into vouchers(voucher_id ,discount,voucher_type, use_status_type) values (UUID_TO_BIN(:voucherId), :discount, :voucherType, :useStatusType)",
+                toParamMap(voucher));
+
+        if(result != 1){
+            throw ErrorMessage.error("저장에 실패했습니다.");
+        }
+
+        return voucher;
+    }
+
+    @Override
+    public List<Voucher> findAll() {
+        return jdbcTemplate.query(
+                "select * from vouchers",
+                voucherRowMapper);
+    }
+
+    @Override
+    public Optional<Voucher> findById(UUID voucherId) {
+        try {
+            Voucher voucher = jdbcTemplate.queryForObject(
+                    "select * from vouchers where voucher_id = UUID_TO_BIN(:voucherId)",
+                    Map.of(VOUCHER_ID, voucherId.toString().getBytes()),
+                    voucherRowMapper);
+            return Optional.ofNullable(voucher);
+        } catch (EmptyResultDataAccessException e) {
+            return Optional.empty();
+        }
+    }
+
+    @Transactional
+    @Override
+    public void registerVoucher(UUID customerId, UUID voucherId) {
+        int result = jdbcTemplate.update(
+                "update vouchers set customer_id = UUID_TO_BIN(:customerId) where voucher_id = UUID_TO_BIN(:voucherId)",
+                Map.of(
+                    CUSTOMER_ID, customerId.toString().getBytes(),
+                    VOUCHER_ID, voucherId.toString().getBytes()
+                ));
+
+        if(result != 1){
+            throw ErrorMessage.error("바우처 등록에 실패했습니다.");
+        }
+    }
+
+    @Override
+    public List<Voucher> findVouchersByCustomerId(UUID customerId) {
+        return jdbcTemplate.query(
+                "select * from vouchers where customer_id = UUID_TO_BIN(:customerId)",
+                Map.of(CUSTOMER_ID, customerId.toString().getBytes()),
+                voucherRowMapper);
+    }
+
+    @Transactional
+    @Override
+    public void removeVoucher(UUID voucherId) {
+        int result = jdbcTemplate.update(
+                "update vouchers set customer_id = null where voucher_id = UUID_TO_BIN(:voucherId)",
+                Map.of(VOUCHER_ID, voucherId.toString().getBytes()));
+
+        if(result != 1){
+            throw ErrorMessage.error("바우처 제거에 실패했습니다.");
+        }
+    }
+
+    @Override
+    public Optional<UUID> findVoucherOwner(UUID voucherId) {
+        return Optional.ofNullable(jdbcTemplate.queryForObject(
+                        "SELECT customer_id FROM vouchers WHERE voucher_id = UUID_TO_BIN(:voucherId)",
+                        Map.of("voucherId", voucherId.toString().getBytes()),
+                        byte[].class))
+                .map(UuidConverter::toUUID);
+    }
+
+    @Transactional
+    @Override
+    public void deleteById(UUID voucherId) {
+        jdbcTemplate.update(
+                "delete from vouchers where voucher_id = UUID_TO_BIN(:voucherId)",
+                Map.of(VOUCHER_ID, voucherId.toString().getBytes()));
+    }
+
+    @Transactional
+    @Override
+    public void deleteAll() {
+        jdbcTemplate.getJdbcOperations().update("delete from vouchers");
+    }
+
+    @Transactional
+    @Override
+    public void update(UUID voucherId, VoucherUpdateRequest voucherUpdateRequest) {
+        int result = jdbcTemplate.update(
+                "update vouchers set discount = :discount, use_status_type = :useStatusType where voucher_id = UUID_TO_BIN(:voucherId)",
+                Map.of(
+                        DISCOUNT, voucherUpdateRequest.getDiscount(),
+                        USE_STATUS_TYPE, voucherUpdateRequest.getUseStatusType().toString(),
+                        VOUCHER_ID, voucherId.toString().getBytes()));
+
+        if(result != 1){
+            throw ErrorMessage.error("바우처 업데이트에 실패했습니다.");
+        }
+    }
+
+    private MapSqlParameterSource toParamMap(Voucher voucher) {
+        return new MapSqlParameterSource()
+                .addValue(VOUCHER_ID, voucher.getVoucherId().toString().getBytes())
+                .addValue(DISCOUNT, voucher.getDiscount())
+                .addValue(VOUCHER_TYPE, voucher.getVoucherType().toString())
+                .addValue(USE_STATUS_TYPE, voucher.getUseStatusType().toString());
+    }
+}
