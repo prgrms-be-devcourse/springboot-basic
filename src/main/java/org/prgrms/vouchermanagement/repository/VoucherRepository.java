e26c1869e2dad08b845f7591d5531304fda68005
kimnamgyu <lethargic.hood@gmail.com>
2023-10-24 13:49:50 +0900

    Refactor : create 메서드의 인자 변경

--- src/main/java/org/prgrms/vouchermanagement/repository/VoucherRepository.java
+++ src/main/java/org/prgrms/vouchermanagement/repository/VoucherRepository.java
@@ -1,13 +1,13 @@
 package org.prgrms.vouchermanagement.repository;
 
-import org.prgrms.vouchermanagement.voucher.PolicyStatus;
+import org.prgrms.vouchermanagement.voucher.DiscountPolicy;
 import org.prgrms.vouchermanagement.voucher.Voucher;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface VoucherRepository {
-     void create(UUID voucherId, long amountOrPercent, PolicyStatus policy);
+     void create(UUID voucherId, DiscountPolicy discountPolicy);
 
      Voucher getById(UUID voucherId);
 
