00eae83d2e68578fa05cb529f0cb8a2a7d0db41e
KarmaPol <kkhkr98@gmail.com>
2023-10-25 00:35:18 +0900

    feat : configuration @EnableTransactionalManagement 추가

--- src/main/java/com/pgms/part1/config/AppConfig.java
+++ src/main/java/com/pgms/part1/config/AppConfig.java
@@ -5,9 +5,11 @@ import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
 
+@EnableTransactionManagement
 @Configuration
 public class AppConfig {
 
