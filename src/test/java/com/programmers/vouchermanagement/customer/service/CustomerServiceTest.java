4a0ce9229642ebdf9a76e6a12f01c9ef9f28067e
Jay <injaesong0@gmail.com>
2023-10-25 04:52:26 +0900

    test: return blacklist successfully

--- src/test/java/com/programmers/vouchermanagement/customer/service/CustomerServiceTest.java
+++ src/test/java/com/programmers/vouchermanagement/customer/service/CustomerServiceTest.java
@@ -1,21 +1,23 @@
 package com.programmers.vouchermanagement.customer.service;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.UUID;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import com.programmers.vouchermanagement.customer.domain.Customer;
+import com.programmers.vouchermanagement.customer.domain.CustomerType;
 import com.programmers.vouchermanagement.customer.dto.CustomerResponse;
 import com.programmers.vouchermanagement.customer.repository.CustomerRepository;
-import com.programmers.vouchermanagement.voucher.service.VoucherService;
 
 class CustomerServiceTest {
     static CustomerRepository customerRepository;
@@ -38,5 +40,20 @@ class CustomerServiceTest {
 
         //then
         assertThat(blacklist.isEmpty(), is(true));
+    }
+
+    @Test
+    @DisplayName("저장된 블랙리스트 조회에 성공한다.")
+    void testReadBlacklistSuccessful_ReturnList() {
+        //given
+        Customer firstCustomer = new Customer(UUID.randomUUID(), "black 1", CustomerType.BLACK);
+        Customer secondCustomer = new Customer(UUID.randomUUID(), "black 2", CustomerType.BLACK);
+        doReturn(List.of(firstCustomer, secondCustomer)).when(customerRepository).findBlackCustomers();
+
+        //when
+        List<CustomerResponse> blacklist = customerService.readBlacklist();
+
+        //then
+        assertThat(blacklist, hasSize(2));
     }
 }
