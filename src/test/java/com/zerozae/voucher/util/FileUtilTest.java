7a300644f2a8e8ca05c2e24fd9ce3697e65d0b20
park0jae <okvv26@naver.com>
2023-10-24 20:31:55 +0900

    test : FileUtil 클래스에 대한 파일 관련 로직 테스트 코드 작성

---
+++ src/test/java/com/zerozae/voucher/util/FileUtilTest.java
@@ -0,0 +1,121 @@
+package com.zerozae.voucher.util;
+
+import com.zerozae.voucher.domain.customer.Customer;
+import com.zerozae.voucher.domain.customer.CustomerType;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class FileUtilTest {
+
+    private final String testFilePath = "test.csv";
+    Customer normalCustomer;
+    Customer blacklistCustomer;
+    FileUtil fileUtil;
+    File testFile;
+
+    @BeforeEach
+    void setUp() {
+        fileUtil = new FileUtil();
+        normalCustomer = new Customer(UUID.randomUUID(), "normalUser", CustomerType.NORMAL);
+        blacklistCustomer= new Customer(UUID.randomUUID(), "blackUser", CustomerType.BLACKLIST);
+
+        testFile = new File(testFilePath);
+        try {
+            testFile.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @AfterEach
+    void cleanUp() {
+        if (testFile.exists()) {
+            testFile.delete();
+        }
+    }
+
+    @Test
+    @DisplayName("파일에 회원 저장 테스트")
+    void saveCustomerToFile_success_Test() {
+        // Given
+
+        // When
+        fileUtil.saveToCsvFile(getCustomerInfo(normalCustomer), testFilePath);
+
+        // Then
+        List<String> strings = fileUtil.loadFromCsvFile(testFilePath);
+        assertTrue(strings.contains(getCustomerInfo(normalCustomer)));
+    }
+
+    @Test
+    @DisplayName("파일 회원 삭제 테스트")
+    void deleteCustomerFromFile_success_Test() {
+        // Given
+        fileUtil.saveToCsvFile(getCustomerInfo(normalCustomer), testFilePath);
+        fileUtil.deleteFileDataById(normalCustomer.getCustomerId(), testFilePath);
+
+        // When & Then
+        List<String> strings = fileUtil.loadFromCsvFile(testFilePath);
+        assertTrue(strings.isEmpty());
+    }
+
+    @Test
+    @DisplayName("파일 회원 수정 테스트")
+    void updateCustomerFromFile_success_Test() {
+        // Given
+        fileUtil.saveToCsvFile(getCustomerInfo(normalCustomer), testFilePath);
+
+        // When
+        fileUtil.updateFile(getCustomerInfo(blacklistCustomer),normalCustomer.getCustomerId(), testFilePath);
+
+        // Then
+        List<String> strings = fileUtil.loadFromCsvFile(testFilePath);
+        assertTrue(strings.contains(getCustomerInfo(blacklistCustomer)));
+    }
+
+    @Test
+    @DisplayName("파일 회원 load 테스트")
+    void loadCustomer_success_Test() {
+        // Given
+        fileUtil.saveToCsvFile(getCustomerInfo(normalCustomer), testFilePath);
+        fileUtil.saveToCsvFile(getCustomerInfo(blacklistCustomer), testFilePath);
+
+        // When
+        List<String> strings = fileUtil.loadFromCsvFile(testFilePath);
+
+        // Then
+        String compareString = getCustomerInfo(normalCustomer) + getCustomerInfo(blacklistCustomer);
+        assertTrue(strings.contains(compareString));
+    }
+
+    @Test
+    @DisplayName("파일 제거 후 재생성 테스트")
+    void clearDataFile_success_Test() {
+        // Given
+        fileUtil.saveToCsvFile(getCustomerInfo(normalCustomer), testFilePath);
+        fileUtil.saveToCsvFile(getCustomerInfo(blacklistCustomer), testFilePath);
+
+        // When
+        fileUtil.clearDataFile(testFilePath);
+
+        // Then
+        List<String> strings = fileUtil.loadFromCsvFile(testFilePath);
+        assertTrue(strings.isEmpty());
+    }
+
+    private String getCustomerInfo(Customer customer) {
+        String customerId = String.valueOf(customer.getCustomerId());
+        String customerName = customer.getCustomerName();
+        String customerType = String.valueOf(customer.getCustomerType());
+        return String.join(",", customerId,customerName, customerType);
+    }
+}
