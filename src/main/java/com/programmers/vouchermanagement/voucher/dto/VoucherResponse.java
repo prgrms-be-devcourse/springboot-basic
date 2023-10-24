784c62e39264b2dca497b95d7c712503df9d1582
Jay <injaesong0@gmail.com>
2023-10-24 11:31:28 +0900

    refactor: enable VoucherResponse to tell more information

--- src/main/java/com/programmers/vouchermanagement/voucher/dto/VoucherResponse.java
+++ src/main/java/com/programmers/vouchermanagement/voucher/dto/VoucherResponse.java
@@ -6,8 +6,34 @@ import java.util.UUID;
 import com.programmers.vouchermanagement.voucher.domain.Voucher;
 import com.programmers.vouchermanagement.voucher.domain.VoucherType;
 
-public record VoucherResponse(UUID voucherId, BigDecimal discountValue, VoucherType voucherType) {
+public class VoucherResponse {
+    private final UUID voucherId;
+    private final BigDecimal discountValue;
+    private final VoucherType voucherType;
+
+    private VoucherResponse(UUID voucherId, BigDecimal discountValue, VoucherType voucherType) {
+        this.voucherId = voucherId;
+        this.discountValue = discountValue;
+        this.voucherType = voucherType;
+    }
+
     public static VoucherResponse from(Voucher voucher) {
         return new VoucherResponse(voucher.getVoucherId(), voucher.getDiscountValue(), voucher.getVoucherType());
+    }
+
+    public UUID getVoucherId() {
+        return voucherId;
+    }
+
+    public BigDecimal getDiscountValue() {
+        return discountValue;
+    }
+
+    public boolean isPercentVoucher() {
+        return voucherType.isPercent();
+    }
+
+    public String getVoucherTypeName() {
+        return voucherType.displayTypeName();
     }
 }
