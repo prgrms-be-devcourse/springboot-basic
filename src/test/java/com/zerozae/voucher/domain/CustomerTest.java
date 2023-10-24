cca845b795e0eeee1de8adf1a4288807f177ccc5
park0jae <okvv26@naver.com>
2023-10-24 20:31:11 +0900

    test : Customer, Voucher 각각 도메인에 대한 테스트 코드 추가 및 수정

--- src/test/java/com/zerozae/voucher/domain/CustomerTest.java
+++ src/test/java/com/zerozae/voucher/domain/CustomerTest.java
@@ -2,6 +2,7 @@ package com.zerozae.voucher.domain;
 
 import com.zerozae.voucher.domain.customer.Customer;
 import com.zerozae.voucher.domain.customer.CustomerType;
+import com.zerozae.voucher.dto.customer.CustomerRequest;
 import com.zerozae.voucher.exception.ErrorMessage;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -10,11 +11,11 @@ import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class CustomerTest {
+class CustomerTest {
 
     @Test
     @DisplayName("회원 생성 성공 테스트")
-    void createCustomerSuccessTest(){
+    void createCustomer_Success_Test() {
         // Given
         UUID customerId = UUID.randomUUID();
         String username = "영재";
@@ -28,20 +29,57 @@ public class CustomerTest {
         assertEquals(createdCustomer.getCustomerId(), customerId);
     }
 
-
     @Test
-    @DisplayName("회원 생성 실패 테스트")
-    void createCustomerFailedTest(){
+    @DisplayName("회원 생성 실패 테스트 (빈 값 입력)")
+    void createCustomer_emptyInput_Failed_Test() {
         // Given
         UUID customerId = UUID.randomUUID();
         String username = "";
         CustomerType customerType = CustomerType.BLACKLIST;
 
-        // When
-
-        // Then
+        // When & Then
         assertThrows(ErrorMessage.class, () -> {
             new Customer(customerId, username, customerType);
+        });
+    }
+
+    @Test
+    @DisplayName("회원 수정 성공 테스트")
+    void customerUpdate_Success_Test() {
+        // Given
+        UUID customerId = UUID.randomUUID();
+        String username = "영재";
+        CustomerType customerType = CustomerType.BLACKLIST;
+        Customer createdCustomer = new Customer(customerId, username, customerType);
+
+        String newUsername = "zerozae";
+        CustomerType newCustomerType = CustomerType.NORMAL;
+        CustomerRequest customerRequest = new CustomerRequest(newUsername, newCustomerType);
+
+        // When
+        createdCustomer.updateCustomerInfo(customerRequest);
+
+        // Then
+        assertEquals(createdCustomer.getCustomerName(), newUsername);
+        assertEquals(createdCustomer.getCustomerType() , newCustomerType);
+    }
+
+    @Test
+    @DisplayName("회원 수정 실패 테스트(빈 값 입력)")
+    void customerUpdate_emptyInput_Failed_Test() {
+        // Given
+        UUID customerId = UUID.randomUUID();
+        String username = "영재";
+        CustomerType customerType = CustomerType.BLACKLIST;
+        Customer createdCustomer = new Customer(customerId, username, customerType);
+
+        String newUsername = "";
+        CustomerType newCustomerType = CustomerType.NORMAL;
+        CustomerRequest customerRequest = new CustomerRequest(newUsername, newCustomerType);
+
+        // When & Then
+        assertThrows(ErrorMessage.class, () -> {
+            createdCustomer.updateCustomerInfo(customerRequest);
         });
     }
 }
--- src/test/java/com/zerozae/voucher/domain/VoucherTest.java
+++ src/test/java/com/zerozae/voucher/domain/VoucherTest.java
@@ -1,18 +1,21 @@
 package com.zerozae.voucher.domain;
 
 import com.zerozae.voucher.domain.voucher.FixedDiscountVoucher;
+import com.zerozae.voucher.domain.voucher.UseStatusType;
 import com.zerozae.voucher.domain.voucher.Voucher;
+import com.zerozae.voucher.domain.voucher.VoucherType;
+import com.zerozae.voucher.dto.voucher.VoucherUpdateRequest;
 import com.zerozae.voucher.exception.ErrorMessage;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class VoucherTest {
+class VoucherTest {
 
     @Test
     @DisplayName("바우처 생성 성공 테스트")
-    void createVoucherSuccessTest(){
+    void createVoucher_Success_Test(){
         // Given
         long discount = 10L;
 
@@ -25,15 +28,52 @@ public class VoucherTest {
 
     @Test
     @DisplayName("바우처 생성 실패 테스트 (음수 값 입력)")
-    void createVoucherFailedTest(){
+    void createVoucher_negativeInput_Failed_Test(){
         // Given
         long discount = -10L;
 
-        // When
-
-        // Then
+        // When & Then
         assertThrows(ErrorMessage.class, () -> {
             new FixedDiscountVoucher(discount);
+        });
+    }
+
+    @Test
+    @DisplayName("바우처 수정 성공 테스트")
+    void updateVoucher_Success_Test(){
+        // Given
+        long discount = 10L;
+        Voucher voucher = new FixedDiscountVoucher(discount);
+
+        long newDiscount = 20L;
+        UseStatusType newUseStatusType = UseStatusType.UNAVAILABLE;
+
+        VoucherUpdateRequest voucherUpdateRequest = new VoucherUpdateRequest(newDiscount, newUseStatusType);
+
+        // When
+        voucher.updateVoucherInfo(voucherUpdateRequest);
+
+
+        // Then
+        assertEquals(voucher.getDiscount(), newDiscount);
+        assertEquals(voucher.getUseStatusType(), newUseStatusType);
+    }
+
+    @Test
+    @DisplayName("바우처 수정 실패 테스트 (음수 값 입력)")
+    void updateVoucher_Failed_Test(){
+        // Given
+        long discount = 10L;
+        Voucher voucher = new FixedDiscountVoucher(discount);
+
+        long newDiscount = -20L;
+        UseStatusType newUseStatusType = UseStatusType.UNAVAILABLE;
+
+        VoucherUpdateRequest voucherUpdateRequest = new VoucherUpdateRequest(newDiscount, newUseStatusType);
+
+        // When & Then
+        assertThrows(ErrorMessage.class, () -> {
+            voucher.updateVoucherInfo(voucherUpdateRequest);
         });
     }
 }
