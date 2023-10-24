619de63bea1d983dc5a7ba936d3f3f3f26edae88
suzzingv <84590065+suzzingv@users.noreply.github.com>
2023-10-25 02:43:13 +0900

    refactor : console message 정리

--- src/main/java/com/prgrms/vouchermanager/message/ConsoleMessage.java
+++ src/main/java/com/prgrms/vouchermanager/message/ConsoleMessage.java
@@ -4,20 +4,20 @@ import lombok.Getter;
 
 @Getter
 public enum ConsoleMessage {
+    SELECT_PROGRAM("""
+    Select the program. voucher or customer or wallet?
+    If you want to exit, type exit."""),
+    COMPLETE_UPDATE_CUSTOMER("Update completed."),
+    GET_CUSTOMER_ID("Type the customer ID."),
+    GET_VOUCHER_ID("Type the voucher ID."),
+    COMPLETE_DELETE_CUSTOMER("Delete completed."),
+    FINISH_PROGRAM("Exit the program."),
+
     SELECT_FUNCTION_VOUCHER("""
             === Voucher Program ===
             Type exit to exit the program.
             Type create to create a new voucher.
             Type list to list all vouchers."""),
-    SELECT_FUNCTION_CUSTOMER("""
-            === Customer Program ===
-            Type exit to exit the program.
-            Type create to create a new customer.
-            Type list to list all customers.
-            Type update to update customer.
-            Type delete to delete customer.
-            Type blacklist to show blacklist."""),
-    FINISH_PROGRAM("Exit the program."),
     GET_VOUCHER_TYPE("""
             Type fixed if you want a fixed discount voucher.
             Type percent if you want a percent discount voucher."""),
@@ -31,18 +31,21 @@ public enum ConsoleMessage {
     NOT_CORRECT_FORM("The input format is correctly."),
     NOT_CORRECT_SCOPE("Percent range is incorrect."),
     NOT_CORRECT_ID("ID is not correct."),
-    SELECT_PROGRAM("""
-    Select the program. voucher or customer or wallet?
-    If you want to exit, type exit."""),
+
+    SELECT_FUNCTION_CUSTOMER("""
+            === Customer Program ===
+            Type exit to exit the program.
+            Type create to create a new customer.
+            Type list to list all customers.
+            Type update to update customer.
+            Type delete to delete customer.
+            Type blacklist to show blacklist."""),
     GET_CUSTOMER_NAME("Type the name."),
     GET_CUSTOMER_YEAR("Type the year of birth."),
     SELECT_UPDATE_TARGET("""
             Type name if you want to update name.
             Type year if you want to update year of birth."""),
-    COMPLETE_UPDATE_CUSTOMER("Update completed."),
-    GET_CUSTOMER_ID("Type the customer ID."),
-    GET_VOUCHER_ID("Type the voucher ID."),
-    COMPLETE_DELETE_CUSTOMER("Delete completed."),
+
     SELECT_FUNCTION_WALLET("""
             === Wallet Program ===
             Type exit to exit the program.
