61c30b958aa67e66dc763081b2002dc62fc61a02
kimnamgyu <lethargic.hood@gmail.com>
2023-10-24 13:37:37 +0900

    Refactor : record 클래스 변환

--- src/main/java/org/prgrms/vouchermanagement/dto/VoucherInfo.java
+++ src/main/java/org/prgrms/vouchermanagement/dto/VoucherInfo.java
@@ -4,4 +4,5 @@ import org.prgrms.vouchermanagement.voucher.PolicyStatus;
 
 public record VoucherInfo(
         PolicyStatus policy,
-        long amountOrPercent) {}
+        long amountOrPercent
+) { }
