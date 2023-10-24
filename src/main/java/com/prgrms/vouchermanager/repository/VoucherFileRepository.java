ab0192b4c50603818aab26d0020798fd447cb594
suzzingv <84590065+suzzingv@users.noreply.github.com>
2023-10-24 20:11:57 +0900

    feat : file voucher db에 적용

--- src/main/java/com/prgrms/vouchermanager/repository/VoucherFileRepository.java
+++ src/main/java/com/prgrms/vouchermanager/repository/VoucherFileRepository.java
@@ -5,6 +5,7 @@ import com.prgrms.vouchermanager.io.FileIO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
 import java.util.HashMap;
--- src/main/java/com/prgrms/vouchermanager/repository/VoucherJdbcRepository.java
+++ src/main/java/com/prgrms/vouchermanager/repository/VoucherJdbcRepository.java
@@ -4,6 +4,8 @@ import com.prgrms.vouchermanager.domain.Customer;
 import com.prgrms.vouchermanager.domain.FixedAmountVoucher;
 import com.prgrms.vouchermanager.domain.PercentAmountVoucher;
 import com.prgrms.vouchermanager.domain.Voucher;
+import com.prgrms.vouchermanager.io.FileIO;
+import lombok.Value;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -11,8 +13,11 @@ import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
+import javax.swing.*;
 import java.nio.ByteBuffer;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 import static com.prgrms.vouchermanager.message.QueryMessage.*;
@@ -26,6 +31,7 @@ public class VoucherJdbcRepository implements VoucherRepository {
 
     public VoucherJdbcRepository(DataSource dataSource) {
         jdbcTemplate = new JdbcTemplate(dataSource);
+        fileToDb();
     }
 
     public Voucher create(Voucher voucher) {
@@ -47,6 +53,18 @@ public class VoucherJdbcRepository implements VoucherRepository {
     public UUID delete(UUID id) {
         jdbcTemplate.update(DELETE_QUERY_VOUCHER.getMessage(), id.toString().getBytes());
         return id;
+    }
+
+    private void fileToDb() {
+        Map<UUID, Voucher> voucherMap = new HashMap<>();
+        String filePath = "src/main/resources/voucher_list.csv";
+        FileIO fileIO = new FileIO(filePath);
+        fileIO.fileToVoucherMap(voucherMap);
+        voucherMap
+                .values()
+                .stream()
+                .toList()
+                .forEach(this::create);
     }
 
     private RowMapper<Voucher> customerRowMapper() {
