a63f60e81e163f9986a4b6b83eb76fff5cdf9914
kimnamgyu <lethargic.hood@gmail.com>
2023-10-24 13:45:45 +0900

    Refactor : 사용하지 않는 getVoucherId() 메서드 제거

--- src/main/java/org/prgrms/vouchermanagement/voucher/DiscountPolicy.java
+++ src/main/java/org/prgrms/vouchermanagement/voucher/DiscountPolicy.java
@@ -1,11 +1,9 @@
 package org.prgrms.vouchermanagement.voucher;
 
-import java.util.UUID;
-
 public interface DiscountPolicy {
-    UUID getVoucherId();
-
     long getAmountOrPercent();
 
     PolicyStatus getPolicyStatus();
+
+    long discount(long price);
 }
