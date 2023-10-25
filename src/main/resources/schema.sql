b9b81307e56a815e2505f59e337397faad31bdb7
Hanna <hanna@trdst.com>
2023-10-26 08:29:21 +0900

    fix: schema.sql 수정

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -7,9 +7,9 @@ CREATE TABLE IF NOT EXISTS vouchers
 
 CREATE TABLE IF NOT EXISTS customers
 (
-    id          CHAR(36) PRIMARY KEY DEFAULT (UUID()),
-    name        VARCHAR(255) NOT NULL,
-    blacklisted BOOLEAN      NOT NULL
+    id          CHAR(36) PRIMARY KEY  DEFAULT (UUID()),
+    email       VARCHAR(255) NOT NULL UNIQUE,
+    blacklisted BOOLEAN      NOT NULL DEFAULT FALSE
 );
 
 CREATE TABLE IF NOT EXISTS wallets
@@ -17,6 +17,7 @@ CREATE TABLE IF NOT EXISTS wallets
     id          INT PRIMARY KEY AUTO_INCREMENT,
     customer_id CHAR(36) NOT NULL,
     voucher_id  CHAR(36) NOT NULL,
+    used        BOOLEAN  NOT NULL DEFAULT FALSE,
     FOREIGN KEY (customer_id) REFERENCES customers (id),
     FOREIGN KEY (voucher_id) REFERENCES vouchers (id)
 );
--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -7,20 +7,21 @@ CREATE TABLE vouchers
     id     CHAR(36) PRIMARY KEY DEFAULT (UUID()),
     type   VARCHAR(255) NOT NULL,
     amount BIGINT       NOT NULL
-    );
+);
 
 CREATE TABLE customers
 (
-    id          CHAR(36) PRIMARY KEY DEFAULT (UUID()),
-    name        VARCHAR(255) NOT NULL,
-    blacklisted BOOLEAN      NOT NULL
-    );
+    id          CHAR(36) PRIMARY KEY  DEFAULT (UUID()),
+    email       VARCHAR(255) NOT NULL UNIQUE,
+    blacklisted BOOLEAN      NOT NULL DEFAULT FALSE
+);
 
 CREATE TABLE wallets
 (
     id          INT PRIMARY KEY AUTO_INCREMENT,
     customer_id CHAR(36) NOT NULL,
     voucher_id  CHAR(36) NOT NULL,
+    used        BOOLEAN  NOT NULL DEFAULT FALSE,
     FOREIGN KEY (customer_id) REFERENCES customers (id),
     FOREIGN KEY (voucher_id) REFERENCES vouchers (id)
 );
