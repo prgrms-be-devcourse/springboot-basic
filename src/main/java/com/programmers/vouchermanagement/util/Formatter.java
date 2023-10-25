c68d1fdf5f8bf9ae73a8aa1fc8035991935d644a
Jay <injaesong0@gmail.com>
2023-10-25 20:51:17 +0900

    refactor: set format templates to be constants

--- src/main/java/com/programmers/vouchermanagement/util/Formatter.java
+++ src/main/java/com/programmers/vouchermanagement/util/Formatter.java
@@ -9,24 +9,29 @@ public class Formatter {
     private static final String EMPTY = "";
     private static final String NO_CONTENT = "There is no %s stored yet!";
 
+    private static final String CUSTOMER_PRESENTAITON_FORMAT = """
+            Customer ID : %s
+            Customer Name : %s
+            -------------------------""";
+    private static final String VOUCHER_PRESENTATION_FORMAT = """
+            Voucher ID : %s
+            Voucher Type : %s Discount Voucher
+            Discount Amount : %s
+            -------------------------""";
+
+    //messages
+
     public static String formatNoContent(String contentType) {
         return NO_CONTENT.formatted(contentType);
     }
 
     public static String formatCustomer(CustomerResponse customerResponse) {
-        return """
-                Customer ID : %s
-                Customer Name : %s
-                -------------------------"""
+        return CUSTOMER_PRESENTAITON_FORMAT
                 .formatted(customerResponse.customerId(), customerResponse.name());
     }
 
     public static String formatVoucher(VoucherResponse voucherResponse) {
-        return """
-                Voucher ID : %s
-                Voucher Type : %s Discount Voucher
-                Discount Amount : %s
-                -------------------------"""
+        return VOUCHER_PRESENTATION_FORMAT
                 .formatted(voucherResponse.getVoucherId(),
                         voucherResponse.getVoucherTypeName(),
                         voucherResponse.getDiscountValue() +
