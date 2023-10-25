6e2df13a6209c3156071bfc2bd8bdef466390901
KarmaPol <kkhkr98@gmail.com>
2023-10-25 20:57:33 +0900

    feat : Wallet 엔티티 추가

--- src/main/java/com/pgms/part1/domain/wallet/entity/Wallet.java
+++ src/main/java/com/pgms/part1/domain/wallet/entity/Wallet.java
@@ -1,4 +1,14 @@
 package com.pgms.part1.domain.wallet.entity;
 
 public class Wallet {
+
+    private final Long voucherId;
+
+    private final Long customerId;
+
+    public Wallet(Long voucherId, Long customerId) {
+        this.voucherId = voucherId;
+        this.customerId = customerId;
+    }
+
 }
