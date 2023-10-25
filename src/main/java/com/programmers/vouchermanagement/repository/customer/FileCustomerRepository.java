c5025020683822c16bbcd5777f0084d303701d5b
marooo326 <ktnqkr01@gmail.com>
2023-10-25 22:23:36 +0900

    feat: CustomerRepository 기능 구현

--- src/main/java/com/programmers/vouchermanagement/repository/customer/FileCustomerRepository.java
+++ src/main/java/com/programmers/vouchermanagement/repository/customer/FileCustomerRepository.java
@@ -2,47 +2,93 @@ package com.programmers.vouchermanagement.repository.customer;
 
 import com.programmers.vouchermanagement.common.ErrorMessage;
 import com.programmers.vouchermanagement.domain.customer.Customer;
+import jakarta.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Repository;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.text.MessageFormat;
+import java.io.*;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
 
-/**
- * @deprecated JdbcCustomerRepository 로 대체
- */
 @Repository
-@Profile("dev")
-@Deprecated
+@Profile("local")
 public class FileCustomerRepository implements CustomerRepository {
     private static final String CSV_SEPARATOR = ",";
 
-    private final String blacklistFilePath;
+    private final String customerFilePath;
+    private final ConcurrentHashMap<UUID, Customer> customers = new ConcurrentHashMap<>();
     private final Logger logger = LoggerFactory.getLogger(FileCustomerRepository.class);
 
-    public FileCustomerRepository(@Value("${csv.file.customer.blacklist_path}") String blacklistFilePath) {
-        this.blacklistFilePath = blacklistFilePath;
+    public FileCustomerRepository(@Value("${csv.file.customer.path}") String customerFilePath) {
+        this.customerFilePath = customerFilePath;
+    }
+
+    @PostConstruct
+    public void init() {
+        readCustomerFile();
     }
 
     @Override
-    public List<Customer> findAllBannedCustomers() {
-        return readBlackListFile();
+    public List<Customer> findAll() {
+        return customers.values().stream().toList();
     }
 
-    private List<Customer> readBlackListFile() {
+    @Override
+    public Optional<Customer> findById(UUID customerId) {
+        return Optional.ofNullable(customers.get(customerId));
+    }
+
+    @Override
+    public List<Customer> findByName(String name) {
+        return customers.values().stream()
+                .filter(customer -> customer.getName().equals(name))
+                .toList();
+    }
+
+    @Override
+    public List<Customer> findBannedCustomers() {
+        return customers.values().stream()
+                .filter(Customer::isBanned)
+                .toList();
+    }
+
+    @Override
+    public void save(Customer customer) {
+        if (isCustomerPresent(customer.getId())) {
+            throw new IllegalArgumentException("Customer already exists");
+        }
+        customers.put(customer.getId(), customer);
+        updateFile();
+    }
+
+    @Override
+    public void update(Customer customer) {
+        if (!isCustomerPresent(customer.getId())) {
+            throw new NoSuchElementException();
+        }
+        customers.put(customer.getId(), customer);
+        updateFile();
+    }
+
+    @Override
+    public void delete(UUID id) {
+        if (!isCustomerPresent(id)) {
+            throw new NoSuchElementException();
+        }
+        customers.remove(id);
+        updateFile();
+    }
+
+    private void readCustomerFile() {
         String line;
-        List<Customer> bannedCustomers = new ArrayList<>();
-        try (BufferedReader br = new BufferedReader(new FileReader(blacklistFilePath))) {
+        try (BufferedReader br = new BufferedReader(new FileReader(customerFilePath))) {
             while ((line = br.readLine()) != null) {
                 String[] strings = line.split(CSV_SEPARATOR);
                 Customer customer = new Customer(
@@ -51,13 +97,33 @@ public class FileCustomerRepository implements CustomerRepository {
                         LocalDateTime.parse(strings[2]),
                         Boolean.parseBoolean(strings[3])
                 );
-                bannedCustomers.add(customer);
+                customers.put(customer.getId(), customer);
             }
         } catch (FileNotFoundException e) {
-            logger.warn(MessageFormat.format("{0} : {1}", ErrorMessage.FILE_NOT_FOUND_MESSAGE.getMessage(), blacklistFilePath));
+            logger.warn("{} : {}", ErrorMessage.FILE_NOT_FOUND_MESSAGE.getMessage(), customerFilePath);
         } catch (IOException e) {
             logger.error("Error occurred at FileReader: ", e);
         }
-        return bannedCustomers;
+    }
+
+    private void updateFile() {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(customerFilePath))) {
+            customers.values().stream()
+                    .map(customer -> customer.joinInfo(CSV_SEPARATOR))
+                    .forEach(line -> {
+                        try {
+                            bw.write(line);
+                            bw.newLine();
+                        } catch (IOException e) {
+                            logger.error("Error occurred at FileWriter: ", e);
+                        }
+                    });
+        } catch (IOException e) {
+            logger.error("Error occurred af FileWriter: ", e);
+        }
+    }
+
+    private boolean isCustomerPresent(UUID customerId) {
+        return customers.containsKey(customerId);
     }
 }
--- src/main/java/com/programmers/vouchermanagement/repository/customer/JdbcCustomerRepository.java
+++ src/main/java/com/programmers/vouchermanagement/repository/customer/JdbcCustomerRepository.java
@@ -1,16 +1,20 @@
 package com.programmers.vouchermanagement.repository.customer;
 
 import com.programmers.vouchermanagement.domain.customer.Customer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Profile;
 import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.nio.ByteBuffer;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDateTime;
-import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -18,31 +22,84 @@ import java.util.UUID;
 @Repository
 @Profile("dev")
 public class JdbcCustomerRepository implements CustomerRepository {
-    private final JdbcTemplate jdbcTemplate;
+    private final NamedParameterJdbcTemplate jdbcTemplate;
 
-    public JdbcCustomerRepository(JdbcTemplate jdbcTemplate) {
+    private final Logger logger = LoggerFactory.getLogger(JdbcCustomerRepository.class);
+
+    public JdbcCustomerRepository(NamedParameterJdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
     }
 
+    @Override
     public List<Customer> findAll() {
-        return jdbcTemplate.query("select * from customers", (resultSet, i) -> mapToCustomer(resultSet));
+        String sql = "select * from customers";
+        return jdbcTemplate.query(sql, (resultSet, i) -> mapToCustomer(resultSet));
     }
 
-    public Optional<Customer> findById(UUID customerId) {
-        try {
-            return Optional.ofNullable(jdbcTemplate.queryForObject("select * from customers where customer_id = UUID_TO_BIN(?)",
-                    (resultSet, i) -> mapToCustomer(resultSet),
-                    customerId));
+    @Override
+    public List<Customer> findBannedCustomers() {
+        String sql = "select * from customers where is_banned = TRUE";
+        return jdbcTemplate.query(sql, (resultSet, i) -> mapToCustomer(resultSet));
+    }
 
+    @Override
+    public Optional<Customer> findById(UUID id) {
+        String sql = "select * from customers where id = UUID_TO_BIN(:customerId)";
+        SqlParameterSource namedParameters = new MapSqlParameterSource("id", id.toString());
+        try {
+            return Optional.ofNullable(jdbcTemplate.queryForObject(
+                    sql,
+                    namedParameters,
+                    (resultSet, i) -> mapToCustomer(resultSet)));
         } catch (EmptyResultDataAccessException e) {
             return Optional.empty();
         }
     }
 
+    @Override
     public List<Customer> findByName(String name) {
-        return jdbcTemplate.query("select * from customers where name like ? ",
-                (resultSet, i) -> mapToCustomer(resultSet),
-                "%" + name + "%");
+        String sql = "select * from customers where name like :name";
+        SqlParameterSource namedParameters = new MapSqlParameterSource("name", name);
+        return jdbcTemplate.query(
+                sql,
+                namedParameters,
+                (resultSet, i) -> mapToCustomer(resultSet));
+    }
+
+    @Override
+    @Transactional
+    public void save(Customer customer) {
+        String sql = "INSERT INTO customers VALUES :id, :name, :createdAt, :isBanned";
+        SqlParameterSource namedParameters = new MapSqlParameterSource()
+                .addValue("id", customer.getId())
+                .addValue("name", customer.getName())
+                .addValue("createdAt", customer.getCreatedAt())
+                .addValue("isBanned", customer.isBanned());
+        int affectedRow = jdbcTemplate.update(sql, namedParameters);
+        logger.debug("Affected Row on save: {}", affectedRow);
+    }
+
+    @Override
+    @Transactional
+    public void update(Customer customer) {
+        String sql = "UPDATE customers SET :id, :name, :createdAt, :isBanned WHERE id= :id";
+        SqlParameterSource namedParameters = new MapSqlParameterSource()
+                .addValue("id", customer.getId())
+                .addValue("name", customer.getName())
+                .addValue("createdAt", customer.getCreatedAt())
+                .addValue("isBanned", customer.isBanned());
+        int affectedRow = jdbcTemplate.update(sql, namedParameters);
+        logger.debug("Affected Row on update: {}", affectedRow);
+    }
+
+    @Override
+    @Transactional
+    public void delete(UUID id) {
+        String sql = "DELETE FROM customers WHERE id = :id";
+        SqlParameterSource namedParameters = new MapSqlParameterSource()
+                .addValue("id", id);
+        int affectedRow = jdbcTemplate.update(sql, namedParameters);
+        logger.debug("Affected Row on delete: {}", affectedRow);
     }
 
     private Customer mapToCustomer(ResultSet resultSet) throws SQLException {
@@ -50,17 +107,11 @@ public class JdbcCustomerRepository implements CustomerRepository {
         String customerName = resultSet.getString("name");
         final LocalDateTime createdAt = resultSet.getTimestamp("created_at").toLocalDateTime();
         final boolean isBanned = resultSet.getBoolean("is_banned");
-
         return new Customer(customerId, customerName, createdAt, isBanned);
     }
 
     private UUID toUUID(byte[] bytes) {
         final ByteBuffer byteBuffer = ByteBuffer.wrap(bytes);
         return new UUID(byteBuffer.getLong(), byteBuffer.getLong());
-    }
-
-    @Override
-    public List<Customer> findAllBannedCustomers() {
-        return Collections.emptyList();
     }
 }
