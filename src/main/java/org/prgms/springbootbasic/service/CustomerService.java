57775e7ab7ce8c235ce0833620c2035a51d6e953
ZZAMBAs <rksidksrksi@naver.com>
2023-10-25 22:56:28 +0900

    Feat: Service 단을 추가한다.

---
+++ src/main/java/org/prgms/springbootbasic/service/CustomerService.java
@@ -0,0 +1,22 @@
+package org.prgms.springbootbasic.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.prgms.springbootbasic.domain.customer.Customer;
+import org.prgms.springbootbasic.repository.CustomerRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Slf4j
+public class CustomerService {
+    private final CustomerRepository customerRepository;
+
+    public CustomerService(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    public List<Customer> findBlackAll(){
+        return customerRepository.findBlackAll();
+    }
+}
