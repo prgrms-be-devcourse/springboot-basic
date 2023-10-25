0d730aecb4bb7cf834e084b8aa5a772079973b18
ZZAMBAs <rksidksrksi@naver.com>
2023-10-25 23:01:49 +0900

    Feat: 테스트, 로컬 구동용 CustomerMemoryRepository를 추가한다.

---
+++ src/main/java/org/prgms/springbootbasic/repository/CustomerMemoryRepository.java
@@ -0,0 +1,17 @@
+package org.prgms.springbootbasic.repository;
+
+import org.prgms.springbootbasic.domain.customer.Customer;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+@Profile({"local", "test"})
+public class CustomerMemoryRepository implements CustomerRepository {
+    @Override
+    public List<Customer> findBlackAll() {
+        return new ArrayList<>();
+    }
+}
