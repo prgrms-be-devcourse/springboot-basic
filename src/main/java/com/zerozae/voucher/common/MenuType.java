d4fd742bd0e3f2441a6e0c827c085065597d6c55
park0jae <okvv26@naver.com>
2023-10-25 23:10:07 +0900

    feat : MenuType 값 추가

--- src/main/java/com/zerozae/voucher/common/MenuType.java
+++ src/main/java/com/zerozae/voucher/common/MenuType.java
@@ -13,15 +13,16 @@ public enum MenuType {
     BLACKLIST,
     VOUCHER,
     CUSTOMER,
+    SEARCH,
+    DELETE,
+    DELETE_ALL,
+    UPDATE,
     BACK;
-
-    private static final Logger logger = LoggerFactory.getLogger(MenuType.class);
 
     public static MenuType of(String input){
         try {
             return MenuType.valueOf(input.toUpperCase());
         }catch (RuntimeException e){
-            logger.warn("Error Message = {}", e.getMessage());
             throw ErrorMessage.error("존재하지 않는 메뉴입니다. 다시 입력해주세요.");
         }
     }
