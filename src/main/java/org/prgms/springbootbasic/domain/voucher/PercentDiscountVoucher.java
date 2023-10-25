b3ffcfbfcd32170323a7b1599e54196d78a44549
ZZAMBAs <rksidksrksi@naver.com>
2023-10-26 00:06:00 +0900

    Feat: 범위를 벗어남을 표현하는 예외를 정의하고 사용한다.

--- src/main/java/org/prgms/springbootbasic/domain/voucher/PercentDiscountVoucher.java
+++ src/main/java/org/prgms/springbootbasic/domain/voucher/PercentDiscountVoucher.java
@@ -1,6 +1,7 @@
 package org.prgms.springbootbasic.domain.voucher;
 
 import lombok.extern.slf4j.Slf4j;
+import org.prgms.springbootbasic.exception.OutOfRangeException;
 
 import java.util.UUID;
 
@@ -10,14 +11,10 @@ public class PercentDiscountVoucher implements VoucherPolicy {
     private final long percent;
 
     public PercentDiscountVoucher(UUID voucherId, long percent) {
-        if (percent > 100 || percent < 0) {
+        if (percent > 100 || percent <= 0) {
             log.warn("percent value is out of range.");
-            throw new IllegalArgumentException("percent value is out of range.");
+            throw new OutOfRangeException("percent value is out of range.");
         }
-        if (percent == 0){
-            log.warn("percent value cannot be 0.");
-            throw new IllegalArgumentException("percent value is out of range.");
-        } // 커스텀 예외 사용 고려, 0일때가 중요한지
 
         this.voucherId = voucherId;
         this.percent = percent;
@@ -36,7 +33,7 @@ public class PercentDiscountVoucher implements VoucherPolicy {
     @Override
     public long discount(long beforeDiscount) { // 음수 고려
         if (beforeDiscount < 0)
-            throw new IllegalArgumentException("beforeDiscount is less than 0.");
+            throw new OutOfRangeException("beforeDiscount is less than 0.");
         return beforeDiscount * percent / 100L;
     }
 }
