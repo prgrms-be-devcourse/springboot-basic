98166a360ae0e3ba2f2ccba36992fa422fe8d788
shoeone96 <dlwnddnjs96@gmail.com>
2023-10-25 16:56:39 +0900

    feat: 회원을 저장하는 jdbc 로직 추가

--- src/main/java/org/programmers/springorder/customer/repository/JdbcCustomerRepository.java
+++ src/main/java/org/programmers/springorder/customer/repository/JdbcCustomerRepository.java
@@ -7,6 +7,7 @@ import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.nio.ByteBuffer;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -19,14 +20,14 @@ public class JdbcCustomerRepository implements CustomerRepository {
     private final NamedParameterJdbcTemplate jdbcTemplate;
 
     private final String SELECT_ALL_CUSTOMER = "SELECT * FROM customers";
-//    private final String INSERT_CUSTOMER = "INSERT INTO customers(customer_id, customer_name, customer_type) VALUES(:customerId, :customerName, :customerType)";
+    private final String INSERT_CUSTOMER = "INSERT INTO customers(customer_id, customer_name, customer_type) VALUES(UUID_TO_BIN(:customerId), :customerName, :customerType)";
 
     public JdbcCustomerRepository(NamedParameterJdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
     }
 
     private final RowMapper<Customer> customerRowMapper = (resultSet, rowNum) -> {
-        UUID customerId = UUID.fromString(resultSet.getString("customer_id"));
+        UUID customerId = toUUID(resultSet.getBytes("customer_id"));
         String customerName = resultSet.getString("customer_name");
         CustomerType customerType = CustomerType.valueOf(resultSet.getString("customer_type"));
         return Customer.toCustomer(customerId, customerName, customerType);
@@ -42,11 +43,24 @@ public class JdbcCustomerRepository implements CustomerRepository {
         return jdbcTemplate.query(SELECT_ALL_CUSTOMER, customerRowMapper);
     }
 
+    public Customer insert(Customer customer) {
+        int update = jdbcTemplate.update(INSERT_CUSTOMER, toParamMap(customer));
+        if( update != 1){
+            throw new RuntimeException("Nothing was inserted");
+        }
+        return customer;
+    }
+
     private Map<String, Object> toParamMap(Customer customer) {
         return new HashMap<>() {{
             put("customerId", customer.getCustomerId().toString().getBytes());
             put("customerName", customer.getName());
             put("customerType", customer.getCustomerType().name());
         }};
+    }
+
+    static UUID toUUID(byte[] bytes) {
+        var byteBuffer = ByteBuffer.wrap(bytes);
+        return new UUID(byteBuffer.getLong(), byteBuffer.getLong());
     }
 }
