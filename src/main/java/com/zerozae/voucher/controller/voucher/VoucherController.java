cca6a5484bd39326a02fe7e1a57310be11007edc
park0jae <okvv26@naver.com>
2023-10-25 22:59:32 +0900

    feat : VoucherController CRUD 관련 메서드 추가 구현

--- src/main/java/com/zerozae/voucher/controller/voucher/VoucherController.java
+++ src/main/java/com/zerozae/voucher/controller/voucher/VoucherController.java
@@ -3,11 +3,13 @@ package com.zerozae.voucher.controller.voucher;
 import com.zerozae.voucher.common.response.Response;
 import com.zerozae.voucher.dto.voucher.VoucherRequest;
 import com.zerozae.voucher.dto.voucher.VoucherResponse;
+import com.zerozae.voucher.dto.voucher.VoucherUpdateRequest;
 import com.zerozae.voucher.exception.ErrorMessage;
 import com.zerozae.voucher.service.voucher.VoucherService;
 import org.springframework.stereotype.Controller;
 
 import java.util.List;
+import java.util.UUID;
 
 
 @Controller
@@ -24,12 +26,48 @@ public class VoucherController {
             voucherService.createVoucher(voucherRequest);
             return Response.success();
         }catch (ErrorMessage e){
-            return Response.failure(e.getMessage());
+            throw ErrorMessage.error(e.getMessage());
         }
     }
 
     public Response findAllVouchers(){
         List<VoucherResponse> vouchers = voucherService.findAllVouchers();
         return Response.success(vouchers);
+    }
+
+    public Response findVoucherById(UUID voucherId) {
+        try{
+            VoucherResponse findVoucher = voucherService.findById(voucherId);
+            return Response.success(findVoucher);
+        }catch (Exception e) {
+            throw ErrorMessage.error(e.getMessage());
+        }
+    }
+
+    public Response deleteById(UUID voucherId) {
+        try {
+            voucherService.deleteById(voucherId);
+            return Response.success();
+        }catch (Exception e) {
+            throw ErrorMessage.error(e.getMessage());
+        }
+    }
+
+    public Response deleteAll() {
+        try {
+            voucherService.deleteAll();
+            return Response.success();
+        }catch (Exception e) {
+            throw ErrorMessage.error(e.getMessage());
+        }
+    }
+
+    public Response updateVoucher(UUID voucherId, VoucherUpdateRequest voucherUpdateRequest) {
+        try {
+            voucherService.update(voucherId, voucherUpdateRequest);
+            return Response.success();
+        }catch (Exception e) {
+            throw ErrorMessage.error(e.getMessage());
+        }
     }
 }
