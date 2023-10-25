6d805468281807e9037fbb03acd1f538a53dda9f
EunChanNam <namhm23@gmail.com>
2023-10-25 10:54:04 +0900

    feat: Wallet 엔티티 구현

---
+++ src/main/java/study/dev/spring/wallet/domain/Wallet.java
@@ -0,0 +1,42 @@
+package study.dev.spring.wallet.domain;
+
+import java.util.UUID;
+
+public class Wallet {
+
+	private final String uuid;
+
+	private final String customerId;
+
+	private final String voucherId;
+
+	private Wallet(
+		String customerId,
+		String voucherId
+	) {
+		this.uuid = UUID.randomUUID().toString();
+		this.customerId = customerId;
+		this.voucherId = voucherId;
+	}
+
+	//==Factory method==//
+	public static Wallet of(
+		String customerId,
+		String voucherId
+	) {
+		return new Wallet(customerId, voucherId);
+	}
+
+	//==Utility method==//
+	public String getUuid() {
+		return uuid;
+	}
+
+	public String getCustomerId() {
+		return customerId;
+	}
+
+	public String getVoucherId() {
+		return voucherId;
+	}
+}
