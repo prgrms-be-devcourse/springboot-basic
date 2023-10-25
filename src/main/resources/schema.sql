4cb0b579c42bd9775d5229f251df7aa82d3fb7f8
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2023-10-26 02:19:31 +0900

    chore: add Database schema

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,21 @@
+CREATE TABLE customers
+(
+    id   BINARY(16) PRIMARY KEY,
+    name varchar(20) NOT NULL,
+    CONSTRAINT name UNIQUE (name)
+);
+
+CREATE TABLE vouchers
+(
+    id             BINARY(16) PRIMARY KEY,
+    type           enum ('FIXED', 'PERCENT') NOT NULL,
+    discount_value varchar(50)               NOT NULL,
+    created_at     datetime(6)               NOT NULL DEFAULT CURRENT_TIMESTAMP(6)
+);
+
+CREATE TABLE voucher_own_info
+(
+    id          INT PRIMARY KEY AUTO_INCREMENT,
+    customer_id BINARY(16) NOT NULL REFERENCES customers (id) ON DELETE CASCADE,
+    voucher_id  BINARY(16) NOT NULL REFERENCES vouchers (id) ON DELETE CASCADE
+);
