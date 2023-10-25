3242ccb841f20e5e2ca205b9d7556f4448d7d00e
hs12 <loveanna1228@gmail.com>
2023-10-25 10:48:36 +0900

    [Feat] Voucher 저장소 테스트 추가

---
+++ src/test/java/com/prgrms/voucher_manage/domain/voucher/repository/MemoryVoucherRepositoryTest.java
@@ -0,0 +1,44 @@
+package com.prgrms.voucher_manage.domain.voucher.repository;
+
+import com.prgrms.voucher_manage.domain.voucher.entity.FixedAmountVoucher;
+import com.prgrms.voucher_manage.domain.voucher.entity.Voucher;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static com.prgrms.voucher_manage.domain.voucher.entity.VoucherType.FIXED;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MemoryVoucherRepositoryTest {
+    private final MemoryVoucherRepository repository = new MemoryVoucherRepository();
+
+    @Test
+    @DisplayName("Voucher를 저장할 수 있다.")
+    public void memoryVoucherRepository_save(){
+        //given
+        Voucher fixedAmountVoucher = new FixedAmountVoucher(1000L);
+        //when
+        Voucher savedVoucher = repository.save(fixedAmountVoucher);
+        //then
+        assertThat(savedVoucher).isNotNull();
+        assertThat(savedVoucher.getVoucherType()).isEqualTo(FIXED);
+        assertThat(savedVoucher.getDiscountAmount()).isEqualTo(1000L);
+    }
+
+    @Test
+    @DisplayName("Voucher 리스트를 반환받을 수 있다.")
+    public void memoryVoucherRepository_findAll(){
+        //given
+        Voucher fixedAmountVoucher1 = new FixedAmountVoucher(1000L);
+        Voucher fixedAmountVoucher2 = new FixedAmountVoucher(1000L);
+
+        repository.save(fixedAmountVoucher1);
+        repository.save(fixedAmountVoucher2);
+
+        //when
+        List<Voucher> voucherList = repository.findAll();
+        //then
+        assertThat(voucherList.size()).isEqualTo(2);
+    }
+}
