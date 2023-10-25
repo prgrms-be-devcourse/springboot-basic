eb7e04b3a609b1c2118c9b967e0fc41680a219b6
hyoguoo <hyoguoo@gmail.com>
2023-10-26 01:37:22 +0900

    refactor: JdbcCustomerRepository 및 JdbcVoucherRepository 클래스 리팩터링

--- src/main/java/devcourse/springbootbasic/repository/customer/JdbcCustomerRepository.java
+++ src/main/java/devcourse/springbootbasic/repository/customer/JdbcCustomerRepository.java
@@ -7,84 +7,88 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-import java.util.UUID;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.*;
 
-// TODO: 리팩터링 필요
 @Profile("prod")
 @Repository
 @RequiredArgsConstructor
 public class JdbcCustomerRepository implements CustomerRepository {
 
+    private static final String ID = "id";
+    private static final String NAME = "name";
+    private static final String IS_BLACKLISTED = "is_blacklisted";
+
     private final NamedParameterJdbcTemplate jdbcTemplate;
 
     @Override
     public List<Customer> findAllBlacklistedCustomers() {
-        String sql = """
+        String query = """
                         SELECT *
                         FROM customer
                         WHERE is_blacklisted = TRUE
                 """;
 
-        return jdbcTemplate.query(sql, (rs, rowNum) -> {
-            byte[] uuidBytes = rs.getBytes("id");
-            UUID id = UUIDUtil.byteToUUID(uuidBytes);
-            String name = rs.getString("name");
-            boolean isBlacklisted = rs.getBoolean("is_blacklisted");
-
-            return Customer.createCustomer(id, name, isBlacklisted);
-        });
+        return jdbcTemplate.query(
+                query,
+                (rs, rowNum) -> mapCustomerFromResultSet(rs));
     }
 
     @Override
     public Customer save(Customer customer) {
-        String sql = """
+        String query = """
                         INSERT INTO customer (id, name, is_blacklisted)
                         VALUES (UUID_TO_BIN(:id), :name, :is_blacklisted)
                 """;
 
-        jdbcTemplate.update(sql, Map.of(
-                "id", customer.getId().toString(),
-                "name", customer.getName(),
-                "is_blacklisted", customer.isBlacklisted()
-        ));
-
+        jdbcTemplate.update(query, mapCustomerParameters(customer));
         return customer;
     }
 
     @Override
     public Optional<Customer> findById(UUID customerId) {
-        String sql = """
+        String query = """
                         SELECT *
                         FROM customer
                         WHERE id = UUID_TO_BIN(:id)
                         FOR UPDATE
                 """;
 
-        return jdbcTemplate.query(sql, Map.of("id", customerId.toString()), (rs, rowNum) -> {
-            byte[] uuidBytes = rs.getBytes("id");
-            UUID id = UUIDUtil.byteToUUID(uuidBytes);
-            String name = rs.getString("name");
-            boolean isBlacklisted = rs.getBoolean("is_blacklisted");
+        List<Customer> customers = jdbcTemplate.query(
+                query,
+                Map.of(ID, customerId.toString()),
+                (rs, rowNum) -> mapCustomerFromResultSet(rs));
 
-            return Customer.createCustomer(id, name, isBlacklisted);
-        }).stream().findFirst();
+        return customers.stream().findFirst();
     }
 
     @Override
     public int update(Customer customer) {
-        String sql = """
+        String query = """
                         UPDATE customer
                         SET name = :name, is_blacklisted = :is_blacklisted
                         WHERE id = UUID_TO_BIN(:id)
                 """;
 
-        return jdbcTemplate.update(sql, Map.of(
-                "id", customer.getId().toString(),
-                "name", customer.getName(),
-                "is_blacklisted", customer.isBlacklisted()
-        ));
+        return jdbcTemplate.update(query, mapCustomerParameters(customer));
+    }
+
+    private Map<String, Object> mapCustomerParameters(Customer customer) {
+        Map<String, Object> params = new HashMap<>();
+
+        params.put(ID, customer.getId().toString());
+        params.put(NAME, customer.getName());
+        params.put(IS_BLACKLISTED, customer.isBlacklisted());
+
+        return params;
+    }
+
+    private Customer mapCustomerFromResultSet(ResultSet rs) throws SQLException {
+        UUID id = UUIDUtil.byteToUUID(rs.getBytes(ID));
+        String name = rs.getString(NAME);
+        boolean isBlacklisted = rs.getBoolean(IS_BLACKLISTED);
+
+        return Customer.createCustomer(id, name, isBlacklisted);
     }
 }
--- src/main/java/devcourse/springbootbasic/repository/voucher/JdbcVoucherRepository.java
+++ src/main/java/devcourse/springbootbasic/repository/voucher/JdbcVoucherRepository.java
@@ -8,6 +8,9 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
@@ -17,38 +20,48 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class JdbcVoucherRepository implements VoucherRepository {
 
+    private static final String ID = "id";
+    private static final String VOUCHER_TYPE = "voucher_type";
+    private static final String DISCOUNT_VALUE = "discount_value";
+
     private final NamedParameterJdbcTemplate jdbcTemplate;
 
     @Override
-    public Voucher save(Voucher voucher) {
-        String sql = """
-                        INSERT INTO voucher (id, voucher_type, discount_value)
-                        VALUES (UUID_TO_BIN(:id), :voucher_type, :discount_value)
-                """;
-
-        jdbcTemplate.update(sql, Map.of(
-                "id", voucher.getId().toString(),
-                "voucher_type", voucher.getVoucherType().toString(),
-                "discount_value", voucher.getDiscountValue()
-        ));
-
-        return voucher;
-    }
-
-    @Override
     public List<Voucher> findAll() {
-        String sql = """
+        String query = """
                         SELECT *
                         FROM voucher
                 """;
 
-        return jdbcTemplate.query(sql, (rs, rowNum) -> {
-            byte[] uuidBytes = rs.getBytes("id");
-            UUID id = UUIDUtil.byteToUUID(uuidBytes);
-            VoucherType voucherType = VoucherType.valueOf(rs.getString("voucher_type"));
-            long discountValue = rs.getLong("discount_value");
+        return jdbcTemplate.query(query, (rs, rowNum) -> mapVoucherFromResultSet(rs));
+    }
 
-            return Voucher.createVoucher(id, voucherType, discountValue);
-        });
+    @Override
+    public Voucher save(Voucher voucher) {
+        String query = """
+                        INSERT INTO voucher (id, voucher_type, discount_value)
+                        VALUES (UUID_TO_BIN(:id), :voucher_type, :discount_value)
+                """;
+
+        jdbcTemplate.update(query, mapVoucherParameters(voucher));
+        return voucher;
+    }
+
+    private Map<String, Object> mapVoucherParameters(Voucher voucher) {
+        Map<String, Object> params = new HashMap<>();
+
+        params.put(ID, voucher.getId().toString());
+        params.put(VOUCHER_TYPE, voucher.getVoucherType().toString());
+        params.put(DISCOUNT_VALUE, voucher.getDiscountValue());
+
+        return params;
+    }
+
+    private Voucher mapVoucherFromResultSet(ResultSet rs) throws SQLException {
+        UUID id = UUIDUtil.byteToUUID(rs.getBytes(ID));
+        VoucherType voucherType = VoucherType.valueOf(rs.getString(VOUCHER_TYPE));
+        long discountValue = rs.getLong(DISCOUNT_VALUE);
+
+        return Voucher.createVoucher(id, voucherType, discountValue);
     }
 }
