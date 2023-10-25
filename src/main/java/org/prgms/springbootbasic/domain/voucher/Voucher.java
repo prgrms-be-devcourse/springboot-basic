8565bd3832d79061dc6caf616373da3a44313bbe
ZZAMBAs <rksidksrksi@naver.com>
2023-10-25 22:58:41 +0900

    Refactor: 명확한 이름으로 변경한다.

--- src/main/java/org/prgms/springbootbasic/domain/voucher/Voucher.java
+++
@@ -1,9 +0,0 @@
-package org.prgms.springbootbasic.domain.voucher;
-
-import java.util.UUID;
-
-public interface Voucher {
-    UUID getVoucherId();
-    long discount(long beforeDiscount);
-    long getDiscountAmount();
-}
