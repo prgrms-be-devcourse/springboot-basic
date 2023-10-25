290dc3d04dfe4100afae1da1803497d29f92f7c9
park0jae <okvv26@naver.com>
2023-10-25 22:53:07 +0900

    refactor : InputValidator 정규표현식 수정

--- src/main/java/com/zerozae/voucher/validator/InputValidator.java
+++ src/main/java/com/zerozae/voucher/validator/InputValidator.java
@@ -5,25 +5,22 @@ import com.zerozae.voucher.exception.ErrorMessage;
 
 public class InputValidator {
 
-    private static final String KOREAN_PATTERN = "^[ㄱ-ㅎ가-힣]*$";
-    private static final String ALPHABET_PATTERN =  "^[a-zA-Z]*$";
+    private static final String KOREAN_ALPHABET_PATTERN = "^[a-z|A-Z|ㄱ-ㅎ|ㅏ-ㅣ|가-힣|_]*$";
     private static final String NUMBER_PATTERN = "-?\\d+";
 
     public static Long validateInputDiscount(String input) {
         if (input != null && input.matches(NUMBER_PATTERN)) {
             return Long.valueOf(input);
         } else {
-            String message = "입력값은 숫자여야 합니다.";
-            throw ErrorMessage.error(message);
+            throw ErrorMessage.error("입력값은 숫자여야 합니다.");
         }
     }
 
     public static String validateInputString(String input) {
-        if (input != null && (input.matches(ALPHABET_PATTERN) || input.matches(KOREAN_PATTERN))) {
+        if (input != null && input.matches(KOREAN_ALPHABET_PATTERN)) {
             return input;
         } else {
-            String message = "입력값은 문자열이어야 합니다.";
-            throw ErrorMessage.error(message);
+            throw ErrorMessage.error("입력값은 문자열이어야 합니다.");
         }
     }
 }
