36c18d24f3558fea0523449782200daf0e8453bf
YongNyeo <won05121@gmail.com>
2023-10-26 21:30:33 +0900

    refactor: 입출력 메서드 추가

--- src/main/java/com/prgms/vouchermanager/util/io/ConsoleInput.java
+++ src/main/java/com/prgms/vouchermanager/util/io/ConsoleInput.java
@@ -8,6 +8,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.util.Scanner;
+import java.util.UUID;
 
 import static com.prgms.vouchermanager.exception.ExceptionType.*;
 import static com.prgms.vouchermanager.exception.ExceptionType.INVALID_VOUCHER_INFO;
@@ -74,6 +75,38 @@ public class ConsoleInput {
             return scanner.nextLong();
         } catch (RuntimeException e) {
             throw new RuntimeException(INVALID_VOUCHER_INFO.getMessage());
+        }
+    }
+
+    public UUID inputVoucherId() {
+        try {
+            return UUID.fromString(scanner.next());
+        }catch (RuntimeException e) {
+            throw new RuntimeException(INVALID_VOUCHER_INFO.getMessage());
+        }
+    }
+
+    public String inputCustomerName() {
+        return scanner.next();
+    }
+
+    public String inputCustomerEmail() {
+        return scanner.next();
+    }
+
+    public int inputBlackList() {
+        try {
+            return  scanner.nextInt();
+        } catch (RuntimeException e) {
+            throw new RuntimeException(INVALID_CUSTOMER_INFO.getMessage());
+        }
+    }
+
+    public Long inputCustomerId() {
+        try {
+            return scanner.nextLong();
+        } catch (RuntimeException e) {
+            throw new RuntimeException(INVALID_CUSTOMER_INFO.getMessage());
         }
     }
 }
--- src/main/java/com/prgms/vouchermanager/util/io/ConsoleOutput.java
+++ src/main/java/com/prgms/vouchermanager/util/io/ConsoleOutput.java
@@ -1,5 +1,7 @@
 package com.prgms.vouchermanager.util.io;
 
+import com.prgms.vouchermanager.domain.customer.Customer;
+import com.prgms.vouchermanager.domain.wallet.Wallet;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -17,7 +19,9 @@ public class ConsoleOutput {
                 
                 2. Customer
                 
-                3. EXIT
+                3. Wallet
+                
+                4. EXIT
                 """);
     }
 
@@ -29,7 +33,15 @@ public class ConsoleOutput {
                 
                 1. create
                 
-                2. list
+                2. update
+                
+                3. list
+                
+                4. find one
+                
+                5. delete one
+                
+                6. delete all
                 
                 """);
     }
@@ -52,13 +64,74 @@ public class ConsoleOutput {
                 
                 이용하실 고객 메뉴를 입력해주세요
                 
-                1. black list
+                1. CREATE
+                
+                2. UPDATE
+                
+                3. FIND ALL
+                
+                4. FIND ONE
+                
+                5. DELETE ONE
+                
+                6. DELETE ALL
+                
+                7. BLACK LIST
                 
                 """);
     }
 
-
+    public void printWalletMenu() {
+        System.out.println("""
+                이용하실 지갑 메뉴를 입력해주세요
+                
+                1. CREATE
+                
+                2. FIND BY CUSTOMER ID
+                
+                3. FIND BY VOUCHER ID
+                
+                4. DELETE BY CUSTOMER ID
+                
+                """);
+    }
     public void printVoucherAmount() {
         System.out.println("쿠폰의 할인값을 입력해주세요");
+    }
+
+    public void printVoucherId() {
+        System.out.println("쿠폰 번호를 입력해주세요.");
+    }
+
+    public void printSuccessDelete() {
+        System.out.println("성공적으로 삭제되었습니다.");
+    }
+
+    public void printCustomerName() {
+        System.out.println("고객명을 입력해주세요.{");
+    }
+
+    public void printCustomerEmail() {
+        System.out.println("이메일을 입력해주세요.");
+    }
+
+    public void printBlackList() {
+        System.out.println("블랙리스트에 등록하시려면 1번, 아니라면 2번을 입력해주세요");
+    }
+
+    public void printCustomerId() {
+        System.out.println("고객 ID를 입력해주세요.");
+    }
+
+    public void printCustomer(Customer customer) {
+        System.out.println(customer.toString());
+    }
+
+    public void printSuccessUpdate() {
+        System.out.println("성공적으로 업데이트 되었습니다.");
+    }
+
+    public void printWallet(Wallet wallet) {
+        System.out.println(wallet.toString());
     }
 }
