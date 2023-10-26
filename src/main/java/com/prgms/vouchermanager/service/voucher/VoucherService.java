b06bdb4eda6435615e049fee560ea382d9feb144
YongNyeo <won05121@gmail.com>
2023-10-26 21:53:20 +0900

    refactor: VoucherService가 내 필요 메서드 추가

--- src/main/java/com/prgms/vouchermanager/service/voucher/VoucherService.java
+++ src/main/java/com/prgms/vouchermanager/service/voucher/VoucherService.java
@@ -5,42 +5,87 @@ import com.prgms.vouchermanager.domain.voucher.FixedAmountVoucher;
 import com.prgms.vouchermanager.domain.voucher.PercentDiscountVoucher;
 import com.prgms.vouchermanager.domain.voucher.Voucher;
 import com.prgms.vouchermanager.dto.CreateVoucherDto;
-import com.prgms.vouchermanager.exception.ExceptionType;
+import com.prgms.vouchermanager.dto.UpdateVoucherDto;
 import com.prgms.vouchermanager.repository.voucher.VoucherRepository;
 import com.prgms.vouchermanager.validation.InputValidation;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.UUID;
 
+import static com.prgms.vouchermanager.exception.ExceptionType.*;
+
 @Service
 public class VoucherService {
 
-    private final InputValidation validation;
+    private final InputValidation inputValidation;
     private final VoucherRepository voucherRepository;
 
-    public VoucherService(InputValidation validation, VoucherRepository voucherRepository) {
-        this.validation = validation;
+    public VoucherService(InputValidation inputValidation, VoucherRepository voucherRepository) {
+        this.inputValidation = inputValidation;
         this.voucherRepository = voucherRepository;
     }
 
-    public void create(CreateVoucherDto dto) {
+    @Transactional
+    public Voucher create(CreateVoucherDto dto) {
 
         Voucher voucher = null;
+
         if (dto.getVoucherType() == 1) {
             voucher = new FixedAmountVoucher(UUID.randomUUID(), dto.getValue());
         } else if (dto.getVoucherType() == 2) {
-            if (!validation.validVoucherPercent(dto.getValue())) {
-                throw new RuntimeException(ExceptionType.INVALID_VOUCHER_PERCENT.getMessage());
+            if (!inputValidation.validVoucherPercent(dto.getValue())) {
+                throw new RuntimeException(INVALID_VOUCHER_PERCENT.getMessage());
             }
             voucher = new PercentDiscountVoucher(UUID.randomUUID(), dto.getValue());
-        } else
-            return;
+        } else throw new RuntimeException(INVALID_VOUCHER_INFO.getMessage());
 
-        voucherRepository.create(voucher);
+        try {
+            return voucherRepository.save(voucher);
+        } catch (DuplicateKeyException e) {
+            throw new DuplicateKeyException(DUPLICATED_KEY.getMessage());
+        }
     }
 
-    public List<Voucher> getVoucherList() {
-        return voucherRepository.getAllVouchers();
+    @Transactional
+    public void update(UUID id, UpdateVoucherDto dto) {
+
+        Voucher voucher = null;
+
+        if (dto.getVoucherType() == 1) {
+            voucher = new FixedAmountVoucher(id, dto.getValue());
+        } else if (dto.getVoucherType() == 2) {
+            if (!inputValidation.validVoucherPercent(dto.getValue())) {
+                throw new RuntimeException(INVALID_VOUCHER_PERCENT.getMessage());
+            }
+            voucher = new PercentDiscountVoucher(id, dto.getValue());
+        } else throw new RuntimeException(INVALID_VOUCHER_INFO.getMessage());
+
+        voucherRepository.update(voucher);
+
+    }
+
+    public Voucher findById(UUID id) {
+
+        return voucherRepository.findById(id).orElseThrow(() -> new EmptyResultDataAccessException(INVALID_VOUCHER_ID.getMessage(),1));
+
+    }
+
+    public List<Voucher> findAll() {
+        return voucherRepository.findAll();
+    }
+
+    @Transactional
+    public void deleteById(UUID id) {
+        voucherRepository.deleteById(id);
+    }
+
+    @Transactional
+    public void deleteAll() {
+        voucherRepository.deleteAll();
     }
 }
+
