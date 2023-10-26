d0ab64517806b97810f6e9e824c3536f55775268
Jay <injaesong0@gmail.com>
2023-10-27 02:59:43 +0900

    test: generate voucher repository by being injected template

--- src/test/java/com/programmers/vouchermanagement/voucher/repository/JdbcVoucherRepositoryTest.java
+++ src/test/java/com/programmers/vouchermanagement/voucher/repository/JdbcVoucherRepositoryTest.java
@@ -2,14 +2,24 @@ package com.programmers.vouchermanagement.voucher.repository;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
 
+import javax.sql.DataSource;
+
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
+import com.zaxxer.hikari.HikariDataSource;
+
 @Testcontainers(disabledWithoutDocker = true)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class JdbcVoucherRepositoryTest {
     @Container
     private static final MySQLContainer<?> mySQLContainer = new MySQLContainer<>()
@@ -17,10 +27,33 @@ class JdbcVoucherRepositoryTest {
             .withPassword("1234")
             .withInitScript("init.sql");
 
+    DataSource dataSource;
+    NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+    VoucherRepository voucherRepository;
+
+    @BeforeAll
+    void setUp() {
+        dataSource = DataSourceBuilder.create()
+                .url(mySQLContainer.getJdbcUrl())
+                .username(mySQLContainer.getUsername())
+                .password(mySQLContainer.getPassword())
+                .type(HikariDataSource.class)
+                .build();
+
+        namedParameterJdbcTemplate = new NamedParameterJdbcTemplate(dataSource);
+        voucherRepository = new JdbcVoucherRepository(namedParameterJdbcTemplate);
+    }
+
     @Test
     @DisplayName("테스트 컨테이너 생성을 성공한다.")
     void testMySQLContainerRunning() {
         assertThat(mySQLContainer.isRunning(), is(true));
         assertThat(mySQLContainer.getUsername(), is("test"));
+    }
+
+    @Test
+    @DisplayName("JdbcVoucherRepostory 템플릿을 주입 받아 생성된다.")
+    void testJdbcVoucherRepositoryCreation() {
+        assertThat(voucherRepository, notNullValue());
     }
 }
