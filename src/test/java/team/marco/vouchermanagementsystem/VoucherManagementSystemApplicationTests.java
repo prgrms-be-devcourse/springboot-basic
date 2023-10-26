eaf8053a0daf88c0f0731db46165b864a013f617
ASak1104 <khw56184@gmail.com>
2023-10-27 05:41:06 +0900

    style : 필요 없는 공백 문자 및 EOF 개행 문자 추가

--- src/test/java/team/marco/vouchermanagementsystem/VoucherManagementSystemApplicationTests.java
+++ src/test/java/team/marco/vouchermanagementsystem/VoucherManagementSystemApplicationTests.java
@@ -5,9 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class VoucherManagementSystemApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
