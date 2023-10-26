de7dcc83a5b4471e9775a81a19f7cb21f54273a4
Jay <injaesong0@gmail.com>
2023-10-27 05:38:50 +0900

    refactor: manually generate datasource and inject

--- src/main/java/com/programmers/vouchermanagement/configuration/DBConfiguration.java
+++ src/main/java/com/programmers/vouchermanagement/configuration/DBConfiguration.java
@@ -2,14 +2,28 @@ package com.programmers.vouchermanagement.configuration;
 
 import javax.sql.DataSource;
 
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
+import com.programmers.vouchermanagement.configuration.properties.datasource.DataSourceProperties;
+import com.zaxxer.hikari.HikariDataSource;
+
 @Configuration
 @Profile("jdbc")
 public class DBConfiguration {
+    @Bean
+    public DataSource dataSource(DataSourceProperties dataSourceProperties) {
+        return DataSourceBuilder.create()
+                .url(dataSourceProperties.getUrl())
+                .username(dataSourceProperties.getUsername())
+                .password(dataSourceProperties.getPassword())
+                .type(HikariDataSource.class)
+                .build();
+    }
+
     @Bean
     public NamedParameterJdbcTemplate namedParameterJdbcTemplate(DataSource dataSource) {
         return new NamedParameterJdbcTemplate(dataSource);
