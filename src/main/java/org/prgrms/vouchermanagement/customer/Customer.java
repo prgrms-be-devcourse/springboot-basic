3dabe080fff15d9b9e79fffa47737765681be84a
kimnamgyu <lethargic.hood@gmail.com>
2023-10-26 19:15:24 +0900

    feat : customer 도메인 추가

--- src/main/java/org/prgrms/vouchermanagement/customer/Customer.java
+++ src/main/java/org/prgrms/vouchermanagement/customer/Customer.java
@@ -1,4 +1,27 @@
 package org.prgrms.vouchermanagement.customer;
 
+import java.util.UUID;
+
 public class Customer {
+    private final UUID userId;
+    private final String userName;
+    private final int userAge;
+
+    public Customer(UUID userId, String userName, int userAge) {
+        this.userId = userId;
+        this.userName = userName;
+        this.userAge = userAge;
+    }
+
+    public UUID getUserId() {
+        return userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public int getUserAge() {
+        return userAge;
+    }
 }
