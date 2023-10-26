12a14ba93ab38ca414140d4f50a405784a851beb
Jay <injaesong0@gmail.com>
2023-10-27 05:53:49 +0900

    chore: fix sql init as suggested

--- src/test/resources/init.sql
+++ src/test/resources/init.sql
@@ -1,7 +1,7 @@
 CREATE TABLE customers (
     customer_id     BINARY(16) PRIMARY KEY,
     name            VARCHAR(20) NOT NULL,
-    customer_type   VARCHAR(10) NOT NULL default "NORMAL"
+    customer_type   VARCHAR(10) NOT NULL default 'NORMAL'
 );
 
 CREATE TABLE vouchers (
