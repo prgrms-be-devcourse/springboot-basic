0b7e9b308e6cb07cc3aa04a47e42b9bdf84923ab
shoeone96 <dlwnddnjs96@gmail.com>
2023-10-27 04:00:19 +0900

    feat: voucher와 customer 건색 후 voucher를 할당하는 로직 작성

--- src/main/java/org/programmers/springorder/voucher/service/VoucherService.java
+++ src/main/java/org/programmers/springorder/voucher/service/VoucherService.java
@@ -1,5 +1,7 @@
 package org.programmers.springorder.voucher.service;
 
+import org.programmers.springorder.customer.model.Customer;
+import org.programmers.springorder.customer.repository.CustomerRepository;
 import org.programmers.springorder.voucher.dto.VoucherRequestDto;
 import org.programmers.springorder.voucher.dto.VoucherResponseDto;
 import org.programmers.springorder.voucher.model.Voucher;
@@ -16,10 +18,12 @@ public class VoucherService {
 
     private static final Logger log = LoggerFactory.getLogger(VoucherService.class);
     private final VoucherRepository voucherRepository;
+    private final CustomerRepository customerRepository;
 
 
-    public VoucherService(VoucherRepository voucherRepository) {
+    public VoucherService(VoucherRepository voucherRepository, CustomerRepository customerRepository) {
         this.voucherRepository = voucherRepository;
+        this.customerRepository = customerRepository;
     }
 
     public List<VoucherResponseDto> getAllVoucher(){
@@ -33,5 +37,14 @@ public class VoucherService {
         Voucher voucher = Voucher.of(UUID.randomUUID(), voucherDto);
         voucherRepository.save(voucher);
         log.info("등록된 Voucher => iD: {}, type: {}, value: {}", voucher.getVoucherId(), voucher.getVoucherType(), voucher.getDiscountValue());
+    }
+
+    public String update(UUID voucherId, UUID customerID){
+        Voucher voucher = voucherRepository.findById(voucherId)
+                .orElseThrow( () -> new RuntimeException("해당 바우처를 찾을 수 없습니다."));
+        Customer customer = customerRepository.findByID(customerID)
+                .orElseThrow( () -> new RuntimeException("해당 고객을 찾을 수 없습니다."));
+        Voucher updatedVoucher = voucherRepository.updateVoucherOwner(voucher, customer);
+        return updatedVoucher.getVoucherId().toString();
     }
 }
