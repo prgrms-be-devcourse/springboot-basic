213811f4c8a9ae59dd07723fb8661f9ddf857e8f
kkangh00n <kig2454@gmail.com>
2023-10-26 14:22:18 +0900

    test: voucher의 필드 타입 변환 rollback

--- src/test/java/com/weeklyMission/voucher/domain/PercentDiscountVoucherTest.java
+++ src/test/java/com/weeklyMission/voucher/domain/PercentDiscountVoucherTest.java
@@ -13,14 +13,14 @@ class PercentDiscountVoucherTest {
     @Test
     @DisplayName("기본 할인 기능 테스트")
     void testDiscount() {
-        PercentDiscountVoucher percentDiscountVoucher = new PercentDiscountVoucher(1L, 90);
+        PercentDiscountVoucher percentDiscountVoucher = new PercentDiscountVoucher(UUID.randomUUID(), 90);
         assertThat(percentDiscountVoucher.discount(1000)).isEqualTo(100L);
     }
 
     @Test
     @DisplayName("할인 금액은 마이너스가 될 수 없다.")
     void testWithMinus() {
-        assertThatThrownBy(() -> new PercentDiscountVoucher(2L, -100))
+        assertThatThrownBy(() -> new PercentDiscountVoucher(UUID.randomUUID(), -100))
             .isInstanceOf(IllegalArgumentException.class)
             .hasMessage("음수는 될 수 없음");
     }
@@ -28,7 +28,7 @@ class PercentDiscountVoucherTest {
     @Test
     @DisplayName("할인 금액은 0이 될 수 없다.")
     void testWithZero() {
-        assertThatThrownBy(() -> new PercentDiscountVoucher(3L, 0))
+        assertThatThrownBy(() -> new PercentDiscountVoucher(UUID.randomUUID(), 0))
             .isInstanceOf(IllegalArgumentException.class)
             .hasMessage("0이 될 수 없음");
     }
@@ -37,7 +37,7 @@ class PercentDiscountVoucherTest {
     @DisplayName("할인 금액은 최대 금액을 넘을 수 없다.")
     void testWithMaximum() {
         long MAX_FIXED_VOUCHER_AMOUNT = 99l;
-        assertThatThrownBy(() -> new PercentDiscountVoucher(4L, 100))
+        assertThatThrownBy(() -> new PercentDiscountVoucher(UUID.randomUUID(), 100))
             .isInstanceOf(IllegalArgumentException.class)
             .hasMessage("%d보다 작아야 함".formatted(MAX_FIXED_VOUCHER_AMOUNT));
     }
