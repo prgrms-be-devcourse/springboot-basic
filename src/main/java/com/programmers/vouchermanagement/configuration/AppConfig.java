3bfb167f1070f53d3915a2ad65169be229b4513c
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2023-10-26 16:42:56 +0900

    feat: configure dataSource and add JdbcTemplate,NamedParameterJdbcTemplate Beans

--- src/main/java/com/programmers/vouchermanagement/configuration/AppConfig.java
+++ src/main/java/com/programmers/vouchermanagement/configuration/AppConfig.java
@@ -1,14 +1,43 @@
 package com.programmers.vouchermanagement.configuration;
 
+import com.zaxxer.hikari.HikariDataSource;
 import org.beryx.textio.TextIO;
 import org.beryx.textio.TextIoFactory;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class AppConfig {
     @Bean
     public TextIO textIO() {
         return TextIoFactory.getTextIO();
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        var dataSource = DataSourceBuilder.create()
+                .url("jdbc:mysql://localhost:3306/test")
+                .username("root")
+                .password("980726")
+                .type(HikariDataSource.class)
+                .build();
+        dataSource.setMaximumPoolSize(1000);
+        dataSource.setMinimumIdle(100);
+        return dataSource;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
+
+    @Bean
+    public NamedParameterJdbcTemplate namedParameterJdbcTemplate(JdbcTemplate jdbcTemplate) {
+        return new NamedParameterJdbcTemplate(jdbcTemplate);
     }
 }
