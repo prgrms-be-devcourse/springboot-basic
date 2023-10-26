fd74e30ed307c5402072c66f948faee1848b0274
YongNyeo <won05121@gmail.com>
2023-10-26 21:03:53 +0900

    refactor: 도메인 Customer내 필요 메서드 및 필드 추가

--- src/main/java/com/prgms/vouchermanager/domain/customer/Customer.java
+++ src/main/java/com/prgms/vouchermanager/domain/customer/Customer.java
@@ -1,18 +1,29 @@
 package com.prgms.vouchermanager.domain.customer;
 
 
+import lombok.Getter;
+@Getter
 public class Customer {
 
-    private final Long id;
+    private Long id;
 
     private final String name;
 
     private final String email;
 
-    public Customer(Long id, String name, String email) {
+    private final boolean blackList;
+
+    public Customer(Long id, String name, String email, boolean blackList) {
+
         this.id = id;
         this.name = name;
         this.email = email;
+        this.blackList = blackList;
+
+    }
+
+    public void setId(long longValue) {
+        this.id = longValue;
     }
 
     @Override
@@ -21,6 +32,7 @@ public class Customer {
                 "id=" + id +
                 ", name='" + name + '\'' +
                 ", email='" + email + '\'' +
+                ", blackList=" + blackList +
                 '}';
     }
 }
