068682fd96ae6dd64687de7b2f425f52c3aafb18
ASak1104 <khw56184@gmail.com>
2023-10-27 08:47:01 +0900

    profile : dev 환경에서 jdbc를 사용하기 위한 프로파일 설정

--- src/main/java/team/marco/vouchermanagementsystem/repository/JSONFileVoucherRepository.java
+++ src/main/java/team/marco/vouchermanagementsystem/repository/JSONFileVoucherRepository.java
@@ -17,7 +17,7 @@ import team.marco.vouchermanagementsystem.model.Voucher;
 import team.marco.vouchermanagementsystem.model.VoucherType;
 import team.marco.vouchermanagementsystem.properties.FilePathProperties;
 
-@Profile({"local", "dev"})
+@Profile("local")
 @Repository
 public class JSONFileVoucherRepository implements VoucherRepository, DisposableBean {
     private final Map<UUID, Voucher> voucherMap;
