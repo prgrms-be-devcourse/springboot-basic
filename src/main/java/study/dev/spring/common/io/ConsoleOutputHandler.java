f700d99cb870283aa61bc30aa500806d5503d868
EunChanNam <namhm23@gmail.com>
2023-10-26 18:28:27 +0900

    feat: 시작 메뉴얼 메세지에 새로운 기능에 대한 메세지 추가

--- src/main/java/study/dev/spring/common/io/ConsoleOutputHandler.java
+++ src/main/java/study/dev/spring/common/io/ConsoleOutputHandler.java
@@ -18,10 +18,15 @@ public class ConsoleOutputHandler implements OutputHandler {
 	@Override
 	public void showStartMessage() {
 		String message = "=== Voucher Program ===" + NEW_LINE
-			+ "Type exit to exit the program." + NEW_LINE
-			+ "Type create to create a new voucher." + NEW_LINE
-			+ "Type list to list all vouchers." + NEW_LINE
-			+ "Type black_list to list all black_list of customer." + NEW_LINE;
+						 + "종료 : exit" + NEW_LINE
+						 + "바우처 생성 : create_voucher" + NEW_LINE
+						 + "모든 바우처 조회 : all_vouchers" + NEW_LINE
+						 + "특정 고객이 지닌 바우처 조회 : vouchers_by_customer" + NEW_LINE
+						 + "블랙리스트 고객 조회 : black_list" + NEW_LINE
+						 + "모든 고객 조회 : all_customer" + NEW_LINE
+						 + "특정 바우처를 지닌 고객 조회 : customers_by_voucher" + NEW_LINE
+						 + "지갑 추가 : add_wallet" + NEW_LINE
+						 + "특정 고객의 바우처 지갑 삭제 : remove_wallet_by_customer" + NEW_LINE;
 
 		out.println(message);
 	}
