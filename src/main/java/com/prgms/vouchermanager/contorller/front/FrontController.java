a1616a960338c595079a9ffc650ff0a2e2fefb41
YongNyeo <won05121@gmail.com>
2023-10-26 20:55:45 +0900

    refactor: FrontController의 메뉴 추가 및 switch-case문 도입

--- src/main/java/com/prgms/vouchermanager/contorller/front/FrontController.java
+++ src/main/java/com/prgms/vouchermanager/contorller/front/FrontController.java
@@ -2,7 +2,11 @@ package com.prgms.vouchermanager.contorller.front;
 
 
 import com.prgms.vouchermanager.contorller.customer.CustomerController;
+import com.prgms.vouchermanager.contorller.customer.CustomerMenuType;
 import com.prgms.vouchermanager.contorller.voucher.VoucherController;
+import com.prgms.vouchermanager.contorller.voucher.VoucherMenuType;
+import com.prgms.vouchermanager.contorller.wallet.WalletController;
+import com.prgms.vouchermanager.contorller.wallet.WalletMenuType;
 import com.prgms.vouchermanager.util.io.ConsoleInput;
 import com.prgms.vouchermanager.util.io.ConsoleOutput;
 import com.prgms.vouchermanager.validation.InputValidation;
@@ -10,10 +14,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 
-import static com.prgms.vouchermanager.contorller.customer.CustomerMenuType.BLACK_LIST;
-import static com.prgms.vouchermanager.contorller.front.FrontMenuType.*;
-import static com.prgms.vouchermanager.contorller.voucher.VoucherMenuType.CREATE;
-import static com.prgms.vouchermanager.contorller.voucher.VoucherMenuType.LIST;
 import static com.prgms.vouchermanager.exception.ExceptionType.*;
 import static com.prgms.vouchermanager.exception.ExceptionType.INVALID_CUSTOMER_MENU;
 
@@ -24,30 +24,27 @@ public class FrontController {
     private final static Logger logger = LoggerFactory.getLogger(FrontController.class);
 
     private final ConsoleInput consoleInput;
-
     private final ConsoleOutput consoleOutput;
+    private final VoucherController voucherController;
+    private final CustomerController customerController;
+    private final WalletController walletController;
 
     private final InputValidation inputValidation;
 
-    private final VoucherController voucherController;
-
-    private final CustomerController customerController;
-
-
-    public FrontController(ConsoleInput consoleInput, ConsoleOutput consoleOutput, InputValidation inputValidation, VoucherController voucherController, CustomerController customerController) {
+    public FrontController(ConsoleInput consoleInput, ConsoleOutput consoleOutput, VoucherController voucherController, CustomerController customerController, WalletController walletController, InputValidation inputValidation) {
 
         this.consoleInput = consoleInput;
         this.consoleOutput = consoleOutput;
-        this.inputValidation = inputValidation;
         this.voucherController = voucherController;
         this.customerController = customerController;
-
+        this.walletController = walletController;
+        this.inputValidation = inputValidation;
     }
 
     public void run() {
 
         boolean end = false;
-        int menu =0;
+        int menu = 0;
 
         while (!end) {
 
@@ -57,46 +54,48 @@ public class FrontController {
                 if (!inputValidation.validFrontMenu(menu)) {
                     throw new RuntimeException(INVALID_FRONT_MENU.getMessage());
                 }
+                switch (FrontMenuType.fromValue(menu)) {
+                    case VOUCHER -> runVoucherController();
+                    case CUSTOMER -> runCustomerContoller();
+                    case WALLET -> runWalletController();
+                    case END -> end = true;
+                }
+
             } catch (RuntimeException e) {
-                System.out.println(e.getMessage());
                 logger.warn(e.getMessage());
                 continue;
             }
 
-            if (menu == VOUCHER.getNumber()) {
-                runVoucherController();
-            } else if (menu == CUSTOMER.getNumber()) {
-                runCustomerContoller();
-            } else {
-                end = true;
-            }
-
         }
     }
+
     private void runVoucherController() {
 
         consoleOutput.printVoucherMenu();
-        int menu = 0;
+        int menu;
         try {
             menu = consoleInput.inputVoucherMenu();
 
             if (!inputValidation.validVoucherMenu(menu)) {
-                logger.warn(INVALID_VOUCHER_MENU.getMessage());
                 throw new RuntimeException(INVALID_VOUCHER_MENU.getMessage());
             }
 
-        } catch (RuntimeException e) {
+            switch (VoucherMenuType.fromValue(menu)) {
+                case CREATE -> voucherController.create();
+                case UPDATE -> voucherController.update();
+                case LIST -> voucherController.getList();
+                case ONE -> voucherController.findById();
+                case DELETE_ONE -> voucherController.deleteById();
+                case DELETE_ALL -> voucherController.deleteAll();
 
-            System.out.println(e.getMessage());
+            }
+
+        } catch (RuntimeException e) {
             logger.warn(e.getMessage());
             return;
         }
 
-        if (menu == CREATE.getNumber()) {
-            voucherController.create();
-        } else if (menu == LIST.getNumber()) {
-            voucherController.getList();
-        }
+
     }
 
     private void runCustomerContoller() {
@@ -107,17 +106,50 @@ public class FrontController {
             menu = consoleInput.inputCustomerMenu();
 
             if (!inputValidation.validCustomerMenu(menu)) {
-                logger.warn(INVALID_CUSTOMER_MENU.getMessage());
                 throw new RuntimeException(INVALID_CUSTOMER_MENU.getMessage());
             }
+            switch (CustomerMenuType.fromValue(menu)) {
+                case CREATE -> customerController.create();
+                case UPDATE -> customerController.update();
+                case FIND_ALL -> customerController.getList();
+                case FIND_ONE -> customerController.findById();
+                case DELETE_ONE -> customerController.deleteById();
+                case DELETE_ALL -> customerController.deleteAll();
+                case BLACK_LIST -> customerController.findBlackList();
 
+            }
         } catch (RuntimeException e) {
-            System.out.println(e.getMessage());
             logger.warn(e.getMessage());
             return;
         }
-        if (menu == BLACK_LIST.getNumber()) {
-            customerController.getBlackList();
+
+
+    }
+
+    private void runWalletController() {
+
+        consoleOutput.printWalletMenu();
+        int menu = 0;
+        try {
+            menu = consoleInput.inputVoucherMenu();
+
+            if (!inputValidation.validWalletMenu(menu)) {
+                throw new RuntimeException(INVALID_WALLET_MENU.getMessage());
+            }
+
+            switch (WalletMenuType.fromValue(menu)) {
+
+                case CREATE -> walletController.create();
+                case FIND_BY_CUSTOMER_ID -> walletController.findByCustomerId();
+                case FIND_BY_VOUCHER_ID -> walletController.findByVoucherId();
+                case DELETE_BY_CUSTOMER_ID -> walletController.deleteByCustomerId();
+
+            }
+
+        } catch (RuntimeException e) {
+
+            logger.warn(e.getMessage());
+            return;
         }
     }
 }
--- src/main/java/com/prgms/vouchermanager/contorller/front/FrontMenuType.java
+++ src/main/java/com/prgms/vouchermanager/contorller/front/FrontMenuType.java
@@ -1,7 +1,9 @@
 package com.prgms.vouchermanager.contorller.front;
 
+import java.util.Arrays;
+
 public enum FrontMenuType {
-    VOUCHER(1),CUSTOMER(2),END(3);
+    VOUCHER(1),CUSTOMER(2),WALLET(3),END(4);
 
     private final int number;
     FrontMenuType(int number) {
@@ -10,5 +12,13 @@ public enum FrontMenuType {
 
     public int getNumber() {
         return number;
+    }
+
+    public static FrontMenuType fromValue(int menu) {
+
+        return Arrays.stream(FrontMenuType.values())
+                .filter(frontMenuType -> frontMenuType.getNumber() == menu)
+                .findFirst()
+                .get();
     }
 }
