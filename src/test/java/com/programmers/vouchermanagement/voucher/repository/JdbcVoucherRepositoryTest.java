5c90f450e5da2baa5e56d62e551a6bfe41975539
Jay <injaesong0@gmail.com>
2023-10-27 03:22:05 +0900

    test: return empty list for no voucher in db

--- src/test/java/com/programmers/vouchermanagement/voucher/repository/JdbcVoucherRepositoryTest.java
+++ src/test/java/com/programmers/vouchermanagement/voucher/repository/JdbcVoucherRepositoryTest.java
@@ -4,21 +4,28 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
 
+import java.util.List;
+
 import javax.sql.DataSource;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
+import com.programmers.vouchermanagement.voucher.domain.Voucher;
 import com.zaxxer.hikari.HikariDataSource;
 
 @Testcontainers(disabledWithoutDocker = true)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class JdbcVoucherRepositoryTest {
     @Container
@@ -45,6 +52,7 @@ class JdbcVoucherRepositoryTest {
     }
 
     @Test
+    @Order(1)
     @DisplayName("테스트 컨테이너 생성을 성공한다.")
     void testMySQLContainerRunning() {
         assertThat(mySQLContainer.isRunning(), is(true));
@@ -52,8 +60,20 @@ class JdbcVoucherRepositoryTest {
     }
 
     @Test
+    @Order(2)
     @DisplayName("JdbcVoucherRepostory 템플릿을 주입 받아 생성된다.")
     void testJdbcVoucherRepositoryCreation() {
         assertThat(voucherRepository, notNullValue());
+    }
+
+    @Test
+    @Order(3)
+    @DisplayName("저장된 바우처가 없을 시 빈 리스트를 반환한다.")
+    void testFindAllVouchersSuccessful_ReturnEmptyList() {
+        //when
+        final List<Voucher> vouchers = voucherRepository.findAll();
+
+        //then
+        assertThat(vouchers.isEmpty(), is(true));
     }
 }
