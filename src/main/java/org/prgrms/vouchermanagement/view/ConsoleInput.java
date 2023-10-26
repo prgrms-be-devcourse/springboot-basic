3de24dd69da2e543c664c378ee43abd3dd17f81b
kimnamgyu <lethargic.hood@gmail.com>
2023-10-26 19:15:07 +0900

    feat : wallet 관련 입출력 추가

--- src/main/java/org/prgrms/vouchermanagement/view/ConsoleInput.java
+++ src/main/java/org/prgrms/vouchermanagement/view/ConsoleInput.java
@@ -1,10 +1,13 @@
 package org.prgrms.vouchermanagement.view;
 
-import org.prgrms.vouchermanagement.dto.VoucherInfo;
+import org.prgrms.vouchermanagement.dto.VoucherCreateInfo;
+import org.prgrms.vouchermanagement.dto.VoucherUpdateInfo;
+import org.prgrms.vouchermanagement.dto.WalletCreateInfo;
 import org.prgrms.vouchermanagement.exception.InvalidInputException;
 import org.prgrms.vouchermanagement.voucher.PolicyStatus;
 
 import java.util.Scanner;
+import java.util.UUID;
 
 public class ConsoleInput {
 
@@ -17,7 +20,7 @@ public class ConsoleInput {
         return validateAndConvertToCommand(inputCommand);
     }
 
-    public VoucherInfo createVoucherInput() {
+    public VoucherCreateInfo createVoucherInput() {
         consoleOutput.getInput();
         String inputPolicy = scanner.nextLine();
         PolicyStatus policy = validateAndConvertPolicy(inputPolicy);
@@ -25,7 +28,41 @@ public class ConsoleInput {
         consoleOutput.getInput();
         String inputAmountOrPercent = scanner.nextLine();
         long amountOrPercent = validateAndConvertAmountOrPercent(inputAmountOrPercent);
-        return new VoucherInfo(policy, amountOrPercent);
+        return new VoucherCreateInfo(policy, amountOrPercent);
+    }
+
+    public VoucherUpdateInfo updateVoucherInput() {
+        consoleOutput.getInput();
+        UUID voucherId = UUID.fromString(scanner.nextLine());
+
+        consoleOutput.getInput();
+        String inputAmountOrPercent = scanner.nextLine();
+        long amountOrPercent = validateAndConvertAmountOrPercent(inputAmountOrPercent);
+        return new VoucherUpdateInfo(voucherId, amountOrPercent);
+    }
+
+    public WalletCreateInfo createWalletInput() {
+        consoleOutput.getInput();
+        UUID customerId = UUID.fromString(scanner.nextLine());
+
+        consoleOutput.getInput();
+        UUID voucherId = UUID.fromString(scanner.nextLine());
+        return new WalletCreateInfo(customerId, voucherId);
+    }
+
+    public UUID findAllVouchersByCustomerId() {
+        consoleOutput.getInput();
+        return UUID.fromString(scanner.nextLine());
+    }
+
+    public UUID deleteVoucherByCustomerId() {
+        consoleOutput.getInput();
+        return UUID.fromString(scanner.nextLine());
+    }
+
+    public UUID findCustomerByVoucherId() {
+        consoleOutput.getInput();
+        return UUID.fromString(scanner.nextLine());
     }
 
     private Command validateAndConvertToCommand(String inputCommand) {
--- src/main/java/org/prgrms/vouchermanagement/view/ConsoleOutput.java
+++ src/main/java/org/prgrms/vouchermanagement/view/ConsoleOutput.java
@@ -1,6 +1,7 @@
 package org.prgrms.vouchermanagement.view;
 
 import org.prgrms.vouchermanagement.blackCustomer.BlackCustomer;
+import org.prgrms.vouchermanagement.customer.Customer;
 import org.prgrms.vouchermanagement.voucher.Voucher;
 
 import java.util.List;
@@ -12,7 +13,11 @@ public class ConsoleOutput {
             === Voucher Program ===
             Type create to create a new voucher.
             Type list to list all vouchers.
+            Type customer to list all customers
             Type blacklist to list blackCustomer in blacklist
+            Type update to update voucher amount or percent
+            Type delete to delete all vouchers
+            Type wallet is new wallet mode
             Type exit to exit the program.
             """;
 
@@ -33,10 +38,74 @@ public class ConsoleOutput {
             === VOUCHER LISTS ===
             """;
 
+    private static final String showCustomerList = """
+                            
+            === CUSTOMER LISTS ===
+            """;
+
     private static final String showBlackList = """
                             
             === CUSTOMER BLACKLIST ===
             """;
+
+    private static final String updateVoucher = """
+                            
+            === VOUCHER UPDATE ===
+            update amount or percent by voucherId
+            
+            input voucherId(UUID)
+            input amount or percent
+            """;
+
+    private static final String updateVoucherComplete = """
+                            
+            === COMPLETE UPDATE VOUCHER ===
+            """;
+
+    private static final String deleteVoucher = """
+                            
+            === VOUCHER DELETE ===
+            """;
+
+    private static final String wallet = """
+                            
+            === WALLET PAGE ===
+            Type create to create a new wallet
+            Type list to find all vouchers by customerId
+            Type delete to delete voucher by customerId
+            Type find to find customer by voucherId
+            Type exit to exit the program.
+            """;
+
+    private static final String createWallet = """
+            
+            == CREATE WALLET ==
+            
+            input customerId(UUID)
+            input voucherId(UUID)
+            """;
+
+    private static final String findVoucherWallet = """
+            
+            == FIND ALL VOUCHERS BY CUSTOMER ID ==
+            
+            input customerId(UUID)
+            """;
+
+    private static final String deleteVoucherWallet = """
+            
+            == DELETE VOUCHER BY CUSTOMER ID ==
+            
+            input customerId(UUID)
+            """;
+
+    private static final String findCustomerWallet = """
+            
+            == FIND CUSTOMER BY VOUCHER ID
+            
+            input voucherId(UUID)
+            """;
+
 
     private static final String exit = """
             
@@ -65,6 +134,49 @@ public class ConsoleOutput {
 
     public void printCreateVoucherCompleteMessage() {
         printMessage(createVoucherComplete);
+    }
+
+    public void printUpdateVoucherMessage() {
+        printMessage(updateVoucher);
+    }
+
+    public void printUpdateVoucherCompleteMessage() {
+        printMessage(updateVoucherComplete);
+    }
+
+    public void printDeleteVoucherMessage() {
+        printMessage(deleteVoucher);
+    }
+
+    public void printWalletMessage() {
+        printMessage(wallet);
+    }
+
+    public void printCreateWalletMessage() {
+        printMessage(createWallet);
+    }
+
+    public void printAllVouchersByCustomerId() {
+        printMessage(findVoucherWallet);
+    }
+
+    public void printDeleteVoucherByCustomerId() {
+        printMessage(deleteVoucherWallet);
+    }
+
+    public void printCustomerByVoucherId() {
+        printMessage(findCustomerWallet);
+    }
+
+    public void printCustomerList(List<Customer> customerList) {
+        printMessage(showCustomerList);
+        for (Customer customer : customerList) {
+            printlnMessage("customerId : " + customer.getUserId() + ", name : " + customer.getUserName() + ", age : " + customer.getUserAge());
+        }
+    }
+
+    public void printCustomer(Customer customer) {
+        printlnMessage("customerId : " + customer.getUserId() + ", name : " + customer.getUserName() + ", age : " + customer.getUserAge());
     }
 
     public void printBlackList(List<BlackCustomer> blackList) {
