3daf7eebd0a0e06df47afd458fe720cb3ba07594
EomYerim <djadpfa1002@naver.com>
2023-10-27 02:02:18 +0900

    feat: CustomerMenu enum 생성 고객 저장, 블랙리스트 조회, 뒤로가기 메뉴 추가

---
+++ src/main/java/org/programmers/springorder/utils/CustomerMenuType.java
@@ -0,0 +1,32 @@
+package org.programmers.springorder.utils;
+
+import org.programmers.springorder.consts.ErrorMessage;
+
+import java.util.Arrays;
+
+public enum CustomerMenuType {
+    CREATE("1"),
+    BLACK("2"),
+    BACK("3");
+
+    private final String menuNum;
+
+    CustomerMenuType(String menuNum) {
+        this.menuNum = menuNum;
+    }
+
+    private boolean isEqual(String customerMenu) {
+        return this.menuNum.equals(customerMenu);
+    }
+
+    public static CustomerMenuType selectCustomerMenu(String customerMenu) {
+        return Arrays.stream(CustomerMenuType.values())
+                .filter(menu -> menu.isEqual(customerMenu))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException(ErrorMessage.INVALID_VALUE_MESSAGE));
+    }
+
+    public boolean isBack() {
+        return this == BACK;
+    }
+}
