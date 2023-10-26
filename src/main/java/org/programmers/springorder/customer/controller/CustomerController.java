b9c8efd5e699339b49d87da60aa8104fb490665e
EomYerim <djadpfa1002@naver.com>
2023-10-27 02:06:35 +0900

    refactor: 고객 메뉴 관련 메서드 리팩토링 고객 저장, 블랙리스트 고객 조회, 뒤로 가기 메뉴

--- src/main/java/org/programmers/springorder/customer/controller/CustomerController.java
+++ src/main/java/org/programmers/springorder/customer/controller/CustomerController.java
@@ -1,13 +1,16 @@
 package org.programmers.springorder.customer.controller;
 
 import org.programmers.springorder.console.Console;
+import org.programmers.springorder.consts.Message;
+import org.programmers.springorder.customer.dto.CustomerRequestDto;
 import org.programmers.springorder.customer.service.CustomerService;
+import org.programmers.springorder.utils.CustomerMenuType;
+import org.programmers.springorder.utils.ExceptionHandler;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class CustomerController {
     private final Console console;
-
     private final CustomerService customerService;
 
     public CustomerController(Console console, CustomerService customerService) {
@@ -15,7 +18,31 @@ public class CustomerController {
         this.customerService = customerService;
     }
 
-    public void printBlackList(){
+    public void run() {
+        boolean isRunning = true;
+
+        while (isRunning) {
+            CustomerMenuType menu = ExceptionHandler.input(Console::inputCustomerMenu);
+
+            switch (menu) {
+                case CREATE -> createCustomer();
+                case BLACK -> printBlackList();
+                case BACK -> {
+                    isRunning = false;
+                    console.printMessage(Message.BACK_TO_MENU_MESSAGE);
+                }
+            }
+        }
+    }
+
+    private void createCustomer() {
+        CustomerRequestDto customerRequestDto = console.inputCustomerInfo();
+        customerService.createCustomer(customerRequestDto);
+        console.printMessage(Message.CUSTOMER_REGISTERED);
+    }
+
+    private void printBlackList() {
         console.showBlackList(customerService.getBlackList());
     }
+
 }
