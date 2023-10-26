ad616a4478832d816522e27bf65a0289505bfd06
ebcho <joeunbi514@naver.com>
2023-10-26 23:15:54 +0900

    feature: schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,13 +1,21 @@
 CREATE TABLE IF NOT EXISTS customer (
-      customer_id    CHAR(36) PRIMARY KEY,
-      name           VARCHAR(20) NOT NULL,
-      email          VARCHAR(50) NOT NULL UNIQUE,
-      created_at     DATETIME NOT NULL
+    customer_id    CHAR(36) PRIMARY KEY,
+    name           VARCHAR(20) NOT NULL,
+    email          VARCHAR(50) NOT NULL UNIQUE,
+    created_at     DATETIME NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS voucher (
-     voucher_id CHAR(36) PRIMARY KEY,
-     voucher_type VARCHAR(30) NOT NULL,
-     percent INT,
-     amount INT
+    voucher_id CHAR(36) PRIMARY KEY,
+    voucher_type VARCHAR(30) NOT NULL,
+    percent INT,
+    amount INT
 );
+
+CREATE TABLE IF NOT EXISTS wallet (
+    wallet_id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    customer_id CHAR(36) REFERENCES customer(customer_id),
+    voucher_id CHAR(36) REFERENCES voucher(voucher_id)
+);
+
+
