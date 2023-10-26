45faa580593fd6b3e09d9f7a085679b257577057
EunChanNam <namhm23@gmail.com>
2023-10-26 22:54:27 +0900

    enhancement: DataSourceTestSupport 프로퍼티 파일 적용

--- src/test/java/study/dev/spring/global/support/DataSourceTestSupport.java
+++ src/test/java/study/dev/spring/global/support/DataSourceTestSupport.java
@@ -2,11 +2,13 @@ package study.dev.spring.global.support;
 
 import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,18 +17,28 @@ import com.zaxxer.hikari.HikariDataSource;
 
 @SpringJUnitConfig
 @Transactional
+@TestPropertySource("classpath:application.properties")
 public abstract class DataSourceTestSupport {
 
 	@TestConfiguration
 	static class DataSourceConfig {
 
+		@Value("${spring.datasource.url}")
+		private String url;
+		@Value("${spring.datasource.driver-class-name}")
+		private String driverClasName;
+		@Value("${spring.datasource.username}")
+		private String username;
+		@Value("${spring.datasource.password}")
+		private String password;
+
 		@Bean
 		public DataSource dataSource() {
 			DataSource dataSource = DataSourceBuilder.create()
-				.url("jdbc:h2:mem:test;MODE=MySQL;DB_CLOSE_DELAY=-1")
-				.driverClassName("org.h2.Driver")
-				.username("sa")
-				.password("")
+				.url(url)
+				.driverClassName(driverClasName)
+				.username(username)
+				.password(password)
 				.type(HikariDataSource.class)
 				.build();
 
