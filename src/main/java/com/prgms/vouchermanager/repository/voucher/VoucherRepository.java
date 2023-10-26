8ffee0a0069d46ffca0226386531c78b700e6e1e
YongNyeo <won05121@gmail.com>
2023-10-26 21:47:34 +0900

    refactor: VoucherRepository 에 필요 메서드 추가

--- src/main/java/com/prgms/vouchermanager/repository/voucher/VoucherRepository.java
+++ src/main/java/com/prgms/vouchermanager/repository/voucher/VoucherRepository.java
@@ -3,10 +3,19 @@ package com.prgms.vouchermanager.repository.voucher;
 import com.prgms.vouchermanager.domain.voucher.Voucher;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 public interface VoucherRepository {
 
-    void create(Voucher voucher);
+    Voucher save(Voucher voucher);
+    void update(Voucher voucher);
 
-    List<Voucher> getAllVouchers();
+    Optional<Voucher> findById(UUID id);
+
+    List<Voucher> findAll();
+
+    void deleteById(UUID id);
+
+    void deleteAll();
 }
