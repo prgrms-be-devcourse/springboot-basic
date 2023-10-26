9bd9c4cd58aa7d97d5c678442692c4c47fe904f0
hyoguoo <hyoguoo@gmail.com>
2023-10-26 19:28:27 +0900

    add: JdbcCustomerRepository 테스트 코드 추가

---
+++ src/test/java/devcourse/springbootbasic/repository/customer/JdbcCustomerRepositoryTest.java
@@ -0,0 +1,114 @@
+package devcourse.springbootbasic.repository.customer;
+
+import devcourse.springbootbasic.domain.customer.Customer;
+import devcourse.springbootbasic.util.UUIDUtil;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Transactional
+@SpringBootTest
+class JdbcCustomerRepositoryTest {
+
+    @Autowired
+    private JdbcCustomerRepository customerRepository;
+
+    @Autowired
+    private NamedParameterJdbcTemplate jdbcTemplate;
+
+    @BeforeEach
+    void setUp() {
+        String query = """
+                DELETE FROM customer
+                """;
+        jdbcTemplate.update(query, (SqlParameterSource) null);
+    }
+
+    @Test
+    @DisplayName("블랙리스트인 고객을 조회할 수 있습니다.")
+    void testFindAllBlacklistedCustomers() {
+        // Given
+        Customer notBlacklistedCustomer = Customer.createCustomer(UUIDUtil.generateRandomUUID(), "Customer 1", false);
+        Customer blacklistedCustomer = Customer.createCustomer(UUIDUtil.generateRandomUUID(), "Customer 2", true);
+        customerRepository.save(notBlacklistedCustomer);
+        customerRepository.save(blacklistedCustomer);
+
+        // When
+        List<Customer> blacklistedCustomers = customerRepository.findAllBlacklistedCustomers();
+
+        // Then
+        assertThat(blacklistedCustomers).hasSize(1);
+    }
+
+    @Test
+    @DisplayName("고객을 저장할 수 있습니다.")
+    void testSaveCustomer() {
+        // Given
+        Customer newCustomer = Customer.createCustomer(UUIDUtil.generateRandomUUID(), "New Customer", true);
+        Customer savedCustomer = customerRepository.save(newCustomer);
+
+        // When
+        Customer foundCustomer = customerRepository.findById(savedCustomer.getId()).orElse(null);
+
+        // Then
+        assertThat(foundCustomer).isNotNull();
+        assertThat(foundCustomer.getId()).isEqualTo(savedCustomer.getId());
+    }
+
+    @Test
+    @DisplayName("고객 ID로 고객을 조회할 수 있습니다.")
+    void testFindById() {
+        // Given
+        UUID customerId = UUIDUtil.generateRandomUUID();
+        customerRepository.save(Customer.createCustomer(customerId, "Customer 1", true));
+
+        // When
+        Optional<Customer> customer = customerRepository.findById(customerId);
+
+        // Then
+        assertThat(customer).isPresent();
+        assertThat(customer.get().getId()).isEqualTo(customerId);
+    }
+
+    @Test
+    @DisplayName("고객을 수정할 수 있습니다.")
+    void testUpdateCustomer() {
+        // Given
+        UUID customerId = UUIDUtil.generateRandomUUID();
+        customerRepository.save(Customer.createCustomer(customerId, "Customer 1", false));
+
+        // When
+        Customer customer = customerRepository.findById(customerId).orElseThrow();
+        int updatedRows = customerRepository.update(customer.updateBlacklistStatus(true));
+
+        // Then
+        assertThat(updatedRows).isEqualTo(1);
+        assertThat(customer.isBlacklisted()).isTrue();
+    }
+
+    @Test
+    @DisplayName("없는 고객을 수정하면 0을 반환합니다.")
+    void testUpdateCustomerFail() {
+        // Given
+        UUID customerId = UUIDUtil.generateRandomUUID();
+        customerRepository.save(Customer.createCustomer(customerId, "Customer 1", false));
+
+        // When
+        Customer customer = Customer.createCustomer(UUIDUtil.generateRandomUUID(), "Customer 2", true);
+        int updatedRows = customerRepository.update(customer);
+
+        // Then
+        assertThat(updatedRows).isZero();
+    }
+}
