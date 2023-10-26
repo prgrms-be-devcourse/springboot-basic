0826d04bf33cd12e8bf6bacc8295fc09837209d2
voidmelody <sss9073@naver.com>
2023-10-26 23:01:18 +0900

    Refactor: "Customer equals & hashCode 함수 추가"

--- src/main/java/org/prgrms/prgrmsspring/entity/user/Customer.java
+++ src/main/java/org/prgrms/prgrmsspring/entity/user/Customer.java
@@ -1,19 +1,20 @@
 package org.prgrms.prgrmsspring.entity.user;
 
+import java.util.Objects;
 import java.util.UUID;
 
 
 public class Customer {
     private final UUID customerId;
     private final String name;
-    private final Boolean isBlack;
     private final String email;
+    private final Boolean isBlack;
 
-    public Customer(UUID customerId, String name, Boolean isBlack, String email) {
+    public Customer(UUID customerId, String name, String email, Boolean isBlack) {
         this.customerId = customerId;
         this.name = name;
-        this.isBlack = isBlack;
         this.email = email;
+        this.isBlack = isBlack;
     }
 
     public Customer(UUID customerId, String name, String email) {
@@ -40,12 +41,25 @@ public class Customer {
     }
 
     @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        Customer customer = (Customer) object;
+        return Objects.equals(customerId, customer.customerId) && Objects.equals(name, customer.name) && Objects.equals(email, customer.email) && Objects.equals(isBlack, customer.isBlack);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(customerId, name, email, isBlack);
+    }
+
+    @Override
     public String toString() {
         return "Customer{" +
                 "customerId=" + customerId +
                 ", name='" + name + '\'' +
-                ", isBlack=" + isBlack +
                 ", email='" + email + '\'' +
+                ", isBlack=" + isBlack +
                 '}';
     }
 }
