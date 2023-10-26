46142df4f1305cc64d347af269e8135187a94cbf
ParkJuHan94 <1004sunye@naver.com>
2023-10-26 23:26:07 +0900

    feat: KdtApplication 고민되는 주석 추가

--- src/main/java/org/prgrms/kdt/KdtApplication.java
+++ src/main/java/org/prgrms/kdt/KdtApplication.java
@@ -3,14 +3,13 @@ package org.prgrms.kdt;
 import org.prgrms.kdt.voucher.controller.VoucherController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 import java.io.IOException;
 
 @SpringBootApplication
-@ComponentScan(
-        basePackages = {"org.prgrms.kdt.app", "org.prgrms.kdt.customer", "org.prgrms.kdt.voucher"}
-)
+//@ComponentScan(
+//        basePackages = {"org.prgrms.kdt.app", "org.prgrms.kdt.customer", "org.prgrms.kdt.voucher"}
+//)
 public class KdtApplication {
 
     public static void main(String[] args) throws IOException {
@@ -20,6 +19,7 @@ public class KdtApplication {
         var isRepeat = true;
 
         while (isRepeat) {
+            // 여기서 메뉴 입력에 따른 VoucherController / CustomerController 로 분배 ??
             isRepeat = voucherController.startVoucherMenu();
         }
     }
