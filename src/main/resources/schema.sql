6ee0b35909e139406fc349fa2a0457ec0b6238b3
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2023-10-27 01:16:49 +0900

    Revert "enhance: set unique value(voucher_id) at wallet table"
    This reverts commit 35919e33e3f36fdb60f7c1c67b3aad90e2a2547f.

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -14,10 +14,9 @@ CREATE TABLE vouchers
     created_at     datetime(6) DEFAULT CURRENT_TIMESTAMP(6)
 );
 
-CREATE TABLE wallet
+CREATE TABLE voucher_customer_own_info
 (
     id          INT PRIMARY KEY AUTO_INCREMENT,
     customer_id BINARY(16) NOT NULL REFERENCES customers (id) ON DELETE CASCADE,
-    voucher_id  BINARY(16) NOT NULL REFERENCES vouchers (id) ON DELETE CASCADE,
-    CONSTRAINT voucher_id UNIQUE (voucher_id)
+    voucher_id  BINARY(16) NOT NULL REFERENCES vouchers (id) ON DELETE CASCADE
 );
