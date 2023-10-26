500c79b79952d0f7eba0cf423eba7fc57817950f
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2023-10-26 20:18:22 +0900

    feat: implement update, delete, findById mathod at InMemoryVoucherRepository

--- src/main/java/com/programmers/vouchermanagement/voucher/repository/InMemoryVoucherRepository.java
+++ src/main/java/com/programmers/vouchermanagement/voucher/repository/InMemoryVoucherRepository.java
@@ -1,11 +1,10 @@
 package com.programmers.vouchermanagement.voucher.repository;
 
-import java.util.*;
-
+import com.programmers.vouchermanagement.voucher.domain.Voucher;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Repository;
 
-import com.programmers.vouchermanagement.voucher.domain.Voucher;
+import java.util.*;
 
 @Repository
 @Profile("dev")
@@ -24,5 +23,20 @@ public class InMemoryVoucherRepository implements VoucherRepository {
     @Override
     public List<Voucher> findAll() {
         return vouchers.values().stream().toList();
+    }
+
+    @Override
+    public Optional<Voucher> findById(UUID id) {
+        return Optional.ofNullable(vouchers.get(id));
+    }
+
+    @Override
+    public void delete(UUID id) {
+        vouchers.remove(id);
+    }
+
+    @Override
+    public void update(Voucher voucher) {
+        vouchers.put(voucher.getVoucherId(), voucher);
     }
 }
