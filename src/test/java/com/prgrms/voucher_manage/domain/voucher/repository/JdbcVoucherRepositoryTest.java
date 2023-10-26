d59d6876bcddb1556723ab51d6104b3e768c5362
hs12 <loveanna1228@gmail.com>
2023-10-26 21:13:13 +0900

    [Feat] jdbc 저장소 테스트 코드 추가

--- src/test/java/com/prgrms/voucher_manage/domain/voucher/repository/JdbcVoucherRepositoryTest.java
+++ src/test/java/com/prgrms/voucher_manage/domain/voucher/repository/JdbcVoucherRepositoryTest.java
@@ -18,7 +18,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 @Import(JdbcVoucherRepository.class)
 public class JdbcVoucherRepositoryTest {
     @Autowired
-    private JdbcVoucherRepository jdbcVoucherRepository;
+    private JdbcVoucherRepository repository;
 
 
     @Test
@@ -27,7 +27,7 @@ public class JdbcVoucherRepositoryTest {
         //given
         FixedAmountVoucher voucher = new FixedAmountVoucher(1000L);
         //when
-        Voucher savedVoucher = jdbcVoucherRepository.save(voucher);
+        Voucher savedVoucher = repository.save(voucher);
         //then
         assertThat(voucher).isNotNull();
         assertThat(voucher.getDiscountAmount()).isEqualTo(1000L);
@@ -35,10 +35,60 @@ public class JdbcVoucherRepositoryTest {
     }
 
     @Test
-    @DisplayName("voucher를 찾을 수 있다.")
-    public void jdbcVoucherRepository_find(){
+    @DisplayName("voucher를 id로 찾을 수 있다.")
+    public void jdbcVoucherRepository_findById(){
         //given
-        List<Voucher> all = jdbcVoucherRepository.findAll();
-        assertThat(all.get(0).getDiscountAmount()).isEqualTo(1000L);
+        Voucher voucher = saveVoucher();
+        //when
+        Voucher foundVoucher = repository.findById(voucher.getId()).orElse(null);
+        //then
+        assertThat(foundVoucher).isNotNull();
+        assertThat(foundVoucher.getId()).isEqualTo(voucher.getId());
+        assertThat(foundVoucher.getDiscountAmount()).isEqualTo(1000L);
+    }
+
+    @Test
+    @DisplayName("모든 voucher를 찾을 수 있다.")
+    public void jdbcVoucherRepository_findVoucher(){
+        //given
+        Voucher voucher1 = saveVoucher();
+        Voucher voucher2 = saveVoucher();
+        //when
+        List<Voucher> vouchers = repository.findAll();
+        //then
+        assertThat(vouchers.size()).isEqualTo(2);
+        assertThat(voucher1.getId()).isEqualTo(vouchers.get(0).getId());
+        assertThat(voucher2.getId()).isEqualTo(vouchers.get(1).getId());
+    }
+
+    @Test
+    @DisplayName("voucher를 삭제할 수 있다.")
+    public void jdbcVoucherRepository_deleteById(){
+        //given
+        Voucher voucher = saveVoucher();
+        //when
+        int delete = repository.deleteById(voucher.getId());
+        //then
+        assertThat(delete).isEqualTo(1);
+        assertThat(repository.findById(voucher.getId())).isEmpty();
+    }
+
+
+    @Test
+    @DisplayName("voucher를 업데이트할 수 있다.")
+    public void jdbcVoucherRepository_update(){
+        //given
+        Voucher voucher = saveVoucher();
+        //when
+        Voucher updatedVoucher = new FixedAmountVoucher(voucher.getId(), 2000L);
+        int update = repository.update(updatedVoucher);
+        //then
+        assertThat(update).isEqualTo(1);
+        assertThat(updatedVoucher.getDiscountAmount()).isEqualTo(2000L);
+
+    }
+
+    public Voucher saveVoucher(){
+        return repository.save(new FixedAmountVoucher(1000L));
     }
 }
--- src/test/resources/data.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO voucher(voucher_id, amount, type)
-VALUES ('123e4567-e89b-12d3-a456-426614174000', 1000, 'FIXED');
