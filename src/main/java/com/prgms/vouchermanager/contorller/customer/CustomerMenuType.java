5bde0635d60f247281386e03261e944bea8b8e75
YongNyeo <won05121@gmail.com>
2023-10-26 20:56:25 +0900

    refactor: CustomerController의 메서드 추가 및 메뉴Enum 추가

--- src/main/java/com/prgms/vouchermanager/contorller/customer/CustomerMenuType.java
+++ src/main/java/com/prgms/vouchermanager/contorller/customer/CustomerMenuType.java
@@ -1,7 +1,9 @@
 package com.prgms.vouchermanager.contorller.customer;
 
+import java.util.Arrays;
+
 public enum CustomerMenuType {
-    BLACK_LIST(1);
+    CREATE(1), UPDATE(2), FIND_ALL(3), FIND_ONE(4), DELETE_ONE(5), DELETE_ALL(6),BLACK_LIST(7);
 
     private final int number;
 
@@ -11,5 +13,13 @@ public enum CustomerMenuType {
 
     public int getNumber() {
         return number;
+    }
+
+    public static CustomerMenuType fromValue(int menu) {
+
+        return Arrays.stream(CustomerMenuType.values())
+                .filter(customerMenuType -> customerMenuType.getNumber() == menu)
+                .findFirst()
+                .get();
     }
 }
