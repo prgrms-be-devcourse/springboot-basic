3e2facf45524eeef82fe0ee4f29c5d7847acac6a
EomYerim <djadpfa1002@naver.com>
2023-10-27 02:00:31 +0900

    refactor: CustomerService 고객 저장, 블랙리스트 조회 메서드 생성 및 리팩토링 고객 저장, 블랙리스트 조회 메서드 트랜잭션 적용

--- src/main/java/org/programmers/springorder/customer/service/CustomerService.java
+++ src/main/java/org/programmers/springorder/customer/service/CustomerService.java
@@ -1,10 +1,14 @@
 package org.programmers.springorder.customer.service;
 
+import org.programmers.springorder.customer.dto.CustomerRequestDto;
 import org.programmers.springorder.customer.dto.CustomerResponseDto;
+import org.programmers.springorder.customer.model.Customer;
 import org.programmers.springorder.customer.repository.CustomerRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class CustomerService {
@@ -15,6 +19,15 @@ public class CustomerService {
         this.customerRepository = customerRepository;
     }
 
+    @Transactional
+    public Customer createCustomer(CustomerRequestDto customerRequestDto) {
+        Customer customer = Customer.of(UUID.randomUUID(), customerRequestDto);
+        System.out.println("CustomerService.createCustomer");
+        customerRepository.save(customer);
+        return customer;
+    }
+
+    @Transactional
     public List<CustomerResponseDto> getBlackList() {
         return customerRepository.findAllBlackList()
                 .stream()
