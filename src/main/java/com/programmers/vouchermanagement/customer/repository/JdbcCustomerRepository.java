febc115b760c6ce1b993ea67de964824c227ad43
Jay <injaesong0@gmail.com>
2023-10-27 06:39:58 +0900

    feat: implement save to insert customer

--- src/main/java/com/programmers/vouchermanagement/customer/repository/JdbcCustomerRepository.java
+++ src/main/java/com/programmers/vouchermanagement/customer/repository/JdbcCustomerRepository.java
@@ -1,6 +1,12 @@
 package com.programmers.vouchermanagement.customer.repository;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -10,6 +16,8 @@ import org.springframework.stereotype.Repository;
 
 import com.programmers.vouchermanagement.configuration.properties.file.FileProperties;
 import com.programmers.vouchermanagement.customer.domain.Customer;
+import com.programmers.vouchermanagement.customer.domain.CustomerType;
+import com.programmers.vouchermanagement.util.UUIDConverter;
 
 @Repository
 @Profile("jdbc")
@@ -25,7 +33,11 @@ public class JdbcCustomerRepository implements CustomerRepository {
 
     @Override
     public Customer save(Customer customer) {
-        return null;
+        int savedVoucher = findById(customer.getCustomerId()).isEmpty() ? insert(customer) : update(customer);
+        if (savedVoucher != 1) {
+            throw new NoSuchElementException("Exception is raised while saving the customer %s".formatted(customer.getCustomerId()));
+        }
+        return customer;
     }
 
     @Override
@@ -55,5 +67,32 @@ public class JdbcCustomerRepository implements CustomerRepository {
     @Override
     public void loadBlacklistToStorage() {
         List<Customer> blacklist = loadBlacklist(blacklistFilePath);
+    }
+
+    private int insert(Customer customer) {
+        final String saveSQL = "INSERT INTO customers(customer_id, name, customer_type) VALUES (UUID_TO_BIN(:customerId), :name, :customerType)";
+        Map<String, Object> parameterMap = toParameterMap(customer);
+        return namedParameterJdbcTemplate.update(saveSQL, parameterMap);
+    }
+
+    private int update(Customer customer) {
+        return 0;
+    }
+
+    private Map<String, Object> toParameterMap(Customer customer) {
+        Map<String, Object> parameterMap = new HashMap<>();
+        parameterMap.put("customerId", customer.getCustomerId().toString().getBytes());
+        parameterMap.put("name", customer.getName());
+        parameterMap.put("customerType", customer.getCustomerType().name());
+
+        return Collections.unmodifiableMap(parameterMap);
+    }
+
+    private static Customer mapToCustomer(ResultSet resultSet) throws SQLException {
+        final UUID customer_id = UUIDConverter.toUUID(resultSet.getBytes("customer_id"));
+        final String name = resultSet.getString("name");
+        final String customerTypeName = resultSet.getString("customer_type");
+        final CustomerType customerType = CustomerType.findCustomerType(customerTypeName);
+        return new Customer(customer_id, name, customerType);
     }
 }
