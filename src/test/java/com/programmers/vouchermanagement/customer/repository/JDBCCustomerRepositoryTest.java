25d43608af4f6764f230a674181f284c7a9b59e8
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2023-10-26 19:30:30 +0900

    test: test JDBCCustomerRepository

---
+++ src/test/java/com/programmers/vouchermanagement/customer/repository/JDBCCustomerRepositoryTest.java
@@ -0,0 +1,70 @@
+package com.programmers.vouchermanagement.customer.repository;
+
+import com.programmers.vouchermanagement.customer.domain.Customer;
+import com.zaxxer.hikari.HikariDataSource;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+import javax.sql.DataSource;
+import java.util.List;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringJUnitConfig
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class JDBCCustomerRepositoryTest {
+    @Autowired
+    JDBCCustomerRepository jdbcCustomerRepository;
+    @Autowired
+    DataSource dataSource;
+
+    @Test
+    @DisplayName("블랙리스트를 조회할 수 있다.")
+    void findAllBlackCustomerSucceed() {
+        jdbcCustomerRepository.save(new Customer(UUID.randomUUID(), "이세희7", false));
+        jdbcCustomerRepository.save(new Customer(UUID.randomUUID(), "이세희8", true));
+        jdbcCustomerRepository.save(new Customer(UUID.randomUUID(), "이세희9", false));
+        List<Customer> customers = jdbcCustomerRepository.findAllBlackCustomer();
+        assertThat(customers.isEmpty()).isFalse();
+        assertThat(customers.stream().filter(customer -> !customer.isBlack()).toList()).isEmpty();
+    }
+
+    @Configuration
+    @ComponentScan(
+            basePackages = {"com.programmers.vouchermanagement.customer.repository"}
+    )
+    static class Config {
+        @Bean
+        public DataSource dataSource() {
+            var dataSource = DataSourceBuilder.create()
+                    .url("jdbc:mysql://localhost:3306/test")
+                    .username("root")
+                    .password("980726")
+                    .type(HikariDataSource.class)
+                    .build();
+            dataSource.setMaximumPoolSize(1000);
+            dataSource.setMinimumIdle(100);
+            return dataSource;
+        }
+
+        @Bean
+        public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+            return new JdbcTemplate(dataSource);
+        }
+
+        @Bean
+        public NamedParameterJdbcTemplate namedParameterJdbcTemplate(JdbcTemplate jdbcTemplate) {
+            return new NamedParameterJdbcTemplate(jdbcTemplate);
+        }
+    }
+}
