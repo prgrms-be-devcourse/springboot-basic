90c4fc8a15d32f6fabb3b864855b81dc144ea49c
ASak1104 <khw56184@gmail.com>
2023-10-27 21:24:35 +0900

    feat : wallet 테이블 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,4 +1,4 @@
-DROP TABLE IF EXISTS voucher;
+DROP TABLE voucher IF EXISTS voucher;
 
 CREATE TABLE voucher
 (
@@ -7,7 +7,7 @@ CREATE TABLE voucher
     data INT         NOT NULL
 )
 
-DROP TABLE IF EXISTS customer;
+DROP TABLE customer IF EXISTS customer;
 
 CREATE TABLE customer
 (
@@ -17,4 +17,15 @@ CREATE TABLE customer
     last_login_at datetime(6) DEFAULT NULL,
     created_at    datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP (6),
     CONSTRAINT unq_user_email UNIQUE (email)
+);
+
+DROP TABLE wallet IF EXISTS wallet;
+
+create table wallet
+(
+    customer_id BINARY(16)    NOT NULL,
+    voucher_id  BINARY(16)    NOT NULL,
+    FOREIGN KEY (customer_id) REFERENCES customer(id),
+    FOREIGN KEY (voucher_id)  REFERENCES voucher(id),
+    PRIMARY KEY (customer_id, voucher_id)
 );
