b4aa7bd591579301fdb6e4746732c83dcb917f27
marooo326 <ktnqkr01@gmail.com>
2023-10-27 13:58:40 +0900

    refactor: 공통 메서드 Console Util로 분리

---
+++ src/main/java/com/programmers/vouchermanagement/util/ConsoleUtil.java
@@ -0,0 +1,40 @@
+package com.programmers.vouchermanagement.util;
+
+import com.programmers.vouchermanagement.common.ErrorMessage;
+import org.beryx.textio.TextIO;
+import org.beryx.textio.TextIoFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.UUID;
+
+public class ConsoleUtil {
+    private static final int NAME_MIN_LENGTH = 1;
+    private static final TextIO textIO = TextIoFactory.getTextIO();
+    private static final Logger logger = LoggerFactory.getLogger(ConsoleUtil.class);
+
+    private static void displayMessage(String message) {
+        textIO.getTextTerminal().println(message);
+    }
+
+    public static UUID getUUID() {
+        String uuidString = textIO.newStringInputReader().read("UUID: ");
+        try {
+            return UUID.fromString(uuidString);
+        } catch (IllegalArgumentException e) {
+            logger.warn("Invalid UUID: {}", uuidString);
+            throw new IllegalArgumentException(ErrorMessage.INVALID_UUID.getMessage());
+        }
+    }
+
+    public static UUID getUUID(String message) {
+        displayMessage(message);
+        return getUUID();
+    }
+
+    public static String getName() {
+        return textIO.newStringInputReader()
+                .withMinLength(NAME_MIN_LENGTH)
+                .read("Name: ");
+    }
+}
