aa9004187ee13b7dc31c02d52fbdcdb9fe363c94
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2023-10-27 15:36:33 +0900

    feat: validate if discount value in long type boundary

--- src/main/java/com/programmers/vouchermanagement/util/Validator.java
+++ src/main/java/com/programmers/vouchermanagement/util/Validator.java
@@ -1,41 +1,46 @@
 package com.programmers.vouchermanagement.util;
 
-import java.math.BigDecimal;
-
+import com.programmers.vouchermanagement.voucher.domain.VoucherType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.programmers.vouchermanagement.voucher.domain.VoucherType;
-
 public class Validator {
     private static final Logger logger = LoggerFactory.getLogger(Validator.class);
-
-    //constants
-    private static final int COMPARATOR_FLAG = 0;
-    private static final BigDecimal MAX_PERCENT = BigDecimal.valueOf(100);
+    private static final long MAX_LONG_LENGTH = String.valueOf(Long.MAX_VALUE).length();
 
     //messages
     private static final String INVALID_DISCOUNT_AMOUNT_MESSAGE =
             "Input should be a number greater than 0";
     private static final String INVALID_DISCOUNT_PERCENT_MESSAGE =
             "Input should be a number greater than 0 and smaller than 100";
+    private static final String INVALID_DISCOUNT_VALUE_MESSAGE =
+            "Input should be a number greater than 0 and smaller than 999999999999999999(length: 18)";
 
-    public static void validateDiscountValue(VoucherType voucherType, BigDecimal discountValue) {
+    public static void validateDiscountValue(VoucherType voucherType, String discountValueStr) {
+        validateLongBoundary(discountValueStr);
+        long discountValue = Long.parseLong(discountValueStr);
         switch (voucherType) {
             case FIXED -> validateDiscountAmount(discountValue);
             case PERCENT -> validateDiscountPercent(discountValue);
         }
     }
 
-    private static void validateDiscountAmount(BigDecimal discountAmount) {
-        if (discountAmount.compareTo(BigDecimal.ZERO) <= COMPARATOR_FLAG) {
+    private static void validateLongBoundary(String discountAmount) {
+        if (MAX_LONG_LENGTH <= discountAmount.length()) {
+            logger.error(INVALID_DISCOUNT_VALUE_MESSAGE);
+            throw new IllegalArgumentException(INVALID_DISCOUNT_VALUE_MESSAGE);
+        }
+    }
+
+    private static void validateDiscountAmount(long discountAmount) {
+        if (discountAmount <= 0) {
             logger.error(INVALID_DISCOUNT_AMOUNT_MESSAGE);
             throw new IllegalArgumentException(INVALID_DISCOUNT_AMOUNT_MESSAGE);
         }
     }
 
-    private static void validateDiscountPercent(BigDecimal discountPercent) {
-        if (discountPercent.compareTo(BigDecimal.ZERO) <= COMPARATOR_FLAG || discountPercent.compareTo(MAX_PERCENT) > COMPARATOR_FLAG) {
+    private static void validateDiscountPercent(long discountPercent) {
+        if (discountPercent <= 0 || discountPercent > 100) {
             logger.error(INVALID_DISCOUNT_PERCENT_MESSAGE);
             throw new IllegalArgumentException(INVALID_DISCOUNT_PERCENT_MESSAGE);
         }
