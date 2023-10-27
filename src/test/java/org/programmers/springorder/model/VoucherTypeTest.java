4f2c03d6cec522338b2d91b76d6602c26dadc28d
EomYerim <djadpfa1002@naver.com>
2023-10-27 18:50:38 +0900

    test: VoucherType 관련 테스트 수행

--- src/test/java/org/programmers/springorder/model/VoucherTypeTest.java
+++ src/test/java/org/programmers/springorder/model/VoucherTypeTest.java
@@ -1,74 +1,54 @@
 package org.programmers.springorder.model;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.programmers.springorder.model.voucher.VoucherType;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-@DisplayName("VoucherType 종류 관련 테스트")
 class VoucherTypeTest {
 
+    @ParameterizedTest
+    @CsvSource(value = {"1, FIXED", "2, PERCENT"})
+    @DisplayName("유효한 바우처 타입을 입력하면 바우처 타입을 반환한다.")
+    void selectVoucherType(String menuNum, VoucherType expectedVoucherType) {
+        // when
+        VoucherType result = VoucherType.selectVoucherType(menuNum);
 
-    @Test
-    @DisplayName("유효하지 않은 바우처 타입을 입력하면 에러 메시지를 띄운다.")
-    void invalidMenuNumber() {
-        //given
-        String menuNum = "4";
+        // then
+        assertThat(result).isEqualTo(expectedVoucherType);
+    }
 
-        //when
-        //then
-
+    @ParameterizedTest
+    @CsvSource(value = {"3", "o"})
+    @DisplayName("유효하지 않은 바우처 타입을 입력하면, 에러 메시지를 띄운다.")
+    void selectVoucherTypeFail(String menuNum) {
         assertThatThrownBy(() -> VoucherType.selectVoucherType(menuNum))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("유효하지 않은 값입니다. 다시 입력해주세요.");
 
     }
 
-    @Test
-    @DisplayName("유효한 바우처 타입을 입력하면 바우처 타입을 반환한다.")
-    void validMenuNumber() {
-        //given
-        String menuNum = "1";
-        String menuNum2 = "2";
 
-        //when
-        VoucherType voucherType = VoucherType.selectVoucherType(menuNum);
-        VoucherType voucherType2 = VoucherType.selectVoucherType(menuNum2);
-
-        //then
-        assertThat(voucherType).isEqualTo(VoucherType.FIXED);
-        assertThat(voucherType2).isEqualTo(VoucherType.PERCENT);
-    }
-
-
-    @Test
-    @DisplayName("Fixed Amount 쿠폰을 사용해 할인받는다.")
-    void useFixedAmountVoucher() {
-        // given
-        int beforeDiscount = 10000;
-        int discountAmount = 1000;
-
+    @ParameterizedTest
+    @CsvSource(value = {"FIXED, 10000, 1000, 9000", "PERCENT, 10000, 10, 9000"})
+    @DisplayName("쿠폰을 사용해 할인받는다.")
+    void calculate(VoucherType voucherType, long beforeDiscount, long discountValue, long expectedResult) {
         // when
-        long result = VoucherType.FIXED.calculate(beforeDiscount, discountAmount);
+        long result = voucherType.calculate(beforeDiscount, discountValue);
 
         // then
-        assertThat(result).isEqualTo(9000);
+        assertThat(result).isEqualTo(expectedResult);
     }
 
-    @Test
-    @DisplayName("Percent Amount 쿠폰을 사용해 할인 받는다.")
-    void usePercentDiscountVoucher() {
-        // given
-        int beforeDiscount = 10000;
-        int discountAmount = 10;
-
-        // when
-        long result = VoucherType.PERCENT.calculate(beforeDiscount, discountAmount);
-
-        // then
-        assertThat(result).isEqualTo(9000);
+    @ParameterizedTest
+    @CsvSource(value = {"FIXED, 10", "PERCENT, 100"})
+    @DisplayName("할인 금액 또는 할인율이 유효하지 않다면, 에러 메시지를 띄운다.")
+    void validateDiscountRangeFail(VoucherType voucherType, long discountValue) {
+        assertThatThrownBy(() -> VoucherType.validateDiscountRange(voucherType, discountValue))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
 }
