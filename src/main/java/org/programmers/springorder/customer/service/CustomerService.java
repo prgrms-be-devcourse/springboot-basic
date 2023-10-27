10fdb7c9002a83f052cf21d4d4dfd31124342f71
shoeone96 <dlwnddnjs96@gmail.com>
2023-10-27 15:12:58 +0900

    feat: 특정 voucher의 주인을 찾는 서비스 로직 추가 * 이 로직에서 CustomerService에 기존에 사용하지 않던 voucherRepository 의존성이 추가됐는데 이렇게 추가되도 괜찮은지 궁금합니다.

--- src/main/java/org/programmers/springorder/customer/service/CustomerService.java
+++ src/main/java/org/programmers/springorder/customer/service/CustomerService.java
@@ -1,18 +1,24 @@
 package org.programmers.springorder.customer.service;
 
 import org.programmers.springorder.customer.dto.CustomerResponseDto;
+import org.programmers.springorder.customer.model.Customer;
 import org.programmers.springorder.customer.repository.CustomerRepository;
+import org.programmers.springorder.voucher.model.Voucher;
+import org.programmers.springorder.voucher.repository.VoucherRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class CustomerService {
 
     private final CustomerRepository customerRepository;
+    private final VoucherRepository voucherRepository;
 
-    public CustomerService(CustomerRepository customerRepository) {
+    public CustomerService(CustomerRepository customerRepository, VoucherRepository voucherRepository) {
         this.customerRepository = customerRepository;
+        this.voucherRepository = voucherRepository;
     }
 
     public List<CustomerResponseDto> getBlackList() {
@@ -20,5 +26,15 @@ public class CustomerService {
                 .stream()
                 .map(CustomerResponseDto::of)
                 .toList();
+    }
+
+    public Customer findOwnerOfVoucher(UUID voucherId){
+        Voucher voucher = voucherRepository.findById(voucherId)
+                .orElseThrow(() -> new RuntimeException("찾으시는 voucher가 존재하지 않습니다."));
+        if(voucher.getCustomerId() == null) {
+            throw new RuntimeException("해당 voucher는 주인이 존재하지 않습니다.");
+        }
+        return customerRepository.findByID(voucher.getCustomerId())
+                .orElseThrow(() -> new RuntimeException("해당 고객을 찾을 수 없습니다."));
     }
 }
