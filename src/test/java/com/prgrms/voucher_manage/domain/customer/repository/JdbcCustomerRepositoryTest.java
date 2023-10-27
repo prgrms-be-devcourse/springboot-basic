02420c3a2f6cdbdf177d31c215ca84c41f998305
hs12 <loveanna1228@gmail.com>
2023-10-27 16:18:59 +0900

    [Feat] 회원 저장소 테스트 함수 추가

--- src/test/java/com/prgrms/voucher_manage/domain/customer/repository/JdbcCustomerRepositoryTest.java
+++ src/test/java/com/prgrms/voucher_manage/domain/customer/repository/JdbcCustomerRepositoryTest.java
@@ -12,6 +12,7 @@ import java.util.List;
 import static com.prgrms.voucher_manage.domain.customer.entity.CustomerType.BLACK;
 import static com.prgrms.voucher_manage.domain.customer.entity.CustomerType.NORMAL;
 import static org.assertj.core.api.Assertions.assertThat;
+
 @JdbcTest
 @Import(JdbcCustomerRepository.class)
 public class JdbcCustomerRepositoryTest {
@@ -32,48 +33,10 @@ public class JdbcCustomerRepositoryTest {
     }
 
     @Test
-    @DisplayName("회원을 이름으로 조회할 수 있다.")
-    public void jdbcCustomerRepository_findByName() {
-        //given
-        Customer customer = new Customer( "와", BLACK);
-        repository.save(customer);
-        //when
-        Customer foundCustomer = repository.findByName("와").orElse(null);
-
-        //then
-        assertThat(foundCustomer).isNotNull();
-        assertThat(foundCustomer.getName()).isEqualTo("와");
-    }
-
-//    @Test
-//    @DisplayName("회원 정보를 업데이트할 수 있다.")
-//    public void jdbcCustomerRepository_update(){
-//        //given
-//        Customer customer = new Customer("와", BLACK);
-//        repository.save(customer);
-//        Customer updatedCustomer = new Customer("와와", NORMAL);
-//
-//        //when
-//        repository.update(updatedCustomer);
-//        Customer foundCustomer = repository.findByName(customer.getName()).orElse(null);
-//        //then
-//        assert foundCustomer != null;
-//        assertThat(foundCustomer.getName()).isEqualTo("와와");
-//        assertThat(foundCustomer.getType()).isEqualTo(NORMAL);
-//
-//    }
-//
-//    private Customer saveCustomer() {
-//        Customer customer = new Customer(UUID.randomUUID(), "와", BLACK);
-//        repository.save(customer);
-//        return customer;
-//    }
-
-    @Test
     @DisplayName("회원 목록을 가져올 수 있다.")
-    public void jdbcCustomerRepository_findAll(){
+    public void jdbcCustomerRepository_findAll() {
         //given
-        Customer customer1 = new Customer( "야", BLACK);
+        Customer customer1 = new Customer("야", BLACK);
         Customer customer2 = new Customer("호", BLACK);
         repository.save(customer1);
         repository.save(customer2);
@@ -84,8 +47,35 @@ public class JdbcCustomerRepositoryTest {
     }
 
     @Test
+    @DisplayName("회원을 이름으로 조회할 수 있다.")
+    public void jdbcCustomerRepository_findByName() {
+        //given
+        Customer customer = new Customer("와", BLACK);
+        repository.save(customer);
+        //when
+        Customer foundCustomer = repository.findByName("와").orElse(null);
+
+        //then
+        assertThat(foundCustomer).isNotNull();
+        assertThat(foundCustomer.getName()).isEqualTo("와");
+    }
+
+    @Test
+    @DisplayName("회원 정보를 업데이트할 수 있다.")
+    public void jdbcCustomerRepository_update() {
+        //given
+        Customer customer = repository.save(new Customer("전", BLACK));
+        //when
+        Customer updatedCustomer = new Customer(customer.getId(), "후", customer.getType());
+        int update = repository.update(updatedCustomer);
+        //then
+        assertThat(update).isEqualTo(1);
+        assertThat(updatedCustomer.getName()).isEqualTo("후");
+    }
+
+    @Test
     @DisplayName("블랙 리스트를 가져올 수 있다.")
-    public void jdbcCustomerRepository_findBlackList(){
+    public void jdbcCustomerRepository_findBlackList() {
         //given
         Customer customer1 = new Customer("야", NORMAL);
         Customer customer2 = new Customer("호", BLACK);
