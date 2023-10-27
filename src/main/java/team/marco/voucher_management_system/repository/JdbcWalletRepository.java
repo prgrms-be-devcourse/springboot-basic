2a753ffe1baae0b65d2b25a3d4102419ef752388
ASak1104 <khw56184@gmail.com>
2023-10-27 21:23:45 +0900

    feat : JdbcWalletRepository 기능 구현

--- src/main/java/team/marco/voucher_management_system/repository/JdbcWalletRepository.java
+++ src/main/java/team/marco/voucher_management_system/repository/JdbcWalletRepository.java
@@ -1,29 +1,63 @@
 package team.marco.voucher_management_system.repository;
 
+import java.io.ByteArrayInputStream;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
+import team.marco.voucher_management_system.util.UUIDConverter;
 
 @Repository
 public class JdbcWalletRepository implements WalletRepository {
+    private final NamedParameterJdbcTemplate jdbcTemplate;
+
+    public JdbcWalletRepository(NamedParameterJdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
     @Override
     public int link(String customerId, String voucherId) {
-        return 0;
+        return jdbcTemplate.update(
+                "INSERT INTO wallet(customer_id, voucher_id)"
+                        + " VALUES (UUID_TO_BIN(:customerId), UUID_TO_BIN(:voucherId))",
+                walletToMap(customerId, voucherId));
     }
 
     @Override
     public int unlink(String customerId, String voucherId) {
-        return 0;
+        return jdbcTemplate.update(
+                "DELETE FROM wallet"
+                        + " WHERE (customer_id, voucher_id) = (UUID_TO_BIN(:customerId), UUID_TO_BIN(:voucherId))",
+                walletToMap(customerId, voucherId));
     }
 
     @Override
     public List<UUID> getVoucherIds(String customerId) {
-        return Collections.emptyList();
+        return jdbcTemplate.queryForList(
+                        "SELECT voucher_id FROM wallet"
+                                + " WHERE customer_id = UUID_TO_BIN(:customerId)",
+                        Collections.singletonMap("customerId", customerId), ByteArrayInputStream.class)
+                .stream()
+                .map(byteArray -> UUIDConverter.convert(byteArray.readAllBytes()))
+                .toList();
     }
 
     @Override
     public List<UUID> getCustomerIds(String voucherId) {
-        return Collections.emptyList();
+        return jdbcTemplate.queryForList(
+                        "SELECT customer_id FROM wallet"
+                                + " WHERE voucher_id = UUID_TO_BIN(:voucherId)",
+                        Collections.singletonMap("voucherId", voucherId), ByteArrayInputStream.class)
+                .stream()
+                .map(byteArray -> UUIDConverter.convert(byteArray.readAllBytes()))
+                .toList();
+    }
+
+    private Map<String, Object> walletToMap(String customerId, String voucherId) {
+        return Map.ofEntries(
+                Map.entry("customerId", customerId),
+                Map.entry("voucherId", voucherId));
     }
 }
