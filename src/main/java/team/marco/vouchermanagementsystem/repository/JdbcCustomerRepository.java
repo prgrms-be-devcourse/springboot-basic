109d85201fea06832ca9a6b4a72f4527c9673b7e
ASak1104 <khw56184@gmail.com>
2023-10-27 13:28:26 +0900

    fix : JdbcCustomerRepository 버그 수정
    오타로 인해 예외가 발생하는 버그 수정
    null 값을 저장하지 못하는 버그 수정
    와일드카드 쿼리 시 오류가 나는 버그 수정

--- src/main/java/team/marco/vouchermanagementsystem/repository/JdbcCustomerRepository.java
+++ src/main/java/team/marco/vouchermanagementsystem/repository/JdbcCustomerRepository.java
@@ -3,8 +3,10 @@ package team.marco.vouchermanagementsystem.repository;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Timestamp;
+import java.text.MessageFormat;
 import java.time.LocalDateTime;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
@@ -25,13 +27,15 @@ public class JdbcCustomerRepository {
         String name = resultSet.getString("name");
         String email = resultSet.getString("email");
         Timestamp lastLoginAt = resultSet.getTimestamp("last_login_at");
-        LocalDateTime createAt = resultSet.getTimestamp("create_at").toLocalDateTime();
+        LocalDateTime createAt = resultSet.getTimestamp("created_at").toLocalDateTime();
+
+        LocalDateTime localDateTimeLastLoginAt = null;
 
         if (!Objects.isNull(lastLoginAt)) {
-            return new Customer(id, name, email, lastLoginAt.toLocalDateTime(), createAt);
+            localDateTimeLastLoginAt = lastLoginAt.toLocalDateTime();
         }
 
-        return new Customer(id, name, email, null, createAt);
+        return new Customer(id, name, email, localDateTimeLastLoginAt, createAt);
     }
 
     private final NamedParameterJdbcTemplate jdbcTemplate;
@@ -42,8 +46,8 @@ public class JdbcCustomerRepository {
 
     public int create(Customer customer) {
         return jdbcTemplate.update(
-                "INSERT INTO customers(id, name, email, created_at)"
-                        + " VALUES (UUID_TO_BIN(:id), :name, :email, :cratedAt)",
+                "INSERT INTO customer(id, name, email, last_login_at, created_at)"
+                        + " VALUES (UUID_TO_BIN(:id), :name, :email, :lastLoginAt, :createdAt)",
                 customerToMap(customer));
     }
 
@@ -53,7 +57,7 @@ public class JdbcCustomerRepository {
 
     public int update(Customer customer) {
         return jdbcTemplate.update(
-                "UPDATE customers SET name = :name, email = :email, last_login_at = :lastLoginAt"
+                "UPDATE customer SET name = :name, email = :email, last_login_at = :lastLoginAt"
                         + " WHERE id = UUID_TO_BIN(:id)",
                 customerToMap(customer));
     }
@@ -71,25 +75,38 @@ public class JdbcCustomerRepository {
     public List<Customer> findByName(String name) {
         return jdbcTemplate.query(
                 "SELECT * FROM customer"
-                        + " WHERE name LIKE %:name%",
-                Collections.singletonMap("name", name),
+                        + " WHERE name LIKE :name",
+                Collections.singletonMap("name", withWildCards(name)),
                 customerRowMapper);
     }
 
     public List<Customer> findByEmail(String email) {
         return jdbcTemplate.query(
                 "SELECT * FROM customer"
-                        + " WHERE email LIKE %:email%",
-                Collections.singletonMap("email", email),
+                        + " WHERE email LIKE :email",
+                Collections.singletonMap("email", withWildCards(email)),
                 customerRowMapper);
     }
 
     private Map<String, Object> customerToMap(Customer customer) {
-        return Map.ofEntries(
-                Map.entry("id", customer.getId().toString().getBytes()),
-                Map.entry("name", customer.getName()),
-                Map.entry("email", customer.getEmail()),
-                Map.entry("lastLoginAt", customer.getLastLoginAt()),
-                Map.entry("createdAt", customer.getCreatedAt()));
+        Map<String, Object> customerFields = new HashMap<>() {
+            {
+                put("id", customer.getId().toString().getBytes());
+                put("name", customer.getName());
+                put("email", customer.getEmail());
+                put("lastLoginAt", null);
+                put("createdAt", Timestamp.valueOf(customer.getCreatedAt()));
+            }
+        };
+
+        if (!Objects.isNull(customer.getLastLoginAt())) {
+            customerFields.put("lastLoginAt", Timestamp.valueOf(customer.getLastLoginAt()));
+        }
+
+        return customerFields;
+    }
+
+    private String withWildCards(String word) {
+        return MessageFormat.format("%{0}%", word);
     }
 }
