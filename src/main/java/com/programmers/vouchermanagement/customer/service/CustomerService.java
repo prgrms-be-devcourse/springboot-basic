51ae27a0ee3ad0fd3168beda3f17ac933d84f298
Jay <injaesong0@gmail.com>
2023-10-27 13:55:24 +0900

    feat: implement create customer service

--- src/main/java/com/programmers/vouchermanagement/customer/service/CustomerService.java
+++ src/main/java/com/programmers/vouchermanagement/customer/service/CustomerService.java
@@ -2,12 +2,14 @@ package com.programmers.vouchermanagement.customer.service;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.stereotype.Service;
 
 import com.programmers.vouchermanagement.customer.domain.Customer;
 import com.programmers.vouchermanagement.customer.dto.CustomerResponse;
 import com.programmers.vouchermanagement.customer.repository.CustomerRepository;
+import com.programmers.vouchermanagement.util.Validator;
 
 @Service
 public class CustomerService {
@@ -27,5 +29,12 @@ public class CustomerService {
         return blacklist.stream()
                 .map(CustomerResponse::from)
                 .toList();
+    }
+
+    public CustomerResponse create(String name) {
+        Validator.validateCustomerName(name);
+        Customer customer = new Customer(UUID.randomUUID(), name);
+        customerRepository.save(customer);
+        return CustomerResponse.from(customer);
     }
 }
