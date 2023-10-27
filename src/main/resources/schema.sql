c76050789b73589f0d4b66875b63462013242395
letskuku <yujung9288@naver.com>
2023-10-28 02:15:28 +0900

    feat: customer, voucher 테이블 정의

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,15 @@
+USE voucher_management;
+
+DROP TABLE IF EXISTS customer;
+CREATE TABLE IF NOT EXISTS customer (
+    customer_id BINARY(16) PRIMARY KEY,
+    name VARCHAR(255),
+    customer_type ENUM('NORMAL', 'BLACK')
+);
+
+DROP TABLE IF EXISTS voucher;
+CREATE TABLE IF NOT EXISTS voucher (
+    voucher_id BINARY(16) PRIMARY KEY,
+    discount BIGINT,
+    voucher_type ENUM('FIXED', 'PERCENT')
+);
