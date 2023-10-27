2fb64997f65fa9eb3b94b91b8d269b09161b044f
Jay <injaesong0@gmail.com>
2023-10-27 11:50:51 +0900

    refactor: apply changes of the structure

--- src/test/java/com/programmers/vouchermanagement/consoleapp/menu/MenuHandlerTest.java
+++ src/test/java/com/programmers/vouchermanagement/consoleapp/menu/MenuHandlerTest.java
@@ -73,7 +73,7 @@ class MenuHandlerTest {
     }
 
     @Test
-    @DisplayName("바우처 생성 시 잘못된 타입을 입력하면 메뉴 실행을 실패하고, 시스템은 계속 된다.")
+    @DisplayName("바우처 생성 시 잘못된 타입을 입력하면 메뉴 실행을 실패하지만 시스템은 계속 된다.")
     void testHandleMenuFailed_IncorrectVoucherType() {
         //given
         final IllegalArgumentException exception = new IllegalArgumentException();
@@ -91,7 +91,7 @@ class MenuHandlerTest {
     }
 
     @Test
-    @DisplayName("바우처 생성 시 유효하지 않은 할인 값을 입력하면 메뉴 실행을 실패하고, 시스템은 계속 된다.")
+    @DisplayName("바우처 생성 시 유효하지 않은 할인 값을 입력하면 메뉴 실행을 실패하지만 시스템은 계속 된다.")
     void testHandleMenuFailed_InvalidDiscountValue() {
         //given
         final IllegalArgumentException exception = new IllegalArgumentException();
@@ -111,15 +111,12 @@ class MenuHandlerTest {
     }
 
     @Test
-    @DisplayName("바우처 생성 메뉴 실행을 성공하고, 시스템은 계속 된다.")
+    @DisplayName("바우처 생성 메뉴 실행을 성공한다.")
     void testHandleMenuSuccessful_VoucherCreated() {
         //given
         final CreateVoucherRequest request = new CreateVoucherRequest(new BigDecimal(10000), VoucherType.FIXED);
-        final Voucher voucher = new Voucher(UUID.randomUUID(), request.discountValue(), request.voucherType());
-        final VoucherResponse response = VoucherResponse.from(voucher);
         doReturn(Menu.CREATE).when(consoleManager).selectMenu();
         doReturn(request).when(consoleManager).instructCreate();
-        doReturn(response).when(voucherController).create(request);
 
         //when
         final boolean isRunning = menuHandler.handleMenu();
@@ -128,38 +125,14 @@ class MenuHandlerTest {
         assertThat(isRunning, is(true));
 
         //verify
-        verify(consoleManager).printCreateResult(response);
+        verify(voucherController).create(request);
     }
 
     @Test
-    @DisplayName("바우처 전체 조회 시 저장된 바우처가 없어도 실패하지 않는다.")
-    void testHandleMenuSuccessful_EmptyVoucherList() {
-        //given
-        final List<VoucherResponse> vouchers = Collections.emptyList();
-        doReturn(Menu.LIST).when(consoleManager).selectMenu();
-        doReturn(vouchers).when(voucherController).readAllVouchers();
-
-        //when
-        final boolean isRunning = menuHandler.handleMenu();
-
-        //then
-        assertThat(isRunning, is(true));
-
-        //verify
-        verify(consoleManager).printReadAllVouchers(vouchers);
-    }
-
-    @Test
-    @DisplayName("저장된 바우처가 있을 때 바우처 전체 조회를 성공한다.")
+    @DisplayName("바우처 전체 조회를 성공한다.")
     void testHandleMenuSuccessful_ReadAllVouchers() {
         //given
-        final Voucher firstVoucher = new Voucher(UUID.randomUUID(), new BigDecimal(10000), VoucherType.FIXED);
-        final Voucher secondVoucher = new Voucher(UUID.randomUUID(), new BigDecimal(40), VoucherType.PERCENT);
-        final VoucherResponse firstResponse = VoucherResponse.from(firstVoucher);
-        final VoucherResponse secondResponse = VoucherResponse.from(secondVoucher);
-        final List<VoucherResponse> vouchers = List.of(firstResponse, secondResponse);
         doReturn(Menu.LIST).when(consoleManager).selectMenu();
-        doReturn(vouchers).when(voucherController).readAllVouchers();
 
         //when
         final boolean isRunning = menuHandler.handleMenu();
@@ -168,38 +141,14 @@ class MenuHandlerTest {
         assertThat(isRunning, is(true));
 
         //verify
-        verify(consoleManager).printReadAllVouchers(vouchers);
+        verify(voucherController).readAllVouchers();
     }
 
     @Test
-    @DisplayName("블랙리스트 조회 시 저장된 고객이 없어도 실패하지 않는다.")
-    void testHandleMenuSuccessful_EmptyBlacklist() {
-        //given
-        final List<CustomerResponse> blacklist = Collections.emptyList();
-        doReturn(Menu.BLACKLIST).when(consoleManager).selectMenu();
-        doReturn(blacklist).when(customerController).readBlacklist();
-
-        //when
-        final boolean isRunning = menuHandler.handleMenu();
-
-        //then
-        assertThat(isRunning, is(true));
-
-        //verify
-        verify(consoleManager).printReadBlacklist(blacklist);
-    }
-
-    @Test
-    @DisplayName("저장된 고객이 있을 때 블랙리스트 조회를 성공한다.")
+    @DisplayName("블랙리스트 조회를 성공한다.")
     void testHandleMenuSuccessful_ReadBlacklist() {
         //given
-        final Customer firstCustomer = new Customer(UUID.randomUUID(), "test-user1");
-        final Customer secondCustomer = new Customer(UUID.randomUUID(), "test-user2");
-        final CustomerResponse firstResponse = CustomerResponse.from(firstCustomer);
-        final CustomerResponse secondResponse = CustomerResponse.from(secondCustomer);
-        final List<CustomerResponse> blacklist = List.of(firstResponse, secondResponse);
         doReturn(Menu.BLACKLIST).when(consoleManager).selectMenu();
-        doReturn(blacklist).when(customerController).readBlacklist();
 
         //when
         final boolean isRunning = menuHandler.handleMenu();
@@ -208,6 +157,6 @@ class MenuHandlerTest {
         assertThat(isRunning, is(true));
 
         //verify
-        verify(consoleManager).printReadBlacklist(blacklist);
+        verify(customerController).readBlacklist();
     }
 }
