269df29b59329c15d84b972bb4a0f3d5f6b676a3
shoeone96 <dlwnddnjs96@gmail.com>
2023-10-27 18:43:20 +0900

    feat: file repository 메서드 추가

--- src/main/java/org/programmers/springorder/voucher/repository/FileVoucherRepository.java
+++ src/main/java/org/programmers/springorder/voucher/repository/FileVoucherRepository.java
@@ -3,24 +3,25 @@ package org.programmers.springorder.voucher.repository;
 import org.programmers.springorder.console.Console;
 import org.programmers.springorder.consts.ErrorMessage;
 import org.programmers.springorder.customer.model.Customer;
+import org.programmers.springorder.utils.Properties;
 import org.programmers.springorder.voucher.model.Voucher;
 import org.programmers.springorder.voucher.model.VoucherType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Repository;
 
 import java.io.*;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Repository
 @Profile("dev")
-public class FileVoucherRepository implements VoucherRepository{
+public class FileVoucherRepository implements VoucherRepository {
 
     private static final Logger logger = LoggerFactory.getLogger(FileVoucherRepository.class);
-    @Value(("${voucherFilePath}"))
-    private String filePath;
     private final Console console;
 
     public FileVoucherRepository(Console console) {
@@ -29,7 +30,7 @@ public class FileVoucherRepository implements VoucherRepository{
 
     @Override
     public Voucher save(Voucher voucher) {
-        try(BufferedWriter bw = new BufferedWriter(new FileWriter(filePath, true))) {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(Properties.getVoucherFilePath(), true))) {
             String data = voucher.insertVoucherDataInFile();
             bw.write(data);
             bw.newLine();
@@ -42,17 +43,19 @@ public class FileVoucherRepository implements VoucherRepository{
 
     @Override
     public List<Voucher> findAll() {
-        List<Voucher> voucherList = new ArrayList<>(); // TODO: 동시성 처리 ReadLock, WriteLock synchronized x queue
-
-        try(BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+        List<Voucher> voucherList = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(Properties.getVoucherFilePath()))) {
             String line;
-            while((line = br.readLine()) != null) {
+            while ((line = br.readLine()) != null) {
                 String[] data = line.split(",");
                 UUID voucherId = UUID.fromString(data[0]);
                 long discountValue = Long.parseLong(data[1]);
                 VoucherType voucherType = VoucherType.valueOf(data[2]);
-
                 Voucher voucher = Voucher.toVoucher(voucherId, discountValue, voucherType);
+                if (data.length == 4) {
+                    UUID customerId = UUID.fromString(data[3]);
+                    voucher = Voucher.toVoucher(voucherId, discountValue, voucherType, customerId);
+                }
                 voucherList.add(voucher);
             }
         } catch (FileNotFoundException e) {
@@ -63,7 +66,7 @@ public class FileVoucherRepository implements VoucherRepository{
             console.printMessage(ErrorMessage.FILE_FAIL_LOADING_MESSAGE);
         }
 
-        return Collections.unmodifiableList(voucherList);
+        return voucherList;
     }
 
     @Override
@@ -72,20 +75,44 @@ public class FileVoucherRepository implements VoucherRepository{
                 .filter(voucher -> voucher.isSameId(voucherId))
                 .findFirst();
     }
+
     //TODO: 구현 필요 기능(jdbc 우선)
     @Override
     public Voucher updateVoucherOwner(Voucher voucher, Customer customer) {
+        List<Voucher> allVouchers = findAll();
+        int index = allVouchers.indexOf(voucher);
+        voucher.updateOwner(customer);
+        allVouchers.set(index, voucher);
+        writeAllVouchers(allVouchers);
         return voucher;
+    }
+
+    private void writeAllVouchers(List<Voucher> allVouchers) {
+        clear();
+        for (Voucher rewriteVoucher : allVouchers) {
+            save(rewriteVoucher);
+        }
     }
 
     @Override
     public List<Voucher> findAllByCustomerId(Customer customer) {
-        return null;
+        return findAll().stream()
+                .filter(voucher -> voucher.isSameCustomerId(customer.getCustomerId()))
+                .toList();
     }
 
     @Override
     public void deleteVoucher(Voucher voucher) {
+        List<Voucher> voucherList = findAll();
+        voucherList.remove(voucher);
+        writeAllVouchers(voucherList);
+    }
 
+    public void clear() {
+        try (FileOutputStream fos = new FileOutputStream(Properties.getVoucherFilePath(), false)) {
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
