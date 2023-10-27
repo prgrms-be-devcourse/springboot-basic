4e7e5a85167e2817c54ee5f81d0010bdef9e9d4a
EomYerim <djadpfa1002@naver.com>
2023-10-27 17:56:40 +0900

    refactor: 바우처 관련 메뉴 및 기능 추가 바우처 생성, 전체 조회, ID로 조회, 수정, 삭제 기능 추가

--- src/main/java/org/programmers/springorder/controller/VoucherController.java
+++ src/main/java/org/programmers/springorder/controller/VoucherController.java
@@ -4,12 +4,15 @@ import org.programmers.springorder.console.Console;
 import org.programmers.springorder.constant.Message;
 import org.programmers.springorder.dto.voucher.VoucherRequestDto;
 import org.programmers.springorder.service.VoucherService;
+import org.programmers.springorder.utils.ExceptionHandler;
+import org.programmers.springorder.utils.VoucherMenuType;
 import org.springframework.stereotype.Controller;
+
+import java.util.UUID;
 
 @Controller
 public class VoucherController {
     private final Console console;
-
     private final VoucherService voucherService;
 
     public VoucherController(Console console, VoucherService voucherService) {
@@ -17,13 +20,47 @@ public class VoucherController {
         this.voucherService = voucherService;
     }
 
-    public void getVoucherList() {
-        console.showList(voucherService.getAllVoucher());
+    public void run() {
+        boolean isRunning = true;
+
+        while (isRunning) {
+            VoucherMenuType menu = ExceptionHandler.input(Console::inputVoucherMenu);
+
+            switch (menu) {
+                case CREATE -> createVoucher();
+                case LIST -> getVoucherList();
+                case UPDATE -> ExceptionHandler.process(VoucherController::updateVoucher, this);
+                case DELETE -> ExceptionHandler.process(VoucherController::deleteVoucher, this);
+                case BACK -> {
+                    isRunning = false;
+                    console.printMessage(Message.BACK_TO_MENU_MESSAGE);
+                }
+            }
+        }
     }
 
-    public void createVoucher() {
+    private void createVoucher() {
         VoucherRequestDto request = console.inputVoucherInfo();
         voucherService.save(request);
         console.printMessage(Message.VOUCHER_REGISTERED);
     }
+
+    private void getVoucherList() {
+        console.showVoucherList(voucherService.getAllVoucher());
+    }
+
+    private void updateVoucher() {
+        UUID voucherId = UUID.fromString(console.inputVoucherId());
+        VoucherRequestDto request = console.inputVoucherInfo();
+        voucherService.updateVoucher(voucherId, request);
+        console.printMessage(Message.VOUCHER_UPDATED);
+    }
+
+    private void deleteVoucher() {
+        UUID voucherId = UUID.fromString(console.inputVoucherId());
+        voucherService.deleteVoucher(voucherId);
+        console.printMessage(Message.VOUCHER_DELETED);
+    }
+
+
 }
