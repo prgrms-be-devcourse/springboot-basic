6cbfcc320a02e9ed68e32de9e50e61d45997c19a
kkangh00n <kig2454@gmail.com>
2023-10-27 16:11:05 +0900

    feat: DBMemberRepository 기능 추가

--- src/main/java/com/weeklyMission/member/repository/DBMemberRepository.java
+++ src/main/java/com/weeklyMission/member/repository/DBMemberRepository.java
@@ -2,10 +2,16 @@ package com.weeklyMission.member.repository;
 
 import com.weeklyMission.member.domain.Member;
 import java.nio.ByteBuffer;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 import java.util.UUID;
 import javax.sql.DataSource;
 import org.springframework.context.annotation.Profile;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
@@ -23,17 +29,51 @@ public class DBMemberRepository implements MemberRepository{
     private static final RowMapper<Member> memberRowMapper = (resultSet, i) -> {
         UUID memberId = toUUID(resultSet.getBytes("member_id"));
         String name = resultSet.getString("name");
+        String email = resultSet.getString("email");
         Integer age = Integer.parseInt(resultSet.getString("age"));
-        String reason = resultSet.getString("reason");
 
-        return new Member(memberId, name, age, reason);
+        return new Member(memberId, name, email, age);
     };
 
+    private Map<String, Object> toParamMap(Member member){
+        Map<String, Object> map = new HashMap<>();
+        map.put("memberId", member.memberId().toString().getBytes());
+        map.put("name", member.name());
+        map.put("email", member.email());
+        map.put("age", member.age());
+        return map;
+    }
 
+    @Override
+    public Member save(Member member) {
+        jdbcTemplate.update(
+            "INSERT INTO members (member_id, name, age, reason) VALUES (UUID_TO_BIN(:memberId), :name, :age, :reason)",
+            toParamMap(member));
+        return member;
+    }
 
     @Override
     public List<Member> findAll() {
-        return jdbcTemplate.query("select * from member", memberRowMapper);
+        return jdbcTemplate.query("select * from members", memberRowMapper);
+    }
+
+    @Override
+    public Optional<Member> findById(UUID id) {
+        Member member;
+        try{
+            member = jdbcTemplate.queryForObject("select * from members where member_id = UUID_TO_BIN(:memberId)",
+                Collections.singletonMap("memberId", id.toString().getBytes()), memberRowMapper);
+        } catch (EmptyResultDataAccessException de){
+            throw new NoSuchElementException("존재하지 않는 id 입니다.", de);
+        }
+        return Optional.ofNullable(member);
+    }
+
+    @Override
+    public void deleteById(UUID id) {
+        findById(id);
+        jdbcTemplate.update("delete from members where member_id = UUID_TO_BIN(:memberId)",
+            Collections.singletonMap("voucherId", id.toString().getBytes()));
     }
 
     static UUID toUUID(byte[] bytes){
--- src/main/java/com/weeklyMission/member/repository/MemberRepository.java
+++ src/main/java/com/weeklyMission/member/repository/MemberRepository.java
@@ -1,10 +1,17 @@
 package com.weeklyMission.member.repository;
 
 import com.weeklyMission.member.domain.Member;
+import com.weeklyMission.voucher.domain.Voucher;
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 public interface MemberRepository {
     Member save(Member member);
 
     List<Member> findAll();
+
+    Optional<Member> findById(UUID id);
+
+    void deleteById(UUID id);
 }
