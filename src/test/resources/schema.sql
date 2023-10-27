c96f792c9eab039bbe95c27baea7c28d151344f4
marooo326 <ktnqkr01@gmail.com>
2023-10-27 17:35:17 +0900

    docs: 테이블 create schema 수정

--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -1,14 +1,24 @@
-CREATE TABLE customers
+CREATE TABLE customer
 (
-    customer_id   BINARY(16) PRIMARY KEY,
-    name          varchar(20) NOT NULL,
-    email         varchar(50) NOT NULL,
-    last_login_at datetime(6)          DEFAULT NULL,
-    created_at    datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
-    is_banned     boolean     NOT NULL DEFAULT FALSE,
-    CONSTRAINT unq_user_email UNIQUE (email)
+    id         BINARY(16) PRIMARY KEY,
+    name       varchar(20) NOT NULL UNIQUE,
+    created_at datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+    is_banned  boolean     NOT NULL DEFAULT FALSE,
+    CONSTRAINT unq_user_name UNIQUE (name)
 );
 
-INSERT INTO customers(customer_id,name,email) VALUES (UUID_TO_BIN(UUID()),'test1','test1@gmail.com');
-INSERT INTO customers(customer_id,name,email) VALUES (UUID_TO_BIN(UUID()),'test2','test2@gmail.com');
-INSERT INTO customers(customer_id,name,email) VALUES (UUID_TO_BIN(UUID()),'test3','test3@gmail.com');
+CREATE TABLE voucher
+(
+    id              BINARY(16) PRIMARY KEY,
+    name            varchar(20) NOT NULL UNIQUE,
+    discount_amount float       NOT NULL DEFAULT FALSE,
+    created_at      datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+    CONSTRAINT unq_voucher_name UNIQUE (name)
+);
+
+CREATE TABLE wallet
+(
+    id          BINARY(16) PRIMARY KEY,
+    customer_id BINARY(16) NOT NULL,
+    voucher_id  BINARY(16) NOT NULL
+);
