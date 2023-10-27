d70be0526d9174050b1b55568d871f7aa70ab122
ASak1104 <khw56184@gmail.com>
2023-10-27 18:27:39 +0900

    test : BlacklistUser validation 및 테스트 코드 추가

--- src/main/java/team/marco/vouchermanagementsystem/model/BlacklistUser.java
+++ src/main/java/team/marco/vouchermanagementsystem/model/BlacklistUser.java
@@ -9,11 +9,19 @@ public class BlacklistUser {
     private final String name;
 
     public BlacklistUser(UUID id, String name) {
+        validateName(name);
+
         this.id = id;
         this.name = name;
     }
 
     public String getInfo() {
         return format("id: {0}, 고객명: {1} ", id, name);
+    }
+
+    private void validateName(String name) {
+        if (name.isBlank()) {
+            throw new IllegalArgumentException("이름은 빈 문자열 일 수 없습니다.");
+        }
     }
 }
