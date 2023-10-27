0a0aad1fff4b44c28bd102acf12db8f03f7a58cd
Jay <injaesong0@gmail.com>
2023-10-27 20:17:03 +0900

    test: succeed in finding customer by voucher id

--- src/test/java/com/programmers/vouchermanagement/customer/service/CustomerServiceTest.java
+++ src/test/java/com/programmers/vouchermanagement/customer/service/CustomerServiceTest.java
@@ -3,12 +3,16 @@ package com.programmers.vouchermanagement.customer.service;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.samePropertyValuesAs;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
 
+import java.math.BigDecimal;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -19,15 +23,20 @@ import com.programmers.vouchermanagement.customer.domain.Customer;
 import com.programmers.vouchermanagement.customer.domain.CustomerType;
 import com.programmers.vouchermanagement.customer.dto.CustomerResponse;
 import com.programmers.vouchermanagement.customer.repository.CustomerRepository;
+import com.programmers.vouchermanagement.voucher.domain.Voucher;
+import com.programmers.vouchermanagement.voucher.domain.VoucherType;
+import com.programmers.vouchermanagement.voucher.repository.VoucherRepository;
 
 class CustomerServiceTest {
     CustomerRepository customerRepository;
+    VoucherRepository voucherRepository;
     CustomerService customerService;
 
     @BeforeEach
     void setUp() {
-        customerRepository = mock();
-        customerService = new CustomerService(customerRepository);
+        voucherRepository = mock(VoucherRepository.class);
+        customerRepository = mock(CustomerRepository.class);
+        customerService = new CustomerService(customerRepository, voucherRepository);
     }
 
     @Test
@@ -62,5 +71,21 @@ class CustomerServiceTest {
 
         //verify
         verify(customerRepository).findBlackCustomers();
+    }
+
+    @Test
+    @DisplayName("특정 바우처를 보유한 고객 조회를 성공한다.")
+    void testFindByVoucherId() {
+        //given
+        final Voucher voucher = new Voucher(UUID.randomUUID(), new BigDecimal(10000), VoucherType.FIXED, UUID.randomUUID());
+        final Customer customer = new Customer(voucher.getCustomerId(), "test-customer");
+        doReturn(Optional.of(voucher)).when(voucherRepository).findById(any(UUID.class));
+        doReturn(Optional.of(customer)).when(customerRepository).findById(any(UUID.class));
+
+        //when
+        CustomerResponse customerResponse = customerService.findByVoucherId(UUID.randomUUID());
+
+        //then
+        assertThat(customerResponse, samePropertyValuesAs(CustomerResponse.from(customer)));
     }
 }
