055615b4f411d40a741143379ac50379f2f6596d
JeongeunChoi <77786996+JeongeunChoi@users.noreply.github.com>
2023-10-28 23:11:00 +0900

    feat: CustomerRepository에 CRUD 함수 선언

--- src/main/java/org/prgms/kdtspringweek1/customer/repository/CustomerRepository.java
+++ src/main/java/org/prgms/kdtspringweek1/customer/repository/CustomerRepository.java
@@ -3,7 +3,22 @@ package org.prgms.kdtspringweek1.customer.repository;
 import org.prgms.kdtspringweek1.customer.entity.Customer;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 public interface CustomerRepository {
+
+    Customer save(Customer customer);
+
+    List<Customer> findAllCustomers();
+
+    Optional<Customer> findById(UUID customerId);
+
+    Customer update(Customer customer);
+
+    void deleteAll();
+
+    Optional<Customer> deleteById(UUID customerId);
+
     List<Customer> findAllBlackConsumer();
 }
