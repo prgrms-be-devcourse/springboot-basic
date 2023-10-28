122592030537052a7d3210c1ca70b9541e47c2d0
JeongeunChoi <77786996+JeongeunChoi@users.noreply.github.com>
2023-10-28 23:03:07 +0900

    feat: VoucherRepository에 CRUD 함수 선언

--- src/main/java/org/prgms/kdtspringweek1/voucher/repository/VoucherRepository.java
+++ src/main/java/org/prgms/kdtspringweek1/voucher/repository/VoucherRepository.java
@@ -3,9 +3,19 @@ package org.prgms.kdtspringweek1.voucher.repository;
 import org.prgms.kdtspringweek1.voucher.entity.Voucher;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 public interface VoucherRepository {
     Voucher save(Voucher voucher);
 
     List<Voucher> findAllVouchers();
+
+    Optional<Voucher> findById(UUID voucherId);
+
+    Voucher update(Voucher voucher);
+
+    void deleteAll();
+
+    Optional<Voucher> deleteById(UUID voucherId);
 }
