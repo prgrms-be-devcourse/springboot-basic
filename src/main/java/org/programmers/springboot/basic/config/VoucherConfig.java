7b0fee345386e1fb1ba6a77523bde27b9902b661
JIN-076 <andy230@naver.com>
2023-10-29 03:58:09 +0900

    feature : Discount 유효성 검사를 위한 validateHandler 클래스 추가

--- src/main/java/org/programmers/springboot/basic/config/VoucherConfig.java
+++ src/main/java/org/programmers/springboot/basic/config/VoucherConfig.java
@@ -1,64 +1,39 @@
 package org.programmers.springboot.basic.config;
 
-import org.programmers.springboot.basic.domain.voucher.dto.CsvVoucherDto;
-import org.programmers.springboot.basic.domain.voucher.dto.VoucherRequestDto;
-import org.programmers.springboot.basic.domain.voucher.dto.VoucherResponseDto;
-import org.programmers.springboot.basic.domain.voucher.entity.FixedAmountVoucher;
-import org.programmers.springboot.basic.domain.voucher.entity.PercentDiscountVoucher;
-import org.programmers.springboot.basic.domain.voucher.entity.Voucher;
 import org.programmers.springboot.basic.domain.voucher.entity.VoucherType;
 import org.programmers.springboot.basic.domain.voucher.service.validate.FixedAmountVoucherValidator;
 import org.programmers.springboot.basic.domain.voucher.service.validate.PercentDiscountVoucherValidator;
 import org.programmers.springboot.basic.domain.voucher.service.validate.ValidateHandler;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.UUID;
 
-@Component
+@Configuration
+@ComponentScan(basePackages = {
+        "org.programmers.springboot.basic",
+        "org.programmers.springboot.basic.domain.voucher.mapper",
+        "org.programmers.springboot.basic.domain.customer.mapper"
+})
 public class VoucherConfig {
 
-    private final FixedAmountVoucherValidator fixedAmountVoucherValidator;
-    private final PercentDiscountVoucherValidator percentDiscountVoucherValidator;
-
-    @Autowired
-    public VoucherConfig(FixedAmountVoucherValidator fixedAmountVoucherValidator, PercentDiscountVoucherValidator percentDiscountVoucherValidator) {
-        this.fixedAmountVoucherValidator = fixedAmountVoucherValidator;
-        this.percentDiscountVoucherValidator = percentDiscountVoucherValidator;
+    @Bean
+    public ValidateHandler PercentVoucherValidator()  {
+        return new PercentDiscountVoucherValidator();
     }
 
-    public Voucher getFixedAmountVoucher(VoucherRequestDto requestDto) {
-        return new FixedAmountVoucher(UUID.randomUUID(), requestDto.discount(), requestDto.voucherType());
-    }
-
-    public Voucher getFixedAmountVoucher(CsvVoucherDto csvVoucherDto) {
-        return new FixedAmountVoucher(csvVoucherDto.voucherId(), csvVoucherDto.discount(), csvVoucherDto.voucherType());
-    }
-
-    public Voucher getPercentDiscountVoucher(VoucherRequestDto requestDto) {
-        return new PercentDiscountVoucher(UUID.randomUUID(), requestDto.discount(), requestDto.voucherType());
-    }
-
-    public Voucher getPercentDiscountVoucher(CsvVoucherDto csvVoucherDto) {
-        return new PercentDiscountVoucher(csvVoucherDto.voucherId(), csvVoucherDto.discount(), csvVoucherDto.voucherType());
-    }
-
-    public VoucherResponseDto getVoucherResponseDto(Voucher voucher) {
-        return new VoucherResponseDto(voucher.getVoucherId(), voucher.getVoucherType(), voucher.getDiscount());
-    }
-
-    public CsvVoucherDto getCsvVoucherDto(UUID voucherId, VoucherType voucherType, Long discount) {
-        return new CsvVoucherDto(voucherId, voucherType, discount);
+    @Bean
+    public ValidateHandler FixedVoucherValidator() {
+        return new FixedAmountVoucherValidator();
     }
 
     @Bean
     public Map<VoucherType, ValidateHandler> validateHandlers() {
         Map<VoucherType, ValidateHandler> handler = new HashMap<>();
-        handler.put(VoucherType.FIXED, this.fixedAmountVoucherValidator);
-        handler.put(VoucherType.PERCENT, this.percentDiscountVoucherValidator);
+        handler.put(VoucherType.FIXED, FixedVoucherValidator());
+        handler.put(VoucherType.PERCENT, PercentVoucherValidator());
         return handler;
     }
 }
