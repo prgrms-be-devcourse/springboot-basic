69b00ca7c85d0dd09de2a25d90d06a6617f78ab5
happyjamy <78072370+happyjamy@users.noreply.github.com>
2023-10-29 05:36:08 +0900

    [add] 사용자 요청 처리 함수 추가

--- src/main/java/com/programmers/springbootbasic/infrastructure/IO/ConsoleInteractionAggregator.java
+++ src/main/java/com/programmers/springbootbasic/infrastructure/IO/ConsoleInteractionAggregator.java
@@ -1,7 +1,10 @@
 package com.programmers.springbootbasic.infrastructure.IO;
 
+import com.programmers.springbootbasic.domain.userVoucherWallet.presentation.dto.CreateUserVoucherWalletRequest;
 import com.programmers.springbootbasic.domain.voucher.presentation.dto.CreateVoucherRequest;
+import com.programmers.springbootbasic.mediator.dto.UpdateVoucherMediatorRequest;
 import com.programmers.springbootbasic.util.Messages;
+import java.util.UUID;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -20,8 +23,35 @@ public class ConsoleInteractionAggregator {
     public CreateVoucherRequest collectVoucherInput() {
         String title = console.collectStringInput(Messages.VOUCHER_REGISTER_TYPE.getMessage());
         int amount = console.collectIntegerInput(Messages.VOUCHER_REGISTER_AMOUNT.getMessage());
-        return CreateVoucherRequest.from(title, amount);
+        return CreateVoucherRequest.of(title, amount);
     }
+
+    //TODO: 메세지 어찌할지
+    public Long collectIdInput() {
+        return console.collectLongInput(Messages.INPUT_ID.getMessage());
+    }
+
+    public UUID collectUUIDInput() {
+        return console.collectUUIDInput(Messages.INPUT_ID.getMessage());
+    }
+
+    public UpdateVoucherMediatorRequest collectUpdateVoucherInput() {
+        UUID id = console.collectUUIDInput(Messages.INPUT_ID.getMessage());
+        String voucherType = console.collectStringInput(Messages.VOUCHER_UPDATE_TYPE.getMessage());
+        int amount = console.collectIntegerInput(Messages.VOUCHER_UPDATE_AMOUNT.getMessage());
+        return UpdateVoucherMediatorRequest.of(id, voucherType, amount);
+    }
+
+    public CreateUserVoucherWalletRequest collectUserVoucherWalletInput() {
+        String userNickname = console.collectStringInput(Messages.INPUT_NICKNAME.getMessage());
+        UUID voucherId = console.collectUUIDInput(Messages.INPUT_ID.getMessage());
+        return CreateUserVoucherWalletRequest.of(userNickname, voucherId);
+    }
+
+    public String collectNicknameInput() {
+        return console.collectStringInput(Messages.INPUT_NICKNAME.getMessage());
+    }
+
 
     public void displayMessage(String message) {
         console.print(message);
