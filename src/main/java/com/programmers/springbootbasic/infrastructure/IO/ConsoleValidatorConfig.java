6fe1726b73be1776e48d16d8a6635334b593d6b1
happyjamy <78072370+happyjamy@users.noreply.github.com>
2023-10-29 05:35:44 +0900

    [add] Long, UUID input 유효성 검사기 등록

--- src/main/java/com/programmers/springbootbasic/infrastructure/IO/ConsoleValidatorConfig.java
+++ src/main/java/com/programmers/springbootbasic/infrastructure/IO/ConsoleValidatorConfig.java
@@ -1,6 +1,7 @@
 package com.programmers.springbootbasic.infrastructure.IO;
 
 import com.programmers.springbootbasic.presentation.validation.InputValidator;
+import java.util.UUID;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -22,6 +23,22 @@ public class ConsoleValidatorConfig {
         return integerValidator;
     }
 
+    @Bean
+    public InputValidator<String> getLongValidator() {
+        InputValidator<String> longValidator = new InputValidator<>();
+        longValidator.addValidator(this::validateString);
+        longValidator.addValidator(this::validateInteger);
+        return longValidator;
+    }
+
+    @Bean
+    public InputValidator<String> getUUIDValidator() {
+        InputValidator<String> uuidValidator = new InputValidator<>();
+        uuidValidator.addValidator(this::validateString);
+        uuidValidator.addValidator(this::validateUUID);
+        return uuidValidator;
+    }
+
     private String validateString(String input) {
         if (input == null || input.isEmpty()) {
             return "입력 값이 없습니다.";
@@ -35,6 +52,24 @@ public class ConsoleValidatorConfig {
             return null;
         } catch (NumberFormatException e) {
             return "숫자가 아닙니다.";
+        }
+    }
+
+    private String validateLong(String input) {
+        try {
+            Long.parseLong(input);
+            return null;
+        } catch (NumberFormatException e) {
+            return "Long 형태의 숫자가 아닙니다.";
+        }
+    }
+
+    private String validateUUID(String input) {
+        try {
+            UUID.fromString(input);
+            return null;
+        } catch (IllegalArgumentException e) {
+            return "UUID 형태의 문자가 아닙니다.";
         }
     }
 
