3740da008808e9a249c7cd91e5065d33add5142f
JIN-076 <andy230@naver.com>
2023-10-29 04:02:30 +0900

    feature : Voucher 관련 request, responseDto 클래스 추가

--- src/main/java/org/programmers/springboot/basic/domain/voucher/dto/VoucherRequestDto.java
+++ src/main/java/org/programmers/springboot/basic/domain/voucher/dto/VoucherRequestDto.java
@@ -1,6 +1,16 @@
 package org.programmers.springboot.basic.domain.voucher.dto;
 
+import lombok.Builder;
+import lombok.Getter;
 import org.programmers.springboot.basic.domain.voucher.entity.VoucherType;
 
-public record VoucherRequestDto(VoucherType voucherType, Long discount) {
+import java.util.UUID;
+
+@Builder
+@Getter
+public class VoucherRequestDto {
+
+    private final UUID voucherId;
+    private final VoucherType voucherType;
+    private final Long discount;
 }
