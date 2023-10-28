ca3a2b7d21b93bf6e85eb92910e8ace0961d453c
JeongeunChoi <77786996+JeongeunChoi@users.noreply.github.com>
2023-10-29 05:01:21 +0900

    feat: WalletService 구현

---
+++ src/main/java/org/prgms/kdtspringweek1/wallet/service/WalletService.java
@@ -0,0 +1,41 @@
+package org.prgms.kdtspringweek1.wallet.service;
+
+import org.prgms.kdtspringweek1.controller.dto.customerDto.FindCustomerResponseDto;
+import org.prgms.kdtspringweek1.controller.dto.voucherDto.FindVoucherResponseDto;
+import org.prgms.kdtspringweek1.wallet.entity.Wallet;
+import org.prgms.kdtspringweek1.wallet.repository.WalletRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Service
+public class WalletService {
+
+    private final WalletRepository walletRepository;
+
+    public WalletService(WalletRepository walletRepository) {
+        this.walletRepository = walletRepository;
+    }
+
+    public Wallet registerWallet(Wallet wallet) {
+        return walletRepository.save(wallet);
+    }
+
+    public List<FindVoucherResponseDto> searchAllVouchersByCustomerId(UUID customerId) {
+        return walletRepository.findAllVouchersByCustomerId(customerId).stream()
+                .map(FindVoucherResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    public void deleteWalletByVoucherIdAndCustomerId(UUID voucherId, UUID customerId) {
+        walletRepository.deleteByVoucherIdAndCustomerId(voucherId, customerId);
+    }
+
+    public List<FindCustomerResponseDto> searchAllCustomersByVoucherId(UUID voucherId) {
+        return walletRepository.findAllCustomersByVoucherId(voucherId).stream()
+                .map(FindCustomerResponseDto::new)
+                .collect(Collectors.toList());
+    }
+}
