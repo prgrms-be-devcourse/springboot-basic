c23126ec0916d71dd749fc19f23130b1174dbc30
JeongeunChoi <77786996+JeongeunChoi@users.noreply.github.com>
2023-10-28 23:29:23 +0900

    feat: CustomerService CRUD 접근 가능하도록 구현

--- src/main/java/org/prgms/kdtspringweek1/customer/CustomerService.java
+++ src/main/java/org/prgms/kdtspringweek1/customer/CustomerService.java
@@ -1,10 +1,13 @@
 package org.prgms.kdtspringweek1.customer;
 
 import org.prgms.kdtspringweek1.controller.dto.FindCustomerResponseDto;
+import org.prgms.kdtspringweek1.customer.entity.Customer;
 import org.prgms.kdtspringweek1.customer.repository.CustomerRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 @Service
@@ -16,9 +19,36 @@ public class CustomerService {
         this.customerRepository = customerRepository;
     }
 
+    public Customer registerCustomer(Customer customer) {
+        return customerRepository.save(customer);
+    }
+
     public List<FindCustomerResponseDto> searchAllBlackCustomers() {
         return customerRepository.findAllBlackConsumer().stream()
                 .map(FindCustomerResponseDto::new)
                 .collect(Collectors.toList());
     }
+
+    public List<FindCustomerResponseDto> searchAllCustomers() {
+        return customerRepository.findAllCustomers().stream()
+                .map(FindCustomerResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    public Optional<FindCustomerResponseDto> searchCustomerById(UUID customerId) {
+        return customerRepository.findById(customerId).map(FindCustomerResponseDto::new);
+    }
+
+    public Customer update(Customer customer) {
+        return customerRepository.update(customer);
+    }
+
+    public void deleteAllCustomers() {
+        customerRepository.deleteAll();
+    }
+
+    public Optional<Customer> deleteCustomerById(UUID customerId) {
+        return customerRepository.deleteById(customerId);
+    }
+
 }
