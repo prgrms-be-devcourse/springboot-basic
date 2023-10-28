baaaa735acd01ab42db4d7a7d5b031d959490590
park0jae <okvv26@naver.com>
2023-10-28 19:07:43 +0900

    feat : UuidConverter 유틸 클래스에 private 생성자 추가

--- src/main/java/com/zerozae/voucher/util/UuidConverter.java
+++ src/main/java/com/zerozae/voucher/util/UuidConverter.java
@@ -4,6 +4,10 @@ import java.nio.ByteBuffer;
 import java.util.UUID;
 
 public class UuidConverter {
+
+    private UuidConverter() {
+    }
+
     public static UUID toUUID(byte[] bytes) {
         ByteBuffer byteBuffer = ByteBuffer.wrap(bytes);
         return new UUID(byteBuffer.getLong(), byteBuffer.getLong());
