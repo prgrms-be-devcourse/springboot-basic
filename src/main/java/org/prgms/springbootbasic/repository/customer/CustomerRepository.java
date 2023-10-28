124ab3cbe4a1c470173541c40ae35846747da919
ZZAMBAs <rksidksrksi@naver.com>
2023-10-29 01:07:02 +0900

    Feat: CustomerRepository에 CRUD 기능을 전부 추가한다.

--- src/main/java/org/prgms/springbootbasic/repository/customer/CustomerRepository.java
+++ src/main/java/org/prgms/springbootbasic/repository/customer/CustomerRepository.java
@@ -3,7 +3,14 @@ package org.prgms.springbootbasic.repository.customer;
 import org.prgms.springbootbasic.domain.customer.Customer;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 public interface CustomerRepository {
+    Customer save(Customer customer);
+    Optional<Customer> findById(UUID customerId);
+    Optional<Customer> findByEmail(String email);
     List<Customer> findBlackAll();
+    Optional<Customer> deleteById(UUID customerId);
+    void deleteAll();
 }
