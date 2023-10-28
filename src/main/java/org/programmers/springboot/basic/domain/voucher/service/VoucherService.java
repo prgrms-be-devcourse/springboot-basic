a4d2098686ea4cae54e0224aa8a06d65d421b0e3
JIN-076 <andy230@naver.com>
2023-10-29 04:02:47 +0900

    feature : Voucher 관련 Service 클래스 추가

--- src/main/java/org/programmers/springboot/basic/domain/voucher/service/VoucherService.java
+++ src/main/java/org/programmers/springboot/basic/domain/voucher/service/VoucherService.java
@@ -5,65 +5,110 @@ import org.programmers.springboot.basic.domain.voucher.dto.VoucherRequestDto;
 import org.programmers.springboot.basic.domain.voucher.dto.VoucherResponseDto;
 import org.programmers.springboot.basic.domain.voucher.entity.Voucher;
 import org.programmers.springboot.basic.domain.voucher.entity.VoucherType;
+import org.programmers.springboot.basic.domain.voucher.exception.DuplicateVoucherException;
 import org.programmers.springboot.basic.domain.voucher.exception.VoucherNotFoundException;
 import org.programmers.springboot.basic.domain.voucher.exception.VoucherValidatorNotFoundException;
-import org.programmers.springboot.basic.domain.voucher.mapper.VoucherMapper;
+import org.programmers.springboot.basic.domain.voucher.mapper.VoucherEntityMapper;
+import org.programmers.springboot.basic.util.generator.UUIDGenerator;
 import org.programmers.springboot.basic.domain.voucher.repository.VoucherRepository;
 import org.programmers.springboot.basic.domain.voucher.service.validate.ValidateHandler;
-import org.programmers.springboot.basic.util.exception.CSVFileIOFailureException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
+import java.util.Optional;
+import java.util.UUID;
 
 @Slf4j
 @Service
 public class VoucherService {
 
-    private final Map<VoucherType, ValidateHandler> validateHandlers;
-    private final VoucherMapper voucherMapper;
     private final VoucherRepository voucherRepository;
+    private final Map<VoucherType, ValidateHandler> validateHandlers;
+    private final UUIDGenerator uuidGenerator;
 
-    public VoucherService(Map<VoucherType, ValidateHandler> validateHandlers, VoucherMapper voucherMapper, VoucherRepository voucherRepository) {
-        this.validateHandlers = validateHandlers;
-        this.voucherMapper = voucherMapper;
+    @Autowired
+    public VoucherService(VoucherRepository voucherRepository, Map<VoucherType, ValidateHandler> validateHandlers, UUIDGenerator uuidGenerator) {
         this.voucherRepository = voucherRepository;
+        this.validateHandlers = validateHandlers;
+        this.uuidGenerator = uuidGenerator;
     }
 
+    @Transactional
     public void create(VoucherRequestDto voucherRequestDto) {
 
-        Long discount = voucherRequestDto.discount();
-        VoucherType voucherType = voucherRequestDto.voucherType();
+        Long discount = voucherRequestDto.getDiscount();
+        VoucherType voucherType = voucherRequestDto.getVoucherType();
 
-        ValidateHandler handler = validateHandlers.get(voucherType);
-
-        if (handler == null) {
-            throw new VoucherValidatorNotFoundException();
-        }
+        ValidateHandler handler;
+        Optional.ofNullable(handler = validateHandlers.get(voucherType))
+                .orElseThrow(VoucherValidatorNotFoundException::new);
 
         handler.validate(discount);
-        Voucher voucher = voucherMapper.mapToVoucher(voucherRequestDto);
-        this.voucherRepository.save(voucher);
+        Voucher voucher = VoucherEntityMapper.INSTANCE.mapToVoucherWithGenerator(voucherRequestDto, uuidGenerator);
+
+        if (voucherRepository.findByTypeNDiscount(voucherType, discount).isPresent()) {
+            log.warn("voucher of voucherType '{}' and discount '{}' is already exists", voucherType, discount);
+            throw new DuplicateVoucherException();
+        }
+        add(voucher);
     }
 
     public List<VoucherResponseDto> findAll() {
 
-        List<Voucher> vouchers;
-
-        try {
-            vouchers = voucherRepository.findAll();
-        } catch (CSVFileIOFailureException e) {
-            log.error(e.toString());
-            throw new VoucherNotFoundException();
-        }
-
-        if (vouchers.isEmpty()) {
-            throw new VoucherNotFoundException();
-        }
-
+        List<Voucher> vouchers = getAll();
         return vouchers.stream()
-                .map(voucherMapper::mapToVoucherDto)
-                .collect(Collectors.toList());
+                .map(VoucherEntityMapper.INSTANCE::entityToDto)
+                .toList();
+    }
+
+    public VoucherResponseDto findById(VoucherRequestDto voucherRequestDto) {
+
+        UUID voucherId = voucherRequestDto.getVoucherId();
+        Voucher voucher = get(voucherId);
+        return VoucherEntityMapper.INSTANCE.entityToDto(voucher);
+    }
+
+    @Transactional
+    public void deleteVoucher(VoucherRequestDto voucherRequestDto) {
+        UUID voucherId = voucherRequestDto.getVoucherId();
+        delete(voucherId);
+    }
+
+    @Transactional
+    public void updateVoucher(VoucherRequestDto voucherRequestDto) {
+        Voucher voucher = VoucherEntityMapper.INSTANCE.mapToVoucher(voucherRequestDto);
+        update(voucher);
+    }
+
+    private void add(Voucher voucher) {
+        voucherRepository.add(voucher);
+    }
+
+    private Voucher get(UUID voucherId) {
+        return voucherRepository.get(voucherId)
+                .orElseThrow(() -> {
+                    log.warn("No voucher found for voucherId: {}" , voucherId);
+                    return new VoucherNotFoundException();
+                });
+    }
+
+    private List<Voucher> getAll() {
+        return voucherRepository.getAll();
+    }
+
+    private void delete(UUID voucherId) {
+        voucherRepository.delete(voucherId);
+    }
+
+    private void update(Voucher voucher) {
+        voucherRepository.update(voucher);
+    }
+
+    @Transactional
+    public void deleteAll() {
+        voucherRepository.deleteAll();
     }
 }
