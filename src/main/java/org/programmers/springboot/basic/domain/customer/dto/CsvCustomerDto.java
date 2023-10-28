5f6848368de5f71e7cd67c1212bcaaa7b316d653
JIN-076 <andy230@naver.com>
2023-10-29 04:38:21 +0900

    feature : 기존 코드 삭제

--- src/main/java/org/programmers/springboot/basic/domain/customer/dto/CsvCustomerDto.java
+++
@@ -1,8 +0,0 @@
-package org.programmers.springboot.basic.domain.customer.dto;
-
-import org.programmers.springboot.basic.domain.customer.entity.CustomerType;
-
-import java.util.UUID;
-
-public record CsvCustomerDto(UUID customerId, String name, CustomerType customerType) {
-}
