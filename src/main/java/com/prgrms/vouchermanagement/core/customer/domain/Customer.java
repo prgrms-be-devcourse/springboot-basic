b7851adc2cf567021a23fd4a40ec77ad30670f44
수지니 <sssssjin99@naver.com>
2023-10-29 23:04:37 +0900

    [feat]: Customer 도메인 추가

---
+++ src/main/java/com/prgrms/vouchermanagement/core/customer/domain/Customer.java
@@ -0,0 +1,41 @@
+package com.prgrms.vouchermanagement.core.customer.domain;
+
+import java.util.Objects;
+
+public class Customer {
+
+    private final String id;
+    private String name;
+    private final String email;
+
+    public Customer(String id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Customer customer = (Customer) o;
+        return Objects.equals(id, customer.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
