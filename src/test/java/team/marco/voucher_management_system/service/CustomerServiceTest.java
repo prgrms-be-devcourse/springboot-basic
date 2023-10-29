c474e19821caaa1b0f3aa7d7e32ae18cc95916b8
uijin-j <uijin0531@gmail.com>
2023-10-30 01:02:12 +0900

    test: CustomerSerivce 테스트 코드 작성

---
+++ src/test/java/team/marco/voucher_management_system/service/CustomerServiceTest.java
@@ -0,0 +1,46 @@
+package team.marco.voucher_management_system.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.transaction.annotation.Transactional;
+import team.marco.voucher_management_system.model.Customer;
+import team.marco.voucher_management_system.repository.custromer.CustomerRepository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Transactional
+@SpringBootTest
+class CustomerServiceTest {
+    @Autowired
+    private CustomerService customerService;
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @BeforeEach
+    void setUp() {
+        String query = "DELETE FROM customers";
+        jdbcTemplate.update(query);
+    }
+
+    @Test
+    void 사용자_ID로_조회() {
+        // 사용자 등록
+        Customer customer = new Customer("test", "test@gmail.com");
+        customerRepository.insert(customer);
+
+        // 사용자 조회
+        Customer found = customerService.findCustomer(customer.getId());
+
+        // 등록된 사용자와 동일한 사용자가 조회됨
+        assertThat(found.getId()).isEqualTo(customer.getId());
+        assertThat(found.getName()).isEqualTo(customer.getName());
+        assertThat(found.getEmail()).isEqualTo(customer.getEmail());
+    }
+}
