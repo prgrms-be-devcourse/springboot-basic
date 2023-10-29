14b76091e739ce2fb9ef6eb7a831304485553007
shoeone96 <dlwnddnjs96@gmail.com>
2023-10-29 14:11:36 +0900

    feat: docker 로 mysql 띄우기 추가
    pull 이후 바로 사용해볼 수 있게 docker 사용 추가

---
+++ mysql/init.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE IF NOT EXISTS vouchers;
+
+USE vouchers;
+
+CREATE TABLE customers (
+                           customer_id BINARY(16),
+                           customer_name VARCHAR(30),
+                           customer_type VARCHAR(30),
+                           CONSTRAINT customer_pk PRIMARY KEY(customer_id)
+);
+
+CREATE TABLE vouchers (
+                          voucher_id BINARY(16),
+                          discount_value BIGINT,
+                          voucher_type VARCHAR(30),
+                          customer_id BINARY(16),
+                          CONSTRAINT voucher_pk PRIMARY KEY(voucher_id),
+                          CONSTRAINT voucher_fk FOREIGN KEY (customer_id) REFERENCES customers (customer_id)
+);
