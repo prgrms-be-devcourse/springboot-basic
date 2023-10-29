11bc09a59e65a72c9c45b6e55be72eb7712779bb
KyungMin Lee <hello.tidavid@gmail.com>
2023-10-29 22:28:17 +0900

    FEAT: Voucher API Test Case 추가

--- src/main/java/com/programmers/springbootbasic/domain/voucher/dto/VoucherControllerResponseDto.java
+++ src/main/java/com/programmers/springbootbasic/domain/voucher/dto/VoucherControllerResponseDto.java
@@ -5,6 +5,7 @@ import com.programmers.springbootbasic.domain.voucher.service.VoucherType;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+import java.time.LocalDate;
 import java.util.UUID;
 
 @Getter
@@ -13,11 +14,13 @@ public class VoucherControllerResponseDto {
     private final UUID voucherId;
     private final long value;
     private final int voucherType;
+    private final LocalDate createdAt;
 
     public static VoucherControllerResponseDto of(Voucher voucher) {
         return new VoucherControllerResponseDto(
                 voucher.getVoucherId(),
                 voucher.getValue(),
-                VoucherType.predictVoucherType(voucher));
+                VoucherType.predictVoucherType(voucher),
+                voucher.getCreatedAt());
     }
 }
