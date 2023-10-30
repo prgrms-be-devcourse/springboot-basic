59553b4bdd862966f9735d6bac7e99a3be31662b
Hanna <hanna@trdst.com>
2023-10-30 22:13:24 +0900

    feat: docker-compose 작성

--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -1,26 +1,22 @@
-DROP TABLE IF EXISTS wallets;
-DROP TABLE IF EXISTS vouchers;
-DROP TABLE IF EXISTS customers;
-
-CREATE TABLE vouchers
+CREATE TABLE IF NOT EXISTS vouchers
 (
     id     CHAR(36) PRIMARY KEY DEFAULT (UUID()),
     type   VARCHAR(255) NOT NULL,
     amount BIGINT       NOT NULL
-);
+    );
 
-CREATE TABLE customers
+CREATE TABLE IF NOT EXISTS customers
 (
     id          CHAR(36) PRIMARY KEY  DEFAULT (UUID()),
     email       VARCHAR(255) NOT NULL UNIQUE,
     blacklisted BOOLEAN      NOT NULL DEFAULT FALSE
-);
+    );
 
-CREATE TABLE wallets
+CREATE TABLE IF NOT EXISTS wallets
 (
     id          INT PRIMARY KEY AUTO_INCREMENT,
     customer_id CHAR(36) NOT NULL,
     voucher_id  CHAR(36) NOT NULL,
     used        BOOLEAN  NOT NULL DEFAULT FALSE,
     UNIQUE (customer_id, voucher_id)
-);
+    );
