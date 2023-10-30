facf373be92b5503ec75381d808ac26f68bcb927
ZZAMBAs <rksidksrksi@naver.com>
2023-10-30 13:38:36 +0900

    Test: DB 관리 VoucherRepository 테스트를 작성한다.

---
+++ src/test/java/org/prgms/springbootbasic/repository/voucher/VoucherDatabaseRepositoryTest.java
@@ -0,0 +1,106 @@
+package org.prgms.springbootbasic.repository.voucher;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.prgms.springbootbasic.domain.voucher.FixedAmountVoucher;
+import org.prgms.springbootbasic.domain.voucher.PercentDiscountVoucher;
+import org.prgms.springbootbasic.domain.voucher.VoucherPolicy;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+
+@SpringBootTest
+@ActiveProfiles("dev")
+class VoucherDatabaseRepositoryTest {
+    @Autowired
+    private VoucherDatabaseRepository voucherDatabaseRepository;
+
+    private PercentDiscountVoucher setUpVoucher;
+    @BeforeEach
+    void setUp() {
+        setUpVoucher = new PercentDiscountVoucher(UUID.randomUUID(), 40);
+        voucherDatabaseRepository.save(setUpVoucher);
+    }
+
+    @AfterEach
+    void clean() {
+        voucherDatabaseRepository.deleteAll();
+    }
+
+    @Test
+    void saveNewVoucherToDB() {
+        FixedAmountVoucher fixedAmountVoucher = new FixedAmountVoucher(UUID.randomUUID(), 1000);
+
+        voucherDatabaseRepository.save(fixedAmountVoucher);
+
+        Optional<VoucherPolicy> retrievedVoucher = voucherDatabaseRepository.findById(fixedAmountVoucher.getVoucherId());
+
+        assertThat(retrievedVoucher.isPresent(), is(true));
+        assertThat(retrievedVoucher.get(), samePropertyValuesAs(fixedAmountVoucher));
+    }
+
+    @Test
+    void updateVoucherInDB() {
+        FixedAmountVoucher updateVoucher = new FixedAmountVoucher(setUpVoucher.getVoucherId(), 2000);
+
+        voucherDatabaseRepository.save(updateVoucher);
+
+        Optional<VoucherPolicy> retrievedVoucher = voucherDatabaseRepository.findById(setUpVoucher.getVoucherId());
+
+        assertThat(retrievedVoucher.isPresent(), is(true));
+        assertThat(retrievedVoucher.get(), samePropertyValuesAs(updateVoucher));
+    }
+
+    @Test
+    void findVoucherByIdInDB() {
+        Optional<VoucherPolicy> retrievedVoucher = voucherDatabaseRepository.findById(setUpVoucher.getVoucherId());
+
+        assertThat(retrievedVoucher.isPresent(), is(true));
+        assertThat(retrievedVoucher.get(), samePropertyValuesAs(setUpVoucher));
+    }
+
+    @Test
+    void findAllVouchersInDB() {
+        FixedAmountVoucher fixedAmountVoucher = new FixedAmountVoucher(UUID.randomUUID(), 3000);
+
+        voucherDatabaseRepository.save(fixedAmountVoucher);
+
+        List<VoucherPolicy> vouchers = voucherDatabaseRepository.findAll();
+
+        assertThat(vouchers, hasSize(2));
+        assertThat(vouchers, hasItem(samePropertyValuesAs(fixedAmountVoucher)));
+        assertThat(vouchers, hasItem(samePropertyValuesAs(setUpVoucher)));
+    }
+
+    @Test
+    void deleteVoucherByIdInDB() {
+        Optional<VoucherPolicy> deleteVoucher = voucherDatabaseRepository.deleteById(setUpVoucher.getVoucherId());
+
+        List<VoucherPolicy> vouchers = voucherDatabaseRepository.findAll();
+
+        assertThat(deleteVoucher.isPresent(), is(true));
+        assertThat(deleteVoucher.get(), samePropertyValuesAs(setUpVoucher));
+        assertThat(vouchers, hasSize(0));
+    }
+
+    @Test
+    void deleteAllVouchersInDB() {
+        FixedAmountVoucher fixedAmountVoucher = new FixedAmountVoucher(UUID.randomUUID(), 3000);
+
+        voucherDatabaseRepository.save(fixedAmountVoucher);
+        voucherDatabaseRepository.deleteAll();
+
+        List<VoucherPolicy> vouchers = voucherDatabaseRepository.findAll();
+
+        assertThat(vouchers.size(), is(0));
+    }
+}
