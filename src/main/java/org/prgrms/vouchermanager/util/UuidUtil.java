98e3e8e20bdf98c5193cfb8e6b49c0c3bee5bbe5
dlswns2480 <dlswns2480@naver.com>
2023-10-30 10:57:28 +0900

    refactor : 미사용 메서드 제거

--- src/main/java/org/prgrms/vouchermanager/util/UuidUtil.java
+++ src/main/java/org/prgrms/vouchermanager/util/UuidUtil.java
@@ -8,12 +8,6 @@ public class UuidUtil {
         var byteBuffer = ByteBuffer.wrap(bytes);
         return new UUID(byteBuffer.getLong(), byteBuffer.getLong());
     }
-    public static UUID intToUUID(int value) {
-        long mostSignificantBits = (value) & 0xFFFFFFFFL;
-        long leastSignificantBits = 0x5E05000000000000L;
-
-        return new UUID(mostSignificantBits, leastSignificantBits);
-    }
     public static UUID stringToUUID(String inputString) {
         try {
             return UUID.fromString(inputString);
