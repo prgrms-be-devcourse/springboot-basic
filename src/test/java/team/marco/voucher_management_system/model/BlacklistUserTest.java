408969dc417f43deb8d6662b9b955dc2e436fa03
ASak1104 <khw56184@gmail.com>
2023-10-31 00:12:22 +0900

    test : 블랙리스트 유저 테스트 강화

--- src/test/java/team/marco/voucher_management_system/model/BlacklistUserTest.java
+++ src/test/java/team/marco/voucher_management_system/model/BlacklistUserTest.java
@@ -1,32 +1,57 @@
 package team.marco.voucher_management_system.model;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.assertj.core.api.Assertions.assertThatNoException;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 import java.util.UUID;
+import org.assertj.core.api.ThrowableAssert.ThrowingCallable;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class BlacklistUserTest {
     @Test
     @DisplayName("이름이 빈 문자열이 아니면 생성에 성공해야한다.")
-    void successCreation() {
+    void testSuccessCreation() {
         // given
         UUID id = UUID.randomUUID();
         String name = "test";
 
         // when
+        ThrowingCallable targetMethod = () -> new BlacklistUser(id, name);
+
         // then
-        new BlacklistUser(id, name);
+        assertThatNoException().isThrownBy(targetMethod);
     }
 
     @Test
     @DisplayName("이름이 빈 문자열이면 생성에 실패해야한다.")
-    void failCreation() {
+    void testFailCreation() {
         UUID id = UUID.randomUUID();
         String name = "";
 
         // when
+        ThrowingCallable targetMethod = () -> new BlacklistUser(id, name);
+
         // then
-        assertThrows(IllegalArgumentException.class, () -> new BlacklistUser(id, name));
+        assertThatIllegalArgumentException().isThrownBy(targetMethod)
+                .withMessage("이름은 빈 문자열 일 수 없습니다.");
+    }
+
+    @Test
+    @DisplayName("블랙리스트 사용자의 정보에는 id와 이름이 포함되어야 한다.")
+    void testGetInfo() {
+        UUID id = UUID.randomUUID();
+        String name = "smith";
+
+        // when
+        BlacklistUser blacklistUser = new BlacklistUser(id, name);
+
+        // then
+        String blacklistUserInfo = blacklistUser.getInfo();
+
+        assertThat(blacklistUserInfo, Matchers.containsString(id.toString()));
+        assertThat(blacklistUserInfo, Matchers.containsString(name));
     }
 }
