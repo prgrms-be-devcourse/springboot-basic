0b2fd623fc330e8fb5b5e3558d9b6f0986beb4da
kkangh00n <kig2454@gmail.com>
2023-10-30 10:27:34 +0900

    refactor: 변수 이름 재설정 및 미사용 코드 제거

--- src/main/java/com/weeklyMission/exception/ExceptionHandler.java
+++ src/main/java/com/weeklyMission/exception/ExceptionHandler.java
@@ -22,16 +22,12 @@ public class ExceptionHandler extends Client{
         } catch(IncorrectInputException ne){
             System.out.println(ne.getMessage());
             logger.error("발생 지문 : {} , 입력 받은 값 : {}", ne.getQuest(), ne.getInput());
-        } catch (IllegalArgumentException ie) {
-            System.out.println(ie.getMessage());
-            logger.error(ie.getMessage());
-        } catch(NoSuchElementException noe){
-            System.out.println(noe.getMessage());
-            logger.error(noe.getMessage());
+        } catch (IllegalArgumentException e2) {
+            System.out.println(e2.getMessage());
+            logger.error(e2.getMessage());
+        } catch(NoSuchElementException e3){
+            System.out.println(e3.getMessage());
+            logger.error(e3.getMessage());
         }
-//        catch(Exception e){
-//            System.out.println("서버 내부 오류입니다." + System.lineSeparator());
-//            logger.error("서버 내부 오류");
-//        }
     }
 }
