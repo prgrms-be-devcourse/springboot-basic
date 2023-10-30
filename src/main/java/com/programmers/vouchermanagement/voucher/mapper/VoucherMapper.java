64b3f10172469463a27c75e5229c2444ca4f32fb
letskuku <yujung9288@naver.com>
2023-10-31 03:03:36 +0900

    refactor: 필요없는 메서드 사용 삭제

--- src/main/java/com/programmers/vouchermanagement/voucher/mapper/VoucherMapper.java
+++ src/main/java/com/programmers/vouchermanagement/voucher/mapper/VoucherMapper.java
@@ -13,12 +13,10 @@ public class VoucherMapper {
     public static Voucher toEntity(String[] voucherData) {
 
         UUID voucherId = UUID.fromString(voucherData[0]);
-        VoucherType voucherType = VoucherType.getVoucherTypeByName(voucherData[1].toLowerCase());
+        VoucherType voucherType = VoucherType.getVoucherTypeByName(voucherData[1]);
         Long discount = Long.parseLong(voucherData[2]);
         VoucherPolicy voucherPolicy = VoucherPolicyMapper.toEntity(discount, voucherType);
 
-        Voucher voucher = new Voucher(voucherId, voucherType, voucherPolicy);
-
-        return voucher;
+        return new Voucher(voucherId, voucherType, voucherPolicy);
     }
 }
