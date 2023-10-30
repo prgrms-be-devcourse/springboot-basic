4f7c45be0a3dae32401bc5a876773ae045032d74
letskuku <yujung9288@naver.com>
2023-10-31 03:38:52 +0900

    remove: 필요없는 파일 삭제

--- src/main/java/com/programmers/vouchermanagement/wallet/exception/WalletNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.programmers.vouchermanagement.wallet.exception;
-
-public class WalletNotFoundException extends RuntimeException {
-
-    public WalletNotFoundException() {
-        super("Wallet not found.");
-    }
-}
