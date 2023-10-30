c4877d2dbff1edaa6e5b9689424f846f41e83efc
kkangh00n <kig2454@gmail.com>
2023-10-30 10:46:16 +0900

    refactor: 사용 하지 않는 메서드 제거

--- src/main/java/com/weeklyMission/member/domain/Member.java
+++ src/main/java/com/weeklyMission/member/domain/Member.java
@@ -1,17 +1,10 @@
 package com.weeklyMission.member.domain;
 
 
-import java.util.UUID;
-
 public record Member(
     String memberId,
     String name,
     String email,
     Integer age
 ) {
-
-    @Override
-    public String toString() {
-        return "이름: " + name + " 이메일 : " + email + " 나이: " + age;
-    }
 }
