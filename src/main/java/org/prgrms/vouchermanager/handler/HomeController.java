be8cf58de731bc55c2f1767d8151c8c42e0afefd
dlswns2480 <dlswns2480@naver.com>
2023-10-31 10:07:30 +0900

    feat : ì시작페이지 생성

---
+++ src/main/java/org/prgrms/vouchermanager/handler/HomeController.java
@@ -0,0 +1,15 @@
+package org.prgrms.vouchermanager.handler;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String runApp(){
+        return "home";
+    }
+}
--- src/main/java/org/prgrms/vouchermanager/handler/VoucherController.java
+++ src/main/java/org/prgrms/vouchermanager/handler/VoucherController.java
@@ -7,22 +7,26 @@ import org.prgrms.vouchermanager.domain.voucher.PercentDiscountVoucher;
 import org.prgrms.vouchermanager.domain.voucher.Voucher;
 import org.prgrms.vouchermanager.service.VoucherService;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-@Component
+@Controller
 @RequiredArgsConstructor
 public class VoucherController {
 
     private final VoucherService voucherService;
 
+
     public void create(MenuType menuType) {
-        if(menuType == MenuType.FIXED){
+
+        if (menuType == MenuType.FIXED) {
             voucherService.createVoucher(new FixedAmountVoucher(UUID.randomUUID(), 10, MenuType.FIXED));
-        }
-        else{
+        } else {
             voucherService.createVoucher(new PercentDiscountVoucher(UUID.randomUUID(), 10, MenuType.PERCENT));
         }
     }
