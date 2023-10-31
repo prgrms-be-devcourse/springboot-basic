d5090e2a9208c9c9b68c66cb936c96a9164152f1
KarmaPol <kkhkr98@gmail.com>
2023-10-31 17:44:59 +0900

    feat : 바우처 리스트 페이지 추가

--- src/main/java/com/pgms/part1/domain/voucher/controller/VoucherWebController.java
+++ src/main/java/com/pgms/part1/domain/voucher/controller/VoucherWebController.java
@@ -1,8 +1,29 @@
 package com.pgms.part1.domain.voucher.controller;
 
+import com.pgms.part1.domain.voucher.dto.VoucherResponseDto;
+import com.pgms.part1.domain.voucher.service.VoucherService;
+import com.pgms.part1.domain.wallet.service.WalletService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class VoucherWebController {
 
+    private final VoucherService voucherService;
+    private final WalletService walletService;
+
+    public VoucherWebController(VoucherService voucherService, WalletService walletService) {
+        this.voucherService = voucherService;
+        this.walletService = walletService;
+    }
+
+    @GetMapping("/vouchers")
+    public String getVouchersList(Model model){
+        List<VoucherResponseDto> voucherResponseDtos = voucherService.listVoucher();
+        model.addAttribute("vouchers", voucherResponseDtos);
+        return "vouchers/vouchersListPage";
+    }
 }
