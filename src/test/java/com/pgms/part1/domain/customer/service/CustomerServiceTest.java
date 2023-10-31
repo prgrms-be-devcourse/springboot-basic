77b9ef64f45b1afce9b0af04eaeec697ddc28b7d
KarmaPol <kkhkr98@gmail.com>
2023-10-31 10:59:38 +0900

    build : web, 타임리프 의존성 추가

--- src/test/java/com/pgms/part1/domain/customer/service/CustomerServiceTest.java
+++ src/test/java/com/pgms/part1/domain/customer/service/CustomerServiceTest.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.jdbc.Sql;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -18,7 +17,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 @ActiveProfiles("test")
 @Transactional
 @SpringBootTest
-@Sql("/truncate.sql")
+//@Sql("/truncate.sql")
 class CustomerServiceTest {
 
     @Autowired
--- src/test/java/com/pgms/part1/domain/voucher/service/VoucherServiceTest.java
+++ src/test/java/com/pgms/part1/domain/voucher/service/VoucherServiceTest.java
@@ -9,7 +9,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.jdbc.Sql;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -17,7 +16,7 @@ import java.util.List;
 @ActiveProfiles("test")
 @Transactional
 @SpringBootTest
-@Sql("/truncate.sql")
+//@Sql("/truncate.sql")
 class VoucherServiceTest {
 
     @Autowired
