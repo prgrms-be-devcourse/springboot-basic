a7e5e21be92dc89b3d58a0e5d9a1815c749be298
park0jae <okvv26@naver.com>
2023-11-01 01:04:23 +0900

    feat : Web 전역 예외처리 클래스 -> WebExceptionAdvisor 추가

---
+++ src/main/java/com/zerozae/voucher/common/advisor/WebExceptionAdvisor.java
@@ -0,0 +1,31 @@
+package com.zerozae.voucher.common.advisor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.Objects;
+
+@Slf4j
+@ControllerAdvice
+public class WebExceptionAdvisor {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public String handleValidationException(Model model, MethodArgumentNotValidException ex) {
+        BindingResult bindingResult = ex.getBindingResult();
+        String errorMessage = Objects.requireNonNull(bindingResult.getFieldError()).getDefaultMessage();
+        model.addAttribute("error", errorMessage);
+        log.warn("ErrorMessage = {}", errorMessage);
+        return "/error";
+    }
+
+    @ExceptionHandler(Exception.class)
+    public String handleException(Model model, Exception e) {
+        model.addAttribute("error", e.getMessage());
+        log.warn("ErrorMessage = {}", e.getMessage());
+        return "/error";
+    }
+}
