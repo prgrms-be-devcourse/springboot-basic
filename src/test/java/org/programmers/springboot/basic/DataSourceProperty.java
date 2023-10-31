e7880547346b8a4843d558d1b32a3d6818f93c73
JIN-076 <andy230@naver.com>
2023-10-31 23:19:19 +0900

    update : 테스트용 DataSource 바인딩을 위한 Property 클래스 추가

---
+++ src/test/java/org/programmers/springboot/basic/DataSourceProperty.java
@@ -0,0 +1,36 @@
+package org.programmers.springboot.basic;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataSourceProperty {
+
+    @Value("${spring.datasource.driver-class-name}")
+    private String driverClassName;
+
+    @Value("${spring.datasource.url}")
+    private String url;
+
+    @Value("${spring.datasource.username}")
+    private String username;
+
+    @Value("${spring.datasource.password}")
+    private String password;
+
+    public String getDriverClassName() {
+        return driverClassName;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
