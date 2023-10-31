b62ca79f217057186d9ca1a72b00e953dc416e11
dlswns2480 <dlswns2480@naver.com>
2023-11-01 00:30:01 +0900

    feat : CustomerApiController 구현

---
+++ src/main/java/org/prgrms/vouchermanager/handler/customer/CustomerApiController.java
@@ -0,0 +1,27 @@
+package org.prgrms.vouchermanager.handler.customer;
+
+import lombok.RequiredArgsConstructor;
+import org.prgrms.vouchermanager.domain.customer.Customer;
+import org.prgrms.vouchermanager.domain.customer.CustomerRequest;
+import org.prgrms.vouchermanager.service.CustomerService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/customers")
+@RequiredArgsConstructor
+public class CustomerApiController {
+    private final CustomerService customerService;
+
+    @GetMapping("/")
+    public List<Customer> findAll() {
+        List<Customer> customers = customerService.findAll();
+        return customers;
+    }
+
+    @PostMapping("/create")
+    public Customer createCustomer(@RequestBody CustomerRequest request){
+        return customerService.createCustomer(request);
+    }
+}
