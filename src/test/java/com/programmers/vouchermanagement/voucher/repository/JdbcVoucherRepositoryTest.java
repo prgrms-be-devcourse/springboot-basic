75add9a935fc5ddd2f34c2ed9d219fbfb6fcc208
Jay <injaesong0@gmail.com>
2023-10-31 23:46:41 +0900

    refactor: remove order of tests

--- src/test/java/com/programmers/vouchermanagement/voucher/repository/JdbcVoucherRepositoryTest.java
+++ src/test/java/com/programmers/vouchermanagement/voucher/repository/JdbcVoucherRepositoryTest.java
@@ -16,29 +16,24 @@ import javax.sql.DataSource;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
-import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
 import com.programmers.vouchermanagement.voucher.domain.Voucher;
 import com.programmers.vouchermanagement.voucher.domain.VoucherType;
 import com.zaxxer.hikari.HikariDataSource;
 
 @Testcontainers(disabledWithoutDocker = true)
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class JdbcVoucherRepositoryTest {
     @Container
-    private static final MySQLContainer<?> mySQLContainer = new MySQLContainer<>("mysql:latest")
-            .withUsername("test")
-            .withPassword("1234")
+    private static final MySQLContainer<?> mySQLContainer = new MySQLContainer<>(DockerImageName.parse("mysql:latest"))
             .withInitScript("init.sql");
 
     DataSource dataSource;
@@ -59,7 +54,6 @@ class JdbcVoucherRepositoryTest {
     }
 
     @Test
-    @Order(1)
     @DisplayName("테스트 컨테이너 생성을 성공한다.")
     void testMySQLContainerRunning() {
         assertThat(mySQLContainer.isRunning(), is(true));
@@ -67,58 +61,56 @@ class JdbcVoucherRepositoryTest {
     }
 
     @Test
-    @Order(2)
     @DisplayName("JdbcVoucherRepository가 템플릿을 주입 받아 생성된다.")
     void testJdbcVoucherRepositoryCreation() {
         assertThat(voucherRepository, notNullValue());
     }
 
     @Test
-    @Order(3)
     @DisplayName("저장된 바우처가 없을 시 빈 리스트를 반환한다.")
     void testFindAllVouchersSuccessful_ReturnEmptyList() {
+        //given
+        voucherRepository.deleteAll();
+
         //when
-        final List<Voucher> vouchers = voucherRepository.findAll();
+        List<Voucher> vouchers = voucherRepository.findAll();
 
         //then
         assertThat(vouchers.isEmpty(), is(true));
     }
 
     @Test
-    @Order(4)
     @DisplayName("바우처 생성을 성공한다")
     void testVoucherCreationSuccessful() {
         //given
-        final Voucher fixedVoucher = new Voucher(UUID.randomUUID(), new BigDecimal(10000), VoucherType.FIXED);
+        Voucher fixedVoucher = new Voucher(UUID.randomUUID(), new BigDecimal(10000), VoucherType.FIXED);
 
         //when
-        final Voucher savedVoucher = voucherRepository.save(fixedVoucher);
+        Voucher savedVoucher = voucherRepository.save(fixedVoucher);
 
         //then
         assertThat(savedVoucher, samePropertyValuesAs(fixedVoucher));
     }
 
     @Test
-    @Order(5)
     @DisplayName("존재하지 않는 아이디 조회를 실패한다.")
     void testFindVoucherByIdFailed() {
         //when
-        final Optional<Voucher> voucher = voucherRepository.findById(UUID.randomUUID());
+        Optional<Voucher> voucher = voucherRepository.findById(UUID.randomUUID());
 
         //then
         assertThat(voucher.isEmpty(), is(true));
     }
 
     @Test
-    @Order(6)
     @DisplayName("바우처 아이디 조회를 성공한다.")
     void testFindVoucherByIdSuccessful_ReturnList() {
         //given
-        final Voucher voucher = new Voucher(UUID.randomUUID(), new BigDecimal(40), VoucherType.PERCENT);
+        Voucher voucher = new Voucher(UUID.randomUUID(), new BigDecimal(40), VoucherType.PERCENT);
         voucherRepository.save(voucher);
 
         //when
-        final Optional<Voucher> foundVoucher = voucherRepository.findById(voucher.getVoucherId());
+        Optional<Voucher> foundVoucher = voucherRepository.findById(voucher.getVoucherId());
 
         //then
         assertThat(foundVoucher.isEmpty(), is(false));
@@ -126,44 +118,41 @@ class JdbcVoucherRepositoryTest {
     }
 
     @Test
-    @Order(7)
     @DisplayName("바우처 전체 조회를 성공한다.")
     void testFindAllVouchersSuccessful() {
         //given
-        final Voucher voucher = new Voucher(UUID.randomUUID(), new BigDecimal(5000), VoucherType.FIXED);
+        Voucher voucher = new Voucher(UUID.randomUUID(), new BigDecimal(5000), VoucherType.FIXED);
         voucherRepository.save(voucher);
 
         //when
-        final List<Voucher> vouchers = voucherRepository.findAll();
+        List<Voucher> vouchers = voucherRepository.findAll();
 
         //then
         assertThat(vouchers, hasSize(greaterThanOrEqualTo(1)));
     }
 
     @Test
-    @Order(8)
     @DisplayName("바우처 정보 수정을 성공한다.")
     void testVoucherUpdateSuccessful() {
         //given
-        final Voucher voucher = new Voucher(UUID.randomUUID(), new BigDecimal(10000), VoucherType.FIXED);
+        Voucher voucher = new Voucher(UUID.randomUUID(), new BigDecimal(10000), VoucherType.FIXED);
         voucherRepository.save(voucher);
-        final Voucher foundVoucher = voucherRepository.findById(voucher.getVoucherId()).get();
+        Voucher foundVoucher = voucherRepository.findById(voucher.getVoucherId()).get();
 
         //when
-        final Voucher updatedVoucher = new Voucher(voucher.getVoucherId(), new BigDecimal(5000), voucher.getVoucherType());
+        Voucher updatedVoucher = new Voucher(voucher.getVoucherId(), new BigDecimal(5000), voucher.getVoucherType());
         voucherRepository.save(updatedVoucher);
-        final Voucher newlyFoundVoucher = voucherRepository.findById(foundVoucher.getVoucherId()).get();
+        Voucher newlyFoundVoucher = voucherRepository.findById(foundVoucher.getVoucherId()).get();
 
         //then
         assertThat(newlyFoundVoucher, samePropertyValuesAs(updatedVoucher));
     }
 
     @Test
-    @Order(9)
     @DisplayName("바우처 삭제를 성공한다.")
     void testDeleteVoucherSuccessful() {
         //given
-        final Voucher voucher = new Voucher(UUID.randomUUID(), new BigDecimal(50), VoucherType.PERCENT);
+        Voucher voucher = new Voucher(UUID.randomUUID(), new BigDecimal(50), VoucherType.PERCENT);
         voucherRepository.save(voucher);
 
         //when
