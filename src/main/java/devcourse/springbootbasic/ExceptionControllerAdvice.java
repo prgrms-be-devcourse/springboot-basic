b15e5c052458e41e3e09b70003e6af59a7b67e36
hyoguoo <hyoguoo@gmail.com>
2023-11-01 10:24:29 +0900

    feat: Exception Handler 추가

---
+++ src/main/java/devcourse/springbootbasic/ExceptionControllerAdvice.java
@@ -0,0 +1,38 @@
+package devcourse.springbootbasic;
+
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class ExceptionControllerAdvice {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<Object> handleRuntimeException(Exception ex) {
+        log.warn(ex.getMessage());
+        ErrorMessage errorMessage = new ErrorMessage(ex.getClass().getSimpleName(), ex.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorMessage);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Object> handleInternalServerError(Exception ex) {
+        log.error(ex.getMessage());
+        ErrorMessage errorMessage = new ErrorMessage(ex.getClass().getSimpleName(), ex.getMessage());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorMessage);
+    }
+
+    @Getter
+    private static final class ErrorMessage {
+        private final String error;
+        private final String message;
+
+        ErrorMessage(String error, String message) {
+            this.error = error;
+            this.message = message;
+        }
+    }
+}
