07aa3b1cb03ac066ce3c20200f868bffe70ebe31
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2023-11-02 00:47:28 +0900

    refactor: delete datasource from AppConfig

--- src/main/java/com/programmers/vouchermanagement/configuration/AppConfig.java
+++ src/main/java/com/programmers/vouchermanagement/configuration/AppConfig.java
@@ -1,9 +1,7 @@
 package com.programmers.vouchermanagement.configuration;
 
-import com.zaxxer.hikari.HikariDataSource;
 import org.beryx.textio.TextIO;
 import org.beryx.textio.TextIoFactory;
-import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -16,19 +14,6 @@ public class AppConfig {
     @Bean
     public TextIO textIO() {
         return TextIoFactory.getTextIO();
-    }
-
-    @Bean
-    public DataSource dataSource() {
-        var dataSource = DataSourceBuilder.create()
-                .url("jdbc:mysql://localhost:3306/test")
-                .username("root")
-                .password("20231028")
-                .type(HikariDataSource.class)
-                .build();
-        dataSource.setMaximumPoolSize(1000);
-        dataSource.setMinimumIdle(100);
-        return dataSource;
     }
 
     @Bean
