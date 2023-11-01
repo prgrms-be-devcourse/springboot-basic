992acc99c61d301479d6c9c4fd73d17545f06c56
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2023-11-02 00:49:08 +0900

    refactor: check empty result readAllVoucherByCustomerId at WalletService

--- src/main/java/com/programmers/vouchermanagement/wallet/service/WalletService.java
+++ src/main/java/com/programmers/vouchermanagement/wallet/service/WalletService.java
@@ -2,12 +2,14 @@ package com.programmers.vouchermanagement.wallet.service;
 
 import com.programmers.vouchermanagement.customer.domain.Customer;
 import com.programmers.vouchermanagement.voucher.domain.Voucher;
+import com.programmers.vouchermanagement.voucher.dto.VoucherResponse;
 import com.programmers.vouchermanagement.wallet.domain.Ownership;
 import com.programmers.vouchermanagement.wallet.repository.WalletRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.UUID;
@@ -28,16 +30,16 @@ public class WalletService {
     }
 
     public Customer readCustomerByVoucherId(UUID voucherId) {
-        return walletRepository.findCustomerByVoucherId(voucherId).orElseThrow(
-                () -> {
-                    logger.error(NOT_FOUND_VOUCHER_ALLOCATION);
-                    return new NoSuchElementException(NOT_FOUND_VOUCHER_ALLOCATION);
-                }
-        );
+        return walletRepository.findCustomerByVoucherId(voucherId).orElseThrow(() -> {
+            logger.error(NOT_FOUND_VOUCHER_ALLOCATION);
+            return new NoSuchElementException(NOT_FOUND_VOUCHER_ALLOCATION);
+        });
     }
 
-    public List<Voucher> readAllVoucherByCustomerId(UUID customerId) {
-        return walletRepository.findAllVoucherByCustomerId(customerId);
+    public List<VoucherResponse> readAllVoucherByCustomerId(UUID customerId) {
+        List<Voucher> vouchers = walletRepository.findAllVoucherByCustomerId(customerId);
+        if (vouchers.isEmpty()) return Collections.emptyList();
+        return vouchers.stream().map(VoucherResponse::from).toList();
     }
 
     public void deleteVoucherFromCustomer(UUID voucherId) {
