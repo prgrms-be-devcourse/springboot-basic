399a3af947ee0974b69b3957ba3f8d8e0b69cc2b
happyjamy <78072370+happyjamy@users.noreply.github.com>
2023-11-02 04:00:57 +0900

    [add] 타임리프 에러 핸들러

---
+++ src/main/java/com/programmers/springbootbasic/thyme/CustomErrorController.java
@@ -0,0 +1,14 @@
+package com.programmers.springbootbasic.thyme;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class CustomErrorController implements ErrorController {
+
+    @RequestMapping("/error")
+    public String handleError() {
+        return "defaultError";
+    }
+}
