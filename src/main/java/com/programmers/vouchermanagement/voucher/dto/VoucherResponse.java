57a79e268ee38bd3dad3dd1b06ae4f3d15eb72a9
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2023-11-02 00:46:33 +0900

    refactor: change VoucherResponse to record

--- src/main/java/com/programmers/vouchermanagement/voucher/dto/VoucherResponse.java
+++ src/main/java/com/programmers/vouchermanagement/voucher/dto/VoucherResponse.java
@@ -5,34 +5,9 @@ import com.programmers.vouchermanagement.voucher.domain.VoucherType;
 
 import java.util.UUID;
 
-public class VoucherResponse {
-    private final UUID voucherId;
-    private final long discountValue;
-    private final VoucherType voucherType;
-
-    private VoucherResponse(UUID voucherId, long discountValue, VoucherType voucherType) {
-        this.voucherId = voucherId;
-        this.discountValue = discountValue;
-        this.voucherType = voucherType;
-    }
-
+public record VoucherResponse(UUID voucherId, long discountValue, String voucherTypeName, boolean isPercent) {
     public static VoucherResponse from(Voucher voucher) {
-        return new VoucherResponse(voucher.voucherId(), voucher.discountValue(), voucher.voucherType());
-    }
-
-    public UUID getVoucherId() {
-        return voucherId;
-    }
-
-    public long getDiscountValue() {
-        return discountValue;
-    }
-
-    public boolean isPercentVoucher() {
-        return voucherType.isPercent();
-    }
-
-    public String getVoucherTypeName() {
-        return voucherType.displayTypeName();
+        VoucherType voucherType = voucher.voucherType();
+        return new VoucherResponse(voucher.voucherId(), voucher.discountValue(), voucherType.displayTypeName(), voucherType.isPercent());
     }
 }
