143ea8695d1925f9a763a1cdc9a84713be00aba9
shoeone96 <dlwnddnjs96@gmail.com>
2023-11-01 20:19:46 +0900

    feat: voucher 받는 페이지 전송 컨트롤러 추가

---
+++ src/main/java/org/programmers/springorder/voucher/controller/VoucherPageController.java
@@ -0,0 +1,28 @@
+package org.programmers.springorder.voucher.controller;
+
+import org.programmers.springorder.voucher.dto.VoucherResponseDto;
+import org.programmers.springorder.voucher.service.VoucherService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class VoucherPageController {
+
+    private final VoucherService voucherService;
+
+    public VoucherPageController(VoucherService voucherService) {
+        this.voucherService = voucherService;
+    }
+
+    @GetMapping("/vouchers")
+    public String getVoucherList(Model model) {
+        List<VoucherResponseDto> allVoucher = voucherService.getAllVoucher();
+        model.addAttribute("voucherList", allVoucher);
+        return "vouchers";
+    }
+
+}
+
