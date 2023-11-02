f537f11cca06e5d72efb0c437829879fa1eb1e6b
letskuku <yujung9288@naver.com>
2023-11-03 02:21:46 +0900

    feat: Spring MVC 적용해서 JSON과 XML을 지원하는 REST API controller 구현

---
+++ src/main/java/com/programmers/vouchermanagement/voucher/presentation/VoucherApiController.java
@@ -0,0 +1,52 @@
+package com.programmers.vouchermanagement.voucher.presentation;
+
+import com.programmers.vouchermanagement.voucher.application.VoucherService;
+import com.programmers.vouchermanagement.voucher.domain.VoucherType;
+import com.programmers.vouchermanagement.voucher.dto.VoucherRequestDto;
+import com.programmers.vouchermanagement.voucher.dto.VoucherResponseDto;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1/voucher")
+public class VoucherApiController {
+
+    private final VoucherService voucherService;
+
+    public VoucherApiController(VoucherService voucherService) {
+        this.voucherService = voucherService;
+    }
+
+    @PostMapping
+    public void createVoucher(@RequestBody VoucherRequestDto voucherRequestDto) {
+        voucherService.createVoucher(voucherRequestDto);
+    }
+
+    @GetMapping
+    public List<VoucherResponseDto> readAllVoucher() {
+        return voucherService.readAllVoucher();
+    }
+
+    @GetMapping("/{voucherId}")
+    public VoucherResponseDto readVoucherById(@PathVariable UUID voucherId) {
+        return voucherService.readVoucherById(voucherId);
+    }
+
+    @GetMapping
+    public List<VoucherResponseDto> readAllVoucherByCreatedAtAndType(@RequestParam LocalDateTime createdAt, @RequestParam VoucherType voucherType) {
+        return voucherService.readAllVoucherByCreatedAtAndType(createdAt, voucherType);
+    }
+
+    @DeleteMapping
+    public void removeAllVoucher() {
+        voucherService.removeAllVoucher();
+    }
+
+    @DeleteMapping("/{voucherId}")
+    public void removeVoucherById(@PathVariable UUID voucherId) {
+        voucherService.removeVoucherById(voucherId);
+    }
+}
