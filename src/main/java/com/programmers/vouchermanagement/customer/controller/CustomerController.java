fd762fcdbd7d01093efeb5526f29a60b6ca7a5d8
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2023-11-02 14:43:41 +0900

    feat: add "api", "console" profile and apply url mapping at CustomerController

--- src/main/java/com/programmers/vouchermanagement/customer/controller/CustomerController.java
+++ src/main/java/com/programmers/vouchermanagement/customer/controller/CustomerController.java
@@ -1,11 +1,19 @@
 package com.programmers.vouchermanagement.customer.controller;
 
+import com.programmers.vouchermanagement.customer.dto.CreateCustomerRequest;
 import com.programmers.vouchermanagement.customer.dto.CustomerDto;
 import com.programmers.vouchermanagement.customer.service.CustomerService;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
+@Profile({"api", "console"})
+@RequestMapping("/api/v1/customers")
 @Controller
 public class CustomerController {
     private final CustomerService customerService;
@@ -14,6 +22,14 @@ public class CustomerController {
         this.customerService = customerService;
     }
 
+    @PostMapping
+    @ResponseBody
+    public void create(CreateCustomerRequest createCustomerRequest) {
+        customerService.create(createCustomerRequest);
+    }
+
+    @GetMapping("/blacklist")
+    @ResponseBody
     public List<CustomerDto> readAllBlackCustomer() {
         return customerService.readAllBlackCustomer();
     }
