5d8ff44cbaeb79c249e8341ced6122911b2ba725
kkangh00n <kig2454@gmail.com>
2023-11-02 17:00:39 +0900

    refactor: RequestDto record -> class 변경

--- src/main/java/com/weeklyMission/member/dto/MemberRequest.java
+++ src/main/java/com/weeklyMission/member/dto/MemberRequest.java
@@ -1,13 +1,31 @@
 package com.weeklyMission.member.dto;
 
 import com.weeklyMission.member.domain.Member;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-public record MemberRequest(
-    String name,
-    String email,
-    Integer age
-) {
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class MemberRequest{
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotNull
+    private Integer age;
+
     public Member toEntity(){
         return new Member(UUID.randomUUID().toString(), name, email, age);
     }
--- src/main/java/com/weeklyMission/voucher/dto/VoucherRequest.java
+++ src/main/java/com/weeklyMission/voucher/dto/VoucherRequest.java
@@ -4,12 +4,24 @@ import com.weeklyMission.client.VoucherType;
 import com.weeklyMission.voucher.domain.FixedAmountVoucher;
 import com.weeklyMission.voucher.domain.PercentDiscountVoucher;
 import com.weeklyMission.voucher.domain.Voucher;
+import jakarta.validation.constraints.NotNull;
 import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-public record VoucherRequest(
-    String type,
-    int amount
-) {
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class VoucherRequest{
+
+    private String type;
+
+    @NotNull
+    private Integer amount;
+
     public Voucher toEntity(){
         if(type.equals(VoucherType.Fixed.getType())){
             return new FixedAmountVoucher(UUID.randomUUID().toString(), amount);
