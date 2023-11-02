7a3e0e8c935d0d09e18fe81cd7e0679df1c7269a
Jay <injaesong0@gmail.com>
2023-11-02 10:56:15 +0900

    feat: map to find vouchers by type

--- src/main/java/com/programmers/vouchermanagement/voucher/controller/VoucherRestController.java
+++ src/main/java/com/programmers/vouchermanagement/voucher/controller/VoucherRestController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.programmers.vouchermanagement.voucher.domain.VoucherType;
 import com.programmers.vouchermanagement.voucher.dto.CreateVoucherRequest;
 import com.programmers.vouchermanagement.voucher.dto.VoucherResponse;
 import com.programmers.vouchermanagement.voucher.service.VoucherService;
@@ -28,20 +29,24 @@ public class VoucherRestController {
         return voucherService.create(createVoucherRequest);
     }
 
-    @GetMapping("/{voucherId}")
-    public VoucherResponse findById(@PathVariable UUID voucherId) {
-        return voucherService.findById(voucherId);
-    }
-
     @GetMapping
     public List<VoucherResponse> readAllVouchers() {
         return voucherService.readAllVouchers();
     }
 
-    //TODO: 조건별 조회 추가
+    @GetMapping("/{voucherId}")
+    public VoucherResponse findById(@PathVariable UUID voucherId) {
+        return voucherService.findById(voucherId);
+    }
 
     @DeleteMapping("/{voucherId}")
     public void deleteById(@PathVariable UUID voucherId) {
         voucherService.deleteById(voucherId);
+    }
+
+    @GetMapping("/type/{typeName}")
+    public List<VoucherResponse> findByType(@PathVariable String typeName) {
+        VoucherType voucherType = VoucherType.findVoucherTypeByName(typeName);
+        return voucherService.findByType(voucherType);
     }
 }
