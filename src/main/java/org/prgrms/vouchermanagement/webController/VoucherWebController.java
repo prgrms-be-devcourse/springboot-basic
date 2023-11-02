0c4c11f898deed057b83e1d6ffc37bae204771d6
kimnamgyu <lethargic.hood@gmail.com>
2023-11-02 15:52:37 +0900

    feat : voucher web controller 구현

---
+++ src/main/java/org/prgrms/vouchermanagement/webController/VoucherWebController.java
@@ -0,0 +1,90 @@
+package org.prgrms.vouchermanagement.webController;
+
+import org.prgrms.vouchermanagement.exception.InvalidInputException;
+import org.prgrms.vouchermanagement.voucher.domain.Voucher;
+import org.prgrms.vouchermanagement.voucher.policy.PolicyStatus;
+import org.prgrms.vouchermanagement.voucher.service.VoucherService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@Controller
+@RequestMapping("/voucher")
+public class VoucherWebController {
+
+    private final VoucherService voucherService;
+
+    public VoucherWebController(VoucherService voucherService) {
+        this.voucherService = voucherService;
+    }
+
+    @GetMapping("/voucher-home")
+    public String voucherHome() {
+        return "voucher/voucher-home";
+    }
+
+    @GetMapping("/create")
+    public String createVoucher() {
+        return "voucher/create";
+    }
+
+    @PostMapping("/create")
+    public String createVoucher(@RequestParam("policy") String policy, @RequestParam("amount") long amount) {
+        voucherService.createVoucher(validateAndConvertPolicy(policy), validateAndConvertAmountOrPercent(String.valueOf(amount)));
+
+        return "redirect:/voucher/create";
+    }
+
+    @GetMapping("/find")
+    public String findVoucher() {
+        return "voucher/find";
+    }
+
+    @PostMapping("/find")
+    public String findVoucher(@RequestParam("voucherId") String uuid, Model model) {
+        UUID voucherId =  UUID.fromString(uuid);
+        Voucher voucher = voucherService.findVoucher(voucherId);
+
+        model.addAttribute("voucher", voucher);
+
+        return "/voucher/find-voucher";
+    }
+
+    @GetMapping("/list")
+    public String voucherList(Model model) {
+        List<Voucher> vouchers = voucherService.voucherLists();
+        model.addAttribute("vouchers", vouchers);
+        return "voucher/list";
+    }
+
+    @GetMapping("/delete")
+    public String deleteVoucher() {
+        return "voucher/delete";
+    }
+
+    @PostMapping("/delete")
+    public String deleteVoucher(@RequestParam("voucherId") String uuid) {
+        UUID voucherId =  UUID.fromString(uuid);
+        voucherService.deleteVoucher(voucherId);
+        return "redirect:/voucher/delete";
+    }
+
+    private PolicyStatus validateAndConvertPolicy(String inputPolicy) {
+        try {
+            return PolicyStatus.valueOf(inputPolicy.toUpperCase());
+        } catch (IllegalArgumentException e) {
+            throw new InvalidInputException("잘못된 policy 입력입니다.");
+        }
+    }
+
+    private long validateAndConvertAmountOrPercent(String inputAmountOrPercent) {
+        try {
+            return Long.parseLong(inputAmountOrPercent);
+        } catch (NumberFormatException e) {
+            throw new InvalidInputException("잘못된 amountOrPercent 입력입니다.");
+        }
+    }
+}
