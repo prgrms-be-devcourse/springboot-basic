2f5e04795cc5d049872261ef76674673c52b2939
shoeone96 <dlwnddnjs96@gmail.com>
2023-11-02 15:36:20 +0900

    refactor: requestDto를 class -> record로 변경

--- src/main/java/org/programmers/springorder/voucher/dto/VoucherRequestDto.java
+++ src/main/java/org/programmers/springorder/voucher/dto/VoucherRequestDto.java
@@ -2,20 +2,5 @@ package org.programmers.springorder.voucher.dto;
 
 import org.programmers.springorder.voucher.model.VoucherType;
 
-public class VoucherRequestDto {
-    private final long discountValue;
-    private final VoucherType voucherType;
-
-    public VoucherRequestDto(long discountValue, VoucherType voucherType) {
-        this.discountValue = discountValue;
-        this.voucherType = voucherType;
-    }
-
-    public long getDiscountValue() {
-        return discountValue;
-    }
-
-    public VoucherType getVoucherType() {
-        return voucherType;
-    }
+public record VoucherRequestDto(long discountValue, VoucherType voucherType) {
 }
