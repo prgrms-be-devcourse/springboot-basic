3c70b49a9834c046501b5602eda436fe68fc9e71
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2023-11-02 13:49:29 +0900

    feat: implement CustomerInMemoryRepository for test mode

---
+++ src/main/java/com/programmers/vouchermanagement/customer/repository/CustomerInMemoryRepository.java
@@ -0,0 +1,36 @@
+package com.programmers.vouchermanagement.customer.repository;
+
+import com.programmers.vouchermanagement.customer.domain.Customer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Repository;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+@Repository
+@Profile("memory")
+public class CustomerInMemoryRepository implements CustomerRepository {
+    private static final Logger logger = LoggerFactory.getLogger(CustomerInMemoryRepository.class);
+    private final Map<UUID, Customer> customers;
+
+    public CustomerInMemoryRepository() {
+        customers = new HashMap<>();
+    }
+
+    @Override
+    public void save(Customer customer) {
+        customers.put(customer.getCustomerId(), customer);
+    }
+
+    @Override
+    public List<Customer> findAllBlackCustomer() {
+        return customers.values()
+                .stream()
+                .filter(Customer::isBlack)
+                .toList();
+    }
+}
