a903890abfc982f892d95ae9ba139f6cdfbbc390
shoeone96 <dlwnddnjs96@gmail.com>
2023-11-02 15:45:16 +0900

    chore: 사용하지 않는 import 문 제거

--- src/main/java/org/programmers/springorder/SpringOrderApplication.java
+++ src/main/java/org/programmers/springorder/SpringOrderApplication.java
@@ -1,7 +1,5 @@
 package org.programmers.springorder;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
