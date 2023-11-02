4f6472daa0bc4279ac26774bc36fb874a23fea72
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2023-11-02 11:28:12 +0900

    feat:apply CustomerFileManager and implement save method

--- src/main/java/com/programmers/vouchermanagement/customer/repository/CustomerFileRepository.java
+++ src/main/java/com/programmers/vouchermanagement/customer/repository/CustomerFileRepository.java
@@ -1,63 +1,34 @@
 package com.programmers.vouchermanagement.customer.repository;
 
 import com.programmers.vouchermanagement.customer.domain.Customer;
-import com.programmers.vouchermanagement.properties.AppProperties;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Repository;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.UncheckedIOException;
-import java.util.*;
-
-import static com.programmers.vouchermanagement.constant.Constant.COMMA_SEPARATOR;
-import static com.programmers.vouchermanagement.constant.Message.IO_EXCEPTION;
+import java.util.List;
 
 @Repository
-@Profile({"dev", "prod"})
+@Profile({"file"})
 public class CustomerFileRepository implements CustomerRepository {
     private static final Logger logger = LoggerFactory.getLogger(CustomerFileRepository.class);
-    private final String filePath;
-    private final Map<UUID, Customer> customers;
+    private final CustomerFileManager customerFileManager;
 
-    public CustomerFileRepository(AppProperties appProperties) {
-        this.filePath = appProperties.resources().path() + appProperties.domains().get("customer").fileName();
-        this.customers = new HashMap<>();
-        loadBlacklist();
+    public CustomerFileRepository(CustomerFileManager customerFileManager) {
+        this.customerFileManager = customerFileManager;
+    }
+
+    @Override
+    public void save(Customer customer) {
+        customerFileManager.customers.put(customer.getCustomerId(), customer);
+        customerFileManager.saveFile();
     }
 
     @Override
     public List<Customer> findAllBlackCustomer() {
-        return customers.values()
+        return customerFileManager.customers.values()
                 .stream()
                 .filter(Customer::isBlack)
                 .toList();
-    }
-
-    private void loadBlacklist() {
-        List<Customer> blacklist = new ArrayList<>();
-
-        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
-            br.readLine(); // skip the first line
-            String str;
-            while ((str = br.readLine()) != null) {
-                String[] line = str.split(COMMA_SEPARATOR);
-
-                UUID blackCustomerId = UUID.fromString(line[0]);
-                String name = line[1];
-
-                Customer blackCustomer = new Customer(blackCustomerId, name, true);
-
-                blacklist.add(blackCustomer);
-            }
-        } catch (IOException e) {
-            logger.error(IO_EXCEPTION);
-            throw new UncheckedIOException(e);
-        }
-
-        blacklist.forEach(blackCustomer -> customers.put(blackCustomer.getCustomerId(), blackCustomer));
     }
 }
