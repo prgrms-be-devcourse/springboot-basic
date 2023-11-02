417b278b5ec6dfd4b10dfce93f1013931927d3e6
EomYerim <djadpfa1002@naver.com>
2023-11-02 19:38:31 +0900

    refactor: VoucherRequestDto에 validate 추가 할인 금액 범위에 대해 validation 처리

--- src/main/java/org/programmers/springorder/dto/voucher/VoucherRequestDto.java
+++ src/main/java/org/programmers/springorder/dto/voucher/VoucherRequestDto.java
@@ -9,6 +9,7 @@ public class VoucherRequestDto {
     public VoucherRequestDto(long discountValue, VoucherType voucherType) {
         this.discountValue = discountValue;
         this.voucherType = voucherType;
+        validateDiscountRange();
     }
 
     public long getDiscountValue() {
@@ -17,5 +18,11 @@ public class VoucherRequestDto {
 
     public VoucherType getVoucherType() {
         return voucherType;
+    }
+
+    private void validateDiscountRange() {
+        if (discountValue < voucherType.getMinimumValue() || discountValue > voucherType.getMaximumValue()) {
+            throw new IllegalArgumentException(String.format("잘못된 입력 값입니다. %d ~ %d 사이의 값을 입력해주세요.", voucherType.getMinimumValue(), voucherType.getMaximumValue()));
+        }
     }
 }
