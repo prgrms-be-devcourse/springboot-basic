7d59d19b52774c798dc96e6b1f82bcbec083ff1d
YongNyeo <won05121@gmail.com>
2023-11-02 13:07:01 +0900

    refactor: profile 수정

--- src/main/java/com/prgms/vouchermanager/VouchermanagerApplication.java
+++ src/main/java/com/prgms/vouchermanager/VouchermanagerApplication.java
@@ -3,17 +3,38 @@ package com.prgms.vouchermanager;
 import com.prgms.vouchermanager.contorller.front.FrontController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 public class VouchermanagerApplication {
 
-    public static void main(String[] args) {
+    private static ApplicationContext ac;
 
-        ApplicationContext ac = SpringApplication.run(VouchermanagerApplication.class, args);
+
+    public VouchermanagerApplication(ConfigurableApplicationContext applicationContext) {
+        this.ac = applicationContext;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(VouchermanagerApplication.class, args);
+    }
+
+    @Profile(value = "tomcat")
+    @Bean
+    public ServletWebServerFactory servletTomcatServerContainer() {
+        return new TomcatServletWebServerFactory();
+    }
+
+    @Profile("console")
+    @Bean
+    public void init() {
         FrontController frontController = ac.getBean(FrontController.class);
         frontController.run();
-
     }
 
 }
