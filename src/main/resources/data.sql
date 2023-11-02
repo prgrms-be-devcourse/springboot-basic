0b8fa3426e89002cd0a48ed1f79b5bb0aa2e88bb
YongNyeo <won05121@gmail.com>
2023-11-02 13:07:34 +0900

    refactor: schema 변경

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,10 +1,17 @@
-insert into vouchers values ('bc2e6a9d-1319-4db0-83ba-e07a85fffab8',22,'PERCENT_DISCOUNT');
-insert into vouchers values ('50976229-81af-4dd2-a36d-0f15a58d6f33',10000,'FIXED_AMOUNT');
+insert into vouchers
+values ('bc2e6a9d-1319-4db0-83ba-e07a85fffab8', 22, 'PERCENT_DISCOUNT', now());
+insert into vouchers
+values ('50976229-81af-4dd2-a36d-0f15a58d6f33', 10000, 'FIXED_AMOUNT', now());
 
 
-insert into customers values (1,'kim','won05121@naver.com',0);
-insert into customers values (2,'park','park@naver.com',1);
-insert into customers values (3,'lee','lee@naver.com',0);
+insert into customers
+values (1, 'kim', 'won05121@naver.com', 0);
+insert into customers
+values (2, 'park', 'park@naver.com', 1);
+insert into customers
+values (3, 'lee', 'lee@naver.com', 0);
 
-insert into wallets values (1,2,'bc2e6a9d-1319-4db0-83ba-e07a85fffab8');
-insert into wallets values (2,3,'50976229-81af-4dd2-a36d-0f15a58d6f33');
+insert into wallets
+values (1, 2, 'bc2e6a9d-1319-4db0-83ba-e07a85fffab8');
+insert into wallets
+values (2, 3, '50976229-81af-4dd2-a36d-0f15a58d6f33');
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -2,26 +2,30 @@ DROP TABLE IF EXISTS wallets;
 DROP TABLE IF EXISTS vouchers;
 DROP TABLE IF EXISTS customers;
 
-CREATE TABLE vouchers(
-                         id varchar(36) primary key ,
-                         discount_value INT,
-                         type varchar(20)
+CREATE TABLE vouchers
+(
+    id             varchar(36) primary key,
+    discount_value INT,
+    type           varchar(20),
+    created_at     datetime default now()
 );
 
 
-CREATE TABLE customers(
-                          id BIGINT primary key auto_increment,
-                          name varchar(10),
-                          email varchar(40),
-                          black_list bool
+CREATE TABLE customers
+(
+    id         BIGINT primary key auto_increment,
+    name       varchar(10),
+    email      varchar(40),
+    black_list bool
 );
 
 
-CREATE TABLE wallets(
-                        id BIGINT primary key auto_increment ,
-                        customer_id BIGINT,
-                        voucher_id VARCHAR(36),
-                        foreign key (customer_id) REFERENCES customers(id) on delete cascade on update cascade,
-                        foreign key (voucher_id) REFERENCES vouchers(id)  on delete cascade on update cascade
+CREATE TABLE wallets
+(
+    id          BIGINT primary key auto_increment,
+    customer_id BIGINT,
+    voucher_id  VARCHAR(36),
+    foreign key (customer_id) REFERENCES customers (id) on delete cascade on update cascade,
+    foreign key (voucher_id) REFERENCES vouchers (id) on delete cascade on update cascade
 );
 
