5057fae32c67e3a6213f4c7d26bf4dd0fb3171b7
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2023-11-02 11:28:34 +0900

    feat: save method at CustomerRepository interface

--- src/main/java/com/programmers/vouchermanagement/customer/repository/CustomerRepository.java
+++ src/main/java/com/programmers/vouchermanagement/customer/repository/CustomerRepository.java
@@ -1,9 +1,10 @@
 package com.programmers.vouchermanagement.customer.repository;
 
-import java.util.List;
-
 import com.programmers.vouchermanagement.customer.domain.Customer;
 
+import java.util.List;
+
 public interface CustomerRepository {
+    void save(Customer customer);
     List<Customer> findAllBlackCustomer();
 }
