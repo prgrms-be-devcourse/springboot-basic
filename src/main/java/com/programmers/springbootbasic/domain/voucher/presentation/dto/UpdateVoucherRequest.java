4a43d7ed83cfe2797d24ab5cd326c413be7abd8e
happyjamy <78072370+happyjamy@users.noreply.github.com>
2023-11-03 21:04:03 +0900

    [change] Nullable 선언

--- src/main/java/com/programmers/springbootbasic/domain/voucher/presentation/dto/UpdateVoucherRequest.java
+++ src/main/java/com/programmers/springbootbasic/domain/voucher/presentation/dto/UpdateVoucherRequest.java
@@ -2,10 +2,12 @@ package com.programmers.springbootbasic.domain.voucher.presentation.dto;
 
 import com.programmers.springbootbasic.domain.voucher.domain.VoucherType.VoucherType;
 import com.programmers.springbootbasic.domain.voucher.domain.VoucherType.VoucherTypeEnum;
+import jakarta.annotation.Nullable;
 
 public class UpdateVoucherRequest {
-
+    @Nullable
     private final VoucherType voucherType;
+    @Nullable
     private final Integer benefitValue;
 
     public UpdateVoucherRequest(VoucherTypeEnum voucherType, Integer benefitValue) {
