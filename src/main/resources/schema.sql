71f944ffcbfe9d834d2389d8012502c19d853039
ebcho <joeunbi514@naver.com>
2023-11-03 12:47:38 +0900

    feature: schema, data.sql 수정

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -11,7 +11,8 @@ CREATE TABLE IF NOT EXISTS voucher
     voucher_id   CHAR(36) PRIMARY KEY,
     voucher_type VARCHAR(30) NOT NULL,
     percent      INT,
-    amount       INT
+    amount       INT,
+    created_at   DATETIME NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS wallet
