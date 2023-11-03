46eb1d559fbb8b7b7d9505598259abcc2650e6d4
수지니 <sssssjin99@naver.com>
2023-11-03 13:03:58 +0900

    [refactor]: Voucher mapper 리팩토링

--- src/main/java/com/prgrms/vouchermanagement/core/voucher/utils/mapper/Mapper.java
+++ src/main/java/com/prgrms/vouchermanagement/core/voucher/utils/mapper/Mapper.java
@@ -1,12 +1,12 @@
 package com.prgrms.vouchermanagement.core.voucher.utils.mapper;
 
-import com.prgrms.vouchermanagement.core.voucher.controller.request.VoucherCreateRequest;
 import com.prgrms.vouchermanagement.core.voucher.domain.Voucher;
+import com.prgrms.vouchermanagement.core.voucher.dto.VoucherDto;
 
 public class Mapper {
 
-    public static Voucher toVoucher(VoucherCreateRequest voucherCreateRequest) {
-        return new Voucher(voucherCreateRequest.getName(), voucherCreateRequest.getAmount(), voucherCreateRequest.getVoucherType());
+    public static Voucher toVoucher(VoucherDto voucherDto) {
+        return new Voucher(voucherDto.getName(), voucherDto.getAmount(), voucherDto.getVoucherType().toString());
     }
 
 }
