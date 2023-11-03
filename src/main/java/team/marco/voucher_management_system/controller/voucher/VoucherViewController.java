3747c37993cc0cb7c6e34869f16260f45e90b1b3
uijin-j <uijin0531@gmail.com>
2023-11-03 18:49:56 +0900

    feat: VoucherViewController에 Voucher 상세 페이지 기능 추가

--- src/main/java/team/marco/voucher_management_system/controller/voucher/VoucherViewController.java
+++ src/main/java/team/marco/voucher_management_system/controller/voucher/VoucherViewController.java
@@ -4,10 +4,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import team.marco.voucher_management_system.controller.voucher.dto.VoucherResponse;
+import team.marco.voucher_management_system.domain.voucher.Voucher;
 import team.marco.voucher_management_system.service.voucher.VoucherService;
 
 import java.util.List;
@@ -34,6 +36,15 @@ public class VoucherViewController {
     }
 
     @GetMapping("/{voucherId}")
+    public String findVoucher(@PathVariable Long voucherId, Model model) {
+        Voucher voucher = voucherService.getVoucher(voucherId);
+
+        model.addAttribute("voucher", voucher);
+
+        return "voucher/voucher_detail";
+    }
+
+    @DeleteMapping("/{voucherId}")
     public String deleteVoucher(@PathVariable Long voucherId, Model model) {
         voucherService.deleteVoucher(voucherId);
 
