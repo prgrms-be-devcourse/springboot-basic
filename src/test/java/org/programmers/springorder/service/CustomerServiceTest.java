08e904d48cba50130331b69b6787dbaf0637b55b
EomYerim <djadpfa1002@naver.com>
2023-11-03 19:20:27 +0900

    refactor: 전체 고객 조회 및 ID로 고객 삭제 테스트 수행

--- src/test/java/org/programmers/springorder/service/CustomerServiceTest.java
+++ src/test/java/org/programmers/springorder/service/CustomerServiceTest.java
@@ -12,9 +12,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @SpringBootTest(properties = "command.line.runner.enabled=false")
 class CustomerServiceTest {
@@ -47,6 +49,23 @@ class CustomerServiceTest {
     }
 
     @Test
+    @DisplayName("전체 회원 조회에 성공한다.")
+    void getAllCustomer() {
+        // given
+        CustomerRequestDto request1 = new CustomerRequestDto("홍길동");
+        CustomerRequestDto request2 = new CustomerRequestDto("세종대왕");
+
+        customerService.createCustomer(request1);
+        customerService.createCustomer(request2);
+
+        // when
+        List<CustomerResponseDto> customerList = customerService.getAllCustomer();
+
+        // then
+        assertThat(customerList).hasSize(2);
+    }
+
+    @Test
     @DisplayName("블랙리스트 조회에 성공한다.")
     void getBlackList() {
         // given
@@ -61,5 +80,45 @@ class CustomerServiceTest {
 
         // then
         assertThat(blacklist).hasSize(1);
+    }
+
+    @Test
+    @DisplayName("고객 ID가 존재하지 않으면, 에러 메시지를 띄운다.")
+    void findByIdFail() {
+        // given
+        UUID findCustomerId = UUID.randomUUID();
+
+        // then
+        assertThatThrownBy(() -> customerService.findById(findCustomerId))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("고객 ID가 존재하지 않습니다.");
+    }
+
+    @Test
+    @DisplayName("고객 삭제에 성공한다.")
+    void deleteCustomer() {
+        // given
+        CustomerRequestDto request = new CustomerRequestDto("홍길동");
+        Customer customer = customerService.createCustomer(request);
+        UUID savedCustomerId = customer.getCustomerId();
+
+        // when
+        customerService.deleteCustomer(savedCustomerId);
+        Optional<Customer> findCustomer = customerRepository.findById(savedCustomerId);
+
+        // then
+        assertThat(findCustomer).isNotPresent();
+    }
+
+    @Test
+    @DisplayName("고객 ID가 존재하지 않으면, 삭제에 실패하고 에러 메시지를 띄운다.")
+    void deleteCustomerFail() {
+        // given
+        UUID findCustomerId = UUID.randomUUID();
+
+        // then
+        assertThatThrownBy(() -> customerService.deleteCustomer(findCustomerId))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("고객 ID가 존재하지 않습니다.");
     }
 }
