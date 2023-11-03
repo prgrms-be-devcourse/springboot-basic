8dfc1e31ed889ebf8aa176ef1a04778955f5a155
EomYerim <djadpfa1002@naver.com>
2023-11-03 18:05:51 +0900

    refactor: 바우처 View Controller 리팩토링

--- src/main/java/org/programmers/springorder/controller/VoucherViewController.java
+++ src/main/java/org/programmers/springorder/controller/VoucherViewController.java
@@ -1,19 +1,12 @@
 package org.programmers.springorder.controller;
 
-import jakarta.annotation.PostConstruct;
 import org.programmers.springorder.dto.voucher.VoucherRequestDto;
 import org.programmers.springorder.dto.voucher.VoucherResponseDto;
 import org.programmers.springorder.model.voucher.Voucher;
-import org.programmers.springorder.model.voucher.VoucherType;
 import org.programmers.springorder.service.VoucherService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
@@ -22,7 +15,6 @@ import java.util.UUID;
 @RequestMapping("/vouchers")
 public class VoucherViewController {
 
-    private final Logger log = LoggerFactory.getLogger(VoucherViewController.class);
     private final VoucherService voucherService;
 
     public VoucherViewController(VoucherService voucherService) {
@@ -49,7 +41,7 @@ public class VoucherViewController {
     }
 
     @PostMapping("/new-voucher")
-    public String newVoucher(VoucherRequestDto request) {   // TODO: discountValue Validation 필요
+    public String newVoucher(@ModelAttribute VoucherRequestDto request) {   // TODO: discountValue Validation 필요
         voucherService.createVoucher(request);
         return "redirect:/vouchers";
     }
@@ -58,13 +50,5 @@ public class VoucherViewController {
     public String deleteVoucher(@PathVariable UUID id) {
         voucherService.deleteVoucher(id);
         return "redirect:/vouchers";
-    }
-
-    @PostConstruct  // TODO: 삭제하기
-    public void init() {
-        VoucherRequestDto request1 = new VoucherRequestDto(1000, VoucherType.FIXED);
-        VoucherRequestDto request2 = new VoucherRequestDto(10, VoucherType.PERCENT);
-        voucherService.createVoucher(request1);
-        voucherService.createVoucher(request2);
     }
 }
