6ea78d81ecb4d91b9702461d7c0757cfcf00d321
hs12 <loveanna1228@gmail.com>
2023-11-03 21:47:45 +0900

    [Feat] 뷰 버튼 추가

--- src/main/java/com/prgrms/voucher_manage/domain/voucher/controller/VoucherViewController.java
+++ src/main/java/com/prgrms/voucher_manage/domain/voucher/controller/VoucherViewController.java
@@ -6,7 +6,10 @@ import com.prgrms.voucher_manage.domain.voucher.service.VoucherService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 import java.util.UUID;
