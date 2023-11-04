f3a6e484ebae5cd65105c3bf1e5749c992bae51a
Jay <injaesong0@gmail.com>
2023-11-04 21:52:27 +0900

    feat: implement create voucher controller

--- src/main/java/com/programmers/vouchermanagement/voucher/controller/VoucherAdminController.java
+++ src/main/java/com/programmers/vouchermanagement/voucher/controller/VoucherAdminController.java
@@ -11,9 +11,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.programmers.vouchermanagement.voucher.domain.Voucher;
+import com.programmers.vouchermanagement.voucher.dto.CreateVoucherRequest;
 import com.programmers.vouchermanagement.voucher.dto.UpdateVoucherRequest;
 import com.programmers.vouchermanagement.voucher.dto.VoucherResponse;
 import com.programmers.vouchermanagement.voucher.service.VoucherService;
@@ -32,6 +33,19 @@ public class VoucherAdminController {
         List<VoucherResponse> vouchers = voucherService.readAllVouchers();
         model.addAttribute("vouchers", vouchers);
         return "vouchers/vouchers";
+    }
+
+    @GetMapping("/create")
+    public String create(Model model) {
+        model.addAttribute("voucher", new Voucher());
+        return "vouchers/voucher";
+    }
+
+    @PostMapping("/create")
+    public String create(@ModelAttribute CreateVoucherRequest request, Model model) {
+        VoucherResponse voucher = voucherService.create(request);
+        model.addAttribute("voucher", voucher);
+        return "redirect:/vouchers/" + voucher.voucherId();
     }
 
     @GetMapping("/{voucherId}")
