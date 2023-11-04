a1f83b4def513fd5f2990bcb3da7fa96482e3d9f
Jay <injaesong0@gmail.com>
2023-11-04 11:29:33 +0900

    enhance: attach response status

--- src/main/java/com/programmers/vouchermanagement/voucher/controller/VoucherRestController.java
+++ src/main/java/com/programmers/vouchermanagement/voucher/controller/VoucherRestController.java
@@ -5,12 +5,14 @@ import java.util.UUID;
 
 import jakarta.validation.Valid;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.programmers.vouchermanagement.voucher.domain.VoucherType;
@@ -29,32 +31,38 @@ public class VoucherRestController {
     }
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public VoucherResponse create(@Valid CreateVoucherRequest createVoucherRequest) {
         return voucherService.create(createVoucherRequest);
     }
 
     @GetMapping
+    @ResponseStatus(HttpStatus.OK)
     public List<VoucherResponse> readAllVouchers() {
         return voucherService.readAllVouchers();
     }
 
     @GetMapping("/{voucherId}")
+    @ResponseStatus(HttpStatus.OK)
     public VoucherResponse findById(@PathVariable UUID voucherId) {
         return voucherService.findById(voucherId);
     }
 
     @DeleteMapping("/{voucherId}")
+    @ResponseStatus(HttpStatus.ACCEPTED)
     public void deleteById(@PathVariable UUID voucherId) {
         voucherService.deleteById(voucherId);
     }
 
     @GetMapping("/type/{typeName}")
+    @ResponseStatus(HttpStatus.OK)
     public List<VoucherResponse> findByType(@PathVariable String typeName) {
         VoucherType voucherType = VoucherType.findVoucherTypeByName(typeName);
         return voucherService.findByType(voucherType);
     }
 
     @GetMapping("/creation-date")
+    @ResponseStatus(HttpStatus.OK)
     public List<VoucherResponse> findByCreatedAt(@Valid SearchCreatedAtRequest request) {
         return voucherService.findByCreatedAt(request);
     }
