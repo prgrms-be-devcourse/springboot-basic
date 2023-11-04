4687ebeae46a2096d6a89706a48a4ae6c24d0411
Jay <injaesong0@gmail.com>
2023-11-04 21:27:12 +0900

    enhance: add validation to UpdateVoucherRequest

--- src/main/java/com/programmers/vouchermanagement/voucher/dto/UpdateVoucherRequest.java
+++ src/main/java/com/programmers/vouchermanagement/voucher/dto/UpdateVoucherRequest.java
@@ -2,5 +2,11 @@ package com.programmers.vouchermanagement.voucher.dto;
 
 import java.util.UUID;
 
-public record UpdateVoucherRequest(UUID voucherId, long discountValue, String voucherType) {
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+
+public record UpdateVoucherRequest(
+        UUID voucherId,
+        @Min(value = 0, message = "Discount value must be greater than 0.") long discountValue,
+        @NotBlank(message = "Voucher type cannot be blank.") String voucherType) {
 }
