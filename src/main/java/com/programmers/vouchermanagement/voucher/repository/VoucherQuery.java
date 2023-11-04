54cb574d5cefb4bcb9dc212c47809294d3ae2da9
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2023-11-04 20:31:30 +0900

    feat: FIND_ALL_BY_TYPE query at VoucherQuery

--- src/main/java/com/programmers/vouchermanagement/voucher/repository/VoucherQuery.java
+++ src/main/java/com/programmers/vouchermanagement/voucher/repository/VoucherQuery.java
@@ -5,6 +5,7 @@ public class VoucherQuery {
     public static final String FIND_BY_ID = "SELECT * FROM test.vouchers WHERE id = UUID_TO_BIN(:id)";
     public static final String FIND_ALL = "SELECT * FROM test.vouchers";
     public static final String FIND_ALL_BY_CREATED_AT = "SELECT * FROM test.vouchers WHERE created_at BETWEEN DATE(:from) AND DATE(:to)";
+    public static final String FIND_ALL_BY_TYPE = "SELECT * FROM test.vouchers WHERE type = :type";
     public static final String DELETE_VOUCHER = "DELETE FROM test.vouchers WHERE id = UUID_TO_BIN(:id)";
     public static final String DELETE_ALL = "DELETE FROM test.vouchers";
     public static final String UPDATE_VOUCHER = "UPDATE test.vouchers SET type = :type, discount_value = :discount_value WHERE id = UUID_TO_BIN(:id)";
