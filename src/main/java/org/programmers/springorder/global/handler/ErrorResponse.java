615daebd2b7376d4215d5cf73f0bf806da359b67
EomYerim <djadpfa1002@naver.com>
2023-11-04 23:56:18 +0900

    feat: 전역 예외 핸들러 생성

---
+++ src/main/java/org/programmers/springorder/global/handler/ErrorResponse.java
@@ -0,0 +1,20 @@
+package org.programmers.springorder.global.handler;
+
+public class ErrorResponse {
+
+    private final String code;
+    private final String message;
+
+    public ErrorResponse(String code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getCode() {
+        return code;
+    }
+}
