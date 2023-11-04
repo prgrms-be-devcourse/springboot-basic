e9e2d157a82ca999dcbce89620cfc29fb8236a68
JIN-076 <andy230@naver.com>
2023-11-04 17:30:37 +0900

    init: 프로젝트 초기 세팅

--- src/main/java/org/programmers/springboot/basic/domain/voucher/dto/CsvVoucherDto.java
+++
@@ -1,8 +0,0 @@
-package org.programmers.springboot.basic.domain.voucher.dto;
-
-import org.programmers.springboot.basic.domain.voucher.entity.VoucherType;
-
-import java.util.UUID;
-
-public record CsvVoucherDto(UUID voucherId, VoucherType voucherType, Long discount) {
-}
