dafa627c02fb19404ec6e9c86e8fd9dc73b151c2
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2023-11-04 18:24:07 +0900

    refactor: add voucher create time field at VoucherDto

--- src/main/java/com/programmers/vouchermanagement/voucher/dto/VoucherDto.java
+++ src/main/java/com/programmers/vouchermanagement/voucher/dto/VoucherDto.java
@@ -3,11 +3,13 @@ package com.programmers.vouchermanagement.voucher.dto;
 import com.programmers.vouchermanagement.voucher.domain.Voucher;
 import com.programmers.vouchermanagement.voucher.domain.VoucherType;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
-public record VoucherDto(UUID id, long discountValue, String voucherTypeName, boolean isPercent) {
+public record VoucherDto(UUID id, long discountValue, String voucherTypeName, boolean isPercent,
+                         LocalDateTime createdAt) {
     public static VoucherDto from(Voucher voucher) {
         VoucherType voucherType = voucher.voucherType();
-        return new VoucherDto(voucher.voucherId(), voucher.discountValue(), voucherType.displayTypeName(), voucherType.isPercent());
+        return new VoucherDto(voucher.voucherId(), voucher.discountValue(), voucherType.displayTypeName(), voucherType.isPercent(), voucher.createdAt());
     }
 }
