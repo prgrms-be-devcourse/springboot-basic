abcf986022e03de516a1323d795b16c26ef2ebf8
JIN-076 <andy230@naver.com>
2023-11-04 17:45:20 +0900

    feat: CustomerType 관련 ENUM 클래스 추가

--- src/main/java/org/programmers/springboot/basic/domain/customer/entity/CustomerType.java
+++ src/main/java/org/programmers/springboot/basic/domain/customer/entity/CustomerType.java
@@ -1,7 +1,29 @@
 package org.programmers.springboot.basic.domain.customer.entity;
 
+import org.programmers.springboot.basic.domain.customer.exception.CustomerNotFoundException;
+
+import java.util.Arrays;
+
 public enum CustomerType {
 
-    NORMAL,
-    BLACK
+    NORMAL(0),
+    BLACK(1);
+
+    private final int value;
+
+    CustomerType(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return this.value;
+    }
+
+    public static CustomerType valueOf(int value) {
+
+        return Arrays.stream(values())
+                .filter(customer -> customer.getValue() == value)
+                .findAny()
+                .orElseThrow(CustomerNotFoundException::new);
+    }
 }
