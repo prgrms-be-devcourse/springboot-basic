e0eb63e7aef7ff1f8b8df864db4246eaa0ce87ad
JIN-076 <andy230@naver.com>
2023-11-04 17:51:03 +0900

    feat: Customer 도메인 클래스, TDA 적용을 위해 상태 변경의 set 메서드와 equal 메서드 추가

--- src/main/java/org/programmers/springboot/basic/domain/customer/entity/Customer.java
+++ src/main/java/org/programmers/springboot/basic/domain/customer/entity/Customer.java
@@ -1,28 +1,45 @@
 package org.programmers.springboot.basic.domain.customer.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import org.programmers.springboot.basic.domain.customer.entity.vo.Email;
+import org.programmers.springboot.basic.domain.customer.exception.BlackCustomerNotFoundException;
+import org.programmers.springboot.basic.domain.customer.exception.DuplicateBlackCustomerException;
+
 import java.util.UUID;
 
+@Slf4j
+@Getter
+@AllArgsConstructor
 public class Customer {
 
     private final UUID customerId;
     private final String name;
-    private final CustomerType customerType;
+    private final Email email;
+    private CustomerType customerType;
 
-    public Customer(UUID customerId, String name, CustomerType customerType) {
-        this.customerId = customerId;
-        this.name = name;
-        this.customerType = customerType;
+    public int getCustomerTypeValue() {
+        return this.customerType.getValue();
     }
 
-    public UUID getCustomerId() {
-        return this.customerId;
+    public boolean equalType(CustomerType type) {
+        return this.customerType.equals(type);
     }
 
-    public String getName() {
-        return this.name;
+    public void setBlack() {
+        if (this.equalType(CustomerType.BLACK)) {
+            log.warn("customer of customerId {} is already exists from blacklist", this.customerId);
+            throw new DuplicateBlackCustomerException();
+        }
+        this.customerType = CustomerType.BLACK;
     }
 
-    public CustomerType getCustomerType() {
-        return this.customerType;
+    public void setNormal() {
+        if (this.equalType(CustomerType.NORMAL)) {
+            log.warn("customer of customerId {} is not exists from blacklist", this.customerId);
+            throw new BlackCustomerNotFoundException();
+        }
+        this.customerType = CustomerType.NORMAL;
     }
 }
