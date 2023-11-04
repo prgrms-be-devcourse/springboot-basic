34ffcff452c0db2d4ab80410807c81630f52d5b6
JIN-076 <andy230@naver.com>
2023-11-04 18:12:00 +0900

    feat: 중복 체크를 위한 Duplicate 메서드, TDA를 적용한 Email 검증 메서드 추가

--- src/main/java/org/programmers/springboot/basic/domain/customer/service/CustomerService.java
+++ src/main/java/org/programmers/springboot/basic/domain/customer/service/CustomerService.java
@@ -1,38 +1,125 @@
 package org.programmers.springboot.basic.domain.customer.service;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.programmers.springboot.basic.config.CustomerConfig;
+import org.programmers.springboot.basic.domain.customer.dto.CustomerRequestDto;
 import org.programmers.springboot.basic.domain.customer.dto.CustomerResponseDto;
 import org.programmers.springboot.basic.domain.customer.entity.Customer;
-import org.programmers.springboot.basic.domain.customer.exception.CustomerNotFoundException;
+import org.programmers.springboot.basic.domain.customer.entity.vo.Email;
+import org.programmers.springboot.basic.domain.customer.exception.*;
+import org.programmers.springboot.basic.domain.customer.mapper.CustomerMapper;
 import org.programmers.springboot.basic.domain.customer.repository.CustomerRepository;
+import org.programmers.springboot.basic.util.generator.UUIDGenerator;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.UUID;
 
 @Slf4j
 @Service
+@RequiredArgsConstructor
 public class CustomerService {
 
     private final CustomerRepository customerRepository;
-    private final CustomerConfig customerConfig;
+    private final UUIDGenerator uuidGenerator;
 
-    public CustomerService(CustomerRepository customerRepository, CustomerConfig customerConfig) {
-        this.customerRepository = customerRepository;
-        this.customerConfig = customerConfig;
+    @Transactional
+    public void create(CustomerRequestDto customerRequestDto) {
+
+        Email email = customerRequestDto.getEmail();
+
+        email.validate();
+        Customer customer = CustomerMapper.INSTANCE.mapToEntityAllArgsWithGenerator(customerRequestDto, uuidGenerator);
+
+        if (isDuplicate(customer)) {
+            log.warn("Duplicate email {} is already exists", email);
+            throw new DuplicateEmailException();
+        } else {
+            customerRepository.save(customer);
+        }
     }
 
-    public List<CustomerResponseDto> findBlackList() {
+    private boolean isDuplicate(Customer customer) {
+        return customerRepository.findByEmail(customer.getEmail()).isPresent();
+    }
 
-        List<Customer> customers = this.customerRepository.findBlackList();
+    public List<CustomerResponseDto> findAllCustomer() {
 
-        if (customers.isEmpty()) {
-            throw new CustomerNotFoundException();
-        }
-
+        List<Customer> customers = findAll();
         return customers.stream()
-                .map(customerConfig::getCustomerResponseDto)
-                .collect(Collectors.toList());
+                .map(CustomerMapper.INSTANCE::mapToResponseDto)
+                .toList();
+    }
+
+    public List<CustomerResponseDto> findByCustomerIsBlack() {
+
+        List<Customer> customers = findBlackCustomer();
+        return customers.stream()
+                .map(CustomerMapper.INSTANCE::mapToResponseDto)
+                .toList();
+    }
+
+    @Transactional
+    public void addBlackCustomer(CustomerRequestDto customerRequestDto) {
+
+        UUID customerId = customerRequestDto.getCustomerId();
+
+        Customer customer = findById(customerId);
+        customer.setBlack();
+        customerRepository.update(customer);
+    }
+
+    @Transactional
+    public void deleteCustomer(CustomerRequestDto customerRequestDto) {
+
+        UUID customerId = customerRequestDto.getCustomerId();
+
+        Customer customer = findById(customerId);
+        customerRepository.delete(customer);
+    }
+
+    @Transactional
+    public void deleteBlackCustomer(CustomerRequestDto customerRequestDto) {
+
+        UUID customerId = customerRequestDto.getCustomerId();
+
+        Customer customer = findById(customerId);
+        customer.setNormal();
+        customerRepository.update(customer);
+    }
+
+    public Customer findById(UUID customerId) {
+
+        return customerRepository.findById(customerId)
+                .orElseThrow(() -> {
+                    log.warn("No customer found for customerId: {}", customerId);
+                    return new CustomerNotFoundException();
+                });
+    }
+
+    public CustomerResponseDto findByEmail(Email email) {
+
+        email.validate();
+        Customer customer = customerRepository.findByEmail(email)
+                .orElseThrow(() -> {
+                    log.warn("No customer found for email: {}", email);
+                    return new CustomerNotFoundException();
+                });
+        return CustomerMapper.INSTANCE.mapToResponseDto(customer);
+    }
+
+
+    public List<Customer> findAll() {
+        return customerRepository.findAll();
+    }
+
+    public List<Customer> findBlackCustomer() {
+        return customerRepository.findBlack();
+    }
+
+    @Transactional
+    public void deleteAll() {
+        customerRepository.deleteAll();
     }
 }
