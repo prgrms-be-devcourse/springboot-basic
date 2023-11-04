eab97e181296b3807365000930e0ae862eb310cc
hs12 <loveanna1228@gmail.com>
2023-11-04 18:00:53 +0900

    [Refactor] Voucher DTO 로직 분리

--- src/main/java/com/prgrms/voucher_manage/domain/voucher/controller/dto/UpdateVoucherReq.java
+++ src/main/java/com/prgrms/voucher_manage/domain/voucher/controller/dto/UpdateVoucherReq.java
@@ -1,25 +1,15 @@
 package com.prgrms.voucher_manage.domain.voucher.controller.dto;
 
-import com.prgrms.voucher_manage.domain.voucher.entity.FixedAmountVoucher;
-import com.prgrms.voucher_manage.domain.voucher.entity.PercentDiscountVoucher;
-import com.prgrms.voucher_manage.domain.voucher.entity.Voucher;
 import com.prgrms.voucher_manage.domain.voucher.entity.VoucherType;
 
 import java.util.UUID;
 
-import static com.prgrms.voucher_manage.domain.voucher.entity.VoucherType.FIXED;
-
 
 public record UpdateVoucherReq(
-    UUID id,
-    VoucherType type,
-    Long discountAmount
-)
-{
-    public Voucher of() {
-        if (type == FIXED) {
-            return new FixedAmountVoucher(id, discountAmount);
-        }
-        return new PercentDiscountVoucher(id, discountAmount);
-    }
+        UUID id,
+        VoucherType type,
+        Long discountAmount
+
+) {
 }
+
--- src/main/java/com/prgrms/voucher_manage/domain/voucher/service/VoucherService.java
+++ src/main/java/com/prgrms/voucher_manage/domain/voucher/service/VoucherService.java
@@ -2,7 +2,10 @@ package com.prgrms.voucher_manage.domain.voucher.service;
 
 import com.prgrms.voucher_manage.domain.voucher.controller.dto.CreateVoucherReq;
 import com.prgrms.voucher_manage.domain.voucher.controller.dto.UpdateVoucherReq;
+import com.prgrms.voucher_manage.domain.voucher.entity.FixedAmountVoucher;
+import com.prgrms.voucher_manage.domain.voucher.entity.PercentDiscountVoucher;
 import com.prgrms.voucher_manage.domain.voucher.entity.Voucher;
+import com.prgrms.voucher_manage.domain.voucher.entity.VoucherType;
 import com.prgrms.voucher_manage.domain.voucher.repository.VoucherRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -10,6 +13,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.UUID;
 
+import static com.prgrms.voucher_manage.domain.voucher.entity.VoucherType.FIXED;
 import static com.prgrms.voucher_manage.exception.ErrorMessage.*;
 
 
@@ -19,7 +23,7 @@ public class VoucherService {
     private final VoucherRepository voucherRepository;
 
     public Voucher createVoucher(CreateVoucherReq dto) {
-        return voucherRepository.save(dto.of());
+        return voucherRepository.save(getVoucherEntity(dto.type(), dto.discountAmount()));
     }
 
     public List<Voucher> getVouchers() {
@@ -35,10 +39,25 @@ public class VoucherService {
     }
 
     public void updateVoucher(UpdateVoucherReq dto) {
-        voucherRepository.update(dto.of());
+        voucherRepository.update(getVoucherEntity(dto.id(), dto.type(), dto.discountAmount()));
     }
 
     public void deleteVoucher(UUID voucherId) {
         voucherRepository.deleteById(voucherId);
+    }
+    public Voucher getVoucherEntity(UUID id, VoucherType type, Long discountAmount){
+        if (type == FIXED) {
+            return new FixedAmountVoucher(id, discountAmount);
+        } else {
+            return new PercentDiscountVoucher(id, discountAmount);
+        }
+    }
+
+    public Voucher getVoucherEntity(VoucherType type, Long discountAmount){
+        if (type == FIXED) {
+            return new FixedAmountVoucher(discountAmount);
+        } else {
+            return new PercentDiscountVoucher(discountAmount);
+        }
     }
 }
