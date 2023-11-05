ec70b009a9fd4f67601c37d89655d9b294b530c8
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2023-11-05 23:41:29 +0900

    refactor: fix query

--- src/main/java/com/programmers/vouchermanagement/customer/repository/CustomerQuery.java
+++ src/main/java/com/programmers/vouchermanagement/customer/repository/CustomerQuery.java
@@ -1,9 +1,10 @@
 package com.programmers.vouchermanagement.customer.repository;
 
 public class CustomerQuery {
-    public static final String FIND_ALL_BLACK_CUSTOMER = "SELECT * FROM test.customers WHERE black = TRUE";
-    public static final String INSERT = "INSERT INTO test.customers(id, name, black) VALUES (UUID_TO_BIN(:id), :name, :black)";
-    public static final String FIND_ALL = "SELECT * FROM test.customers";
+    public static final String FIND_ALL_BLACK_CUSTOMER = "SELECT * FROM customers WHERE black = TRUE";
+    public static final String INSERT = "INSERT INTO customers(id, name, black) VALUES (UUID_TO_BIN(:id), :name, :black)";
+    public static final String FIND_ALL = "SELECT * FROM customers";
+
     private CustomerQuery() {
     }
 }
--- src/main/java/com/programmers/vouchermanagement/voucher/repository/VoucherQuery.java
+++ src/main/java/com/programmers/vouchermanagement/voucher/repository/VoucherQuery.java
@@ -1,14 +1,14 @@
 package com.programmers.vouchermanagement.voucher.repository;
 
 public class VoucherQuery {
-    public static final String INSERT = "INSERT INTO test.vouchers(id, type, discount_value) VALUES (UUID_TO_BIN(:id), :type, :discount_value)";
-    public static final String FIND_BY_ID = "SELECT * FROM test.vouchers WHERE id = UUID_TO_BIN(:id)";
-    public static final String FIND_ALL = "SELECT * FROM test.vouchers";
-    public static final String FIND_ALL_BY_CREATED_AT = "SELECT * FROM test.vouchers WHERE created_at BETWEEN DATE(:from) AND DATE(:to)";
-    public static final String FIND_ALL_BY_TYPE = "SELECT * FROM test.vouchers WHERE type = :type";
-    public static final String DELETE_VOUCHER = "DELETE FROM test.vouchers WHERE id = UUID_TO_BIN(:id)";
-    public static final String DELETE_ALL = "DELETE FROM test.vouchers";
-    public static final String UPDATE_VOUCHER = "UPDATE test.vouchers SET type = :type, discount_value = :discount_value WHERE id = UUID_TO_BIN(:id)";
+    public static final String INSERT = "INSERT INTO vouchers(id, type, discount_value) VALUES (UUID_TO_BIN(:id), :type, :discount_value)";
+    public static final String FIND_BY_ID = "SELECT * FROM vouchers WHERE id = UUID_TO_BIN(:id)";
+    public static final String FIND_ALL = "SELECT * FROM vouchers";
+    public static final String FIND_ALL_BY_CREATED_AT = "SELECT * FROM vouchers WHERE created_at BETWEEN DATE(:from) AND DATE(:to)";
+    public static final String FIND_ALL_BY_TYPE = "SELECT * FROM vouchers WHERE type = :type";
+    public static final String DELETE_VOUCHER = "DELETE FROM vouchers WHERE id = UUID_TO_BIN(:id)";
+    public static final String DELETE_ALL = "DELETE FROM vouchers";
+    public static final String UPDATE_VOUCHER = "UPDATE vouchers SET type = :type, discount_value = :discount_value WHERE id = UUID_TO_BIN(:id)";
     private VoucherQuery() {
     }
 }
