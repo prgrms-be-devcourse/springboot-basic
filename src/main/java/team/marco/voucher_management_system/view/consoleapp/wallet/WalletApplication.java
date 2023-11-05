06dad6dbf0d5ad2e7e5ddde02c5132c868c57c73
uijin-j <uijin0531@gmail.com>
2023-11-05 21:55:16 +0900

    feat: 콘솔 지갑 서비스 "준비중"으로 변경
    기능 구현이 완료되면, 다시 서비스

--- src/main/java/team/marco/voucher_management_system/view/consoleapp/wallet/WalletApplication.java
+++ src/main/java/team/marco/voucher_management_system/view/consoleapp/wallet/WalletApplication.java
@@ -1,7 +1,5 @@
 package team.marco.voucher_management_system.view.consoleapp.wallet;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import team.marco.voucher_management_system.controller.customer.CustomerController;
 import team.marco.voucher_management_system.controller.voucher.VoucherController;
@@ -16,12 +14,13 @@ import static team.marco.voucher_management_system.view.consoleapp.ConsoleUtil.*
 
 @Component
 public class WalletApplication {
-    private static final Logger logger = LoggerFactory.getLogger(WalletApplication.class);
     public static final String  CUSTOMER_EMAIL_REQUEST = "사용자 이메일을 입력해주세요.";
     public static final String WALLET_HEADER = "==== 지갑 페이지 ====";
     public static final String SELECT_SERVICE = "Q. 이용하실 서비스를 선택해 주세요.";
     public static final String VOUCHER_ID_REQUEST = "쿠폰 번호를 입력해 주세요.";
     public static final String  VOUCHER_DELETE_COMPLETE = "쿠폰 삭제가 완료되었습니다.";
+    // Preparing for service
+    private static final String PREPARING_FOR_SERVICE = "현재 지갑 서비스를 준비 중입니다.";
 
     private final VoucherController voucherController;
     private final CustomerController customerController;
@@ -36,18 +35,21 @@ public class WalletApplication {
     }
 
     public void run() {
-        isRunning = true;
-        customerId = validateCustomer();
+        print(PREPARING_FOR_SERVICE);
+        println();
 
-        while (isRunning) {
-            try {
-                provideCommandManual();
-                WalletCommandType input = getCommandType();
-                handleCommand(input);
-            } catch (IllegalArgumentException e) {
-                handleException(e);
-            }
-        }
+//        isRunning = true;
+//        customerId = validateCustomer();
+//
+//        while (isRunning) {
+//            try {
+//                provideCommandManual();
+//                WalletCommandType input = getCommandType();
+//                handleCommand(input);
+//            } catch (IllegalArgumentException e) {
+//                handleException(e);
+//            }
+//        }
     }
 
     private UUID validateCustomer() {
