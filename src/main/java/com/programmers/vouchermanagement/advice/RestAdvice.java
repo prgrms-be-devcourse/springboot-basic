609c3c7656060de641e8fe1968e648495dbf16c8
Jay <injaesong0@gmail.com>
2023-11-05 22:39:19 +0900

    feat: add general advice for thymeleaf error page

--- src/main/java/com/programmers/vouchermanagement/advice/RestAdvice.java
+++ src/main/java/com/programmers/vouchermanagement/advice/RestAdvice.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
-public class ControllerAdvice {
+public class RestAdvice {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
