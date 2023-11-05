6ae254f61c748844bf97c02f76e23a2d52748802
shoeone96 <dlwnddnjs96@gmail.com>
2023-11-05 22:53:37 +0900

    refactor: dto record -> class로 재 변경

--- src/test/java/org/programmers/springorder/voucher/service/VoucherServiceTest.java
+++ src/test/java/org/programmers/springorder/voucher/service/VoucherServiceTest.java
@@ -70,7 +70,7 @@ class VoucherServiceTest {
         voucherRepository.save(Voucher.toVoucher(uuids.get(3), 2000, VoucherType.FIXED));
 
         List<VoucherResponseDto> allVoucher = voucherService.getAllVoucher();
-        List<UUID> rs = allVoucher.stream().map(VoucherResponseDto::voucherId).toList();
+        List<UUID> rs = allVoucher.stream().map(VoucherResponseDto::getVoucherId).toList();
 
         assertThat(allVoucher).hasSize(4);
         assertThat(rs.containsAll(uuids))
@@ -191,7 +191,7 @@ class VoucherServiceTest {
             List<VoucherResponseDto> customerOwnedVouchers = voucherService.getCustomerOwnedVouchers(customer.getCustomerId());
             List<UUID> foundVoucherId = customerOwnedVouchers
                     .stream()
-                    .map(VoucherResponseDto::voucherId)
+                    .map(VoucherResponseDto::getVoucherId)
                     .toList();
 
             assertThat(customerOwnedVouchers).hasSize(2);
