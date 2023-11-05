fcd1e1cf6d385272ec69a5b4882338185bfd4fa8
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2023-11-05 18:27:56 +0900

    refactor: add jdbc profile and separate useless Bean in jdbc profile

--- src/main/java/com/programmers/vouchermanagement/configuration/JdbcConfig.java
+++ src/main/java/com/programmers/vouchermanagement/configuration/JdbcConfig.java
@@ -1,21 +1,16 @@
 package com.programmers.vouchermanagement.configuration;
 
-import org.beryx.textio.TextIO;
-import org.beryx.textio.TextIoFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
 import javax.sql.DataSource;
 
+@Profile("jdbc")
 @Configuration
-public class AppConfig {
-    @Bean
-    public TextIO textIO() {
-        return TextIoFactory.getTextIO();
-    }
-
+public class JdbcConfig {
     @Bean
     public JdbcTemplate jdbcTemplate(DataSource dataSource) {
         return new JdbcTemplate(dataSource);
