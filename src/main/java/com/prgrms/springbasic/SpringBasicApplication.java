a801dc5af586e5360af06e1569d371ac42ead575
byulcode <bybit0408@gmail.com>
2023-11-05 21:05:53 +0900

    refactor : hiddenHttpMethodFilter 빈 등록 제거

--- src/main/java/com/prgrms/springbasic/SpringBasicApplication.java
+++ src/main/java/com/prgrms/springbasic/SpringBasicApplication.java
@@ -2,18 +2,11 @@ package com.prgrms.springbasic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.filter.HiddenHttpMethodFilter;
 
 @SpringBootApplication
 public class SpringBasicApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBasicApplication.class, args);
-    }
-
-    @Bean
-    public HiddenHttpMethodFilter hiddenHttpMethodFilter() {
-        return new HiddenHttpMethodFilter();
     }
 }
