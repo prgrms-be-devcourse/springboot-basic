cdc57aff43be5d7fd2dda1f5bf5c1a63bcbe0fb6
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2023-11-05 20:42:18 +0900

    fix: set convention

--- src/main/java/com/programmers/vouchermanagement/util/Message.java
+++ src/main/java/com/programmers/vouchermanagement/util/Message.java
@@ -1,10 +1,10 @@
 package com.programmers.vouchermanagement.util;
 
 public class Message {
-    public final static String NOT_INSERTED = "Noting was inserted!";
-    public final static String NOT_UPDATED = "Noting was updated!";
-    public final static String NOT_DELETED = "Noting was deleted!";
-    public final static String EMPTY_RESULT = "Got empty result!";
+    public static final String NOT_INSERTED = "Noting was inserted!";
+    public static final String NOT_UPDATED = "Noting was updated!";
+    public static final String NOT_DELETED = "Noting was deleted!";
+    public static final String EMPTY_RESULT = "Got empty result!";
     public static final String IO_EXCEPTION = "Error raised while reading file!";
     public static final String NOT_FOUND_VOUCHER = "There is no such voucher.";
     public static final String NOT_FOUND_CUSTOMER = "There is no such customer.";
@@ -14,6 +14,7 @@ public class Message {
     public static final String ALREADY_EMPTY_TABLE = "The table is already empty.";
     public static final String FILE_EXCEPTION = "Error raised while opening the file.";
     public static final String INVALID_VOUCHER_TYPE = "Voucher type should be either fixed amount or percent discount voucher.";
+
     private Message() {
     }
 }
