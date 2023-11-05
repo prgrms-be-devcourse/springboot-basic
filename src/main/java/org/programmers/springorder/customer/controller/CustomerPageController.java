1f0a4145283f4aa97f41292997b5d05606c31b57
shoeone96 <dlwnddnjs96@gmail.com>
2023-11-05 16:24:07 +0900

    feat: 고객 생성 페이지 및 생성 api 추가

--- src/main/java/org/programmers/springorder/customer/controller/CustomerPageController.java
+++ src/main/java/org/programmers/springorder/customer/controller/CustomerPageController.java
@@ -1,11 +1,13 @@
 package org.programmers.springorder.customer.controller;
 
 import org.programmers.springorder.console.Console;
+import org.programmers.springorder.customer.dto.CustomerRequestDto;
 import org.programmers.springorder.customer.dto.CustomerResponseDto;
 import org.programmers.springorder.customer.service.CustomerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -25,6 +27,17 @@ public class CustomerPageController {
         List<CustomerResponseDto> allCustomers = customerService.getAllCustomers();
         model.addAttribute("customerList", allCustomers);
         return "customers";
+    }
+
+    @GetMapping("/new-customer")
+    public String getNewCustomerPage(Model model){
+        return "new-customer";
+    }
+
+    @PostMapping("/customers")
+    public String enrollCustomer(CustomerRequestDto requestDto){
+        customerService.newCustomer(requestDto);
+        return "redirect:/customers";
     }
 
 }
