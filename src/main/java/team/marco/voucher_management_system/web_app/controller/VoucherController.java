8c7aada9548b91acc39d1e526d5746003750b5d3
ASak1104 <khw56184@gmail.com>
2023-11-06 06:10:40 +0900

    feat : xml response 기능 추가

--- src/main/java/team/marco/voucher_management_system/web_app/controller/VoucherController.java
+++ src/main/java/team/marco/voucher_management_system/web_app/controller/VoucherController.java
@@ -5,20 +5,22 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import team.marco.voucher_management_system.model.Voucher;
 import team.marco.voucher_management_system.service.VoucherService;
 import team.marco.voucher_management_system.type_enum.VoucherType;
 import team.marco.voucher_management_system.web_app.dto.CreateVoucherRequest;
 
-@Controller
+@RestController
+@RequestMapping(produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
 public class VoucherController {
     private static final String DATE_PATTERN = "yyyy-MM-dd HH:mm:ss";
 
@@ -29,13 +31,11 @@ public class VoucherController {
     }
 
     @GetMapping("/vouchers")
-    @ResponseBody
     public List<Voucher> findAll() {
         return voucherService.getVouchers();
     }
 
     @PostMapping("/vouchers")
-    @ResponseBody
     public ResponseEntity<Voucher> create(CreateVoucherRequest createVoucherRequest) {
         Voucher voucher = voucherService.create(createVoucherRequest);
 
@@ -43,7 +43,6 @@ public class VoucherController {
     }
 
     @GetMapping("/vouchers/{id}")
-    @ResponseBody
     public ResponseEntity<Voucher> findById(@PathVariable("id") UUID id) {
         Optional<Voucher> optionalVoucher = voucherService.findById(id);
 
@@ -53,7 +52,6 @@ public class VoucherController {
     }
 
     @DeleteMapping("/vouchers/{id}")
-    @ResponseBody
     public ResponseEntity<Void> delete(@PathVariable("id") UUID id) {
         boolean isDeleted = voucherService.deleteById(id);
 
@@ -67,7 +65,6 @@ public class VoucherController {
     }
 
     @GetMapping("/vouchers/createdAt")
-    @ResponseBody
     public List<Voucher> findByCreateAt(@RequestParam("from")
                                         @DateTimeFormat(pattern = DATE_PATTERN)
                                         LocalDateTime from,
@@ -78,7 +75,6 @@ public class VoucherController {
     }
 
     @GetMapping("/vouchers/type/{type}")
-    @ResponseBody
     public List<Voucher> findByType(@PathVariable("type") VoucherType voucherType) {
         return voucherService.findByType(voucherType);
     }
