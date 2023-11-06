c3ced3fca399e714efe90681194cd70cc4fc6a72
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2023-11-06 18:12:49 +0900

    refactor: make code shorter

--- src/main/java/com/programmers/vouchermanagement/configuration/JdbcConfig.java
+++ src/main/java/com/programmers/vouchermanagement/configuration/JdbcConfig.java
@@ -4,7 +4,6 @@ import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
 import javax.sql.DataSource;
@@ -15,7 +14,6 @@ import javax.sql.DataSource;
 public class JdbcConfig {
     @Bean
     public NamedParameterJdbcTemplate namedParameterJdbcTemplate(DataSource dataSource) {
-        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
-        return new NamedParameterJdbcTemplate(jdbcTemplate);
+        return new NamedParameterJdbcTemplate(dataSource);
     }
 }
