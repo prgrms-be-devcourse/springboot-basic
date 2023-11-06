dbeddfa6519c9de017f54a3a9ad74269e956d5fa
ASak1104 <khw56184@gmail.com>
2023-11-06 18:19:48 +0900

    logging : repository pointcut 변경

--- src/main/java/team/marco/voucher_management_system/aspect/LoggingPointCut.java
+++ src/main/java/team/marco/voucher_management_system/aspect/LoggingPointCut.java
@@ -14,7 +14,7 @@ public final class LoggingPointCut {
     public static void serviceMethodPointcut() {
     }
 
-    @Pointcut("execution(* team.marco.voucher_management_system.repository..*(..))")
+    @Pointcut("execution(* team.marco.voucher_management_system..repository..*(..))")
     public static void repositorySavePointcut() {
     }
 }
