57ad5c6df3141fe807a3f1c919786ff1ebbfc16d
park0jae <okvv26@naver.com>
2023-11-06 13:43:09 +0900

    feat : ResponseEntityExceptionHandler 적용

--- src/main/java/com/zerozae/voucher/common/advisor/ApiExceptionAdvisor.java
+++ src/main/java/com/zerozae/voucher/common/advisor/ApiExceptionAdvisor.java
@@ -2,33 +2,40 @@ package com.zerozae.voucher.common.advisor;
 
 import com.zerozae.voucher.common.response.ErrorResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.Objects;
 
-@Slf4j
-@RestControllerAdvice(annotations = RestController.class)
-public class ApiExceptionAdvisor {
+import static org.springframework.http.HttpStatus.*;
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+@Slf4j
+@ControllerAdvice(annotations = RestController.class)
+public class ApiExceptionAdvisor extends ResponseEntityExceptionHandler {
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
         BindingResult bindingResult = ex.getBindingResult();
         String errorMessage = Objects.requireNonNull(bindingResult.getFieldError()).getDefaultMessage();
-        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST.value(), errorMessage);
+        ErrorResponse errorResponse = new ErrorResponse(status.value(), errorMessage);
         log.warn("Error Message = {}", errorMessage);
-        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(errorResponse, headers, status);
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ErrorResponse> exceptionMessage(Exception e){
-        log.error("Error Message ={}", e.getMessage());
-        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(), e.getMessage());
-        return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    protected ResponseEntity<ErrorResponse> handleGlobalException(Exception e) {
+        log.error("Error Message = {}", e.getMessage());
+        ErrorResponse errorResponse = new ErrorResponse(INTERNAL_SERVER_ERROR.value(), e.getMessage());
+        return new ResponseEntity<>(errorResponse, INTERNAL_SERVER_ERROR);
     }
 }
