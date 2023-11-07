3b77085dae00f3e36bc81c823f7170a763f146b3
ASak1104 <khw56184@gmail.com>
2023-11-07 10:25:30 +0900

    feat : 고객 view 기능 추가

--- src/main/java/team/marco/voucher_management_system/web_app/controller/CustomerController.java
+++ src/main/java/team/marco/voucher_management_system/web_app/controller/CustomerController.java
@@ -1,10 +1,17 @@
 package team.marco.voucher_management_system.web_app.controller;
 
+import java.util.List;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import team.marco.voucher_management_system.model.Customer;
 import team.marco.voucher_management_system.service.CustomerService;
+import team.marco.voucher_management_system.web_app.dto.CreateCustomerRequest;
 
 @Controller
+@RequestMapping(path = "/customers")
 public class CustomerController {
     private final CustomerService customerService;
 
@@ -12,8 +19,24 @@ public class CustomerController {
         this.customerService = customerService;
     }
 
-    @GetMapping("/customers")
-    public String viewCustomersPage() {
+    @GetMapping
+    public String viewCustomers(Model model) {
+        List<Customer> customers = customerService.getCustomers();
+
+        model.addAttribute("customers", customers);
+
         return "views/customers";
+    }
+
+    @GetMapping("/new")
+    public String viewNewCustomer() {
+        return "views/new-customer";
+    }
+
+    @PostMapping("/new")
+    public String createCustomer(CreateCustomerRequest createCustomerRequest) {
+        customerService.create(createCustomerRequest.name(), createCustomerRequest.email());
+
+        return "redirect:/customers";
     }
 }
---
+++ src/main/java/team/marco/voucher_management_system/web_app/dto/CreateCustomerRequest.java
@@ -0,0 +1,6 @@
+package team.marco.voucher_management_system.web_app.dto;
+
+public record CreateCustomerRequest(
+        String name,
+        String email) {
+}
