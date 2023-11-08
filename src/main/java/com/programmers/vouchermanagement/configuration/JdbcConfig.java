0be97c64268c973689b5c950c62d64145029079a
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2023-11-09 02:52:12 +0900

    refactor: exclude autoconfigure datasource when file profile active

--- src/main/java/com/programmers/vouchermanagement/configuration/JdbcConfig.java
+++ src/main/java/com/programmers/vouchermanagement/configuration/JdbcConfig.java
@@ -1,6 +1,5 @@
 package com.programmers.vouchermanagement.configuration;
 
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
