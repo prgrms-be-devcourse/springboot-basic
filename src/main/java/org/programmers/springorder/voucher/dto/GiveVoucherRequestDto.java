b46dce4186301c322c6b35410d139ab21e1db98a
shoeone96 <dlwnddnjs96@gmail.com>
2023-11-09 11:16:14 +0900

    chore: naming convention 지키기

--- src/main/java/org/programmers/springorder/voucher/dto/GiveVoucherRequestDto.java
+++ src/main/java/org/programmers/springorder/voucher/dto/GiveVoucherRequestDto.java
@@ -3,16 +3,16 @@ package org.programmers.springorder.voucher.dto;
 import java.util.UUID;
 
 public class GiveVoucherRequestDto {
-    private final UUID VoucherId;
+    private final UUID voucherId;
 
     private final UUID customerId;
     public GiveVoucherRequestDto(UUID voucherId, UUID customerId) {
-        VoucherId = voucherId;
+        this.voucherId = voucherId;
         this.customerId = customerId;
     }
 
     public UUID getVoucherId() {
-        return VoucherId;
+        return voucherId;
     }
 
     public UUID getCustomerId() {
