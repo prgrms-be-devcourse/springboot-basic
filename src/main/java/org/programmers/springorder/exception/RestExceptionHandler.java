ecdea71840496d36dfcb1adc3dd738c5287a11b1
shoeone96 <dlwnddnjs96@gmail.com>
2023-11-09 11:17:42 +0900

    feat: voucher 등록 시 타입이 잘못 기록된 경우 처리해줄 수 있는 예외 처리

--- src/main/java/org/programmers/springorder/exception/RestExceptionHandler.java
+++ src/main/java/org/programmers/springorder/exception/RestExceptionHandler.java
@@ -1,8 +1,11 @@
 package org.programmers.springorder.exception;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.programmers.springorder.voucher.dto.Response;
 import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -22,5 +25,11 @@ public class RestExceptionHandler {
     public ResponseEntity<?> applicationHandler(RuntimeException e){
         return ResponseEntity.status(ErrorCode.SERVER_ERROR.getHttpStatus())
                 .body(Response.error(ErrorCode.SERVER_ERROR.getMessage()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> methodValidException(MethodArgumentNotValidException e, HttpServletRequest request){
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(ErrorCode.INVALID_INPUT.getMessage());
     }
 }
