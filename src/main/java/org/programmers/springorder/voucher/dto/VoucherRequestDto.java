efe9095e1dd4f71fb7f9a31e3798b94aa959c10b
shoeone96 <dlwnddnjs96@gmail.com>
2023-11-09 11:16:44 +0900

    feat: voucher 등록 시 필요한 validation 작성

--- src/main/java/org/programmers/springorder/voucher/dto/VoucherRequestDto.java
+++ src/main/java/org/programmers/springorder/voucher/dto/VoucherRequestDto.java
@@ -1,6 +1,25 @@
 package org.programmers.springorder.voucher.dto;
 
+import org.programmers.springorder.exception.ErrorCode;
+import org.programmers.springorder.exception.VoucherException;
 import org.programmers.springorder.voucher.model.VoucherType;
+import org.springframework.lang.NonNull;
 
-public record VoucherRequestDto(long discountValue, VoucherType voucherType) {
+public record VoucherRequestDto(
+        @NonNull long discountValue,
+        @NonNull VoucherType voucherType) {
+    public VoucherRequestDto(long discountValue, VoucherType voucherType) {
+        validatingVoucherInfo(discountValue, voucherType);
+        this.discountValue = discountValue;
+        this.voucherType = voucherType;
+    }
+
+    private static void validatingVoucherInfo(long discountValue, VoucherType voucherType) {
+        if (voucherType == VoucherType.FIXED && (discountValue < 100 || discountValue > 10000)) {
+            throw new VoucherException(ErrorCode.INVALID_DISCOUNT_VALUE);
+        }
+        if (voucherType == VoucherType.PERCENT && (discountValue < 3 || discountValue > 50)) {
+            throw new VoucherException(ErrorCode.INVALID_DISCOUNT_PERCENT);
+        }
+    }
 }
