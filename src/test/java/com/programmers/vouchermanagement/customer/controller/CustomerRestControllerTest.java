ea48f3ff104d65aae2ee7a5fd2fd8bf744d82565
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2023-11-09 16:04:01 +0900

    refactor: apply param method at CustomerRestControllerTest

--- src/test/java/com/programmers/vouchermanagement/customer/controller/CustomerRestControllerTest.java
+++ src/test/java/com/programmers/vouchermanagement/customer/controller/CustomerRestControllerTest.java
@@ -1,11 +1,6 @@
 package com.programmers.vouchermanagement.customer.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.programmers.vouchermanagement.customer.controller.dto.CreateCustomerRequest;
-import com.programmers.vouchermanagement.customer.controller.dto.CustomerResponse;
-import com.programmers.vouchermanagement.customer.domain.Customer;
 import com.programmers.vouchermanagement.customer.service.CustomerService;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,8 +10,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.util.List;
-
+import static com.programmers.vouchermanagement.customer.controller.MvcControllerResource.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -26,56 +20,31 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @WebMvcTest(CustomerRestController.class)
 @ActiveProfiles("api")
 class CustomerRestControllerTest {
-    static ObjectMapper objectMapper;
-    static List<CustomerResponse> customers;
-    static List<CustomerResponse> blacklist;
-    static Customer customer;
-    static CreateCustomerRequest createCustomerRequest;
-    static CustomerResponse customerResponse;
-
     @Autowired
     MockMvc mockMvc;
     @MockBean
     CustomerService customerService;
 
-    @BeforeAll
-    static void init() {
-        objectMapper = new ObjectMapper();
-
-        customers = List.of(
-                CustomerResponse.from(new Customer("customer1", false)),
-                CustomerResponse.from(new Customer("customer2", true))
-        );
-        blacklist = List.of(
-                CustomerResponse.from(new Customer("customer3", true)),
-                CustomerResponse.from(new Customer("customer4", true))
-        );
-
-        createCustomerRequest = new CreateCustomerRequest("customer5", true);
-        customer = new Customer(createCustomerRequest.name(), createCustomerRequest.isBlack());
-        customerResponse = CustomerResponse.from(customer);
-    }
-
     @Test
     @DisplayName("고객 생성을 요청한다.")
     void createCustomer() throws Exception {
-        when(customerService.create(createCustomerRequest)).thenReturn(customerResponse);
+        when(customerService.create(CREATE_CUSTOMER_REQUEST)).thenReturn(CUSTOMER_RESPONSE);
 
         String response = mockMvc.perform(post("/api/v1/customers")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(createCustomerRequest)))
+                        .content(OBJECT_MAPPER.writeValueAsString(CREATE_CUSTOMER_REQUEST)))
                 .andExpect(status().isCreated())
                 .andReturn()
                 .getResponse()
                 .getContentAsString();
 
-        assertThat(response).isEqualTo(objectMapper.writeValueAsString(customerResponse));
+        assertThat(response).isEqualTo(OBJECT_MAPPER.writeValueAsString(CUSTOMER_RESPONSE));
     }
 
     @Test
     @DisplayName("모든 고객 조회를 요청한다.")
     void readAllCustomers1() throws Exception {
-        when(customerService.readAll()).thenReturn(customers);
+        when(customerService.readAll()).thenReturn(CUSTOMERS);
 
         String response = mockMvc.perform(get("/api/v1/customers"))
                 .andExpect(status().isOk())
@@ -83,34 +52,36 @@ class CustomerRestControllerTest {
                 .getResponse()
                 .getContentAsString();
 
-        assertThat(response).isEqualTo(objectMapper.writeValueAsString(customers));
+        assertThat(response).isEqualTo(OBJECT_MAPPER.writeValueAsString(CUSTOMERS));
     }
 
     @Test
     @DisplayName("모든 고객 조회를 요청한다. + 쿼리 스트링 사용(type=all)")
     void readAllCustomers2() throws Exception {
-        when(customerService.readAll()).thenReturn(customers);
+        when(customerService.readAll()).thenReturn(CUSTOMERS);
 
-        String response = mockMvc.perform(get("/api/v1/customers?type=all"))
+        String response = mockMvc.perform(get("/api/v1/customers")
+                        .param("type", "all"))
                 .andExpect(status().isOk())
                 .andReturn()
                 .getResponse()
                 .getContentAsString();
 
-        assertThat(response).isEqualTo(objectMapper.writeValueAsString(customers));
+        assertThat(response).isEqualTo(OBJECT_MAPPER.writeValueAsString(CUSTOMERS));
     }
 
     @Test
     @DisplayName("모든 블랙리스트 고객 조회를 요청한다.")
     void readAllBlacklist() throws Exception {
-        when(customerService.readAllBlackCustomer()).thenReturn(blacklist);
+        when(customerService.readAllBlackCustomer()).thenReturn(BLACKLIST);
 
-        String response = mockMvc.perform(get("/api/v1/customers?type=blacklist"))
+        String response = mockMvc.perform(get("/api/v1/customers")
+                        .param("type","blacklist"))
                 .andExpect(status().isOk())
                 .andReturn()
                 .getResponse()
                 .getContentAsString();
 
-        assertThat(response).isEqualTo(objectMapper.writeValueAsString(blacklist));
+        assertThat(response).isEqualTo(OBJECT_MAPPER.writeValueAsString(BLACKLIST));
     }
 }
