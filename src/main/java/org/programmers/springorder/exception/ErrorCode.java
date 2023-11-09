a33518426fa4486ede7dd2dec4d5a7a9e2026793
shoeone96 <dlwnddnjs96@gmail.com>
2023-11-09 11:17:17 +0900

    feat: voucher 등록 시 발생할 수 있는 enum 작성

--- src/main/java/org/programmers/springorder/exception/ErrorCode.java
+++ src/main/java/org/programmers/springorder/exception/ErrorCode.java
@@ -6,8 +6,11 @@ public enum ErrorCode {
     SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 장애로 인해 잠시 문제가 발생하였습니다." ),
     VOUCHER_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 바우처를 찾을 수 없습니다."),
     CUSTOMER_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 고객을 찾을 수 없습니다."),
-    VOUCHER_OWNER_NOT_EXIST(HttpStatus.NOT_FOUND, "해당 바우처를 소유한 고객이 없습니다.");
+    VOUCHER_OWNER_NOT_EXIST(HttpStatus.NOT_FOUND, "해당 바우처를 소유한 고객이 없습니다."),
+    INVALID_DISCOUNT_VALUE(HttpStatus.BAD_REQUEST, "잘못된 고정 할인 금액입니다."),
+    INVALID_DISCOUNT_PERCENT(HttpStatus.BAD_REQUEST, "잘못된 할인율입니다."),
 
+    INVALID_INPUT(HttpStatus.BAD_REQUEST, "잘못된 입력입니다.") ;
     private final HttpStatus httpStatus;
     private final String message;
 
